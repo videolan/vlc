7929ae1b9befc2f3f53b8907cb96315f0a1573f8
Christophe Massiot <massiot@videolan.org>
2000-01-10 23:29:41 +0000

    Heureusement que j'avais dit de faire gaffe a ne pas committer le mauvais video_decoder.h :-ppppp

--- include/video_decoder.h
+++ include/video_decoder.h
@@ -1,8 +1,8 @@
-/*****************************************************************************
+/*******************************************************************************
  * video_decoder.h : video decoder thread
  * (c)1999 VideoLAN
- *****************************************************************************
- *****************************************************************************
+ *******************************************************************************
+ *******************************************************************************
  * Requires:
  *  "config.h"
  *  "common.h"
@@ -12,21 +12,21 @@
  *  "video.h"
  *  "video_output.h"
  *  "decoder_fifo.h"
- *****************************************************************************/
+ *******************************************************************************/
 
-/*****************************************************************************
+/*******************************************************************************
  * vdec_thread_t: video decoder thread descriptor
- *****************************************************************************
+ *******************************************************************************
  * ??
- *****************************************************************************/
+ *******************************************************************************/
 typedef struct vdec_thread_s
 {
     /* Thread properties and locks */
-    boolean_t           b_die;                                 /* `die' flag */
-    boolean_t           b_run;                                 /* `run' flag */
-    boolean_t           b_error;                             /* `error' flag */
-    boolean_t           b_active;                           /* `active' flag */
-    vlc_thread_t        thread_id;                /* id for thread functions */
+    boolean_t           b_die;                                   /* `die' flag */
+    boolean_t           b_run;                                   /* `run' flag */
+    boolean_t           b_error;                               /* `error' flag */
+    boolean_t           b_active;                             /* `active' flag */
+    vlc_thread_t        thread_id;                  /* id for thread functions */
 
     /* Thread configuration */
     /* ?? */
@@ -35,27 +35,39 @@ typedef struct vdec_thread_s
     
 
     /* Input properties */
-    video_parser_t *  	p_vpar;                       /* video_parser thread */
-    short int           p_pre_idct[64*64];        /* initialization of sparse idct routines */
+    decoder_fifo_t      fifo;                                /* PES input fifo */
 
+    /* The bit stream structure handles the PES stream at the bit level */
+    bit_stream_t        bit_stream;
+
+    /* Output properties */
+    vout_thread_t *     p_vout;                         /* video output thread */
+    int                 i_stream;                           /* video stream id */
+    
+        
 #ifdef STATS
     /* Statistics */
-    count_t         c_loops;                              /* number of loops */
-    count_t         c_idle_loops;                    /* number of idle loops */
-    count_t         c_decoded_pictures;        /* number of pictures decoded */
-    count_t         c_decoded_i_pictures;    /* number of I pictures decoded */
-    count_t         c_decoded_p_pictures;    /* number of P pictures decoded */
-    count_t         c_decoded_b_pictures;    /* number of B pictures decoded */
+    count_t         c_loops;                               /* number of loops */
+    count_t         c_idle_loops;                     /* number of idle loops */
+    count_t         c_pictures;                    /* number of pictures read */
+    count_t         c_i_pictures;                /* number of I pictures read */
+    count_t         c_p_pictures;                /* number of P pictures read */
+    count_t         c_b_pictures;                /* number of B pictures read */    
+    count_t         c_decoded_pictures;         /* number of pictures decoded */
+    count_t         c_decoded_i_pictures;     /* number of I pictures decoded */
+    count_t         c_decoded_p_pictures;     /* number of P pictures decoded */
+    count_t         c_decoded_b_pictures;     /* number of B pictures decoded */
 #endif
 } vdec_thread_t;
 
-/*****************************************************************************
+/*******************************************************************************
  * Prototypes
- *****************************************************************************/
+ *******************************************************************************/
 
 /* Thread management functions */
-vdec_thread_t * vdec_CreateThread       ( vpar_thread_t *p_vpar /*, int *pi_status */ );
-void            vdec_DestroyThread      ( vdec_thread_t *p_vdec /*, int *pi_status */ );
+vdec_thread_t * vdec_CreateThread       ( /* video_cfg_t *p_cfg, */ input_thread_t *p_input /*,
+                                          vout_thread_t *p_vout, int *pi_status */ );
+void             vdec_DestroyThread      ( vdec_thread_t *p_vdec /*, int *pi_status */ );
 
 /* Time management functions */
 /* ?? */
