f5c814d6c11d6eef74271ae48193a0c43c6e296f
Sam Hocevar <sam@videolan.org>
2002-02-24 22:12:45 +0000

    * Replacement header for aout_common.h.

---
+++ src/audio_output/aout_spdif.h
@@ -0,0 +1,26 @@
+/*****************************************************************************
+ * aout_spdif.h: AC3 passthrough output
+ *****************************************************************************
+ * Copyright (C) 2001 VideoLAN
+ * $Id: aout_spdif.h,v 1.1 2002/02/24 22:12:45 sam Exp $
+ *
+ * Authors: Michel Kaempf <maxx@via.ecp.fr>
+ *          Stéphane Borel <stef@via.ecp.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
+ *****************************************************************************/
+
+void aout_SpdifThread ( aout_thread_t * p_aout );
+
