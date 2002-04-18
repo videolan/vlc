46b2dec0ff08906d35d2e903dd100643b50f2fdc
Christophe Massiot <massiot@videolan.org>
2002-04-18 21:32:23 +0000

    Fixed typos.

--- plugins/macosx/vout_macosx.c
+++ plugins/macosx/vout_macosx.c
@@ -518,33 +518,33 @@ static void QTScaleMatrix( vout_thread_t *p_vout )
 
     GetPortBounds( p_vout->p_sys->p_qdport, &s_rect );
 
-    if( (s_rect.bottom - s_rect.top) * p_vout->render.i_aspect
+    if( (s_rect.bottom - s_rect.top) * p_vout->output.i_aspect
         < (s_rect.right - s_rect.left) * VOUT_ASPECT_FACTOR )
     {
         factor_x = FixDiv( Long2Fix( (s_rect.bottom - s_rect.top)
-                            * p_vout->render.i_aspect / VOUT_ASPECT_FACTOR ),
-                           Long2Fix( p_vout->render.i_width ) );
+                            * p_vout->output.i_aspect / VOUT_ASPECT_FACTOR ),
+                           Long2Fix( p_vout->output.i_width ) );
         factor_y = FixDiv( Long2Fix( s_rect.bottom - s_rect.top ),
-                           Long2Fix( p_vout->render.i_height ) );
+                           Long2Fix( p_vout->output.i_height ) );
 #if 0
         p_vout->p_sys->display_rect.top = 0;
         p_vout->p_sys->display_rect.bottom = s_rect.bottom - s_rect.top;
         p_vout->p_sys->display_rect.left = 12;
         p_vout->p_sys->display_rect.right = 12 + (s_rect.bottom - s_rect.top)
-                            * p_vout->render.i_aspect / VOUT_ASPECT_FACTOR;
+                            * p_vout->output.i_aspect / VOUT_ASPECT_FACTOR;
 #endif
     }
     else
     {
         factor_x = FixDiv( Long2Fix( s_rect.right - s_rect.left ),
-                           Long2Fix( p_vout->render.i_width ) );
+                           Long2Fix( p_vout->output.i_width ) );
         factor_y = FixDiv( Long2Fix( (s_rect.right - s_rect.left)
-                            * VOUT_ASPECT_FACTOR / p_vout->render.i_aspect ),
-                           Long2Fix( p_vout->render.i_height ) );
+                            * VOUT_ASPECT_FACTOR / p_vout->output.i_aspect ),
+                           Long2Fix( p_vout->output.i_height ) );
 #if 0
         p_vout->p_sys->display_rect.top = 12;
         p_vout->p_sys->display_rect.bottom = 12 + (s_rect.right - s_rect.left)
-                            * VOUT_ASPECT_FACTOR / p_vout->render.i_aspect;
+                            * VOUT_ASPECT_FACTOR / p_vout->output.i_aspect;
         p_vout->p_sys->display_rect.left = 0;
         p_vout->p_sys->display_rect.right = s_rect.right - s_rect.left;
 #endif
@@ -581,9 +581,7 @@ static int QTCreateSequence( vout_thread_t *p_vout )
     p_descr->spatialQuality = codecLosslessQuality;
     p_descr->frameCount = 1;
     p_descr->clutID = -1;
-
-    p_descr->dataSize = p_vout->output.i_width *
-                        p_vout->output.i_height * 3 / 2;
+    p_descr->dataSize = 0;
     p_descr->depth = 12;
 
     HUnlock( (Handle)p_vout->p_sys->h_img_descr );
