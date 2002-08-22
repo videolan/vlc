3c2a9131dd4b04e91dd712faa790b16f61c55e1e
Sam Hocevar <sam@videolan.org>
2002-08-22 21:41:23 +0000

    Forgot one file in my previous commit.

--- modules/codec/mad/libmad.h
+++ modules/codec/mad/libmad.h
@@ -31,20 +31,16 @@
  */
 
 /* enum mad_flow (*input_func)(void *, struct mad_stream *);*/
-enum mad_flow libmad_input(void *data, struct mad_stream *p_libmad_stream);
+enum mad_flow libmad_input(void *, struct mad_stream *);
 
 /* enum mad_flow (*header_func)(void *, struct mad_header const *);*/
-enum mad_flow libmad_header(void *data, struct mad_header const *p_libmad_header);
-
-/* enum mad_flow (*filter_func)(void *, struct mad_stream const *, struct mad_frame *); */
-// enum mad_flow libmad_filter(void *data, struct mad_stream const *p_libmad_stream, struct mad_frame *p_libmad_frame);
+enum mad_flow libmad_header(void *, struct mad_header const *);
 
 /* enum mad_flow (*output_func)(void *, struct mad_header const *, struct mad_pcm *); */
-//enum mad_flow libmad_output(void *data, struct mad_header const *p_libmad_header, struct mad_pcm *p_libmad_pcm);
-enum mad_flow libmad_output3(void *data, struct mad_header const *p_libmad_header, struct mad_pcm *p_libmad_pcm);
+enum mad_flow libmad_output(void *, struct mad_header const *, struct mad_pcm *);
 
 /* enum mad_flow (*error_func)(void *, struct mad_stream *, struct mad_frame *); */
-enum mad_flow libmad_error(void *data, struct mad_stream *p_libmad_stream, struct mad_frame *p_libmad_frame);
+enum mad_flow libmad_error(void *, struct mad_stream *, struct mad_frame *);
 
 /* enum mad_flow (*message_func)(void *, void *, unsigned int *); */
 /* enum mad_flow libmad_message(void *, void*, unsigned int*); */
