c920c5cb35e6bd410e01539fa3d413297e924db6
Jean-Paul Saman <jpsaman@videolan.org>
2002-11-26 19:54:12 +0000

    Fix namespace clash of source files for Qte video output module. It currently only display video in landscape orientation.

--- plugins/qte/qte.c
+++ plugins/qte/qte.c
@@ -2,7 +2,7 @@
  * qte.c : Qt Embedded video output
  *****************************************************************************
  * Copyright (C) 2002 VideoLAN
- * $Id: qte.c,v 1.1.2.1 2002/09/30 20:32:46 jpsaman Exp $
+ * $Id: qte.c,v 1.1.2.2 2002/11/26 19:54:12 jpsaman Exp $
  *
  * Authors: Gerald Hansink <gerald.hansink@ordina.nl>
  *          Jean-Paul Saman <jpsaman@wxs.nl>
@@ -25,28 +25,38 @@
 /*****************************************************************************
  * Preamble
  *****************************************************************************/
-#include <errno.h>                                                 /* ENOMEM */
-#include <stdlib.h>                                                /* free() */
-#include <string.h>                                                /* strerror() */
+#include <errno.h>                                             /* ENOMEM */
+#include <stdlib.h>                                            /* free() */
+#include <string.h>                                            /* strerror() */
 
 #include <videolan/vlc.h>
+#include "video.h"
+#include "video_output.h"
+
+#define WINDOW_TEXT N_("Qte video output size (video,fullscreen)")
+#define WINDOW_LONGTEXT N_("Specify size output of the window to use.\nBy default the " \
+     "video mode will be used ")
+#define DISPLAY_TEXT N_("Qte video output display orientatione (portrait,landscape)")
+#define DISPLAY_LONGTEXT N_( \
+    "Specify the orientation for output display to use.\nBy default Qte video output will " \
+    "use the portrait orientation.")
 
 void _M( vout_getfunctions )( function_list_t * p_function_list );
 
 MODULE_CONFIG_START
-ADD_CATEGORY_HINT( N_("Miscellaneous"), NULL )
-ADD_STRING  ( "qte-display", NULL, NULL, NULL, NULL )
-ADD_BOOL    ( "qte-altfullscreen", NULL, NULL, NULL, NULL )
+  ADD_CATEGORY_HINT( N_("Miscellaneous"), NULL )
+  ADD_STRING  ( N_("qte-display"), "portrait", NULL, DISPLAY_TEXT, DISPLAY_LONGTEXT )
+  ADD_STRING  ( N_("qte-window"), "video", NULL, WINDOW_TEXT, WINDOW_LONGTEXT )
 MODULE_CONFIG_STOP
 
 MODULE_INIT_START
-    SET_DESCRIPTION( _("Qt Embedded video output module") )
-    ADD_CAPABILITY( VOUT, 150 )
-    ADD_SHORTCUT( "qtevlc" )
+  SET_DESCRIPTION( _("Qt Embedded video output module") )
+  ADD_CAPABILITY( VOUT, 150 )
+  ADD_SHORTCUT( "qte-vlc" )
 MODULE_INIT_STOP
 
 MODULE_ACTIVATE_START
-    _M( vout_getfunctions )( &p_module->p_functions->vout );
+  _M( vout_getfunctions )( &p_module->p_functions->vout );
 MODULE_ACTIVATE_STOP
 
 MODULE_DEACTIVATE_START
