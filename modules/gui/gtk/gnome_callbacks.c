a6987dc0bca414bd138c8f4923ff5225117bd821
Laurent Aimar <fenrir@videolan.org>
2003-01-26 14:52:12 +0000

    * all: raah, clean up glade output.

--- modules/gui/gtk/gnome_callbacks.c
+++ modules/gui/gtk/gnome_callbacks.c
@@ -1,20 +1,3 @@
 /*****************************************************************************
  * This file is not used: everything is in gtk_callbacks.c
  *****************************************************************************/
-
-
-void
-GtkOpenSoutShow                        (GtkButton       *button,
-                                        gpointer         user_data)
-{
-
-}
-
-
-void
-GtkSoutSettings                        (GtkButton       *button,
-                                        gpointer         user_data)
-{
-
-}
-
--- modules/gui/gtk/gnome_callbacks.h
+++ modules/gui/gtk/gnome_callbacks.h
@@ -2,13 +2,3 @@
  * This file is not needed: everything is in gtk_callbacks.h
  *****************************************************************************/
 #include "gtk_callbacks.h"
-
-
-
-void
-GtkOpenSoutShow                        (GtkButton       *button,
-                                        gpointer         user_data);
-
-void
-GtkSoutSettings                        (GtkButton       *button,
-                                        gpointer         user_data);
