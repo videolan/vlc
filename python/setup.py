9f6362609025b1895c2a3172a12bd5439cd2645c
Cyril Deguet <asmax@videolan.org>
2003-04-12 13:06:55 +0000

    * added libraries to please Bill ;)

--- python/setup.py
+++ python/setup.py
@@ -4,9 +4,14 @@ FFMPEG_DIR = '/home/cyril/ffmpeg'
 
 vlc = Extension('vlc',
                 sources = ['vlcmodule.c'],
-                libraries = ['vlc', 'rt', 'dl' , 'pthread', 'ffmpeg', 'm', 
-                             'avcodec'],
-                library_dirs = ['../lib', '../modules/codec/ffmpeg', 
+                libraries = ['vlc', 'rt', 'dl' , 'pthread', 'ffmpeg', 'm',
+                             'avcodec','mpeg_video','idct','idctclassic',
+                             'motion','memcpymmx','idctmmx','motionmmx',
+                             'i420_rgb_mmx','i420_yuy2_mmx','i420_ymga_mmx',
+                             'i422_yuy2_mmx','memcpymmxext','idctmmxext',
+                             'motionmmxext','memcpy3dn'],
+                library_dirs = ['/usr/local/lib/vlc', '../lib', 
+'../modules/codec/ffmpeg',
                                 FFMPEG_DIR + '/libavcodec'])
 
 
