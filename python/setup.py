2c2871fb1fa755ae1d8dd4603546857335e14614
Cyril Deguet <asmax@videolan.org>
2003-05-22 18:30:33 +0000

    * started to repair build of python module

--- python/setup.py
+++ python/setup.py
@@ -5,14 +5,14 @@ FFMPEG_DIR = '/home/cyril/ffmpeg'
 vlc = Extension('vlc',
                 sources = ['vlcmodule.c'],
                 libraries = ['vlc', 'rt', 'dl' , 'pthread', 'ffmpeg', 'm',
-                             'avcodec','mpeg_video','idct','idctclassic',
-                             'motion','memcpymmx','idctmmx','motionmmx',
+                             'avcodec','memcpymmx','stream_out_transcode',
                              'i420_rgb_mmx','i420_yuy2_mmx','i420_ymga_mmx',
-                             'i422_yuy2_mmx','memcpymmxext','idctmmxext',
-                             'motionmmxext','memcpy3dn'],
-                library_dirs = ['/usr/local/lib/vlc', '../lib', 
-'../modules/codec/ffmpeg',
-                                FFMPEG_DIR + '/libavcodec'])
+                             'i422_yuy2_mmx','memcpymmxext','memcpy3dn',
+                             'encoder_ffmpeg'],
+                library_dirs = [ '../lib', 
+                                '../modules/stream_out', '../modules/encoder/ffmpeg',
+                                '../modules/misc/memcpy','../modules/video_chroma',
+                                '../modules/codec/ffmpeg', FFMPEG_DIR + '/libavcodec'])
 
 
 setup (name = 'PackageName',
