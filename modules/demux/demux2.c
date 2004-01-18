8b56b5fe22a1ed1d38da2253ef066af4bf6af963
Gildas Bazin <gbazin@videolan.org>
2004-01-18 11:14:23 +0000

    * configure.ac, modules/demux/Modules.am: added ps2, ts2 and dvdnav modules to the build system. * modules/demux/demux2.c: added an "ffmpeg" shortcut for the ffmpeg/libavformat demuxer.

--- modules/demux/demux2.c
+++ modules/demux/demux2.c
@@ -2,7 +2,7 @@
  * demux2 adaptation layer.
  *****************************************************************************
  * Copyright (C) 2004 VideoLAN
- * $Id: demux2.c,v 1.4 2004/01/18 02:21:38 fenrir Exp $
+ * $Id: demux2.c,v 1.5 2004/01/18 11:14:23 gbazin Exp $
  *
  * Authors: Laurent Aimar <fenrir@via.ecp.fr>
  *
@@ -48,6 +48,7 @@ vlc_module_begin();
     add_shortcut( "ts2" );
     add_shortcut( "ps2" );
     add_shortcut( "dvdnav" );
+    add_shortcut( "ffmpeg" );
 vlc_module_end();
 
 /*****************************************************************************
