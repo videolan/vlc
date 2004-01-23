84fb943b21da165ede64f3dad5901494ce195489
Rocky Bernstein <rocky@videolan.org>
2004-01-23 08:36:48 +0000

    Omitted option -duration-scaling which caused plugin to stop working.

--- modules/codec/ogt/cvd.c
+++ modules/codec/ogt/cvd.c
@@ -1,8 +1,8 @@
 /*****************************************************************************
  * cvd.c : CVD Subtitle decoder thread
  *****************************************************************************
- * Copyright (C) 2003 VideoLAN
- * $Id: cvd.c,v 1.12 2004/01/16 13:32:37 rocky Exp $
+ * Copyright (C) 2003, 2004 VideoLAN
+ * $Id: cvd.c,v 1.13 2004/01/23 08:36:48 rocky Exp $
  *
  * Authors: Rocky Bernstein
  *   based on code from:
@@ -60,6 +60,10 @@ vlc_module_begin();
     add_string( MODULE_STRING "-aspect-ratio", "", NULL,
                 SUB_ASPECT_RATIO_TEXT, SUB_ASPECT_RATIO_LONGTEXT, 
 		VLC_TRUE );
+
+    add_integer( MODULE_STRING "-duration-scaling", 9, NULL,
+		 DURATION_SCALE_TEXT, DURATION_SCALE_LONGTEXT,
+		 VLC_TRUE );
 
     add_submodule();
     set_description( _("Chaoji VCD subtitle packetizer") );
