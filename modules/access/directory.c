aa7904dacb53dba503e05e95bac9e141e8c3df77
Clément Stenac <zorglub@videolan.org>
2004-02-16 17:16:24 +0000

    Added an option "recursive" 	- none: subdirectories are skipped 	- collapse: former behaviour, subdirectories are not expanded 	- expand: all subdirectories are fully expanded
    I dislike these strings, please find other ones :)

--- modules/access/directory.c
+++ modules/access/directory.c
@@ -2,7 +2,7 @@
  * directory.c: expands a directory (directory: access plug-in)
  *****************************************************************************
  * Copyright (C) 2001, 2002 VideoLAN
- * $Id: directory.c,v 1.4 2003/03/30 18:14:35 gbazin Exp $
+ * $Id: directory.c,v 1.5 2004/02/16 17:16:24 zorglub Exp $
  *
  * Authors: Derk-Jan Hartman <thedj@users.sourceforge.net>
  *
@@ -10,7 +10,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -56,13 +56,18 @@
 /*****************************************************************************
  * Constants and structures
  *****************************************************************************/
-#define MAX_DIR_SIZE 50000
+#define MAX_DIR_SIZE 100000
+
+#define MODE_EXPAND 0
+#define MODE_COLLAPSE 1
+#define MODE_NONE 2
 
 typedef struct input_directory_s
 {
     char   p_dir_buffer[MAX_DIR_SIZE];
     int    i_buf_pos;
     int    i_buf_length;
+    int    i_pos;
 } input_directory_t;
 
 
@@ -73,16 +78,30 @@ static int     Open   ( vlc_object_t * );
 static void    Close  ( vlc_object_t * );
 
 static ssize_t Read   ( input_thread_t *, byte_t *, size_t );
+int ReadDir( input_thread_t *p_input, char *psz_name , int i_mode );
 
 /*****************************************************************************
  * Module descriptor
  *****************************************************************************/
+#define RECURSIVE_TEXT N_("Includes subdirectories ?")
+#define RECURSIVE_LONGTEXT N_( \
+        "Select whether subdirectories must be expanded.\n" \
+        "none : subdirectories do not appear in the playlist.\n" \
+        "collapse : subdirectories appear but are expanded on first play.\n" \
+        "expand : all subdirectories are expanded.\n" )
+
+static char *psz_recursive_list[] = { "none", "collapse", "expand" };
+static char *psz_recursive_list_text[] = { N_("none"), N_("collapse"),
+                                           N_("expand") };
 
 vlc_module_begin();
     set_description( _("Standard filesystem directory input") );
     set_capability( "access", 55 );
     add_shortcut( "directory" );
     add_shortcut( "dir" );
+    add_string( "recursive", "expand" , NULL, RECURSIVE_TEXT,
+                RECURSIVE_LONGTEXT, VLC_FALSE );
+    //change_string_list( psz_recursive_list, psz_recursive_list_text, 0 );
     set_callbacks( Open, Close );
 vlc_module_end();
 
@@ -95,13 +114,12 @@ static int Open( vlc_object_t *p_this )
     input_thread_t *            p_input = (input_thread_t *)p_this;
     char *                      psz_name;
     input_directory_t *         p_access_data;
+    char *                      psz_mode;
+    int                         i_mode;
 #ifdef HAVE_SYS_STAT_H
     struct stat                 stat_info;
 #endif
-    DIR *                       p_current_dir;
-    struct dirent *             p_dir_content;
-    int                         i_pos=0;
-    
+
     /* Initialize access plug-in structures. */
     if( p_input->i_mtu == 0 )
     {
@@ -125,7 +143,6 @@ static int Open( vlc_object_t *p_this )
         psz_name[strlen(psz_name)-1] = '\0';
     }
 
-
 #ifdef HAVE_SYS_STAT_H
     if( ( stat( psz_name, &stat_info ) == -1 ) ||
         !S_ISDIR( stat_info.st_mode ) )
@@ -137,6 +154,7 @@ static int Open( vlc_object_t *p_this )
         return VLC_EGENERIC;
     }
 
+    /* Initialize structure */
     msg_Dbg( p_input, "opening directory `%s'", psz_name );
     p_access_data = malloc( sizeof(input_directory_t) );
     p_input->p_access_data = (void *)p_access_data;
@@ -146,55 +164,41 @@ static int Open( vlc_object_t *p_this )
         free( psz_name );
         return VLC_ENOMEM;
     }
-        
-    /* have to cd into this dir */
-    p_current_dir = opendir( psz_name );
+    p_access_data->i_pos = 0;
 
-    if( p_current_dir == NULL )
+    psz_mode = config_GetPsz( p_input , "recursive" );
+    if( !psz_mode )
     {
-        /* something went bad, get out of here ! */
-#   ifdef HAVE_ERRNO_H
-        msg_Warn( p_input, "cannot open directory `%s' (%s)",
-                  psz_name, strerror(errno));
-#   else
-        msg_Warn( p_input, "cannot open directory `%s'", psz_name );
-#   endif
+        msg_Err( p_input, "Unable to get configuration" );
+        return VLC_EGENERIC;
+    }
+
+    if( !strncmp( psz_mode, "none" , 4 )  )
+    {
+        i_mode = MODE_NONE;
+    }
+    else if( !strncmp( psz_mode, "collapse", 8 )  )
+    {
+        i_mode = MODE_COLLAPSE;
+    }
+    else
+    {
+        i_mode = MODE_EXPAND;
+    }
+    if( ReadDir( p_input, psz_name , i_mode ) != VLC_SUCCESS )
+    {
         free( p_access_data );
         free( psz_name );
         return VLC_EGENERIC;
     }
-    
-    p_dir_content = readdir( p_current_dir );
 
-    /* while we still have entries in the directory */
-    while( p_dir_content != NULL && i_pos < MAX_DIR_SIZE )
-    {
-        int i_size_entry = strlen( psz_name ) +
-                           strlen( p_dir_content->d_name ) + 2;
-        /* if it is "." or "..", forget it */
-        if( strcmp( p_dir_content->d_name, "." ) &&
-            strcmp( p_dir_content->d_name, ".." ) &&
-            i_pos + i_size_entry < MAX_DIR_SIZE )
-        {
-            msg_Dbg( p_input, "%s", p_dir_content->d_name );
-            sprintf( &p_access_data->p_dir_buffer[i_pos], "%s/%s",
-                     psz_name, p_dir_content->d_name );
-            msg_Dbg( p_input, "%s", &p_access_data->p_dir_buffer[i_pos] );
-            i_pos += i_size_entry - 1;
-            p_access_data->p_dir_buffer[i_pos] = '\n';
-            i_pos++;
-        }
-        p_dir_content = readdir( p_current_dir );
-    }
-    p_access_data->p_dir_buffer[i_pos] = '\0';
-    i_pos++;
-    p_access_data->i_buf_length = i_pos;
+    msg_Dbg(p_input,"Directory read complete. Read %i bytes",
+                    p_access_data->i_pos);
+
+    p_access_data->p_dir_buffer[p_access_data->i_pos] = '\0';
+    p_access_data->i_pos++;
+    p_access_data->i_buf_length = p_access_data->i_pos;
     p_access_data->i_buf_pos = 0;
-    
-    msg_Dbg( p_input, "%s", p_access_data->p_dir_buffer );
-
-    closedir( p_current_dir );
-    free( psz_name );
 
     /* Force m3u demuxer */
     p_input->psz_demux = "m3u";
@@ -211,7 +215,7 @@ static void Close( vlc_object_t * p_this )
     input_directory_t * p_access_data =
         (input_directory_t *)p_input->p_access_data;
 
-    msg_Info( p_input, "closing `%s/%s://%s'", 
+    msg_Info( p_input, "closing `%s/%s://%s'",
               p_input->psz_access, p_input->psz_demux, p_input->psz_name );
 
     free( p_access_data );
@@ -238,6 +242,95 @@ static ssize_t Read( input_thread_t * p_input, byte_t * p_buffer, size_t i_len )
         p_access_data->i_buf_pos += i_ret;
         return (ssize_t) i_ret;
     }
-
     return 0;
+}
+
+/* Local functions */
+
+/*****************************************************************************
+ * ReadDir: read a directory and add its content to the list
+ *****************************************************************************/
+int ReadDir( input_thread_t *p_input, char *psz_name , int i_mode )
+{
+    DIR *                       p_current_dir;
+    struct dirent *             p_dir_content;
+
+    input_directory_t * p_access_data =
+        (input_directory_t *)p_input->p_access_data;
+
+    /* have to cd into this dir */
+    p_current_dir = opendir( psz_name );
+
+    if( p_current_dir == NULL )
+    {
+        /* something went bad, get out of here ! */
+#   ifdef HAVE_ERRNO_H
+        msg_Warn( p_input, "cannot open directory `%s' (%s)",
+                  psz_name, strerror(errno));
+#   else
+        msg_Warn( p_input, "cannot open directory `%s'", psz_name );
+#   endif
+        return VLC_EGENERIC;
+    }
+
+    p_dir_content = readdir( p_current_dir );
+
+    /* while we still have entries in the directory */
+    while( p_dir_content != NULL && p_access_data->i_pos < MAX_DIR_SIZE )
+    {
+        int i_size_entry = strlen( psz_name ) +
+                           strlen( p_dir_content->d_name ) + 2;
+        char *psz_entry = (char *)malloc( sizeof(char)*i_size_entry);
+
+        sprintf( psz_entry, "%s/%s",psz_name,p_dir_content->d_name);
+
+#if 0 /* Disable this message, it makes too much output */
+        msg_Dbg( p_input, "Entry %s",psz_entry );
+#endif
+
+        /* if it is "." or "..", forget it */
+        if( strcmp( p_dir_content->d_name, "." ) &&
+            strcmp( p_dir_content->d_name, ".." ) &&
+            p_access_data->i_pos + i_size_entry < MAX_DIR_SIZE )
+        {
+            if( p_dir_content->d_type == DT_DIR )
+            {
+                if( i_mode == MODE_NONE )
+                {
+                    msg_Dbg( p_input, "Skipping subdirectory %s",psz_entry );
+                    p_dir_content = readdir( p_current_dir );
+                    continue;
+                }
+                else if(i_mode == MODE_EXPAND )
+                {
+                    msg_Dbg(p_input, "Reading subdirectory %s",psz_entry );
+                    if( ReadDir( p_input, psz_entry , MODE_EXPAND )
+                                 != VLC_SUCCESS )
+                    {
+                        return VLC_EGENERIC;
+                    }
+                }
+                else
+                {
+                    sprintf( &p_access_data->p_dir_buffer[p_access_data->i_pos],
+                             "%s", psz_entry );
+                    p_access_data->i_pos += i_size_entry -1 ;
+                    p_access_data->p_dir_buffer[p_access_data->i_pos] = '\n';
+                    p_access_data->i_pos++;
+                }
+            }
+            else
+            {
+                sprintf( &p_access_data->p_dir_buffer[p_access_data->i_pos],
+                         "%s", psz_entry );
+                p_access_data->i_pos += i_size_entry - 1;
+                p_access_data->p_dir_buffer[p_access_data->i_pos] = '\n';
+                p_access_data->i_pos++;
+            }
+        }
+        free( psz_entry );
+        p_dir_content = readdir( p_current_dir );
+    }
+    closedir( p_current_dir );
+    return VLC_SUCCESS;
 }
