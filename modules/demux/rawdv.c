070b6a3abf9a0e47d8510fee0ab2b76e2e22476b
Gildas Bazin <gbazin@videolan.org>
2004-02-29 19:01:22 +0000

    * modules/demux/rawdv.c: DV audio support was removed from libavcodec so reimplemented it here.

--- modules/demux/rawdv.c
+++ modules/demux/rawdv.c
@@ -2,7 +2,7 @@
  * rawdv.c : raw dv input module for vlc
  *****************************************************************************
  * Copyright (C) 2001-2003 VideoLAN
- * $Id: rawdv.c,v 1.14 2004/01/25 20:05:28 hartman Exp $
+ * $Id: rawdv.c,v 1.15 2004/02/29 19:01:22 gbazin Exp $
  *
  * Authors: Gildas Bazin <gbazin@netcourrier.com>
  *
@@ -34,6 +34,9 @@
  *****************************************************************************/
 static int  Open ( vlc_object_t * );
 static void Close( vlc_object_t * );
+
+static block_t *dv_extract_audio( input_thread_t * p_input,
+                                  block_t* p_frame_block );
 
 vlc_module_begin();
     set_description( _("raw dv demuxer") );
@@ -95,6 +98,7 @@ struct demux_sys_t
     es_out_id_t *p_es_audio;
     es_format_t  fmt_audio;
 
+    int    i_dsf;
     double f_rate;
     int    i_bitrate;
 
@@ -135,7 +139,8 @@ static int Open( vlc_object_t * p_this )
         return VLC_EGENERIC;
     }
 
-    if( stream_Peek( p_input->s, &p_peek, DV_PAL_FRAME_SIZE ) < DV_NTSC_FRAME_SIZE )
+    if( stream_Peek( p_input->s, &p_peek, DV_PAL_FRAME_SIZE ) <
+        DV_NTSC_FRAME_SIZE )
     {
         /* Stream too short ... */
         msg_Err( p_input, "cannot peek()" );
@@ -193,6 +198,7 @@ static int Open( vlc_object_t * p_this )
     p_input->pf_demux_control = demux_vaControlDefault;
     p_input->p_demux_data = p_sys = malloc( sizeof( demux_sys_t ) );
 
+    p_sys->i_dsf = dv_header.dsf;
     p_sys->frame_size = dv_header.dsf ? 12 * 150 * 80 : 10 * 150 * 80;
     p_sys->f_rate = dv_header.dsf ? 25 : 29.97;
 
@@ -202,26 +208,9 @@ static int Open( vlc_object_t * p_this )
 
     p_sys->i_bitrate = 0;
 
-    es_format_Init( &p_sys->fmt_video, VIDEO_ES, VLC_FOURCC( 'd','v','s','d' ) );
+    es_format_Init( &p_sys->fmt_video, VIDEO_ES, VLC_FOURCC('d','v','s','d') );
     p_sys->fmt_video.video.i_width = 720;
     p_sys->fmt_video.video.i_height= dv_header.dsf ? 576 : 480;;
-
-    /* Audio stuff */
-#if 0
-    p_peek = p_peek_backup + 80*6+80*16*3 + 3; /* beginning of AAUX pack */
-
-    if( *p_peek != 0x50 || *p_peek != 0x51 )
-    {
-        msg_Err( p_input, "AAUX should begin with 0x50" );
-    }
-#endif
-
-    es_format_Init( &p_sys->fmt_audio, AUDIO_ES, VLC_FOURCC( 'd','v','a','u' ) );
-    p_sys->fmt_audio.audio.i_channels = 2;
-    p_sys->fmt_audio.audio.i_rate = 44100;  /* FIXME */
-    p_sys->fmt_audio.audio.i_bitspersample = 16;
-    p_sys->fmt_audio.audio.i_blockalign = p_sys->frame_size;    /* ??? */
-    p_sys->fmt_audio.i_bitrate = p_sys->f_rate * p_sys->frame_size; /* ??? */
 
     /* necessary because input_SplitBuffer() will only get
      * INPUT_DEFAULT_BUFSIZE bytes at a time. */
@@ -239,7 +228,34 @@ static int Open( vlc_object_t * p_this )
     vlc_mutex_unlock( &p_input->stream.stream_lock );
 
     p_sys->p_es_video = es_out_Add( p_input->p_es_out, &p_sys->fmt_video );
-    p_sys->p_es_audio = es_out_Add( p_input->p_es_out, &p_sys->fmt_audio );
+
+    /* Audio stuff */
+    p_peek = p_peek_backup + 80*6+80*16*3 + 3; /* beginning of AAUX pack */
+    if( *p_peek == 0x50 )
+    {
+        es_format_Init( &p_sys->fmt_audio, AUDIO_ES,
+                        VLC_FOURCC('a','r','a','w') );
+
+        p_sys->fmt_audio.audio.i_channels = 2;
+        switch( (p_peek[4] >> 3) & 0x07 )
+        {
+        case 0:
+            p_sys->fmt_audio.audio.i_rate = 48000;
+            break;
+        case 1:
+            p_sys->fmt_audio.audio.i_rate = 44100;
+            break;
+        case 2:
+        default:
+            p_sys->fmt_audio.audio.i_rate = 32000;
+            break;
+        }
+
+        /* 12 bits non-linear will be converted to 16 bits linear */
+        p_sys->fmt_audio.audio.i_bitspersample = 16;
+
+        p_sys->p_es_audio = es_out_Add( p_input->p_es_out, &p_sys->fmt_audio );
+    }
 
     return VLC_SUCCESS;
 }
@@ -264,8 +280,7 @@ static int Demux( input_thread_t * p_input )
 {
     demux_sys_t    *p_sys = p_input->p_demux_data;
     block_t        *p_block;
-    vlc_bool_t     b_audio, b_video;
-
+    vlc_bool_t     b_audio = VLC_FALSE, b_video = VLC_FALSE;
 
     if( p_input->stream.p_selected_program->i_synchro_state == SYNCHRO_REINIT )
     {
@@ -298,39 +313,179 @@ static int Demux( input_thread_t * p_input )
     }
 
     es_out_Control( p_input->p_es_out, ES_OUT_GET_ES_STATE,
-                    p_sys->p_es_audio, &b_audio );
-    es_out_Control( p_input->p_es_out, ES_OUT_GET_ES_STATE,
                     p_sys->p_es_video, &b_video );
+
+    if( p_sys->p_es_audio )
+    {
+        es_out_Control( p_input->p_es_out, ES_OUT_GET_ES_STATE,
+                        p_sys->p_es_audio, &b_audio );
+    }
 
     p_block->i_dts =
     p_block->i_pts = input_ClockGetTS( p_input,
                                        p_input->stream.p_selected_program,
                                        p_sys->i_pcr );
-    if( b_audio && b_video )
+    if( b_audio )
     {
-        block_t *p_dup = block_Duplicate( p_block );
-
-        es_out_Send( p_input->p_es_out, p_sys->p_es_video, p_block );
-        if( p_dup )
+        block_t *p_audio_block = dv_extract_audio( p_input, p_block );
+        if( p_audio_block )
         {
-            es_out_Send( p_input->p_es_out, p_sys->p_es_video, p_dup );
+            p_audio_block->i_pts = p_audio_block->i_dts = p_block->i_dts;
+            es_out_Send( p_input->p_es_out, p_sys->p_es_audio, p_audio_block );
         }
     }
-    else if( b_audio )
-    {
-        es_out_Send( p_input->p_es_out, p_sys->p_es_audio, p_block );
-    }
-    else if( b_video )
-    {
+
+    if( b_video )
         es_out_Send( p_input->p_es_out, p_sys->p_es_video, p_block );
-    }
     else
-    {
         block_Release( p_block );
-    }
 
     p_sys->i_pcr += ( 90000 / p_sys->f_rate );
 
     return 1;
 }
 
+static const uint16_t dv_audio_shuffle525[10][9] = {
+  {  0, 30, 60, 20, 50, 80, 10, 40, 70 }, /* 1st channel */
+  {  6, 36, 66, 26, 56, 86, 16, 46, 76 },
+  { 12, 42, 72,  2, 32, 62, 22, 52, 82 },
+  { 18, 48, 78,  8, 38, 68, 28, 58, 88 },
+  { 24, 54, 84, 14, 44, 74,  4, 34, 64 },
+  
+  {  1, 31, 61, 21, 51, 81, 11, 41, 71 }, /* 2nd channel */
+  {  7, 37, 67, 27, 57, 87, 17, 47, 77 },
+  { 13, 43, 73,  3, 33, 63, 23, 53, 83 },
+  { 19, 49, 79,  9, 39, 69, 29, 59, 89 },
+  { 25, 55, 85, 15, 45, 75,  5, 35, 65 },
+};
+
+static const uint16_t dv_audio_shuffle625[12][9] = {
+  {   0,  36,  72,  26,  62,  98,  16,  52,  88}, /* 1st channel */
+  {   6,  42,  78,  32,  68, 104,  22,  58,  94},
+  {  12,  48,  84,   2,  38,  74,  28,  64, 100},
+  {  18,  54,  90,   8,  44,  80,  34,  70, 106},
+  {  24,  60,  96,  14,  50,  86,   4,  40,  76},  
+  {  30,  66, 102,  20,  56,  92,  10,  46,  82},
+	
+  {   1,  37,  73,  27,  63,  99,  17,  53,  89}, /* 2nd channel */
+  {   7,  43,  79,  33,  69, 105,  23,  59,  95},
+  {  13,  49,  85,   3,  39,  75,  29,  65, 101},
+  {  19,  55,  91,   9,  45,  81,  35,  71, 107},
+  {  25,  61,  97,  15,  51,  87,   5,  41,  77},  
+  {  31,  67, 103,  21,  57,  93,  11,  47,  83},
+};
+
+static inline uint16_t dv_audio_12to16( uint16_t sample )
+{
+    uint16_t shift, result;
+    
+    sample = (sample < 0x800) ? sample : sample | 0xf000;
+    shift = (sample & 0xf00) >> 8;
+
+    if (shift < 0x2 || shift > 0xd) {
+        result = sample;
+    } else if (shift < 0x8) {
+        shift--;
+        result = (sample - (256 * shift)) << shift;
+    } else {
+        shift = 0xe - shift;
+        result = ((sample + ((256 * shift) + 1)) << shift) - 1;
+    }
+
+    return result;
+}
+
+static block_t *dv_extract_audio( input_thread_t * p_input,
+                                  block_t* p_frame_block )
+{
+    demux_sys_t *p_sys = p_input->p_demux_data;
+    block_t *p_block;
+    uint8_t *p_frame, *p_buf;
+    int i_audio_quant, i_samples, i_size, i_half_ch;
+    const uint16_t (*audio_shuffle)[9];
+    int i, j, d, of;
+    uint16_t lc;
+
+    /* Beginning of AAUX pack */
+    p_buf = p_frame_block->p_buffer + 80*6+80*16*3 + 3;
+    if( *p_buf != 0x50 ) return NULL;
+
+    i_audio_quant = p_buf[4] & 0x07; /* 0 - 16bit, 1 - 12bit */
+    if( i_audio_quant > 1 )
+    {
+        msg_Dbg( p_input, "Unsupported quantization for DV audio");
+        return NULL;
+    }
+
+    i_samples = p_buf[1] & 0x3f; /* samples in this frame - min samples */
+    switch( (p_buf[4] >> 3) & 0x07 )
+    {
+    case 0:
+        i_size = p_sys->i_dsf ? 1896 : 1580;
+        break;
+    case 1:
+        i_size = p_sys->i_dsf ? 1742 : 1452;
+        break;
+    case 2:
+    default:
+        i_size = p_sys->i_dsf ? 1264 : 1053;
+        break;
+    }
+    i_size = (i_size + i_samples) * 4; /* 2ch, 2bytes */
+
+    p_block = block_New( p_input, i_size );
+
+    /* for each DIF segment */
+    p_frame = p_frame_block->p_buffer;
+    audio_shuffle = p_sys->i_dsf ? dv_audio_shuffle625 : dv_audio_shuffle525;
+    i_half_ch = (p_sys->i_dsf ? 12 : 10)/2;
+    for( i = 0; i < (p_sys->i_dsf ? 12 : 10); i++ )
+    {
+        p_frame += 6 * 80; /* skip DIF segment header */
+
+        if( i_audio_quant == 1 && i == i_half_ch ) break;
+
+        for( j = 0; j < 9; j++ )
+        {
+            for( d = 8; d < 80; d += 2 )
+            {
+                if( i_audio_quant == 0 )
+                {
+                    /* 16bit quantization */
+                    of = audio_shuffle[i][j] + (d - 8) / 2 *
+                           (p_sys->i_dsf ? 108 : 90);
+
+                    if( of * 2 >= i_size ) continue;
+
+                    /* big endian */
+                    p_block->p_buffer[of*2] = p_frame[d+1];
+                    p_block->p_buffer[of*2+1] = p_frame[d];
+
+                    if( p_block->p_buffer[of*2+1] == 0x80 &&
+                        p_block->p_buffer[of*2] == 0x00 )
+                        p_block->p_buffer[of*2+1] = 0;
+                }
+                else
+                {
+                    /* 12bit quantization */
+                    lc = ((uint16_t)p_frame[d] << 4) | 
+                         ((uint16_t)p_frame[d+2] >> 4);
+                    lc = (lc == 0x800 ? 0 : dv_audio_12to16(lc));
+
+                    of = audio_shuffle[i][j] + (d - 8) / 3 *
+                         (p_sys->i_dsf ? 108 : 90);
+                    if( of*2 >= i_size ) continue;
+
+                    /* big endian */
+                    p_block->p_buffer[of*2] = lc & 0xff;
+                    p_block->p_buffer[of*2+1] = lc >> 8;
+                    ++d;
+                }
+            }
+
+            p_frame += 16 * 80; /* 15 Video DIFs + 1 Audio DIF */
+        }
+    }
+
+    return p_block;
+}
