fe99472176584762da5ee7e3300aaaaea4add1f7
Gildas Bazin <gbazin@videolan.org>
2004-04-28 07:05:10 +0000

    * modules/codec/ffmpeg/ffmpeg.c: fixed stupid bug preventing the decoder to be closed properly.

--- modules/codec/ffmpeg/ffmpeg.c
+++ modules/codec/ffmpeg/ffmpeg.c
@@ -244,7 +244,7 @@ static void CloseDecoder( vlc_object_t *p_this )
     decoder_t *p_dec = (decoder_t *)p_this;
     decoder_sys_t *p_sys = p_dec->p_sys;
 
-    if( !p_sys->p_context )
+    if( p_sys->p_context )
     {
         if( p_sys->p_context->extradata )
             free( p_sys->p_context->extradata );
