91c59aebcb6049fbd8e1a0da88d33898181411c2
Laurent Aimar <fenrir@videolan.org>
2004-06-22 20:02:47 +0000

    * vorbis: compilation fix.

--- modules/codec/vorbis.c
+++ modules/codec/vorbis.c
@@ -26,6 +26,7 @@
  *****************************************************************************/
 #include <vlc/vlc.h>
 #include <vlc/decoder.h>
+#include <vlc/input.h>
 #include <vlc/sout.h>
 
 #include <ogg/ogg.h>
