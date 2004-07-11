080dfc9b77ebfcc3001130a6ca732f56e1e4134d
Laurent Aimar <fenrir@videolan.org>
2004-07-11 09:58:24 +0000

    *  theora: compilation fix.

--- modules/codec/theora.c
+++ modules/codec/theora.c
@@ -491,7 +491,7 @@ static int OpenEncoder( vlc_object_t *p_this )
     p_enc->fmt_in.i_codec = VLC_FOURCC('I','4','2','0');
     p_enc->fmt_out.i_codec = VLC_FOURCC('t','h','e','o');
 
-    sout_ParseCfg( p_enc, ENC_CFG_PREFIX, ppsz_enc_options, p_enc->p_cfg );
+    sout_CfgParse( p_enc, ENC_CFG_PREFIX, ppsz_enc_options, p_enc->p_cfg );
 
     var_Get( p_enc, ENC_CFG_PREFIX "quality", &val );
     i_quality = val.i_int;
