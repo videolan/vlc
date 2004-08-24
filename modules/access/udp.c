b58037d325d29204174b07954ed854e3f4167577
Laurent Aimar <fenrir@videolan.org>
2004-08-24 20:03:15 +0000

    * udp: unused line.

--- modules/access/udp.c
+++ modules/access/udp.c
@@ -265,7 +265,6 @@ static int Control( access_t *p_access, int i_query, va_list args )
     vlc_bool_t   *pb_bool;
     int          *pi_int;
     int64_t      *pi_64;
-    vlc_value_t  val;
 
     switch( i_query )
     {
@@ -285,7 +284,6 @@ static int Control( access_t *p_access, int i_query, va_list args )
 
         case ACCESS_GET_PTS_DELAY:
             pi_64 = (int64_t*)va_arg( args, int64_t * );
-            var_Get( p_access, "udp-caching", &val );
             *pi_64 = var_GetInteger( p_access, "udp-caching" ) * 1000;
             break;
 
