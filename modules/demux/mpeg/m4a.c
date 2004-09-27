fe1911b072f85e5c3755c076994a5e61a428be6a
Gildas Bazin <gbazin@videolan.org>
2004-09-27 19:57:26 +0000

    * modules/demux/mpeg/m4a.c: MPEG 4 audio demuxer using the MPEG 4 audio packetizer (obsoletes the aac demuxer).

---
+++ modules/demux/mpeg/m4a.c
@@ -0,0 +1,203 @@
+/*****************************************************************************
+ * m4a.c : MPEG-4 audio demuxer
+ *****************************************************************************
+ * Copyright (C) 2002-2004 VideoLAN
+ * $Id: m4v.c 7754 2004-05-23 14:43:14Z fenrir $
+ *
+ * Authors: Gildas Bazin <gbazin@videolan.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
+ *****************************************************************************/
+
+/*****************************************************************************
+ * Preamble
+ *****************************************************************************/
+#include <stdlib.h>                                      /* malloc(), free() */
+
+#include <vlc/vlc.h>
+#include <vlc/input.h>
+#include "vlc_codec.h"
+
+/*****************************************************************************
+ * Module descriptor
+ *****************************************************************************/
+static int  Open ( vlc_object_t * );
+static void Close( vlc_object_t * );
+
+vlc_module_begin();
+    set_description( _("MPEG-4 audio demuxer" ) );
+    set_capability( "demux2", 110 );
+    set_callbacks( Open, Close );
+    add_shortcut( "m4a" );
+    add_shortcut( "mp4a" );
+    add_shortcut( "aac" );
+vlc_module_end();
+
+/*****************************************************************************
+ * Local prototypes
+ *****************************************************************************/
+struct demux_sys_t
+{
+    vlc_bool_t  b_start;
+    es_out_id_t *p_es;
+
+    decoder_t   *p_packetizer;
+};
+
+static int Demux( demux_t * );
+static int Control( demux_t *, int, va_list );
+
+#define M4A_PACKET_SIZE 4096
+
+/*****************************************************************************
+ * Open: initializes demux structures
+ *****************************************************************************/
+static int Open( vlc_object_t * p_this )
+{
+    demux_t     *p_demux = (demux_t*)p_this;
+    demux_sys_t *p_sys;
+    uint8_t     *p_peek;
+    module_t    *p_id3;
+    int         b_forced = VLC_FALSE;
+
+    if( p_demux->psz_path )
+    {
+        int i_len = strlen( p_demux->psz_path );
+
+        if( i_len > 4 && !strcasecmp( &p_demux->psz_path[i_len - 4], ".aac" ) )
+        {
+            b_forced = VLC_TRUE;
+        }
+    }
+
+    if( !p_demux->b_force && !b_forced )
+    {
+        return VLC_EGENERIC;
+    }
+
+    /* skip possible id3 header */
+    if( ( p_id3 = module_Need( p_demux, "id3", NULL, 0 ) ) )
+    {
+        module_Unneed( p_demux, p_id3 );
+    }
+
+    /* peek the begining (10 is for adts header) */
+    if( stream_Peek( p_demux->s, &p_peek, 10 ) < 10 )
+    {
+        msg_Err( p_demux, "cannot peek" );
+        return VLC_EGENERIC;
+    }
+    if( !strncmp( p_peek, "ADIF", 4 ) )
+    {
+        msg_Err( p_demux, "ADIF file. Not yet supported. (Please report)" );
+        return VLC_EGENERIC;
+    }
+
+    p_demux->pf_demux  = Demux;
+    p_demux->pf_control= Control;
+    p_demux->p_sys     = p_sys = malloc( sizeof( demux_sys_t ) );
+    p_sys->p_es        = NULL;
+    p_sys->b_start     = VLC_TRUE;
+
+    /*
+     * Load the mpeg 4 audio packetizer
+     */
+    p_sys->p_packetizer = vlc_object_create( p_demux, VLC_OBJECT_PACKETIZER );
+    p_sys->p_packetizer->pf_decode_audio = NULL;
+    p_sys->p_packetizer->pf_decode_video = NULL;
+    p_sys->p_packetizer->pf_decode_sub = NULL;
+    p_sys->p_packetizer->pf_packetize = NULL;
+    es_format_Init( &p_sys->p_packetizer->fmt_in, AUDIO_ES,
+                    VLC_FOURCC( 'm', 'p', '4', 'a' ) );
+    es_format_Init( &p_sys->p_packetizer->fmt_out, UNKNOWN_ES, 0 );
+    p_sys->p_packetizer->p_module =
+        module_Need( p_sys->p_packetizer, "packetizer", NULL, 0 );
+
+    if( p_sys->p_packetizer->p_module == NULL)
+    {
+        vlc_object_destroy( p_sys->p_packetizer );
+        msg_Err( p_demux, "cannot find mp4a packetizer" );
+        free( p_sys );
+        return VLC_EGENERIC;
+    }
+
+    return VLC_SUCCESS;
+}
+
+/*****************************************************************************
+ * Close: frees unused data
+ *****************************************************************************/
+static void Close( vlc_object_t * p_this )
+{
+    demux_t     *p_demux = (demux_t*)p_this;
+    demux_sys_t *p_sys = p_demux->p_sys;
+
+    module_Unneed( p_sys->p_packetizer, p_sys->p_packetizer->p_module );
+    vlc_object_destroy( p_sys->p_packetizer );
+
+    free( p_sys );
+}
+
+/*****************************************************************************
+ * Demux: reads and demuxes data packets
+ *****************************************************************************
+ * Returns -1 in case of error, 0 in case of EOF, 1 otherwise
+ *****************************************************************************/
+static int Demux( demux_t *p_demux)
+{
+    demux_sys_t *p_sys = p_demux->p_sys;
+    block_t *p_block_in, *p_block_out;
+
+    if( ( p_block_in = stream_Block( p_demux->s, M4A_PACKET_SIZE ) ) == NULL )
+    {
+        return 0;
+    }
+
+    p_block_in->i_pts = p_block_in->i_dts = p_sys->b_start ? 1 : 0;
+
+    while( (p_block_out = p_sys->p_packetizer->pf_packetize(
+                                          p_sys->p_packetizer, &p_block_in )) )
+    {
+        while( p_block_out )
+        {
+            block_t *p_next = p_block_out->p_next;
+
+            if( p_sys->p_es == NULL )
+            {
+                p_sys->p_packetizer->fmt_out.b_packetized = VLC_TRUE;
+                p_sys->p_es = es_out_Add( p_demux->out,
+                                          &p_sys->p_packetizer->fmt_out);
+            }
+
+            es_out_Control( p_demux->out, ES_OUT_SET_PCR, p_block_out->i_dts );
+
+            p_block_out->p_next = NULL;
+            es_out_Send( p_demux->out, p_sys->p_es, p_block_out );
+
+            p_block_out = p_next;
+        }
+    }
+    return 1;
+}
+
+/*****************************************************************************
+ * Control:
+ *****************************************************************************/
+static int Control( demux_t *p_demux, int i_query, va_list args )
+{
+    if( i_query == DEMUX_SET_TIME ) return VLC_EGENERIC;
+    else return demux2_vaControlHelper( p_demux->s, 0, -1,
+                                        0, 1, i_query, args );
+}
