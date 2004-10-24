dc69e660b9e53dd7921c77b375aeaa77373f86a5
Mark Moriarty <markfm@videolan.org>
2004-10-24 15:56:37 +0000

    Truncated packet debug message -- state reported vs. actual length, not the delta.

--- modules/access/mms/mmstu.c
+++ modules/access/mms/mmstu.c
@@ -1211,8 +1211,8 @@ static int  mms_ParsePacket( access_t *p_access,
     if( i_packet_length > i_data || i_packet_length <= 8)
     {
         msg_Dbg( p_access,
-                 "truncated packet (missing %d bytes)",
-                 i_packet_length - i_data  );
+                 "truncated packet (Declared %d bytes, Actual %d bytes)",
+                 i_packet_length, i_data  );
         *pi_used = 0;
         return -1;
     }
