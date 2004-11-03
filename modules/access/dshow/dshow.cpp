08b960e8a57877815f51062c668eb82683b7b9b7
Gildas Bazin <gbazin@videolan.org>
2004-11-03 11:56:16 +0000

    * modules/access/dshow/dshow.cpp: fixed initialization bug.

--- modules/access/dshow/dshow.cpp
+++ modules/access/dshow/dshow.cpp
@@ -315,6 +315,9 @@ static int CommonOpen( vlc_object_t *p_this, access_sys_t *p_sys,
     p_sys->p_capture_graph_builder2 = NULL;
     p_sys->p_control = NULL;
 
+    vlc_mutex_init( p_this, &p_sys->lock );
+    vlc_cond_init( p_this, &p_sys->wait );
+
     /* Build directshow graph */
     CreateDirectShowGraph( p_sys );
 
@@ -413,8 +416,6 @@ static int CommonOpen( vlc_object_t *p_this, access_sys_t *p_sys,
 
     /* Initialize some data */
     p_sys->i_current_stream = 0;
-    vlc_mutex_init( p_this, &p_sys->lock );
-    vlc_cond_init( p_this, &p_sys->wait );
 
     if( !p_sys->i_streams ) return VLC_EGENERIC;
 
