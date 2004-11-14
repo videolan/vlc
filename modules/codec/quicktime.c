4c1faa32b2784b5484167cc8c129bb24ff98844c
Gildas Bazin <gbazin@videolan.org>
2004-11-14 17:59:33 +0000

    * modules/codec/quicktime.c: fixed small mem leak.

--- modules/codec/quicktime.c
+++ modules/codec/quicktime.c
@@ -2,7 +2,7 @@
  * quicktime.c: a quicktime decoder that uses the QT library/dll
  *****************************************************************************
  * Copyright (C) 2003 VideoLAN
- * $Id: quicktime.c,v 1.21 2004/01/25 18:20:12 bigben Exp $
+ * $Id$
  *
  * Authors: Laurent Aimar <fenrir at via.ecp.fr>
  *          Derk-Jan Hartman <thedj at users.sf.net>
@@ -485,6 +485,8 @@ exit_error:
     vlc_mutex_unlock( &p_dec->p_vlc->quicktime_lock );
 #endif
     vlc_mutex_unlock( lockval.p_address );
+
+    free( p_sys );
     return VLC_EGENERIC;
 }
 
