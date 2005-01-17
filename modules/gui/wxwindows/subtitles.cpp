75f950e65a9eae8522e92753537670282c78a3b1
Yoann Peronneau <yoann@videolan.org>
2005-01-17 11:47:41 +0000

    * wxwindows/subtitles.cpp: subtitle delay can also be <0 (now from -30000 to +30000)

--- modules/gui/wxwindows/subtitles.cpp
+++ modules/gui/wxwindows/subtitles.cpp
@@ -231,7 +231,7 @@ SubsFileDialog::SubsFileDialog( intf_thread_t *_p_intf, wxWindow* _p_parent ):
     delay_spinctrl = new wxSpinCtrl( panel, -1, format_delay,
                                      wxDefaultPosition, wxDefaultSize,
                                      wxSP_ARROW_KEYS,
-                                     0, 16000, i_delay );
+                                     -30000, 30000, i_delay );
     delay_spinctrl->SetToolTip( wxU(_("Set subtitle delay (in 1/10s)" ) ) );
 
     grid_sizer->Add( label_delay , 0, wxALIGN_CENTER, 5 );
