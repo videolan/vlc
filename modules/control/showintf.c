3c98fc758620210553298dcad2897ab4578b4b5d
Benjamin Pracht <bigben@videolan.org>
2005-01-19 22:28:47 +0000

    Add a category to the showintf control interface, so that it gets displayed in the prefs...

--- modules/control/showintf.c
+++ modules/control/showintf.c
@@ -63,6 +63,8 @@ static int  MouseEvent( vlc_object_t *, char const *,
 #define THRESHOLD_LONGTEXT N_( "Height of the zone triggering the interface" )
 
 vlc_module_begin();
+    set_category( CAT_INTERFACE );
+    set_subcategory( SUBCAT_INTERFACE_CONTROL );
     add_integer( "showintf-threshold", 10, NULL, THRESHOLD_TEXT, THRESHOLD_LONGTEXT, VLC_TRUE );
     set_description( _("Interface showing control interface") );
 
