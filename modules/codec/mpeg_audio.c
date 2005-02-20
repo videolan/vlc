d06602cf449f3cf0f817602980d5f44497c713fc
Gildas Bazin <gbazin@videolan.org>
2005-02-20 21:38:36 +0000

    * modules/codec/mpeg_audio.c: let ffmpeg have the priority under WinCE.

--- modules/codec/mpeg_audio.c
+++ modules/codec/mpeg_audio.c
@@ -106,8 +106,8 @@ vlc_module_begin();
     set_description( _("MPEG audio layer I/II/III parser") );
     set_category( CAT_INPUT );
     set_subcategory( SUBCAT_INPUT_ACODEC );
-#if defined(SYS_DARWIN)
-    set_capability( "decoder", 5 );
+#if defined(SYS_DARWIN) || defined(UNDER_CE)
+   set_capability( "decoder", 5 );
 #else
     set_capability( "decoder", 100 );
 #endif
