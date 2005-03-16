44d1b3d39f3aa8ef023995d070ec17c25d52e903
Rémi Denis-Courmont <rem@videolan.org>
2005-03-16 10:51:10 +0000

    - TY should have a lower priority than PS

--- modules/demux/ty.c
+++ modules/demux/ty.c
@@ -70,7 +70,11 @@ vlc_module_begin();
     set_description(_("TY Stream audio/video demux"));
     set_category( CAT_INPUT );
     set_subcategory( SUBCAT_INPUT_DEMUX );
-    set_capability("demux2", 10);
+    set_capability("demux2", 8);
+    /* FIXME: there seems to be a segfault when using PVR access
+     * and TY demux has a bigger priority than PS
+     * Something must be wrong.
+     */
     set_callbacks(TyOpen, TyClose);
     add_shortcut("ty");
     add_shortcut("tivo");
