4e247307758d8124da393fa1eb95e3105e553a46
Eric Petit <titer@videolan.org>
2005-03-24 16:05:55 +0000

    nuv.c: compile fix (for big endian platforms)

--- modules/demux/nuv.c
+++ modules/demux/nuv.c
@@ -524,10 +524,10 @@ static inline void GetDoubleLE( double *pd, void *src )
 {
     /* FIXME works only if sizeof(double) == 8 */
 #ifdef WORDS_BIGENDIAN
-    uint8_t *p = (uint8_t*)pd;
+    uint8_t *p = (uint8_t*)pd, *q = (uint8_t*)src;
     int i;
     for( i = 0; i < 8; i++ )
-        p[i] = src[7-i];
+        p[i] = q[7-i];
 #else
     memcpy( pd, src, 8 );
 #endif
