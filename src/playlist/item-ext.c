7df5cf2f6602f2f13ba492f104969d4a9d01749f
Sigmund Augdal Helberg <sigmunau@videolan.org>
2005-04-15 22:28:24 +0000

    playlist/item-ext.c: fixed off-by-one error causing a crash in ItemGetById

--- src/playlist/item-ext.c
+++ src/playlist/item-ext.c
@@ -441,7 +441,7 @@ playlist_item_t *playlist_LockItemGetByPos( playlist_t *p_playlist, int i_pos )
 playlist_item_t * playlist_ItemGetById( playlist_t * p_playlist , int i_id )
 {
     int i, i_top, i_bottom;
-    i_bottom = 0; i_top = p_playlist->i_all_size;
+    i_bottom = 0; i_top = p_playlist->i_all_size - 1;
     i = i_top / 2;
     while( p_playlist->pp_all_items[i]->input.i_id != i_id &&
            i_top > i_bottom )
