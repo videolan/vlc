057de5d64d2d82c71723a4fb72230ae1d305ad94
Laurent Aimar <fenrir@videolan.org>
2005-04-26 08:42:19 +0000

    * all: added set_shortname.

--- modules/access_filter/record.c
+++ modules/access_filter/record.c
@@ -45,6 +45,7 @@ static int  Open ( vlc_object_t * );
 static void Close( vlc_object_t * );
 
 vlc_module_begin();
+    set_shortname( _("Record") );
     set_description( _("Record") );
     set_category( CAT_INPUT );
     set_subcategory( SUBCAT_INPUT_ACCESS_FILTER );
