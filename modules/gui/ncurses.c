49a88c0e0f1e634f278fb14068853b313d849900
Felix Paul Kühne <fkuehne@videolan.org>
2005-05-06 17:41:09 +0000

    * playlist.m: use stringWithUTF8String instead of stringWithCString when creating the Services-Discovery-menus to show UTF8-encoded strings properly * ncurses.c: don't localise "Ncurses"

--- modules/gui/ncurses.c
+++ modules/gui/ncurses.c
@@ -96,7 +96,7 @@ static void ReadDir        ( intf_thread_t * );
     "will show you initially.")
 
 vlc_module_begin();
-    set_shortname( _("Ncurses"));
+    set_shortname( "Ncurses" );
     set_description( _("Ncurses interface") );
     set_capability( "interface", 10 );
     set_category( CAT_INTERFACE );
