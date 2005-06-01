fb34f08160f832b3891d9a047f652f4d5c9338d7
Rémi Denis-Courmont <rem@videolan.org>
2005-06-01 18:34:45 +0000

    Backport 11231 to 0.8.2

--- src/misc/block.c
+++ src/misc/block.c
@@ -100,9 +100,9 @@ block_t *block_Realloc( block_t *p_block, int i_prebody, int i_body )
 
     if( i_body < 0 || i_buffer_size <= 0 ) return NULL;
 
-    if( i_prebody < ( p_block->p_buffer - p_block->p_sys->p_allocated_buffer +
-                      p_block->p_sys->i_allocated_buffer ) ||
-        p_block->p_buffer - i_prebody > p_block->p_sys->p_allocated_buffer )
+    if( p_block->p_buffer - i_prebody > p_block->p_sys->p_allocated_buffer &&
+        p_block->p_buffer - i_prebody < p_block->p_sys->p_allocated_buffer +
+        p_block->p_sys->i_allocated_buffer )
     {
         p_block->p_buffer -= i_prebody;
         p_block->i_buffer += i_prebody;
