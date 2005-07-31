231b13c0405db91d018c7bbab8df46ae04cb45a0
Rémi Denis-Courmont <rem@videolan.org>
2005-07-31 19:01:09 +0000

    Add const

--- include/network.h
+++ include/network.h
@@ -475,7 +475,7 @@ VLC_EXPORT( void, vlc_freeaddrinfo, ( struct addrinfo * ) );
  * net_AddressIsMulticast: This function returns VLC_FALSE if the psz_addr does
  * not specify a multicast address or if the address is not a valid address.
  *****************************************************************************/
-static inline vlc_bool_t net_AddressIsMulticast( vlc_object_t *p_object, char *psz_addr )
+static inline vlc_bool_t net_AddressIsMulticast( vlc_object_t *p_object, const char *psz_addr )
 {
     struct addrinfo hints, *res;
     vlc_bool_t b_multicast = VLC_FALSE;
