d8fc9f3b89c2b0409bcda5fa81a5e604103c20ef
Jean-Paul Saman <jpsaman@videolan.org>
2005-08-10 21:47:13 +0000

    By default --packetizer-mpegvideo-sync-iframe is disabled.

--- modules/packetizer/mpegvideo.c
+++ modules/packetizer/mpegvideo.c
@@ -67,7 +67,7 @@ vlc_module_begin();
     set_capability( "packetizer", 50 );
     set_callbacks( Open, Close );
 
-    add_bool( "packetizer-mpegvideo-sync-iframe", 1, NULL, SYNC_INTRAFRAME_TEXT,
+    add_bool( "packetizer-mpegvideo-sync-iframe", 0, NULL, SYNC_INTRAFRAME_TEXT,
               SYNC_INTRAFRAME_LONGTEXT, VLC_TRUE );
 vlc_module_end();
 
