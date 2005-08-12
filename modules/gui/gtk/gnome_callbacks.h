681264553565add5192868a451fa892e87f3d39c
Clément Stenac <zorglub@videolan.org>
2005-08-12 21:49:28 +0000

    Remove gtk, gtk2, qt and kde plugins
    If you want them, take them back from the svn attic, fix them and reintroduce them. I didn't remove the configure.ac snippets, they are still in but commented

--- modules/gui/gtk/gnome_callbacks.h
+++
@@ -1,8 +0,0 @@
-/*****************************************************************************
- * This file is not needed: everything is in gtk_callbacks.h
- *****************************************************************************/
-#include "gtk_callbacks.h"
-
-void
-GtkDiscOpenCDDA                        (GtkToggleButton *togglebutton,
-                                        gpointer         user_data);
