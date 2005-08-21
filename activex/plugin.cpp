e373df9de49e94237944cb31f917b4ee5815506e
Damien Fouilleul <damienf@videolan.org>
2005-08-21 18:49:07 +0000

    all: make sure that the plugin use that registry for plugin path

--- activex/plugin.cpp
+++ activex/plugin.cpp
@@ -497,7 +497,7 @@ HRESULT VLCPlugin::onInit(void)
              RegCloseKey( h_key );
         }
 
-#if 1
+#if 0
         ppsz_argv[0] = "C:\\cygwin\\home\\Damien_Fouilleul\\dev\\videolan\\vlc-trunk\\vlc";
 #endif
 
