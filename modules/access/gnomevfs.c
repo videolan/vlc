33c723d672ea9faeab145914636c6dc8547d237f
Benjamin Pracht <bigben@videolan.org>
2005-09-13 10:41:08 +0000

    Cosmetics

--- modules/access/gnomevfs.c
+++ modules/access/gnomevfs.c
@@ -28,28 +28,8 @@
 #include <vlc/input.h>
 #include <libgnomevfs/gnome-vfs.h>
 
-#if 0
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
-#ifdef HAVE_SYS_TYPES_H
-#   include <sys/types.h>
-#endif
-#ifdef HAVE_SYS_TIME_H
-#   include <sys/time.h>
-#endif
-#ifdef HAVE_SYS_STAT_H
-#   include <sys/stat.h>
-#endif
-#ifdef HAVE_FCNTL_H
-#   include <fcntl.h>
-#endif
-
-#ifdef HAVE_UNISTD_H
-#   include <unistd.h>
-#endif
-
-#endif
 
 #include "charset.h"
 
