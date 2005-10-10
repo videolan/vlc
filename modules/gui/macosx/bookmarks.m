1760568d6bb9c926ededac2010011d9309222c62
Felix Paul Kühne <fkuehne@videolan.org>
2005-10-10 15:41:52 +0000

    * set keywords properly and removed an outdated FIXME

--- modules/gui/macosx/bookmarks.m
+++ modules/gui/macosx/bookmarks.m
@@ -259,7 +259,7 @@ static VLCBookmarks *_o_sharedInstance = nil;
     {
         free( pp_bookmarks[i]->psz_name );
     }
-    /* FIXME: putting the name to core does not work!! -- FK*/
+
     pp_bookmarks[i]->psz_name = strdup([[o_edit_fld_name stringValue] UTF8String]); 
     pp_bookmarks[i]->i_byte_offset = [[o_edit_fld_bytes stringValue] intValue];
     pp_bookmarks[i]->i_time_offset = ([[o_edit_fld_time stringValue] intValue]  * 1000000);
