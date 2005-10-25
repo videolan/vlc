bc9929eef5e2b7b290a8f9ca9478e55091af53be
Marian Durkovic <md@videolan.org>
2005-10-25 16:27:06 +0000

    once again...

--- src/libvlc.h
+++ src/libvlc.h
@@ -336,7 +336,7 @@ static char *ppsz_clock_descriptions[] =
 
 #define MTU_TEXT N_("MTU of the network interface")
 #define MTU_LONGTEXT N_( \
-    "This is the maximum packet size that can be transmitted " \ 
+    "This is the maximum packet size that can be transmitted " \
     "over network interface. On Ethernet it is usually 1500 bytes.")
 
 #define TTL_TEXT N_("Time To Live")
