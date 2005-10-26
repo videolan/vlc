6da562f9bfaa51e09c4bc8b1e75c5f30229c5057
Marian Durkovic <md@videolan.org>
2005-10-26 09:14:14 +0000

    fixed SAP c= line TTL issue in #standard, --sout-udp-ttl takes precedence over -ttl

--- modules/stream_out/standard.c
+++ modules/stream_out/standard.c
@@ -368,7 +368,9 @@ static int Open( vlc_object_t *p_this )
             p_session->i_port = url.i_port;
             p_session->psz_sdp = NULL;
 
-            p_session->i_ttl = config_GetInt( p_sout, "ttl" );
+            var_Get( p_access, "sout-udp-ttl", &val );
+            p_session->i_ttl = val.i_int ? 
+                val.i_int : config_GetInt( p_sout, "ttl" );
             p_session->i_payload = 33;
             p_session->b_rtp = strstr( psz_access, "rtp") ? 1 : 0;
 
