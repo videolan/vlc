e328a29da456766a9a06b8de55177df0857c0400
Clément Stenac <zorglub@videolan.org>
2006-01-21 13:49:42 +0000

    Get rid of warning

--- modules/gui/skins2/unzip/unzip.c
+++ modules/gui/skins2/unzip/unzip.c
@@ -204,7 +204,7 @@ local int unzlocal_getShort (pzlib_filefunc_def,filestream,pX)
     uLong *pX;
 {
     uLong x ;
-    int i;
+    int i = 0;
     int err;
 
     err = unzlocal_getByte(pzlib_filefunc_def,filestream,&i);
@@ -232,7 +232,7 @@ local int unzlocal_getLong (pzlib_filefunc_def,filestream,pX)
     uLong *pX;
 {
     uLong x ;
-    int i;
+    int i = 0;
     int err;
 
     err = unzlocal_getByte(pzlib_filefunc_def,filestream,&i);
