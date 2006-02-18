c7c8ed7a2cd66a4147b1d065faf01453e4fdb71f
Antoine Cellerier <dionoea@videolan.org>
2006-02-18 23:08:23 +0000

    rss.c: change module shortname

--- modules/video_filter/rss.c
+++ modules/video_filter/rss.c
@@ -156,7 +156,7 @@ static char *ppsz_pos_descriptions[] =
  *****************************************************************************/
 vlc_module_begin();
     set_capability( "sub filter", 0 );
-    set_shortname( "RSS" );
+    set_shortname( "RSS / Atom" );
     set_callbacks( CreateFilter, DestroyFilter );
     set_category( CAT_VIDEO );
     set_subcategory( SUBCAT_VIDEO_SUBPIC );
