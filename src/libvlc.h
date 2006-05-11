4815d7f68835e4fb08b6381988dd214f651f4069
Christophe Mutricy <xtophe@videolan.org>
2006-05-11 21:46:51 +0000

    Synchronise the po file from the 0.8.5 branch. Add gl, he, hu and sv to activated l10n.

--- src/libvlc.h
+++ src/libvlc.h
@@ -28,16 +28,17 @@
 #include "vlc_keys.h"
 
 static char *ppsz_language[] =
-{ "auto", "en", "en_GB", "ca", "da", "de", "es", "fr", "it", "ja",
-"ka","ko", "nl", "oc", "pt_BR", "ro", "ru", "tr", "zh_CN", "zh_TW" };
+{ "auto", "en", "en_GB", "ca", "da", "de", "es", "fr", "gl", "he", "hu",
+  "it", "ja", "ka", "ko", "nl", "oc", "pt_BR", "ro", "ru", "sv", "tr",
+  "zh_CN", "zh_TW" };
 
 static char *ppsz_language_text[] =
 { N_("Auto"), N_("American English"), N_("British English"),
 N_("Catalan"), N_("Danish"), N_("German"), N_("Spanish"), N_("French"),
-N_("Italian"), N_("Japanese"),N_("Georgian"), N_("Korean"),
-N_("Dutch"), N_("Occitan"), N_("Brazilian Portuguese"), N_("Romanian"),
-N_("Russian"), N_("Turkish"), N_("Simplified Chinese"),
-N_("Chinese Traditional") };
+N_("Galician"), N_("Hebrew"), N_("Hungarian"),N_("Italian"),
+N_("Japanese"),N_("Georgian"), N_("Korean"), N_("Dutch"), N_("Occitan"),
+N_("Brazilian Portuguese"), N_("Romanian"), N_("Russian"), N_("Swedish"),
+N_("Turkish"), N_("Simplified Chinese"), N_("Chinese Traditional") };
 
 static char *ppsz_snap_formats[] =
 { "png", "jpg" };
