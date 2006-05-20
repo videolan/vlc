a63d4a7744503f81b96b796f1e1558e9b558fcfe
Jean-Paul Saman <jpsaman@videolan.org>
2006-05-20 11:45:33 +0000

    Remove unused variables and #ifdef DEBUG_SPUDEC the variables used when DEBUG_SPUDEC is active.

--- modules/codec/spudec/parse.c
+++ modules/codec/spudec/parse.c
@@ -145,8 +145,6 @@ static int ParseControlSeq( decoder_t *p_dec, subpicture_t *p_spu,
     uint8_t i_command = SPU_CMD_END;
     mtime_t date = 0;
 
-    unsigned int i, pi_alpha[4];
-
     /* Initialize the structure */
     p_spu->i_start = p_spu->i_stop = 0;
     p_spu->b_ephemer = VLC_FALSE;
@@ -216,6 +214,7 @@ static int ParseControlSeq( decoder_t *p_dec, subpicture_t *p_spu,
             if( p_dec->fmt_in.subs.spu.palette[0] == 0xBeeF )
             {
                 unsigned int idx[4];
+                int i;
 
                 p_spu_data->b_palette = VLC_TRUE;
 
@@ -556,9 +555,10 @@ static int ParseRLE( decoder_t *p_dec, subpicture_t * p_spu,
     /* Crop if necessary */
     if( i_skipped_top || i_skipped_bottom )
     {
+#ifdef DEBUG_SPUDEC
         int i_y = p_spu->i_y + i_skipped_top;
         int i_height = p_spu->i_height - (i_skipped_top + i_skipped_bottom);
-
+#endif
         p_spu_data->i_y_top_offset = i_skipped_top;
         p_spu_data->i_y_bottom_offset = i_skipped_bottom;
 #ifdef DEBUG_SPUDEC
