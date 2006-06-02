640fe7328c3efafc924a03d11710a3378ba5ed7c
Christophe Mutricy <xtophe@videolan.org>
2006-06-02 22:56:38 +0000

    Fix Stream title. Patch by Diego "Flameeyes" Petteno

--- modules/access/http.c
+++ modules/access/http.c
@@ -641,7 +641,8 @@ static int ReadICYMeta( access_t *p_access )
         p += strlen( "StreamTitle=" );
         if( *p == '\'' || *p == '"' )
         {
-            char *psz = strchr( &p[1], p[0] );
+            char closing[] = { p[0], ';', '\0' };
+            char *psz = strstr( &p[1], closing );
             if( !psz )
                 psz = strchr( &p[1], ';' );
 
