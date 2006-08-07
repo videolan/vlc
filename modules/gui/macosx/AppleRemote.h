afab53090bd616cbde57a9c19206fdbb9a58ac34
Felix Paul Kühne <fkuehne@videolan.org>
2006-08-07 16:36:57 +0000

    * configure.ac: enabled Obj-C exceptions in the OBJCFLAGS (they're currently used by AppleRemote only, but we should extend them to further classes) * AppleRemote.*: added svn id and proper notes about the MIT-style and GPL dual licensing

--- modules/gui/macosx/AppleRemote.h
+++ modules/gui/macosx/AppleRemote.h
@@ -1,10 +1,55 @@
-//
-//  AppleRemote.h
-//  AppleRemote
-//
-//  Created by Martin Kahr on 11.03.06.
-//  Copyright 2006 martinkahr.com. All rights reserved.
-//
+/*****************************************************************************
+ * AppleRemote.h
+ * AppleRemote
+ * $Id:$
+ *
+ * Created by Martin Kahr on 11.03.06 under a MIT-style license. 
+ * Copyright (c) 2006 martinkahr.com. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ *****************************************************************************
+ *
+ * Note that changes made by any members or contributors of the VideoLAN team
+ * (i.e. changes that were checked in into one of VideoLAN's source code
+ * repositories are licensed under the GNU General Public License version 2,
+ * or (at your option) any later version. 
+ * Thus, the following statements apply to our changes:
+ *
+ * Copyright (C) 2006 the VideoLAN team
+ * Authors: Eric Petit <titer@m0k.org>
+ *          Felix Kühne <fkuehne at videolan dot org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #import <Cocoa/Cocoa.h>
 #import <mach/mach.h>
