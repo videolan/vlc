e76c46ddcc4c2603994113f30eefa70cc7d42275
Jean-Baptiste Kempf <jb@videolan.org>
2006-09-13 23:16:25 +0000

    * QT timeout=>timeout() in the MACRO to avoid hanging. Object::connect: Parentheses expected, signal QTimer::timeout

--- modules/gui/qt4/qt4.hpp
+++ modules/gui/qt4/qt4.hpp
@@ -57,7 +57,7 @@ struct intf_sys_t
 
 #define CONNECT( a, b, c, d ) connect( a, SIGNAL( b ), c, SLOT(d) )
 #define BUTTONACT( b, a ) connect( b, SIGNAL( clicked() ), this, SLOT(a) )
-#define ON_TIMEOUT( act ) CONNECT( THEDP->fixed_timer, timeout, this, act )
+#define ON_TIMEOUT( act ) CONNECT( THEDP->fixed_timer, timeout(), this, act )
 
 static int DialogEvent_Type = QEvent::User + 1;
 
