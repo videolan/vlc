f03e39c385ddbdf61b7021eaa17d03cdbd8fef5a
Pavlov Konstantin <thresh@videolan.org>
2006-10-10 15:54:41 +0000

    yeah, writing 4 0 times is fun, but..

--- modules/demux/ty.c
+++ modules/demux/ty.c
@@ -308,7 +308,7 @@ static int check_sync_pes( demux_t *p_demux, block_t *p_block,
         if( offset < 0 )
         {
             /* no header found, fake some 00's (this works, believe me) */
-            memset( p_sys->pes_buffer, 4, 0 );
+            memset( p_sys->pes_buffer, 0, 4 );
             p_sys->i_pes_buf_cnt = 4;
             if( rec_len > 4 )
                 msg_Err( p_demux, "PES header not found in record of %d bytes!",
