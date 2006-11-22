218990780ed565d4f2ccf5a424e3abce11a84db0
Rémi Denis-Courmont <rem@videolan.org>
2006-11-22 19:50:58 +0000

    int16_t needs <inttypes.h>. Should fix FreeBSD unhapiness with addPCM().

--- modules/visualization/galaktos/PCM.c
+++ modules/visualization/galaktos/PCM.c
@@ -32,6 +32,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 double **PCMd;    //data structure to store PCM data  PCM[channels][maxsamples]
 int maxsamples;   //size of PCM buffer
