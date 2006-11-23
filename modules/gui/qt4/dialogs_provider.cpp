f7166b8bd7a46cdd8175fbe4169f5162f287f74d
Jean-Baptiste Kempf <jb@videolan.org>
2006-11-23 13:33:05 +0000

    Qt4: forgotten files in parent directory. Sorry!

--- modules/gui/qt4/dialogs_provider.cpp
+++ modules/gui/qt4/dialogs_provider.cpp
@@ -33,7 +33,7 @@
 /* The dialogs */
 #include "dialogs/playlist.hpp"
 #include "dialogs/prefs_dialog.hpp"
-#include "dialogs/streaminfo.hpp"
+#include "dialogs/mediainfo.hpp"
 #include "dialogs/messages.hpp"
 #include "dialogs/extended.hpp"
 #include "dialogs/sout.hpp"
