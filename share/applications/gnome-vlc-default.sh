1e89271e8fd2f1fa9541c4468f20caefe027d225
Jean-Paul Saman <jpsaman@videolan.org>
2006-12-27 10:21:13 +0000

    Make gnome-vlc-default.sh script work on FC4/5/6 too.

--- share/applications/gnome-vlc-default.sh
+++ share/applications/gnome-vlc-default.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
+# share/applications/gnome-vlc-default.sh
+# $Id:$
+#
+# Copyright (C) 2006, VideoLAN team
+# Authors: Torsten Spindler
+#          Jean-Paul Saman
+#
 # Set vlc as default player on the gnome desktop
 # Tested on Ubuntu 6.10 Edgy
-gconftool -t string -s /desktop/gnome/volume_manager/autoplay_vcd_command "vlc %m"
-gconftool -t string -s /desktop/gnome/volume_manager/autoplay_dvd_command "vlc %m"
-gconftool -t string -s /desktop/gnome/url-handlers/mms "vlc %s"
-gconftool -t string -s /desktop/gnome/url-handlers/mmsh "vlc %s"
-gconftool -t string -s /desktop/gnome/url-handlers/rtsp "vlc %s"
-gconftool -t string -s /desktop/gnome/url-handlers/rtp "vlc %s"
+#
+if test -x /usr/bin/gconftool; then
+	GCONFTOOL=/usr/bin/gconftool
+	OPTS="-t string -s"
+fi
+if test -x /usr/bin/gconftool-2; then
+	GCONFTOOL=/usr/bin/gconftool-2
+	OPTS="--type string --set"
+fi
+$GCONFTOOL $OPTS /desktop/gnome/volume_manager/autoplay_vcd_command "vlc %m"
+$GCONFTOOL $OPTS /desktop/gnome/volume_manager/autoplay_dvd_command "vlc %m"
+$GCONFTOOL $OPTS /desktop/gnome/url-handlers/mms "vlc %s"
+$GCONFTOOL $OPTS /desktop/gnome/url-handlers/mmsh "vlc %s"
+$GCONFTOOL $OPTS /desktop/gnome/url-handlers/rtsp "vlc %s"
+$GCONFTOOL $OPTS /desktop/gnome/url-handlers/rtp "vlc %s"
 
+exit 0
