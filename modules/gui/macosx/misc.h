f49699d4bf1a3ef7dd155a2dfefb6d3abd3ee3e7
Pierre d'Herbemont <pdherbemont@videolan.org>
2007-03-30 14:34:51 +0000

    Mac OS X gui: Attempt to fix compilation error.

--- modules/gui/macosx/misc.h
+++ modules/gui/macosx/misc.h
@@ -21,6 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#import <Cocoa/Cocoa.h>
 #import <ApplicationServices/ApplicationServices.h>
 
 /*****************************************************************************
