db9d9941618ed8641ed18c34a9968e51892979c6
Rafaël Carré <funman@videolan.org>
2007-04-05 17:08:49 +0000

    Streaming output works again. Closes #1047

--- src/playlist/loadsave.c
+++ src/playlist/loadsave.c
@@ -86,9 +86,6 @@ int playlist_Export( playlist_t * p_playlist, const char *psz_filename ,
 
 int playlist_MLLoad( playlist_t *p_playlist )
 {
-#ifndef THIS_PIECE_OF_CODE_IS_FIXED // see #1047 and possibly others
-    (void)p_playlist;
-#else
     char *psz_uri, *psz_homedir =p_playlist->p_libvlc->psz_homedir;
     input_item_t *p_input;
 
@@ -113,7 +110,6 @@ int playlist_MLLoad( playlist_t *p_playlist )
     p_playlist->b_doing_ml = VLC_FALSE;
 
     free( psz_uri );
-#endif
     return VLC_SUCCESS;
 }
 
