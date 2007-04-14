19bbed6ad6967effa2e8e1068f419d3706d806cf
Rémi Denis-Courmont <rem@videolan.org>
2007-04-14 14:15:12 +0000

    Clean header file

--- src/misc/configuration.h
+++ src/misc/configuration.h
@@ -1,3 +1,30 @@
+/*****************************************************************************
+ * configuration.h management of the modules configuration
+ *****************************************************************************
+ * Copyright (C) 2007 the VideoLAN team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef LIBVLC_CONFIGURATION_H
+# define LIBVLC_CONFIGURATION_H 1
+
+# ifdef __cplusplus
+extern "C" {
+# endif
+
 /* Internal configuration prototypes and structures */
 
 int  config_CreateDir( vlc_object_t *, const char * );
@@ -15,3 +42,8 @@ int   __config_LoadCmdLine  ( vlc_object_t *, int *, char *[], vlc_bool_t );
 char *   config_GetHomeDir     ( void );
 char *   config_GetUserDir     ( void );
 int    __config_LoadConfigFile ( vlc_object_t *, const char * );
+
+# ifdef __cplusplus
+}
+# endif
+#endif
