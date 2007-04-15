975c0e3f6b0ed35408c8072613f8fc7051a810ea
Rémi Denis-Courmont <rem@videolan.org>
2007-04-15 12:16:43 +0000

    Fix segmentation fault

--- modules/access_output/udp.c
+++ modules/access_output/udp.c
@@ -275,7 +275,7 @@ static int Open( vlc_object_t *p_this )
         if (psz_parser[0] == '[')
             psz_parser = strchr (psz_parser, ']');
 
-        psz_parser = strchr (psz_parser, ':');
+        psz_parser = strchr (psz_parser ?: psz_dst_addr, ':');
         if (psz_parser != NULL)
         {
             *psz_parser++ = '\0';
