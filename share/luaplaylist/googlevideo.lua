906a3f2fe8422bc16e987237ffb3cfff3b28dae8
Antoine Cellerier <dionoea@videolan.org>
2007-05-17 20:46:31 +0000

    * googlevideo.lua: add small comment to explain why we're not getting the meta data.

--- share/luaplaylist/googlevideo.lua
+++ share/luaplaylist/googlevideo.lua
@@ -9,5 +9,7 @@ end
 
 -- Parse function.
 function parse()
+    -- We don't need to get the meta data here since it'll be available in
+    -- the GVP file.
     return { { path = string.gsub( vlc.path, "^.*(docid=[^&]*).*$", "http://video.google.com/videogvp?%1" ) } }
 end
