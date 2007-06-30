c7977d5d05c34086ae8d96c341fae3d2c5bfcb5b
Christophe Mutricy <xtophe@videolan.org>
2007-06-30 00:11:32 +0000

    Activate the polish translation

--- src/libvlc-module.c
+++ src/libvlc-module.c
@@ -40,17 +40,17 @@
 #if defined (WIN32) || defined (__APPLE__)
 static const char *ppsz_language[] =
 { "auto", "ar", "en", "en_GB", "ca", "cs", "da", "de", "es", "fa"  "fr", "gl",
-    "he", "hu", "it", "ja", "ka", "ko", "ms", "nl", "oc", "pt_BR", "ro", "ru",
-    "sk", "sl", "sv", "tr", "zh_CN", "zh_TW" };
+    "he", "hu", "it", "ja", "ka", "ko", "ms", "nl", "oc", "pl", "pt_BR", "ro", 
+    "ru", "sk", "sl", "sv", "tr", "zh_CN", "zh_TW" };
 
 static const char *ppsz_language_text[] =
 { N_("Auto"), N_("Arabic"), N_("American English"), N_("British English"),
 N_("Catalan"), N_("Czech"), N_("Danish"), N_("German"), N_("Spanish"),
 N_("Persian"), N_("French"), N_("Galician"), N_("Hebrew"), N_("Hungarian"),
 N_("Italian"), N_("Japanese"),N_("Georgian"), N_("Korean"), N_("Malay"),
-N_("Dutch"), N_("Occitan"), N_("Brazilian Portuguese"), N_("Romanian"),
-N_("Russian"), N_("Slovak"), N_("Slovenian"), N_("Swedish"), N_("Turkish"),
-N_("Simplified Chinese"), N_("Chinese Traditional") };
+N_("Dutch"), N_("Occitan"), N_("Polish"), N_("Brazilian Portuguese"), 
+N_("Romanian"), N_("Russian"), N_("Slovak"), N_("Slovenian"), N_("Swedish"),
+N_("Turkish"), N_("Simplified Chinese"), N_("Chinese Traditional") };
 #endif
 
 static const char *ppsz_snap_formats[] =
