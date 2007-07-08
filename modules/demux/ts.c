230aaffb1eb9d9656746a5d48b8e815482047865
Christophe Mutricy <xtophe@videolan.org>
2007-07-08 20:05:02 +0000

    Ressucitate the telx module and make it to conflict with the zvbi module

--- modules/demux/ts.c
+++ modules/demux/ts.c
@@ -1662,8 +1662,9 @@ static void ParsePES( demux_t *p_demux, ts_pid_t *pid )
         i_skip += 2;
     }
     else if( pid->es->fmt.i_codec == VLC_FOURCC( 't', 'e', 'l', 'x' ) )
-        i_skip = 0;
-
+#ifdef ZVBI_COMPILED
+        i_skip = 0; /*hack for zvbi support */
+#endif
     /* skip header */
     while( p_pes && i_skip > 0 )
     {
