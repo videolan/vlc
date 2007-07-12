9eae287a95231be78b98f09257fc07d06bf6594a
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2007-07-12 16:57:43 +0000

    vlc_common.h: fix typo in define variable name

--- include/vlc_common.h
+++ include/vlc_common.h
@@ -511,7 +511,7 @@ typedef int ( * vlc_callback_t ) ( vlc_object_t *,      /* variable's object */
 #    endif
 #  else
 #    define VLC_PUBLIC_API 
-#    define VLC_RIVATE_API 
+#    define VLC_PRIVATE_API 
 #    define   VLC_EXPORT( type, name, args ) struct _u_n_u_s_e_d_
 #    define VLC_INTERNAL( type, name, args ) struct _u_n_u_s_e_d_
      extern module_symbols_t* p_symbols;
