060d116f00759bb7525da1b88fcb244759584d7a
Pierre d'Herbemont <pdherbemont@videolan.org>
2007-08-15 04:28:26 +0000

    share/luameta/googleimage.lua: Rework a bit to better handle meta going wrong.

--- share/luameta/googleimage.lua
+++ share/luameta/googleimage.lua
@@ -2,19 +2,20 @@
 -- $Id$
 
 -- Replace non alphanumeric char by +
-function get_query( )
-    if vlc.title then
-        title = vlc.title
-    else
-        title = vlc.name
-    end
-    vlc.msg_dbg( title )
+function get_query( title )
     return title:gsub( "[^(a-z|A-Z|0-9)]", "+" )
 end
 
 -- Return the artwork
 function fetch_art()
-    fd = vlc.stream_new( "http://images.google.com/images?q=" .. get_query( ) )
+    if vlc.title then
+        title = vlc.title
+    elseif vlc.name then
+        title = vlc.name
+    else
+        return nil
+    end
+    fd = vlc.stream_new( "http://images.google.com/images?q=" .. get_query( title ) )
     page = vlc.stream_read( fd, 65653 )
     vlc.stream_delete( fd )
     _, _, arturl = string.find( page, "imgurl=([^&]+)" )
