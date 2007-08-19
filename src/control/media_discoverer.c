7e042a1c43bd226714572c3d534602e1bad9310d
Pierre d'Herbemont <pdherbemont@videolan.org>
2007-08-19 21:45:00 +0000

    control/media_discoverer.c: Fix a warning+bug.

--- src/control/media_discoverer.c
+++ src/control/media_discoverer.c
@@ -135,7 +135,7 @@ libvlc_media_discoverer_localized_name( libvlc_media_discoverer_t * p_mdis )
 libvlc_media_list_t *
 libvlc_media_discoverer_media_list( libvlc_media_discoverer_t * p_mdis )
 {
-    libvlc_media_list_retain( p_mdis->p_sd );
-    return p_mdis->p_sd;
+    libvlc_media_list_retain( p_mdis->p_mlist );
+    return p_mdis->p_mlist;
 }
 
