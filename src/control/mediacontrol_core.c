eeeb5ae2130d9ee25e0bd86376c8baedb26a112b
Rémi Denis-Courmont <rem@videolan.org>
2007-08-20 19:04:45 +0000

    Missing vlc/vlc.h here

--- src/control/mediacontrol_core.c
+++ src/control/mediacontrol_core.c
@@ -21,6 +21,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#include <vlc/vlc.h>
 #include <vlc/mediacontrol.h>
 
 #include <vlc/libvlc.h>
