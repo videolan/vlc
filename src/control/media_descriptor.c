dddce3f44f066effa9fabb84e30461f1c9244844
Pierre d'Herbemont <pdherbemont@videolan.org>
2007-08-25 17:40:11 +0000

    control/media_descriptor.c: Remove a typo that makes the subitems to appear empty.

--- src/control/media_descriptor.c
+++ src/control/media_descriptor.c
@@ -89,7 +89,7 @@ static void input_item_subitem_added( const vlc_event_t *p_event,
     /* Add this to our media list */
     if( !p_md->p_subitems )
         p_md->p_subitems = libvlc_media_list_new( p_md->p_libvlc_instance, NULL );
-    if( !p_md->p_subitems )
+    if( p_md->p_subitems )
     {
         libvlc_media_list_add_media_descriptor( p_md->p_subitems, p_md_child, NULL );
     }
