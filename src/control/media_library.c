7fecb1e1f5d74b8c30507c19cbda4bbb0872c286
Pierre d'Herbemont <pdherbemont@videolan.org>
2007-08-27 19:16:10 +0000

    control: Fix multiple event_manager leaks.

--- src/control/media_library.c
+++ src/control/media_library.c
@@ -67,6 +67,7 @@ void libvlc_media_library_release( libvlc_media_library_t * p_mlib )
     if( p_mlib->i_refcount > 0 )
         return;
 
+    libvlc_event_manager_release( p_mlib->p_event_manager );
 	free( p_mlib );
 }
 
