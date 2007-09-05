c0777c319477a09c46f34429285b6c329edae44b
Jean-Baptiste Kempf <jb@videolan.org>
2007-09-05 15:08:03 +0000

    Qt4 - cosmetic

--- modules/gui/qt4/util/customwidgets.hpp
+++ modules/gui/qt4/util/customwidgets.hpp
@@ -21,7 +21,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA. *****************************************************************************/
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA. 
+ *****************************************************************************/
 
 #ifndef _CUSTOMWIDGETS_H_
 #define _CUSTOMWIDGETS_H_
