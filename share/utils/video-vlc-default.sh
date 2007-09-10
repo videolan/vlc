3fb19e73c471595886734d258d7d9e48b0c8bfe2
Rafaël Carré <funman@videolan.org>
2007-09-10 03:43:27 +0000

    Adds 2 scripts to make vlc the default audio/video mime-type handler on freedesktop, contributed by forum user.

---
+++ share/utils/video-vlc-default.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# $Id $
+#
+# Copyright © 2007 VideoLAN team
+#
+# Authors: http://forum.videolan.org/viewtopic.php?f=13&t=40560
+#          Rafaël Carré <funman@videolanorg>
+#
+# Sets VLC media player the default application for video mime types
+# on a freedesktop compliant desktop
+#
+
+MIME_FILE=~/.local/share/applications/defaults.list
+
+if [ ! -f $MIME_FILE ]
+    then echo "[Default Applications]" > $MIME_FILE
+    else grep -v 'video/' $MIME_FILE > /tmp/vlc.defaults.list.tmp
+    mv /tmp/vlc.defaults.list.tmp $MIME_FILE
+fi
+
+ls /usr/share/mime/video/* | sed -e 's@/usr/share/mime/@@' -e 's/\.xml/=vlc.desktop/' >> $MIME_FILE
