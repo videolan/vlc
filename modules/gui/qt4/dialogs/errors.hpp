1674135c30240612d72acf07c6aac1e27b593da6
Jean-Baptiste Kempf <jb@videolan.org>
2007-10-09 02:28:01 +0000

    Qt4 - header corrections.

--- modules/gui/qt4/dialogs/errors.hpp
+++ modules/gui/qt4/dialogs/errors.hpp
@@ -18,7 +18,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA. *****************************************************************************/
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #ifndef _ERRORS_DIALOG_H_
 #define _ERRORS_DIALOG_H_
