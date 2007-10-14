aea60015e1671fdf63b89a7f17dc9013cd87c4b4
Laurent Aimar <fenrir@videolan.org>
2007-10-14 12:18:17 +0000

    Removed meta-preparsed.

--- modules/demux/mpc.c
+++ modules/demux/mpc.c
@@ -194,7 +194,6 @@ static void Close( vlc_object_t * p_this )
     demux_t        *p_demux = (demux_t*)p_this;
     demux_sys_t    *p_sys = p_demux->p_sys;
 
-    var_Destroy( p_demux, "meta-preparsed" );
     free( p_sys );
 }
 
