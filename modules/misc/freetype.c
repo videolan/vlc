edead8b8df5005c8210bde4d7b94b7ab0701e6d1
Jean-Paul Saman <jpsaman@videolan.org>
2007-10-15 09:39:17 +0000

    small codingstyle fix

--- modules/misc/freetype.c
+++ modules/misc/freetype.c
@@ -467,7 +467,7 @@ static void Destroy( vlc_object_t *p_this )
 
 #ifdef HAVE_FONTCONFIG
 
-static void FontBuilder( vlc_object_t *p_this)
+static void FontBuilder( vlc_object_t *p_this )
 {
     filter_t *p_filter = (filter_t*)p_this;
     filter_sys_t *p_sys = p_filter->p_sys;
