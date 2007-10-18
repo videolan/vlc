b3e157d9b1a0d7a61e680df9d50d13135939f84e
Rafaël Carré <funman@videolan.org>
2007-10-18 22:30:34 +0000

    lua demuxer for lelombrik.net

---
+++ share/luaplaylist/lelombrik.lua
@@ -0,0 +1,27 @@
+-- $Id$
+-- French humor site: http://lelombrik.net
+
+-- Probe function.
+function probe()
+    return vlc.access == "http"
+        and string.match( vlc.path, "lelombrik.net/videos" ) 
+end
+
+-- Parse function.
+function parse()
+    vidtitle = ""
+    while true do
+        line = vlc.readline()
+        if not line then break end
+        if string.match( line, "id=\"nom_fichier\">" ) then
+            vidtitle = string.gsub( line, ".*\"nom_fichier\">([^<]*).*", "%1" )
+        end
+        if string.match( line, "<param name=\"movie\"" ) then
+            -- fallback: retrieve the title from the url if we didn't find it
+            if vidtitle == "" then
+                vidtitle = string.gsub( vlc.path, ".*/([^.]*).*", "%1" )
+            end
+            return { { path = string.gsub( line, ".*%?file=([^&]*).*", "%1" ); arturl = string.gsub( line, ".*&image=([^&]*).*", "%1" ); title = vidtitle } }
+        end
+    end
+end
