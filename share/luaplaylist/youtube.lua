d2a20afcd776a48537cdf43b989b4401f7823d07
Rafaël Carré <funman@videolan.org>
2007-11-03 14:28:52 +0000

    lua: adds a copyright notice and a GPLv2+ header to each lua script authors: feel free to change to the licence you want, and to add yourself to the Authors list additionnally, adds break.lua to its Makefile.am

--- share/luaplaylist/youtube.lua
+++ share/luaplaylist/youtube.lua
@@ -1,4 +1,24 @@
--- $Id$
+--[[
+ French humor site: http://lelombrik.net
+
+ $Id$
+
+ Copyright © 2007 the VideoLAN team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+--]]
 
 -- Helper function to get a parameter's value in a URL
 function get_url_param( url, name )
