f631d60bc8726b7f1614d7089462fd1156163e34
Pierre d'Herbemont <pdherbemont@videolan.org>
2007-11-19 09:22:13 +0000

    MacOSX/VLC_app: Update and simplify to match current Framework state.

--- extras/MacOSX/VLC_app/VLCController.m
+++ extras/MacOSX/VLC_app/VLCController.m
@@ -22,89 +22,28 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-/* this code needs rework, but it does nice thing */
-
 #import <VLC/VLC.h>
+
 #import "VLCController.h"
-#import "VLCCategoryCell.h"
-NSArrayController * treeController;
-VLCPlaylist * currentPlaylist;
+
 @implementation VLCController
 - (void)awakeFromNib
 {
-    [NSApp setDelegate:self];
-    [detailList setTarget:self];
-    [detailList setDoubleAction:@selector(detailListItemDoubleClicked:)];
-
-    [detailList setDataSource: [[VLCPlaylistDataSource alloc] initWithPlaylist:nil videoView:videoView]];
-    [detailList registerForDraggedTypes: [NSArray arrayWithObjects:NSFilenamesPboardType, NSURLPboardType, nil]];
-
-    [categoryList setTarget:self];
-    [categoryList setAction:@selector(categoryListItemClicked:)];
-    [[[categoryList tableColumns] objectAtIndex:0] setDataCell:[[[VLCCategoryCell alloc] init] autorelease]];
-}
-
-- (void)detailListItemDoubleClicked:(id)sender
-{
-    if( [sender isKindOfClass:[NSTableView class]] && [sender selectedRow] >= 0)
-    {
-        [videoView setPlaylist: currentPlaylist];
-        [videoView playMedia: [[treeController selectedObjects] objectAtIndex: 0]];
-    }
-
-}
-
-- (void)categoryListItemClicked:(id)sender
-{
-    VLCPlaylist * aPlaylist;
-    id selectedItem = [sender itemAtRow:[sender selectedRow]];
-
-    if([selectedItem isKindOfClass:[VLCMediaLibrary class]])
-        aPlaylist = [selectedItem allMedia];
-    else if([selectedItem isKindOfClass:[VLCMediaDiscoverer class]])
-        aPlaylist = [selectedItem playlist];
-    else if([selectedItem isKindOfClass:[VLCPlaylist class]])
-        aPlaylist = [selectedItem flatPlaylist];
-    else
-    {
-        return;
-    }
-    currentPlaylist= aPlaylist;
-    treeController = [[NSArrayController alloc] init]; /* XXX: We leak */
-    
-    //[treeController setAutomaticallyPreparesContent: YES];
-    [[detailList dataSource] release];
-    NSLog(@"currentPlaylist %@", currentPlaylist);
-    [detailList setDataSource: [[VLCPlaylistDataSource alloc] initWithPlaylist:aPlaylist videoView:videoView]];
-    //[treeController setContent: [[VLCMediaLibrary sharedMediaLibrary] allMedia]];
-    [treeController bind:@"contentArray" toObject: aPlaylist
-                    withKeyPath:@"items" options:nil];
-	NSTableColumn *tableColumn;
+    VLCMediaList * mediaList = [[VLCMediaList alloc] init];
+    [mediaList addMedia:[VLCMedia mediaWithURL:@"/dev/null"]];
+    NSArrayController * arrayController = [[NSArrayController alloc] init];
+    [arrayController bind:@"contentArray" toObject:mediaList withKeyPath:@"media" options:nil];
     NSMutableDictionary *bindingOptions = [NSMutableDictionary dictionary];
 
-    [bindingOptions setObject:@"metaInformation.title contains[c] $value" forKey:NSPredicateFormatBindingOption];
     [bindingOptions setObject:@"No Title" forKey:NSDisplayNameBindingOption];
-
-    [detailSearchField bind:@"predicate" toObject: treeController
-                    withKeyPath:@"filterPredicate" options:bindingOptions];
-
-    //[treeController setChildrenKeyPath:@"subitems.items"];
-	
-    [bindingOptions removeAllObjects];
-	[bindingOptions setObject:@"Search" forKey:@"NSNullPlaceholder"];
 		
 	/* binding for "title" column */
-    tableColumn = [detailList tableColumnWithIdentifier:@"title"];
+    NSTableColumn * tableColumn = [detailList tableColumnWithIdentifier:@"title"];
 	
-	[tableColumn bind:@"value" toObject: treeController
-		  withKeyPath:@"arrangedObjects.metaInformation.title" options:bindingOptions];
-
+	[tableColumn bind:@"value" toObject: arrayController
+		  withKeyPath:@"arrangedObjects.url" options:bindingOptions];
+    
 }
-@end
 
-@implementation VLCController (NSAppDelegating)
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    [self categoryListItemClicked: categoryList];
-}
+
 @end
