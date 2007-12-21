efecf95f446cbe0652123f0210e07feb4e6c5f0f
Jean-Baptiste Kempf <jb@videolan.org>
2007-12-21 00:42:57 +0000

    Qt4 - typo.

--- modules/gui/qt4/dialogs/open.cpp
+++ modules/gui/qt4/dialogs/open.cpp
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * open.cpp : Advanced open dialog
  *****************************************************************************
- * Copyright ( C ) 2006-2007 the VideoLAN team
+ * Copyright © 2006-2007 the VideoLAN team
  * $Id$
  *
  * Authors: Jean-Baptiste Kempf <jb@videolan.org>
@@ -9,7 +9,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
- * ( at your option ) any later version.
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
