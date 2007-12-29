89b604d4c3f1f232665dab0149da23b1a375dab7
Rafaël Carré <funman@videolan.org>
2007-12-29 14:32:01 +0000

    cosmetics

--- modules/demux/playlist/xspf.c
+++ modules/demux/playlist/xspf.c
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
- *******************************************************************************/
+ ******************************************************************************/
 /**
  * \file modules/demux/playlist/xspf.c
  * \brief XSPF playlist import functions
