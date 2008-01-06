d8ad30f984b74e714af5695cc58af94ba36fb401
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-01-06 20:07:56 +0000

    MacOSX/Framework/VLCVideoCommon.m: Fix fillscreen.

--- extras/MacOSX/Framework/Sources/VLCVideoCommon.m
+++ extras/MacOSX/Framework/Sources/VLCVideoCommon.m
@@ -50,7 +50,7 @@
         
         CGFloat xRatio = CGRectGetWidth(bounds)/originalVideoSize.width;
         CGFloat yRatio = CGRectGetHeight(bounds)/originalVideoSize.height;
-        CGFloat ratio = fillScreenEntirely ? MAX(xRatio, yRatio) : MIN(xRatio, yRatio);
+        CGFloat ratio = fillScreenEntirely ? MIN(xRatio, yRatio) : MAX(xRatio, yRatio);
         
         videoRect.size.width = ratio*originalVideoSize.width;
         videoRect.size.height = ratio*originalVideoSize.height;
