e564db69c6e3b707a666792d63f10d9549113e39
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-01-13 01:23:51 +0000

    MacOSX/Framework/Example: Initial creation of a nice application that does iPod exporting.

---
+++ extras/MacOSX/Framework/Examples/iPodConverter/GradientBackgroundView.h
@@ -0,0 +1,16 @@
+//
+//  GradientBackgroundView.h
+//  iPodConverter
+//
+//  Created by Pierre d'Herbemont on 1/12/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface GradientBackgroundView : NSImageView {
+
+}
+
+@end
---
+++ extras/MacOSX/Framework/Examples/iPodConverter/MovieReceiver.h
@@ -0,0 +1,16 @@
+//
+//  MovieReceiver.h
+//  iPodConverter
+//
+//  Created by Pierre d'Herbemont on 1/12/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "Controller.h"
+
+@interface MovieReceiver : NSView {
+    IBOutlet Controller * controller;
+}
+
+@end
