5c35841422ce3a8b843a3b05aae9c97fa36366da
Olivier Aubert <olivier.aubert@liris.cnrs.fr>
2008-01-18 14:19:31 +0000

    python bindings: add a warning in the docstring of vlc_internal.c

--- bindings/python/vlc_internal.c
+++ bindings/python/vlc_internal.c
@@ -719,7 +719,7 @@ static PyTypeObject vlcObject_Type =
     0,                         /*tp_setattro*/
     0,                         /*tp_as_buffer*/
     Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "Expose VLC object infrastructure.\n\nConstructor: vlc.Object(object_id)",  /* tp_doc */
+    "Expose VLC object internal infrastructure.\n\nConstructor: vlc.Object(object_id)\n\nPLEASE BE AWARE that accessing internal features of VLC voids the guarantee for the product and is not advised except if you know what you are doing.",  /* tp_doc */
     0,                     /* tp_traverse */
     0,                     /* tp_clear */
     0,                     /* tp_richcompare */
