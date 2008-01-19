e70bce6cf627880d284f012bd570b73df9c6d4e8
Antoine Cellerier <dionoea@videolan.org>
2008-01-19 12:35:35 +0000

    Simplify.

--- modules/video_filter/mosaic.h
+++ modules/video_filter/mosaic.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * mosaic.h:
  *****************************************************************************
- * Copyright (C) 2004-2005 the VideoLAN team
+ * Copyright (C) 2004-2008 the VideoLAN team
  * $Id$
  *
  * Authors: Antoine Cellerier <dionoea@videolan.org>
@@ -41,18 +41,15 @@ typedef struct bridge_t
 static bridge_t *__GetBridge( vlc_object_t *p_object )
 {
     vlc_object_t *p_libvlc = VLC_OBJECT( p_object->p_libvlc );
-    bridge_t *p_bridge;
     vlc_value_t val;
 
     if( var_Get( p_libvlc, "mosaic-struct", &val ) != VLC_SUCCESS )
     {
-        p_bridge = NULL;
+        return NULL;
     }
     else
     {
-        p_bridge = val.p_address;
+        return val.p_address;
     }
-
-    return p_bridge;
 }
 
