04a4bec2d8b672cfe92061f3e3953b4a527d6cf3
Dennis van Amerongen <trax@videolan.org>
2008-01-24 14:21:14 +0000

    * modules/access/bda/bda.h: remove white space

--- modules/access/bda/bda.h
+++ modules/access/bda/bda.h
@@ -24,8 +24,8 @@
  * Preamble
  *****************************************************************************/
 
-#ifdef HAVE_CONFIG_H 
-# include "config.h" 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
 #endif
 
 #include <stdlib.h>
