59bc158988d5db1c29381151c133f0babc7a198f
Rémi Denis-Courmont <rem@videolan.org>
2008-01-25 16:41:35 +0000

    Same as previous commit

--- include/vlc_modules_macros.h
+++ include/vlc_modules_macros.h
@@ -73,7 +73,7 @@
 #if defined( __PLUGIN__ ) && ( defined( WIN32 ) || defined( UNDER_CE ) )
 #   define DLL_SYMBOL              __declspec(dllexport)
 #   define CDECL_SYMBOL            __cdecl
-#elif HAVE_ATTRIBUTE_VISIBILITY
+#elif defined (HAVE_ATTRIBUTE_VISIBILITY)
 #   define DLL_SYMBOL __attribute__((visibility("default")))
 #   define CDECL_SYMBOL
 #else
