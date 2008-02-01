a950a071e98a15da3ffedd07cd7f1bf4f1560709
Rémi Denis-Courmont <rem@videolan.org>
2008-02-01 17:16:11 +0000

    Set the Win32 console title

--- src/libvlc-common.c
+++ src/libvlc-common.c
@@ -1828,6 +1828,7 @@ static void ShowConsole( vlc_bool_t b_dofile )
      * Unicode/locale subsystem. By default, we have the obsolecent OEM code
      * page (e.g. CP437 or CP850). */
     SetConsoleOutputCP (GetACP ());
+    SetConsoleTitle ("VLC media player version "PACKAGE_VERSION);
 
     freopen( "CONOUT$", "w", stderr );
     freopen( "CONIN$", "r", stdin );
