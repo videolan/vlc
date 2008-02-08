d3615517c3435aa2f489370b06b91ce00645a36d
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-02-08 20:49:52 +0000

    cmake: Correctly remove files prior to recmake.

--- extras/buildsystem/cmake/scripts/convert_vlc_to_cmake.sh
+++ extras/buildsystem/cmake/scripts/convert_vlc_to_cmake.sh
@@ -11,21 +11,30 @@ then
 fi
 
 echo "Installing CMakeLists.txt"
+rm -f CMakeLists.txt
 ln -sf $cmake_dir/CMakeLists/root_CMakeLists.txt CMakeLists.txt
 
 echo "Installing src/CMakeLists.txt"
+rm -f src/CMakeLists.txt
 ln -sf ../$cmake_dir/CMakeLists/src_CMakeLists.txt src/CMakeLists.txt
 
+echo "Removing old modules/gui/qt4/CMakeLists.txt"
+rm -f modules/gui/qt4/CMakeLists.txt
+
 echo "Installing modules/CMakeLists.txt"
+rm -f modules/CMakeLists.txt
 ln -s ../$cmake_dir/CMakeLists/modules_CMakeLists.txt modules/CMakeLists.txt
 
 echo "Installing po/CMakeLists.txt"
+rm -f po/CMakeLists.txt
 ln -s ../$cmake_dir/CMakeLists/po_CMakeLists.txt po/CMakeLists.txt
 
 echo "Installing include/config.h.cmake"
+rm -f include/config.h.cmake
 ln -sf ../$cmake_dir/config.h.cmake include/config.h.cmake
 
 echo "Installing cmake/"
+rm -f cmake
 ln -sf $cmake_dir/include cmake
 
 echo "Generating CMakeLists for modules/"
