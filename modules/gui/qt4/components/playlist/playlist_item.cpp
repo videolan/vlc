fb700e8f478244ce694a4955f61ca4514418d74a
Ilkka Ollakka <ileoo@videolan.org>
2008-02-11 05:46:29 +0000

    show default info in playlist if i_showflag is too low to show anything at all

--- modules/gui/qt4/components/playlist/playlist_item.cpp
+++ modules/gui/qt4/components/playlist/playlist_item.cpp
@@ -70,6 +70,8 @@ void PLItem::init( int _i_id, int _i_input_id, PLItem *parent, PLModel *m )
         {
             QSettings settings( "vlc", "vlc-qt-interface" );
             i_showflags = settings.value( "qt-pl-showflags" ).toInt();
+            if( i_showflags < 1) 
+                i_showflags = 39; //reasonable default to show something;
             updateColumnHeaders();
         }
     }
