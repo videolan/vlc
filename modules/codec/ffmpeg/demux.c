244601ee1e5157c478bd818b49de6765bf393397
Faustino Osuna <riquedafreak@videolan.org>
2008-02-29 17:04:45 +0000

    Fix previous commit.

--- modules/codec/ffmpeg/demux.c
+++ modules/codec/ffmpeg/demux.c
@@ -49,7 +49,7 @@
 //#define AVFORMAT_DEBUG 1
 
 /* Version checking */
-#if defined(HAVE_LIBSWSCALE_SWSCALE_H) || defined(HAVE_LIBSWSCALE_SWSCALE_H)  || defined(HAVE_FFMPEG_SWSCALE_H) || defined(HAVE_LIBSWSCALE_TREE)
+#if defined(HAVE_FFMPEG_AVFORMAT_H) || defined(HAVE_LIBAVFORMAT_AVFORMAT_H) || defined(HAVE_LIBAVFORMAT_TREE)
 
 /*****************************************************************************
  * demux_sys_t: demux descriptor
