77f7cdd4cb65332ed8343e3ed973b92f77fbaa03
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-03-23 01:50:51 +0100

    Re-checking src/interface/interface.h.

---
+++ src/interface/interface.h
@@ -0,0 +1,40 @@
+/*****************************************************************************
+ * interface.h: Internal interface prototypes and structures
+ *****************************************************************************
+ * Copyright (C) 1998-2006 the VideoLAN team
+ * $Id$
+ *
+ * Authors: Vincent Seguin <seguin@via.ecp.fr>
+ *          Clément Stenac <zorglub@videolan.org>
+ *          Felix Kühne <fkuehne@videolan.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#if defined(__PLUGIN__) || defined(__BUILTIN__) || !defined(__LIBVLC__)
+# error This header file can only be included from LibVLC.
+#endif
+
+#ifndef __LIBVLC_INTERFACE_H
+# define __LIBVLC_INTERFACE_H 1
+
+/**********************************************************************
+ * Interaction
+ **********************************************************************/
+
+/* release via vlc_object_release() */
+vlc_object_t * interaction_Init( libvlc_int_t *p_libvlc );
+
+#endif
