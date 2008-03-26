310a72b65ebcc0cf36165c59902344d7c05e8950
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-03-26 18:44:36 +0100

    vlc_modules_macros.h: Bump ABI version.

--- include/vlc_modules_macros.h
+++ include/vlc_modules_macros.h
@@ -35,8 +35,8 @@
 /**
  * Current plugin ABI version
  */
-# define MODULE_SYMBOL 0_9_0i
-# define MODULE_SUFFIX "__0_9_0i"
+# define MODULE_SYMBOL 0_9_0j
+# define MODULE_SUFFIX "__0_9_0j"
 
 /*****************************************************************************
  * Add a few defines. You do not want to read this section. Really.
