990f4ea384ba4d607da02168202cb10cb5932639
Jean-Baptiste Kempf <jb@videolan.org>
2008-03-30 22:23:27 -0700

    String renaming.

--- modules/gui/qt4/dialogs/extended.cpp
+++ modules/gui/qt4/dialogs/extended.cpp
@@ -70,7 +70,7 @@ ExtendedDialog::ExtendedDialog( intf_thread_t *_p_intf ): QVLCFrame( _p_intf )
     mainTabW->addTab( videoWidget, qtr( "Video Effects" ) );
 
     SyncControls *syncW = new SyncControls( p_intf, videoTab );
-    mainTabW->addTab( syncW, qtr( "A/V Synchronisation" ) );
+    mainTabW->addTab( syncW, qtr( "Synchro." ) );
 
     if( module_Exists( p_intf, "v4l2" ) )
     {
