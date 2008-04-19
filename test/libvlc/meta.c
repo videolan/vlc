3f465bd277f2c9dca0b26642d5398ff5b7c9ee58
Rafaël Carré <funman@videolan.org>
2008-04-19 03:43:28 +0200

    Mikael Stanojevic rules them all (especially Bonnevile)

--- test/libvlc/meta.c
+++ test/libvlc/meta.c
@@ -50,7 +50,7 @@ static void test_meta (const char ** argv, int argc)
     artist = libvlc_media_get_meta (media, libvlc_meta_Artist, &ex);
     catch ();
 
-    assert (artist && *artist);
+    assert (artist && !strncmp (artist, "mike"));
 
     log ("+ got '%s' as Artist\n", artist);
 
