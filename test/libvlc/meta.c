4c22822980aac3a89898f6aaa2fb117114adc4b0
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-04-19 03:34:43 +0200

    test: Use my copyright in libvlc/meta.c, and make sure the artist isn't an empty string.

--- test/libvlc/meta.c
+++ test/libvlc/meta.c
@@ -49,7 +49,7 @@ static void test_meta (const char ** argv, int argc)
     artist = libvlc_media_get_meta( media, libvlc_meta_Artist, &ex );
     catch ();
 
-    assert (artist);
+    assert (artist && *artist);
 
     free (artist);
     libvlc_media_release (media);
