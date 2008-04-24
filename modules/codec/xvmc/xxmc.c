50ce52514c1bc2d4a38b3836fa80c47cfb48a2b5
Jean-Paul Saman <jpsaman@videolan.org>
2008-04-24 23:48:21 +0200

    Lower score of modules/xvmc/xxmc.c because it is not as stable as libmpeg2.

--- modules/codec/xvmc/xxmc.c
+++ modules/codec/xvmc/xxmc.c
@@ -101,7 +101,7 @@ static picture_t *GetNewPicture( decoder_t *, uint8_t ** );
  *****************************************************************************/
 vlc_module_begin();
     set_description( _("MPEG I/II hw video decoder (using libmpeg2)") );
-    set_capability( "decoder", 160 );
+    set_capability( "decoder", 140 );
     set_callbacks( OpenDecoder, CloseDecoder );
     add_shortcut( "xxmc" );
 vlc_module_end();
