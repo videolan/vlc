4b8ce41a01d64ef213fde735fc73e6323aa0cdee
Rémi Denis-Courmont <rem@videolan.org>
2008-05-01 23:08:41 +0300

    We need <inttypes.h>, always

--- include/vlc_common.h
+++ include/vlc_common.h
@@ -46,6 +46,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <inttypes.h>
 
 #ifdef HAVE_SYS_TYPES_H
 #   include <sys/types.h>
@@ -54,44 +55,6 @@
 /*****************************************************************************
  * Basic types definitions
  *****************************************************************************/
-#if defined( HAVE_INTTYPES_H )
-#   include <inttypes.h>
-#elif defined( SYS_CYGWIN )
-    /* Cygwin only defines half of these... */
-    typedef u_int8_t            uint8_t;
-    typedef u_int16_t           uint16_t;
-    typedef u_int32_t           uint32_t;
-    typedef u_int64_t           uint64_t;
-#else
-    /* Fallback types (very x86-centric, sorry) */
-    typedef unsigned char       uint8_t;
-    typedef signed char         int8_t;
-    typedef unsigned short      uint16_t;
-    typedef signed short        int16_t;
-    typedef unsigned int        uint32_t;
-    typedef signed int          int32_t;
-#   if defined( _MSC_VER ) \
-      || defined( UNDER_CE ) \
-      || ( defined( WIN32 ) && !defined( __MINGW32__ ) )
-    typedef unsigned __int64    uint64_t;
-    typedef signed __int64      int64_t;
-#   else
-    typedef unsigned long long  uint64_t;
-    typedef signed long long    int64_t;
-#   endif
-    typedef uint32_t            uintptr_t;
-    typedef int32_t             intptr_t;
-#endif
-
-/* Systems that don't have stdint.h may not define INT64_MIN and
-   INT64_MAX */
-#ifndef INT64_MIN
-#define INT64_MIN (-9223372036854775807LL-1)
-#endif
-#ifndef INT64_MAX
-#define INT64_MAX (9223372036854775807LL)
-#endif
-
 #if defined( WIN32 ) || defined( UNDER_CE )
 #   include <malloc.h>
 #   ifndef PATH_MAX
