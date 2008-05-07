b8909e164c63626771d3751e94d1c3f8e7af6c13
Rémi Denis-Courmont <rem@videolan.org>
2008-05-07 21:50:25 +0300

    set_destructor has no need for the structure lock

--- src/misc/objects.c
+++ src/misc/objects.c
@@ -307,10 +307,7 @@ void __vlc_object_set_destructor( vlc_object_t *p_this,
                                   vlc_destructor_t pf_destructor )
 {
     vlc_object_internals_t *p_priv = vlc_internals(p_this );
-
-    vlc_mutex_lock( &structure_lock );
     p_priv->pf_destructor = pf_destructor;
-    vlc_mutex_unlock( &structure_lock );
 }
 
 /**
