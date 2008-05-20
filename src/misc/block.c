c65b4ea96c47550a8876513222aab4b81cfc42dd
Rémi Denis-Courmont <rem@videolan.org>
2008-05-20 22:57:54 +0300

    (Almost) pread() for Win32

--- src/misc/block.c
+++ src/misc/block.c
@@ -228,6 +228,25 @@ block_t *block_mmap_Alloc (void *addr, size_t length)
 }
 #endif
 
+#ifdef WIN32
+static
+ssize_t pread (int fd, void *buf, size_t count, off_t offset)
+{
+    HANDLE handle = (HANDLE)(intptr_t)_get_osfhandle (fd);
+    if (handle == INVALID_HANDLE_VALUE)
+        return -1;
+
+    OVERLAPPED olap = { .Offset = offset, .OffsetHigh = (offset >> 32), };
+    DWORD written;
+    /* This braindead API will override the file pointer even if we specify
+     * an explicit read offset... So do not expect this to mix well with
+     * regular read() calls. */
+    if (ReadFile (handle, buf, count, &written, &olap))
+        return written;
+    return -1;
+}
+#endif
+
 /*****************************************************************************
  * block_fifo_t management
  *****************************************************************************/
