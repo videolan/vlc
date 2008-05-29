003c1216d89735a273e075c66a3af0f7655a54ae
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-05-29 23:32:11 +0200

    standard: Special case for AI_NUMERICSERV (if not defined).

--- modules/stream_out/standard.c
+++ modules/stream_out/standard.c
@@ -394,7 +394,10 @@ static int Open( vlc_object_t *p_this )
             .ai_family = AF_UNSPEC,
             .ai_socktype = SOCK_DGRAM,
             .ai_protocol = 0,
-            .ai_flags = AI_NUMERICHOST | AI_NUMERICSERV
+            .ai_flags = AI_NUMERICHOST
+#ifdef AI_NUMERICSERV
+                      | AI_NUMERICSERV
+#endif
         };
         char *shost = var_GetNonEmptyString (p_access, "src-addr");
         char *dhost = var_GetNonEmptyString (p_access, "dst-addr");
