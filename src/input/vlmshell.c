90ab070189879c021ab380e9505aeb25e1e6e8ee
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-05-31 23:09:10 +0300

    Fix memory leak (CID 95)

--- src/input/vlmshell.c
+++ src/input/vlmshell.c
@@ -968,17 +968,12 @@ vlm_media_sys_t *vlm_MediaSearch( vlm_t *vlm, const char *psz_name )
  *****************************************************************************/
 static vlm_schedule_sys_t *vlm_ScheduleNew( vlm_t *vlm, const char *psz_name )
 {
-    vlm_schedule_sys_t *p_sched = malloc( sizeof( vlm_schedule_sys_t ) );
-
-    if( !p_sched )
-    {
-        return NULL;
-    }
-
     if( !psz_name )
-    {
         return NULL;
-    }
+
+    vlm_schedule_sys_t *p_sched = malloc( sizeof( vlm_schedule_sys_t ) );
+    if( !p_sched )
+        return NULL;
 
     p_sched->psz_name = strdup( psz_name );
     p_sched->b_enabled = false;
