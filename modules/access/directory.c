37708112ca12f0bacbed99fc5a1a7e774b7a8511
Jean-Baptiste Kempf <jb@videolan.org>
2008-06-14 15:39:57 -0700

    Remove unused variable.

--- modules/access/directory.c
+++ modules/access/directory.c
@@ -199,7 +199,7 @@ static ssize_t ReadNull( access_t *p_access, uint8_t *p_buffer, size_t i_len)
 static ssize_t Read( access_t *p_access, uint8_t *p_buffer, size_t i_len)
 {
     char               *psz;
-    int                 i_mode, i_activity;
+    int                 i_mode;
     char               *psz_name = strdup (p_access->psz_path);
 
     if( psz_name == NULL )
