a1123b0a179ebeba90f29d0456dee475703e2775
Pierre d'Herbemont <pdherbemont@videolan.org>
2008-06-15 15:43:29 +0200

    playlist: Fix a typo in event detaching.

--- src/playlist/item.c
+++ src/playlist/item.c
@@ -138,16 +138,16 @@ static void install_input_item_observer( playlist_item_t * p_item )
 static void uninstall_input_item_observer( playlist_item_t * p_item )
 {
     vlc_event_manager_t * p_em = &p_item->p_input->event_manager;
+    vlc_event_detach( p_em, vlc_InputItemSubItemAdded,
+                      input_item_subitem_added, p_item );
     vlc_event_detach( p_em, vlc_InputItemMetaChanged,
                       input_item_changed, p_item );
     vlc_event_detach( p_em, vlc_InputItemDurationChanged,
                       input_item_changed, p_item );
-    vlc_event_detach( p_em, vlc_InputItemSubItemAdded,
-                      input_item_subitem_added, p_item );
     vlc_event_detach( p_em, vlc_InputItemNameChanged,
-                      input_item_subitem_added, p_item );
+                      input_item_changed, p_item );
     vlc_event_detach( p_em, vlc_InputItemInfoChanged,
-                      input_item_subitem_added, p_item );
+                      input_item_changed, p_item );
 }
 
 /*****************************************************************************
