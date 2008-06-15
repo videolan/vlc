videolan/vlc

commit ba06700b854653f4c1af9175aefcb92796d8fd2e

Kill a warning on windows.

modules/access/smb.c
@@ -444,7 +444,8 @@ static void Win32AddConnection( access_t *p_access, char *psz_path,
     char psz_remote[MAX_PATH], psz_server[MAX_PATH], psz_share[MAX_PATH];
     NETRESOURCE net_resource;
     DWORD i_result;
-    char *psz_parser;
+    char *ps_parser;
+    VLC_UNUSED( psz_domain );
 
     HINSTANCE hdll = LoadLibrary(_T("MPR.DLL"));
     if( !hdll )

commit aa1388193ede252fe2a5d181b921d5ded7205953

Fix typo introduced in [ba06700b854653f4c1af9175aefcb92796d8fd2e]
Sorry about that.

modules/access/smb.c
@@ -444,7 +444,7 @@ static void Win32AddConnection( access_t *p_access, char *psz_path,
     char psz_remote[MAX_PATH], psz_server[MAX_PATH], psz_share[MAX_PATH];
     NETRESOURCE net_resource;
     DWORD i_result;
-    char *ps_parser;
+    char *psz_parser;
     VLC_UNUSED( psz_domain );
 
     HINSTANCE hdll = LoadLibrary(_T("MPR.DLL"));