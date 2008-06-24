4f541d3b05be7729e48a28e5daf8497102598bb8
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-06-24 23:25:02 +0300

    We actually need to deliver the signal synchronously here
    Otherwise I sometimes get:
    [????????] x11 video output error: X11 request 2.0 failed with error
    code 3:
     BadWindow (invalid Window parameter)
     X Error of failed request:  BadWindow (invalid Window parameter)
     Major opcode of failed request:  2 (X_ChangeWindowAttributes)
     Resource id in failed request:  0x1000027
     Serial number of failed request:  26
     Current serial number in output stream:  27

--- modules/gui/qt4/components/interface_widgets.cpp
+++ modules/gui/qt4/components/interface_widgets.cpp
@@ -74,7 +74,7 @@ VideoWidget::VideoWidget( intf_thread_t *_p_i ) : QFrame( NULL ), p_intf( _p_i )
     setAttribute( Qt::WA_PaintOnScreen, true );
 
     /* The core can ask through a callback to show the video. */
-    CONNECT( this, askVideoWidgetToShow(), this, show() );
+    connect( this, SIGNAL(askVideoWidgetToShow()), this, SLOT(show()), Qt::BlockingQueuedConnection );
 
     /* The core can ask through a callback to resize the video */
    // CONNECT( this, askResize( int, int ), this, SetSizing( int, int ) );
