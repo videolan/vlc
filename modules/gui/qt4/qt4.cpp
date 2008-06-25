c4c75ec7733e6dbceff2babccc73b21e733a8f3a
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-06-25 22:10:27 +0300

    Qt4: small memory leak

--- modules/gui/qt4/qt4.cpp
+++ modules/gui/qt4/qt4.cpp
@@ -403,8 +403,14 @@ static void Init( intf_thread_t *p_intf )
 
     /* And quit */
 
+    windowLock.lock ();
+    var_Get (p_intf, "window_widget", &val);
+    if (val.p_address)
+        delete (QPointer<MainInterface> *)val.p_address;
     val.p_address = NULL;
     var_Set (p_intf, "window_widget", val);
+    windowLock.unlock();
+
     /* Destroy first the main interface because it is connected to some slots
        in the MainInputManager */
     delete p_intf->p_sys->p_mi;
