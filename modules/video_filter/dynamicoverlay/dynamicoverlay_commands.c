6ce6dc06545e12f9f04c6afe60c99e06fdc71502
Rémi Duraffort <ivoire@videolan.org>
2008-07-04 20:59:29 +0200

    Fix non utf-8 encoding.

--- modules/video_filter/dynamicoverlay/dynamicoverlay_commands.c
+++ modules/video_filter/dynamicoverlay/dynamicoverlay_commands.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2008 the VideoLAN team
  * $Id$
  *
- * Author: SÃ¸ren BÃ¸g <avacore@videolan.org>
+ * Author: Soren Bog <avacore@videolan.org>
  *         Jean-Paul Saman <jpsaman@videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify
