b588a4430681501a8f5ce2ce2fc9cdc684afe634
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-07-06 14:24:01 +0300

    Factorize a translated message

--- src/extras/getopt.c
+++ src/extras/getopt.c
@@ -707,12 +707,15 @@ int
             {
                 if (argv[optind][1] == '-')
                     /* --option */
-                    fprintf(stderr, _("%s: unrecognized option `--%s'\n"),
-                        argv[0], nextchar);
+                    fprintf(stderr, _("%s: unrecognized option `%s%s'\n"),
+                            "--", argv[0], nextchar);
                 else
+                {
+                    char t[2] = { argv[optind][0], '\0' };
                     /* +option or -option */
-                    fprintf(stderr, _("%s: unrecognized option `%c%s'\n"),
-                    argv[0], argv[optind][0], nextchar);
+                    fprintf(stderr, _("%s: unrecognized option `%s%s'\n"),
+                            argv[0], t, nextchar);
+                }
             }
             nextchar = (char *) "";
             optind++;
