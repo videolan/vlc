videolan/vlc

commit 23cc599d06abb5c84913c7399dd08c6b283fc49e

Patch by Bernie Purcell : "This is part of a former submission stripped out for easier  assimilation. It modifies vout_subpictures to use the existing alignment  field on each region, rather than the alignment on the subpicture plane  itself. Modifications are made to everywhere that depended on the former  behaviour, to make it use the new alignment location instead."

modules/codec/dvbsub.c
@@ -1492,6 +1492,7 @@ static subpicture_t *render( decoder_t *p_dec )
         }
         p_spu_region->i_x = p_regiondef->i_x;
         p_spu_region->i_y = p_regiondef->i_y;
+        p_spu_region->i_align = p_sys->i_spu_position;
         *pp_spu_region = p_spu_region;
         pp_spu_region = &p_spu_region->p_next;
 
@@ -1546,6 +1547,7 @@ static subpicture_t *render( decoder_t *p_dec )
             p_spu_region->psz_text = strdup( p_object_def->psz_text );
             p_spu_region->i_x = p_regiondef->i_x + p_object_def->i_x;
             p_spu_region->i_y = p_regiondef->i_y + p_object_def->i_y;
+            p_spu_region->i_align = p_sys->i_spu_position;
             *pp_spu_region = p_spu_region;
             pp_spu_region = &p_spu_region->p_next;
         }

include/vlc_vout.h
@@ -306,6 +306,8 @@ struct subpicture_t
 #define SUBPICTURE_ALIGN_RIGHT 0x2
 #define SUBPICTURE_ALIGN_TOP 0x4
 #define SUBPICTURE_ALIGN_BOTTOM 0x8
+#define SUBPICTURE_ALIGN_MASK ( SUBPICTURE_ALIGN_LEFT|SUBPICTURE_ALIGN_RIGHT| \
+                                SUBPICTURE_ALIGN_TOP |SUBPICTURE_ALIGN_BOTTOM )
 
 /*****************************************************************************
  * Prototypes

modules/codec/subsdec.c
@@ -448,7 +448,7 @@ static subpicture_t *ParseText( decoder_t *p_dec, block_t *p_block )
         p_dec->fmt_in.i_codec != VLC_FOURCC('u','s','f',' ') )
     {
         /* Normal text subs, easy markup */
-        p_spu->i_flags = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
+        p_spu->p_region->i_align = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
         p_spu->i_x = p_sys->i_align ? 20 : 0;
         p_spu->i_y = 10;
 
@@ -574,7 +574,7 @@ static void ParseUSFString( decoder_t *p_dec, char *psz_subtitle, subpicture_t *
 
     if( p_style == NULL )
     {
-        p_spu->i_flags = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
+        p_spu->p_region->i_align = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
         p_spu->i_x = p_sys->i_align ? 20 : 0;
         p_spu->i_y = 10;
     }
@@ -582,7 +582,7 @@ static void ParseUSFString( decoder_t *p_dec, char *psz_subtitle, subpicture_t *
     {
         msg_Dbg( p_dec, "style is: %s", p_style->psz_stylename);
         p_spu->p_region->p_style = &p_style->font_style;
-        p_spu->i_flags = p_style->i_align;
+        p_spu->p_region->i_align = p_style->i_align;
     }
 }
 
@@ -678,7 +678,7 @@ static void ParseSSAString( decoder_t *p_dec, char *psz_subtitle, subpicture_t *
     p_spu->p_region->psz_text = psz_new_subtitle;
     if( p_style == NULL )
     {
-        p_spu->i_flags = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
+        p_spu->p_region->i_align = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
         p_spu->i_x = p_sys->i_align ? 20 : 0;
         p_spu->i_y = 10;
     }
@@ -686,7 +686,7 @@ static void ParseSSAString( decoder_t *p_dec, char *psz_subtitle, subpicture_t *
     {
         msg_Dbg( p_dec, "style is: %s", p_style->psz_stylename);
         p_spu->p_region->p_style = &p_style->font_style;
-        p_spu->i_flags = p_style->i_align;
+        p_spu->p_region->i_align = p_style->i_align;
         if( p_style->i_align & SUBPICTURE_ALIGN_LEFT )
         {
             p_spu->i_x = (i_margin_l) ? i_margin_l : p_style->i_margin_h;
@@ -1147,7 +1147,7 @@ static void ParseSSAHeader( decoder_t *p_dec )
                     //p_style->font_style.f_angle = f_angle;
 
                     p_style->i_align = 0;
-                    if( i_align == 0x1 || i_align == 0x4 || i_align == 0x1 ) p_style->i_align |= SUBPICTURE_ALIGN_LEFT;
+                    if( i_align == 0x1 || i_align == 0x4 || i_align == 0x7 ) p_style->i_align |= SUBPICTURE_ALIGN_LEFT;
                     if( i_align == 0x3 || i_align == 0x6 || i_align == 0x9 ) p_style->i_align |= SUBPICTURE_ALIGN_RIGHT;
                     if( i_align == 0x7 || i_align == 0x8 || i_align == 0x9 ) p_style->i_align |= SUBPICTURE_ALIGN_TOP;
                     if( i_align == 0x1 || i_align == 0x2 || i_align == 0x3 ) p_style->i_align |= SUBPICTURE_ALIGN_BOTTOM;

modules/codec/telx.c
@@ -711,7 +711,7 @@ static subpicture_t *Decode( decoder_t *p_dec, block_t **pp_block )
     }
 
     /* Normal text subs, easy markup */
-    p_spu->i_flags = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
+    p_spu->p_region->i_align = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
     p_spu->i_x = p_sys->i_align ? 20 : 0;
     p_spu->i_y = 10;
 

modules/video_filter/logo.c
@@ -882,14 +882,14 @@ static subpicture_t *Filter( filter_t *p_filter, mtime_t date )
     /*  where to locate the logo: */
     if( p_sys->posx < 0 || p_sys->posy < 0 )
     {   /* set to one of the 9 relative locations */
-        p_spu->i_flags = p_sys->pos;
+        p_spu->p_region->i_align = p_sys->pos;
         p_spu->i_x = 0;
         p_spu->i_y = 0;
         p_spu->b_absolute = VLC_FALSE;
     }
     else
     {   /*  set to an absolute xy, referenced to upper left corner */
-        p_spu->i_flags = OSD_ALIGN_LEFT | OSD_ALIGN_TOP;
+        p_spu->p_region->i_align = OSD_ALIGN_LEFT | OSD_ALIGN_TOP;
         p_spu->i_x = p_sys->posx;
         p_spu->i_y = p_sys->posy;
         p_spu->b_absolute = VLC_TRUE;

modules/video_filter/marq.c
@@ -296,14 +296,14 @@ static subpicture_t *Filter( filter_t *p_filter, mtime_t date )
     /*  where to locate the string: */
     if( p_sys->i_xoff < 0 || p_sys->i_yoff < 0 )
     {   /* set to one of the 9 relative locations */
-        p_spu->i_flags = p_sys->i_pos;
+        p_spu->p_region->i_align = p_sys->i_pos;
         p_spu->i_x = 0;
         p_spu->i_y = 0;
         p_spu->b_absolute = VLC_FALSE;
     }
     else
     {   /*  set to an absolute xy, referenced to upper left corner */
-        p_spu->i_flags = OSD_ALIGN_LEFT | OSD_ALIGN_TOP;
+        p_spu->p_region->i_align = OSD_ALIGN_LEFT | OSD_ALIGN_TOP;
         p_spu->i_x = p_sys->i_xoff;
         p_spu->i_y = p_sys->i_yoff;
         p_spu->b_absolute = VLC_TRUE;

modules/video_filter/mosaic.c
@@ -710,6 +710,7 @@ static subpicture_t *Filter( filter_t *p_filter, mtime_t date )
                     + ( i_row * p_sys->i_borderh ) / p_sys->i_rows
                     + ( row_inner_height - fmt_out.i_height ) / 2;
         }
+        p_region->i_align = p_sys->i_align;
 
         if( p_region_prev == NULL )
         {

modules/video_filter/rss.c
@@ -482,14 +482,14 @@ static subpicture_t *Filter( filter_t *p_filter, mtime_t date )
     /*  where to locate the string: */
     if( p_sys->i_xoff < 0 || p_sys->i_yoff < 0 )
     {   /* set to one of the 9 relative locations */
-        p_spu->i_flags = p_sys->i_pos;
+        p_spu->p_region->i_align = p_sys->i_pos;
         p_spu->i_x = 0;
         p_spu->i_y = 0;
         p_spu->b_absolute = VLC_FALSE;
     }
     else
     {   /*  set to an absolute xy, referenced to upper left corner */
-        p_spu->i_flags = OSD_ALIGN_LEFT | OSD_ALIGN_TOP;
+        p_spu->p_region->i_align = OSD_ALIGN_LEFT | OSD_ALIGN_TOP;
         p_spu->i_x = p_sys->i_xoff;
         p_spu->i_y = p_sys->i_yoff;
         p_spu->b_absolute = VLC_TRUE;

commit 9dc1a1ae673fbe4c9519de2e794e5855b15bff8a

Fix positioning of dvbsub. closes #1686
This was caused by commit 23cc599d06abb5c84913c7399dd08c6b283fc49e and an attempt at fixing it in commit 251648e32246c48729986e65678bac16e5061e47.

modules/codec/dvbsub.c
@@ -329,7 +329,7 @@ static int Open( vlc_object_t *p_this )
     i_posy = val.i_int;
 
     /* Check if subpicture position was overridden */
-    p_sys->b_absolute = false;
+    p_sys->b_absolute = true;
     p_sys->i_spu_x = p_sys->i_spu_y = 0;
 
     if( ( i_posx >= 0 ) && ( i_posy >= 0 ) )
@@ -1592,7 +1592,6 @@ static subpicture_t *render( decoder_t *p_dec )
 
     /* Correct positioning of SPU */
     p_spu->b_absolute = p_sys->b_absolute;
-    p_spu->i_flags = p_sys->i_spu_position;
     p_spu->i_x = p_sys->i_spu_x;
     p_spu->i_y = p_sys->i_spu_y;
     p_spu->i_original_picture_width = 720;