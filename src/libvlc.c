d632ffe1a6f5e6cea5dd796105e1708cdb0b3631
Rémi Duraffort <ivoire@videolan.org>
2008-07-31 17:55:29 +0200

    Prefer setenv to putenv (evenmore with local variables).

--- src/libvlc.c
+++ src/libvlc.c
@@ -1212,10 +1212,7 @@ static void SetLanguage ( const char *psz_lang )
      * the language at runtime under eg. Windows. Beware that this
      * makes the environment unconsistent when libvlc is unloaded and
      * should probably be moved to a safer place like vlc.c. */
-    static char psz_lcall[20];
-    snprintf( psz_lcall, 19, "LC_ALL=%s", psz_lang );
-    psz_lcall[19] = '\0';
-    putenv( psz_lcall );
+    setenv( "LC_ALL", psz_lang, 1 );
 #endif
 
     setlocale( LC_ALL, psz_lang );
