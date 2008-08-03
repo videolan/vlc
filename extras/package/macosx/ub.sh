5e897851c774df59a3b2c5eb3f30fbb3d44d0e54
Felix Paul Kühne <fkuehne@videolan.org>
2008-08-03 20:29:18 +0200

    package/osx: make sure that we also put the platform-dependent modules in the ub oO

--- extras/package/macosx/ub.sh
+++ extras/package/macosx/ub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INTELROOT=/Volumes/vlc-0.8.6a/VLC.app
-PPCROOT=/Applications/VLC.app
-UBROOT=/Users/fpk/0.8.6a/VLC-release.app
+INTELROOT=/Applications/VLC.app
+PPCROOT=/Volumes/vlc-0.9.0-test3/VLC.app
+UBROOT=/Users/fpk/VLC-release.app
 
 for i in `ls $INTELROOT/Contents/MacOS/lib/`
 do
@@ -15,3 +15,7 @@ do
         lipo $INTELROOT/Contents/MacOS/modules/$i $PPCROOT/Contents/MacOS/modules/$i -create -output $UBROOT/Contents/MacOS/modules/$i
 done
 lipo $INTELROOT/Contents/MacOS/VLC $PPCROOT/Contents/MacOS/VLC -create -output $UBROOT/Contents/MacOS/VLC
+cp $INTELROOT/Contents/MacOS/modules/*mmx* $UBROOT/Contents/MacOS/modules/
+cp $INTELROOT/Contents/MacOS/modules/*sse* $UBROOT/Contents/MacOS/modules/
+cp $INTELROOT/Contents/MacOS/modules/*3dn* $UBROOT/Contents/MacOS/modules/
+cp $PPCROOT/Contents/MacOS/modules/*altivec* $UBROOT/Contents/MacOS/modules/
