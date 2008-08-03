7fa8ead1a2192a06c8d5f7709bb18a614337199c
Derk-Jan Hartman <hartman@videolan.org>
2008-08-03 03:50:00 +0200

    motiondetect: fix categorization of this module in the advanced prefs.

--- modules/video_filter/motiondetect.c
+++ modules/video_filter/motiondetect.c
@@ -48,9 +48,9 @@ static void Destroy   ( vlc_object_t * );
 vlc_module_begin();
     set_description( N_("Motion detect video filter") );
     set_shortname( N_( "Motion Detect" ));
-    set_capability( "video filter2", 0 );
     set_category( CAT_VIDEO );
     set_subcategory( SUBCAT_VIDEO_VFILTER );
+    set_capability( "video filter2", 0 );
 
     add_shortcut( "motion" );
     set_callbacks( Create, Destroy );
