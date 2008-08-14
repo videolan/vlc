72676243da6f631bdcf063a5584c672364510103
Derk-Jan Hartman <hartman@videolan.org>
2008-08-14 02:15:19 +0200

    Revert "Add Private Stream 1 case according to the norm." Some of my TS files were no longer playing. Verified with the spec, and "Private stream 1" is not in play in this case for the PES packet parsing. (see table 2-17 of ISO 13818-1)
    This reverts commit 0c791020418689ebd96b7fee640ab03c4ef69709.

--- modules/demux/ts.c
+++ modules/demux/ts.c
@@ -1668,7 +1668,6 @@ static void ParsePES( demux_t *p_demux, ts_pid_t *pid )
     switch( header[3] )
     {
         case 0xBC:  /* Program stream map */
-        case 0xBD:  /* Private stream 1 */
         case 0xBE:  /* Padding */
         case 0xBF:  /* Private stream 2 */
         case 0xF0:  /* ECM */
