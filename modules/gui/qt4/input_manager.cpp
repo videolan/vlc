videolan/vlc

commit f9b25cf4378f25164f7b4bebef7cb03876144309

Qt: Fix "chapter buttons not hiding when stop is clicked" issue
http://forum.videolan.org/viewtopic.php?f=14&t=49386

modules/gui/qt4/input_manager.cpp
@@ -181,6 +181,12 @@ void InputManager::customEvent( QEvent *event )
          type != ItemStateChanged_Type )
         return;
 
+    if( type == ItemStateChanged_Type )
+    {
+        UpdateStatus();
+        UpdateNavigation();
+    }
+
     if( !hasInput() ) return;
 
     if( ( type != PositionUpdate_Type  &&
@@ -242,7 +248,8 @@ void InputManager::UpdateNavigation()
 {
     /* Update navigation status */
     vlc_value_t val; val.i_int = 0;
-    var_Change( p_input, "title", VLC_VAR_CHOICESCOUNT, &val, NULL );
+    if( hasInput() )
+        var_Change( p_input, "title", VLC_VAR_CHOICESCOUNT, &val, NULL );
     if( val.i_int > 0 )
     {
         val.i_int = 0;
@@ -355,8 +362,6 @@ void InputManager::UpdateTeletext()
     else
         telexToggle( false );
 }
-
-
 
 void InputManager::UpdateArt()
 {

commit 83782f296d303e791b45c1747c0d8e0c1f47321c

Fix a segfault regression introduced in [f9b25cf4378f25164f7b4bebef7cb03876144309]

modules/gui/qt4/input_manager.cpp
@@ -183,8 +183,8 @@ void InputManager::customEvent( QEvent *event )
 
     if( type == ItemStateChanged_Type )
     {
-        UpdateStatus();
         UpdateNavigation();
+        UpdateTelextext();
     }
 
     if( !hasInput() ) return;