32fd321a8427696229ceb2e5ae737560c9e06a11
Jean-Philippe Andre <jpeg@via.ecp.fr>
2008-08-23 01:59:08 -0400

    Try to close ticket '#1775' (Qt intf startup size is too big) Simply resize to sizeHint() on startup.

--- modules/gui/qt4/main_interface.cpp
+++ modules/gui/qt4/main_interface.cpp
@@ -237,6 +237,7 @@ MainInterface::MainInterface( intf_thread_t *_p_intf ) : QVLCMW( _p_intf )
 
     /* Update the geometry TODO: is it useful ?*/
     updateGeometry();
+    resize( sizeHint() );
 
     /*****************************************************
      * End everything by creating the Systray Management *
