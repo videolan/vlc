916717a706f7d658694d8ff33f770b1d3ba9e2bc
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-08-26 08:15:23 +0300

    Win32: do not add argv[0] (vlc.exe) to the playlist

--- bin/winvlc.c
+++ bin/winvlc.c
@@ -71,7 +71,7 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance,
     libvlc_exception_init (&dummy);
 
     /* Initialize libvlc */
-    libvlc_instance_t *vlc = libvlc_new (argc, (const char **)argv, &ex);
+    libvlc_instance_t *vlc = libvlc_new (argc, (const char **)(argv + 1), &ex);
     if (vlc != NULL)
     {
         libvlc_add_intf (vlc, NULL, &ex);
