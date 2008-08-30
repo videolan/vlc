2734c1b4d4f9ddbeff77ad5929c60d486a3151bf
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-08-30 23:49:36 +0300

    memset outside of lock

--- src/input/es_out.c
+++ src/input/es_out.c
@@ -1039,8 +1039,8 @@ static es_out_id_t *EsOutAdd( es_out_t *out, es_format_t *fmt )
 
         es->i_channel = p_sys->i_audio;
 
-        vlc_mutex_lock( &p_input->p->input.p_item->lock );
         memset( &rg, 0, sizeof(rg) );
+        vlc_mutex_lock( &p_input->p->input.p_item->lock );
         vlc_audio_replay_gain_MergeFromMeta( &rg, p_input->p->input.p_item->p_meta );
         vlc_mutex_unlock( &p_input->p->input.p_item->lock );
 
