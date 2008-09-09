13c381a0f15e7d5657eb02ddfdf91e9fe95a7cc4
Antoine Cellerier <dionoea@videolan.org>
2008-09-09 18:12:53 +0200

    Make it possible to use configure script for out of dir builds.

--- extras/package/win32/configure-cygwin.sh
+++ extras/package/win32/configure-cygwin.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
+root=$(echo $0|sed 's#extras/package/win32/configure-cygwin.sh##')./
+
 PATH="/usr/win32/bin:$PATH" \
 PKG_CONFIG_LIBDIR=/usr/win32/lib/pkgconfig \
 CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
 LDFLAGS="-L/usr/win32/lib" \
 CC="gcc -mno-cygwin" CXX="g++ -mno-cygwin" \
-CONFIG="./configure --host=i686-pc-mingw32 --disable-taglib --disable-mkv --enable-debug" \
-sh extras/package/win32/configure-common.sh
+CONFIG="${root}configure --host=i686-pc-mingw32 --disable-taglib --disable-mkv --enable-debug" \
+sh ${root}extras/package/win32/configure-common.sh
