bbb45caacac5a39003c87e139a575c13acb09821
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-09-14 10:47:49 +0300

    Remove VLC_OBJECT_ANNOUNCE

--- include/vlc_objects.h
+++ include/vlc_objects.h
@@ -42,7 +42,6 @@
 #define VLC_OBJECT_AOUT        (-10)
 #define VLC_OBJECT_PACKETIZER  (-13)
 #define VLC_OBJECT_ENCODER     (-14)
-#define VLC_OBJECT_ANNOUNCE    (-17)
 #define VLC_OBJECT_OPENGL      (-21)
 #define VLC_OBJECT_OSDMENU     (-28)
 /* Please add new object types below -34 */
