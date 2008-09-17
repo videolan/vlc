c7db9ba61adbdec970690dce7e8325d9213c9499
Jean-Baptiste Kempf <jb@videolan.org>
2008-09-17 11:52:49 -0700

    Add .nuv in interface and installer.

--- include/vlc_interface.h
+++ include/vlc_interface.h
@@ -177,7 +177,7 @@ typedef enum vlc_dialog {
 
 #define EXTENSIONS_VIDEO "*.asf;*.avi;*.divx;*.dv;*.flv;*.gxf;*.m1v;*.m2v;" \
                          "*.m2ts;*.m4v;*.mkv;*.mov;*.mp2;*.mp4;*.mpeg;*.mpeg1;" \
-                         "*.mpeg2;*.mpeg4;*.mpg;*.mts;*.mxf;" \
+                         "*.mpeg2;*.mpeg4;*.mpg;*.mts;*.mxf;*.nuv;" \
                          "*.ogg;*.ogm;*.ogv;*.ogx;" \
                          "*.ps;*.ts;*.vob;*.wmv"
 
