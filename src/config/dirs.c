ec2e9e013e3ccdf60ef9303fa50683b251408026
Jean-Baptiste Kempf <jb@videolan.org>
2008-09-17 17:50:25 -0700

    ConfigDir mess up on windows.

--- src/config/dirs.c
+++ src/config/dirs.c
@@ -87,8 +87,8 @@ static const char *GetDir( bool b_appdata, bool b_common_appdata )
 # else
     /* Get the "Application Data" folder for the current user */
     if( S_OK == SHGetFolderPathW( NULL,
-              (b_appdata ? CSIDL_APPDATA :
-               (b_common_appdata ? CSIDL_PERSONAL : CSIDL_COMMON_APPDATA))
+              ( b_appdata ? CSIDL_APPDATA :
+               ( b_common_appdata ? CSIDL_COMMON_APPDATA: CSIDL_PERSONAL ) )
               | CSIDL_FLAG_CREATE,
                                   NULL, SHGFP_TYPE_CURRENT, wdir ) )
 # endif
