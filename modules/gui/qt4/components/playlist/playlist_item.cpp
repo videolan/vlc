ad87b9243e4314f6cef58362cfddb525db35de6e
Ilkka Ollakka <ileoo@videolan.org>
2008-09-21 14:43:21 +0300

    use items showflags, parentItem can be NULL

--- modules/gui/qt4/components/playlist/playlist_item.cpp
+++ modules/gui/qt4/components/playlist/playlist_item.cpp
@@ -185,10 +185,10 @@ void PLItem::update( playlist_item_t *p_item, bool iscurrent )
         return;
     }
 
-    assert( parentItem->i_showflags < COLUMN_END );
+    assert( i_showflags < COLUMN_END );
 
     /* Meta: ID */
-    if( parentItem->i_showflags & COLUMN_NUMBER )
+    if( i_showflags & COLUMN_NUMBER )
     {
         QModelIndex idx = model->index( this, 0 );
         item_col_strings.append( QString::number( idx.row() + 1 ) );
@@ -196,7 +196,7 @@ void PLItem::update( playlist_item_t *p_item, bool iscurrent )
     /* Other meta informations */
     for( uint32_t i_index=2; i_index < COLUMN_END; i_index <<= 1 )
     {
-        if( parentItem->i_showflags & i_index )
+        if( i_showflags & i_index )
         {
             char *psz = psz_column_meta( p_item->p_input, i_index );
             item_col_strings.append( qfu( psz ) );
