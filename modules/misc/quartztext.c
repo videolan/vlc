74d20fa836f656eff2e713e7a96c819c2ce1000e
Laurent Aimar <fenrir@videolan.org>
2008-09-22 20:19:29 +0200

    First pass for extracting common text renderer code.

--- modules/misc/quartztext.c
+++ modules/misc/quartztext.c
@@ -43,6 +43,8 @@
 
 #include <Carbon/Carbon.h>
 
+#include "text_renderer.h"
+
 #define DEFAULT_FONT           "Arial Black"
 #define DEFAULT_FONT_COLOR     0xffffff
 #define DEFAULT_REL_FONT_SIZE  16
@@ -125,16 +127,6 @@ vlc_module_begin();
     add_shortcut( "text" );
     set_callbacks( Create, Destroy );
 vlc_module_end();
-
-typedef struct font_stack_t font_stack_t;
-struct font_stack_t
-{
-    char          *psz_name;
-    int            i_size;
-    uint32_t       i_color;            // ARGB
-
-    font_stack_t  *p_next;
-};
 
 typedef struct offscreen_bitmap_t offscreen_bitmap_t;
 struct offscreen_bitmap_t
@@ -496,92 +488,6 @@ static ATSUStyle CreateStyle( char *psz_fontname, int i_font_size, uint32_t i_fo
     return NULL;
 }
 
-static int PushFont( font_stack_t **p_font, const char *psz_name, int i_size,
-                     uint32_t i_color )
-{
-    font_stack_t *p_new;
-
-    if( !p_font )
-        return VLC_EGENERIC;
-
-    p_new = malloc( sizeof( font_stack_t ) );
-    if( ! p_new )
-        return VLC_ENOMEM;
-
-    p_new->p_next = NULL;
-
-    if( psz_name )
-        p_new->psz_name = strdup( psz_name );
-    else
-        p_new->psz_name = NULL;
-
-    p_new->i_size   = i_size;
-    p_new->i_color  = i_color;
-
-    if( !*p_font )
-    {
-        *p_font = p_new;
-    }
-    else
-    {
-        font_stack_t *p_last;
-
-        for( p_last = *p_font;
-             p_last->p_next;
-             p_last = p_last->p_next )
-        ;
-
-        p_last->p_next = p_new;
-    }
-    return VLC_SUCCESS;
-}
-
-static int PopFont( font_stack_t **p_font )
-{
-    font_stack_t *p_last, *p_next_to_last;
-
-    if( !p_font || !*p_font )
-        return VLC_EGENERIC;
-
-    p_next_to_last = NULL;
-    for( p_last = *p_font;
-         p_last->p_next;
-         p_last = p_last->p_next )
-    {
-        p_next_to_last = p_last;
-    }
-
-    if( p_next_to_last )
-        p_next_to_last->p_next = NULL;
-    else
-        *p_font = NULL;
-
-    free( p_last->psz_name );
-    free( p_last );
-
-    return VLC_SUCCESS;
-}
-
-static int PeekFont( font_stack_t **p_font, char **psz_name, int *i_size,
-                     uint32_t *i_color )
-{
-    font_stack_t *p_last;
-
-    if( !p_font || !*p_font )
-        return VLC_EGENERIC;
-
-    for( p_last=*p_font;
-         p_last->p_next;
-         p_last=p_last->p_next )
-    ;
-
-    *psz_name = p_last->psz_name;
-    *i_size   = p_last->i_size;
-    *i_color  = p_last->i_color;
-
-    return VLC_SUCCESS;
-}
-
 static ATSUStyle GetStyleFromFontStack( filter_sys_t *p_sys,
         font_stack_t **p_fonts, bool b_bold, bool b_italic,
         bool b_uline )
@@ -590,278 +496,16 @@ static ATSUStyle GetStyleFromFontStack( filter_sys_t *p_sys,
 
     char     *psz_fontname = NULL;
     uint32_t  i_font_color = p_sys->i_font_color;
+    uint32_t  i_karaoke_bg_color = i_font_color; /* Use it */
     int       i_font_size  = p_sys->i_font_size;
 
     if( VLC_SUCCESS == PeekFont( p_fonts, &psz_fontname, &i_font_size,
-                                 &i_font_color ))
+                                 &i_font_color, &i_karaoke_bg_color ))
     {
         p_style = CreateStyle( psz_fontname, i_font_size, i_font_color,
                                b_bold, b_italic, b_uline );
     }
     return p_style;
-}
-
-static const struct {
-    const char *psz_name;
-    uint32_t   i_value;
-} p_html_colors[] = {
-    /* Official html colors */
-    { "Aqua",    0x00FFFF },
-    { "Black",   0x000000 },
-    { "Blue",    0x0000FF },
-    { "Fuchsia", 0xFF00FF },
-    { "Gray",    0x808080 },
-    { "Green",   0x008000 },
-    { "Lime",    0x00FF00 },
-    { "Maroon",  0x800000 },
-    { "Navy",    0x000080 },
-    { "Olive",   0x808000 },
-    { "Purple",  0x800080 },
-    { "Red",     0xFF0000 },
-    { "Silver",  0xC0C0C0 },
-    { "Teal",    0x008080 },
-    { "White",   0xFFFFFF },
-    { "Yellow",  0xFFFF00 },
-
-    /* Common ones */
-    { "AliceBlue", 0xF0F8FF },
-    { "AntiqueWhite", 0xFAEBD7 },
-    { "Aqua", 0x00FFFF },
-    { "Aquamarine", 0x7FFFD4 },
-    { "Azure", 0xF0FFFF },
-    { "Beige", 0xF5F5DC },
-    { "Bisque", 0xFFE4C4 },
-    { "Black", 0x000000 },
-    { "BlanchedAlmond", 0xFFEBCD },
-    { "Blue", 0x0000FF },
-    { "BlueViolet", 0x8A2BE2 },
-    { "Brown", 0xA52A2A },
-    { "BurlyWood", 0xDEB887 },
-    { "CadetBlue", 0x5F9EA0 },
-    { "Chartreuse", 0x7FFF00 },
-    { "Chocolate", 0xD2691E },
-    { "Coral", 0xFF7F50 },
-    { "CornflowerBlue", 0x6495ED },
-    { "Cornsilk", 0xFFF8DC },
-    { "Crimson", 0xDC143C },
-    { "Cyan", 0x00FFFF },
-    { "DarkBlue", 0x00008B },
-    { "DarkCyan", 0x008B8B },
-    { "DarkGoldenRod", 0xB8860B },
-    { "DarkGray", 0xA9A9A9 },
-    { "DarkGrey", 0xA9A9A9 },
-    { "DarkGreen", 0x006400 },
-    { "DarkKhaki", 0xBDB76B },
-    { "DarkMagenta", 0x8B008B },
-    { "DarkOliveGreen", 0x556B2F },
-    { "Darkorange", 0xFF8C00 },
-    { "DarkOrchid", 0x9932CC },
-    { "DarkRed", 0x8B0000 },
-    { "DarkSalmon", 0xE9967A },
-    { "DarkSeaGreen", 0x8FBC8F },
-    { "DarkSlateBlue", 0x483D8B },
-    { "DarkSlateGray", 0x2F4F4F },
-    { "DarkSlateGrey", 0x2F4F4F },
-    { "DarkTurquoise", 0x00CED1 },
-    { "DarkViolet", 0x9400D3 },
-    { "DeepPink", 0xFF1493 },
-    { "DeepSkyBlue", 0x00BFFF },
-    { "DimGray", 0x696969 },
-    { "DimGrey", 0x696969 },
-    { "DodgerBlue", 0x1E90FF },
-    { "FireBrick", 0xB22222 },
-    { "FloralWhite", 0xFFFAF0 },
-    { "ForestGreen", 0x228B22 },
-    { "Fuchsia", 0xFF00FF },
-    { "Gainsboro", 0xDCDCDC },
-    { "GhostWhite", 0xF8F8FF },
-    { "Gold", 0xFFD700 },
-    { "GoldenRod", 0xDAA520 },
-    { "Gray", 0x808080 },
-    { "Grey", 0x808080 },
-    { "Green", 0x008000 },
-    { "GreenYellow", 0xADFF2F },
-    { "HoneyDew", 0xF0FFF0 },
-    { "HotPink", 0xFF69B4 },
-    { "IndianRed", 0xCD5C5C },
-    { "Indigo", 0x4B0082 },
-    { "Ivory", 0xFFFFF0 },
-    { "Khaki", 0xF0E68C },
-    { "Lavender", 0xE6E6FA },
-    { "LavenderBlush", 0xFFF0F5 },
-    { "LawnGreen", 0x7CFC00 },
-    { "LemonChiffon", 0xFFFACD },
-    { "LightBlue", 0xADD8E6 },
-    { "LightCoral", 0xF08080 },
-    { "LightCyan", 0xE0FFFF },
-    { "LightGoldenRodYellow", 0xFAFAD2 },
-    { "LightGray", 0xD3D3D3 },
-    { "LightGrey", 0xD3D3D3 },
-    { "LightGreen", 0x90EE90 },
-    { "LightPink", 0xFFB6C1 },
-    { "LightSalmon", 0xFFA07A },
-    { "LightSeaGreen", 0x20B2AA },
-    { "LightSkyBlue", 0x87CEFA },
-    { "LightSlateGray", 0x778899 },
-    { "LightSlateGrey", 0x778899 },
-    { "LightSteelBlue", 0xB0C4DE },
-    { "LightYellow", 0xFFFFE0 },
-    { "Lime", 0x00FF00 },
-    { "LimeGreen", 0x32CD32 },
-    { "Linen", 0xFAF0E6 },
-    { "Magenta", 0xFF00FF },
-    { "Maroon", 0x800000 },
-    { "MediumAquaMarine", 0x66CDAA },
-    { "MediumBlue", 0x0000CD },
-    { "MediumOrchid", 0xBA55D3 },
-    { "MediumPurple", 0x9370D8 },
-    { "MediumSeaGreen", 0x3CB371 },
-    { "MediumSlateBlue", 0x7B68EE },
-    { "MediumSpringGreen", 0x00FA9A },
-    { "MediumTurquoise", 0x48D1CC },
-    { "MediumVioletRed", 0xC71585 },
-    { "MidnightBlue", 0x191970 },
-    { "MintCream", 0xF5FFFA },
-    { "MistyRose", 0xFFE4E1 },
-    { "Moccasin", 0xFFE4B5 },
-    { "NavajoWhite", 0xFFDEAD },
-    { "Navy", 0x000080 },
-    { "OldLace", 0xFDF5E6 },
-    { "Olive", 0x808000 },
-    { "OliveDrab", 0x6B8E23 },
-    { "Orange", 0xFFA500 },
-    { "OrangeRed", 0xFF4500 },
-    { "Orchid", 0xDA70D6 },
-    { "PaleGoldenRod", 0xEEE8AA },
-    { "PaleGreen", 0x98FB98 },
-    { "PaleTurquoise", 0xAFEEEE },
-    { "PaleVioletRed", 0xD87093 },
-    { "PapayaWhip", 0xFFEFD5 },
-    { "PeachPuff", 0xFFDAB9 },
-    { "Peru", 0xCD853F },
-    { "Pink", 0xFFC0CB },
-    { "Plum", 0xDDA0DD },
-    { "PowderBlue", 0xB0E0E6 },
-    { "Purple", 0x800080 },
-    { "Red", 0xFF0000 },
-    { "RosyBrown", 0xBC8F8F },
-    { "RoyalBlue", 0x4169E1 },
-    { "SaddleBrown", 0x8B4513 },
-    { "Salmon", 0xFA8072 },
-    { "SandyBrown", 0xF4A460 },
-    { "SeaGreen", 0x2E8B57 },
-    { "SeaShell", 0xFFF5EE },
-    { "Sienna", 0xA0522D },
-    { "Silver", 0xC0C0C0 },
-    { "SkyBlue", 0x87CEEB },
-    { "SlateBlue", 0x6A5ACD },
-    { "SlateGray", 0x708090 },
-    { "SlateGrey", 0x708090 },
-    { "Snow", 0xFFFAFA },
-    { "SpringGreen", 0x00FF7F },
-    { "SteelBlue", 0x4682B4 },
-    { "Tan", 0xD2B48C },
-    { "Teal", 0x008080 },
-    { "Thistle", 0xD8BFD8 },
-    { "Tomato", 0xFF6347 },
-    { "Turquoise", 0x40E0D0 },
-    { "Violet", 0xEE82EE },
-    { "Wheat", 0xF5DEB3 },
-    { "White", 0xFFFFFF },
-    { "WhiteSmoke", 0xF5F5F5 },
-    { "Yellow", 0xFFFF00 },
-    { "YellowGreen", 0x9ACD32 },
-
-    { NULL, 0 }
-};
-
-static int HandleFontAttributes( xml_reader_t *p_xml_reader,
-                                  font_stack_t **p_fonts, int i_scale )
-{
-    int        rv;
-    char      *psz_fontname = NULL;
-    uint32_t   i_font_color = 0xffffff;
-    int        i_font_alpha = 0;
-    int        i_font_size  = 24;
-
-    // Default all attributes to the top font in the stack -- in case not
-    // all attributes are specified in the sub-font
-    if( VLC_SUCCESS == PeekFont( p_fonts,
-                                 &psz_fontname,
-                                 &i_font_size,
-                                 &i_font_color ))
-    {
-        psz_fontname = strdup( psz_fontname );
-        i_font_size = i_font_size * 1000 / i_scale;
-    }
-    i_font_alpha = (i_font_color >> 24) & 0xff;
-    i_font_color &= 0x00ffffff;
-
-    while ( xml_ReaderNextAttr( p_xml_reader ) == VLC_SUCCESS )
-    {
-        char *psz_name = xml_ReaderName( p_xml_reader );
-        char *psz_value = xml_ReaderValue( p_xml_reader );
-
-        if( psz_name && psz_value )
-        {
-            if( !strcasecmp( "face", psz_name ) )
-            {
-                free( psz_fontname );
-                psz_fontname = strdup( psz_value );
-            }
-            else if( !strcasecmp( "size", psz_name ) )
-            {
-                if( ( *psz_value == '+' ) || ( *psz_value == '-' ) )
-                {
-                    int i_value = atoi( psz_value );
-
-                    if( ( i_value >= -5 ) && ( i_value <= 5 ) )
-                        i_font_size += ( i_value * i_font_size ) / 10;
-                    else if( i_value < -5 )
-                        i_font_size = - i_value;
-                    else if( i_value > 5 )
-                        i_font_size = i_value;
-                }
-                else
-                    i_font_size = atoi( psz_value );
-            }
-            else if( !strcasecmp( "color", psz_name ) )
-            {
-                if( psz_value[0] == '#' )
-                {
-                    i_font_color = strtol( psz_value + 1, NULL, 16 );
-                    i_font_color &= 0x00ffffff;
-                }
-                else
-                {
-                    for( int i = 0; p_html_colors[i].psz_name != NULL; i++ )
-                    {
-                        if( !strncasecmp( psz_value, p_html_colors[i].psz_name, strlen(p_html_colors[i].psz_name) ) )
-                        {
-                            i_font_color = p_html_colors[i].i_value;
-                            break;
-                        }
-                    }
-                }
-            }
-            else if( !strcasecmp( "alpha", psz_name ) &&
-                     ( psz_value[0] == '#' ) )
-            {
-                i_font_alpha = strtol( psz_value + 1, NULL, 16 );
-                i_font_alpha &= 0xff;
-            }
-        }
-        free( psz_name );
-        free( psz_value );
-    }
-    rv = PushFont( p_fonts,
-                   psz_fontname,
-                   i_font_size * i_scale / 1000,
-                   (i_font_color & 0xffffff) | ((i_font_alpha & 0xff) << 24) );
-
-    free( psz_fontname );
-
-    return rv;
 }
 
 static int ProcessNodes( filter_t *p_filter,
@@ -896,7 +540,13 @@ static int ProcessNodes( filter_t *p_filter,
                p_font_style->psz_fontname,
                p_font_style->i_font_size * i_scale / 1000,
                (p_font_style->i_font_color & 0xffffff) |
-                   ((p_font_style->i_font_alpha & 0xff) << 24) );
+                   ((p_font_style->i_font_alpha & 0xff) << 24),
+                   /* TODO no idea how ATSUStyle works
+               (p_font_style->i_karaoke_background_color & 0xffffff) |
+                   ((p_font_style->i_karaoke_background_alpha & 0xff) << 24)
+                   */
+                   0x00ffffff );
+
 
         if( p_font_style->i_style_flags & STYLE_BOLD )
             b_bold = true;
@@ -910,7 +560,8 @@ static int ProcessNodes( filter_t *p_filter,
         rv = PushFont( &p_fonts,
                        p_sys->psz_font_name,
                        p_sys->i_font_size,
-                       p_sys->i_font_color );
+                       p_sys->i_font_color,
+                       0x00ffffff );
     }
     if( rv != VLC_SUCCESS )
         return rv;
