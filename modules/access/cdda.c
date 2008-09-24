4be4d65897e69c6c14c48874720b6e555405ff00
Derk-Jan Hartman <hartman@videolan.org>
2008-09-24 17:10:26 +0200

    cdda: Fix issue with pausing of CDDA

--- modules/access/cdda.c
+++ modules/access/cdda.c
@@ -383,6 +383,8 @@ static int Control( access_t *p_access, int i_query, va_list args )
             break;
 
         case ACCESS_SET_PAUSE_STATE:
+            break;
+
         case ACCESS_GET_TITLE_INFO:
         case ACCESS_SET_TITLE:
         case ACCESS_GET_META:
