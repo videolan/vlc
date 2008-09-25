ca6669acaae8a1e0c4ec8ad7e98e6a3a837a8d61
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-09-25 18:33:12 +0300

    Probe interface plugis strictly
    We don't want a different interface than the one requested (if any)

--- src/interface/interface.c
+++ src/interface/interface.c
@@ -101,7 +101,7 @@ intf_thread_t* __intf_Create( vlc_object_t *p_this, const char *psz_module )
 
     /* Choose the best module */
     p_intf->psz_intf = strdup( psz_module );
-    p_intf->p_module = module_need( p_intf, "interface", psz_module, false );
+    p_intf->p_module = module_need( p_intf, "interface", psz_module, true );
 
     if( p_intf->p_module == NULL )
     {
