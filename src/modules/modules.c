ab6145b20376c78c61570a6d529b6464022e9a7c
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-09-26 18:47:09 +0300

    module_need: fix broken logic

--- src/modules/modules.c
+++ src/modules/modules.c
@@ -601,7 +601,7 @@ found_shortcut:
     else if( count == 0 )
     {
         if( !strcmp( psz_capability, "access_demux" )
-         && !strcmp( psz_capability, "vout_window" ) )
+         || !strcmp( psz_capability, "vout_window" ) )
         {
             msg_Dbg( p_this, "no %s module matched \"%s\"",
                 psz_capability, (psz_name && *psz_name) ? psz_name : "any" );
