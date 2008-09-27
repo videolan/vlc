dae39fd78466c5036fc5e959090ffc4dd02eeea9
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-09-27 12:00:10 +0300

    Add extensions for MIDI and Creative Voice

--- src/input/demux.c
+++ src/input/demux.c
@@ -107,7 +107,10 @@ demux_t *__demux_New( vlc_object_t *p_obj,
             { "pva",  "pva" },
             { "rm",   "rm" },
             { "m4v",  "m4v" },
-            { "h264",  "h264" },
+            { "h264", "h264" },
+            { "voc",  "voc" },
+            { "mid",  "smf" },
+            { "rmi",  "smf" },
             { "",  "" },
         };
         /* Here, we don't mind if it does not work, it must be quick */
