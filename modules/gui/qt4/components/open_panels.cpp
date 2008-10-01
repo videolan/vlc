d96bc2354ee2917d5fa7eda1636fb9850223b333
Jean-Baptiste Kempf <jb@videolan.org>
2008-09-30 20:16:16 -0700

    Typo.

--- modules/gui/qt4/components/open_panels.cpp
+++ modules/gui/qt4/components/open_panels.cpp
@@ -235,7 +235,7 @@ DiscOpenPanel::DiscOpenPanel( QWidget *_parent, intf_thread_t *_p_intf ) :
     ui.browseDiscButton->setToolTip( qtr( I_DEVICE_TOOLTIP ));
     ui.deviceCombo->setToolTip( I_DEVICE_TOOLTIP );
 
-#if WIN32 /* Disc drives probing for Windows */
+#ifdef WIN32 /* Disc drives probing for Windows */
     char szDrives[512];
     szDrives[0] = '\0';
     if( GetLogicalDriveStringsA( sizeof( szDrives ) - 1, szDrives ) )
