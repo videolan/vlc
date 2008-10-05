a610e2fc41551b15f52e35cd54a4701f524a1d90
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-10-05 17:16:52 +0300

    ifdef out dead "future" code (CID#148)

--- modules/stream_out/rtp.c
+++ modules/stream_out/rtp.c
@@ -1250,10 +1250,12 @@ static sout_stream_id_t *Add( sout_stream_t *p_stream, es_format_t *p_fmt )
         /* Mark dynamic payload type in use */
         p_sys->payload_bitmap |= 1 << (id->i_payload_type - 96);
 
+#if 0 /* No payload formats sets this at the moment */
     if( cscov != -1 )
         cscov += 8 /* UDP */ + 12 /* RTP */;
     if( id->sinkc > 0 )
         net_SetCSCov( id->sinkv[0].rtp_fd, cscov, -1 );
+#endif
 
     if( p_sys->rtsp != NULL )
         id->rtsp_id = RtspAddId( p_sys->rtsp, id, p_sys->i_es,
