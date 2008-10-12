aa765201bb44d6affc5a35c3900579856d5c01f8
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-10-12 11:07:51 +0300

    Memory leak (CID#266)

--- modules/control/http/util.c
+++ modules/control/http/util.c
@@ -352,10 +352,11 @@ void PlaylistListNode( intf_thread_t *p_intf, playlist_t *p_pl,
     {
         char value[512];
         char *psz;
-        mvar_t *itm = mvar_New( name, "set" );
         playlist_item_t * p_item = playlist_CurrentPlayingItem( p_pl );
         if( !p_item || !p_item->p_input )
             return;
+
+        mvar_t *itm = mvar_New( name, "set" );
         if( p_item->p_input->i_id == p_node->p_input->i_id )
             mvar_AppendNewVar( itm, "current", "1" );
         else
