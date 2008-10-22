4f460e2ffb86479e88509a2aca86609adeded1e9
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-10-22 23:09:27 +0300

    block_FifoWake (deprecated): wake-up _all_ threads, not just one

--- src/misc/block.c
+++ src/misc/block.c
@@ -431,7 +431,7 @@ void block_FifoWake( block_fifo_t *p_fifo )
     vlc_mutex_lock( &p_fifo->lock );
     if( p_fifo->p_first == NULL )
         p_fifo->b_force_wake = true;
-    vlc_cond_signal( &p_fifo->wait );
+    vlc_cond_broadcast( &p_fifo->wait );
     vlc_mutex_unlock( &p_fifo->lock );
 }
 
