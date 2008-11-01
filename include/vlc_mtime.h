08d7a20d7bfda963eb3b71443042165b19b86531
Rémi Denis-Courmont <rdenis@simphalempin.com>
2008-11-01 15:24:51 +0200

    Only check msleep on Linux (seems to depend on GNU ELF extensions)

--- include/vlc_mtime.h
+++ include/vlc_mtime.h
@@ -68,13 +68,9 @@ VLC_EXPORT( void,    mwait,    ( mtime_t date ) );
 VLC_EXPORT( void,    msleep,   ( mtime_t delay ) );
 VLC_EXPORT( char *,  secstotimestr, ( char *psz_buffer, int secs ) );
 
-#ifdef __GNUC__
-# ifdef __linux__
-#  define VLC_HARD_MIN_SLEEP 1000 /* Linux has 100, 250, 300 or 1000Hz */
-# else
-#  define VLC_HARD_MIN_SLEEP 0
-# endif
-#define VLC_SOFT_MIN_SLEEP 9000000
+#if defined (__GNUC__) && defined (__linux__)
+# define VLC_HARD_MIN_SLEEP 1000 /* Linux has 100, 250, 300 or 1000Hz */
+# define VLC_SOFT_MIN_SLEEP 9000000
 
 static
 __attribute__((unused))
