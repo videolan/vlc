4ee5170c5d781905a78ed70e3f983b498b133889
Dominique Leuenberger <dominique@leuenberger.net>
2008-11-10 22:48:53 +0100

    Add support for bbc.co.uk iPLayer URLs (LUA Script)
    Radio only for the moment
    
    Signed-off-by: Christophe Mutricy <xtophe@videolan.org>

---
+++ share/lua/playlist/bbc_co_uk.lua
@@ -0,0 +1,45 @@
+--[[
+ $Id$
+
+ Copyright © 2008 the VideoLAN team
+
+ Authors: Dominique Leuenberger <dominique-vlc.suse@leuenberger.net>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+--]]
+
+-- Probe function.
+function probe()
+    return vlc.access == "http"
+        and string.match( vlc.path, "bbc.co.uk/iplayer/" )
+end
+
+-- Parse function.
+function parse()
+    p = {}
+    while true do
+        -- Try to find the video's title
+        line = vlc.readline()
+        if not line then break end
+        if string.match( line, "title: " ) then
+            _,_,name = string.find( line, "title: \"(.*)\"" )
+        end
+        if string.match( line, "metaFile: \".*%.ram\"" ) then
+            _,_,video = string.find( line, "metaFile: \"(.-)\"" )
+            table.insert( p, { path = video; name = name } )
+        end
+    end
+    return p
+end
