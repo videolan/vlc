8239dd267432fc01273b5b05b1e0c4d94370d88b
Jean-Baptiste Kempf <jb@videolan.org>
2008-11-13 02:29:06 +0100

    Correct typo in previous commits.

--- extras/package/win32/configure-mingw.sh
+++ extras/package/win32/configure-mingw.sh
@@ -2,7 +2,7 @@ PATH="/usr/win32/bin:$PATH" \
 PKG_CONFIG_LIBDIR=/usr/win32/lib/pkgconfig \
 CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
 LDFLAGS="-L/usr/win32/lib" \
-CC=gcc CXX=g++ \
+CC=i586-mingw32msvc-gcc CXX=i586-mingw32msvc-g++ \
 CONFIG="./configure --host=i586-mingw32msvc --build=i386-linux
   --disable-mkv --disable-taglib --enable-debug" \
 sh extras/package/win32/configure-common.sh
