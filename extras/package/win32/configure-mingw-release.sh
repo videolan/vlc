f6d65479de1af38157ac70158fa7eee1a0c3af59
Jean-Baptiste Kempf <jb@videolan.org>
2008-11-27 00:51:54 +0100

    [Win32] Update the package configuration.

--- extras/package/win32/configure-mingw-release.sh
+++ extras/package/win32/configure-mingw-release.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
+
+root=$(echo $0|sed 's#extras/package/win32/configure-cygwin.sh##')./
+
 PATH="/usr/win32/bin:$PATH" \
 PKG_CONFIG_LIBDIR=/usr/win32/lib/pkgconfig \
 CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
 LDFLAGS="-L/usr/win32/lib" \
 CC=i586-mingw32msvc-gcc CXX=i586-mingw32msvc-g++ \
-CONFIG="./configure --host=i586-mingw32msvc --build=i386-linux
+CONFIG="${root}configure --host=i586-mingw32msvc --build=i386-linux
  --enable-mkv --enable-release --enable-nls" \
-sh extras/package/win32/configure-common.sh
+sh ${root}extras/package/win32/configure-common.sh
--- extras/package/win32/configure-msys.sh
+++ extras/package/win32/configure-msys.sh
@@ -1,8 +1,12 @@
+#!/bin/sh
+
+root=$(echo $0|sed 's#extras/package/win32/configure-cygwin.sh##')./
+
 PATH="/usr/win32/bin:$PATH" \
 PKG_CONFIG_LIBDIR=/usr/win32/lib/pkgconfig \
 CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
 LDFLAGS="-L/usr/win32/lib" \
 CC=gcc CXX=g++ \
-CONFIG="./configure --host=i586-mingw32msvc --build=i386-linux
+CONFIG="${root}configure --host=i586-mingw32msvc --build=i386-linux
     --disable-mkv --disable-taglib --disable-nls --enable-debug" \
-sh extras/package/win32/configure-common.sh
+sh ${root}extras/package/win32/configure-common.sh
