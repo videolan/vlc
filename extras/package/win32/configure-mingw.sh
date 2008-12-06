1c6e1a227797fd23b68dbbd7774adfa3fc70e868
Jean-Baptiste Kempf <jb@videolan.org>
2008-12-06 02:00:02 +0100

    Try to fix Win32 configures.

--- extras/package/win32/configure-mingw.sh
+++ extras/package/win32/configure-mingw.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-root=$(echo $0|sed 's#extras/package/win32/configure-cygwin.sh##')./
+root=$(echo $0|sed 's#extras/package/win32/configure-mingw.sh##')./
 
 PATH="/usr/win32/bin:$PATH" \
 PKG_CONFIG_LIBDIR=/usr/win32/lib/pkgconfig \
