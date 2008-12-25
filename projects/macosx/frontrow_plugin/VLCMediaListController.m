1ca5880fa1b4a20b108c1b09ef712bb18913e920
Felix Paul Kühne <fkuehne@videolan.org>
2008-12-25 15:02:52 +0100

    Added our GPL copyright header to all files

--- projects/macosx/frontrow_plugin/VLCMediaListController.m
+++ projects/macosx/frontrow_plugin/VLCMediaListController.m
@@ -1,10 +1,25 @@
-//
-//  VLCMediaListController.m
-//  FRVLC
-//
-//  Created by Pierre d'Herbemont on 2/11/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
+/*****************************************************************************
+ * VLCMediaListController.m: Front Row plugin
+ *****************************************************************************
+ * Copyright (C) 2007 - 2008 the VideoLAN Team
+ * $Id$
+ *
+ * Authors: Pierre d'Herbemont <pdherbemont at videolan dot org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #import "VLCMediaListController.h"
 #import "VLCPlayerController.h"
