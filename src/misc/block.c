530585d11283626347daa78e393f75187fdc5063
Rémi Denis-Courmont <rdenis@simphalempin.com>
2009-01-06 12:19:38 +0200

    block_FifoShow: fix condition variable usage

--- src/misc/block.c
+++ src/misc/block.c
@@ -526,7 +526,7 @@ block_t *block_FifoShow( block_fifo_t *p_fifo )
     vlc_mutex_lock( &p_fifo->lock );
     mutex_cleanup_push( &p_fifo->lock );
 
-    if( p_fifo->p_first == NULL )
+    while( p_fifo->p_first == NULL )
         vlc_cond_wait( &p_fifo->wait, &p_fifo->lock );
 
     b = p_fifo->p_first;
