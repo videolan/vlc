videolan/vlc

commit 8e0a2de24336bb68b6aee5a701025bfb1e01ac24

Skins2: partial vout window fixes
This remains seemingly badly unsafe

modules/gui/skins2/src/skin_main.cpp
@@ -302,7 +302,7 @@ static int WindowOpen( vlc_object_t *p_this )
 
     /* FIXME: most probably not thread-safe,
      * albeit no worse than ever before */
-    pWnd->handle = VlcProc::getWindow( pIntf, pWnd->vout,
+    pWnd->handle.hwnd = VlcProc::getWindow( pIntf, pWnd->vout,
                                        &pWnd->pos_x, &pWnd->pos_y,
                                        &pWnd->width, &pWnd->height );
     pWnd->p_private = pIntf;
@@ -506,7 +506,11 @@ vlc_module_begin ()
     add_shortcut( "skins" )
 
     add_submodule ()
-        set_capability( "vout_window", 51 )
+#ifndef WIN32
+        set_capability( "xwindow", 51 )
+#else
+        set_capability( "hwnd", 51 )
+#endif
         set_callbacks( WindowOpen, WindowClose )
 
     add_submodule ()

commit 679b54c7b9a6cd9e6ffc749f8f0fe1c0bb5f03eb

Fix skins2 compile (broken by 8e0a2de24336bb68b6aee5a701025bfb1e01ac24)
Signed-off-by: Rémi Denis-Courmont <rdenis@simphalempin.com>

modules/gui/skins2/src/skin_main.cpp
@@ -316,7 +316,7 @@ static void WindowClose( vlc_object_t *p_this )
     vout_window_t *pWnd = (vout_window_t *)p_this;
     intf_thread_t *pIntf = (intf_thread_t *)p_this->p_private;
 
-    VlcProc::releaseWindow( pIntf, pWnd->handle );
+    VlcProc::releaseWindow( pIntf, pWnd->handle.hwnd );
 }
 
 //---------------------------------------------------------------------------