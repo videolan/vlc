567a3b099a789ce689d466b3920ef4e8e14ebc37
Laurent Aimar <fenrir@videolan.org>
2009-02-16 20:54:18 +0100

    Added missing lock assert to playlist_CurrentPlayingItem and playlist_Status.

--- src/playlist/engine.c
+++ src/playlist/engine.c
@@ -304,11 +304,15 @@ static void VariablesInit( playlist_t *p_playlist )
 
 playlist_item_t * playlist_CurrentPlayingItem( playlist_t * p_playlist )
 {
+    PL_ASSERT_LOCKED;
+
     return pl_priv(p_playlist)->status.p_item;
 }
 
 int playlist_Status( playlist_t * p_playlist )
 {
+    PL_ASSERT_LOCKED;
+
     return pl_priv(p_playlist)->status.i_status;
 }
 
