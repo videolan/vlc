b706620b1012130eeeb4075fe93e250f87c4ddd2
Laurent Aimar <fenrir@videolan.org>
2009-02-19 00:01:27 +0100

    Fixed a memleak in cdda.

--- modules/access/cdda.c
+++ modules/access/cdda.c
@@ -245,6 +245,7 @@ static int Open( vlc_object_t *p_this )
     return VLC_SUCCESS;
 
 error:
+    free( p_sys->p_sectors );
     ioctl_Close( VLC_OBJECT(p_access), p_sys->vcddev );
     free( p_sys );
     return VLC_EGENERIC;
@@ -257,6 +258,8 @@ static void Close( vlc_object_t *p_this )
 {
     access_t     *p_access = (access_t *)p_this;
     access_sys_t *p_sys = p_access->p_sys;
+
+    free( p_sys->p_sectors );
     ioctl_Close( p_this, p_sys->vcddev );
     free( p_sys );
 }
