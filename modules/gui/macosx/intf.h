videolan/vlc

commit 5c30d75fcc257c8479e2d03f12b634b7fb87c3d5

macosx: NSApplication was subclassed in [408f769e5768bf1c8dbc840bef1c560a91de6d5f], so let's set the file's owner to use the correct class
This also fixes some minor type errors in the main nib

modules/gui/macosx/intf.h
@@ -124,7 +124,7 @@ static void MsgCallback( msg_cb_data_t *, msg_item_t *, unsigned );
     BOOL nib_vlm_loaded;        /* VLM Panel nibfile */
     BOOL nib_coredialogs_loaded; /* CoreDialogs nibfile */
 
-    IBOutlet VLControllerWindow * o_window;                     /* main window */
+    IBOutlet VLCControllerWindow * o_window;                     /* main window */
     IBOutlet NSView * o_playlist_view;                          /* playlist view  */
     IBOutlet id o_scrollfield;                                  /* info field */
     IBOutlet NSTextField * o_timefield;                         /* time field */
@@ -223,7 +223,7 @@ static void MsgCallback( msg_cb_data_t *, msg_item_t *, unsigned );
     IBOutlet NSMenuItem * o_mi_fwd5m;
     IBOutlet NSMenuItem * o_mi_bwd5m;
     IBOutlet NSMenuItem * o_mi_program;
-    IBOutlet NSMenuItem * o_mu_program;
+    IBOutlet NSMenu * o_mu_program;
     IBOutlet NSMenuItem * o_mi_title;
     IBOutlet NSMenu * o_mu_title;
     IBOutlet NSMenuItem * o_mi_chapter;
@@ -264,7 +264,7 @@ static void MsgCallback( msg_cb_data_t *, msg_item_t *, unsigned );
     IBOutlet NSMenuItem * o_mi_deinterlace;
     IBOutlet NSMenu * o_mu_deinterlace;
     IBOutlet NSMenuItem * o_mi_ffmpeg_pp;
-    IBOutlet NSMenuItem * o_mu_ffmpeg_pp;
+    IBOutlet NSMenu * o_mu_ffmpeg_pp;
     IBOutlet NSMenuItem * o_mi_teletext;
     IBOutlet NSMenuItem * o_mi_teletext_transparent;
     IBOutlet NSMenuItem * o_mi_teletext_index;

extras/package/macosx/Resources/English.lproj/About.nib/info.nib
@@ -11,10 +11,10 @@
 	<key>IBOpenObjects</key>
 	<array>
 		<integer>2245</integer>
-		<integer>2239</integer>
 		<integer>1345</integer>
+		<integer>2239</integer>
 	</array>
 	<key>IBSystem Version</key>
-	<string>9E17</string>
+	<string>9G55</string>
 </dict>
 </plist>

extras/package/macosx/Resources/English.lproj/MainMenu.nib/classes.nib
@@ -30,6 +30,11 @@
             SUPERCLASS = NSImageView;
         },
                 {
+            CLASS = VLCApplication;
+            LANGUAGE = ObjC;
+            SUPERCLASS = NSApplication;
+        },
+                {
             CLASS = VLCControllerView;
             LANGUAGE = ObjC;
             SUPERCLASS = NSView;
@@ -195,6 +200,7 @@
                 "o_btn_rewind" = NSButton;
                 "o_btn_stop" = NSButton;
                 "o_controls" = VLCControls;
+                "o_coredialogs" = id;
                 "o_crashrep_desc_txt" = NSTextField;
                 "o_crashrep_dontSend_btn" = NSButton;
                 "o_crashrep_fld" = NSTextView;
@@ -215,7 +221,7 @@
                 "o_info" = id;
                 "o_interaction_list" = id;
                 "o_main_pgbar" = NSProgressIndicator;
-                "o_messages" = id;
+                "o_messages" = NSTextView;
                 "o_mi_about" = NSMenuItem;
                 "o_mi_addSub" = NSMenuItem;
                 "o_mi_add_intf" = NSMenuItem;
@@ -289,6 +295,7 @@
                 "o_mi_show_all" = NSMenuItem;
                 "o_mi_slower" = NSMenuItem;
                 "o_mi_snapshot" = NSMenuItem;
+                "o_mi_sprefs" = NSMenuItem;
                 "o_mi_stop" = NSMenuItem;
                 "o_mi_subtitle" = NSMenuItem;
                 "o_mi_teletext" = NSMenuItem;
@@ -305,8 +312,8 @@
                 "o_mi_vol_up" = NSMenuItem;
                 "o_mi_website" = NSMenuItem;
                 "o_msgs_crashlog_btn" = NSButton;
-                "o_msgs_panel" = id;
-                "o_msgs_save_btn" = id;
+                "o_msgs_panel" = NSWindow;
+                "o_msgs_save_btn" = NSButton;
                 "o_mu_add_intf" = NSMenu;
                 "o_mu_aspect_ratio" = NSMenu;
                 "o_mu_audio" = NSMenu;
@@ -318,10 +325,10 @@
                 "o_mu_deinterlace" = NSMenu;
                 "o_mu_device" = NSMenu;
                 "o_mu_edit" = NSMenu;
-                "o_mu_ffmpeg_pp" = NSMenuItem;
+                "o_mu_ffmpeg_pp" = NSMenu;
                 "o_mu_file" = NSMenu;
                 "o_mu_help" = NSMenu;
-                "o_mu_program" = NSMenuItem;
+                "o_mu_program" = NSMenu;
                 "o_mu_screen" = NSMenu;
                 "o_mu_subtitle" = NSMenu;
                 "o_mu_title" = NSMenu;
@@ -350,7 +357,7 @@
                 "o_vmi_volup" = NSMenuItem;
                 "o_volumeslider" = NSSlider;
                 "o_vout_menu" = NSMenu;
-                "o_window" = VLControllerWindow;
+                "o_window" = VLCControllerWindow;
                 "o_wizard" = id;
                 toolbarMediaControl = NSView;
             };

extras/package/macosx/Resources/English.lproj/MainMenu.nib/info.nib
@@ -6,6 +6,8 @@
 	<string>246 207 356 240 0 0 1280 778 </string>
 	<key>IBEditorPositions</key>
 	<dict>
+		<key>2197</key>
+		<string>342 506 596 123 0 0 1280 778 </string>
 		<key>29</key>
 		<string>391 728 438 44 0 0 1280 778 </string>
 		<key>3568</key>
@@ -21,10 +23,11 @@
 	<integer>4</integer>
 	<key>IBOpenObjects</key>
 	<array>
-		<integer>21</integer>
-		<integer>2416</integer>
 		<integer>3568</integer>
+		<integer>2416</integer>
 		<integer>29</integer>
+		<integer>21</integer>
+		<integer>2197</integer>
 	</array>
 	<key>IBSystem Version</key>
 	<string>9G55</string>

extras/package/macosx/Resources/English.lproj/Preferences.nib/classes.nib
@@ -15,9 +15,223 @@
             LANGUAGE = ObjC;
         },
                 {
+            CLASS = VLCApplication;
+            LANGUAGE = ObjC;
+            SUPERCLASS = NSApplication;
+        },
+                {
             CLASS = VLCHotkeyChangeWindow;
             LANGUAGE = ObjC;
             SUPERCLASS = NSWindow;
+        },
+                {
+            ACTIONS =             {
+                checkForUpdate = id;
+                clearRecentItems = id;
+                crashReporterAction = id;
+                intfOpenCapture = id;
+                intfOpenDisc = id;
+                intfOpenFile = id;
+                intfOpenFileGeneric = id;
+                intfOpenNet = id;
+                openCrashLog = id;
+                openDocumentation = id;
+                openDonate = id;
+                openForum = id;
+                openReadMe = id;
+                openRecentItem = id;
+                openWebsite = id;
+                saveDebugLog = id;
+                showBookmarks = id;
+                showExtended = id;
+                showInformationPanel = id;
+                showLicense = id;
+                showMessagesPanel = id;
+                showVLM = id;
+                showWizard = id;
+                timeFieldWasClicked = id;
+                timesliderUpdate = id;
+                togglePlaylist = id;
+                viewAbout = id;
+                viewErrorsAndWarnings = id;
+                viewHelp = id;
+                viewPreferences = id;
+            };
+            CLASS = VLCMain;
+            LANGUAGE = ObjC;
+            OUTLETS =             {
+                "o_about" = id;
+                "o_bookmarks" = id;
+                "o_btn_equalizer" = NSButton;
+                "o_btn_ff" = NSButton;
+                "o_btn_fullscreen" = NSButton;
+                "o_btn_next" = NSButton;
+                "o_btn_play" = NSButton;
+                "o_btn_playlist" = NSButton;
+                "o_btn_prev" = NSButton;
+                "o_btn_rewind" = NSButton;
+                "o_btn_stop" = NSButton;
+                "o_controls" = VLCControls;
+                "o_coredialogs" = id;
+                "o_crashrep_desc_txt" = NSTextField;
+                "o_crashrep_dontSend_btn" = NSButton;
+                "o_crashrep_fld" = NSTextView;
+                "o_crashrep_includeEmail_ckb" = NSButton;
+                "o_crashrep_includeEmail_txt" = NSTextField;
+                "o_crashrep_send_btn" = NSButton;
+                "o_crashrep_title_txt" = NSTextField;
+                "o_crashrep_win" = NSWindow;
+                "o_dmi_mute" = NSMenuItem;
+                "o_dmi_next" = NSMenuItem;
+                "o_dmi_play" = NSMenuItem;
+                "o_dmi_previous" = NSMenuItem;
+                "o_dmi_stop" = NSMenuItem;
+                "o_embedded_list" = id;
+                "o_embedded_window" = VLCEmbeddedWindow;
+                "o_extended" = id;
+                "o_eyetv" = id;
+                "o_main_pgbar" = NSProgressIndicator;
+                "o_messages" = NSTextView;
+                "o_mi_about" = NSMenuItem;
+                "o_mi_addSub" = NSMenuItem;
+                "o_mi_add_intf" = NSMenuItem;
+                "o_mi_aspect_ratio" = NSMenuItem;
+                "o_mi_audiotrack" = NSMenuItem;
+                "o_mi_bookmarks" = NSMenuItem;
+                "o_mi_bring_atf" = NSMenuItem;
+                "o_mi_bwd" = NSMenuItem;
+                "o_mi_bwd1m" = NSMenuItem;
+                "o_mi_bwd5m" = NSMenuItem;
+                "o_mi_channels" = NSMenuItem;
+                "o_mi_chapter" = NSMenuItem;
+                "o_mi_checkForUpdate" = NSMenuItem;
+                "o_mi_clear" = NSMenuItem;
+                "o_mi_close_window" = NSMenuItem;
+                "o_mi_controller" = NSMenuItem;
+                "o_mi_copy" = NSMenuItem;
+                "o_mi_crop" = NSMenuItem;
+                "o_mi_cut" = NSMenuItem;
+                "o_mi_deinterlace" = NSMenuItem;
+                "o_mi_device" = NSMenuItem;
+                "o_mi_documentation" = NSMenuItem;
+                "o_mi_donation" = NSMenuItem;
+                "o_mi_double_window" = NSMenuItem;
+                "o_mi_equalizer" = NSMenuItem;
+                "o_mi_errorsAndWarnings" = NSMenuItem;
+                "o_mi_extended" = NSMenuItem;
+                "o_mi_faster" = NSMenuItem;
+                "o_mi_ffmpeg_pp" = NSMenuItem;
+                "o_mi_fittoscreen" = NSMenuItem;
+                "o_mi_floatontop" = NSMenuItem;
+                "o_mi_forum" = NSMenuItem;
+                "o_mi_fullscreen" = NSMenuItem;
+                "o_mi_fwd" = NSMenuItem;
+                "o_mi_fwd1m" = NSMenuItem;
+                "o_mi_fwd5m" = NSMenuItem;
+                "o_mi_half_window" = NSMenuItem;
+                "o_mi_help" = NSMenuItem;
+                "o_mi_hide" = NSMenuItem;
+                "o_mi_hide_others" = NSMenuItem;
+                "o_mi_info" = NSMenuItem;
+                "o_mi_license" = NSMenuItem;
+                "o_mi_loop" = NSMenuItem;
+                "o_mi_messages" = NSMenuItem;
+                "o_mi_minimize" = NSMenuItem;
+                "o_mi_mute" = NSMenuItem;
+                "o_mi_next" = NSMenuItem;
+                "o_mi_normal_window" = NSMenuItem;
+                "o_mi_open_capture" = NSMenuItem;
+                "o_mi_open_disc" = NSMenuItem;
+                "o_mi_open_file" = NSMenuItem;
+                "o_mi_open_generic" = NSMenuItem;
+                "o_mi_open_net" = NSMenuItem;
+                "o_mi_open_recent" = NSMenuItem;
+                "o_mi_open_recent_cm" = NSMenuItem;
+                "o_mi_open_vlm" = NSMenuItem;
+                "o_mi_open_wizard" = NSMenuItem;
+                "o_mi_paste" = NSMenuItem;
+                "o_mi_play" = NSMenuItem;
+                "o_mi_playlist" = NSMenuItem;
+                "o_mi_prefs" = NSMenuItem;
+                "o_mi_previous" = NSMenuItem;
+                "o_mi_program" = NSMenuItem;
+                "o_mi_quit" = NSMenuItem;
+                "o_mi_random" = NSMenuItem;
+                "o_mi_readme" = NSMenuItem;
+                "o_mi_repeat" = NSMenuItem;
+                "o_mi_screen" = NSMenuItem;
+                "o_mi_select_all" = NSMenuItem;
+                "o_mi_services" = NSMenuItem;
+                "o_mi_show_all" = NSMenuItem;
+                "o_mi_slower" = NSMenuItem;
+                "o_mi_snapshot" = NSMenuItem;
+                "o_mi_sprefs" = NSMenuItem;
+                "o_mi_stop" = NSMenuItem;
+                "o_mi_subtitle" = NSMenuItem;
+                "o_mi_teletext" = NSMenuItem;
+                "o_mi_teletext_blue" = NSMenuItem;
+                "o_mi_teletext_green" = NSMenuItem;
+                "o_mi_teletext_index" = NSMenuItem;
+                "o_mi_teletext_red" = NSMenuItem;
+                "o_mi_teletext_transparent" = NSMenuItem;
+                "o_mi_teletext_yellow" = NSMenuItem;
+                "o_mi_title" = NSMenuItem;
+                "o_mi_videotrack" = NSMenuItem;
+                "o_mi_visual" = NSMenuItem;
+                "o_mi_vol_down" = NSMenuItem;
+                "o_mi_vol_up" = NSMenuItem;
+                "o_mi_website" = NSMenuItem;
+                "o_msgs_crashlog_btn" = NSButton;
+                "o_msgs_panel" = NSWindow;
+                "o_msgs_save_btn" = NSButton;
+                "o_mu_add_intf" = NSMenu;
+                "o_mu_aspect_ratio" = NSMenu;
+                "o_mu_audio" = NSMenu;
+                "o_mu_audiotrack" = NSMenu;
+                "o_mu_channels" = NSMenu;
+                "o_mu_chapter" = NSMenu;
+                "o_mu_controls" = NSMenu;
+                "o_mu_crop" = NSMenu;
+                "o_mu_deinterlace" = NSMenu;
+                "o_mu_device" = NSMenu;
+                "o_mu_edit" = NSMenu;
+                "o_mu_ffmpeg_pp" = NSMenu;
+                "o_mu_file" = NSMenu;
+                "o_mu_help" = NSMenu;
+                "o_mu_program" = NSMenu;
+                "o_mu_screen" = NSMenu;
+                "o_mu_subtitle" = NSMenu;
+                "o_mu_title" = NSMenu;
+                "o_mu_video" = NSMenu;
+                "o_mu_videotrack" = NSMenu;
+                "o_mu_visual" = NSMenu;
+                "o_mu_window" = NSMenu;
+                "o_open" = id;
+                "o_playlist" = VLCPlaylist;
+                "o_playlist_view" = NSView;
+                "o_prefs" = id;
+                "o_scrollfield" = id;
+                "o_sprefs" = id;
+                "o_timefield" = NSTextField;
+                "o_timeslider" = NSSlider;
+                "o_update" = id;
+                "o_vlm" = id;
+                "o_vmi_fullscreen" = NSMenuItem;
+                "o_vmi_mute" = NSMenuItem;
+                "o_vmi_next" = NSMenuItem;
+                "o_vmi_play" = NSMenuItem;
+                "o_vmi_prev" = NSMenuItem;
+                "o_vmi_snapshot" = NSMenuItem;
+                "o_vmi_stop" = NSMenuItem;
+                "o_vmi_voldown" = NSMenuItem;
+                "o_vmi_volup" = NSMenuItem;
+                "o_volumeslider" = NSSlider;
+                "o_vout_menu" = NSMenu;
+                "o_window" = VLCControllerWindow;
+                "o_wizard" = id;
+                toolbarMediaControl = NSView;
+            };
+            SUPERCLASS = NSObject;
         },
                 {
             ACTIONS =             {

extras/package/macosx/Resources/English.lproj/Preferences.nib/info.nib
@@ -15,7 +15,7 @@
 		<key>2523</key>
 		<string>345 341 590 343 0 0 1280 778 </string>
 		<key>2562</key>
-		<string>344 333 590 444 0 0 1280 778 </string>
+		<string>345 266 590 444 0 0 1280 778 </string>
 		<key>2668</key>
 		<string>345 256 590 457 0 0 1280 778 </string>
 	</dict>

commit 32dccae02f6e335a72f8d13dc426fed76f64a743

macosx: fix compilation error introduced in [5c30d75fcc257c8479e2d03f12b634b7fb87c3d5]

modules/gui/macosx/intf.h
@@ -90,7 +90,7 @@ static void MsgCallback( msg_cb_data_t *, msg_item_t *, unsigned );
  *****************************************************************************/
 @class AppleRemote;
 @class VLCInformation;
-@class VLControllerWindow;
+@class VLCControllerWindow;
 @class VLCEmbeddedWindow;
 @class VLCControls;
 @class VLCPlaylist;