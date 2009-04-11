31921d280c74b8355d39780bb4a8cadeef00c01e
Rémi Denis-Courmont <remi@remlab.net>
2009-04-11 15:20:31 +0300

    Rationalize string

--- modules/codec/subtitles/subsdec.c
+++ modules/codec/subtitles/subsdec.c
@@ -94,7 +94,7 @@ static const char *const ppsz_encodings[] = {
 };
 
 static const char *const ppsz_encoding_names[] = {
-    N_("Autodetect"),
+    N_("Auto"),
     N_("Universal (UTF-8)"),
     N_("Universal (UTF-16)"),
     N_("Universal (big endian UTF-16)"),
