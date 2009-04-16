5da4387487027c2cc9edcfdd72fc612c040445af
Rémi Denis-Courmont <remi@remlab.net>
2009-04-16 15:40:50 +0300

    Fix use after free

--- modules/stream_out/es.c
+++ modules/stream_out/es.c
@@ -423,8 +423,9 @@ static int Del( sout_stream_t *p_stream, sout_stream_id_t *id )
 {
     VLC_UNUSED(p_stream);
     sout_access_out_t *p_access = id->p_mux->p_access;
-    sout_MuxDelete( id->p_mux );
+
     sout_MuxDeleteStream( id->p_mux, id->p_input );
+    sout_MuxDelete( id->p_mux );
     if( !sout_AccessOutCanControlPace( p_access ) )
         p_stream->p_sout->i_out_pace_nocontrol--;
     sout_AccessOutDelete( p_access );
