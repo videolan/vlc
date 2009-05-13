dfd0f74dd7e83136d7d7130d26820535c9a52fad
Laurent Aimar <fenrir@videolan.org>
2009-05-13 23:41:54 +0200

    Fixed the option "auto-preparse".
     It is now respected. It is part of the privacy problem with
    meta data/art.
     I think that
      --noauto-preparse --album-art 0
     will workaround the issue until a new option is added.

--- src/playlist/engine.c
+++ src/playlist/engine.c
@@ -101,8 +101,8 @@ playlist_t * playlist_Create( vlc_object_t *p_parent )
 
     pl_priv(p_playlist)->b_doing_ml = false;
 
-    pl_priv(p_playlist)->b_auto_preparse =
-                        var_CreateGetBool( p_playlist, "auto-preparse" ) ;
+    const bool b_auto_preparse = var_CreateGetBool( p_playlist, "auto-preparse" );
+    pl_priv(p_playlist)->b_auto_preparse = b_auto_preparse;
 
     PL_LOCK; /* playlist_NodeCreate will check for it */
     p_playlist->p_root_category = playlist_NodeCreate( p_playlist, NULL, NULL,
@@ -156,7 +156,7 @@ playlist_t * playlist_Create( vlc_object_t *p_parent )
 
     pl_priv(p_playlist)->b_auto_preparse = false;
     playlist_MLLoad( p_playlist );
-    pl_priv(p_playlist)->b_auto_preparse = true;
+    pl_priv(p_playlist)->b_auto_preparse = b_auto_preparse;
 
     vlc_object_set_destructor( p_playlist, playlist_Destructor );
 
