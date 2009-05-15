10524df53cda24ba58619f2b45b4227ac2cd43da
Laurent Aimar <fenrir@videolan.org>
2009-05-15 23:46:05 +0200

    Correctly detect rewind (rtsp).

--- src/input/input.c
+++ src/input/input.c
@@ -2433,7 +2433,7 @@ static int InputSourceInit( input_thread_t *p_input,
             in->b_can_pause = false;
         var_SetBool( p_input, "can-pause", in->b_can_pause || !in->b_can_pace_control ); /* XXX temporary because of es_out_timeshift*/
         var_SetBool( p_input, "can-rate", !in->b_can_pace_control || in->b_can_rate_control ); /* XXX temporary because of es_out_timeshift*/
-        var_SetBool( p_input, "can-rewind", !in->b_rescale_ts && !in->b_can_pace_control );
+        var_SetBool( p_input, "can-rewind", !in->b_rescale_ts && !in->b_can_pace_control && in->b_can_rate_control );
 
         bool b_can_seek;
         if( demux_Control( in->p_demux, DEMUX_CAN_SEEK, &b_can_seek ) )
