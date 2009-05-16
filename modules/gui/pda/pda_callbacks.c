videolan/vlc

commit ebd7cf22ae7d5b3a1bbf1cd8728926e78f831a9c

Fixed typo in pda intf.

modules/gui/pda/pda_callbacks.c
@@ -136,7 +136,7 @@ static void PlaylistAddItem(GtkWidget *widget, gchar *name, char **ppsz_options,
                               (const char*)name,
                               PLAYLIST_APPEND, PLAYLIST_END,
                               (mtime_t) 0,
-                              (const char **) ppsz_options, i_pos,
+                              (const char **) ppsz_options, i_options,
                               true, pl_Unlocked );
             }
 

commit 2f0c83bd43674753d2fd2d7fb8cc4193fbdfcfca

pda: fix typo introduced by [ebd7cf22ae7d5b3a1bbf1cd8728926e78f831a9c]
This should fix the compilation of pda interface.

modules/gui/pda/pda_callbacks.c
@@ -136,7 +136,7 @@ static void PlaylistAddItem(GtkWidget *widget, gchar *name, char **ppsz_options,
                               (const char*)name,
                               PLAYLIST_APPEND, PLAYLIST_END,
                               (mtime_t) 0,
-                              i_options, (const char **) ppsz_options, VLC_INPUT_OPTION_TRUSTED,
+                              i_size, (const char **) ppsz_options, VLC_INPUT_OPTION_TRUSTED,
                               true, pl_Unlocked );
             }
 