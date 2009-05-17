404a1c6e1820d6304194bdd004a56ab446cb4cf3
Jean-Baptiste Kempf <jb@videolan.org>
2009-05-18 00:31:50 +0200

    Ensure that people don't compile Dirac on Windows and Gcc 3

--- extras/package/win32/configure-cygwin.sh
+++ extras/package/win32/configure-cygwin.sh
@@ -8,5 +8,5 @@ CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
 LDFLAGS="-L/usr/win32/lib" \
 CC="gcc -mno-cygwin" CXX="g++ -mno-cygwin" \
 CONFIG="${root}configure --host=i686-pc-mingw32
-       --disable-nls --disable-taglib --disable-mkv --enable-debug" \
+       --disable-nls --disable-taglib --disable-mkv --disable-dirac --enable-debug" \
 sh ${root}extras/package/win32/configure-common.sh
--- extras/package/win32/configure-msys.sh
+++ extras/package/win32/configure-msys.sh
@@ -8,5 +8,5 @@ CPPFLAGS="-I/usr/win32/include -I/usr/win32/include/ebml" \
 LDFLAGS="-L/usr/win32/lib" \
 CC=gcc CXX=g++ \
 CONFIG="${root}configure --host=i586-mingw32msvc --build=i386-linux
-    --disable-mkv --disable-taglib --disable-nls --enable-debug" \
+    --disable-mkv --disable-taglib --disable-nls --disable-dirac --enable-debug" \
 sh ${root}extras/package/win32/configure-common.sh
