5f9ea6614282bd752597f0e15eec0d50a2f26a3d
Laurent Aimar <fenrir@videolan.org>
2009-05-22 21:30:16 +0200

    Fixed demux_vaControlHelper GET_TIME implementation.
    It should return the current time even when the size isn't known.

--- src/input/demux.c
+++ src/input/demux.c
@@ -234,7 +234,7 @@ int demux_vaControlHelper( stream_t *s,
 
         case DEMUX_GET_TIME:
             pi64 = (int64_t*)va_arg( args, int64_t * );
-            if( i_bitrate > 0 && i_end > i_start )
+            if( i_bitrate > 0 && i_tell >= i_start )
             {
                 *pi64 = INT64_C(8000000) * (i_tell - i_start) / i_bitrate;
                 return VLC_SUCCESS;
