bbf31a42e6109a4d2fb8a7b7e08cc8a62d29a3d6
Rémi Denis-Courmont <remi@remlab.net>
2009-05-23 19:14:08 +0300

    Remove non-existent types

--- include/vlc_common.h
+++ include/vlc_common.h
@@ -179,8 +179,6 @@ typedef struct config_category_t config_category_t;
 /* Interface */
 typedef struct intf_thread_t intf_thread_t;
 typedef struct intf_sys_t intf_sys_t;
-typedef struct intf_msg_t intf_msg_t;
-typedef struct user_widget_t user_widget_t;
 
 /* Input */
 typedef struct input_thread_t input_thread_t;
