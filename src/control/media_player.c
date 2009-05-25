295904fac56671cda96aa1fa2d9f789ceb770728
Geoffroy Couprie <geal@videolan.org>
2009-05-25 15:09:40 +0200

    Add missing config.h

--- src/control/media_player.c
+++ src/control/media_player.c
@@ -21,6 +21,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <assert.h>
 
 #include <vlc/libvlc.h>
