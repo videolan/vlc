videolan/vlc

commit 3d5ff7dc53d5ac154fd56faca23a3b6364037aa2

64bit compilation fixes

modules/gui/macosx/vout.m
@@ -37,6 +37,9 @@
 /* BeginFullScreen, EndFullScreen */
 #include <QuickTime/QuickTime.h>
 
+/* prevent system sleep */
+#import <CoreServices/CoreServices.h>
+
 #include <vlc_keys.h>
 
 #include "intf.h"
@@ -447,6 +450,7 @@ int DeviceCallback( vlc_object_t *p_this, const char *psz_variable,
     if( !VLCIntf || !VLCIntf->p_sys )
         return;
 
+    UInt8 UsrActivity;
     if( VLCIntf->p_sys->i_play_status == PLAYING_S )
         UpdateSystemActivity( UsrActivity );
 }

modules/control/unimotion.c
@@ -138,8 +138,8 @@ static int probe_sms(int kernFunc, char *servMatch, int dataType, void *data)
     io_object_t aDevice;
     io_connect_t  dataPort;
 
-    IOItemCount structureInputSize;
-    IOByteCount structureOutputSize;
+    size_t structureInputSize;
+    size_t structureOutputSize;
 
     union motion_data inputStructure;
     union motion_data *outputStructure;
@@ -189,8 +189,13 @@ static int probe_sms(int kernFunc, char *servMatch, int dataType, void *data)
     memset(&inputStructure, 0, sizeof(union motion_data));
     memset(outputStructure, 0, sizeof(union motion_data));
 
+#ifdef __LP64__
+    result = IOConnectCallStructMethod(dataPort, kernFunc, &inputStructure, 
+                structureInputSize, outputStructure, &structureOutputSize );
+#else
     result = IOConnectMethodStructureIStructureO(dataPort, kernFunc, structureInputSize,
                 &structureOutputSize, &inputStructure, outputStructure);
+#endif
 
     IOServiceClose(dataPort);
 

commit 8d5c75701a7351e90b617bb5395d71a6ac200666

Mac OS X vout: fix surprpression of the screensaver.
This was broken in [3d5ff7dc53d5ac154fd56faca23a3b6364037aa2]

modules/gui/macosx/vout.m
@@ -458,7 +458,6 @@ int DeviceCallback( vlc_object_t *p_this, const char *psz_variable,
     if( !VLCIntf || !VLCIntf->p_sys )
         return;
 
-    UInt8 UsrActivity;
     if( VLCIntf->p_sys->i_play_status == PLAYING_S )
         UpdateSystemActivity( UsrActivity );
 }