b74ce937e05dad5ed00a0290284f18846cfe71ed
Derk-Jan Hartman <hartman@videolan.org>
2009-05-27 15:26:00 +0200

    macosx playlist: fix an unreachable update of the Media Info panel (cherry picked from commit cbccd7297c241830e796f0f239b4b90d19c16c75)
    Signed-off-by: Derk-Jan Hartman <hartman@videolan.org>

--- modules/gui/macosx/playlist.m
+++ modules/gui/macosx/playlist.m
@@ -584,6 +584,9 @@
         /* update the state of our Reveal-in-Finder menu items */
         NSMutableString *o_mrl;
         char *psz_uri = input_item_GetURI( p_item->p_input );
+
+        [o_mi_revealInFinder setEnabled: NO];
+        [o_mm_mi_revealInFinder setEnabled: NO];
         if( psz_uri )
         {
             o_mrl = [NSMutableString stringWithUTF8String: psz_uri];
@@ -597,11 +600,8 @@
             {
                 [o_mi_revealInFinder setEnabled: YES];
                 [o_mm_mi_revealInFinder setEnabled: YES];
-                return;
             }
         }
-        [o_mi_revealInFinder setEnabled: NO];
-        [o_mm_mi_revealInFinder setEnabled: NO];
 
         if( [[VLCMain sharedInstance] isPlaylistCollapsed] == NO )
         {
