baa4816840cd01d338abc5d9030a15ddc7895995
Laurent Aimar <fenrir@videolan.org>
2009-06-06 19:17:38 +0200

    Temporary workaround for video filter wrapper in qt4 (wall).

--- modules/gui/qt4/components/extended_panels.cpp
+++ modules/gui/qt4/components/extended_panels.cpp
@@ -260,6 +260,7 @@ void ExtVideo::ChangeVFiltersString( const char *psz_name, bool b_add )
     if( !strcmp( psz_name, "magnify" ) ||
         !strcmp( psz_name, "puzzle" ) ||
         !strcmp( psz_name, "logo" ) ||
+        !strcmp( psz_name, "wall" ) ||
         !strcmp( psz_name, "clone" ) )
         psz_module_name = "video_filter_wrapper";
 
