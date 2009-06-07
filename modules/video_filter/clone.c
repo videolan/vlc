8ede742f1461b525d439034ee4d6d7937a8e5556
Laurent Aimar <fenrir@videolan.org>
2009-06-07 23:15:13 +0200

    Fixed a picture memleak in clone.

--- modules/video_filter/clone.c
+++ modules/video_filter/clone.c
@@ -181,10 +181,14 @@ static int Filter( video_splitter_t *p_splitter,
                    picture_t *pp_dst[], picture_t *p_src )
 {
     if( video_splitter_NewPicture( p_splitter, pp_dst ) )
+    {
+        picture_Release( p_src );
         return VLC_EGENERIC;
+    }
 
     for( int i = 0; i < p_splitter->i_output; i++ )
         picture_Copy( pp_dst[i], p_src );
+
     picture_Release( p_src );
     return VLC_SUCCESS;
 }
