1a912ab730421717b01bf05d0bd5cf8da1da4745
Laurent Aimar <fenrir@videolan.org>
2009-06-09 20:15:03 +0200

    Correctly update "title %2i" variable.
    It is a partial revert of [a8c9c25d2c137daf94292dcbbf4e889c6de0d6e6]

--- src/input/event.c
+++ src/input/event.c
@@ -149,10 +149,16 @@ void input_SendEventSeekpoint( input_thread_t *p_input, int i_title, int i_seekp
 {
 	vlc_value_t val;
 
-	VLC_UNUSED( i_title );
+    /* "chapter" */
 	val.i_int = i_seekpoint;
 	var_Change( p_input, "chapter", VLC_VAR_SETVALUE, &val, NULL );
 
+    /* "title %2i" */
+    char psz_title[10];
+    snprintf( psz_title, sizeof(psz_title), "title %2i", i_title );
+    var_Change( p_input, psz_title, VLC_VAR_SETVALUE, &val, NULL );
+
+    /* */
     Trigger( p_input, INPUT_EVENT_CHAPTER );
 }
 
