01bec0077e7d2da2bf059297d3572e0c49e36453
Laurent Aimar <fenrir@videolan.org>
2009-06-18 22:34:59 +0200

    Fixed a potential race condition in input_resource_HoldVout*.

--- src/input/resource.c
+++ src/input/resource.c
@@ -275,15 +275,12 @@ static vout_thread_t *RequestVout( input_resource_t *p_resource,
 }
 static vout_thread_t *HoldVout( input_resource_t *p_resource )
 {
-    if( p_resource->i_vout <= 0 )
-        return NULL;
-
     /* TODO FIXME: p_resource->pp_vout order is NOT stable */
     vlc_mutex_lock( &p_resource->lock_hold );
 
-    vout_thread_t *p_vout = p_resource->pp_vout[0];
-
-    vlc_object_hold( p_vout );
+    vout_thread_t *p_vout = p_resource->i_vout > 0 ? p_resource->pp_vout[0] : NULL;
+    if( p_vout )
+        vlc_object_hold( p_vout );
 
     vlc_mutex_unlock( &p_resource->lock_hold );
 
