28ec736ffa0d414ea0cc6146c231731e544556a6
Rémi Duraffort <ivoire@videolan.org>
2009-06-29 15:54:21 +0200

    Fix encoding.

--- modules/video_filter/blendbench.c
+++ modules/video_filter/blendbench.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2007 the VideoLAN team
  * $Id$
  *
- * Author: SÃ¸ren BÃ¸g <avacore@videolan.org>
+ * Author: Søren Bøg <avacore@videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
