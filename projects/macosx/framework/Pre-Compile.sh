94dfb7fbca6a4fb51623426b0a2b851eaf629dfe
Pierre d'Herbemont <pdherbemont@free.fr>
2009-06-28 18:19:13 -0700

    framework: Fix the Pre-Compile script.
    This script starts to get unreadable, it's probably time to refresh it.

--- projects/macosx/framework/Pre-Compile.sh
+++ projects/macosx/framework/Pre-Compile.sh
@@ -153,10 +153,7 @@ if test "${ACTION}" = "build"; then
         esac
     done
 
-    install_library "${VLC_BUILD_DIR}/src/${prefix}libvlc.2.dylib" "${target_lib}" "library"
-    install_library "${VLC_BUILD_DIR}/src/${prefix}libvlccore.2.dylib" "${target_lib}" "library"
-    ln -sf ${target_lib}/libvlc.2.dylib ${target_lib}/libvlc.dylib
-    ln -sf ${target_lib}/libvlccore.2.dylib ${target_lib}/libvlccore.dylib
+    install_library "${VLC_BUILD_DIR}/src/${prefix}libvlc.dylib" "${target_lib}" "library"
 
     ##########################
     # Build the share folder
