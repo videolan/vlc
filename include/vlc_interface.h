d060d810a016f8f69441e4225d360aa38380a597
Jean-Francois Massol <jf.massol@gmail.com>
2009-07-10 01:27:29 +0200

    Add .mpc to the file open dialog

--- include/vlc_interface.h
+++ include/vlc_interface.h
@@ -168,7 +168,7 @@ typedef enum vlc_dialog {
 #define INTF_ABOUT_MSG LICENSE_MSG
 
 #define EXTENSIONS_AUDIO "*.a52;*.aac;*.ac3;*.ape;*.dts;*.flac;*.it;" \
-                         "*.m4a;*.m4p;*.mka;*.mlp;*.mod;*.mp1;*.mp2;*.mp3;" \
+                         "*.m4a;*.m4p;*.mka;*.mlp;*.mod;*.mp1;*.mp2;*.mp3;*.mpc" \
                          "*.oga;*.ogg;*.oma;*.s3m;*.spx;" \
                          "*.wav;*.wma;*.wv;*.xm"
 
