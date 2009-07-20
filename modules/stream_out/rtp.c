96f5cc4d7641d69cfcf108b033edf5c8291a1895
Rémi Denis-Courmont <remi@remlab.net>
2009-07-20 19:51:30 +0300

    Do not generate a SDP file if SDP is not yet defined (fixes: #2981)

--- modules/stream_out/rtp.c
+++ modules/stream_out/rtp.c
@@ -1399,6 +1399,9 @@ static int FileSetup( sout_stream_t *p_stream )
     sout_stream_sys_t *p_sys = p_stream->p_sys;
     FILE            *f;
 
+    if( p_sys->psz_sdp == NULL )
+        return VLC_EGENERIC; /* too early */
+
     if( ( f = utf8_fopen( p_sys->psz_sdp_file, "wt" ) ) == NULL )
     {
         msg_Err( p_stream, "cannot open file '%s' (%m)",
