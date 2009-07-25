1f952b3d3380ebb606f03c452b635461cfd69283
Jean-Baptiste Kempf <jb@videolan.org>
2009-07-26 01:28:11 +0200

    Deactivate FluidSynth on Win32 still.
    This is not ready yet.

--- extras/package/win32/configure-common.sh
+++ extras/package/win32/configure-common.sh
@@ -26,6 +26,7 @@
       --enable-mozilla --with-mozilla-sdk-path=/usr/win32/gecko-sdk \
       --enable-activex \
       --enable-sse --enable-mmx \
+      --disable-fluidsynth \
       --disable-gnomevfs --disable-hal \
       --disable-cddax --disable-vcdx --disable-libcdio --disable-vcdinfo \
       --disable-libcddb --disable-dvb --enable-zvbi --disable-telx \
