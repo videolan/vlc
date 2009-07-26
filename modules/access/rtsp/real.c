f29f7e10f0f4d74ef833da6603be7c7cef77e737
Rémi Denis-Courmont <remi@remlab.net>
2009-07-26 21:08:24 +0300

    Typo

--- modules/access/rtsp/real.c
+++ modules/access/rtsp/real.c
@@ -600,7 +600,7 @@ int real_get_rdt_chunk(rtsp_client_t *rtsp_session, rmff_pheader_t *ph,
 
   int n;
   rmff_dump_pheader(ph, (char*)*buffer);
-  if (pf->length<12) return 0;
+  if (ph->length<12) return 0;
   n=rtsp_read_data(rtsp_session, (uint8_t*)(*buffer + 12), ph->length - 12);
   return (n <= 0) ? 0 : n+12;
 }
