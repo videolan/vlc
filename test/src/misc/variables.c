d94eb977dd6cb39af1f5609357f44a54112179ef
Rémi Duraffort <ivoire@videolan.org>
2009-07-27 09:51:10 +0200

    Add a basic test for var_ToggleBool.

--- test/src/misc/variables.c
+++ test/src/misc/variables.c
@@ -72,6 +72,12 @@ static void test_booleans( libvlc_int_t *p_libvlc )
         assert( var_GetBool( p_libvlc, psz_var_name[i] ) == var_value[i].b_bool );
 
     for( i = 0; i < i_var_count; i++ )
+        var_ToggleBool( p_libvlc, psz_var_name[i] );
+
+    for( i = 0; i < i_var_count; i++ )
+        assert( var_GetBool( p_libvlc, psz_var_name[i] ) != var_value[i].b_bool );
+
+    for( i = 0; i < i_var_count; i++ )
         var_Destroy( p_libvlc, psz_var_name[i] );
 }
 
