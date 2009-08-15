0884a16caf862e30dc70a0c11fb8168128f2cce9
Rémi Denis-Courmont <remi@remlab.net>
2009-08-15 15:57:23 +0300

    Missing config.h -> no _GNU_SOURCE -> no prototype for asprintf

--- src/control/vlm.c
+++ src/control/vlm.c
@@ -21,6 +21,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include <vlc/libvlc.h>
 #include <vlc/libvlc_vlm.h>
 #include <vlc_es.h>
