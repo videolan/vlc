videolan/vlc

commit aee0317592c43b4fa6d9d84bc2e70968e4b93f5f

utf8_open: make third parameter optional

src/text/filesystem.c
@@ -85,11 +85,20 @@ static int convert_path (const char *restrict path, wchar_t *restrict wpath)
  *
  * @param filename file path to open (with UTF-8 encoding)
  * @param flags open() flags, see the C library open() documentation
- * @param mode file permissions if creating a new file
  * @return a file handle on success, -1 on error (see errno).
+ * @note Contrary to standard open(), this function returns file handles
+ * with the close-on-exec flag enabled.
  */
-int utf8_open (const char *filename, int flags, mode_t mode)
+int utf8_open (const char *filename, int flags, ...)
 {
+    mode_t mode = 0;
+    va_list ap;
+
+    va_start (ap, flags);
+    if (flags & O_CREAT)
+        mode = va_arg (ap, mode_t);
+    va_end (ap);
+
 #ifdef UNDER_CE
     /*_open translates to wchar internally on WinCE*/
     return _open (filename, flags, mode);

include/vlc_charset.h
@@ -41,7 +41,7 @@ VLC_EXPORT( char *, ToLocale, ( const char * ) LIBVLC_USED );
 VLC_EXPORT( char *, ToLocaleDup, ( const char * ) LIBVLC_USED );
 
 /* TODO: move all of this to "vlc_fs.h" or something like that */
-VLC_EXPORT( int, utf8_open, ( const char *filename, int flags, mode_t mode ) LIBVLC_USED );
+VLC_EXPORT( int, utf8_open, ( const char *filename, int flags, ... ) LIBVLC_USED );
 VLC_EXPORT( FILE *, utf8_fopen, ( const char *filename, const char *mode ) LIBVLC_USED );
 VLC_EXPORT( DIR *, utf8_opendir, ( const char *dirname ) LIBVLC_USED );
 VLC_EXPORT( char *, utf8_readdir, ( DIR *dir ) LIBVLC_USED );

commit 4fd41d71393f627f94c48f8d867f0a8e2da34902

Fixed crashes introduced in [aee0317592c43b4fa6d9d84bc2e70968e4b93f5f]

src/text/filesystem.c
@@ -91,12 +91,12 @@ static int convert_path (const char *restrict path, wchar_t *restrict wpath)
  */
 int utf8_open (const char *filename, int flags, ...)
 {
-    mode_t mode = 0;
+    unsigned int mode = 0;
     va_list ap;
 
     va_start (ap, flags);
     if (flags & O_CREAT)
-        mode = va_arg (ap, mode_t);
+        mode = va_arg (ap, unsigned int);
     va_end (ap);
 
 #ifdef UNDER_CE