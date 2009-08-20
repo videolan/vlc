72d74337f77b0d7c489071daace942277a7252d6
Pierre d'Herbemont <pdherbemont@free.fr>
2009-08-20 12:04:54 +0200

    es: RGB masks needs to be uint32_t.

--- include/vlc_es.h
+++ include/vlc_es.h
@@ -122,7 +122,7 @@ struct video_format_t
     unsigned int i_frame_rate;                     /**< frame rate numerator */
     unsigned int i_frame_rate_base;              /**< frame rate denominator */
 
-    int i_rmask, i_gmask, i_bmask;          /**< color masks for RGB chroma */
+    uint32_t i_rmask, i_gmask, i_bmask;          /**< color masks for RGB chroma */
     int i_rrshift, i_lrshift;
     int i_rgshift, i_lgshift;
     int i_rbshift, i_lbshift;
