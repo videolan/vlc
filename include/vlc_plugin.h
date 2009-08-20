3a2d5d8a8cd44ac801a6fc14e559369133b08abd
Rafaël Carré <rafael.carre@gmail.com>
2009-08-20 09:44:10 +0200

    bump plugin ABI to 1.1.0a (avoid using 1.0 modules after [073c5a24])

--- include/vlc_plugin.h
+++ include/vlc_plugin.h
@@ -119,8 +119,8 @@ enum vlc_module_properties
 /**
  * Current plugin ABI version
  */
-# define MODULE_SYMBOL 1_0_0e
-# define MODULE_SUFFIX "__1_0_0e"
+# define MODULE_SYMBOL 1_1_0a
+# define MODULE_SUFFIX "__1_1_0a"
 
 /*****************************************************************************
  * Add a few defines. You do not want to read this section. Really.
