91c4ffbb29f66fcd3be76482a0a65bce40083822
Pierre d'Herbemont <pdherbemont@free.fr>
2009-08-20 16:38:06 +0200

    shout: Flag unused param.

--- modules/access_output/shout.c
+++ modules/access_output/shout.c
@@ -560,6 +560,7 @@ static ssize_t Write( sout_access_out_t *p_access, block_t *p_buffer )
  *****************************************************************************/
 static int Seek( sout_access_out_t *p_access, off_t i_pos )
 {
+    VLC_UNUSED(i_pos);
     msg_Err( p_access, "cannot seek on shout" );
     return VLC_EGENERIC;
 }
