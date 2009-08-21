279339a6e4040ea57e53dae2cbd9c8ff0f7742b3
Pierre d'Herbemont <pdherbemont@free.fr>
2009-08-21 02:28:45 +0200

    loader: Remove unused variables.

--- libs/loader/pe_image.c
+++ libs/loader/pe_image.c
@@ -302,7 +302,6 @@ static DWORD fixup_imports( WINE_MODREF *wm )
      */
  
     for (i = 0, pe_imp = pem->pe_import; pe_imp->Name ; pe_imp++) {
-    	WINE_MODREF		*wmImp;
 	IMAGE_IMPORT_BY_NAME	*pe_name;
 	PIMAGE_THUNK_DATA	import_list,thunk_list;
  	char			*name = (char *) RVA(pe_imp->Name);
@@ -451,13 +450,13 @@ static void do_relocations( unsigned int load_addr, IMAGE_BASE_RELOCATION *r )
  */
 HMODULE PE_LoadImage( int handle, LPCSTR filename, WORD *version )
 {
+    (void)filename;
     HMODULE	hModule;
     HANDLE	mapping;
 
     IMAGE_NT_HEADERS *nt;
     IMAGE_SECTION_HEADER *pe_sec;
     IMAGE_DATA_DIRECTORY *dir;
-    BY_HANDLE_FILE_INFORMATION bhfi;
     int	i, rawsize, lowest_va, vma_size, file_size = 0;
     DWORD load_addr = 0, aoep, reloc = 0;
 //    struct get_read_fd_request *req = get_req_buffer();
@@ -717,10 +716,6 @@ WINE_MODREF *PE_CreateModule( HMODULE hModule,
     IMAGE_EXPORT_DIRECTORY *pe_export = NULL;
     IMAGE_RESOURCE_DIRECTORY *pe_resource = NULL;
     WINE_MODREF *wm;
-    int	result;
-
-
-    
 
     dir = nt->OptionalHeader.DataDirectory+IMAGE_DIRECTORY_ENTRY_EXPORT;
     if (dir->Size)
