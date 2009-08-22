2858f56aa42e99ca79dd6a49e4f6d57186a04df4
Rémi Denis-Courmont <remi@remlab.net>
2009-08-22 21:38:53 +0300

    Cosmetic

--- src/video_output/window.c
+++ src/video_output/window.c
@@ -51,11 +51,11 @@ vout_window_t *vout_window_New(vlc_object_t *obj,
     case VOUT_WINDOW_TYPE_HWND:
         type = "vout window hwnd";
         break;
-    default:
-        assert(0);
     case VOUT_WINDOW_TYPE_XID:
         type = "vout window xid";
         break;
+    default:
+        assert(0);
     }
 
     window->module = module_need(window, type,
