aa774ef953dbb63c74877daaccb3fb1a3e36221f
Jakob Leben <jleben@videolan.org>
2009-08-30 16:48:47 +0200

    qt4 playlist model: add myself to authors

--- modules/gui/qt4/components/playlist/playlist_model.cpp
+++ modules/gui/qt4/components/playlist/playlist_model.cpp
@@ -6,6 +6,7 @@
  *
  * Authors: Clément Stenac <zorglub@videolan.org>
  *          Ilkka Ollakkka <ileoo (at) videolan dot org>
+ *          Jakob Leben <jleben@videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
