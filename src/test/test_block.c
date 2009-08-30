cb81fa6f2d10d1545054a507b6e23337981e0242
Rémi Denis-Courmont <remi@remlab.net>
2009-08-30 21:37:23 +0300

    Fix test case

--- src/test/test_block.c
+++ src/test/test_block.c
@@ -74,7 +74,7 @@ static void test_block (void)
     assert (block->i_buffer == 200 + sizeof (text) + 200);
     assert (!memcmp (block->p_buffer + 200, text, sizeof (text)));
 
-    block = block_Realloc (block, -200, sizeof (text));
+    block = block_Realloc (block, -200, sizeof (text) + 200);
     assert (block != NULL);
     assert (block->i_buffer == sizeof (text));
     assert (!memcmp (block->p_buffer, text, sizeof (text)));
