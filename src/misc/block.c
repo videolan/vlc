fdf97225e5f008c1fc29d32bfe12b0b70481e189
Laurent Aimar <fenrir@videolan.org>
2009-09-01 21:13:15 +0200

    Fixed block_Realloc.

--- src/misc/block.c
+++ src/misc/block.c
@@ -78,6 +78,7 @@ static void BlockRelease( block_t *p_block )
 
 static void BlockMetaCopy( block_t *restrict out, const block_t *in )
 {
+    out->p_next    = in->p_next;
     out->i_dts     = in->i_dts;
     out->i_pts     = in->i_pts;
     out->i_flags   = in->i_flags;
