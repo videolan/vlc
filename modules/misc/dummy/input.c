80e58d03041b981ef25617d424440c93a972f784
Laurent Aimar <fenrir@videolan.org>
2009-09-03 23:01:09 +0200

    Fixed dummy input Control function.
    DEMUX_GET_PTS_DELAY must not fail for an access_demux.

--- modules/misc/dummy/input.c
+++ modules/misc/dummy/input.c
@@ -117,5 +117,15 @@ void CloseDemux ( vlc_object_t *p_this )
 static int DemuxControl( demux_t *p_demux, int i_query, va_list args )
 {
     (void)p_demux; (void)i_query; (void)args;
-    return VLC_EGENERIC;
+    switch( i_query )
+    {
+    case DEMUX_GET_PTS_DELAY:
+    {
+        int64_t *pi_pts_delay = va_arg( args, int64_t * );
+        *pi_pts_delay = DEFAULT_PTS_DELAY;
+        return VLC_SUCCESS;
+    }
+    default:
+        return VLC_EGENERIC;
+    }
 }
