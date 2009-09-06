videolan/vlc

commit 96998812a21d05d8711db846e37df91cebc8eac0

qt4: ensure uri-encoded files are passed to the core

modules/gui/qt4/dialogs_provider.cpp
@@ -418,10 +418,12 @@ void DialogsProvider::addFromSimple( bool pl, bool go)
     files.sort();
     foreach( const QString &file, files )
     {
-        playlist_Add( THEPL, qtu( toNativeSeparators( file ) ), NULL,
+        char* psz_uri = make_URI( qtu( file ) );
+        playlist_Add( THEPL, psz_uri, NULL,
                       go ? ( PLAYLIST_APPEND | ( i ? PLAYLIST_PREPARSE : PLAYLIST_GO ) )
                          : ( PLAYLIST_APPEND | PLAYLIST_PREPARSE ),
                       PLAYLIST_END, pl, pl_Unlocked );
+        free( psz_uri );
         RecentsMRL::getInstance( p_intf )->addRecent(
                 toNativeSeparators( file ) );
         i++;

modules/gui/qt4/dialogs/open.cpp
@@ -339,7 +339,9 @@ void OpenDialog::finish( bool b_enqueue = false )
         bool b_start = !i && !b_enqueue;
 
         input_item_t *p_input;
-        p_input = input_item_New( p_intf, qtu( itemsMRL[i] ), NULL );
+        char* psz_uri = make_URI( qtu( itemsMRL[i] ) );
+        p_input = input_item_New( p_intf, psz_uri, NULL );
+        free( psz_uri );
 
         /* Insert options only for the first element.
            We don't know how to edit that anyway. */

modules/gui/qt4/dialogs/open.hpp
@@ -29,6 +29,7 @@
 #endif
 
 #include <vlc_common.h>
+#include <vlc_url.h>
 
 #include "util/qvlcframe.hpp"
 #include "ui/open.h"

commit 935cc5d2d393d3295f7bc9ca709d40e08c6d01fe

Win32: fix DIR_SEP failed assertion introduced in 96998812a21d05d8711db846e37df91cebc8eac0

modules/gui/qt4/dialogs_provider.cpp
@@ -418,7 +418,7 @@ void DialogsProvider::addFromSimple( bool pl, bool go)
     files.sort();
     foreach( const QString &file, files )
     {
-        char* psz_uri = make_URI( qtu( file ) );
+        char* psz_uri = make_URI( qtu( toNativeSeparators(file) ) );
         playlist_Add( THEPL, psz_uri, NULL,
                       go ? ( PLAYLIST_APPEND | ( i ? PLAYLIST_PREPARSE : PLAYLIST_GO ) )
                          : ( PLAYLIST_APPEND | PLAYLIST_PREPARSE ),