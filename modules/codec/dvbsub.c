8e51900f5e4bd47d44d5bb308d9dc301e025c700
Jean-Baptiste Kempf <jb@videolan.org>
2009-09-29 23:08:59 +0200

    Formating of comment

--- modules/codec/dvbsub.c
+++ modules/codec/dvbsub.c
@@ -24,8 +24,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
 /*****************************************************************************
@@ -37,28 +37,31 @@
  * character table identified in the subtitle descriptor.
  *
  * The spec is quite vague in this area, but what is meant is perhaps that it
- * refers to the character index in the codepage belonging to the language specified
- * in the subtitle descriptor. Potentially it's designed for widechar
+ * refers to the character index in the codepage belonging to the language
+ * specified in the subtitle descriptor. Potentially it's designed for widechar
  * (but not for UTF-*) codepages.
  *****************************************************************************
  *
  *****************************************************************************
  * Notes on DDS (Display Definition Segment)
- * DDS (Display Definition Segment) tells the decoder how the subtitle image relates to
- * the video image.
- * For SD, the subtitle image is always considered to be for display at 720x576
- * (although it's assumed that for NTSC, this is 720x480, this is not documented well)
- * Also, for SD, the subtitle image is drawn 'on the glass' (i.e. after video scaling,
-       + * letterbox, etc.)
- * For 'HD' (subs marked type 0x14/0x24 in PSI), a DDS must be present, and the subs area
- * is drawn onto the video area (scales if necessary).  The DDS tells the decoder what
- * resolution the subtitle images were intended for, and hence how to scale the subtitle
- * images for a particular video size
- * i.e. if HD video is presented as letterbox, the subs will
- * be in the same place on the video as if the video was presented on an HD set
- * indeed, if the HD video was pillarboxed by the decoder, the subs may be cut off as
- * well as the video.  The intent here is that the subs can be placed accurately on the video
- * - somthing which was missed in the original spec.
+ * -----------------------------------------
+ * DDS (Display Definition Segment) tells the decoder how the subtitle image
+ * relates to the video image.
+ * For SD, the subtitle image is always considered to be for display at
+ * 720x576 (although it's assumed that for NTSC, this is 720x480, this
+ * is not documented well) Also, for SD, the subtitle image is drawn 'on
+ * the glass' (i.e. after video scaling, letterbox, etc.)
+ * For 'HD' (subs marked type 0x14/0x24 in PSI), a DDS must be present,
+ * and the subs area is drawn onto the video area (scales if necessary).
+ * The DDS tells the decoder what resolution the subtitle images were
+ * intended for, and hence how to scale the subtitle images for a
+ * particular video size
+ * i.e. if HD video is presented as letterbox, the subs will be in the
+ * same place on the video as if the video was presented on an HD set
+ * indeed, if the HD video was pillarboxed by the decoder, the subs may
+ * be cut off as well as the video. The intent here is that the subs can
+ * be placed accurately on the video - something which was missed in the
+ * original spec.
  *
  * A DDS may also specify a window - this is where the subs images are moved so that the (0,0)
  * origin of decode is offset.
