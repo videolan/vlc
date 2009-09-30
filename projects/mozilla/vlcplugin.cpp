9dee7a82fa1a61ca457fa4d390f2df7a7881085b
Jean-Baptiste Kempf <jb@videolan.org>
2009-10-01 01:57:15 +0200

    Don't ignore-config on the webplugin
    This is bad, because many times you customize your vout configuration to make it work.

--- projects/mozilla/vlcplugin.cpp
+++ projects/mozilla/vlcplugin.cpp
@@ -127,7 +127,6 @@ NPError VlcPlugin::init(int argc, char* const argn[], char* const argv[])
     ppsz_argv[ppsz_argc++] = "-vv";
     ppsz_argv[ppsz_argc++] = "--no-stats";
     ppsz_argv[ppsz_argc++] = "--no-media-library";
-    ppsz_argv[ppsz_argc++] = "--ignore-config";
     ppsz_argv[ppsz_argc++] = "--intf=dummy";
     ppsz_argv[ppsz_argc++] = "--no-video-title-show";
 
