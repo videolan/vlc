d5f3c2a5f50c5b384079f624e86b841ba4b95fda
Rémi Duraffort <ivoire@videolan.org>
2009-10-15 10:59:35 +0200

    mosaic_bridge: add missing var_DelCallback (fix a race condition)

--- modules/stream_out/mosaic_bridge.c
+++ modules/stream_out/mosaic_bridge.c
@@ -272,6 +272,13 @@ static void Close( vlc_object_t * p_this )
     sout_stream_t     *p_stream = (sout_stream_t*)p_this;
     sout_stream_sys_t *p_sys = p_stream->p_sys;
 
+    /* Delete the callbacks */
+    var_DelCallback( p_stream, CFG_PREFIX "height", HeightCallback, p_stream );
+    var_DelCallback( p_stream, CFG_PREFIX "width", WidthCallback, p_stream );
+    var_DelCallback( p_stream, CFG_PREFIX "alpha", alphaCallback, p_stream );
+    var_DelCallback( p_stream, CFG_PREFIX "x", xCallback, p_stream );
+    var_DelCallback( p_stream, CFG_PREFIX "y", yCallback, p_stream );
+
     p_stream->p_sout->i_out_pace_nocontrol--;
 
     free( p_sys->psz_id );
