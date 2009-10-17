6ad8e69b0bc91107196977be5f62fdd2ecdf3bc6
Rémi Denis-Courmont <remi@remlab.net>
2009-10-17 23:53:10 +0300

    HTTPd: stat -> utf8_stat

--- modules/control/http/util.c
+++ modules/control/http/util.c
@@ -177,7 +177,7 @@ int ParseDirectory( intf_thread_t *p_intf, char *psz_root,
         ACL_Destroy( p_acl );
 
         struct stat st;
-        if( stat( dir, &st ) == 0 )
+        if( utf8_stat( dir, &st ) == 0 )
         {
             free( user );
             free( password );
