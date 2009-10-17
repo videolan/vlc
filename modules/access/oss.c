3e3a32b36ca76e5b87844706dbc8af23f95ab35e
Rémi Denis-Courmont <remi@remlab.net>
2009-10-17 23:30:26 +0300

    OSS: path from VLC core (or ASCII), need utf8_open()

--- modules/access/oss.c
+++ modules/access/oss.c
@@ -36,7 +36,7 @@
 #include <vlc_plugin.h>
 #include <vlc_access.h>
 #include <vlc_demux.h>
-#include <vlc_input.h>
+#include <vlc_charset.h>
 
 #include <ctype.h>
 #include <fcntl.h>
@@ -341,7 +341,7 @@ static int OpenAudioDevOss( demux_t *p_demux )
     int i_fd;
     int i_format;
 
-    i_fd = open( p_demux->p_sys->psz_device, O_RDONLY | O_NONBLOCK );
+    i_fd = utf8_open( p_demux->p_sys->psz_device, O_RDONLY | O_NONBLOCK );
 
     if( i_fd < 0 )
     {
@@ -417,7 +417,7 @@ static int OpenAudioDev( demux_t *p_demux )
 static bool ProbeAudioDevOss( demux_t *p_demux, const char *psz_device )
 {
     int i_caps;
-    int i_fd = open( psz_device, O_RDONLY | O_NONBLOCK );
+    int i_fd = utf8_open( psz_device, O_RDONLY | O_NONBLOCK );
 
     if( i_fd < 0 )
     {
--- modules/audio_output/oss.c
+++ modules/audio_output/oss.c
@@ -38,6 +38,7 @@
 
 #include <vlc_common.h>
 #include <vlc_plugin.h>
+#include <vlc_charset.h>
 
 #include <vlc_aout.h>
 
