videolan/vlc

commit 724461bdf250e856eb32f6c0b7c51b065e482982

Used VLC_CODEC_* and vlc_fourcc_GetCodec when suitable.
 There will be some regressions, I used automatic sed replacement with manual
checks but there was a lot to change.
 It's only the first pass.

modules/video_output/omapfb.c
@@ -185,9 +185,8 @@ static int Create( vlc_object_t *p_this )
     vout_thread_t *p_vout = (vout_thread_t *)p_this;
     vout_sys_t    *p_sys;
 
-    if( p_vout->fmt_in.i_chroma != VLC_FOURCC('I','4','2','0') &&
-        p_vout->fmt_in.i_chroma != VLC_FOURCC('I','Y','U','V') &&
-        p_vout->fmt_in.i_chroma != VLC_FOURCC('Y','V','1','2') )
+    if( p_vout->fmt_in.i_chroma != VLC_CODEC_I420 &&
+        p_vout->fmt_in.i_chroma != VLC_CODEC_YV12 )
         return VLC_EGENERIC;
 
     /* Allocate instance and initialize some members */
@@ -308,7 +307,7 @@ static int Init( vout_thread_t *p_vout )
     }
 
     p_vout->output.i_chroma =
-    p_vout->fmt_out.i_chroma = VLC_FOURCC( 'Y','4','2','0' );
+    p_vout->fmt_out.i_chroma = VLC_CODEC_I420;
     p_sys->i_color_format = OMAPFB_COLOR_YUV420;
 
     // place in the framebuffer where we have to write

commit 8f6d32b1239732488a11a21da24d31d32ff5b201

OMAP framebuffer: I420 != Y420
Regression from 724461bdf250e856eb32f6c0b7c51b065e482982

modules/video_output/omapfb.c
@@ -271,7 +271,7 @@ static int Init( vout_thread_t *p_vout )
     }
 
     p_vout->output.i_chroma =
-    p_vout->fmt_out.i_chroma = VLC_CODEC_I420;
+    p_vout->fmt_out.i_chroma = VLC_FOURCC( 'Y','4','2','0' );
     p_sys->i_color_format = OMAPFB_COLOR_YUV420;
 
     // place in the framebuffer where we have to write