92f73e2ae544d4cb6c94fa91b76c525c94bb769a
Rémi Denis-Courmont <remi@remlab.net>
2009-10-18 23:27:19 +0300

    Fix goofed inconsistent license text from myself.

--- modules/video_output/xcb/keys.c
+++ modules/video_output/xcb/keys.c
@@ -6,8 +6,8 @@
  * Copyright © 2009 Rémi Denis-Courmont
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2.0
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1
  * of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
