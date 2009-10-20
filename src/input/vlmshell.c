2d164b6ff2ba706829fb65c53d15d5c70799c377
Pierre Ynard <linkfanel@yahoo.fr>
2009-10-20 18:58:00 +0200

    vod: remove useless include

--- src/input/vlmshell.c
+++ src/input/vlmshell.c
@@ -50,7 +50,6 @@
 #include "input_internal.h"
 #include <vlc_stream.h>
 #include "vlm_internal.h"
-#include <vlc_vod.h>
 #include <vlc_charset.h>
 #include <vlc_sout.h>
 #include "../stream_output/stream_output.h"
