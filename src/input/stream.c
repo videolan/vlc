videolan/vlc

commit 0d1f966a0b3581d3e96423ce34f32b4da7725c31

Implement access_GetParentInput and demux_GetParentInput and use.
This try to avoid vlc_object_find() as much as possible.
This is conservative, because where there is no associated parent input, we'll try to find in certain cases the parent input. This will probably be
 removed later on. Because yes, there is not necessarily a parent input for access and demux, especially if created from stream_UrlNew().

src/input/stream.c
@@ -229,6 +229,7 @@ stream_t *stream_CommonNew( vlc_object_t *p_obj )
 
     return s;
 }
+
 void stream_CommonDelete( stream_t *s )
 {
     if( s->p_text )
@@ -258,8 +259,15 @@ stream_t *__stream_UrlNew( vlc_object_t *p_parent, const char *psz_url )
     strcpy( psz_dup, psz_url );
     input_SplitMRL( &psz_access, &psz_demux, &psz_path, psz_dup );
 
+    /* Get a weak link to the parent input */
+    /* FIXME: This should probably be removed in favor of a NULL input. */
+    input_thread_t *p_input = (input_thread_t *)vlc_object_find( p_parent, VLC_OBJECT_INPUT, FIND_PARENT );
+    
     /* Now try a real access */
-    p_access = access_New( p_parent, psz_access, psz_demux, psz_path );
+    p_access = access_New( p_parent, p_input, psz_access, psz_demux, psz_path );
+
+    if(p_input)
+        vlc_object_release((vlc_object_t*)p_input);
 
     if( p_access == NULL )
     {
@@ -285,6 +293,7 @@ stream_t *stream_AccessNew( access_t *p_access, char **ppsz_list )
     if( !s )
         return NULL;
 
+    s->p_input = p_access->p_input;
     s->psz_path = strdup( p_access->psz_path );
     s->p_sys = p_sys = malloc( sizeof( *p_sys ) );
     if( !s->psz_path || !s->p_sys )
@@ -348,7 +357,7 @@ stream_t *stream_AccessNew( access_t *p_access, char **ppsz_list )
             if( !psz_name )
                 break;
 
-            access_t *p_tmp = access_New( p_access,
+            access_t *p_tmp = access_New( p_access, p_access->p_input,
                                           p_access->psz_access, "", psz_name );
             if( !p_tmp )
                 continue;
@@ -1533,8 +1542,7 @@ char *stream_ReadLine( stream_t *s )
                 }
 
                 /* FIXME that's UGLY */
-                input_thread_t *p_input;
-                p_input = (input_thread_t *)vlc_object_find( s, VLC_OBJECT_INPUT, FIND_PARENT );
+                input_thread_t *p_input = s->p_input;
                 if( p_input != NULL)
                 {
                     var_Create( p_input, "subsdec-encoding", VLC_VAR_STRING | VLC_VAR_DOINHERIT );
@@ -1716,7 +1724,7 @@ static int AReadStream( stream_t *s, void *p_read, unsigned int i_read )
 
         msg_Dbg( s, "opening input `%s'", psz_name );
 
-        p_list_access = access_New( s, p_access->psz_access, "", psz_name );
+        p_list_access = access_New( s, s->p_input, p_access->psz_access, "", psz_name );
 
         if( !p_list_access ) return 0;
 
@@ -1788,7 +1796,7 @@ static block_t *AReadBlock( stream_t *s, bool *pb_eof )
 
         msg_Dbg( s, "opening input `%s'", psz_name );
 
-        p_list_access = access_New( s, p_access->psz_access, "", psz_name );
+        p_list_access = access_New( s, s->p_input, p_access->psz_access, "", psz_name );
 
         if( !p_list_access ) return 0;
 
@@ -1843,7 +1851,7 @@ static int ASeek( stream_t *s, int64_t i_pos )
         if( i != p_sys->i_list_index && i != 0 )
         {
             p_list_access =
-                access_New( s, p_access->psz_access, "", psz_name );
+                access_New( s, s->p_input, p_access->psz_access, "", psz_name );
         }
         else if( i != p_sys->i_list_index )
         {

include/vlc_access.h
@@ -115,6 +115,9 @@ struct access_t
         int          i_seekpoint;/* idem, start from 0 */
     } info;
     access_sys_t *p_sys;
+
+    /* Weak link to parent input */
+    input_thread_t *p_input;
 };
 
 static inline int access_vaControl( access_t *p_access, int i_query, va_list args )
@@ -143,6 +146,12 @@ static inline void access_InitFields( access_t *p_a )
     p_a->info.i_title = 0;
     p_a->info.i_seekpoint = 0;
 }
+
+/**
+ * This function will return the parent input of this access.
+ * It is retained. It can return NULL.
+ */
+VLC_EXPORT( input_thread_t *, access_GetParentInput, ( access_t *p_access ) );
 
 #define ACCESS_SET_CALLBACKS( read, block, control, seek )              \
     p_access->pf_read = read;                                           \

include/vlc_demux.h
@@ -71,6 +71,9 @@ struct demux_t
         int          i_seekpoint;   /* idem, start from 0 */
     } info;
     demux_sys_t *p_sys;
+
+    /* Weak link to parent input */
+    input_thread_t *p_input;
 };
 
 
@@ -189,6 +192,12 @@ VLC_EXPORT( decoder_t *,demux_PacketizerNew, ( demux_t *p_demux, es_format_t *p_
  * This function will destroy a packetizer create by demux_PacketizerNew.
  */
 VLC_EXPORT( void, demux_PacketizerDestroy, ( decoder_t *p_packetizer ) );
+
+/**
+ * This function will return the parent input of this demux.
+ * It is retained. Can return NULL.
+ */
+VLC_EXPORT( input_thread_t *, demux_GetParentInput, ( demux_t *p_demux ) );
 
 /* */
 #define DEMUX_INIT_COMMON() do {            \

include/vlc_stream.h
@@ -75,6 +75,9 @@ struct stream_t
 
     /* Text reader state */
     stream_text_t *p_text;
+
+    /* Weak link to parent input */
+    input_thread_t *p_input;
 };
 
 /**
@@ -157,8 +160,8 @@ static inline char *stream_ContentType( stream_t *s )
  * Create a special stream and a demuxer, this allows chaining demuxers
  * You must delete it using stream_Delete.
  */
-#define stream_DemuxNew( a, b, c ) __stream_DemuxNew( VLC_OBJECT(a), b, c)
-VLC_EXPORT( stream_t *,__stream_DemuxNew, ( vlc_object_t *p_obj, const char *psz_demux, es_out_t *out ) );
+VLC_EXPORT( stream_t *, stream_DemuxNew, ( demux_t *p_demux, const char *psz_demux, es_out_t *out ) );
+    
 /**
  * Send data to a stream_t handle created by stream_DemuxNew.
  */

src/input/access.c
@@ -31,8 +31,9 @@
 /*****************************************************************************
  * access_New:
  *****************************************************************************/
-access_t *__access_New( vlc_object_t *p_obj, const char *psz_access,
-                        const char *psz_demux, const char *psz_path )
+access_t *__access_New( vlc_object_t *p_obj, input_thread_t *p_parent_input,
+                        const char *psz_access, const char *psz_demux,
+                        const char *psz_path )
 {
     access_t *p_access = vlc_custom_create( p_obj, sizeof (*p_access),
                                             VLC_OBJECT_GENERIC, "access" );
@@ -43,6 +44,8 @@ access_t *__access_New( vlc_object_t *p_obj, const char *psz_access,
     /* */
     msg_Dbg( p_obj, "creating access '%s' path='%s'",
              psz_access, psz_path );
+
+    p_access->p_input = p_parent_input;
 
     p_access->psz_path   = strdup( psz_path );
     p_access->psz_access = strdup( psz_access );
@@ -87,5 +90,14 @@ void access_Delete( access_t *p_access )
     free( p_access->psz_demux );
 
     vlc_object_release( p_access );
+}
+
+
+/*****************************************************************************
+ * access_GetParentInput:
+ *****************************************************************************/
+input_thread_t * access_GetParentInput( access_t *p_access )
+{
+    return p_access->p_input ? vlc_object_hold((vlc_object_t *)p_access->p_input) : NULL;
 }
 

src/input/access.h
@@ -32,9 +32,10 @@
 #include <vlc_common.h>
 #include <vlc_access.h>
 
-#define access_New( a, b, c, d ) __access_New(VLC_OBJECT(a), b, c, d )
-access_t * __access_New( vlc_object_t *p_obj, const char *psz_access,
-                          const char *psz_demux, const char *psz_path );
+#define access_New( a, b, c, d, e ) __access_New(VLC_OBJECT(a), b, c, d, e )
+access_t * __access_New( vlc_object_t *p_obj, input_thread_t *p_input,
+                         const char *psz_access, const char *psz_demux,
+                         const char *psz_path );
 void access_Delete( access_t * );
 
 #endif

src/input/demux.c
@@ -37,7 +37,7 @@ static bool SkipAPETag( demux_t *p_demux );
  * demux_New:
  *  if s is NULL then load a access_demux
  *****************************************************************************/
-demux_t *__demux_New( vlc_object_t *p_obj,
+demux_t *__demux_New( vlc_object_t *p_obj, input_thread_t *p_parent_input,
                        const char *psz_access, const char *psz_demux,
                        const char *psz_path,
                        stream_t *s, es_out_t *out, bool b_quick )
@@ -48,6 +48,8 @@ demux_t *__demux_New( vlc_object_t *p_obj,
     const char *psz_module;
 
     if( p_demux == NULL ) return NULL;
+
+    p_demux->p_input = p_parent_input;
 
     /* Parse URL */
     p_demux->psz_access = strdup( psz_access );
@@ -202,6 +204,15 @@ void demux_Delete( demux_t *p_demux )
 
     vlc_object_release( p_demux );
 }
+
+/*****************************************************************************
+ * demux_GetParentInput:
+ *****************************************************************************/
+input_thread_t * demux_GetParentInput( demux_t *p_demux )
+{
+    return p_demux->p_input ? vlc_object_hold((vlc_object_t*)p_demux->p_input) : NULL;
+}
+
 
 /*****************************************************************************
  * demux_vaControlHelper:

src/input/demux.h
@@ -35,8 +35,8 @@
 #include "stream.h"
 
 /* stream_t *s could be null and then it mean a access+demux in one */
-#define demux_New( a, b, c, d, e, f,g ) __demux_New(VLC_OBJECT(a),b,c,d,e,f,g)
-demux_t *__demux_New( vlc_object_t *p_obj, const char *psz_access, const char *psz_demux, const char *psz_path, stream_t *s, es_out_t *out, bool );
+#define demux_New( a, b, c, d, e, f, g, h ) __demux_New(VLC_OBJECT(a),b,c,d,e,f,g,h)
+demux_t *__demux_New( vlc_object_t *p_obj, input_thread_t *p_parent_input, const char *psz_access, const char *psz_demux, const char *psz_path, stream_t *s, es_out_t *out, bool );
 
 void demux_Delete( demux_t * );
 

commit aeaf15c3bee2514e8722f6207301deb3ad65bb07

Don't release an object that was not held, fix crash
This piece of code (transparent UTF-16 to UTF-8 conversion) is wrong in
many ways though. This really does NOT belong here, and should be done
in the subsdec plugin instead.

This fixes a regression from [0d1f966a0b3581d3e96423ce34f32b4da7725c31]

src/input/stream.c
@@ -1547,7 +1547,6 @@ char *stream_ReadLine( stream_t *s )
                 {
                     var_Create( p_input, "subsdec-encoding", VLC_VAR_STRING | VLC_VAR_DOINHERIT );
                     var_SetString( p_input, "subsdec-encoding", "UTF-8" );
-                    vlc_object_release( p_input );
                 }
             }
         }