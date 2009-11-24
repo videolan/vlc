136ebd846f72d5707e7d31f91ac7299b1c35e992
Rémi Duraffort <ivoire@videolan.org>
2009-11-24 22:05:11 +0100

    tdestroy: fix encoding.

--- compat/tdestroy.c
+++ compat/tdestroy.c
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * tdestroy.c: GNU tdestroy() replacement
  *****************************************************************************
- * Copyright © 1998-2009 the VideoLAN project
+ * Copyright Â© 1998-2009 the VideoLAN project
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
