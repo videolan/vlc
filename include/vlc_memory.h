f7600a71bbdee8407a4eddee197e8d599f6d002b
JP Dinger <jpd@videolan.org>
2009-12-05 09:06:20 +0100

    Introduce realloc_or_free(), which does what realloc() does but will free the argument if the reallocation fails. This is useful in some, but not all, use cases of realloc().

---
+++ include/vlc_memory.h
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * vlc_memory.h: Memory functions
+ *****************************************************************************
+ * Copyright (C) 2009 the VideoLAN team
+ *
+ * Authors: JP Dinger <jpd at videolan dot org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef VLC_MEMORY_H
+#define VLC_MEMORY_H 1
+
+/**
+ * \file
+ * This file deals with memory fixups
+ */
+
+/**
+ * \defgroup memory Memory
+ * @{
+ */
+
+/**
+ * This wrapper around realloc() will free the input pointer when
+ * realloc() returns NULL. The use case ptr = realloc(ptr, newsize) will
+ * cause a memory leak when ptr pointed to a heap allocation before,
+ * leaving the buffer allocated but unreferenced. vlc_realloc() is a
+ * drop-in replacement for that use case (and only that use case).
+ */
+static inline void *realloc_or_free( void *p, size_t sz )
+{
+    void *n = realloc(p,sz);
+    if( !n )
+        free(p);
+    return n;
+}
+
+/**
+ * @}
+ */
+
+#endif
