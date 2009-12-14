141de283295737e6a0f5ea24856ad520beb74d7b
Rémi Denis-Courmont <remi@remlab.net>
2009-12-14 20:44:15 +0200

    Comment typo

--- modules/codec/a52.c
+++ modules/codec/a52.c
@@ -159,7 +159,7 @@ static int OpenCommon( vlc_object_t *p_this, bool b_packetizer )
 
 static int OpenDecoder( vlc_object_t *p_this )
 {
-    /* HACK: Don't use this codec if we don't have an dts audio filter */
+    /* HACK: Don't use this codec if we don't have an a52 audio filter */
     if( !module_exists( "a52tofloat32" ) )
         return VLC_EGENERIC;
     return OpenCommon( p_this, false );
