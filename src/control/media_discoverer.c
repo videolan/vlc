2d69f76b7a9731eeb86e4d8da4af77b6a8988234
Pierre d'Herbemont <pdherbemont@free.fr>
2009-12-16 20:29:53 +0100

    libvlc: Make sure we remove object observers in media_discoverer.

--- src/control/media_discoverer.c
+++ src/control/media_discoverer.c
@@ -247,7 +247,25 @@ libvlc_media_discoverer_release( libvlc_media_discoverer_t * p_mdis )
 {
     int i;
 
+    vlc_event_detach( services_discovery_EventManager( p_mdis->p_sd ),
+                     vlc_ServicesDiscoveryItemAdded,
+                     services_discovery_item_added,
+                     p_mdis );
+    vlc_event_detach( services_discovery_EventManager( p_mdis->p_sd ),
+                     vlc_ServicesDiscoveryItemRemoved,
+                     services_discovery_item_removed,
+                     p_mdis );
+    vlc_event_detach( services_discovery_EventManager( p_mdis->p_sd ),
+                     vlc_ServicesDiscoveryStarted,
+                     services_discovery_started,
+                     p_mdis );
+    vlc_event_detach( services_discovery_EventManager( p_mdis->p_sd ),
+                     vlc_ServicesDiscoveryEnded,
+                     services_discovery_ended,
+                     p_mdis );
+
     libvlc_media_list_release( p_mdis->p_mlist );
+
     vlc_sd_StopAndDestroy( p_mdis->p_sd );
 
     /* Free catname_to_submedialist and all the mlist */
