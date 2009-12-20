8fe7e00b905f185e7aa4c15e139f2974df723465
Laurent Aimar <fenrir@videolan.org>
2009-12-20 23:52:31 +0100

    Fixed audio date on rate change in FLAC packetizer.

--- modules/codec/flac.c
+++ modules/codec/flac.c
@@ -478,6 +478,7 @@ static block_t *PacketizeBlock( decoder_t *p_dec, block_t **pp_block )
             {
                 p_dec->fmt_out.audio.i_rate = p_sys->i_rate;
                 date_Init( &p_sys->end_date, p_sys->i_rate, 1 );
+                date_Set( &p_sys->end_date, p_sys->i_pts );
             }
             p_sys->i_state = STATE_NEXT_SYNC;
             p_sys->i_frame_size = 1;
