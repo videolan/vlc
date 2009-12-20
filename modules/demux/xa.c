a6a7af28ea33c88e2791bd043cd0ae05b7c4c470
Laurent Aimar <fenrir@videolan.org>
2009-12-20 22:11:17 +0100

    Fixed initial timestamp value in xa demuxer.

--- modules/demux/xa.c
+++ modules/demux/xa.c
@@ -176,11 +176,12 @@ static int Demux( demux_t *p_demux )
     }
 
     i_frames = p_block->i_buffer / p_sys->fmt.audio.i_bytes_per_frame;
-    p_block->i_dts = p_block->i_pts =
-        date_Increment( &p_sys->pts,
-                        i_frames * p_sys->fmt.audio.i_frame_length );
+    p_block->i_dts = p_block->i_pts = VLC_TS_0 + date_Get( &p_sys->pts );
     es_out_Control( p_demux->out, ES_OUT_SET_PCR, p_block->i_pts );
     es_out_Send( p_demux->out, p_sys->p_es, p_block );
+
+    date_Increment( &p_sys->pts, i_frames * p_sys->fmt.audio.i_frame_length );
+
     return 1;
 }
 
