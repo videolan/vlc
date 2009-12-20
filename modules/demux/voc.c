f587eaf89c5952625dd935ab4e5228d8173e6baf
Laurent Aimar <fenrir@videolan.org>
2009-12-20 22:09:14 +0100

    Fixed initial timestamp value in VOC demuxer.

--- modules/demux/voc.c
+++ modules/demux/voc.c
@@ -462,12 +462,13 @@ static int Demux( demux_t *p_demux )
         p_sys->i_silence_countdown -= i;
     }
 
-    p_block->i_dts = p_block->i_pts =
-        date_Increment( &p_sys->pts, p_sys->fmt.audio.i_frame_length * i );
+    p_block->i_dts = p_block->i_pts = VLC_TS_0 + date_Get( &p_sys->pts );
 
     es_out_Control( p_demux->out, ES_OUT_SET_PCR, p_block->i_pts );
 
     es_out_Send( p_demux->out, p_sys->p_es, p_block );
+
+    date_Increment( &p_sys->pts, p_sys->fmt.audio.i_frame_length * i );
 
     return 1;
 }
