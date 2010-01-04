7642c71dee850dcd57c714b5861157d2e61cc7fd
Rafaël Carré <rafael.carre@gmail.com>
2010-01-04 16:51:25 +0100

    wma_fixed: raise priority above avcodec float decoder

--- modules/codec/wmafixed/wma.c
+++ modules/codec/wmafixed/wma.c
@@ -81,7 +81,7 @@ vlc_module_begin();
     set_category( CAT_INPUT );
     set_subcategory( SUBCAT_INPUT_ACODEC );
     set_description( _("WMA v1/v2 fixed point audio decoder") );
-    set_capability( "decoder", 50 );
+    set_capability( "decoder", 80 );
     add_shortcut( "wmafixed" )
     set_callbacks( OpenDecoder, CloseDecoder );
 vlc_module_end();
