a3bcd2df4f6ac2825d5b02e407d3258800a8b899
Rafaël Carré <rafael.carre@gmail.com>
2010-01-04 13:28:36 +0100

    Revert "macosx/framework: Support multiple architectures."
    This reverts commit 5a20b90e8c6920a79d7d7b7ca56fb011b97dfc73.

--- projects/macosx/framework/Configure.sh
+++ projects/macosx/framework/Configure.sh
@@ -48,6 +48,7 @@ args="--disable-ncurses $args"
 args="--disable-httpd $args"
 args="--disable-vlm $args"
 args="--disable-skins2 $args"
+args="--disable-x11 $args"
 args="--disable-glx $args"
 args="--disable-xvideo $args"
 args="--disable-xcb $args"
@@ -70,28 +71,12 @@ else
 	args="--enable-release $args"
 fi
 
-archcount=0
-
 # 64 bits switches
-for a in $ARCHS; do
-	archs_args="-arch $a $archs_args"
-    let archcount=archcount+1
-done
-
-if test $archcount -ge 2; then
-    echo "****"
-    echo "WARNING: You are building a VLC with --disable-dependency-tracking"
-    echo "         because FAT architecture are not compatible with "
-    echo "         disable-dependency-tracking."
-    echo ""
-    echo "         You will have to do a clean build manually."
-    echo ""
-    echo "To disable this message, build only one architecture."
-    echo ""
-    echo "****"
-    args="--disable-dependency-tracking $args"
+if test $ARCHS = "x86_64"
+then
+	args="--build=x86_64-apple-darwin10 $args"
 fi
 
-echo "Running [$archs_args] configure $args"
+echo "Running configure $args"
 top_srcdir="$SRCROOT/../../.."
-CFLAGS="$archs_args" CXXFLAGS="$archs_args" OBJCFLAGS="$archs_args" exec $top_srcdir/configure $args
+CFLAGS="-arch $ARCHS" CXXFLAGS="-arch $ARCHS" CPPFLAGS="-arch $ARCHS" OBJCFLAGS="-arch $ARCHS" exec $top_srcdir/configure $args
