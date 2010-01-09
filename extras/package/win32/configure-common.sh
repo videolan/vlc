c86d8f7b48d928a44cbba0712d54e86466192ea1
Jean-Baptiste Kempf <jb@videolan.org>
2010-01-09 02:13:31 +0100

    Small fixes for Win32 configure

--- extras/package/win32/configure-common.sh
+++ extras/package/win32/configure-common.sh
@@ -28,6 +28,6 @@
       --enable-sse --enable-mmx \
       --enable-libcddb \
       --enable-zvbi --disable-telx \
-      --disable-vcdx --disable-libcdio --disable-vcdinfo \
+      --disable-vcdx --disable-libcdio --disable-libvcdinfo \
       --disable-dvb \
       --enable-peflags
