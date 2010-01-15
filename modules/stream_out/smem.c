5304cd0f5b91f3fab7ebd5552d072d147ae4539c
Jean-Baptiste Kempf <jb@videolan.org>
2010-01-15 08:20:06 +0100

    Better Shortname for smem

--- modules/stream_out/smem.c
+++ modules/stream_out/smem.c
@@ -94,7 +94,7 @@ static void Close( vlc_object_t * );
 #define SOUT_PREFIX_AUDIO SOUT_CFG_PREFIX"audio-"
 
 vlc_module_begin ()
-    set_shortname( N_("smem"))
+    set_shortname( N_("Smem"))
     set_description( N_("Stream output to memory buffer") )
     set_capability( "sout stream", 0 )
     add_shortcut( "smem" )
