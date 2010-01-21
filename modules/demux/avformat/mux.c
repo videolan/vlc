837e80b8e71c22f2a6acca1c2e6554a44533b231
Rafaël Carré <rafael.carre@gmail.com>
2010-01-21 10:08:32 +0100

    avformat mux: only write trailer if header was written

--- modules/demux/avformat/mux.c
+++ modules/demux/avformat/mux.c
@@ -174,7 +174,7 @@ void CloseMux( vlc_object_t *p_this )
     sout_mux_sys_t *p_sys = p_mux->p_sys;
     unsigned int i;
 
-    if( av_write_trailer( p_sys->oc ) < 0 )
+    if( !p_sys->b_write_header && av_write_trailer( p_sys->oc ) < 0 )
     {
         msg_Err( p_mux, "could not write trailer" );
     }
