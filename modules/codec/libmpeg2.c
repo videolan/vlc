7ad6676d1b65a222682dc814782b57de798c068c
Laurent Aimar <fenrir@videolan.org>
2010-01-24 15:26:09 +0100

    Decreased priority of libmpeg2 decoder below avcodec.
     It still segfaults on broken streams (or on seeks #3153) and I am not
    willing to spend more time on it.

--- modules/codec/libmpeg2.c
+++ modules/codec/libmpeg2.c
@@ -126,7 +126,7 @@ static int DpbDisplayPicture( decoder_t *, picture_t * );
  *****************************************************************************/
 vlc_module_begin ()
     set_description( N_("MPEG I/II video decoder (using libmpeg2)") )
-    set_capability( "decoder", 150 )
+    set_capability( "decoder", 50 )
     set_category( CAT_INPUT )
     set_subcategory( SUBCAT_INPUT_VCODEC )
     set_callbacks( OpenDecoder, CloseDecoder )
