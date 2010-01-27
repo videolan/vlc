26fb927690e52e0c6bcd7deb304a814620a8b997
Rémi Denis-Courmont <remi@remlab.net>
2010-01-27 20:33:31 +0200

    aout: use vlc_custom_create

--- src/audio_output/common.c
+++ src/audio_output/common.c
@@ -79,7 +79,8 @@ aout_instance_t * __aout_New( vlc_object_t * p_parent )
     aout_instance_t * p_aout;
 
     /* Allocate descriptor. */
-    p_aout = vlc_object_create( p_parent, VLC_OBJECT_AOUT );
+    p_aout = vlc_custom_create( p_parent, sizeof( *p_aout ), VLC_OBJECT_AOUT,
+                                "audio output" );
     if( p_aout == NULL )
     {
         return NULL;
