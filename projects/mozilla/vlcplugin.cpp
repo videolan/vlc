32ec9c1e6d9a3453046adec942b4d081d108a040
Rémi Duraffort <ivoire@videolan.org>
2010-02-03 16:51:48 +0100

    mozilla: fix inverted logic (thanks jpd).

--- projects/mozilla/vlcplugin.cpp
+++ projects/mozilla/vlcplugin.cpp
@@ -453,7 +453,7 @@ int VlcPlugin::playlist_add( const char *mrl, libvlc_exception_t *ex )
 {
     int item = -1;
     libvlc_media_t *p_m = libvlc_media_new(libvlc_instance,mrl);
-    if( p_m )
+    if( !p_m )
         return -1;
 
     libvlc_media_list_lock(libvlc_media_list);
@@ -472,7 +472,7 @@ int VlcPlugin::playlist_add_extended_untrusted( const char *mrl, const char *nam
 {
     libvlc_media_t *p_m = libvlc_media_new(libvlc_instance, mrl);
     int item = -1;
-    if( p_m )
+    if( !p_m )
         return -1;
 
     for( int i = 0; i < optc; ++i )
