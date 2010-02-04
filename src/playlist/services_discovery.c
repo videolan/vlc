videolan/vlc

commit 0a69bbfd9e69bed4760995eccf99552a68a7e193

luasd: use a configuration chain to pass parameters to SD
Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

src/playlist/services_discovery.c
@@ -280,10 +280,13 @@ int playlist_ServicesDiscoveryAdd( playlist_t *p_playlist, const char *psz_modul
     if( !p_sd )
         return VLC_ENOMEM;
 
-    module_t *m = module_find_by_shortcut( psz_module );
+    char *psz_name = NULL;
+    config_ChainCreate( &psz_name, &p_sd->p_cfg, psz_module );
+
+    module_t *m = module_find_by_shortcut( psz_name );
     if( !m )
     {
-        msg_Err( p_playlist, "No such module: %s", psz_module );
+        msg_Err( p_playlist, "No such module: %s", psz_name );
         vlc_sd_Destroy( p_sd );
         return VLC_EGENERIC;
     }
@@ -300,7 +303,7 @@ int playlist_ServicesDiscoveryAdd( playlist_t *p_playlist, const char *psz_modul
     playlist_item_t *p_node;
 
     PL_LOCK;
-    p_node = playlist_NodeCreate( p_playlist, module_get_name( m, true ),
+    p_node = playlist_NodeCreate( p_playlist, psz_module,
                                   p_playlist->p_root, 0, NULL );
     PL_UNLOCK;
     module_release( m );
@@ -313,7 +316,7 @@ int playlist_ServicesDiscoveryAdd( playlist_t *p_playlist, const char *psz_modul
                       vlc_ServicesDiscoveryItemRemoved,
                       playlist_sd_item_removed, p_node );
 
-    if( !vlc_sd_Start( p_sd, psz_module ) )
+    if( !vlc_sd_Start( p_sd, psz_name ) )
     {
         vlc_sd_Destroy( p_sd );
         free( p_sds );

include/vlc_services_discovery.h
@@ -48,6 +48,8 @@ struct services_discovery_t
 
     vlc_event_manager_t event_manager;      /* Accessed through Setters for non class function */
 
+    config_chain_t *p_cfg;
+
     services_discovery_sys_t *p_sys;
 };
 

modules/gui/qt4/components/playlist/selector.cpp
@@ -173,7 +173,7 @@ void PLSelector::setSource( QTreeWidgetItem *item )
 
     if( i_type == SD_TYPE )
     {
-        pl_item = playlist_ChildSearchName( THEPL->p_root, qtu( item->data(0, LONGNAME_ROLE ).toString() ) );
+        pl_item = playlist_ChildSearchName( THEPL->p_root, qtu( item->data(0, NAME_ROLE ).toString() ) );
         if( item->data( 0, SPECIAL_ROLE ).toInt() == IS_PODCAST )
         {
             if( pl_item && !sd_loaded )

commit 5ec48fbcda446d0fd4050f5db6f3a52cf772c2a2

De-uglify root SD item name
Regression from [0a69bbfd9e69bed4760995eccf99552a68a7e193].

src/playlist/services_discovery.c
@@ -303,7 +303,7 @@ int playlist_ServicesDiscoveryAdd( playlist_t *p_playlist, const char *psz_modul
     playlist_item_t *p_node;
 
     PL_LOCK;
-    p_node = playlist_NodeCreate( p_playlist, psz_module,
+    p_node = playlist_NodeCreate( p_playlist, module_get_name( m, true ),
                                   p_playlist->p_root, 0, NULL );
     PL_UNLOCK;
     module_release( m );