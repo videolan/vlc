3f6288216a3f3e92d4ea04add0be59097b08a29a
Antoine Cellerier <dionoea@videolan.org>
2010-02-08 17:21:15 +0100

    Second blind attempt at fixing linking with --disable-vlm

--- src/missing.c
+++ src/missing.c
@@ -377,6 +377,7 @@ vlm_message_t *vlm_MessageNew (const char *a, const char *fmt, ...)
     return vlm_MessageSimpleNew (a);
 }
 
+#undef vlm_New
 vlm_t *vlm_New (vlc_object_t *obj)
 {
      msg_Err (obj, "VLM not compiled-in!");
