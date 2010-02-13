1f849e1dce1017c2aaf29a2dfb4c9063785f079a
Rémi Denis-Courmont <remi@remlab.net>
2010-02-13 18:49:43 +0200

    You shall include <config.h>

--- modules/gui/qt4/components/epg/EPGWidget.cpp
+++ modules/gui/qt4/components/epg/EPGWidget.cpp
@@ -21,6 +21,10 @@
  * 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include "EPGWidget.hpp"
 
 #include <QGridLayout>
