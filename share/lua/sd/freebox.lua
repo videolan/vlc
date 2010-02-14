b1b0b33e2d5488fa555cfc3feabfb3aa78ddfdb5
Fabio Ritrovato <sephiroth87@videolan.org>
2010-02-14 21:09:16 +0100

    Replace Freebox and French TV SD from shout.c with lua scripts
    They should work fine, but I can't test them from here...

---
+++ share/lua/sd/freebox.lua
@@ -0,0 +1,26 @@
+--SD_Description=Freebox TV
+--[[
+ $Id$
+
+ Copyright © 2010 the VideoLAN team
+ 
+ Authors: Fabio Ritrovato <sephiroth87 at videolan dot org>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+--]]
+
+function main()
+    vlc.sd.add_item( {url="http://mafreebox.freebox.fr/freeboxtv/playlist.m3u",options={"deinterlace=1"}} )
+end
