ba1fea8d5e1c18719499ca7b1b5346ee6ca2d3c2
Pierre d'Herbemont <pdherbemont@free.fr>
2010-02-25 23:03:35 +0100

    test: Alloc more time for media_list_player test.

--- test/libvlc/media_list_player.c
+++ test/libvlc/media_list_player.c
@@ -518,6 +518,9 @@ int main (void)
 {
     test_init();
 
+    // There are 6 tests. And they take some times.
+    alarm(6 * 5);
+
     test_media_list_player_pause_stop (test_defaults_args, test_defaults_nargs);
     test_media_list_player_play_item_at_index (test_defaults_args, test_defaults_nargs);
     test_media_list_player_previous (test_defaults_args, test_defaults_nargs);
