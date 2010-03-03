c83becf565eee9c5c867a2455fd203b5373ba166
Geoffroy Couprie <geal@videolan.org>
2010-03-03 20:38:02 +0100

    Win32: use ShellExecuteW instead of ShellExecuteA in the update code
    Fixes #3355

--- src/misc/update.c
+++ src/misc/update.c
@@ -739,7 +739,9 @@ static void* update_DownloadReal( vlc_object_t *p_this )
 
     if(answer == 1)
     {
-        answer = ShellExecuteA( NULL, "open", psz_destfile, NULL, NULL, SW_SHOW);
+        wchar_t psz_wdestfile[MAX_PATH];
+        MultiByteToWideChar( CP_UTF8, 0, psz_destfile, -1, psz_wdestfile, MAX_PATH );
+        answer = ShellExecuteW( NULL, L"open", psz_wdestfile, NULL, NULL, SW_SHOW);
         if(answer > 32)
             libvlc_Quit(p_this->p_libvlc);
     }
