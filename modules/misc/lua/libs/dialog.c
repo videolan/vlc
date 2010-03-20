83ab54255b2ae6883d0d839bd8fb75c20471c1fc
Jean-Philippe André <jpeg@videolan.org>
2010-03-20 16:53:28 +0100

    Lua: Extensions, fix position of buttons
    Fix #3375

--- modules/misc/lua/libs/dialog.c
+++ modules/misc/lua/libs/dialog.c
@@ -394,9 +394,9 @@ static int vlclua_dialog_add_button( lua_State *L )
     extension_widget_t *p_widget = calloc( 1, sizeof( extension_widget_t ) );
     p_widget->type = EXTENSION_WIDGET_BUTTON;
     p_widget->psz_text = strdup( luaL_checkstring( L, 2 ) );
-    lua_settop( L, 3 );
+    lua_settop( L, 10 );
     lua_pushlightuserdata( L, p_widget );
-    lua_insert( L, 3 );
+    lua_insert( L, 10 );
     lua_settable( L, LUA_REGISTRYINDEX );
     p_widget->p_sys = NULL;
 
