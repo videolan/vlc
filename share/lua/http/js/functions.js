8ffc1871cf4466a327f4615775a6a8f3dbe43863
Rémi Duraffort <ivoire@videolan.org>
2010-04-08 15:59:52 +0200

    Revert "luahttp: fix service discovery loading."
    This reverts commit a9e4799e43b57739786acf23efaa65e1379887ea.

--- share/lua/http/js/functions.js
+++ share/lua/http/js/functions.js
@@ -360,9 +360,9 @@ function pl_repeat()
 {
     loadXMLDoc( 'requests/status.xml?command=pl_repeat', parse_status );
 }
-function pl_sd( value, islua )
+function pl_sd( value )
 {
-    loadXMLDoc( 'requests/status.xml?command=pl_sd&val='+value+'&islua='+islua, parse_status );
+    loadXMLDoc( 'requests/status.xml?command=pl_sd&val='+value, parse_status );
 }
 
 /* misc actions */
