389dcfadc1a417abe9f5d9a07ac7c7ad524562ea
Rémi Denis-Courmont <remi@remlab.net>
2010-04-12 19:22:30 +0300

    Print dynamic library error message if applicable

--- bin/override.c
+++ bin/override.c
@@ -89,7 +89,8 @@ static void *getsym (const char *name)
     void *sym = dlsym (RTLD_NEXT, name);
     if (sym == NULL)
     {
-        fprintf (stderr, "Cannot resolve symbol %s!\n", name);
+        fprintf (stderr, "Cannot resolve symbol %s: %s\n", name,
+                 dlerror ());
         abort ();
     }
     return sym;
