c63cc6b04c979c6eadfad6fef740ea4cac89110a
Laurent Aimar <fenrir@videolan.org>
2010-04-24 00:00:19 +0200

    Manual revert of now unneeded a1b58a771b1bdfcca56d24541b43ce5bf6caf8b7.
    Picture reserved/linked state has been merged.

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -1342,7 +1342,6 @@ static void DecoderPlayVideo( decoder_t *p_dec, picture_t *p_picture,
         vout_ReleasePicture( p_vout, p_picture );
         return;
     }
-    vout_HoldPicture( p_vout, p_picture );
 
     /* */
     vlc_mutex_lock( &p_owner->lock );
@@ -1422,7 +1421,6 @@ static void DecoderPlayVideo( decoder_t *p_dec, picture_t *p_picture,
                 p_owner->i_last_rate = i_rate;
             }
             vout_PutPicture( p_vout, p_picture );
-            vout_ReleasePicture( p_vout, p_picture );
         }
         else
         {
@@ -1432,7 +1430,6 @@ static void DecoderPlayVideo( decoder_t *p_dec, picture_t *p_picture,
                 msg_Warn( p_vout, "non-dated video buffer received" );
 
             *pi_lost_sum += 1;
-            vout_ReleasePicture( p_vout, p_picture );
             vout_ReleasePicture( p_vout, p_picture );
         }
         int i_tmp_display;
@@ -1687,7 +1684,6 @@ static void DecoderFlushBuffering( decoder_t *p_dec )
 
         if( p_owner->p_vout )
         {
-            vout_ReleasePicture( p_owner->p_vout, p_picture );
             vout_ReleasePicture( p_owner->p_vout, p_picture );
         }
 
