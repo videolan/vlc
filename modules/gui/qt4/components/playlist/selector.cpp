5cf83d9c99fe2554b74124a11c2a1e7e1751c82b
Jean-Baptiste Kempf <jb@videolan.org>
2010-04-27 01:19:36 +0200

    Qt: fix missing translations

--- modules/gui/qt4/components/playlist/selector.cpp
+++ modules/gui/qt4/components/playlist/selector.cpp
@@ -226,8 +226,8 @@ PLSelItem * PLSelector::addItem (
 
 PLSelItem * putSDData( PLSelItem* item, const char* name, const char* longname )
 {
-    item->treeItem()->setData( 0, NAME_ROLE, qfu( name ) );
-    item->treeItem()->setData( 0, LONGNAME_ROLE, qfu( longname ) );
+    item->treeItem()->setData( 0, NAME_ROLE, qtr( name ) );
+    item->treeItem()->setData( 0, LONGNAME_ROLE, qtr( longname ) );
     return item;
 }
 
