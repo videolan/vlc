2e2bf09f3f55246dd65a0d479f9b4cc04d747b46
Rémi Denis-Courmont <remi@remlab.net>
2010-05-01 22:26:56 +0300

    XCB/X11: disable ARGB support
    VLC does not currently handle color masks for ARGB chromas, so this can
    not work yet. Instead, fail safe to the GLX output.

--- modules/video_output/xcb/x11.c
+++ modules/video_output/xcb/x11.c
@@ -178,8 +178,13 @@ static int Open (vlc_object_t *obj)
           case 32:
             if (fmt->bits_per_pixel != 32)
                 continue;
+#ifdef FIXED_VLC_RGBA_MASK
             fmt_pic.i_chroma = VLC_CODEC_RGBA;
             break;
+#else
+            msg_Dbg (vd, "X11 visual with alpha-channel not supported");
+            continue;
+#endif
           case 24:
             if (fmt->bits_per_pixel == 32)
                 fmt_pic.i_chroma = VLC_CODEC_RGB32;
