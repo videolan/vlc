2383257ee96e98742be9cc63682e257325d7e72b
Laurent Aimar <fenrir@videolan.org>
2010-05-05 21:23:59 +0200

    Removed useless includes (video_epg.c).

--- src/video_output/video_epg.c
+++ src/video_output/video_epg.c
@@ -23,13 +23,10 @@
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
-#include <assert.h>
 
 #include <vlc_common.h>
 #include <vlc_vout.h>
-#include <vlc_block.h>
-#include <vlc_filter.h>
-#include <vlc_osd.h>
+#include <vlc_vout_osd.h>
 #include <vlc_events.h>
 #include <vlc_input_item.h>
 #include <vlc_epg.h>
