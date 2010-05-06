27fa51a4ed9e03f45a643367537dffa59005cafa
Laurent Aimar <fenrir@videolan.org>
2010-05-06 23:51:57 +0200

    Cosmetics.

--- src/video_output/vout_internal.h
+++ src/video_output/vout_internal.h
@@ -39,9 +39,6 @@
 #include "statistic.h"
 #include "chrono.h"
 
-/* Number of pictures required to computes the FPS rate */
-#define VOUT_FPS_SAMPLES                20
-
 /* */
 struct vout_thread_sys_t
 {
