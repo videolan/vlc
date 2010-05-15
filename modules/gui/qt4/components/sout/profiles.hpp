9589b73d0d6e10ef61f5464b93da5aea1b3550f1
Ilkka Ollakka <ileoo@videolan.org>
2010-05-15 19:53:19 +0300

    qt4: change mp4v/mp4 profile to h264/mp4 and make it default profile

--- modules/gui/qt4/components/sout/profiles.hpp
+++ modules/gui/qt4/components/sout/profiles.hpp
@@ -2,11 +2,11 @@
     (sizeof(video_profile_value_list)/sizeof(video_profile_value_list[0]))
 
 static const char video_profile_name_list[][32] = {
+    "Video - H.264 + AAC (MP4)",
     "Video - H.264 + AAC (TS)",
     "Video - Dirac + AAC (TS)",
     "Video - Theora + Vorbis (OGG)",
     "Video - Theora + Flac (OGG)",
-    "Video - MPEG-4 + AAC (MP4)",
     "Video - MPEG-2 + MPGA (TS)",
     "Video - WMV + WMA (ASF)",
     "Video - DIV3 + MP3 (ASF)",
@@ -23,11 +23,11 @@ static const char video_profile_value_list[][48] = {
     /* scale(float), fps(float), width(integer, height(integer), */
     /* audio codec(string), audio bitrate(integer), channels(integer), */
     /* samplerate(integer), subtitle codec(string), subtitle overlay(bool) */
+    "mp4;1;1;0;h264;0;0;0;0;0;mp4a;128;2;44100;0;0",
     "ts;1;1;0;h264;800;1;0;0;0;mp4a;128;2;44100;0;0",
     "ts;1;1;0;drac;800;1;0;0;0;mp4a;128;2;44100;0;0",
     "ogg;1;1;0;theo;800;1;0;0;0;vorb;128;2;44100;0;0",
     "ogg;1;1;0;theo;800;1;0;0;0;flac;128;2;44100;0;0",
-    "mp4;1;1;0;mp4v;800;1;0;0;0;mp4a;128;2;44100;0;0",
     "ts;1;1;0;mp2v;800;1;0;0;0;mpga;128;2;44100;0;0",
     "asf;1;1;0;WMV2;800;1;0;0;0;wma2;128;2;44100;0;0",
     "asf;1;1;0;DIV3;800;1;0;0;0;mp3;128;2;44100;0;0",
