99039b1133635e43381e60b10590cafd7691e6b8
Jean-Baptiste Kempf <jb@videolan.org>
2010-05-21 10:53:27 +0200

    Rename On2's VP8 to Google/On2's VP8

--- src/misc/fourcc.c
+++ src/misc/fourcc.c
@@ -448,7 +448,7 @@ static const entry_t p_list_video[] = {
     B(VLC_CODEC_VP6A, "On2's VP6 A Video"),
         A("VP6A"),
 
-    B(VLC_CODEC_VP8, "On2's VP8 Video"),
+    B(VLC_CODEC_VP8, "Google/On2's VP8 Video"),
         A("VP80"),
 
 
