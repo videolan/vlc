videolan/vlc

commit 38ed2e8ef37df763e187c1dca6b6a6c1da4887c4

Used vout_control_Push for vout_RegisterSubpictureChannel.

src/video_output/video_output.c
@@ -357,7 +357,16 @@ void vout_PutSubpicture( vout_thread_t *vout, subpicture_t *subpic )
 }
 int vout_RegisterSubpictureChannel( vout_thread_t *vout )
 {
-    return spu_RegisterChannel(vout->p->p_spu);
+    int channel = SPU_DEFAULT_CHANNEL;
+
+    vout_control_cmd_t cmd;
+    vout_control_cmd_Init(&cmd, VOUT_CONTROL_REGISTER_SUBPICTURE);
+    cmd.u.integer_ptr = &channel;
+
+    vout_control_Push(&vout->p->control, &cmd);
+    vout_control_WaitEmpty(&vout->p->control);
+
+    return channel;
 }
 void vout_FlushSubpictureChannel( vout_thread_t *vout, int channel )
 {
@@ -760,6 +769,11 @@ static void ThreadDisplaySubpicture(vout_thread_t *vout,
     spu_DisplaySubpicture(vout->p->p_spu, subpicture);
 }
 
+static void ThreadRegisterSubpicture(vout_thread_t *vout, int *channel)
+{
+    *channel = spu_RegisterChannel(vout->p->p_spu);
+}
+
 static void ThreadFlushSubpicture(vout_thread_t *vout, int channel)
 {
     spu_ClearChannel(vout->p->p_spu, channel);
@@ -1122,6 +1136,9 @@ static void *Thread(void *object)
                 break;
             case VOUT_CONTROL_FLUSH_SUBPICTURE:
                 ThreadFlushSubpicture(vout, cmd.u.integer);
+                break;
+            case VOUT_CONTROL_REGISTER_SUBPICTURE:
+                ThreadRegisterSubpicture(vout, cmd.u.integer_ptr);
                 break;
             case VOUT_CONTROL_OSD_TITLE:
                 ThreadDisplayOsdTitle(vout, cmd.u.string);

src/video_output/control.h
@@ -41,6 +41,7 @@ enum {
 #endif
     VOUT_CONTROL_SUBPICTURE,            /* subpicture */
     VOUT_CONTROL_FLUSH_SUBPICTURE,      /* integer */
+    VOUT_CONTROL_REGISTER_SUBPICTURE,   /* integer_ptr */
     VOUT_CONTROL_OSD_TITLE,             /* string */
     VOUT_CONTROL_CHANGE_FILTERS,        /* string */
     VOUT_CONTROL_CHANGE_SUB_FILTERS,    /* string */
@@ -70,6 +71,7 @@ typedef struct {
         mtime_t *time_ptr;
         char    *string;
         int     integer;
+        int     *integer_ptr;
         struct {
             int a;
             int b;

commit f8d0db86464dfaa0f231edd082cf149859077176

Reimplemented vout_RegisterSubpictureChannel.
 This avoids the potential deadlock introduced by
[38ed2e8ef37df763e187c1dca6b6a6c1da4887c4] when it is called inside
an event emited by the vout.

src/video_output/video_output.c
@@ -126,6 +126,7 @@ static vout_thread_t *VoutCreate(vlc_object_t *object,
     /* Initialize locks */
     vlc_mutex_init(&vout->p->picture_lock);
     vlc_mutex_init(&vout->p->vfilter_lock);
+    vlc_mutex_init(&vout->p->spu_lock);
 
     /* Attach the new object now so we can use var inheritance below */
     vlc_object_attach(vout, object);
@@ -235,7 +236,10 @@ void vout_Close(vout_thread_t *vout)
     vout_control_PushVoid(&vout->p->control, VOUT_CONTROL_CLEAN);
     vlc_join(vout->p->thread, NULL);
 
+    vlc_mutex_lock(&vout->p->spu_lock);
     spu_Destroy(vout->p->p_spu);
+    vout->p->p_spu = NULL;
+    vlc_mutex_unlock(&vout->p->spu_lock);
 }
 
 /* */
@@ -249,6 +253,7 @@ static void VoutDestructor(vlc_object_t *object)
     free(vout->p->splitter_name);
 
     /* Destroy the locks */
+    vlc_mutex_destroy(&vout->p->spu_lock);
     vlc_mutex_destroy(&vout->p->picture_lock);
     vlc_mutex_destroy(&vout->p->vfilter_lock);
     vout_control_Clean(&vout->p->control);
@@ -356,7 +361,14 @@ void vout_PutSubpicture( vout_thread_t *vout, subpicture_t *subpic )
 }
 int vout_RegisterSubpictureChannel( vout_thread_t *vout )
 {
-    return spu_RegisterChannel(vout->p->p_spu);
+    int channel = SPU_DEFAULT_CHANNEL;
+
+    vlc_mutex_lock(&vout->p->spu_lock);
+    if (vout->p->p_spu)
+        channel = spu_RegisterChannel(vout->p->p_spu);
+    vlc_mutex_unlock(&vout->p->spu_lock);
+
+    return channel;
 }
 void vout_FlushSubpictureChannel( vout_thread_t *vout, int channel )
 {

src/video_output/vout_internal.h
@@ -68,6 +68,7 @@ struct vout_thread_sys_t
     vout_statistic_t statistic;
 
     /* Subpicture unit */
+    vlc_mutex_t     spu_lock;
     spu_t           *p_spu;
 
     /* Monitor Pixel Aspect Ratio */