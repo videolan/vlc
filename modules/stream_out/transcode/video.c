75a524c518a587d088b10a8b89e68d52e0b3bae3
Ilkka Ollakka <ileoo@videolan.org>
2010-05-25 17:20:01 +0300

    transcode: use same pts with and without threads on duplicating

--- modules/stream_out/transcode/video.c
+++ modules/stream_out/transcode/video.c
@@ -758,7 +758,7 @@ int transcode_video_process( sout_stream_t *p_stream, sout_stream_id_t *id,
                    if( p_pic2 != NULL )
                    {
                        picture_Copy( p_pic2, p_pic );
-                       p_pic2->date = i_pts + 1;
+                       p_pic2->date = i_pts;
                    }
                }
                else
