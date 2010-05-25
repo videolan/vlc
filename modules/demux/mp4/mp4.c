fb279135cd71565c84ae291c33419a3696d651c9
Jean-Baptiste Kempf <jb@videolan.org>
2010-05-25 22:17:52 +0200

    MP4: update Copyright

--- modules/demux/mp4/mp4.c
+++ modules/demux/mp4/mp4.c
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * mp4.c : MP4 file input module for vlc
  *****************************************************************************
- * Copyright (C) 2001-2004 the VideoLAN team
+ * Copyright (C) 2001-2004, 2010 the VideoLAN team
  * $Id$
  * Authors: Laurent Aimar <fenrir@via.ecp.fr>
  *
