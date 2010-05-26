c6bcc788be1ca4a91c9637a7b136d305f6d3ee08
Pierre Ynard <linkfanel@yahoo.fr>
2010-05-26 06:46:38 +0200

    rtp sout: don't add a=rtcp: in SDP if RTP port is not specified

--- modules/stream_out/rtp.c
+++ modules/stream_out/rtp.c
@@ -831,7 +831,8 @@ char *SDPGenerate( sout_stream_t *p_stream, const char *rtsp_url )
                       id->psz_enc, id->i_clock_rate, id->i_channels,
                       id->psz_fmtp);
 
-        if( !p_sys->rtcp_mux && (id->i_port & 1) ) /* cf RFC4566 §5.14 */
+        /* cf RFC4566 §5.14 */
+        if( inclport && !p_sys->rtcp_mux && (id->i_port & 1) )
             sdp_AddAttribute ( &psz_sdp, "rtcp", "%u", id->i_port + 1 );
 
         if( rtsp_url != NULL )
