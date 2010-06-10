5422c4fa7ef126f40eefa692b4a14b35f55f01c5
Jean-Baptiste Kempf <jb@videolan.org>
2010-06-10 23:25:06 +0200

    Qt: Remove redundant code

--- modules/gui/qt4/components/interface_widgets.cpp
+++ modules/gui/qt4/components/interface_widgets.cpp
@@ -64,15 +64,13 @@
  **********************************************************************/
 
 VideoWidget::VideoWidget( intf_thread_t *_p_i )
-    : QFrame( NULL )
-      , p_intf( _p_i )
+            : QFrame( NULL ) , p_intf( _p_i )
 {
     /* Set the policy to expand in both directions */
     // setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Expanding );
 
     layout = new QHBoxLayout( this );
     layout->setContentsMargins( 0, 0, 0, 0 );
-    setLayout( layout );
     stable = NULL;
 }
 
