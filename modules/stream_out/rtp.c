videolan/vlc

commit dedd7cd2ae0864dedfad367c1cbf916e1d315a00

rtp: fix h264 sprop-parameter-sets generation
Patch from An L. Ber whitespace-modified by me
Signed-off-by: Ilkka Ollakka <ileoo@videolan.org>

modules/stream_out/rtp.c
@@ -1197,34 +1197,58 @@ static sout_stream_id_t *Add( sout_stream_t *p_stream, es_format_t *p_fmt )
                 char    *p_64_pps = NULL;
                 char    hexa[6+1];
 
-                while( i_buffer > 4 &&
-                       p_buffer[0] == 0 && p_buffer[1] == 0 &&
-                       p_buffer[2] == 0 && p_buffer[3] == 1 )
+                while( i_buffer > 4 )
                 {
-                    const int i_nal_type = p_buffer[4]&0x1f;
-                    int i_offset;
+                    int i_offset    = 0;
                     int i_size      = 0;
+
+                    while( p_buffer[0] != 0 || p_buffer[1] != 0 ||
+                           p_buffer[2] != 1 )
+                    {
+                        p_buffer++;
+                        i_buffer--;
+                        if( i_buffer == 0 ) break;
+                    }
+
+                    if( i_buffer < 4 || memcmp(p_buffer, "\x00\x00\x01", 3 ) )
+                    {
+                        msg_Dbg( p_stream, "No startcode found..");
+                        break;
+                    }
+                    p_buffer += 3;
+                    i_buffer -= 3;
+
+                    const int i_nal_type = p_buffer[0]&0x1f;
 
                     msg_Dbg( p_stream, "we found a startcode for NAL with TYPE:%d", i_nal_type );
 
                     i_size = i_buffer;
-                    for( i_offset = 4; i_offset+3 < i_buffer ; i_offset++)
+                    for( i_offset = 0; i_offset+2 < i_buffer ; i_offset++)
                     {
-                        if( !memcmp (p_buffer + i_offset, "\x00\x00\x00\x01", 4 ) )
+                        if( !memcmp(p_buffer + i_offset, "\x00\x00\x01", 3 ) )
                         {
                             /* we found another startcode */
+                            while( i_offset > 0 && 0 == p_buffer[ i_offset - 1 ] )
+                                i_offset--;
                             i_size = i_offset;
                             break;
                         }
                     }
+
+                    if( i_size == 0 )
+                    {
+                        msg_Dbg( p_stream, "No-info found in nal ");
+                        continue;
+                    }
+
                     if( i_nal_type == 7 )
                     {
-                        p_64_sps = vlc_b64_encode_binary( &p_buffer[4], i_size - 4 );
+                        p_64_sps = vlc_b64_encode_binary( p_buffer, i_size );
                         sprintf_hexa( hexa, &p_buffer[5], 3 );
                     }
                     else if( i_nal_type == 8 )
                     {
-                        p_64_pps = vlc_b64_encode_binary( &p_buffer[4], i_size - 4 );
+                        p_64_pps = vlc_b64_encode_binary( p_buffer, i_size );
                     }
                     i_buffer -= i_size;
                     p_buffer += i_size;

commit 9f5bb41a34b73cca36ad83c1fd563fe2b2b7fd4a

rtp: fix h264 profile-level-id generation
This was broken in the previous dedd7cd2ae0864dedfad367c1cbf916e1d315a00

modules/stream_out/rtp.c
@@ -1244,7 +1244,8 @@ static sout_stream_id_t *Add( sout_stream_t *p_stream, es_format_t *p_fmt )
                     if( i_nal_type == 7 )
                     {
                         p_64_sps = vlc_b64_encode_binary( p_buffer, i_size );
-                        sprintf_hexa( hexa, &p_buffer[5], 3 );
+                        /* XXX: nothing ensures that i_size >= 4 ?? */
+                        sprintf_hexa( hexa, &p_buffer[1], 3 );
                     }
                     else if( i_nal_type == 8 )
                     {