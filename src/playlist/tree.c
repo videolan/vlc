afbc7600a302fe2617fe409b06e0939f31b2bcb2
Jakob Leben <jleben@videolan.org>
2010-06-20 14:47:40 +0200

    playlist_NodeRemoveItem(): set item's parent to NULL

--- src/playlist/tree.c
+++ src/playlist/tree.c
@@ -248,15 +248,23 @@ int playlist_NodeRemoveItem( playlist_t *p_playlist,
     PL_ASSERT_LOCKED;
    (void)p_playlist;
 
+   int ret = VLC_EGENERIC;
+
    for(int i= 0; i< p_parent->i_children ; i++ )
    {
        if( p_parent->pp_children[i] == p_item )
        {
            REMOVE_ELEM( p_parent->pp_children, p_parent->i_children, i );
+           ret = VLC_SUCCESS;
        }
    }
 
-   return VLC_SUCCESS;
+    if( ret == VLC_SUCCESS ) {
+        assert( p_item->p_parent == p_parent );
+        p_item->p_parent = NULL;
+    }
+
+    return ret;
 }
 
 /**
