e06d5fd0dd06e5dbd4456974d0225c386b083569
Joseph S. Atkinson <jsa@wickedmachine.net>
2010-06-22 08:19:52 +0200

    file.c: Typo fix
    Signed-off-by: Rafaël Carré <rafael.carre@gmail.com>

--- modules/access/file.c
+++ modules/access/file.c
@@ -96,7 +96,7 @@ static bool IsRemote (int fd)
     if (fstatvfs (fd, &stf))
         return false;
     /* fstatvfs() is in POSIX, but MNT_LOCAL is not */
-    return !(s.f_flag & MNT_LOCAL);
+    return !(stf.f_flag & MNT_LOCAL);
 
 #elif defined (HAVE_LINUX_MAGIC_H)
     struct statfs stf;
