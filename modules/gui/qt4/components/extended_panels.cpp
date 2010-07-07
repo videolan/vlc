edeb8f7bc73ca5bf1eeeca767469098252add6fa
Rémi Duraffort <ivoire@videolan.org>
2010-07-07 20:38:32 +0200

    Qt4: fix #3872

--- modules/gui/qt4/components/extended_panels.cpp
+++ modules/gui/qt4/components/extended_panels.cpp
@@ -1201,8 +1201,7 @@ Spatializer::~Spatializer()
 void Spatializer::enable()
 {
     bool en = enableCheck->isChecked();
-    aout_EnableFilter( VLC_OBJECT( p_intf ), "spatializer",
-            en ? true : false );
+    aout_EnableFilter( THEPL, "spatializer", en );
     enable( en );
 }
 
