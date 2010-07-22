3056962ed16e48b438dc43cf1ca5092615821e4a
Juho Vähä-Herttua <juhovh@iki.fi>
2010-07-22 14:53:27 +0300

    qt4: Add OSX to supported platforms of the towlower function.
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/gui/qt4/util/customwidgets.cpp
+++ modules/gui/qt4/util/customwidgets.cpp
@@ -392,7 +392,7 @@ int qtEventToVLCKey( QKeyEvent *e )
 
     if( qtk <= 0xff )
         /* VLC and X11 use lowercase whereas Qt uses uppercase */
-#if defined( __STDC_ISO_10646__ ) || defined( _WIN32 )
+#if defined( __STDC_ISO_10646__ ) || defined( _WIN32 ) || defined( __APPLE__ )
         i_vlck = towlower( qtk );
 #else
 # error FIXME
