1d7df6fad180b4697ef76fb6148621c758b77566
Rafaël Carré <rafael.carre@gmail.com>
2010-07-30 19:55:46 +0200

    main_interface.cpp: remove trailing ':' after #ifdef

--- modules/gui/qt4/main_interface.cpp
+++ modules/gui/qt4/main_interface.cpp
@@ -971,7 +971,7 @@ void MainInterface::handleSystrayClick(
     {
         case QSystemTrayIcon::Trigger:
         case QSystemTrayIcon::DoubleClick:
-#ifdef Q_WS_MAC:
+#ifdef Q_WS_MAC
             QVLCMenu::updateSystrayMenu( this, p_intf );
 #else
             toggleUpdateSystrayMenu();
