1bfc1c42dfd68f873a844bb5c4779815d07f50c6
Jean-Baptiste Kempf <jb@videolan.org>
2010-08-15 01:29:19 +0200

    DirectX: kill a warning from unused variable

--- modules/video_output/msw/directx.c
+++ modules/video_output/msw/directx.c
@@ -962,6 +962,7 @@ static int DirectXLockSurface(LPDIRECTDRAWSURFACE2 front_surface,
 static void DirectXUnlockSurface(LPDIRECTDRAWSURFACE2 front_surface,
                                  LPDIRECTDRAWSURFACE2 surface)
 {
+    VLC_UNUSED(front_surface);
     IDirectDrawSurface2_Unlock(surface, NULL);
 }
 static int DirectXCheckLockingSurface(LPDIRECTDRAWSURFACE2 front_surface,
