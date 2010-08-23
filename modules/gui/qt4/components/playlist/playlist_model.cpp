89f5d36846718c86518e11d6f542e61b8823b5ab
Jean-Baptiste Kempf <jb@videolan.org>
2010-08-23 23:59:24 +0200

    Qt: try to fix the "open containing folder" on Win32
    Should close #3536

--- modules/gui/qt4/components/playlist/playlist_model.cpp
+++ modules/gui/qt4/components/playlist/playlist_model.cpp
@@ -1064,6 +1064,12 @@ void PLModel::popupExplore()
                    !strncasecmp( psz_access, "file", 4 ) ||
                    !strncasecmp( psz_access, "dire", 4 ) ))
            {
+#ifdef WIN32
+           /* Qt openURL doesn't know to open files that starts with a / or \ */
+               if( psz_path[0] == '/' || psz_path[0] == '\\'  )
+                   psz_path++;
+#endif
+
                QFileInfo info( qfu( decode_URI( psz_path ) ) );
                QDesktopServices::openUrl(
                                QUrl::fromLocalFile( info.absolutePath() ) );
