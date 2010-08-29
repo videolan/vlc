2776c67e6abdeec44196fc73b3b9f113b1b6b829
Jean-Paul Saman <jean-paul.saman@m2x.nl>
2010-08-29 17:24:25 +0200

    modules/video_filter/deinterlace.c: make unused parameter 'p_old'

--- modules/video_filter/deinterlace.c
+++ modules/video_filter/deinterlace.c
@@ -1643,6 +1643,7 @@ static void Flush( filter_t *p_filter )
 static int Mouse( filter_t *p_filter,
                   vlc_mouse_t *p_mouse, const vlc_mouse_t *p_old, const vlc_mouse_t *p_new )
 {
+    VLC_UNUSED(p_old);
     *p_mouse = *p_new;
     if( p_filter->p_sys->b_half_height )
         p_mouse->i_y *= 2;
