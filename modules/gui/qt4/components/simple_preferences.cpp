4ba419b6cbd377c668d6a221154df358b610ebcc
Jean-Baptiste Kempf <jb@videolan.org>
2010-09-20 00:28:45 +0200

    Qt Sprefs: remove mpeg2 skip-frames

--- modules/gui/qt4/components/simple_preferences.cpp
+++ modules/gui/qt4/components/simple_preferences.cpp
@@ -458,7 +458,6 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
             optionWidgets.append( ui.DVDDevice );
             optionWidgets.append( ui.cachingCombo );
             CONFIG_GENERIC( "ffmpeg-skiploopfilter", IntegerList, ui.filterLabel, loopFilterBox );
-            CONFIG_BOOL( "skip-frames", skipFrames );
             CONFIG_GENERIC( "sout-x264-tune", StringList, ui.x264Label, tuneBox );
             CONFIG_GENERIC( "sout-x264-preset", StringList, ui.x264Label, presetBox );
             CONFIG_GENERIC( "sout-x264-profile", StringList, ui.x264profileLabel, profileBox );
