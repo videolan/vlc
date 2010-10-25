6c24f08b6b396f2e9737277a2b02c3446474dd16
Rémi Denis-Courmont <remi@remlab.net>
2010-10-25 18:35:32 +0300

    TS mux: fix use of non-existent variables
    See http://forum.videolan.org/viewtopic.php?f=13&t=83706

--- modules/mux/mpeg/ts.c
+++ modules/mux/mpeg/ts.c
@@ -253,10 +253,13 @@ vlc_module_end ()
  * Local data structures
  *****************************************************************************/
 static const char *const ppsz_sout_options[] = {
-    "pid-video", "pid-audio", "pid-spu", "pid-pmt", "tsid", "netid",
+    "pid-video", "pid-audio", "pid-spu", "pid-pmt", "tsid",
+#ifdef HAVE_DVBPSI_SDT
+    "netid", "sdtdesc",
+#endif
     "es-id-pid", "shaping", "pcr", "bmin", "bmax", "use-key-frames",
     "dts-delay", "csa-ck", "csa2-ck", "csa-use", "csa-pkt", "crypt-audio", "crypt-video",
-    "muxpmt", "sdtdesc", "program-pmt", "alignment",
+    "muxpmt", "program-pmt", "alignment",
     NULL
 };
 
