c4c29a8b02882a7ec76a6985109116dabd96010a
Pierre Ynard <linkfanel@yahoo.fr>
2010-10-30 22:14:11 +0200

    VAAPI: build fix

--- modules/codec/avcodec/vaapi.c
+++ modules/codec/avcodec/vaapi.c
@@ -498,8 +498,9 @@ vlc_va_t *vlc_va_NewVaapi( vlc_object_t *obj, int i_codec_id )
     return &p_va->va;
 }
 #else
-vlc_va_t *vlc_va_NewVaapi( int i_codec_id )
+vlc_va_t *vlc_va_NewVaapi( vlc_object_t *obj, int i_codec_id )
 {
+    VLC_UNUSED( obj );
     VLC_UNUSED( i_codec_id );
     return NULL;
 }
