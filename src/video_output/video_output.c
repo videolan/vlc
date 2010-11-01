4d2de10dca4a2368d2d7e99b6a65115710add5da
Laurent Aimar <fenrir@videolan.org>
2010-11-01 16:03:35 +0100

    Cosmetics (vout).

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -1023,15 +1023,13 @@ static void ThreadChangeFilters(vout_thread_t *vout, const char *filters)
 
     vlc_mutex_lock(&vout->p->filter.lock);
 
-    filter_chain_Reset(vout->p->filter.chain_static,      &fmt, &fmt);
-    filter_chain_Reset(vout->p->filter.chain_interactive, &fmt, &fmt);
-
     for (int a = 0; a < 2; a++) {
         vlc_array_t    *array = a == 0 ? &array_static :
                                          &array_interactive;
         filter_chain_t *chain = a == 0 ? vout->p->filter.chain_static :
                                          vout->p->filter.chain_interactive;
 
+        filter_chain_Reset(chain, &fmt, &fmt);
         for (int i = 0; i < vlc_array_count(array); i++) {
             vout_filter_t *e = vlc_array_item_at_index(array, i);
             msg_Dbg(vout, "Adding '%s' as %s", e->name, a == 0 ? "static" : "interactive");
