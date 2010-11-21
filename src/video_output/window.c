79bb2f751a8a4a90d56d728828a82a462298bce7
Rémi Denis-Courmont <remi@remlab.net>
2010-11-21 19:12:32 +0200

    Disable screensaver preemption before destroying the module

--- src/video_output/window.c
+++ src/video_output/window.c
@@ -106,7 +106,10 @@ void vout_window_Delete(vout_window_t *window)
 
     window_t *w = (window_t *)window;
     if (w->inhibit)
+    {
+        vlc_inhibit_Set (w->inhibit, false);
         vlc_inhibit_Destroy (w->inhibit);
+    }
 
     module_unneed(window, w->module);
 
