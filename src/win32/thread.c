ae79d25dac4e2067530dd375369ddc08c39900ed
Rémi Denis-Courmont <remi@remlab.net>
2010-11-23 23:36:17 +0200

    Win32: implement variable cleanup more similar to POSIX...
    ...and less prone to deadlocks. This restarts until all keys are NULL,
    in case cleaning up reset one key back to a non-NULL value.
    There is still a slight difference with POSIX still. POSIX will restarts
    only after all destructors have been invoked, not after the first one.

--- src/win32/thread.c
+++ src/win32/thread.c
@@ -517,13 +517,19 @@ static void vlc_threadvar_cleanup (void)
 {
     vlc_threadvar_t key;
 
+retry:
     /* TODO: use RW lock or something similar */
     vlc_mutex_lock (&super_mutex);
     for (key = vlc_threadvar_last; key != NULL; key = key->prev)
     {
         void *value = vlc_threadvar_get (key);
         if (value != NULL)
+        {
+            vlc_mutex_unlock (&super_mutex);
+            vlc_threadvar_set (key, NULL);
             key->destroy (value);
+            goto retry;
+        }
     }
     vlc_mutex_unlock (&super_mutex);
 }
