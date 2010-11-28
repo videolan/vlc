c7ba2edbac9a312d825802525dee66799e1f9407
Pierre Ynard <linkfanel@yahoo.fr>
2010-11-28 01:30:17 +0100

    Update comments relevent to --sout #description

--- include/vlc/libvlc_media.h
+++ include/vlc/libvlc_media.h
@@ -487,16 +487,8 @@ VLC_PUBLIC_API void *libvlc_media_get_user_data( libvlc_media_t *p_md );
  * Get media descriptor's elementary streams description
  *
  * Note, you need to play the media _one_ time with --sout="#description"
- * Not doing this will result in an empty array, and doing it more than once
- * will duplicate the entries in the array each time. Something like this:
- *
- * @begincode
- * libvlc_media_player_t *player = libvlc_media_player_new_from_media(media);
- * libvlc_media_add_option_flag(media, "sout=#description");
- * libvlc_media_player_play(player);
- * // ... wait until playing
- * libvlc_media_player_release(player);
- * @endcode
+ * but this is broken because #description needs special support now.
+ * Not doing this will result in an empty array.
  *
  * This is very likely to change in next release, and be done at the parsing
  * phase.
