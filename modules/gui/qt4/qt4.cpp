videolan/vlc

commit 7f485998dd63c9685f0a470439c9b2ea127ef5c5

Qt: fix WinXP quitting issue

modules/gui/qt4/qt4.cpp
@@ -396,8 +396,15 @@ static void Close( vlc_object_t *p_this )
         var_Destroy (pl_Get(p_this), "qt4-iface");
     }
 
+    /* And quit */
+    msg_Dbg( p_intf, "Please die, die, die..." );
+    QApplication::closeAllWindows();
+
+//    QApplication::quit();
+
     QVLCApp::triggerQuit();
 
+    msg_Dbg( p_intf, "Please die, die, die 2..." );
 #ifndef Q_WS_MAC
     vlc_join (p_sys->thread, NULL);
 #endif
@@ -445,7 +452,7 @@ static void *Thread( void *obj )
 #endif
             QSettings::UserScope, "vlc", "vlc-qt-interface" );
 
-    /* Icon setting */
+    /* Icon setting, Mac uses icon from .icns */
 #ifndef Q_WS_MAC
     if( QDate::currentDate().dayOfYear() >= 352 ) /* One Week before Xmas */
         app.setWindowIcon( QIcon(vlc_christmas_xpm) );
@@ -456,7 +463,7 @@ static void *Thread( void *obj )
     /* Initialize timers and the Dialog Provider */
     DialogsProvider::getInstance( p_intf );
 
-    /* Detect screensize for small screens like TV or EEEpc*/
+    /* Detect screensize for small screens like TV or Netbooks */
     p_intf->p_sys->i_screenHeight =
         app.QApplication::desktop()->availableGeometry().height();
 
@@ -499,7 +506,7 @@ static void *Thread( void *obj )
 #endif
 
     /* Last settings */
-    app.setQuitOnLastWindowClosed( false );
+    app.setQuitOnLastWindowClosed( true );
 
     /* Retrieve last known path used in file browsing */
     p_intf->p_sys->filepath =
@@ -513,9 +520,7 @@ static void *Thread( void *obj )
     /* Launch */
     app.exec();
 
-    /* And quit */
-    QApplication::closeAllWindows();
-
+    msg_Dbg( p_intf, "QApp exec() finished" );
     if (p_mi != NULL)
     {
 #warning BUG!

modules/gui/qt4/main_interface.cpp
@@ -226,6 +226,8 @@ MainInterface::MainInterface( intf_thread_t *_p_intf ) : QVLCMW( _p_intf )
 
     CONNECT( THEDP, toolBarConfUpdated(), this, recreateToolbars() );
 
+    CONNECT( this, askToQuit(), THEDP, quit() );
+
     /** END of CONNECTS**/
 
 
@@ -264,6 +266,8 @@ MainInterface::~MainInterface()
     /* Unsure we hide the videoWidget before destroying it */
     if( stackCentralOldWidget == videoWidget )
         showTab( bgWidget );
+
+    releaseVideoSlot();
 
 #ifdef WIN32
     if( himl )
@@ -597,7 +601,8 @@ void MainInterface::releaseVideo( void )
 /* Function that is CONNECTED to the previous emit */
 void MainInterface::releaseVideoSlot( void )
 {
-    videoWidget->release();
+    if( videoWidget )
+        videoWidget->release();
     setVideoOnTop( false );
     setVideoFullScreen( false );
 
@@ -1180,9 +1185,9 @@ void MainInterface::wheelEvent( QWheelEvent *e )
 
 void MainInterface::closeEvent( QCloseEvent *e )
 {
-    e->accept();
-    hide();
-    THEDP->quit();
+    e->ignore();      /* Do not quit */
+//  hide();
+    emit askToQuit(); /* ask THEDP to quit, so we have a unique method */
 }
 
 void MainInterface::setInterfaceFullScreen( bool fs )

modules/gui/qt4/main_interface.hpp
@@ -244,6 +244,7 @@ signals:
     void askVideoOnTop( bool );
     void minimalViewToggled( bool );
     void fullscreenInterfaceToggled( bool );
+    void askToQuit();
 
 };
 

commit 3326e7ef0e333e7a1aa6419c4547703dcbd23618

qt4: fix skins2 crash after exiting the preferences dialog
This fixes a regression brought by 7f485998dd63c9685f0a470439c9b2ea127ef5c5

On skins2(tested on Linux), open and close the preferences dialog leads to a
crash, because the whole qt4 thread terminates unexpectedly.

This is due to setQuitOnLastWindowClosed set to true. As a dialog provider,
qt4 doesn't run any windows, and therefore just closing the preferences
 dialog box (via accept() or reject()) signals the qt4 thread to quit.

modules/gui/qt4/qt4.cpp
@@ -525,7 +525,10 @@ static void *Thread( void *obj )
 #endif
 
     /* Last settings */
-    app.setQuitOnLastWindowClosed( true );
+    if( p_intf->p_sys->b_isDialogProvider )
+        app.setQuitOnLastWindowClosed( false );
+    else
+        app.setQuitOnLastWindowClosed( true );
 
     /* Retrieve last known path used in file browsing */
     p_intf->p_sys->filepath =