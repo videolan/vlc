4e60dedd06fa5eb3ecb980ed91f476a644ac8c15
Jean-Baptiste Kempf <jb@videolan.org>
2010-12-19 20:20:23 +0100

    Qt, sprefs: link the systray options together

--- modules/gui/qt4/components/simple_preferences.cpp
+++ modules/gui/qt4/components/simple_preferences.cpp
@@ -593,6 +593,8 @@ SPrefsPanel::SPrefsPanel( intf_thread_t *_p_intf, QWidget *_parent,
             CONFIG_BOOL( "qt-fs-controller", fsController );
             CONFIG_BOOL( "qt-system-tray", systrayBox );
             CONFIG_BOOL( "qt-notification", sysPop );
+            CONNECT( ui.systrayBox, toggled( bool ), ui.sysPop, setEnabled( bool ) );
+
             CONFIG_BOOL( "playlist-tree", treePlaylist );
             CONFIG_GENERIC_FILE( "skins2-last", File, ui.skinFileLabel,
                                  ui.fileSkin, ui.skinBrowse );
