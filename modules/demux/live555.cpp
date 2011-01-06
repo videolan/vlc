4089fc27c4dad3b25f64eeb9773c9c8b5d982980
Rémi Denis-Courmont <remi@remlab.net>
2011-01-06 18:52:57 +0200

    live: add audio/L24
    This requires a bleeding edge liblive555 - not tested.

--- modules/demux/live555.cpp
+++ modules/demux/live555.cpp
@@ -849,6 +849,11 @@ static int SessionsSetup( demux_t *p_demux )
                     tk->fmt.i_codec = VLC_CODEC_S16B;
                     tk->fmt.audio.i_bitspersample = 16;
                 }
+                else if( !strcmp( sub-codecName(), "L24" ) )
+                {
+                    tk->fmt.i_codec = VLC_CODEC_S24B;
+                    tk->fmt.audio.i_bitspersample = 24;
+                }
                 else if( !strcmp( sub->codecName(), "L8" ) )
                 {
                     tk->fmt.i_codec = VLC_CODEC_U8;
