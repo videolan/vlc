3f50c72acbf4fe8ca4f9631a3bff596cba2d3a05
Laurent Aimar <fenrir@videolan.org>
2011-01-09 21:49:34 +0100

    Properly reset pictures created for vout filters.

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -662,8 +662,10 @@ static picture_t *VoutVideoFilterInteractiveNewPicture(filter_t *filter)
     vout_thread_t *vout = (vout_thread_t*)filter->p_owner;
 
     picture_t *picture = picture_pool_Get(vout->p->private_pool);
-    if (picture)
+    if (picture) {
+        picture_Reset(picture);
         VideoFormatCopyCropAr(&picture->format, &filter->fmt_out.video);
+    }
     return picture;
 }
 static picture_t *VoutVideoFilterStaticNewPicture(filter_t *filter)
