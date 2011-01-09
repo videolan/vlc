c1a3bb47846f913493a5fa240cbcf403c9f21a5e
Pankaj Yadav <pk@videolan.org>
2011-01-10 02:59:33 +0530

    Symbian App private path and build changes
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

---
+++ src/symbian/path.h
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ * path.h: Symbian Aplications Private Path
+ *****************************************************************************
+ * Copyright © 2010 Pankaj Yadav
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+extern "C" char * GetConstPrivatePath(void);
+
