videolan/vlc

commit f01fe561104e3c62fe77366d3c19b77bf8776a9a

qt4: don't add duplicate on convert if not needed

modules/gui/qt4/dialogs/convert.cpp
@@ -135,10 +135,12 @@ void ConvertDialog::close()
             mrl.remove( '}' );
             mrl += ",deinterlace}";
         }
-        mrl += ":duplicate{";
-        if( displayBox->isChecked() ) mrl += "dst=display,";
-        mrl += "dst=std{access=file,mux=" + profile->getMux() +
-            ",dst='" + fileLine->text() + "'}";
+        mrl += ":";
+        if( displayBox->isChecked() )
+            mrl += "duplicate{dst=display,dst=";
+        mrl += "file{dst='" + fileLine->text() + "'}";
+        if( displayBox->isChecked() )
+            mrl += "}";
     }
 
     msg_Warn( p_intf, "Transcode MRL: %s", qtu( mrl ) );

commit 3085193997f34b3b91e36fbe54f93fa32da31632

Qt4: fix convert dialog
The mux was not passed in the chain.
This is a regression in f01fe561104e3c62fe77366d3c19b77bf8776a9a.

modules/gui/qt4/dialogs/convert.cpp
@@ -138,7 +138,8 @@ void ConvertDialog::close()
         mrl += ":";
         if( displayBox->isChecked() )
             mrl += "duplicate{dst=display,dst=";
-        mrl += "file{dst='" + fileLine->text() + "'}";
+        mrl += "std{access=file,mux=" + profile->getMux()
+             + ",dst='" + fileLine->text() + "'}";
         if( displayBox->isChecked() )
             mrl += "}";
     }