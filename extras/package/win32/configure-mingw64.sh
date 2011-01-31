7641f57079751f66f1cb4da1ba76b5aa940173b2
Jean-Baptiste Kempf <jb@videolan.org>
2011-01-31 12:08:42 +0100

    Win64: do not build dirac

--- extras/package/win32/configure-mingw64.sh
+++ extras/package/win32/configure-mingw64.sh
@@ -13,6 +13,6 @@ export CONTRIB_DIR
 CC=amd64-mingw32msvc-gcc CXX=amd64-mingw32msvc-g++ \
 CONFIGURE="${root}configure" \
 CONFIGOPTS="--host=amd64-mingw32msvc --build=i386-linux
- --enable-dirac --enable-mkv --enable-taglib --enable-debug --enable-projectm
+ --enable-mkv --enable-taglib --enable-debug --enable-projectm
  --disable-qt4 --disable-skins2" \
 sh ${root}extras/package/win32/configure-common.sh
