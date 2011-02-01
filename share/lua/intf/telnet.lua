d16bdb968e2d1ef40e1cc109b79a4a0f3322b61f
Pierre Ynard <linkfanel@yahoo.fr>
2011-02-01 23:32:51 +0100

    luatelnet: end remotely closed connections
    Fix a busy loop and socket leak

--- share/lua/intf/telnet.lua
+++ share/lua/intf/telnet.lua
@@ -183,13 +183,10 @@ while not vlc.misc.should_die() do
     for _, client in pairs(r) do
         local str = client.cmds .. client:recv(1000)
 
-        if not str then -- the telnet client program has left
-            client.cmds = "quit\n"
-        elseif string.match(str,"\n") then
+        if string.match(str,"\n") then
             client.cmds = str
-        elseif client.buffer == ""
-           and ((client.type == host.client_type.stdio and str == "")
-           or  (client.type == host.client_type.net and str == "\004")) then
+        elseif not str or str == "" -- the telnet client program has left
+           or  (client.type == host.client_type.net and str == "\004") then
             -- Caught a ^D
             client.cmds = "quit\n"
         end
