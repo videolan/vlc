e142a23e5a3d4d2c8602e3e9ef8e3c68274170b1
Rémi Duraffort <ivoire@videolan.org>
2011-03-07 21:17:54 +0100

    Small cleanup.

--- modules/video_filter/grain.c
+++ modules/video_filter/grain.c
@@ -367,11 +367,10 @@ static int Callback(vlc_object_t *object, char const *cmd,
 {
     filter_t     *filter = (filter_t *)object;
     filter_sys_t *sys = filter->p_sys;
-    VLC_UNUSED(oldval); VLC_UNUSED(data);
+    VLC_UNUSED(cmd); VLC_UNUSED(oldval); VLC_UNUSED(data);
 
     vlc_mutex_lock(&sys->cfg.lock);
-    //if (!strcmp(cmd, CFG_PREFIX "variance"))
-        sys->cfg.variance = newval.f_float;
+    sys->cfg.variance = newval.f_float;
     vlc_mutex_unlock(&sys->cfg.lock);
 
     return VLC_SUCCESS;
