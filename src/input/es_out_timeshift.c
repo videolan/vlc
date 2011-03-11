b882858fa15c8a7033d25fd3442f9c819f861eda
Jean-Baptiste Kempf <jb@videolan.org>
2011-03-11 19:13:16 +0100

    Factorize some msg_Dbg about timeshift

--- src/input/es_out_timeshift.c
+++ src/input/es_out_timeshift.c
@@ -332,12 +332,12 @@ es_out_t *input_EsOutTimeshiftNew( input_thread_t *p_input, es_out_t *p_next_out
         p_sys->i_tmp_size_max = 50*1024*1024;
     else
         p_sys->i_tmp_size_max = __MAX( i_tmp_size_max, 1*1024*1024 );
-    msg_Dbg( p_input, "using timeshift granularity of %d MiB",
-             (int)p_sys->i_tmp_size_max/(1024*1024) );
 
     char *psz_tmp_path = var_CreateGetNonEmptyString( p_input, "input-timeshift-path" );
     p_sys->psz_tmp_path = GetTmpPath( psz_tmp_path );
-    msg_Dbg( p_input, "using timeshift path '%s'", p_sys->psz_tmp_path );
+
+    msg_Dbg( p_input, "using timeshift granularity of %d MiB, in path '%s'",
+             (int)p_sys->i_tmp_size_max/(1024*1024), p_sys->psz_tmp_path );
 
 #if 0
 #define S(t) msg_Err( p_input, "SIZEOF("#t")=%d", sizeof(t) )
