fc617621647fdfe9549510b6ea0d7ae30a318f4b
Felix Paul Kühne <fkuehne@videolan.org>
2011-04-04 15:46:56 +0200

    eyetv plugin: change logic to support HD streams as suggested by cbscpe on the forums and prepare for a potential release

--- extras/package/macosx/eyetvplugin/eyetvplugin.c
+++ extras/package/macosx/eyetvplugin/eyetvplugin.c
@@ -309,13 +309,9 @@ static long VLCEyeTVPluginPacketsArrived(VLCEyeTVPluginGlobals_t *globals, EyeTV
                             {
                                 if( globals->activePIDs[i].pid == pid )
                                 {
-                                    if( packetBufferSize <= (sizeof(packetBuffer)-sizeof(TransportStreamPacket)) )
-                                    {
-                                        /* copy packet in our buffer */
-                                        memcpy(packetBuffer+packetBufferSize, *packets, sizeof(TransportStreamPacket));
-                                        packetBufferSize += sizeof(TransportStreamPacket);
-                                    }
-                                    else
+                                    memcpy(packetBuffer+packetBufferSize, *packets, sizeof(TransportStreamPacket));
+                                    packetBufferSize += sizeof(TransportStreamPacket);
+                                    if( packetBufferSize > (sizeof(packetBuffer)-sizeof(TransportStreamPacket)) )
                                     {
                                         /* flush buffer to VLC */
                                         ssize_t sent = write(i_vlcSock, packetBuffer, packetBufferSize);
@@ -331,6 +327,7 @@ static long VLCEyeTVPluginPacketsArrived(VLCEyeTVPluginGlobals_t *globals, EyeTV
                                         }
                                         packetBufferSize = 0;
                                     }
+
                                     if( i > 0 )
                                     {
                                        /* if we assume that consecutive packets would have the same PID in most cases,
