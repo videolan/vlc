a79616661d48ffc47efa03f1edb565a1dee58654
Jean-Baptiste Kempf <jb@videolan.org>
2011-04-06 00:38:33 +0200

    DVBSub: change the end-of-field marker according to v1.3.1
    300 743 v1.2.1 mentionned '1111 1111', but 1.3.1 mentions '11 1111' only.
    I believe it is a typo, (maybe, maybe not) but the spec is out, so people
    will write encoders following it.
    So, we do not look at the 2 bits of strong weight.

--- modules/codec/dvbsub.c
+++ modules/codec/dvbsub.c
@@ -441,7 +441,7 @@ static subpicture_t *Decode( decoder_t *p_dec, block_t **pp_block )
         decode_segment( p_dec, &p_sys->bs );
     }
 
-    if( bs_read( &p_sys->bs, 8 ) != 0xff ) /* End marker */
+    if( ( bs_read( &p_sys->bs, 8 ) & 0x3f ) != 0x3f ) /* End marker */
     {
         msg_Warn( p_dec, "end marker not found (corrupted subtitle ?)" );
         block_Release( p_block );
