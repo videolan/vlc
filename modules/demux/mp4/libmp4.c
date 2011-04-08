5637ca8141bf39f263ecdb62035d2cb45c740821
Rémi Denis-Courmont <remi@remlab.net>
2011-04-08 19:22:40 +0300

    MP4: fix heap-based buffer overflow

--- modules/demux/mp4/libmp4.c
+++ modules/demux/mp4/libmp4.c
@@ -2247,7 +2247,7 @@ static int MP4_ReadBox_frma( stream_t *p_stream, MP4_Box_t *p_box )
 
 static int MP4_ReadBox_skcr( stream_t *p_stream, MP4_Box_t *p_box )
 {
-    MP4_READBOX_ENTER( MP4_Box_data_frma_t );
+    MP4_READBOX_ENTER( MP4_Box_data_skcr_t );
 
     MP4_GET4BYTES( p_box->data.p_skcr->i_init );
     MP4_GET4BYTES( p_box->data.p_skcr->i_encr );
