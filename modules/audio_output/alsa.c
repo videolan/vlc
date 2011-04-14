ed973e247faf95f99ee30c20b00540d19615148c
Rémi Denis-Courmont <remi@remlab.net>
2011-04-14 22:09:23 +0300

    ALSA: default normally includes plug, don't add it again
    This should work around distortion problems with some cards (probably
    an ALSA-lib bug).

--- modules/audio_output/alsa.c
+++ modules/audio_output/alsa.c
@@ -85,7 +85,7 @@ struct aout_sys_t
 /* Why not ? --Bozo */
 /* Right. --Meuuh */
 
-#define DEFAULT_ALSA_DEVICE "plug:default"
+#define DEFAULT_ALSA_DEVICE "default"
 
 /*****************************************************************************
  * Local prototypes
