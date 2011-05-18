8a9be8a8363216913ca1cf2dbc8a1d939e7526c3
Rémi Denis-Courmont <remi@remlab.net>
2011-05-18 23:06:08 +0300

    XCB/XVideo: match RGBT

--- modules/video_output/xcb/xvideo.c
+++ modules/video_output/xcb/xvideo.c
@@ -158,6 +158,8 @@ static vlc_fourcc_t ParseFormat (vout_display_t *vd,
                     return VLC_CODEC_RGB16;
                 break;
               case 15:
+                if (f->bpp == 16 && f->depth == 16)
+                    return VLC_CODEC_RGBT;
                 if (f->bpp == 16 && f->depth == 15)
                     return VLC_CODEC_RGB15;
                 break;
