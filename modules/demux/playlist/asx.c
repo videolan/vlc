videolan/vlc

commit 4a590357633e5ad7419034bd7f20bf175cc12f45

asx.c: check for </MoreInfo> tag also
this should fix #3013

modules/demux/playlist/asx.c
@@ -433,6 +433,8 @@ static int Demux( demux_t *p_demux )
                 }
                 if( ( psz_parse = strcasestr( psz_parse, "/>" ) ) )
                     psz_parse += 2;
+                else if( ( psz_parse = strcasestr( psz_parse, "</MoreInfo>") ) )
+                    psz_parse += 11;
                 else continue;
             }
             else if( !strncasecmp( psz_parse, "<ABSTRACT>", 10 ) )

commit 719c0015f8a6889fd60de0d0ec95c189de8a8976

ASX: fix NULL derefence (LP#785979)
Regression introduced by commit 4a590357633e5ad7419034bd7f20bf175cc12f45.

modules/demux/playlist/asx.c
@@ -439,11 +439,15 @@ static int Demux( demux_t *p_demux )
                     }
                     else continue;
                 }
-                if( ( psz_parse = strcasestr( psz_parse, "/>" ) ) )
-                    psz_parse += 2;
-                else if( ( psz_parse = strcasestr( psz_parse, "</MoreInfo>") ) )
-                    psz_parse += 11;
-                else continue;
+                if( ( psz_backup = strcasestr( psz_parse, "/>" ) ) )
+                    psz_parse = psz_backup + 2;
+                else if( ( psz_backup = strcasestr( psz_parse, "</MoreInfo>") ) )
+                    psz_parse = psz_backup + 11;
+                else
+                {
+                    psz_parse = NULL;
+                    continue;
+                }
             }
             else if( !strncasecmp( psz_parse, "<ABSTRACT>", 10 ) )
             {
@@ -483,7 +487,6 @@ static int Demux( demux_t *p_demux )
                 }
                 if( ( psz_parse = strcasestr( psz_parse, "/>" ) ) )
                     psz_parse += 2;
-                else continue;
             }
             else if( !strncasecmp( psz_parse, "</Entry>", 8 ) )
             {