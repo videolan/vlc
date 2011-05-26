6a36c6c04ed6adfa4eb4e282ca1aeeed213b8f2b
Laurent Aimar <fenrir@videolan.org>
2011-05-26 22:48:57 +0200

    Added missing config.h include (opengl)

--- modules/video_output/opengl.c
+++ modules/video_output/opengl.c
@@ -24,6 +24,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 
 #include <vlc_common.h>
 #include <vlc_picture_pool.h>
