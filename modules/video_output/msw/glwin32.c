7cc37b375fc7f5cff2d83ab275f88e46bb7e3dc0
Laurent Aimar <fenrir@videolan.org>
2011-05-31 20:14:33 +0200

    Fixed memory leak in glwin32.

--- modules/video_output/msw/glwin32.c
+++ modules/video_output/msw/glwin32.c
@@ -191,7 +191,8 @@ static void Display(vout_display_t *vd, picture_t *picture, subpicture_t *subpic
     vout_display_opengl_Display(sys->vgl, &vd->source);
 
     picture_Release(picture);
-    VLC_UNUSED(subpicture);
+    if (subpicture)
+        subpicture_Delete(subpicture);
 
     CommonDisplay(vd);
 }
