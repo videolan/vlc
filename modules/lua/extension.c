0bade2cde2129fb7b719fd48a89190bcd3d5c650
Hugo Beauzée-Luyssen <beauze.h@gmail.com>
2011-07-04 17:59:55 +0200

    lua: Don't crash when reactivating an extension.
    Close #5019
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/lua/extension.c
+++ modules/lua/extension.c
@@ -654,6 +654,7 @@ int lua_ExtensionDeactivate( extensions_manager_t *p_mgr, extension_t *p_ext )
             vlc_gc_decref( p_item );
         }
         vlc_object_release( p_ext->p_sys->p_input );
+        p_ext->p_sys->p_input = NULL;
     }
 
     int i_ret = lua_ExecuteFunction( p_mgr, p_ext, "deactivate", LUA_END );
