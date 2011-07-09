fd4a0ddc54b4571272e68d29c9ad733b9544d811
Alexey Sokolov <alexey@alexeysokolov.co.cc>
2011-07-09 10:54:46 +0700

    Fix doxygen for libvlc_audio_output_set
    Tests show that current docs are wrong.
    
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- include/vlc/libvlc_media_player.h
+++ include/vlc/libvlc_media_player.h
@@ -1296,7 +1296,7 @@ LIBVLC_API void libvlc_audio_output_list_release( libvlc_audio_output_t *p_list 
  * \param p_mi media player
  * \param psz_name name of audio output,
  *               use psz_name of \see libvlc_audio_output_t
- * \return true if function succeded
+ * \return 0 if function succeded, -1 on error
  */
 LIBVLC_API int libvlc_audio_output_set( libvlc_media_player_t *p_mi,
                                             const char *psz_name );
