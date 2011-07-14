a4d304c79393f0acc9b4519792d62b7ebb72ea3f
Rémi Denis-Courmont <remi@remlab.net>
2011-07-14 20:46:51 +0300

    Remove unused gettimeofday() Win32 replacement

--- src/misc/mtime.c
+++ src/misc/mtime.c
@@ -229,51 +229,6 @@ mtime_t date_Decrement( date_t *p_date, uint32_t i_nb_samples )
     return p_date->date;
 }
 
-#ifndef HAVE_GETTIMEOFDAY
-
-#ifdef WIN32
-
-/*
- * Number of micro-seconds between the beginning of the Windows epoch
- * (Jan. 1, 1601) and the Unix epoch (Jan. 1, 1970).
- *
- * This assumes all Win32 compilers have 64-bit support.
- */
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS) || defined(__WATCOMC__)
-#   define DELTA_EPOCH_IN_USEC  11644473600000000Ui64
-#else
-#   define DELTA_EPOCH_IN_USEC  11644473600000000ULL
-#endif
-
-static uint64_t filetime_to_unix_epoch (const FILETIME *ft)
-{
-    uint64_t res = (uint64_t) ft->dwHighDateTime << 32;
-
-    res |= ft->dwLowDateTime;
-    res /= 10;                   /* from 100 nano-sec periods to usec */
-    res -= DELTA_EPOCH_IN_USEC;  /* from Win epoch to Unix epoch */
-    return (res);
-}
-
-static int gettimeofday (struct timeval *tv, void *tz )
-{
-    FILETIME  ft;
-    uint64_t tim;
-
-    if (!tv) {
-        return VLC_EGENERIC;
-    }
-    GetSystemTimeAsFileTime (&ft);
-    tim = filetime_to_unix_epoch (&ft);
-    tv->tv_sec  = (long) (tim / 1000000L);
-    tv->tv_usec = (long) (tim % 1000000L);
-    return (0);
-}
-
-#endif
-
-#endif
-
 /**
  * @return NTP 64-bits timestamp in host byte order.
  */
