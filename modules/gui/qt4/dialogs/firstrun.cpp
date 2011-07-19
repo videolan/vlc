2662d6ac5e9fb23596aff9a6b7be07b6d7571d8d
Francois Cartegnie <fcvlcdev@free.fr>
2011-07-19 21:35:58 +0200

    Qt: Firstrun privacy text accuracy changes

--- modules/gui/qt4/dialogs/firstrun.cpp
+++ modules/gui/qt4/dialogs/firstrun.cpp
@@ -61,7 +61,7 @@ void FirstRun::save()
 
 void FirstRun::buildPrivDialog()
 {
-    setWindowTitle( qtr( "Privacy and Network Policies" ) );
+    setWindowTitle( qtr( "Privacy and Network Access Policy" ) );
     setWindowRole( "vlc-privacy" );
     setWindowModality( Qt::ApplicationModal );
     setWindowFlags( Qt::Dialog );
@@ -69,38 +69,44 @@ void FirstRun::buildPrivDialog()
 
     QGridLayout *gLayout = new QGridLayout( this );
 
-    QGroupBox *blabla = new QGroupBox( qtr( "Privacy and Network Warning" ) );
+    QGroupBox *blabla = new QGroupBox( qtr( "Privacy and Network Access Policy" ) );
     QGridLayout *blablaLayout = new QGridLayout( blabla );
     QLabel *text = new QLabel( qtr(
-        "<p><i>VideoLAN</i> prefers when applications request authorization "
-        "before accessing Internet.</p>\n"
-        "<p><b>VLC media player</b> can get information from the Internet "
-        "in order to get <b>medias information</b> or to check for available <b>updates</b>.</p>\n"
-        "<p><i>VLC media player</i> <b>doesn't</b> send or collect any "
-        "information, even anonymously, about your usage.</p>\n" ) );
+        "<p><i>VLC media player</i> does <b>not</b> send or collect any "
+        "information, even anonymously, about your usage.</p>\n"
+        "<p>However, it can connect to the Internet "
+        "in order to display <b>medias information</b> "
+#ifdef UPDATE_CHECK
+        "or to check for available <b>updates</b>"
+#endif
+        ".</p>\n"
+        "<p><i>VideoLAN</i> (the authors) requires you to express your consent "
+        "before allowing this software to access the Internet.</p>\n"
+        "<p>According to your choices, please check or uncheck the following options:</p>\n"
+        ) );
     text->setWordWrap( true );
     text->setTextFormat( Qt::RichText );
 
     blablaLayout->addWidget( text, 0, 0 ) ;
 
-    QGroupBox *options = new QGroupBox( qtr( "Options" ) );
+    QGroupBox *options = new QGroupBox( qtr( "Network Access Policy" ) );
     QGridLayout *optionsLayout = new QGridLayout( options );
 
     gLayout->addWidget( blabla, 0, 0, 1, 3 );
     gLayout->addWidget( options, 1, 0, 1, 3 );
     int line = 0;
 
-    checkbox = new QCheckBox( qtr( "Allow fetching media information from Internet" ) );
+    checkbox = new QCheckBox( qtr( "Allow downloading media information" ) );
     checkbox->setChecked( true );
     optionsLayout->addWidget( checkbox, line++, 0 );
 
 #ifdef UPDATE_CHECK
-    checkbox2 = new QCheckBox( qtr( "Check for updates" ) );
+    checkbox2 = new QCheckBox( qtr( "Allow checking for VLC updates" ) );
     checkbox2->setChecked( true );
     optionsLayout->addWidget( checkbox2, line++, 0 );
 #endif
 
-    QPushButton *ok = new QPushButton( qtr( "OK" ) );
+    QPushButton *ok = new QPushButton( qtr( "Save and Continue" ) );
 
     gLayout->addWidget( ok, 2, 2 );
 
