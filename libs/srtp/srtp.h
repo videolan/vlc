dc24a3361a77122bdac93168c77814c9484247bb
Rémi Denis-Courmont <remi@remlab.net>
2011-07-19 17:52:35 +0300

    SRTP: fix flags mask and document enumerations
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- libs/srtp/srtp.h
+++ libs/srtp/srtp.h
@@ -24,36 +24,36 @@ typedef struct srtp_session_t srtp_session_t;
 
 enum
 {
-    SRTP_UNENCRYPTED=0x1,   // do not encrypt SRTP packets
-    SRTCP_UNENCRYPTED=0x2,  // do not encrypt SRTCP packets
-    SRTP_UNAUTHENTICATED=0x4, // authenticate only SRTCP packets
+    SRTP_UNENCRYPTED=0x1,   //< do not encrypt SRTP packets
+    SRTCP_UNENCRYPTED=0x2,  //< do not encrypt SRTCP packets
+    SRTP_UNAUTHENTICATED=0x4, //< authenticate only SRTCP packets
 
-    SRTP_RCC_MODE1=0x10,    // use Roll-over-Counter Carry mode 1
-    SRTP_RCC_MODE2=0x20,    // use Roll-over-Counter Carry mode 2
-    SRTP_RCC_MODE3=0x30,    // use Roll-over-Counter Carry mode 3 (insecure)
+    SRTP_RCC_MODE1=0x10,    //< use Roll-over-Counter Carry mode 1
+    SRTP_RCC_MODE2=0x20,    //< use Roll-over-Counter Carry mode 2
+    SRTP_RCC_MODE3=0x30,    //< use Roll-over-Counter Carry mode 3 (insecure)
 
-    SRTP_FLAGS_MASK=0x38
+    SRTP_FLAGS_MASK=0x37    //< mask for valid flags
 };
 
-/* SRTP encryption algorithms (ciphers); same values as MIKEY */
+/** SRTP encryption algorithms (ciphers); same values as MIKEY */
 enum
 {
-    SRTP_ENCR_NULL=0,
-    SRTP_ENCR_AES_CM=1,
-    SRTP_ENCR_AES_F8=2 // not implemented
+    SRTP_ENCR_NULL=0,   //< no encryption
+    SRTP_ENCR_AES_CM=1, //< AES counter mode
+    SRTP_ENCR_AES_F8=2, //< AES F8 mode (not implemented)
 };
 
-/* SRTP authenticaton algorithms; same values as MIKEY */
+/** SRTP authenticaton algorithms; same values as MIKEY */
 enum
 {
-    SRTP_AUTH_NULL=0,
-    SRTP_AUTH_HMAC_SHA1=1
+    SRTP_AUTH_NULL=0,      //< no authentication code
+    SRTP_AUTH_HMAC_SHA1=1, //< HMAC-SHA1
 };
 
-/* SRTP pseudo random function; same values as MIKEY */
+/** SRTP pseudo random function; same values as MIKEY */
 enum
 {
-    SRTP_PRF_AES_CM=0
+    SRTP_PRF_AES_CM=0, //< AES counter mode
 };
 
 # ifdef __cplusplus
