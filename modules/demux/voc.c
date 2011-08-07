88f6d80d7ef9819edf8f280d18bdaf646a846ba2
Rémi Denis-Courmont <remi@remlab.net>
2011-08-07 10:31:01 +0300

    voc: remove useless #include

--- modules/demux/voc.c
+++ modules/demux/voc.c
@@ -32,7 +32,6 @@
 #include <vlc_common.h>
 #include <vlc_plugin.h>
 #include <vlc_demux.h>
-#include <vlc_aout.h>
 
 /*****************************************************************************
  * Module descriptor
