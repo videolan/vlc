d8f5258db8a4122fc146619f605583eb91d89d2a
Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
2011-08-08 18:17:31 +0300

    mono downmix: manually selected so zero priority
    This plugin modifies the output format. It caused a busy loop
    and broke libmad on fixed-point architectures (fixes #5151).

--- modules/audio_filter/channel_mixer/mono.c
+++ modules/audio_filter/channel_mixer/mono.c
@@ -106,7 +106,7 @@ static const uint32_t pi_channels_out[] =
  *****************************************************************************/
 vlc_module_begin ()
     set_description( N_("Audio filter for stereo to mono conversion") )
-    set_capability( "audio filter", 2 )
+    set_capability( "audio filter", 0 )
     set_category( CAT_AUDIO )
     set_subcategory( SUBCAT_AUDIO_AFILTER )
     set_callbacks( OpenFilter, CloseFilter )
