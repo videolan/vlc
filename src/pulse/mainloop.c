6b43a1d3cdd2047bbe86d0b584e550e1fda060e0
Rémi Denis-Courmont <remi@remlab.net>
2011-08-10 22:27:23 +0300

    PulseAudio: destroy context like stream

--- src/pulse/mainloop.c
+++ src/pulse/mainloop.c
@@ -262,9 +262,9 @@ void vlc_pa_disconnect (vlc_object_t *obj, pa_context *ctx)
 {
     pa_threaded_mainloop *mainloop = vlc_pa_mainloop;
 
-    pa_threaded_mainloop_lock (mainloop);
+    pa_context_disconnect (ctx);
+    pa_context_set_state_callback (ctx, NULL, NULL);
     pa_context_unref (ctx);
-    pa_threaded_mainloop_unlock (mainloop);
 
     vlc_pa_mainloop_deinit (mainloop);
     (void) obj;
