be84287b2f1a43739cc0db1da05942bff6aaa477
Felix Paul Kühne <fkuehne@videolan.org>
2011-08-12 12:33:30 +0200

    compilation fix

--- src/missing.c
+++ src/missing.c
@@ -34,7 +34,7 @@
 
 char *httpd_ClientIP (const httpd_client_t *cl, char *psz_ip, int *port)
 {
-    (void) cl; (void) psz_ip; (void) port
+    (void) cl; (void) psz_ip; (void) port;
     assert (0);
 }
 
