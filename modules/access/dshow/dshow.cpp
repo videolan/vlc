69ec06251eeda08638d8e35405402eecb298dcfa
Jean-Baptiste Kempf <jb@videolan.org>
2011-08-30 22:04:15 +0200

    DShow: fix compilation after change on caching

--- modules/access/dshow/dshow.cpp
+++ modules/access/dshow/dshow.cpp
@@ -1986,8 +1986,8 @@ static int DemuxControl( demux_t *p_demux, int i_query, va_list args )
 
     case DEMUX_GET_PTS_DELAY:
         pi64 = (int64_t*)va_arg( args, int64_t * );
-        *pi_64 =
-            INT64_C(1000) * var_InheritInteger( p_access, "live-caching" );
+        *pi64 =
+            INT64_C(1000) * var_InheritInteger( p_demux, "live-caching" );
         return VLC_SUCCESS;
 
     case DEMUX_GET_TIME:
