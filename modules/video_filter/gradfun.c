05f6eeb5ddeb65f9402058cbba41f9c575a8d6c0
Rémi Denis-Courmont <remi@remlab.net>
2011-08-31 19:56:07 +0300

    gradfun: remove callback before destroying callback data

--- modules/video_filter/gradfun.c
+++ modules/video_filter/gradfun.c
@@ -160,6 +160,8 @@ static void Close(vlc_object_t *object)
     filter_t     *filter = (filter_t *)object;
     filter_sys_t *sys = filter->p_sys;
 
+    var_DelCallback(filter, CFG_PREFIX "radius",   Callback, NULL);
+    var_DelCallback(filter, CFG_PREFIX "strength", Callback, NULL);
     free(sys->base_buf);
     vlc_mutex_destroy(&sys->lock);
     free(sys);
