c768a6cfcff60e73a1b9f4f910f61d0c9b11df0f
Rémi Denis-Courmont <remi@remlab.net>
2011-08-31 19:56:27 +0300

    grain: remove callback before destroying callback data

--- modules/video_filter/grain.c
+++ modules/video_filter/grain.c
@@ -427,6 +427,7 @@ static void Close(vlc_object_t *object)
     filter_t     *filter = (filter_t *)object;
     filter_sys_t *sys    = filter->p_sys;
 
+    var_DelCallback(filter, CFG_PREFIX "variance", Callback, NULL);
     vlc_mutex_destroy(&sys->cfg.lock);
     free(sys);
 }
