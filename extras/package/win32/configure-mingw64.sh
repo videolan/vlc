9bd4eaa5e9b412d99606431d266e6985ebbe6ee4
Rafaël Carré <funman@videolan.org>
2011-09-23 02:07:09 -0400

    update configure-mingw64.sh

--- extras/package/win32/configure-mingw64.sh
+++ extras/package/win32/configure-mingw64.sh
@@ -10,9 +10,8 @@ else
 fi
 export CONTRIB_DIR
 
-CC=amd64-mingw32msvc-gcc CXX=amd64-mingw32msvc-g++ \
 CONFIGURE="${root}configure" \
-CONFIGOPTS="--host=amd64-mingw32msvc --build=i386-linux
- --enable-mkv --enable-taglib --enable-debug --enable-projectm
+CONFIGOPTS="--host=x86_64-w64-mingw32 --build=i386-linux
+ --enable-mkv --disable-taglib --enable-debug --disable-projectm
  --disable-qt4 --disable-skins2" \
 sh ${root}extras/package/win32/configure-common.sh
