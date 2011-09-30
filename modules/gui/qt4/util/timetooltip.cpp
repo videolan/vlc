videolan/vlc

commit 9367ce0b3a40851a656e136557347fe8e830770c

Qt: fix tooltip events handling on Win32
Close #4779

modules/gui/qt4/util/timetooltip.cpp
@@ -40,6 +40,7 @@ TimeTooltip::TimeTooltip( QWidget *parent ) :
     // Tell Qt that it doesn't need to erase the background before
     // a paintEvent occurs. This should save some CPU cycles.
     setAttribute( Qt::WA_OpaquePaintEvent );
+    setAttribute( Qt::WA_ShowWithoutActivating );
 
     // Inherit from the system default font size -5
     mFont = QFont( "Verdana", qMax( qApp->font().pointSize() - 5, 7 ) );

commit 744bf6f336e380bec9143da02542b3ef104ae5a4

Qt: fix a regression on Linux
Regression introduced in 9367ce0b3a40851a656e136557347fe8e830770c

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/gui/qt4/util/timetooltip.cpp
@@ -40,7 +40,14 @@ TimeTooltip::TimeTooltip( QWidget *parent ) :
     // Tell Qt that it doesn't need to erase the background before
     // a paintEvent occurs. This should save some CPU cycles.
     setAttribute( Qt::WA_OpaquePaintEvent );
+
+#ifdef Q_WS_WIN
+    /*
+    - This attribute is required on Windows to avoid focus stealing of other windows.
+    - When set on Linux the TimeTooltip appears behind the FSController in fullscreen.
+    */
     setAttribute( Qt::WA_ShowWithoutActivating );
+#endif
 
     // Inherit from the system default font size -5
     mFont = QFont( "Verdana", qMax( qApp->font().pointSize() - 5, 7 ) );