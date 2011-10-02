895e45bd2aa5d61b01e5be6c7908225b796e0f59
Rémi Denis-Courmont <remi@remlab.net>
2011-10-02 13:55:15 +0300

    Fix unlikely memory leak

--- modules/access/v4l2/controls.c
+++ modules/access/v4l2/controls.c
@@ -470,8 +470,10 @@ static vlc_v4l2_ctrl_t *ControlAddButton (vlc_object_t *obj, int fd,
         return NULL;
 
     if (var_Create (obj, c->name, VLC_VAR_VOID | VLC_VAR_ISCOMMAND))
+    {
+        free (c);
         return NULL;
-    (void) fd;
+    }
     return c;
 }
 
