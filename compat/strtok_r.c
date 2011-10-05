42419926ec6da22e863c0dfc06937369bdd2942f
Jean-Baptiste Kempf <jb@videolan.org>
2011-10-05 10:55:32 +0200

    strtok_r: remove tabs

--- compat/strtok_r.c
+++ compat/strtok_r.c
@@ -26,27 +26,27 @@
 
 char *strtok_r(char *s, const char *delim, char **save_ptr)
 {
-	char *token;
+    char *token;
 
-	if (s == NULL)
-		s = *save_ptr;
+    if (s == NULL)
+        s = *save_ptr;
 
-	/* Scan leading delimiters. */
-	s += strspn (s, delim);
-	if (*s == '\0')
-		return NULL;
+    /* Scan leading delimiters. */
+    s += strspn (s, delim);
+    if (*s == '\0')
+        return NULL;
 
-	/* Find the end of the token. */
-	token = s;
-	s = strpbrk (token, delim);
-	if (s == NULL)
-		/* This token finishes the string. */
-		*save_ptr = strchr (token, '\0');
-	else
-	{
-		/* Terminate the token and make *SAVE_PTR point past it. */
-		*s = '\0';
-		*save_ptr = s + 1;
-	}
-	return token;
+    /* Find the end of the token. */
+    token = s;
+    s = strpbrk (token, delim);
+    if (s == NULL)
+        /* This token finishes the string. */
+        *save_ptr = strchr (token, '\0');
+    else
+    {
+        /* Terminate the token and make *SAVE_PTR point past it. */
+        *s = '\0';
+        *save_ptr = s + 1;
+    }
+    return token;
 }
