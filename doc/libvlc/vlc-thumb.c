295594f691159ac6fc47112c354d8f4ba9641a05
Rafaël Carré <funman@videolan.org>
2011-11-02 22:26:16 -0400

    vlc-thumb: works also with gnome 3

--- doc/libvlc/vlc-thumb.c
+++ doc/libvlc/vlc-thumb.c
@@ -5,7 +5,10 @@
 /* Works with : libvlc 1.2.0
    gcc -pedantic -Wall -Werror -Wextra `pkg-config --cflags --libs libvlc` -lpthread
 
-  # to register the thumbnailer:
+  # to register the thumbnailer on gnome 3.x:
+  cp vlc-thumb.thumbnailer /usr/share/thumbnailers
+
+  # to register the thumbnailer on gnome 2.x:
   list=`grep ^Mime vlc.desktop|cut -d= -f2-|sed -e s/";"/\\\n/g -e s,/,@,g`
   vid=`echo $mimes|grep ^vid`
   for i in $vid
