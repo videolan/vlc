3f311b7280df3e9dae7c0f69134336a841f01803
Rafaël Carré <funman@videolan.org>
2011-11-20 14:15:33 -0500

    win32 configure helpers: merge into one script
    autodetect cygwin compiler
    you now have to specify --host option (and --disable-nls or --enable-debug) as argument

---
+++ extras/package/win32/configure.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+OPTIONS="
+      --enable-update-check
+      --enable-lua
+      --enable-faad
+      --enable-flac
+      --enable-theora
+      --enable-twolame
+      --enable-quicktime
+      --enable-avcodec --enable-merge-ffmpeg
+      --enable-dca
+      --enable-mpc
+      --enable-libass
+      --enable-x264
+      --enable-schroedinger
+      --enable-realrtsp
+      --enable-live555
+      --enable-dvdread
+      --enable-shout
+      --enable-goom
+      --enable-caca
+      --disable-portaudio
+      --disable-sdl
+      --enable-qt4
+      --enable-skins2
+      --enable-sse --enable-mmx
+      --enable-libcddb
+      --enable-zvbi --disable-telx
+      --enable-sqlite
+      --disable-dirac"
+
+if gcc -v 2>/dev/null -a echo | gcc -mno-cygwin -E -2>/dev/null 2>&1
+then
+    echo Cygwin detected, adjusting options
+    export CC="gcc -mno-cygwin"
+    export CXX="g++ -mno-cygwin"
+    OPTIONS="${OPTIONS} --disable-taglib --disable-mkv"
+fi
+
+sh "$(dirname $0)"/../../../configure ${OPTIONS} $*
