2bb4972e1e4b0a7ab735a6902c2bb616c0f07be4
Rafaël Carré <funman@videolan.org>
2011-11-23 17:28:08 -0500

    remove --enable-macosx-defaults, move to shell script

---
+++ extras/package/macosx/configure.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+OPTIONS="
+        --prefix=`pwd`/vlc_install_dir
+        --enable-faad
+        --enable-flac
+        --enable-theora
+        --enable-shout
+        --enable-vcdx
+        --enable-caca
+        --enable-ncurses
+        --enable-twolame
+        --enable-realrtsp
+        --enable-libass
+        --disable-skins2
+        --disable-xcb
+"
+
+sh "$(dirname $0)"/../../../configure ${OPTIONS} $*
