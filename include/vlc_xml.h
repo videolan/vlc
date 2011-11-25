17f189f20e51841d3a18986e435be3bbd88e8876
Jean-Baptiste Kempf <jb@videolan.org>
2011-11-25 01:55:15 +0100

    vlc_xml.h: reindent the standard header

--- include/vlc_xml.h
+++ include/vlc_xml.h
@@ -1,24 +1,24 @@
 /*****************************************************************************
-* xml.h: XML abstraction layer
-*****************************************************************************
-* Copyright (C) 2004-2010 the VideoLAN team
-*
-* Author: Gildas Bazin <gbazin@videolan.org>
-*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
-*****************************************************************************/
+ * xml.h: XML abstraction layer
+ *****************************************************************************
+ * Copyright (C) 2004-2010 the VideoLAN team
+ *
+ * Author: Gildas Bazin <gbazin@videolan.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 
 #ifndef VLC_XML_H
 #define VLC_XML_H
