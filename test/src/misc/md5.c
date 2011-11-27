4f0b2161aa473c6d2a88216ad32087ff3586d77f
Rémi Denis-Courmont <remi@remlab.net>
2011-11-27 13:11:22 +0200

    MD5: improve test coverage

--- test/src/misc/md5.c
+++ test/src/misc/md5.c
@@ -37,6 +37,15 @@ typedef struct
 
 static const md5_sample_t md5_samples[] =
 {
+    { "", "d41d8cd98f00b204e9800998ecf8427e" },
+    { "a", "0cc175b9c0f1b6a831c399e269772661" },
+    { "abc", "900150983cd24fb0d6963f7d28e17f72" },
+    { "message digest", "f96b697d7cb7938d525a2f31aaf161d0" },
+    { "abcdefghijklmnopqrstuvwxyz", "c3fcd3d76192e4007dfb496cca67e13b" },
+    { "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+      "d174ab98d277d9f5a5611c2c9f419d9f" },
+    { "12345678901234567890123456789012345678901234567890123456789012345678901"
+      "234567890", "57edf4a22be3c955ac49da2e2107b67a" },
     { "azertyuiop", "7682fe272099ea26efe39c890b33675b"    },
     { NULL,         NULL            }
 };
