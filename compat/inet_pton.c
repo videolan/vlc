6d0a1fce490cf2d4c9f23f6c0781efc91fa794b6
Naohiro KORIYAMA <nkoriyama@gmail.com>
2011-12-03 11:45:36 +0900

    inet_ntop always returns NULL on Windows.
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- compat/inet_pton.c
+++ compat/inet_pton.c
@@ -55,7 +55,7 @@ const char *inet_ntop (int af, const void *src, char *dst, int len)
     {
         case AF_INET:
             if (snprintf (dst, len, "%hhu.%hhu.%hhu.%hhu",
-                          b[0], b[1], b[2], b[3]) <= len)
+                          b[0], b[1], b[2], b[3]) >= len)
             {
                 errno = ENOSPC;
                 return NULL;
