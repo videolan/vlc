2cff61a8f84903b69e37b2d191d9349997683953
Pierre Ynard <linkfanel@yahoo.fr>
2011-12-03 22:36:55 +0100

    lua: don't document rc and telnet, they're gone

--- share/lua/intf/dummy.lua
+++ share/lua/intf/dummy.lua
@@ -3,9 +3,9 @@
 msg = [[
 This is the `dummy' VLC Lua interface module.
 Please specify a VLC Lua interface to load with the --lua-intf option.
-VLC Lua interface modules include: `rc', `telnet' and `http'.
-For example: vlc -I luaintf --lua-intf rc
-You can also use the alternate syntax: vlc -I "luaintf{intf=rc}"
+VLC Lua interface modules include: `cli' and `http'.
+For example: vlc -I luaintf --lua-intf cli
+You can also use the alternate syntax: vlc -I "luaintf{intf=cli}"
 See share/lua/intf/README.txt for more information about lua interface modules.]]
 
 for line in string.gmatch(msg,"([^\n]+)\n*") do
