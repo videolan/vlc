087541029a5d8f3feb80c541a57ec706782ae9bd
Rafaël Carré <funman@videolan.org>
2011-12-06 17:44:56 -0500

    Remove INTF_GAMMA_*
    It was not used for 10 years (minus 3 days)

--- include/vlc_config.h
+++ include/vlc_config.h
@@ -52,10 +52,6 @@
 /* Base delay in micro second for interface sleeps */
 #define INTF_IDLE_SLEEP                 (CLOCK_FREQ/20)
 
-/* Step for changing gamma, and minimum and maximum values */
-#define INTF_GAMMA_STEP                 .1
-#define INTF_GAMMA_LIMIT                3
-
 /*****************************************************************************
  * Input thread configuration
  *****************************************************************************/
