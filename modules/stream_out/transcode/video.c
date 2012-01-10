45ee0e5c08f3ace0cb314f9108839ce856ba3f4e
Laurent Aimar <fenrir@videolan.org>
2012-01-10 20:48:40 +0100

    Fixed video AR given to the encoder when transcoding.

--- modules/stream_out/transcode/video.c
+++ modules/stream_out/transcode/video.c
@@ -480,8 +480,8 @@ static void transcode_video_encoder_init( sout_stream_t *p_stream,
                      id->p_decoder->fmt_out.video.i_sar_den,
                      0 );
 
-        id->p_encoder->fmt_out.video.i_sar_num = id->p_decoder->fmt_out.video.i_sar_num * i_src_width / i_dst_width;
-        id->p_encoder->fmt_out.video.i_sar_den = id->p_decoder->fmt_out.video.i_sar_den * i_src_height / i_dst_height;
+        id->p_encoder->fmt_out.video.i_sar_num = id->p_decoder->fmt_out.video.i_sar_num * i_src_width  * i_dst_height;
+        id->p_encoder->fmt_out.video.i_sar_den = id->p_decoder->fmt_out.video.i_sar_den * i_src_height * i_dst_width;
     }
     vlc_ureduce( &id->p_encoder->fmt_out.video.i_sar_num,
                  &id->p_encoder->fmt_out.video.i_sar_den,
