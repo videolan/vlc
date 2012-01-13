videolan/vlc

commit f69cdcd31f4c95257871db04e9616b731e4b10a7

Using strings 3 times, defining each only once
Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/gui/macosx/simple_prefs.m
@@ -31,6 +31,7 @@
 #import <vlc_interface.h>
 #import <vlc_dialog.h>
 #import <vlc_modules.h>
+#import <vlc_config_cat.h>
 #import "misc.h"
 #import "intf.h"
 #import "AppleRemote.h"
@@ -185,11 +186,11 @@ create_toolbar_item( NSString * o_itemIdent, NSString * o_name, NSString * o_des
     }
     else if( [o_itemIdent isEqual: VLCOSDSettingToolbarIdentifier] )
     {
-        CreateToolbarItem( _NS("Subtitles & OSD"), _NS("Subtitles & On Screen Display Settings"), @"spref_cone_Subtitles_64", showOSDSettings );
+        CreateToolbarItem( SUBPIC_TITLE, _NS("Subtitles & On Screen Display Settings"), @"spref_cone_Subtitles_64", showOSDSettings );
     }
     else if( [o_itemIdent isEqual: VLCInputSettingToolbarIdentifier] )
     {
-        CreateToolbarItem( _NS("Input & Codecs"), _NS("Input & Codec settings"), @"spref_cone_Input_64", showInputSettings );
+        CreateToolbarItem( INPUT_TITLE, _NS("Input & Codec settings"), @"spref_cone_Input_64", showInputSettings );
     }
     else if( [o_itemIdent isEqual: VLCHotkeysSettingToolbarIdentifier] )
     {

modules/gui/qt4/components/simple_preferences.cpp
@@ -89,9 +89,9 @@ SPrefsCatList::SPrefsCatList( intf_thread_t *_p_intf, QWidget *_parent, bool sma
                   cone_audio_64, 1 );
     ADD_CATEGORY( SPrefsVideo, qtr("Video"), qtr("Video Settings"),
                   cone_video_64, 2 );
-    ADD_CATEGORY( SPrefsSubtitles, qtr("Subtitles && OSD"), qtr("Subtitles & On Screen Display Settings"),
+    ADD_CATEGORY( SPrefsSubtitles, SUBPIC_TITLE, qtr("Subtitles & On Screen Display Settings"),
                   cone_subtitles_64, 3 );
-    ADD_CATEGORY( SPrefsInputAndCodecs, qtr("Input && Codecs"), qtr("Input & Codecs Settings"),
+    ADD_CATEGORY( SPrefsInputAndCodecs, INPUT_TITLE, qtr("Input & Codecs Settings"),
                   cone_input_64, 4 );
     ADD_CATEGORY( SPrefsHotkeys, qtr("Hotkeys"), qtr("Configure Hotkeys"),
                   cone_hotkeys_64, 5 );

commit e80360db5a05a7ea87cc994daac29e493c98d0e2

macosx: fixed crash when opening the simple preferences introduced in [f69cdcd31f4c95257871db04e9616b731e4b10a7]

modules/gui/macosx/simple_prefs.m
@@ -186,11 +186,11 @@ create_toolbar_item( NSString * o_itemIdent, NSString * o_name, NSString * o_des
     }
     else if( [o_itemIdent isEqual: VLCOSDSettingToolbarIdentifier] )
     {
-        CreateToolbarItem( SUBPIC_TITLE, _NS("Subtitles & On Screen Display Settings"), @"spref_cone_Subtitles_64", showOSDSettings );
+        CreateToolbarItem( _NS(SUBPIC_TITLE), _NS("Subtitles & On Screen Display Settings"), @"spref_cone_Subtitles_64", showOSDSettings );
     }
     else if( [o_itemIdent isEqual: VLCInputSettingToolbarIdentifier] )
     {
-        CreateToolbarItem( INPUT_TITLE, _NS("Input & Codec settings"), @"spref_cone_Input_64", showInputSettings );
+        CreateToolbarItem( _NS(INPUT_TITLE), _NS("Input & Codec settings"), @"spref_cone_Input_64", showInputSettings );
     }
     else if( [o_itemIdent isEqual: VLCHotkeysSettingToolbarIdentifier] )
     {