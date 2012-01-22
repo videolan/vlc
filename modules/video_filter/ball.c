432a626a513e768023cfd5bf9fff735b146611fc
Rémi Denis-Courmont <remi@remlab.net>
2012-01-22 18:56:59 +0100

    ball: fix filter variable names

--- modules/video_filter/ball.c
+++ modules/video_filter/ball.c
@@ -149,7 +149,7 @@ vlc_module_begin ()
 vlc_module_end ()
 
 static const char *const ppsz_filter_options[] = {
-    "ball-color", "ball-speed", "ball-size",
+    "color", "speed", "size",
     "gradient-threshold", "edge-visible", NULL
 };
 
