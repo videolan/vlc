c4df4fdf819370fb3c8d26c1f8236b33dea0d009
Rafaël Carré <funman@videolan.org>
2012-01-26 20:17:13 -0500

    dash: fix windows compile
    include config.h

--- modules/stream_filter/dash/mpd/CommonAttributesElements.cpp
+++ modules/stream_filter/dash/mpd/CommonAttributesElements.cpp
@@ -21,6 +21,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include "CommonAttributesElements.h"
 
 #include "mpd/ContentDescription.h"
