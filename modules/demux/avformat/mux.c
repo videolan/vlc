62229a3b5c564b9d3c6401fe731a70c4504e6f14
Rafaël Carré <funman@videolan.org>
2012-01-28 21:52:04 -0500

    avformat mux: don't write trailer if writing header failed

--- modules/demux/avformat/mux.c
+++ modules/demux/avformat/mux.c
@@ -173,7 +173,7 @@ void CloseMux( vlc_object_t *p_this )
     sout_mux_t *p_mux = (sout_mux_t*)p_this;
     sout_mux_sys_t *p_sys = p_mux->p_sys;
 
-    if( !p_sys->b_write_header && av_write_trailer( p_sys->oc ) < 0 )
+    if( !p_sys->b_write_header && !p_sys->b_error && av_write_trailer( p_sys->oc ) < 0 )
     {
         msg_Err( p_mux, "could not write trailer" );
     }
