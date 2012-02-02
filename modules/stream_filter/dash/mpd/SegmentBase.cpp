91da0b24c6110d776a690664715bca83b3adf0d7
Hugo Beauzée-Luyssen <beauze.h@gmail.com>
2012-02-02 12:43:04 +0100

    dash: Include <config.h> in new cpp files.

--- modules/stream_filter/dash/mpd/SegmentBase.cpp
+++ modules/stream_filter/dash/mpd/SegmentBase.cpp
@@ -22,6 +22,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include "SegmentBase.h"
 
 using namespace dash::mpd;
--- modules/stream_filter/dash/mpd/SegmentList.cpp
+++ modules/stream_filter/dash/mpd/SegmentList.cpp
@@ -22,6 +22,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include "SegmentList.h"
 
 using namespace dash::mpd;
