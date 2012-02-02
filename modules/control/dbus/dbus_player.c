2e1a59d0a1fcca58171b890416a39e22db7adb6f
Edward Sheldrake <ejsheldrake@gmail.com>
2012-02-02 16:23:47 +0000

    Fix dbus error (close #5977)
    Fix dbus assertion failure which aborts vlc when the dbus control
    plugin is active and you try to launch vlc with a large number of files
    on the command line (such as when doing "vlc *" to play all the files
    in a directory).
    
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/control/dbus/dbus_player.c
+++ modules/control/dbus/dbus_player.c
@@ -309,7 +309,7 @@ MarshalCanPlay( intf_thread_t *p_intf, DBusMessageIter *container )
     playlist_t *p_playlist = p_intf->p_sys->p_playlist;
 
     PL_LOCK;
-    dbus_bool_t b_can_play = playlist_CurrentSize( p_playlist );
+    dbus_bool_t b_can_play = playlist_CurrentSize( p_playlist ) > 0;
     PL_UNLOCK;
 
     dbus_message_iter_append_basic( container, DBUS_TYPE_BOOLEAN, &b_can_play );
