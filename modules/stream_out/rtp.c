83a9a56dfa12649723f521e75c5c0b6bf14e9197
Pierre Ynard <linkfanel@yahoo.fr>
2012-02-05 03:34:20 +0100

    rtp sout: fix comment

--- modules/stream_out/rtp.c
+++ modules/stream_out/rtp.c
@@ -1576,7 +1576,7 @@ static int64_t rtp_init_ts( const vod_media_t *p_media,
     /* As per RFC 2326, session identifiers are at least 8 bytes long */
     strncpy((char *)&i_ts_init, psz_vod_session, sizeof(uint64_t));
     i_ts_init ^= (uintptr_t)p_media;
-    /* Limit the timestamp to 48 bytes, this is enough and allows us
+    /* Limit the timestamp to 48 bits, this is enough and allows us
      * to stay away from overflows */
     i_ts_init &= 0xFFFFFFFFFFFF;
     return i_ts_init;
