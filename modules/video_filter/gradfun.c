c7098cce3e72f298a2045f38772e2f6ef4bced24
Jean-Baptiste Kempf <jb@videolan.org>
2012-02-11 15:42:51 +0100

    Gradfun: missing translation

--- modules/video_filter/gradfun.c
+++ modules/video_filter/gradfun.c
@@ -55,7 +55,7 @@ static void Close(vlc_object_t *);
 vlc_module_begin()
     set_description(N_("Gradfun video filter"))
     set_shortname(N_("Gradfun"))
-    set_help("Debanding algorithm")
+    set_help(N_("Debanding algorithm"))
     set_capability("video filter2", 0)
     set_category(CAT_VIDEO)
     set_subcategory(SUBCAT_VIDEO_VFILTER)
