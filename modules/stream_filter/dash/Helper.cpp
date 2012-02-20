21d55988afbb310f8580cb52f7c2930a043fbbcb
Christopher Mueller <christopher.mueller@itec.aau.at>
2012-02-20 19:17:43 +0100

    dash: helper added config.h
    Signed-off-by: Hugo Beauzée-Luyssen <beauze.h@gmail.com>

--- modules/stream_filter/dash/Helper.cpp
+++ modules/stream_filter/dash/Helper.cpp
@@ -21,6 +21,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
 
 #include "Helper.h"
 
