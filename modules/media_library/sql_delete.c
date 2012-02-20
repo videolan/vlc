3c95653a0f02d84b6b02067b9b4eadb05bcef971
Daniel Mierswa <impulze@impulze.org>
2012-02-20 15:56:06 +0100

    include config.h in media library sourcefiles
    otherwise asprintf will be undeclared and compilation will fail due to
    -Werror-implicit-declaration
    
    Signed-off-by: Daniel Mierswa <impulze@impulze.org>
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/media_library/sql_delete.c
+++ modules/media_library/sql_delete.c
@@ -25,6 +25,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include "sql_media_library.h"
 
 
