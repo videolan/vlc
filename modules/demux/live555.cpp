01cdee1abf28a456dcba1d9b17c59913faca7c50
Sébastien Escudier <sebastien-devel@celeos.eu>
2012-03-23 11:25:15 +0100

    live555 : better error message

--- modules/demux/live555.cpp
+++ modules/demux/live555.cpp
@@ -659,7 +659,7 @@ describe:
                 msg_Dbg( p_demux, "connection error %d", i_code );
                 if( i_code == 403 )
                     dialog_Fatal( p_demux, _("RTSP connection failed"),
-                                    _("You are not allowed to access this server.") );
+                                  _("Access to the stream is denied by the server configuration.") );
             }
             if( p_sys->rtsp ) RTSPClient::close( p_sys->rtsp );
             p_sys->rtsp = NULL;
