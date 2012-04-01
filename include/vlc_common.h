4d26b98a1509d1f76d13d72dd8e501840c099fcf
Rémi Denis-Courmont <remi@remlab.net>
2012-04-01 23:16:21 +0300

    Remove core ACL support
    I wrote this as a quick hack after Gildas made the HTTP server bind to
    all addresses instead of only loopback by default. It turns out it
    lasted a bit longer :D

--- include/vlc_common.h
+++ include/vlc_common.h
@@ -325,7 +325,6 @@ typedef struct filter_sys_t filter_sys_t;
 
 /* Network */
 typedef struct virtual_socket_t v_socket_t;
-typedef struct vlc_acl_t vlc_acl_t;
 typedef struct vlc_url_t vlc_url_t;
 
 /* Misc */
