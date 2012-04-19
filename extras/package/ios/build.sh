08c26f8c5fdcd431c197f2b5de0042bc3e7ccd96
Felix Paul Kühne <fkuehne@videolan.org>
2012-04-19 19:20:35 +0200

    ios/build.sh: export CCAS correctly, since this is needed to compile VLC's neon code

--- extras/package/ios/build.sh
+++ extras/package/ios/build.sh
@@ -203,6 +203,7 @@ spopd
 if [ "$PLATFORM" = "OS" ]; then
   export AS="${IOS_GAS_PREPROCESSOR} ${CC}"
   export ASCPP="${IOS_GAS_PREPROCESSOR} ${CC}"
+  export CCAS="${IOS_GAS_PREPROCESSOR} ${CC}"
 else
   export AS="xcrun as"
   export ASCPP="xcrun as"
