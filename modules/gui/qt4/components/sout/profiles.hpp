4323b8256cccdd92d656a7bb33f3f097cb6bd95a
Rafaël Carré <funman@videolan.org>
2012-04-20 12:49:50 -0400

    qt4 sout: replace AAC with MP3
    There is no GPLv2 compatible AAC encoder available today
    FFmpeg's one is considered to be useless crap (including by its authors)

--- modules/gui/qt4/components/sout/profiles.hpp
+++ modules/gui/qt4/components/sout/profiles.hpp
@@ -2,10 +2,10 @@
     (sizeof(video_profile_value_list)/sizeof(video_profile_value_list[0]))
 
 static const char video_profile_name_list[][35] = {
-    "Video - H.264 + AAC (MP4)",
+    "Video - H.264 + MP3 (MP4)",
     "Video - VP80 + Vorbis (Webm)",
-    "Video - H.264 + AAC (TS)",
-    "Video - Dirac + AAC (TS)",
+    "Video - H.264 + MP3 (TS)",
+    "Video - Dirac + MP3 (TS)",
     "Video - Theora + Vorbis (OGG)",
     "Video - Theora + Flac (OGG)",
     "Video - MPEG-2 + MPGA (TS)",
@@ -13,7 +13,7 @@ static const char video_profile_name_list[][35] = {
     "Video - DIV3 + MP3 (ASF)",
     "Audio - Vorbis (OGG)",
     "Audio - MP3",
-    "Audio - AAC (MP4)",
+    "Audio - MP3 (MP4)",
     "Audio - FLAC",
     "Audio - CD",
 };
@@ -24,10 +24,10 @@ static const char video_profile_value_list[][53] = {
     /* scale(float), fps(float), width(integer, height(integer), */
     /* audio codec(string), audio bitrate(integer), channels(integer), */
     /* samplerate(integer), subtitle codec(string), subtitle overlay(bool) */
-    "mp4;1;1;0;h264;0;0;0;0;0;mp4a;128;2;44100;0;1",
+    "mp4;1;1;0;h264;0;0;0;0;0;mpga;128;2;44100;0;1",
     "webm;1;1;0;VP80;2000;0;0;0;0;vorb;128;2;44100;0;1",
-    "ts;1;1;0;h264;800;1;0;0;0;mp4a;128;2;44100;0;0",
-    "ts;1;1;0;drac;800;1;0;0;0;mp4a;128;2;44100;0;0",
+    "ts;1;1;0;h264;800;1;0;0;0;mpga;128;2;44100;0;0",
+    "ts;1;1;0;drac;800;1;0;0;0;mpga;128;2;44100;0;0",
     "ogg;1;1;0;theo;800;1;0;0;0;vorb;128;2;44100;0;0",
     "ogg;1;1;0;theo;800;1;0;0;0;flac;128;2;44100;0;0",
     "ts;1;1;0;mp2v;800;1;0;0;0;mpga;128;2;44100;0;0",
@@ -35,7 +35,7 @@ static const char video_profile_value_list[][53] = {
     "asf;1;1;0;DIV3;800;1;0;0;0;mp3;128;2;44100;0;0",
     "ogg;1;1;0;none;800;1;0;0;0;vorb;128;2;44100;none;0",
     "raw;1;1;0;none;800;1;0;0;0;mp3;128;2;44100;none;0",
-    "mp4;1;1;0;none;800;1;0;0;0;mp4a;128;2;44100;none;0",
+    "mp4;1;1;0;none;800;1;0;0;0;mpga;128;2;44100;none;0",
     "raw;1;1;0;none;800;1;0;0;0;flac;128;2;44100;none;0",
     "wav;1;1;0;none;800;1;0;0;0;s16l;128;2;44100;none;0",
 };
