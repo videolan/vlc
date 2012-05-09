videolan/vlc

commit 55c6f68897a18e906edebc596c84c2ad43f78f3f

macosx: since Cocoa expects the main thread to quit (and needs to run on the main thread in the first place), we cannot join the macosx intf's thread (fixes #6026)

src/interface/interface.c
@@ -202,6 +202,9 @@ void intf_DestroyAll( libvlc_int_t *p_libvlc )
         if( p_intf->pf_run )
         {
             vlc_cancel( p_intf->thread );
+#ifdef __APPLE__
+            if (!p_intf->b_should_run_on_first_thread)
+#endif
             vlc_join( p_intf->thread, NULL );
         }
         module_unneed( p_intf, p_intf->p_module );

modules/gui/macosx/intf.m
@@ -192,16 +192,6 @@ static int f_appExit = 0;           // set to 1 when application termination sig
 
 static void Run( intf_thread_t *p_intf )
 {
-    sigset_t set;
-
-    /* Make sure the "force quit" menu item does quit instantly.
-     * VLC overrides SIGTERM which is sent by the "force quit"
-     * menu item to make sure daemon mode quits gracefully, so
-     * we un-override SIGTERM here. */
-    sigemptyset( &set );
-    sigaddset( &set, SIGTERM );
-    pthread_sigmask( SIG_UNBLOCK, &set, NULL );
-
     NSAutoreleasePool * o_pool = [[NSAutoreleasePool alloc] init];
     [VLCApplication sharedApplication];
 
@@ -712,9 +702,6 @@ static VLCMain *_o_sharedMainInstance = nil;
     config_PutInt( p_intf, "random", var_GetBool( p_playlist, "random" ) );
     config_PutInt( p_intf, "loop", var_GetBool( p_playlist, "loop" ) );
     config_PutInt( p_intf, "repeat", var_GetBool( p_playlist, "repeat" ) );
-
-    // save stuff
-    config_SaveConfigFile( p_intf );
 
     msg_Dbg( p_intf, "Terminating" );
 

commit 2af549a680ba0fd88b4803d0899b8aed1442a9fb

Remove non-sensical condition
If a thread can be cancelled, it can be joined.

Regression introduced in 55c6f68897a18e906edebc596c84c2ad43f78f3f.

src/interface/interface.c
@@ -134,8 +134,8 @@ int intf_Create( vlc_object_t *p_this, const char *chain )
         libvlc_SetExitHandler( p_libvlc, vlc_object_kill, p_intf );
         assert( p_intf->pf_run );
         p_intf->pf_run( p_intf );
+        p_intf->pf_run = NULL;
     }
-    else
 #endif
     /* Run the interface in a separate thread */
     if( p_intf->pf_run
@@ -189,9 +189,6 @@ void intf_DestroyAll( libvlc_int_t *p_libvlc )
         if( p_intf->pf_run )
         {
             vlc_cancel( p_intf->thread );
-#ifdef __APPLE__
-            if (!p_intf->b_should_run_on_first_thread)
-#endif
             vlc_join( p_intf->thread, NULL );
         }
         module_unneed( p_intf, p_intf->p_module );