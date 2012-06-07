92770fd071b8af4c3483103a8d8c1e37d41c30d1
Felix Paul Kühne <fkuehne@videolan.org>
2012-06-07 20:08:41 +0200

    extras/macosx: added the optimization flags removed from configure.ac
    These can be triggered by setting the ARCH variable respectively, which will be done by the OS X build script

--- extras/package/macosx/configure.sh
+++ extras/package/macosx/configure.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+CFLAGS=${CFLAGS}
+LDFLAGS=${LDFLAGS}
+
+case "${ARCH}" in
+    x86_64*)
+        CFLAGS="${CFLAGS} -m64 -march=core2 -mtune=core2"
+        LDFLAGS="${LDFLAGS} -m64"
+        ;;
+    ppc)
+        CFLAGS="${CFLAGS} -arch ppc -mtune=G4"
+        LDFLAGS="${LDFLAGS} -arch ppc"
+        ;;
+    *x86*)
+        CFLAGS="${CFLAGS} -m32 -march=prescott -mtune=generic"
+        LDFLAGS="${LDFLAGS} -m32"
+        ;;
+esac
+
 OPTIONS="
         --prefix=`pwd`/vlc_install_dir
         --enable-macosx
@@ -24,5 +42,8 @@ OPTIONS="
         --disable-sdl
         --disable-samplerate
 "
+
+export CFLAGS
+export LDFLAGS
 
 sh "$(dirname $0)"/../../../configure ${OPTIONS} $*
