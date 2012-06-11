adcd6fe49003b6016f1246b377e1f768f1dfb6c9
Rafaël Carré <funman@videolan.org>
2012-06-11 16:04:34 -0400

    change_prefix: fix help

--- contrib/src/change_prefix.sh
+++ contrib/src/change_prefix.sh
@@ -27,7 +27,7 @@ LANG=C
 export LANG
 
 if test "$1" = "-h" -o "$1" = "--help" -o $# -gt 2; then
-  echo "Usage: $0 [new_prefix] [old prefix]
+  echo "Usage: $0 [old prefix] [new prefix]
 
 Without arguments, this script assumes old prefix = @@CONTRIB_PREFIX@@,
 and new prefix = current directory.
