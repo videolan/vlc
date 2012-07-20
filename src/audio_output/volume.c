videolan/vlc

commit dcf3f71b3b48090881f8deab305cc31ea1067ca3

aout: add --gain to control gain regardless of output module
This linear gain works also for outputs with native volume or without
volume. (Obviously though, it does not work for digital pass-through).

src/audio_output/volume.c
@@ -145,7 +145,6 @@ int aout_volume_Amplify(aout_volume_t *vol, block_t *block)
 static float aout_ReplayGainSelect(vlc_object_t *obj, const char *str,
                                    const audio_replay_gain_t *replay_gain)
 {
-    float gain = 0.;
     unsigned mode = AUDIO_REPLAY_GAIN_MAX;
 
     if (likely(str != NULL))
@@ -165,21 +164,32 @@ static float aout_ReplayGainSelect(vlc_object_t *obj, const char *str,
     }
 
     /* */
+    float multiplier;
+
     if (mode == AUDIO_REPLAY_GAIN_MAX)
-        return 1.;
-
-    if (replay_gain->pb_gain[mode])
-        gain = replay_gain->pf_gain[mode]
-             + var_InheritFloat (obj, "audio-replay-gain-preamp");
+    {
+        multiplier = 1.f;
+    }
     else
-        gain = var_InheritFloat (obj, "audio-replay-gain-default");
+    {
+        float gain;
 
-    float multiplier = pow (10., gain / 20.);
+        if (replay_gain->pb_gain[mode])
+            gain = replay_gain->pf_gain[mode]
+                 + var_InheritFloat (obj, "audio-replay-gain-preamp");
+        else
+            gain = var_InheritFloat (obj, "audio-replay-gain-default");
+
+        multiplier = pow (10., gain / 20.);
+    }
 
     if (replay_gain->pb_peak[mode]
      && var_InheritBool (obj, "audio-replay-gain-peak-protection")
      && replay_gain->pf_peak[mode] * multiplier > 1.0)
         multiplier = 1.0f / replay_gain->pf_peak[mode];
+
+    /* Command line / configuration gain */
+    multiplier *= var_InheritFloat (obj, "gain");
 
     return multiplier;
 }

src/libvlc-module.c
@@ -149,6 +149,10 @@ static const char *const ppsz_snap_formats[] =
 #define MONO_LONGTEXT N_("This will force a mono audio output.")
 #endif
 
+#define GAIN_TEXT N_("Audio gain")
+#define GAIN_LONGTEXT N_( \
+    "Thus linear gain will be applied to outputted audio.")
+
 #define VOLUME_TEXT N_("Default audio volume")
 #define VOLUME_LONGTEXT N_( \
     "You can set the default audio output volume here.")
@@ -1470,6 +1474,8 @@ vlc_module_begin ()
 
     add_bool( "audio", 1, AUDIO_TEXT, AUDIO_LONGTEXT, false )
         change_safe ()
+    add_float( "gain", 1., GAIN_TEXT, GAIN_LONGTEXT, true )
+        change_float_range( 0., 8. )
     add_integer_with_range( "volume", AOUT_VOLUME_DEFAULT, 0,
                             AOUT_VOLUME_MAX, VOLUME_TEXT,
                             VOLUME_LONGTEXT, false )

commit 4a7c50fc2621f20afce6e9bde433b7288ae8df4c

aout: fix read overflow
pb_peak and pf_peak have only two elements.
(regression from dcf3f71b3b48090881f8deab305cc31ea1067ca3)

src/audio_output/volume.c
@@ -181,12 +181,12 @@ static float aout_ReplayGainSelect(vlc_object_t *obj, const char *str,
             gain = var_InheritFloat (obj, "audio-replay-gain-default");
 
         multiplier = pow (10., gain / 20.);
-    }
 
-    if (replay_gain->pb_peak[mode]
-     && var_InheritBool (obj, "audio-replay-gain-peak-protection")
-     && replay_gain->pf_peak[mode] * multiplier > 1.0)
-        multiplier = 1.0f / replay_gain->pf_peak[mode];
+        if (replay_gain->pb_peak[mode]
+         && var_InheritBool (obj, "audio-replay-gain-peak-protection")
+         && replay_gain->pf_peak[mode] * multiplier > 1.f)
+            multiplier = 1.f / replay_gain->pf_peak[mode];
+    }
 
     /* Command line / configuration gain */
     multiplier *= var_InheritFloat (obj, "gain");