ed9be19b059989eb29a3b33bfda52ad314048ca0
KO Myung-Hun <komh78@gmail.com>
2012-07-23 21:30:56 +0900

    OS/2: Add an extra configure script
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

---
+++ extras/package/os2/configure.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+OPTIONS="
+      --enable-shared
+      --enable-run-as-root
+      --enable-lua
+      --enable-live555
+      --enable-dvdread
+      --enable-dvdnav
+      --enable-vcd
+      --enable-libcddb
+      --enable-realrtsp
+      --enable-dvbpsi
+      --enable-ogg
+      --enable-mux_ogg
+      --enable-mkv
+      --enable-mad
+      --enable-merge-ffmpeg
+      --enable-avcodec
+      --enable-avformat
+      --enable-swscale
+      --enable-postproc
+      --enable-a52
+      --enable-flac
+      --enable-libmpeg2
+      --enable-vorbis
+      --enable-png
+      --enable-x264
+      --disable-xcb
+      --disable-xvideo
+      --disable-glx
+      --enable-freetype
+      --disable-fribidi
+      --enable-fontconfig
+      --enable-kva
+      --enable-kai
+      --enable-qt
+      --disable-skins2
+      --enable-libxml2
+      --enable-libgcrypt
+      --enable-vlc
+"
+
+export ARCHFLAGS=${ARCHFLAGS-"-march=i486"}
+export CFLAGS="${CFLAGS} ${ARCHFLAGS}"
+export CXXFLAGS="${CXXFLAGS} ${ARCHFLAGS}"
+
+sh "$(dirname $0)"/../../../configure ${OPTIONS} $*
