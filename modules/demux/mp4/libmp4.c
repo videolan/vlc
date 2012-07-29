c209bfba272630c3ef0f04f3713f0294cbe5e4ee
Jean-Baptiste Kempf <jb@videolan.org>
2012-07-29 08:57:20 +0200

    MP4: kill a unused variable warning

--- modules/demux/mp4/libmp4.c
+++ modules/demux/mp4/libmp4.c
@@ -2539,6 +2539,7 @@ static int MP4_ReadBox_skcr( stream_t *p_stream, MP4_Box_t *p_box )
 
 static int MP4_ReadBox_drms( stream_t *p_stream, MP4_Box_t *p_box )
 {
+    VLC_UNUSED(p_box);
     /* ATOMs 'user', 'key', 'iviv', and 'priv' will be skipped,
      * so unless data decrypt itself by magic, there will be no playback,
      * but we never know... */
