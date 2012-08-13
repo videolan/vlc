8acf84f171bd02e6c65c895285cb1dc2f183eca5
Jean-Baptiste Kempf <jb@videolan.org>
2012-08-13 17:39:34 +0200

    Core: SD, kill a warning

--- src/playlist/services_discovery.c
+++ src/playlist/services_discovery.c
@@ -322,6 +322,7 @@ static void playlist_sd_item_removed( const vlc_event_t * p_event, void * user_d
 /* A request to remove all ideas from SD */
 static void playlist_sd_item_removeall( const vlc_event_t * p_event, void * user_data )
 {
+    VLC_UNUSED(p_event);
     playlist_item_t* p_sd_node = user_data;
     if( p_sd_node == NULL ) return;
     playlist_t* p_playlist = p_sd_node->p_playlist;
