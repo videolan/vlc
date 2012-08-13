5ea1694ee914b958e720f00a2af2d7b2a5c2d59d
Jean-Baptiste Kempf <jb@videolan.org>
2012-08-13 17:53:28 +0200

    OpenCV example: move to C++ and opencv2 APIs

--- modules/video_filter/opencv_example.cpp
+++ modules/video_filter/opencv_example.cpp
@@ -39,8 +39,9 @@
 #include <vlc_image.h>
 #include "filter_event_info.h"
 
-#include <cxcore.h>
-#include <cv.h>
+#include <opencv2/core/core_c.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
 
 /*****************************************************************************
  * filter_sys_t : filter descriptor
