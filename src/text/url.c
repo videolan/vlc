85b213028c1f04d831422381d1ac523316102843
Rémi Denis-Courmont <remi@remlab.net>
2012-08-20 23:06:18 +0300

    vlc_UrlParse: rewrite, de-inline and simplify

--- src/text/url.c
+++ src/text/url.c
@@ -369,3 +369,116 @@ out:
     free (path);
     return ret; /* unknown scheme */
 }
+
+/**
+ * Splits an URL into parts.
+ * \param url structure of URL parts [OUT]
+ * \param str nul-terminated URL string to split
+ * \param opt if non-zero, character separating paths from options,
+ *            normally the question mark
+ * \note Use vlc_UrlClean() to free associated resources
+ * \bug Errors cannot be detected.
+ * \return nothing
+ */
+void vlc_UrlParse (vlc_url_t *restrict url, const char *str, unsigned char opt)
+{
+    url->psz_protocol = NULL;
+    url->psz_username = NULL;
+    url->psz_password = NULL;
+    url->psz_host = NULL;
+    url->i_port = 0;
+    url->psz_path = NULL;
+    url->psz_option = NULL;
+    url->psz_buffer = NULL;
+
+    if (str == NULL)
+        return;
+
+    char *buf = strdup (str);
+    if (unlikely(buf == NULL))
+        abort ();
+    url->psz_buffer = buf;
+
+    char *cur = buf, *next;
+
+    /* URL scheme */
+    next = strchr (cur, ':');
+    /* This is not strictly correct. In principles, the scheme is always
+     * present in an absolute URL and followed by a colon. Depending on the
+     * URL scheme, the two subsequent slashes are not required.
+     * VLC uses a different scheme for historical compatibility reasons - the
+     * scheme is often implicit. */
+    if (next != NULL && !strncmp (next + 1, "//", 2))
+    {
+        *next = '\0';
+        next += 3;
+        url->psz_protocol = cur;
+        cur = next;
+    }
+
+    /* Path */
+    next = strchr (cur, '/');
+    if (next != NULL)
+    {
+        *next = '\0'; /* temporary nul, reset to slash later */
+        url->psz_path = next;
+        if (opt && (next = strchr (next, opt)) != NULL)
+        {
+            *(next++) = '\0';
+            url->psz_option = next;
+        }
+    }
+    /*else
+        url->psz_path = "/";*/
+
+    /* User name */
+    next = strchr (cur, '@');
+    if (next != NULL)
+    {
+        *(next++) = '\0';
+        url->psz_username = cur;
+        cur = next;
+
+        /* Password (obsolete) */
+        next = strchr (url->psz_username, ':');
+        if (next != NULL)
+        {
+            *(next++) = '\0';
+            url->psz_password = next;
+            decode_URI (url->psz_password);
+        }
+        decode_URI (url->psz_username);
+    }
+
+    /* Host name */
+    if (*cur == '[' && (next = strstr (cur, "]:")) != NULL)
+    {
+        /* IPv6 numeral within brackets */
+        *(next++) = '\0';
+        url->psz_host = strdup (cur + 1);
+    }
+    else
+    {
+        url->psz_host = strdup (cur);
+        next = strchr (cur, ':');
+    }
+
+    /* Port number */
+    if (next != NULL)
+    {
+        assert (*next == ':');
+        url->i_port = atoi (next + 1);
+    }
+
+    if (url->psz_path != NULL)
+        *url->psz_path = '/'; /* restore leading slash */
+}
+
+/**
+ * Releases resources allocated by vlc_UrlParse().
+ */
+void vlc_UrlClean (vlc_url_t *restrict url)
+{
+    free (url->psz_host);
+    free (url->psz_buffer);
+}
