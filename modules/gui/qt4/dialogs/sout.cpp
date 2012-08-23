94238454983d30f0713da88f65ac23302ffae0a9
Rémi Denis-Courmont <remi@remlab.net>
2012-08-23 17:20:39 +0300

    Qt4: remove non-working stream output wizard TTL field (refs #4322)
    In fact, the defaut values in the preferences are better than the
    default value in the wizard. If the user really needs to change the
    default for IPv4 multicast, (s)he can always save it in the
    preferences more permantly.

--- modules/gui/qt4/dialogs/sout.cpp
+++ modules/gui/qt4/dialogs/sout.cpp
@@ -84,7 +84,7 @@ SoutDialog::SoutDialog( QWidget *parent, intf_thread_t *_p_intf, const QString& 
 #define CC( x ) CONNECT( ui.x, currentIndexChanged( int ), this, updateMRL() );
 
     /* Misc */
-    CB( soutAll );  CS( ttl ); CT( sapName ); CT( sapGroup );
+    CB( soutAll );  CT( sapName ); CT( sapGroup );
     CB( localOutput ); CB( transcodeBox );
     CONNECT( ui.profileSelect, optionsChanged(), this, updateMRL() );
 
@@ -245,7 +245,6 @@ void SoutDialog::updateMRL()
 
     if( ui.soutAll->isChecked() ) mrl.append( " :sout-all" );
 
-    mrl.append( " :ttl=" + QString::number( ui.ttl->value() ) );
     mrl.append( " :sout-keep" );
 
     ui.mrlEdit->setPlainText( mrl );
