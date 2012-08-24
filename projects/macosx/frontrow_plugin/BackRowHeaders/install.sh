f76f40b29f21c407d9694afc50e86061a31a3643
Jean-Baptiste Kempf <jb@videolan.org>
2012-08-24 12:37:48 +0200

    Remove frontrow plugin

--- projects/macosx/frontrow_plugin/BackRowHeaders/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if test ! -e $PWD/Headers/BREvent.h; then
-    echo "Please run this script from BackRowHeaders directory"
-    exit 1
-else
-    sudo ln -sf $PWD/Headers /System/Library/PrivateFrameworks/BackRow.framework
-fi
