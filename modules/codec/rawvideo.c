d56415db77f354bcac401e795408404b2958f8b8
Jean-Baptiste Kempf <jb@videolan.org>
2012-08-26 17:27:00 +0200

    Support J440 and I440 in raw video codec
    This happens in some jpeg files

--- modules/codec/rawvideo.c
+++ modules/codec/rawvideo.c
@@ -94,6 +94,8 @@ static int OpenDecoder( vlc_object_t *p_this )
         /* Planar YUV */
         case VLC_CODEC_I444:
         case VLC_CODEC_J444:
+        case VLC_CODEC_I440:
+        case VLC_CODEC_J440:
         case VLC_CODEC_I422:
         case VLC_CODEC_J422:
         case VLC_CODEC_I420:
