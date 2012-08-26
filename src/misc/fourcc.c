8206f9ff4a9916e12d8ac1e69a9ac4e65f153229
Jean-Baptiste Kempf <jb@videolan.org>
2012-08-26 17:33:55 +0200

    Add a FourCC for vorbis
    Close #7389

--- src/misc/fourcc.c
+++ src/misc/fourcc.c
@@ -1201,6 +1201,7 @@ static const entry_t p_list_audio[] = {
 
     B(VLC_CODEC_VORBIS, "Vorbis Audio"),
         A("vorb"),
+        A("vor1"),
 
     B(VLC_CODEC_OPUS, "Opus Audio"),
         A("Opus"),
