4e2ad6dffd3191c9cecacb1dfe3c2d35b58b4d17
Rafaël Carré <funman@videolan.org>
2012-09-05 01:59:27 +0200

    videodev2.h: fixes for non Linux builds
    Signed-off-by: Rafaël Carré <funman@videolan.org>

--- modules/access/v4l2/videodev2.h
+++ modules/access/v4l2/videodev2.h
@@ -58,8 +58,42 @@
 
 #include <sys/time.h>
 
+#ifdef __linux__
 #include <linux/ioctl.h>
 #include <linux/types.h>
+#else
+#include <sys/ioctl.h>
+#ifndef HAVE_LINUX_INTEGER_TYPES
+#ifndef __u64
+typedef uint64_t __u64;
+#endif
+#ifndef __le32
+typedef uint32_t __le32;
+#endif
+#ifndef __u32
+typedef uint32_t __u32;
+#endif
+#ifndef __u16
+typedef uint16_t __u16;
+#endif
+#ifndef __u8
+typedef uint8_t __u8;
+#endif
+
+#ifndef __s64
+typedef int64_t __s64;
+#endif
+#ifndef __s32
+typedef int32_t __s32;
+#endif
+#ifndef __s16
+typedef int16_t __s16;
+#endif
+#ifndef __s8
+typedef int8_t __s8;
+#endif
+#endif
+#endif
 
 /*
  * Common stuff for both V4L1 and V4L2
