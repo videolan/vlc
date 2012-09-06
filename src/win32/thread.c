397b9e094c6e1cc175c3cb91cf5f8118c951c06a
Rémi Denis-Courmont <remi@remlab.net>
2012-09-07 00:10:16 +0300

    win32: cosmetic

--- src/win32/thread.c
+++ src/win32/thread.c
@@ -590,8 +590,7 @@ void vlc_join (vlc_thread_t th, void **result)
 {
     do
         vlc_testcancel ();
-    while (vlc_WaitForSingleObject (th->id, INFINITE)
-                                                        == WAIT_IO_COMPLETION);
+    while (vlc_WaitForSingleObject (th->id, INFINITE) == WAIT_IO_COMPLETION);
 
     if (result != NULL)
         *result = th->data;
