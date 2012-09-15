8d59ba01d33c96d61db5e0e7ab9b8a30b052ebab
Rémi Denis-Courmont <remi@remlab.net>
2012-09-15 21:33:05 +0300

    avcodec: disable threading by default
    Threading does not currently work with some codec profiles and is thus
    not suited as default. Threading is also known to deadlock in certain
    circumstances, such as if no data is ever received (a rather common
    scenario with network or DVB reception).
    
    Fixed avcodec version should be checked before this is re-enabled.
    
    Acked-by: Ilkka Ollakka <ileoo@videolan.org>

--- modules/codec/avcodec/avcodec.c
+++ modules/codec/avcodec/avcodec.c
@@ -159,7 +159,7 @@ vlc_module_begin ()
 #endif
 #if defined(FF_THREAD_FRAME)
     add_obsolete_integer( "ffmpeg-threads" ) /* removed since 2.1.0 */
-    add_integer( "avcodec-threads", 0, THREADS_TEXT, THREADS_LONGTEXT, true );
+    add_integer( "avcodec-threads", 1, THREADS_TEXT, THREADS_LONGTEXT, true );
 #endif
 
 
