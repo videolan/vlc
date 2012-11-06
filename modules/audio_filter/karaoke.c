b2e76597910ba6f1d2841b792bbcbc410d990a7a
Rémi Denis-Courmont <remi@remlab.net>
2012-11-06 19:37:05 +0200

    karaoke: change format when needed instead of failing

--- modules/audio_filter/karaoke.c
+++ modules/audio_filter/karaoke.c
@@ -47,16 +47,14 @@ static int Open (vlc_object_t *obj)
 {
     filter_t *filter = (filter_t *)obj;
 
-    if (filter->fmt_in.audio.i_format != VLC_CODEC_FL32
-     || !AOUT_FMTS_IDENTICAL(&filter->fmt_in.audio, &filter->fmt_out.audio))
-        return VLC_EGENERIC;
-
     if (filter->fmt_in.audio.i_channels != 2)
     {
         msg_Err (filter, "voice removal requires stereo");
         return VLC_EGENERIC;
     }
 
+    filter->fmt_in.audio.i_format = VLC_CODEC_FL32;
+    filter->fmt_out.audio = filter->fmt_in.audio;
     filter->pf_audio_filter = Process;
     return VLC_SUCCESS;
 }
