2640a57cad6773ad5609d020d01f86f57e2d4464
Jean-Baptiste Kempf <jb@videolan.org>
2012-11-12 18:14:49 +0100

    DTS: fix Win32 compilation

--- modules/codec/dts_header.c
+++ modules/codec/dts_header.c
@@ -22,6 +22,10 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
 #include <vlc_common.h>
 #include <vlc_bits.h>
 
