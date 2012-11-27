dcd19cba03bb9c0c37281dffde7a74b5ede3ef65
Rémi Denis-Courmont <remi@remlab.net>
2012-11-27 19:50:01 +0200

    LIRC: keep same pollfd
    There is no need to reinitialize the structure.

--- modules/control/lirc.c
+++ modules/control/lirc.c
@@ -159,16 +159,18 @@ static void *Run( void *data )
     intf_thread_t *p_intf = data;
     intf_sys_t *p_sys = p_intf->p_sys;
 
+    struct pollfd ufd;
+    ufd.fd = p_sys->i_fd;
+    ufd.events = POLLIN;
+
     for( ;; )
     {
         /* Wait for data */
-        struct pollfd ufd = { .fd = p_sys->i_fd, .events = POLLIN, .revents = 0 };
         if( poll( &ufd, 1, -1 ) == -1 )
         {
             if( errno == EINTR )
                 continue;
-            else
-                break;
+            break;
         }
 
         /* Process */
