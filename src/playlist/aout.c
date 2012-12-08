3de8fd13f26610b4f5ae4d87cb4e5653b4498c28
Rémi Denis-Courmont <remi@remlab.net>
2012-12-08 23:58:38 +0200

    Revert "input: create an aout if UI wants to configure audio"
    Due to mismatched "resource" and "hold" locks, this introduced a race
    where more than one audio output gets created.
    
    This reverts commit 4e57bd052e25c0538a6b1d7e83b8370910ef71dd.

--- src/playlist/aout.c
+++ src/playlist/aout.c
@@ -33,6 +33,10 @@
 
 audio_output_t *playlist_GetAout(playlist_t *pl)
 {
+    /* NOTE: it is assumed that the input resource exists. In practice,
+     * the playlist must have been activated. This is automatic when calling
+     * pl_Get(). FIXME: input resources are deleted at deactivation, this can
+     * be too early. */
     playlist_private_t *sys = pl_priv(pl);
     return input_resource_HoldAout(sys->p_input_resource);
 }
