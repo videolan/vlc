ba15fcb6e61aee6e0d8c8cbcd61ebf5db479811c
Jean-Baptiste Kempf <jb@videolan.org>
2013-01-14 18:15:42 +0100

    CDROM: use CreateFileA for opening drive letters
    If I understood correctly, drive letters are always ANSI

--- modules/access/vcd/cdrom.c
+++ modules/access/vcd/cdrom.c
@@ -1016,7 +1016,7 @@ static int win32_vcd_open( vlc_object_t * p_this, const char *psz_dev,
 
     sprintf( psz_win32_drive, "\\\\.\\%c:", psz_dev[0] );
 
-    p_vcddev->h_device_handle = CreateFile( psz_win32_drive, GENERIC_READ,
+    p_vcddev->h_device_handle = CreateFileA( psz_win32_drive, GENERIC_READ,
                                             FILE_SHARE_READ | FILE_SHARE_WRITE,
                                             NULL, OPEN_EXISTING,
                                             FILE_FLAG_NO_BUFFERING |
