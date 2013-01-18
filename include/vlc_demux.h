b1c7cb9b29985671ef36f5be2d418390a56af477
Rémi Denis-Courmont <remi@remlab.net>
2013-01-19 00:20:05 +0200

    DEMUX_SET_NEXT_DEMUX_TIME: fix documentation

--- include/vlc_demux.h
+++ include/vlc_demux.h
@@ -121,7 +121,7 @@ enum demux_query_e
      * XXX: not mandatory (except for subtitle demux) but will help a lot
      * for multi-input
      */
-    DEMUX_SET_NEXT_DEMUX_TIME,  /* arg1= int64_t *      can fail */
+    DEMUX_SET_NEXT_DEMUX_TIME,  /* arg1= int64_t        can fail */
     /* FPS for correct subtitles handling */
     DEMUX_GET_FPS,              /* arg1= double *       res=can fail    */
 
