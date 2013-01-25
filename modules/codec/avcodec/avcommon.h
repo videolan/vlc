a92d777d0b430070ebc6aff422aec622323a59bf
Rafaël Carré <funman@videolan.org>
2013-01-25 07:08:11 +0100

    Add LIBAVUTIL_VERSION_CHECK (based on LIBAVCODEC_VERSION_CHECK)

--- modules/codec/avcodec/avcommon.h
+++ modules/codec/avcodec/avcommon.h
@@ -31,6 +31,15 @@
 
 #include <limits.h>
 
+/* LIBAVUTIL_VERSION_CHECK checks for the right version of libav and FFmpeg
+ * a is the major version
+ * b and c the minor and micro versions of libav
+ * d and e the minor and micro versions of FFmpeg */
+#define LIBAVUTIL_VERSION_CHECK( a, b, c, d, e ) \
+    (LIBAVUTIL_VERSION_MICRO <  100 && LIBAVUTIL_VERSION_INT >= AV_VERSION_INT( a, b, c ) ) || \
+    (LIBAVUTIL_VERSION_MICRO >= 100 && LIBAVUTIL_VERSION_INT >= AV_VERSION_INT( a, d, e ) )
+
+
 unsigned GetVlcDspMask( void );
 
 #ifdef HAVE_LIBAVFORMAT_AVFORMAT_H
