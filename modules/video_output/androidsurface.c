eeb932db8424f35c11c40d8c0db3a11347f631b7
Rafaël Carré <funman@videolan.org>
2013-02-12 08:13:09 +0100

    androidsurface: explain how how picture is rendered

--- modules/video_output/androidsurface.c
+++ modules/video_output/androidsurface.c
@@ -382,10 +382,11 @@ static void AndroidUnlockSurface(picture_t *picture)
 
 static void Display(vout_display_t *vd, picture_t *picture, subpicture_t *subpicture)
 {
-    /* FIXME ? */
-
     VLC_UNUSED(vd);
     VLC_UNUSED(subpicture);
+
+    /* refcount lowers to 0, and pool_cfg.unlock is called */
+
     picture_Release(picture);
 }
 
