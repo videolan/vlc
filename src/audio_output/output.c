665998149a64fc01d1fe960d6578b47684f1585c
Rémi Denis-Courmont <remi@remlab.net>
2013-02-28 18:31:28 +0200

    aout: fix mutex leak

--- src/audio_output/output.c
+++ src/audio_output/output.c
@@ -277,6 +277,8 @@ static void aout_Destructor (vlc_object_t *obj)
     audio_output_t *aout = (audio_output_t *)obj;
     aout_owner_t *owner = aout_owner (aout);
 
+    assert (owner->req.device == unset_str);
+    vlc_mutex_destroy (&owner->req.lock);
     vlc_mutex_destroy (&owner->lock);
 }
 
