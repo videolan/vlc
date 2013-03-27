79d91b369223d02bb197884dc3c0971c0b393e35
Jean-Baptiste Kempf <jb@videolan.org>
2013-03-27 14:56:13 +0100

    Avcodec: Fix double FPS display
    Close #5671
    Patch by "rycius" on trac

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -172,7 +172,7 @@ static inline picture_t *ffmpeg_NewPictBuf( decoder_t *p_dec,
     else if( p_context->time_base.num > 0 && p_context->time_base.den > 0 )
     {
         p_dec->fmt_out.video.i_frame_rate = p_context->time_base.den;
-        p_dec->fmt_out.video.i_frame_rate_base = p_context->time_base.num;
+        p_dec->fmt_out.video.i_frame_rate_base = p_context->time_base.num * __MAX( p_context->ticks_per_frame, 1 );
     }
 
     return decoder_NewPicture( p_dec );
