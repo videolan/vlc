f1a9cf167526cb33f5ae5d90030fbe265487d281
Francois Cartegnie <fcvlcdev@free.fr>
2013-03-28 15:08:21 +0100

    access: rdp: relicence.
    Relicencing by author as it is still not altered.

--- modules/access/rdp.c
+++ modules/access/rdp.c
@@ -6,7 +6,7 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
