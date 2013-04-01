videolan/vlc

commit ff74e967e9777ed821734077051f55ac84a173a5

skins2: resize new layout first when switching between layouts.
When switching layouts the windows was resized to the default state at design time.
Now the layout being switched to is first resized to the size of the current layout.
Switching layouts now nolonger results in the layout being restored to its default size,
instead it keeps the size the user inteded it to have.

modules/gui/skins2/src/top_window.cpp
@@ -358,6 +358,7 @@ void TopWindow::setActiveLayout( GenericLayout *pLayout )
         }
         // The current layout becomes inactive
         m_pActiveLayout->getActiveVar().set( false );
+        pLayout->resize( m_pActiveLayout->getWidth(), m_pActiveLayout->getHeight() );
     }
 
     pLayout->setWindow( this );

commit fe29bc614b315398b69ab735f01b54d1f669fa42

skins2: resize only similar layouts on-the-fly
This fixes a regression brought by ff74e967e9777ed821734077051f55ac84a173a5

Forcefully resizing only makes sense when layouts were designed to be similar
in size in the first place. Furthermore, it cuts off a larger layout if a
smaller one is first defined when loading a skin.

Ideally, the skins2 dtd should be extended for skins developers to explicitly
express whether two layouts are tightly-coupled or not, i.e should always be
kept the same size whatever the subsequent resizing performed by the user.

modules/gui/skins2/src/top_window.cpp
@@ -351,7 +351,13 @@ void TopWindow::setActiveLayout( GenericLayout *pLayout )
         }
         // The current layout becomes inactive
         m_pActiveLayout->getActiveVar().set( false );
-        pLayout->resize( m_pActiveLayout->getWidth(), m_pActiveLayout->getHeight() );
+
+        // if both layouts have the same original size, infer a
+        // subsequent resize of the active layout has to be applied
+        // to the new layout about to become active
+        if( pLayout->isTightlyCoupledWith( *m_pActiveLayout ) )
+            pLayout->resize( m_pActiveLayout->getWidth(),
+                             m_pActiveLayout->getHeight() );
     }
 
     pLayout->setWindow( this );