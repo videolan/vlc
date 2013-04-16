005aff8b753064933ce6d2dd2862324e5056f165
Jean-Baptiste Kempf <jb@videolan.org>
2013-04-16 14:47:20 +0200

    AVI: free p_sys resources on Close (WTF?)
    Close #8448

--- modules/mux/avi.c
+++ modules/mux/avi.c
@@ -222,6 +222,16 @@ static void Close( vlc_object_t * p_this )
     p_hdr = avi_HeaderCreateRIFF( p_mux );
     sout_AccessOutSeek( p_mux->p_access, 0 );
     sout_AccessOutWrite( p_mux->p_access, p_hdr );
+
+    for( i_stream = 0; i_stream < p_sys->i_streams; i_stream++ )
+    {
+        avi_stream_t *p_stream;
+        p_stream = &p_sys->stream[i_stream];
+        free( p_stream->p_bih );
+        free( p_stream->p_wf );
+    }
+    free( p_sys->idx1.entry );
+    free( p_sys );
 }
 
 static int Control( sout_mux_t *p_mux, int i_query, va_list args )
