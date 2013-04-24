1632c6dd8d7ecb41e0e9f77854481b2dc1dc49f4
Rémi Denis-Courmont <remi@remlab.net>
2013-04-24 19:08:37 +0300

    objects: fix NDEBUG-only warning

--- src/misc/objects.c
+++ src/misc/objects.c
@@ -463,9 +463,12 @@ vlc_object_t *vlc_object_find_name( vlc_object_t *p_this, const char *psz_name )
 void * vlc_object_hold( vlc_object_t *p_this )
 {
     vlc_object_internals_t *internals = vlc_internals( p_this );
+#ifndef NDEBUG
     unsigned refs = atomic_fetch_add (&internals->refs, 1);
-
-    assert (refs > 0); /* Avoid obvious freed object uses */ (void) refs;
+    assert (refs > 0); /* Avoid obvious freed object uses */
+#else
+    atomic_fetch_add (&internals->refs, 1);
+#endif
     return p_this;
 }
 
