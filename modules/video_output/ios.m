f9ec354337a20576c352b5038b7ceb17d02a942c
Jean-Baptiste Kempf <jb@videolan.org>
2013-05-18 14:54:51 +0200

    Reduce iOS.m priority

--- modules/video_output/ios.m
+++ modules/video_output/ios.m
@@ -66,7 +66,7 @@ vlc_module_begin ()
     set_description( N_("iOS OpenGL ES video output (requires UIView)"))
     set_category(CAT_VIDEO)
     set_subcategory(SUBCAT_VIDEO_VOUT )
-    set_capability("vout display", 300)
+    set_capability("vout display", 210)
     set_callbacks(Open, Close)
 
     add_shortcut("ios", "vout_ios")
