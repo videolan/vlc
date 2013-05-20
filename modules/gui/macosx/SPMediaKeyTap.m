4eaf633347e554cb83121b06623dbcba1ea36c3f
Felix Paul Kühne <fkuehne@videolan.org>
2013-05-20 19:34:04 +0200

    macosx/SPMediaKeyTap: update list of collaborating apps (close #8644)

--- modules/gui/macosx/SPMediaKeyTap.m
+++ modules/gui/macosx/SPMediaKeyTap.m
@@ -148,9 +148,15 @@ static CGEventRef tapEventCallback(CGEventTapProxy proxy, CGEventType type, CGEv
              @"com.bitcartel.pandorajam",
              @"org.clementine-player.clementine",
              @"fm.last.Last.fm",
+             @"fm.last.Scrobbler",
              @"com.beatport.BeatportPro",
              @"com.Timenut.SongKey",
              @"com.macromedia.fireworks", // the tap messes up their mouse input
+             @"at.justp.Theremin",
+             @"ru.ya.themblsha.YandexMusic",
+             @"com.jriver.MediaCenter18",
+             @"com.jriver.MediaCenter19",
+             @"com.jriver.MediaCenter20",
     ];
 }
 
