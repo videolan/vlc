b50c7652c71962d64df75ec60a901131e577fdc7
Takahito HIRANO <hiranotaka@zng.info>
2013-06-10 19:36:23 +0300

    dtv: fix ISDB-S tuning
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/access/dtv/linux.c
+++ modules/access/dtv/linux.c
@@ -1035,7 +1035,7 @@ int dvb_set_isdbs (dvb_device_t *d, uint64_t freq_Hz, uint16_t ts_id)
 
     if (dvb_find_frontend (d, ISDB_S))
         return -1;
-    return dvb_set_props (d, 5, DTV_CLEAR, 0, DTV_DELIVERY_SYSTEM, SYS_ISDBS,
+    return dvb_set_props (d, 4, DTV_CLEAR, 0, DTV_DELIVERY_SYSTEM, SYS_ISDBS,
                           DTV_FREQUENCY, freq,
 #if DVBv5(8)
                           DTV_STREAM_ID,
