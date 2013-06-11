76d20512abbc6d7498bc4e49153024993978ed55
Rémi Denis-Courmont <remi@remlab.net>
2013-06-11 19:42:17 +0300

    avcodec: adjust MT contention scope

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -1023,6 +1023,7 @@ static int ffmpeg_GetFrameBuf( struct AVCodecContext *p_context,
     }
 
     p_context->draw_horiz_band = NULL;
+    post_mt( p_sys );
 
     p_ff_pic->opaque = (void*)p_pic;
     p_ff_pic->type = FF_BUFFER_TYPE_USER;
@@ -1036,7 +1037,6 @@ static int ffmpeg_GetFrameBuf( struct AVCodecContext *p_context,
     p_ff_pic->linesize[2] = p_pic->p[2].i_pitch;
     p_ff_pic->linesize[3] = 0;
 
-    post_mt( p_sys );
     return 0;
 }
 
