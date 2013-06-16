5730213041c854d77818f04a6b39b1e118c8d2a8
Rémi Denis-Courmont <remi@remlab.net>
2013-06-16 17:39:48 +0300

    Lua: intf: fix cancellation handling
    The target thread has no defined cancellation points, so simply remove
    the vlc_cancel() call. (Adding vlc_savecancel() would work to the same
    effects, but with extra code.)

--- modules/lua/intf.c
+++ modules/lua/intf.c
@@ -381,7 +381,6 @@ void Close_LuaIntf( vlc_object_t *p_this )
     intf_thread_t *p_intf = (intf_thread_t*)p_this;
     intf_sys_t *p_sys = p_intf->p_sys;
 
-    vlc_cancel( p_sys->thread );
     vlc_join( p_sys->thread, NULL );
     lua_close( p_sys->L );
 
