0a423efa0b464361250ddb66725e26ef5155e1b6
Ilkka Ollakka <ileoo@videolan.org>
2013-06-19 11:09:12 +0300

    httplive: turn STREAM_CAN_CONTROL_PACE on to restore playback
    This is just quick fix and workaround for now.
    
    Should Fix #8619 and #8713

--- modules/stream_filter/httplive.c
+++ modules/stream_filter/httplive.c
@@ -2514,9 +2514,11 @@ static int Control(stream_t *s, int i_query, va_list args)
         case STREAM_CAN_SEEK:
             *(va_arg (args, bool *)) = hls_MaySeek(s);
             break;
+        case STREAM_CAN_CONTROL_PACE:
+            *(va_arg (args, bool *)) = true;
+            break;
         case STREAM_CAN_FASTSEEK:
         case STREAM_CAN_PAUSE: /* TODO */
-        case STREAM_CAN_CONTROL_PACE:
             *(va_arg (args, bool *)) = false;
             break;
         case STREAM_GET_POSITION:
