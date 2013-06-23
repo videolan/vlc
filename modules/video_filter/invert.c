908a51341bfa366f5ec7b650d32e423ae38ac5f0
Rémi Denis-Courmont <remi@remlab.net>
2013-06-23 08:51:03 +0300

    invert: check supported chroma (fixes #8840, fixes #8841)

--- modules/video_filter/invert.c
+++ modules/video_filter/invert.c
@@ -64,9 +64,19 @@ vlc_module_end ()
 static int Create( vlc_object_t *p_this )
 {
     filter_t *p_filter = (filter_t *)p_this;
+    vlc_fourcc_t fourcc = p_filter->fmt_in.video.i_chroma;
+
+    if( fourcc == VLC_CODEC_YUVP || fourcc == VLC_CODEC_RGBP
+     || fourcc == VLC_CODEC_RGBA )
+        return VLC_EGENERIC;
+
+    const vlc_chroma_description_t *p_chroma =
+        vlc_fourcc_GetChromaDescription( fourcc );
+    if( p_chroma == NULL
+     || p_chroma->pixel_size * 8 != p_chroma->pixel_bits )
+        return VLC_EGENERIC;
 
     p_filter->pf_video_filter = Filter;
-
     return VLC_SUCCESS;
 }
 
