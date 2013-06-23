58599d6f690d43f3f636cf963c84efaa9fe0575b
Rémi Denis-Courmont <remi@remlab.net>
2013-06-23 09:13:02 +0300

    ripple: reject 0 planes formats

--- modules/video_filter/ripple.c
+++ modules/video_filter/ripple.c
@@ -80,6 +80,11 @@ static int Create( vlc_object_t *p_this )
 {
     filter_t *p_filter = (filter_t *)p_this;
 
+    const vlc_chroma_description_t *p_chroma =
+        vlc_fourcc_GetChromaDescription( p_filter->fmt_in.video.i_chroma );
+    if( p_chroma == NULL || p_chroma->plane_count == 0 )
+        return VLC_EGENERIC;
+
     /* Allocate structure */
     p_filter->p_sys = malloc( sizeof( filter_sys_t ) );
     if( p_filter->p_sys == NULL )
