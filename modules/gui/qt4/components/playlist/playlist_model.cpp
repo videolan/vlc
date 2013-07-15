a6020fcc9ab2781daf1727aa220ca9f8215388fc
Ludovic Fauvet <etix@videolan.org>
2013-07-15 11:38:11 +0200

    Qt: use explicit constructor for the xpm

--- modules/gui/qt4/components/playlist/playlist_model.cpp
+++ modules/gui/qt4/components/playlist/playlist_model.cpp
@@ -64,7 +64,7 @@ PLModel::PLModel( playlist_t *_p_playlist,  /* THEPL */
 
     /* Icons initialization */
 #define ADD_ICON(type, x) icons[ITEM_TYPE_##type] = QIcon( x )
-    ADD_ICON( UNKNOWN , type_unknown_xpm );
+    ADD_ICON( UNKNOWN , QPixmap( type_unknown_xpm ) );
     ADD_ICON( FILE, ":/type/file" );
     ADD_ICON( DIRECTORY, ":/type/directory" );
     ADD_ICON( DISC, ":/type/disc" );
