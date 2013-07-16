9a74aaad855d14a2e82d8599624280c12dcce5d5
Alain Degreffe <eczema@ecze.com>
2013-07-16 08:38:29 +0200

    var_buffer_getmemory: invalid memcpy source pointer
    Signed-off-by: Rafaël Carré <funman@videolan.org>

--- modules/access/mms/buffer.c
+++ modules/access/mms/buffer.c
@@ -198,7 +198,7 @@ int var_buffer_getmemory ( var_buffer_t *p_buf, void *p_mem, int64_t i_mem )
     i_copy = __MIN( i_mem, p_buf->i_size - p_buf->i_data );
     if( i_copy > 0 && p_mem != NULL)
     {
-        memcpy( p_mem, p_buf + p_buf->i_data, i_copy );
+        memcpy( p_mem, p_buf->p_data + p_buf->i_data , i_copy );
     }
     if( i_copy < 0 )
     {
