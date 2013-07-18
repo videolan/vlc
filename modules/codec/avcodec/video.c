videolan/vlc

commit b71c85b3d88b8d0ad2d4a63bf58ebcd2ad771cbf

fix video playback when width and height differ from coded_width and coded_height
Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

modules/codec/avcodec/video.c
@@ -125,8 +125,14 @@ static inline picture_t *ffmpeg_NewPictBuf( decoder_t *p_dec,
 {
     decoder_sys_t *p_sys = p_dec->p_sys;
 
-    p_dec->fmt_out.video.i_width = p_context->width;
-    p_dec->fmt_out.video.i_height = p_context->height;
+    if( p_context->coded_width != p_context->width ||
+        p_context->coded_height != p_context->height )
+    {
+        p_dec->fmt_out.video.i_visible_width = p_context->width;
+        p_dec->fmt_out.video.i_visible_height = p_context->height;
+    }
+    p_dec->fmt_out.video.i_width = p_context->coded_width;
+    p_dec->fmt_out.video.i_height = p_context->coded_height;
 
     if( !p_context->width || !p_context->height )
     {

commit 01c4f44e88bfbb8ecf108c0fe40f3797bddac686

avcodec: pass coded dimensions to hardware decoder
Regression from b71c85b3d88b8d0ad2d4a63bf58ebcd2ad771cbf.

modules/codec/avcodec/video.c
@@ -907,7 +907,7 @@ static int ffmpeg_va_GetFrameBuf( struct AVCodecContext *p_context, AVFrame *p_f
     /* hwaccel_context is not present in old ffmpeg version */
     if( vlc_va_Setup( p_va,
                 &p_context->hwaccel_context, &p_dec->fmt_out.video.i_chroma,
-                p_context->width, p_context->height ) )
+                p_context->coded_width, p_context->coded_height ) )
     {
         msg_Err( p_dec, "vlc_va_Setup failed" );
         return -1;