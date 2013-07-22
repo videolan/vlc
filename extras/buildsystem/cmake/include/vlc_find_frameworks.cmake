4b3bc75d739138aa41c76ad8fdb2c472f3c0f926
Rémi Denis-Courmont <remi@remlab.net>
2013-07-22 19:41:04 +0300

    Remove unmaintained cmake support

--- extras/buildsystem/cmake/include/vlc_find_frameworks.cmake
+++
@@ -1,7 +0,0 @@
-include(CMakeFindFrameworks)
-
-MACRO(vlc_find_frameworks frameworks)
-    foreach(framework ${ARGV})
-        cmake_find_frameworks(${framework})
-    endforeach(framework)
-ENDMACRO(vlc_find_frameworks)
