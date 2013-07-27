ed7b81cbb825a1f94ff2c2caa8491b98e5744508
Wills Wang <wills.wang@wareran.com>
2013-07-27 22:30:03 +0300

    avformat: set pixel aspect ratio
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/demux/avformat/demux.c
+++ modules/demux/avformat/demux.c
@@ -337,6 +337,8 @@ int OpenDemux( vlc_object_t *p_this )
             psz_type = "video";
             fmt.video.i_frame_rate = cc->time_base.den;
             fmt.video.i_frame_rate_base = cc->time_base.num * __MAX( cc->ticks_per_frame, 1 );
+            fmt.video.i_sar_num = s->sample_aspect_ratio.num;
+            fmt.video.i_sar_den = s->sample_aspect_ratio.den;
             break;
 
         case AVMEDIA_TYPE_SUBTITLE:
