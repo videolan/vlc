62605f279bd632a09bdc037feceb65f62b236d89
Pierre Ynard <linkfanel@yahoo.fr>
2013-08-05 03:12:14 +0200

    youtube.lua: fix encoded characters in author name (API)
    With google accounts it's happening now

--- share/lua/playlist/youtube.lua
+++ share/lua/playlist/youtube.lua
@@ -269,7 +269,7 @@ function parse()
         local artist = string.match( line, "&author=([^&]*)" )
         if artist then
             artist = string.gsub( artist, "+", " " )
-            -- Not sure if there may be encoded characters to decode here
+            artist = vlc.strings.decode_uri( artist )
         end
         local arturl = string.match( line, "&thumbnail_url=([^&]*)" )
         if arturl then
