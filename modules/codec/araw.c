videolan/vlc

commit 46f782a9f775156c1f9e53a39632e28e9854c6ef

araw: zero copy for native audio sample formats

modules/codec/araw.c
@@ -297,10 +297,13 @@ static int DecoderOpen( vlc_object_t *p_this )
 static block_t *DecodeBlock( decoder_t *p_dec, block_t **pp_block )
 {
     decoder_sys_t *p_sys = p_dec->p_sys;
-
-    if( !pp_block || !*pp_block ) return NULL;
+    if( pp_block == NULL )
+        return NULL;
 
     block_t *p_block = *pp_block;
+    if( p_block == NULL )
+        return NULL;
+    *pp_block = NULL;
 
     if( p_block->i_pts > VLC_TS_INVALID &&
         p_block->i_pts != date_Get( &p_sys->end_date ) )
@@ -308,47 +311,37 @@ static block_t *DecodeBlock( decoder_t *p_dec, block_t **pp_block )
         date_Set( &p_sys->end_date, p_block->i_pts );
     }
     else if( !date_Get( &p_sys->end_date ) )
-    {
         /* We've just started the stream, wait for the first PTS. */
-        block_Release( p_block );
-        return NULL;
-    }
-
-    /* Don't re-use the same pts twice */
-    p_block->i_pts = VLC_TS_INVALID;
+        goto skip;
 
     unsigned samples = (8 * p_block->i_buffer) / p_sys->framebits;
     if( samples == 0 )
-    {
-        block_Release( p_block );
-        return NULL;
-    }
-
-    /* Create chunks of max 1024 samples */
-    if( samples > 1024 ) samples = 1024;
-
-    block_t *p_out = decoder_NewAudioBuffer( p_dec, samples );
-    if( p_out == NULL )
-    {
-        block_Release( p_block );
-        return NULL;
-    }
-
-    p_out->i_pts = date_Get( &p_sys->end_date );
-    p_out->i_length = date_Increment( &p_sys->end_date, samples )
-                      - p_out->i_pts;
+        goto skip;
 
     if( p_sys->decode != NULL )
+    {
+        block_t *p_out = decoder_NewAudioBuffer( p_dec, samples );
+        if( p_out == NULL )
+            goto skip;
+
         p_sys->decode( p_out->p_buffer, p_block->p_buffer,
                        samples * p_dec->fmt_in.audio.i_channels );
+        block_Release( p_block );
+        p_block = p_out;
+    }
     else
-        memcpy( p_out->p_buffer, p_block->p_buffer, p_out->i_buffer );
+    {
+        decoder_UpdateAudioFormat( p_dec );
+        p_block->i_buffer = samples * (p_sys->framebits / 8);
+    }
 
-    samples = (samples * p_sys->framebits) / 8;
-    p_block->p_buffer += samples;
-    p_block->i_buffer -= samples;
-
-    return p_out;
+    p_block->i_pts = date_Get( &p_sys->end_date );
+    p_block->i_length = date_Increment( &p_sys->end_date, samples )
+                      - p_block->i_pts;
+    return p_block;
+skip:
+    block_Release( p_block );
+    return NULL;
 }
 
 static void S8Decode( void *outp, const uint8_t *in, unsigned samples )

commit 47376e177707bfe585959911971de3e69fe841f4

araw: fix samples count on raw audio
(regression from 46f782a9f775156c1f9e53a39632e28e9854c6ef)

modules/codec/araw.c
@@ -332,6 +332,7 @@ static block_t *DecodeBlock( decoder_t *p_dec, block_t **pp_block )
     else
     {
         decoder_UpdateAudioFormat( p_dec );
+        p_block->i_nb_samples = samples;
         p_block->i_buffer = samples * (p_sys->framebits / 8);
     }
 