833ff513c35e39f29eb5c29e622c3fc74317f858
Ilkka Ollakka <ileoo@videolan.org>
2013-08-13 18:12:44 +0300

    marq: add file-option to options

--- modules/video_filter/marq.c
+++ modules/video_filter/marq.c
@@ -186,7 +186,7 @@ vlc_module_end ()
 
 static const char *const ppsz_filter_options[] = {
     "marquee", "x", "y", "position", "color", "size", "timeout", "refresh",
-    "opacity",
+    "opacity","file",
     NULL
 };
 
