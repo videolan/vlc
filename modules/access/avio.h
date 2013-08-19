28dcca75ff67757d6ee111075e24a63d16ed2c63
Jean-Baptiste Kempf <jb@videolan.org>
2013-08-19 15:24:01 +0200

    Name AVIO as AVIO not FFmpeg or libavformat

--- modules/access/avio.h
+++ modules/access/avio.h
@@ -30,22 +30,22 @@ int  OutOpenAvio (vlc_object_t *);
 void OutCloseAvio(vlc_object_t *);
 
 #define AVIO_MODULE \
-    set_shortname(N_("FFmpeg"))             \
-    set_description(N_("FFmpeg access") )   \
-    set_category(CAT_INPUT)                 \
-    set_subcategory(SUBCAT_INPUT_ACCESS)    \
-    set_capability("access", -1)            \
-    add_shortcut("avio", "rtmp", "rtmpe", "rtmps", "rtmpt", "rtmpte", "rtmpts") \
-    set_callbacks(OpenAvio, CloseAvio) \
-    set_section(N_("Input"), NULL ) \
+    set_shortname(N_("AVIO"))                                                    \
+    set_description(N_("libavformat AVIO access") )                              \
+    set_category(CAT_INPUT)                                                      \
+    set_subcategory(SUBCAT_INPUT_ACCESS)                                         \
+    set_capability("access", -1)                                                 \
+    add_shortcut("avio", "rtmp", "rtmpe", "rtmps", "rtmpt", "rtmpte", "rtmpts")  \
+    set_callbacks(OpenAvio, CloseAvio)                                           \
+    set_section(N_("Input"), NULL )                                              \
     add_string("avio-options", NULL, AV_OPTIONS_TEXT, AV_OPTIONS_LONGTEXT, true) \
-    add_submodule () \
-        set_shortname( "libavformat" ) \
-        set_description( N_("libavformat access output") ) \
-        set_capability( "sout access", -1 ) \
-        set_category( CAT_SOUT ) \
-        set_subcategory( SUBCAT_SOUT_ACO ) \
-        add_shortcut( "avio", "rtmp" ) \
-        set_callbacks( OutOpenAvio, OutCloseAvio) \
-        set_section(N_("Stream output"), NULL ) \
+    add_submodule ()                                                             \
+        set_shortname( "AVIO" )                                                  \
+        set_description( N_("libavformat AVIO access output") )                  \
+        set_capability( "sout access", -1 )                                      \
+        set_category( CAT_SOUT )                                                 \
+        set_subcategory( SUBCAT_SOUT_ACO )                                       \
+        add_shortcut( "avio", "rtmp" )                                           \
+        set_callbacks( OutOpenAvio, OutCloseAvio)                                \
+        set_section(N_("Stream output"), NULL )                                  \
         add_string("sout-avio-options", NULL, AV_OPTIONS_TEXT, AV_OPTIONS_LONGTEXT, true)
