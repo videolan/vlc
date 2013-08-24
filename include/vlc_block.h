6d0f45bb56f01cc1e72134bc9bdfe89fa9436860
Rémi Denis-Courmont <remi@remlab.net>
2013-08-24 13:04:26 +0300

    Export block_FifoWake() :-(
    This mostly reverts commit 8e3dd71128904eecf8439ef636bfdf5abadfaa1e.

--- include/vlc_block.h
+++ include/vlc_block.h
@@ -312,7 +312,7 @@ VLC_API void block_FifoRelease( block_fifo_t * );
 VLC_API void block_FifoPace( block_fifo_t *fifo, size_t max_depth, size_t max_size );
 VLC_API void block_FifoEmpty( block_fifo_t * );
 VLC_API size_t block_FifoPut( block_fifo_t *, block_t * );
-void block_FifoWake( block_fifo_t * );
+VLC_API void block_FifoWake( block_fifo_t * );
 VLC_API block_t * block_FifoGet( block_fifo_t * ) VLC_USED;
 VLC_API block_t * block_FifoShow( block_fifo_t * );
 size_t block_FifoSize( const block_fifo_t *p_fifo ) VLC_USED;
