834987a1fd817b00f88ef665e033264e2ea78e11
Ilkka Ollakka <ileoo@videolan.org>
2013-09-04 13:52:56 +0300

    livehttp: use playlist max duration instead segment max duration to calculate if we can remove segment in live case

--- modules/access_output/livehttp.c
+++ modules/access_output/livehttp.c
@@ -513,13 +513,13 @@ static uint32_t segmentAmountNeeded( sout_access_out_sys_t *p_sys )
 /************************************************************************
  * isFirstItemRemovable: Check for draft 11 section 6.2.2 
  * check that the first item has been around outside playlist 
- * segment->f_seglength + p_sys->i_seglen before it is removed.
+ * segment->f_seglength + (p_sys->i_numsegs * p_sys->i_seglen) before it is removed.
  ************************************************************************/
 static bool isFirstItemRemovable( sout_access_out_sys_t *p_sys, uint32_t i_firstseg, uint32_t i_index_offset )
 {
     float duration = .0f;
 
-    /* Check that segment has been out of playlist for seglenght + p_sys->i_seglen amount
+    /* Check that segment has been out of playlist for seglenght + (p_sys->i_numsegs * p_sys->i_seglen) amount
      * We check this by calculating duration of the items that replaced first item in playlist
      */
     for(int index=0; index < i_index_offset; index++ )
@@ -529,7 +529,7 @@ static bool isFirstItemRemovable( sout_access_out_sys_t *p_sys, uint32_t i_first
     }
     output_segment_t *first = vlc_array_item_at_index( p_sys->segments_t, 0 );
 
-    return duration >= (first->f_seglength + (float)p_sys->i_seglen);
+    return duration >= (first->f_seglength + (float)(p_sys->i_numsegs * p_sys->i_seglen));
 }
 
 /************************************************************************
