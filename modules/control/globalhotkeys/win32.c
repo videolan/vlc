6ef248d6518300009ca7d8d1c1cd2811bc3b080a
Rémi Denis-Courmont <remi@remlab.net>
2013-09-15 15:26:45 +0300

    globalhotkeys: use different names for Win32 and XCB targets
    This works around an automake limitation with multiple directory
    prefixes.

--- modules/control/globalhotkeys/win32.c
+++ modules/control/globalhotkeys/win32.c
@@ -49,6 +49,7 @@ vlc_module_begin()
     set_description( N_("Global Hotkeys interface") )
     set_capability( "interface", 0 )
     set_callbacks( Open, Close )
+    add_shortcut( "globalhotkeys" )
 vlc_module_end()
 
 struct intf_sys_t
