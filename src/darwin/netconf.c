b062c14b11a1f2dc7df560fd77f4310995d20a6c
Felix Paul Kühne <fkuehne@videolan.org>
2013-10-07 12:21:29 +0200

    src: fix crashes on proxy lookups by deleting the Mac specific and using the iOS code which works just fine on 10.6+ (close #9561)

--- src/darwin/netconf.c
+++ src/darwin/netconf.c
@@ -28,13 +28,8 @@
 #include <vlc_common.h>
 #include <vlc_network.h>
 
-#import <TargetConditionals.h>
 #include <CoreFoundation/CoreFoundation.h>
-#if TARGET_OS_IPHONE
 #include <CFNetwork/CFProxySupport.h>
-#else
-#include <SystemConfiguration/SystemConfiguration.h>
-#endif
 
 /**
  * Determines the network proxy server to use (if any).
@@ -44,7 +39,6 @@
 char *vlc_getProxyUrl(const char *url)
 {
     VLC_UNUSED(url);
-#if TARGET_OS_IPHONE
     char *proxy_url = NULL;
     CFDictionaryRef dicRef = CFNetworkCopySystemProxySettings();
     if (NULL != dicRef) {
@@ -64,7 +58,6 @@ char *vlc_getProxyUrl(const char *url)
             if (CFStringGetCString(proxyCFstr, host_buffer, sizeof(host_buffer)
                                    - 1, kCFStringEncodingUTF8)) {
                 char buffer[4096];
-                memset(host_buffer, 0, sizeof(host_buffer));
                 sprintf(buffer, "%s:%d", host_buffer, port);
                 proxy_url = strdup(buffer);
             }
@@ -74,58 +67,4 @@ char *vlc_getProxyUrl(const char *url)
     }
 
     return proxy_url;
-#else
-    CFDictionaryRef proxies = SCDynamicStoreCopyProxies(NULL);
-    char *proxy_url = NULL;
-
-    if (proxies) {
-        CFNumberRef cfn_httpProxyOn =
-            (CFNumberRef)CFDictionaryGetValue(proxies,
-                                              kSCPropNetProxiesHTTPEnable);
-        if (cfn_httpProxyOn) {
-            int i_httpProxyOn;
-            CFNumberGetValue(cfn_httpProxyOn, kCFNumberIntType, &i_httpProxyOn);
-            CFRelease(cfn_httpProxyOn);
-
-            if (i_httpProxyOn == 1) // http proxy is on
-            {
-                CFStringRef httpProxy =
-                    (CFStringRef)CFDictionaryGetValue(proxies,
-                                                      kSCPropNetProxiesHTTPProxy);
-
-                if (httpProxy) {
-                    CFNumberRef cfn_httpProxyPort =
-                        (CFNumberRef)CFDictionaryGetValue(proxies,
-                                                        kSCPropNetProxiesHTTPPort);
-                    int i_httpProxyPort;
-                    CFNumberGetValue(cfn_httpProxyPort,
-                                     kCFNumberIntType,
-                                     &i_httpProxyPort);
-                    CFRelease(cfn_httpProxyPort);
-
-                    CFMutableStringRef outputURL =
-                        CFStringCreateMutableCopy(kCFAllocatorDefault,
-                                                  0,
-                                                  httpProxy);
-                    if (i_httpProxyPort > 0)
-                        CFStringAppendFormat(outputURL,
-                                             NULL,
-                                             CFSTR(":%i"),
-                                             i_httpProxyPort);
-
-                    char buffer[4096];
-                    if (CFStringGetCString(outputURL, buffer, sizeof(buffer),
-                        kCFStringEncodingUTF8))
-                        proxy_url = strdup(buffer);
-
-                    CFRelease(outputURL);
-                }
-                CFRelease(httpProxy);
-            }
-        }
-        CFRelease(proxies);
-    }
-
-    return proxy_url;
-#endif
 }
