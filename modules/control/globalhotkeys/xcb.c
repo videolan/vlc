be6723352a807f00fb1c4827790c93ba8949735c
Rémi Denis-Courmont <remi@remlab.net>
2013-11-06 22:30:10 +0200

    XCB/globalhostkeys: add support for meta modifier

--- modules/control/globalhotkeys/xcb.c
+++ modules/control/globalhotkeys/xcb.c
@@ -237,12 +237,17 @@ static unsigned GetX11Modifier( xcb_connection_t *p_connection,
     if( i_vlc & KEY_MODIFIER_ALT )
         i_mask |= GetModifier( p_connection, p_symbols, XK_Alt_L ) |
                   GetModifier( p_connection, p_symbols, XK_Alt_R );
-    if( i_vlc & KEY_MODIFIER_CTRL )
-        i_mask |= GetModifier( p_connection, p_symbols, XK_Control_L ) |
-                  GetModifier( p_connection, p_symbols, XK_Control_R );
     if( i_vlc & KEY_MODIFIER_SHIFT )
         i_mask |= GetModifier( p_connection, p_symbols, XK_Shift_L ) |
                   GetModifier( p_connection, p_symbols, XK_Shift_R );
+    if( i_vlc & KEY_MODIFIER_CTRL )
+        i_mask |= GetModifier( p_connection, p_symbols, XK_Control_L ) |
+                  GetModifier( p_connection, p_symbols, XK_Control_R );
+    if( i_vlc & KEY_MODIFIER_META )
+        i_mask |= GetModifier( p_connection, p_symbols, XK_Meta_L ) |
+                  GetModifier( p_connection, p_symbols, XK_Meta_R ) |
+                  GetModifier( p_connection, p_symbols, XK_Super_L ) |
+                  GetModifier( p_connection, p_symbols, XK_Super_R );
     return i_mask;
 }
 
