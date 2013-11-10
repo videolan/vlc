9973008454e6f866405fbf94fa26013493a74ac3
Felix Paul Kühne <fkuehne@videolan.org>
2013-11-10 23:19:18 +0100

    macosx: fix compilation warnings

--- modules/gui/macosx/intf.h
+++ modules/gui/macosx/intf.h
@@ -176,13 +176,8 @@ struct intf_sys_t
 - (void)showMainWindow;
 - (void)showFullscreenController;
 - (void)updateDelays;
-- (void)initStrings;
-
-- (void)processReceivedlibvlcMessage:(const vlc_log_t *) item ofType: (int)type withStr: (char *)str;
 
 - (void)updateTogglePlaylistState;
-
-- (void)windowDidBecomeKey:(NSNotification *)o_notification;
 
 - (void)mediaKeyTap:(SPMediaKeyTap*)keyTap receivedMediaKeyEvent:(NSEvent*)event;
 @end
