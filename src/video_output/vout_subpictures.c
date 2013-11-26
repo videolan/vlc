160f187de2622e8108c40289a14677240de457fd
Felix Abecassis <felix.abecassis@gmail.com>
2013-11-26 14:21:31 +0100

    vout: set visible resolution of input/output video formats in SpuRenderCreateAndLoadScale().
    Enable video filters using the visible dimension of the picture.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/video_output/vout_subpictures.c
+++ src/video_output/vout_subpictures.c
@@ -238,12 +238,16 @@ static filter_t *SpuRenderCreateAndLoadScale(vlc_object_t *object,
     es_format_Init(&scale->fmt_in, VIDEO_ES, 0);
     scale->fmt_in.video.i_chroma = src_chroma;
     scale->fmt_in.video.i_width =
-    scale->fmt_in.video.i_height = 32;
+    scale->fmt_in.video.i_visible_width =
+    scale->fmt_in.video.i_height =
+    scale->fmt_in.video.i_visible_height = 32;
 
     es_format_Init(&scale->fmt_out, VIDEO_ES, 0);
     scale->fmt_out.video.i_chroma = dst_chroma;
     scale->fmt_out.video.i_width =
-    scale->fmt_out.video.i_height = require_resize ? 16 : 32;
+    scale->fmt_out.video.i_visible_width =
+    scale->fmt_out.video.i_height =
+    scale->fmt_out.video.i_visible_height = require_resize ? 16 : 32;
 
     scale->pf_video_buffer_new = spu_new_video_buffer;
     scale->pf_video_buffer_del = spu_del_video_buffer;
