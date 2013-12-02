c5b2b0a284ba6b544646f3a205aa1a2e1117d431
Jean-Baptiste Kempf <jb@videolan.org>
2013-12-02 17:32:07 +0100

    Freetype: fix compilation with 2.5.1
    Close #10019

--- modules/text_renderer/freetype.c
+++ modules/text_renderer/freetype.c
@@ -88,10 +88,11 @@
 #endif
 
 /* Freetype */
-#include <freetype/ftsynth.h>
+#include <ft2build.h>
 #include FT_FREETYPE_H
 #include FT_GLYPH_H
 #include FT_STROKER_H
+#include FT_SYNTHESIS_H
 
 #define FT_FLOOR(X)     ((X & -64) >> 6)
 #define FT_CEIL(X)      (((X + 63) & -64) >> 6)
