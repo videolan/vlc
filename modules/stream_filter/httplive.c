b9a080559f0e0491e459fe16a07540eb708e056b
Ilkka Ollakka <ileoo@videolan.org>
2013-12-11 09:03:22 +0200

    livehttp: parse key="value" style options

--- modules/stream_filter/httplive.c
+++ modules/stream_filter/httplive.c
@@ -496,9 +496,21 @@ static char *parse_Attributes(const char *line, const char *attr)
         if (strncasecmp(begin, attr, strlen(attr)) == 0
           && begin[strlen(attr)] == '=')
         {
-            /* <attr>=<value>[,]* */
+            /* <attr>="<value>"[,]* */
             p = strchr(begin, ',');
             begin += strlen(attr) + 1;
+
+            /* Check if we have " " marked value*/
+            if( begin[0] == '"' )
+            {
+                char *valueend = strchr( begin+1, '"');
+
+                /* No ending " so bail out */
+                if( unlikely( !valueend ) )
+                    return NULL;
+
+                p = strchr( valueend, ',');
+            }
             if (begin >= end)
                 return NULL;
             if (p == NULL) /* last attribute */
