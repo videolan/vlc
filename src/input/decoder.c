bedf87eba1f29b622b203847935e65803a9117d2
Rafaël Carré <funman@videolan.org>
2013-12-18 16:40:31 +0100

    Simplify message when codec could not be found
    People do not read that far and keep asking for support anyway.

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -687,12 +687,9 @@ static void DecoderUnsupportedCodec( decoder_t *p_dec, vlc_fourcc_t codec )
         const char *desc = vlc_fourcc_GetDescription(p_dec->fmt_in.i_cat, codec);
         if (!desc || !*desc)
             desc = N_("No description for this codec");
-        msg_Err( p_dec, "no suitable decoder module for fourcc `%4.4s' (%s). "
-                "VLC probably does not support this sound or video format.",
-                (char*)&codec, desc );
-        dialog_Fatal( p_dec, _("No suitable decoder module"),
-                _("VLC does not support the audio or video format \"%4.4s\" (%s). "
-                    "Unfortunately there is no way for you to fix this."),
+        msg_Err( p_dec, "Codec `%4.4s' (%s) is not supported.", (char*)&codec, desc );
+        dialog_Fatal( p_dec, _("Codec not supported"),
+                _("VLC could not decode the format \"%4.4s\" (%s)"),
                 (char*)&codec, desc );
     } else {
         msg_Err( p_dec, "could not identify codec" );
