6295836c45f08c7186ee505a2fd481ad061c87df
Ilkka Ollakka <ileoo@videolan.org>
2013-12-27 16:52:59 +0200

    transcode: add chroma conversion in actually checked filter output

--- modules/stream_out/transcode/video.c
+++ modules/stream_out/transcode/video.c
@@ -365,6 +365,7 @@ static void conversion_video_filter_append( sout_stream_id_t *id )
     const es_format_t *p_fmt_out = &id->p_decoder->fmt_out;
     if( id->p_f_chain )
         p_fmt_out = filter_chain_GetFmtOut( id->p_f_chain );
+
     if( id->p_uf_chain )
         p_fmt_out = filter_chain_GetFmtOut( id->p_uf_chain );
 
@@ -372,7 +373,7 @@ static void conversion_video_filter_append( sout_stream_id_t *id )
         ( p_fmt_out->video.i_width != id->p_encoder->fmt_in.video.i_width ) ||
         ( p_fmt_out->video.i_height != id->p_encoder->fmt_in.video.i_height ) )
     {
-        filter_chain_AppendFilter( id->p_f_chain,
+        filter_chain_AppendFilter( id->p_uf_chain ? id->p_uf_chain : id->p_f_chain,
                                    NULL, NULL,
                                    p_fmt_out,
                                    &id->p_encoder->fmt_in );
