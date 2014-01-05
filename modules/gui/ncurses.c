8c17cf44c589221323fb2b449ff042ee807e79fe
Rémi Denis-Courmont <remi@remlab.net>
2014-01-05 18:33:59 +0200

    ncurses: pass interface thread pointer to pl_Get() directly

--- modules/gui/ncurses.c
+++ modules/gui/ncurses.c
@@ -1830,7 +1830,7 @@ static int Open(vlc_object_t *p_this)
 {
     intf_thread_t *intf = (intf_thread_t *)p_this;
     intf_sys_t    *sys  = intf->p_sys = calloc(1, sizeof(intf_sys_t));
-    playlist_t    *p_playlist = pl_Get(p_this);
+    playlist_t    *p_playlist = pl_Get(intf);
 
     if (!sys)
         return VLC_ENOMEM;
