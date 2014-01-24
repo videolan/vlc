987349d7c90cacca2f8383bfb64cdc2a4551b1b1
Rémi Denis-Courmont <remi@remlab.net>
2014-01-24 18:06:33 +0200

    input: cosmetic

--- src/input/input.c
+++ src/input/input.c
@@ -2245,8 +2245,8 @@ static int InputSourceInit( input_thread_t *p_input,
         }
 
         /* Try access_demux first */
-        in->p_demux = demux_New( p_input, p_input, psz_access, psz_demux, psz_path,
-                                  NULL, p_input->p->p_es_out, false );
+        in->p_demux = demux_New( p_input, p_input, psz_access, psz_demux,
+                                 psz_path, NULL, p_input->p->p_es_out, false );
     }
     else
     {
