9f7bfd93e3a749a2912adda6380370310cd4dffa
Felix Abecassis <felix.abecassis@gmail.com>
2014-01-28 10:56:46 +0100

    hotkeys: fullscreen state of the vout should have precedence over fullscreen state of the playlist
    Double clicking to toggle to fullscreen mode does not trigger this code
    path since 7b8083d228. The state of the playlist was therefore not in
    sync with the vout. Pressing the fullscreen hotkey caused the state of
    the playlist to be used to set the state of the vout, thus causing the bug.
    
    Close #10433.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/control/hotkeys.c
+++ modules/control/hotkeys.c
@@ -733,9 +733,13 @@ static int PutAction( intf_thread_t *p_intf, int i_action )
 
         case ACTIONID_TOGGLE_FULLSCREEN:
         {
-            bool fs = var_ToggleBool( p_playlist, "fullscreen" );
             if( p_vout )
-                var_SetBool( p_vout, "fullscreen", fs );
+            {
+                bool fs = var_ToggleBool( p_vout, "fullscreen" );
+                var_SetBool( p_playlist, "fullscreen", fs );
+            }
+            else
+                var_ToggleBool( p_playlist, "fullscreen" );
             break;
         }
 
