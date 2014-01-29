cabe71212a0c4bce61e8ebcc52953eadd951597b
Felix Abecassis <felix.abecassis@gmail.com>
2014-01-29 14:37:43 +0000

    vout: reduce zoom ratio in order to avoid signalling unnecessary zoom events
    For instance changing from 10:10 to 1:1 should not generate an event
    if we are not in display fill mode.

--- src/video_output/display.c
+++ src/video_output/display.c
@@ -1145,6 +1145,13 @@ void vout_SetDisplayZoom(vout_display_t *vd, int num, int den)
 {
     vout_display_owner_sys_t *osys = vd->owner.sys;
 
+    if (num > 0 && den > 0) {
+        vlc_ureduce(&num, &den, num, den, 0);
+    } else {
+        num = 1;
+        den = 1;
+    }
+
     if (osys->is_display_filled ||
         osys->zoom.num != num || osys->zoom.den != den) {
         osys->ch_zoom = true;
