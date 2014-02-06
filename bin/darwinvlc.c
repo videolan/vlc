videolan/vlc

commit 9fc66e6950f19f8fdcb030336e609fc2902b2ec4

bin/mac: do not start playlist while quitting VLC
close #8353

bin/darwinvlc.c
@@ -226,8 +226,6 @@ int main( int i_argc, const char *ppsz_argv[] )
     if (libvlc_add_intf (vlc, NULL))
         goto out;
 
-    libvlc_playlist_play (vlc, -1, 0, NULL);
-
     /* Qt4 insists on catching SIGCHLD via signal handler. To work around that,
      * unblock it after all our child threads are created. */
     sigdelset (&set, SIGCHLD);

commit ed1bd9b2b1232f24c6f11194996f031a594a0469

darwinvlc: fix transcoding on command-line broken in 9fc66e695 while retaining the fix

bin/darwinvlc.c
@@ -213,6 +213,8 @@ int main( int i_argc, const char *ppsz_argv[] )
     libvlc_set_app_id (vlc, "org.VideoLAN.VLC", PACKAGE_VERSION, PACKAGE_NAME);
     libvlc_set_user_agent (vlc, "VLC media player", "VLC/"PACKAGE_VERSION);
 
+    libvlc_playlist_play (vlc, -1, 0, NULL);
+
     libvlc_add_intf (vlc, "hotkeys,none");
 
     if (libvlc_add_intf (vlc, NULL))