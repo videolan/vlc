4d67619bda8c1f62c54293840120410bb4fa660b
KO Myung-Hun <komh78@gmail.com>
2014-02-15 18:00:22 +0900

    addons: fix compilation error
    -----
      CC       addons/vorepository.lo
    addons/vorepository.c: In function 'Close':
    addons/vorepository.c:490:9: error: implicit declaration of function 'unlink' [-Werror=implicit-function-declaration]
    cc1: some warnings being treated as errors
    -----
    
    Signed-off-by: Francois Cartegnie <fcvlcdev@free.fr>

--- modules/misc/addons/vorepository.c
+++ modules/misc/addons/vorepository.c
@@ -487,7 +487,7 @@ static void Close(vlc_object_t *p_this)
     addons_finder_t *p_finder = (addons_finder_t*) p_this;
     if ( p_finder->p_sys->psz_tempfile )
     {
-        unlink( p_finder->p_sys->psz_tempfile );
+        vlc_unlink( p_finder->p_sys->psz_tempfile );
         free( p_finder->p_sys );
     }
 }
