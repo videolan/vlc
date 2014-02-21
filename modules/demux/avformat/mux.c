39d0b32282316ca6de2fdb6907fddc1a0b93dec9
Rafaël Carré <funman@videolan.org>
2014-02-21 14:47:00 +0100

    avformat mux: use a variable

--- modules/demux/avformat/mux.c
+++ modules/demux/avformat/mux.c
@@ -441,6 +441,7 @@ static int Control( sout_mux_t *p_mux, int i_query, va_list args )
 static int IOWrite( void *opaque, uint8_t *buf, int buf_size )
 {
     sout_mux_t *p_mux = opaque;
+    sout_mux_sys_t *p_sys = p_mux->p_sys;
     int i_ret;
 
 #ifdef AVFORMAT_DEBUG
@@ -450,13 +451,13 @@ static int IOWrite( void *opaque, uint8_t *buf, int buf_size )
     block_t *p_buf = block_Alloc( buf_size );
     if( buf_size > 0 ) memcpy( p_buf->p_buffer, buf, buf_size );
 
-    if( p_mux->p_sys->b_write_header )
+    if( p_sys->b_write_header )
         p_buf->i_flags |= BLOCK_FLAG_HEADER;
 
-    if( p_mux->p_sys->b_write_keyframe )
+    if( p_sys->b_write_keyframe )
     {
         p_buf->i_flags |= BLOCK_FLAG_TYPE_I;
-        p_mux->p_sys->b_write_keyframe = false;
+        p_sys->b_write_keyframe = false;
     }
 
     i_ret = sout_AccessOutWrite( p_mux->p_access, p_buf );
