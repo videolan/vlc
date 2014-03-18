4c156e1054ce76bf305ae103646ce92ed6295e7b
Tristan Matthews <le.businessman@gmail.com>
2014-03-18 13:30:15 -0400

    png: set visible_ width/height

--- modules/codec/png.c
+++ modules/codec/png.c
@@ -251,8 +251,8 @@ static picture_t *DecodeBlock( decoder_t *p_dec, block_t **pp_block )
 
     /* Set output properties */
     p_dec->fmt_out.i_codec = VLC_CODEC_RGBA;
-    p_dec->fmt_out.video.i_width = i_width;
-    p_dec->fmt_out.video.i_height = i_height;
+    p_dec->fmt_out.video.i_visible_width = p_dec->fmt_out.video.i_width = i_width;
+    p_dec->fmt_out.video.i_visible_height = p_dec->fmt_out.video.i_height = i_height;
     p_dec->fmt_out.video.i_sar_num = 1;
     p_dec->fmt_out.video.i_sar_den = 1;
     p_dec->fmt_out.video.i_rmask = 0x000000ff;
