114d584be08283d59e962d38f0b1a96159d15592
Ilkka Ollakka <ileoo@videolan.org>
2014-03-21 11:12:34 +0200

    use CLOCK_FREQ

--- modules/codec/twolame.c
+++ modules/codec/twolame.c
@@ -275,7 +275,7 @@ static block_t *Encode( encoder_t *p_enc, block_t *p_aout_buf )
 
         p_block = block_Alloc( i_used );
         memcpy( p_block->p_buffer, p_sys->p_out_buffer, i_used );
-        p_block->i_length = (mtime_t)1000000 *
+        p_block->i_length = CLOCK_FREQ *
                 (mtime_t)MPEG_FRAME_SIZE / (mtime_t)p_enc->fmt_out.audio.i_rate;
         p_block->i_dts = p_block->i_pts = p_sys->i_pts;
         p_sys->i_pts += p_block->i_length;
@@ -305,7 +305,7 @@ static block_t *Encode( encoder_t *p_enc, block_t *p_aout_buf )
         p_sys->i_nb_samples = 0;
         p_block = block_Alloc( i_used );
         memcpy( p_block->p_buffer, p_sys->p_out_buffer, i_used );
-        p_block->i_length = (mtime_t)1000000 *
+        p_block->i_length = CLOCK_FREQ *
                 (mtime_t)MPEG_FRAME_SIZE / (mtime_t)p_enc->fmt_out.audio.i_rate;
         p_block->i_dts = p_block->i_pts = p_sys->i_pts;
         p_sys->i_pts += p_block->i_length;
