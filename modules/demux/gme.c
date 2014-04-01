f4fafb0a113a7dd49f7da3bb21dd612f910ec56f
Rémi Denis-Courmont <remi@remlab.net>
2014-04-01 23:43:26 +0300

    gme: fix warning

--- modules/demux/gme.c
+++ modules/demux/gme.c
@@ -248,8 +248,12 @@ static int Control (demux_t *demux, int query, va_list args)
              || (sys->titlev[sys->track_id]->i_length == 0))
                 *pos = 0.;
             else
-                *pos = (double)(gme_tell (sys->emu))
+            {
+                int offset = gme_tell (sys->emu);
+
+                *pos = (double)offset
                     / (double)(sys->titlev[sys->track_id]->i_length / 1000);
+            }
             return VLC_SUCCESS;
         }
 
