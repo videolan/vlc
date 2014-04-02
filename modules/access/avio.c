01c1d49be8c2b9eb375327c79145fa74e58c2f21
Rémi Denis-Courmont <remi@remlab.net>
2014-04-02 18:37:20 +0300

    avio: fix busy loop on EOF (fixes #11076)

--- modules/access/avio.c
+++ modules/access/avio.c
@@ -307,8 +307,10 @@ static ssize_t Read(access_t *access, uint8_t *data, size_t size)
     int r = avio_read(access->p_sys->context, data, size);
     if (r > 0)
         access->info.i_pos += r;
-    else
+    else {
         access->info.b_eof = true;
+        r = 0;
+    }
     return r;
 }
 
