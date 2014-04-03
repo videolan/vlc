2f966bd6edebb262b4e5e0977d84c9737e8043c1
John Stebbins <stebbins@jetheaddev.com>
2014-04-03 11:08:02 -0600

    substx3g: Allow multiple simultanious face styles
    Signed-off-by: Francois Cartegnie <fcvlcdev@free.fr>

--- modules/codec/substx3g.c
+++ modules/codec/substx3g.c
@@ -80,9 +80,9 @@ static int ConvertFlags( int i_atomflags )
     int i_vlcstyles_flags = 0;
     if ( i_atomflags & FONT_FACE_BOLD )
         i_vlcstyles_flags |= STYLE_BOLD;
-    else if ( i_atomflags & FONT_FACE_ITALIC )
+    if ( i_atomflags & FONT_FACE_ITALIC )
         i_vlcstyles_flags |= STYLE_ITALIC;
-    else if ( i_atomflags & FONT_FACE_UNDERLINE )
+    if ( i_atomflags & FONT_FACE_UNDERLINE )
         i_vlcstyles_flags |= STYLE_UNDERLINE;
     return i_vlcstyles_flags;
 }
