8ccdd27c8c49a5e8199ba4fb50d7cd8d2a3284e7
Rafaël Carré <funman@videolan.org>
2014-04-16 13:45:56 +0200

    audiobargraph_a: simplify Close

--- modules/audio_filter/audiobargraph_a.c
+++ modules/audio_filter/audiobargraph_a.c
@@ -264,17 +264,14 @@ static void Close( vlc_object_t *p_this )
 {
     filter_t * p_filter = (filter_t *)p_this;
     filter_sys_t *p_sys = p_filter->p_sys;
-    ValueDate_t* current;
 
     var_Destroy( p_filter->p_libvlc, "audiobargraph_v-i_values" );
     var_Destroy( p_filter->p_libvlc, "audiobargraph_v-alarm" );
 
-    p_sys->last = NULL;
     while (p_sys->first != NULL) {
-        current = p_sys->first;
+        ValueDate_t *current = p_sys->first;
         p_sys->first = p_sys->first->next;
         free(current);
     }
-    //free(p_sys->value);
-    free( p_filter->p_sys );
+    free( p_sys );
 }
