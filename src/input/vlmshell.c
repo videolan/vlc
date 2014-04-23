8a4edbb7929b5fc1e8b97b85c3771883f8d63b9d
Rémi Denis-Courmont <remi@remlab.net>
2014-04-23 19:21:24 +0300

    vlm: fix error message / uninitialized variable

--- src/input/vlmshell.c
+++ src/input/vlmshell.c
@@ -854,8 +854,8 @@ int ExecuteCommand( vlm_t *p_vlm, const char *psz_command,
 
     if( !psz_buf || !ppsz_command )
     {
-        p_message = vlm_MessageNew( ( ppsz_command ) ? ppsz_command[0] : NULL,
-                        "Memory allocation failed for command of length %zu",
+        p_message = vlm_MessageNew( "Memory error",
+                        "allocation failed for command of length %zu",
                         i_command_len );
         goto error;
     }
