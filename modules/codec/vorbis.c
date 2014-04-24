b54f2c636cc97f73007d6f574243cc43b7e2f0f8
Rémi Denis-Courmont <remi@remlab.net>
2014-04-24 20:59:46 +0300

    vorbis: really fix out-of-bound read
    Also fix format string.

--- modules/codec/vorbis.c
+++ modules/codec/vorbis.c
@@ -362,9 +362,9 @@ static int ProcessHeaders( decoder_t *p_dec )
     p_dec->fmt_out.audio.i_rate     = p_sys->vi.rate;
     p_dec->fmt_out.audio.i_channels = p_sys->vi.channels;
 
-    if( p_dec->fmt_out.audio.i_channels > ARRAY_SIZE(pi_channels_maps) )
+    if( p_dec->fmt_out.audio.i_channels >= ARRAY_SIZE(pi_channels_maps) )
     {
-        msg_Err( p_dec, "invalid number of channels (not between 1 and %lu): %i",
+        msg_Err( p_dec, "invalid number of channels (1-%zu): %i",
                  ARRAY_SIZE(pi_channels_maps),
                  p_dec->fmt_out.audio.i_channels );
         return VLC_EGENERIC;
