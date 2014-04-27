videolan/vlc

commit fe0a075ef37b4b0ab6464c169afdbdd955b0d9b7

file: do not turn off caching on BSD (fixes #8446)
Turning off the cache makes no sense here, especially right after
enabling reading ahead (which intrisically means caching).

modules/access/file.c
@@ -236,15 +236,11 @@ int FileOpen( vlc_object_t *p_this )
         posix_fadvise (fd, 0, 4096, POSIX_FADV_WILLNEED);
         /* In most cases, we only read the file once. */
         posix_fadvise (fd, 0, 0, POSIX_FADV_NOREUSE);
-#if defined(HAVE_FCNTL)
-        /* We'd rather use any available memory for reading ahead
-         * than for caching what we've already seen/heard */
-# if defined(F_RDAHEAD)
+#ifdef F_RDAHEAD
         fcntl (fd, F_RDAHEAD, 1);
-# endif
-# if defined(F_NOCACHE)
-        fcntl (fd, F_NOCACHE, 1);
-# endif
+#endif
+#ifdef F_NOCACHE
+        fcntl (fd, F_NOCACHE, 0);
 #endif
     }
     else

commit b8b8c438f8f65a93da82364c8fea1dbf987c4a8e

access/file: don't read-ahead when processing remote contents (closes #9885, refs #8446)
OS X Mavericks takes read-ahead very seriously when accessing SMB drives, so it caches up to a 100 MB. However, when the end of said cache is reached, VLC will stutter because the file system needs to re-connect to the server first. Disabling read-ahead leads to a continous data flow at the media's bitrate.

This solves a regression introduced in fe0a075ef3

modules/access/file.c
@@ -235,11 +235,14 @@ int FileOpen( vlc_object_t *p_this )
         posix_fadvise (fd, 0, 4096, POSIX_FADV_WILLNEED);
         /* In most cases, we only read the file once. */
         posix_fadvise (fd, 0, 0, POSIX_FADV_NOREUSE);
-#ifdef F_RDAHEAD
-        fcntl (fd, F_RDAHEAD, 1);
-#endif
 #ifdef F_NOCACHE
         fcntl (fd, F_NOCACHE, 0);
+#endif
+#ifdef F_RDAHEAD
+        if (IsRemote(fd, p_access->psz_filepath))
+            fcntl (fd, F_RDAHEAD, 0);
+        else
+            fcntl (fd, F_RDAHEAD, 1);
 #endif
     }
     else