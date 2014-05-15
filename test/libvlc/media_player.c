d7420aadc69a1bcfcce2dcb64cc2a844379fb5e6
Rémi Denis-Courmont <remi@remlab.net>
2014-05-15 21:47:35 +0800

    audio: add test for the new audio device functions

--- test/libvlc/media_player.c
+++ test/libvlc/media_player.c
@@ -71,6 +71,13 @@ static void test_audio_video(libvlc_media_player_t *mp)
     libvlc_video_set_scale(mp, 0.);
     libvlc_video_set_scale(mp, 0.); /* no-op */
     assert(libvlc_video_get_scale(mp) == 0.);
+
+    libvlc_audio_output_device_t *aouts = libvlc_audio_output_device_enum(mp);
+    for (libvlc_audio_output_device_t *e = aouts; e != NULL; e = e->p_next)
+    {
+        libvlc_audio_output_device_set( mp, NULL, e->psz_device );
+    }
+    libvlc_audio_output_device_list_release( aouts );
 }
 
 static void test_media_player_set_media(const char** argv, int argc)
