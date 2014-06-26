3d90771a7b113a554ddda9f3ea98ac137f5c9dcc
Thomas Guillem <guillem@archos.com>
2014-06-26 14:05:51 +0200

    Android vout: fix possible mutex leak.
    Do not leak mutex if i_chroma is VLC_CODEC_ANDROID_OPAQUE.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- modules/video_output/android/surface.c
+++ modules/video_output/android/surface.c
@@ -173,6 +173,10 @@ static void *InitLibrary(vout_display_sys_t *sys)
 static int Open(vlc_object_t *p_this)
 {
     vout_display_t *vd = (vout_display_t *)p_this;
+    video_format_t fmt = vd->fmt;
+
+    if (fmt.i_chroma == VLC_CODEC_ANDROID_OPAQUE)
+        return VLC_EGENERIC;
 
     /* */
     if (vlc_mutex_trylock(&single_instance) != 0) {
@@ -200,11 +204,6 @@ static int Open(vlc_object_t *p_this)
     }
 
     /* Setup chroma */
-    video_format_t fmt = vd->fmt;
-
-    if (fmt.i_chroma == VLC_CODEC_ANDROID_OPAQUE)
-        return VLC_EGENERIC;
-
     char *psz_fcc = var_InheritString(vd, CFG_PREFIX "chroma");
     if( psz_fcc ) {
         fmt.i_chroma = vlc_fourcc_GetCodecFromString(VIDEO_ES, psz_fcc);
