ae93285ea221edb5916ff6f8a6e9731a52276239
Martin Storsjö <martin@martin.st>
2014-07-28 10:58:00 +0300

    iomx: Support building for honeycomb with ANDROID_API=13 as well
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- modules/codec/omxil/iomx.cpp
+++ modules/codec/omxil/iomx.cpp
@@ -193,7 +193,7 @@ static OMX_ERRORTYPE iomx_use_buffer(OMX_HANDLETYPE component, OMX_BUFFERHEADERT
     OMXNode* node = (OMXNode*) ((OMX_COMPONENTTYPE*)component)->pComponentPrivate;
     OMXBuffer* info = new OMXBuffer;
     info->dealer = NULL;
-#if ANDROID_API == 11
+#if ANDROID_API <= 13
     info->graphicBuffer = new GraphicBuffer((android_native_buffer_t*) data, false);
 #else
     info->graphicBuffer = new GraphicBuffer((ANativeWindowBuffer*) data, false);
