0643589050694bdc42c5709ef2b3d5dbc62748d7
Felix Abecassis <felix.abecassis@gmail.com>
2014-08-01 11:18:40 +0200

    h264: refactor emulation prevention byte handling

--- modules/packetizer/h264.c
+++ modules/packetizer/h264.c
@@ -555,22 +555,7 @@ static void CreateDecodedNAL( uint8_t **pp_ret, int *pi_ret,
     *pp_ret = dst;
 
     if( dst )
-    {
-        while( src < end )
-        {
-            if( src < end - 3 && src[0] == 0x00 && src[1] == 0x00 &&
-                src[2] == 0x03 )
-            {
-                *dst++ = 0x00;
-                *dst++ = 0x00;
-
-                src += 3;
-                continue;
-            }
-            *dst++ = *src++;
-        }
-    }
-    *pi_ret = dst - *pp_ret;
+        *pi_ret = nal_decode(src, dst, i_src);
 }
 
 /*****************************************************************************
