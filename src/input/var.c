73906514e8788a405d998772e64fc48d6a3d1479
Rémi Denis-Courmont <remi@remlab.net>
2014-08-13 21:09:29 +0300

    input var: avoid implicit double upconversion

--- src/input/var.c
+++ src/input/var.c
@@ -592,17 +592,17 @@ static int PositionCallback( vlc_object_t *p_this, char const *psz_cmd,
     if( !strcmp( psz_cmd, "position-offset" ) )
     {
         float f_position = var_GetFloat( p_input, "position" ) + newval.f_float;
-        if( f_position < 0.0 )
-            f_position = 0.0;
-        else if( f_position > 1.0 )
-            f_position = 1.0;
+        if( f_position < 0.f )
+            f_position = 0.f;
+        else if( f_position > 1.f )
+            f_position = 1.f;
         var_SetFloat( p_this, "position", f_position );
     }
     else
     {
         /* Update "length" for better intf behavour */
         const mtime_t i_length = var_GetTime( p_input, "length" );
-        if( i_length > 0 && newval.f_float >= 0.0 && newval.f_float <= 1.0 )
+        if( i_length > 0 && newval.f_float >= 0.f && newval.f_float <= 1.f )
         {
             vlc_value_t val;
 
