ae8de060162e08318cbf935dc0a2c9e0a65f62b3
Rémi Denis-Courmont <remi@remlab.net>
2014-08-13 21:20:10 +0300

    rotate: stick to single precision arithmetic

--- modules/video_filter/rotate.c
+++ modules/video_filter/rotate.c
@@ -92,7 +92,7 @@ struct filter_sys_t
 
 static void store_trigo( struct filter_sys_t *sys, float f_angle )
 {
-    f_angle *= M_PI / 180.f; /* degrees -> radians */
+    f_angle *= (float)(M_PI / 180.); /* degrees -> radians */
 
     uint16_t i_sin = lroundf(sinf(f_angle) * 4096.f);
     uint16_t i_cos = lroundf(cosf(f_angle) * 4096.f);
