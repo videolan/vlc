3d549e2bc9909c58a9db24c6d1f72d21fb044d07
Rémi Denis-Courmont <remi@remlab.net>
2014-08-18 19:04:58 +0300

    DTV: open device nodes in non-blocking mode

--- modules/access/dtv/linux.c
+++ modules/access/dtv/linux.c
@@ -181,10 +181,7 @@ static int dvb_open_node (dvb_device_t *d, const char *type, int flags)
     char path[strlen (type) + 4];
 
     snprintf (path, sizeof (path), "%s%u", type, d->device);
-    fd = vlc_openat (d->dir, path, flags);
-    if (fd != -1)
-        fcntl (fd, F_SETFL, fcntl (fd, F_GETFL) | O_NONBLOCK);
-    return fd;
+    return vlc_openat (d->dir, path, flags | O_NONBLOCK);
 }
 
 /**
