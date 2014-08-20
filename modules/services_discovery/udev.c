4ee185d18115325da2acc10393e353e5ee19a859
Rémi Denis-Courmont <remi@remlab.net>
2014-08-20 21:15:31 +0300

    udev: fix signed overflow

--- modules/services_discovery/udev.c
+++ modules/services_discovery/udev.c
@@ -137,11 +137,12 @@ struct services_discovery_sys_t
 static int cmpdev (const void *a, const void *b)
 {
     const dev_t *da = a, *db = b;
-    dev_t delta = *da - *db;
 
-    if (sizeof (delta) > sizeof (int))
-        return delta ? (((signed)delta > 0) ? 1 : -1) : 0;
-    return (signed)delta;
+    if (*da > *db)
+        return +1;
+    if (*da < *db)
+        return -1;
+    return 0;
 }
 
 static void DestroyDevice (void *data)
