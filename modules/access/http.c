b3b724f1628319e78be769eb725529a1a00fd96b
Rémi Denis-Courmont <remi@remlab.net>
2014-08-23 23:24:37 +0300

    http: negotiate HTTP/1.1 explicitly over TLS

--- modules/access/http.c
+++ modules/access/http.c
@@ -1108,8 +1108,11 @@ static int Connect( access_t *p_access, uint64_t i_tell )
         }
 
         /* TLS/SSL handshake */
+        const char *alpn[] = { "http/1.1", NULL };
+
         p_sys->p_tls = vlc_tls_ClientSessionCreate( p_sys->p_creds, p_sys->fd,
-                                    p_sys->url.psz_host, "https", NULL, NULL );
+                p_sys->url.psz_host, "https",
+                p_sys->i_version ? alpn : NULL, NULL );
         if( p_sys->p_tls == NULL )
         {
             msg_Err( p_access, "cannot establish HTTP/TLS session" );
