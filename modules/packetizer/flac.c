832510eb27f32d6e08451d7591a8149caadff935
Hannes Domani <ssbssa@yahoo.de>
2014-08-29 18:17:17 +0200

    packetizer: flac: fix memory leak

--- modules/packetizer/flac.c
+++ modules/packetizer/flac.c
@@ -769,6 +769,7 @@ static void Close(vlc_object_t *p_this)
     decoder_t *p_dec = (decoder_t *)p_this;
     decoder_sys_t *p_sys = p_dec->p_sys;
 
+    es_format_Clean(&p_dec->fmt_out);
     block_BytestreamRelease(&p_sys->bytestream);
     free(p_sys);
 }
