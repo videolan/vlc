c5f806f480c9eda637b741d29c11b1b00e2a6f06
Rémi Denis-Courmont <remi@remlab.net>
2014-09-04 22:54:13 +0300

    mmdevice: fix crash if several channel volumes change (fixes #12086)

--- modules/audio_output/mmdevice.c
+++ modules/audio_output/mmdevice.c
@@ -306,8 +306,12 @@ vlc_AudioSessionEvents_OnChannelVolumeChanged(IAudioSessionEvents *this,
     aout_sys_t *sys = vlc_AudioSessionEvents_sys(this);
     audio_output_t *aout = sys->aout;
 
-    msg_Dbg(aout, "channel volume %lu of %lu changed: %f", changed, count,
-            vols[changed]);
+    if (changed != (DWORD)-1)
+        msg_Dbg(aout, "channel volume %lu of %lu changed: %f", changed, count,
+                vols[changed]);
+    else
+        msg_Dbg(aout, "%lu channels volume changed", count);
+
     (void) ctx;
     return S_OK;
 }
