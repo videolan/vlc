19b9baa2b03af847ab0afc91e78c3ccd3e538cb4
Felix Abecassis <felix.abecassis@gmail.com>
2014-09-12 19:40:54 +0200

    yuv_rgb: bail out if input/output orientation does not match

--- modules/arm_neon/yuv_rgb.c
+++ modules/arm_neon/yuv_rgb.c
@@ -139,7 +139,8 @@ static int Open (vlc_object_t *obj)
 
     if (((filter->fmt_in.video.i_width | filter->fmt_in.video.i_height) & 1)
      || (filter->fmt_in.video.i_width != filter->fmt_out.video.i_width)
-     || (filter->fmt_in.video.i_height != filter->fmt_out.video.i_height))
+     || (filter->fmt_in.video.i_height != filter->fmt_out.video.i_height)
+     || (filter->fmt_in.video.orientation != filter->fmt_out.video.orientation))
         return VLC_EGENERIC;
 
     switch (filter->fmt_out.video.i_chroma)
