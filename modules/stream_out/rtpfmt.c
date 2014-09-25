ec288167c8c26ed545523818dbada162d624b9a5
Rémi Denis-Courmont <remi@remlab.net>
2014-09-25 21:27:21 +0300

    rtp: set the marker bit correctly for Opus

--- modules/stream_out/rtpfmt.c
+++ modules/stream_out/rtpfmt.c
@@ -851,11 +851,13 @@ static int rtp_packetize_ac3( sout_stream_id_sys_t *id, block_t *in )
 
 static int rtp_packetize_simple(sout_stream_id_sys_t *id, block_t *block)
 {
+    bool marker = (block->i_flags & BLOCK_FLAG_DISCONTINUITY) != 0;
+
     block = block_Realloc(block, 12, block->i_buffer);
     if (unlikely(block == NULL))
         return VLC_ENOMEM;
 
-    rtp_packetize_common(id, block, true, block->i_pts);
+    rtp_packetize_common(id, block, marker, block->i_pts);
     rtp_packetize_send(id, block);
     return VLC_SUCCESS;
 }
