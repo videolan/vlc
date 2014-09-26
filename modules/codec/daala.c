9e2997dd6b6dfe42faa951d07c9cdbbf95e26abf
Tristan Matthews <le.businessman@gmail.com>
2014-09-26 07:48:04 -0400

    daala: encoder: fix stride

--- modules/codec/daala.c
+++ modules/codec/daala.c
@@ -686,8 +686,7 @@ static block_t *Encode( encoder_t *p_enc, picture_t *p_pict )
         img.planes[i].xdec = p_sys->di.plane_info[i].xdec;
         img.planes[i].ydec = p_sys->di.plane_info[i].ydec;
         img.planes[i].xstride = 1;
-        img.planes[i].ystride = (img.width
-     + (1 << img.planes[i].xdec) - 1)  >>  img.planes[i].xdec;
+        img.planes[i].ystride = p_pict->p[i].i_pitch;
     }
 
     if( daala_encode_img_in( p_sys->dcx, &img, 0 ) < 0 )
