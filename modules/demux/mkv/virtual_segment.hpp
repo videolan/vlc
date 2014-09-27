videolan/vlc

commit aaebd49a151fae2fd86242dadd3e6e93cbb41e3b

demux: mkv: fix type and c++ type cast
kills that signed / unsigned comparison warning

modules/demux/mkv/virtual_segment.hpp
@@ -114,14 +114,14 @@ public:
     virtual_segment_c( std::vector<matroska_segment_c*> * opened_segments );
     ~virtual_segment_c();
     std::vector<virtual_edition_c*> editions;
-    int                             i_current_edition;
+    std::vector<virtual_edition_c*>::size_type i_current_edition;
     virtual_chapter_c               *p_current_chapter;
     int                             i_sys_title;
 
 
     inline virtual_edition_c * CurrentEdition()
     {
-        if( i_current_edition >= 0 && (size_t) i_current_edition < editions.size() )
+        if( i_current_edition > 0 && i_current_edition < editions.size() )
             return editions[i_current_edition];
         return NULL;
     }

modules/demux/mkv/matroska_segment.hpp
@@ -122,7 +122,7 @@ public:
     /* when you remove this variable the compiler issues an atomicity error */
     /* this variable only works when using std::vector<chapter_edition_c> */
     std::vector<chapter_edition_c*> stored_editions;
-    int                             i_default_edition;
+    std::vector<chapter_edition_c*>::size_type i_default_edition;
 
     std::vector<chapter_translation_c*> translations;
     std::vector<KaxSegmentFamily*>  families;

modules/demux/mkv/virtual_segment.cpp
@@ -261,7 +261,7 @@ void virtual_edition_c::retimeChapters()
 virtual_segment_c::virtual_segment_c( std::vector<matroska_segment_c*> * p_opened_segments )
 {
     /* Main segment */
-    size_t i;
+    std::vector<chapter_edition_c*>::size_type i;
     matroska_segment_c *p_segment = (*p_opened_segments)[0];
     i_current_edition = 0;
     i_sys_title = 0;

commit 2ca410382c2ac6443d99700a7e4399e0af591076

demux: mkv: fix previous type fix
first segment is always 0, not an undefined value
regression introduced by aaebd49a151fae2fd86242dadd3e6e93cbb41e3b
instead of removing the tautology

modules/demux/mkv/virtual_segment.hpp
@@ -121,7 +121,7 @@ public:
 
     inline virtual_edition_c * CurrentEdition()
     {
-        if( i_current_edition > 0 && i_current_edition < editions.size() )
+        if( i_current_edition < editions.size() )
             return editions[i_current_edition];
         return NULL;
     }