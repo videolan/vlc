724620213239aba12dad94458e0f3a626e42285c
Rémi Denis-Courmont <remi@remlab.net>
2014-10-09 22:14:36 +0300

    vout: remove no-op

--- src/video_output/display.c
+++ src/video_output/display.c
@@ -1444,8 +1444,6 @@ static vout_window_t *SplitterNewWindow(vout_display_t *vd, const vout_window_cf
 
     vout_window_cfg_t cfg = *cfg_ptr;
     cfg.is_standalone = true;
-    cfg.x += 0;//output->window.i_x; FIXME
-    cfg.y += 0;//output->window.i_y;
 
     return vout_NewDisplayWindow(osys->vout, &cfg);
 }
