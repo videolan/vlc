9c9f912bd3574865eb831116cf43ecccd188db41
Rémi Denis-Courmont <remi@remlab.net>
2014-10-11 22:28:22 +0300

    vmem: remove stub controls

--- modules/video_output/vmem.c
+++ modules/video_output/vmem.c
@@ -313,20 +313,8 @@ static void Display(vout_display_t *vd, picture_t *picture, subpicture_t *subpic
 
 static int Control(vout_display_t *vd, int query, va_list args)
 {
-    switch (query) {
-    case VOUT_DISPLAY_CHANGE_FULLSCREEN:
-    case VOUT_DISPLAY_CHANGE_DISPLAY_SIZE: {
-        const vout_display_cfg_t *cfg = va_arg(args, const vout_display_cfg_t *);
-        if (cfg->display.width  != vd->fmt.i_width ||
-            cfg->display.height != vd->fmt.i_height)
-            return VLC_EGENERIC;
-        if (cfg->is_fullscreen)
-            return VLC_EGENERIC;
-        return VLC_SUCCESS;
-    }
-    default:
-        return VLC_EGENERIC;
-    }
+    (void) vd; (void) query; (void) args;
+    return VLC_EGENERIC;
 }
 
 /* */
