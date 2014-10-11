e0b809180e3f2984e0e9d71aaa19b3eda70e4ab8
Rémi Denis-Courmont <remi@remlab.net>
2014-10-11 22:28:32 +0300

    yuv: remove stub controls

--- modules/video_output/yuv.c
+++ modules/video_output/yuv.c
@@ -282,16 +282,6 @@ static void Display(vout_display_t *vd, picture_t *picture, subpicture_t *subpic
 
 static int Control(vout_display_t *vd, int query, va_list args)
 {
-    VLC_UNUSED(vd);
-    switch (query) {
-    case VOUT_DISPLAY_CHANGE_FULLSCREEN: {
-        const vout_display_cfg_t *cfg = va_arg(args, const vout_display_cfg_t *);
-        if (cfg->is_fullscreen)
-            return VLC_EGENERIC;
-        return VLC_SUCCESS;
-    }
-    default:
-        return VLC_EGENERIC;
-    }
+    (void) vd; (void) query; (void) args;
+    return VLC_EGENERIC;
 }
-
