a7cf217db750e4bc7fe5c317ffd163599580abc4
Rémi Denis-Courmont <remi@remlab.net>
2014-10-12 18:55:05 +0300

    ios: remove incorrect handling of window state change
    The parameter is not "const vout_display_cfg_t *". This could not work.

--- modules/video_output/ios2.m
+++ modules/video_output/ios2.m
@@ -264,7 +264,6 @@ static int Control(vout_display_t *vd, int query, va_list ap)
             return VLC_EGENERIC;
 
         case VOUT_DISPLAY_CHANGE_FULLSCREEN:
-        case VOUT_DISPLAY_CHANGE_WINDOW_STATE:
         case VOUT_DISPLAY_CHANGE_DISPLAY_FILLED:
         case VOUT_DISPLAY_CHANGE_ZOOM:
         case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
