80099418383178d26030ee461026d325a76a0db1
Rémi Denis-Courmont <remi@remlab.net>
2014-10-13 19:37:52 +0300

    vout: set fullscreen mode window straight away

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -177,6 +177,8 @@ static vout_thread_t *VoutCreate(vlc_object_t *object,
         vout_window_t *window = vout_display_window_New(vout, &wcfg);
         if (window != NULL)
         {
+            if (var_InheritBool(vout, "fullscreen"))
+                vout_window_SetFullScreen(window, true);
             if (var_InheritBool(vout, "video-wallpaper"))
                 vout_window_SetState(window, VOUT_WINDOW_STATE_BELOW);
             else if (var_InheritBool(vout, "video-on-top"))
