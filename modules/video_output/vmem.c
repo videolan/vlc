0887539916acdb4276eed910cf88178084d9dba6
Rémi Denis-Courmont <remi@remlab.net>
2014-11-03 18:10:15 +0200

    vmem: unlock before display
    The documentation promises that the picture is unlocked before it is
    displayed, so actually do that.

--- modules/video_output/vmem.c
+++ modules/video_output/vmem.c
@@ -303,11 +303,13 @@ static picture_pool_t *Pool(vout_display_t *vd, unsigned count)
 static void Display(vout_display_t *vd, picture_t *picture, subpicture_t *subpicture)
 {
     vout_display_sys_t *sys = vd->sys;
+    void *id = picture->p_sys->id;
 
     assert(!picture_IsReferenced(picture));
-    if (sys->display != NULL)
-        sys->display(sys->opaque, picture->p_sys->id);
     picture_Release(picture);
+
+    if (sys->display != NULL)
+        sys->display(sys->opaque, id);
     VLC_UNUSED(subpicture);
 }
 
