5e7d42f71adcbc81c539b2e511e799a59b868d84
Jean-Baptiste Kempf <jb@videolan.org>
2014-11-04 17:15:40 +0100

    Add .au extension

--- include/vlc_interface.h
+++ include/vlc_interface.h
@@ -184,7 +184,7 @@ typedef enum vlc_dialog {
 #define INTF_ABOUT_MSG LICENSE_MSG
 
 #define EXTENSIONS_AUDIO_CSV "3ga", "669", "a52", "aac", "ac3", "adt", "adts", "aif", "aifc", "aiff", \
-                         "amr", "aob", "ape", "awb", "caf", "dts", "flac", "it", "kar", \
+                         "amr", "aob", "ape", "au", "awb", "caf", "dts", "flac", "it", "kar", \
                          "m4a", "m4b", "m4p", "m5p", "mka", "mlp", "mod", "mpa", "mp1", "mp2", "mp3", "mpc", "mpga", "mus", \
                          "oga", "ogg", "oma", "opus", "qcp", "ra", "rmi", "s3m", "sid", "spx", "thd", "tta", \
                          "voc", "vqf", "w64", "wav", "wma", "wv", "xa", "xm"
@@ -212,6 +212,7 @@ typedef enum vlc_dialog {
     "*.amr;" \
     "*.aob;" \
     "*.ape;" \
+    "*.au;" \
     "*.awb;" \
     "*.caf;" \
     "*.dts;" \
