aacca35310f0395b6715d282548ff8221beee836
Jean-Baptiste Kempf <jb@videolan.org>
2014-11-11 12:45:52 +0100

    Koreus: improve parsing and improve HTTPS
    Close #12744
    
    Thanks to Ruito

--- share/lua/playlist/koreus.lua
+++ share/lua/playlist/koreus.lua
@@ -19,7 +19,7 @@
 
 -- Probe function.
 function probe()
-    if vlc.access ~= "http" then
+    if vlc.access ~= "http" and vlc.access ~= "https" then
         return false
     end
 	koreus_site = string.match( vlc.path, "koreus" )
@@ -52,10 +52,37 @@ function parse()
 			_,_,arturl = string.find( line, "href=\"(.-)\"" )
 		end
 
-        vid_url = string.match( line, '(http://embed%.koreus%.com/%d+/%d+/[%w-]*%.mp4)' )
+		vid_url = string.match( line, '(http://embed%.koreus%.com/%d+/%d+/[%w-]*%.mp4)' )
 		if vid_url then
-			return { { path = vid_url; name = name; description = description; artist = artist; arturl = arturl } }
+			path_url = vid_url
 		end
+
+        vid_url_hd = string.match( line, '(http://embed%.koreus%.com/%d+/%d+/[%w-]*%-hd%.mp4)' )
+		if vid_url_hd then
+			path_url_hd = vid_url_hd
+		end
+
+		vid_url_webm = string.match( line, '(http://embed%.koreus%.com/%d+/%d+/[%w-]*%.webm)' )
+		if vid_url_webm then
+			path_url_webm = vid_url_webm
+		end
+
+		vid_url_flv = string.match( line, '(http://embed%.koreus%.com/%d+/%d+/[%w-]*%.flv)' )
+		if vid_ulr_flv then
+			path_url_flv = vid_url_flv
+		end
+
 	end
-    return {}
+
+	if path_url_hd then
+		return { { path = path_url_hd; name = name; description = description; artist = artist; arturl = arturl } }
+	elseif path_url then
+		return { { path = path_url; name = name; description = description; artist = artist; arturl = arturl } }
+	elseif path_url_webm then
+		return { { path = path_url_webm; name = name; description = description; artist = artist; arturl = arturl } }
+	elseif path_url_flv then
+		return { { path = path_url_flv; name = name; description = description; artist = artist; arturl = arturl } }
+	else
+		return {}
+	end
 end
