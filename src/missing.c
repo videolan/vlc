193b7447cad5439171e6fc7916c133f12048bc61
Jean-Baptiste Kempf <jb@videolan.org>
2014-11-15 12:50:20 +0100

    Fix compilation when --disable-sout

--- src/missing.c
+++ src/missing.c
@@ -295,7 +295,7 @@ void sout_MuxDeleteStream (sout_mux_t *mux, sout_input_t *input)
     assert (0);
 }
 
-int sout_MuxGetStream (sout_mux_t *p_mux, int i_blocks, mtime_t *pi_dts)
+int sout_MuxGetStream (sout_mux_t *p_mux, unsigned int i_blocks, mtime_t *pi_dts)
 {
     VLC_UNUSED (p_mux); VLC_UNUSED (i_blocks); VLC_UNUSED (pi_dts);
     assert (0);
