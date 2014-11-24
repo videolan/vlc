5c8d3f0602e8a0aaa64475f6bb48ff59a823aacd
Rémi Denis-Courmont <remi@remlab.net>
2014-11-24 19:16:42 +0200

    win32: remove duplicate check in vlc_cond_signal()
    The same test is done in the underlying vlc_cond_broadcast().

--- src/win32/thread.c
+++ src/win32/thread.c
@@ -226,9 +226,6 @@ void vlc_cond_destroy (vlc_cond_t *p_condvar)
 
 void vlc_cond_signal (vlc_cond_t *p_condvar)
 {
-    if (!p_condvar->clock)
-        return;
-
     /* This is suboptimal but works. */
     vlc_cond_broadcast (p_condvar);
 }
