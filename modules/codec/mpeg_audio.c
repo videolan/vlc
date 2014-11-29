ac5caa2c888144ff27c30e0eb26536f7ffedc8e5
Rémi Denis-Courmont <remi@remlab.net>
2014-11-29 13:14:22 +0200

    mad: clean up build rules

--- modules/codec/mpeg_audio.c
+++ modules/codec/mpeg_audio.c
@@ -161,7 +161,7 @@ static int Open( vlc_object_t *p_this )
 static int OpenDecoder( vlc_object_t *p_this )
 {
     /* HACK: Don't use this codec if we don't have an mpga audio filter */
-    if( !module_exists( "mpgatofixed32" ) )
+    if( !module_exists( "mad" ) )
         return VLC_EGENERIC;
 
     return Open( p_this );
