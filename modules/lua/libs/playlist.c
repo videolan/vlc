6efc10f0a54c3164f5df8ca50482fb35cd9d3d12
Rémi Denis-Courmont <remi@remlab.net>
2014-12-15 17:39:39 +0200

    lua/playlist: do not handle impossible error

--- modules/lua/libs/playlist.c
+++ modules/lua/libs/playlist.c
@@ -130,11 +130,10 @@ static int vlclua_playlist_gotoitem( lua_State * L )
     int i_id = luaL_checkint( L, 1 );
     playlist_t *p_playlist = vlclua_get_playlist_internal( L );
     PL_LOCK;
-    int i_ret = playlist_Control( p_playlist, PLAYLIST_VIEWPLAY,
-                                  true, NULL,
-                                  playlist_ItemGetById( p_playlist, i_id ) );
+    playlist_Control( p_playlist, PLAYLIST_VIEWPLAY, true, NULL,
+                      playlist_ItemGetById( p_playlist, i_id ) );
     PL_UNLOCK;
-    return vlclua_push_ret( L, i_ret );
+    return vlclua_push_ret( L, VLC_SUCCESS );
 }
 
 static int vlclua_playlist_delete( lua_State * L )
