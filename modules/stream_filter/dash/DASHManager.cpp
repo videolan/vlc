e36f9d18bdb4d62ed770623564107aaffcb79a41
Jean-Baptiste Kempf <jb@videolan.org>
2014-12-18 23:10:15 +0100

    Dash: fix Android compilation

--- modules/stream_filter/dash/DASHManager.cpp
+++ modules/stream_filter/dash/DASHManager.cpp
@@ -21,11 +21,11 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
+#define __STDC_CONSTANT_MACROS
+
 #ifdef HAVE_CONFIG_H
 # include "config.h"
 #endif
-
-#define __STDC_CONSTANT_MACROS
 
 #include "DASHManager.h"
 #include "adaptationlogic/AdaptationLogicFactory.h"
