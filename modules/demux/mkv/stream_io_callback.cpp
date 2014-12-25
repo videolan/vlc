5aa03f3785ecd2e3c871cc22f0ab83d90640b45d
Francois Cartegnie <fcvlcdev@free.fr>
2014-12-25 17:40:39 +0100

    demux: mkv: don't seek on current pos

--- modules/demux/mkv/stream_io_callback.cpp
+++ modules/demux/mkv/stream_io_callback.cpp
@@ -47,6 +47,7 @@ uint32 vlc_stream_io_callback::read( void *p_buffer, size_t i_size )
 void vlc_stream_io_callback::setFilePointer(int64_t i_offset, seek_mode mode )
 {
     int64_t i_pos, i_size;
+    int64_t i_current = stream_Tell( s );
 
     switch( mode )
     {
@@ -57,9 +58,12 @@ void vlc_stream_io_callback::setFilePointer(int64_t i_offset, seek_mode mode )
             i_pos = stream_Size( s ) - i_offset;
             break;
         default:
-            i_pos= stream_Tell( s ) + i_offset;
+            i_pos= i_current + i_offset;
             break;
     }
+
+    if(i_pos == i_current)
+        return;
 
     if( i_pos < 0 || ( ( i_size = stream_Size( s ) ) != 0 && i_pos >= i_size ) )
     {
