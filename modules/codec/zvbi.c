videolan/vlc

commit 29a4015294b64c2439df12862a38a1c971a1ccbd

telx/zvbi: initialize aspect ratio

modules/codec/zvbi.c
@@ -503,10 +503,7 @@ static subpicture_t *Subpicture( decoder_t *p_dec, video_format_t *p_fmt,
         return NULL;
     }
 
-    memset( &fmt, 0, sizeof(video_format_t) );
-    fmt.i_chroma = b_text ? VLC_CODEC_TEXT : VLC_CODEC_RGBA;
-    fmt.i_sar_num = 0;
-    fmt.i_sar_den = 1;
+    video_format_Init(&fmt, b_text ? VLC_CODEC_TEXT : VLC_CODEC_RGBA);
     if( b_text )
     {
         fmt.i_bits_per_pixel = 0;

modules/codec/telx.c
@@ -693,8 +693,7 @@ static subpicture_t *Decode( decoder_t *p_dec, block_t **pp_block )
     }
  
     /* Create a new subpicture region */
-    memset( &fmt, 0, sizeof(video_format_t) );
-    fmt.i_chroma = VLC_CODEC_TEXT;
+    video_format_Init(&fmt, VLC_CODEC_TEXT);
     fmt.i_width = fmt.i_height = 0;
     fmt.i_x_offset = fmt.i_y_offset = 0;
     p_spu->p_region = subpicture_region_New( &fmt );

commit 47f215557953d40ed0d0b38a7834917e9117cc2b

Fix teletext page aspect ratio
It was broken by 29a4015294, which should only have modified text "subpictures"
Closes #13050
Tested-by: Julian Scheel <julian@jusst.de>

modules/codec/zvbi.c
@@ -513,6 +513,7 @@ static subpicture_t *Subpicture( decoder_t *p_dec, video_format_t *p_fmt,
         fmt.i_width = fmt.i_visible_width = i_columns * 12;
         fmt.i_height = fmt.i_visible_height = i_rows * 10;
         fmt.i_bits_per_pixel = 32;
+        fmt.i_sar_num = fmt.i_sar_den = 0; /* let the vout set the correct AR */
     }
     fmt.i_x_offset = fmt.i_y_offset = 0;
 