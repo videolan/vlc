987df6dc0d363f3dafee3ec430002d8d904919ee
Rémi Denis-Courmont <remi@remlab.net>
2015-02-16 19:51:39 +0200

    sout_standard: remove unused typedef

--- modules/stream_out/standard.c
+++ modules/stream_out/standard.c
@@ -128,10 +128,6 @@ struct sout_stream_sys_t
     session_descriptor_t *p_session;
 };
 
-struct sout_stream_id_sys_t
-{
-};
-
 static sout_stream_id_sys_t * Add( sout_stream_t *p_stream, es_format_t *p_fmt )
 {
     return (sout_stream_id_sys_t*)sout_MuxAddStream( p_stream->p_sys->p_mux, p_fmt );
