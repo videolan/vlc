b2acddfdbb32aeea04bc205c8765f4dbd130df5d
Jean-Baptiste Kempf <jb@videolan.org>
2015-02-26 21:15:55 +0100

    Revert "Qt: force main window maximum size to be some big enough"
    This reverts commit b5a43debee2232ca9b3be252f58c0520fcc413b8.
    
    This is not enough and introduces more regressions

--- modules/gui/qt4/main_interface.cpp
+++ modules/gui/qt4/main_interface.cpp
@@ -260,7 +260,6 @@ MainInterface::MainInterface( intf_thread_t *_p_intf ) : QVLCMW( _p_intf )
     if( b_minimalView )
         toggleMinimalView( true );
 
-    setMaximumSize( QSize( 16386, 16386 ) );
     computeMinimumSize();
 }
 
