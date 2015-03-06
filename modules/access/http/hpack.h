7b645ddc0c51f3a36e457195f6a991a1635809a4
Rémi Denis-Courmont <remi@remlab.net>
2015-03-06 23:21:32 +0200

    HPACK Header Compression HTTP/2 minimalistic encoder

--- modules/access/http/hpack.h
+++ modules/access/http/hpack.h
@@ -25,3 +25,8 @@ void hpack_decode_destroy(struct hpack_decoder *);
 
 int hpack_decode(struct hpack_decoder *dec, const uint8_t *data,
                  size_t length, char *headers[][2], unsigned max);
+
+size_t hpack_encode_hdr_neverindex(uint8_t *restrict buf, size_t size,
+                                   const char *name, const char *value);
+size_t hpack_encode(uint8_t *restrict buf, size_t size,
+                    const char *const headers[][2], unsigned count);
