eadf306345f0e545c51af9a49925756e258538ba
Steve Lhomme <robUx4@videolabs.io>
2015-03-10 10:48:47 +0000

    MKV: VP9 has incremental frames, no B-frames
    Fixes #11690
    
    #11690 still fails to play with avcodec after that fix but is fine with libvpx
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/demux/mkv/matroska_segment_parse.cpp
+++ modules/demux/mkv/matroska_segment_parse.cpp
@@ -1373,6 +1373,7 @@ int32_t matroska_segment_c::TrackInit( mkv_track_t * p_tk )
     {
         p_tk->fmt.i_codec = VLC_CODEC_VP9;
         p_tk->fmt.b_packetized = false;
+        p_tk->b_pts_only = true;
         fill_extra_data( p_tk, 0 );
     }
     else if( !strncmp( p_tk->psz_codec, "V_MPEG4", 7 ) )
