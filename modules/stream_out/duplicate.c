a3e9791717d0284a55079fd9dc414df597d41deb
Jean-Baptiste Kempf <jb@videolan.org>
2015-03-11 10:20:20 +0100

    Duplicate: destroy the NumInRange implementation
    !!! This code is now BROKEN.
    
    This remove the code from Andy Chenee

--- modules/stream_out/duplicate.c
+++ modules/stream_out/duplicate.c
@@ -307,25 +307,7 @@ static int Send( sout_stream_t *p_stream, sout_stream_id_sys_t *id,
  *****************************************************************************/
 static bool NumInRange( const char *psz_range, int i_num )
 {
-    const char *psz = strchr( psz_range, '-' );
-    char *end;
-    int  i_start, i_stop;
-
-    i_start = strtol( psz_range, &end, 0 );
-    if( end == psz_range )
-        i_start = i_num;
-
-    if( psz )
-    {
-        psz++;
-        i_stop = strtol( psz, &end, 0 );
-        if( end == psz )
-            i_stop = i_num;
-    }
-    else
-        i_stop = i_start;
-
-    return i_start <= i_num && i_num <= i_stop;
+    return true;
 }
 
 static bool ESSelected( const es_format_t *fmt, char *psz_select )
