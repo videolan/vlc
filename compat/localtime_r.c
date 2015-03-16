d60d5102b861525edf1ba22f9e6c6b3747a4ed3e
Rémi Denis-Courmont <remi@remlab.net>
2015-03-16 19:20:44 +0200

    compat: fix localtime_r() replacement
     - Do not clobber thread-specific state on Windows
     - Be thread-safe on non-Windows

--- compat/localtime_r.c
+++ compat/localtime_r.c
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * localtime_r.c: POSIX localtime_r() replacement
  *****************************************************************************
- * Copyright © 1998-2008 VLC authors and VideoLAN
+ * Copyright © 1998-2015 VLC authors and VideoLAN
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -18,20 +18,26 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#if (__STDC_VERSION__ >= 201112L)
+# define __STDC_WANT_LIB_EXT1__ 1
+#endif
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 
+#include <errno.h>
 #include <time.h>
 
 /* If localtime_r() is not provided, we assume localtime() uses
  * thread-specific storage. */
 struct tm *localtime_r (const time_t *timep, struct tm *result)
 {
-    struct tm *s = localtime (timep);
-    if (s == NULL)
-        return NULL;
-
-    *result = *s;
-    return result;
+#if (__STDC_VERSION__ >= 201112L)
+    return localtime_s(timep, result);
+#elif defined (_WIN32)
+    return ((errno = localtime_s(result, timep)) == 0) ? result : NULL;
+#else
+# warning localtime_r() not implemented!
+    return gmtime_r(timep, result);
+#endif
 }
