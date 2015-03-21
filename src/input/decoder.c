videolan/vlc

commit 621400186a49b282197267e588a030b425829a64

decoder: remove tautology, add asserts to waiting code

src/input/decoder.c
@@ -516,17 +516,15 @@ void input_DecoderStartWait( decoder_t *p_dec )
 {
     decoder_owner_sys_t *p_owner = p_dec->p_owner;
 
-    vlc_mutex_lock( &p_owner->lock );
+    assert( !p_owner->b_waiting );
 
+    vlc_mutex_lock( &p_owner->lock );
     DecoderFlush( p_dec );
 
     p_owner->b_first = true;
     p_owner->b_has_data = false;
-
     p_owner->b_waiting = true;
-
     vlc_cond_signal( &p_owner->wait_request );
-
     vlc_mutex_unlock( &p_owner->lock );
 }
 
@@ -534,12 +532,11 @@ void input_DecoderStopWait( decoder_t *p_dec )
 {
     decoder_owner_sys_t *p_owner = p_dec->p_owner;
 
+    assert( p_owner->b_waiting );
+
     vlc_mutex_lock( &p_owner->lock );
-
     p_owner->b_waiting = false;
-
     vlc_cond_signal( &p_owner->wait_request );
-
     vlc_mutex_unlock( &p_owner->lock );
 }
 
@@ -547,14 +544,14 @@ void input_DecoderWait( decoder_t *p_dec )
 {
     decoder_owner_sys_t *p_owner = p_dec->p_owner;
 
-    vlc_mutex_lock( &p_owner->lock );
+    assert( p_owner->b_waiting );
 
-    while( p_owner->b_waiting && !p_owner->b_has_data )
+    vlc_mutex_lock( &p_owner->lock );
+    while( !p_owner->b_has_data )
     {
         block_FifoWake( p_owner->p_fifo );
         vlc_cond_wait( &p_owner->wait_acknowledge, &p_owner->lock );
     }
-
     vlc_mutex_unlock( &p_owner->lock );
 }
 

commit cc116115681a9c484ee23592ef16d724f843ae70

es_out: do not start buffering when already buferring
This fixes an assertion failure.
Regression from 621400186a49b282197267e588a030b425829a64.

src/input/decoder.c
@@ -533,8 +533,6 @@ void input_DecoderStartWait( decoder_t *p_dec )
 
     assert( !p_owner->b_waiting );
 
-    input_DecoderFlush( p_dec );
-
     vlc_mutex_lock( &p_owner->lock );
     p_owner->b_first = true;
     p_owner->b_has_data = false;

src/input/es_out.c
@@ -573,13 +573,16 @@ static void EsOutChangePosition( es_out_t *out )
     {
         es_out_id_t *p_es = p_sys->es[i];
 
-        if( !p_es->p_dec )
-            continue;
-
-        input_DecoderStartWait( p_es->p_dec );
-
-        if( p_es->p_dec_record )
-            input_DecoderStartWait( p_es->p_dec_record );
+        if( p_es->p_dec != NULL )
+        {
+            input_DecoderFlush( p_es->p_dec );
+            if( !p_sys->b_buffering )
+            {
+                input_DecoderStartWait( p_es->p_dec );
+                if( p_es->p_dec_record != NULL )
+                    input_DecoderStartWait( p_es->p_dec_record );
+            }
+        }
     }
 
     for( int i = 0; i < p_sys->i_pgrm; i++ )