7f649acf5275749181c1c9336c1fb50a53de783a
Jean-Baptiste Kempf <jb@videolan.org>
2015-04-06 18:13:41 +0200

    Subpicture: remove NULL assignment after calloc
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/misc/subpicture.c
+++ src/misc/subpicture.c
@@ -220,11 +220,6 @@ subpicture_region_t *subpicture_region_New( const video_format_t *p_fmt )
             *p_region->fmt.p_palette = *p_fmt->p_palette;
     }
     p_region->i_alpha = 0xff;
-    p_region->p_next = NULL;
-    p_region->p_private = NULL;
-    p_region->psz_text = NULL;
-    p_region->p_style = NULL;
-    p_region->p_picture = NULL;
 
     if( p_fmt->i_chroma == VLC_CODEC_TEXT )
         return p_region;
