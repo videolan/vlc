3774ef9abe576730950e47697b7068e73cdde52d
Steve Lhomme <robux4@videolabs.io>
2015-04-20 12:57:44 +0200

    misc: NV12 has 12 bits per pixel like I420
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/misc/es_format.c
+++ src/misc/es_format.c
@@ -176,6 +176,7 @@ void video_format_Setup( video_format_t *p_fmt, vlc_fourcc_t i_chroma,
     case VLC_CODEC_YV12:
     case VLC_CODEC_I420:
     case VLC_CODEC_J420:
+    case VLC_CODEC_NV12:
         p_fmt->i_bits_per_pixel = 12;
         break;
     case VLC_CODEC_YV9:
