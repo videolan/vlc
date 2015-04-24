5d4f1f915d52de83359930b22f0984b788792c48
Rémi Denis-Courmont <remi@remlab.net>
2015-04-24 22:33:22 +0300

    vaapi: remove tautology
    The configuration ID must exist in this code path.

--- modules/codec/avcodec/vaapi.c
+++ modules/codec/avcodec/vaapi.c
@@ -420,8 +420,7 @@ static void Delete( vlc_va_t *va, AVCodecContext *avctx )
     if( sys->i_surface_width || sys->i_surface_height )
         DestroySurfaces( sys );
 
-    if( sys->i_config_id != VA_INVALID_ID )
-        vaDestroyConfig( sys->p_display, sys->i_config_id );
+    vaDestroyConfig(sys->p_display, sys->i_config_id);
     vaTerminate( sys->p_display );
 #ifdef VLC_VA_BACKEND_XLIB
     XCloseDisplay( sys->p_display_x11 );
