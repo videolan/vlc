824e18312671f975a1da0da7c1a605f6ddc648c9
Ramon Gabarró <calanor@gmail.com>
2015-05-08 14:17:43 +0200

    AudioBar: Lower the threshold for alarm
    It makes more sense post move to the IEC scale
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/audio_filter/audiobargraph_a.c
+++ modules/audio_filter/audiobargraph_a.c
@@ -79,7 +79,7 @@ vlc_module_begin ()
     add_integer( CFG_PREFIX "bargraph_repetition", 4, BARGRAPH_REPETITION_TEXT, BARGRAPH_REPETITION_LONGTEXT, false )
     add_integer( CFG_PREFIX "silence", 1, SILENCE_TEXT, SILENCE_LONGTEXT, false ) // FIXME: this is a bool
     add_integer( CFG_PREFIX "time_window", 5000, TIME_WINDOW_TEXT, TIME_WINDOW_LONGTEXT, false )
-    add_float( CFG_PREFIX "alarm_threshold", 0.1, ALARM_THRESHOLD_TEXT, ALARM_THRESHOLD_LONGTEXT, false )
+    add_float( CFG_PREFIX "alarm_threshold", 0.02, ALARM_THRESHOLD_TEXT, ALARM_THRESHOLD_LONGTEXT, false )
     add_integer( CFG_PREFIX "repetition_time", 2000, REPETITION_TIME_TEXT, REPETITION_TIME_LONGTEXT, false )
     add_obsolete_integer( CFG_PREFIX "connection_reset" )
 
