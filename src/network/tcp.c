7d28f8a24c75fd5ffcd393f047208a0d7f55a187
Rémi Denis-Courmont <remi@remlab.net>
2015-05-09 22:41:23 +0300

    network: remove useless vlc_object_waitpipe() call in net_Accept()
    None of the call sites use the input thread and wait pipe.

--- src/network/tcp.c
+++ src/network/tcp.c
@@ -270,26 +270,23 @@ int net_AcceptSingle (vlc_object_t *obj, int lfd)
  */
 int net_Accept (vlc_object_t *p_this, int *pi_fd)
 {
-    int evfd = vlc_object_waitpipe (p_this);
-
     assert (pi_fd != NULL);
 
     unsigned n = 0;
     while (pi_fd[n] != -1)
         n++;
-    struct pollfd ufd[n + 1];
 
+    struct pollfd ufd[n];
     /* Initialize file descriptor set */
-    for (unsigned i = 0; i <= n; i++)
+    for (unsigned i = 0; i < n; i++)
     {
-        ufd[i].fd = (i < n) ? pi_fd[i] : evfd;
+        ufd[i].fd = pi_fd[i];
         ufd[i].events = POLLIN;
     }
-    ufd[n].revents = 0;
 
     for (;;)
     {
-        while (poll (ufd, n + (evfd != -1), -1) == -1)
+        while (poll (ufd, n, -1) == -1)
         {
             if (net_errno != EINTR)
             {
@@ -315,12 +312,6 @@ int net_Accept (vlc_object_t *p_this, int *pi_fd)
             memmove (pi_fd + i, pi_fd + i + 1, n - (i + 1));
             pi_fd[n - 1] = sfd;
             return fd;
-        }
-
-        if (ufd[n].revents)
-        {
-            errno = EINTR;
-            break;
         }
     }
     return -1;
