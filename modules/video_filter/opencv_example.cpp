b82416d7000a993b33e903095a590fe32212a85e
Samuel Martin <s.martin49@gmail.com>
2015-05-24 14:30:10 +0200

    opencv_example: add missing #include statements
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/video_filter/opencv_example.cpp
+++ modules/video_filter/opencv_example.cpp
@@ -41,6 +41,8 @@
 
 #include <opencv2/core/core_c.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 
 /*****************************************************************************
