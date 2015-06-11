351869f2f4827fea4b00d7b6e89f2bb419b8a075
Rémi Denis-Courmont <remi@remlab.net>
2015-06-11 19:21:06 +0300

    fourcc: derecursify the Find() function

--- src/misc/fourcc.c
+++ src/misc/fourcc.c
@@ -1666,11 +1666,11 @@ static entry_t Find( int i_cat, vlc_fourcc_t i_fourcc )
         return Lookup( p_list_spu, i_fourcc );
 
     default:
-        e = Find( VIDEO_ES, i_fourcc );
+        e = Lookup( p_list_video, i_fourcc );
         if( CreateFourcc( e.p_class ) == 0 )
-            e = Find( AUDIO_ES, i_fourcc );
+            e = Lookup( p_list_audio, i_fourcc );
         if( CreateFourcc( e.p_class ) == 0 )
-            e = Find( SPU_ES, i_fourcc );
+            e = Lookup( p_list_spu, i_fourcc );
         return e;
     }
 }
