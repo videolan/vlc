ebe6e7de1bbdf7181cdbeca61b5591f9b31ac711
Jean-Baptiste Kempf <jb@videolan.org>
2015-06-13 18:15:06 +0200

    Lua.cli: improve lua 5.3 compatibility
    Close #14803

--- share/lua/intf/cli.lua
+++ share/lua/intf/cli.lua
@@ -343,7 +343,7 @@ function help(name,client,arg)
                 end
                 if val.args then str = str .. " " .. val.args end
                 if #str%2 == 1 then str = str .. " " end
-                str = str .. string.rep(" .",(width-(#str+#val.help)-1)/2)
+                str = str .. string.rep(" .",math.floor((width-(#str+#val.help)-1)/2))
                 str = str .. string.rep(" ",width-#str-#val.help) .. val.help
             end
             client:append(str)
