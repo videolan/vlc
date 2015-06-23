videolan/vlc

commit e3b809c1e4d16d858732d467b5ecd10b81d16db8

misc/image: fixed overwriting of correct visible dimensions
In some cases fmt_in already contains correct i_visible_width and i_visible_height values.
This patch fixes incorrect overwriting of valid values.

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

src/misc/image.c
@@ -707,33 +707,38 @@ static encoder_t *CreateEncoder( vlc_object_t *p_this, video_format_t *fmt_in,
     p_enc->p_module = NULL;
     es_format_Init( &p_enc->fmt_in, VIDEO_ES, fmt_in->i_chroma );
     p_enc->fmt_in.video = *fmt_in;
-    if( fmt_out->i_width > 0 && fmt_out->i_height > 0 )
-    {
-        p_enc->fmt_in.video.i_width = fmt_out->i_width;
-        p_enc->fmt_in.video.i_height = fmt_out->i_height;
 
-        if( fmt_out->i_visible_width > 0 &&
-            fmt_out->i_visible_height > 0 )
-        {
-            p_enc->fmt_in.video.i_visible_width = fmt_out->i_visible_width;
-            p_enc->fmt_in.video.i_visible_height = fmt_out->i_visible_height;
-        }
-        else
-        {
-            p_enc->fmt_in.video.i_visible_width = fmt_out->i_width;
-            p_enc->fmt_in.video.i_visible_height = fmt_out->i_height;
-        }
-    }
-    else if( fmt_out->i_sar_num && fmt_out->i_sar_den &&
-             fmt_out->i_sar_num * fmt_in->i_sar_den !=
-             fmt_out->i_sar_den * fmt_in->i_sar_num )
+    if( p_enc->fmt_in.video.i_visible_width == 0 ||
+        p_enc->fmt_in.video.i_visible_height == 0 )
     {
-        p_enc->fmt_in.video.i_width =
-            fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den * fmt_in->i_width /
-            fmt_in->i_sar_den / fmt_out->i_sar_num;
-        p_enc->fmt_in.video.i_visible_width =
-            fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den *
-            fmt_in->i_visible_width / fmt_in->i_sar_den / fmt_out->i_sar_num;
+        if( fmt_out->i_width > 0 && fmt_out->i_height > 0 )
+        {
+            p_enc->fmt_in.video.i_width = fmt_out->i_width;
+            p_enc->fmt_in.video.i_height = fmt_out->i_height;
+
+            if( fmt_out->i_visible_width > 0 &&
+                fmt_out->i_visible_height > 0 )
+            {
+                p_enc->fmt_in.video.i_visible_width = fmt_out->i_visible_width;
+                p_enc->fmt_in.video.i_visible_height = fmt_out->i_visible_height;
+            }
+            else
+            {
+                p_enc->fmt_in.video.i_visible_width = fmt_out->i_width;
+                p_enc->fmt_in.video.i_visible_height = fmt_out->i_height;
+            }
+        }
+        else if( fmt_out->i_sar_num && fmt_out->i_sar_den &&
+                 fmt_out->i_sar_num * fmt_in->i_sar_den !=
+                 fmt_out->i_sar_den * fmt_in->i_sar_num )
+        {
+            p_enc->fmt_in.video.i_width =
+                fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den * fmt_in->i_width /
+                fmt_in->i_sar_den / fmt_out->i_sar_num;
+            p_enc->fmt_in.video.i_visible_width =
+                fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den *
+                fmt_in->i_visible_width / fmt_in->i_sar_den / fmt_out->i_sar_num;
+        }
     }
 
     p_enc->fmt_in.video.i_frame_rate = 25;

commit c342e2ef72c9ab83f5bd7add27b41b5de6eeab2c

src/image: correctly handle anamorphic content sizes (closes #13862)
This fixes a regression introduced in e3b809c1

src/misc/image.c
@@ -677,17 +677,16 @@ static encoder_t *CreateEncoder( vlc_object_t *p_this, video_format_t *fmt_in,
                 p_enc->fmt_in.video.i_visible_height = fmt_out->i_height;
             }
         }
-        else if( fmt_out->i_sar_num && fmt_out->i_sar_den &&
-                 fmt_out->i_sar_num * fmt_in->i_sar_den !=
-                 fmt_out->i_sar_den * fmt_in->i_sar_num )
-        {
-            p_enc->fmt_in.video.i_width =
-                fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den * fmt_in->i_width /
-                fmt_in->i_sar_den / fmt_out->i_sar_num;
-            p_enc->fmt_in.video.i_visible_width =
-                fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den *
-                fmt_in->i_visible_width / fmt_in->i_sar_den / fmt_out->i_sar_num;
-        }
+    } else if( fmt_out->i_sar_num && fmt_out->i_sar_den &&
+               fmt_out->i_sar_num * fmt_in->i_sar_den !=
+               fmt_out->i_sar_den * fmt_in->i_sar_num )
+    {
+        p_enc->fmt_in.video.i_width =
+            fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den * fmt_in->i_width /
+            fmt_in->i_sar_den / fmt_out->i_sar_num;
+        p_enc->fmt_in.video.i_visible_width =
+            fmt_in->i_sar_num * (int64_t)fmt_out->i_sar_den *
+            fmt_in->i_visible_width / fmt_in->i_sar_den / fmt_out->i_sar_num;
     }
 
     p_enc->fmt_in.video.i_frame_rate = 25;