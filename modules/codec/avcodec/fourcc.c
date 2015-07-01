6d383e69f980e97a61aab57c98955119eb266b93
Jean-Baptiste Kempf <jb@videolan.org>
2015-07-01 15:52:38 +0200

    Map G2M3 and G2M2
    Close #2327

--- modules/codec/avcodec/fourcc.c
+++ modules/codec/avcodec/fourcc.c
@@ -259,7 +259,10 @@ static const struct
 #if LIBAVCODEC_VERSION_CHECK( 56, 23, 0, 35, 100 )
     { VLC_CODEC_HQ_HQA, AV_CODEC_ID_HQ_HQA, VIDEO_ES },
 #endif
-
+#if LIBAVCODEC_VERSION_CHECK( 56, 31, 0, 46, 100 )
+    { VLC_CODEC_G2M2, AV_CODEC_ID_G2M, VIDEO_ES },
+    { VLC_CODEC_G2M3, AV_CODEC_ID_G2M, VIDEO_ES },
+#endif
 
     /* ffmpeg only: AV_CODEC_ID_BRENDER_PIX */
     /* ffmpeg only: AV_CODEC_ID_Y41P */
