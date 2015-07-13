35664fc827a235c9da796d83cea90479e6c6dbab
Rémi Denis-Courmont <remi@remlab.net>
2015-07-13 21:59:54 +0300

    timer: speed up test case

--- src/test/timer.c
+++ src/test/timer.c
@@ -22,12 +22,13 @@
 # include "config.h"
 #endif
 
-#include <vlc_common.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #undef NDEBUG
 #include <assert.h>
+
+#include <vlc_common.h>
+#undef msleep
 
 struct timer_data
 {
@@ -58,8 +59,8 @@ int main (void)
     assert (val == 0);
 
     /* Relative timer */
-    vlc_timer_schedule (data.timer, false, 1, CLOCK_FREQ / 10);
-    msleep (CLOCK_FREQ);
+    vlc_timer_schedule (data.timer, false, 1, CLOCK_FREQ / 100);
+    msleep (CLOCK_FREQ / 10);
     vlc_mutex_lock (&data.lock);
     data.count += vlc_timer_getoverrun (data.timer);
     printf ("Count = %u\n", data.count);
@@ -71,8 +72,8 @@ int main (void)
     /* Absolute timer */
     mtime_t now = mdate ();
 
-    vlc_timer_schedule (data.timer, true, now, CLOCK_FREQ / 10);
-    msleep (CLOCK_FREQ);
+    vlc_timer_schedule (data.timer, true, now, CLOCK_FREQ / 100);
+    msleep (CLOCK_FREQ / 10);
     vlc_mutex_lock (&data.lock);
     data.count += vlc_timer_getoverrun (data.timer);
     printf ("Count = %u\n", data.count);
