videolan/vlc

commit f450457875207163ddb4f435e0bb5dc868e35ee8

macosx: refactor main class object life management and main menu

modules/gui/macosx/intf.m
@@ -49,7 +49,6 @@
 #import "prefs.h"
 #import "playlist.h"
 #import "playlistinfo.h"
-#import "controls.h"
 #import "open.h"
 #import "bookmarks.h"
 #import "coredialogs.h"
@@ -73,6 +72,8 @@
  * Local prototypes.
  *****************************************************************************/
 
+static VLCMain *sharedInstance = nil;
+
 #pragma mark -
 #pragma mark VLC Interface Object Callbacks
 
@@ -91,11 +92,6 @@ int OpenIntf (vlc_object_t *p_this)
 
         [VLCVoutWindowController sharedInstance];
 
-        [NSBundle loadNibNamed:@"MainMenu" owner:NSApp];
-        [NSBundle loadNibNamed:@"MainWindow" owner:[VLCMain sharedInstance]];
-
-        [[[VLCMain sharedInstance] mainWindow] makeKeyAndOrderFront:nil];
-
         return VLC_SUCCESS;
     }
 }
@@ -105,6 +101,7 @@ void CloseIntf (vlc_object_t *p_this)
     @autoreleasepool {
         msg_Dbg(p_this, "Closing macosx interface");
         [[VLCMain sharedInstance] applicationWillTerminate:nil];
+        sharedInstance = nil;
     }
 }
 
@@ -151,8 +148,8 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     BOOL nib_main_loaded;       /* main nibfile */
     BOOL nib_open_loaded;       /* open nibfile */
     BOOL nib_about_loaded;      /* about nibfile */
-    BOOL nib_prefs_loaded;      /* preferences nibfile */
-    BOOL nib_info_loaded;       /* information panel nibfile */
+    BOOL nib_prefs_loaded;      /* preferences xibfile */
+    BOOL nib_sprefs_loaded;      /* simple preferences xibfile */
     BOOL nib_coredialogs_loaded; /* CoreDialogs nibfile */
     BOOL nib_bookmarks_loaded;   /* Bookmarks nibfile */
     BOOL b_active_videoplayback;
@@ -162,7 +159,6 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     VLCSimplePrefs *_sprefs;
     VLCOpen *_open;
     VLCCoreDialogProvider *_coredialogs;
-    VLCInfo *_info;
     VLCEyeTVController *_eyetv;
     VLCBookmarks *_bookmarks;
     VLCCoreInteraction *_coreinteraction;
@@ -185,7 +181,6 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
 + (VLCMain *)sharedInstance
 {
     static dispatch_once_t pred;
-    static VLCMain *sharedInstance = nil;
 
     dispatch_once(&pred, ^{
         sharedInstance = [VLCMain new];
@@ -200,9 +195,7 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
 
     p_intf = NULL;
 
-    _open = [[VLCOpen alloc] init];
-    _mainmenu = [VLCMainMenu sharedInstance];
-    _eyetv = [[VLCEyeTVController alloc] init];
+    [VLCApplication sharedApplication].delegate = self;
 
     /* announce our launch to a potential eyetv plugin */
     [[NSDistributedNotificationCenter defaultCenter] postNotificationName: @"VLCOSXGUIInit"
@@ -219,6 +212,20 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     return self;
 }
 
+- (void)dealloc
+{
+    _mainmenu = nil;
+    _prefs = nil;
+    _sprefs = nil;
+    _open = nil;
+    _coredialogs = nil;
+    _eyetv = nil;
+    _bookmarks = nil;
+    _coreinteraction = nil;
+    _resume_dialog = nil;
+    _input_manager = nil;
+}
+
 - (void)setIntf: (intf_thread_t *)p_mainintf
 {
     p_intf = p_mainintf;
@@ -229,13 +236,35 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     return p_intf;
 }
 
+- (void)applicationWillFinishLaunching:(NSNotification *)aNotification
+{
+    _open = [[VLCOpen alloc] init];
+    _mainmenu = [[VLCMainMenu alloc] init];
+    [NSBundle loadNibNamed:@"MainMenu" owner:_mainmenu];
+
+
+    [NSBundle loadNibNamed:@"MainWindow" owner:[VLCMain sharedInstance]];
+    [[[VLCMain sharedInstance] mainWindow] makeKeyAndOrderFront:nil];
+    _eyetv = [[VLCEyeTVController alloc] init];
+
+    _coreinteraction = [VLCCoreInteraction sharedInstance];
+
+    playlist_t * p_playlist = pl_Get(VLCIntf);
+    PL_LOCK;
+    items_at_launch = p_playlist->p_local_category->i_children;
+    PL_UNLOCK;
+
+#ifdef HAVE_SPARKLE
+    [[SUUpdater sharedUpdater] setDelegate:self];
+#endif
+}
+
 - (void)awakeFromNib
 {
     if (!p_intf) return;
     var_Create(p_intf, "intf-change", VLC_VAR_BOOL);
 
-    /* Check if we already did this once. Opening the other nibs calls it too,
-     because VLCMain is the owner */
+    /* Check if we already did this once */
     if (nib_main_loaded)
         return;
 
@@ -268,20 +297,6 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     }
 
     nib_main_loaded = TRUE;
-}
-
-- (void)applicationWillFinishLaunching:(NSNotification *)aNotification
-{
-    _coreinteraction = [VLCCoreInteraction sharedInstance];
-
-    playlist_t * p_playlist = pl_Get(VLCIntf);
-    PL_LOCK;
-    items_at_launch = p_playlist->p_local_category->i_children;
-    PL_UNLOCK;
-
-#ifdef HAVE_SPARKLE
-    [[SUUpdater sharedUpdater] setDelegate:self];
-#endif
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
@@ -525,11 +540,6 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     return o_mainwindow;
 }
 
-- (VLCControls *)controls
-{
-    return o_controls;
-}
-
 - (VLCInputManager *)inputManager
 {
     return _input_manager;
@@ -559,8 +569,8 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
     if (!_sprefs)
         _sprefs = [[VLCSimplePrefs alloc] init];
 
-    if (!nib_prefs_loaded)
-        nib_prefs_loaded = [NSBundle loadNibNamed:@"SimplePreferences" owner: _sprefs];
+    if (!nib_sprefs_loaded)
+        nib_sprefs_loaded = [NSBundle loadNibNamed:@"SimplePreferences" owner: _sprefs];
 
     return _sprefs;
 }

extras/package/macosx/vlc.xcodeproj/project.pbxproj
@@ -374,8 +374,6 @@
 		1CCB5F5B1A62A724004C3E90 /* bookmarks.h in Sources */ = {isa = PBXBuildFile; fileRef = DCE7BD0708A5724D007B10AE /* bookmarks.h */; };
 		1CCB5F5C1A62A724004C3E90 /* bookmarks.m in Sources */ = {isa = PBXBuildFile; fileRef = DCE7BD0608A5724D007B10AE /* bookmarks.m */; };
 		1CCB5F5D1A62A724004C3E90 /* CompatibilityFixes.h in Sources */ = {isa = PBXBuildFile; fileRef = CCB0BB3013FAC9F40095BD0A /* CompatibilityFixes.h */; };
-		1CCB5F5E1A62A724004C3E90 /* controls.h in Sources */ = {isa = PBXBuildFile; fileRef = 8E6BC6FA041684EC0059A3A7 /* controls.h */; };
-		1CCB5F5F1A62A724004C3E90 /* controls.m in Sources */ = {isa = PBXBuildFile; fileRef = 8ED6C27B03E2EB1C0059A3A7 /* controls.m */; };
 		1CCB5F601A62A724004C3E90 /* ControlsBar.h in Sources */ = {isa = PBXBuildFile; fileRef = E0382C00160BA09E0031D7FF /* ControlsBar.h */; };
 		1CCB5F611A62A724004C3E90 /* ControlsBar.m in Sources */ = {isa = PBXBuildFile; fileRef = E0382C01160BA09E0031D7FF /* ControlsBar.m */; };
 		1CCB5F621A62A724004C3E90 /* ConvertAndSave.h in Sources */ = {isa = PBXBuildFile; fileRef = CC0663341566C34C003A411C /* ConvertAndSave.h */; };
@@ -1272,12 +1270,10 @@
 		8E55FB800459B0FD00FB3317 /* output.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = output.m; path = ../../../modules/gui/macosx/output.m; sourceTree = SOURCE_ROOT; };
 		8E6BC6F6041643860059A3A7 /* applescript.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = applescript.h; path = ../../../modules/gui/macosx/applescript.h; sourceTree = SOURCE_ROOT; };
 		8E6BC6F7041643860059A3A7 /* applescript.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = applescript.m; path = ../../../modules/gui/macosx/applescript.m; sourceTree = SOURCE_ROOT; };
-		8E6BC6FA041684EC0059A3A7 /* controls.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = controls.h; path = ../../../modules/gui/macosx/controls.h; sourceTree = SOURCE_ROOT; };
 		8E9CA1A306D7DEE800B7186C /* prefs_widgets.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = prefs_widgets.h; path = ../../../modules/gui/macosx/prefs_widgets.h; sourceTree = SOURCE_ROOT; };
 		8E9CA1A406D7DEE800B7186C /* prefs_widgets.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = prefs_widgets.m; path = ../../../modules/gui/macosx/prefs_widgets.m; sourceTree = SOURCE_ROOT; };
 		8EBF3FA303F13FFB0059A3A7 /* vlc.scriptSuite */ = {isa = PBXFileReference; lastKnownFileType = text; name = vlc.scriptSuite; path = Resources/vlc.scriptSuite; sourceTree = SOURCE_ROOT; };
 		8EBF3FA503F140960059A3A7 /* vlc.scriptTerminology */ = {isa = PBXFileReference; lastKnownFileType = text.xml; name = vlc.scriptTerminology; path = Resources/vlc.scriptTerminology; sourceTree = SOURCE_ROOT; };
-		8ED6C27B03E2EB1C0059A3A7 /* controls.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = controls.m; path = ../../../modules/gui/macosx/controls.m; sourceTree = SOURCE_ROOT; };
 		8ED6C27C03E2EB1C0059A3A7 /* intf.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = intf.h; path = ../../../modules/gui/macosx/intf.h; sourceTree = SOURCE_ROOT; };
 		8ED6C27D03E2EB1C0059A3A7 /* intf.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = intf.m; path = ../../../modules/gui/macosx/intf.m; sourceTree = SOURCE_ROOT; };
 		8ED6C27E03E2EB1C0059A3A7 /* macosx.m */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.objc; name = macosx.m; path = ../../../modules/gui/macosx/macosx.m; sourceTree = SOURCE_ROOT; };
@@ -1827,8 +1823,6 @@
 				DCE7BD0708A5724D007B10AE /* bookmarks.h */,
 				DCE7BD0608A5724D007B10AE /* bookmarks.m */,
 				CCB0BB3013FAC9F40095BD0A /* CompatibilityFixes.h */,
-				8E6BC6FA041684EC0059A3A7 /* controls.h */,
-				8ED6C27B03E2EB1C0059A3A7 /* controls.m */,
 				E0382C00160BA09E0031D7FF /* ControlsBar.h */,
 				E0382C01160BA09E0031D7FF /* ControlsBar.m */,
 				CC0663341566C34C003A411C /* ConvertAndSave.h */,
@@ -3854,8 +3848,6 @@
 				1CCB5F5B1A62A724004C3E90 /* bookmarks.h in Sources */,
 				1CCB5F5C1A62A724004C3E90 /* bookmarks.m in Sources */,
 				1CCB5F5D1A62A724004C3E90 /* CompatibilityFixes.h in Sources */,
-				1CCB5F5E1A62A724004C3E90 /* controls.h in Sources */,
-				1CCB5F5F1A62A724004C3E90 /* controls.m in Sources */,
 				1CCB5F601A62A724004C3E90 /* ControlsBar.h in Sources */,
 				1CCB5F611A62A724004C3E90 /* ControlsBar.m in Sources */,
 				1CCB5F621A62A724004C3E90 /* ConvertAndSave.h in Sources */,

modules/gui/macosx/ControlsBar.m
@@ -1104,7 +1104,7 @@ else \
 
 - (IBAction)effects:(id)sender
 {
-    [[VLCMainMenu sharedInstance] showAudioEffects: sender];
+    [[[VLCMain sharedInstance] mainMenu] showAudioEffects: sender];
 }
 
 #pragma mark -
@@ -1161,7 +1161,7 @@ else \
         [o_next_btn setEnabled: (b_seekable || b_plmul || b_chapters)];
     }
 
-    [[VLCMainMenu sharedInstance] setRateControlsEnabled: b_control];
+    [[[VLCMain sharedInstance] mainMenu] setRateControlsEnabled: b_control];
 }
 
 @end

modules/gui/macosx/CoreInteraction.h
@@ -66,6 +66,7 @@
 - (void)volumeUp;
 - (void)volumeDown;
 - (void)toggleMute;
+- (void)showPosition;
 - (void)startListeningWithAppleRemote;
 - (void)stopListeningWithAppleRemote;
 
@@ -82,6 +83,7 @@
 - (void)setVideoFilterProperty: (const char *)psz_name forFilter: (const char *)psz_filter string: (const char *)psz_value;
 - (void)setVideoFilterProperty: (const char *)psz_name forFilter: (const char *)psz_filter boolean: (BOOL)b_value;
 
+- (BOOL)keyEvent:(NSEvent *)o_event;
 - (void)updateCurrentlyUsedHotkeys;
 - (BOOL)hasDefinedShortcutKey:(NSEvent *)o_event force:(BOOL)b_force;
 

modules/gui/macosx/CoreInteraction.m
@@ -40,7 +40,6 @@
 #import "SPMediaKeyTap.h"
 #import "AppleRemote.h"
 #import "InputManager.h"
-#import "controls.h"
 
 static int BossCallback(vlc_object_t *p_this, const char *psz_var,
                         vlc_value_t oldval, vlc_value_t new_val, void *param)
@@ -616,6 +615,19 @@ static int BossCallback(vlc_object_t *p_this, const char *psz_var,
     vlc_object_release(p_input);
 }
 
+- (void)showPosition
+{
+    input_thread_t *p_input = pl_CurrentInput(VLCIntf);
+    if (p_input != NULL) {
+        vout_thread_t *p_vout = input_GetVout(p_input);
+        if (p_vout != NULL) {
+            var_SetInteger(VLCIntf->p_libvlc, "key-action", ACTIONID_POSITION);
+            vlc_object_release(p_vout);
+        }
+        vlc_object_release(p_input);
+    }
+}
+
 #pragma mark - drag and drop support for VLCVoutView, VLCDragDropView and VLCThreePartDropView
 - (BOOL)performDragOperation:(id <NSDraggingInfo>)sender
 {
@@ -1152,7 +1164,7 @@ static int BossCallback(vlc_object_t *p_this, const char *psz_var,
             }
             break;
         case kRemoteButtonMenu:
-            [[[VLCMain sharedInstance] controls] showPosition: self]; //FIXME
+            [self showPosition];
             break;
         case kRemoteButtonPlay_Sleep:
         {
@@ -1174,6 +1186,36 @@ static int BossCallback(vlc_object_t *p_this, const char *psz_var,
  * shortcut key.  If it is, pass it off to VLC for handling and return YES,
  * otherwise ignore it and return NO (where it will get handled by Cocoa).
  *****************************************************************************/
+
+- (BOOL)keyEvent:(NSEvent *)o_event
+{
+    BOOL eventHandled = NO;
+    NSString * characters = [o_event charactersIgnoringModifiers];
+    if ([characters length] > 0) {
+        unichar key = [characters characterAtIndex: 0];
+
+        if (key) {
+            input_thread_t * p_input = pl_CurrentInput(VLCIntf);
+            if (p_input != NULL) {
+                vout_thread_t *p_vout = input_GetVout(p_input);
+
+                if (p_vout != NULL) {
+                    /* Escape */
+                    if (key == (unichar) 0x1b) {
+                        if (var_GetBool(p_vout, "fullscreen")) {
+                            [self toggleFullscreen];
+                            eventHandled = YES;
+                        }
+                    }
+                    vlc_object_release(p_vout);
+                }
+                vlc_object_release(p_input);
+            }
+        }
+    }
+    return eventHandled;
+}
+
 - (BOOL)hasDefinedShortcutKey:(NSEvent *)o_event force:(BOOL)b_force
 {
     intf_thread_t *p_intf = VLCIntf;

commit 329b882090e0a522b59adb86c49bdf76959d7977

macosx: refix bugs by initializing MainWindow in OpenIntf
Fixes regressions introduced in f450457875207163ddb4f435e0bb5dc868e35ee8.

modules/gui/macosx/intf.m
@@ -83,6 +83,8 @@ int OpenIntf (vlc_object_t *p_this)
         [[VLCMain sharedInstance] setIntf: p_intf];
 
         [NSBundle loadNibNamed:@"MainMenu" owner:[[VLCMain sharedInstance] mainMenu]];
+        [NSBundle loadNibNamed:@"MainWindow" owner:[VLCMain sharedInstance]];
+        [[[VLCMain sharedInstance] mainWindow] makeKeyAndOrderFront:nil];
 
         [VLCVoutWindowController sharedInstance];
 
@@ -204,6 +206,7 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
 
         _mainmenu = [[VLCMainMenu alloc] init];
         _voutController = [[VLCVoutWindowController alloc] init];
+        _playlist = [[VLCPlaylist alloc] init];
     }
 
     return self;
@@ -221,9 +224,6 @@ static int ShowController(vlc_object_t *p_this, const char *psz_variable,
 
 - (void)applicationWillFinishLaunching:(NSNotification *)aNotification
 {
-    _playlist = [[VLCPlaylist alloc] init];
-    [NSBundle loadNibNamed:@"MainWindow" owner:self];
-    [[self mainWindow] makeKeyAndOrderFront:nil];
     _open = [[VLCOpen alloc] init];
     _coreinteraction = [VLCCoreInteraction sharedInstance];
 