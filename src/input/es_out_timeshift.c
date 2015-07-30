b455948738918e677481774112df32c018f0de4b
Francois Cartegnie <fcvlcdev@free.fr>
2015-07-30 14:19:21 +0200

    es_out_timeshift: use switch for CmdCleanControl

--- src/input/es_out_timeshift.c
+++ src/input/es_out_timeshift.c
@@ -1513,22 +1513,26 @@ static int CmdExecuteControl( es_out_t *p_out, ts_cmd_t *p_cmd )
 }
 static void CmdCleanControl( ts_cmd_t *p_cmd )
 {
-    if( ( p_cmd->u.control.i_query == ES_OUT_SET_GROUP_META ||
-          p_cmd->u.control.i_query == ES_OUT_SET_META ) &&
-        p_cmd->u.control.u.int_meta.p_meta )
+    switch( p_cmd->u.control.i_query )
     {
-        vlc_meta_Delete( p_cmd->u.control.u.int_meta.p_meta );
-    }
-    else if( p_cmd->u.control.i_query == ES_OUT_SET_GROUP_EPG &&
-             p_cmd->u.control.u.int_epg.p_epg )
-    {
-        vlc_epg_Delete( p_cmd->u.control.u.int_epg.p_epg );
-    }
-    else if( p_cmd->u.control.i_query == ES_OUT_SET_ES_FMT &&
-             p_cmd->u.control.u.es_fmt.p_fmt )
-    {
-        es_format_Clean( p_cmd->u.control.u.es_fmt.p_fmt );
-        free( p_cmd->u.control.u.es_fmt.p_fmt );
+    case ES_OUT_SET_GROUP_META:
+    case ES_OUT_SET_META:
+        if( p_cmd->u.control.u.int_meta.p_meta )
+            vlc_meta_Delete( p_cmd->u.control.u.int_meta.p_meta );
+        break;
+    case ES_OUT_SET_GROUP_EPG:
+        if( p_cmd->u.control.u.int_epg.p_epg )
+            vlc_epg_Delete( p_cmd->u.control.u.int_epg.p_epg );
+        break;
+    case ES_OUT_SET_ES_FMT:
+        if( p_cmd->u.control.u.es_fmt.p_fmt )
+        {
+            es_format_Clean( p_cmd->u.control.u.es_fmt.p_fmt );
+            free( p_cmd->u.control.u.es_fmt.p_fmt );
+        }
+        // ft
+    default:
+        break;
     }
 }
 
