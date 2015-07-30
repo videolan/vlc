3fc28a0bc620ea3668181eb24ddb0b5f0d00e869
Thomas Guillem <thomas@gllm.fr>
2015-07-30 17:37:34 +0200

    avcodec: fix invalid free
    Initialize frame to NULL before goto end

--- modules/codec/avcodec/audio.c
+++ modules/codec/avcodec/audio.c
@@ -244,6 +244,7 @@ static block_t *DecodeAudio( decoder_t *p_dec, block_t **pp_block )
 {
     decoder_sys_t *p_sys = p_dec->p_sys;
     AVCodecContext *ctx = p_sys->p_context;
+    AVFrame *frame = NULL;
 
     if( !pp_block || !*pp_block )
         return NULL;
@@ -290,7 +291,7 @@ static block_t *DecodeAudio( decoder_t *p_dec, block_t **pp_block )
         p_block->i_flags |= BLOCK_FLAG_PRIVATE_REALLOCATED;
     }
 
-    AVFrame *frame = av_frame_alloc();
+    frame = av_frame_alloc();
     if (unlikely(frame == NULL))
         goto end;
 
