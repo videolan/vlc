videolan/vlc

commit cf1d55617efc01b7f239aea214f9228cc573d2e6

demux: adaptative: fix memory leak
Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/demux/adaptative/PlaylistManager.cpp
@@ -60,6 +60,7 @@ PlaylistManager::PlaylistManager( demux_t *p_demux_,
 PlaylistManager::~PlaylistManager   ()
 {
     delete conManager;
+    delete playlist;
     delete streamOutputFactory;
     unsetPeriod();
 }

commit f35018526a7a7f953030e8406cc2f19a0e9546fc

demux: adaptative: fix assertion on exit/release order
introduced by cf1d55617efc01b7f239aea214f9228cc573d2e6

modules/demux/adaptative/PlaylistManager.cpp
@@ -60,9 +60,9 @@ PlaylistManager::PlaylistManager( demux_t *p_demux_,
 PlaylistManager::~PlaylistManager   ()
 {
     delete conManager;
-    delete playlist;
     delete streamOutputFactory;
     unsetPeriod();
+    delete playlist;
 }
 
 void PlaylistManager::unsetPeriod()