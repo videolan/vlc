7d54a3ab14af46006e75dfeaca79e53ff3e510a5
Tristan Matthews <tmatth@videolan.org>
2015-08-31 13:59:23 -0400

    video_filter: sepia: cosmetics

--- modules/video_filter/sepia.c
+++ modules/video_filter/sepia.c
@@ -319,7 +319,7 @@ static void PlanarI420Sepia( picture_t *p_pic, picture_t *p_outpic,
     const uint8_t filling_const_8v = 128 + i_intensity / 14;
 
     /* iterate for every two visible line in the frame */
-    for( int y = 0; y < p_pic->p[Y_PLANE].i_visible_lines - 1; y += 2)
+    for( int y = 0; y < p_pic->p[Y_PLANE].i_visible_lines - 1; y += 2 )
     {
         const int i_dy_line1_start = y * p_outpic->p[Y_PLANE].i_pitch;
         const int i_dy_line2_start = ( y + 1 ) * p_outpic->p[Y_PLANE].i_pitch;
@@ -331,7 +331,7 @@ static void PlanarI420Sepia( picture_t *p_pic, picture_t *p_outpic,
                   ? (p_pic->p[Y_PLANE].i_visible_pitch - 1) :
                   (p_outpic->p[Y_PLANE].i_visible_pitch - 1);
         /* iterate for every two visible line in the frame */
-        for( int x = 0; x < i_picture_size_limit; x += 2)
+        for( int x = 0; x < i_picture_size_limit; x += 2 )
         {
             // y = y - y/4 {to prevent overflow} + intensity / 4
             p_outpic->p[Y_PLANE].p_pixels[i_dy_line1_start + x] =
