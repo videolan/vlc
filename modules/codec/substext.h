a863d60d1943ecac31498474d3e37501fe5e6565
Jean-Baptiste Kempf <jb@videolan.org>
2015-09-08 10:27:13 +0200

    Substext.h: add a license header

--- modules/codec/substext.h
+++ modules/codec/substext.h
@@ -1,3 +1,26 @@
+/*****************************************************************************
+ * subsdec.c : text subtitle decoder
+ *****************************************************************************
+ * Copyright © 2011-2015 VLC authors and VideoLAN
+ *
+ * Authors: Laurent Aimer <fenrir@videolan.org>
+ *          Jean-Baptiste Kempf <jb@videolan.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include <vlc_strings.h>
 #include <vlc_text_style.h>
 
