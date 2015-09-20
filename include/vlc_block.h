de4be0a4505564a5f758f06edd74f49498107f6e
Jean-Paul Saman <jpsaman@videolan.org>
2015-09-20 10:37:57 +0200

    vlc_block.h: Clarify usage of BLOCK_FLAG_DISCONTINUITY
    The BLOCK_FLAG_DISCONTINUITY signals timeline changes from the source content or due to seeking.
    BLOCK_FLAG_CORRUPTION signals data corruption due to corrupted or dropped packets, this includes
    transport stream continuity counter mismatches.
    
    Signed-off-by: Ilkka Ollakka <ileoo@videolan.org>

--- include/vlc_block.h
+++ include/vlc_block.h
@@ -51,7 +51,8 @@
  *   optimised for preheader/postdatas increase)
  ****************************************************************************/
 
-/** The content doesn't follow the last block, or is probably broken */
+/** The content doesn't follow the last block and indicates a timeline change
+ *  without data loss or corruption */
 #define BLOCK_FLAG_DISCONTINUITY 0x0001
 /** Intra frame */
 #define BLOCK_FLAG_TYPE_I        0x0002
