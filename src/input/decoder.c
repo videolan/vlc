ee9f587bc08de89b38ea4f224d836654adf01609
Rémi Denis-Courmont <remi@remlab.net>
2015-09-27 19:50:24 +0300

    decoder: simplify based on vout_GetPicture() change
    Also fixes lot of spurious decoder errors when stopping due to
    excessive flush checks.

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -480,18 +480,7 @@ static picture_t *vout_new_buffer( decoder_t *p_dec )
 {
     decoder_owner_sys_t *p_owner = p_dec->p_owner;
 
-    for( ;; )
-    {
-        if( DecoderIsFlushing( p_dec ) || p_dec->b_error )
-            return NULL;
-
-        picture_t *p_picture = vout_GetPicture( p_owner->p_vout );
-        if( p_picture )
-            return p_picture;
-
-        /* FIXME add a vout_WaitPictureAvailable (timedwait) */
-        msleep( VOUT_OUTMEM_SLEEP );
-    }
+    return vout_GetPicture( p_owner->p_vout );
 }
 
 static subpicture_t *spu_new_buffer( decoder_t *p_dec,
