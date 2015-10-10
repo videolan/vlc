ffa25effb598b6d58dac62903fb08fc9b8965296
David Fuhrmann <dfuhrmann@videolan.org>
2015-10-10 16:46:36 +0200

    macosx: disable fullscreen hack on yosemite and higher
    It seems that Apple fixed this issue starting with Yosemite.

--- modules/gui/macosx/Windows.m
+++ modules/gui/macosx/Windows.m
@@ -258,7 +258,7 @@
 
     BOOL b_inFullscreen = [self fullscreen] || ([self respondsToSelector:@selector(inFullscreenTransition)] && [(VLCVideoWindowCommon *)self inFullscreenTransition]);
 
-    if((OSX_MAVERICKS || OSX_YOSEMITE) && b_inFullscreen && constrainedRect.size.width == screenRect.size.width
+    if((OSX_MAVERICKS) && b_inFullscreen && constrainedRect.size.width == screenRect.size.width
           && constrainedRect.size.height != screenRect.size.height
           && fabs(screenRect.size.height - constrainedRect.size.height) <= 25.) {
 
