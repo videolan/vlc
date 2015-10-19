999d056fd67d041d7874be837b05ff8c7656ef00
Francois Cartegnie <fcvlcdev@free.fr>
2015-10-19 22:33:39 +0200

    vlc_bits: fix invalid conversion

--- include/vlc_bits.h
+++ include/vlc_bits.h
@@ -51,7 +51,7 @@ typedef struct bs_s
 
 static inline void bs_init( bs_t *s, const void *p_data, size_t i_data )
 {
-    s->p_start = (void *)p_data;
+    s->p_start = (uint8_t *)p_data;
     s->p       = s->p_start;
     s->p_end   = s->p_start + i_data;
     s->i_left  = 8;
