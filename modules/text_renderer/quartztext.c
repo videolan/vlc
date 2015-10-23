f8ab474b225fff5a8279b933d4bf37c48f7e077a
Boris Egorov <egorov@linux.com>
2015-10-23 15:42:23 +0600

    text_renderer: Fix syntax error
    Signed-off-by: Boris Egorov <egorov@linux.com>
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/text_renderer/quartztext.c
+++ modules/text_renderer/quartztext.c
@@ -260,7 +260,7 @@ static void Destroy(vlc_object_t *p_this)
     filter_sys_t *p_sys = p_filter->p_sys;
 #ifndef TARGET_OS_IPHONE
     if (p_sys->p_fonts) {
-        for (int k = 0; k < p_sys->i_fonts; k++) {
+        for (int k = 0; k < p_sys->i_fonts; k++)
             ATSFontDeactivate(p_sys->p_fonts[k], NULL, kATSOptionFlagsDefault);
 
         free(p_sys->p_fonts);
