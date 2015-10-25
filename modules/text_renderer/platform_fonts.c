e85a00c7fa77383bbd524974c1e12078661cd1e9
Jean-Baptiste Kempf <jb@videolan.org>
2015-10-25 19:26:04 +0100

    Freetype: split OS X code to its own file

--- modules/text_renderer/platform_fonts.c
+++ modules/text_renderer/platform_fonts.c
@@ -40,16 +40,6 @@
 #include <vlc_input.h>                             /* vlc_input_attachment_* */
 #include <ctype.h>
 
-/* apple stuff */
-#ifdef __APPLE__
-# include <TargetConditionals.h>
-# if !TARGET_OS_IPHONE
-#  include <Carbon/Carbon.h>
-# endif
-# include <sys/param.h>                         /* for MAXPATHLEN */
-# undef HAVE_FONTCONFIG
-#endif
-
 #include "platform_fonts.h"
 #include "freetype.h"
 
@@ -549,90 +539,6 @@ char* Generic_Select( filter_t *p_filter, const char* psz_family,
 
     return File_Select( SYSTEM_DEFAULT_FONT_FILE );
 }
-
-#ifdef __APPLE__
-#if !TARGET_OS_IPHONE
-char* MacLegacy_Select( filter_t *p_filter, const char* psz_fontname,
-                        bool b_bold, bool b_italic,
-                        int *i_idx, uni_char_t codepoint )
-{
-    VLC_UNUSED( b_bold );
-    VLC_UNUSED( b_italic );
-    VLC_UNUSED( codepoint );
-    FSRef ref;
-    unsigned char path[MAXPATHLEN];
-    char * psz_path;
-
-    CFStringRef  cf_fontName;
-    ATSFontRef   ats_font_id;
-
-    *i_idx = 0;
-
-    if( psz_fontname == NULL )
-        return NULL;
-
-    msg_Dbg( p_filter, "looking for %s", psz_fontname );
-    cf_fontName = CFStringCreateWithCString( kCFAllocatorDefault, psz_fontname, kCFStringEncodingUTF8 );
-
-    ats_font_id = ATSFontFindFromName( cf_fontName, kATSOptionFlagsIncludeDisabledMask );
-
-    if ( ats_font_id == 0 || ats_font_id == 0xFFFFFFFFUL )
-    {
-        msg_Dbg( p_filter, "ATS couldn't find %s by name, checking family", psz_fontname );
-        ats_font_id = ATSFontFamilyFindFromName( cf_fontName, kATSOptionFlagsDefault );
-
-        if ( ats_font_id == 0 || ats_font_id == 0xFFFFFFFFUL )
-        {
-            msg_Dbg( p_filter, "ATS couldn't find either %s nor its family, checking PS name", psz_fontname );
-            ats_font_id = ATSFontFindFromPostScriptName( cf_fontName, kATSOptionFlagsDefault );
-
-            if ( ats_font_id == 0 || ats_font_id == 0xFFFFFFFFUL )
-            {
-                msg_Err( p_filter, "ATS couldn't find %s (no font name, family or PS name)", psz_fontname );
-                CFRelease( cf_fontName );
-                return NULL;
-            }
-        }
-    }
-    CFRelease( cf_fontName );
-
-    if ( noErr != ATSFontGetFileReference( ats_font_id, &ref ) )
-    {
-        msg_Err( p_filter, "ATS couldn't get file ref for %s", psz_fontname );
-        return NULL;
-    }
-
-    /* i_idx calculation by searching preceding fontIDs */
-    /* with same FSRef                                       */
-    {
-        ATSFontRef  id2 = ats_font_id - 1;
-        FSRef       ref2;
-
-        while ( id2 > 0 )
-        {
-            if ( noErr != ATSFontGetFileReference( id2, &ref2 ) )
-                break;
-            if ( noErr != FSCompareFSRefs( &ref, &ref2 ) )
-                break;
-
-            id2 --;
-        }
-        *i_idx = ats_font_id - ( id2 + 1 );
-    }
-
-    if ( noErr != FSRefMakePath( &ref, path, sizeof(path) ) )
-    {
-        msg_Err( p_filter, "failure when getting path from FSRef" );
-        return NULL;
-    }
-    msg_Dbg( p_filter, "found %s", path );
-
-    psz_path = strdup( (char *)path );
-
-    return psz_path;
-}
-#endif
-#endif
 
 #ifndef _WIN32
 char* Dummy_Select( filter_t *p_filter, const char* psz_font,
