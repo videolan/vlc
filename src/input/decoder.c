122bccd0ec5c54af53d8110f121f67aa93138933
Steve Lhomme <robUx4@videolabs.io>
2015-11-13 13:15:28 +0100

    decoder: remove unnecessary cast
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -709,7 +709,7 @@ static int DecoderPlaySout( decoder_t *p_dec, block_t *p_sout_block )
  */
 static void DecoderProcessSout( decoder_t *p_dec, block_t *p_block )
 {
-    decoder_owner_sys_t *p_owner = (decoder_owner_sys_t *)p_dec->p_owner;
+    decoder_owner_sys_t *p_owner = p_dec->p_owner;
     block_t *p_sout_block;
 
     while( ( p_sout_block =
@@ -943,7 +943,7 @@ static void DecoderDecodeVideo( decoder_t *p_dec, block_t *p_block )
  */
 static void DecoderProcessVideo( decoder_t *p_dec, block_t *p_block, bool b_flush )
 {
-    decoder_owner_sys_t *p_owner = (decoder_owner_sys_t *)p_dec->p_owner;
+    decoder_owner_sys_t *p_owner = p_dec->p_owner;
 
     if( p_owner->p_packetizer )
     {
@@ -1098,7 +1098,7 @@ static void DecoderDecodeAudio( decoder_t *p_dec, block_t *p_block )
  */
 static void DecoderProcessAudio( decoder_t *p_dec, block_t *p_block, bool b_flush )
 {
-    decoder_owner_sys_t *p_owner = (decoder_owner_sys_t *)p_dec->p_owner;
+    decoder_owner_sys_t *p_owner = p_dec->p_owner;
 
     if( p_owner->p_packetizer )
     {
@@ -1252,7 +1252,7 @@ static void DecoderProcessSpu( decoder_t *p_dec, block_t *p_block, bool b_flush 
  */
 static void DecoderProcess( decoder_t *p_dec, block_t *p_block )
 {
-    decoder_owner_sys_t *p_owner = (decoder_owner_sys_t *)p_dec->p_owner;
+    decoder_owner_sys_t *p_owner = p_dec->p_owner;
     const bool b_flush_request = p_block && (p_block->i_flags & BLOCK_FLAG_CORE_FLUSH);
 
     if( p_dec->b_error )
