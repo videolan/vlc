9826be044cd79753ec1fa47845dde4bf1925dec4
Rémi Denis-Courmont <remi@remlab.net>
2015-11-29 15:48:41 +0200

    Inline path_sanitize()
    Also optimize it away where applicable.

--- src/text/strings.c
+++ src/text/strings.c
@@ -887,29 +887,3 @@ void filename_sanitize( char *str )
         *str = '_';
     }
 }
-
-/**
- * Remove forbidden characters from full paths (leaves slashes)
- */
-void path_sanitize( char *str )
-{
-#if defined( _WIN32 ) || defined( __OS2__ )
-    /* check drive prefix if path is absolute */
-    if( (((unsigned char)(str[0] - 'A') < 26)
-      || ((unsigned char)(str[0] - 'a') < 26)) && (':' == str[1]) )
-        str += 2;
-#endif
-    while( *str )
-    {
-#if defined( __APPLE__ )
-        if( *str == ':' )
-            *str = '_';
-#elif defined( _WIN32 ) || defined( __OS2__ )
-        if( strchr( "*\"?:|<>", *str ) )
-            *str = '_';
-        if( *str == '/' )
-            *str = DIR_SEP_CHAR;
-#endif
-        str++;
-    }
-}
