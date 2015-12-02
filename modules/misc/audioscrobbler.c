1de948c790301ede4f7900ac317ada671d56a7d2
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-12-02 11:46:44 +0100

    audioscrobbler: Don't mark a used variable unused

--- modules/misc/audioscrobbler.c
+++ modules/misc/audioscrobbler.c
@@ -348,7 +348,7 @@ static int ItemChange(vlc_object_t *p_this, const char *psz_var,
     intf_sys_t     *p_sys   = p_intf->p_sys;
     input_thread_t *p_input = newval.p_address;
 
-    VLC_UNUSED(p_this); VLC_UNUSED(psz_var);
+    VLC_UNUSED(psz_var);
     VLC_UNUSED(oldval);
 
     p_sys->b_meta_read      = false;
