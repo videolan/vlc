8c4fff2f85e7916f2e18abf00605e7302bfba5f6
Thomas Guillem <thomas@gllm.fr>
2015-12-03 18:14:04 +0100

    decoder: remove legacy flush call

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -210,19 +210,6 @@ static void DecoderUpdateFormatLocked( decoder_t *p_dec )
     p_owner->b_fmt_description = true;
 }
 
-static block_t *DecoderBlockFlushNew()
-{
-    block_t *p_null = block_Alloc( 128 );
-    if( !p_null )
-        return NULL;
-
-    p_null->i_flags |= BLOCK_FLAG_DISCONTINUITY |
-                       BLOCK_FLAG_CORRUPTED;
-    memset( p_null->p_buffer, 0, p_null->i_buffer );
-
-    return p_null;
-}
-
 /*****************************************************************************
  * Buffers allocation callbacks for the decoders
  *****************************************************************************/
@@ -1280,17 +1267,6 @@ static void DecoderProcessFlush( decoder_t *p_dec )
 
     if ( p_dec->pf_flush != NULL )
         p_dec->pf_flush( p_dec );
-    else
-    {
-        /* legacy call, pass a CORRUPTED + DISCONTINUITY block */
-        block_t *p_flush = DecoderBlockFlushNew();
-        if( unlikely( p_flush == NULL ) )
-        {
-            msg_Err( p_dec, "cannot flush" );
-            return;
-        }
-        DecoderProcess( p_dec, p_flush );
-    }
 
     if( p_dec->fmt_out.i_cat == AUDIO_ES )
     {
