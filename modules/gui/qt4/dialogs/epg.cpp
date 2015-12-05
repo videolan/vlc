5f4cb46d97beadbb4b4b4b144d0af0733c0a3729
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-12-05 13:08:15 +0100

    qt4: epg: Fix inputChanged signal prototype

--- modules/gui/qt4/dialogs/epg.cpp
+++ modules/gui/qt4/dialogs/epg.cpp
@@ -77,7 +77,7 @@ EpgDialog::EpgDialog( intf_thread_t *_p_intf ): QVLCFrame( _p_intf )
 
     CONNECT( epg, itemSelectionChanged( EPGItem *), this, displayEvent( EPGItem *) );
     CONNECT( THEMIM->getIM(), epgChanged(), this, updateInfos() );
-    CONNECT( THEMIM, inputChanged( ), this, updateInfos() );
+    CONNECT( THEMIM, inputChanged( bool ), this, updateInfos() );
 
     QDialogButtonBox *buttonsBox = new QDialogButtonBox( this );
 
