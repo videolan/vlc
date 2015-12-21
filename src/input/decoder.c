videolan/vlc

commit 7ada6eae5567c7d716ca03f3d7ddab90ce50e763

decoder: also drain the decoder when draining the packetizer
When p_block was NULL, DecoderThread was draining its packetizer or its
decoder, but was not draining both (when a packetizer was used).

src/input/decoder.c
@@ -987,6 +987,9 @@ static void DecoderProcessVideo( decoder_t *p_dec, block_t *p_block )
                 p_packetized_block = p_next;
             }
         }
+        /* Drain the decoder after the packetizer is drained */
+        if( !p_block )
+            DecoderDecodeVideo( p_dec, NULL );
     }
     else
     {
@@ -1127,6 +1130,9 @@ static void DecoderProcessAudio( decoder_t *p_dec, block_t *p_block )
                 p_packetized_block = p_next;
             }
         }
+        /* Drain the decoder after the packetizer is drained */
+        if( !p_block )
+            DecoderDecodeAudio( p_dec, NULL );
     }
     else
     {

commit 4fe75d523a79c3ab7f37ac3e764b32ab82e97d1b

decoder: fix decoder drained when using a packetizer
The packetizer set the *pp_block to NULL, therefore p_block was NULL too.

This fixes a regression from 7ada6eae5567c7d716ca03f3d7ddab90ce50e763

Closes #16128

src/input/decoder.c
@@ -1036,7 +1036,7 @@ static void DecoderProcessVideo( decoder_t *p_dec, block_t *p_block )
             }
         }
         /* Drain the decoder after the packetizer is drained */
-        if( !p_block )
+        if( !pp_block )
             DecoderDecodeVideo( p_dec, NULL );
     }
     else
@@ -1217,7 +1217,7 @@ static void DecoderProcessAudio( decoder_t *p_dec, block_t *p_block )
             }
         }
         /* Drain the decoder after the packetizer is drained */
-        if( !p_block )
+        if( !pp_block )
             DecoderDecodeAudio( p_dec, NULL );
     }
     else