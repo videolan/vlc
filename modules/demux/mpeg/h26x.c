a1b2802286fa18a39906084c75ef79e524510364
Francois Cartegnie <fcvlcdev@free.fr>
2015-12-24 15:24:34 +0100

    demuxers: h26x: merge H264 and HEVC
    Those ones now only differed in probing, codec and packetizer.
    Merged as h26x.
    Kills one string.
    Renames hevc-force-fps to hevc-fps.

--- modules/demux/mpeg/h26x.c
+++ modules/demux/mpeg/h26x.c
@@ -1,10 +1,10 @@
 /*****************************************************************************
- * hevc.c : HEVC Video demuxer
+ * h26x.c : raw H264 and HEVC Video demuxers
  *****************************************************************************
- * Copyright (C) 2014 VLC authors and VideoLAN
- * $Id$
+ * Copyright (C) 2002-2004, 2014-2015 VLC authors and VideoLAN
  *
- * Authors: Denis Charmet <typx@videolan.org>
+ * Authors: Laurent Aimar <fenrir@via.ecp.fr>
+ *          Denis Charmet <typx@videolan.org>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -33,36 +33,42 @@
 #include <vlc_plugin.h>
 #include <vlc_demux.h>
 #include <vlc_codec.h>
-#include <vlc_mtime.h>
 
 /*****************************************************************************
  * Module descriptor
  *****************************************************************************/
-static int  Open ( vlc_object_t * );
+static int  OpenH264 ( vlc_object_t * );
+static int  OpenHEVC ( vlc_object_t * );
 static void Close( vlc_object_t * );
 
 #define FPS_TEXT N_("Frames per Second")
 #define FPS_LONGTEXT N_("Desired frame rate for the stream.")
 
-
 vlc_module_begin ()
-    set_shortname( "HEVC")
+    set_shortname( "H264")
     set_category( CAT_INPUT )
     set_subcategory( SUBCAT_INPUT_DEMUX )
-    set_description( N_("HEVC/H.265 video demuxer" ) )
+    set_description( N_("H264 video demuxer" ) )
     set_capability( "demux", 0 )
-    add_float( "hevc-force-fps", 0.0, FPS_TEXT, FPS_LONGTEXT, true )
-    set_callbacks( Open, Close )
-    add_shortcut( "hevc" )
+    add_float( "h264-fps", 0.0, FPS_TEXT, FPS_LONGTEXT, true )
+    set_callbacks( OpenH264, Close )
+    add_shortcut( "h264" )
+
+    add_submodule()
+        set_shortname( "HEVC")
+        set_category( CAT_INPUT )
+        set_subcategory( SUBCAT_INPUT_DEMUX )
+        set_description( N_("HEVC/H.265 video demuxer" ) )
+        set_capability( "demux", 0 )
+        add_float( "hevc-fps", 0.0, FPS_TEXT, FPS_LONGTEXT, true )
+        set_callbacks( OpenHEVC, Close )
+        add_shortcut( "hevc" )
+
 vlc_module_end ()
 
 /*****************************************************************************
  * Local prototypes
  *****************************************************************************/
-
-static int Demux( demux_t * );
-static int Control( demux_t *, int, va_list );
-
 struct demux_sys_t
 {
     es_out_id_t *p_es;
@@ -70,69 +76,119 @@ struct demux_sys_t
     date_t      dts;
     unsigned    frame_rate_num;
     unsigned    frame_rate_den;
+
     decoder_t *p_packetizer;
 };
-#define HEVC_BLOCK_SIZE 2048
+
+static int Demux( demux_t * );
+static int Control( demux_t *, int, va_list );
+
+#define H26X_PACKET_SIZE 2048
 
 /*****************************************************************************
- * Open: initializes demux structures
+ * Probing
  *****************************************************************************/
-static int Open( vlc_object_t * p_this )
+static bool ProbeHEVC( const uint8_t *p_peek, size_t i_peek )
 {
-    demux_t     *p_demux = (demux_t*)p_this;
+    if( i_peek < 5 )
+        return false;
+
+    if( (p_peek[4]&0xFE) != 0x40 /* VPS & forbidden zero bit*/ )
+        return false;
+    else
+        return true;
+}
+
+static bool ProbeH264( const uint8_t *p_peek, size_t i_peek )
+{
+    if( i_peek < 5 )
+        return false;
+    const uint8_t i_nal_type = p_peek[4] & 0x1F;
+    const uint8_t i_ref_idc = p_peek[4] & 0x60;
+    if( (p_peek[4] & 0x80) || /* reserved 0 */
+        i_nal_type == 0 || i_nal_type > 12 ||
+        ( !i_ref_idc && (i_nal_type < 6 || i_nal_type == 7 || i_nal_type == 8) ) ||
+        (  i_ref_idc && (i_nal_type == 6 || i_nal_type >= 9) )
+    )
+        return false;
+    else
+        return true;
+}
+
+/*****************************************************************************
+ * Shared Open code
+ *****************************************************************************/
+static int GenericOpen( demux_t *p_demux, const char *psz_module,
+                        vlc_fourcc_t i_codec,
+                        bool(*pf_probe)(const uint8_t *, size_t) )
+{
     demux_sys_t *p_sys;
     const uint8_t *p_peek;
     es_format_t fmt;
 
     if( stream_Peek( p_demux->s, &p_peek, 5 ) < 5 ) return VLC_EGENERIC;
 
-    if( p_peek[0] != 0x00 || p_peek[1] != 0x00 ||
-        p_peek[2] != 0x00 || p_peek[3] != 0x01 ||
-        (p_peek[4]&0xFE) != 0x40 ) /* VPS & forbidden zero bit*/
+    if( memcmp( p_peek, "\x00\x00\x00\x01", 4 ) || !pf_probe( p_peek, 5 ) )
     {
         if( !p_demux->b_force )
         {
-            msg_Warn( p_demux, "hevc module discarded (no startcode)" );
+            msg_Warn( p_demux, "%s module discarded (no startcode)", psz_module );
             return VLC_EGENERIC;
         }
 
-        msg_Err( p_demux, "this doesn't look like a HEVC ES stream, "
-                 "continuing anyway" );
+        msg_Err( p_demux, "this doesn't look like a %s ES stream, "
+                 "continuing anyway", psz_module );
     }
 
+    p_demux->pf_demux  = Demux;
+    p_demux->pf_control= Control;
     p_demux->p_sys     = p_sys = malloc( sizeof( demux_sys_t ) );
-
-    if( !p_demux->p_sys )
-        return VLC_ENOMEM;
-
     p_sys->p_es        = NULL;
-    p_sys->frame_rate_num = p_sys->frame_rate_den = 0;
-    float f_force_fps = var_CreateGetFloat( p_demux, "hevc-force-fps" );
-    if( f_force_fps != 0.0f )
+    p_sys->frame_rate_num = 0;
+    p_sys->frame_rate_den = 0;
+
+    float f_fps = 0;
+    char *psz_fpsvar;
+    if( asprintf( &psz_fpsvar, "%s-fps", psz_module ) )
     {
-        if ( f_force_fps < 0.001f ) f_force_fps = 0.001f;
+        f_fps = var_CreateGetFloat( p_demux, psz_fpsvar );
+        free( psz_fpsvar );
+    }
+
+    if( f_fps )
+    {
+        if ( f_fps < 0.001f ) f_fps = 0.001f;
         p_sys->frame_rate_den = 1000;
-        p_sys->frame_rate_num = 1000 * f_force_fps;
+        p_sys->frame_rate_num = 1000 * f_fps;
         date_Init( &p_sys->dts, p_sys->frame_rate_num, p_sys->frame_rate_den );
     }
     else
-        date_Init( &p_sys->dts, 25000, 1000 ); /* Will be overwritten */
+        date_Init( &p_sys->dts, 25000, 1000 );
     date_Set( &p_sys->dts, VLC_TS_0 );
 
-    /* Load the hevc packetizer */
-    es_format_Init( &fmt, VIDEO_ES, VLC_CODEC_HEVC );
-    p_sys->p_packetizer = demux_PacketizerNew( p_demux, &fmt, "hevc" );
+    /* Load the mpegvideo packetizer */
+    es_format_Init( &fmt, VIDEO_ES, i_codec );
+    p_sys->p_packetizer = demux_PacketizerNew( p_demux, &fmt, psz_module );
     if( !p_sys->p_packetizer )
     {
         free( p_sys );
         return VLC_EGENERIC;
     }
 
-    p_sys->p_es = NULL;
-    p_demux->pf_demux  = Demux;
-    p_demux->pf_control= Control;
-
     return VLC_SUCCESS;
+}
+
+/*****************************************************************************
+ * Open: initializes demux structures
+ *****************************************************************************/
+static int OpenH264( vlc_object_t * p_this )
+{
+    return GenericOpen( (demux_t*)p_this, "h264", VLC_CODEC_H264, ProbeH264 );
+}
+
+static int OpenHEVC( vlc_object_t * p_this )
+{
+    return GenericOpen( (demux_t*)p_this, "hevc", VLC_CODEC_HEVC, ProbeHEVC );
 }
 
 /*****************************************************************************
@@ -143,11 +199,7 @@ static void Close( vlc_object_t * p_this )
     demux_t     *p_demux = (demux_t*)p_this;
     demux_sys_t *p_sys = p_demux->p_sys;
 
-    if( p_sys->p_es )
-        es_out_Del( p_demux->out, p_sys->p_es );
-
     demux_PacketizerDestroy( p_sys->p_packetizer );
-
     free( p_sys );
 }
 
@@ -161,7 +213,7 @@ static int Demux( demux_t *p_demux)
     demux_sys_t *p_sys = p_demux->p_sys;
     block_t *p_block_in, *p_block_out;
 
-    if( ( p_block_in = stream_Block( p_demux->s, HEVC_BLOCK_SIZE ) ) == NULL )
+    if( ( p_block_in = stream_Block( p_demux->s, H26X_PACKET_SIZE ) ) == NULL )
     {
         return 0;
     }
@@ -171,21 +223,21 @@ static int Demux( demux_t *p_demux)
         while( p_block_out )
         {
             block_t *p_next = p_block_out->p_next;
+
             p_block_out->p_next = NULL;
 
-            /* As we'll receive the pts of the head of the packetizer helper blockstream
-             * the dts/pts will always be late until next frame. */
             p_block_in->i_dts = date_Get( &p_sys->dts );
             p_block_in->i_pts = VLC_TS_INVALID;
 
             if( p_sys->p_es == NULL )
             {
                 p_sys->p_packetizer->fmt_out.b_packetized = true;
-                p_sys->p_es = es_out_Add( p_demux->out, &p_sys->p_packetizer->fmt_out);
+                p_sys->p_es = es_out_Add( p_demux->out, &p_sys->p_packetizer->fmt_out );
                 if( !p_sys->p_es )
                     return VLC_DEMUXER_EOF;
             }
 
+            /* h264 packetizer does merge multiple NAL into AU, but slice flag persists */
             bool frame = p_block_out->i_flags & BLOCK_FLAG_TYPE_MASK;
             es_out_Send( p_demux->out, p_sys->p_es, p_block_out );
             if( frame )
@@ -213,7 +265,6 @@ static int Demux( demux_t *p_demux)
             }
 
             p_block_out = p_next;
-
         }
     }
     return 1;
@@ -224,8 +275,13 @@ static int Demux( demux_t *p_demux)
  *****************************************************************************/
 static int Control( demux_t *p_demux, int i_query, va_list args )
 {
-    return demux_vaControlHelper( p_demux->s,
-                                  0, -1,
-                                  0, 1, i_query, args );
+    /* demux_sys_t *p_sys  = p_demux->p_sys; */
+    /* FIXME calculate the bitrate */
+    if( i_query == DEMUX_SET_TIME )
+        return VLC_EGENERIC;
+    else
+        return demux_vaControlHelper( p_demux->s,
+                                       0, -1,
+                                       0, 1, i_query, args );
 }
 
