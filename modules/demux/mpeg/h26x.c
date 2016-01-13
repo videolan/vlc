bc2c083d7de3ccefa1de9cebd3d5bbdf527d7151
Francois Cartegnie <fcvlcdev@free.fr>
2016-01-13 17:49:11 +0100

    demux: hevc: add .hevc extension
    used by ffmpeg

--- modules/demux/mpeg/h26x.c
+++ modules/demux/mpeg/h26x.c
@@ -378,7 +378,7 @@ static int OpenH264( vlc_object_t * p_this )
 static int OpenHEVC( vlc_object_t * p_this )
 {
     hevc_probe_ctx_t ctx = { 0, 0, 0 };
-    const char *rgi_psz_ext[] = { ".h265", ".265", ".bin", ".bit", ".raw", NULL };
+    const char *rgi_psz_ext[] = { ".h265", ".265", ".hevc", ".bin", ".bit", ".raw", NULL };
     const char *rgi_psz_mime[] = { "video/h265", "video/hevc", "video/HEVC", NULL };
 
     return GenericOpen( (demux_t*)p_this, "hevc", VLC_CODEC_HEVC, ProbeHEVC,
