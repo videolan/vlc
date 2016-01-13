a3f92eaf5e2a5316cf4eb48ebda636c126620082
Rémi Denis-Courmont <remi@remlab.net>
2016-01-13 18:37:39 +0200

    input: memory leak
    (demux_Packetizer... could use some factorization with decoder_...)

--- src/input/demux.c
+++ src/input/demux.c
@@ -584,6 +584,7 @@ void demux_PacketizerDestroy( decoder_t *p_packetizer )
     if( p_packetizer->p_module )
         module_unneed( p_packetizer, p_packetizer->p_module );
     es_format_Clean( &p_packetizer->fmt_in );
+    es_format_Clean( &p_packetizer->fmt_out );
     if( p_packetizer->p_description )
         vlc_meta_Delete( p_packetizer->p_description );
     vlc_object_release( p_packetizer );
