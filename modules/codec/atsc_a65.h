910e577de179cd58b37dce056d644e2e02520838
Francois Cartegnie <fcvlcdev@free.fr>
2016-02-05 14:56:13 +0100

    atsc_a65: add gps time to epoch helper

--- modules/codec/atsc_a65.h
+++ modules/codec/atsc_a65.h
@@ -19,11 +19,18 @@
 #ifndef VLC_ATSC_A65_H
 #define VLC_ATSC_A65_H
 
+#define GPS_UTC_EPOCH_OFFSET 315964800
+
 typedef struct atsc_a65_handle_t atsc_a65_handle_t;
 
 atsc_a65_handle_t *atsc_a65_handle_New( const char *psz_lang );
 void atsc_a65_handle_Release( atsc_a65_handle_t * );
 
 char * atsc_a65_Decode_multiple_string( atsc_a65_handle_t *, const uint8_t *, size_t );
+
+static inline time_t atsc_a65_GPSTimeToEpoch( time_t i_seconds, time_t i_gpstoepoch_leaptime_offset )
+{
+    return i_seconds + GPS_UTC_EPOCH_OFFSET - i_gpstoepoch_leaptime_offset;
+}
 
 #endif
