0f8cd63dc23c830c87a7179cde9d8ffdbb947dee
Rémi Denis-Courmont <remi@remlab.net>
2016-02-10 22:48:34 +0200

    test: add dummy and rc tests, work-around Qt leaks

--- test/run_vlc.sh
+++ test/run_vlc.sh
@@ -5,5 +5,11 @@ set -e
 VLC="./vlc --ignore-config"
 
 $VLC -H
-$VLC -vv vlc://quit
-$VLC -vv --play-and-exit vlc://nop
+$VLC -Idummy vlc://quit
+$VLC -vv -Irc vlc://quit
+$VLC -vv -Irc --play-and-exit vlc://nop
+
+LSAN_OPTIONS=exitcode=0
+export LSAN_OPTIONS
+
+$VLC --play-and-exit vlc://nop
