0135c943e6556c83862260079a8af8ad9ba735df
Wieland Hoffmann <themineo@gmail.com>
2016-02-28 21:06:52 +0100

    Change vlc_meta_DiscNumber's english string to "Disc number"
    This mimics the english string of vlc_meta_TrackNumber
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/input/meta.c
+++ src/input/meta.c
@@ -77,7 +77,7 @@ const char * vlc_meta_TypeToLocalizedString( vlc_meta_type_t meta_type )
         [vlc_meta_ShowName]    = N_("Show Name"),
         [vlc_meta_Actors]      = N_("Actors"),
         [vlc_meta_AlbumArtist] = N_("Album Artist"),
-        [vlc_meta_DiscNumber]  = N_("DiscNumber")
+        [vlc_meta_DiscNumber]  = N_("Disc number")
     };
 
     assert (meta_type < (sizeof(posix_names) / sizeof(posix_names[0])));
