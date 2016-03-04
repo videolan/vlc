4757ae1af6de974fed6e1ab53c440a58d195a5fa
Rémi Denis-Courmont <remi@remlab.net>
2016-03-04 04:38:01 +0200

    avcodec: disable hwaccel when unsupported/broken (fixes #16642)

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -1171,6 +1171,15 @@ static enum PixelFormat ffmpeg_GetFormat( AVCodecContext *p_context,
     if (!can_hwaccel)
         return swfmt;
 
+#if (LIBAVCODEC_VERSION_MICRO >= 100) /* FFmpeg only */
+    if (p_context->active_thread_type)
+    {
+        msg_Warn(p_dec, "thread type %d: disabling hardware acceleration",
+                 p_context->active_thread_type);
+        return swfmt;
+    }
+#endif
+
     wait_mt(p_sys);
 
     for( size_t i = 0; pi_fmt[i] != AV_PIX_FMT_NONE; i++ )
