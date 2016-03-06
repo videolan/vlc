c7497d033e247a29ee888a56c08b5515f502da98
Francois Cartegnie <fcvlcdev@free.fr>
2016-03-06 21:22:27 +0100

    demux: ts: remove indirect eit callbacks

--- modules/demux/mpeg/ts_psi_eit.c
+++ modules/demux/mpeg/ts_psi_eit.c
@@ -361,13 +361,12 @@ static void TDTCallBack( demux_t *p_demux, dvbpsi_tot_t *p_tdt )
     dvbpsi_tot_delete(p_tdt);
 }
 
-static void EITCallBack( demux_t *p_demux,
-                         dvbpsi_eit_t *p_eit, bool b_current_following )
+static void EITCallBack( demux_t *p_demux, dvbpsi_eit_t *p_eit )
 {
     demux_sys_t        *p_sys = p_demux->p_sys;
     dvbpsi_eit_event_t *p_evt;
     vlc_epg_t *p_epg;
-    VLC_UNUSED(b_current_following);
+    //bool b_current_following = (p_eit->i_table_id == 0x4e);
 
     msg_Dbg( p_demux, "EITCallBack called" );
     if( !p_eit->b_current_next )
@@ -583,14 +582,6 @@ static void EITCallBack( demux_t *p_demux,
 
     dvbpsi_eit_delete( p_eit );
 }
-static void EITCallBackCurrentFollowing( demux_t *p_demux, dvbpsi_eit_t *p_eit )
-{
-    EITCallBack( p_demux, p_eit, true );
-}
-static void EITCallBackSchedule( demux_t *p_demux, dvbpsi_eit_t *p_eit )
-{
-    EITCallBack( p_demux, p_eit, false );
-}
 
 static void SINewTableCallBack( dvbpsi_t *h, uint8_t i_table_id,
                                 uint16_t i_extension, void *p_pid_cbdata )
@@ -620,11 +611,8 @@ static void SINewTableCallBack( dvbpsi_t *h, uint8_t i_table_id,
         /* Do not attach decoders if we can't decode timestamps */
         if( p_demux->p_sys->i_network_time > 0 )
         {
-            dvbpsi_eit_callback cb = i_table_id == 0x4e ?
-                                        (dvbpsi_eit_callback)EITCallBackCurrentFollowing :
-                                        (dvbpsi_eit_callback)EITCallBackSchedule;
-
-            if( !dvbpsi_eit_attach( h, i_table_id, i_extension, cb, p_demux ) )
+            if( !dvbpsi_eit_attach( h, i_table_id, i_extension,
+                                    (dvbpsi_eit_callback)EITCallBack, p_demux ) )
                 msg_Err( p_demux, "SINewTableCallback: failed attaching EITCallback" );
         }
     }
