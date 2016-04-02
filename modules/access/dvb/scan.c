665fedde5e24f25830fe2d0656b4a40fd31e6d60
Eric Engestrom <eric@engestrom.ch>
2016-04-02 15:43:23 +0100

    dvb: remove `break` after `return`
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/access/dvb/scan.c
+++ modules/access/dvb/scan.c
@@ -164,11 +164,11 @@ static int scan_service_type( int service_type )
 {
     switch( service_type )
     {
-    case 0x01: return SERVICE_DIGITAL_TELEVISION; break;
-    case 0x02: return SERVICE_DIGITAL_RADIO; break;
-    case 0x16: return SERVICE_DIGITAL_TELEVISION_AC_SD; break;
-    case 0x19: return SERVICE_DIGITAL_TELEVISION_AC_HD; break;
-    default:   return SERVICE_UNKNOWN; break;
+    case 0x01: return SERVICE_DIGITAL_TELEVISION;
+    case 0x02: return SERVICE_DIGITAL_RADIO;
+    case 0x16: return SERVICE_DIGITAL_TELEVISION_AC_SD;
+    case 0x19: return SERVICE_DIGITAL_TELEVISION_AC_HD;
+    default:   return SERVICE_UNKNOWN;
     }
 }
 
