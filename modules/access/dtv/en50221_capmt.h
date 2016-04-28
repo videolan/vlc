1d4445d8a1d6264fc9f20616ddd7834c1146cf24
Steve Lhomme <robux4@videolabs.io>
2016-04-28 16:22:40 +0200

    access: dtv: fix realloc usage
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- modules/access/dtv/en50221_capmt.h
+++ modules/access/dtv/en50221_capmt.h
@@ -56,11 +56,12 @@ static inline en50221_capmt_es_info_t *en50221_capmt_EsAdd( en50221_capmt_info_t
                                                             uint8_t i_stream_type,
                                                             uint16_t i_es_pid )
 {
-    en50221_capmt_es_info_t *p_realloc = realloc( p_en->p_es, p_en->i_es_count + 1 );
+    en50221_capmt_es_info_t *p_realloc = realloc( p_en->p_es, sizeof(en50221_capmt_es_info_t) *
+                                                              (p_en->i_es_count + 1) );
     if( likely(p_realloc) )
     {
         p_en->p_es = p_realloc;
-        en50221_capmt_es_info_t *p_es = &p_en->p_es[ p_en->i_es_count ];
+        en50221_capmt_es_info_t *p_es = &p_en->p_es[ p_en->i_es_count++ ];
         p_es->i_es_pid = i_es_pid;
         p_es->i_stream_type = i_stream_type;
         p_es->i_descriptors = 0;
