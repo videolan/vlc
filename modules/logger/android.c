0fd217b29c5b14ba8058c819fc96ff783e6ca06e
Thomas Guillem <thomas@gllm.fr>
2016-05-26 13:58:59 +0200

    logger: android: display thread id

--- modules/logger/android.c
+++ modules/logger/android.c
@@ -46,8 +46,8 @@ static void AndroidPrintMsg(void *opaque, int type, const vlc_log_t *p_item,
     if (verbose < type)
         return;
 
-    if (asprintf(&format2, "[%0*"PRIxPTR"] %s %s: %s",
-                 ptr_width, p_item->i_object_id, p_item->psz_module,
+    if (asprintf(&format2, "[%0*"PRIxPTR"/%lx] %s %s: %s",
+                 ptr_width, p_item->i_object_id, p_item->tid, p_item->psz_module,
                  p_item->psz_object_type, format) < 0)
         return;
     switch (type) {
