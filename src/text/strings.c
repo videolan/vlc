b771b09a49190ac2fcd1d6cd607f30755ce210ea
Francois Cartegnie <fcvlcdev@free.fr>
2016-07-19 21:02:54 +0200

    strings: vlc_strfinput: use checked alloc

--- src/text/strings.c
+++ src/text/strings.c
@@ -526,7 +526,7 @@ static int write_meta(FILE *stream, input_item_t *item, vlc_meta_type_t type)
 
 char *vlc_strfinput(input_thread_t *input, const char *s)
 {
-    char *str;
+    char *str = NULL;
     size_t len;
 #ifdef HAVE_OPEN_MEMSTREAM
     FILE *stream = open_memstream(&str, &len);
@@ -812,15 +812,16 @@ char *vlc_strfinput(input_thread_t *input, const char *s)
     return (fclose(stream) == 0) ? str : NULL;
 #else
     len = ftell(stream);
-    if (len != (size_t)-1)
+    if (len != (size_t)-1 && len != SIZE_MAX)
     {
         rewind(stream);
-        str = xmalloc(len + 1);
-        fread(str, len, 1, stream);
-        str[len] = '\0';
+        str = malloc(len + 1);
+        if(str)
+        {
+            fread(str, len, 1, stream);
+            str[len] = '\0';
+        }
     }
-    else
-        str = NULL;
     fclose(stream);
     return str;
 #endif
