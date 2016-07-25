6efb88ff345c61cbc36a372a8d7fc2aa91a7b5b4
Rémi Denis-Courmont <remi@remlab.net>
2016-07-25 19:10:11 +0300

    threads: vlc_thread_t is opaque
    Comparison with zero means nothing.

--- include/vlc_threads.h
+++ include/vlc_threads.h
@@ -52,7 +52,11 @@ VLC_API void vlc_testcancel(void);
 #  define ETIMEDOUT 10060 /* This is the value in winsock.h. */
 # endif
 
-typedef struct vlc_thread *vlc_thread_t;
+typedef struct
+{
+    struct vlc_thread *thread;
+} vlc_thread_t;
+
 # define VLC_THREAD_CANCELED NULL
 # define LIBVLC_NEED_SLEEP
 typedef struct
@@ -249,7 +253,10 @@ typedef struct vlc_timer *vlc_timer_t;
 /**
  * Thread handle.
  */
-typedef pthread_t       vlc_thread_t;
+typedef struct
+{
+    pthread_t handle;
+} vlc_thread_t;
 
 /**
  * Return value of a canceled thread.
@@ -660,12 +667,13 @@ void vlc_addr_broadcast(void *addr);
  * The thread must be <i>joined</i> with vlc_join() to reclaim resources
  * when it is not needed anymore.
  *
- * @param th [OUT] pointer to write the handle of the created thread to
- *                 (mandatory, must be non-NULL)
+ * @param th storage space for the handle of the new thread (cannot be NULL)
+ *           [OUT]
  * @param entry entry point for the thread
  * @param data data parameter given to the entry point
  * @param priority thread priority value
  * @return 0 on success, a standard error code on error.
+ * @note In case of error, the value of *th is undefined.
  */
 VLC_API int vlc_clone(vlc_thread_t *th, void *(*entry)(void *), void *data,
                       int priority) VLC_USED;
