d45925ab3e1f31a001dab57ac839a3bf1db9afd5
Jean-Baptiste Kempf <jb@videolan.org>
2016-07-25 17:47:37 +0200

    UPnP: relicense to LGPLv2.1+
    Thanks to (in no particular order, and not exhaustive):
    Thomas Guillem
    Hugo Beauzée-Luyssen
    Pavlov Konstantin
    Mirsal Ennaime
    Rémi Denis-Courmont
    Steve Lhomme
    Jean-Baptiste Kempf
    Bastien Penavayre
    Chris Clayton
    Felix Paul Kühne
    Alexandre Pereira Nunes
    Antoine Cellerier
    Austin Burrow
    Denis Charmet
    Fabrizio Gennari
    Jean-Paul Saman
    Naohiro Koriyama
    Pere Orga
    Ludovic Fauvet
    Pierre d'Herbemont
    Samuel Hocevar
    Rémi Duraffort

--- modules/services_discovery/upnp.hpp
+++ modules/services_discovery/upnp.hpp
@@ -1,7 +1,7 @@
 /*****************************************************************************
  * upnp.hpp :  UPnP discovery module (libupnp) header
  *****************************************************************************
- * Copyright (C) 2004-2010 the VideoLAN team
+ * Copyright (C) 2004-2016 VLC authors and VideoLAN
  * $Id$
  *
  * Authors: Rémi Denis-Courmont <rem # videolan.org> (original plugin)
@@ -9,21 +9,19 @@
  *          Mirsal Ennaime <mirsal dot ennaime at gmail dot com>
  *          Hugo Beauzée-Luyssen <hugo@beauzee.fr>
  *
- * UPnP Plugin using the Intel SDK (libupnp) instead of CyberLink
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
 #ifdef HAVE_CONFIG_H
