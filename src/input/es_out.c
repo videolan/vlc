4ff1a9062a6fe3e5e27d9b6f73a1bf064bc2780d
Filip Roséen <filip@atch.se>
2016-08-01 10:53:29 +0200

    input/es_out: remove trailing whitespace
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/input/es_out.c
+++ src/input/es_out.c
@@ -1805,7 +1805,7 @@ static void EsOutSelect( es_out_t *out, es_out_id_t *es, bool b_force )
             }
             else
             {
-                /* Select the first one if there is no selected audio yet 
+                /* Select the first one if there is no selected audio yet
                  * then choose by ES priority */
                 if( !p_sys->p_es_audio ||
                     p_sys->p_es_audio->fmt.i_priority < es->fmt.i_priority )
@@ -1833,7 +1833,7 @@ static void EsOutSelect( es_out_t *out, es_out_id_t *es, bool b_force )
                 {
                     /* Select the language if it's in the list */
                     if( es_idx >= 0 ||
-                        /*FIXME: Should default subtitle not in the list be 
+                        /*FIXME: Should default subtitle not in the list be
                          * displayed if not forbidden by none? */
                         ( p_sys->i_default_sub_id >= 0 &&
                           /* check if the subtitle isn't forbidden by none */
@@ -1866,12 +1866,12 @@ static void EsOutSelect( es_out_t *out, es_out_id_t *es, bool b_force )
             }
             else
             {
-                /* If there is no user preference, select the default subtitle 
+                /* If there is no user preference, select the default subtitle
                  * or adapt by ES priority */
                 if( ( !p_sys->p_es_sub &&
                       ( p_sys->i_default_sub_id >= 0 &&
                         es->i_id == p_sys->i_default_sub_id ) ) ||
-                    ( p_sys->p_es_sub && 
+                    ( p_sys->p_es_sub &&
                       p_sys->p_es_sub->fmt.i_priority < es->fmt.i_priority ) )
                     i_wanted = es->i_channel;
                 else if( p_sys->p_es_sub &&
