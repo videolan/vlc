2c5d6c23453424744b9c68d3cb08327e8ff66693
Steve Lhomme <robUx4@videolabs.io>
2016-08-08 17:06:07 +0200

    cropadd: log when the chroma is not supported or NULL
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/video_filter/croppadd.c
+++ modules/video_filter/croppadd.c
@@ -147,7 +147,11 @@ static int OpenFilter( vlc_object_t *p_this )
     const vlc_chroma_description_t *p_chroma =
         vlc_fourcc_GetChromaDescription( p_filter->fmt_in.video.i_chroma );
     if( p_chroma == NULL || p_chroma->plane_count == 0 )
+    {
+        msg_Err( p_filter, "Unknown input chroma %4.4s", p_filter->fmt_in.video.i_chroma?
+                     (const char*)&p_filter->fmt_in.video.i_chroma : "xxxx" );
         return VLC_EGENERIC;
+    }
 
     p_filter->p_sys = (filter_sys_t *)malloc( sizeof( filter_sys_t ) );
     if( !p_filter->p_sys ) return VLC_ENOMEM;
