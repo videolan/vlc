461b9cb189fc09649484f52eb83f10eda440c39c
Tristan Matthews <tmatth@videolan.org>
2016-08-19 14:00:27 -0700

    aom: add AV1 decoder

---
+++ modules/codec/aom.c
@@ -0,0 +1,236 @@
+/*****************************************************************************
+ * aom.c: libaom decoder (AV1) module
+ *****************************************************************************
+ * Copyright (C) 2016 VLC authors and VideoLAN
+ *
+ * Authors: Tristan Matthews <tmatth@videolan.org>
+ * Based on vpx.c by: Rafaël Carré <funman@videolan.org>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+/*****************************************************************************
+ * Preamble
+ *****************************************************************************/
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <vlc_common.h>
+#include <vlc_plugin.h>
+#include <vlc_codec.h>
+
+#include <aom/aom_decoder.h>
+#include <aom/aomdx.h>
+
+/****************************************************************************
+ * Local prototypes
+ ****************************************************************************/
+static int OpenDecoder(vlc_object_t *);
+static void CloseDecoder(vlc_object_t *);
+
+/*****************************************************************************
+ * Module descriptor
+ *****************************************************************************/
+
+vlc_module_begin ()
+    set_shortname("aom")
+    set_description(N_("AOM video decoder"))
+    set_capability("decoder", 100)
+    set_callbacks(OpenDecoder, CloseDecoder)
+    set_category(CAT_INPUT)
+    set_subcategory(SUBCAT_INPUT_VCODEC)
+vlc_module_end ()
+
+static void aom_err_msg(vlc_object_t *this, aom_codec_ctx_t *ctx,
+                        const char *msg)
+{
+    const char *error  = aom_codec_error(ctx);
+    const char *detail = aom_codec_error_detail(ctx);
+    if (!detail)
+        detail = "no specific information";
+    msg_Err(this, msg, error, detail);
+}
+
+#define AOM_ERR(this, ctx, msg) aom_err_msg(VLC_OBJECT(this), ctx, msg ": %s (%s)")
+
+/*****************************************************************************
+ * decoder_sys_t: libaom decoder descriptor
+ *****************************************************************************/
+struct decoder_sys_t
+{
+    aom_codec_ctx_t ctx;
+};
+
+/****************************************************************************
+ * Decode: the whole thing
+ ****************************************************************************/
+static picture_t *Decode(decoder_t *dec, block_t **pp_block)
+{
+    aom_codec_ctx_t *ctx = &dec->p_sys->ctx;
+
+    if( !pp_block || !*pp_block )
+        return NULL;
+    block_t *block = *pp_block;
+
+    if (block->i_flags & (BLOCK_FLAG_CORRUPTED)) {
+        block_Release(block);
+        return NULL;
+    }
+
+    /* Associate packet PTS with decoded frame */
+    mtime_t *pkt_pts = malloc(sizeof(*pkt_pts));
+    if (!pkt_pts) {
+        block_Release(block);
+        *pp_block = NULL;
+        return NULL;
+    }
+
+    *pkt_pts = block->i_pts;
+
+    aom_codec_err_t err;
+    err = aom_codec_decode(ctx, block->p_buffer, block->i_buffer, pkt_pts, 0);
+
+    block_Release(block);
+    *pp_block = NULL;
+
+    if (err != AOM_CODEC_OK) {
+        free(pkt_pts);
+        AOM_ERR(dec, ctx, "Failed to decode frame");
+        return NULL;
+    }
+
+    const void *iter = NULL;
+    struct aom_image *img = aom_codec_get_frame(ctx, &iter);
+    if (!img) {
+        free(pkt_pts);
+        return NULL;
+    }
+
+    /* fetches back the PTS */
+    pkt_pts = img->user_priv;
+    mtime_t pts = *pkt_pts;
+    free(pkt_pts);
+
+    if (img->fmt != AOM_IMG_FMT_I420) {
+        msg_Err(dec, "Unsupported output colorspace %d", img->fmt);
+        return NULL;
+    }
+
+    video_format_t *v = &dec->fmt_out.video;
+
+    if (img->d_w != v->i_visible_width || img->d_h != v->i_visible_height) {
+        v->i_visible_width = img->d_w;
+        v->i_visible_height = img->d_h;
+    }
+
+    if( !dec->fmt_out.video.i_sar_num || !dec->fmt_out.video.i_sar_den )
+    {
+        dec->fmt_out.video.i_sar_num = 1;
+        dec->fmt_out.video.i_sar_den = 1;
+    }
+
+    picture_t *pic = decoder_NewPicture(dec);
+    if (!pic)
+        return NULL;
+
+    for (int plane = 0; plane < pic->i_planes; plane++ ) {
+        uint8_t *src = img->planes[plane];
+        uint8_t *dst = pic->p[plane].p_pixels;
+        int src_stride = img->stride[plane];
+        int dst_stride = pic->p[plane].i_pitch;
+
+        int size = __MIN( src_stride, dst_stride );
+        for( int line = 0; line < pic->p[plane].i_visible_lines; line++ ) {
+            memcpy( dst, src, size );
+            src += src_stride;
+            dst += dst_stride;
+        }
+    }
+
+    pic->b_progressive = true; /* codec does not support interlacing */
+    pic->date = pts;
+
+    return pic;
+}
+
+/*****************************************************************************
+ * OpenDecoder: probe the decoder
+ *****************************************************************************/
+static int OpenDecoder(vlc_object_t *p_this)
+{
+    decoder_t *dec = (decoder_t *)p_this;
+    const aom_codec_iface_t *iface;
+    int av_version;
+
+    switch (dec->fmt_in.i_codec)
+    {
+    case VLC_CODEC_AV1:
+        iface = &aom_codec_av1_dx_algo;
+        av_version = 1;
+        break;
+    default:
+        return VLC_EGENERIC;
+    }
+
+    decoder_sys_t *sys = malloc(sizeof(*sys));
+    if (!sys)
+        return VLC_ENOMEM;
+    dec->p_sys = sys;
+
+    struct aom_codec_dec_cfg deccfg = {
+        .threads = __MIN(vlc_GetCPUCount(), 16)
+    };
+
+    msg_Dbg(p_this, "AV%d: using libaom version %s (build options %s)",
+        av_version, aom_codec_version_str(), aom_codec_build_config());
+
+    if (aom_codec_dec_init(&sys->ctx, iface, &deccfg, 0) != AOM_CODEC_OK) {
+        AOM_ERR(p_this, &sys->ctx, "Failed to initialize decoder");
+        free(sys);
+        return VLC_EGENERIC;;
+    }
+
+    dec->pf_decode_video = Decode;
+
+    dec->fmt_out.i_cat = VIDEO_ES;
+    dec->fmt_out.video.i_width = dec->fmt_in.video.i_width;
+    dec->fmt_out.video.i_height = dec->fmt_in.video.i_height;
+    dec->fmt_out.i_codec = VLC_CODEC_I420;
+
+    return VLC_SUCCESS;
+}
+
+/*****************************************************************************
+ * CloseDecoder: decoder destruction
+ *****************************************************************************/
+static void CloseDecoder(vlc_object_t *p_this)
+{
+    decoder_t *dec = (decoder_t *)p_this;
+    decoder_sys_t *sys = dec->p_sys;
+
+    /* Free our PTS */
+    const void *iter = NULL;
+    for (;;) {
+        struct aom_image *img = aom_codec_get_frame(&sys->ctx, &iter);
+        if (!img)
+            break;
+        free(img->user_priv);
+    }
+
+    aom_codec_destroy(&sys->ctx);
+
+    free(sys);
+}
