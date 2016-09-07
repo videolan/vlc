videolan/vlc

commit d150b4bce968ffb97f387d4634b1d1d579259447

avcodec: audio. use send_packet/receive_frame in decoding

modules/codec/avcodec/audio.c
@@ -323,7 +323,15 @@ static block_t *DecodeAudio( decoder_t *p_dec, block_t **pp_block )
         pkt.data = p_block->p_buffer;
         pkt.size = p_block->i_buffer;
 
-        int used = avcodec_decode_audio4( ctx, frame, &got_frame, &pkt );
+        int used = avcodec_send_packet( ctx, &pkt );
+        if( used < 0 )
+        {
+            msg_Warn( p_dec, "cannot decode one frame (%zu bytes)",
+                      p_block->i_buffer );
+            goto end;
+        }
+        used = avcodec_receive_frame( ctx, frame );
+        got_frame = used == 0;
         if( used < 0 )
         {
             msg_Warn( p_dec, "cannot decode one frame (%zu bytes)",

commit f581d0629378748e3303818afd682100c39a031e

avcodec: audio: fix handling of block
regression introduced in d150b4bce968ffb97f387d4634b1d1d579259447
spotted by Rémi Denis-Courmont

modules/codec/avcodec/audio.c
@@ -343,8 +343,8 @@ static block_t *DecodeAudio( decoder_t *p_dec, block_t **pp_block )
         if( (unsigned)used > p_block->i_buffer )
             used = p_block->i_buffer;
 
-        p_block->p_buffer += used;
-        p_block->i_buffer -= used;
+        p_block->p_buffer += p_block->i_buffer;
+        p_block->i_buffer = 0;
     }
 
     if( ctx->channels <= 0 || ctx->channels > 8 || ctx->sample_rate <= 0 )