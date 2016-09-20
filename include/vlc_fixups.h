312655fce1c8501817c825c07bf05157e97596e7
Rémi Denis-Courmont <remi@remlab.net>
2016-09-20 13:10:11 +0300

    include: do not use HAVE_CXX11
    It is never defined (as of 2.2.0-git-8982-g4b1c9dc).

--- include/vlc_fixups.h
+++ include/vlc_fixups.h
@@ -33,7 +33,7 @@
 
 /* C++11 says there's no need to define __STDC_*_MACROS when including
  * inttypes.h and stdint.h. */
-#if defined (__cplusplus) && (!defined(HAVE_CXX11) || defined(__MINGW32__) || defined(__UCLIBC__))
+#if defined (__cplusplus) && (defined(__MINGW32__) || defined(__UCLIBC__))
 # ifndef __STDC_FORMAT_MACROS
 #  define __STDC_FORMAT_MACROS 1
 # endif
