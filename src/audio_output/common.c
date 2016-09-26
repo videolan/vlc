5b6d77b00fd8cecd007bce37c67622ca93c59d0f
Thomas Guillem <thomas@gllm.fr>
2016-09-26 17:06:39 +0200

    aout: simplify
    Use the i_channels value that is set just before.

--- src/audio_output/common.c
+++ src/audio_output/common.c
@@ -92,7 +92,7 @@ void aout_FormatPrepare( audio_sample_format_t * p_format )
     if( p_format->i_bitspersample > 0 )
     {
         p_format->i_bytes_per_frame = ( p_format->i_bitspersample / 8 )
-                                    * aout_FormatNbChannels( p_format );
+                                    * p_format->i_channels;
         p_format->i_frame_length = 1;
     }
 }
