eeddbc4c4f73088512b0658028e84eb7adde3985
Tristan Matthews <tmatth@videolan.org>
2016-09-26 11:29:14 -0400

    aes3: set p_aout_buffer before bailing
    Otherwise it was returned uninitialized.

--- modules/codec/aes3.c
+++ modules/codec/aes3.c
@@ -147,7 +147,11 @@ static block_t *Decode( decoder_t *p_dec, block_t **pp_block )
         return NULL;
 
     if( decoder_UpdateAudioFormat( p_dec ) )
+    {
+        p_aout_buffer = NULL;
         goto exit;
+    }
+
     p_aout_buffer = decoder_NewAudioBuffer( p_dec, i_frame_length );
     if( p_aout_buffer == NULL )
         goto exit;
