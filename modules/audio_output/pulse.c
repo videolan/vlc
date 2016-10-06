e400088de4571dae52059701dcf4788490b0e535
Thomas Guillem <thomas@gllm.fr>
2016-10-06 18:10:01 +0200

    pulse: no TRUEHD/MLP support
    Sending this codec via passthrough is not supported in pulseaudio (yet).

--- modules/audio_output/pulse.c
+++ modules/audio_output/pulse.c
@@ -744,6 +744,9 @@ static int Start(audio_output_t *aout, audio_sample_format_t *restrict fmt)
             ss.format = PA_SAMPLE_S16NE;
 
             break;
+        case VLC_CODEC_TRUEHD:
+        case VLC_CODEC_MLP:
+            return VLC_EGENERIC;
         default:
             if (HAVE_FPU)
             {
