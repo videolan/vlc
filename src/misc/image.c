22213432355d5a2526759c2caa35b64926d8d2b6
Filip Roséen <filip@atch.se>
2016-10-10 18:17:59 +0200

    misc/image: narrow scope of iteration variable
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- src/misc/image.c
+++ src/misc/image.c
@@ -593,8 +593,7 @@ static const struct
 
 vlc_fourcc_t image_Mime2Fourcc( const char *psz_mime )
 {
-    int i;
-    for( i = 0; mime_table[i].i_codec; i++ )
+    for( int i = 0; mime_table[i].i_codec; i++ )
         if( !strcmp( psz_mime, mime_table[i].psz_mime ) )
             return mime_table[i].i_codec;
     return 0;
