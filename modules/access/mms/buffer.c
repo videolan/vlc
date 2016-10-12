ce4803809ecdf92b1662b98d31b0df07060af180
Filip Roséen <filip@atch.se>
2016-10-12 20:08:10 +0200

    access/mms: narrow scope of iteration variables
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/access/mms/buffer.c
+++ modules/access/mms/buffer.c
@@ -215,14 +215,10 @@ int var_buffer_readempty( var_buffer_t *p_buf )
 
 void var_buffer_getguid( var_buffer_t *p_buf, guid_t *p_guid )
 {
-    int i;
-
     p_guid->Data1 = var_buffer_get32( p_buf );
     p_guid->Data2 = var_buffer_get16( p_buf );
     p_guid->Data3 = var_buffer_get16( p_buf );
 
-    for( i = 0; i < 8; i++ )
-    {
+    for( int i = 0; i < 8; i++ )
         p_guid->Data4[i] = var_buffer_get8( p_buf );
-    }
 }
