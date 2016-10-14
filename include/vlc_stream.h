10c5ded53ce716b63da9f831d07c63673bb84c25
Thomas Guillem <thomas@gllm.fr>
2016-10-14 16:38:11 +0200

    stream: update documentation

--- include/vlc_stream.h
+++ include/vlc_stream.h
@@ -51,7 +51,7 @@ struct stream_t
     module_t    *p_module;
 
     char        *psz_name;
-    char        *psz_url; /**< Full URL or MRL */
+    char        *psz_url; /**< Full URL or MRL (can be NULL) */
     const char  *psz_location; /**< Location (URL with the scheme stripped) */
     char        *psz_filepath; /**< Local file path (if applicable) */
     bool         b_preparsing; /**< True if this access is used to preparse */
