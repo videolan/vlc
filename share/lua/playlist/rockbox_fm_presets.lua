5f3a29794485aab1516b902b6ffac46b936a1d8d
Pierre Ynard <linkfanel@yahoo.fr>
2016-10-26 11:58:49 +0200

    rockbox_fm_presets.lua: big clean up
    Remove debug output, add license, improve probing...
    
    Ref. #17488

--- share/lua/playlist/rockbox_fm_presets.lua
+++ share/lua/playlist/rockbox_fm_presets.lua
@@ -1,27 +1,44 @@
+--[[
+ $Id$
+
+ Copyright © 2009, 2016 the VideoLAN team
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+--]]
+
 -- Parser script from Rockbox FM radio presets
 -- See http://www.rockbox.org/wiki/FmPresets
 
+local MRL_base = "v4l2c:///dev/radio0:tuner-frequency="
+
 function probe()
-	if not string.match( vlc.path, ".fmr$" ) then return false end
+	if not string.match( vlc.path, "%.[fF][mM][rR]$" ) then return false end
 	local line = vlc.peek(256)
-	vlc.msg.err(line)
-	local freq = tonumber(string.match( line, "(%d*):" ))
-	if not freq then return false end
-	return freq > 80000000	and freq < 110000000
+	local freq = tonumber(string.match( line, "^[^%d]?[^%d]?[^%d]?[^%d]?(%d+):" )) -- handle BOM
+	return freq and freq > 80000000 and freq < 110000000
 end
 
 function parse()
-	vlc.msg.err("test")
 	local p = {}
 	while true do
-		line = vlc.readline()
+		local line = vlc.readline()
 		if not line then break end
-	vlc.msg.err(line)
-		for freq, name in string.gmatch( line, "(%d*):(.*)" ) do
-			vlc.msg.info(freq)
-			table.insert( p, { path = "v4l2c:///dev/radio0:tuner-frequency="..freq, name = name } )
+		local freq, name = string.match( line, "(%d+):(.*)" )
+                if freq then
+			table.insert( p, { path = MRL_base..freq, name = name } )
 		end
 	end
-vlc.msg.err("test test")
 	return p
 end
