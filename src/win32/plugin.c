videolan/vlc

commit e44f6165c138a1e715b5dd738155f9eb6dc90143

Win32: Use SetErrorMode as fallback on XP, 2003, 2003R2
This is quite bad, but this is the only fallback solution.

src/win32/plugin.c
@@ -56,6 +56,15 @@ static BOOL WINAPI SetThreadErrorModeFallback(DWORD mode, DWORD *oldmode)
     GetErrorModeReal = (void *)GetProcAddress(h, "GetErrorMode");
     if (GetErrorModeReal != NULL)
         curmode = GetErrorModeReal();
+    else
+    {
+        /* We are on XP, 2003, 2003/R2 or some special versions of Vista:
+           No SetThreadErrorMode, no GetErrorMode.
+           We will set the mode for the whole process, which is quite bad,
+           but is our only solution */
+        SetErrorMode( mode );
+        return TRUE;
+    }
 # endif
     /* Extra flags should be OK. Missing flags are NOT OK. */
     if ((mode & curmode) != mode)

commit 281252c2dc66637795d5d5ed3b3dc54f84fee974

win32: don´t clobber process error mode
Regression from e44f6165c138a1e715b5dd738155f9eb6dc90143

src/win32/plugin.c
@@ -49,9 +49,10 @@ static BOOL WINAPI SetThreadErrorModeFallback(DWORD mode, DWORD *oldmode)
     if (SetThreadErrorModeReal != NULL)
         return SetThreadErrorModeReal(mode, oldmode);
 
-# if (_WIN32_WINNT >= 0x600)
-    DWORD curmode = GetErrorMode();
-# else
+# if (_WIN32_WINNT < _WIN32_WINNT_VISTA)
+    /* As per libvlc_new() documentation, the calling process is responsible
+     * for setting a proper error mode on Windows 2008 and earlier versions.
+     * This is only a sanity check. */
     UINT WINAPI (*GetErrorModeReal)(void);
     DWORD curmode = 0;
 
@@ -59,14 +60,9 @@ static BOOL WINAPI SetThreadErrorModeFallback(DWORD mode, DWORD *oldmode)
     if (GetErrorModeReal != NULL)
         curmode = GetErrorModeReal();
     else
-    {
-        /* We are on XP, 2003, 2003/R2 or some special versions of Vista:
-           No SetThreadErrorMode, no GetErrorMode.
-           We will set the mode for the whole process, which is quite bad,
-           but is our only solution */
-        SetErrorMode( mode );
-        return TRUE;
-    }
+        curmode = SEM_FAILCRITICALERRORS;
+# else
+    DWORD curmode = GetErrorMode();
 # endif
     /* Extra flags should be OK. Missing flags are NOT OK. */
     if ((mode & curmode) != mode)