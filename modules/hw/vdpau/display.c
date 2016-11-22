bb08b9c452baf0f1878a04dc9fc312191a06c828
Rémi Denis-Courmont <remi@remlab.net>
2016-11-22 19:37:27 +0200

    vdpau/display: fix subpicture leak on error

--- modules/hw/vdpau/display.c
+++ modules/hw/vdpau/display.c
@@ -309,13 +309,13 @@ static void Wait(vout_display_t *vd, picture_t *pic, subpicture_t *subpicture)
             msg_Err(vd, "presentation queue blocking error: %s",
                     vdp_get_error_string(sys->vdp, err));
             picture_Release(pic);
-            return;
+            goto out;
         }
         picture_Release(current);
     }
 
     sys->current = pic;
-
+out:
     /* We already dealt with the subpicture in the Queue phase, so it's safe to
        delete at this point */
     if (subpicture)
