f7686767fc7f135d45e294e7ff2409d65240145a
Pierre Ynard <linkfanel@yahoo.fr>
2016-12-02 11:30:14 +0100

    soundcloud.lua: update API magic

--- share/lua/playlist/soundcloud.lua
+++ share/lua/playlist/soundcloud.lua
@@ -83,10 +83,10 @@ function parse()
 
     if track then
         -- API magic
-        local client_id = "02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
+        local client_id = "fDoItMDbsbZz8dY16ZzARCZmzgHBPotA"
         -- app_version is not required by the API but we send it anyway
         -- to remain unconspicuous
-        local app_version = "a089efd"
+        local app_version = "1480607078"
 
         local api = vlc.stream( vlc.access.."://api.soundcloud.com/i1/tracks/"..track.."/streams?client_id="..client_id.."&app_version="..app_version..( secret and "&secret_token="..secret or "" ) )
 
