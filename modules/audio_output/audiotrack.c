f56819d166dde3afde0b4f94609162508f49f686
Thomas Guillem <thomas@gllm.fr>
2016-12-03 10:22:13 +0100

    aout: audiotrack: remove translations
    No point in having translations for a module that is only used by libvlc.

--- modules/audio_output/audiotrack.c
+++ modules/audio_output/audiotrack.c
@@ -171,7 +171,7 @@ struct aout_sys_t {
 
 vlc_module_begin ()
     set_shortname( "AudioTrack" )
-    set_description( N_( "Android AudioTrack audio output" ) )
+    set_description( "Android AudioTrack audio output" )
     set_capability( "audio output", 180 )
     set_category( CAT_AUDIO )
     set_subcategory( SUBCAT_AUDIO_AOUT )
