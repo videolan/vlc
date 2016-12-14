aeb22b2cb088d15a14bf5a8b61868349682451ec
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-12-14 12:46:36 +0100

    codec: ass: Don't force fonts that aren't shipped anymore in the winstore app

--- modules/codec/libass.c
+++ modules/codec/libass.c
@@ -218,9 +218,6 @@ static int Create( vlc_object_t *p_this )
 #elif defined( __APPLE__ )
     const char *psz_font = NULL; /* We don't ship a default font with VLC */
     const char *psz_family = "Helvetica Neue"; /* Use HN if we can't find anything more suitable - Arial is not on all Apple platforms */
-#elif VLC_WINSTORE_APP
-    const char *psz_font = "NotoSans-Regular.ttf";
-    const char *psz_family = "Noto Sans";
 #else
     const char *psz_font = NULL; /* We don't ship a default font with VLC */
     const char *psz_family = "Arial"; /* Use Arial if we can't find anything more suitable */
