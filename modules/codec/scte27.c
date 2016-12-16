57b6d3be82f6447481c126b1b8d3d8b3885d18d7
Francois Cartegnie <fcvlcdev@free.fr>
2016-12-16 15:52:03 +0100

    codec: scte27: fix reading YCrCb into YCbCr (fix #17787)

--- modules/codec/scte27.c
+++ modules/codec/scte27.c
@@ -76,8 +76,8 @@ static scte27_color_t bs_read_color(bs_t *bs)
     /* XXX it's unclear if a value of 0 in Y/U/V means a transparent pixel */
     color.y     = bs_read(bs, 5) << 3;
     color.alpha = bs_read1(bs) ? 0xff : 0x80;
-    color.u     = bs_read(bs, 5) << 3;
     color.v     = bs_read(bs, 5) << 3;
+    color.u     = bs_read(bs, 5) << 3;
 
     return color;
 }
