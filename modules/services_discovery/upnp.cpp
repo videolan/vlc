6c968674c2239a64554fb03fbddd7385de33204d
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-01-18 14:25:33 +0100

    upnp: Fix potential NULL dereference
    Fix CID #1398413

--- modules/services_discovery/upnp.cpp
+++ modules/services_discovery/upnp.cpp
@@ -309,6 +309,8 @@ bool MediaServerList::addServer( MediaServerDesc* desc )
         p_input_item = input_item_NewDirectory( desc->location.c_str(),
                                                 desc->friendlyName.c_str(),
                                                 ITEM_NET );
+        if ( !p_input_item )
+            return false;
 
         input_item_SetSetting( p_input_item, SATIP_SERVER_DEVICE_TYPE );
 
@@ -329,12 +331,13 @@ bool MediaServerList::addServer( MediaServerDesc* desc )
         p_input_item = input_item_NewDirectory( psz_mrl,
                                                 desc->friendlyName.c_str(),
                                                 ITEM_NET );
+        free( psz_mrl );
+
+        if ( !p_input_item )
+            return false;
 
         input_item_SetSetting( p_input_item, MEDIA_SERVER_DEVICE_TYPE );
-        free( psz_mrl );
     }
-    if ( !p_input_item )
-        return false;
 
     if ( desc->iconUrl.empty() == false )
         input_item_SetArtworkURL( p_input_item, desc->iconUrl.c_str() );
