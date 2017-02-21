151e802223b18ae3ebf2e25264a17727e8143058
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-02-21 17:35:11 +0100

    freetype: Fix aspect ratio
    Fix #18034

--- modules/text_renderer/freetype/freetype.c
+++ modules/text_renderer/freetype/freetype.c
@@ -424,6 +424,7 @@ static int RenderYUVP( filter_t *p_filter, subpicture_region_t *p_region,
     fmt.i_visible_width  = p_bbox->xMax - p_bbox->xMin + 4;
     fmt.i_height         =
     fmt.i_visible_height = p_bbox->yMax - p_bbox->yMin + 4;
+    fmt.i_sar_num = fmt.i_sar_den = 1;
 
     assert( !p_region->p_picture );
     p_region->p_picture = picture_NewFromFormat( &fmt );
@@ -824,6 +825,7 @@ static inline int RenderAXYZ( filter_t *p_filter,
     fmt.i_visible_width  = i_text_width  + 2 * i_margin;
     fmt.i_height         =
     fmt.i_visible_height = i_text_height + 2 * i_margin;
+    fmt.i_sar_num = fmt.i_sar_den = 1;
 
     picture_t *p_picture = p_region->p_picture = picture_NewFromFormat( &fmt );
     if( !p_region->p_picture )
