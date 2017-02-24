7641dfe3eabcd7c3f1c2ae56cac3ed98b099d892
Rémi Denis-Courmont <remi@remlab.net>
2017-02-24 22:01:38 +0200

    strings: use vlc_memstream in vlc_xml_encode()

--- src/text/strings.c
+++ src/text/strings.c
@@ -284,38 +284,46 @@ void vlc_xml_decode( char *psz_value )
 
 char *vlc_xml_encode (const char *str)
 {
-    assert (str != NULL);
-
-    const size_t len = strlen (str);
-    char *const buf = malloc (6 * len + 1), *ptr = buf;
-    if (unlikely(buf == NULL))
-        return NULL;
-
+    struct vlc_memstream stream;
     size_t n;
     uint32_t cp;
+
+    assert(str != NULL);
+    vlc_memstream_open(&stream);
 
     while ((n = vlc_towc (str, &cp)) != 0)
     {
         if (unlikely(n == (size_t)-1))
         {
-            free (buf);
+            if (vlc_memstream_close(&stream))
+                free(stream.ptr);
             errno = EILSEQ;
             return NULL;
         }
 
         switch (cp)
         {
-            case '\"': strcpy (ptr, "&quot;"); ptr += 6; break;
-            case '&':  strcpy (ptr, "&amp;");  ptr += 5; break;
-            case '\'': strcpy (ptr, "&#39;");  ptr += 5; break;
-            case '<':  strcpy (ptr, "&lt;");   ptr += 4; break;
-            case '>':  strcpy (ptr, "&gt;");   ptr += 4; break;
+            case '\"':
+                vlc_memstream_puts(&stream, "&quot;");
+                break;
+            case '&':
+                vlc_memstream_puts(&stream, "&amp;");
+                break;
+            case '\'':
+                vlc_memstream_puts(&stream, "&#39;");
+                break;
+            case '<':
+                vlc_memstream_puts(&stream, "&lt;");
+                break;
+            case '>':
+                vlc_memstream_puts(&stream, "&gt;");
+                break;
             default:
                 if (cp < 32) /* C0 code not allowed (except 9, 10 and 13) */
                     break;
                 if (cp >= 128 && cp < 160) /* C1 code encoded (except 133) */
                 {
-                    ptr += sprintf (ptr, "&#%"PRIu32";", cp);
+                    vlc_memstream_printf(&stream, "&#%"PRIu32";", cp);
                     break;
                 }
                 /* fall through */
@@ -323,16 +331,15 @@ char *vlc_xml_encode (const char *str)
             case 10:
             case 13:
             case 133:
-                memcpy (ptr, str, n);
-                ptr += n;
+                vlc_memstream_write(&stream, str, n);
                 break;
         }
         str += n;
     }
-    *(ptr++) = '\0';
 
-    ptr = realloc (buf, ptr - buf);
-    return likely(ptr != NULL) ? ptr : buf; /* cannot fail */
+    if (vlc_memstream_close(&stream))
+        return NULL;
+    return stream.ptr;
 }
 
 /* Base64 encoding */
