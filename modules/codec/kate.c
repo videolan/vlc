33ec46a8df59b252db1c0ee3b28a1b22574724ec
Filip Roséen <filip@atch.se>
2017-02-25 07:39:02 +0100

    codec/kate: remove TigerConfigurationCallback + OnConfigurationChanged (unused)
    The last usage of these functions, TigerConfigurationCallback is the
    only one that ever called OnConfigurationChanged, was removed as part
    of 231b08e in May 2011.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/codec/kate.c
+++ modules/codec/kate.c
@@ -179,11 +179,6 @@ static void UpdateTigerBackgroundColor( decoder_t *p_dec );
 static void UpdateTigerFontEffect( decoder_t *p_dec );
 static void UpdateTigerQuality( decoder_t *p_dec );
 static void UpdateTigerFontDesc( decoder_t *p_dec );
-static int TigerConfigurationCallback( vlc_object_t *p_this, const char *psz_var,
-                                       vlc_value_t oldvar, vlc_value_t newval,
-                                       void *p_data );
-static int OnConfigurationChanged( decoder_t *p_dec, const char *psz_var,
-                                   vlc_value_t oldval, vlc_value_t newval);
 #endif
 
 #define DEFAULT_NAME "Default"
@@ -1023,114 +1018,6 @@ static void UpdateTigerFontEffect( decoder_t *p_dec )
                                                              p_sys->e_tiger_default_font_effect,
                                                              p_sys->f_tiger_default_font_effect_strength ) );
     p_sys->b_dirty = true;
-}
-
-static int OnConfigurationChanged( decoder_t *p_dec, const char *psz_var,
-                                   vlc_value_t oldval, vlc_value_t newval )
-{
-    decoder_sys_t *p_sys = (decoder_sys_t*)p_dec->p_sys;
-
-    VLC_UNUSED( oldval );
-
-    vlc_mutex_lock( &p_sys->lock );
-
-    msg_Dbg( p_dec, "OnConfigurationChanged: %s", psz_var );
-
-    if( !p_sys->b_use_tiger || !p_sys->p_tr )
-    {
-        vlc_mutex_unlock( &p_sys->lock );
-        return VLC_SUCCESS;
-    }
-
-#define TEST_TIGER_VAR( name ) \
-    if( !strcmp( name, psz_var ) )
-
-    TEST_TIGER_VAR( "kate-tiger-quality" )
-    {
-        p_sys->f_tiger_quality = newval.f_float;
-        UpdateTigerQuality( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-font-desc" )
-    {
-        if( p_sys->psz_tiger_default_font_desc )
-        {
-            free( p_sys->psz_tiger_default_font_desc );
-            p_sys->psz_tiger_default_font_desc = NULL;
-        }
-        if( newval.psz_string )
-        {
-            p_sys->psz_tiger_default_font_desc = strdup( newval.psz_string );
-        }
-        UpdateTigerFontDesc( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-font-color" )
-    {
-        p_sys->i_tiger_default_font_color = (p_sys->i_tiger_default_font_color & 0xff000000U) | newval.i_int;
-        UpdateTigerFontColor( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-font-alpha" )
-    {
-        p_sys->i_tiger_default_font_color = (p_sys->i_tiger_default_font_color & 0x00ffffff) | (newval.i_int<<24);
-        UpdateTigerFontColor( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-background-color" )
-    {
-        p_sys->i_tiger_default_background_color = (p_sys->i_tiger_default_background_color & 0xff000000U) | newval.i_int;
-        UpdateTigerBackgroundColor( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-background-alpha" )
-    {
-        p_sys->i_tiger_default_background_color = (p_sys->i_tiger_default_background_color & 0x00ffffff) | (newval.i_int<<24);
-        UpdateTigerBackgroundColor( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-font-effect" )
-    {
-        p_sys->e_tiger_default_font_effect = (tiger_font_effect)newval.i_int;
-        UpdateTigerFontEffect( p_dec );
-    }
-
-    TEST_TIGER_VAR( "kate-tiger-default-font-effect-strength" )
-    {
-        p_sys->f_tiger_default_font_effect_strength = newval.f_float;
-        UpdateTigerFontEffect( p_dec );
-    }
-
-#undef TEST_TIGER_VAR
-
-    vlc_mutex_unlock( &p_sys->lock );
-
-    return VLC_SUCCESS;
-}
-
-static int TigerConfigurationCallback( vlc_object_t *p_this, const char *psz_var,
-                                       vlc_value_t oldval, vlc_value_t newval,
-                                       void *p_data )
-{
-    size_t i_idx;
-
-    VLC_UNUSED( p_this );
-    VLC_UNUSED( oldval );
-    VLC_UNUSED( newval );
-    VLC_UNUSED( p_data );
-
-    vlc_mutex_lock( &kate_decoder_list_mutex );
-
-    /* Update all existing decoders from the global user prefs */
-    for( i_idx = 0; i_idx < kate_decoder_list_size; i_idx++ )
-    {
-        decoder_t *p_dec = kate_decoder_list[ i_idx ];
-        OnConfigurationChanged( p_dec, psz_var, oldval, newval );
-    }
-
-    vlc_mutex_unlock( &kate_decoder_list_mutex );
-
-    return VLC_SUCCESS;
 }
 
 #endif
