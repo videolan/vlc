videolan/vlc

commit 0a9d2d08d82aab076533d5007f90434e78ec0ede

contrib: mfx: check git hash

contrib/src/mfx/rules.mak
@@ -1,6 +1,7 @@
 # mfx (Media SDK)
 
 mfx_GITURL := https://github.com/lu-zero/mfx_dispatch.git
+MFX_GITHASH := 7adf2e463149adf6820de745a4d9e5d9a1ba8763
 
 ifeq ($(call need_pkg,"mfx"),)
 PKGS_FOUND += mfx
@@ -10,13 +11,13 @@ PKGS += mfx
 endif
 
 $(TARBALLS)/mfx-git.tar.xz:
-	$(call download_git,$(mfx_GITURL),,7adf2e4)
+	$(call download_git,$(mfx_GITURL),,$(MFX_GITHASH))
 
 .sum-mfx: mfx-git.tar.xz
-	$(warning $@ not implemented)
+	$(call check_githash,$(MFX_GITHASH))
 	touch $@
 
-mfx: mfx-git.tar.xz
+mfx: mfx-git.tar.xz .sum-mfx
 	$(UNPACK)
 	cd $(UNPACK_DIR) && autoreconf -ivf
 	$(MOVE)

commit d12e3442aaf70e0181c21855753d777954e3d74c

contribs: fix mfx rule broken in 0a9d2d0

contrib/src/mfx/rules.mak
@@ -17,7 +17,7 @@ $(TARBALLS)/mfx-$(MFX_GITHASH).tar.xz:
 	$(call check_githash,$(MFX_GITHASH))
 	touch $@
 
-mfx: mfx-git.tar.xz .sum-mfx
+mfx: mfx-$(MFX_GITHASH).tar.xz .sum-mfx
 	$(UNPACK)
 	cd $(UNPACK_DIR) && autoreconf -ivf
 	$(MOVE)