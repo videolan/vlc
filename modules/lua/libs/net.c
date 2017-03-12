5ed80c35421fef6fb900c48cb496720a37fb5641
Rémi Denis-Courmont <remi@remlab.net>
2017-03-12 19:26:57 +0200

    lua: use net_ConnectTCP() where applicable

--- modules/lua/libs/net.c
+++ modules/lua/libs/net.c
@@ -252,7 +252,7 @@ static int vlclua_net_connect_tcp( lua_State *L )
     vlc_object_t *p_this = vlclua_get_this( L );
     const char *psz_host = luaL_checkstring( L, 1 );
     int i_port = luaL_checkint( L, 2 );
-    int i_fd = net_Connect( p_this, psz_host, i_port, SOCK_STREAM, IPPROTO_TCP );
+    int i_fd = net_ConnectTCP( p_this, psz_host, i_port );
     lua_pushinteger( L, vlclua_fd_map_safe( L, i_fd ) );
     return 1;
 }
