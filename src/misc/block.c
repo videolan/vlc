c41af569b2d64be974cf06725c695566429f2089
Steve Lhomme <robUx4@videolabs.io>
2017-04-05 14:55:10 +0200

    block: do not use pass uninitialized data to ReadFile()
    All the others fields were uninitialized causing ReadFile() to fail.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/misc/block.c
+++ src/misc/block.c
@@ -348,7 +348,7 @@ ssize_t pread (int fd, void *buf, size_t count, off_t offset)
     if (handle == INVALID_HANDLE_VALUE)
         return -1;
 
-    OVERLAPPED olap; olap.Offset = offset; olap.OffsetHigh = (offset >> 32);
+    OVERLAPPED olap = {.Offset = offset, .OffsetHigh = (offset >> 32)};
     DWORD written;
     /* This braindead API will override the file pointer even if we specify
      * an explicit read offset... So do not expect this to mix well with
