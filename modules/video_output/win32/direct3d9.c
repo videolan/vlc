70ca97dd0c9484c07eea00f0553c5b7f971b09ed
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-04-07 15:57:19 +0200

    direct3d9: Enable software decoding direct rendering

--- modules/video_output/win32/direct3d9.c
+++ modules/video_output/win32/direct3d9.c
@@ -273,10 +273,10 @@ static int Open(vlc_object_t *object)
 
     /* */
     vout_display_info_t info = vd->info;
-    info.is_slow = !is_d3d9_opaque(fmt.i_chroma);
+    info.is_slow = !is_d3d9_opaque(fmt.i_chroma) && (vd->fmt.i_chroma != vd->source.i_chroma);
     info.has_double_click = true;
     info.has_hide_mouse = false;
-    info.has_pictures_invalid = !is_d3d9_opaque(fmt.i_chroma);
+    info.has_pictures_invalid = info.is_slow;
     if (var_InheritBool(vd, "direct3d9-hw-blending") &&
         sys->d3dregion_format != D3DFMT_UNKNOWN &&
         (sys->d3dcaps.SrcBlendCaps  & D3DPBLENDCAPS_SRCALPHA) &&
