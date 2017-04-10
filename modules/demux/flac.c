038cd29831604ce39346ddd1b01b888fb38824b0
Francois Cartegnie <fcvlcdev@free.fr>
2017-04-10 15:45:45 +0200

    demux: flac: replace xmalloc

--- modules/demux/flac.c
+++ modules/demux/flac.c
@@ -548,8 +548,10 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
         if( p_sys->i_attachments <= 0 )
             return VLC_EGENERIC;
 
+        *ppp_attach = malloc( sizeof(input_attachment_t*) * p_sys->i_attachments );
+        if( !*ppp_attach )
+            return VLC_EGENERIC;
         *pi_int = p_sys->i_attachments;
-        *ppp_attach = xmalloc( sizeof(input_attachment_t*) * p_sys->i_attachments );
         for( int i = 0; i < p_sys->i_attachments; i++ )
             (*ppp_attach)[i] = vlc_input_attachment_Duplicate( p_sys->attachments[i] );
         return VLC_SUCCESS;
