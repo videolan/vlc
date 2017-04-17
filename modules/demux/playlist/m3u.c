4af4f54bc42c907e339bf18686ec54f659501da0
Rémi Denis-Courmont <remi@remlab.net>
2017-04-17 23:13:12 +0300

    m3u: retrieve Content-Type only once

--- modules/demux/playlist/m3u.c
+++ modules/demux/playlist/m3u.c
@@ -77,19 +77,26 @@ int Import_M3U( vlc_object_t *p_this )
         offset = 3;
     }
 
+    char *type = stream_MimeType(p_demux->s);
+
     if( demux_IsPathExtension( p_demux, ".m3u8" )
      || demux_IsForced( p_demux, "m3u8" )
-     || CheckMimeType( p_demux->s, "application/vnd.apple.mpegurl" ) )
+     || (type != NULL && strcasecmp(type, "application/vnd.apple.mpegurl")))
+    {
         pf_dup = CheckUnicode; /* UTF-8 file type */
+        free(type);
+    }
     else
     if( demux_IsPathExtension( p_demux, ".m3u" )
      || demux_IsPathExtension( p_demux, ".vlc" )
      || demux_IsForced( p_demux, "m3u" )
      || ContainsURL( p_demux )
-     || CheckMimeType( p_demux->s, "audio/x-mpegurl") )
-        ; /* Guess encoding */
+     || (type != NULL && strcasecmp(type, "audio/x-mpegurl")))
+        free(type); /* Guess encoding */
     else
     {
+        free(type);
+
         if( vlc_stream_Peek( p_demux->s, &p_peek, 8 + offset ) < (8 + offset) )
             return VLC_EGENERIC;
 
