c9219a6f84a2a6ef8b81cd6b6b5fe9d2bb14bf6a
Rémi Denis-Courmont <remi@remlab.net>
2017-04-19 20:40:58 +0300

    mux_avi: remove redundant casts

--- modules/mux/avi.c
+++ modules/mux/avi.c
@@ -275,17 +275,17 @@ static int Control( sout_mux_t *p_mux, int i_query, va_list args )
    switch( i_query )
    {
        case MUX_CAN_ADD_STREAM_WHILE_MUXING:
-           pb_bool = (bool*)va_arg( args, bool * );
+           pb_bool = va_arg( args, bool * );
            *pb_bool = false;
            return VLC_SUCCESS;
 
        case MUX_GET_ADD_STREAM_WAIT:
-           pb_bool = (bool*)va_arg( args, bool * );
+           pb_bool = va_arg( args, bool * );
            *pb_bool = true;
            return VLC_SUCCESS;
 
        case MUX_GET_MIME:
-           ppsz = (char**)va_arg( args, char ** );
+           ppsz = va_arg( args, char ** );
            *ppsz = strdup( "video/avi" );
            return VLC_SUCCESS;
 
