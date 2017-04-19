663fd965fa6a65889c36a78d814c8ebc01ce2045
Rémi Denis-Courmont <remi@remlab.net>
2017-04-19 20:20:57 +0300

    avcapture: remove redundant casts

--- modules/access/avcapture.m
+++ modules/access/avcapture.m
@@ -449,17 +449,17 @@ static int Control(demux_t *p_demux, int i_query, va_list args)
         case DEMUX_CAN_SEEK:
         case DEMUX_SET_PAUSE_STATE:
         case DEMUX_CAN_CONTROL_PACE:
-           pb = (bool*)va_arg(args, bool *);
+           pb = va_arg(args, bool *);
            *pb = false;
            return VLC_SUCCESS;
 
         case DEMUX_GET_PTS_DELAY:
-           pi64 = (int64_t*)va_arg(args, int64_t *);
+           pi64 = va_arg(args, int64_t *);
            *pi64 = INT64_C(1000) * var_InheritInteger(p_demux, "live-caching");
            return VLC_SUCCESS;
 
         case DEMUX_GET_TIME:
-            pi64 = (int64_t*)va_arg(args, int64_t *);
+            pi64 = va_arg(args, int64_t *);
             *pi64 = mdate();
             return VLC_SUCCESS;
 
