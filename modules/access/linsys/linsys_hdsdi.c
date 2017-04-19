95821c4b5683955cd87be597f080c30817184095
Rémi Denis-Courmont <remi@remlab.net>
2017-04-19 20:25:24 +0300

    linsys_hdsdi: remove redundant casts

--- modules/access/linsys/linsys_hdsdi.c
+++ modules/access/linsys/linsys_hdsdi.c
@@ -309,12 +309,12 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
         case DEMUX_CAN_PAUSE:
         case DEMUX_CAN_CONTROL_PACE:
             /* TODO */
-            pb = (bool*)va_arg( args, bool * );
+            pb = va_arg( args, bool * );
             *pb = false;
             return VLC_SUCCESS;
 
         case DEMUX_GET_PTS_DELAY:
-            pi64 = (int64_t*)va_arg( args, int64_t * );
+            pi64 = va_arg( args, int64_t * );
             *pi64 = INT64_C(1000)
                   * var_InheritInteger( p_demux, "live-caching" );
             return VLC_SUCCESS;
