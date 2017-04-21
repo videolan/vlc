a16e72792aab02922a4a4b50dd711bf8a09ba13a
Thomas Guillem <thomas@gllm.fr>
2017-04-21 18:21:47 +0200

    vout: opengl: fix green lines with PBO
    Fixes #18213

--- modules/video_output/opengl/converters.c
+++ modules/video_output/opengl/converters.c
@@ -622,7 +622,7 @@ tc_pbo_update(const opengl_tex_converter_t *tc, GLuint *textures,
 
     for (int i = 0; i < pic->i_planes; i++)
     {
-        GLsizeiptr size = pic->p[i].i_visible_lines * pic->p[i].i_visible_pitch;
+        GLsizeiptr size = pic->p[i].i_lines * pic->p[i].i_pitch;
         const GLvoid *data = pic->p[i].p_pixels;
         tc->api->BindBuffer(GL_PIXEL_UNPACK_BUFFER,
                             display_pic->p_sys->buffers[i]);
