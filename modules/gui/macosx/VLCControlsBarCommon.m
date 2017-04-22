583a75c97aea6f8bc2a05050bd34ce03bc6bef75
David Fuhrmann <dfuhrmann@videolan.org>
2017-04-22 09:48:40 +0200

    macosx: detached window: Hide fullscreen button if native mode is on
    Simply remove the button from the view, autolayout and lower
    priority constraints take care of the rest.

--- modules/gui/macosx/VLCControlsBarCommon.m
+++ modules/gui/macosx/VLCControlsBarCommon.m
@@ -54,8 +54,8 @@
 
 - (void)awakeFromNib
 {
-    _darkInterface = config_GetInt(getIntf(), "macosx-interfacestyle");
-    _nativeFullscreenMode = NO;
+    _darkInterface = var_InheritBool(getIntf(), "macosx-interfacestyle");
+    _nativeFullscreenMode = var_InheritBool(getIntf(), "macosx-nativefullscreenmode");
 
     [self.dropView setDrawBorder: NO];
 
@@ -153,6 +153,7 @@
 
     // remove fullscreen button for lion fullscreen
     if (_nativeFullscreenMode) {
+        // TODO remove frame calculations
         CGFloat f_width = [self.fullscreenButton frame].size.width;
 
         NSRect frame = [self.timeField frame];
@@ -163,7 +164,7 @@
         frame.size.width = f_width + frame.size.width;
         [self.progressView setFrame: frame];
 
-        [self.fullscreenButton removeFromSuperviewWithoutNeedingDisplay];
+        [self.fullscreenButton removeFromSuperview];
     }
 
     if (config_GetInt(getIntf(), "macosx-show-playback-buttons"))
