73e1d1786cd552ca2a6317e5293dc83f2027a93a
Thomas Guillem <thomas@gllm.fr>
2017-04-28 08:24:16 +0200

    access: bluray: also flag discontinuity on SET_SEEKPOINT

--- modules/access/bluray.c
+++ modules/access/bluray.c
@@ -1838,6 +1838,7 @@ static int blurayControl(demux_t *p_demux, int query, va_list args)
     {
         int i_chapter = va_arg(args, int);
         bd_seek_chapter(p_sys->bluray, i_chapter);
+        notifyDiscontinuity( p_sys );
         p_demux->info.i_update |= INPUT_UPDATE_SEEKPOINT;
         break;
     }
