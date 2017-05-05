videolan/vlc

commit 869db8d97312545567506a4c06df045c14c6d02d

avcodec: remove unecessary check for thread safety on safe libavcodec builds
Technically there's not a version set after Anton's patches merge in FFmpeg yet

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/codec/avcodec/video.c
@@ -1348,7 +1348,7 @@ static enum PixelFormat ffmpeg_GetFormat( AVCodecContext *p_context,
     if (!can_hwaccel)
         return swfmt;
 
-#if (LIBAVCODEC_VERSION_MICRO >= 100) /* FFmpeg only */
+#if !LIBAVCODEC_VERSION_CHECK(57, 30, 1, 83, 101)
     if (p_context->active_thread_type)
     {
         msg_Warn(p_dec, "thread type %d: disabling hardware acceleration",

commit e167dd4e80a4afc7fc0e692c96e09977468cf037

avcodec: unbreak libav hardware acceleration (fixes #18170)
Regression from commit 869db8d97312545567506a4c06df045c14c6d02d.

modules/codec/avcodec/video.c
@@ -1463,7 +1463,8 @@ static enum PixelFormat ffmpeg_GetFormat( AVCodecContext *p_context,
     if (!can_hwaccel)
         return swfmt;
 
-#if !LIBAVCODEC_VERSION_CHECK(57, 30, 1, 83, 101)
+#if (LIBAVCODEC_VERSION_MICRO >= 100) \
+  && (LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 83, 101))
     if (p_context->active_thread_type)
     {
         msg_Warn(p_dec, "thread type %d: disabling hardware acceleration",