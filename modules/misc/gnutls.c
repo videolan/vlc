db06f5fba63e04913af03405e5d81e6fabbd0aab
Rémi Denis-Courmont <remi@remlab.net>
2017-05-09 18:09:36 +0300

    tls: fix typo

--- modules/misc/gnutls.c
+++ modules/misc/gnutls.c
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Öesser General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
