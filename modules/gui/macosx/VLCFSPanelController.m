674745e61f4e0b64a28bae46cda9a37096ba8cf2
Filip Roséen <filip@atch.se>
2017-05-10 03:30:09 +0200

    gui/macosx: VLCFSPanelController: s/continous/continuous/
    Signed-off-by: Marvin Scholz <epirat07@gmail.com>

--- modules/gui/macosx/VLCFSPanelController.m
+++ modules/gui/macosx/VLCFSPanelController.m
@@ -150,7 +150,7 @@ static NSString *kAssociatedFullscreenRect = @"VLCFullscreenAssociatedWindowRect
 {
     static NSTimeInterval last_event = 0;
     if (([NSDate timeIntervalSinceReferenceDate] - last_event) > 0.16) {
-        /* We just skipped 4 "continous" events, otherwise we are too fast */
+        /* We just skipped 4 "continuous" events, otherwise we are too fast */
         [[VLCCoreInteraction sharedInstance] forwardExtraShort];
         last_event = [NSDate timeIntervalSinceReferenceDate];
     }
@@ -160,7 +160,7 @@ static NSString *kAssociatedFullscreenRect = @"VLCFullscreenAssociatedWindowRect
 {
     static NSTimeInterval last_event = 0;
     if (([NSDate timeIntervalSinceReferenceDate] - last_event) > 0.16) {
-        /* We just skipped 4 "continous" events, otherwise we are too fast */
+        /* We just skipped 4 "continuous" events, otherwise we are too fast */
         [[VLCCoreInteraction sharedInstance] backwardExtraShort];
         last_event = [NSDate timeIntervalSinceReferenceDate];
     }
@@ -185,7 +185,7 @@ static NSString *kAssociatedFullscreenRect = @"VLCFullscreenAssociatedWindowRect
 {
     switch([[NSApp currentEvent] type]) {
         case NSLeftMouseUp:
-            /* Ignore mouse up, as this is a continous slider and
+            /* Ignore mouse up, as this is a continuous slider and
              * when the user does a single click to a position on the slider,
              * the action is called twice, once for the mouse down and once
              * for the mouse up event. This results in two short seeks one
