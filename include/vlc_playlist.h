a3b24fafb2cf1d9c325749212dfab369d923eb9b
Rémi Denis-Courmont <remi@remlab.net>
2017-05-14 17:21:22 +0300

    playlist: add playlist_ViewPlay() convenience wrapper
    This wraps playlist_Control() with type safety, and enforces
    playlist locking.

--- include/vlc_playlist.h
+++ include/vlc_playlist.h
@@ -306,6 +306,12 @@ VLC_API void playlist_Deactivate( playlist_t * );
  */
 VLC_API void playlist_Control( playlist_t *p_playlist, int i_query, bool b_locked, ...  );
 
+static inline void playlist_ViewPlay(playlist_t *pl, playlist_item_t *node,
+                                     playlist_item_t *item)
+{
+    playlist_Control(pl, PLAYLIST_VIEWPLAY, pl_Locked, node, item);
+}
+
 /** Get current playing input. The object is retained.
  */
 VLC_API input_thread_t * playlist_CurrentInput( playlist_t *p_playlist ) VLC_USED;
