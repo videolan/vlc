185a90ef642343982af4bdd29a149ddc0ed16480
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-05-16 18:02:13 +0200

    vorepository: Fix memleak

--- modules/misc/addons/vorepository.c
+++ modules/misc/addons/vorepository.c
@@ -512,7 +512,10 @@ static void Close(vlc_object_t *p_this)
 {
     addons_finder_t *p_finder = (addons_finder_t*) p_this;
     if ( p_finder->p_sys->psz_tempfile )
+    {
         vlc_unlink( p_finder->p_sys->psz_tempfile );
+        free( p_finder->p_sys->psz_tempfile );
+    }
     free( p_finder->p_sys );
 }
 
