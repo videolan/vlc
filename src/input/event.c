469c0f3bc9c7c38b48ccf6be44961e1bc7851f47
Rémi Denis-Courmont <remi@remlab.net>
2017-05-17 20:18:14 +0300

    input: deduplicate input item info event
    input_SendEventMetaInfo() is called after changing the "infos" of the
    input thread's input item. The input item meta event is already sent
    by the input item code.

--- src/input/event.c
+++ src/input/event.c
@@ -200,12 +200,6 @@ void input_SendEventMeta( input_thread_t *p_input )
 void input_SendEventMetaInfo( input_thread_t *p_input )
 {
     Trigger( p_input, INPUT_EVENT_ITEM_INFO );
-
-    /* FIXME remove this ugliness */
-    vlc_event_t event;
-
-    event.type = vlc_InputItemInfoChanged;
-    vlc_event_send( &input_priv(p_input)->p_item->event_manager, &event );
 }
 
 void input_SendEventMetaName( input_thread_t *p_input, const char *psz_name )
