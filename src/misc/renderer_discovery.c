00764b2bced8512b18303197ec030be6d3d55d29
Filip Roséen <filip@atch.se>
2017-05-18 13:26:57 +0200

    misc/renderer_discovery: vlc_rd_new: fix leak on error
    If no module can be found, the previous implementation would leak the
    resources potentially allocated through config_ChainCreate.
    
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- src/misc/renderer_discovery.c
+++ src/misc/renderer_discovery.c
@@ -257,6 +257,8 @@ vlc_rd_new(vlc_object_t *p_obj, const char *psz_name,
     if (p_rd->p_module == NULL)
     {
         msg_Err(p_rd, "no suitable renderer discovery module");
+        free(p_rd->psz_name);
+        config_ChainDestroy(p_rd->p_cfg);
         vlc_object_release(p_rd);
         p_rd = NULL;
     }
