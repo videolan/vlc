09b7dca92094b797ad9d65dd1164e389c48f0ad1
Filip Roséen <filip@atch.se>
2017-05-18 12:01:08 +0200

    playlist/item: playlist_InsertInputItemTree: simplify
    This changes gets rid of an unnecessary named variable, while also
    wrapping the previously very long line so that it does not span across
    80 cols.
    
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- src/playlist/item.c
+++ src/playlist/item.c
@@ -579,8 +579,8 @@ int playlist_InsertInputItemTree (
     playlist_t *p_playlist, playlist_item_t *p_parent,
     input_item_node_t *p_node, int i_pos, bool b_flat )
 {
-    playlist_item_t *p_first_leaf = NULL;
-    return RecursiveAddIntoParent ( p_playlist, p_parent, p_node, i_pos, b_flat, &p_first_leaf );
+    return RecursiveAddIntoParent( p_playlist, p_parent, p_node, i_pos, b_flat,
+                                   &(playlist_item_t*){ NULL } );
 }
 
 
