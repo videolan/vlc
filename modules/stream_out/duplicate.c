bb28dbbe7f21dfbeb6fff9563b66e8a3fdcc267c
Filip Roséen <filip@atch.se>
2017-05-18 12:19:28 +0200

    stream_out/duplicate: Close: narrow scope of iteration variable
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/stream_out/duplicate.c
+++ modules/stream_out/duplicate.c
@@ -165,10 +165,8 @@ static void Close( vlc_object_t * p_this )
     sout_stream_t     *p_stream = (sout_stream_t*)p_this;
     sout_stream_sys_t *p_sys = p_stream->p_sys;
 
-    int i;
-
     msg_Dbg( p_stream, "closing a duplication" );
-    for( i = 0; i < p_sys->i_nb_streams; i++ )
+    for( int i = 0; i < p_sys->i_nb_streams; i++ )
     {
         sout_StreamChainDelete(p_sys->pp_streams[i], p_sys->pp_last_streams[i]);
         free( p_sys->ppsz_select[i] );
