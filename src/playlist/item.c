videolan/vlc

commit 6f63de13746b52bffc387426729e96e5f82f1ce9

playlist/item: do not stop playback on empty node
fixes: #17352

Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

src/playlist/item.c
@@ -163,15 +163,11 @@ static void input_item_add_subitem_tree ( const vlc_event_t * p_event,
     //control playback only if it was the current playing item that got subitems
     if( b_current )
     {
-        if( last_pos == pos || ( b_stop && !b_flat ) || !b_autostart )
+        if( ( b_stop && !b_flat ) || !b_autostart )
         {
-            /* We stop, either because no sub-item was actually created, or some
-            flags/settings want us to do so at this point */
-            PL_UNLOCK;
             playlist_Stop( p_playlist );
-            return;
         }
-        else
+        else if( last_pos != pos ) /* any children? */
         {
             /* Continue to play, either random or the first new item */
             playlist_item_t *p_play_item;

commit c1f04fba2fefd3202e7feb5b0203b90a8fcacc6b

playlist/item: input_item_add_subitem_tree: do not try to lock already locked playlist
This is a fixup of 6f63de1, which accidentally resulted in the
implementation trying to acquire an already acquired mutex.

Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

src/playlist/item.c
@@ -165,7 +165,7 @@ static void input_item_add_subitem_tree ( const vlc_event_t * p_event,
     {
         if( ( b_stop && !b_flat ) || !b_autostart )
         {
-            playlist_Stop( p_playlist );
+            playlist_Control( p_playlist, PLAYLIST_STOP, pl_Locked );
         }
         else if( last_pos != pos ) /* any children? */
         {