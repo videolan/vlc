3fb5f7ce6f00b65198b5044571f73fedd9605612
Filip Roséen <filip@atch.se>
2017-05-18 12:19:30 +0200

    text/strings: vlc_xml_decode: use ARRAY_SIZE
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/text/strings.c
+++ src/text/strings.c
@@ -47,6 +47,7 @@
 
 #include <vlc_strings.h>
 #include <vlc_charset.h>
+#include <vlc_arrays.h>
 #include <libvlc.h>
 #include <errno.h>
 
@@ -254,7 +255,7 @@ void vlc_xml_decode( char *psz_value )
                 const struct xml_entity_s *ent;
 
                 ent = bsearch (psz_value + 1, xml_entities,
-                               sizeof (xml_entities) / sizeof (*ent),
+                               ARRAY_SIZE (xml_entities),
                                sizeof (*ent), cmp_entity);
                 if (ent != NULL)
                 {
