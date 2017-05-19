300e7430801d4446ec928402d454abe0e14a0bed
Filip Roséen <filip@atch.se>
2017-05-19 14:44:30 +0200

    demux/pva: Close: properly release allocated block-chain
    p_sys->p_es and p_sys->p_pes are treated as block-chains, and not as
    single blocks. As such block_ChainRelease shall be used to release the
    full chain, as block_Release will only release the front-most block.
    
    fixes: #17997
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/demux/pva.c
+++ modules/demux/pva.c
@@ -128,8 +128,8 @@ static void Close( vlc_object_t *p_this )
     demux_t     *p_demux = (demux_t*)p_this;
     demux_sys_t *p_sys = p_demux->p_sys;
 
-    if( p_sys->p_es )  block_Release( p_sys->p_es );
-    if( p_sys->p_pes ) block_Release( p_sys->p_pes );
+    if( p_sys->p_es )  block_ChainRelease( p_sys->p_es );
+    if( p_sys->p_pes ) block_ChainRelease( p_sys->p_pes );
 
     free( p_sys );
 }
