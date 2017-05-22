b18794b12c0d00c402ae4a232377fa2d2df566a6
Rémi Denis-Courmont <remi@remlab.net>
2017-05-22 20:42:37 +0300

    ifo: convert to stream filter

--- modules/demux/playlist/ifo.c
+++ modules/demux/playlist/ifo.c
@@ -29,7 +29,7 @@
 #endif
 
 #include <vlc_common.h>
-#include <vlc_demux.h>
+#include <vlc_access.h>
 #include <assert.h>
 
 #include "playlist.h"
@@ -37,23 +37,23 @@
 /*****************************************************************************
  * Local prototypes
  *****************************************************************************/
-static int Demux( demux_t *p_demux);
-static int DemuxDVD_VR( demux_t *p_demux);
+static int ReadDVD( stream_t *, input_item_node_t * );
+static int ReadDVD_VR( stream_t *, input_item_node_t * );
 
 /*****************************************************************************
  * Import_IFO: main import function
  *****************************************************************************/
 int Import_IFO( vlc_object_t *p_this )
 {
-    demux_t *p_demux = (demux_t *)p_this;
+    stream_t *p_demux = (stream_t *)p_this;
 
     CHECK_FILE(p_demux);
-    if( !p_demux->psz_file )
+    if( p_demux->psz_filepath == NULL )
         return VLC_EGENERIC;
 
-    size_t len = strlen( p_demux->psz_file );
+    size_t len = strlen( p_demux->psz_filepath );
 
-    char *psz_file = p_demux->psz_file + len - strlen( "VIDEO_TS.IFO" );
+    char *psz_file = p_demux->psz_filepath + len - strlen( "VIDEO_TS.IFO" );
     /* Valid filenames are :
      *  - VIDEO_TS.IFO
      *  - VTS_XX_X.IFO where X are digits
@@ -63,37 +63,34 @@ int Import_IFO( vlc_object_t *p_this )
         || (!strncasecmp( psz_file, "VTS_", 4 )
         && !strcasecmp( psz_file + strlen( "VTS_00_0" ) , ".IFO" ) ) ) )
     {
-        int i_peek;
         const uint8_t *p_peek;
-        i_peek = vlc_stream_Peek( p_demux->s, &p_peek, 8 );
+        ssize_t i_peek = vlc_stream_Peek( p_demux->p_source, &p_peek, 8 );
 
         if( i_peek != 8 || memcmp( p_peek, "DVDVIDEO", 8 ) )
             return VLC_EGENERIC;
 
-        p_demux->pf_demux = Demux;
+        p_demux->pf_readdir = ReadDVD;
     }
     /* Valid filename for DVD-VR is VR_MANGR.IFO */
-    else if( len >= 12 && !strcmp( &p_demux->psz_file[len-12], "VR_MANGR.IFO" ) )
+    else if( len >= 12 && !strcmp( &p_demux->psz_filepath[len-12], "VR_MANGR.IFO" ) )
     {
-        int i_peek;
         const uint8_t *p_peek;
-        i_peek = vlc_stream_Peek( p_demux->s, &p_peek, 8 );
+        ssize_t i_peek = vlc_stream_Peek( p_demux->p_source, &p_peek, 8 );
 
         if( i_peek != 8 || memcmp( p_peek, "DVD_RTR_", 8 ) )
             return VLC_EGENERIC;
 
-        p_demux->pf_demux = DemuxDVD_VR;
+        p_demux->pf_readdir = ReadDVD_VR;
     }
     else
         return VLC_EGENERIC;
 
-//    STANDARD_DEMUX_INIT_MSG( "found valid VIDEO_TS.IFO" )
-    p_demux->pf_control = Control;
+    p_demux->pf_control = access_vaDirectoryControlHelper;
 
     return VLC_SUCCESS;
 }
 
-static int Demux( demux_t *p_demux )
+static int ReadDVD( stream_t *p_demux, input_item_node_t *node )
 {
     char *psz_url, *psz_dir;
 
@@ -104,17 +101,16 @@ static int Demux( demux_t *p_demux )
     if( asprintf( &psz_url, "dvd://%s", p_demux->psz_location ) == -1 )
         return 0;
 
-    input_item_t *p_current_input = GetCurrentItem(p_demux);
     input_item_t *p_input = input_item_New( psz_url, psz_url );
-    input_item_PostSubItem( p_current_input, p_input );
+    input_item_node_AppendItem( node, p_input );
     input_item_Release( p_input );
 
     free( psz_url );
 
-    return 0; /* Needed for correct operation of go back */
+    return VLC_SUCCESS;
 }
 
-static int DemuxDVD_VR( demux_t *p_demux )
+static int ReadDVD_VR( stream_t *p_demux, input_item_node_t *node )
 {
     size_t len = strlen( p_demux->psz_location );
     char *psz_url = malloc( len + 1 );
@@ -126,13 +122,11 @@ static int DemuxDVD_VR( demux_t *p_demux )
     memcpy( psz_url, p_demux->psz_location, len );
     memcpy( psz_url + len, "VR_MOVIE.VRO", 13 );
 
-    input_item_t *p_current_input = GetCurrentItem(p_demux);
     input_item_t *p_input = input_item_New( psz_url, psz_url );
-    input_item_PostSubItem( p_current_input, p_input );
-
+    input_item_node_AppendItem( node, p_input );
     input_item_Release( p_input );
 
     free( psz_url );
 
-    return 0; /* Needed for correct operation of go back */
+    return VLC_SUCCESS;
 }
