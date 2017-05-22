af61af4557909340220e3d2ecaaf905e9ca48d5c
Thomas Guillem <thomas@gllm.fr>
2017-05-22 15:58:20 +0200

    playlist: fix null-deref if there is no aout

--- src/playlist/aout.c
+++ src/playlist/aout.c
@@ -133,7 +133,7 @@ void playlist_EnableAudioFilter (playlist_t *pl, const char *name, bool add)
 {
     audio_output_t *aout = playlist_GetAout (pl);
 
-    aout_ChangeFilterString (VLC_OBJECT(pl), VLC_OBJECT(aout),
+    aout_ChangeFilterString (VLC_OBJECT(pl), aout ? VLC_OBJECT(aout) : NULL,
                              "audio-filter", name, add);
     if (aout != NULL)
         vlc_object_release (aout);
