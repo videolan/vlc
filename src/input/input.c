e6f85aeaa734aeace475ad6a5ca4cb1351a6652b
Filip Roséen <filip@atch.se>
2017-05-25 21:58:05 +0200

    input: InputGetExtraFiles: handle rar splits
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/input/input.c
+++ src/input/input.c
@@ -2902,6 +2902,10 @@ static void InputGetExtraFiles( input_thread_t *p_input,
     } patterns[] = {
         /* XXX the order is important */
         { "concat", ".001", "%s.%.3d", 2, 999 },
+        { NULL, ".part1.rar","%s.part%.1d.rar", 2, 9 },
+        { NULL, ".part01.rar","%s.part%.2d.rar", 2, 99, },
+        { NULL, ".part001.rar", "%s.part%.3d.rar", 2, 999 },
+        { NULL, ".rar", "%s.r%.2d", 0, 99 },
     };
 
     TAB_INIT( *pi_list, *pppsz_list );
