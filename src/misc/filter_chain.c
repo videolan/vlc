6aa25676966d9da65ca929c18b4b021cbcf783c0
Thomas Guillem <thomas@gllm.fr>
2017-05-26 15:57:14 +0200

    filter_chain: load "chain" video filter as a fallback
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/misc/filter_chain.c
+++ src/misc/filter_chain.c
@@ -211,8 +211,18 @@ static filter_t *filter_chain_AppendInner( filter_chain_t *chain,
     filter->owner.sys = chain;
 
     assert( capability != NULL );
+    if( name != NULL && filter->b_allow_fmt_out_change )
+    {
+        /* Append the "chain" video filter to the current list.
+         * This filter will be used if the requested filter fails to load.
+         * It will then try to add a video converter before. */
+        char name_chained[strlen(name) + sizeof(",chain")];
+        sprintf( name_chained, "%s,chain", name );
+        filter->p_module = module_need( filter, capability, name_chained, true );
+    }
+    else
+        filter->p_module = module_need( filter, capability, name, name != NULL );
 
-    filter->p_module = module_need( filter, capability, name, name != NULL );
     if( filter->p_module == NULL )
         goto error;
 
