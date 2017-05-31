ed635879f16bea96437d117a072c18175dcb4705
Francois Cartegnie <fcvlcdev@free.fr>
2017-05-31 11:35:51 +0200

    vlc_subpicture: fix alignment usage comments

--- include/vlc_subpicture.h
+++ include/vlc_subpicture.h
@@ -59,9 +59,9 @@ struct subpicture_region_t
     video_format_t  fmt;                          /**< format of the picture */
     picture_t       *p_picture;          /**< picture comprising this region */
 
-    int             i_x;                             /**< position of region */
-    int             i_y;                             /**< position of region */
-    int             i_align;                  /**< alignment within a region */
+    int             i_x;      /**< position of region, relative to alignment */
+    int             i_y;      /**< position of region, relative to alignment */
+    int             i_align;      /**< alignment flags of region and content */
     int             i_alpha;                               /**< transparency */
 
     text_segment_t  *p_text;         /**< subtitle text, made of a list of segments */
