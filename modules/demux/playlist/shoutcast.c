06d64083aea9f390e3cfa048e87c06a878c736a2
Rémi Denis-Courmont <remi@remlab.net>
2017-06-03 22:39:07 +0300

    shoutcast: drop priority to zero
    This demuxer will only run when forced. So don´t bother provbing it if
    it was not forced.

--- modules/demux/playlist/shoutcast.c
+++ modules/demux/playlist/shoutcast.c
@@ -60,8 +60,6 @@ int Import_Shoutcast( vlc_object_t *p_this )
     demux_t *p_demux = (demux_t *)p_this;
 
     CHECK_FILE(p_demux);
-    if( !demux_IsForced( p_demux, "shout-winamp" ) )
-        return VLC_EGENERIC;
 
     p_demux->pf_demux = Demux;
     p_demux->pf_control = Control;
