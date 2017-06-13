a8e0c5c8c2392668d2b12ea49fe7e431ac285c9d
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-06-13 14:05:25 +0200

    direct3d9: Use a big enough back buffer
    This fixes display of videos with resolution bigger than the screen
    resolution when autoscale is off.
    Fix #18413

--- modules/video_output/win32/direct3d9.c
+++ modules/video_output/win32/direct3d9.c
@@ -814,9 +814,9 @@ static int Direct3D9FillPresentationParameters(vout_display_t *vd)
     d3dpp->Windowed               = TRUE;
     d3dpp->hDeviceWindow          = vd->sys->sys.hvideownd;
     d3dpp->BackBufferWidth        = __MAX((unsigned int)GetSystemMetrics(SM_CXVIRTUALSCREEN),
-                                          d3ddm.Width);
+                                          vd->source.i_width);
     d3dpp->BackBufferHeight       = __MAX((unsigned int)GetSystemMetrics(SM_CYVIRTUALSCREEN),
-                                          d3ddm.Height);
+                                          vd->source.i_height);
     d3dpp->SwapEffect             = D3DSWAPEFFECT_COPY;
     d3dpp->MultiSampleType        = D3DMULTISAMPLE_NONE;
     d3dpp->PresentationInterval   = D3DPRESENT_INTERVAL_DEFAULT;
