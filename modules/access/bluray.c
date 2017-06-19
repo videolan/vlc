videolan/vlc

commit 740dd861a4b2e5780e48720af50935aa57a677b7

access: allocate p_sys vlc_malloc/vlc_calloc

modules/access/bluray.c
@@ -653,7 +653,7 @@ static int blurayOpen(vlc_object_t *object)
     }
 
     /* */
-    p_demux->p_sys = p_sys = calloc(1, sizeof(*p_sys));
+    p_demux->p_sys = p_sys = vlc_calloc(obj, 1, sizeof(*p_sys));
     if (unlikely(!p_sys))
         return VLC_ENOMEM;
 
@@ -902,7 +902,6 @@ static void blurayClose(vlc_object_t *object)
     vlc_mutex_destroy(&p_sys->read_block_lock);
 
     free(p_sys->psz_bd_path);
-    free(p_sys);
 }
 
 /*****************************************************************************

modules/access/alsa.c
@@ -341,8 +341,7 @@ static uint16_t channel_maps[] = {
 static int Open (vlc_object_t *obj)
 {
     demux_t *demux = (demux_t *)obj;
-    demux_sys_t *sys = malloc (sizeof (*sys));
-
+    demux_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 
@@ -361,7 +360,6 @@ static int Open (vlc_object_t *obj)
     {
         msg_Err (demux, "cannot open ALSA device \"%s\": %s", device,
                  snd_strerror (val));
-        free (sys);
         return VLC_EGENERIC;
     }
     sys->pcm = pcm;
@@ -510,7 +508,6 @@ static int Open (vlc_object_t *obj)
     return VLC_SUCCESS;
 error:
     snd_pcm_close (pcm);
-    free (sys);
     return VLC_EGENERIC;
 }
 
@@ -523,5 +520,4 @@ static void Close (vlc_object_t *obj)
     vlc_join (sys->thread, NULL);
 
     snd_pcm_close (sys->pcm);
-    free (sys);
 }

modules/access/attachment.c
@@ -73,7 +73,7 @@ static int Open(vlc_object_t *object)
     if (!input)
         return VLC_EGENERIC;
 
-    access_sys_t *sys = malloc(sizeof (*sys));
+    access_sys_t *sys = vlc_malloc(object, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 
@@ -84,7 +84,6 @@ static int Open(vlc_object_t *object)
     if (sys->attachment == NULL) {
         msg_Err(access, "Failed to find the attachment '%s'",
                 access->psz_location);
-        free(sys);
         return VLC_EGENERIC;
     }
 
@@ -106,7 +105,6 @@ static void Close(vlc_object_t *object)
     access_sys_t *sys = access->p_sys;
 
     vlc_input_attachment_Delete(sys->attachment);
-    free(sys);
 }
 
 /* */

modules/access/avio.c
@@ -124,7 +124,7 @@ static int SetupAvioCb(vlc_object_t *access)
 int OpenAvio(vlc_object_t *object)
 {
     access_t *access = (access_t*)object;
-    access_sys_t *sys = malloc(sizeof(*sys));
+    access_sys_t *sys = vlc_malloc(object, sizeof(*sys));
     if (!sys)
         return VLC_ENOMEM;
     sys->context = NULL;
@@ -140,10 +140,8 @@ int OpenAvio(vlc_object_t *object)
                       access->psz_location) < 0)
         url = NULL;
 
-    if (!url) {
-        free(sys);
+    if (!url)
         return VLC_ENOMEM;
-    }
 
     /* */
     vlc_init_avformat(object);
@@ -172,7 +170,7 @@ int OpenAvio(vlc_object_t *object)
         msg_Err(access, "Failed to open %s: %s", url,
                 vlc_strerror_c(AVUNERROR(ret)));
         free(url);
-        goto error;
+        return VLC_EGENERIC;
     }
     free(url);
 
@@ -181,7 +179,7 @@ int OpenAvio(vlc_object_t *object)
     if (SetupAvioCb(VLC_OBJECT(access))) {
         msg_Err(access, "Module already in use");
         avio_close(sys->context);
-        goto error;
+        return VLC_EGENERIC;
     }
 #endif
 
@@ -202,10 +200,6 @@ int OpenAvio(vlc_object_t *object)
     access->p_sys = sys;
 
     return VLC_SUCCESS;
-
-error:
-    free(sys);
-    return VLC_EGENERIC;
 }
 
 /* */
@@ -221,7 +215,7 @@ int OutOpenAvio(vlc_object_t *object)
 
     config_ChainParse( access, "sout-avio-", ppsz_sout_options, access->p_cfg );
 
-    sout_access_out_sys_t *sys = malloc(sizeof(*sys));
+    sout_access_out_sys_t *sys = vlc_malloc(object, sizeof(*sys));
     if (!sys)
         return VLC_ENOMEM;
     sys->context = NULL;
@@ -230,7 +224,7 @@ int OutOpenAvio(vlc_object_t *object)
     vlc_init_avformat(object);
 
     if (!access->psz_path)
-        goto error;
+        return VLC_EGENERIC;
 
     int ret;
 #if LIBAVFORMAT_VERSION_MAJOR < 54
@@ -252,14 +246,14 @@ int OutOpenAvio(vlc_object_t *object)
     if (ret < 0) {
         errno = AVUNERROR(ret);
         msg_Err(access, "Failed to open %s", access->psz_path);
-        goto error;
+        return VLC_EGENERIC;
     }
 
 #if LIBAVFORMAT_VERSION_MAJOR < 54
     /* We can accept only one active user at any time */
     if (SetupAvioCb(VLC_OBJECT(access))) {
         msg_Err(access, "Module already in use");
-        goto error;
+        return VLC_EGENERIC;
     }
 #endif
 
@@ -269,10 +263,6 @@ int OutOpenAvio(vlc_object_t *object)
     access->p_sys = sys;
 
     return VLC_SUCCESS;
-
-error:
-    free(sys);
-    return VLC_EGENERIC;
 }
 
 void CloseAvio(vlc_object_t *object)
@@ -285,7 +275,6 @@ void CloseAvio(vlc_object_t *object)
 #endif
 
     avio_close(sys->context);
-    free(sys);
 }
 
 void OutCloseAvio(vlc_object_t *object)
@@ -298,7 +287,6 @@ void OutCloseAvio(vlc_object_t *object)
 #endif
 
     avio_close(sys->context);
-    free(sys);
 }
 
 static ssize_t Read(access_t *access, void *data, size_t size)

modules/access/cdda.c
@@ -223,17 +223,11 @@ static int DemuxOpen(vlc_object_t *obj)
         return VLC_EGENERIC;
 
     if (track == 0 /* Whole disc -> use access plugin */)
-    {
-        ioctl_Close(obj, dev);
-        return VLC_EGENERIC;
-    }
+        goto error;
 
-    demux_sys_t *sys = malloc(sizeof (*sys));
+    demux_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
-    {
-        ioctl_Close(obj, dev);
-        return VLC_ENOMEM;
-    }
+        goto error;
 
     demux->p_sys = sys;
     sys->vcddev = dev;
@@ -275,7 +269,6 @@ static int DemuxOpen(vlc_object_t *obj)
 
 error:
     ioctl_Close(obj, dev);
-    free(sys);
     return VLC_EGENERIC;
 }
 
@@ -285,7 +278,6 @@ static void DemuxClose(vlc_object_t *obj)
     demux_sys_t *sys = demux->p_sys;
 
     ioctl_Close(obj, sys->vcddev);
-    free(sys);
 }
 
 /*****************************************************************************
@@ -628,7 +620,7 @@ static int AccessOpen(vlc_object_t *obj)
         return VLC_EGENERIC;
     }
 
-    access_sys_t *sys = malloc(sizeof (*sys));
+    access_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
     {
         ioctl_Close(obj, dev);
@@ -679,7 +671,6 @@ static int AccessOpen(vlc_object_t *obj)
 error:
     free(sys->p_sectors);
     ioctl_Close(obj, dev);
-    free(sys);
     return VLC_EGENERIC;
 }
 
@@ -703,7 +694,6 @@ static void AccessClose(vlc_object_t *obj)
 
     free(sys->p_sectors);
     ioctl_Close(obj, sys->vcddev);
-    free(sys);
 }
 
 /*****************************************************************************

modules/access/concat.c
@@ -188,7 +188,7 @@ static int Open(vlc_object_t *obj)
     if (list == NULL)
         return VLC_EGENERIC;
 
-    access_sys_t *sys = malloc(sizeof (*sys));
+    access_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
     {
         free(list);
@@ -298,7 +298,6 @@ static void Close(vlc_object_t *obj)
     }
 
     var_Destroy(access, "concat-list");
-    free(sys);
 }
 
 #define INPUT_LIST_TEXT N_("Inputs list")

modules/access/dc1394.c
@@ -176,7 +176,7 @@ static int Open( vlc_object_t *p_this )
     p_demux->info.i_title = 0;
     p_demux->info.i_seekpoint = 0;
 
-    p_demux->p_sys = p_sys = calloc( 1, sizeof( demux_sys_t ) );
+    p_demux->p_sys = p_sys = vlc_calloc( p_this, 1, sizeof( demux_sys_t ) );
     if( !p_sys )
         return VLC_ENOMEM;
 
@@ -201,7 +201,6 @@ static int Open( vlc_object_t *p_this )
         msg_Err( p_demux, "Bad MRL, please check the option line "
                           "(MRL was: %s)",
                           p_demux->psz_location );
-        free( p_sys );
         return VLC_EGENERIC;
     }
 
@@ -209,14 +208,12 @@ static int Open( vlc_object_t *p_this )
     if( !p_sys->p_dccontext )
     {
         msg_Err( p_demux, "Failed to initialise libdc1394");
-        free( p_sys );
         return VLC_EGENERIC;
     }
 
     if( FindCamera( p_sys, p_demux ) != VLC_SUCCESS )
     {
         dc1394_free( p_sys->p_dccontext );
-        free( p_sys );
         return VLC_EGENERIC;
     }
 
@@ -224,7 +221,6 @@ static int Open( vlc_object_t *p_this )
     {
         msg_Err( p_demux, "No camera found !!" );
         dc1394_free( p_sys->p_dccontext );
-        free( p_sys );
         return VLC_EGENERIC;
     }
 
@@ -390,7 +386,6 @@ static void Close( vlc_object_t *p_this )
     dc1394_free(p_sys->p_dccontext);
 
     free( p_sys->video_device );
-    free( p_sys );
 }
 
 #if 0

modules/access/directory.c
@@ -53,7 +53,7 @@ struct access_sys_t
  *****************************************************************************/
 int DirInit (access_t *access, DIR *dir)
 {
-    access_sys_t *sys = malloc(sizeof (*sys));
+    access_sys_t *sys = vlc_malloc(VLC_OBJECT(access), sizeof (*sys));
     if (unlikely(sys == NULL))
         goto error;
 
@@ -77,7 +77,6 @@ int DirInit (access_t *access, DIR *dir)
 
 error:
     closedir(dir);
-    free(sys);
     return VLC_ENOMEM;
 }
 
@@ -108,7 +107,6 @@ void DirClose(vlc_object_t *obj)
 
     free(sys->base_uri);
     closedir(sys->dir);
-    free(sys);
 }
 
 int DirRead (access_t *access, input_item_node_t *node)

commit 287bca2710fc8809af0cd9ccd0664b704a082563

access: bluray: fix build
Regression from 740dd861a4b2e5780e48720af50935aa57a677b7

modules/access/bluray.c
@@ -653,7 +653,7 @@ static int blurayOpen(vlc_object_t *object)
     }
 
     /* */
-    p_demux->p_sys = p_sys = vlc_calloc(obj, 1, sizeof(*p_sys));
+    p_demux->p_sys = p_sys = vlc_calloc(object, 1, sizeof(*p_sys));
     if (unlikely(!p_sys))
         return VLC_ENOMEM;
 