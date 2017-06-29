e050dc2535f82db639402e40f3cf971e209db8dc
Francois Cartegnie <fcvlcdev@free.fr>
2017-06-29 09:49:37 +0200

    codec: avcodec: check avcodec visible sizes
    refs #18467

--- modules/codec/avcodec/video.c
+++ modules/codec/avcodec/video.c
@@ -154,9 +154,11 @@ static int lavc_GetVideoFormat(decoder_t *dec, video_format_t *restrict fmt,
     else /* hardware decoding */
         fmt->i_chroma = vlc_va_GetChroma(pix_fmt, sw_pix_fmt);
 
-    if( width == 0 || height == 0 || width > 8192 || height > 8192 )
+    if( width == 0 || height == 0 || width > 8192 || height > 8192 ||
+        width < ctx->width || height < ctx->height )
     {
-        msg_Err(dec, "Invalid frame size %dx%d.", width, height);
+        msg_Err(dec, "Invalid frame size %dx%d vsz %dx%d",
+                     width, height, ctx->width, ctx->height );
         return -1; /* invalid display size */
     }
 
