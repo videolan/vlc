98c64ee8ad13e0e97a3bfd0e3a5fc9725b53194c
Steve Lhomme <robux4@videolabs.io>
2017-07-06 17:10:44 +0200

    demux:au: map raw PCM directly to the right format
    Rather than the interim raw signed big-endian.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/demux/au.c
+++ modules/demux/au.c
@@ -180,28 +180,28 @@ static int Open( vlc_object_t *p_this )
             break;
 
         case AU_LINEAR_8:      /* 8-bit linear PCM */
-            p_sys->fmt.i_codec               = VLC_FOURCC( 't','w','o','s' );
+            p_sys->fmt.i_codec               = VLC_CODEC_S8;
             p_sys->fmt.audio.i_bitspersample = 8;
             p_sys->fmt.audio.i_blockalign    = 1 * p_sys->fmt.audio.i_channels;
             i_cat                    = AU_CAT_PCM;
             break;
 
         case AU_LINEAR_16:     /* 16-bit linear PCM */
-            p_sys->fmt.i_codec               = VLC_FOURCC( 't','w','o','s' );
+            p_sys->fmt.i_codec               = VLC_CODEC_S16B;
             p_sys->fmt.audio.i_bitspersample = 16;
             p_sys->fmt.audio.i_blockalign    = 2 * p_sys->fmt.audio.i_channels;
             i_cat                    = AU_CAT_PCM;
             break;
 
         case AU_LINEAR_24:     /* 24-bit linear PCM */
-            p_sys->fmt.i_codec               = VLC_FOURCC( 't','w','o','s' );
+            p_sys->fmt.i_codec               = VLC_CODEC_S24B;
             p_sys->fmt.audio.i_bitspersample = 24;
             p_sys->fmt.audio.i_blockalign    = 3 * p_sys->fmt.audio.i_channels;
             i_cat                    = AU_CAT_PCM;
             break;
 
         case AU_LINEAR_32:     /* 32-bit linear PCM */
-            p_sys->fmt.i_codec               = VLC_FOURCC( 't','w','o','s' );
+            p_sys->fmt.i_codec               = VLC_CODEC_S32B;
             p_sys->fmt.audio.i_bitspersample = 32;
             p_sys->fmt.audio.i_blockalign    = 4 * p_sys->fmt.audio.i_channels;
             i_cat                    = AU_CAT_PCM;
