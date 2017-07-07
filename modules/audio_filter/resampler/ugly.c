90591791eec98a3ab204d63595311fc90f0111fd
Thomas Guillem <thomas@gllm.fr>
2017-07-07 11:40:18 +0200

    ugly: don't require a valid channel layout

--- modules/audio_filter/resampler/ugly.c
+++ modules/audio_filter/resampler/ugly.c
@@ -74,10 +74,7 @@ static int CreateResampler( vlc_object_t *p_this )
     filter_t * p_filter = (filter_t *)p_this;
 
     if( p_filter->fmt_in.audio.i_format != p_filter->fmt_out.audio.i_format
-     || p_filter->fmt_in.audio.i_physical_channels
-                                 != p_filter->fmt_out.audio.i_physical_channels
-     || p_filter->fmt_in.audio.i_original_channels
-                                 != p_filter->fmt_out.audio.i_original_channels
+     || p_filter->fmt_in.audio.i_channels != p_filter->fmt_out.audio.i_channels
      || !AOUT_FMT_LINEAR( &p_filter->fmt_in.audio ) )
         return VLC_EGENERIC;
 
