5f03dad116a4dfe192a925cc026aa22fcd03dedd
Steve Lhomme <robux4@videolabs.io>
2017-07-10 16:20:13 +0200

    cvdsub: fix the codec ID on the packetizer output
    All other decoders that also double as a packetizer do this.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/codec/cvdsub.c
+++ modules/codec/cvdsub.c
@@ -139,6 +139,7 @@ static int PacketizerOpen( vlc_object_t *p_this )
 
     if( DecoderOpen( p_this ) != VLC_SUCCESS ) return VLC_EGENERIC;
 
+    p_dec->fmt_out.i_codec = VLC_CODEC_CVD;
     p_dec->p_sys->b_packetizer = true;
 
     return VLC_SUCCESS;
