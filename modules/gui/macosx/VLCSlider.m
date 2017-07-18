105fa81d759f2dd0978c8e754482899821229438
Marvin Scholz <epirat07@gmail.com>
2017-07-18 03:02:20 +0200

    macosx: Add VLCSlider(Cell)

---
+++ modules/gui/macosx/VLCSlider.m
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ * VLCSlider.m
+ *****************************************************************************
+ * Copyright (C) 2017 VLC authors and VideoLAN
+ * $Id$
+ *
+ * Authors: Marvin Scholz <epirat07 at gmail dot com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#import "VLCSlider.h"
+#import "VLCSliderCell.h"
+
+@implementation VLCSlider
+
+// Workaround for 10.7
+// http://stackoverflow.com/questions/3985816/custom-nsslidercell
+- (void)setNeedsDisplayInRect:(NSRect)invalidRect {
+    [super setNeedsDisplayInRect:[self bounds]];
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    if(![self.cell isKindOfClass:[VLCSliderCell class]]) {
+        self.cell = [[VLCSliderCell alloc] init];
+    }
+}
+
+- (BOOL)getIndefinite
+{
+    return [(VLCSliderCell*)[self cell] indefinite];
+}
+
+- (void)setIndefinite:(BOOL)indefinite
+{
+    [(VLCSliderCell*)[self cell] setIndefinite:indefinite];
+}
+
+- (BOOL)isFlipped
+{
+    return NO;
+}
+
+@end
