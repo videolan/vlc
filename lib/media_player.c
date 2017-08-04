da7f1b805f6bebf1f4f9fd53910c42af15a84368
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-08-04 15:05:02 +0200

    lib: Automatically select the default teletext page on activation
    Fix #18638

--- lib/media_player.c
+++ lib/media_player.c
@@ -663,7 +663,7 @@ libvlc_media_player_new( libvlc_instance_t *instance )
     var_Create (mp, "deinterlace-mode", VLC_VAR_STRING | VLC_VAR_DOINHERIT);
 
     var_Create (mp, "vbi-page", VLC_VAR_INTEGER);
-    var_SetInteger (mp, "vbi-page", 0);
+    var_SetInteger (mp, "vbi-page", 100);
 
     var_Create (mp, "video-filter", VLC_VAR_STRING | VLC_VAR_DOINHERIT);
     var_Create (mp, "sub-source", VLC_VAR_STRING | VLC_VAR_DOINHERIT);
