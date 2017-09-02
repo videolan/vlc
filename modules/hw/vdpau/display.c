c8f218b5e2f68356cb5ce8b328df75a17e421246
Thomas Guillem <thomas@gllm.fr>
2017-09-02 13:27:55 +0200

    vdpau/display: drop capability
    Favor OpenGL vout with VDPAU interop.

--- modules/hw/vdpau/display.c
+++ modules/hw/vdpau/display.c
@@ -46,7 +46,7 @@ vlc_module_begin()
     set_description(N_("VDPAU output"))
     set_category(CAT_VIDEO)
     set_subcategory(SUBCAT_VIDEO_VOUT)
-    set_capability("vout display", 300)
+    set_capability("vout display", 0)
     set_callbacks(Open, Close)
 
     add_shortcut("vdpau", "xid")
