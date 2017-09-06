videolan/vlc

commit fc4114f44981aee52987a385649ce331d0f26c10

gl: fix incorrect plugin dependencies
The GL/GLES2 plugins doesn't depends anymore on X11, WAYLAND, VAAPI or other
hardware libs.

Move all converters (that were built in GL plugins) in separate plugins, with a
"glconv" plugin capability. This new type of module will be loaded by GL
plugins when a video format is opaque. Otherwise, the built-in software
converter (that handle RGB,YUV,YXZ12) will be used.

Each new converters plugins depends only on hardware libs like VAAPI-X11,
VAAPI-WL. These news plugins don't depend on Open GL / GLES2. Therefore, one
glconv plugin can be used by a GL and a GLES2 plugin.

Fixes #18575

modules/video_output/opengl/vout_helper.c
@@ -37,31 +37,17 @@
 #include <vlc_subpicture.h>
 #include <vlc_opengl.h>
 #include <vlc_memory.h>
+#include <vlc_modules.h>
 #include <vlc_vout.h>
 #include <vlc_viewpoint.h>
 
-#include "vout_helper.h"
-#include "converter.h"
+#include "internal.h"
 
 #ifndef GL_CLAMP_TO_EDGE
 # define GL_CLAMP_TO_EDGE 0x812F
 #endif
 
 #define SPHERE_RADIUS 1.f
-
-static opengl_tex_converter_init_cb opengl_tex_converter_init_cbs[] =
-{
-#ifdef VLCGL_CONV_VA
-    opengl_tex_converter_vaapi_init,
-#endif
-#ifdef __ANDROID__
-    opengl_tex_converter_anop_init,
-#endif
-#ifdef VLCGL_CONV_CVPX
-    opengl_tex_converter_cvpx_init,
-#endif
-    opengl_tex_converter_generic_init,
-};
 
 typedef struct {
     GLuint   texture;
@@ -82,7 +68,7 @@ typedef struct {
 struct prgm
 {
     GLuint id;
-    opengl_tex_converter_t tc;
+    opengl_tex_converter_t *tc;
 
     struct {
         GLfloat OrientationMatrix[16];
@@ -435,7 +421,7 @@ DelTextures(const opengl_tex_converter_t *tc, GLuint *textures)
 static int
 opengl_link_program(struct prgm *prgm)
 {
-    opengl_tex_converter_t *tc = &prgm->tc;
+    opengl_tex_converter_t *tc = prgm->tc;
 
     GLuint vertex_shader = BuildVertexShader(tc, tc->tex_count);
     GLuint shaders[] = { tc->fshader, vertex_shader };
@@ -512,18 +498,18 @@ opengl_link_program(struct prgm *prgm)
     GET_ALOC(VertexPosition, "VertexPosition");
     GET_ALOC(MultiTexCoord[0], "MultiTexCoord0");
     /* MultiTexCoord 1 and 2 can be optimized out if not used */
-    if (prgm->tc.tex_count > 1)
+    if (prgm->tc->tex_count > 1)
         GET_ALOC(MultiTexCoord[1], "MultiTexCoord1");
     else
         prgm->aloc.MultiTexCoord[1] = -1;
-    if (prgm->tc.tex_count > 2)
+    if (prgm->tc->tex_count > 2)
         GET_ALOC(MultiTexCoord[2], "MultiTexCoord2");
     else
         prgm->aloc.MultiTexCoord[2] = -1;
 #undef GET_LOC
 #undef GET_ULOC
 #undef GET_ALOC
-    int ret = prgm->tc.pf_fetch_locations(&prgm->tc, prgm->id);
+    int ret = prgm->tc->pf_fetch_locations(prgm->tc, prgm->id);
     assert(ret == VLC_SUCCESS);
     if (ret != VLC_SUCCESS)
     {
@@ -542,8 +528,12 @@ error:
 static void
 opengl_deinit_program(vout_display_opengl_t *vgl, struct prgm *prgm)
 {
-    if (prgm->tc.pf_release != NULL)
-        prgm->tc.pf_release(&prgm->tc);
+    opengl_tex_converter_t *tc = prgm->tc;
+    if (tc->p_module != NULL)
+        module_unneed(tc, tc->p_module);
+    else if (tc->priv != NULL)
+        opengl_tex_converter_generic_deinit(tc);
+    vlc_object_release(tc);
     if (prgm->id != 0)
         vgl->vt.DeleteProgram(prgm->id);
 }
@@ -552,61 +542,81 @@ static int
 opengl_init_program(vout_display_opengl_t *vgl, struct prgm *prgm,
                     const char *glexts, const video_format_t *fmt, bool subpics)
 {
-    int ret;
-    prgm->tc = (opengl_tex_converter_t) {
-        .gl = vgl->gl,
-        .vt = &vgl->vt,
-        .glexts = glexts,
-#if defined(USE_OPENGL_ES2)
-        .is_gles = true,
-        .glsl_version = 100,
-        .glsl_precision_header = "precision highp float;\n",
-#else
-        .is_gles = false,
-        .glsl_version = 120,
-        .glsl_precision_header = "",
-#endif
-        .fmt = *fmt,
-    };
+    opengl_tex_converter_t *tc =
+        vlc_object_create(vgl->gl, sizeof(opengl_tex_converter_t));
+    if (tc == NULL)
+        return VLC_ENOMEM;
 
+    tc->gl = vgl->gl;
+    tc->vt = &vgl->vt;
+    tc->pf_fragment_shader_init = opengl_fragment_shader_init_impl;
+    tc->glexts = glexts;
+#if defined(USE_OPENGL_ES2)
+    tc->is_gles = true;
+    tc->glsl_version = 100;
+    tc->glsl_precision_header = "precision highp float;\n";
+#else
+    tc->is_gles = false;
+    tc->glsl_version = 120;
+    tc->glsl_precision_header = "";
+#endif
+    tc->fmt = *fmt;
+
+    int ret;
     if (subpics)
     {
-        prgm->tc.fmt.i_chroma = VLC_CODEC_RGB32;
+        tc->fmt.i_chroma = VLC_CODEC_RGB32;
         /* Normal orientation and no projection for subtitles */
-        prgm->tc.fmt.orientation = ORIENT_NORMAL;
-        prgm->tc.fmt.projection_mode = PROJECTION_MODE_RECTANGULAR;
+        tc->fmt.orientation = ORIENT_NORMAL;
+        tc->fmt.projection_mode = PROJECTION_MODE_RECTANGULAR;
 
-        ret = opengl_tex_converter_subpictures_init(&prgm->tc);
+        ret = opengl_tex_converter_generic_init(tc, false);
     }
     else
     {
-        for (size_t i = 0; i < ARRAY_SIZE(opengl_tex_converter_init_cbs); ++i)
+        const vlc_chroma_description_t *desc =
+            vlc_fourcc_GetChromaDescription(fmt->i_chroma);
+
+        if (desc == NULL)
+            return VLC_EGENERIC;
+        if (desc->plane_count == 0)
         {
-            ret = opengl_tex_converter_init_cbs[i](&prgm->tc);
-            if (ret == VLC_SUCCESS)
-                break;
+            /* Opaque chroma: load a module to handle it */
+            tc->p_module = module_need(tc, "glconv", "$glconv", true);
+        }
+
+        if (tc->p_module != NULL)
+            ret = VLC_SUCCESS;
+        else
+        {
+            /* Software chroma or gl hw converter failed: use a generic
+             * converter */
+            ret = opengl_tex_converter_generic_init(tc, true);
         }
     }
 
     if (ret != VLC_SUCCESS)
-        return ret;
+    {
+        vlc_object_release(tc);
+        return VLC_EGENERIC;
+    }
 
-    assert(prgm->tc.fshader != 0 && prgm->tc.tex_target != 0 &&
-           prgm->tc.tex_count > 0 && prgm->tc.pf_update != NULL &&
-           prgm->tc.pf_fetch_locations != NULL &&
-           prgm->tc.pf_prepare_shader != NULL);
+    assert(tc->fshader != 0 && tc->tex_target != 0 && tc->tex_count > 0 &&
+           tc->pf_update != NULL && tc->pf_fetch_locations != NULL &&
+           tc->pf_prepare_shader != NULL);
+
+    prgm->tc = tc;
 
     ret = opengl_link_program(prgm);
     if (ret != VLC_SUCCESS)
     {
-        if (prgm->tc.pf_release != NULL)
-            prgm->tc.pf_release(&prgm->tc);
+        opengl_deinit_program(vgl, prgm);
         return VLC_EGENERIC;
     }
 
-    getOrientationTransformMatrix(prgm->tc.fmt.orientation,
+    getOrientationTransformMatrix(tc->fmt.orientation,
                                   prgm->var.OrientationMatrix);
-    getViewpointMatrixes(vgl, prgm->tc.fmt.projection_mode, prgm);
+    getViewpointMatrixes(vgl, tc->fmt.projection_mode, prgm);
 
     return VLC_SUCCESS;
 }
@@ -783,12 +793,12 @@ vout_display_opengl_t *vout_display_opengl_New(video_format_t *fmt,
         return NULL;
     }
     /* Update the fmt to main program one */
-    vgl->fmt = vgl->prgm->tc.fmt;
+    vgl->fmt = vgl->prgm->tc->fmt;
     /* The orientation is handled by the orientation matrix */
     vgl->fmt.orientation = ORIENT_NORMAL;
 
     /* Texture size */
-    const opengl_tex_converter_t *tc = &vgl->prgm->tc;
+    const opengl_tex_converter_t *tc = vgl->prgm->tc;
     for (unsigned j = 0; j < tc->tex_count; j++) {
         const GLsizei w = vgl->fmt.i_visible_width  * tc->texs[j].w.num
                         / tc->texs[j].w.den;
@@ -804,11 +814,11 @@ vout_display_opengl_t *vout_display_opengl_New(video_format_t *fmt,
     }
 
     /* Allocates our textures */
-    assert(!vgl->sub_prgm->tc.handle_texs_gen);
+    assert(!vgl->sub_prgm->tc->handle_texs_gen);
 
-    if (!vgl->prgm->tc.handle_texs_gen)
+    if (!vgl->prgm->tc->handle_texs_gen)
     {
-        ret = GenTextures(&vgl->prgm->tc, vgl->tex_width, vgl->tex_height,
+        ret = GenTextures(vgl->prgm->tc, vgl->tex_width, vgl->tex_height,
                           vgl->texture);
         if (ret != VLC_SUCCESS)
         {
@@ -827,7 +837,7 @@ vout_display_opengl_t *vout_display_opengl_New(video_format_t *fmt,
 
     vgl->vt.GenBuffers(1, &vgl->vertex_buffer_object);
     vgl->vt.GenBuffers(1, &vgl->index_buffer_object);
-    vgl->vt.GenBuffers(vgl->prgm->tc.tex_count, vgl->texture_buffer_object);
+    vgl->vt.GenBuffers(vgl->prgm->tc->tex_count, vgl->texture_buffer_object);
 
     /* Initial number of allocated buffer objects for subpictures, will grow dynamically. */
     int subpicture_buffer_object_count = 8;
@@ -864,12 +874,12 @@ void vout_display_opengl_Delete(vout_display_opengl_t *vgl)
     glFinish();
     glFlush();
 
-    opengl_tex_converter_t *tc = &vgl->prgm->tc;
+    opengl_tex_converter_t *tc = vgl->prgm->tc;
     if (!tc->handle_texs_gen)
         DelTextures(tc, vgl->texture);
     opengl_deinit_program(vgl, vgl->prgm);
 
-    tc = &vgl->sub_prgm->tc;
+    tc = vgl->sub_prgm->tc;
     for (int i = 0; i < vgl->region_count; i++)
     {
         if (vgl->region[i].texture)
@@ -881,7 +891,7 @@ void vout_display_opengl_Delete(vout_display_opengl_t *vgl)
     vgl->vt.DeleteBuffers(1, &vgl->vertex_buffer_object);
     vgl->vt.DeleteBuffers(1, &vgl->index_buffer_object);
 
-    vgl->vt.DeleteBuffers(vgl->prgm->tc.tex_count, vgl->texture_buffer_object);
+    vgl->vt.DeleteBuffers(vgl->prgm->tc->tex_count, vgl->texture_buffer_object);
     if (vgl->subpicture_buffer_object_count > 0)
         vgl->vt.DeleteBuffers(vgl->subpicture_buffer_object_count, vgl->subpicture_buffer_object);
     free(vgl->subpicture_buffer_object);
@@ -965,7 +975,7 @@ picture_pool_t *vout_display_opengl_GetPool(vout_display_opengl_t *vgl, unsigned
     if (vgl->pool)
         return vgl->pool;
 
-    opengl_tex_converter_t *tc = &vgl->prgm->tc;
+    opengl_tex_converter_t *tc = vgl->prgm->tc;
     requested_count = __MIN(VLCGL_PICTURE_MAX, requested_count);
     /* Allocate with tex converter pool callback if it exists */
     if (tc->pf_get_pool != NULL)
@@ -1007,7 +1017,7 @@ error:
 int vout_display_opengl_Prepare(vout_display_opengl_t *vgl,
                                 picture_t *picture, subpicture_t *subpicture)
 {
-    opengl_tex_converter_t *tc = &vgl->prgm->tc;
+    opengl_tex_converter_t *tc = vgl->prgm->tc;
 
     /* Update the texture */
     int ret = tc->pf_update(tc, vgl->texture, vgl->tex_width, vgl->tex_height,
@@ -1021,7 +1031,7 @@ int vout_display_opengl_Prepare(vout_display_opengl_t *vgl,
     vgl->region_count = 0;
     vgl->region       = NULL;
 
-    tc = &vgl->sub_prgm->tc;
+    tc = vgl->sub_prgm->tc;
     if (subpicture) {
 
         int count = 0;
@@ -1370,19 +1380,19 @@ static int SetupCoords(vout_display_opengl_t *vgl,
     switch (vgl->fmt.projection_mode)
     {
     case PROJECTION_MODE_RECTANGULAR:
-        i_ret = BuildRectangle(vgl->prgm->tc.tex_count,
+        i_ret = BuildRectangle(vgl->prgm->tc->tex_count,
                                &vertexCoord, &textureCoord, &nbVertices,
                                &indices, &nbIndices,
                                left, top, right, bottom);
         break;
     case PROJECTION_MODE_EQUIRECTANGULAR:
-        i_ret = BuildSphere(vgl->prgm->tc.tex_count,
+        i_ret = BuildSphere(vgl->prgm->tc->tex_count,
                             &vertexCoord, &textureCoord, &nbVertices,
                             &indices, &nbIndices,
                             left, top, right, bottom);
         break;
     case PROJECTION_MODE_CUBEMAP_LAYOUT_STANDARD:
-        i_ret = BuildCube(vgl->prgm->tc.tex_count,
+        i_ret = BuildCube(vgl->prgm->tc->tex_count,
                           (float)vgl->fmt.i_cubemap_padding / vgl->fmt.i_width,
                           (float)vgl->fmt.i_cubemap_padding / vgl->fmt.i_height,
                           &vertexCoord, &textureCoord, &nbVertices,
@@ -1397,7 +1407,7 @@ static int SetupCoords(vout_display_opengl_t *vgl,
     if (i_ret != VLC_SUCCESS)
         return i_ret;
 
-    for (unsigned j = 0; j < vgl->prgm->tc.tex_count; j++)
+    for (unsigned j = 0; j < vgl->prgm->tc->tex_count; j++)
     {
         vgl->vt.BindBuffer(GL_ARRAY_BUFFER, vgl->texture_buffer_object[j]);
         vgl->vt.BufferData(GL_ARRAY_BUFFER, nbVertices * 2 * sizeof(GLfloat),
@@ -1423,10 +1433,10 @@ static int SetupCoords(vout_display_opengl_t *vgl,
 
 static void DrawWithShaders(vout_display_opengl_t *vgl, struct prgm *prgm)
 {
-    opengl_tex_converter_t *tc = &prgm->tc;
+    opengl_tex_converter_t *tc = prgm->tc;
     tc->pf_prepare_shader(tc, vgl->tex_width, vgl->tex_height, 1.0f);
 
-    for (unsigned j = 0; j < vgl->prgm->tc.tex_count; j++) {
+    for (unsigned j = 0; j < vgl->prgm->tc->tex_count; j++) {
         assert(vgl->texture[j] != 0);
         vgl->vt.ActiveTexture(GL_TEXTURE0+j);
         vgl->vt.BindTexture(tc->tex_target, vgl->texture[j]);
@@ -1479,7 +1489,7 @@ int vout_display_opengl_Display(vout_display_opengl_t *vgl,
         float top[PICTURE_PLANE_MAX];
         float right[PICTURE_PLANE_MAX];
         float bottom[PICTURE_PLANE_MAX];
-        const opengl_tex_converter_t *tc = &vgl->prgm->tc;
+        const opengl_tex_converter_t *tc = vgl->prgm->tc;
         for (unsigned j = 0; j < tc->tex_count; j++)
         {
             float scale_w = (float)tc->texs[j].w.num / tc->texs[j].w.den
@@ -1519,7 +1529,7 @@ int vout_display_opengl_Display(vout_display_opengl_t *vgl,
     // Change the program for overlays
     struct prgm *prgm = vgl->sub_prgm;
     GLuint program = prgm->id;
-    opengl_tex_converter_t *tc = &prgm->tc;
+    opengl_tex_converter_t *tc = prgm->tc;
     vgl->vt.UseProgram(program);
 
     glEnable(GL_BLEND);

modules/MODULES_LIST
@@ -162,6 +162,11 @@ $Id$
  * gaussianblur: gaussian blur video filter
  * gestures: mouse gestures control plugin
  * gl: OpenGL video output using the generic OpenGL provider
+ * glconv_android: Android SurfaceTexture OpenGL hardware converter
+ * glconv_cvpx: Apple CVPX OpenGL hardware converter (for iOS and macOS)
+ * glconv_vaapi_drm: VA-API OpenGL hardware converter for DRM
+ * glconv_vaapi_wl: VA-API OpenGL hardware converter for Wayland
+ * glconv_vaapi_x11: VA-API OpenGL hardware converter for X11
  * gles2: OpenGL ES2 video output using the generic OpenGL provider
  * glspectrum: 3D OpenGL spectrum visualization
  * glwin32: a opengl provider using DirectX OpenGL

modules/video_output/Makefile.am
@@ -3,53 +3,10 @@ vout_LTLIBRARIES =
 
 EXTRA_DIST += video_output/README
 
-OPENGL_COMMONCFLAGS =
-OPENGL_COMMONLDFLAGS =
-OPENGL_COMMONLIBS =
 OPENGL_COMMONSOURCES = video_output/opengl/vout_helper.c \
 	video_output/opengl/vout_helper.h video_output/opengl/converter.h \
-	video_output/opengl/converters.c
-if HAVE_ANDROID
-OPENGL_COMMONSOURCES += video_output/opengl/converter_android.c
-endif
-if HAVE_OSX
-OPENGL_COMMONSOURCES += video_output/opengl/converter_cvpx.c \
-	codec/vt_utils.c codec/vt_utils.h
-OPENGL_COMMONCFLAGS += -DVLCGL_CONV_CVPX
-OPENGL_COMMONLDFLAGS += -Wl,-framework,IOSurface -Wl,-framework,CoreVideo
-endif
-if HAVE_IOS
-OPENGL_COMMONSOURCES += video_output/opengl/converter_cvpx.c
-OPENGL_COMMONCFLAGS += -DVLCGL_CONV_CVPX
-endif
-if HAVE_TVOS
-OPENGL_COMMONSOURCES += video_output/opengl/converter_cvpx.c
-OPENGL_COMMONCFLAGS += -DVLCGL_CONV_CVPX
-endif
-if HAVE_EGL
-if HAVE_VAAPI
-OPENGL_COMMONLIBS += $(LIBVA_LIBS) $(LIBVA_EGL_LIBS)
-OPENGL_COMMONSOURCES += video_output/opengl/converter_vaapi.c \
-	hw/vaapi/vlc_vaapi.c hw/vaapi/vlc_vaapi.h
-OPENGL_COMMONCFLAGS += -DVLCGL_CONV_VA
-if HAVE_WAYLAND_EGL
-if HAVE_VAAPI_WL
-OPENGL_COMMONLIBS += $(LIBVA_WL_LIBS)
-OPENGL_COMMONCFLAGS += -DHAVE_VA_WL $(LIBVA_WL_CFLAGS)
-endif
-endif
-if HAVE_VAAPI_X11
-if HAVE_XCB
-OPENGL_COMMONLIBS += $(LIBVA_X11_LIBS) $(X_LIBS) $(X_PRE_LIBS) -lX11
-OPENGL_COMMONCFLAGS += -DHAVE_VA_X11
-endif
-endif
-if HAVE_VAAPI_DRM
-OPENGL_COMMONLIBS += $(LIBVA_DRM_LIBS)
-OPENGL_COMMONCFLAGS += -DHAVE_VA_DRM
-endif
-endif
-endif
+	video_output/opengl/internal.h video_output/opengl/fragment_shaders.c \
+	video_output/opengl/converter_sw.c
 
 if HAVE_DECKLINK
 libdecklinkoutput_plugin_la_SOURCES = video_output/decklink.cpp
@@ -58,27 +15,32 @@ libdecklinkoutput_plugin_la_LIBADD = $(LIBS_decklink) $(LIBDL) -lpthread
 vout_LTLIBRARIES += libdecklinkoutput_plugin.la
 endif
 
+libglconv_cvpx_plugin_la_SOURCES = video_output/opengl/converter_cvpx.c \
+	video_output/opengl/converter.h
+
 if HAVE_OSX
 libvout_macosx_plugin_la_SOURCES = video_output/macosx.m $(OPENGL_COMMONSOURCES)
-libvout_macosx_plugin_la_CFLAGS = $(AM_CFLAGS) $(OPENGL_COMMONCFLAGS)
 libvout_macosx_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)' \
-	-Wl,-framework,OpenGL,-framework,Cocoa $(OPENGL_COMMONLDFLAGS)
+	-Wl,-framework,OpenGL,-framework,Cocoa
 
 libcaopengllayer_plugin_la_SOURCES = video_output/caopengllayer.m $(OPENGL_COMMONSOURCES)
-libcaopengllayer_plugin_la_CFLAGS = $(AM_CFLAGS) $(OPENGL_COMMONCFLAGS)
 libcaopengllayer_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)' \
-	-Wl,-framework,OpenGL,-framework,Cocoa,-framework,QuartzCore \
-	$(OPENGL_COMMONLDFLAGS)
-vout_LTLIBRARIES += libvout_macosx_plugin.la libcaopengllayer_plugin.la
+	-Wl,-framework,OpenGL,-framework,Cocoa,-framework,QuartzCore
+
+libglconv_cvpx_plugin_la_SOURCES += codec/vt_utils.c codec/vt_utils.h
+libglconv_cvpx_plugin_la_LDFLAGS = -Wl,-framework,IOSurface -Wl,-framework,CoreVideo
+vout_LTLIBRARIES += libvout_macosx_plugin.la libcaopengllayer_plugin.la \
+	libglconv_cvpx_plugin.la
+endif
+if HAVE_IOS
+libglconv_cvpx_plugin_la_CFLAGS = $(AM_CFLAGS) -DUSE_OPENGL_ES2
 endif
 
 libvout_ios_plugin_la_SOURCES = video_output/ios.m $(OPENGL_COMMONSOURCES)
-libvout_ios_plugin_la_CFLAGS = $(AM_CFLAGS) $(OPENGL_COMMONCFLAGS)
 libvout_ios_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)' \
-	-Wl,-framework,OpenGLES,-framework,QuartzCore,-framework,UIKit \
-	$(OPENGL_COMMONLDFLAGS)
+	-Wl,-framework,OpenGLES,-framework,QuartzCore,-framework,UIKit
 if HAVE_IOS
-vout_LTLIBRARIES += libvout_ios_plugin.la
+vout_LTLIBRARIES += libvout_ios_plugin.la libglconv_cvpx_plugin.la
 endif
 if HAVE_TVOS
 vout_LTLIBRARIES += libvout_ios_plugin.la
@@ -86,22 +48,54 @@ endif
 
 ### OpenGL ###
 libgles2_plugin_la_SOURCES = $(OPENGL_COMMONSOURCES) video_output/opengl/display.c
-libgles2_plugin_la_CFLAGS = $(AM_CFLAGS) $(GLES2_CFLAGS) -DUSE_OPENGL_ES2 \
-	$(OPENGL_COMMONCFLAGS)
-libgles2_plugin_la_LIBADD = $(GLES2_LIBS) $(LIBM) $(OPENGL_COMMONLIBS)
-libgles2_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)' $(OPENGL_COMMONLDFLAGS)
+libgles2_plugin_la_CFLAGS = $(AM_CFLAGS) $(GLES2_CFLAGS) -DUSE_OPENGL_ES2
+libgles2_plugin_la_LIBADD = $(GLES2_LIBS) $(LIBM)
+libgles2_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)'
 
 EXTRA_LTLIBRARIES += libgles2_plugin.la
 vout_LTLIBRARIES += $(LTLIBgles2)
 
 libgl_plugin_la_SOURCES = $(OPENGL_COMMONSOURCES) video_output/opengl/display.c
-libgl_plugin_la_CFLAGS = $(AM_CFLAGS) $(GL_CFLAGS) $(OPENGL_COMMONCFLAGS)
-libgl_plugin_la_LIBADD = $(GL_LIBS) $(LIBM) $(OPENGL_COMMONLIBS)
-libgl_plugin_la_LDFLAGS = $(AM_LDFLAGS) $(OPENGL_COMMONLDFLAGS)
+libgl_plugin_la_CFLAGS = $(AM_CFLAGS) $(GL_CFLAGS)
+libgl_plugin_la_LIBADD = $(GL_LIBS) $(LIBM)
+
+libglconv_vaapi_wl_plugin_la_SOURCES = video_output/opengl/converter_vaapi.c \
+	video_output/opengl/converter.h \
+	hw/vaapi/vlc_vaapi.c hw/vaapi/vlc_vaapi.h
+libglconv_vaapi_wl_plugin_la_CFLAGS = $(AM_CFLAGS) $(GL_CFLAGS) -DHAVE_VA_WL $(LIBVA_WL_CFLAGS)
+libglconv_vaapi_wl_plugin_la_LIBADD = $(LIBVA_LIBS) $(LIBVA_EGL_LIBS) \
+	$(LIBVA_WL_LIBS)
+
+libglconv_vaapi_x11_plugin_la_SOURCES = $(libglconv_vaapi_wl_plugin_la_SOURCES)
+libglconv_vaapi_x11_plugin_la_CFLAGS = $(AM_CFLAGS) -DHAVE_VA_X11
+libglconv_vaapi_x11_plugin_la_LIBADD = $(LIBVA_LIBS) $(LIBVA_EGL_LIBS) \
+	$(LIBVA_X11_LIBS) $(X_LIBS) $(X_PRE_LIBS) -lX11
+
+libglconv_vaapi_drm_plugin_la_SOURCES = $(libglconv_vaapi_wl_plugin_la_SOURCES)
+libglconv_vaapi_drm_plugin_la_CFLAGS = $(AM_CFLAGS) -DHAVE_VA_DRM
+libglconv_vaapi_drm_plugin_la_LIBADD = $(LIBVA_LIBS) $(LIBVA_EGL_LIBS) \
+	$(LIBVA_DRM_LIBS)
+
 if HAVE_GL
 vout_LTLIBRARIES += libgl_plugin.la
+if HAVE_EGL
+if HAVE_VAAPI
+if HAVE_WAYLAND_EGL
+if HAVE_VAAPI_WL
+vout_LTLIBRARIES += libglconv_vaapi_wl_plugin.la
 endif
-
+endif
+if HAVE_XCB
+if HAVE_VAAPI_X11
+vout_LTLIBRARIES += libglconv_vaapi_x11_plugin.la
+endif
+endif
+if HAVE_VAAPI_DRM
+vout_LTLIBRARIES += libglconv_vaapi_drm_plugin.la
+endif
+endif
+endif # HAVE_EGL
+endif # HAVE_GL
 
 ### XCB ###
 libvlc_xcb_events_la_SOURCES = \
@@ -318,14 +312,12 @@ libglwin32_plugin_la_CPPFLAGS = $(AM_CPPFLAGS) \
 	-DMODULE_NAME_IS_glwin32
 libwgl_plugin_la_CPPFLAGS = $(AM_CPPFLAGS) \
         -DMODULE_NAME_IS_wgl
-libglwin32_plugin_la_CFLAGS = $(AM_CFLAGS) $(OPENGL_COMMONCFLAGS)
-libwgl_plugin_la_CFLAGS = $(AM_CFLAGS) $(OPENGL_COMMONCFLAGS)
 
-libglwin32_plugin_la_LIBADD = -lopengl32 -lgdi32 $(LIBCOM) -luuid $(OPENGL_COMMONLIBS)
-libwgl_plugin_la_LIBADD = -lopengl32 -lgdi32 $(OPENGL_COMMONLIBS)
+libglwin32_plugin_la_LIBADD = -lopengl32 -lgdi32 $(LIBCOM) -luuid
+libwgl_plugin_la_LIBADD = -lopengl32 -lgdi32
 
-libglwin32_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)' $(OPENGL_COMMONLDFLAGS)
-libwgl_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)' $(OPENGL_COMMONLDFLAGS)
+libglwin32_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)'
+libwgl_plugin_la_LDFLAGS = $(AM_LDFLAGS) -rpath '$(voutdir)'
 
 if HAVE_WIN32_DESKTOP
 vout_LTLIBRARIES += $(LTLIBglwin32) $(LTLIBwgl)
@@ -389,10 +381,14 @@ libandroid_display_plugin_la_SOURCES = video_output/android/display.c \
 libandroid_display_plugin_la_CFLAGS = $(AM_CFLAGS)
 libandroid_display_plugin_la_LIBADD = $(LIBDL)
 
+libglconv_android_plugin_la_SOURCES = video_output/opengl/converter_android.c \
+	video_output/opengl/converter.h
+libglconv_android_plugin_la_CFLAGS = $(AM_CFLAGS) -DUSE_OPENGL_ES2
+
 if HAVE_ANDROID
 vout_LTLIBRARIES += libandroid_window_plugin.la libandroid_display_plugin.la
 if HAVE_EGL
-vout_LTLIBRARIES += libegl_android_plugin.la
+vout_LTLIBRARIES += libegl_android_plugin.la libglconv_android_plugin.la
 endif
 endif
 

modules/video_output/macosx.m
@@ -49,6 +49,10 @@
 #include <vlc_dialog.h>
 #include "opengl/vout_helper.h"
 
+#define GLHW_TEXT N_("GL/GLES hw converter")
+#define GLHW_LONGTEXT N_( \
+    "Force an \"gl hw converter\" module.")
+
 /**
  * Forward declarations
  */
@@ -77,6 +81,8 @@ vlc_module_begin ()
     set_subcategory (SUBCAT_VIDEO_VOUT)
     set_capability ("vout display", 300)
     set_callbacks (Open, Close)
+    add_module ("glhw", NULL, NULL,
+                GLHW_TEXT, GLHW_LONGTEXT, true)
 
     add_shortcut ("macosx", "vout_macosx")
 vlc_module_end ()

modules/video_output/opengl/converter.h
@@ -22,6 +22,7 @@
 #define VLC_OPENGL_CONVERTER_H
 
 #include "vout_helper.h"
+#include <vlc_plugin.h>
 
 #define VLCGL_PICTURE_MAX 128
 
@@ -163,31 +164,28 @@ typedef struct {
     PFNGLCLIENTWAITSYNCPROC         ClientWaitSync; /* can be NULL */
 } opengl_vtable_t;
 
+/*
+ * Structure that is filled by "glhw converter" module probe function
+ * The implementation should initialize every members of the struct that are
+ * not set by the caller
+ */
 typedef struct opengl_tex_converter_t opengl_tex_converter_t;
-
-/*
- * Callback to initialize an opengl_tex_converter_t struct
- *
- * The implementation should initialize every members of the struct in regards
- * of the video format.
- *
- * \param fmt video format, fmt->i_chroma can be modified in order to match a
- * shader
- * \param fc OpenGL tex converter that needs to be filled on success
- * \return VLC_SUCCESS or a VLC error
- */
-typedef int (*opengl_tex_converter_init_cb)(opengl_tex_converter_t *fc);
-
-/*
- * Structure that is filled by an opengl_tex_converter_init_cb function
- */
 struct opengl_tex_converter_t
 {
-    /* Pointer to object gl, set by the caller of the init cb */
+    VLC_COMMON_MEMBERS
+
+    module_t *p_module;
+
+    /* Pointer to object gl, set by the caller */
     vlc_gl_t *gl;
 
     /* Function pointers to OpenGL functions, set by the caller */
     const opengl_vtable_t *vt;
+
+    /* Function pointer to the shader init command, set by the caller, see
+     * opengl_fragment_shader_init() documentation. */
+    GLuint (*pf_fragment_shader_init)(opengl_tex_converter_t *, GLenum,
+                                      vlc_fourcc_t, video_color_space_t);
 
     /* Available gl extensions (from GL_EXTENSIONS) */
     const char *glexts;
@@ -317,14 +315,6 @@ struct opengl_tex_converter_t
     void (*pf_prepare_shader)(const opengl_tex_converter_t *fc,
                               const GLsizei *tex_width, const GLsizei *tex_height,
                               float alpha);
-
-    /*
-     * Callback to release the private context
-     *
-     * This function pointer can be NULL.
-     * \param fc OpenGL tex converter
-     */
-    void (*pf_release)(const opengl_tex_converter_t *fc);
 };
 
 /*
@@ -334,38 +324,20 @@ struct opengl_tex_converter_t
  * generic fragment shader. It will compile a fragment shader generated from
  * the chroma and the tex target. This will initialize all elements of the
  * opengl_tex_converter_t struct except for priv, pf_allocate_texture,
- * pf_get_pool, pf_update, and pf_release.
+ * pf_get_pool, pf_update
  *
  * \param tc OpenGL tex converter
  * \param tex_target GL_TEXTURE_2D or GL_TEXTURE_RECTANGLE
  * \param chroma chroma used to generate the fragment shader
- * \param yuv_space if not COLOR_SPACE_UNDEF, YUV planes will be converted to
- * RGB according to the color space
+ * \param if not COLOR_SPACE_UNDEF, YUV planes will be converted to RGB
+ * according to the color space
  * \return the compiled fragment shader or 0 in case of error
  */
-GLuint
+static inline GLuint
 opengl_fragment_shader_init(opengl_tex_converter_t *tc, GLenum tex_target,
-                            vlc_fourcc_t chroma, video_color_space_t yuv_space);
-
-int
-opengl_tex_converter_subpictures_init(opengl_tex_converter_t *);
-
-int
-opengl_tex_converter_generic_init(opengl_tex_converter_t *);
-
-#ifdef __ANDROID__
-int
-opengl_tex_converter_anop_init(opengl_tex_converter_t *);
-#endif
-
-#ifdef VLCGL_CONV_CVPX
-int
-opengl_tex_converter_cvpx_init(opengl_tex_converter_t *tc);
-#endif
-
-#ifdef VLCGL_CONV_VA
-int
-opengl_tex_converter_vaapi_init(opengl_tex_converter_t *tc);
-#endif
+                            vlc_fourcc_t chroma, video_color_space_t yuv_space)
+{
+    return tc->pf_fragment_shader_init(tc, tex_target, chroma, yuv_space);
+}
 
 #endif /* include-guard */

modules/video_output/opengl/converter_android.c
@@ -181,8 +181,9 @@ tc_anop_prepare_shader(const opengl_tex_converter_t *tc,
 }
 
 static void
-tc_anop_release(const opengl_tex_converter_t *tc)
+Close(vlc_object_t *obj)
 {
+    opengl_tex_converter_t *tc = (void *)obj;
     struct priv *priv = tc->priv;
 
     if (priv->stex_attached)
@@ -191,9 +192,11 @@ tc_anop_release(const opengl_tex_converter_t *tc)
     free(priv);
 }
 
-int
-opengl_tex_converter_anop_init(opengl_tex_converter_t *tc)
+static int
+Open(vlc_object_t *obj)
 {
+    opengl_tex_converter_t *tc = (void *) obj;
+
     if (tc->fmt.i_chroma != VLC_CODEC_ANDROID_OPAQUE
      || !tc->gl->surface->handle.anativewindow)
         return VLC_EGENERIC;
@@ -212,10 +215,9 @@ opengl_tex_converter_anop_init(opengl_tex_converter_t *tc)
     tc->pf_update         = tc_anop_update;
     tc->pf_fetch_locations = tc_anop_fetch_locations;
     tc->pf_prepare_shader = tc_anop_prepare_shader;
-    tc->pf_release        = tc_anop_release;
 
     tc->tex_count = 1;
-    tc->texs[0] = (struct opengl_tex_cfg) { { 1, 1 }, { 1, 1 } };
+    tc->texs[0] = (struct opengl_tex_cfg) { { 1, 1 }, { 1, 1 }, 0, 0, 0 };
 
     tc->tex_target   = GL_TEXTURE_EXTERNAL_OES;
 
@@ -273,3 +275,11 @@ opengl_tex_converter_anop_init(opengl_tex_converter_t *tc)
 
     return VLC_SUCCESS;
 }
+
+vlc_module_begin ()
+    set_description("Android OpenGL SurfaceTexture converter")
+    set_capability("glconv", 1)
+    set_callbacks(Open, Close)
+    set_category(CAT_VIDEO)
+    set_subcategory(SUBCAT_VIDEO_VOUT)
+vlc_module_end ()

commit d61a3e999e411b5cfa58bad45e53ac3648fe3a21

gl: fix double rotation
Don't trigger a video filter to rotate an already rotated surface.

This fixes a regression from fc4114f44981aee52987a385649ce331d0f26c10

modules/video_output/opengl/vout_helper.c
@@ -797,7 +797,7 @@ vout_display_opengl_t *vout_display_opengl_New(video_format_t *fmt,
     /* Update the fmt to main program one */
     vgl->fmt = vgl->prgm->tc->fmt;
     /* The orientation is handled by the orientation matrix */
-    vgl->fmt.orientation = ORIENT_NORMAL;
+    vgl->fmt.orientation = fmt->orientation;
 
     /* Texture size */
     const opengl_tex_converter_t *tc = vgl->prgm->tc;