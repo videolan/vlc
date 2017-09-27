d19f46dec61d9f4f39c775e27f6e1306c1d46921
Thomas Guillem <thomas@gllm.fr>
2017-09-27 11:34:00 +0200

    vout: macosx: use add_glconv()
    Fixes the "option glconv does not exist" error log.

--- modules/video_output/macosx.m
+++ modules/video_output/macosx.m
@@ -78,6 +78,7 @@ vlc_module_begin ()
     set_capability ("vout display", 300)
     set_callbacks (Open, Close)
     add_shortcut ("macosx", "vout_macosx")
+    add_glconv ()
 vlc_module_end ()
 
 /**
