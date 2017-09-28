6ab01528c3f4e95d1c2522e04c4ee8be743f1db0
Thomas Guillem <thomas@gllm.fr>
2017-09-28 11:04:17 +0200

    codec: videotoolbox: error if SetH264DecoderInfo fails

--- modules/codec/videotoolbox.m
+++ modules/codec/videotoolbox.m
@@ -884,7 +884,8 @@ static int SetupDecoderExtradata(decoder_t *p_dec)
             p_sys->extradataInfo = ExtradataInfoCreate(CFSTR("avcC"),
                                             p_dec->fmt_in.p_extra,
                                             p_dec->fmt_in.i_extra);
-            SetH264DecoderInfo(p_dec, p_sys->extradataInfo);
+            if (SetH264DecoderInfo(p_dec, p_sys->extradataInfo) != VLC_SUCCESS)
+                return VLC_EGENERIC;
         }
         else
         {
