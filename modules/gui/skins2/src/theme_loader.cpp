8e453e61d60ef990beb32f77f7f8d21b7afd19b3
Thomas Guillem <thomas@gllm.fr>
2017-10-12 13:28:31 +0200

    skins2: fix unarchive
    Tell the stream extractor to read all files and flatten the result.

--- modules/gui/skins2/src/theme_loader.cpp
+++ modules/gui/skins2/src/theme_loader.cpp
@@ -229,6 +229,8 @@ bool ThemeLoader::unarchive( const std::string& fileName, const std::string &tem
         if( !item || !node )
             return false;
 
+        input_item_AddOption( item.get(), "ignore-filetypes=\"\"", VLC_INPUT_OPTION_TRUSTED );
+        input_item_AddOption( item.get(), "extractor-flatten", VLC_INPUT_OPTION_TRUSTED );
         node->p_item = item.release();
 
         if( vlc_stream_ReadDir( input.get(), node.get() ) )
