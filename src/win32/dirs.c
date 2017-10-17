11aa88f3f845ff4f7993670f2a5315216c0e2fb9
Steve Lhomme <robUx4@videolabs.io>
2017-10-17 16:41:09 +0200

    win32:dirs: the cache folder should not be backed up in the cloud on winstore
    See LocalCacheFolder
    https://docs.microsoft.com/en-us/uwp/api/windows.storage.applicationdata#Windows_Storage_ApplicationData_LocalCacheFolder

--- src/win32/dirs.c
+++ src/win32/dirs.c
@@ -256,8 +256,13 @@ char *config_GetUserDir (vlc_userdir_t type)
             return config_GetShellDir (CSIDL_PERSONAL);
         case VLC_CONFIG_DIR:
         case VLC_DATA_DIR:
-        case VLC_CACHE_DIR:
             return config_GetAppDir ();
+        case VLC_CACHE_DIR:
+#if !VLC_WINSTORE_APP
+            return config_GetAppDir ();
+#else
+            return config_GetShellDir (CSIDL_LOCAL_APPDATA);
+#endif
 
         case VLC_DESKTOP_DIR:
         case VLC_DOWNLOAD_DIR:
