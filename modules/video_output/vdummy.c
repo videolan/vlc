037728322c1145ff077f6bf94806cb8dd33bee58
Steve Lhomme <robUx4@videolabs.io>
2017-10-17 15:52:20 +0200

    vdummy: don't crash when used with hardware decoding
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/video_output/vdummy.c
+++ modules/video_output/vdummy.c
@@ -147,7 +147,8 @@ static void DisplayStat(vout_display_t *vd, picture_t *picture, subpicture_t *su
 {
     VLC_UNUSED(vd);
     VLC_UNUSED(subpicture);
-    if (vd->fmt.i_width*vd->fmt.i_height >= sizeof(mtime_t)) {
+    if ( vd->fmt.i_width*vd->fmt.i_height >= sizeof(mtime_t) &&
+         (picture->p->i_pitch * picture->p->i_lines) >= sizeof(mtime_t) ) {
         mtime_t date;
         memcpy(&date, picture->p->p_pixels, sizeof(date));
         msg_Dbg(vd, "VOUT got %"PRIu64" ms offset",
