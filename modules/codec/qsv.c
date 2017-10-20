e75195e501b8410e61a2e005bc75c672e071439a
Steve Lhomme <robux4@videolabs.io>
2017-10-20 15:50:41 +0200

    qsv: remove extra const

--- modules/codec/qsv.c
+++ modules/codec/qsv.c
@@ -153,19 +153,19 @@ static void     Close(vlc_object_t *);
      "numbers may result on better throughput depending on hardware. " \
      "MPEG2 needs at least 1 here.")
 
-static const int const profile_h264_list[] =
+static const int profile_h264_list[] =
       { MFX_PROFILE_UNKNOWN, MFX_PROFILE_AVC_CONSTRAINED_BASELINE, MFX_PROFILE_AVC_MAIN,
       MFX_PROFILE_AVC_EXTENDED, MFX_PROFILE_AVC_HIGH };
 static const char *const profile_h264_text[] =
     { "decide", "baseline", "main", "extended", "high" };
 
-static const int const profile_mpeg2_list[] =
+static const int profile_mpeg2_list[] =
     { MFX_PROFILE_UNKNOWN, MFX_PROFILE_MPEG2_SIMPLE, MFX_PROFILE_MPEG2_MAIN,
       MFX_PROFILE_MPEG2_HIGH };
 static const char *const profile_mpeg2_text[] =
     { "decide", "simple", "main", "high" };
 
-static const int const level_h264_list[] =
+static const int level_h264_list[] =
     { MFX_LEVEL_UNKNOWN, MFX_LEVEL_AVC_1, MFX_LEVEL_AVC_1b, MFX_LEVEL_AVC_12,
       MFX_LEVEL_AVC_13, MFX_LEVEL_AVC_2, MFX_LEVEL_AVC_21, MFX_LEVEL_AVC_22,
       MFX_LEVEL_AVC_3, MFX_LEVEL_AVC_31, MFX_LEVEL_AVC_32, MFX_LEVEL_AVC_4,
@@ -175,19 +175,19 @@ static const char *const level_h264_text[] =
     { "decide", "1", "1.1b", "1.2", "1.3", "2", "2.1", "2.2", "3", "3.1",
       "3.2", "4", "4.1",   "4.2",   "5", "5.1", "5.2" };
 
-static const int const level_mpeg2_list[] =
+static const int level_mpeg2_list[] =
     { MFX_LEVEL_UNKNOWN, MFX_LEVEL_MPEG2_LOW, MFX_LEVEL_MPEG2_MAIN,
       MFX_LEVEL_MPEG2_HIGH, MFX_LEVEL_MPEG2_HIGH1440 };
 static const char *const level_mpeg2_text[] =
     { "decide", "low", "main", "high", "high1440" };
 
-static const int const target_usage_list[] =
+static const int target_usage_list[] =
     { MFX_TARGETUSAGE_UNKNOWN, MFX_TARGETUSAGE_BEST_QUALITY, MFX_TARGETUSAGE_BALANCED,
       MFX_TARGETUSAGE_BEST_SPEED };
 static const char *const target_usage_text[] =
     { "decide", "quality", "balanced", "speed" };
 
-static const int const rc_method_list[] =
+static const int rc_method_list[] =
     { MFX_RATECONTROL_CBR, MFX_RATECONTROL_VBR,
       MFX_RATECONTROL_CQP, MFX_RATECONTROL_AVBR};
 static const char *const rc_method_text[] =
@@ -379,7 +379,7 @@ static mfxFrameSurface1 *qsv_frame_pool_Get(encoder_sys_t *sys, picture_t *pic)
 }
 
 static uint64_t qsv_params_get_value(const char *const *text,
-                                     const int const *list,
+                                     const int *list,
                                      size_t size, char *sel)
 {
     size_t result = 0;
