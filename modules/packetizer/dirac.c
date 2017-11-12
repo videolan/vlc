70c9111fc15dcfa6593cb208f1900db3164881c5
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-11-12 10:37:03 +0100

    dirac: Use GetDWBE

--- modules/packetizer/dirac.c
+++ modules/packetizer/dirac.c
@@ -481,8 +481,8 @@ static bool dirac_UnpackParseInfo( parse_info_t *p_pi, block_bytestream_t *p_bs,
         return false;
 
     p_pi->i_parse_code = p_d[4];
-    p_pi->u_next_offset = p_d[5] << 24 | p_d[6] << 16 | p_d[7] << 8 | p_d[8];
-    p_pi->u_prev_offset = p_d[9] << 24 | p_d[10] << 16 | p_d[11] << 8 | p_d[12];
+    p_pi->u_next_offset = GetDWBE( &p_d[5] );
+    p_pi->u_prev_offset = GetDWBE( &p_d[9] );
     return true;
 }
 
