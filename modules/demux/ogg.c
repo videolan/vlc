051b844532f9849f22792b4e8f9fd2154c765c1c
Thomas Guillem <thomas@gllm.fr>
2017-11-12 11:43:28 +0100

    ogg: replace xmalloc with vlc_alloc
    And handle the error case.

--- modules/demux/ogg.c
+++ modules/demux/ogg.c
@@ -827,8 +827,10 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
             if( p_sys->i_attachments <= 0 )
                 return VLC_EGENERIC;
 
+            *ppp_attach = vlc_alloc( p_sys->i_attachments, sizeof(input_attachment_t*) );
+            if (!**ppp_attach)
+                return VLC_ENOMEM;
             *pi_int = p_sys->i_attachments;
-            *ppp_attach = xmalloc( sizeof(input_attachment_t*) * p_sys->i_attachments );
             for( int i = 0; i < p_sys->i_attachments; i++ )
                 (*ppp_attach)[i] = vlc_input_attachment_Duplicate( p_sys->attachments[i] );
             return VLC_SUCCESS;
