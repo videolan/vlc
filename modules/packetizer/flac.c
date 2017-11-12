e8907f2e32a8c1211a45f5bdb4d83e5deacf96e9
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-11-12 18:43:03 +0100

    packetizer: flac: Remove invalid es_format_Clean
    This is the core's responsibility

--- modules/packetizer/flac.c
+++ modules/packetizer/flac.c
@@ -827,7 +827,6 @@ static void Close(vlc_object_t *p_this)
     decoder_t *p_dec = (decoder_t *)p_this;
     decoder_sys_t *p_sys = p_dec->p_sys;
 
-    es_format_Clean(&p_dec->fmt_out);
     block_BytestreamRelease(&p_sys->bytestream);
     free(p_sys->p_buf);
     free(p_sys);
