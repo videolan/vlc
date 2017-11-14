videolan/vlc

commit 31a4ceafbc7777ce1894e9061798877040cb1118

a52: eliminate date-set flag
Use VLC_TS_INVALID value instead.

modules/packetizer/a52.c
@@ -64,7 +64,6 @@ struct decoder_sys_t
      * Common properties
      */
     date_t  end_date;
-    bool    b_date_set;
 
     mtime_t i_pts;
     bool    b_discontuinity;
@@ -77,7 +76,7 @@ static void PacketizeFlush( decoder_t *p_dec )
     decoder_sys_t *p_sys = p_dec->p_sys;
 
     p_sys->b_discontuinity = true;
-    date_Set( &p_sys->end_date, 0 );
+    date_Set( &p_sys->end_date, VLC_TS_INVALID );
     p_sys->i_state = STATE_NOSYNC;
     block_BytestreamEmpty( &p_sys->bytestream );
 }
@@ -86,7 +85,7 @@ static block_t *GetOutBuffer( decoder_t *p_dec )
 {
     decoder_sys_t *p_sys = p_dec->p_sys;
 
-    if( !p_sys->b_date_set
+    if( date_Get( &p_sys->end_date ) == VLC_TS_INVALID
      || p_dec->fmt_out.audio.i_rate != p_sys->frame.i_rate )
     {
         msg_Dbg( p_dec, "A/52 channels:%d samplerate:%d bitrate:%d",
@@ -94,7 +93,6 @@ static block_t *GetOutBuffer( decoder_t *p_dec )
 
         date_Init( &p_sys->end_date, p_sys->frame.i_rate, 1 );
         date_Set( &p_sys->end_date, p_sys->i_pts );
-        p_sys->b_date_set = true;
     }
 
     p_dec->fmt_out.audio.i_rate     = p_sys->frame.i_rate;
@@ -328,9 +326,8 @@ static int Open( vlc_object_t *p_this )
 
     /* Misc init */
     p_sys->i_state = STATE_NOSYNC;
-    date_Set( &p_sys->end_date, 0 );
+    date_Set( &p_sys->end_date, VLC_TS_INVALID );
     p_sys->i_pts = VLC_TS_INVALID;
-    p_sys->b_date_set = false;
     p_sys->b_discontuinity = false;
     memset(&p_sys->frame, 0, sizeof(vlc_a52_header_t));
 

commit ba6b88f01dea8ed4566f376d89dde0934c455d93

a52: fix FPE when fmt_out.i_rate is already initialized
If fmt_out.i_rate is already set to a good value from Open(), date_Init() is
not called and date_Increment() triggers a FPE.

This is a regression from 31a4ceafbc7777ce1894e9061798877040cb1118

modules/packetizer/a52.c
@@ -343,6 +343,7 @@ static int Open( vlc_object_t *p_this )
     /* Set output properties (Passthrough ONLY) */
     p_dec->fmt_out.i_codec = p_dec->fmt_in.i_codec;
     p_dec->fmt_out.audio = p_dec->fmt_in.audio;
+    p_dec->fmt_out.audio.i_rate = 0;
 
     /* Set callback */
     p_dec->pf_packetize = PacketizeBlock;