464ab9724e91436a6616ee66d8c667e0a3eb6f82
Rémi Denis-Courmont <remi@remlab.net>
2017-11-18 21:32:26 +0200

    svcdsub: fix obsolete setting name
    This should be the then plugin name, not the current one (not that I
    expect it to change ever).

--- modules/codec/svcdsub.c
+++ modules/codec/svcdsub.c
@@ -51,7 +51,7 @@ vlc_module_begin ()
     set_capability( "spu decoder", 50 )
     set_callbacks( DecoderOpen, DecoderClose )
 
-    add_obsolete_integer ( MODULE_STRING "-debug" )
+    add_obsolete_integer ( "svcdsub-debug" )
 
     add_submodule ()
     set_description( N_("Philips OGT (SVCD subtitle) packetizer") )
