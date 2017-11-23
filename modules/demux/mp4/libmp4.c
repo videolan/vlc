fbb9d1fcbae45844a6f6331cc6322aa200bf26a7
Rémi Denis-Courmont <remi@remlab.net>
2017-11-23 22:10:33 +0200

    mp4: avoid assignment as predicate

--- modules/demux/mp4/libmp4.c
+++ modules/demux/mp4/libmp4.c
@@ -529,11 +529,12 @@ static int MP4_ReadBox_ftyp( stream_t *p_stream, MP4_Box_t *p_box )
     MP4_GETFOURCC( p_box->data.p_ftyp->i_major_brand );
     MP4_GET4BYTES( p_box->data.p_ftyp->i_minor_version );
 
-    if( ( p_box->data.p_ftyp->i_compatible_brands_count = i_read / 4 ) )
+    p_box->data.p_ftyp->i_compatible_brands_count = i_read / 4;
+    if( p_box->data.p_ftyp->i_compatible_brands_count > 0 )
     {
         uint32_t *tab = p_box->data.p_ftyp->i_compatible_brands =
-            calloc( p_box->data.p_ftyp->i_compatible_brands_count,
-                    sizeof(uint32_t));
+            vlc_alloc( p_box->data.p_ftyp->i_compatible_brands_count,
+                       sizeof(uint32_t) );
 
         if( unlikely( tab == NULL ) )
             MP4_READBOX_EXIT( 0 );
