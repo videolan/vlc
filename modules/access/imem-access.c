95cc776fec3f44c99abee34f9bd0e5d20cfab40b
Michał Trzebiatowski <michtrz@gmail.com>
2017-11-25 15:50:46 +0100

    i18n: Nemory stream -> Memory stream
    Signed-off-by: Michał Trzebiatowski <michtrz@gmail.com>
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/access/imem-access.c
+++ modules/access/imem-access.c
@@ -156,7 +156,7 @@ static void Close(vlc_object_t *object)
 }
 
 vlc_module_begin()
-    set_shortname(N_("Nemory stream"))
+    set_shortname(N_("Memory stream"))
     set_description(N_("In-memory stream input"))
     set_category(CAT_INPUT)
     set_subcategory(SUBCAT_INPUT_ACCESS)
