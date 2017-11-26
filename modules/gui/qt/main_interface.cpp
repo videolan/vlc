ba8922fbd21ce64f7353556e1659f3da88a335eb
Marvin Scholz <epirat07@gmail.com>
2017-11-26 04:37:49 +0100

    qt: Remove macOS specific main interface styling
    We already have the macosx interface, if the user wants something thats
    native. No need to try hard here, additionally it looks much better
    without this.

--- modules/gui/qt/main_interface.cpp
+++ modules/gui/qt/main_interface.cpp
@@ -127,9 +127,6 @@ MainInterface::MainInterface( intf_thread_t *_p_intf ) : QVLCMW( _p_intf )
     setWindowRole( "vlc-main" );
     setWindowIcon( QApplication::windowIcon() );
     setWindowOpacity( var_InheritFloat( p_intf, "qt-opacity" ) );
-#ifdef Q_OS_MAC
-    setAttribute( Qt::WA_MacBrushedMetal );
-#endif
 
     /* Is video in embedded in the UI or not */
     b_videoEmbedded = var_InheritBool( p_intf, "embedded-video" );
