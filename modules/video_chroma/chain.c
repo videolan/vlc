videolan/vlc

commit 6e5e08677d9e279602397fcbc767e6d2a15b4ce5

chroma: chain: force one level of iteration for filters

modules/video_chroma/chain.c
@@ -192,6 +192,11 @@ static int ActivateFilter( vlc_object_t *p_this )
     if( !p_filter->b_allow_fmt_out_change || p_filter->psz_name == NULL )
         return VLC_EGENERIC;
 
+    /* Force only one level of iteration when using the chain converter from a
+     * filter. */
+    var_Create( p_filter, MODULE_STRING "-level", VLC_VAR_INTEGER );
+    var_SetInteger( p_filter, MODULE_STRING "-level", CHAIN_LEVEL_MAX - 1 );
+
     /* Try to add a converter before the requested filter */
     return Activate( p_filter, BuildFilterChain );
 }

commit 2aaca35c09878ba1095b14367fa0a4ae06eac660

chroma: chain: fix "force one level of iteration for filters"
This commit fixes a regression from 6e5e08677d9e279602397fcbc767e6d2a15b4ce5
that caused filter loading to never stop until a stack overflow.

modules/video_chroma/chain.c
@@ -195,6 +195,8 @@ static int ActivateFilter( vlc_object_t *p_this )
 
     /* Force only one level of iteration when using the chain converter from a
      * filter. */
+    if( var_InheritInteger( p_filter, "chain-level" ) > 0 )
+        return VLC_EGENERIC;
     var_Create( p_filter, "chain-level", VLC_VAR_INTEGER );
     var_SetInteger( p_filter, "chain-level", CHAIN_LEVEL_MAX - 1 );
 