videolan/vlc

commit cf4676c1e8cdf69db4598f7d10ffdf6a695c3ba0

core: rename vlc_*alloc to vlc_obj_*alloc
In order to avoid confusion with future vlc_alloc.

include/vlc_objects.h
@@ -67,8 +67,8 @@ VLC_API char *vlc_object_get_name( const vlc_object_t * ) VLC_USED;
 #define vlc_list_children(a) \
     vlc_list_children( VLC_OBJECT(a) )
 
-VLC_API VLC_MALLOC void *vlc_malloc(vlc_object_t *, size_t);
-VLC_API VLC_MALLOC void *vlc_calloc(vlc_object_t *, size_t, size_t);
-VLC_API void vlc_free(vlc_object_t *, void *);
+VLC_API VLC_MALLOC void *vlc_obj_alloc(vlc_object_t *, size_t, size_t);
+VLC_API VLC_MALLOC void *vlc_obj_calloc(vlc_object_t *, size_t, size_t);
+VLC_API void vlc_obj_free(vlc_object_t *, void *);
 
 /** @} */

modules/access/alsa.c
@@ -341,7 +341,7 @@ static uint16_t channel_maps[] = {
 static int Open (vlc_object_t *obj)
 {
     demux_t *demux = (demux_t *)obj;
-    demux_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/attachment.c
@@ -73,7 +73,7 @@ static int Open(vlc_object_t *object)
     if (!input)
         return VLC_EGENERIC;
 
-    access_sys_t *sys = vlc_malloc(object, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_alloc(object, 1, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/avio.c
@@ -83,7 +83,7 @@ struct sout_access_out_sys_t {
 int OpenAvio(vlc_object_t *object)
 {
     stream_t *access = (stream_t*)object;
-    access_sys_t *sys = vlc_malloc(object, sizeof(*sys));
+    access_sys_t *sys = vlc_obj_alloc(object, 1, sizeof(*sys));
     if (!sys)
         return VLC_ENOMEM;
     sys->context = NULL;
@@ -159,7 +159,7 @@ int OutOpenAvio(vlc_object_t *object)
 
     config_ChainParse( access, "sout-avio-", ppsz_sout_options, access->p_cfg );
 
-    sout_access_out_sys_t *sys = vlc_malloc(object, sizeof(*sys));
+    sout_access_out_sys_t *sys = vlc_obj_alloc(object, 1, sizeof(*sys));
     if (!sys)
         return VLC_ENOMEM;
     sys->context = NULL;

modules/access/cdda.c
@@ -225,7 +225,7 @@ static int DemuxOpen(vlc_object_t *obj)
     if (track == 0 /* Whole disc -> use access plugin */)
         goto error;
 
-    demux_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
     if (unlikely(sys == NULL))
         goto error;
 
@@ -619,7 +619,7 @@ static int AccessOpen(vlc_object_t *obj)
         return VLC_EGENERIC;
     }
 
-    access_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
     if (unlikely(sys == NULL))
     {
         ioctl_Close(obj, dev);

modules/access/concat.c
@@ -188,7 +188,7 @@ static int Open(vlc_object_t *obj)
     if (list == NULL)
         return VLC_EGENERIC;
 
-    access_sys_t *sys = vlc_malloc(obj, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
     if (unlikely(sys == NULL))
     {
         free(list);

modules/access/directory.c
@@ -53,7 +53,7 @@ struct access_sys_t
  *****************************************************************************/
 int DirInit (stream_t *access, DIR *dir)
 {
-    access_sys_t *sys = vlc_malloc(VLC_OBJECT(access), sizeof (*sys));
+    access_sys_t *sys = vlc_obj_alloc(VLC_OBJECT(access), 1, sizeof (*sys));
     if (unlikely(sys == NULL))
         goto error;
 

modules/access/dv.c
@@ -128,7 +128,7 @@ static int Open( vlc_object_t *p_this )
     /* Set up p_access */
     ACCESS_SET_CALLBACKS( NULL, Block, Control, NULL );
 
-    p_access->p_sys = p_sys = vlc_malloc( p_this, sizeof( access_sys_t ) );
+    p_access->p_sys = p_sys = vlc_obj_alloc( p_this, 1, sizeof( access_sys_t ) );
     if( !p_sys )
         return VLC_EGENERIC;
 

commit bf51e088023ae35dfba1a9b781fed853739c4a8c

objres: restore malloc() wrapper
Regression from cf4676c1e8cdf69db4598f7d10ffdf6a695c3ba0.

include/vlc_objects.h
@@ -67,7 +67,7 @@ VLC_API char *vlc_object_get_name( const vlc_object_t * ) VLC_USED;
 #define vlc_list_children(a) \
     vlc_list_children( VLC_OBJECT(a) )
 
-VLC_API VLC_MALLOC void *vlc_obj_alloc(vlc_object_t *, size_t, size_t);
+VLC_API VLC_MALLOC void *vlc_obj_malloc(vlc_object_t *, size_t);
 VLC_API VLC_MALLOC void *vlc_obj_calloc(vlc_object_t *, size_t, size_t);
 VLC_API void vlc_obj_free(vlc_object_t *, void *);
 

modules/access/alsa.c
@@ -341,7 +341,7 @@ static uint16_t channel_maps[] = {
 static int Open (vlc_object_t *obj)
 {
     demux_t *demux = (demux_t *)obj;
-    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/attachment.c
@@ -73,7 +73,7 @@ static int Open(vlc_object_t *object)
     if (!input)
         return VLC_EGENERIC;
 
-    access_sys_t *sys = vlc_obj_alloc(object, 1, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_malloc(object, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/avio.c
@@ -83,7 +83,7 @@ struct sout_access_out_sys_t {
 int OpenAvio(vlc_object_t *object)
 {
     stream_t *access = (stream_t*)object;
-    access_sys_t *sys = vlc_obj_alloc(object, 1, sizeof(*sys));
+    access_sys_t *sys = vlc_obj_malloc(object, sizeof(*sys));
     if (!sys)
         return VLC_ENOMEM;
     sys->context = NULL;
@@ -159,7 +159,7 @@ int OutOpenAvio(vlc_object_t *object)
 
     config_ChainParse( access, "sout-avio-", ppsz_sout_options, access->p_cfg );
 
-    sout_access_out_sys_t *sys = vlc_obj_alloc(object, 1, sizeof(*sys));
+    sout_access_out_sys_t *sys = vlc_obj_malloc(object, sizeof(*sys));
     if (!sys)
         return VLC_ENOMEM;
     sys->context = NULL;

modules/access/cdda.c
@@ -225,7 +225,7 @@ static int DemuxOpen(vlc_object_t *obj)
     if (track == 0 /* Whole disc -> use access plugin */)
         goto error;
 
-    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
         goto error;
 
@@ -619,7 +619,7 @@ static int AccessOpen(vlc_object_t *obj)
         return VLC_EGENERIC;
     }
 
-    access_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
     {
         ioctl_Close(obj, dev);

modules/access/concat.c
@@ -188,7 +188,7 @@ static int Open(vlc_object_t *obj)
     if (list == NULL)
         return VLC_EGENERIC;
 
-    access_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
     {
         free(list);

modules/access/directory.c
@@ -53,7 +53,7 @@ struct access_sys_t
  *****************************************************************************/
 int DirInit (stream_t *access, DIR *dir)
 {
-    access_sys_t *sys = vlc_obj_alloc(VLC_OBJECT(access), 1, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_malloc(VLC_OBJECT(access), sizeof (*sys));
     if (unlikely(sys == NULL))
         goto error;
 

modules/access/dv.c
@@ -128,7 +128,7 @@ static int Open( vlc_object_t *p_this )
     /* Set up p_access */
     ACCESS_SET_CALLBACKS( NULL, Block, Control, NULL );
 
-    p_access->p_sys = p_sys = vlc_obj_alloc( p_this, 1, sizeof( access_sys_t ) );
+    p_access->p_sys = p_sys = vlc_obj_malloc( p_this, sizeof( *p_sys ) );
     if( !p_sys )
         return VLC_EGENERIC;
 

modules/access/file.c
@@ -205,7 +205,7 @@ int FileOpen( vlc_object_t *p_this )
 #endif
     }
 
-    access_sys_t *p_sys = vlc_obj_alloc(p_this, 1, sizeof (*p_sys));
+    access_sys_t *p_sys = vlc_obj_malloc(p_this, sizeof (*p_sys));
     if (unlikely(p_sys == NULL))
         goto error;
     p_access->pf_read = Read;

modules/access/http.c
@@ -156,7 +156,7 @@ static int Open( vlc_object_t *p_this )
     int ret = VLC_EGENERIC;
     vlc_credential credential;
 
-    access_sys_t *p_sys = vlc_obj_alloc( p_this, 1, sizeof(*p_sys) );
+    access_sys_t *p_sys = vlc_obj_malloc( p_this, sizeof(*p_sys) );
     if( unlikely(p_sys == NULL) )
         return VLC_ENOMEM;
 

modules/access/idummy.c
@@ -184,7 +184,7 @@ nop:
         if( length == 0 )
             goto nop; /* avoid division by zero */
 
-        demux_sys_t *p_sys = vlc_obj_alloc( p_this, 1, sizeof( *p_sys ) );
+        demux_sys_t *p_sys = vlc_obj_malloc( p_this, sizeof( *p_sys ) );
         if( p_sys == NULL )
             return VLC_ENOMEM;
 

modules/access/imem-access.c
@@ -112,7 +112,7 @@ static int Open(vlc_object_t *object)
 {
     stream_t *access = (stream_t *)object;
 
-    access_sys_t *sys = vlc_obj_alloc(object, 1, sizeof (*sys));
+    access_sys_t *sys = vlc_obj_malloc(object, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/pulse.c
@@ -255,7 +255,7 @@ static int Open(vlc_object_t *obj)
 {
     demux_t *demux = (demux_t *)obj;
 
-    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/sdp.c
@@ -55,7 +55,7 @@ static int Open (vlc_object_t *obj)
     stream_t *access = (stream_t *)obj;
     size_t len = strlen (access->psz_location);
 
-    access_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof(*sys) + len);
+    access_sys_t *sys = vlc_obj_malloc(obj, sizeof(*sys) + len);
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/shm.c
@@ -138,7 +138,7 @@ struct demux_sys_t
 static int Open (vlc_object_t *obj)
 {
     demux_t *demux = (demux_t *)obj;
-    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/access/srt.c
@@ -172,7 +172,7 @@ static int Open(vlc_object_t *p_this)
     }, *res = NULL;
     int stat;
 
-    p_sys = vlc_obj_alloc( p_this, 1, sizeof( *p_sys ) );
+    p_sys = vlc_obj_malloc( p_this, sizeof( *p_sys ) );
     if( unlikely( p_sys == NULL ) )
         return VLC_ENOMEM;
 

modules/access/timecode.c
@@ -167,7 +167,7 @@ static int Control (demux_t *demux, int query, va_list args)
 static int Open (vlc_object_t *obj)
 {
     demux_t *demux = (demux_t *)obj;
-    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
 
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;

modules/access/udp.c
@@ -100,7 +100,7 @@ static int Open( vlc_object_t *p_this )
     if( p_access->b_preparsing )
         return VLC_EGENERIC;
 
-    sys = vlc_obj_alloc( p_this, 1, sizeof( *sys ) );
+    sys = vlc_obj_malloc( p_this, sizeof( *sys ) );
     if( unlikely( sys == NULL ) )
         return VLC_ENOMEM;
 

modules/access/wasapi.c
@@ -387,7 +387,7 @@ static int Open(vlc_object_t *obj)
     if (demux->psz_location != NULL && *demux->psz_location != '\0')
         return VLC_EGENERIC; /* TODO non-default device */
 
-    demux_sys_t *sys = vlc_obj_alloc(obj, 1, sizeof (*sys));
+    demux_sys_t *sys = vlc_obj_malloc(obj, sizeof (*sys));
     if (unlikely(sys == NULL))
         return VLC_ENOMEM;
 

modules/video_chroma/i420_10_p010.c
@@ -75,8 +75,8 @@ static int Create( vlc_object_t *p_this )
     if ( p_filter->fmt_in.video.i_chroma != VLC_CODEC_I420_10L)
         return -1;
 
-    filter_sys_t *p_sys = vlc_obj_alloc( VLC_OBJECT( p_filter ), 1,
-                                         sizeof(filter_sys_t) );
+    filter_sys_t *p_sys = vlc_obj_malloc( VLC_OBJECT( p_filter ),
+                                          sizeof(*p_sys) );
     if (!p_sys)
          return VLC_ENOMEM;
 

modules/video_chroma/i420_nv12.c
@@ -89,8 +89,8 @@ static int Create( vlc_object_t *p_this )
             return -1;
     }
 
-    filter_sys_t *p_sys = vlc_obj_alloc( VLC_OBJECT( p_filter ), 1,
-                                         sizeof(filter_sys_t) );
+    filter_sys_t *p_sys = vlc_obj_malloc( VLC_OBJECT( p_filter ),
+                                          sizeof(*p_sys) );
     if (!p_sys)
          return VLC_ENOMEM;
 

modules/video_output/ios.m
@@ -183,7 +183,7 @@ static int Open(vlc_object_t *this)
             goto bailout;
 
         struct gl_sys *glsys = sys->gl->sys =
-            vlc_obj_alloc(this, 1, sizeof(struct gl_sys));
+            vlc_obj_malloc(this, sizeof(struct gl_sys));
         if (unlikely(!sys->gl->sys))
             goto bailout;
         glsys->glESView = sys->glESView;

src/libvlccore.sym
@@ -275,7 +275,7 @@ vlc_LogSet
 vlc_vaLog
 vlc_strerror
 vlc_strerror_c
-vlc_obj_alloc
+vlc_obj_malloc
 vlc_obj_calloc
 vlc_obj_free
 msleep

src/misc/objres.c
@@ -133,6 +133,14 @@ static bool ptrcmp(void *a, void *b)
     return a == b;
 }
 
+void *vlc_obj_malloc(vlc_object_t *obj, size_t size)
+{
+    void *ptr = vlc_objres_new(size, dummy_release);
+    if (likely(ptr != NULL))
+        vlc_objres_push(obj, ptr);
+    return ptr;
+}
+
 static void *vlc_obj_alloc_common(vlc_object_t *obj, size_t nmemb, size_t size,
                                   bool do_memset)
 {
@@ -151,11 +159,6 @@ static void *vlc_obj_alloc_common(vlc_object_t *obj, size_t nmemb, size_t size,
         vlc_objres_push(obj, ptr);
     }
     return ptr;
-}
-
-void *vlc_obj_alloc(vlc_object_t *obj, size_t nmemb, size_t size)
-{
-    return vlc_obj_alloc_common(obj, nmemb, size, false);
 }
 
 void *vlc_obj_calloc(vlc_object_t *obj, size_t nmemb, size_t size)