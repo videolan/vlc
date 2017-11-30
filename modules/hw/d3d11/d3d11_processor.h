a672bff22ecd0f2d26cbccb6e0e022967713d542
Steve Lhomme <robux4@videolabs.io>
2017-11-30 13:15:07 +0100

    hw:d3d11: add d3d11_processor_t for generic VideoProcessor handling

---
+++ modules/hw/d3d11/d3d11_processor.h
@@ -0,0 +1,40 @@
+/*****************************************************************************
+ * d3d11_processor.h: D3D11 VideoProcessor helper
+ *****************************************************************************
+ * Copyright © 2017 VLC authors, VideoLAN and VideoLabs
+ *
+ * Authors: Steve Lhomme <robux4@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef VLC_D3D11_PROCESSOR_H
+#define VLC_D3D11_PROCESSOR_H
+
+#include <vlc_common.h>
+
+#include "../../video_chroma/d3d11_fmt.h"
+
+#ifdef ID3D11VideoContext_VideoProcessorBlt
+typedef struct
+{
+    ID3D11VideoDevice              *d3dviddev;
+    ID3D11VideoContext             *d3dvidctx;
+    ID3D11VideoProcessorEnumerator *procEnumerator;
+    ID3D11VideoProcessor           *videoProcessor;
+} d3d11_processor_t;
+#endif
+
+#endif /* VLC_D3D11_PROCESSOR_H */
