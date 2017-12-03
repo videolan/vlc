71bf8aa7ca6ea5837ad1ab50ed2783c679897ba1
Rémi Denis-Courmont <remi@remlab.net>
2017-12-03 17:05:50 +0200

    cdg: prepare for void *p_sys

--- modules/demux/cdg.c
+++ modules/demux/cdg.c
@@ -166,14 +166,14 @@ static void Close ( vlc_object_t * p_this )
  *****************************************************************************/
 static int Control( demux_t *p_demux, int i_query, va_list args )
 {
+    demux_sys_t *p_sys = p_demux->p_sys;
     uint64_t i_old_offset = vlc_stream_Tell( p_demux->s );
     int i_ret = demux_vaControlHelper( p_demux->s, 0, -1,
                                        8*CDG_FRAME_SIZE*CDG_FRAME_RATE, CDG_FRAME_SIZE,
                                        i_query, args );
     if( !i_ret && ( i_query == DEMUX_SET_POSITION || i_query == DEMUX_SET_TIME ) )
     {
-        date_Set( &p_demux->p_sys->pts,
-                  vlc_stream_Tell( p_demux->s ) / CDG_FRAME_SIZE *
+        date_Set( &p_sys->pts, vlc_stream_Tell( p_demux->s ) / CDG_FRAME_SIZE *
                     INT64_C(1000000) / CDG_FRAME_RATE );
         if ( i_old_offset > vlc_stream_Tell( p_demux->s ) )
             i_ret = vlc_stream_Seek( p_demux->s, 0 );
