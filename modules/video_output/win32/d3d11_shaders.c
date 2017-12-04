ce51968ce07b52d32a7a1ff6ccdcf821cfefc058
Steve Lhomme <robux4@videolabs.io>
2017-12-04 12:37:44 +0100

    d3d11_shaders: add support for DXGI_FORMAT_AYUV rendering

--- modules/video_output/win32/d3d11_shaders.c
+++ modules/video_output/win32/d3d11_shaders.c
@@ -226,6 +226,13 @@ HRESULT D3D11_CompilePixelShader(vlc_object_t *o, d3d11_handle_t *hd3d, bool leg
                 sample.z  = shaderTexture[0].Sample(samplerState, coords).a;\
                 sample.a  = 1;";
         break;
+    case DXGI_FORMAT_AYUV:
+        psz_sampler =
+                "sample.x  = shaderTexture[0].Sample(SampleType, In.Texture).z;\
+                sample.y  = shaderTexture[0].Sample(SampleType, In.Texture).y;\
+                sample.z  = shaderTexture[0].Sample(SampleType, In.Texture).x;\
+                sample.a  = shaderTexture[0].Sample(SampleType, In.Texture).a;";
+        break;
     case DXGI_FORMAT_R8G8B8A8_UNORM:
     case DXGI_FORMAT_B8G8R8A8_UNORM:
     case DXGI_FORMAT_B8G8R8X8_UNORM:
