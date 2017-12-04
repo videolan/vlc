9bf0ef7b771d5250002846a1ed9bce236393b04f
Michał Trzebiatowski <michtrz@gmail.com>
2017-12-04 01:12:55 +0100

    i18n: capital letter at the beginning
    Signed-off-by: Michał Trzebiatowski <michtrz@gmail.com>
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>
    (cherry picked from commit ec0992fbfb5cca619b34da48502d317ab51a94c0)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/keystore/memory.c
+++ modules/keystore/memory.c
@@ -38,8 +38,8 @@ static int Open(vlc_object_t *);
 static void Close(vlc_object_t *);
 
 vlc_module_begin()
-    set_shortname(N_("memory keystore"))
-    set_description(N_("secrets are stored in memory"))
+    set_shortname(N_("Memory keystore"))
+    set_description(N_("Secrets are stored in memory"))
     set_category(CAT_ADVANCED)
     set_subcategory(SUBCAT_ADVANCED_MISC)
     set_capability("keystore", 0)
