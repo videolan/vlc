c0bf5aeefc611f8d1e1ace6111c88956c0a8fa6c
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-12-05 18:39:42 +0100

    configure: Assume if_nametoindex to be present

--- include/vlc_fixups.h
+++ include/vlc_fixups.h
@@ -411,9 +411,6 @@ struct if_nameindex
     unsigned if_index;
     char    *if_name;
 };
-# ifndef HAVE_IF_NAMETOINDEX
-#  define if_nametoindex(name)   atoi(name)
-# endif
 # define if_nameindex()         (errno = ENOBUFS, NULL)
 # define if_freenameindex(list) (void)0
 #endif
