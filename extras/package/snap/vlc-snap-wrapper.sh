0e932d9d816c5d78cfd5e37423f91e752a1ddf2b
Alan Pope <alan.pope@canonical.com>
2017-12-07 21:28:39 +0300

    snap: fixes to be able to build on Ubuntu 16.0.4
    - Fixed the desktop file and icon location
    - Fixed the required gettext version (0.19.8 is not available on 16.04)
    - Added ersion-script and 'echo' in the install stanza to easily
      identify git id from which the build was made
    - Fixed vlc-wrapper to set the right environment variables to expose
      GPU drivers for VDPAU VA-API to work out of the box
    - Bundle ffmpeg-3.4 and wayland-protocols

--- extras/package/snap/vlc-snap-wrapper.sh
+++ extras/package/snap/vlc-snap-wrapper.sh
@@ -1,5 +1,28 @@
-#!/bin/sh
+#!/bin/bash
+case "$SNAP_ARCH" in
+	"amd64") ARCH='x86_64-linux-gnu'
+	;;
+	"i386") ARCH='i386-linux-gnu'
+	;;
+	*)
+		echo "Unsupported architecture for this app build"
+		exit 1
+	;;
+esac
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/lib/vlc"
+VENDOR=$(glxinfo | grep "OpenGL vendor")
 
-exec desktop-launch vlc "$@"
+if [[ $VENDOR == *"Intel"* ]]; then
+  export VDPAU_DRIVER_PATH="$SNAP/usr/lib/$ARCH/dri"
+  export LIBVA_DRIVERS_PATH="$SNAP/usr/lib/$ARCH/dri"
+fi
+
+if [[ $VENDOR == *"NVIDIA"* ]]; then
+  export VDPAU_DRIVER_PATH="/var/lib/snapd/lib/gl/vdpau"
+elif [[ $VENDOR == *"X.Org"* ]]; then
+  export VDPAU_DRIVER_PATH="/usr/lib/$ARCH/vdpau/"
+fi
+
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/usr/lib/vlc"
+
+exec $SNAP/usr/bin/vlc "$@"
