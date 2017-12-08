videolan/vlc

commit 790d6b1f5437b1307c831e6ac509baa40d69e3fd

codec: faad: don't reorder with random table
heap buffer ofw on no reorder

modules/codec/faad.c
@@ -525,7 +525,7 @@ static int DecodeBlock( decoder_t *p_dec, block_t *p_block )
             else pi_faad_channels_positions[i] = 0;
         }
 
-        aout_CheckChannelReorder( pi_faad_channels_positions, NULL,
+        bool b_reorder = aout_CheckChannelReorder( pi_faad_channels_positions, NULL,
                                   p_dec->fmt_out.audio.i_physical_channels, pi_neworder_table );
 
 
@@ -546,9 +546,10 @@ static int DecodeBlock( decoder_t *p_dec, block_t *p_block )
                 memset( p_out->p_buffer, 0, p_out->i_buffer );
 
             /* FIXME: replace when aout_channel_reorder can take samples from a different buffer */
-            DoReordering( (uint32_t *)p_out->p_buffer, samples,
-                          frame.samples / frame.channels, frame.channels,
-                          pi_neworder_table );
+            if( b_reorder )
+                DoReordering( (uint32_t *)p_out->p_buffer, samples,
+                              frame.samples / frame.channels, frame.channels,
+                              pi_neworder_table );
 
             if( p_sys->b_discontinuity )
             {

commit 001b765d6699676077f147471f62ef701a5f478c

codec: faad: fix !reordering case
Regression from 790d6b1f5437b1307c831e6ac509baa40d69e3fd

modules/codec/faad.c
@@ -550,6 +550,8 @@ static int DecodeBlock( decoder_t *p_dec, block_t *p_block )
                 DoReordering( (uint32_t *)p_out->p_buffer, samples,
                               frame.samples / frame.channels, frame.channels,
                               pi_neworder_table );
+            else
+                 memcpy( p_out->p_buffer, samples, p_out->i_buffer );
 
             if( p_sys->b_discontinuity )
             {