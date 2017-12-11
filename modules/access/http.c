b180121763aeb686fc331d6665c622ce02738bc4
Marvin Scholz <epirat07@gmail.com>
2017-12-11 12:52:20 +0100

    access/http: Fix off-by-one in ICY parser
    This fixes a off-by-one issue in the ICY parser that would happen in the
    case the ICY metadata is unquoted. (StreamTitle=test;).
    With empty metadata without ; (StreamTitle=) this would lead to a buffer
    over-read.
    
    Credit to Filip Roséen who discovered this issue.
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/access/http.c
+++ modules/access/http.c
@@ -525,18 +525,19 @@ static int ReadICYMeta( stream_t *p_access )
                 psz = strchr( &p[1], ';' );
 
             if( psz ) *psz = '\0';
+            p++;
         }
         else
         {
-            char *psz = strchr( &p[1], ';' );
+            char *psz = strchr( p, ';' );
             if( psz ) *psz = '\0';
         }
 
         if( !p_sys->psz_icy_title ||
-            strcmp( p_sys->psz_icy_title, &p[1] ) )
+            strcmp( p_sys->psz_icy_title, p ) )
         {
             free( p_sys->psz_icy_title );
-            char *psz_tmp = strdup( &p[1] );
+            char *psz_tmp = strdup( p );
             p_sys->psz_icy_title = EnsureUTF8( psz_tmp );
             if( !p_sys->psz_icy_title )
                 free( psz_tmp );
