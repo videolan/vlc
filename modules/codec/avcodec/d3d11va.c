52d4f9bcb4a4e302e603dd318f20ddca427c05a2
Steve Lhomme <robUx4@videolabs.io>
2017-12-13 16:33:39 +0100

    d3d11va: do the Xbox size check after the texture dimensions are definitive
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>
    (cherry picked from commit 067e582bd813e7d2038610df561c83afa0e6b9a0)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/codec/avcodec/d3d11va.c
+++ modules/codec/avcodec/d3d11va.c
@@ -686,17 +686,6 @@ static int DxCreateDecoderSurfaces(vlc_va_t *va, int codec_id,
         sys->textureHeight = fmt->i_height;
     }
 
-#if VLC_WINSTORE_APP
-    /* On the Xbox 1/S, any decoding of H264 with one dimension over 2304
-     * crashes totally the device */
-    if (codec_id == AV_CODEC_ID_H264 &&
-        (fmt->i_width > 2304 || fmt->i_height > 2304) &&
-        isXboxHardware(sys->d3d_dev.d3ddevice))
-    {
-        msg_Warn(va, "%dx%d resolution not supported by your hardware", fmt->i_width, fmt->i_height);
-        return VLC_EGENERIC;
-    }
-#endif
     if ((sys->textureWidth != fmt->i_width || sys->textureHeight != fmt->i_height) &&
         !CanUseDecoderPadding(sys))
     {
@@ -706,6 +695,17 @@ static int DxCreateDecoderSurfaces(vlc_va_t *va, int codec_id,
         sys->textureWidth  = fmt->i_width;
         sys->textureHeight = fmt->i_height;
     }
+#if VLC_WINSTORE_APP
+    /* On the Xbox 1/S, any decoding of H264 with one dimension over 2304
+     * crashes totally the device */
+    if (codec_id == AV_CODEC_ID_H264 &&
+        (sys->textureWidth > 2304 || sys->textureHeight > 2304) &&
+        isXboxHardware(sys->d3d_dev.d3ddevice))
+    {
+        msg_Warn(va, "%dx%d resolution not supported by your hardware", fmt->i_width, fmt->i_height);
+        return VLC_EGENERIC;
+    }
+#endif
 
     D3D11_VIDEO_DECODER_OUTPUT_VIEW_DESC viewDesc;
     ZeroMemory(&viewDesc, sizeof(viewDesc));
