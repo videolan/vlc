99f88a0b8cf3e14c51dd8516708e901a96cf44d0
Thomas Guillem <thomas@gllm.fr>
2017-12-20 14:09:08 +0100

    aout: add HACK to differentiate DTS from DTS-HD
    This HACK need to be replaced in 4.0 by adding a profile to audio_format_t or
    by passing it to aout modules.
    
    (cherry picked from commit 542c20c7c35fedfecbb698d0b3222b2c489ff1a4)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -365,6 +365,11 @@ static int aout_update_format( decoder_t *p_dec )
         p_aout = input_resource_GetAout( p_owner->p_resource );
         if( p_aout )
         {
+            /* TODO: 3.0 HACK: we need to put i_profile inside audio_format_t
+             * for 4.0 */
+            if( p_dec->fmt_out.i_codec == VLC_CODEC_DTS )
+                var_SetBool( p_aout, "dtshd", p_dec->fmt_out.i_profile > 0 );
+
             if( aout_DecNew( p_aout, &format,
                              &p_dec->fmt_out.audio_replay_gain,
                              &request_vout ) )
