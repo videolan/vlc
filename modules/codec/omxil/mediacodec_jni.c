62a817e53cd1f042ad487d6898d7f500aa9284ec
Thomas Guillem <thomas@gllm.fr>
2018-01-10 14:19:55 +0100

    mediacodec: add OMX_VIDEO_HEVCProfileMain10HDR10
    (cherry picked from commit b88eb97a98d81d45ac7b4c45617a588e18d5a092)
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- modules/codec/omxil/mediacodec_jni.c
+++ modules/codec/omxil/mediacodec_jni.c
@@ -427,7 +427,8 @@ char* MediaCodec_GetName(vlc_object_t *p_obj, const char *psz_mime,
                                 case 0x1: /* OMX_VIDEO_HEVCProfileMain */
                                     codec_profile = HEVC_PROFILE_MAIN;
                                     break;
-                                case 0x2: /* OMX_VIDEO_HEVCProfileMain10 */
+                                case 0x2:    /* OMX_VIDEO_HEVCProfileMain10 */
+                                case 0x1000: /* OMX_VIDEO_HEVCProfileMain10HDR10 */
                                     codec_profile = HEVC_PROFILE_MAIN_10;
                                     break;
                             }
