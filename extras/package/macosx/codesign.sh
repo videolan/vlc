2e2a1b34374f617ae2dac11bcbec8fd76a33eacb
David Fuhrmann <dfuhrmann@videolan.org>
2018-01-13 18:40:42 +0100

    macOS codesign: Also use the org.videolan. prefix in gk mode
    This is a per apple guideline.

--- extras/package/macosx/codesign.sh
+++ extras/package/macosx/codesign.sh
@@ -85,7 +85,7 @@ sign()
     IDENTIFIER="$2"
     if [ -z "$IDENTIFIER" ]; then
         filename=$(basename "$1")
-        IDENTIFIER="${filename%.*}"
+        IDENTIFIER="org.videolan.${filename%.*}"
     fi
 
     # info "Signing file $1 with identifier $IDENTIFIER"
