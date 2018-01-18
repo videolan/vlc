c3ac6661b7cdb2432d53216379969ded65a468ed
Steve Lhomme <robux4@ycbcr.xyz>
2018-01-18 14:02:08 +0100

    demux:mkv: detect bogus MPEG-2 framing from MakeMKV
    This software doesn't put mpeg-2 frames properly in mkv frames. Without a
    packetizer we get a lot of decoding issues.
    
    Fixes #18916

--- modules/demux/mkv/matroska_segment_parse.cpp
+++ modules/demux/mkv/matroska_segment_parse.cpp
@@ -1517,6 +1517,8 @@ bool matroska_segment_c::TrackInit( mkv_track_t * p_tk )
         }
         S_CASE("V_MPEG2") {
             vars.p_fmt->i_codec = VLC_CODEC_MPGV;
+            if (strstr(vars.obj->psz_muxing_application,"libmakemkv"))
+                vars.p_fmt->b_packetized = false;
             fill_extra_data( vars.p_tk, 0 );
         }
         S_CASE("V_THEORA") {
