92c10217afaec03941c2794bbf1ac4b2f6dc9cb6
Jean-Baptiste Kempf <jb@videolan.org>
2018-01-19 13:10:06 +0100

    DxVA: fix compilation after backport

--- modules/hw/d3d9/dxa9.c
+++ modules/hw/d3d9/dxa9.c
@@ -68,6 +68,13 @@ static bool GetLock(filter_t *p_filter, LPDIRECT3DSURFACE9 d3d,
     return true;
 }
 
+static inline void plane_SwapUV(plane_t p[PICTURE_PLANE_MAX])
+{
+    uint8_t *buf = p[V_PLANE].p_pixels;
+    p[V_PLANE].p_pixels = p[U_PLANE].p_pixels;
+    p[U_PLANE].p_pixels = buf;
+}
+
 static void DXA9_YV12(filter_t *p_filter, picture_t *src, picture_t *dst)
 {
     copy_cache_t *p_copy_cache = (copy_cache_t*) p_filter->p_sys;
