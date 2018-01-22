videolan/vlc

commit e82fce4409620daf7a7c293c0c2233afc4c1a123

demux: Implement legacy fallbacks in the modules
This is a pre-requisite to revert
a2beb67ed706e299010c28d8733e2c0123b57168

modules/demux/mp4/mp4.c
@@ -2139,6 +2139,13 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
         case DEMUX_SET_GROUP:
         case DEMUX_HAS_UNSUPPORTED_META:
         case DEMUX_CAN_RECORD:
+            return VLC_EGENERIC;
+
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+            return demux_vaControlHelper( p_demux->s, 0, -1, 0, 1, i_query, args );
+
         default:
             return VLC_EGENERIC;
     }

modules/demux/aiff.c
@@ -344,6 +344,14 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
         }
         case DEMUX_SET_TIME:
         case DEMUX_GET_FPS:
+            return VLC_EGENERIC;
+
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+        case DEMUX_GET_PTS_DELAY:
+             return demux_vaControlHelper( p_demux->s, 0, -1, 0, 1, i_query, args );
+
         default:
             return VLC_EGENERIC;
     }

modules/demux/avformat/demux.c
@@ -1167,7 +1167,11 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
                 return VLC_EGENERIC;
             *va_arg( args, int * ) = p_sys->i_seekpoint;
             return VLC_SUCCESS;
-
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+        case DEMUX_GET_PTS_DELAY:
+            return demux_vaControlHelper( p_demux->s, 0, -1, 0, 1, i_query, args );
         default:
             return VLC_EGENERIC;
     }

modules/demux/avi/avi.c
@@ -1792,6 +1792,12 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
             return VLC_EGENERIC;
         }
 
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+        case DEMUX_GET_PTS_DELAY:
+            return demux_vaControlHelper( p_demux->s, 0, -1, 0, 1, i_query, args );
+
         default:
             return VLC_EGENERIC;
     }

modules/demux/caf.c
@@ -1046,6 +1046,13 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
         case DEMUX_GET_META:
             return vlc_stream_Control( p_demux->s, STREAM_GET_META, args );
 
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+        case DEMUX_GET_PTS_DELAY:
+            return demux_vaControlHelper( p_demux->s, p_sys->i_data_offset,
+                                          p_sys->i_data_size, 0, 1, i_query, args );
+
         default:
             return VLC_EGENERIC;
     }

modules/demux/gme.c
@@ -333,6 +333,15 @@ static int Control (demux_t *demux, int query, va_list args)
         case DEMUX_GET_TITLE:
             *va_arg(args, int *) = sys->track_id;
             return VLC_SUCCESS;
+
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+        case DEMUX_GET_PTS_DELAY:
+            return demux_vaControlHelper( demux->s, 0, -1, 0, 1, query, args );
+
+        default:
+            return VLC_EGENERIC;
     }
 
     return VLC_EGENERIC;

modules/demux/image.c
@@ -283,8 +283,17 @@ static int Control(demux_t *demux, int query, va_list args)
     case DEMUX_GET_META:
     case DEMUX_HAS_UNSUPPORTED_META:
     case DEMUX_GET_ATTACHMENTS:
+        return VLC_EGENERIC;
+
+    case DEMUX_CAN_PAUSE:
+    case DEMUX_SET_PAUSE_STATE:
+    case DEMUX_CAN_CONTROL_PACE:
+    case DEMUX_GET_PTS_DELAY:
+        return demux_vaControlHelper( demux->s, 0, -1, 0, 1, query, args );
+
     default:
         return VLC_EGENERIC;
+
     }
 }
 

modules/demux/mkv/mkv.cpp
@@ -459,6 +459,13 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
             b = va_arg( args, int ); /* precise? */
             msg_Dbg(p_demux,"SET_TIME to %" PRId64, i64 );
             return Seek( p_demux, i64, -1, NULL, b );
+
+        case DEMUX_CAN_PAUSE:
+        case DEMUX_SET_PAUSE_STATE:
+        case DEMUX_CAN_CONTROL_PACE:
+        case DEMUX_GET_PTS_DELAY:
+            return demux_vaControlHelper( p_demux->s, 0, -1, 0, 1, i_query, args );
+
         default:
             return VLC_EGENERIC;
     }

commit 103c285eb7fe03b0fa0be45745712cdaeb9a17ff

mp4: GET_PTS_DELAY: Fallback to demux_vaControlHelper when no LOAD box is present
Fix regression introduced by e82fce4409620daf7a7c293c0c2233afc4c1a123 /
dd6a06e2ee71a4fc06983984ea87d9e3d6c1f0ce

modules/demux/mp4/mp4.c
@@ -2133,7 +2133,7 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
                     return VLC_SUCCESS;
                 }
             }
-            return VLC_EGENERIC;
+            return demux_vaControlHelper( p_demux->s, 0, -1, 0, 1, i_query, args );
         }
         case DEMUX_SET_NEXT_DEMUX_TIME:
         case DEMUX_SET_GROUP: