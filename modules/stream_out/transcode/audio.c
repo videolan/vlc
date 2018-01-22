videolan/vlc

commit 67e993164249d81bb38bf3f3030238d04d36b176

transcode: change error handling
Don't reset b_transcode to false in case of error, otherwise transcoding
resources won't be freed.

modules/stream_out/transcode/audio.c
@@ -285,7 +285,7 @@ int transcode_audio_process( sout_stream_t *p_stream,
         p_audio_bufs = p_audio_bufs->p_next;
         p_audio_buf->p_next = NULL;
 
-        if( b_error )
+        if( id->b_error )
         {
             block_Release( p_audio_buf );
             continue;
@@ -312,7 +312,10 @@ int transcode_audio_process( sout_stream_t *p_stream,
             {
                 id->id = sout_StreamIdAdd( p_stream->p_next, &id->p_encoder->fmt_out );
                 if (!id->id)
-                    id->b_transcode = false;
+                {
+                    vlc_mutex_unlock(&id->fifo.lock);
+                    goto error;
+                }
             }
         }
 
@@ -365,10 +368,7 @@ int transcode_audio_process( sout_stream_t *p_stream,
         p_audio_buf = aout_FiltersPlay( id->p_af_chain, p_audio_buf,
                                         INPUT_RATE_DEFAULT );
         if( !p_audio_buf )
-        {
-            b_error = true;
-            continue;
-        }
+            goto error;
 
         p_audio_buf->i_dts = p_audio_buf->i_pts;
 
@@ -378,8 +378,9 @@ int transcode_audio_process( sout_stream_t *p_stream,
         block_Release( p_audio_buf );
         continue;
 error:
-        block_Release( p_audio_buf );
-        b_error = true;
+        if( p_audio_buf )
+            block_Release( p_audio_buf );
+        id->b_error = true;
     } while( p_audio_bufs );
 
 end:

modules/stream_out/transcode/video.c
@@ -61,9 +61,6 @@ static int video_update_format_decoder( decoder_t *p_dec )
         .sys = sys,
     };
 
-    if( !id->b_transcode )
-        return 0;
-
     if( id->p_encoder->fmt_in.i_codec == p_dec->fmt_out.i_codec ||
         video_format_IsSimilar( &id->video_dec_out,
                                 &p_dec->fmt_out.video ) )
@@ -434,7 +431,6 @@ static void transcode_video_framerate_init( sout_stream_t *p_stream,
         id->p_decoder->fmt_out.video.i_frame_rate_base,
         id->p_encoder->fmt_in.video.i_frame_rate,
         id->p_encoder->fmt_in.video.i_frame_rate_base );
-
 }
 
 static void transcode_video_size_init( sout_stream_t *p_stream,
@@ -782,7 +778,7 @@ int transcode_video_process( sout_stream_t *p_stream, sout_stream_id_sys_t *id,
         p_pics = p_pics->p_next;
         p_pic->p_next = NULL;
 
-        if( b_error )
+        if( id->b_error )
         {
             picture_Release( p_pic );
             continue;
@@ -868,8 +864,7 @@ int transcode_video_process( sout_stream_t *p_stream, sout_stream_id_sys_t *id,
 error:
         if( p_pic )
             picture_Release( p_pic );
-        id->b_transcode = false;
-        b_error = true;
+        id->b_error = true;
     } while( p_pics );
 
     if( p_sys->i_threads >= 1 )

modules/stream_out/transcode/transcode.c
@@ -586,6 +586,9 @@ static int Send( sout_stream_t *p_stream, sout_stream_id_sys_t *id,
 {
     block_t *p_out = NULL;
 
+    if( id->b_error )
+        goto error;
+
     if( !id->b_transcode )
     {
         if( id->id )

modules/stream_out/transcode/transcode.h
@@ -75,6 +75,7 @@ struct aout_filters;
 struct sout_stream_id_sys_t
 {
     bool            b_transcode;
+    bool            b_error;
 
     /* id of the out stream */
     void *id;

commit a6f6c3416fbf5f0cca5fe7dc076df90c5122c627

transcode: fix error handling
Forget a b_error case. Regression from 67e993164249d81bb38bf3f3030238d04d36b176

CID #1427674 #1427673

modules/stream_out/transcode/audio.c
@@ -269,7 +269,6 @@ int transcode_audio_process( sout_stream_t *p_stream,
 {
     sout_stream_sys_t *p_sys = p_stream->p_sys;
     *out = NULL;
-    bool b_error = false;
 
     int ret = id->p_decoder->pf_decode( id->p_decoder, in );
     if( ret != VLCDEC_SUCCESS )
@@ -385,7 +384,7 @@ error:
 
 end:
     /* Drain encoder */
-    if( unlikely( !b_error && in == NULL ) )
+    if( unlikely( !id->b_error && in == NULL ) )
     {
         if( id->p_encoder->p_module )
         {
@@ -397,7 +396,7 @@ end:
         }
     }
 
-    return b_error ? VLC_EGENERIC : VLC_SUCCESS;
+    return id->b_error ? VLC_EGENERIC : VLC_SUCCESS;
 }
 
 bool transcode_audio_add( sout_stream_t *p_stream, const es_format_t *p_fmt,

modules/stream_out/transcode/video.c
@@ -767,7 +767,6 @@ int transcode_video_process( sout_stream_t *p_stream, sout_stream_id_sys_t *id,
 {
     sout_stream_sys_t *p_sys = p_stream->p_sys;
     *out = NULL;
-    bool b_error = false;
 
     int ret = id->p_decoder->pf_decode( id->p_decoder, in );
     if( ret != VLCDEC_SUCCESS )
@@ -884,7 +883,8 @@ error:
     }
 
 end:
-    if( unlikely( in == NULL ) )
+    /* Drain encoder */
+    if( unlikely( !id->b_error && in == NULL ) )
     {
         if( p_sys->i_threads == 0 )
         {
@@ -915,7 +915,7 @@ end:
         }
     }
 
-    return b_error ? VLC_EGENERIC : VLC_SUCCESS;
+    return id->b_error ? VLC_EGENERIC : VLC_SUCCESS;
 }
 
 bool transcode_video_add( sout_stream_t *p_stream, const es_format_t *p_fmt,
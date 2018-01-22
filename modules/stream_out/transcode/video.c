a6f6c3416fbf5f0cca5fe7dc076df90c5122c627
Thomas Guillem <thomas@gllm.fr>
2018-01-22 08:47:49 +0100

    transcode: fix error handling
    Forget a b_error case. Regression from 67e993164249d81bb38bf3f3030238d04d36b176
    
    CID #1427674 #1427673

--- modules/stream_out/transcode/video.c
+++ modules/stream_out/transcode/video.c
@@ -767,7 +767,6 @@ int transcode_video_process( sout_stream_t *p_stream, sout_stream_id_sys_t *id,
 {
     sout_stream_sys_t *p_sys = p_stream->p_sys;
     *out = NULL;
-    bool b_error = false;
 
     int ret = id->p_decoder->pf_decode( id->p_decoder, in );
     if( ret != VLCDEC_SUCCESS )
@@ -884,7 +883,8 @@ error:
     }
 
 end:
-    if( unlikely( in == NULL ) )
+    /* Drain encoder */
+    if( unlikely( !id->b_error && in == NULL ) )
     {
         if( p_sys->i_threads == 0 )
         {
@@ -915,7 +915,7 @@ end:
         }
     }
 
-    return b_error ? VLC_EGENERIC : VLC_SUCCESS;
+    return id->b_error ? VLC_EGENERIC : VLC_SUCCESS;
 }
 
 bool transcode_video_add( sout_stream_t *p_stream, const es_format_t *p_fmt,
