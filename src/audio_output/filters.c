eda670d9ec1f94fd832fc27daa2e2e84c170b536
Rémi Denis-Courmont <remi@remlab.net>
2018-01-30 21:19:45 +0200

    aout: get audio-resampler value explicitly

--- src/audio_output/filters.c
+++ src/audio_output/filters.c
@@ -101,8 +101,11 @@ static filter_t *FindResampler (vlc_object_t *obj,
                                 const audio_sample_format_t *infmt,
                                 const audio_sample_format_t *outfmt)
 {
-    return CreateFilter (obj, "audio resampler", "$audio-resampler", NULL,
-                         infmt, outfmt, NULL, true);
+    char *modlist = var_InheritString(obj, "audio-resampler");
+    filter_t *filter = CreateFilter (obj, "audio resampler", modlist, NULL,
+                                     infmt, outfmt, NULL, true);
+    free(modlist);
+    return filter;
 }
 
 /**
