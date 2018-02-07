02daf5d3a5e8816534aa71d66b93f4aa1e05b3f4
Steve Lhomme <robux4@ycbcr.xyz>
2018-02-07 16:01:03 +0100

    d3d9_filters: fix a potential crash (again)
    Fixes #19612

--- modules/hw/d3d9/d3d9_filters.c
+++ modules/hw/d3d9/d3d9_filters.c
@@ -107,7 +107,7 @@ static picture_t *Filter(filter_t *p_filter, picture_t *p_pic)
     picture_sys_t *p_src_sys = ActivePictureSys(p_pic);
 
     picture_t *p_outpic = filter_NewPicture( p_filter );
-    if( !p_outpic || !p_outpic->p_sys->surface )
+    if( !p_outpic || !p_outpic->p_sys || !p_outpic->p_sys->surface )
         goto failed;
 
     picture_CopyProperties( p_outpic, p_pic );
