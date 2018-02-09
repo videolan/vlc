f41725e379f55156263c0d631cd739582efc8399
Thomas Guillem <thomas@gllm.fr>
2018-02-09 15:58:13 +0100

    videotoolbox: fix SEI header size
    This fixes interlaced false detection that triggered an interlacer that slowed
    down HEVC playback.
    
    Co-authored-by: Francois Cartegnie <fcvlcdev@free.fr>
    (cherry picked from commit a799daf628a31c69b4bb02624d67c32bf8127f97)
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- modules/codec/videotoolbox.m
+++ modules/codec/videotoolbox.m
@@ -301,7 +301,7 @@ static bool FillReorderInfoH264(decoder_t *p_dec, const block_t *p_block,
                 sei.i_pic_struct = UINT8_MAX;
 
                 for(size_t i=0; i<i_sei_count; i++)
-                    HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal, 1,
+                    HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal, 2,
                                  ParseH264SEI, &sei);
 
                 p_info->i_num_ts = h264_get_num_ts(p_sps, &slice, sei.i_pic_struct,
@@ -643,7 +643,7 @@ static bool FillReorderInfoHEVC(decoder_t *p_dec, const block_t *p_block,
 
                 for(size_t i=0; i<i_sei_count; i++)
                     HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal,
-                                 1, ParseHEVCSEI, &sei);
+                                 2, ParseHEVCSEI, &sei);
 
                 p_info->i_poc = POC;
                 p_info->i_foc = POC; /* clearly looks wrong :/ */
