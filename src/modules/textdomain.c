1b760f188173b3b6be95b95a3c8896d4c95ef2db
Rémi Denis-Courmont <remi@remlab.net>
2018-02-11 16:49:21 +0200

    Revert "textdomain: compute path at run-time (refs #1478)"
    This reverts commit 914fa9dd697d28caa01304b3477bfeb064cfaeb9.

--- src/modules/textdomain.c
+++ src/modules/textdomain.c
@@ -27,13 +27,24 @@
 
 #ifdef ENABLE_NLS
 # include <libintl.h>
-# include <vlc_charset.h>
+# if defined (__APPLE__) || defined (_WIN32) || defined(__OS2__)
+#  include <vlc_charset.h>
+# endif
 #endif
 
 int vlc_bindtextdomain (const char *domain)
 {
 #if defined (ENABLE_NLS)
     /* Specify where to find the locales for current domain */
+# if !defined (__APPLE__) && !defined (_WIN32) && !defined(__OS2__)
+    static const char path[] = LOCALEDIR;
+
+    if (bindtextdomain (domain, path) == NULL)
+    {
+        fprintf (stderr, "%s: text domain not found in %s\n", domain, path);
+        return -1;
+    }
+# else
     char *datadir = config_GetDataDir();
     if (unlikely(datadir == NULL))
         return -1;
@@ -54,6 +65,7 @@ int vlc_bindtextdomain (const char *domain)
     }
     LocaleFree(lpath);
     free (upath);
+# endif
 
     /* LibVLC wants all messages in UTF-8.
      * Unfortunately, we cannot ask UTF-8 for strerror_r(), strsignal_r()
