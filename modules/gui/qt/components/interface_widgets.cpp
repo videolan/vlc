videolan/vlc

commit 8784800a751548149d74707aa0be78b316b43c2b

Qt: fix syncing elapsed / remaining time in TimeLabel
Fix #8292

Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

modules/gui/qt/components/interface_widgets.cpp
@@ -797,7 +797,18 @@ TimeLabel::TimeLabel( intf_thread_t *_p_intf, TimeLabel::Display _displayType  )
     CONNECT( THEMIM->getIM(), positionUpdated( float, int64_t, int ),
               this, setDisplayPosition( float, int64_t, int ) );
 
+    connect( this, SIGNAL( broadcastRemainingTime( bool ) ),
+	     THEMIM->getIM(), SIGNAL( remainingTimeChanged( bool ) ) );
+
+    CONNECT( THEMIM->getIM(), remainingTimeChanged( bool ),
+              this, setRemainingTime( bool ) );
+
     setStyleSheet( "QLabel { padding-left: 4px; padding-right: 4px; }" );
+}
+
+void TimeLabel::setRemainingTime( bool remainingTime )
+{
+    b_remainingTime = remainingTime;
 }
 
 void TimeLabel::setDisplayPosition( float pos, int64_t t, int length )
@@ -890,5 +901,6 @@ void TimeLabel::toggleTimeDisplay()
 {
     b_remainingTime = !b_remainingTime;
     getSettings()->setValue( "MainWindow/ShowRemainingTime", b_remainingTime );
+    emit broadcastRemainingTime( b_remainingTime );
 }
 

modules/gui/qt/components/interface_widgets.hpp
@@ -209,8 +209,11 @@ private:
     char psz_time[MSTRTIME_MAX_SIZE];
     void toggleTimeDisplay();
 private slots:
+    void setRemainingTime( bool );
     void setDisplayPosition( float pos, int64_t time, int length );
     void setDisplayPosition( float pos );
+signals:
+    void broadcastRemainingTime( bool );
 };
 
 class SpeedLabel : public QLabel

modules/gui/qt/input_manager.hpp
@@ -212,6 +212,7 @@ private slots:
 signals:
     /// Send new position, new time and new length
     void positionUpdated( float , int64_t, int );
+    void remainingTimeChanged( bool );
     void seekRequested( float pos );
     void rateChanged( float );
     void nameChanged( const QString& );

commit da46603300f0d68c8a312added17c8928571e1ab

qt: Don't handle remaining time in Elapsed TimeLabel
Fix #19677
Regression introduced by 8784800a751548149d74707aa0be78b316b43c2b

modules/gui/qt/components/interface_widgets.cpp
@@ -943,7 +943,8 @@ TimeLabel::TimeLabel( intf_thread_t *_p_intf, TimeLabel::Display _displayType  )
 
 void TimeLabel::setRemainingTime( bool remainingTime )
 {
-    b_remainingTime = remainingTime;
+    if (displayType != TimeLabel::Elapsed)
+        b_remainingTime = remainingTime;
 }
 
 void TimeLabel::setDisplayPosition( float pos, int64_t t, int length )