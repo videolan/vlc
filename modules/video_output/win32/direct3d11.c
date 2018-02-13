a01d9fa288a7148b759bcf7cfd5c6084063f0338
Steve Lhomme <robux4@ycbcr.xyz>
2018-02-13 10:46:10 +0100

    direct3d11: adjust HLG luminance scaling

--- modules/video_output/win32/direct3d11.c
+++ modules/video_output/win32/direct3d11.c
@@ -1094,7 +1094,7 @@ static float GetFormatLuminance(vlc_object_t *o, const video_format_t *fmt)
             /* that's the default PQ value if the metadata are not set */
             return MAX_PQ_BRIGHTNESS;
         case TRANSFER_FUNC_HLG:
-            return 1000;
+            return 2000;
         case TRANSFER_FUNC_BT470_BG:
         case TRANSFER_FUNC_BT470_M:
         case TRANSFER_FUNC_BT709:
