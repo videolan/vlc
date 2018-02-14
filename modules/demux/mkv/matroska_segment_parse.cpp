afdf229fa903a4a3c4d0d67502c5efa1bdec237f
Steve Lhomme <robux4@ycbcr.xyz>
2018-02-14 13:21:02 +0100

    demux:mkv: don't read the first Cluster if we can't seek back

--- modules/demux/mkv/matroska_segment_parse.cpp
+++ modules/demux/mkv/matroska_segment_parse.cpp
@@ -1415,6 +1415,11 @@ bool matroska_segment_c::ParseCluster( KaxCluster *cluster, bool b_update_start_
         return false;
     }
 
+    bool b_seekable;
+    vlc_stream_Control( sys.demuxer.s, STREAM_CAN_SEEK, &b_seekable );
+    if (!b_seekable)
+        return false;
+
     try
     {
         EbmlElement *el;
