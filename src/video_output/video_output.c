c2f6cab0d7cebf56b80d87d2d342688fc5a9c2e0
Steve Lhomme <robux4@ycbcr.xyz>
2018-02-16 09:50:32 +0100

    video_output: a crop event is not an error
    Lower the log level
    
    (cherry picked from commit ce9cc6baf930b7037ae4ad98e3d858867d17d928)

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -1467,7 +1467,7 @@ static void ThreadExecuteCropBorder(vout_thread_t *vout,
                                     unsigned left, unsigned top,
                                     unsigned right, unsigned bottom)
 {
-    msg_Err(vout, "ThreadExecuteCropBorder %d.%d %dx%d", left, top, right, bottom);
+    msg_Dbg(vout, "ThreadExecuteCropBorder %d.%d %dx%d", left, top, right, bottom);
     vout_SetDisplayCrop(vout->p->display.vd, 0, 0,
                         left, top, -(int)right, -(int)bottom);
 }
