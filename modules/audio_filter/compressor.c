53e0ff270770c66822972cb9947e298daa075e14
Rémi Denis-Courmont <remi@remlab.net>
2018-02-16 11:00:27 +0200

    compressor: fix double translation (refs #19769)

--- modules/audio_filter/compressor.c
+++ modules/audio_filter/compressor.c
@@ -174,8 +174,8 @@ static int MakeupGainCallback   ( vlc_object_t *, char const *, vlc_value_t,
 #define MAKEUP_GAIN_LONGTEXT N_( "Set the makeup gain in dB (0 ... 24)." )
 
 vlc_module_begin()
-    set_shortname( _("Compressor") )
-    set_description( _("Dynamic range compressor") )
+    set_shortname( N_("Compressor") )
+    set_description( N_("Dynamic range compressor") )
     set_capability( "audio filter", 0 )
     set_category( CAT_AUDIO )
     set_subcategory( SUBCAT_AUDIO_AFILTER )
