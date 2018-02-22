78a54781dc0e9c48eef139c0cb96bd69c078004d
Francois Cartegnie <fcvlcdev@free.fr>
2018-02-22 15:16:37 +0100

    demux: ts: set dolbyvision to non default
    (cherry picked from commit d1ad873a5d46183ea31b7bd4e9f92d326b42a1fc)

--- modules/demux/mpeg/ts_psi.c
+++ modules/demux/mpeg/ts_psi.c
@@ -1578,6 +1578,12 @@ static void FillPESFromDvbpsiES( demux_t *p_demux,
         SetupAudioExtendedDescriptors( p_demux, p_pes->p_es, p_dvbpsies );
     }
 
+    /* Disable dolbyvision */
+    if ( registration_type == TS_PMT_REGISTRATION_BLURAY &&
+         p_dvbpsies->i_pid == 0x1015 &&
+         PMTEsHasRegistration( p_demux, p_dvbpsies, "HDMV" ) )
+        p_pes->p_es->fmt.i_priority = ES_PRIORITY_NOT_DEFAULTABLE;
+
     /* PES packets usually contain truncated frames */
     p_pes->p_es->fmt.b_packetized = false;
 
