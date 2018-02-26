6f14081af7325d334a53126c4eea52bc30fc08a0
Rémi Denis-Courmont <remi@remlab.net>
2018-02-26 20:43:03 +0200

    dynamicoverlay: fix memory corruption
    Font alpha is 8-bits, not 32-bits.

--- modules/spu/dynamicoverlay/dynamicoverlay_commands.c
+++ modules/spu/dynamicoverlay/dynamicoverlay_commands.c
@@ -238,8 +238,12 @@ static int parser_SetTextAlpha( char *psz_command, char *psz_end,
     skip_space( &psz_command );
     if( isdigit( (unsigned char)*psz_command ) )
     {
-        if( parse_digit( &psz_command, &p_params->fontstyle.i_font_alpha ) == VLC_EGENERIC )
+        int32_t value;
+
+        if( parse_digit( &psz_command, &value ) == VLC_EGENERIC )
             return VLC_EGENERIC;
+
+        p_params->fontstyle.i_font_alpha = value;
     }
     return VLC_SUCCESS;
 }
