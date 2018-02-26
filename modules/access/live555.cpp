7302fe9d256b2bac816319b718974ae990e03b20
Rémi Denis-Courmont <remi@remlab.net>
2018-02-26 22:35:53 +0200

    access_demux: increase priority by 300
    For the time being, access_demux modules should have a higher priority
    than access modules, so that order is maintained if/when the two
    capabilities are merged. The highest access module priority is below
    300 as of now.

--- modules/access/live555.cpp
+++ modules/access/live555.cpp
@@ -104,7 +104,7 @@ vlc_module_begin ()
     add_submodule ()
         set_description( N_("RTSP/RTP access and demux") )
         add_shortcut( "rtsp", "pnm", "live", "livedotcom" )
-        set_capability( "access_demux", 0 )
+        set_capability( "access_demux", 300 )
         set_callbacks( Open, Close )
         add_bool( "rtsp-tcp", false,
                   N_("Use RTP over RTSP (TCP)"),
