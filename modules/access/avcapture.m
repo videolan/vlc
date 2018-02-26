9c23f5b5189992242dda6beaea9323cdb1ad2792
Rémi Denis-Courmont <remi@remlab.net>
2018-02-26 22:19:44 +0200

    avcapture: drop priority to zero
    This plugin does not need priority since no other plugins have any
    alias in common.

--- modules/access/avcapture.m
+++ modules/access/avcapture.m
@@ -59,7 +59,7 @@ vlc_module_begin ()
    set_category(CAT_INPUT)
    set_subcategory(SUBCAT_INPUT_ACCESS)
    add_shortcut("avcapture")
-   set_capability("access_demux", 10)
+   set_capability("access_demux", 0)
    set_callbacks(Open, Close)
 vlc_module_end ()
 
