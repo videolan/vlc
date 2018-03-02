videolan/vlc

commit a799daf628a31c69b4bb02624d67c32bf8127f97

videotoolbox: fix SEI header size
This fixes interlaced false detection that triggered an interlacer that slowed
down HEVC playback.

Co-authored-by: Francois Cartegnie <fcvlcdev@free.fr>

modules/codec/videotoolbox.m
@@ -301,7 +301,7 @@ static bool FillReorderInfoH264(decoder_t *p_dec, const block_t *p_block,
                 sei.i_pic_struct = UINT8_MAX;
 
                 for(size_t i=0; i<i_sei_count; i++)
-                    HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal, 1,
+                    HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal, 2,
                                  ParseH264SEI, &sei);
 
                 p_info->i_num_ts = h264_get_num_ts(p_sps, &slice, sei.i_pic_struct,
@@ -643,7 +643,7 @@ static bool FillReorderInfoHEVC(decoder_t *p_dec, const block_t *p_block,
 
                 for(size_t i=0; i<i_sei_count; i++)
                     HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal,
-                                 1, ParseHEVCSEI, &sei);
+                                 2, ParseHEVCSEI, &sei);
 
                 p_info->i_poc = POC;
                 p_info->i_foc = POC; /* clearly looks wrong :/ */

commit 61ed111c17912a1d8ec11532895eaf442bc61cf8

videotoolbox: fix SEI header size for H264
Regression from a799daf628a31c69b4bb02624d67c32bf8127f97 (should had been done
only for HEVC)

This caused a high value of i_nb_fields and a deadlock (cf. previvous commit).

Fixes #19897

modules/codec/videotoolbox.m
@@ -315,7 +315,7 @@ static bool FillReorderInfoH264(decoder_t *p_dec, const block_t *p_block,
                 sei.i_pic_struct = UINT8_MAX;
 
                 for(size_t i=0; i<i_sei_count; i++)
-                    HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal, 2,
+                    HxxxParseSEI(sei_array[i].p_nal, sei_array[i].i_nal, 1,
                                  ParseH264SEI, &sei);
 
                 p_info->i_num_ts = h264_get_num_ts(p_sps, &slice, sei.i_pic_struct,