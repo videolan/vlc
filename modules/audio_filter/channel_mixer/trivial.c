videolan/vlc

commit 91f426996c98642b5180bb7375b188c93b2701ea

es: replace i_original_channels with i_chan_mode
Rename i_original_channels to i_chan_mode. For now, chan modes can be
DOLBYSTEREO or DUALMONO.

This new value, i_chan_mode, should only be set by demuxes/codecs/packetizers
if there is a special stereo mode to handle.

modules/audio_filter/channel_mixer/trivial.c
@@ -193,8 +193,7 @@ static int Create( vlc_object_t *p_this )
      || infmt->i_rate != outfmt->i_rate
      || infmt->i_format != VLC_CODEC_FL32 )
         return VLC_EGENERIC;
-    if( infmt->i_physical_channels == outfmt->i_physical_channels
-     && infmt->i_original_channels == outfmt->i_original_channels )
+    if( infmt->i_physical_channels == outfmt->i_physical_channels )
         return VLC_EGENERIC;
 
     p_filter->p_sys = NULL;

commit 31e11f5bedef66b70b7853d3d11cebf5a2d235cc

trivial: accept different chan_modes
This fixes a regression from 91f426996c98642b5180bb7375b188c93b2701ea

This regressions caused the audio converter chain to fail when a chan_mode
conversion was needed but no converter were found.

In the case of AOUT_CHANMODE_DOLBYSTEREO. The dolby converter won't work with a
stereo output. The lowest priority converter still need to handle this
conversion and drop the chan_mode conversion.

Fixes #19646

modules/audio_filter/channel_mixer/trivial.c
@@ -193,7 +193,12 @@ static int Create( vlc_object_t *p_this )
      || infmt->i_rate != outfmt->i_rate
      || infmt->i_format != VLC_CODEC_FL32 )
         return VLC_EGENERIC;
-    if( infmt->i_physical_channels == outfmt->i_physical_channels )
+
+    /* trivial is the lowest priority converter: if chan_mode are different
+     * here, this filter will still need to convert channels (and ignore
+     * chan_mode). */
+    if( infmt->i_physical_channels == outfmt->i_physical_channels
+     && infmt->i_chan_mode == outfmt->i_chan_mode )
         return VLC_EGENERIC;
 
     p_filter->p_sys = NULL;