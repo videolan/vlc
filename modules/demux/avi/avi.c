7f2e931af54dc98527b5919808517199d9dfdee1
Francois Cartegnie <fcvlcdev@free.fr>
2018-03-06 14:28:05 +0100

    demux: avi: reject 0 scaled tracks
    (cherry picked from commit b2d52c22b9018d361c43b24f4edcd307c611af55)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/demux/avi/avi.c
+++ modules/demux/avi/avi.c
@@ -466,7 +466,7 @@ static int Open( vlc_object_t * p_this )
         tk->i_samplesize = p_strh->i_samplesize;
         msg_Dbg( p_demux, "stream[%u] rate:%u scale:%u samplesize:%u",
                 i, tk->i_rate, tk->i_scale, tk->i_samplesize );
-        if( tk->i_scale > tk->i_rate )
+        if( tk->i_scale > tk->i_rate || !tk->i_scale || !tk->i_rate )
         {
             free( tk );
             continue;
