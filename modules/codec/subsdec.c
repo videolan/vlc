3344e511388ecdf05f1085cd2c356d88f0d756bf
Francois Cartegnie <fcvlcdev@free.fr>
2018-03-13 00:11:24 +0100

    codec: subsdec: fix ssa forced alignment
    (cherry picked from commit 4efe1ff533b5514fa86f09fc2867e830603f2ea5)

--- modules/codec/subsdec.c
+++ modules/codec/subsdec.c
@@ -457,8 +457,8 @@ static subpicture_t *ParseText( decoder_t *p_dec, block_t *p_block )
     subpicture_updater_sys_t *p_spu_sys = p_spu->updater.p_sys;
 
     p_spu_sys->region.align = SUBPICTURE_ALIGN_BOTTOM | p_sys->i_align;
-    p_spu_sys->region.inner_align = p_sys->i_align;
-    p_spu_sys->region.p_segments = ParseSubtitles( &p_spu_sys->region.inner_align, psz_subtitle );
+    p_spu_sys->region.inner_align = SUBPICTURE_ALIGN_BOTTOM;
+    p_spu_sys->region.p_segments = ParseSubtitles( &p_spu_sys->region.align, psz_subtitle );
 
     free( psz_subtitle );
 
