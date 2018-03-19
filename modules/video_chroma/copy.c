videolan/vlc

commit 10de9cab52679326c1c05244bdc04b529e392c43

chroma: copy: add CopyPacked()
In order to use Uswc when copying packer pictures (RGB/YUYV).

modules/video_chroma/copy.c
@@ -637,6 +637,21 @@ static void CopyPlane(uint8_t *dst, size_t dst_pitch,
     }
 }
 
+void CopyPacked(picture_t *dst, const uint8_t *src, const size_t src_pitch,
+                unsigned height, const copy_cache_t *cache)
+{
+    assert(dst);
+    assert(src); assert(src_pitch);
+    assert(height);
+
+    if (vlc_CPU_SSE4_1())
+        SSE_CopyPlane(dst->p[0].p_pixels, dst->p[0].i_pitch, src, src_pitch,
+                      cache->buffer, cache->size, height, 0);
+    else
+        CopyPlane(dst->p[0].p_pixels, dst->p[0].i_pitch, src, src_pitch,
+                  height, 0);
+}
+
 void Copy420_SP_to_SP(picture_t *dst, const uint8_t *src[static 2],
                       const size_t src_pitch[static 2], unsigned height,
                       const copy_cache_t *cache)

modules/video_chroma/copy.h
@@ -36,6 +36,11 @@ typedef struct {
 int  CopyInitCache(copy_cache_t *cache, unsigned width);
 void CopyCleanCache(copy_cache_t *cache);
 
+/* YUVY/RGB copies */
+void CopyPacked(picture_t *dst, const uint8_t *src,
+                const size_t src_pitch, unsigned height,
+                const copy_cache_t *cache);
+
 /* Copy planes from NV12/NV21 to NV12/NV21 */
 void Copy420_SP_to_SP(picture_t *dst, const uint8_t *src[static 2],
                       const size_t src_pitch[static 2], unsigned height,

commit b118374094a16a7fbfdca8e11ed7aefa86b2bc38

chroma: copy: Fix compilation for other architectures than x86
This fixes build breakage since 10de9cab52679326c1c05244bdc04b529e392c43.

modules/video_chroma/copy.c
@@ -644,10 +644,13 @@ void CopyPacked(picture_t *dst, const uint8_t *src, const size_t src_pitch,
     assert(src); assert(src_pitch);
     assert(height);
 
+#ifdef CAN_COMPILE_SSE2
     if (vlc_CPU_SSE4_1())
-        SSE_CopyPlane(dst->p[0].p_pixels, dst->p[0].i_pitch, src, src_pitch,
-                      cache->buffer, cache->size, height, 0);
-    else
+        return SSE_CopyPlane(dst->p[0].p_pixels, dst->p[0].i_pitch, src, src_pitch,
+                             cache->buffer, cache->size, height, 0);
+#else
+    (void) cache;
+#endif
         CopyPlane(dst->p[0].p_pixels, dst->p[0].i_pitch, src, src_pitch,
                   height, 0);
 }