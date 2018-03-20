a32e93567b6d5d284a0f2d326f9f446f5b1c5cae
Thomas Guillem <thomas@gllm.fr>
2018-03-20 16:04:20 +0100

    qt: fix wrong audio settings with automatic device
    Fetch the actual audio module instead of relying on the config.

--- modules/gui/qt/components/simple_preferences.hpp
+++ modules/gui/qt/components/simple_preferences.hpp
@@ -113,6 +113,7 @@ private:
     bool addType( const char * psz_ext, QTreeWidgetItem*, QTreeWidgetItem*, QVLCRegistry* );
     void saveLang();
 #endif
+    char *getAoutModuleName();
 
 /* Display only the options for the selected audio output */
 private slots:
