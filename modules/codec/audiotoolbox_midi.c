e084f90a5a72a9079980c8cc7fe4ac9a901cfc51
Marvin Scholz <epirat07@gmail.com>
2018-03-28 14:16:47 +0200

    codec/audiotoolbox_midi: Add accidentally removed prefix
    This would cause the audiotoolbox_midi module to crash as the option
    it later looks for (with prefix) is non-existent.

--- modules/codec/audiotoolbox_midi.c
+++ modules/codec/audiotoolbox_midi.c
@@ -60,7 +60,7 @@ vlc_module_begin()
     set_category(CAT_INPUT)
     set_subcategory(SUBCAT_INPUT_ACODEC)
     set_callbacks(Open, Close)
-    add_loadfile("soundfont", "",
+    add_loadfile(CFG_PREFIX "soundfont", "",
                  SOUNDFONT_TEXT, SOUNDFONT_LONGTEXT, false)
 vlc_module_end()
 
