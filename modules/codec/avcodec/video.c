videolan/vlc

commit dfb767189073493336df0cf43f7d0b6bf51f63e3

avcodec: drop frames from old vouts
This fixes a crash in indirect rendering when the decoder requested a lower
video resultion.

modules/codec/avcodec/video.c
@@ -342,12 +342,19 @@ static int lavc_CopyPicture(decoder_t *dec, picture_t *pic, AVFrame *frame)
 {
     decoder_sys_t *sys = dec->p_sys;
 
-    if (!FindVlcChroma(sys->p_context->pix_fmt))
+    vlc_fourcc_t fourcc = FindVlcChroma(frame->format);
+    if (!fourcc)
     {
-        const char *name = av_get_pix_fmt_name(sys->p_context->pix_fmt);
+        const char *name = av_get_pix_fmt_name(frame->format);
 
         msg_Err(dec, "Unsupported decoded output format %d (%s)",
                 sys->p_context->pix_fmt, (name != NULL) ? name : "unknown");
+        return VLC_EGENERIC;
+    } else if (fourcc != pic->format.i_chroma
+     || frame->width != (int) pic->format.i_visible_width
+     || frame->height != (int) pic->format.i_visible_height)
+    {
+        msg_Warn(dec, "dropping frame because the vout changed");
         return VLC_EGENERIC;
     }
 
@@ -1149,7 +1156,6 @@ static picture_t *DecodeBlock( decoder_t *p_dec, block_t **pp_block, bool *error
             /* Fill picture_t from AVFrame */
             if( lavc_CopyPicture( p_dec, p_pic, frame ) != VLC_SUCCESS )
             {
-                *error = true;
                 av_frame_free(&frame);
                 picture_Release( p_pic );
                 break;

commit c988b8d58b01ef6d628e3051774a2032dd7f6b7d

avcodec: fix black screen with hdtv-fixed videos
In case of indirect rendering.

This fixes a regression from dfb767189073493336df0cf43f7d0b6bf51f63e3

Fixes #20175

modules/codec/avcodec/video.c
@@ -365,8 +365,8 @@ static int lavc_CopyPicture(decoder_t *dec, picture_t *pic, AVFrame *frame)
                 sys->p_context->pix_fmt, (name != NULL) ? name : "unknown");
         return VLC_EGENERIC;
     } else if (fourcc != pic->format.i_chroma
-     || frame->width != (int) pic->format.i_visible_width
-     || frame->height != (int) pic->format.i_visible_height)
+     || frame->width > (int) pic->format.i_width
+     || frame->height > (int) pic->format.i_height)
     {
         msg_Warn(dec, "dropping frame because the vout changed");
         return VLC_EGENERIC;