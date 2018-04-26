videolan/vlc

commit b3b7f62ce15a3faae7a7c78c2d10b4bb49a6a009

core: replace decoder_sys_t* by void*
See #17078

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/codec/png.c
@@ -116,11 +116,12 @@ static int OpenDecoder( vlc_object_t *p_this )
     }
 
     /* Allocate the memory needed to store the decoder's structure */
-    p_dec->p_sys = malloc( sizeof(decoder_sys_t) );
+    decoder_sys_t *p_sys = malloc( sizeof(decoder_sys_t) );
     if( p_dec->p_sys == NULL )
         return VLC_ENOMEM;
+    p_dec->p_sys = p_sys;
 
-    p_dec->p_sys->p_obj = p_this;
+    p_sys->p_obj = p_this;
 
     /* Set output properties */
     p_dec->fmt_out.i_codec = VLC_CODEC_RGBA;

include/vlc_codec.h
@@ -56,7 +56,7 @@ struct decoder_t
 
     /* Module properties */
     module_t *          p_module;
-    decoder_sys_t *     p_sys;
+    void               *p_sys;
 
     /* Input format ie from demuxer (XXX: a lot of field could be invalid) */
     es_format_t         fmt_in;

modules/codec/aom.c
@@ -149,7 +149,8 @@ static vlc_fourcc_t FindVlcChroma( struct aom_image *img )
  ****************************************************************************/
 static int Decode(decoder_t *dec, block_t *block)
 {
-    aom_codec_ctx_t *ctx = &dec->p_sys->ctx;
+    decoder_sys_t *p_sys = dec->p_sys;
+    aom_codec_ctx_t *ctx = &p_sys->ctx;
 
     if (!block) /* No Drain */
         return VLCDEC_SUCCESS;

modules/codec/avcodec/subtitle.c
@@ -317,12 +317,14 @@ static subpicture_t *ConvertSubtitle(decoder_t *dec, AVSubtitle *ffsub, mtime_t 
     if (!spu)
         return NULL;
 
+    decoder_sys_t *p_sys = dec->p_sys;
+
     //msg_Err(dec, "%lld %d %d",
     //        pts, ffsub->start_display_time, ffsub->end_display_time);
     spu->i_start    = pts + ffsub->start_display_time * INT64_C(1000);
     spu->i_stop     = pts + ffsub->end_display_time * INT64_C(1000);
     spu->b_absolute = true; /* We have offset and size for subtitle */
-    spu->b_ephemer  = dec->p_sys->b_need_ephemer;
+    spu->b_ephemer  = p_sys->b_need_ephemer;
                     /* We only show subtitle for i_stop time only */
 
     if (avctx->coded_width != 0 && avctx->coded_height != 0) {

modules/codec/avcodec/video.c
@@ -337,15 +337,17 @@ static int lavc_UpdateVideoFormat(decoder_t *dec, AVCodecContext *ctx,
     if (val)
         return val;
 
+    decoder_sys_t *p_sys = dec->p_sys;
+
     /* always have date in fields/ticks units */
-    if(dec->p_sys->pts.i_divider_num)
-        date_Change(&dec->p_sys->pts, fmt_out.i_frame_rate *
-                                      __MAX(ctx->ticks_per_frame, 1),
-                                      fmt_out.i_frame_rate_base);
+    if(p_sys->pts.i_divider_num)
+        date_Change(&p_sys->pts, fmt_out.i_frame_rate *
+                                 __MAX(ctx->ticks_per_frame, 1),
+                                 fmt_out.i_frame_rate_base);
     else
-        date_Init(&dec->p_sys->pts, fmt_out.i_frame_rate *
-                                    __MAX(ctx->ticks_per_frame, 1),
-                                    fmt_out.i_frame_rate_base);
+        date_Init(&p_sys->pts, fmt_out.i_frame_rate *
+                               __MAX(ctx->ticks_per_frame, 1),
+                               fmt_out.i_frame_rate_base);
 
     fmt_out.p_palette = dec->fmt_out.video.p_palette;
     dec->fmt_out.video.p_palette = NULL;
@@ -1373,7 +1375,8 @@ static int lavc_va_GetFrame(struct AVCodecContext *ctx, AVFrame *frame,
                             picture_t *pic)
 {
     decoder_t *dec = ctx->opaque;
-    vlc_va_t *va = dec->p_sys->p_va;
+    decoder_sys_t *p_sys = dec->p_sys;
+    vlc_va_t *va = p_sys->p_va;
 
     if (vlc_va_Get(va, pic, &frame->data[0]))
     {

modules/codec/cc.c
@@ -502,6 +502,7 @@ static subpicture_t *Subtitle( decoder_t *p_dec, eia608_t *h, mtime_t i_pts )
     p_spu->b_absolute = false;
 
     subpicture_updater_sys_t *p_spu_sys = p_spu->updater.p_sys;
+    decoder_sys_t *p_dec_sys = p_dec->p_sys;
 
     /* Set first region defaults */
     /* The "leavetext" alignment is a special mode where the subpicture
@@ -512,7 +513,7 @@ static subpicture_t *Subtitle( decoder_t *p_dec, eia608_t *h, mtime_t i_pts )
 
     /* Set style defaults (will be added to segments if none set) */
     p_spu_sys->p_default_style->i_style_flags |= STYLE_MONOSPACED;
-    if( p_dec->p_sys->b_opaque )
+    if( p_dec_sys->b_opaque )
     {
         p_spu_sys->p_default_style->i_background_alpha = STYLE_ALPHA_OPAQUE >> 1;
         p_spu_sys->p_default_style->i_features |= STYLE_HAS_BACKGROUND_ALPHA;

modules/codec/crystalhd.c
@@ -183,8 +183,8 @@ static int OpenDecoder( vlc_object_t *p_this )
     p_sys->i_nal_size       = 4; // assume 4 byte start codes
     p_sys->i_sps_pps_size   = 0;
     p_sys->p_sps_pps_buf    = NULL;
-    p_dec->p_sys->p_pic     = NULL;
-    p_dec->p_sys->proc_out  = NULL;
+    p_sys->p_pic            = NULL;
+    p_sys->proc_out         = NULL;
 
     /* Win32 code *
      * We cannot link and ship BCM dll, even with LGPL license (too big)
@@ -392,7 +392,8 @@ static BC_STATUS ourCallback(void *shnd, uint32_t width, uint32_t height, uint32
     VLC_UNUSED(width); VLC_UNUSED(height); VLC_UNUSED(stride);
 
     decoder_t *p_dec          = (decoder_t *)shnd;
-    BC_DTS_PROC_OUT *proc_out = p_dec->p_sys->proc_out;
+    decoder_sys_t *p_sys      = p_dec->p_sys;
+    BC_DTS_PROC_OUT *proc_out = p_sys->proc_out;
     BC_DTS_PROC_OUT *proc_in  = (BC_DTS_PROC_OUT*)pOut;
 
     /* Direct Rendering */
@@ -401,11 +402,11 @@ static BC_STATUS ourCallback(void *shnd, uint32_t width, uint32_t height, uint32
         !(proc_in->PicInfo.flags & VDEC_FLAG_FIELDPAIR) )
     {
         if( !decoder_UpdateVideoFormat( p_dec ) )
-            p_dec->p_sys->p_pic = decoder_NewPicture( p_dec );
+            p_sys->p_pic = decoder_NewPicture( p_dec );
     }
 
     /* */
-    picture_t *p_pic = p_dec->p_sys->p_pic;
+    picture_t *p_pic = p_sys->p_pic;
     if( !p_pic )
         return BC_STS_ERROR;
 

modules/codec/cvdsub.c
@@ -139,8 +139,10 @@ static int PacketizerOpen( vlc_object_t *p_this )
 
     if( DecoderOpen( p_this ) != VLC_SUCCESS ) return VLC_EGENERIC;
 
+    decoder_sys_t *p_sys = p_dec->p_sys;
+
     p_dec->fmt_out.i_codec = VLC_CODEC_CVD;
-    p_dec->p_sys->b_packetizer = true;
+    p_sys->b_packetizer = true;
 
     return VLC_SUCCESS;
 }

commit 0c284ce20bac1dd7da23ebee03748235739f40ca

png: decoder: fix leak (CID 1435160)
Regression from b3b7f62ce15a3faae7a7c78c2d10b4bb49a6a009

modules/codec/png.c
@@ -117,7 +117,7 @@ static int OpenDecoder( vlc_object_t *p_this )
 
     /* Allocate the memory needed to store the decoder's structure */
     decoder_sys_t *p_sys = malloc( sizeof(decoder_sys_t) );
-    if( p_dec->p_sys == NULL )
+    if( p_sys == NULL )
         return VLC_ENOMEM;
     p_dec->p_sys = p_sys;
 