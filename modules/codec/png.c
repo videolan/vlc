videolan/vlc

commit 7442c8c791ec6be8c50ba7768792b52a01ee2b31

core: replace encoder_sys_t* by void*
See #17078

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/codec/png.c
@@ -337,13 +337,15 @@ static int OpenEncoder(vlc_object_t *p_this)
         return VLC_EGENERIC;
 
     /* Allocate the memory needed to store the encoder's structure */
+    encoder_sys_t *p_sys = malloc( sizeof(encoder_sys_t) );
     p_enc->p_sys = malloc( sizeof(encoder_sys_t) );
     if( p_enc->p_sys  == NULL )
         return VLC_ENOMEM;
+    p_enc->p_sys = p_sys;
 
-    p_enc->p_sys->p_obj = p_this;
+    p_sys->p_obj = p_this;
 
-    p_enc->p_sys->i_blocksize = 3 * p_enc->fmt_in.video.i_visible_width *
+    p_sys->i_blocksize = 3 * p_enc->fmt_in.video.i_visible_width *
         p_enc->fmt_in.video.i_visible_height;
 
     p_enc->fmt_in.i_codec = VLC_CODEC_RGB24;

include/vlc_codec.h
@@ -213,7 +213,7 @@ struct encoder_t
 
     /* Module properties */
     module_t *          p_module;
-    encoder_sys_t *     p_sys;
+    void               *p_sys;
 
     /* Properties of the input data fed to the encoder */
     es_format_t         fmt_in;

modules/codec/opus.c
@@ -684,7 +684,7 @@ static int OpenEncoder(vlc_object_t *p_this)
     sys->i_nb_samples = 0;
 
     sys->i_samples_delay = 0;
-    int ret = opus_multistream_encoder_ctl(enc->p_sys->enc,
+    int ret = opus_multistream_encoder_ctl(sys->enc,
             OPUS_GET_LOOKAHEAD(&sys->i_samples_delay));
     if (ret != OPUS_OK)
         msg_Err(enc, "Unable to get number of lookahead samples: %s\n",

commit 17494bc4f43af5d5b1eca9cdc4e64824088e404b

png: encoder: fix leak (CID 1435159)
Regression from 7442c8c791ec6be8c50ba7768792b52a01ee2b31

modules/codec/png.c
@@ -338,8 +338,7 @@ static int OpenEncoder(vlc_object_t *p_this)
 
     /* Allocate the memory needed to store the encoder's structure */
     encoder_sys_t *p_sys = malloc( sizeof(encoder_sys_t) );
-    p_enc->p_sys = malloc( sizeof(encoder_sys_t) );
-    if( p_enc->p_sys  == NULL )
+    if( p_sys == NULL )
         return VLC_ENOMEM;
     p_enc->p_sys = p_sys;
 