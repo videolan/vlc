huzaifa-d/VLC-Stereoscopic-3D-Playback

avcodec: add frame sequential handling

modules/codec/avcodec/video.c
@@ -876,6 +876,11 @@ static void DecodeSidedata( decoder_t *p_dec, const AVFrame *frame, picture_t *p
             p_pic->format.multiview_mode = MULTIVIEW_2D;
             break;
         }
+        p_pic->format.b_multiview_right_eye_first = stereo_data->flags & AV_STEREO3D_FLAG_INVERT;
+        p_pic->format.b_multiview_is_frame0 = stereo_data->flags & AV_STEREO3D_FLAG_FRAME0;
+
+        p_dec->fmt_out.video.b_multiview_right_eye_first = p_pic->format.b_multiview_right_eye_first;
+
         if (p_dec->fmt_out.video.multiview_mode != p_pic->format.multiview_mode)
         {
             p_dec->fmt_out.video.multiview_mode = p_pic->format.multiview_mode;

videolan/vlc

Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

modules/codec/avcodec/video.c
@@ -901,6 +901,11 @@ static int DecodeSidedata( decoder_t *p_dec, const AVFrame *frame, picture_t *p_
             p_pic->format.multiview_mode = MULTIVIEW_2D;
             break;
         }
+        p_pic->format.b_multiview_right_eye_first = stereo_data->flags & AV_STEREO3D_FLAG_INVERT;
+        p_pic->format.b_multiview_left_eye = stereo_data->flags & AV_STEREO3D_VIEW_LEFT;
+
+        p_dec->fmt_out.video.b_multiview_right_eye_first = p_pic->format.b_multiview_right_eye_first;
+
         if (p_dec->fmt_out.video.multiview_mode != p_pic->format.multiview_mode)
         {
             p_dec->fmt_out.video.multiview_mode = p_pic->format.multiview_mode;