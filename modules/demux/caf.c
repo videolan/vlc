9c093e9390c02cfb0e17bcde6cbaeee1096c6781
Rémi Denis-Courmont <remi@remlab.net>
2018-05-03 19:29:13 +0300

    caf: remove redundant es_out_Del()

--- modules/demux/caf.c
+++ modules/demux/caf.c
@@ -1087,8 +1087,6 @@ static void Close( vlc_object_t *p_this )
     demux_t     *p_demux = (demux_t*)p_this;
     demux_sys_t *p_sys = p_demux->p_sys;
 
-    es_out_Del( p_demux->out, p_sys->es );
-
     free( p_sys->fmt.p_extra );
     free( p_sys );
 }
