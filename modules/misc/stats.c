e57aaa698595e36b94e93852ce454aaf9e04675a
Rémi Denis-Courmont <remi@remlab.net>
2018-05-04 13:25:34 +0300

    stats: use VLC_TS_0 explicitly

--- modules/misc/stats.c
+++ modules/misc/stats.c
@@ -150,7 +150,7 @@ static int Demux( demux_t *p_demux )
     if( !p_block ) return 1;
 
     p_block->i_dts = p_block->i_pts =
-        date_Increment( &p_sys->pts, kBufferSize );
+        VLC_TS_0 + date_Increment( &p_sys->pts, kBufferSize );
 
     msg_Dbg( p_demux, "demux got %"PRId64" ms offset",
              (mdate() - *(mtime_t *)p_block->p_buffer) / 1000 );
@@ -190,7 +190,6 @@ static int OpenDemux ( vlc_object_t *p_this )
         return VLC_ENOMEM;
 
     date_Init( &p_sys->pts, 1, 1 );
-    date_Set( &p_sys->pts, 1 );
 
     es_format_Init( &p_sys->fmt, VIDEO_ES, VLC_FOURCC('s','t','a','t') );
     p_sys->fmt.video.i_width = 720;
