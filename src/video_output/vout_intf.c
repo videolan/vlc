cca471def7d197c107c025c664bd0299ad0b5abd
Steve Lhomme <robux4@ycbcr.xyz>
2018-05-05 10:42:07 +0200

    vout_intf: make the snapshot PIP duration explicit to 4s using CLOCK_FREQ
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- src/video_output/vout_intf.c
+++ src/video_output/vout_intf.c
@@ -344,7 +344,7 @@ static int VoutSnapshotPip( vout_thread_t *p_vout, picture_t *p_pic )
      * hardcoded 0 doesn't seem right */
     p_subpic->i_channel = 0;
     p_subpic->i_start = mdate();
-    p_subpic->i_stop  = p_subpic->i_start + 4000000;
+    p_subpic->i_stop  = p_subpic->i_start + CLOCK_FREQ*4;
     p_subpic->b_ephemer = true;
     p_subpic->b_fade = true;
 
