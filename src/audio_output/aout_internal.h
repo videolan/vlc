839ecf84ab65fc3c53bd595bb38a77d212dbdff2
Steve Lhomme <robux4@ycbcr.xyz>
2018-05-05 09:40:57 +0200

    aout: resamp_start_drift is an mtime_t value
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- src/audio_output/aout_internal.h
+++ src/audio_output/aout_internal.h
@@ -72,7 +72,7 @@ typedef struct
     {
         mtime_t end; /**< Last seen PTS */
         float rate; /**< Play-out speed rate */
-        unsigned resamp_start_drift; /**< Resampler drift absolute value */
+        mtime_t resamp_start_drift; /**< Resampler drift absolute value */
         int resamp_type; /**< Resampler mode (FIXME: redundant / resampling) */
         bool discontinuity;
     } sync;
