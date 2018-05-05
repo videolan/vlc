b66a03b162dd67a2ae841cb315a3a5e874507d1c
Steve Lhomme <robux4@ycbcr.xyz>
2018-05-05 09:53:49 +0200

    es_out: no need to init a value that won't be used
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- src/input/es_out.c
+++ src/input/es_out.c
@@ -782,7 +782,7 @@ static void EsOutDecoderChangeDelay( es_out_t *out, es_out_id_t *p_es )
 {
     es_out_sys_t *p_sys = out->p_sys;
 
-    mtime_t i_delay = 0;
+    mtime_t i_delay;
     if( p_es->fmt.i_cat == AUDIO_ES )
         i_delay = p_sys->i_audio_delay;
     else if( p_es->fmt.i_cat == SPU_ES )
