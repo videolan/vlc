f47609a6008ae8598905c5a8db51994bcc677ccb
Rémi Denis-Courmont <remi@remlab.net>
2018-05-05 10:58:34 +0300

    amem: fix inverted logic
    (cherry picked from commit b7d6ed35a84f6c534cb14c9168c1ee2956eb923c)
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- modules/audio_output/amem.c
+++ modules/audio_output/amem.c
@@ -112,7 +112,7 @@ static int VolumeSet (audio_output_t *aout, float vol)
     aout_sys_t *sys = aout->sys;
 
     sys->volume = vol;
-    if (sys->ready)
+    if (!sys->ready)
         return 0; /* sys->opaque is not yet defined... */
     return sys->set_volume (sys->opaque, vol, sys->mute) ? -1 : 0;
 }
