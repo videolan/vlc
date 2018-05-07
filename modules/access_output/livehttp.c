f5de48d9771826b52cf29fa2cffc340da16a44e6
Steve Lhomme <robux4@ycbcr.xyz>
2018-05-07 16:49:02 +0200

    livehttp: remove write-only variable

--- modules/access_output/livehttp.c
+++ modules/access_output/livehttp.c
@@ -181,7 +181,6 @@ typedef struct
     char *psz_keyfile;
     vlc_tick_t i_keyfile_modification;
     vlc_tick_t i_opendts;
-    vlc_tick_t i_dts_offset;
     vlc_tick_t  i_seglenm;
     uint32_t i_segment;
     size_t  i_seglen;
@@ -255,7 +254,6 @@ static int Open( vlc_object_t *p_this )
 
     p_sys->stuffing_size = 0;
     p_sys->i_opendts = VLC_TICK_INVALID;
-    p_sys->i_dts_offset  = 0;
 
     p_sys->psz_indexPath = NULL;
     psz_idx = var_GetNonEmptyString( p_access, SOUT_CFG_PREFIX "index" );
