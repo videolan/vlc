9143eafc854512d8a19e3fa789fb94726ad8689c
Steve Lhomme <robux4@ycbcr.xyz>
2018-05-09 16:06:22 +0200

    stl: check the length is not VLC_TICK_INVALID explicitly

--- modules/codec/stl.c
+++ modules/codec/stl.c
@@ -422,7 +422,7 @@ static int Decode(decoder_t *p_dec, block_t *p_block)
                 {
                     p_sub->i_start    = p_block->i_pts;
                     p_sub->i_stop     = p_block->i_pts + p_block->i_length;
-                    p_sub->b_ephemer  = (p_block->i_length == 0);
+                    p_sub->b_ephemer  = (p_block->i_length == VLC_TICK_INVALID);
                 }
                 decoder_QueueSub(p_dec, p_sub);
             }
