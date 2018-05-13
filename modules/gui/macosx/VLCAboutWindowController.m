46a30d8ab38cc930157ca8a26777750396963dfc
David Fuhrmann <dfuhrmann@videolan.org>
2018-05-13 22:38:16 +0200

    macosx: use https URLs in about dialog
    (cherry picked from commit 099a09dc2e2a340a6709e22eff3bce807181cf31)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/gui/macosx/VLCAboutWindowController.m
+++ modules/gui/macosx/VLCAboutWindowController.m
@@ -133,12 +133,12 @@
     /* setup join us! */
     NSString *joinus = toNSStr(_(""
                                  "<p>VLC media player is a free and open source media player, encoder, and "
-                                 "streamer made by the volunteers of the <a href=\"http://www.videolan.org/"
+                                 "streamer made by the volunteers of the <a href=\"https://www.videolan.org/"
                                  "\"><span style=\" text-decoration: underline; color:#0057ae;\">VideoLAN</"
                                  "span></a> community.</p><p>VLC uses its internal codecs, works on "
                                  "essentially every popular platform, and can read almost all files, CDs, "
                                  "DVDs, network streams, capture cards and other media formats!</p><p><a href="
-                                 "\"http://www.videolan.org/contribute/\"><span style=\" text-decoration: "
+                                 "\"https://www.videolan.org/contribute/\"><span style=\" text-decoration: "
                                  "underline; color:#0057ae;\">Help and join us!</span></a>"));
 
     NSString *fontfamily = (OSX_YOSEMITE_AND_HIGHER) ? @"Helvetica Neue" : @"Lucida Grande";
