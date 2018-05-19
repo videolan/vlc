fbc9feefbbd386124a208d36fa94bc343ac3c4b2
Rémi Denis-Courmont <remi@remlab.net>
2018-05-19 21:36:24 +0300

    vout: always send fullscreen control
    Even if the new value for the fullscreen variable matches the old one,
    we might still need to update the status.
    
    One case is as follows:
    1) Set windowed mode.
    2) Set wallpaper mode: this will go to fullscreen without changing the
       fullscreen variable.
    3) Leave wallpaper mode.
    The window must be returned from fullscreen to windowed mode even though
    the fullscreen variable is false all along.

--- src/video_output/vout_intf.c
+++ src/video_output/vout_intf.c
@@ -548,10 +548,9 @@ static int FullscreenCallback( vlc_object_t *p_this, char const *psz_cmd,
                        vlc_value_t oldval, vlc_value_t newval, void *p_data )
 {
     vout_thread_t *p_vout = (vout_thread_t *)p_this;
-    (void)psz_cmd; (void)p_data;
+    (void)psz_cmd; (void) oldval; (void)p_data;
 
-    if( oldval.b_bool != newval.b_bool )
-        vout_ControlChangeFullscreen( p_vout, newval.b_bool );
+    vout_ControlChangeFullscreen( p_vout, newval.b_bool );
     return VLC_SUCCESS;
 }
 
