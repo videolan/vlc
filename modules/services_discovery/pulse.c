videolan/vlc

commit 3cea7a78999068f71972094e849f95c1591d3e3a

use proper check for the search.h availability

modules/services_discovery/pulse.c
@@ -24,7 +24,9 @@
 # include <config.h>
 #endif
 
+#ifdef HAVE_SEARCH_H
 #include <search.h>
+#endif
 #include <assert.h>
 
 #include <vlc_common.h>

modules/services_discovery/udev.c
@@ -25,7 +25,9 @@
 #endif
 
 #include <errno.h>
+#ifdef HAVE_SEARCH_H
 #include <search.h>
+#endif
 #include <poll.h>
 #include <sys/stat.h>
 #include <fcntl.h>

modules/video_output/xcb/keysym.c
@@ -25,7 +25,9 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <limits.h>
+#ifdef HAVE_SEARCH_H
 #include <search.h>
+#endif
 #include <assert.h>
 
 struct keysym

modules/access/dtv/access.c
@@ -29,7 +29,9 @@
 #include <vlc_input.h>
 #include <vlc_plugin.h>
 #include <vlc_dialog.h>
+#ifdef HAVE_SEARCH_H
 #include <search.h>
+#endif
 
 #include "dtv/dtv.h"
 

commit eca2be7d7fb8a02f7a27f57bd49261eebd115dcb

Remove harmful HAVE_SEARCH_H
This fixes building the keysym generator.
Regression from 3cea7a78999068f71972094e849f95c1591d3e3a.

modules/services_discovery/pulse.c
@@ -24,9 +24,7 @@
 # include <config.h>
 #endif
 
-#ifdef HAVE_SEARCH_H
 #include <search.h>
-#endif
 #include <assert.h>
 
 #include <vlc_common.h>

modules/services_discovery/udev.c
@@ -25,9 +25,7 @@
 #endif
 
 #include <errno.h>
-#ifdef HAVE_SEARCH_H
 #include <search.h>
-#endif
 #include <poll.h>
 #include <sys/stat.h>
 #include <fcntl.h>

modules/video_output/xcb/keysym.c
@@ -25,9 +25,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <limits.h>
-#ifdef HAVE_SEARCH_H
 #include <search.h>
-#endif
 #include <assert.h>
 
 struct keysym