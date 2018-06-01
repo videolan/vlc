1104dd40860a3311745d16f8a0ef46411b55aef9
Steve Lhomme <robux4@ycbcr.xyz>
2018-06-01 10:26:38 +0200

    demux: nsv: fix hardcoded CLOCK_FREQ equivalent

--- modules/demux/nsv.c
+++ modules/demux/nsv.c
@@ -615,7 +615,7 @@ static int ReadNSVs( demux_t *p_demux )
     else if( header[16] != 0 )
     {
         /* Integer frame rate */
-        p_sys->i_pcr_inc = 1000000 / header[16];
+        p_sys->i_pcr_inc = CLOCK_FREQ / header[16];
     }
     else
     {
