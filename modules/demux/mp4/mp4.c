8cdf86bf97f48c55823ed81a188a30177b2154e8
Steve Lhomme <robux4@ycbcr.xyz>
2018-06-08 14:12:55 +0200

    demux:mp4: the local i_time in FragGetMoofByTfraIndex() is not an mtime_t
    (cherry picked from commit c43b6de185878ea645982dcd0e8d1ddee240f769) (edited)
    edited:
    - in 4.0 it was done when mtime_t was still used

--- modules/demux/mp4/mp4.c
+++ modules/demux/mp4/mp4.c
@@ -4906,7 +4906,7 @@ static int FragGetMoofByTfraIndex( demux_t *p_demux, const vlc_tick_t i_target_t
                 stime_t i_track_target_time = MP4_rescale_qtime( i_target_time, p_track->i_timescale );
                 for ( uint32_t i = 0; i<p_data->i_number_of_entries; i += ( p_data->i_version == 1 ) ? 2 : 1 )
                 {
-                    vlc_tick_t i_time;
+                    stime_t i_time;
                     uint64_t i_offset;
                     if ( p_data->i_version == 1 )
                     {
