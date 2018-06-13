c3784ca3c11d4399a6d8a9fff4199d4a924153e2
Steve Lhomme <robux4@ycbcr.xyz>
2018-06-13 15:04:48 +0200

    sout:description: use VLC_TICK_INVALID to signal the start date is not set

--- modules/stream_out/description.c
+++ modules/stream_out/description.c
@@ -86,7 +86,7 @@ static int Open( vlc_object_t *p_this )
         free(p_sys);
         return VLC_EGENERIC;
     }
-    p_sys->i_stream_start = 0;
+    p_sys->i_stream_start = VLC_TICK_INVALID;
 
     return VLC_SUCCESS;
 }
@@ -117,7 +117,7 @@ static void *Add( sout_stream_t *p_stream, const es_format_t *p_fmt )
 
     TAB_APPEND( p_sys->data->i_es, p_sys->data->es, p_fmt_copy );
 
-    if( p_sys->i_stream_start <= 0 )
+    if( p_sys->i_stream_start == VLC_TICK_INVALID )
         p_sys->i_stream_start = vlc_tick_now();
 
     return (void *)p_fmt_copy;
