911e511f43bfabc3e65d35a62ed5df837bc6ee2e
Steve Lhomme <robux4@ycbcr.xyz>
2018-06-13 17:45:06 +0200

    subsdelay: replace hardcoded CLOCK_FREQ fractions by VLC_TICK_FROM_MS()
    Easier to read and more explicit.

--- modules/spu/subsdelay.c
+++ modules/spu/subsdelay.c
@@ -762,7 +762,7 @@ static void SubsdelayEntryNewStopValueUpdated( subsdelay_heap_entry_t *p_entry )
 {
     if( !p_entry->b_update_stop )
     {
-        p_entry->p_subpic->i_stop = p_entry->i_new_stop - 100000; /* 0.1 sec less */
+        p_entry->p_subpic->i_stop = p_entry->i_new_stop - VLC_TICK_FROM_MS(100); /* 0.1 sec less */
     }
 }
 
