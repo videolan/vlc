ed3a861b9cb13f79fa9ea129cde07c8898796ea7
Steve Lhomme <robux4@ycbcr.xyz>
2018-06-13 10:19:00 +0200

    mux:ogg: explicily specifiy the index is in CLOCK_FREQ ticks

--- modules/mux/ogg.c
+++ modules/mux/ogg.c
@@ -717,7 +717,7 @@ static void OggGetSkeletonIndex( uint8_t **pp_buffer, long *pi_size, ogg_stream_
     memcpy( p_buffer, "index", 6 );
     SetDWLE( &p_buffer[6], p_stream->i_serial_no );
     SetQWLE( &p_buffer[10], p_stream->skeleton.i_index_count ); /* num keypoints */
-    SetQWLE( &p_buffer[18], 1000000 );
+    SetQWLE( &p_buffer[18], CLOCK_FREQ );
     SetQWLE( &p_buffer[34], p_stream->i_length );
     memcpy( p_buffer + INDEX_BASE_SIZE, p_stream->skeleton.p_index, p_stream->skeleton.i_index_payload );
     *pi_size = INDEX_BASE_SIZE + p_stream->skeleton.i_index_size;
