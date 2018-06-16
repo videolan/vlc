videolan/vlc

commit a3d1527ecc4c775253c6822502bbaeb2a788933e

contrib: lua: Make sure to use the right AR tool for the target
In many cases, the host AR tool just happens to work fine for e.g.
mingw cross builds, but clang targeting x86 mingw recently started
generating code that trips up GNU binutils ar, with cases like this:

ar: lcode.o: Unrecognized storage class 0 for .rdata symbol `__xmm@8000000000000
0008000000000000000'

Just consistently use the right AR tool for the target, just like
we do for RANLIB and STRIP.

contrib/src/lua/rules.mak
@@ -68,6 +68,7 @@ endif
 		-e 's%CC=%#CC=%' \
 		-e 's%= *strip%=$(STRIP)%' \
 		-e 's%= *ranlib%= $(RANLIB)%' \
+		-e 's%= *ar%= $(AR)%' \
 		Makefile
 	$(MOVE)
 

commit fcd1a0bebde89591c87e3aec8773ef8ad14fd306

contrib: lua: Make the win32 makefile patching stricter
This fixes building for armv7, broken in a3d1527e.

The "= ar" pattern could match any case of an armv7
prefixed tool, like "RANLIB= armv7-w64-mingw32-...".

contrib/src/lua/rules.mak
@@ -68,7 +68,7 @@ endif
 		-e 's%CC=%#CC=%' \
 		-e 's%= *strip%=$(STRIP)%' \
 		-e 's%= *ranlib%= $(RANLIB)%' \
-		-e 's%= *ar%= $(AR)%' \
+		-e 's%AR= *ar%AR= $(AR)%' \
 		Makefile
 	$(MOVE)
 