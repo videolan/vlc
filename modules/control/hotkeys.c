videolan/vlc

commit 73e4a2e8883190230087562fbe9bb280d0423089

hotkeys: the delay up/down are processed in mtime_t
And with 50ms inc/decrements

modules/control/hotkeys.c
@@ -712,7 +712,7 @@ static int PutAction( intf_thread_t *p_intf, input_thread_t *p_input,
         case ACTIONID_SUBDELAY_DOWN:
         case ACTIONID_SUBDELAY_UP:
         {
-            int diff = (i_action == ACTIONID_SUBDELAY_UP) ? 50000 : -50000;
+            mtime_t diff = (i_action == ACTIONID_SUBDELAY_UP) ? 50000 : -50000;
             if( p_input )
             {
                 vlc_value_t val;
@@ -729,7 +729,7 @@ static int PutAction( intf_thread_t *p_intf, input_thread_t *p_input,
                     free(list);
                     break;
                 }
-                int64_t i_delay = var_GetInteger( p_input, "spu-delay" ) + diff;
+                mtime_t i_delay = var_GetInteger( p_input, "spu-delay" ) + diff;
 
                 var_SetInteger( p_input, "spu-delay", i_delay );
                 ClearChannels( p_vout, slider_chan );
@@ -742,10 +742,10 @@ static int PutAction( intf_thread_t *p_intf, input_thread_t *p_input,
         case ACTIONID_AUDIODELAY_DOWN:
         case ACTIONID_AUDIODELAY_UP:
         {
-            int diff = (i_action == ACTIONID_AUDIODELAY_UP) ? 50000 : -50000;
+            mtime_t diff = (i_action == ACTIONID_SUBDELAY_UP) ? 50000 : -50000;
             if( p_input )
             {
-                int64_t i_delay = var_GetInteger( p_input, "audio-delay" )
+                mtime_t i_delay = var_GetInteger( p_input, "audio-delay" )
                                   + diff;
 
                 var_SetInteger( p_input, "audio-delay", i_delay );

commit d03f37b50d8461c1dab08b9662245a891dbcc69c

hotkeys: fix logic, copy-paste error
Regression from 73e4a2e8883190230087562fbe9bb280d0423089.

modules/control/hotkeys.c
@@ -742,7 +742,7 @@ static int PutAction( intf_thread_t *p_intf, input_thread_t *p_input,
         case ACTIONID_AUDIODELAY_DOWN:
         case ACTIONID_AUDIODELAY_UP:
         {
-            mtime_t diff = (i_action == ACTIONID_SUBDELAY_UP) ? 50000 : -50000;
+            mtime_t diff = (i_action == ACTIONID_AUDIODELAY_UP) ? 50000 : -50000;
             if( p_input )
             {
                 mtime_t i_delay = var_GetInteger( p_input, "audio-delay" )