129b682c368e055935657f2724630c8699315bf2
Steve Lhomme <robux4@ycbcr.xyz>
2018-06-29 09:38:50 +0200

    demux: fix potential crash if the renderer has no demux filter

--- src/input/demux.c
+++ src/input/demux.c
@@ -538,8 +538,9 @@ static bool demux_filter_enable_disable(demux_t *p_demux,
 {
     struct vlc_demux_private *priv = vlc_stream_Private(p_demux);
 
-    if (strcmp(module_get_name(priv->module, false), psz_demux) == 0
-     || strcmp(module_get_name(priv->module, true), psz_demux) == 0)
+    if ( psz_demux &&
+        (strcmp(module_get_name(priv->module, false), psz_demux) == 0
+      || strcmp(module_get_name(priv->module, true), psz_demux) == 0) )
     {
         demux_Control( p_demux,
                        b_enable ? DEMUX_FILTER_ENABLE : DEMUX_FILTER_DISABLE );
