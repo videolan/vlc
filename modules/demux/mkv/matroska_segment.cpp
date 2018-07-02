videolan/vlc

commit 6b10c2e60624aee8b9ca689cef8e0688b8ae35ef

demux:mkv: do not preload the first Cluster
We stop preload the Matroska head when encountering the first Cluster ID+Length
and we keep it for when we start reading (unless we seek to a different position
on startup).

Fix #19177

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/demux/mkv/matroska_segment.cpp
@@ -628,21 +628,8 @@ bool matroska_segment_c::Preload( )
             }
             msg_Dbg( &sys.demuxer, "|   + Cluster" );
 
-            if( !ParseCluster( kc_ptr ) )
-                break;
 
             cluster = kc_ptr;
-            IndexAppendCluster( cluster );
-
-            // add first cluster as trusted seekpoint for all tracks
-            for( tracks_map_t::const_iterator it = tracks.begin();
-                 it != tracks.end(); ++it )
-            {
-                _seeker.add_seekpoint( it->first,
-                    SegmentSeeker::Seekpoint( cluster->GetElementPosition(), 0 ) );
-            }
-
-            ep->Down();
             /* stop pre-parsing the stream */
             break;
         }

commit 7bbfee83ea31ab0d46bdd6f8a3d9dff7f5c227ef

demux: mkv: fix seek
This fixes a regression from 6b10c2e6. Seek failed due to empty
_tracks_seekpoints[video_track].

modules/demux/mkv/matroska_segment.cpp
@@ -635,6 +635,16 @@ bool matroska_segment_c::Preload( )
 
 
             cluster = kc_ptr;
+
+            // add first cluster as trusted seekpoint for all tracks
+            for( tracks_map_t::const_iterator it = tracks.begin();
+                 it != tracks.end(); ++it )
+            {
+                _seeker.add_seekpoint( it->first,
+                SegmentSeeker::Seekpoint( cluster->GetElementPosition(), -1,
+                                          SegmentSeeker::Seekpoint::TrustLevel::QUESTIONABLE ) );
+            }
+
             /* stop pre-parsing the stream */
             break;
         }