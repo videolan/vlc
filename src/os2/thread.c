5f4f28d02362e0230a945c1244e04242bfb9e497
Steve Lhomme <robux4@ycbcr.xyz>
2018-07-03 15:08:56 +0200

    os2:threads: convert microseconds to ticks using VLC_TICK_FROM_US()

--- src/os2/thread.c
+++ src/os2/thread.c
@@ -418,8 +418,7 @@ int vlc_cond_timedwait_daytime (vlc_cond_t *p_condvar, vlc_mutex_t *p_mutex,
 
     gettimeofday (&tv, NULL);
 
-    total = vlc_tick_from_sec( tv.tv_sec ) +
-            CLOCK_FREQ * tv.tv_usec / 1000000L;
+    total = vlc_tick_from_sec( tv.tv_sec ) + VLC_TICK_FROM_US( tv.tv_usec );
     total = (deadline - total) / 1000;
     if( total < 0 )
         total = 0;
