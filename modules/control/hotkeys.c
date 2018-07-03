810840157d5fa47b073af87cd132a665ae8738e3
Steve Lhomme <robux4@ycbcr.xyz>
2018-07-03 16:10:26 +0200

    hotkeys: the audio/subtitle time are vlc_tick_t
    They either get 0 or vlc_tick_now().

--- modules/control/hotkeys.c
+++ modules/control/hotkeys.c
@@ -58,8 +58,8 @@ struct intf_sys_t
     /*subtitle_delaybookmarks: placeholder for storing subtitle sync timestamps*/
     struct
     {
-        int64_t i_time_subtitle;
-        int64_t i_time_audio;
+        vlc_tick_t i_time_subtitle;
+        vlc_tick_t i_time_audio;
     } subtitle_delaybookmarks;
 
     struct
@@ -686,12 +686,12 @@ static int PutAction( intf_thread_t *p_intf, input_thread_t *p_input,
                 else
                 {
                     vlc_tick_t i_current_subdelay = var_GetInteger( p_input, "spu-delay" );
-                    int64_t i_additional_subdelay = p_sys->subtitle_delaybookmarks.i_time_audio - p_sys->subtitle_delaybookmarks.i_time_subtitle;
+                    vlc_tick_t i_additional_subdelay = p_sys->subtitle_delaybookmarks.i_time_audio - p_sys->subtitle_delaybookmarks.i_time_subtitle;
                     vlc_tick_t i_total_subdelay = i_current_subdelay + i_additional_subdelay;
                     var_SetInteger( p_input, "spu-delay", i_total_subdelay);
                     ClearChannels( p_vout, slider_chan );
                     DisplayMessage( p_vout, _( "Sub sync: corrected %i ms (total delay = %i ms)" ),
-                                            (int)(i_additional_subdelay / 1000),
+                                            MS_FROM_VLC_TICK( i_additional_subdelay ),
                                             MS_FROM_VLC_TICK( i_total_subdelay ) );
                     p_sys->subtitle_delaybookmarks.i_time_audio = 0;
                     p_sys->subtitle_delaybookmarks.i_time_subtitle = 0;
