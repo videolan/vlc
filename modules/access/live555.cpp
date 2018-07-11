cee79263f32faed74ae855223c3cb04388221aab
Francois Cartegnie <fcvlcdev@free.fr>
2018-07-11 09:53:36 +0200

    live555: set dts on VP8
    (cherry picked from commit 68be47c991683e92b215b87c388673cbbc2f224e)

--- modules/access/live555.cpp
+++ modules/access/live555.cpp
@@ -2133,9 +2133,9 @@ static void StreamRead( void *p_private, unsigned int i_size,
                     case VLC_CODEC_MPGV:
                     case VLC_CODEC_H264:
                     case VLC_CODEC_HEVC:
-                    case VLC_CODEC_VP8:
                         p_block->i_dts = VLC_TS_INVALID;
                         break;
+                    case VLC_CODEC_VP8:
                     default:
                         p_block->i_dts = VLC_TS_0 + i_pts;
                         break;
