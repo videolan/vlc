7fb50ccf27798fe0f2731793f70943fe2cff62b7
Steve Lhomme <robux4@ycbcr.xyz>
2018-07-11 15:32:51 +0200

    video_output: print the dimensions as the unsigned integers that they are

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -1444,7 +1444,7 @@ static void ThreadExecuteCropBorder(vout_thread_t *vout,
                                     unsigned left, unsigned top,
                                     unsigned right, unsigned bottom)
 {
-    msg_Dbg(vout, "ThreadExecuteCropBorder %d.%d %dx%d", left, top, right, bottom);
+    msg_Dbg(vout, "ThreadExecuteCropBorder %u.%u %ux%u", left, top, right, bottom);
     vout_SetDisplayCrop(vout->p->display.vd, 0, 0,
                         left, top, -(int)right, -(int)bottom);
 }
