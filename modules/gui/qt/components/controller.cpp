ee8c6e5d79a585a78077ef337938e02b37e6ca4c
Filip Roséen <filip@atch.se>
2018-07-16 05:26:30 +0200

    gui: qt: prevent fullscreen controller from stealing focus
    A widget will (per default) receive focus when it is displayed,
    something which is not ideal for our fullscreen controller (as it may
    result in VLC grabbing focus from whatever window the user is actually
    interested in at the time).
    
    fixes #8712
    
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>
    (cherry picked from commit d60312d3ad4c4e27f3455b4f579999b826d3948c)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- modules/gui/qt/components/controller.cpp
+++ modules/gui/qt/components/controller.cpp
@@ -807,7 +807,8 @@ FullscreenControllerWidget::FullscreenControllerWidget( intf_thread_t *_p_i, QWi
 
     vout.clear();
 
-    setWindowFlags( Qt::ToolTip );
+    setWindowFlags( Qt::Tool | Qt::FramelessWindowHint );
+    setAttribute( Qt::WA_ShowWithoutActivating );
     setMinimumWidth( FSC_WIDTH );
     isWideFSC = false;
 
