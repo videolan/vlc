Hchnr/VLC-qml6

libvlc: Instantiate media library module

src/libvlc.c
@@ -62,6 +62,7 @@
 #include <vlc_cpu.h>
 #include <vlc_url.h>
 #include <vlc_modules.h>
+#include <vlc_media_library.h>
 
 #include "libvlc.h"
 #include "playlist/playlist_internal.h"
@@ -217,6 +218,13 @@ int libvlc_InternalInit( libvlc_int_t *p_libvlc, int i_argc,
 
     vlc_CPU_dump( VLC_OBJECT(p_libvlc) );
 
+    if( var_InheritBool( p_libvlc, "media-library") )
+    {
+        priv->p_media_library = vlc_ml_create( p_libvlc );
+        if ( priv->p_media_library == NULL )
+            msg_Warn( p_libvlc, "Media library initialization failed" );
+    }
+
     /*
      * Initialize hotkey handling
      */
@@ -362,6 +370,9 @@ void libvlc_InternalCleanup( libvlc_int_t *p_libvlc )
     /* Ask the interfaces to stop and destroy them */
     msg_Dbg( p_libvlc, "removing all interfaces" );
     intf_DestroyAll( p_libvlc );
+
+    if ( priv->p_media_library )
+        vlc_ml_release( priv->p_media_library );
 
     libvlc_InternalDialogClean( p_libvlc );
     libvlc_InternalKeystoreClean( p_libvlc );

videolan/vlc

src/libvlc.c
@@ -62,6 +62,7 @@
 #include <vlc_cpu.h>
 #include <vlc_url.h>
 #include <vlc_modules.h>
+#include <vlc_media_library.h>
 
 #include "libvlc.h"
 #include "playlist/playlist_internal.h"
@@ -217,6 +218,13 @@ int libvlc_InternalInit( libvlc_int_t *p_libvlc, int i_argc,
 
     vlc_CPU_dump( VLC_OBJECT(p_libvlc) );
 
+    if( var_InheritBool( p_libvlc, "media-library") )
+    {
+        priv->p_media_library = libvlc_MlCreate( p_libvlc );
+        if ( priv->p_media_library == NULL )
+            msg_Warn( p_libvlc, "Media library initialization failed" );
+    }
+
     /*
      * Initialize hotkey handling
      */
@@ -362,6 +370,9 @@ void libvlc_InternalCleanup( libvlc_int_t *p_libvlc )
     /* Ask the interfaces to stop and destroy them */
     msg_Dbg( p_libvlc, "removing all interfaces" );
     intf_DestroyAll( p_libvlc );
+
+    if ( priv->p_media_library )
+        libvlc_MlRelease( priv->p_media_library );
 
     libvlc_InternalDialogClean( p_libvlc );
     libvlc_InternalKeystoreClean( p_libvlc );