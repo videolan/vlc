videolan/vlc

commit 6b96ade7dd97acb49303a0a9da9b3d2056b808e0

stream: replace input_thread_t by input_item_t
This will prevent all demuxers/access/streams to mess with the input_thread_t
state.

modules/demux/playlist/playlist.h
@@ -60,7 +60,7 @@ int Import_iTML ( vlc_object_t * );
 int Import_WPL ( vlc_object_t * );
 void Close_WPL ( vlc_object_t * );
 
-#define GetCurrentItem(obj) input_GetItem((obj)->p_input)
+#define GetCurrentItem(obj) input_GetItem((obj)->p_input_item)
 #define GetSource(obj) ((obj)->s)
 
 #define CHECK_FILE(obj) \

include/vlc_stream.h
@@ -52,6 +52,7 @@ struct stream_t
     const char  *psz_location; /**< Location (URL with the scheme stripped) */
     char        *psz_filepath; /**< Local file path (if applicable) */
     bool         b_preparsing; /**< True if this access is used to preparse */
+    input_item_t *p_input_item;/**< Input item (can be NULL) */
 
     union {
         /**
@@ -144,9 +145,6 @@ struct stream_t
      * Private data pointer
      */
     void *p_sys;
-
-    /* Weak link to parent input */
-    input_thread_t *p_input;
 };
 
 /**

modules/access/http.c
@@ -521,13 +521,9 @@ static int ReadICYMeta( stream_t *p_access )
                 free( psz_tmp );
 
             msg_Dbg( p_access, "New Icy-Title=%s", p_sys->psz_icy_title );
-            input_thread_t *p_input = p_access->p_input;
-            if( p_input )
-            {
-                input_item_t *p_input_item = input_GetItem( p_access->p_input );
-                if( p_input_item )
-                    input_item_SetMeta( p_input_item, vlc_meta_NowPlaying, p_sys->psz_icy_title );
-            }
+            if( p_access->p_input_item )
+                input_item_SetMeta( p_access->p_input_item, vlc_meta_NowPlaying,
+                                    p_sys->psz_icy_title );
         }
     }
     free( psz_meta );
@@ -892,13 +888,9 @@ static int Connect( stream_t *p_access )
             else
                 vlc_xml_decode( p_sys->psz_icy_name );
             msg_Dbg( p_access, "Icy-Name: %s", p_sys->psz_icy_name );
-            input_thread_t *p_input = p_access->p_input;
-            if ( p_input )
-            {
-                input_item_t *p_input_item = input_GetItem( p_access->p_input );
-                if ( p_input_item )
-                    input_item_SetMeta( p_input_item, vlc_meta_Title, p_sys->psz_icy_name );
-            }
+            if ( p_access->p_input_item )
+                input_item_SetMeta( p_access->p_input_item, vlc_meta_Title,
+                                    p_sys->psz_icy_name );
 
             p_sys->b_icecast = true; /* be on the safeside. set it here as well. */
             p_sys->b_reconnect = true;
@@ -913,13 +905,9 @@ static int Connect( stream_t *p_access )
             else
                 vlc_xml_decode( p_sys->psz_icy_genre );
             msg_Dbg( p_access, "Icy-Genre: %s", p_sys->psz_icy_genre );
-            input_thread_t *p_input = p_access->p_input;
-            if( p_input )
-            {
-                input_item_t *p_input_item = input_GetItem( p_access->p_input );
-                if( p_input_item )
-                    input_item_SetMeta( p_input_item, vlc_meta_Genre, p_sys->psz_icy_genre );
-            }
+            if( p_access->p_input_item )
+                input_item_SetMeta( p_access->p_input_item, vlc_meta_Genre,
+                                    p_sys->psz_icy_genre );
         }
         else if( !strncasecmp( psz, "Icy-Notice", 10 ) )
         {

modules/demux/directory.c
@@ -32,8 +32,7 @@
 
 static int Demux( demux_t *p_demux )
 {
-    input_item_t *p_input = input_GetItem( p_demux->p_input );
-    input_item_node_t *p_node = input_item_node_Create( p_input );
+    input_item_node_t *p_node = input_item_node_Create( p_demux->p_input_item );
 
     if( vlc_stream_ReadDir( p_demux->s, p_node ) )
     {
@@ -78,7 +77,7 @@ static int Import_Dir( vlc_object_t *p_this )
 
     if( p_demux->s->pf_readdir == NULL )
         return VLC_EGENERIC;
-    if( p_demux->p_input == NULL )
+    if( p_demux->p_input_item == NULL )
         return VLC_ETIMEOUT;
 
     p_demux->pf_demux = Demux;

modules/demux/mp4/mp4.c
@@ -851,15 +851,14 @@ static int Open( vlc_object_t * p_this )
     }
 
     MP4_Box_t *p_rmra = MP4_BoxGet( p_sys->p_root, "/moov/rmra" );
-    if( p_rmra != NULL && p_demux->p_input != NULL )
+    if( p_rmra != NULL && p_demux->p_input_item != NULL )
     {
         int        i_count = MP4_BoxCount( p_rmra, "rmda" );
         int        i;
 
         msg_Dbg( p_demux, "detected playlist mov file (%d ref)", i_count );
 
-        input_thread_t *p_input = p_demux->p_input;
-        input_item_t *p_current = input_GetItem( p_input );
+        input_item_t *p_current = p_demux->p_input_item;
 
         input_item_node_t *p_subitems = input_item_node_Create( p_current );
 

modules/services_discovery/sap.c
@@ -616,18 +616,13 @@ static int Demux( demux_t *p_demux )
 {
     demux_sys_t *p_sys = p_demux->p_sys;
     sdp_t *p_sdp = p_sys->p_sdp;
-    input_thread_t *p_input = p_demux->p_input;
-    input_item_t *p_parent_input;
+    input_item_t *p_parent_input = p_demux->p_input_item;
 
-    if( !p_input )
+    if( !p_parent_input )
     {
         msg_Err( p_demux, "parent input could not be found" );
         return VLC_EGENERIC;
     }
-
-    /* This item hasn't been held by input_GetItem
-     * don't release it */
-    p_parent_input = input_GetItem( p_input );
 
     input_item_SetURI( p_parent_input, p_sdp->psz_uri );
     input_item_SetName( p_parent_input, p_sdp->psz_sessionname );

modules/stream_filter/record.c
@@ -180,7 +180,8 @@ static int Start( stream_t *s, const char *psz_extension )
 
     /* Create file name
      * TODO allow prefix configuration */
-    psz_file = input_CreateFilename( s->p_input, NULL, psz_path, INPUT_RECORD_PREFIX, psz_extension );
+    psz_file = input_CreateFilename( NULL, s->p_input_item, psz_path,
+                                     INPUT_RECORD_PREFIX, psz_extension );
 
     free( psz_path );
 

modules/stream_out/chromecast/chromecast_demux.cpp
@@ -58,8 +58,7 @@ struct demux_cc
         vlc_meta_t *p_meta = vlc_meta_New();
         if( likely(p_meta != NULL) )
         {
-            input_item_t *p_item = p_demux->p_next->p_input ?
-                                   input_GetItem( p_demux->p_next->p_input ) : NULL;
+            input_item_t *p_item = p_demux->p_next->p_input_item;
             if( p_item )
             {
                 /* Favor Meta from the input item of the input_thread since

commit bfb59fc493b4c64db92015f88db9f2b5972966e6

demux: playlist: fix GetCurrentItem helper
This obviously lead to severe crashes.

Regression from 6b96ade7dd97acb49303a0a9da9b3d2056b808e0

modules/demux/playlist/playlist.h
@@ -60,7 +60,7 @@ int Import_iTML ( vlc_object_t * );
 int Import_WPL ( vlc_object_t * );
 void Close_WPL ( vlc_object_t * );
 
-#define GetCurrentItem(obj) input_GetItem((obj)->p_input_item)
+#define GetCurrentItem(obj) ((obj)->p_input_item)
 #define GetSource(obj) ((obj)->s)
 
 #define CHECK_FILE(obj) \