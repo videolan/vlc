16d2a7c98ac95f21a52e98fc71ab691fcbad5776
Francois Cartegnie <fcvlcdev@free.fr>
2018-07-24 13:14:23 +0200

    demux: avi: simplify [re]creating DV es

--- modules/demux/avi/avi.c
+++ modules/demux/avi/avi.c
@@ -2891,13 +2891,15 @@ static void AVI_DvHandleAudio( demux_t *p_demux, avi_track_t *tk, block_t *p_fra
     if( tk->p_es_dv_audio && tk->i_dv_audio_rate != (int)fmt.audio.i_rate )
     {
         es_out_Del( p_demux->out, tk->p_es_dv_audio );
-        tk->p_es_dv_audio = es_out_Add( p_demux->out, &fmt );
+        tk->p_es_dv_audio = NULL;
     }
-    else if( !tk->p_es_dv_audio )
+
+    if( !tk->p_es_dv_audio )
     {
         tk->p_es_dv_audio = es_out_Add( p_demux->out, &fmt );
+        tk->i_dv_audio_rate = fmt.audio.i_rate;
     }
-    tk->i_dv_audio_rate = fmt.audio.i_rate;
+
     es_format_Clean( &fmt );
 
     block_t *p_frame_audio = dv_extract_audio( p_frame );
