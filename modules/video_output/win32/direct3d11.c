44bbb10adad1f2cbae7bb7f44bbec2191dd53ea4
Steve Lhomme <robux4@ycbcr.xyz>
2018-08-03 07:19:14 +0200

    direct3d11: log the actual source pixel format, not the assumed decoder one

--- modules/video_output/win32/direct3d11.c
+++ modules/video_output/win32/direct3d11.c
@@ -1317,10 +1317,10 @@ static int SetupOutputFormat(vout_display_t *vd, video_format_t *fmt)
        return VLC_EGENERIC;
     }
 
-    fmt->i_chroma = decoder_format ? decoder_format->fourcc : sys->picQuad.textureFormat->fourcc;
-
     msg_Dbg( vd, "Using pixel format %s for chroma %4.4s", sys->picQuad.textureFormat->name,
                  (char *)&fmt->i_chroma );
+
+    fmt->i_chroma = decoder_format ? decoder_format->fourcc : sys->picQuad.textureFormat->fourcc;
     DxgiFormatMask( sys->picQuad.textureFormat->formatTexture, fmt );
 
     /* check the region pixel format */
