88a4aef9b59e48f4918303ab2f1f31f5bbdf716d
Steve Lhomme <robux4@ycbcr.xyz>
2018-08-07 13:14:35 +0200

    d3d11_quad: fix support for rendering for some basic DXGI_FORMAT types
    If they are mentioned in dxgi_fmt they also have to be here (except for
    DXGI_FORMAT_420_OPAQUE).
    
    Ref #20681

--- modules/video_output/win32/d3d11_quad.c
+++ modules/video_output/win32/d3d11_quad.c
@@ -866,6 +866,8 @@ void D3D11_UpdateViewport(d3d_quad_t *quad, const RECT *rect, const d3d_format_t
     case DXGI_FORMAT_B5G6R5_UNORM:
     case DXGI_FORMAT_R10G10B10A2_UNORM:
     case DXGI_FORMAT_R16G16B16A16_UNORM:
+    case DXGI_FORMAT_YUY2:
+    case DXGI_FORMAT_AYUV:
         if ( display->formatTexture == DXGI_FORMAT_NV12 ||
              display->formatTexture == DXGI_FORMAT_P010 )
         {
