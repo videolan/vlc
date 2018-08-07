89b3a3946e89a385c79c30b6f4361519461cb57a
KO Myung-Hun <komh78@gmail.com>
2018-08-07 15:48:24 +0900

    src: os2: fix mismatched arguments list
    Signed-off-by: Rémi Denis-Courmont <remi@remlab.net>

--- src/os2/specific.c
+++ src/os2/specific.c
@@ -103,8 +103,7 @@ static void IPCHelperThread( void *arg )
                                  ( char const ** )
                                      ( i_options ? &ppsz_argv[ i_opt + 1 ] :
                                                    NULL ),
-                                 VLC_INPUT_OPTION_TRUSTED,
-                                 true );
+                                 VLC_INPUT_OPTION_TRUSTED );
             }
 
             for( ; i_options >= 0; i_options-- )
