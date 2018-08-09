videolan/vlc

commit 4c8b741e4844a80fb824caccd9606259d5958aa0

core: replace filter_sys_t* by void*
See #17078

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

modules/video_filter/ci_filters.m
@@ -356,10 +356,11 @@ static void filter_PsychedelicControl(filter_t *filter, struct filter_chain *fch
 static picture_t *
 Filter(filter_t *filter, picture_t *src)
 {
-    struct ci_filters_ctx *ctx = filter->p_sys->ctx;
+    filter_sys_t *p_sys = filter->p_sys;
+    struct ci_filters_ctx *ctx = p_sys->ctx;
     enum filter_type filter_types[NUM_MAX_EQUIVALENT_VLC_FILTERS];
 
-    filter_desc_table_GetFilterTypes(filter->p_sys->psz_filter, filter_types);
+    filter_desc_table_GetFilterTypes(p_sys->psz_filter, filter_types);
     if (ctx->fchain->filter != filter_types[0])
         return src;
 
@@ -425,14 +426,14 @@ Filter(filter_t *filter, picture_t *src)
             return NULL;
     }
 
-    filter->p_sys->mouse_moved = false;
+    p_sys->mouse_moved = false;
     return dst;
 
 error:
     if (dst)
         picture_Release(dst);
     picture_Release(src);
-    filter->p_sys->mouse_moved = false;
+    p_sys->mouse_moved = false;
     return NULL;
 }
 
@@ -582,6 +583,7 @@ static int
 Open(vlc_object_t *obj, char const *psz_filter)
 {
     filter_t *filter = (filter_t *)obj;
+    filter_sys_t *p_sys = filter->p_sys;
 
     switch (filter->fmt_in.video.i_chroma)
     {
@@ -666,8 +668,8 @@ Open(vlc_object_t *obj, char const *psz_filter)
     else if (Open_CreateFilters(filter, &ctx->fchain, filter_types))
         goto error;
 
-    filter->p_sys->psz_filter = psz_filter;
-    filter->p_sys->ctx = ctx;
+    p_sys->psz_filter = psz_filter;
+    p_sys->ctx = ctx;
 
     filter->pf_video_filter = Filter;
     filter->pf_video_mouse = Mouse;
@@ -682,7 +684,7 @@ error:
             CVPixelBufferPoolRelease(ctx->cvpx_pool);
         free(ctx);
     }
-    free(filter->p_sys);
+    free(p_sys);
     return VLC_EGENERIC;
 }
 
@@ -732,10 +734,11 @@ static void
 Close(vlc_object_t *obj)
 {
     filter_t *filter = (filter_t *)obj;
-    struct ci_filters_ctx *ctx = filter->p_sys->ctx;
+    filter_sys_t *p_sys = filter->p_sys;
+    struct ci_filters_ctx *ctx = p_sys->ctx;
     enum filter_type filter_types[NUM_MAX_EQUIVALENT_VLC_FILTERS];
 
-    filter_desc_table_GetFilterTypes(filter->p_sys->psz_filter, filter_types);
+    filter_desc_table_GetFilterTypes(p_sys->psz_filter, filter_types);
     for (unsigned int i = 0;
          i < NUM_MAX_EQUIVALENT_VLC_FILTERS && filter_types[i] != FILTER_NONE;
          ++i)
@@ -749,7 +752,7 @@ Close(vlc_object_t *obj)
         free(ctx);
         var_Destroy(filter->obj.parent, "ci-filters-ctx");
     }
-    free(filter->p_sys);
+    free(p_sys);
 }
 
 #define CI_CUSTOM_FILTER_TEXT N_("Use a specific Core Image Filter")

include/vlc_filter.h
@@ -68,7 +68,7 @@ struct filter_t
 
     /* Module properties */
     module_t *          p_module;
-    filter_sys_t *      p_sys;
+    void               *p_sys;
 
     /* Input format */
     es_format_t         fmt_in;

modules/audio_filter/channel_mixer/headphone.c
@@ -494,10 +494,11 @@ static int OpenFilter( vlc_object_t *p_this )
 static void CloseFilter( vlc_object_t *p_this )
 {
     filter_t *p_filter = (filter_t *)p_this;
+    filter_sys_t *p_sys = p_filter->p_sys;
 
-    free( p_filter->p_sys->p_overflow_buffer );
-    free( p_filter->p_sys->p_atomic_operations );
-    free( p_filter->p_sys );
+    free( p_sys->p_overflow_buffer );
+    free( p_sys->p_atomic_operations );
+    free( p_sys );
 }
 
 static block_t *Convert( filter_t *p_filter, block_t *p_block )

modules/audio_filter/channel_mixer/mono.c
@@ -432,7 +432,8 @@ static block_t *Convert( filter_t *p_filter, block_t *p_block )
         return NULL;
     }
 
-    i_out_size = p_block->i_nb_samples * p_filter->p_sys->i_bitspersample/8 *
+    filter_sys_t *p_sys = p_filter->p_sys;
+    i_out_size = p_block->i_nb_samples * p_sys->i_bitspersample/8 *
                  aout_FormatNbChannels( &(p_filter->fmt_out.audio) );
 
     p_out = block_Alloc( i_out_size );
@@ -443,11 +444,11 @@ static block_t *Convert( filter_t *p_filter, block_t *p_block )
         return NULL;
     }
     p_out->i_nb_samples =
-                  (p_block->i_nb_samples / p_filter->p_sys->i_nb_channels) *
+                  (p_block->i_nb_samples / p_sys->i_nb_channels) *
                        aout_FormatNbChannels( &(p_filter->fmt_out.audio) );
 
 #if 0
-    unsigned int i_in_size = in_buf.i_nb_samples  * (p_filter->p_sys->i_bitspersample/8) *
+    unsigned int i_in_size = in_buf.i_nb_samples  * (p_sys->i_bitspersample/8) *
                              aout_FormatNbChannels( &(p_filter->fmt_in.audio) );
     if( (in_buf.i_buffer != i_in_size) && ((i_in_size % 32) != 0) ) /* is it word aligned?? */
     {
@@ -457,7 +458,7 @@ static block_t *Convert( filter_t *p_filter, block_t *p_block )
 #endif
 
     memset( p_out->p_buffer, 0, i_out_size );
-    if( p_filter->p_sys->b_downmix )
+    if( p_sys->b_downmix )
     {
         stereo2mono_downmix( p_filter, p_block, p_out );
         mono( p_filter, p_out, p_block );

modules/audio_filter/channel_mixer/trivial.c
@@ -74,9 +74,11 @@ static block_t *Upmix( filter_t *p_filter, block_t *p_in_buf )
     p_out_buf->i_pts        = p_in_buf->i_pts;
     p_out_buf->i_length     = p_in_buf->i_length;
 
+    filter_sys_t *p_sys = p_filter->p_sys;
+
     float *p_dest = (float *)p_out_buf->p_buffer;
     const float *p_src = (float *)p_in_buf->p_buffer;
-    const int *channel_map = p_filter->p_sys->channel_map;
+    const int *channel_map = p_sys->channel_map;
 
     for( size_t i = 0; i < p_in_buf->i_nb_samples; i++ )
     {
@@ -101,9 +103,11 @@ static block_t *Downmix( filter_t *p_filter, block_t *p_buf )
 
     assert( i_input_nb >= i_output_nb );
 
+    filter_sys_t *p_sys = p_filter->p_sys;
+
     float *p_dest = (float *)p_buf->p_buffer;
     const float *p_src = p_dest;
-    const int *channel_map = p_filter->p_sys->channel_map;
+    const int *channel_map = p_sys->channel_map;
     /* Use an extra buffer to avoid overlapping */
     float buffer[i_output_nb];
 
@@ -293,10 +297,11 @@ static int Create( vlc_object_t *p_this )
         }
     }
 
-    p_filter->p_sys = malloc( sizeof(*p_filter->p_sys) );
-    if(! p_filter->p_sys )
+    filter_sys_t *p_sys = malloc( sizeof(*p_sys) );
+    if( !p_sys )
         return VLC_ENOMEM;
-    memcpy( p_filter->p_sys->channel_map, channel_map, sizeof(channel_map) );
+    p_filter->p_sys = p_sys;
+    memcpy( p_sys->channel_map, channel_map, sizeof(channel_map) );
 
     if( aout_FormatNbChannels( outfmt ) > aout_FormatNbChannels( infmt ) )
         p_filter->pf_audio_filter = Upmix;

modules/audio_filter/converter/tospdif.c
@@ -165,9 +165,10 @@ static void write_data( filter_t *p_filter, const void *p_buf, size_t i_size,
 
 static void write_buffer( filter_t *p_filter, block_t *p_in_buf )
 {
+    filter_sys_t *p_sys = p_filter->p_sys;
     write_data( p_filter, p_in_buf->p_buffer, p_in_buf->i_buffer,
                 is_big_endian( p_filter, p_in_buf ) );
-    p_filter->p_sys->p_out_buf->i_length += p_in_buf->i_length;
+    p_sys->p_out_buf->i_length += p_in_buf->i_length;
 }
 
 static int write_init( filter_t *p_filter, block_t *p_in_buf,
@@ -389,6 +390,7 @@ static int write_buffer_truehd( filter_t *p_filter, block_t *p_in_buf )
 static int write_buffer_dts( filter_t *p_filter, block_t *p_in_buf )
 {
     uint16_t i_data_type;
+    filter_sys_t *p_sys = p_filter->p_sys;
 
     /* Only send the DTS core part */
     vlc_dts_header_t core;
@@ -429,7 +431,7 @@ static int write_buffer_dts( filter_t *p_filter, block_t *p_in_buf )
         return SPDIF_ERROR;
 
     if( i_data_type == 0 )
-        p_filter->p_sys->i_out_offset = 0;
+        p_sys->i_out_offset = 0;
 
     write_buffer( p_filter, p_in_buf );
     write_finalize( p_filter, i_data_type, 8 /* in bits */ );
@@ -481,7 +483,7 @@ static int write_buffer_dtshd( filter_t *p_filter, block_t *p_in_buf )
     size_t i_out_size = i_period * 4;
     uint16_t i_data_type = IEC61937_DTSHD | i_subtype << 8;
 
-    if( p_filter->p_sys->dtshd.b_skip
+    if( p_sys->dtshd.b_skip
      || i_in_size + SPDIF_HEADER_SIZE > i_out_size )
     {
         /* The bitrate is too high, pass only the core part */
@@ -492,7 +494,7 @@ static int write_buffer_dtshd( filter_t *p_filter, block_t *p_in_buf )
 
         /* Don't try to send substreams anymore. That way, we avoid to switch
          * back and forth between DTD and DTS-HD */
-        p_filter->p_sys->dtshd.b_skip = true;
+        p_sys->dtshd.b_skip = true;
     }
 
     if( write_init( p_filter, p_in_buf, i_out_size,

modules/audio_filter/normvol.c
@@ -110,7 +110,7 @@ static int Open( vlc_object_t *p_this )
     if( p_sys->f_max <= 0 ) p_sys->f_max = 0.01;
 
     /* We need to store (nb_buffers+1)*nb_channels floats */
-    p_sys->p_last = calloc( i_channels * (p_filter->p_sys->i_nb + 2), sizeof(float) );
+    p_sys->p_last = calloc( i_channels * (p_sys->i_nb + 2), sizeof(float) );
     if( !p_sys->p_last )
     {
         free( p_sys );

modules/audio_filter/param_eq.c
@@ -155,8 +155,9 @@ static int Open( vlc_object_t *p_this )
 static void Close( vlc_object_t *p_this )
 {
     filter_t *p_filter = (filter_t *)p_this;
-    free( p_filter->p_sys->p_state );
-    free( p_filter->p_sys );
+    filter_sys_t *p_sys = p_filter->p_sys;
+    free( p_sys->p_state );
+    free( p_sys );
 }
 
 /*****************************************************************************
@@ -166,10 +167,11 @@ static void Close( vlc_object_t *p_this )
  *****************************************************************************/
 static block_t *DoWork( filter_t * p_filter, block_t * p_in_buf )
 {
+    filter_sys_t *p_sys = p_filter->p_sys;
     ProcessEQ( (float*)p_in_buf->p_buffer, (float*)p_in_buf->p_buffer,
-               p_filter->p_sys->p_state,
+               p_sys->p_state,
                p_filter->fmt_in.audio.i_channels, p_in_buf->i_nb_samples,
-               p_filter->p_sys->coeffs, 5 );
+               p_sys->coeffs, 5 );
     return p_in_buf;
 }
 

commit d477a1e83533665ea60a385e61c26f02b3c7c7a8

ci_filters: fix null-deref
regression from 4c8b741e4844a80fb824caccd9606259d5958aa0

modules/video_filter/ci_filters.m
@@ -587,7 +587,6 @@ static int
 Open(vlc_object_t *obj, char const *psz_filter)
 {
     filter_t *filter = (filter_t *)obj;
-    filter_sys_t *p_sys = filter->p_sys;
 
     switch (filter->fmt_in.video.i_chroma)
     {
@@ -605,7 +604,7 @@ Open(vlc_object_t *obj, char const *psz_filter)
             return VLC_EGENERIC;
     }
 
-    filter->p_sys = calloc(1, sizeof(filter_sys_t));
+    filter_sys_t *p_sys = filter->p_sys = calloc(1, sizeof(filter_sys_t));
     if (!filter->p_sys)
         return VLC_ENOMEM;
 