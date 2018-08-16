e45628283d53af07e0299599928ded3b674372d5
Zhao Zhili <quinkblack@foxmail.com>
2018-08-16 11:18:36 +0800

    packetizer: hevc_nal: fix visiable size
    Signed-off-by: Francois Cartegnie <fcvlcdev@free.fr>
    (cherry picked from commit 596b67eb798ca4fa37ca61c2e576cefebe3c92c9)

--- modules/packetizer/hevc_nal.c
+++ modules/packetizer/hevc_nal.c
@@ -1065,8 +1065,26 @@ bool hevc_get_picture_size( const hevc_sequence_parameter_set_t *p_sps,
     *p_h = *p_vh = p_sps->pic_height_in_luma_samples;
     if( p_sps->conformance_window_flag )
     {
-        *p_vh -= p_sps->conf_win.bottom_offset + p_sps->conf_win.top_offset;
-        *p_vw -= p_sps->conf_win.left_offset +  p_sps->conf_win.right_offset;
+        unsigned sub_width_c, sub_height_c;
+
+        if( p_sps->chroma_format_idc == 1 )
+        {
+            sub_width_c = 2;
+            sub_height_c = 2;
+        }
+        else if( p_sps->chroma_format_idc == 2 )
+        {
+            sub_width_c = 2;
+            sub_height_c = 1;
+        }
+        else
+        {
+            sub_width_c = 1;
+            sub_height_c = 1;
+        }
+
+        *p_vh -= (p_sps->conf_win.bottom_offset + p_sps->conf_win.top_offset) * sub_height_c;
+        *p_vw -= (p_sps->conf_win.left_offset +  p_sps->conf_win.right_offset) * sub_width_c;
     }
     return true;
 }
