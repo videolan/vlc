1df423f6954ac9772d9ac7e7ed8201293b1afb3d
Thomas Guillem <thomas@gllm.fr>
2018-09-04 18:53:22 +0200

    input: inline input_SendEvent* functions

--- src/input/event.h
+++ src/input/event.h
@@ -2,7 +2,6 @@
  * event.h: Input event functions
  *****************************************************************************
  * Copyright (C) 2008 Laurent Aimar
- * $Id$
  *
  * Authors: Laurent Aimar <fenrir _AT_ videolan _DOT_ fr>
  *
@@ -25,52 +24,254 @@
 #define LIBVLC_INPUT_EVENT_H 1
 
 #include <vlc_common.h>
+#include <vlc_input.h>
+#include "input_internal.h"
+
+static inline void input_SendEvent(input_thread_t *p_input,
+                                   const struct vlc_input_event *event)
+{
+    input_thread_private_t *priv = input_priv(p_input);
+    if(priv->events_cb)
+        priv->events_cb(p_input, event, priv->events_data);
+}
 
 /*****************************************************************************
  * Event for input.c
  *****************************************************************************/
-void input_SendEventDead( input_thread_t *p_input );
-void input_SendEventPosition( input_thread_t *p_input, double f_position, vlc_tick_t i_time );
-void input_SendEventLength( input_thread_t *p_input, vlc_tick_t i_length );
-void input_SendEventStatistics( input_thread_t *p_input );
-void input_SendEventRate( input_thread_t *p_input, int i_rate );
-void input_SendEventCapabilities( input_thread_t *p_input, int capabilities );
-void input_SendEventAudioDelay( input_thread_t *p_input, vlc_tick_t i_delay );
-void input_SendEventSubtitleDelay( input_thread_t *p_input, vlc_tick_t i_delay );
-void input_SendEventRecord( input_thread_t *p_input, bool b_recording );
-void input_SendEventTitle( input_thread_t *p_input, int i_title );
-void input_SendEventSeekpoint( input_thread_t *p_input, int i_title, int i_seekpoint );
-void input_SendEventSignal( input_thread_t *p_input, double f_quality, double f_strength );
-void input_SendEventState( input_thread_t *p_input, int i_state );
-void input_SendEventCache( input_thread_t *p_input, double f_level );
+static inline void input_SendEventDead(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_DEAD,
+    });
+}
 
-/* TODO rename Item* */
-void input_SendEventMeta( input_thread_t *p_input );
-void input_SendEventMetaInfo( input_thread_t *p_input );
-void input_SendEventMetaEpg( input_thread_t *p_input );
+static inline void input_SendEventCapabilities(input_thread_t *p_input,
+                                                int i_capabilities)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_CAPABILITIES,
+        .capabilities = i_capabilities
+    });
+}
 
-void input_SendEventParsing( input_thread_t *p_input, input_item_node_t *p_root );
+static inline void input_SendEventPosition(input_thread_t *p_input,
+                                            double f_position,
+                                            vlc_tick_t i_time)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_POSITION,
+        .position = { f_position, i_time }
+    });
+}
+
+static inline void input_SendEventLength(input_thread_t *p_input,
+                                          vlc_tick_t i_length)
+{
+    input_item_SetDuration(input_priv(p_input)->p_item, i_length);
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_LENGTH,
+        .length = i_length,
+    });
+}
+
+static inline void input_SendEventStatistics(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_STATISTICS,
+    });
+}
+
+static inline void input_SendEventRate(input_thread_t *p_input, int i_rate)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_RATE,
+        .rate = (float)INPUT_RATE_DEFAULT / (float)i_rate,
+    });
+}
+
+static inline void input_SendEventAudioDelay(input_thread_t *p_input,
+                                              vlc_tick_t i_delay)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_AUDIO_DELAY,
+        .audio_delay = i_delay,
+    });
+}
+
+static inline void input_SendEventSubtitleDelay(input_thread_t *p_input,
+                                                 vlc_tick_t i_delay)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_SUBTITLE_DELAY,
+        .subtitle_delay = i_delay,
+    });
+}
+
+static inline void input_SendEventRecord(input_thread_t *p_input,
+                                         bool b_recording)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_RECORD,
+        .record = b_recording
+    });
+}
+
+static inline void input_SendEventTitle(input_thread_t *p_input, int i_title)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_TITLE,
+        .title = i_title
+    });
+}
+
+static inline void input_SendEventSeekpoint(input_thread_t *p_input,
+                                            int i_title, int i_seekpoint)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_CHAPTER,
+        .chapter = { i_title, i_seekpoint }
+    });
+}
+
+static inline void input_SendEventSignal(input_thread_t *p_input,
+                                         double f_quality, double f_strength)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_SIGNAL,
+        .signal = { f_quality, f_strength }
+    });
+}
+
+static inline void input_SendEventState(input_thread_t *p_input, int i_state)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_STATE,
+        .state = i_state
+    });
+}
+
+static inline void input_SendEventCache(input_thread_t *p_input, double f_level)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_CACHE,
+        .cache = f_level
+    });
+}
+
+static inline void input_SendEventMeta(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_ITEM_META,
+    });
+}
+
+static inline void input_SendEventMetaInfo(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_ITEM_INFO,
+    });
+}
+
+static inline void input_SendEventMetaEpg(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_ITEM_EPG,
+    });
+}
 
 /*****************************************************************************
  * Event for es_out.c
  *****************************************************************************/
-void input_SendEventProgramAdd( input_thread_t *p_input,
-                                int i_program, const char *psz_text );
-void input_SendEventProgramDel( input_thread_t *p_input, int i_program );
-void input_SendEventProgramSelect( input_thread_t *p_input, int i_program );
-void input_SendEventProgramScrambled( input_thread_t *p_input, int i_group, bool b_scrambled );
+static inline void input_SendEventProgramAdd(input_thread_t *p_input,
+                                int i_program, const char *psz_text)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_PROGRAM,
+        .program = {
+            .action = VLC_INPUT_PROGRAM_ADDED,
+            .id = i_program,
+            .title = psz_text
+        }
+    });
+}
+static inline void input_SendEventProgramDel(input_thread_t *p_input,
+                                             int i_program)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_PROGRAM,
+        .program = {
+            .action = VLC_INPUT_PROGRAM_DELETED,
+            .id = i_program
+        }
+    });
+}
+static inline void input_SendEventProgramSelect(input_thread_t *p_input,
+                                                int i_program)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_PROGRAM,
+        .program = {
+            .action = VLC_INPUT_PROGRAM_SELECTED,
+            .id = i_program
+        }
+    });
+}
+static inline void input_SendEventProgramScrambled(input_thread_t *p_input,
+                                                   int i_group, bool b_scrambled)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_PROGRAM,
+        .program = {
+            .action = VLC_INPUT_PROGRAM_SCRAMBLED,
+            .id = i_group,
+            .scrambled = b_scrambled 
+        }
+    });
+}
 
-void input_SendEventEs( input_thread_t *p_input, const struct vlc_input_event_es *es_event );
+static inline void input_SendEventEs(input_thread_t *p_input,
+                                     const struct vlc_input_event_es *es_event)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_ES,
+        .es = *es_event,
+    });
+}
+
+static inline void input_SendEventParsing(input_thread_t *p_input,
+                                          input_item_node_t *p_root)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_SUBITEMS,
+        .subitems = p_root,
+    });
+}
 
 /*****************************************************************************
  * Event for decoder.c
  *****************************************************************************/
-void input_SendEventVout( input_thread_t *p_input );
-void input_SendEventAout( input_thread_t *p_input );
+static inline void input_SendEventVout(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_VOUT
+    });
+}
+
+static inline void input_SendEventAout(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_AOUT
+    });
+}
 
 /*****************************************************************************
  * Event for control.c/input.c
  *****************************************************************************/
-void input_SendEventBookmark( input_thread_t *p_input );
+static inline void input_SendEventBookmark(input_thread_t *p_input)
+{
+    input_SendEvent(p_input, &(struct vlc_input_event) {
+        .type = INPUT_EVENT_BOOKMARK
+    });
+}
 
 #endif
