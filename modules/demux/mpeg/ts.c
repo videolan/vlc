videolan/vlc

commit a6e0acd6687fa33d727c8c95a9e76b9b0c4afe03

ts: fix variable shadowing

modules/demux/mpeg/ts.c
@@ -1069,7 +1069,7 @@ static int Control( demux_t *p_demux, int i_query, va_list args )
             else // All ES Mode
             {
                 p_sys->b_es_all = true;
-                ts_pat_t *p_pat = GetPID(p_sys, 0)->u.p_pat;
+                p_pat = GetPID(p_sys, 0)->u.p_pat;
                 for( int i = 0; i < p_pat->programs.i_size; i++ )
                    ARRAY_APPEND( p_sys->programs, p_pat->programs.p_elems[i]->i_pid );
                 UpdatePESFilters( p_demux, true );
@@ -2140,10 +2140,10 @@ static void ProgramSetPCR( demux_t *p_demux, ts_pmt_t *p_pmt, mtime_t i_pcr )
         ts_pat_t *p_pat = GetPID(p_sys, 0)->u.p_pat;
         for( int i=0; i< p_pat->programs.i_size; i++ )
         {
-            ts_pmt_t *p_pmt = p_pat->programs.p_elems[i]->u.p_pmt;
-            for( int j=0; j<p_pmt->e_streams.i_size; j++ )
+            ts_pmt_t *p_opmt = p_pat->programs.p_elems[i]->u.p_pmt;
+            for( int j=0; j<p_opmt->e_streams.i_size; j++ )
             {
-                ts_pid_t *p_pid = p_pmt->e_streams.p_elems[j];
+                ts_pid_t *p_pid = p_opmt->e_streams.p_elems[j];
                 block_t *p_block = p_pid->u.p_stream->prepcr.p_head;
                 while( p_block && p_block->i_dts == VLC_TS_INVALID )
                     p_block = p_block->p_next;

modules/demux/mpeg/ts_psi.c
@@ -829,7 +829,7 @@ static void OpusSetup(demux_t *demux, uint8_t *p, size_t len, es_format_t *p_fmt
         csc = p_csc[channels - 1];
         stream_count = channels - csc;
 
-        static const uint8_t map[6][7] = {
+        static const uint8_t maps[6][7] = {
             { 2,1 },
             { 1,2,3 },
             { 4,1,2,3 },
@@ -838,7 +838,7 @@ static void OpusSetup(demux_t *demux, uint8_t *p, size_t len, es_format_t *p_fmt
             { 6,1,2,3,4,5,7 },
         };
         if (channels > 2)
-            memcpy(&h.stream_map[1], map[channels-3], channels - 1);
+            memcpy(&h.stream_map[1], maps[channels-3], channels - 1);
     } else if (ccc == 0x81) {
         if (len < 4)
             goto explicit_config_too_short;

commit 8660d84e72931aabf8aa630b06bf74c7a3ed5710

demux: ts: fix first dts comparison/pcr offset hack
regression by a6e0acd6687fa33d727c8c95a9e76b9b0c4afe03
refs IMAX.Blue.Planet.ts

modules/demux/mpeg/ts.c
@@ -2398,7 +2398,7 @@ static void PCRFixHandle( demux_t *p_demux, ts_pmt_t *p_pmt, block_t *p_block )
         return;
     }
     /* Record the first data packet timestamp in case there wont be any PCR */
-    else if( !p_pmt->pcr.i_first_dts )
+    else if( p_pmt->pcr.i_first_dts == TS_TICK_UNKNOWN )
     {
         p_pmt->pcr.i_first_dts = TO_SCALE(p_block->i_dts);
     }