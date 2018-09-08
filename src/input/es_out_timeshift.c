6977abc430a889e19348a55533884bad2c6b1fa5
Zhao Zhili <quinkblack@foxmail.com>
2018-09-08 18:41:58 +0800

    es_out_timeshift: fix inverted logic
    A test case:
    vlc -vv --rate 0.5 ~/.local/share/vlc/ml.xspf
    es_out_GetEmpty return false forever
    
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>
    (cherry picked from commit 80f1e7235b7b6a769e6924ed96c739b62f7e3385)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- src/input/es_out_timeshift.c
+++ src/input/es_out_timeshift.c
@@ -913,7 +913,7 @@ static bool TsHasCmd( ts_thread_t *p_ts )
     bool b_cmd;
 
     vlc_mutex_lock( &p_ts->lock );
-    b_cmd =  TsStorageIsEmpty( p_ts->p_storage_r );
+    b_cmd = !TsStorageIsEmpty( p_ts->p_storage_r );
     vlc_mutex_unlock( &p_ts->lock );
 
     return b_cmd;
