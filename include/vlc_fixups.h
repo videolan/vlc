f844c78b272a8e7d4087aa972c9ff7767200c67c
Victorien Le Couviour--Tuffet <victorien.lecouviour.tuffet@gmail.com>
2018-09-12 11:03:42 +0200

    vlc_fixups: fix struct if_nameindex definition
    Fixes android ndk17 build
    (cherry picked from commit 0481899b5d17a18d33491f554a8957b63727d864)
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- include/vlc_fixups.h
+++ include/vlc_fixups.h
@@ -412,11 +412,13 @@ int poll (struct pollfd *, unsigned, int);
 
 #ifndef HAVE_IF_NAMEINDEX
 #include <errno.h>
+# ifndef HAVE_STRUCT_IF_NAMEINDEX
 struct if_nameindex
 {
     unsigned if_index;
     char    *if_name;
 };
+# endif
 # ifndef HAVE_IF_NAMETOINDEX
 #  define if_nametoindex(name)   atoi(name)
 # endif
