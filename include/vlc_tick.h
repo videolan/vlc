90c53e85d37f1b0e0d2e8ef97bc94ccbe37ae872
Steve Lhomme <robux4@ycbcr.xyz>
2018-09-20 13:26:24 +0200

    vlc_tick: add helper functions to convert vlc_tick to/from samples
    Based on a sampling rate.

--- include/vlc_tick.h
+++ include/vlc_tick.h
@@ -95,6 +95,17 @@ static inline vlc_tick_t vlc_tick_rate_duration(float frame_rate)
     return CLOCK_FREQ / frame_rate;
 }
 
+/*
+ * samples<>vlc_tick_t
+ */
+static inline vlc_tick_t vlc_tick_from_samples(int64_t samples, int samp_rate)
+{
+    return CLOCK_FREQ * samples / samp_rate;
+}
+static inline int64_t samples_from_vlc_tick(vlc_tick_t t, int samp_rate)
+{
+    return t * samp_rate / CLOCK_FREQ;
+}
 
 /*
  * vlc_tick_t <> milliseconds (ms) conversions
