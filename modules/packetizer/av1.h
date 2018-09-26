7dc9305e83d919cc7010401d4b0c4885ba44d4b6
Francois Cartegnie <fcvlcdev@free.fr>
2018-09-26 14:36:53 +0200

    packetizer: add AV1 helpers
    (cherry picked from commit e9be573ff0ad162ac2ac545b7cbff491a102e4ad)

---
+++ modules/packetizer/av1.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * av1.h: AV1 definitions
+ *****************************************************************************
+ * Copyright (C) 2018 VideoLabs, VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+#ifndef VLC_AV1_H
+#define VLC_AV1_H
+
+enum av1_profile_e
+{
+    AV1_PROFILE_MAIN          = 0,
+    AV1_PROFILE_HIGH          = 1,
+    AV1_PROFILE_PROFESSIONNAL = 2,
+};
+
+#endif
