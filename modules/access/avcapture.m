fded66483be3cc108affbeef4e86f706950cfb3e
David Fuhrmann <dfuhrmann@videolan.org>
2018-10-21 18:19:53 +0200

    avcapture: Fix compilation with 10.13 SDK

--- modules/access/avcapture.m
+++ modules/access/avcapture.m
@@ -42,6 +42,12 @@
 #import <AVFoundation/AVFoundation.h>
 #import <CoreMedia/CoreMedia.h>
 
+@interface AVCaptureDevice (AVCaptureDeviceAuthorizationSince10_14)
+
++ (void)requestAccessForMediaType:(AVMediaType)mediaType completionHandler:(void (^)(BOOL granted))handler API_AVAILABLE(macos(10.14), ios(7.0));
+
+@end
+
 /*****************************************************************************
 * Local prototypes
 *****************************************************************************/
