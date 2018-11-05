bed5271a6736ec7cc1958051dfb21d8453179a5f
Steve Lhomme <robux4@ycbcr.xyz>
2018-11-05 09:52:47 +0100

    decoder: adjust colorspace for aout based video outputs
    It's done for video just before calling input_resource_RequestVout() as well.
    
    Fixes #21356
    
    (cherry picked from commit 3089bf5a33ec77794d047a44df3da2d1b9b1aeb4)

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -284,6 +284,14 @@ static vout_thread_t *aout_request_vout( void *p_private,
     decoder_t *p_dec = p_private;
     decoder_owner_sys_t *p_owner = p_dec->p_owner;
     input_thread_t *p_input = p_owner->p_input;
+    video_format_t fmt;
+
+    if (p_fmt != NULL)
+    {
+        fmt = *p_fmt;
+        p_fmt = &fmt;
+        video_format_AdjustColorSpace( &fmt );
+    }
 
     p_vout = input_resource_RequestVout( p_owner->p_resource, p_vout, p_fmt, 1,
                                          b_recyle );
