49db4557d54ff62b0dae37a30705be64f31c74c3
Thomas Guillem <thomas@gllm.fr>
2018-11-05 16:42:13 +0100

    filters: use void * for sys

--- src/audio_output/filters.c
+++ src/audio_output/filters.c
@@ -43,7 +43,7 @@
 #include "aout_internal.h"
 
 static filter_t *CreateFilter (vlc_object_t *obj, const char *type,
-                               const char *name, filter_owner_sys_t *owner,
+                               const char *name, void *sys,
                                const audio_sample_format_t *infmt,
                                const audio_sample_format_t *outfmt,
                                config_chain_t *cfg, bool const_fmt)
@@ -52,7 +52,7 @@ static filter_t *CreateFilter (vlc_object_t *obj, const char *type,
     if (unlikely(filter == NULL))
         return NULL;
 
-    filter->owner.sys = owner;
+    filter->owner.sys = sys;
     filter->p_cfg = cfg;
     filter->fmt_in.audio = *infmt;
     filter->fmt_in.i_codec = infmt->i_format;
