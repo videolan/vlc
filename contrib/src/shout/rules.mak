videolan/vlc

commit 63179341afc6b04961c602957bbd0fd61957af79

contrib: shout: Don't build examples nor doc
(cherry picked from commit ef8c7fc596569ce4757bc8e0240bc42503542184)
Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

contrib/src/shout/rules.mak
@@ -23,7 +23,7 @@ libshout: libshout-$(SHOUT_VERSION).tar.gz .sum-shout
 	$(APPLY) $(SRC)/shout/bsd.patch
 	$(APPLY) $(SRC)/shout/libshout-arpa.patch
 	$(APPLY) $(SRC)/shout/fix-xiph_openssl.patch
-	$(call pkg_static,"shout.pc.in")
+	$(APPLY) $(SRC)/shout/no-examples.patch
 	$(UPDATE_AUTOCONFIG)
 	$(MOVE)
 

contrib/src/shout/no-examples.patch
@@ -1,0 +1,11 @@
+--- libshout/Makefile.am.old	2018-11-22 12:46:19.971954640 +0100
++++ libshout/Makefile.am	2018-11-22 12:46:30.695954599 +0100
+@@ -3,7 +3,7 @@
+ AUTOMAKE_OPTIONS = 1.6 foreign
+ ACLOCAL_AMFLAGS = -I m4
+ 
+-SUBDIRS = include src examples doc win32
++SUBDIRS = include src win32
+ 
+ EXTRA_DIST = INSTALL m4/shout.m4 m4/acx_pthread.m4 \
+ 	m4/ogg.m4 m4/vorbis.m4 m4/xiph_compiler.m4 m4/xiph_net.m4 \

commit 8a33aea097a44854b49c816eafcf8f0e4d9fd69b

contrib: Restored pkg-config file fix.
Broken in 63179341afc6b04961c602957bbd0fd61957af79.

contrib/src/shout/rules.mak
@@ -24,6 +24,7 @@ libshout: libshout-$(SHOUT_VERSION).tar.gz .sum-shout
 	$(APPLY) $(SRC)/shout/libshout-arpa.patch
 	$(APPLY) $(SRC)/shout/fix-xiph_openssl.patch
 	$(APPLY) $(SRC)/shout/no-examples.patch
+	$(call pkg_static,"shout.pc.in")
 	$(UPDATE_AUTOCONFIG)
 	$(MOVE)
 