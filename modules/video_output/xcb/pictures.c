fdc57213fc257892c0d92212d65c41223424d057
Rémi Denis-Courmont <remi@remlab.net>
2018-12-09 20:38:53 +0200

    xcb: require MIT-SHM extension version 1.2

--- modules/video_output/xcb/pictures.c
+++ modules/video_output/xcb/pictures.c
@@ -48,23 +48,31 @@
 /** Check MIT-SHM shared memory support */
 bool XCB_shm_Check (vlc_object_t *obj, xcb_connection_t *conn)
 {
-#ifdef HAVE_SYS_SHM_H
     xcb_shm_query_version_cookie_t ck;
     xcb_shm_query_version_reply_t *r;
 
     ck = xcb_shm_query_version (conn);
     r = xcb_shm_query_version_reply (conn, ck, NULL);
-    if (r != NULL)
+    if (r == NULL)
     {
-        free (r);
-        return true;
+        msg_Err(obj, "MIT-SHM extension not available");
+        goto fail;
     }
-    msg_Err (obj, "shared memory (MIT-SHM) not available");
-    msg_Warn (obj, "display will be slow");
-#else
-    msg_Warn (obj, "shared memory (MIT-SHM) not implemented");
-    (void) conn;
-#endif
+
+    msg_Dbg(obj, "MIT-SHM extension version %"PRIu16".%"PRIu16,
+            r->major_version, r->minor_version);
+
+    if (r->major_version == 1 && r->minor_version < 2)
+    {
+        msg_Err(obj, "MIT-SHM extension too old");
+        free(r);
+        goto fail;
+    }
+
+    free (r);
+    return true;
+fail:
+    msg_Warn(obj, "display will be slow");
     return false;
 }
 
