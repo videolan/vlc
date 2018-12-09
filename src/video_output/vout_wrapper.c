a25c5a44a0d655f2eb09f7cab0ef492d3f3961ba
Rémi Denis-Courmont <remi@remlab.net>
2018-12-09 11:21:51 +0200

    vout: remove useless call
    vout_ManageDisplay() returns true if and only if the pictures were
    invalid. Both sides of the logical disjunction were equal.

--- src/video_output/vout_wrapper.c
+++ src/video_output/vout_wrapper.c
@@ -184,10 +184,7 @@ void vout_ManageWrapper(vout_thread_t *vout)
     vout_thread_sys_t *sys = vout->p;
     vout_display_t *vd = sys->display.vd;
 
-    bool reset_display_pool = vout_AreDisplayPicturesInvalid(vd);
-    reset_display_pool |= vout_ManageDisplay(vd);
-
-    if (reset_display_pool) {
+    if (vout_ManageDisplay(vd)) {
         sys->display.use_dr = !vout_IsDisplayFiltered(vd);
         NoDrInit(sys);
     }
