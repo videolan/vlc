2f43315f8863d47fcf908e55f7f20d93af712b81
Rémi Denis-Courmont <remi@remlab.net>
2018-12-11 22:44:39 +0200

    arm_neon: typo

--- modules/arm_neon/chroma_yuv.c
+++ modules/arm_neon/chroma_yuv.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * chroma_yuv.c : ARM NEONv1 YUV 4:2:0 to YUV :2:2 chroma conversion for VLC
+ * chroma_yuv.c : ARM NEONv1 YUV 4:2:0 to YUV 4:2:2 chroma conversion for VLC
  *****************************************************************************
  * Copyright (C) 2009 Rémi Denis-Courmont
  *
