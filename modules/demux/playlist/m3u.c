cd1929d40e9aed06ba48be69f1de2e0c4dbc1730
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-12-19 13:41:51 +0100

    m3u: Don't copy option from NULL current input
    refs #19928

--- modules/demux/playlist/m3u.c
+++ modules/demux/playlist/m3u.c
@@ -312,7 +312,8 @@ static int ReadDir( stream_t *p_demux, input_item_node_t *p_subitems )
             if( !p_input )
                 goto error;
             input_item_AddOptions( p_input, i_options, ppsz_options, 0 );
-            input_item_CopyOptions( p_input, p_current_input );
+            if ( p_current_input )
+                input_item_CopyOptions( p_input, p_current_input );
 
             if( !EMPTY_STR(psz_artist) )
                 input_item_SetArtist( p_input, psz_artist );
