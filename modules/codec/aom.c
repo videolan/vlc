e033427f8d5ea4a2afe45de7286820cc1b53472a
Steve Lhomme <robux4@ycbcr.xyz>
2019-01-09 11:30:58 +0100

    dav1d/aom: forward the colorimetry metadata from the container to the output
    If there is none and the stream contains some, it will be overridden in
    NewPicture() otherwise the container values are used.
    
    Fixes #21578
    
    (cherry picked from commit b59710fbdf8f2f985b675bb5d6085b1459cc5f8c)

--- modules/codec/aom.c
+++ modules/codec/aom.c
@@ -327,6 +327,10 @@ static int OpenDecoder(vlc_object_t *p_this)
         dec->fmt_out.video.i_sar_num = dec->fmt_in.video.i_sar_num;
         dec->fmt_out.video.i_sar_den = dec->fmt_in.video.i_sar_den;
     }
+    dec->fmt_out.video.primaries   = dec->fmt_in.video.primaries;
+    dec->fmt_out.video.transfer    = dec->fmt_in.video.transfer;
+    dec->fmt_out.video.space       = dec->fmt_in.video.space;
+    dec->fmt_out.video.b_color_range_full = dec->fmt_in.video.b_color_range_full;
 
     return VLC_SUCCESS;
 }
