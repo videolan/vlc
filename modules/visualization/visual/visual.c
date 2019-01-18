a010b59b0bdf6bb2e33511ec1299214a231dfa6e
Rémi Denis-Courmont <remi@remlab.net>
2019-01-18 23:09:21 +0200

    visual: use vout_Close() directly

--- modules/visualization/visual/visual.c
+++ modules/visualization/visual/visual.c
@@ -311,7 +311,7 @@ static int Open( vlc_object_t *p_this )
     p_sys->fifo = block_FifoNew();
     if( unlikely( p_sys->fifo == NULL ) )
     {
-        aout_filter_PutVout( p_filter, p_sys->p_vout );
+        vout_Close( p_sys->p_vout );
         goto error;
     }
 
@@ -319,7 +319,7 @@ static int Open( vlc_object_t *p_this )
                    VLC_THREAD_PRIORITY_VIDEO ) )
     {
         block_FifoRelease( p_sys->fifo );
-        aout_filter_PutVout( p_filter, p_sys->p_vout );
+        vout_Close( p_sys->p_vout );
         goto error;
     }
 
@@ -407,7 +407,7 @@ static void Close( vlc_object_t *p_this )
     vlc_cancel( p_sys->thread );
     vlc_join( p_sys->thread, NULL );
     block_FifoRelease( p_sys->fifo );
-    aout_filter_PutVout( p_filter, p_sys->p_vout );
+    vout_Close( p_sys->p_vout );
 
     /* Free the list */
     for( int i = 0; i < p_sys->i_effect; i++ )
