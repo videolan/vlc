videolan/vlc

commit ec474d508320acbb0e765fc5822b953524531ffc

win32: convert timer ticks to milliseconds using MS_FROM_VLC_TICK()

src/win32/timer.c
@@ -83,7 +83,8 @@ void vlc_timer_schedule (vlc_timer_t timer, bool absolute,
     interval = (interval + 999) / 1000;
 
     if (!CreateTimerQueueTimer (&timer->handle, NULL, vlc_timer_do, timer,
-                                value, interval, WT_EXECUTEDEFAULT))
+                                MS_FROM_VLC_TICK(value), MS_FROM_VLC_TICK(interval),
+                                WT_EXECUTEDEFAULT))
         abort ();
 }
 

commit 903a3b56445420fab2a952f8ab2aaa6e75704d7b

win32: fix timer expiration (fixes #21124)
Regression from ec474d508320acbb0e765fc5822b953524531ffc.

src/win32/timer.c
@@ -79,12 +79,10 @@ void vlc_timer_schedule (vlc_timer_t timer, bool absolute,
         if (value < 0)
             value = 0;
     }
-    value = (value + (1000-1)) / 1000;
-    interval = (interval + (1000-1)) / 1000;
 
-    if (!CreateTimerQueueTimer (&timer->handle, NULL, vlc_timer_do, timer,
-                                MS_FROM_VLC_TICK(value), MS_FROM_VLC_TICK(interval),
-                                WT_EXECUTEDEFAULT))
+    if (!CreateTimerQueueTimer(&timer->handle, NULL, vlc_timer_do, timer,
+                               MS_FROM_VLC_TICK(value),
+                               MS_FROM_VLC_TICK(interval), WT_EXECUTEDEFAULT))
         abort ();
 }
 