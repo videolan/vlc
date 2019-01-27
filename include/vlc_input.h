videolan/vlc

commit cad5bc0a9d86797b0ea50036a6fa14e515e16e09

input_internal: constify pointer to master source titles

src/input/input_internal.h
@@ -114,7 +114,7 @@ typedef struct input_thread_private_t
 
     /* Title infos FIXME multi-input (not easy) ? */
     int          i_title;
-    input_title_t **title;
+    const input_title_t **title;
 
     int i_title_offset;
     int i_seekpoint_offset;

commit cafb0b8c8da26434123515ca3b98ebd59549f527

input: fix invalid title array pointer conversion
Regression from cad5bc0a9d86797b0ea50036a6fa14e515e16e09.

include/vlc_input.h
@@ -421,7 +421,7 @@ struct vlc_input_event_title
     {
         struct
         {
-            const input_title_t **array;
+            input_title_t *const *array;
             size_t count;
         } list;
         size_t selected_idx;

src/input/input_internal.h
@@ -154,7 +154,7 @@ typedef struct input_thread_private_t
 
     /* Title infos FIXME multi-input (not easy) ? */
     int          i_title;
-    const input_title_t **title;
+    input_title_t *const *title;
 
     int i_title_offset;
     int i_seekpoint_offset;

src/input/player.c
@@ -549,7 +549,7 @@ seekpoint_GetName(seekpoint_t *seekpoint, int idx, int chapter_offset)
 }
 
 static struct vlc_player_title_list *
-vlc_player_title_list_Create(const input_title_t **array, size_t count,
+vlc_player_title_list_Create(input_title_t *const *array, size_t count,
                              int title_offset, int chapter_offset)
 {
     if (count == 0)