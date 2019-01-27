videolan/vlc

commit d45bc25e13898aeb0096b933572a9d4d9f30f590

contrib: protobuf: check the protoc version matches the protobuf version

contrib/src/protobuf/rules.mak
@@ -5,6 +5,21 @@ PROTOBUF_URL := https://github.com/google/protobuf/releases/download/v$(PROTOBUF
 PKGS += protobuf
 ifeq ($(call need_pkg, "protobuf-lite >= 3.1.0 protobuf-lite < 3.2.0"),)
 PKGS_FOUND += protobuf
+else
+ifeq ($(findstring protobuf,$(PKGS_DISABLE)),)
+# check we have a matching protoc to use
+PROTOC = $(shell PATH="$(PATH)" which protoc)
+ifeq ($(PROTOC),)
+$(error protoc not found in PATH $(PATH) - $(SYS_PROTOC) - $(PROTOC))
+else
+# make sure the installed protoc is compatible with the version we want to build
+SYS_PROTOC_VER = $(shell $(PROTOC) --version)
+SYS_PROTOC = $(word $(words $(SYS_PROTOC_VER)) , $(SYS_PROTOC_VER))
+ifneq ($(PROTOBUF_VERSION),$(SYS_PROTOC))
+$(error $(PROTOC) version $(SYS_PROTOC) doesn't match the protobuf $(PROTOBUF_VERSION) we're building)
+endif
+endif
+endif
 endif
 
 $(TARBALLS)/protobuf-$(PROTOBUF_VERSION)-cpp.tar.gz:

commit 848eaf76791e9a81837b849c688cd72eef4703b3

contrib: protoc: do not break the whole build
So what if protoc does not matc? I should still be able to build almost
everything that does not depend on this crapware.

Regression from d45bc25e13898aeb0096b933572a9d4d9f30f590.

contrib/src/protobuf/rules.mak
@@ -10,13 +10,13 @@ ifeq ($(findstring protobuf,$(PKGS_DISABLE)),)
 # check we have a matching protoc to use
 PROTOC = $(shell PATH="$(PATH)" which protoc)
 ifeq ($(PROTOC),)
-$(error protoc not found in PATH $(PATH) - $(SYS_PROTOC) - $(PROTOC))
+PROTOC = $(error protoc not found in PATH $(PATH) - $(SYS_PROTOC) - $(PROTOC))
 else
 # make sure the installed protoc is compatible with the version we want to build
 SYS_PROTOC_VER = $(shell $(PROTOC) --version)
 SYS_PROTOC = $(word $(words $(SYS_PROTOC_VER)) , $(SYS_PROTOC_VER))
 ifneq ($(PROTOBUF_VERSION),$(SYS_PROTOC))
-$(error $(PROTOC) version $(SYS_PROTOC) doesn't match the protobuf $(PROTOBUF_VERSION) we're building)
+PROTOC = $(error $(PROTOC) version $(SYS_PROTOC) doesn't match the protobuf $(PROTOBUF_VERSION) we're building)
 endif
 endif
 endif