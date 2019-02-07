283253e21f295d230110d287b0be03aa6e74be74
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-02-07 11:28:11 +0100

    mkv: Use existing intermediate variable
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- modules/demux/mkv/util.cpp
+++ modules/demux/mkv/util.cpp
@@ -191,7 +191,7 @@ void handle_real_audio(demux_t * p_demux, mkv_track_t * p_tk, block_t * p_blk, v
         p_tk->fmt.i_codec == VLC_CODEC_ATRAC3 )
     {
         const uint16_t i_num = p_sys->i_frame_size / p_sys->i_subpacket_size;
-        const size_t y = p_sys->i_subpacket / ( p_sys->i_frame_size / p_sys->i_subpacket_size );
+        const size_t y = p_sys->i_subpacket / i_num;
 
         for( uint16_t i = 0; i < i_num; i++ )
         {
