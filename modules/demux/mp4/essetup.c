videolan/vlc

commit 3d24a401e710186220dac597074dfc2ae81ec334

demux: mp4: set bitmap mask when possible

modules/demux/mp4/essetup.c
@@ -1195,13 +1195,22 @@ int SetupAudioES( demux_t *p_demux, mp4_track_t *p_track, MP4_Box_t *p_sample )
             msg_Warn( p_demux, "discarding chan mapping" );
         }
 
-        if( aout_BitsPerSample( p_track->fmt.i_codec ) && i_vlc_mapping > 0 &&
-            aout_CheckChannelReorder( p_rg_chans_order, NULL, i_vlc_mapping,
-                                      p_track->rgi_chans_reordering ) )
+        if( i_vlc_mapping )
         {
-            p_track->b_chans_reorder = true;
-            p_track->fmt.audio.i_channels = i_channels;
-            p_track->fmt.audio.i_physical_channels = i_vlc_mapping;
+            const unsigned i_bps = aout_BitsPerSample( p_track->fmt.i_codec );
+            /* Uncompressed audio */
+            if( i_bps && aout_CheckChannelReorder( p_rg_chans_order, NULL,
+                                                   i_vlc_mapping,
+                                                   p_track->rgi_chans_reordering ) )
+                p_track->b_chans_reorder = true;
+
+            /* we can only set bitmap for VLC mapping or [re]mapped pcm audio
+             * as vlc can't enumerate channels for compressed content */
+            if( i_bps || p_track->rgi_chans_reordering == NULL )
+            {
+                p_track->fmt.audio.i_channels = i_channels;
+                p_track->fmt.audio.i_physical_channels = i_vlc_mapping;
+            }
         }
     }
 

commit 4f258411c8e0d54a2028a5c49e240c76c3abdccd

demux: mp4: fix g711 regression (missing channels count)
introduced with 3d24a401e710186220dac597074dfc2ae81ec334

modules/demux/mp4/essetup.c
@@ -1214,7 +1214,7 @@ int SetupAudioES( demux_t *p_demux, mp4_track_t *p_track, MP4_Box_t *p_sample )
              * as vlc can't enumerate channels for compressed content */
             if( i_bps || p_track->rgi_chans_reordering == NULL )
             {
-                p_track->fmt.audio.i_channels = i_channels;
+                p_track->fmt.audio.i_channels = vlc_popcount(i_vlc_mapping);
                 p_track->fmt.audio.i_physical_channels = i_vlc_mapping;
             }
         }