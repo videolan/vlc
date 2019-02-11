d1d41b3f849d4e260c1ec09c5b1bb53db7f8107f
Steve Lhomme <robux4@ycbcr.xyz>
2019-02-11 09:00:06 +0100

    demux:mkv: fix wrong value reset after clean of an array
    https://hackerone.com/reports/491495

--- modules/demux/mkv/util.cpp
+++ modules/demux/mkv/util.cpp
@@ -178,6 +178,7 @@ void handle_real_audio(demux_t * p_demux, mkv_track_t * p_tk, block_t * p_blk, v
                 p_sys->p_subpackets[i] = NULL;
             }
         p_sys->i_subpacket = 0;
+        p_sys->i_subpackets = 0;
 
         if ( !( p_blk->i_flags & BLOCK_FLAG_TYPE_I) )
         {
@@ -237,6 +238,7 @@ void handle_real_audio(demux_t * p_demux, mkv_track_t * p_tk, block_t * p_blk, v
             p_sys->p_subpackets[i] = NULL;
         }
         p_sys->i_subpacket = 0;
+        p_sys->i_subpackets = 0;
     }
 }
 
