aff97b8cd21887b0bcc121f0eec9afe74ce0850e
Thomas Guillem <thomas@gllm.fr>
2019-02-20 18:40:08 +0100

    test: demux-libfuzzer: initialize libvlc only once

--- test/vlc-demux-libfuzzer.c
+++ test/vlc-demux-libfuzzer.c
@@ -37,18 +37,20 @@ int LLVMFuzzerInitialize(int *argc, char ***argv);
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size);
 
 static struct vlc_run_args args;
+static libvlc_instance_t *vlc;
 
 int LLVMFuzzerInitialize(int *argc, char ***argv)
 {
     (void) argc; (void) argv;
 
     vlc_run_args_init(&args);
+    vlc = libvlc_create(&args);
 
-    return 0;
+    return vlc ? 0 : -1;
 }
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
-    vlc_demux_process_memory(&args, data, size);
+    libvlc_demux_process_memory(vlc, &args, data, size);
     return 0;
 }
