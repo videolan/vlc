c31b32988afd3e003a48f7c77b6cde6132ec8649
Simon Latapie <garf@videolan.org>
2019-02-27 13:13:33 +0100

    mkv: fix GotoAndPlay command uid conversion
    Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>
    (cherry picked from commit 258eec08e62020ac8f89243238da27a715897a4c)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- modules/demux/mkv/chapter_command.cpp
+++ modules/demux/mkv/chapter_command.cpp
@@ -716,7 +716,7 @@ bool matroska_script_interpretor_c::Interpret( const binary * p_command, size_t 
         }
 
         std::string st = sz_command.substr( i+1, j-i-1 );
-        int64_t i_chapter_uid = atoi( st.c_str() );
+        int64_t i_chapter_uid = atoll( st.c_str() );
 
         virtual_segment_c *p_vsegment;
         virtual_chapter_c *p_vchapter = sys.FindChapter( i_chapter_uid, p_vsegment );
