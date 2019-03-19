videolan/vlc

commit 2049d408765d5f0209444025c21833624935ca37

aout: use a separate drain callback
This callback is not mandatory. If it is NULL, the core will wait for the delay
returned by time_get(). This was already the case for most aout plugins:
PulseAudio, coreaudio, Android, DirectSound, Wasapi, and Jack.

modules/audio_output/pulse.c
@@ -533,32 +533,38 @@ static void Pause(audio_output_t *aout, bool paused, vlc_tick_t date)
 /**
  * Flush or drain the playback stream
  */
-static void Flush(audio_output_t *aout, bool wait)
+static void Flush(audio_output_t *aout)
 {
     aout_sys_t *sys = aout->sys;
     pa_stream *s = sys->stream;
-    pa_operation *op;
 
     pa_threaded_mainloop_lock(sys->mainloop);
 
-    if (wait)
-    {
-        op = pa_stream_drain(s, NULL, NULL);
-
-        /* XXX: Loosy drain emulation.
-         * See #18141: drain callback is never received */
-        vlc_tick_t delay;
-        if (TimeGet(aout, &delay) == 0 && delay <= VLC_TICK_FROM_SEC(5))
-            vlc_tick_sleep(delay);
-    }
-    else
-        op = pa_stream_flush(s, NULL, NULL);
+    pa_operation *op = pa_stream_flush(s, NULL, NULL);
     if (op != NULL)
         pa_operation_unref(op);
     sys->last_date = VLC_TICK_INVALID;
     stream_stop(s, aout);
 
     pa_threaded_mainloop_unlock(sys->mainloop);
+}
+
+static void Drain(audio_output_t *aout)
+{
+    aout_sys_t *sys = aout->sys;
+    pa_stream *s = sys->stream;
+
+    pa_threaded_mainloop_lock(sys->mainloop);
+    pa_operation *op = pa_stream_drain(s, NULL, NULL);
+    if (op != NULL)
+        pa_operation_unref(op);
+    pa_threaded_mainloop_unlock(sys->mainloop);
+
+    /* XXX: Loosy drain emulation.
+     * See #18141: drain callback is never received */
+    vlc_tick_t delay;
+    if (TimeGet(aout, &delay) == 0 && delay <= VLC_TICK_FROM_SEC(5))
+        vlc_tick_sleep(delay);
 }
 
 static int VolumeSet(audio_output_t *aout, float vol)
@@ -1006,6 +1012,7 @@ static int Open(vlc_object_t *obj)
     aout->play = Play;
     aout->pause = Pause;
     aout->flush = Flush;
+    aout->drain = Drain;
     aout->volume_set = VolumeSet;
     aout->mute_set = MuteSet;
     aout->device_select = StreamMove;

include/vlc_aout.h
@@ -212,13 +212,19 @@ struct audio_output
       * \note This callback cannot be called in stopped state.
       */
 
-    void (*flush)( audio_output_t *, bool wait);
-    /**< Flushes or drains the playback buffers (mandatory, cannot be NULL).
+    void (*flush)( audio_output_t *);
+    /**< Flushes the playback buffers (mandatory, cannot be NULL).
       *
       * \param wait true to wait for playback of pending buffers (drain),
       *             false to discard pending buffers (flush)
       *
       * \note This callback cannot be called in stopped state.
+      */
+    void (*drain)(audio_output_t *);
+    /**< Drain the playback buffers (can be NULL).
+      *
+      * If NULL, the caller will wait for the delay returned by time_get before
+      * calling stop().
       */
 
     int (*volume_set)(audio_output_t *, float volume);
@@ -456,7 +462,7 @@ static inline void aout_PauseDefault(audio_output_t *aout, bool paused,
                                      vlc_tick_t date)
 {
     if (paused && aout->flush != NULL)
-        aout->flush(aout, false);
+        aout->flush(aout);
     (void) date;
 }
 

modules/audio_output/adummy.c
@@ -52,9 +52,9 @@ static void Pause(audio_output_t *aout, bool paused, vlc_tick_t date)
     (void) aout; (void) paused; (void) date;
 }
 
-static void Flush(audio_output_t *aout, bool wait)
+static void Flush(audio_output_t *aout)
 {
-    (void) aout; (void) wait;
+    (void) aout;
 }
 
 static int Start(audio_output_t *aout, audio_sample_format_t *restrict fmt)

modules/audio_output/alsa.c
@@ -294,7 +294,8 @@ static int TimeGet (audio_output_t *aout, vlc_tick_t *);
 static void Play(audio_output_t *, block_t *, vlc_tick_t);
 static void Pause (audio_output_t *, bool, vlc_tick_t);
 static void PauseDummy (audio_output_t *, bool, vlc_tick_t);
-static void Flush (audio_output_t *, bool);
+static void Flush (audio_output_t *);
+static void Drain (audio_output_t *);
 
 /** Initializes an ALSA playback stream */
 static int Start (audio_output_t *aout, audio_sample_format_t *restrict fmt)
@@ -714,20 +715,26 @@ static void PauseDummy (audio_output_t *aout, bool pause, vlc_tick_t date)
 }
 
 /**
- * Flushes/drains the audio playback buffer.
+ * Flushes the audio playback buffer.
  */
-static void Flush (audio_output_t *aout, bool wait)
+static void Flush (audio_output_t *aout)
 {
     aout_sys_t *p_sys = aout->sys;
     snd_pcm_t *pcm = p_sys->pcm;
-
-    if (wait)
-        snd_pcm_drain (pcm);
-    else
-        snd_pcm_drop (pcm);
+    snd_pcm_drop (pcm);
     snd_pcm_prepare (pcm);
 }
 
+/**
+ * Drains the audio playback buffer.
+ */
+static void Drain (audio_output_t *aout)
+{
+    aout_sys_t *p_sys = aout->sys;
+    snd_pcm_t *pcm = p_sys->pcm;
+    snd_pcm_drain (pcm);
+    snd_pcm_prepare (pcm);
+}
 
 /**
  * Releases the audio output.
@@ -851,6 +858,7 @@ static int Open(vlc_object_t *obj)
     aout->time_get = TimeGet;
     aout->play = Play;
     aout->flush = Flush;
+    aout->drain = Drain;
 
     return VLC_SUCCESS;
 error:

modules/audio_output/amem.c
@@ -104,17 +104,25 @@ static void Pause (audio_output_t *aout, bool paused, vlc_tick_t date)
     }
 }
 
-static void Flush (audio_output_t *aout, bool wait)
+static void Flush (audio_output_t *aout)
 {
     aout_sys_t *sys = aout->sys;
-    void (*cb) (void *) = wait ? sys->drain : sys->flush;
 
-    if (cb != NULL)
+    if (sys->flush != NULL)
     {
         vlc_mutex_lock(&sys->lock);
-        cb (sys->opaque);
+        sys->flush (sys->opaque);
         vlc_mutex_unlock(&sys->lock);
     }
+}
+
+static void Drain (audio_output_t *aout)
+{
+    aout_sys_t *sys = aout->sys;
+
+    vlc_mutex_lock(&sys->lock);
+    sys->drain (sys->opaque);
+    vlc_mutex_unlock(&sys->lock);
 }
 
 static int VolumeSet (audio_output_t *aout, float vol)
@@ -313,6 +321,7 @@ static int Open (vlc_object_t *obj)
     aout->play = Play;
     aout->pause = Pause;
     aout->flush = Flush;
+    aout->drain = sys->drain ? Drain : NULL;
     if (sys->set_volume != NULL)
     {
         aout->volume_set = VolumeSet;

modules/audio_output/audiotrack.c
@@ -2040,7 +2040,7 @@ bailout:
 }
 
 static void
-Flush( audio_output_t *p_aout, bool b_wait )
+Flush( audio_output_t *p_aout )
 {
     aout_sys_t *p_sys = p_aout->sys;
     JNIEnv *env;
@@ -2061,25 +2061,10 @@ Flush( audio_output_t *p_aout, bool b_wait )
      * that has not been played back will be discarded.  No-op if not stopped
      * or paused, or if the track's creation mode is not MODE_STREAM.
      */
-    if( b_wait )
-    {
-        /* Wait for the thread to process the circular buffer */
-        while( !p_sys->b_error
-            && p_sys->circular.i_read != p_sys->circular.i_write )
-            vlc_cond_wait( &p_sys->aout_cond, &p_sys->lock );
-        if( p_sys->b_error )
-            goto bailout;
-
-        JNI_AT_CALL_VOID( stop );
-        if( CHECK_AT_EXCEPTION( "stop" ) )
-            goto bailout;
-    } else
-    {
-        JNI_AT_CALL_VOID( pause );
-        if( CHECK_AT_EXCEPTION( "pause" ) )
-            goto bailout;
-        JNI_AT_CALL_VOID( flush );
-    }
+    JNI_AT_CALL_VOID( pause );
+    if( CHECK_AT_EXCEPTION( "pause" ) )
+        goto bailout;
+    JNI_AT_CALL_VOID( flush );
     p_sys->circular.i_read = p_sys->circular.i_write = 0;
 
     /* HACK: Before Android 4.4, the head position is not reset to zero and is

modules/audio_output/audiounit_ios.m
@@ -409,15 +409,7 @@ Pause (audio_output_t *p_aout, bool pause, vlc_tick_t date)
      * that we loose 1-2 sec of audio when resuming. The order is important
      * here, ca_Flush need to be called when paused. */
     if (pause)
-        ca_Flush(p_aout, false);
-}
-
-static void
-Flush(audio_output_t *p_aout, bool wait)
-{
-    aout_sys_t * p_sys = p_aout->sys;
-
-    ca_Flush(p_aout, wait);
+        ca_Flush(p_aout);
 }
 
 static int
@@ -430,7 +422,7 @@ MuteSet(audio_output_t *p_aout, bool mute)
     {
         Pause(p_aout, mute, 0);
         if (mute)
-            ca_Flush(p_aout, false);
+            ca_Flush(p_aout);
     }
 
     return VLC_SUCCESS;
@@ -562,7 +554,6 @@ Start(audio_output_t *p_aout, audio_sample_format_t *restrict fmt)
     fmt->channel_type = AUDIO_CHANNEL_TYPE_BITMAP;
     p_aout->mute_set  = MuteSet;
     p_aout->pause = Pause;
-    p_aout->flush = Flush;
 
     aout_SoftVolumeStart( p_aout );
 

modules/audio_output/coreaudio_common.c
@@ -230,42 +230,21 @@ ca_TimeGet(audio_output_t *p_aout, vlc_tick_t *delay)
 }
 
 void
-ca_Flush(audio_output_t *p_aout, bool wait)
+ca_Flush(audio_output_t *p_aout)
 {
     struct aout_sys_common *p_sys = (struct aout_sys_common *) p_aout->sys;
 
     lock_lock(p_sys);
-    if (wait)
-    {
-        while (p_sys->i_out_size > 0)
-        {
-            if (p_sys->b_paused)
-            {
-                ca_ClearOutBuffers(p_aout);
-                break;
-            }
 
-            /* Calculate the duration of the circular buffer, in order to wait
-             * for the render thread to play it all */
-            const vlc_tick_t i_frame_us =
-                FramesToUs(p_sys, BytesToFrames(p_sys, p_sys->i_out_size)) + VLC_TICK_FROM_MS(10);
-            lock_unlock(p_sys);
-            vlc_tick_sleep(i_frame_us);
-            lock_lock(p_sys);
-        }
-    }
+    assert(!p_sys->b_do_flush);
+    if (p_sys->b_paused)
+        ca_ClearOutBuffers(p_aout);
     else
     {
-        assert(!p_sys->b_do_flush);
-        if (p_sys->b_paused)
-            ca_ClearOutBuffers(p_aout);
-        else
-        {
-            p_sys->b_do_flush = true;
-            lock_unlock(p_sys);
-            vlc_sem_wait(&p_sys->flush_sem);
-            lock_lock(p_sys);
-        }
+        p_sys->b_do_flush = true;
+        lock_unlock(p_sys);
+        vlc_sem_wait(&p_sys->flush_sem);
+        lock_lock(p_sys);
     }
 
     p_sys->i_render_host_time = 0;

commit e9f52b29c6a5892bfa5ab93c476c2aabe60b45b3

pulse: restore previous drain behavior
Regression from 2049d408765d5f0209444025c21833624935ca37

Forgot to add it back after Rémi mentionned it.

modules/audio_output/pulse.c
@@ -558,13 +558,20 @@ static void Drain(audio_output_t *aout)
     pa_operation *op = pa_stream_drain(s, NULL, NULL);
     if (op != NULL)
         pa_operation_unref(op);
-    pa_threaded_mainloop_unlock(sys->mainloop);
+    sys->last_date = VLC_TICK_INVALID;
 
     /* XXX: Loosy drain emulation.
      * See #18141: drain callback is never received */
     vlc_tick_t delay;
     if (TimeGet(aout, &delay) == 0 && delay <= VLC_TICK_FROM_SEC(5))
+    {
+        pa_threaded_mainloop_unlock(sys->mainloop);
         vlc_tick_sleep(delay);
+        pa_threaded_mainloop_lock(sys->mainloop);
+    }
+
+    stream_stop(s, aout);
+    pa_threaded_mainloop_unlock(sys->mainloop);
 }
 
 static int VolumeSet(audio_output_t *aout, float vol)