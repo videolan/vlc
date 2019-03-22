92654192d6c912a0448016d397d87be39aa95fdb
Lyndon Brown <jnqnfe@gmail.com>
2019-03-22 23:22:54 +0000

    dcp: fix options being hidden in GUI
    options need to be specified after the cat+subcat declarations otherwise
    they may be ignored when constructing the preferences tree in Qt (and
    possibly others).
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/access/dcp/dcp.cpp
+++ modules/access/dcp/dcp.cpp
@@ -76,11 +76,11 @@ static void Close( vlc_object_t * );
 vlc_module_begin()
     set_shortname( N_( "DCP" ) )
     add_shortcut( "dcp" )
-    add_loadfile("kdm", "", KDM_HELP_TEXT, KDM_HELP_LONG_TEXT)
     set_description( N_( "Digital Cinema Package module" ) )
     set_capability( "access", 0 )
     set_category( CAT_INPUT )
     set_subcategory( SUBCAT_INPUT_ACCESS )
+    add_loadfile("kdm", "", KDM_HELP_TEXT, KDM_HELP_LONG_TEXT)
     set_callbacks( Open, Close )
 vlc_module_end()
 
