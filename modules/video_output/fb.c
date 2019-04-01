5c35442d13fcc1ad0793770929b742376b4e506e
Rémi Denis-Courmont <remi@remlab.net>
2019-04-01 19:52:31 +0300

    fb: provide dummy display control callback

--- modules/video_output/fb.c
+++ modules/video_output/fb.c
@@ -343,9 +343,19 @@ static void Display(vout_display_t *vd, picture_t *picture)
 
     picture_Copy(sys->picture, picture);
 }
+
 static int Control(vout_display_t *vd, int query, va_list args)
 {
-    (void) vd; (void) query; (void) args;
+    (void) vd; (void) args;
+
+    switch (query) {
+        case VOUT_DISPLAY_CHANGE_DISPLAY_SIZE:
+        case VOUT_DISPLAY_CHANGE_DISPLAY_FILLED:
+        case VOUT_DISPLAY_CHANGE_ZOOM:
+        case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
+        case VOUT_DISPLAY_CHANGE_SOURCE_CROP:
+            return VLC_SUCCESS;
+    }
     return VLC_EGENERIC;
 }
 
