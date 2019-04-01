5f7094b8bb9c67481de6f9bb611464aff0f9e7cd
Rémi Denis-Courmont <remi@remlab.net>
2019-04-01 19:52:15 +0300

    caca: ignore unsupported resize controls

--- modules/video_output/caca.c
+++ modules/video_output/caca.c
@@ -177,16 +177,15 @@ static int Control(vout_display_t *vd, int query, va_list args)
     (void) args;
 
     switch (query) {
-    case VOUT_DISPLAY_CHANGE_DISPLAY_SIZE:
-    case VOUT_DISPLAY_CHANGE_ZOOM:
-    case VOUT_DISPLAY_CHANGE_DISPLAY_FILLED:
-    case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
-        return VLC_EGENERIC;
-
     case VOUT_DISPLAY_CHANGE_SOURCE_CROP:
         if (sys->dither)
             cucul_free_dither(sys->dither);
         sys->dither = NULL;
+        /* fall through */
+    case VOUT_DISPLAY_CHANGE_DISPLAY_SIZE:
+    case VOUT_DISPLAY_CHANGE_ZOOM:
+    case VOUT_DISPLAY_CHANGE_DISPLAY_FILLED:
+    case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
         return VLC_SUCCESS;
 
     default:
