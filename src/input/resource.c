videolan/vlc

commit 4586ffaf28c0756e48d0fa607083417dda630a2d

resource: do not destroy vout when starting fails

src/input/resource.c
@@ -399,7 +399,12 @@ vout_thread_t *input_resource_GetVout(input_resource_t *p_resource,
         vlc_mutex_unlock(&p_resource->lock_hold);
     }
 
-    if (vout_Request(cfg, p_resource->p_input) == 0) {
+    if (vout_Request(cfg, p_resource->p_input)) {
+        vlc_mutex_unlock(&p_resource->lock);
+        input_resource_PutVout(p_resource, vout);
+        return NULL;
+    }
+
         vout =  cfg->vout;
         DisplayVoutTitle(p_resource, vout);
 
@@ -418,10 +423,6 @@ vout_thread_t *input_resource_GetVout(input_resource_t *p_resource,
                     .action = VLC_INPUT_EVENT_VOUT_ADDED,
                     .vout = vout,
                 });
-    } else {
-       vout_Close(vout);
-       vout = NULL;
-    }
 out:
     vlc_mutex_unlock( &p_resource->lock );
     return vout;

commit 3505ecb9d6b3edb4e68322cedb3dfe9ee895ff5b

vout: fix uninitialized pointer dereference
A failing vout_Request() on a re-used vout caused to save an invalid vout
pointer.

This also fixes the following warning: ‘vout’ may be used uninitialized in this
function [-Wmaybe-uninitialized] input_resource_PutVout(p_resource, vout);

Regression from 4586ffaf28c0756e48d0fa607083417dda630a2d

src/input/resource.c
@@ -401,7 +401,7 @@ vout_thread_t *input_resource_GetVout(input_resource_t *p_resource,
 
     if (vout_Request(cfg, p_resource->p_input)) {
         vlc_mutex_unlock(&p_resource->lock);
-        input_resource_PutVout(p_resource, vout);
+        input_resource_PutVout(p_resource, cfg->vout);
         return NULL;
     }
 