212cd26500ceffe0d7155ca1d37793336c561ac4
Steve Lhomme <robux4@ycbcr.xyz>
2019-04-03 09:09:11 +0200

    skins2: fix leak
    We actually want a WCHAR when calling the Win32 API. And we need to release the
    string we just allocated.

--- modules/gui/skins2/win32/win32_popup.cpp
+++ modules/gui/skins2/win32/win32_popup.cpp
@@ -73,10 +73,11 @@ void Win32Popup::addItem( const std::string &rLabel, int pos )
 //     menuItem.fType = MFT_STRING;
     menuItem.fMask = MIIM_ID | MIIM_STRING;
     menuItem.wID = pos;
-    menuItem.dwTypeData = ToT(rLabel.c_str());
+    menuItem.dwTypeData = ToWide(rLabel.c_str());
     menuItem.cch = rLabel.size();
 
     InsertMenuItem( m_hMenu, findInsertionPoint( pos ), TRUE, &menuItem );
+    free(menuItem.dwTypeData);
 }
 
 
