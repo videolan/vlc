videolan/vlc

commit d357b49f2ea02361c32b202dae6e0fc7079d791d

contrib: replace d3d9/d3d11 idl generator with wine-idl

contrib/src/wine-headers/rules.mak
@@ -1,0 +1,57 @@
+# WINE
+WINE_VERSION := 4.5
+WINE_URL := https://dl.winehq.org/wine/source/4.x/wine-$(WINE_VERSION).tar.xz
+
+ifdef HAVE_WIN32
+PKGS += wine-headers
+endif
+
+# Order is important since *_(n).idl will depends on *_(n-1).idl
+WINE_EXTRA_HEADERS =
+WINE_IDL_D3D9_HEADERS =
+WINE_IDL_HEADERS = \
+	d3d11.idl \
+	dxgicommon.idl dxgitype.idl dxgiformat.idl \
+	dxgidebug.idl \
+	dxgi1_2.idl dxgi1_3.idl dxgi1_4.idl dxgi1_5.idl dxgi1_6.idl
+
+ifndef HAVE_VISUALSTUDIO
+WINE_EXTRA_HEADERS += d3d9caps.h d3d9.h
+WINE_IDL_D3D9_HEADERS += dxva2api.idl
+endif
+
+$(TARBALLS)/wine-$(WINE_VERSION).tar.xz:
+	$(call download_pkg,$(WINE_URL),wine)
+
+.sum-wine-headers: wine-$(WINE_VERSION).tar.xz
+
+wine-headers: wine-$(WINE_VERSION).tar.xz .sum-wine-headers
+	$(UNPACK)
+	$(APPLY) $(SRC)/wine-headers/d3d9caps.patch
+	$(APPLY) $(SRC)/wine-headers/d3d9.patch
+	$(APPLY) $(SRC)/wine-headers/dxva2api.patch
+	$(APPLY) $(SRC)/wine-headers/dxgidebug.patch
+	$(APPLY) $(SRC)/wine-headers/processor_format.patch
+	$(MOVE)
+
+wine_widl = echo "GEN $(1)" && \
+	$(WIDL) -DBOOL=WINBOOL $(3) \
+	-I$(PREFIX)/include -Iwine-headers/idl-include \
+	-I`echo $(MSYSTEM) | tr A-Z a-z`/$(BUILD)/include -h \
+	-o "$(PREFIX)/include/$(1)" "wine-headers/idl-include/$(2)"
+
+.wine-headers: wine-headers
+	@mkdir -p $(PREFIX)/include
+	@mkdir -p wine-headers/idl-include
+	@cp wine-headers/include/*.idl  wine-headers/idl-include # be sure to not use .h from the wine project
+	@for header in $(WINE_IDL_HEADERS); do \
+		$(call wine_widl,"`basename $$header idl`h",$$header,); \
+	done
+	@for header in $(WINE_IDL_D3D9_HEADERS); do \
+		$(call wine_widl,"`basename $$header idl`h",$$header,-D_D3D9_H_ -D__C89_NAMELESS); \
+	done
+	@for header in $(WINE_EXTRA_HEADERS); do \
+		echo "CP  $$header"; \
+		cp "wine-headers/include/$$header" "$(PREFIX)/include"; \
+	done
+	touch $@

contrib/src/d3d11/rules.mak
@@ -1,121 +1,0 @@
-# generate Direct3D11 temporary include
-
-ifdef HAVE_CROSS_COMPILE
-IDL_INCLUDES = -I/usr/include/wine/windows/ -I/usr/include/wine/wine/windows/
-else
-#ugly way to get the default location of standard idl files
-IDL_INCLUDES = -I/`echo $(MSYSTEM) | tr A-Z a-z`/$(BUILD)/include
-endif
-
-D3D11_COMMIT_ID := a0cd5afeb60be3be0860e9a203314c10485bb9b8
-DXGI12_COMMIT_ID := 790a6544347b53c314b9c6f1ea757a2d5504c67e
-DXGITYPE_COMMIT_ID := f4aba520d014ecfe3563e33860de001caf2804e2
-D3D11_IDL_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(D3D11_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/d3d11.idl?format=raw
-DXGI12_IDL_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(DXGI12_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/dxgi1_2.idl?format=raw
-DXGITYPE_H_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(DXGITYPE_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/dxgitype.h?format=raw
-DXGIFORMAT_H_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(DXGITYPE_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/dxgiformat.h?format=raw
-DXGI_IDL_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(DXGITYPE_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/dxgi.idl?format=raw
-DST_D3D11_H = $(PREFIX)/include/d3d11.h
-DST_DXGIDEBUG_H = $(PREFIX)/include/dxgidebug.h
-DST_DXGITYPE_H = $(PREFIX)/include/dxgitype.h
-DST_DXGIFORMAT_H = $(PREFIX)/include/dxgiformat.h
-DST_DXGI_IDL = $(PREFIX)/include/dxgi.idl
-DST_DXGI12_H = $(PREFIX)/include/dxgi1_2.h
-DST_DXGI13_H = $(PREFIX)/include/dxgi1_3.h
-DST_DXGI14_H = $(PREFIX)/include/dxgi1_4.h
-DST_DXGI15_H = $(PREFIX)/include/dxgi1_5.h
-DST_DXGI16_H = $(PREFIX)/include/dxgi1_6.h
-
-
-ifdef HAVE_WIN32
-PKGS += d3d11
-endif
-
-$(TARBALLS)/d3d11.idl:
-	$(call download_pkg,$(D3D11_IDL_URL),d3d11)
-
-$(TARBALLS)/dxgidebug.idl:
-	(cd $(TARBALLS) && patch -fp1) < $(SRC)/d3d11/dxgidebug.patch
-
-$(TARBALLS)/dxgi1_2.idl:
-	$(call download_pkg,$(DXGI12_IDL_URL),d3d11)
-
-$(TARBALLS)/dxgitype.h:
-	$(call download_pkg,$(DXGITYPE_H_URL),d3d11)
-
-$(TARBALLS)/dxgiformat.h:
-	$(call download_pkg,$(DXGIFORMAT_H_URL),d3d11)
-
-$(TARBALLS)/dxgi.idl:
-	$(call download_pkg,$(DXGI_IDL_URL),d3d11)
-
-.sum-d3d11: $(TARBALLS)/d3d11.idl $(TARBALLS)/dxgidebug.idl $(TARBALLS)/dxgi1_2.idl $(TARBALLS)/dxgitype.h $(TARBALLS)/dxgiformat.h $(TARBALLS)/dxgi.idl
-
-d3d11: .sum-d3d11
-	mkdir -p $@
-	cp $(TARBALLS)/d3d11.idl $@ && cd $@ && patch -fp1 < ../$(SRC)/d3d11/processor_format.patch
-
-dxgi12: .sum-d3d11
-	mkdir -p $@
-	cp $(TARBALLS)/dxgi1_2.idl $@ && cd $@ && patch -fp1 < ../$(SRC)/d3d11/dxgi12.patch
-
-$(DST_D3D11_H): d3d11
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $</d3d11.idl
-
-$(DST_DXGIDEBUG_H): $(TARBALLS)/dxgidebug.idl
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $<
-
-$(DST_DXGITYPE_H): $(TARBALLS)/dxgitype.h
-	mkdir -p -- "$(PREFIX)/include/"
-	cp $(TARBALLS)/dxgitype.h $@
-
-$(DST_DXGIFORMAT_H): $(TARBALLS)/dxgiformat.h
-	mkdir -p -- "$(PREFIX)/include/"
-	cp $(TARBALLS)/dxgiformat.h $@
-
-$(DST_DXGI_IDL): $(TARBALLS)/dxgi.idl
-	mkdir -p -- "$(PREFIX)/include/"
-	cp $(TARBALLS)/dxgi.idl $@
-
-$(DST_DXGI12_H): dxgi12
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $</dxgi1_2.idl
-
-$(DST_DXGI13_H): $(SRC)/d3d11/dxgi1_3.idl $(DST_DXGI12_H)
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -Idxgi12 -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $<
-
-$(DST_DXGI14_H): $(SRC)/d3d11/dxgi1_4.idl $(DST_DXGI13_H)
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -Idxgi12 -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $<
-
-$(DST_DXGI15_H): $(SRC)/d3d11/dxgi1_5.idl $(DST_DXGI14_H)
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -Idxgi12 -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $<
-
-$(DST_DXGI16_H): $(SRC)/d3d11/dxgi1_6.idl $(DST_DXGI15_H)
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -Idxgi12 -I$(PREFIX)/include $(IDL_INCLUDES) -h -o $@ $<
-
-.dxgitype: $(DST_DXGITYPE_H) $(DST_DXGIFORMAT_H) $(DST_DXGI_IDL)
-	touch $@
-
-.dxgi12: .dxgitype $(DST_DXGI12_H)
-	touch $@
-
-.dxgi13: .dxgi12 $(DST_DXGI13_H)
-	touch $@
-
-.dxgi14: .dxgi13 $(DST_DXGI14_H)
-	touch $@
-
-.dxgi15: .dxgi14 $(DST_DXGI15_H)
-	touch $@
-
-.dxgi16: .dxgi15 $(DST_DXGI16_H)
-	touch $@
-
-.d3d11: $(DST_D3D11_H) $(DST_DXGIDEBUG_H) .dxgi16
-	touch $@

contrib/src/d3d9/rules.mak
@@ -1,58 +1,0 @@
-# generate Direct3D9 temporary include
-
-ifdef HAVE_CROSS_COMPILE
-IDL_INC_PATH = -I/usr/include/wine/windows/ -I/usr/include/wine/wine/windows/
-else
-#ugly way to get the default location of standard idl files
-IDL_INC_PATH = -I/`echo $(MSYSTEM) | tr A-Z a-z`/$(BUILD)/include
-endif
-
-D3D9CAPS_COMMIT_ID := 477108e5706e73421634436c21cb76e1795b3609
-DXVA2API_COMMIT_ID := 67bb96f54d720ca9e5aaa5da7d385348e0bfac31
-D3D9CAPS_H_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(D3D9CAPS_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/d3d9caps.h?format=raw
-D3D9_H_URL := http://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(D3D9CAPS_COMMIT_ID)/tree/mingw-w64-headers/direct-x/include/d3d9.h?format=raw
-DXVA2API_IDL_URL := https://sourceforge.net/p/mingw-w64/mingw-w64/ci/$(DXVA2API_COMMIT_ID)/tree/mingw-w64-headers/include/dxva2api.idl?format=raw
-DST_D3D9CAPS_H = $(PREFIX)/include/d3d9caps.h
-DST_D3D9_H = $(PREFIX)/include/d3d9.h
-DST_DXVA2API_H = $(PREFIX)/include/dxva2api.h
-
-
-ifdef HAVE_WIN32
-ifndef HAVE_VISUALSTUDIO
-PKGS += d3d9
-endif
-endif
-
-$(TARBALLS)/d3d9caps.h:
-	$(call download_pkg,$(D3D9CAPS_H_URL),d3d9)
-
-$(TARBALLS)/d3d9.h:
-	$(call download_pkg,$(D3D9_H_URL),d3d9)
-
-$(TARBALLS)/dxva2api.idl:
-	$(call download_pkg,$(DXVA2API_IDL_URL),d3d9)
-
-.sum-d3d9: $(TARBALLS)/d3d9caps.h $(TARBALLS)/d3d9.h $(TARBALLS)/dxva2api.idl
-
-$(DST_D3D9CAPS_H): $(TARBALLS)/d3d9caps.h .sum-d3d9
-	mkdir -p -- "$(PREFIX)/include/"
-	cd $(TARBALLS) && patch -fp1 < $(SRC)/d3d9/d3d9caps.patch -o $@
-
-$(DST_D3D9_H): $(TARBALLS)/d3d9.h .sum-d3d9
-	mkdir -p -- "$(PREFIX)/include/"
-	cd $(TARBALLS) && patch -fp1 < $(SRC)/d3d9/d3d9.patch -o $@
-
-dxva2api/dxva2api.idl: .sum-d3d9
-	mkdir -p dxva2api
-	cp $(TARBALLS)/dxva2api.idl $@
-	patch -fp1 < $(SRC)/d3d9/dxva2api.patch
-
-$(DST_DXVA2API_H): dxva2api/dxva2api.idl
-	mkdir -p -- "$(PREFIX)/include/"
-	$(WIDL) -DBOOL=WINBOOL -D_D3D9_H_ -D__C89_NAMELESS $(IDL_INCLUDES) -h -o $@ $<
-
-.d3d9caps: $(DST_D3D9CAPS_H)
-	touch $@
-
-.d3d9: $(DST_D3D9_H) $(DST_D3D9CAPS_H) $(DST_DXVA2API_H)
-	touch $@

contrib/src/ffmpeg/rules.mak
@@ -180,7 +180,7 @@ endif
 # Windows
 ifdef HAVE_WIN32
 ifndef HAVE_VISUALSTUDIO
-DEPS_ffmpeg += d3d11
+DEPS_ffmpeg += wine-headers
 ifndef HAVE_MINGW_W64
 DEPS_ffmpeg += directx
 endif

contrib/src/d3d11/SHA512SUMS
@@ -1,6 +1,0 @@
-0d97a004acd9d99d685aef6ae3c41b9951b41170d197173838ad2dbad4db309debcc2dd6ab2cfaa849ab9ca9841ae80232739b113b3e72c35ec8a481066acff5  d3d11.idl
-04b208ddf9c91f923d262ae68a5fb0588a3ef1a39ea450eaac9031e021bd5efdc7b30e3fb3e589b07bc88e41f8861caeb56934a2fa6cbf2efb7404f5f3453b70  dxgidebug.idl
-1bee79fcaf5db6e53510f49fb40009fe6291ffafe47e1aea46ae0068afd001b405b0d970a55af63e475fbb18108e1ca0f91d86a18b6598b922c454ed082e8941  dxgi1_2.idl
-3f24632a71c96498f3432ed55ebdbe0a90e00c86ff88e4a41d1d04fdda14532aeff7799401803a03c6f69b3a07930d3f190211accef4ca199a21b3671acdcc45  dxgitype.h
-bba687b7cd4fd321f99d70a8137b1aa01851d8fd72a007cf5f8e5bce55fe06e15c27fe82de6493291b7c9932ceb43642234ed07d9a6bcb6140229cdbeb4a49bd  dxgiformat.h
-40eec2c5a96aeafb18ed9936c6ca9fc05303a08572efe6a3483e316b8af4e392eba863af86cdbb1342ac3ccb039be1b962688b05610e07f426bab6474073bfa3  dxgi.idl

contrib/src/d3d11/dxgi12.patch
@@ -1,109 +1,0 @@
---- tarballs/dxgi1_2.idl.orig	2016-03-02 16:01:30.243405100 +0100
-+++ tarballs/dxgi1_2.idl	2016-03-02 16:19:58.006959300 +0100
-@@ -232,3 +232,106 @@ interface IDXGIAdapter2 : IDXGIAdapter1
- {
-     HRESULT GetDesc2([out] DXGI_ADAPTER_DESC2 *pDesc);
- }
-+
-+typedef struct DXGI_OUTDUPL_DESC {
-+    DXGI_MODE_DESC ModeDesc;
-+    DXGI_MODE_ROTATION Rotation;
-+    BOOL DesktopImageInSystemMemory;
-+} DXGI_OUTDUPL_DESC;
-+
-+typedef struct DXGI_OUTDUPL_POINTER_POSITION {
-+    POINT Position;
-+    BOOL Visible;
-+} DXGI_OUTDUPL_POINTER_POSITION;
-+
-+typedef struct DXGI_OUTDUPL_FRAME_INFO {
-+    LARGE_INTEGER LastPresentTime;
-+    LARGE_INTEGER LastMouseUpdateTime;
-+    UINT AccumulatedFrames;
-+    BOOL RectsCoalesced;
-+    BOOL ProtectedContentMaskedOut;
-+    DXGI_OUTDUPL_POINTER_POSITION PointerPosition;
-+    UINT TotalMetadataBufferSize;
-+    UINT PointerShapeBufferSize;
-+} DXGI_OUTDUPL_FRAME_INFO;
-+
-+typedef struct DXGI_OUTDUPL_MOVE_RECT {
-+    POINT SourcePoint;
-+    RECT  DestinationRect;
-+} DXGI_OUTDUPL_MOVE_RECT;
-+
-+typedef struct DXGI_OUTDUPL_POINTER_SHAPE_INFO {
-+    UINT Type;
-+    UINT Width;
-+    UINT Height;
-+    UINT Pitch;
-+    POINT HotSpot;
-+} DXGI_OUTDUPL_POINTER_SHAPE_INFO;
-+[
-+    object,
-+    uuid(191cfac3-a341-470d-b26e-a864f428319c),
-+    local,
-+    pointer_default(unique)
-+]
-+interface IDXGIOutputDuplication :
-+    IDXGIObject
-+{
-+    void GetDesc(
-+            [out] DXGI_OUTDUPL_DESC* pDesc);
-+    HRESULT AcquireNextFrame(
-+            [in] UINT TimeoutInMilliseconds,
-+            [out] DXGI_OUTDUPL_FRAME_INFO* pFrameInfo,
-+            [out] IDXGIResource** ppDesktopResource);
-+    HRESULT GetFrameDirtyRects(
-+            [in] UINT DirtyRectsBufferSize,
-+            [out] RECT* pDirtyRectsBuffer,
-+            [out] UINT* pDirtyRectsBufferSizeRequired);
-+    HRESULT GetFrameMoveRects(
-+            [in] UINT MoveRectsBufferSize,
-+            [out] DXGI_OUTDUPL_MOVE_RECT* pMoveRectBuffer,
-+            [out] UINT* pMoveRectsBufferSizeRequired);
-+    HRESULT GetFramePointerShape(
-+            [in] UINT PointerShapeBufferSize,
-+            [out] void* pPointerShapeBuffer,
-+            [out] UINT* pPointerShapeBufferSizeRequired,
-+            [out] DXGI_OUTDUPL_POINTER_SHAPE_INFO* pPointerShapeInfo);
-+    HRESULT MapDesktopSurface(
-+            [out] DXGI_MAPPED_RECT* pLockedRect);
-+    HRESULT UnMapDesktopSurface();
-+    HRESULT ReleaseFrame();
-+};
-+
-+typedef struct DXGI_MODE_DESC1
-+{
-+    UINT Width;
-+    UINT Height;
-+    DXGI_RATIONAL RefreshRate;
-+    DXGI_FORMAT Format;
-+    DXGI_MODE_SCANLINE_ORDER ScanlineOrdering;
-+    DXGI_MODE_SCALING Scaling;
-+    BOOL Stereo;
-+} DXGI_MODE_DESC1;
-+
-+[
-+    object,
-+    uuid(00cddea8-939b-4b83-a340-a685226666cc),
-+    local,
-+    pointer_default(unique)
-+]
-+interface IDXGIOutput1 : IDXGIOutput
-+{
-+    HRESULT GetDisplayModeList1(
-+            [in] DXGI_FORMAT EnumFormat,
-+            [in] UINT Flags,
-+            [in, out] UINT * pNumModes,
-+            [out] DXGI_MODE_DESC1 *pDesc);
-+    HRESULT FindClosestMatchingMode1(
-+            [in] const DXGI_MODE_DESC1 *pModeToMatch,
-+            [out] DXGI_MODE_DESC1 *pClosestMatch,
-+            [in] IUnknown * pConcernedDevice);
-+    HRESULT GetDisplaySurfaceData1(
-+            [in] IDXGIResource *pDestination);
-+    HRESULT DuplicateOutput(
-+            [in] IUnknown    *pDevice,
-+            [out] IDXGIOutputDuplication **ppOutputDuplication);
-+};

contrib/src/d3d11/dxgi1_3.idl
@@ -1,242 +1,0 @@
-/*
- * Copyright 2015 Martell Malone
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
- */
-
-import "dxgi1_2.idl";
-
-cpp_quote("#define DXGI_CREATE_FACTORY_DEBUG   0x1")
-
-typedef struct DXGI_DECODE_SWAP_CHAIN_DESC
-{
-    UINT Flags;
-} DXGI_DECODE_SWAP_CHAIN_DESC;
-
-typedef enum DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAGS
-{
-    DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAG_NOMINAL_RANGE = 0x1,
-    DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAG_BT709         = 0x2,
-    DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAG_xvYCC         = 0x4,
-} DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAGS;
-
-[
-    object,
-    local,
-    uuid(2633066b-4514-4c7a-8fd8-12ea98059d18)
-]
-interface IDXGIDecodeSwapChain : IUnknown
-{
-    HRESULT PresentBuffer(
-        UINT buffer,
-        UINT sync,
-        UINT flags
-    );
-
-    HRESULT SetSourceRect(
-        const RECT* rect
-    );
-
-    HRESULT SetTargetRect(
-        const RECT* rect
-    );
-
-    HRESULT SetDestSize(
-        UINT width,
-        UINT height
-    );
-
-    HRESULT GetSourceRect(
-        [out] RECT* rect
-    );
-
-    HRESULT GetTargetRect(
-        [out] RECT* rect
-    );
-
-    HRESULT GetDestSize(
-        [out] UINT* width,
-        [out] UINT* height
-    );
-
-    HRESULT SetColorSpace(
-        DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAGS colorSpace
-    );
-
-    DXGI_MULTIPLANE_OVERLAY_YCbCr_FLAGS GetColorSpace();
-}
-
-[
-    object,
-    local,
-    uuid(41e7d1f2-a591-4f7b-a2e5-fa9c843e1c12)
-]
-interface IDXGIFactoryMedia :IUnknown
-{
-    HRESULT CreateSwapChainForCompositionSurfaceHandle(
-        [in] IUnknown* device,
-        [in] HANDLE surface,
-        [in] const DXGI_SWAP_CHAIN_DESC1* desc,
-        [in] IDXGIOutput* output,
-        [out] IDXGISwapChain1** swapchain
-    );
-
-    HRESULT CreateDecodeSwapChainForCompositionSurfaceHandle(
-        [in] IUnknown* device,
-        [in] HANDLE surface,
-        [in] DXGI_DECODE_SWAP_CHAIN_DESC* desc,
-        [in] IDXGIResource* buffer,
-        [in] IDXGIOutput* output,
-        [out] IDXGIDecodeSwapChain** swapchain
-    );
-}
-
-typedef enum DXGI_FRAME_PRESENTATION_MODE
-{
-    DXGI_FRAME_PRESENTATION_MODE_COMPOSED = 0,
-    DXGI_FRAME_PRESENTATION_MODE_OVERLAY = 1,
-    DXGI_FRAME_PRESENTATION_MODE_NONE = 2,
-} DXGI_FRAME_PRESENTATION_MODE;
-
-typedef struct DXGI_FRAME_STATISTICS_MEDIA
-{
-    UINT PresentCount;
-    UINT PresentRefreshCount;
-    UINT SyncRefreshCount;
-    LARGE_INTEGER SyncQPCTime;
-    LARGE_INTEGER SyncGPUTime;
-    DXGI_FRAME_PRESENTATION_MODE CompositionMode;
-    UINT ApprovedPresentDuration;
-} DXGI_FRAME_STATISTICS_MEDIA;
-
-[
-    object,
-    local,
-    uuid(dd95b90b-f05f-4f6a-bd65-25bfb264bd84)
-]
-interface IDXGISwapChainMedia : IUnknown
-{
-    HRESULT GetFrameStatisticsMedia(
-        [out] DXGI_FRAME_STATISTICS_MEDIA* pStats
-    );
-
-    HRESULT SetPresentDuration(
-        UINT duration
-    );
-
-    HRESULT CheckPresentDurationSupport(
-        UINT duration,
-        [out] UINT* smallerduration,
-        [out] UINT* largerduration
-    );
-};
-
-typedef enum DXGI_OVERLAY_SUPPORT_FLAG
-{
-    DXGI_OVERLAY_SUPPORT_FLAG_DIRECT   = 0x1,
-    DXGI_OVERLAY_SUPPORT_FLAG_SCALING  = 0x2
-} DXGI_OVERLAY_SUPPORT_FLAG;
-
-[
-    object,
-    local,
-    uuid(6007896c-3244-4afd-bf18-a6d3beda5023)
-]
-interface IDXGIDevice3 : IDXGIDevice2
-{
-    void Trim();
-}
-
-[
-    object,
-    local,
-    uuid(595e39d1-2724-4663-99b1-da969de28364)
-]
-interface IDXGIOutput2 : IDXGIOutput1
-{
-    BOOL SupportsOverlays();
-}
-
-[
-    object,
-    local,
-    uuid(8a6bb301-7e7e-41F4-a8e0-5b32f7f99b18)
-]
-interface IDXGIOutput3 : IDXGIOutput2
-{
-    HRESULT CheckOverlaySupport(
-        [in] DXGI_FORMAT format,
-        [out, in] IUnknown* device,
-        [out] UINT* flags
-    );
-}
-
-[local] HRESULT __stdcall CreateDXGIFactory2(UINT Flags, REFIID riid, void **factory);
-
-typedef struct DXGI_MATRIX_3X2_F
-{
-    float _11;
-    float _12;
-    float _21;
-    float _22;
-    float _31;
-    float _32;
-} DXGI_MATRIX_3X2_F;
-
-[
-    object,
-    local,
-    uuid(a8be2ac4-199f-4946-b331-79599fb98de7)
-]
-interface IDXGISwapChain2 : IDXGISwapChain1
-{
-    HRESULT SetSourceSize(
-        UINT width,
-        UINT height
-    );
-
-    HRESULT GetSourceSize(
-        [out] UINT* width,
-        [out] UINT* height
-    );
-
-    HRESULT SetMaximumFrameLatency(
-        UINT max_latency
-    );
-
-    HRESULT GetMaximumFrameLatency(
-        [out] UINT* max_latency
-    );
-
-    HANDLE  GetFrameLatencyWaitableObject();
-
-    HRESULT SetMatrixTransform(
-        const DXGI_MATRIX_3X2_F* matrix
-    );
-
-    HRESULT GetMatrixTransform(
-        [out] DXGI_MATRIX_3X2_F* matrix
-    );
-}
-
-[
-    object,
-    local,
-    uuid(25483823-cd46-4c7d-86ca-47aa95b837bd)
-]
-interface IDXGIFactory3 : IDXGIFactory2
-{
-    UINT GetCreationFlags();
-}

contrib/src/d3d11/dxgi1_4.idl
@@ -1,109 +1,0 @@
-/*
- * Copyright 2016 Steve Lhomme
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
- */
-
-import "dxgi1_3.idl";
-
-typedef enum DXGI_COLOR_SPACE_TYPE {
-    DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P709         = 0,
-    DXGI_COLOR_SPACE_RGB_FULL_G10_NONE_P709         = 1,
-    DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P709       = 2,
-    DXGI_COLOR_SPACE_RGB_STUDIO_G22_NONE_P2020      = 3,
-    DXGI_COLOR_SPACE_RESERVED                       = 4,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_NONE_P709_X601  = 5,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P601     = 6,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P601       = 7,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P709     = 8,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P709       = 9,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_LEFT_P2020    = 10,
-    DXGI_COLOR_SPACE_YCBCR_FULL_G22_LEFT_P2020      = 11,
-    DXGI_COLOR_SPACE_RGB_FULL_G2084_NONE_P2020      = 12,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G2084_LEFT_P2020  = 13,
-    DXGI_COLOR_SPACE_RGB_STUDIO_G2084_NONE_P2020    = 14,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G22_TOPLEFT_P2020 = 15,
-    DXGI_COLOR_SPACE_YCBCR_STUDIO_G2084_TOPLEFT_P2020 = 16,
-    DXGI_COLOR_SPACE_RGB_FULL_G22_NONE_P2020        = 17,
-    DXGI_COLOR_SPACE_CUSTOM                         = 0xFFFFFFFF,
-} DXGI_COLOR_SPACE_TYPE;
-
-[
-    object,
-    local,
-    uuid(94d99bdb-f1f8-4ab0-b236-7da0170edab1)
-]
-interface IDXGISwapChain3 : IDXGISwapChain2
-{
-    UINT GetCurrentBackBufferIndex();
-
-    HRESULT CheckColorSpaceSupport(
-        [in] DXGI_COLOR_SPACE_TYPE ColorSpace,
-        [out] UINT* pColorSpaceSupport
-    );
-
-    HRESULT SetColorSpace1(
-        [in] DXGI_COLOR_SPACE_TYPE ColorSpace
-    );
-
-    HRESULT ResizeBuffers1(
-        [in] UINT BufferCount,
-        [in] UINT Width,
-        [in] UINT Height,
-        [in] DXGI_FORMAT Format,
-        [in] UINT SwapChainFlags,
-        [in] const UINT* pCreationNodeMask,
-        [in] IUnknown* const* ppPresentQueue
-    );
-};
-
-typedef enum DXGI_SWAP_CHAIN_COLOR_SPACE_SUPPORT_FLAG
-{
-    DXGI_SWAP_CHAIN_COLOR_SPACE_SUPPORT_FLAG_PRESENT         = 0x01,
-    DXGI_SWAP_CHAIN_COLOR_SPACE_SUPPORT_FLAG_OVERLAY_PRESENT = 0x02,
-} DXGI_SWAP_CHAIN_COLOR_SPACE_SUPPORT_FLAG;
-
-[
-    object,
-    local,
-    uuid(1bc6ea02-ef36-464f-bf0c-21ca39e5168a)
-]
-interface IDXGIFactory4 : IDXGIFactory3
-{
-    HRESULT EnumAdapterByLuid(
-        [in]  LUID     AdapterLuid,
-        [in]  REFIID   riid,
-        [out] void     **ppvAdapter
-    );
-    HRESULT EnumWarpAdapter(
-        [in]  REFIID   riid,
-        [out] void     **ppvAdapter
-    );
-}
-
-[
-    object,
-    local,
-    uuid(dc7dca35-2196-414d-9F53-617884032a60)
-]
-interface IDXGIOutput4 : IDXGIOutput3
-{
-    HRESULT CheckOverlayColorSpaceSupport(
-        [in]  DXGI_FORMAT           Format,
-        [in]  DXGI_COLOR_SPACE_TYPE ColorSpace,
-        [in]  IUnknown              *pConcernedDevice,
-        [out] UINT                  *pFlags
-    );
-}

commit b7cc49eed101b78e43653647a303360f8cd0f57d

contrib: wine-headers: add missing header
Regression from d357b49f2ea02361c32b202dae6e0fc7079d791d.
The dxgi.h header generation was missed.

contrib/src/wine-headers/rules.mak
@@ -14,7 +14,7 @@ WINE_IDL_HEADERS = \
 	d3d11_1.idl d3d11_2.idl d3d11_3.idl \
 	dxgicommon.idl dxgitype.idl dxgiformat.idl \
 	dxgidebug.idl \
-	dxgi1_2.idl dxgi1_3.idl dxgi1_4.idl dxgi1_5.idl dxgi1_6.idl
+	dxgi.idl dxgi1_2.idl dxgi1_3.idl dxgi1_4.idl dxgi1_5.idl dxgi1_6.idl
 
 ifndef HAVE_VISUALSTUDIO
 WINE_EXTRA_HEADERS += d3d9caps.h d3d9.h