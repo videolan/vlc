c182eacdad8759641b61bb8828739e08ac9c26f3
Rémi Denis-Courmont <remi@remlab.net>
2019-04-15 19:48:48 +0300

    dbus: avoid precedence warning

--- modules/control/dbus/dbus_player.c
+++ modules/control/dbus/dbus_player.c
@@ -264,7 +264,7 @@ MarshalCanGoNext( intf_thread_t *p_intf, DBusMessageIter *container )
 
     dbus_bool_t b_can_go_next =
         count != 0 &&
-        (index != -1 && (size_t)index < count - 1 ||
+        ((index != -1 && (size_t)index < count - 1) ||
          repeat_mode != VLC_PLAYLIST_PLAYBACK_REPEAT_NONE);
 
     if( !dbus_message_iter_append_basic( container, DBUS_TYPE_BOOLEAN,
