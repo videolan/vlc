496cbb18768defeae700ead88f7f1e2ed1c94509
Lyndon Brown <jnqnfe@gmail.com>
2019-04-18 00:02:30 +0100

    config: merge unnecessary subcat title defines into lookup table
    We have no need at all for these to be defines, which just adds
    unnecessary clutter. They are all very short and it is much tidier
    with the text merged into the table.

--- include/vlc_config_cat.h
+++ include/vlc_config_cat.h
@@ -34,13 +34,10 @@
 
 #define INTF_GENERAL_HELP N_( "Main interfaces settings" )
 
-#define INTF_MAIN_TITLE  N_( "Main interfaces" )
 #define INTF_MAIN_HELP N_( "Settings for the main interface" )
 
-#define INTF_CONTROL_TITLE N_( "Control interfaces" )
 #define INTF_CONTROL_HELP N_( "Settings for VLC's control interfaces" )
 
-#define INTF_HOTKEYS_TITLE N_( "Hotkeys settings" )
 #define INTF_HOTKEYS_HELP N_( "Hotkeys settings" )
 
 /* Audio */
@@ -49,15 +46,10 @@
 
 #define AUDIO_GENERAL_HELP N_("General audio settings")
 
-#define AFILTER_TITLE N_("Filters")
 #define AFILTER_HELP N_( "Audio filters are used to process the audio stream." )
 
-#define ARESAMPLER_TITLE N_("Audio resampler")
-
-#define AVISUAL_TITLE N_("Visualizations")
 #define AVISUAL_HELP N_( "Audio visualizations" )
 
-#define AOUT_TITLE N_( "Output modules" )
 #define AOUT_HELP N_("General settings for audio output modules.")
 
 /* Video */
@@ -66,17 +58,14 @@
 
 #define VIDEO_GENERAL_HELP N_( "General video settings" )
 
-#define _VOUT_TITLE N_("Output modules" )
 #define VOUT_HELP N_("General settings for video output modules.")
 
-#define VFILTER_TITLE N_("Filters" )
 #define VFILTER_HELP N_("Video filters are used to process the video stream." )
 
 #define SUBPIC_TITLE N_( "Subtitles / OSD")
 #define SUBPIC_HELP N_( "Settings related to On-Screen-Display,"\
         " subtitles and \"overlay subpictures\"")
 
-#define SPLITTER_TITLE N_("Splitters")
 #define SPLITTER_HELP N_("Video splitters separate the stream into multiple videos.")
 
 /* Input */
@@ -84,27 +73,21 @@
 #define INPUT_HELP N_( "Settings for input, demultiplexing, " \
          "decoding and encoding")
 
-#define ACCESS_TITLE N_( "Access modules" )
 #define ACCESS_HELP N_( \
     "Settings related to the various access methods. " \
     "Common settings you may want to alter are HTTP proxy or " \
     "caching settings." )
 
-#define STREAM_FILTER_TITLE N_( "Stream filters" )
 #define STREAM_FILTER_HELP N_( \
     "Stream filters are special modules that allow advanced operations on " \
     "the input side of VLC. Use with care..." )
 
-#define DEMUX_TITLE N_("Demuxers")
 #define DEMUX_HELP N_( "Demuxers are used to separate audio and video streams." )
 
-#define VDEC_TITLE  N_( "Video codecs" )
 #define VDEC_HELP N_( "Settings for the video, images or video+audio decoders and encoders." )
 
-#define ADEC_TITLE  N_( "Audio codecs" )
 #define ADEC_HELP N_( "Settings for the audio-only decoders and encoders." )
 
-#define SDEC_TITLE N_( "Subtitle codecs")
 #define SDEC_HELP N_( "Settings for subtitle, teletext and CC decoders and encoders." )
 
 /* Sout */
@@ -120,7 +103,6 @@
 
 #define SOUT_GENERAL_HELP N_( "General stream output settings")
 
-#define SOUT_MUX_TITLE N_( "Muxers" )
 #define SOUT_MUX_HELP N_( \
        "Muxers create the encapsulation formats that are used to " \
        "put all the elementary streams (video, audio, ...) " \
@@ -128,14 +110,12 @@
        "You should probably not do that.\n" \
        "You can also set default parameters for each muxer." )
 
-#define SOUT_ACO_TITLE N_( "Access output" )
 #define SOUT_ACO_HELP N_( \
    "Access output modules control the ways the muxed streams are sent. " \
    "This setting allows you to always force a specific access output method. " \
    "You should probably not do that.\n" \
    "You can also set default parameters for each access output.")
 
-#define SOUT_PACKET_TITLE N_( "Packetizers" )
 #define SOUT_PACKET_HELP N_( \
         "Packetizers are used to \"preprocess\" the elementary "\
         "streams before muxing. " \
@@ -143,16 +123,13 @@
         "You should probably not do that.\n" \
         "You can also set default parameters for each packetizer." )
 
-#define SOUT_RENDER_TITLE N_( "Renderers" )
 #define SOUT_RENDER_HELP N_( "External renderer discovery related settings." )
 
-#define SOUT_STREAM_TITLE N_("Sout stream")
 #define SOUT_STREAM_HELP N_( "Sout stream modules allow to build a sout " \
                 "processing chain. Please refer to the Streaming 'how-to' for " \
                 "more information. You can configure default options for " \
                 "each sout stream module here.")
 
-#define SOUT_VOD_TITLE N_( "VOD" )
 #define SOUT_VOD_HELP N_( "VLC's implementation of Video On Demand" )
 
 
@@ -163,18 +140,16 @@
         "items to the playlist (\"service discovery\" modules).")
 
 #define PGENERAL_HELP N_( "General playlist behaviour")
-#define SD_TITLE N_("Services discovery")
+
 #define SD_HELP N_("Services discovery modules are facilities "\
         "that automatically add items to playlist.")
 
-#define PEXPORT_TITLE N_( "Export" )
 #define PEXPORT_HELP N_( "Settings relating to exporting playlists." )
 
 /* Advanced */
 #define AADVANCED_TITLE N_( "Advanced" )
 #define AADVANCED_HELP N_( "Advanced settings. Use with care...")
 
-#define ANETWORK_TITLE N_( "Network" )
 #define ANETWORK_HELP N_( "Advanced network settings." )
 
 struct config_category_t
@@ -205,47 +180,47 @@ static const struct config_category_t categories_array[] =
 
 static const struct config_subcategory_t subcategories_array[] =
 {
-    { SUBCAT_PLAYLIST_GENERAL,     CAT_PLAYLIST,   PLAYLIST_TITLE,       PGENERAL_HELP      },
-    { SUBCAT_PLAYLIST_EXPORT,      CAT_PLAYLIST,   PEXPORT_TITLE,        PEXPORT_HELP       },
-    { SUBCAT_PLAYLIST_SD,          CAT_PLAYLIST,   SD_TITLE,             SD_HELP            },
+    { SUBCAT_PLAYLIST_GENERAL,     CAT_PLAYLIST,   PLAYLIST_TITLE,            PGENERAL_HELP      },
+    { SUBCAT_PLAYLIST_EXPORT,      CAT_PLAYLIST,   N_("Export"),              PEXPORT_HELP       },
+    { SUBCAT_PLAYLIST_SD,          CAT_PLAYLIST,   N_("Services discovery"),  SD_HELP            },
 
-    { SUBCAT_INTERFACE_GENERAL,    CAT_INTERFACE,  INTF_TITLE,           INTF_GENERAL_HELP  },
-    { SUBCAT_INTERFACE_MAIN,       CAT_INTERFACE,  INTF_MAIN_TITLE,      INTF_MAIN_HELP     },
-    { SUBCAT_INTERFACE_CONTROL,    CAT_INTERFACE,  INTF_CONTROL_TITLE,   INTF_CONTROL_HELP  },
-    { SUBCAT_INTERFACE_HOTKEYS,    CAT_INTERFACE,  INTF_HOTKEYS_TITLE,   INTF_HOTKEYS_HELP  },
+    { SUBCAT_INTERFACE_GENERAL,    CAT_INTERFACE,  INTF_TITLE,                INTF_GENERAL_HELP  },
+    { SUBCAT_INTERFACE_MAIN,       CAT_INTERFACE,  N_("Main interfaces"),     INTF_MAIN_HELP     },
+    { SUBCAT_INTERFACE_CONTROL,    CAT_INTERFACE,  N_("Control interfaces"),  INTF_CONTROL_HELP  },
+    { SUBCAT_INTERFACE_HOTKEYS,    CAT_INTERFACE,  N_("Hotkeys settings"),    INTF_HOTKEYS_HELP  },
 
-    { SUBCAT_AUDIO_GENERAL,        CAT_AUDIO,      AUDIO_TITLE,          AUDIO_GENERAL_HELP },
-    { SUBCAT_AUDIO_AOUT,           CAT_AUDIO,      AOUT_TITLE,           AOUT_HELP          },
-    { SUBCAT_AUDIO_AFILTER,        CAT_AUDIO,      AFILTER_TITLE,        AFILTER_HELP       },
-    { SUBCAT_AUDIO_RESAMPLER,      CAT_AUDIO,      ARESAMPLER_TITLE,     AFILTER_HELP       },
-    { SUBCAT_AUDIO_VISUAL,         CAT_AUDIO,      AVISUAL_TITLE,        AVISUAL_HELP       },
+    { SUBCAT_AUDIO_GENERAL,        CAT_AUDIO,      AUDIO_TITLE,               AUDIO_GENERAL_HELP },
+    { SUBCAT_AUDIO_AOUT,           CAT_AUDIO,      N_("Output modules"),      AOUT_HELP          },
+    { SUBCAT_AUDIO_AFILTER,        CAT_AUDIO,      N_("Filters"),             AFILTER_HELP       },
+    { SUBCAT_AUDIO_RESAMPLER,      CAT_AUDIO,      N_("Audio resampler"),     AFILTER_HELP       },
+    { SUBCAT_AUDIO_VISUAL,         CAT_AUDIO,      N_("Visualizations"),      AVISUAL_HELP       },
 
-    { SUBCAT_VIDEO_GENERAL,        CAT_VIDEO,      VIDEO_TITLE,          VIDEO_GENERAL_HELP },
-    { SUBCAT_VIDEO_VOUT,           CAT_VIDEO,      _VOUT_TITLE,          VOUT_HELP          },
-    { SUBCAT_VIDEO_VFILTER,        CAT_VIDEO,      VFILTER_TITLE,        VFILTER_HELP       },
-    { SUBCAT_VIDEO_SUBPIC,         CAT_VIDEO,      SUBPIC_TITLE,         SUBPIC_HELP        },
-    { SUBCAT_VIDEO_SPLITTER,       CAT_VIDEO,      SPLITTER_TITLE,       SPLITTER_HELP      },
+    { SUBCAT_VIDEO_GENERAL,        CAT_VIDEO,      VIDEO_TITLE,               VIDEO_GENERAL_HELP },
+    { SUBCAT_VIDEO_VOUT,           CAT_VIDEO,      N_("Output modules"),      VOUT_HELP          },
+    { SUBCAT_VIDEO_VFILTER,        CAT_VIDEO,      N_("Filters"),             VFILTER_HELP       },
+    { SUBCAT_VIDEO_SUBPIC,         CAT_VIDEO,      N_("Subtitles / OSD"),     SUBPIC_HELP        },
+    { SUBCAT_VIDEO_SPLITTER,       CAT_VIDEO,      N_("Splitters"),           SPLITTER_HELP      },
 
-    { SUBCAT_INPUT_GENERAL,        CAT_INPUT,      INPUT_TITLE,          INPUT_HELP         },
-    { SUBCAT_INPUT_ACCESS,         CAT_INPUT,      ACCESS_TITLE,         ACCESS_HELP        },
-    { SUBCAT_INPUT_DEMUX,          CAT_INPUT,      DEMUX_TITLE,          DEMUX_HELP         },
-    { SUBCAT_INPUT_VCODEC,         CAT_INPUT,      VDEC_TITLE,           VDEC_HELP          },
-    { SUBCAT_INPUT_ACODEC,         CAT_INPUT,      ADEC_TITLE,           ADEC_HELP          },
-    { SUBCAT_INPUT_SCODEC,         CAT_INPUT,      SDEC_TITLE,           SDEC_HELP          },
-    { SUBCAT_INPUT_STREAM_FILTER,  CAT_INPUT,      STREAM_FILTER_TITLE,  STREAM_FILTER_HELP },
+    { SUBCAT_INPUT_GENERAL,        CAT_INPUT,      INPUT_TITLE,               INPUT_HELP         },
+    { SUBCAT_INPUT_ACCESS,         CAT_INPUT,      N_("Access modules"),      ACCESS_HELP        },
+    { SUBCAT_INPUT_DEMUX,          CAT_INPUT,      N_("Demuxers"),            DEMUX_HELP         },
+    { SUBCAT_INPUT_VCODEC,         CAT_INPUT,      N_("Video codecs"),        VDEC_HELP          },
+    { SUBCAT_INPUT_ACODEC,         CAT_INPUT,      N_("Audio codecs"),        ADEC_HELP          },
+    { SUBCAT_INPUT_SCODEC,         CAT_INPUT,      N_("Subtitle codecs"),     SDEC_HELP          },
+    { SUBCAT_INPUT_STREAM_FILTER,  CAT_INPUT,      N_("Stream filters"),      STREAM_FILTER_HELP },
 
-    { SUBCAT_SOUT_GENERAL,         CAT_SOUT,       SOUT_TITLE,           SOUT_GENERAL_HELP  },
-    { SUBCAT_SOUT_STREAM,          CAT_SOUT,       SOUT_STREAM_TITLE,    SOUT_STREAM_HELP   },
-    { SUBCAT_SOUT_MUX,             CAT_SOUT,       SOUT_MUX_TITLE,       SOUT_MUX_HELP      },
-    { SUBCAT_SOUT_ACO,             CAT_SOUT,       SOUT_ACO_TITLE,       SOUT_ACO_HELP      },
-    { SUBCAT_SOUT_PACKETIZER,      CAT_SOUT,       SOUT_PACKET_TITLE,    SOUT_PACKET_HELP   },
-    { SUBCAT_SOUT_RENDERER,        CAT_SOUT,       SOUT_RENDER_TITLE,    SOUT_RENDER_HELP   },
-    { SUBCAT_SOUT_VOD,             CAT_SOUT,       SOUT_VOD_TITLE,       SOUT_VOD_HELP      },
+    { SUBCAT_SOUT_GENERAL,         CAT_SOUT,       SOUT_TITLE,                SOUT_GENERAL_HELP  },
+    { SUBCAT_SOUT_STREAM,          CAT_SOUT,       N_("Sout stream"),         SOUT_STREAM_HELP   },
+    { SUBCAT_SOUT_MUX,             CAT_SOUT,       N_("Muxers"),              SOUT_MUX_HELP      },
+    { SUBCAT_SOUT_ACO,             CAT_SOUT,       N_("Access output"),       SOUT_ACO_HELP      },
+    { SUBCAT_SOUT_PACKETIZER,      CAT_SOUT,       N_("Packetizers"),         SOUT_PACKET_HELP   },
+    { SUBCAT_SOUT_RENDERER,        CAT_SOUT,       N_("Renderers"),           SOUT_RENDER_HELP   },
+    { SUBCAT_SOUT_VOD,             CAT_SOUT,       N_("VOD"),                 SOUT_VOD_HELP      },
 
-    { SUBCAT_ADVANCED_MISC,        CAT_ADVANCED,   AADVANCED_TITLE,      AADVANCED_HELP     },
-    { SUBCAT_ADVANCED_NETWORK,     CAT_ADVANCED,   ANETWORK_TITLE,       ANETWORK_HELP      },
+    { SUBCAT_ADVANCED_MISC,        CAT_ADVANCED,   AADVANCED_TITLE,           AADVANCED_HELP     },
+    { SUBCAT_ADVANCED_NETWORK,     CAT_ADVANCED,   N_("Network"),             ANETWORK_HELP      },
 
-    { SUBCAT_HIDDEN,               CAT_HIDDEN,     NULL,                 NULL               },
+    { SUBCAT_HIDDEN,               CAT_HIDDEN,     NULL,                      NULL               },
 };
 
 /** Get the table index for the given category entry. */
