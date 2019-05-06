24dc03640d3cfd89aa87ce63929aba17911c4b85
Niklas Haas <git@haasn.xyz>
2019-05-06 09:56:38 +0200

    opengl: update libplacebo call for changed API
    This will be the last time a change to this signature is needed, since
    we just switched to using a params struct rather than updating the
    signature constantly.
    
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- modules/video_output/opengl/vout_helper.c
+++ modules/video_output/opengl/vout_helper.c
@@ -551,7 +551,9 @@ opengl_init_program(vout_display_opengl_t *vgl, vlc_video_context *context,
     {
         tc->pl_ctx = vlc_placebo_Create(VLC_OBJECT(tc));
         if (tc->pl_ctx) {
-#   if PL_API_VER >= 6
+#   if PL_API_VER >= 20
+            tc->pl_sh = pl_shader_alloc(tc->pl_ctx, NULL);
+#   elif PL_API_VER >= 6
             tc->pl_sh = pl_shader_alloc(tc->pl_ctx, NULL, 0);
 #   else
             tc->pl_sh = pl_shader_alloc(tc->pl_ctx, NULL, 0, 0);
