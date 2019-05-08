587a5e2ac478b151848c06a55b1c9f7960f272de
Rémi Denis-Courmont <remi@remlab.net>
2019-05-08 18:54:01 +0300

    vout: remove dead code
    If the vout was not stopped explicitly, vout_Close() will call
    vout_Stop() anyway. Either way, the window is necessarily disabled when
    the last reference to the vout is dropped.

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -1715,8 +1715,7 @@ void vout_Release(vout_thread_t *vout)
     vlc_mutex_destroy(&vout->p->spu_lock);
     vlc_mutex_destroy(&vout->p->filter.lock);
 
-    if (sys->window_active)
-        vout_window_Disable(sys->display_cfg.window);
+    assert(!sys->window_active);
     vout_display_window_Delete(sys->display_cfg.window);
 
     vout_control_Clean(&vout->p->control);
