videolan/vlc

commit a1924dbd8bb3e3c146ef150191dcf8ffa0be0a59

resource: atomically return vout on failure (fixes #22284)
If vout_Request() fails, atomically mark the still stopped vout as free.
This removes a quick hack from 4586ffaf28c0756e48d0fa607083417dda630a2d.

src/input/resource.c
@@ -356,8 +356,11 @@ vout_thread_t *input_resource_GetVout(input_resource_t *p_resource,
     }
 
     if (vout_Request(cfg, p_resource->p_input)) {
+        if (p_resource->p_vout_free == NULL && p_resource->i_vout == 0)
+            p_resource->p_vout_free = vout;
+        else
+            vout_Close(vout);
         vlc_mutex_unlock(&p_resource->lock);
-        input_resource_PutVout(p_resource, cfg->vout);
         return NULL;
     }
 

commit 9f668f760177a702ba706787d29953589188592f

resource: fix uninitialized pointer dereference
This commit fixes a regression from a1924dbd8bb3e3c146ef150191dcf8ffa0be0a59
that (unintentionally) reverted 3505ecb9d6b3edb4e68322cedb3dfe9ee895ff5b

src/input/resource.c
@@ -357,9 +357,9 @@ vout_thread_t *input_resource_GetVout(input_resource_t *p_resource,
 
     if (vout_Request(cfg, p_resource->p_input)) {
         if (p_resource->p_vout_free == NULL && p_resource->i_vout == 0)
-            p_resource->p_vout_free = vout;
+            p_resource->p_vout_free = cfg->vout;
         else
-            vout_Close(vout);
+            vout_Close(cfg->vout);
         vlc_mutex_unlock(&p_resource->lock);
         return NULL;
     }