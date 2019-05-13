7304217b420e4ef9ddd2e6a13b0de4c093fda7a9
Lyndon Brown <jnqnfe@gmail.com>
2019-05-13 02:28:13 +0100

    directsound: translate option choice labels

--- modules/audio_output/directsound.c
+++ modules/audio_output/directsound.c
@@ -48,8 +48,8 @@ static HRESULT StreamStart( aout_stream_t *, audio_sample_format_t *,
                             const GUID * );
 static void * PlayedDataEraser( void * );
 /* Speaker setup override options list */
-static const char *const speaker_list[] = { "Windows default", "Mono", "Stereo",
-                                            "Quad", "5.1", "7.1" };
+static const char *const speaker_list[] = { N_("Windows default"), N_("Mono"), N_("Stereo"),
+                                            N_("Quad"), N_("5.1"), N_("7.1") };
 
 /*****************************************************************************
  * Module descriptor
