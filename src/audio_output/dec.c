e0ba0752e60fef648ad4ebbfab5b0cb4fe025240
Tristan Matthews <tmatth@videolan.org>
2019-05-15 11:54:35 -0400

    audio_output: increase allowed max sample rate to 384 kHz
    Fixes #21925
    
    (cherry picked from commit 45bd86364e1eafbd1aad5f803e836b18b5b6dcad)
    Signed-off-by: Tristan Matthews <tmatth@videolan.org>

--- src/audio_output/dec.c
+++ src/audio_output/dec.c
@@ -58,7 +58,7 @@ int aout_DecNew( audio_output_t *p_aout,
         }
     }
 
-    if( p_format->i_rate > 352800 )
+    if( p_format->i_rate > 384000 )
     {
         msg_Err( p_aout, "excessive audio sample frequency (%u)",
                  p_format->i_rate );
