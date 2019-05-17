f7cc4f770613f6f30a7a4348f452466ff53b46ba
Alexandre Janniaux <ajanni@videolabs.io>
2019-05-17 15:44:15 +0200

    viewpoint: add test for conversion to 4x4 matrix

--- test/src/misc/viewpoint.c
+++ test/src/misc/viewpoint.c
@@ -22,6 +22,51 @@
 #include <math.h>
 #include "../../libvlc/test.h"
 
+
+static void mat4x4_for_angles( float *m, float *angles )
+{
+    const float yaw   = -angles[0] * (float)M_PI / 180.f;
+    const float pitch = -angles[1] * (float)M_PI / 180.f;
+    const float roll  = -angles[2] * (float)M_PI / 180.f;
+
+    float s, c;
+
+    s = sinf(pitch);
+    c = cosf(pitch);
+    const float x_rot[4][4] = {
+        { 1.f,    0.f,    0.f,    0.f },
+        { 0.f,    c,      -s,      0.f },
+        { 0.f,    s,      c,      0.f },
+        { 0.f,    0.f,    0.f,    1.f } };
+
+    s = sinf(yaw);
+    c = cosf(yaw);
+    const float y_rot[4][4] = {
+        { c,      0.f,    s,     0.f },
+        { 0.f,    1.f,    0.f,    0.f },
+        { -s,      0.f,    c,      0.f },
+        { 0.f,    0.f,    0.f,    1.f } };
+
+    s = sinf(roll);
+    c = cosf(roll);
+    const float z_rot[4][4] = {
+        { c,      s,      0.f,    0.f },
+        { -s,     c,      0.f,    0.f },
+        { 0.f,    0.f,    1.f,    0.f },
+        { 0.f,    0.f,    0.f,    1.f } };
+
+    /**
+     * Column-major matrix multiplication mathematically equal to
+     * z_rot * x_rot * y_rot
+     */
+    memset(m, 0, 16 * sizeof(float));
+    for (int i=0; i<4; ++i)
+        for (int j=0; j<4; ++j)
+            for (int k=0; k<4; ++k)
+                for (int l=0; l<4; ++l)
+                    m[4*i+l] += y_rot[i][j] * x_rot[j][k] * z_rot[k][l];
+}
+
 static bool
 compare_angles(float epsilon, const float a1[3], const float a2[3])
 {
@@ -84,11 +129,93 @@ static void test_conversion()
     assert(reciprocal_euler(epsilon, -45.f, -45.f, -45.f));
 }
 
+static int fuzzy_memcmp(const float *a, const float *b,
+                         size_t size, float epsilon)
+{
+    for (size_t i=0; i < size; ++i)
+    {
+        if (fabs(a[i]-b[i]) > epsilon)
+        {
+            fprintf(stderr, "Difference at %zu, a[%zu]=%f, b[%zu]=%f\n",
+                    i, i, a[i], i, b[i]);
+            return a[i] < b[i] ? -1 : 1;
+        }
+    }
+    return 0;
+}
+
+struct example_mat4x4
+{
+    float angles[3];
+};
+
+struct example_mat4x4 examples_mat4x4[] = {
+    { .angles = {  0.f,   45.f,  45.f } },
+    { .angles = { 45.f,   45.f,  0.f  } },
+    { .angles = { 45.f,   0.f,   0.f  } },
+    { .angles = { -45.f,  0.f,   0.f  } },
+    { .angles = {  0.f,   45.f,  0.f  } },
+    { .angles = {  0.f,    0.f,  45.f } },
+    { .angles = { 90.f, 0.f, 0.f } },
+    { .angles = { 0.f, 90.f, 0.f } },
+    { .angles = { 0.f, 0.f, 90.f } },
+    { .angles = { 90.f, 90.f, 0.f } },
+    { .angles = { 90.f, 0.f, 90.f } },
+    { .angles = { 0.f, 90.f, 90.f } },
+    { .angles = { 90.f, 90.f, 90.f } },
+};
+
+static void
+test_conversion_viewpoint_mat4x4()
+{
+    const float epsilon = 0.1f;
+    vlc_viewpoint_t vp;
+    float mat[16];
+
+#define MATLINE "[%f %f %f %f]\n"
+#define MAT MATLINE MATLINE MATLINE MATLINE
+#define printmat(title, mat) \
+    fprintf(stderr, title ":\n" MAT "\n", \
+           mat[0],  mat[1],  mat[2], mat[3], \
+           mat[4],  mat[5],  mat[6], mat[7], \
+           mat[8],  mat[9],  mat[10], mat[11], \
+           mat[12], mat[13], mat[14], mat[15]);
+
+    bool success = true;
+    for (size_t i=0; i<ARRAY_SIZE(examples_mat4x4); ++i)
+    {
+        struct example_mat4x4 *ex = &examples_mat4x4[i];
+        vlc_viewpoint_from_euler(&vp,
+                                 ex->angles[0],
+                                 ex->angles[1],
+                                 ex->angles[2]);
+        vlc_viewpoint_to_4x4(&vp, mat);
+        fprintf(stderr, "angles: %f %f %f\n",
+                ex->angles[0], ex->angles[1],
+                ex->angles[2]);
+
+        float expect_mat[16];
+        mat4x4_for_angles(expect_mat, ex->angles);
+
+        bool diff = fuzzy_memcmp(mat, expect_mat,
+                                 ARRAY_SIZE(mat), epsilon);
+        if (!diff)
+            continue;
+
+        printmat("EXPECT COMPUTED", expect_mat);
+        printmat("RESULT", mat);
+        success = false;
+    }
+
+    assert(success);
+}
+
 int main( void )
 {
     test_init();
 
     test_conversion();
+    test_conversion_viewpoint_mat4x4();
 
     return 0;
 }
