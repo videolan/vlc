e989a5121d3b4356908f7b4b24a44bb0ba393ff8
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-05-20 17:17:49 +0200

    subsvtt: Don't pass a NULL pointer to qsort
    (cherry picked from commit 5ef3830f385bfa6a47e9088f9a0d13062cd61c3a)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- modules/codec/webvtt/subsvtt.c
+++ modules/codec/webvtt/subsvtt.c
@@ -1807,6 +1807,11 @@ static void Render( decoder_t *p_dec, mtime_t i_start, mtime_t i_stop )
     vlc_array_init( &timedtags );
 
     GetTimedTags( p_sys->p_root->p_child, i_start, i_stop, &timedtags );
+    if( timedtags.i_count == 0 )
+    {
+        vlc_array_clear( &timedtags );
+        return;
+    }
     qsort( timedtags.pp_elems, timedtags.i_count, sizeof(*timedtags.pp_elems), timedtagsArrayCmp );
 
     mtime_t i_substart = i_start;
