videolan/vlc

commit c988b8d58b01ef6d628e3051774a2032dd7f6b7d

avcodec: fix black screen with hdtv-fixed videos
In case of indirect rendering.

This fixes a regression from dfb767189073493336df0cf43f7d0b6bf51f63e3

Fixes #20175

modules/codec/avcodec/video.c
@@ -365,8 +365,8 @@ static int lavc_CopyPicture(decoder_t *dec, picture_t *pic, AVFrame *frame)
                 sys->p_context->pix_fmt, (name != NULL) ? name : "unknown");
         return VLC_EGENERIC;
     } else if (fourcc != pic->format.i_chroma
-     || frame->width != (int) pic->format.i_visible_width
-     || frame->height != (int) pic->format.i_visible_height)
+     || frame->width > (int) pic->format.i_width
+     || frame->height > (int) pic->format.i_height)
     {
         msg_Warn(dec, "dropping frame because the vout changed");
         return VLC_EGENERIC;

commit 603ecaf0f3fdf3b0a83cd2c773e05ac347b2149a

codec: avcodec: fix broken check before copy (fix #22240)
copy parameters are the picture ones

regression by c988b8d58b01ef6d628e3051774a2032dd7f6b7d

modules/codec/avcodec/video.c
@@ -415,8 +415,9 @@ static int lavc_CopyPicture(decoder_t *dec, picture_t *pic, AVFrame *frame)
                 sys->p_context->pix_fmt, (name != NULL) ? name : "unknown");
         return VLC_EGENERIC;
     } else if (!chroma_compatible(fourcc, pic->format.i_chroma)
-     || frame->width > (int) pic->format.i_width
-     || frame->height > (int) pic->format.i_height)
+     /* ensure we never read more than dst lines/pixels from src */
+     || frame->width != (int) pic->format.i_visible_width
+     || frame->height < (int) pic->format.i_visible_height)
     {
         msg_Warn(dec, "dropping frame because the vout changed");
         return VLC_EGENERIC;