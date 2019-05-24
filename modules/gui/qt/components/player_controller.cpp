8d0444f14b7ad4df7aea62efaa124cbf0fc498b4
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-05-24 13:57:37 +0200

    qt: player_controller: Explicitely initalize on_device_changed

--- modules/gui/qt/components/player_controller.cpp
+++ modules/gui/qt/components/player_controller.cpp
@@ -846,6 +846,7 @@ static const struct vlc_player_vout_cbs player_vout_cbs = {
 static const struct vlc_player_aout_cbs player_aout_cbs = {
     on_player_aout_volume_changed,
     on_player_aout_mute_changed,
+    nullptr
 };
 
 PlayerControllerPrivate::PlayerControllerPrivate(PlayerController *playercontroller, intf_thread_t *p_intf)
