c76cb856732de985a0701bcefe88037c3822a92b
Lyndon Brown <jnqnfe@gmail.com>
2019-05-29 05:51:17 +0100

    qt: (hotkeys) remove incorrect hotkey table label tooltip
    the item pointer provided to the constructor is that of a single hotkey
    config item. using the longtext from this item in a tooltip on the label
    for the entire hotkey table makes zero sense, so let's remove it.
    
     - for the table shown in the simple view, the "key-play" option was
       arbitrarily used for this.
     - for the table shown in the advanced view, the text from the first
       hotkey config item encountered was used.
    
    (cherry picked from commit b0385e9324a0f342e51bf5b61a50904adb4d47dd)

--- modules/gui/qt/components/preferences_widgets.cpp
+++ modules/gui/qt/components/preferences_widgets.cpp
@@ -1187,9 +1187,6 @@ void KeySelectorControl::buildAppHotkeysList( QWidget *rootWidget )
 
 void KeySelectorControl::finish()
 {
-    if( label && p_item->psz_longtext )
-        label->setToolTip( formatTooltip( qtr( p_item->psz_longtext ) ) );
-
     /* Fill the table */
 
     /* Get the main Module */
