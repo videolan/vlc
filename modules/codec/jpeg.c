bb405fd42020c877fe9ce301004da499dfc7e050
Steve Lhomme <robux4@ycbcr.xyz>
2019-06-03 14:12:54 +0200

    jpeg: set the colorspace and primaries to sRGB as well
    The transfer was already set.
    
    If we don't do that it may be assumed to be 709 or 601 depending on
    the picture dimensions.

--- modules/codec/jpeg.c
+++ modules/codec/jpeg.c
@@ -180,7 +180,9 @@ static int OpenDecoder(vlc_object_t *p_this)
 
     p_dec->fmt_out.video.i_chroma =
     p_dec->fmt_out.i_codec = VLC_CODEC_RGB24;
-    p_dec->fmt_out.video.transfer = TRANSFER_FUNC_SRGB;
+    p_dec->fmt_out.video.transfer  = TRANSFER_FUNC_SRGB;
+    p_dec->fmt_out.video.space     = COLOR_SPACE_SRGB;
+    p_dec->fmt_out.video.primaries = COLOR_PRIMARIES_SRGB;
     p_dec->fmt_out.video.color_range = COLOR_RANGE_FULL;
     video_format_FixRgb(&p_dec->fmt_out.video);
 
