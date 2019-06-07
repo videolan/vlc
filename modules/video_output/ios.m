a1e111e0b53f7625002b895eefd52339abe53e7f
Marvin Scholz <epirat07@gmail.com>
2019-06-07 19:27:08 +0200

    vout/ios: remove accidentally added #if 0
    The file does not build fine with this, as it does not import
    the needed headers.
    
    This is a partial revert of 1aa83e2cfab4a0e9f02b5a5250e681f7f2799df5
    
    Signed-off-by: Felix Paul Kühne <felix@feepk.net>

--- modules/video_output/ios.m
+++ modules/video_output/ios.m
@@ -29,13 +29,11 @@
  * Preamble
  *****************************************************************************/
 
-#if 0
 #import <UIKit/UIKit.h>
 #import <OpenGLES/EAGL.h>
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
 #import <QuartzCore/QuartzCore.h>
-#endif
 #import <dlfcn.h>
 
 #ifdef HAVE_CONFIG_H
