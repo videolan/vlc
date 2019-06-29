3dfa6d4ddd0922552e6575ded416f62b8ce828f5
Rémi Denis-Courmont <remi@remlab.net>
2019-06-29 17:36:03 +0300

    prefetch: match read size from versions 2.2
    VLC versions 2.2 and earlier had 3 "tracks" buffers of 4 MiB each for
    the input stream. This resulted in single reads of (up to) 4 MiB.
    This patch matches the read size and thus performance of said older
    versions.
    
    This matches 6f58cd15a29ec77f5117e709a5c5a7126b9148c8 in a less invasive
    fashion. Fixes #19806, #19988, #21470.

--- modules/stream_filter/prefetch.c
+++ modules/stream_filter/prefetch.c
@@ -534,7 +534,7 @@ vlc_module_begin()
     add_integer("prefetch-buffer-size", 1 << 14, N_("Buffer size"),
                 N_("Prefetch buffer size (KiB)"), false)
         change_integer_range(4, 1 << 20)
-    add_integer("prefetch-read-size", 1 << 14, N_("Read size"),
+    add_integer("prefetch-read-size", 1 << 24, N_("Read size"),
                 N_("Prefetch background read size (bytes)"), true)
         change_integer_range(1, 1 << 29)
     add_integer("prefetch-seek-threshold", 1 << 14, N_("Seek threshold"),
