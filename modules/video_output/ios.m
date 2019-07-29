3a868df5ebda8dd50274ad520cc37b8f05707fe5
Thomas Guillem <thomas@gllm.fr>
2019-07-29 15:18:28 +0200

    vout/ios: fix leak
    This partially reverts commit 9db9c1121193c2a201eb37e9858a603509f684a6.

--- modules/video_output/ios.m
+++ modules/video_output/ios.m
@@ -147,7 +147,7 @@ static int Open(vout_display_t *vd, const vout_display_cfg_t *cfg,
     if (vout_display_cfg_IsWindowed(cfg))
         return VLC_EGENERIC;
 
-    vout_display_sys_t *sys = calloc(1, sizeof(*sys));
+    vout_display_sys_t *sys = vlc_obj_calloc(vd, 1, sizeof(*sys));
 
     if (!sys)
         return VLC_ENOMEM;
