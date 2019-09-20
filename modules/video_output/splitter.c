50ecf81b85a350f03cfbbc8773aae9a3e0501269
Steve Lhomme <robux4@ycbcr.xyz>
2019-09-20 08:34:52 +0200

    splitter: remove intermediate static function used only once
    And only doing one call.

--- modules/video_output/splitter.c
+++ modules/video_output/splitter.c
@@ -220,14 +220,6 @@ static vout_window_t *video_splitter_CreateWindow(vlc_object_t *obj,
     return window;
 }
 
-static vout_display_t *vlc_vidsplit_CreateDisplay(vlc_object_t *obj,
-    const video_format_t *restrict source,
-    const vout_display_cfg_t *restrict cfg,
-    const char *name)
-{
-    return vout_display_New(obj, source, cfg, name, NULL);
-}
-
 static int vlc_vidsplit_Open(vout_display_t *vd,
                              const vout_display_cfg_t *cfg,
                              video_format_t *fmtp, vlc_video_context *ctx)
@@ -282,7 +274,6 @@ static int vlc_vidsplit_Open(vout_display_t *vd,
         };
         const char *modname = output->psz_module;
         struct vlc_vidsplit_part *part = &sys->parts[i];
-        vout_display_t *display;
 
         vlc_sem_init(&part->lock, 1);
         part->display = NULL;
@@ -298,8 +289,8 @@ static int vlc_vidsplit_Open(vout_display_t *vd,
         }
 
         vdcfg.window = part->window;
-        display = vlc_vidsplit_CreateDisplay(obj, &output->fmt, &vdcfg,
-                                             modname);
+        vout_display_t *display = vout_display_New(obj, &output->fmt, &vdcfg,
+                                                   modname, NULL);
         if (display == NULL) {
             vout_window_Disable(part->window);
             vout_window_Delete(part->window);
