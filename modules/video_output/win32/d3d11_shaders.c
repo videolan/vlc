35678ea5d918c1cfb4e6e6d70486b910f1d11be4
Steve Lhomme <robux4@ycbcr.xyz>
2019-10-14 13:56:54 +0200

    d3d11_shaders: fix AYUV pixel shader

--- modules/video_output/win32/d3d11_shaders.c
+++ modules/video_output/win32/d3d11_shaders.c
@@ -377,10 +377,10 @@ HRESULT D3D11_CompilePixelShader(vlc_object_t *o, d3d11_handle_t *hd3d, bool leg
             break;
         case DXGI_FORMAT_AYUV:
             psz_sampler[0] =
-                    "sample.x  = shaderTexture[0].Sample(SampleType, coords).z;\n"
-                    "sample.y  = shaderTexture[0].Sample(SampleType, coords).y;\n"
-                    "sample.z  = shaderTexture[0].Sample(SampleType, coords).x;\n"
-                    "sample.a  = shaderTexture[0].Sample(SampleType, coords).a;";
+                    "sample.x  = shaderTexture[0].Sample(samplerState, coords).z;\n"
+                    "sample.y  = shaderTexture[0].Sample(samplerState, coords).y;\n"
+                    "sample.z  = shaderTexture[0].Sample(samplerState, coords).x;\n"
+                    "sample.a  = shaderTexture[0].Sample(samplerState, coords).a;";
             break;
         case DXGI_FORMAT_R8G8B8A8_UNORM:
         case DXGI_FORMAT_B8G8R8A8_UNORM:
