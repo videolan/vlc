84134c554a4a3c5be78f818896a372a89bdaa792
Thomas Guillem <thomas@gllm.fr>
2019-10-15 15:44:55 +0200

    audiounit_ios: add support for > 48kHz sample rate
    Call setPreferredSampleRate() to setup the rate of the input. Failure is OK
    since it will fallback to a normal samplerate.
    
    Tested on a USB DAC with 96kHz support, but it should also work via HDMI.

--- modules/audio_output/audiounit_ios.m
+++ modules/audio_output/audiounit_ios.m
@@ -500,6 +500,13 @@ Start(audio_output_t *p_aout, audio_sample_format_t *restrict fmt)
      * should correspond to this number */
     avas_setPreferredNumberOfChannels(p_aout, fmt);
 
+    BOOL success = [p_sys->avInstance setPreferredSampleRate:fmt->i_rate error:nil];
+    if (!success)
+    {
+        /* Not critical, we can use any sample rates */
+        msg_Dbg(p_aout, "failed to set preferred sample rate");
+    }
+
     enum port_type port_type;
     int ret = avas_GetOptimalChannelLayout(p_aout, &port_type, &layout);
     if (ret != VLC_SUCCESS)
