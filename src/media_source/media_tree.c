15c26b36cc410fe0b8a0a6eb9f2950d27b72f8f3
Thomas Guillem <thomas@gllm.fr>
2019-10-17 10:23:28 +0200

    media_tree: interact with the user
    This fixes credential dialogs that were not shown when browsing share from UIs.

--- src/media_source/media_tree.c
+++ src/media_source/media_tree.c
@@ -329,7 +329,8 @@ vlc_media_tree_Preparse(vlc_media_tree_t *tree, libvlc_int_t *libvlc,
     VLC_UNUSED(input_preparser_callbacks);
 #else
     media->i_preparse_depth = 1;
-    vlc_MetadataRequest(libvlc, media, META_REQUEST_OPTION_SCOPE_ANY,
+    vlc_MetadataRequest(libvlc, media, META_REQUEST_OPTION_SCOPE_ANY |
+                        META_REQUEST_OPTION_DO_INTERACT,
                         &input_preparser_callbacks, tree, -1, NULL);
 #endif
 }
