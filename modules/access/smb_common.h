bf3c999938b86639b1fb084e3fd30e2fa6493d3d
Thomas Guillem <thomas@gllm.fr>
2019-10-17 16:31:07 +0200

    dsm: specify the v1 protocol in the dialog credential title

--- modules/access/smb_common.h
+++ modules/access/smb_common.h
@@ -28,6 +28,8 @@
 #define SMB_DOMAIN_LONGTEXT N_("Domain/Workgroup that " \
         "will be used for the connection.")
 
+#define SMB1_LOGIN_DIALOG_TITLE N_( "SMBv1 authentication required" )
+
 #define SMB_LOGIN_DIALOG_TITLE N_( "SMB authentication required" )
 #define SMB_LOGIN_DIALOG_TEXT N_( "The computer (%s) you are trying to connect "   \
     "to requires authentication.\nPlease provide a username (ideally a "  \
