93c3184ec0a19a8440f69f792027bced755af1a5
Steve Lhomme <robux4@ycbcr.xyz>
2019-10-24 10:12:21 +0200

    video output: remove unused variables after removing the decoder pool

--- src/video_output/vout_wrapper.c
+++ src/video_output/vout_wrapper.c
@@ -82,8 +82,6 @@ vout_display_t *vout_OpenWrapper(vout_thread_t *vout,
 
     sys->display_pool = NULL;
 
-    const bool use_dr = !vout_IsDisplayFiltered(vd);
-    const bool allow_dr = !vd->info.has_pictures_invalid && use_dr;
     const unsigned private_picture  = 4; /* XXX 3 for filter, 1 for SPU */
     const unsigned kept_picture     = 1; /* last displayed picture */
     const unsigned reserved_picture = DISPLAY_PICTURE_COUNT +
