97e382290b218746df2d0f45276879b905b2f646
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-11-28 16:29:43 +0100

    test: run_vlc.sh: Don't override existing ASAN_OPTIONS

--- test/run_vlc.sh
+++ test/run_vlc.sh
@@ -9,7 +9,7 @@ $VLC -Idummy vlc://quit
 $VLC -vv -Irc,oldrc vlc://quit
 $VLC -vv -Irc,oldrc --play-and-exit vlc://nop
 
-ASAN_OPTIONS="detect_leaks=0"
+ASAN_OPTIONS="$ASAN_OPTIONS,detect_leaks=0"
 export ASAN_OPTIONS
 
 $VLC --play-and-exit vlc://nop
