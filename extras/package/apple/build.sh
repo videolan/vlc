fd6a0aa839f97cf1f7902b4241094cec615e104b
Marvin Scholz <epirat07@gmail.com>
2019-12-10 15:16:28 +0100

    apple/build: Add FIXME

--- extras/package/apple/build.sh
+++ extras/package/apple/build.sh
@@ -447,6 +447,7 @@ validate_architecture "$VLC_HOST_ARCH"
 set_host_triplet "$VLC_HOST_ARCH"
 
 # Set pseudo-triplet
+# FIXME: This should match the actual clang triplet and should be used for compiler invocation too!
 readonly VLC_PSEUDO_TRIPLET="${VLC_HOST_ARCH}-apple-${VLC_HOST_PLATFORM}_${VLC_DEPLOYMENT_TARGET}"
 # Contrib install dir
 readonly VLC_CONTRIB_INSTALL_DIR="$VLC_BUILD_DIR/contrib/${VLC_HOST_ARCH}-${VLC_APPLE_SDK_NAME}"
