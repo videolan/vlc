f5e69654c1b49cd73e5da4cb6e09704a54db1312
Alexandre Janniaux <ajanni@videolabs.io>
2019-12-11 11:57:13 +0100

    mkv: remove deprecated std function
    std::mem_fun and std::bind2nd have been deprecated in C++11 and will be
    removed in C++17. They were replaced by std::mem_fn and std::bind which
    are easier to use.
    
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>
    (cherry picked from commit 6fce792e89c8f3879705d0fc52caa43f699d5393)
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- modules/demux/mkv/chapters.cpp
+++ modules/demux/mkv/chapters.cpp
@@ -146,13 +146,14 @@ bool chapter_item_c::EnterLeaveHelper_ ( bool do_subs,
     bool f_result = false;
 
     f_result |= std::count_if ( codecs.begin (), codecs.end (),
-      std::mem_fun (co_cb)
+      std::mem_fn (co_cb)
     );
 
     if ( do_subs )
     {
+        using std::placeholders::_1;
         f_result |= count_if ( sub_chapters.begin (), sub_chapters.end (),
-          std::bind2nd( std::mem_fun( ch_cb ), true )
+          std::bind( std::mem_fn( ch_cb ), _1, true )
         );
     }
 
