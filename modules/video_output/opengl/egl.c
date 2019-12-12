videolan/vlc

commit f5a766e3699f19f3a37f6331e62bdc5de0e7fd04

egl: explicitly destroy the context
The eglTerminate() documentation state that any contexts and surfaces
associated with the display are marked for deletion.

But this doesn't seem to be the case on Android with a GLES2 context.

modules/video_output/opengl/egl.c
@@ -182,6 +182,8 @@ static void Close (vlc_object_t *obj)
 
     if (sys->display != EGL_NO_DISPLAY)
     {
+        if (sys->context != EGL_NO_CONTEXT)
+            eglDestroyContext(sys->display, sys->context);
         if (sys->surface != EGL_NO_SURFACE)
             eglDestroySurface(sys->display, sys->surface);
         eglTerminate(sys->display);

commit d31777d83a94c63ed6b06d66fe56da4476bf5e78

egl: initialize context for error handling
Otherwise, we end up calling eglDestroyContext with a bad value in the
case the surface cannot be create. Not being able to create the surface
isn't even rare as it can happen if the format we required is not supported.

Harmless regression from f5a766e3699f19f3a37f6331e62bdc5de0e7fd04

Signed-off-by: Thomas Guillem <thomas@gllm.fr>

modules/video_output/opengl/egl.c
@@ -216,6 +216,7 @@ static int Open(vlc_gl_t *gl, const struct gl_api *api,
     gl->sys = sys;
     sys->display = EGL_NO_DISPLAY;
     sys->surface = EGL_NO_SURFACE;
+    sys->context = EGL_NO_CONTEXT;
     sys->eglCreateImageKHR = NULL;
     sys->eglDestroyImageKHR = NULL;
 
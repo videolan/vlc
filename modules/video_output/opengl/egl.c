d31777d83a94c63ed6b06d66fe56da4476bf5e78
Alexandre Janniaux <ajanni@videolabs.io>
2019-12-09 17:12:59 +0100

    egl: initialize context for error handling
    Otherwise, we end up calling eglDestroyContext with a bad value in the
    case the surface cannot be create. Not being able to create the surface
    isn't even rare as it can happen if the format we required is not supported.
    
    Harmless regression from f5a766e3699f19f3a37f6331e62bdc5de0e7fd04
    
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- modules/video_output/opengl/egl.c
+++ modules/video_output/opengl/egl.c
@@ -216,6 +216,7 @@ static int Open(vlc_gl_t *gl, const struct gl_api *api,
     gl->sys = sys;
     sys->display = EGL_NO_DISPLAY;
     sys->surface = EGL_NO_SURFACE;
+    sys->context = EGL_NO_CONTEXT;
     sys->eglCreateImageKHR = NULL;
     sys->eglDestroyImageKHR = NULL;
 
