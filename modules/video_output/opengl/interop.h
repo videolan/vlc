0f54633131f6ef11a2919474293b3f48373f02ce
Romain Vimont <rom1v@videolabs.io>
2020-01-22 18:58:29 +0100

    opengl: fix indentation
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- modules/video_output/opengl/interop.h
+++ modules/video_output/opengl/interop.h
@@ -158,7 +158,7 @@ struct vlc_gl_interop {
 
 static inline int
 opengl_interop_init(struct vlc_gl_interop *interop, GLenum tex_target,
-                     vlc_fourcc_t chroma, video_color_space_t yuv_space)
+                    vlc_fourcc_t chroma, video_color_space_t yuv_space)
 {
     return interop->init(interop, tex_target, chroma, yuv_space);
 }
