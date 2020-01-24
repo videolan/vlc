videolan/vlc

commit c515dc468dfffe8ed56042c2a3cc28fcb8e9d071

mediacodec: use the new push model
pictures, context and video context are now allocated from the mediacodec
module. This simplify a lot of things since MediaCodec is naturally a push API.

The mediacodec decoder module, via the decoder device will now decide of the vd
plugin used: android-display or gles2. It depends on how the LibVLC user setup
its surfaces or if the format need an OpenGL transformation (360 videos). The
decoder module need to know this informations before configuring MediaCodec in
order to choose the good Surface. The Surface will be either the SurfaceView
directly (AWindow_Video) or a SurfaceTexture that will be attached to a GL
texture (AWindow_SurfaceTexture) by the android gl converter.

Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

modules/codec/omxil/mediacodec.c
@@ -37,6 +37,7 @@
 #include <vlc_block_helper.h>
 #include <vlc_timestamp_helper.h>
 #include <vlc_threads.h>
+#include <vlc_atomic.h>
 #include <vlc_bits.h>
 
 #include "mediacodec.h"
@@ -44,12 +45,14 @@
 #include <OMX_Core.h>
 #include <OMX_Component.h>
 #include "omxil_utils.h"
-#include "../../video_output/android/display.h"
 
 #define BLOCK_FLAG_CSD (0x01 << BLOCK_FLAG_PRIVATE_SHIFT)
 
 #define DECODE_FLAG_RESTART (0x01)
 #define DECODE_FLAG_DRAIN (0x02)
+
+#define MAX_PIC 64
+
 /**
  * Callback called when a new block is processed from DecodeBlock.
  * It returns -1 in case of error, 0 if block should be dropped, 1 otherwise.
@@ -67,6 +70,13 @@ typedef void (*dec_on_flush_cb)(decoder_t *);
  */
 typedef int (*dec_process_output_cb)(decoder_t *, mc_api_out *, picture_t **,
                                      block_t **);
+
+struct android_picture_ctx
+{
+    picture_context_t s;
+    atomic_uint refs;
+    atomic_int index;
+};
 
 typedef struct
 {
@@ -106,19 +116,19 @@ typedef struct
     bool            b_adaptive;
     int             i_decode_flags;
 
+    enum es_format_category_e cat;
     union
     {
         struct
         {
+            vlc_video_context *ctx;
+            struct android_picture_ctx apic_ctxs[MAX_PIC];
             void *p_surface, *p_jsurface;
             unsigned i_angle;
             unsigned i_input_width, i_input_height;
             unsigned int i_stride, i_slice_height;
             int i_pixel_format;
             struct hxxx_helper hh;
-            /* stores the inflight picture for each output buffer or NULL */
-            picture_sys_t** pp_inflight_pictures;
-            unsigned int i_inflight_pictures;
             timestamp_fifo_t *timestamp_fifo;
             int i_mpeg_dar_num, i_mpeg_dar_den;
         } video;
@@ -160,8 +170,7 @@ static void DecodeFlush(decoder_t *);
 static void StopMediaCodec(decoder_sys_t *);
 static void *OutThread(void *);
 
-static void InvalidateAllPictures(decoder_sys_t *);
-static void RemoveInflightPictures(decoder_sys_t *);
+static void ReleaseAllPictureContexts(decoder_sys_t *);
 
 /*****************************************************************************
  * Module descriptor
@@ -437,51 +446,6 @@ static int ParseExtra(decoder_t *p_dec)
         return VLC_SUCCESS;
 }
 
-static int UpdateVout(decoder_t *p_dec)
-{
-    decoder_sys_t *p_sys = p_dec->p_sys;
-
-    if ((p_dec->fmt_in.i_codec == VLC_CODEC_MPGV ||
-         p_dec->fmt_in.i_codec == VLC_CODEC_MP2V) &&
-        (p_sys->video.i_mpeg_dar_num * p_sys->video.i_mpeg_dar_den != 0))
-    {
-        p_dec->fmt_out.video.i_sar_num =
-            p_sys->video.i_mpeg_dar_num * p_dec->fmt_out.video.i_height;
-        p_dec->fmt_out.video.i_sar_den =
-            p_sys->video.i_mpeg_dar_den * p_dec->fmt_out.video.i_width;
-    }
-
-    /* If MediaCodec can handle the rotation, reset the orientation to
-     * Normal in order to ask the vout not to rotate. */
-    if (p_sys->video.i_angle != 0)
-    {
-        assert(p_dec->fmt_out.i_codec == VLC_CODEC_ANDROID_OPAQUE);
-        p_dec->fmt_out.video.orientation = p_dec->fmt_in.video.orientation;
-        video_format_TransformTo(&p_dec->fmt_out.video, ORIENT_NORMAL);
-    }
-
-    if (decoder_UpdateVideoFormat(p_dec) != 0)
-        return VLC_EGENERIC;
-
-    if (p_dec->fmt_out.i_codec != VLC_CODEC_ANDROID_OPAQUE)
-        return VLC_SUCCESS;
-
-    /* Direct rendering: get the surface attached to the VOUT */
-    picture_t *p_dummy_hwpic = decoder_NewPicture(p_dec);
-    if (p_dummy_hwpic == NULL)
-        return VLC_EGENERIC;
-
-    picture_sys_t *p_picsys = p_dummy_hwpic->p_sys;
-    assert(p_picsys);
-    assert(p_picsys->hw.p_surface);
-    assert(p_picsys->hw.p_jsurface);
-
-    p_sys->video.p_surface = p_picsys->hw.p_surface;
-    p_sys->video.p_jsurface = p_picsys->hw.p_jsurface;
-    picture_Release(p_dummy_hwpic);
-    return VLC_SUCCESS;
-}
-
 /*****************************************************************************
  * StartMediaCodec: Create the mediacodec instance
  *****************************************************************************/
@@ -498,6 +462,7 @@ static int StartMediaCodec(decoder_t *p_dec)
 
         args.video.p_surface = p_sys->video.p_surface;
         args.video.p_jsurface = p_sys->video.p_jsurface;
+
         args.video.b_tunneled_playback = args.video.p_surface ?
                 var_InheritBool(p_dec, CFG_PREFIX "tunneled-playback") : false;
         if (p_sys->b_adaptive)
@@ -527,10 +492,214 @@ static void StopMediaCodec(decoder_sys_t *p_sys)
 {
     /* Remove all pictures that are currently in flight in order
      * to prevent the vout from using destroyed output buffers. */
-    if (p_sys->api.b_direct_rendering)
-        RemoveInflightPictures(p_sys);
+    if (p_sys->cat == VIDEO_ES)
+        ReleaseAllPictureContexts(p_sys);
 
     p_sys->api.stop(&p_sys->api);
+}
+
+static bool AndroidPictureContextRelease(struct android_picture_ctx *apctx,
+                                         bool render)
+{
+    int index = atomic_exchange(&apctx->index, -1);
+    if (index >= 0)
+    {
+        android_video_context_t *avctx =
+            vlc_video_context_GetPrivate(apctx->s.vctx, VLC_VIDEO_CONTEXT_AWINDOW);
+        decoder_sys_t *p_sys = avctx->dec_opaque;
+
+        p_sys->api.release_out(&p_sys->api, index, render);
+        return true;
+    }
+    return false;
+}
+
+static bool PictureContextRenderPic(struct picture_context_t *ctx)
+{
+    struct android_picture_ctx *apctx =
+        container_of(ctx, struct android_picture_ctx, s);
+
+    return AndroidPictureContextRelease(apctx, true);
+}
+
+static bool PictureContextRenderPicTs(struct picture_context_t *ctx,
+                                      vlc_tick_t ts)
+{
+    struct android_picture_ctx *apctx =
+        container_of(ctx, struct android_picture_ctx, s);
+
+    int index = atomic_exchange(&apctx->index, -1);
+    if (index >= 0)
+    {
+        android_video_context_t *avctx =
+            vlc_video_context_GetPrivate(ctx->vctx, VLC_VIDEO_CONTEXT_AWINDOW);
+        decoder_sys_t *p_sys = avctx->dec_opaque;
+
+        p_sys->api.release_out_ts(&p_sys->api, index, ts * INT64_C(1000));
+        return true;
+    }
+    return false;
+}
+
+static void PictureContextDestroy(struct picture_context_t *ctx)
+{
+    struct android_picture_ctx *apctx =
+        container_of(ctx, struct android_picture_ctx, s);
+
+    if (atomic_fetch_sub_explicit(&apctx->refs, 1, memory_order_acq_rel) == 1)
+        AndroidPictureContextRelease(apctx, false);
+}
+
+static struct picture_context_t *PictureContextCopy(struct picture_context_t *ctx)
+{
+    struct android_picture_ctx *apctx =
+        container_of(ctx, struct android_picture_ctx, s);
+
+    atomic_fetch_add_explicit(&apctx->refs, 1, memory_order_relaxed);
+    return ctx;
+}
+
+static void CleanFromVideoContext(void *priv)
+{
+    android_video_context_t *avctx = priv;
+    decoder_sys_t *p_sys = avctx->dec_opaque;
+
+    CleanDecoder(p_sys);
+}
+
+static void ReleaseAllPictureContexts(decoder_sys_t *p_sys)
+{
+    for (size_t i = 0; i < ARRAY_SIZE(p_sys->video.apic_ctxs); ++i)
+    {
+        struct android_picture_ctx *apctx = &p_sys->video.apic_ctxs[i];
+
+        /* Don't decrement apctx->refs, the picture_context should stay valid
+         * even if the underlying buffer is released since it might still be
+         * used by the vout (but the vout won't be able to render it). */
+        AndroidPictureContextRelease(apctx, false);
+    }
+}
+
+static struct android_picture_ctx *
+GetPictureContext(decoder_t *p_dec, unsigned index)
+{
+    decoder_sys_t *p_sys = p_dec->p_sys;
+    bool slept = false;
+
+    for (;;)
+    {
+        for (size_t i = 0; i < ARRAY_SIZE(p_sys->video.apic_ctxs); ++i)
+        {
+            struct android_picture_ctx *apctx = &p_sys->video.apic_ctxs[i];
+            /* Find an available picture context (ie. refs == 1) */
+            unsigned expected_refs = 1;
+            if (atomic_compare_exchange_strong(&apctx->refs, &expected_refs, 2))
+            {
+                int expected_index = -1;
+                /* Store the new index */
+                if (likely(atomic_compare_exchange_strong(&apctx->index,
+                                                          &expected_index, index)))
+                    return apctx;
+
+                /* Unlikely: Restore the ref count and try a next one, since
+                 * this picture context is being released. Cf.
+                 * PictureContextDestroy(), this function first decrement the
+                 * ref count before releasing the index.  */
+                atomic_store(&apctx->refs, 1);
+            }
+        }
+
+        /* This is very unlikely since there are generally more picture
+         * contexts than android MediaCodec buffers */
+        if (!slept)
+            msg_Warn(p_dec, "waiting for more picture contexts (unlikely)");
+        vlc_tick_sleep(VOUT_OUTMEM_SLEEP);
+        slept = true;
+    }
+}
+
+static int
+CreateVideoContext(decoder_t *p_dec)
+{
+    decoder_sys_t *p_sys = p_dec->p_sys;
+
+    /* If MediaCodec can handle the rotation, reset the orientation to
+     * Normal in order to ask the vout not to rotate. */
+    if (p_sys->video.i_angle != 0)
+    {
+        assert(p_dec->fmt_out.i_codec == VLC_CODEC_ANDROID_OPAQUE);
+        p_dec->fmt_out.video.orientation = p_dec->fmt_in.video.orientation;
+        video_format_TransformTo(&p_dec->fmt_out.video, ORIENT_NORMAL);
+    }
+
+    vlc_decoder_device *dec_dev = decoder_GetDecoderDevice(p_dec);
+    if (!dec_dev || dec_dev->type != VLC_DECODER_DEVICE_AWINDOW)
+    {
+        msg_Err(p_dec, "Could not find an AWINDOW decoder device");
+        return VLC_EGENERIC;
+    }
+
+    assert(dec_dev->opaque);
+    AWindowHandler *awh = dec_dev->opaque;
+    enum AWindow_ID id;
+
+    const bool has_subtitle_surface =
+        AWindowHandler_getANativeWindow(awh, AWindow_Subtitles) != NULL;
+
+    /* Force OpenGL interop (via AWindow_SurfaceTexture) if there is a
+     * projection or an orientation to handle, if the Surface owner is not able
+     * to modify its layout, or if there is no external subtitle surfaces. */
+
+    if (p_dec->fmt_out.video.projection_mode != PROJECTION_MODE_RECTANGULAR
+     || p_dec->fmt_out.video.orientation != ORIENT_NORMAL
+     || !AWindowHandler_canSetVideoLayout(awh)
+     || !has_subtitle_surface)
+        id = AWindow_SurfaceTexture;
+    else
+        id = AWindow_Video;
+
+    static const struct vlc_video_context_operations ops =
+    {
+        .destroy = CleanFromVideoContext,
+    };
+    p_sys->video.ctx =
+        vlc_video_context_Create(dec_dev, VLC_VIDEO_CONTEXT_AWINDOW,
+                                 sizeof(android_video_context_t), &ops);
+    vlc_decoder_device_Release(dec_dev);
+
+    if (!p_sys->video.ctx)
+        return VLC_EGENERIC;
+
+    p_sys->video.p_surface = AWindowHandler_getANativeWindow(awh, id);
+    p_sys->video.p_jsurface = AWindowHandler_getSurface(awh, id);
+    if (!p_sys->video.p_surface)
+    {
+        msg_Err(p_dec, "Could not find a valid ANativeWindow");
+        vlc_video_context_Release(p_sys->video.ctx);
+        p_sys->video.ctx = NULL;
+        return VLC_EGENERIC;
+    }
+
+    android_video_context_t *avctx =
+        vlc_video_context_GetPrivate(p_sys->video.ctx, VLC_VIDEO_CONTEXT_AWINDOW);
+    avctx->id = id;
+    avctx->dec_opaque = p_dec->p_sys;
+    avctx->render = PictureContextRenderPic;
+    avctx->render_ts = p_sys->api.release_out_ts ? PictureContextRenderPicTs : NULL;
+
+    for (size_t i = 0; i < ARRAY_SIZE(p_sys->video.apic_ctxs); ++i)
+    {
+        struct android_picture_ctx *apctx = &p_sys->video.apic_ctxs[i];
+
+        apctx->s = (picture_context_t) {
+            PictureContextDestroy, PictureContextCopy,
+            p_sys->video.ctx,
+        };
+        atomic_init(&apctx->index, -1);
+        atomic_init(&apctx->refs, 1);
+    }
+
+    return VLC_SUCCESS;
 }
 
 static void CleanInputVideo(decoder_t *p_dec)
@@ -704,9 +873,6 @@ static int OpenDecoder(vlc_object_t *p_this, pf_MediaCodecApi_init pf_init)
         if (!p_sys->video.timestamp_fifo)
             goto bailout;
 
-        TAB_INIT(p_sys->video.i_inflight_pictures,
-                 p_sys->video.pp_inflight_pictures);
-
         if (var_InheritBool(p_dec, CFG_PREFIX "dr"))
         {
             /* Direct rendering: Request a valid OPAQUE Vout in order to get
@@ -746,12 +912,13 @@ static int OpenDecoder(vlc_object_t *p_this, pf_MediaCodecApi_init pf_init)
             p_sys->video.i_input_height =
             p_dec->fmt_out.video.i_visible_height = p_dec->fmt_out.video.i_height;
 
-            if (UpdateVout(p_dec) != VLC_SUCCESS)
+            if (CreateVideoContext(p_dec) != VLC_SUCCESS)
             {
-                msg_Err(p_dec, "Opaque Vout request failed");
+                msg_Err(p_dec, "video context creation failed");
                 goto bailout;
             }
         }
+        p_sys->cat = VIDEO_ES;
     }
     else
     {
@@ -768,6 +935,7 @@ static int OpenDecoder(vlc_object_t *p_this, pf_MediaCodecApi_init pf_init)
         }
 
         p_dec->fmt_out.audio = p_dec->fmt_in.audio;
+        p_sys->cat = AUDIO_ES;
     }
 
     /* Try first to configure CSD */
@@ -869,58 +1037,10 @@ static void CloseDecoder(vlc_object_t *p_this)
 
     CleanInputVideo(p_dec);
 
-    CleanDecoder(p_sys);
-}
-
-/*****************************************************************************
- * vout callbacks
- *****************************************************************************/
-static void ReleasePicture(decoder_t *p_dec, unsigned i_index, bool b_render)
-{
-    decoder_sys_t *p_sys = p_dec->p_sys;
-
-    p_sys->api.release_out(&p_sys->api, i_index, b_render);
-}
-
-static void ReleasePictureTs(decoder_t *p_dec, unsigned i_index, vlc_tick_t i_ts)
-{
-    decoder_sys_t *p_sys = p_dec->p_sys;
-    assert(p_sys->api.release_out_ts);
-
-    p_sys->api.release_out_ts(&p_sys->api, i_index, i_ts * INT64_C(1000));
-}
-
-static void InvalidateAllPictures(decoder_sys_t *p_sys)
-{
-    for (unsigned int i = 0; i < p_sys->video.i_inflight_pictures; ++i)
-        AndroidOpaquePicture_Release(p_sys->video.pp_inflight_pictures[i],
-                                     false);
-}
-
-static int InsertInflightPicture(decoder_t *p_dec, picture_sys_t *p_picsys)
-{
-    decoder_sys_t *p_sys = p_dec->p_sys;
-
-    if (!p_picsys->hw.p_dec)
-    {
-        p_picsys->hw.p_dec = p_dec;
-        p_picsys->hw.pf_release = ReleasePicture;
-        if (p_sys->api.release_out_ts)
-            p_picsys->hw.pf_release_ts = ReleasePictureTs;
-        TAB_APPEND_CAST((picture_sys_t **),
-                        p_sys->video.i_inflight_pictures,
-                        p_sys->video.pp_inflight_pictures,
-                        p_picsys);
-    } /* else already attached */
-    return 0;
-}
-
-static void RemoveInflightPictures(decoder_sys_t *p_sys)
-{
-    for (unsigned int i = 0; i < p_sys->video.i_inflight_pictures; ++i)
-        AndroidOpaquePicture_DetachDecoder(p_sys->video.pp_inflight_pictures[i]);
-    TAB_CLEAN(p_sys->video.i_inflight_pictures,
-              p_sys->video.pp_inflight_pictures);
+    if (p_sys->video.ctx)
+        vlc_video_context_Release(p_sys->video.ctx);
+    else
+        CleanDecoder(p_sys);
 }
 
 static int Video_ProcessOutput(decoder_t *p_dec, mc_api_out *p_out,
@@ -970,9 +1090,11 @@ static int Video_ProcessOutput(decoder_t *p_dec, mc_api_out *p_out,
 
         if (p_sys->api.b_direct_rendering)
         {
-            picture_sys_t *p_picsys = p_pic->p_sys;
-            p_picsys->hw.i_index = p_out->buf.i_index;
-            InsertInflightPicture(p_dec, p_pic->p_sys);
+            struct android_picture_ctx *apctx =
+                GetPictureContext(p_dec,p_out->buf.i_index);
+            assert(apctx);
+            vlc_video_context_Hold(apctx->s.vctx);
+            p_pic->context = &apctx->s;
         } else {
             unsigned int chroma_div;
             GetVlcChromaSizes(p_dec->fmt_out.i_codec,
@@ -1053,7 +1175,18 @@ static int Video_ProcessOutput(decoder_t *p_dec, mc_api_out *p_out,
             p_sys->video.i_stride = p_dec->fmt_out.video.i_width;
         }
 
-        if (UpdateVout(p_dec) != VLC_SUCCESS)
+
+        if ((p_dec->fmt_in.i_codec == VLC_CODEC_MPGV ||
+             p_dec->fmt_in.i_codec == VLC_CODEC_MP2V) &&
+            (p_sys->video.i_mpeg_dar_num * p_sys->video.i_mpeg_dar_den != 0))
+        {
+            p_dec->fmt_out.video.i_sar_num =
+                p_sys->video.i_mpeg_dar_num * p_dec->fmt_out.video.i_height;
+            p_dec->fmt_out.video.i_sar_den =
+                p_sys->video.i_mpeg_dar_den * p_dec->fmt_out.video.i_width;
+        }
+
+        if (decoder_UpdateVideoOutput(p_dec, p_sys->video.ctx) != 0)
         {
             msg_Err(p_dec, "UpdateVout failed");
             return -1;
@@ -1412,7 +1545,8 @@ static int QueueBlockLocked(decoder_t *p_dec, block_t *p_in_block,
             if (!b_dequeue_timeout)
             {
                 msg_Warn(p_dec, "Decoder stuck: invalidate all buffers");
-                InvalidateAllPictures(p_sys);
+                if (p_sys->cat == VIDEO_ES)
+                    ReleaseAllPictureContexts(p_sys);
                 b_dequeue_timeout = true;
                 continue;
             }
@@ -1683,8 +1817,7 @@ static void Video_OnFlush(decoder_t *p_dec)
     /* Invalidate all pictures that are currently in flight
      * since flushing make all previous indices returned by
      * MediaCodec invalid. */
-    if (p_sys->api.b_direct_rendering)
-        InvalidateAllPictures(p_sys);
+    ReleaseAllPictureContexts(p_sys);
 }
 
 static int Audio_OnNewBlock(decoder_t *p_dec, block_t **pp_block)

include/vlc_picture.h
@@ -96,7 +96,7 @@ enum vlc_video_context_type
     VLC_VIDEO_CONTEXT_VDPAU,
     VLC_VIDEO_CONTEXT_DXVA2, /**< private: d3d9_video_context_t* */
     VLC_VIDEO_CONTEXT_D3D11VA,  /**< private: d3d11_video_context_t* */
-    VLC_VIDEO_CONTEXT_AWINDOW,
+    VLC_VIDEO_CONTEXT_AWINDOW, /**< private: android_video_context_t* */
     VLC_VIDEO_CONTEXT_NVDEC,
     VLC_VIDEO_CONTEXT_CVPX,
     VLC_VIDEO_CONTEXT_MMAL,

modules/video_output/Makefile.am
@@ -274,7 +274,6 @@ libandroid_window_plugin_la_CFLAGS = $(AM_CFLAGS)
 libandroid_window_plugin_la_LIBADD = $(LIBDL)
 
 libandroid_display_plugin_la_SOURCES = video_output/android/display.c \
-	video_output/android/display.h \
 	video_output/android/utils.c video_output/android/utils.h
 libandroid_display_plugin_la_CFLAGS = $(AM_CFLAGS)
 libandroid_display_plugin_la_LIBADD = $(LIBDL)

commit 28fe9476d4e856d354c2ae4aa9397383bc4e5299

mediacodec: don't transform the fmt too soon
Regression from c515dc468dfffe8ed56042c2a3cc28fcb8e9d071

The transformation must be done after the fmt_out is fully configured.

modules/codec/omxil/mediacodec.c
@@ -624,15 +624,6 @@ CreateVideoContext(decoder_t *p_dec)
 {
     decoder_sys_t *p_sys = p_dec->p_sys;
 
-    /* If MediaCodec can handle the rotation, reset the orientation to
-     * Normal in order to ask the vout not to rotate. */
-    if (p_sys->video.i_angle != 0)
-    {
-        assert(p_dec->fmt_out.i_codec == VLC_CODEC_ANDROID_OPAQUE);
-        p_dec->fmt_out.video.orientation = p_dec->fmt_in.video.orientation;
-        video_format_TransformTo(&p_dec->fmt_out.video, ORIENT_NORMAL);
-    }
-
     vlc_decoder_device *dec_dev = decoder_GetDecoderDevice(p_dec);
     if (!dec_dev || dec_dev->type != VLC_DECODER_DEVICE_AWINDOW)
     {
@@ -652,7 +643,7 @@ CreateVideoContext(decoder_t *p_dec)
      * to modify its layout, or if there is no external subtitle surfaces. */
 
     if (p_dec->fmt_out.video.projection_mode != PROJECTION_MODE_RECTANGULAR
-     || p_dec->fmt_out.video.orientation != ORIENT_NORMAL
+     || (!p_sys->api.b_support_rotation && p_dec->fmt_out.video.orientation != ORIENT_NORMAL)
      || !AWindowHandler_canSetVideoLayout(awh)
      || !has_subtitle_surface)
         id = AWindow_SurfaceTexture;
@@ -1183,6 +1174,15 @@ static int Video_ProcessOutput(decoder_t *p_dec, mc_api_out *p_out,
                 p_sys->video.i_mpeg_dar_num * p_dec->fmt_out.video.i_height;
             p_dec->fmt_out.video.i_sar_den =
                 p_sys->video.i_mpeg_dar_den * p_dec->fmt_out.video.i_width;
+        }
+
+        /* If MediaCodec can handle the rotation, reset the orientation to
+         * Normal in order to ask the vout not to rotate. */
+        if (p_sys->video.i_angle != 0)
+        {
+            assert(p_dec->fmt_out.i_codec == VLC_CODEC_ANDROID_OPAQUE);
+            p_dec->fmt_out.video.orientation = p_dec->fmt_in.video.orientation;
+            video_format_TransformTo(&p_dec->fmt_out.video, ORIENT_NORMAL);
         }
 
         if (decoder_UpdateVideoOutput(p_dec, p_sys->video.ctx) != 0)