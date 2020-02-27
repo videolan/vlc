36a58d83aaf6cd83934998e9223c18be85fd1b27
Rémi Denis-Courmont <remi@remlab.net>
2020-02-27 22:33:22 +0200

    Remove useless VLC atomic float helpers

--- include/vlc_atomic.h
+++ include/vlc_atomic.h
@@ -34,31 +34,6 @@
 # include <stdatomic.h>
 # include <vlc_common.h>
 
-typedef atomic_uint_least32_t vlc_atomic_float;
-
-static inline void vlc_atomic_init_float(vlc_atomic_float *var, float f)
-{
-    union { float f; uint32_t i; } u;
-    u.f = f;
-    atomic_init(var, u.i);
-}
-
-/** Helper to retrieve a single precision from an atom. */
-static inline float vlc_atomic_load_float(vlc_atomic_float *atom)
-{
-    union { float f; uint32_t i; } u;
-    u.i = atomic_load(atom);
-    return u.f;
-}
-
-/** Helper to store a single precision into an atom. */
-static inline void vlc_atomic_store_float(vlc_atomic_float *atom, float f)
-{
-    union { float f; uint32_t i; } u;
-    u.f = f;
-    atomic_store(atom, u.i);
-}
-
 typedef struct vlc_atomic_rc_t {
     atomic_uint refs;
 } vlc_atomic_rc_t;
