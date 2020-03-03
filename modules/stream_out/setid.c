e37191e86e37abc6d65d2ce53acefa3c891a84c1
Rémi Denis-Courmont <remi@remlab.net>
2020-03-03 23:16:04 +0200

    setid: remove dead code

--- modules/stream_out/setid.c
+++ modules/stream_out/setid.c
@@ -115,12 +115,6 @@ static int OpenCommon( vlc_object_t *p_this )
     sout_stream_t     *p_stream = (sout_stream_t*)p_this;
     sout_stream_sys_t *p_sys;
 
-    if( !p_stream->p_next )
-    {
-        msg_Err( p_stream, "cannot create chain" );
-        return VLC_EGENERIC;
-    }
-
     p_sys = malloc( sizeof( sout_stream_sys_t ) );
     if( unlikely( !p_sys ) )
         return VLC_ENOMEM;
