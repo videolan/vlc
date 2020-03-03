48daf5073a15001a771701ef1f9a36935b17ffca
Rémi Denis-Courmont <remi@remlab.net>
2020-03-03 23:15:43 +0200

    delay: remove dead code

--- modules/stream_out/delay.c
+++ modules/stream_out/delay.c
@@ -90,12 +90,6 @@ static int Open( vlc_object_t *p_this )
     sout_stream_t     *p_stream = (sout_stream_t*)p_this;
     sout_stream_sys_t *p_sys;
 
-    if( !p_stream->p_next )
-    {
-        msg_Err( p_stream, "cannot create chain" );
-        return VLC_EGENERIC;
-    }
-
     p_sys = calloc( 1, sizeof( sout_stream_sys_t ) );
     if( !p_sys )
         return VLC_ENOMEM;
