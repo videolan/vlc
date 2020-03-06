24f264abf3db72a2500c1c00bf9a8e4f7a799bf9
Francois Cartegnie <fcvlcdev@free.fr>
2020-03-06 17:02:46 +0100

    access: bluray: check for overlay pointer overwrite
    (cherry picked from commit 5c3b3c1cff3143174bce9dfdd5c142cc3b081ee1)

--- modules/access/bluray.c
+++ modules/access/bluray.c
@@ -1677,25 +1677,6 @@ static void blurayActivateOverlay(demux_t *p_demux, int plane)
     vlc_mutex_unlock(&ov->lock);
 }
 
-static void blurayInitOverlay(demux_t *p_demux, int plane, int width, int height)
-{
-    demux_sys_t *p_sys = p_demux->p_sys;
-
-    assert(p_sys->p_overlays[plane] == NULL);
-
-    bluray_overlay_t *ov = calloc(1, sizeof(*ov));
-    if (unlikely(ov == NULL))
-        return;
-
-    ov->width = width;
-    ov->height = height;
-    ov->i_channel = -1;
-
-    vlc_mutex_init(&ov->lock);
-
-    p_sys->p_overlays[plane] = ov;
-}
-
 /**
  * Destroy every regions in the subpicture.
  * This is done in two steps:
@@ -1719,6 +1700,31 @@ static void blurayClearOverlay(demux_t *p_demux, int plane)
     ov->status = Outdated;
 
     vlc_mutex_unlock(&ov->lock);
+}
+
+static void blurayInitOverlay(demux_t *p_demux, int plane, int width, int height)
+{
+    demux_sys_t *p_sys = p_demux->p_sys;
+
+    if(p_sys->p_overlays[plane])
+    {
+        /* Should not happen */
+        msg_Warn( p_demux, "Trying to init over an existing overlay" );
+        blurayClearOverlay( p_demux, plane );
+        blurayCloseOverlay( p_demux, plane );
+    }
+
+    bluray_overlay_t *ov = calloc(1, sizeof(*ov));
+    if (unlikely(ov == NULL))
+        return;
+
+    ov->width = width;
+    ov->height = height;
+    ov->i_channel = -1;
+
+    vlc_mutex_init(&ov->lock);
+
+    p_sys->p_overlays[plane] = ov;
 }
 
 /*
