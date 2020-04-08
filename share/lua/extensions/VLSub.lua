6faf917608f39e0a44993ed1d4169ca2928ff382
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2020-04-08 12:10:34 +0200

    VLSub: Bump version
    (cherry picked from commit 5703c3c309714b3e398b686b286d3dd984215fa3)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- share/lua/extensions/VLSub.lua
+++ share/lua/extensions/VLSub.lua
@@ -329,8 +329,8 @@ local select_conf = {} -- Drop down widget / option table association
 
 function descriptor()
   return {
-    title = "VLsub 0.11.0",
-    version = "0.11.0",
+    title = "VLsub 0.11.1",
+    version = "0.11.1",
     author = "exebetche",
     url = 'http://www.opensubtitles.org/',
     shortdesc = "VLsub";
@@ -1010,7 +1010,7 @@ openSub = {
     url = "http://api.opensubtitles.org/xml-rpc",
     path = nil,
     userAgentHTTP = "VLSub",
-    useragent = "VLSub 0.11.0",
+    useragent = "VLSub 0.11.1",
     translations_avail = {},
     downloadBehaviours = nil,
     languages = languages
