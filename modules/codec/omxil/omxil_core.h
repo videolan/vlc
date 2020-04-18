8e50cc985ed1e6e42387af0383c37cfb5ffbf1d7
Rémi Denis-Courmont <remi@remlab.net>
2020-04-18 13:43:14 +0300

    OpenMAX IL: fix missing extern qualifiers (fixes #24617)
    (cherry picked from commit e0173cb140d9c0193d74cb35bd887bb8456ef394)

--- modules/codec/omxil/omxil_core.h
+++ modules/codec/omxil/omxil_core.h
@@ -25,13 +25,13 @@
 
 #include <vlc_common.h>
 
-OMX_ERRORTYPE (*pf_init) (void);
-OMX_ERRORTYPE (*pf_deinit) (void);
-OMX_ERRORTYPE (*pf_get_handle) (OMX_HANDLETYPE *, OMX_STRING,
-                                OMX_PTR, OMX_CALLBACKTYPE *);
-OMX_ERRORTYPE (*pf_free_handle) (OMX_HANDLETYPE);
-OMX_ERRORTYPE (*pf_component_enum)(OMX_STRING, OMX_U32, OMX_U32);
-OMX_ERRORTYPE (*pf_get_roles_of_component)(OMX_STRING, OMX_U32 *, OMX_U8 **);
+extern OMX_ERRORTYPE (*pf_init)(void);
+extern OMX_ERRORTYPE (*pf_deinit)(void);
+extern OMX_ERRORTYPE (*pf_get_handle)(OMX_HANDLETYPE *, OMX_STRING,
+                                      OMX_PTR, OMX_CALLBACKTYPE *);
+extern OMX_ERRORTYPE (*pf_free_handle)(OMX_HANDLETYPE);
+extern OMX_ERRORTYPE (*pf_component_enum)(OMX_STRING, OMX_U32, OMX_U32);
+extern OMX_ERRORTYPE (*pf_get_roles_of_component)(OMX_STRING, OMX_U32 *, OMX_U8 **);
 
 /* Extra IOMX android functions. Can be NULL if we don't link with libiomx */
 OMX_ERRORTYPE (*pf_enable_graphic_buffers)(OMX_HANDLETYPE, OMX_U32, OMX_BOOL);
