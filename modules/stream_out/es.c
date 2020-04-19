00599fa19d948c5a2b8b4e8de7e409a40ccf3d30
Rémi Denis-Courmont <remi@remlab.net>
2020-04-19 14:15:02 +0300

    es: use vlc_obj_malloc() and check for errors

--- modules/stream_out/es.c
+++ modules/stream_out/es.c
@@ -281,10 +281,12 @@ static const char *const ppsz_sout_options[] = {
 static int Open( vlc_object_t *p_this )
 {
     sout_stream_t       *p_stream = (sout_stream_t*)p_this;
-    sout_stream_sys_t   *p_sys;
+    sout_stream_sys_t *p_sys = vlc_obj_malloc(p_this, sizeof (*p_sys));
+
+    if (unlikely(p_sys == NULL))
+        return VLC_ENOMEM;
 
     config_ChainParse( p_stream, SOUT_CFG_PREFIX, ppsz_sout_options, p_stream->p_cfg );
-    p_sys                   = malloc( sizeof( sout_stream_sys_t ) );
 
     p_sys->i_count          = 0;
     p_sys->i_count_audio    = 0;
@@ -331,8 +333,6 @@ static void Close( vlc_object_t * p_this )
     free( p_sys->psz_dst );
     free( p_sys->psz_dst_audio );
     free( p_sys->psz_dst_video );
-
-    free( p_sys );
 }
 
 /*****************************************************************************
