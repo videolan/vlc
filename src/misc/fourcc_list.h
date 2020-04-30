videolan/vlc

commit 1c36cd786a34308e448ca0d292ba090637f179a2

fourcc/es: unify EIA/CEA fourcc with channel in fmt

src/misc/fourcc_list.h
@@ -1602,11 +1602,7 @@ static const staticentry_t p_list_spu[] = {
     B(VLC_CODEC_SCTE_27, "SCTE-27 subtitles"),
         A("SC27"),
 
-    B(VLC_CODEC_EIA608_1, "EIA-608 subtitles"),
-        A("cc1 "),
-        A("cc2 "),
-        A("cc3 "),
-        A("cc4 "),
+    B(VLC_CODEC_CEA608,  "EIA-608 subtitles"),
 
     B(VLC_CODEC_TTML, "TTML subtitles"),
         A("ttml"),

include/vlc_es.h
@@ -536,6 +536,7 @@ struct subs_format_t
     } teletext;
     struct
     {
+        uint8_t i_channel;
         uint8_t i_reorder_depth; /* Reorder depth or transport video */
     } cc;
 

include/vlc_fourcc.h
@@ -539,11 +539,8 @@
 #define VLC_CODEC_EBU_STL   VLC_FOURCC('S','T','L',' ')
 #define VLC_CODEC_SCTE_18   VLC_FOURCC('S','C','1','8')
 #define VLC_CODEC_SCTE_27   VLC_FOURCC('S','C','2','7')
-/* EIA-608 */
-#define VLC_CODEC_EIA608_1  VLC_FOURCC('c','c','1',' ')
-#define VLC_CODEC_EIA608_2  VLC_FOURCC('c','c','2',' ')
-#define VLC_CODEC_EIA608_3  VLC_FOURCC('c','c','3',' ')
-#define VLC_CODEC_EIA608_4  VLC_FOURCC('c','c','4',' ')
+/* EIA/CEA-608 */
+#define VLC_CODEC_CEA608    VLC_FOURCC('c','6','0','8')
 #define VLC_CODEC_TTML      VLC_FOURCC('T','T','M','L')
 
 /* XYZ colorspace 12 bits packed in 16 bits, organisation |XXX0|YYY0|ZZZ0| */

modules/access/decklink.cpp
@@ -313,7 +313,7 @@ HRESULT DeckLinkCaptureDelegate::VideoInputFrameArrived(IDeckLinkVideoInputFrame
                     if (!sys->cc_es) {
                         es_format_t fmt;
 
-                        es_format_Init( &fmt, SPU_ES, VLC_CODEC_EIA608_1 );
+                        es_format_Init( &fmt, SPU_ES, VLC_CODEC_CEA608 );
                         fmt.psz_description = strdup(N_("Closed captions 1"));
                         if (fmt.psz_description) {
                             sys->cc_es = es_out_Add(demux_->out, &fmt);

modules/codec/cc.c
@@ -236,27 +236,18 @@ static int Open( vlc_object_t *p_this )
 {
     decoder_t     *p_dec = (decoder_t*)p_this;
     decoder_sys_t *p_sys;
-    int i_field;
-    int i_channel;
 
-    switch( p_dec->fmt_in.i_codec )
-    {
-        case VLC_CODEC_EIA608_1:
-            i_field = 0; i_channel = 1;
-            break;
-        case VLC_CODEC_EIA608_2:
-            i_field = 0; i_channel = 2;
-            break;
-        case VLC_CODEC_EIA608_3:
-            i_field = 1; i_channel = 1;
-            break;
-        case VLC_CODEC_EIA608_4:
-            i_field = 1; i_channel = 2;
-            break;
+    if( p_dec->fmt_in.i_codec != VLC_CODEC_CEA608 ||
+        p_dec->fmt_in.subs.cc.i_channel > 3 )
+        return VLC_EGENERIC;
 
-        default:
-            return VLC_EGENERIC;
-    }
+    /*  0 -> i_field = 0; i_channel = 1;
+        1 -> i_field = 0; i_channel = 2;
+        2 -> i_field = 1; i_channel = 1;
+        3 -> i_field = 1; i_channel = 2; */
+
+    const int i_field = p_dec->fmt_in.subs.cc.i_channel >> 1;
+    const int i_channel = 1 + (p_dec->fmt_in.subs.cc.i_channel & 1);
 
     p_dec->pf_decode = Decode;
     p_dec->pf_flush  = Flush;

modules/demux/mp4/essetup.c
@@ -1247,7 +1247,7 @@ int SetupSpuES( demux_t *p_demux, mp4_track_t *p_track, MP4_Box_t *p_sample )
             break;
         case ATOM_c608: /* EIA608 closed captions */
         //case ATOM_c708: /* EIA708 closed captions */
-            p_track->fmt.i_codec = VLC_CODEC_EIA608_1;
+            p_track->fmt.i_codec = VLC_CODEC_CEA608;
             break;
 
         case( VLC_FOURCC( 't', 'e', 'x', 't' ) ):

modules/demux/mp4/mp4.c
@@ -586,7 +586,7 @@ static block_t * MP4_Block_Convert( demux_t *p_demux, const mp4_track_t *p_track
             case VLC_CODEC_SPU:
             /* accept as-is */
             break;
-            case VLC_CODEC_EIA608_1:
+            case VLC_CODEC_CEA608:
                 p_block = MP4_EIA608_Convert( p_block );
             break;
             case VLC_CODEC_SUBT:

modules/demux/subtitle.c
@@ -720,7 +720,7 @@ static int Open ( vlc_object_t *p_this )
     }
     else if( p_sys->props.i_type == SUB_TYPE_SCC )
     {
-        es_format_Init( &fmt, SPU_ES, VLC_CODEC_EIA608_1 );
+        es_format_Init( &fmt, SPU_ES, VLC_CODEC_CEA608 );
     }
     else
         es_format_Init( &fmt, SPU_ES, VLC_CODEC_SUBT );

commit 45c8fe015cc6c35ab4849d73736537768990a1ef

fourcc: restore cc1..4 captions alias
seems it was not only an internal fourcc,
but is sent by at least one capture card

regression by 1c36cd786a34308e448ca0d292ba090637f179a2

src/misc/fourcc_list.h
@@ -1742,6 +1742,10 @@ static const staticentry_t p_list_spu[] = {
         A("SC27"),
 
     B(VLC_CODEC_CEA608,  "EIA-608 subtitles"),
+        A("cc1 "), /* acquisition devices */
+        A("cc2 "),
+        A("cc3 "),
+        A("cc4 "),
 
     B(VLC_CODEC_TTML, "TTML subtitles"),
         A("ttml"),