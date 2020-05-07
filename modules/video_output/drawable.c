ea72a487faae446b0a3d30da00020d78870b3312
Steve Lhomme <robux4@ycbcr.xyz>
2020-05-07 12:22:06 +0200

    vout: win32: drawable: move the known hwnd removal in a function
    No functional changes.

--- modules/video_output/drawable.c
+++ modules/video_output/drawable.c
@@ -64,6 +64,30 @@ static const struct vout_window_operations ops = {
     .destroy = Close,
 };
 
+static void RemoveDrawable(uintptr_t val)
+{
+    size_t n = 0;
+
+    /* Remove this drawable from the list of busy ones */
+    vlc_mutex_lock (&serializer);
+    assert (used != NULL);
+    while (used[n] != val)
+    {
+        assert (used[n]);
+        n++;
+    }
+    do
+        used[n] = used[n + 1];
+    while (used[++n] != 0);
+
+    if (n == 0)
+    {
+        free (used);
+        used = NULL;
+    }
+    vlc_mutex_unlock (&serializer);
+}
+
 /**
  * Find the drawable set by libvlc application.
  */
@@ -114,24 +138,6 @@ skip:
 static void Close (vout_window_t *wnd)
 {
     uintptr_t val = (uintptr_t)wnd->sys;
-    size_t n = 0;
 
-    /* Remove this drawable from the list of busy ones */
-    vlc_mutex_lock (&serializer);
-    assert (used != NULL);
-    while (used[n] != val)
-    {
-        assert (used[n]);
-        n++;
-    }
-    do
-        used[n] = used[n + 1];
-    while (used[++n] != 0);
-
-    if (n == 0)
-    {
-         free (used);
-         used = NULL;
-    }
-    vlc_mutex_unlock (&serializer);
+    RemoveDrawable(val);
 }
