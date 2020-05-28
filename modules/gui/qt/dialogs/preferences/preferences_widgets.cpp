f2d32344cfe9638f48981e067d0d94bfe95bf4d7
Pierre Lamot <pierre@videolabs.io>
2020-05-28 10:04:45 +0200

    qt: constructing QFlags from int is deprecated since Qt 5.15
      Zero initializing can be done with default constructor. here the default
      value of the parameter.

--- modules/gui/qt/dialogs/preferences/preferences_widgets.cpp
+++ modules/gui/qt/dialogs/preferences/preferences_widgets.cpp
@@ -270,7 +270,7 @@ void FileConfigControl::fillGrid( QGridLayout *l, int line )
     textAndButton->setMargin( 0 );
     textAndButton->addWidget( text, 2 );
     textAndButton->addWidget( browse, 0 );
-    l->addLayout( textAndButton, line, LAST_COLUMN, 0 );
+    l->addLayout( textAndButton, line, LAST_COLUMN );
 }
 
 FileConfigControl::FileConfigControl( vlc_object_t *_p_this,
@@ -514,7 +514,7 @@ ModuleConfigControl::ModuleConfigControl( vlc_object_t *_p_this,
 void ModuleConfigControl::fillGrid( QGridLayout *l, int line )
 {
     l->addWidget( label, line, 0 );
-    l->addWidget( combo, line, LAST_COLUMN, 0 );
+    l->addWidget( combo, line, LAST_COLUMN );
 }
 
 ModuleConfigControl::ModuleConfigControl( vlc_object_t *_p_this,
@@ -933,7 +933,7 @@ BoolConfigControl::BoolConfigControl( vlc_object_t *_p_this,
 
 void BoolConfigControl::fillGrid( QGridLayout *l, int line )
 {
-    l->addWidget( checkbox, line, 0, 1, -1, 0 );
+    l->addWidget( checkbox, line, 0, 1, -1 );
 }
 
 BoolConfigControl::BoolConfigControl( vlc_object_t *_p_this,
