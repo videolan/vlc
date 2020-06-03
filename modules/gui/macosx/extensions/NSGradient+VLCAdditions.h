2a5d25e03a051287248afb47e540e43b9a6afd6e
Marvin Scholz <epirat07@gmail.com>
2020-06-03 15:45:22 +0200

    macosx: add category for safe NSGradient drawing
    Trying to draw a NSGradient into an empty NSBezierPath throws
    and exception, crashing the application.
    
    To prevent that we have workarounds at every place where we called
    drawInBezierPath:angle:, instead just use a category which makes
    the code cleaner and keeps the logic for the workaround in one place.

---
+++ modules/gui/macosx/extensions/NSGradient+VLCAdditions.h
@@ -0,0 +1,33 @@
+/*****************************************************************************
+ * NSGradient+VLCAdditions.h: Category that adds safer bezier path operations
+ *****************************************************************************
+ * Copyright (C) 2020 VLC authors and VideoLAN
+ *
+ * Authors: Marvin Scholz <epirat07 at gmail dot org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+@interface NSGradient (VLCAdditions)
+
+/* Safe alternative to drawInBezierPath:angle: which will throw an
+ * exception when trying to draw into an empty NSBezierPath.
+ */
+- (void)vlc_safeDrawInBezierPath:(NSBezierPath *)path 
+                           angle:(CGFloat)angle;
+
+@end
