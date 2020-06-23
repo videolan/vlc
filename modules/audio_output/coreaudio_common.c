videolan/vlc

commit f9fce13591bf3f99c4d9401fe3ba67fb30b0fb75

coreaudio: fix invalid delay after a unpause
The render host time was not updated while paused.

This caused the first time_get(), after a unpause, to return a delay way too
early (corresponding to the pause time). This could happen only when the ca
render callback was not triggered between an unpause and a time_get.

This invalid delay caused the insertion of a long silence, that was not
interruptible, hence the impression of a deadlock.

Fixes #24668

modules/audio_output/coreaudio_common.c
@@ -139,7 +139,13 @@ ca_Render(audio_output_t *p_aout, uint32_t i_frames, uint64_t i_host_time,
         vlc_sem_post(&p_sys->flush_sem);
     }
 
-    if (p_sys->b_paused || unlikely(p_sys->i_first_render_host_time == 0))
+    if (p_sys->b_paused)
+    {
+        p_sys->i_render_host_time = i_host_time;
+        goto drop;
+    }
+
+    if (unlikely(p_sys->i_first_render_host_time == 0))
         goto drop;
 
     /* Start deferred: write silence (zeros) until we reach the first render

commit 1530679a64f82c8e4fc0ee485ae8b784a203f3f7

coreaudio: fix possible freeze after pause, seek, unpause
After a flush, i_first_render_host_time is reset to 0 and i_render_host_time
should not be touched since the playback has not started again yet. This caused
the i_first_render_host_time to be never setup.

Regression from f9fce13591bf3f99c4d9401fe3ba67fb30b0fb75
Fixes #24876

modules/audio_output/coreaudio_common.c
@@ -139,14 +139,14 @@ ca_Render(audio_output_t *p_aout, uint32_t i_frames, uint64_t i_host_time,
         vlc_sem_post(&p_sys->flush_sem);
     }
 
+    if (unlikely(p_sys->i_first_render_host_time == 0))
+        goto drop;
+
     if (p_sys->b_paused)
     {
         p_sys->i_render_host_time = i_host_time;
         goto drop;
     }
-
-    if (unlikely(p_sys->i_first_render_host_time == 0))
-        goto drop;
 
     /* Start deferred: write silence (zeros) until we reach the first render
      * host time. */