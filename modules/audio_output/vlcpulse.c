596c6cf0dcaca44debb2aa2e099ce58ce9e7f453
Damian Dixon <damiandixon@users.noreply.github.com>
2020-06-24 09:25:12 +0100

    context_wait return is a bool - return true or false
    context_wait was returning -1 or 0.
    
    The method signature calls for a bool return of true or false.

--- modules/audio_output/vlcpulse.c
+++ modules/audio_output/vlcpulse.c
@@ -64,10 +64,10 @@ static bool context_wait (pa_context *ctx, pa_threaded_mainloop *mainloop)
     while ((state = pa_context_get_state (ctx)) != PA_CONTEXT_READY)
     {
         if (state == PA_CONTEXT_FAILED || state == PA_CONTEXT_TERMINATED)
-            return -1;
+            return false;
         pa_threaded_mainloop_wait (mainloop);
     }
-    return 0;
+    return true;
 }
 
 static void context_event_cb(pa_context *c, const char *name, pa_proplist *pl,
