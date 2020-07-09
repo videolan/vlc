videolan/vlc

commit a8e6a381bd81bbe6838499bc8f1b68fef663f8cb

upnp: add basic network interface discovery for iOS and tvOS

modules/services_discovery/upnp.cpp
@@ -1602,9 +1602,39 @@ inline char *getPreferedAdapter()
 }
 #else
 
-static char *getPreferedAdapter()
+inline bool necessaryFlagsSetOnInterface(struct ifaddrs *anInterface)
 {
-    return NULL;
+    unsigned int flags = anInterface->ifa_flags;
+    if( (flags & IFF_UP) && (flags & IFF_RUNNING) && !(flags & IFF_LOOPBACK) && !(flags & IFF_POINTOPOINT) ) {
+        return true;
+    }
+    return false;
+}
+
+inline char *getPreferedAdapter()
+{
+    struct ifaddrs *listOfInterfaces;
+    struct ifaddrs *anInterface;
+    int ret = getifaddrs(&listOfInterfaces);
+    char *adapterName = NULL;
+
+    if (ret != 0) {
+        return NULL;
+    }
+
+    anInterface = listOfInterfaces;
+    while (anInterface != NULL) {
+        bool ret = necessaryFlagsSetOnInterface(anInterface);
+        if (ret) {
+            adapterName = strdup(anInterface->ifa_name);
+            break;
+        }
+
+        anInterface = anInterface->ifa_next;
+    }
+    freeifaddrs(listOfInterfaces);
+
+    return adapterName;
 }
 
 #endif

commit dd404ea81ded6f542e19fe47ac54a37f6f9e445a

upnp: restore default adapter lookup for Linux and Android
This addresses a build system regression in a8e6a381 and fixes #24943.

modules/services_discovery/upnp.cpp
@@ -1539,7 +1539,6 @@ done:
 
 #ifdef __APPLE__
 #include <TargetConditionals.h>
-#endif
 
 #if defined(TARGET_OS_OSX) && TARGET_OS_OSX
 #include <CoreFoundation/CoreFoundation.h>
@@ -1600,7 +1599,8 @@ inline char *getPreferedAdapter()
 
     return returnValue;
 }
-#else
+
+#else /* iOS and tvOS */
 
 inline bool necessaryFlagsSetOnInterface(struct ifaddrs *anInterface)
 {
@@ -1635,6 +1635,15 @@ inline char *getPreferedAdapter()
     freeifaddrs(listOfInterfaces);
 
     return adapterName;
+}
+
+#endif
+
+#else /* *nix and Android */
+
+inline char *getPreferedAdapter()
+{
+    return NULL;
 }
 
 #endif