582daa4ed6dcdb81ad0158996eaa3b640b273897
Francois Cartegnie <fcvlcdev@free.fr>
2020-07-15 20:10:41 +0200

    demux: mkv: fix potential leak
    (cherry picked from commit a517348372d94280b7eff9d2be51328768cff402)

--- modules/demux/mkv/mkv.cpp
+++ modules/demux/mkv/mkv.cpp
@@ -786,6 +786,7 @@ static int Demux( demux_t *p_demux)
             if( es_out_SetPCR( p_demux->out, i_pcr ) )
             {
                 msg_Err( p_demux, "ES_OUT_SET_PCR failed, aborting." );
+                delete block;
                 return 0;
             }
 
