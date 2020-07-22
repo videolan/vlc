a64695dd92954bd7ff1d62852b28d5d54bb3d002
Thomas Guillem <thomas@gllm.fr>
2020-07-22 15:19:07 +0200

    Revert "input: remove unused input_resource_ResetAout() function"
    This reverts commit 72d3ebd544a49340127577b253a3a4914be604cb.

--- src/input/resource.c
+++ src/input/resource.c
@@ -320,6 +320,22 @@ audio_output_t *input_resource_HoldAout( input_resource_t *p_resource )
     return p_aout;
 }
 
+void input_resource_ResetAout( input_resource_t *p_resource )
+{
+    audio_output_t *p_aout = NULL;
+
+    vlc_mutex_lock( &p_resource->lock_hold );
+    if( !p_resource->b_aout_busy )
+        p_aout = p_resource->p_aout;
+
+    p_resource->p_aout = NULL;
+    p_resource->b_aout_busy = false;
+    vlc_mutex_unlock( &p_resource->lock_hold );
+
+    if( p_aout != NULL )
+        aout_Destroy( p_aout );
+}
+
 /* Common */
 input_resource_t *input_resource_New( vlc_object_t *p_parent )
 {
