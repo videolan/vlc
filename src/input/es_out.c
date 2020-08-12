c213eb1b1c4e0e6d0637cadcb4d670d91717dcdb
Steve Lhomme <robux4@ycbcr.xyz>
2020-08-12 13:28:48 +0200

    es_out: only set the sout pace if it changed
    We already test if it's different so make use of it.

--- src/input/es_out.c
+++ src/input/es_out.c
@@ -2782,9 +2782,10 @@ static int EsOutSend( es_out_t *out, es_out_id_t *es, block_t *p_block )
         bool pace = sout_instance_ControlsPace(input_priv(p_input)->p_sout);
 
         if( input_priv(p_input)->b_out_pace_control != pace )
+        {
             msg_Dbg( p_input, "switching to %ssync mode", pace ? "a" : "" );
-
-        input_priv(p_input)->b_out_pace_control = pace;
+            input_priv(p_input)->b_out_pace_control = pace;
+        }
     }
 #endif
 
