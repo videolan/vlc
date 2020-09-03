85f4c0c8c3e473b4be4a219dd0a1b2449e24640c
Alexandre Janniaux <ajanni@videolabs.io>
2020-09-03 16:36:30 +0200

    video_chroma: grey_yuv: refactor initialization
    Remove switch used for one case and use VLC_EGENERIC/VLC_SUCCESS when
    suitable.

--- modules/video_chroma/grey_yuv.c
+++ modules/video_chroma/grey_yuv.c
@@ -68,35 +68,30 @@ static int Activate( vlc_object_t *p_this )
     if( p_filter->fmt_out.video.i_width & 1
      || p_filter->fmt_out.video.i_height & 1 )
     {
-        return -1;
+        return VLC_EGENERIC;
     }
 
     if( p_filter->fmt_in.video.i_width != p_filter->fmt_out.video.i_width
        || p_filter->fmt_in.video.i_height != p_filter->fmt_out.video.i_height
        || p_filter->fmt_in.video.orientation != p_filter->fmt_out.video.orientation )
-        return -1;
+        return VLC_EGENERIC;
 
-    switch( p_filter->fmt_in.video.i_chroma )
+    if ( p_filter->fmt_in.video.i_chroma != VLC_CODEC_GREY )
+        return VLC_EGENERIC;
+
+    switch( p_filter->fmt_out.video.i_chroma )
     {
-        case VLC_CODEC_GREY:
-            switch( p_filter->fmt_out.video.i_chroma )
-            {
-                case VLC_CODEC_I420:
-                    p_filter->pf_video_filter = GREY_I420_Filter;
-                    break;
-                case VLC_CODEC_YUYV:
-                    p_filter->pf_video_filter = GREY_YUY2_Filter;
-                    break;
-                default:
-                    return -1;
-            }
+        case VLC_CODEC_I420:
+            p_filter->pf_video_filter = GREY_I420_Filter;
             break;
-
+        case VLC_CODEC_YUYV:
+            p_filter->pf_video_filter = GREY_YUY2_Filter;
+            break;
         default:
-            return -1;
+            return VLC_EGENERIC;
     }
 
-    return 0;
+    return VLC_SUCCESS;
 }
 
 /* Following functions are local */
