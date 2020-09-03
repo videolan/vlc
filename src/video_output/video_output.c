videolan/vlc

commit 4563df98a188f4276d2529484d1b3f3dafb9182b

vout: gather window locking/unlocking

src/video_output/video_output.c
@@ -1448,26 +1448,12 @@ static int vout_Start(vout_thread_t *vout, const vout_configuration_t *cfg)
     sys->filter.chain_interactive = filter_chain_NewVideo(vout, true, &owner);
 
     vout_display_cfg_t dcfg;
+    int x = 0, y = 0, w = 0, h = 0;
+    unsigned num, den;
 
     vlc_mutex_lock(&sys->window_lock);
     dcfg = sys->display_cfg;
-    /* Any configuration change after unlocking will involve a control request
-     * that will be processed in the new thread. There may also be some pending
-     * control requests for configuration change already visible in
-     * sys->display_cfg, leading to processing of extra harmless and useless
-     * control request processing.
-     *
-     * TODO: display lock separate from window lock.
-     */
-    vlc_mutex_unlock(&sys->window_lock);
 
-    if (vout_OpenWrapper(vout, sys->splitter_name, &dcfg))
-        goto error;
-
-    unsigned num = 0, den = 0;
-    int x = 0, y = 0, w = 0, h = 0;
-
-    vlc_mutex_lock(&sys->window_lock); /* see above */
     switch (sys->source.crop.mode) {
         case VOUT_CROP_NONE:
             break;
@@ -1488,13 +1474,25 @@ static int vout_Start(vout_thread_t *vout, const vout_configuration_t *cfg)
             h = -(int)sys->source.crop.border.bottom;
             break;
     }
-    vlc_mutex_unlock(&sys->window_lock);
-    vout_SetDisplayCrop(sys->display, num, den, x, y, w, h);
 
-    vlc_mutex_lock(&sys->window_lock); /* see above */
     num = sys->source.dar.num;
     den = sys->source.dar.den;
+    /*
+     * Any configuration change after unlocking will involve a control request
+     * that will be processed in the new thread. There may also be some pending
+     * control requests for configuration change already visible in
+     * sys->display_cfg, leading to processing of extra harmless and useless
+     * control request processing.
+     *
+     * TODO: display lock separate from window lock.
+     */
     vlc_mutex_unlock(&sys->window_lock);
+
+    if (vout_OpenWrapper(vout, sys->splitter_name, &dcfg))
+        goto error;
+
+    vout_SetDisplayCrop(sys->display, num, den, x, y, w, h);
+
     if (num != 0 && den != 0)
         vout_SetDisplayAspect(sys->display, num, den);
 

commit 583ad63665efbf3e91b858797c9e1c7fe35b7b12

video_output: don't apply the source aspect ratio as a cropping ratio
The value read for VOUT_CROP_RATIO should be applied to vout_SetDisplayCrop()
not the source aspect ratio.

vout_SetDisplayAspect() always used "source.dar".

Regression introduced in 4563df98a188f4276d2529484d1b3f3dafb9182b.

src/video_output/video_output.c
@@ -1725,6 +1725,7 @@ static int vout_Start(vout_thread_sys_t *vout, vlc_video_context *vctx, const vo
 
     vout_display_cfg_t dcfg;
     int x = 0, y = 0, w = 0, h = 0;
+    unsigned crop_num = 0, crop_den = 0;
     unsigned num, den;
 
     vlc_mutex_lock(&sys->window_lock);
@@ -1744,8 +1745,8 @@ static int vout_Start(vout_thread_sys_t *vout, vlc_video_context *vctx, const vo
         case VOUT_CROP_NONE:
             break;
         case VOUT_CROP_RATIO:
-            num = sys->source.crop.ratio.num;
-            den = sys->source.crop.ratio.den;
+            crop_num = sys->source.crop.ratio.num;
+            crop_den = sys->source.crop.ratio.den;
             break;
         case VOUT_CROP_WINDOW:
             x = sys->source.crop.window.x;
@@ -1777,7 +1778,7 @@ static int vout_Start(vout_thread_sys_t *vout, vlc_video_context *vctx, const vo
         goto error;
     }
 
-    vout_SetDisplayCrop(sys->display, num, den, x, y, w, h);
+    vout_SetDisplayCrop(sys->display, crop_num, crop_den, x, y, w, h);
 
     if (num != 0 && den != 0)
         vout_SetDisplayAspect(sys->display, num, den);