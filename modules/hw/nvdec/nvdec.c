videolan/vlc

commit 855dbc02b1826e0cf67faeda462aba74f6824139

nvdec: use picture_NewFromFormat for opaque pictures
No need to go through fake resources. No planes will be allocated. We just set
the p_sys manually.

modules/hw/nvdec/nvdec.c
@@ -178,12 +178,10 @@ static nvdec_pool_t* nvdec_pool_Create(vlc_video_context *vctx,
         if (ret != CUDA_SUCCESS || pool->outputDevicePtr[i] == 0)
             goto free_pool;
 
-        picture_resource_t res = {
-            .p_sys = pool->outputDevicePtr[i],
-        };
-        pics[i] = picture_NewFromResource(fmt, &res);
+        pics[i] = picture_NewFromFormat(fmt);
         if (!pics[i])
             goto free_pool;
+        pics[i]->p_sys = (void*)(uintptr_t)pool->outputDevicePtr[i];
     }
 
     pool->picture_pool = picture_pool_New(ARRAY_SIZE(pool->outputDevicePtr), pics);

commit 3a51a7aa0371998cafbf84b5e4fc997f6f3a54f1

nvdec: fix release of pictures in the pool
Since we set the picture p_sys it will try to deallocate the value when the
picture is destroyed.

Regression from 855dbc02b1826e0cf67faeda462aba74f6824139.

modules/hw/nvdec/nvdec.c
@@ -178,7 +178,7 @@ static nvdec_pool_t* nvdec_pool_Create(vlc_video_context *vctx,
         if (ret != CUDA_SUCCESS || pool->outputDevicePtr[i] == 0)
             goto free_pool;
 
-        pics[i] = picture_NewFromFormat(fmt);
+        pics[i] = picture_NewFromResource(fmt, &(picture_resource_t) { 0 });
         if (!pics[i])
             goto free_pool;
         pics[i]->p_sys = (void*)(uintptr_t)pool->outputDevicePtr[i];