05756ed231b43e192751eeb3010e96ef346e8bbf
Alexandre Janniaux <ajanni@videolabs.io>
2020-10-07 11:31:00 +0200

    opengl: gl_common: remove explicit defines
    They are defined from the buildsystem already, leading to redefinition
    warnings, and they don't make any sense if not compiling an iOS module
    that need to be linked with the platform, for example when compiling the
    vgl module.

--- modules/video_output/opengl/gl_common.h
+++ modules/video_output/opengl/gl_common.h
@@ -28,11 +28,9 @@
 #ifdef __APPLE__
 # include <TargetConditionals.h>
 # if !TARGET_OS_IPHONE
-#  undef USE_OPENGL_ES2
 #  define MACOS_OPENGL
 #  include <OpenGL/gl.h>
 # else /* Force ESv2 on iOS */
-#  define USE_OPENGL_ES2
 #  include <OpenGLES/ES1/gl.h>
 #  include <OpenGLES/ES2/gl.h>
 #  include <OpenGLES/ES2/glext.h>
