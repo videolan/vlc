a05988506bdea9422ddcb36becd7f5209e0180b5
Alexandre Janniaux <ajanni@videolabs.io>
2020-10-07 16:23:03 +0200

    videotoolbox: add error counter for VT errors

--- modules/codec/videotoolbox.c
+++ modules/codec/videotoolbox.c
@@ -145,6 +145,8 @@ typedef struct decoder_sys_t
     bool                        b_drop_blocks;
     date_t                      pts;
 
+    int error_counter;
+
     vlc_video_context          *vctx;
     struct pic_pacer           *pic_pacer;
 } decoder_sys_t;
@@ -1363,6 +1365,7 @@ static int OpenDecoder(vlc_object_t *p_this)
     if (!p_sys)
         return VLC_ENOMEM;
     p_dec->p_sys = p_sys;
+    p_sys->error_counter = 0;
     p_sys->session = NULL;
     p_sys->codec = codec;
     p_sys->videoFormatDescription = NULL;
@@ -1685,6 +1688,7 @@ enum {
 static int HandleVTStatus(decoder_t *p_dec, OSStatus status,
                           enum vtsession_status * p_vtsession_status)
 {
+    decoder_sys_t *p_sys = p_dec->p_sys;
 #define VTERRCASE(x) \
     case x: msg_Warn(p_dec, "vt session error: '" #x "'"); break;
 
@@ -1694,6 +1698,7 @@ static int HandleVTStatus(decoder_t *p_dec, OSStatus status,
     switch (status)
     {
         case noErr:
+            p_sys->error_counter = 0;
             return VLC_SUCCESS;
 
         VTERRCASE(kVTPropertyNotSupportedErr)
@@ -1754,8 +1759,14 @@ static int HandleVTStatus(decoder_t *p_dec, OSStatus status,
             case kVTVideoDecoderReferenceMissingErr:
                 *p_vtsession_status = VTSESSION_STATUS_RESTART;
                 break;
-            case -8969 /* codecBadDataErr */:
             case kVTVideoDecoderBadDataErr:
+                p_sys->error_counter++;
+                if (p_sys->error_counter == 3)
+                    *p_vtsession_status = VTSESSION_STATUS_ABORT;
+                else
+                    *p_vtsession_status = VTSESSION_STATUS_RESTART;
+                break;
+            case -8969 /* codecBadDataErr */:
             default:
                 *p_vtsession_status = VTSESSION_STATUS_ABORT;
                 break;
