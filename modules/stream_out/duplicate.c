videolan/vlc

commit 7fc6e51819e060255990b2f5307f5d46efcd6e4c

sout: distinguish filter and output capabilities
This removes the "sout stream" capability, and adds two new ones instead:
- "sout output" for final nodes in the sout chain, and
- "sout filter" for non-final nodes in the sout chain.

This is mainly to prevent impossible / non-working chain constructs.

Note: In practice, only "cycle" and "stats" plugins supported both modes
in any meaningful mannter. They are split into two submodules each.

modules/stream_out/duplicate.c
@@ -41,7 +41,7 @@ static void     Close   ( vlc_object_t * );
 
 vlc_module_begin ()
     set_description( N_("Duplicate stream output") )
-    set_capability( "sout stream", 50 )
+    set_capability( "sout output", 50 )
     add_shortcut( "duplicate", "dup" )
     set_category( CAT_SOUT )
     set_subcategory( SUBCAT_SOUT_STREAM )

modules/services_discovery/upnp.cpp
@@ -167,7 +167,7 @@ vlc_module_begin()
     add_submodule()
         set_shortname("dlna")
         set_description(N_("UPnP/DLNA stream output"))
-        set_capability("sout stream", 0)
+        set_capability("sout output", 0)
         add_shortcut("dlna")
         set_category(CAT_SOUT)
         set_subcategory(SUBCAT_SOUT_STREAM)

modules/stream_out/autodel.c
@@ -44,7 +44,7 @@ static void Close   ( vlc_object_t * );
 vlc_module_begin ()
     set_shortname( N_("Autodel"))
     set_description( N_("Automatically add/delete input streams"))
-    set_capability( "sout stream", 50 )
+    set_capability( "sout filter", 50 )
     add_shortcut( "autodel" )
     set_callbacks( Open, Close )
 vlc_module_end ()

modules/stream_out/bridge.c
@@ -92,7 +92,7 @@ vlc_module_begin ()
     set_description( N_("Bridge stream output"))
     add_submodule ()
     set_section( N_("Bridge out"), NULL )
-    set_capability( "sout stream", 50 )
+    set_capability( "sout output", 50 )
     add_shortcut( "bridge-out" )
     /* Only usable with VLM. No category so not in gui preferences
     set_category( CAT_SOUT )
@@ -105,7 +105,7 @@ vlc_module_begin ()
 
     add_submodule ()
     set_section( N_("Bridge in"), NULL )
-    set_capability( "sout stream", 50 )
+    set_capability( "sout filter", 50 )
     add_shortcut( "bridge-in" )
     /*set_category( CAT_SOUT )
     set_subcategory( SUBCAT_SOUT_STREAM )*/

modules/stream_out/chromaprint.c
@@ -58,7 +58,7 @@ static int   Send( sout_stream_t *, void *, block_t * );
 
 vlc_module_begin ()
     set_description( N_("Chromaprint stream output") )
-    set_capability( "sout stream", 0 )
+    set_capability( "sout output", 0 )
     add_shortcut( "chromaprint" )
     add_integer( "duration", 90, DURATION_TEXT, DURATION_LONGTEXT, true )
     set_callbacks( Open, Close )

modules/stream_out/chromecast/cast.cpp
@@ -212,7 +212,7 @@ vlc_module_begin ()
 
     set_shortname(N_("Chromecast"))
     set_description(N_("Chromecast stream output"))
-    set_capability("sout stream", 0)
+    set_capability("sout output", 0)
     add_shortcut("chromecast")
     set_category(CAT_SOUT)
     set_subcategory(SUBCAT_SOUT_STREAM)
@@ -232,7 +232,7 @@ vlc_module_begin ()
     add_submodule()
         /* sout proxy that start the cc input when all streams are loaded */
         add_shortcut("chromecast-proxy")
-        set_capability("sout stream", 0)
+        set_capability("sout filter", 0)
         set_callback(ProxyOpen)
     add_submodule()
         set_subcategory(SUBCAT_SOUT_ACO)

modules/stream_out/cycle.c
@@ -326,9 +326,13 @@ static void Close(vlc_object_t *obj)
 vlc_module_begin()
     set_shortname(N_("cycle"))
     set_description(N_("Cyclic stream output"))
-    set_capability("sout stream", 0)
+    set_capability("sout output", 0)
     set_category(CAT_SOUT)
     set_subcategory(SUBCAT_SOUT_STREAM)
     set_callbacks(Open, Close)
     add_shortcut("cycle")
+    add_submodule()
+    add_shortcut("cycle")
+    set_capability("sout filter", 0)
+    set_callbacks(Open, Close)
 vlc_module_end()

modules/stream_out/delay.c
@@ -52,7 +52,7 @@ static void Close   ( vlc_object_t * );
 vlc_module_begin()
     set_shortname( N_("Delay"))
     set_description( N_("Delay a stream"))
-    set_capability( "sout stream", 50 )
+    set_capability( "sout filter", 50 )
     add_shortcut( "delay" )
     set_category( CAT_SOUT )
     set_subcategory( SUBCAT_SOUT_STREAM )

commit 3d3895ccded57c0eee3fe90672ba0fed8d9412fe

duplicate: restore filter mode
Regression from 7fc6e51819e060255990b2f5307f5d46efcd6e4c.

modules/stream_out/duplicate.c
@@ -46,6 +46,10 @@ vlc_module_begin ()
     set_category( CAT_SOUT )
     set_subcategory( SUBCAT_SOUT_STREAM )
     set_callbacks( Open, Close )
+    add_submodule()
+    set_capability("sout filter", 0)
+    add_shortcut("duplicate", "dup")
+    set_callbacks(Open, Close)
 vlc_module_end ()
 
 