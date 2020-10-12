86b3c671aead2578d44bc03667f0af961623b121
Rémi Denis-Courmont <remi@remlab.net>
2020-10-12 21:22:01 +0300

    shine: fix close ordering

--- modules/codec/shine.c
+++ modules/codec/shine.c
@@ -271,14 +271,13 @@ static void CloseEncoder( vlc_object_t *p_this )
 {
     encoder_sys_t *p_sys = ((encoder_t*)p_this)->p_sys;
 
-    atomic_store(&busy, false);
-
     /* TODO: we should send the last PCM block padded with 0
      * But we don't know if other blocks will come before it's too late */
     if( p_sys->i_buffer )
         free( p_sys->p_buffer );
 
     shine_close(p_sys->s);
+    atomic_store(&busy, false);
 
     block_FifoRelease( p_sys->p_fifo );
     free( p_sys );
