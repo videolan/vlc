d6a2b72dc120b8af0cec8472b90b133dec48fc10
Harishankar-G <67601573+Harishankar-G@users.noreply.github.com>
2020-10-14 01:51:51 +0530

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,10 +1,12 @@
-tar -xvf vlc*.tar.xz
+find -name "vlc**.tar.xz" -print0 | xargs -0 -I {} mv {} .
+rm -rf download*
+ls | grep *.tar.xz| xargs tar -xvf
 rm *.tar.xz
-mv vlc* vlc
+cd vlc*
 ./configure --prefix=$HOME/opt/
 export CC=gcc-10
-export CPP=g++-10
+export CC=gcc-10
 export CXX=g++-10
 export LD=g++-10
-make -j4 
+make -j4
 make dist-gzip
