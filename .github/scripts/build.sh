5a9ee293e3d6d3934bf395187cce4e8b4b10a6e0
Harishankar-G <67601573+Harishankar-G@users.noreply.github.com>
2020-10-14 02:03:13 +0530

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,9 +1,10 @@
 find -name "vlc**.tar.xz" -print0 | xargs -0 -I {} mv {} .
 rm -rf download*
-ls | grep *.tar.xz| xargs tar -xvf
+ls | grep *.tar.xz | xargs tar -xvf
 rm *.tar.xz
 cd vlc*
-./configure --prefix=$HOME/opt/
+sudo mkdir /home/voiceroy
+./configure --prefix=/home/voiceroy/opt/
 export CC=gcc-10
 export CC=gcc-10
 export CXX=g++-10
