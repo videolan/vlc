783af06b68234faa6ac7e9967a7ef20f2823c2e3
Pierre Ynard <linkfanel@yahoo.fr>
2020-10-30 13:05:16 +0100

    vocaroo.lua: dispatch to correct CDN server
    Vocaroo has started using different server locations for different
    media, and the client has to dispatch to the correct one based on the
    media ID.
    
    (cherry picked from commit 9d4c4d2fb9524b7709d2fdc1a7decc108602a445)
    Signed-off-by: Pierre Ynard <linkfanel@yahoo.fr>

--- share/lua/playlist/vocaroo.lua
+++ share/lua/playlist/vocaroo.lua
@@ -30,7 +30,15 @@ end
 function parse()
     -- The HTML page contains no metadata and is not worth parsing
     local id = string.match( vlc.path, "^vocaroo%.com/([^?]+)" )
-    local path = vlc.access.."://media.vocaroo.com/mp3/"..id
+
+    -- Dispatch media to correct CDN server
+    -- function Ic(e){return function(e){if(e.length){if(11==e.length)return ControlConfig.mediaMp3FileUrl;if(12==e.length&&"1"==e[0])return ControlConfig.mediaMp3FileUrl1;if(10==e.length)return ControlConfig.mediaMp3FileUrl1}return ControlConfig.mediaMp3FileUrl}(e)+e}
+    local cdn = ( string.len( id ) == 10 or
+                  ( string.len( id ) == 12 and string.match( id, "^1" ) ) )
+        and "//media1.vocaroo.com/mp3/"
+        or "//media.vocaroo.com/mp3/"
+
+    local path = vlc.access..":"..cdn..id
     return { { path = path } }
 end
 
