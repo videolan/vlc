a685b7ea261293047234dc4b55e776482861e749
Alexandre Janniaux <ajanni@videolabs.io>
2020-11-01 12:40:55 +0100

    test: modules: add transcoder test
    A unit-test for the transcoder code currently is hard to achieve, as it
    involves decoders, encoders, filters, sout_stream_t object, and general
    transcode code.
    
    This commit introduces an integration test which mocks the extern
    components (filters, decoders, encoders) used by the transcode pipeline
    and starts this pipeline using the usual input item properties.
    
    The main end goals are to:
     - check whether no part of the stream is dropped.
     - check whether push model with video context is supported everywhere.
     - check the different kind of decoder behaviour, in particular those
       for which decode is asynchronous with the decoder_QueueVideo call,
       those for which Open() is asynchronous with the decoder_UpdateVideo*
       call and their synchronous variants.
     - check memory leaks and use-after-free in the pipeline (through asan).
     - check threading within the pipeline (through tsan).
     - check decoder, encoder and filter loading failure.
     - check format adaptation through filters before the encoder.
     - check format conversion (called final_conv_static) before the
       encoder, needed for encoders requesting a conversion.
    
    It's currently written in an asynchronous state way spread across
    multiple feature functions to handle the different stage of the pipeline
    without interaction, like it was done for the video output test, but we
    might want to find a way to rewrite those tests in a more sequential way
    if it were to get more complex even.

--- test/modules/stream_out/transcode_scenarios.c
+++ test/modules/stream_out/transcode_scenarios.c
@@ -60,6 +60,83 @@ static void decoder_fixed_size(decoder_t *dec, vlc_fourcc_t chroma,
 static void decoder_i420_800_600(decoder_t *dec)
     { decoder_fixed_size(dec, VLC_CODEC_I420, 800, 600); }
 
+static void decoder_nv12_800_600(decoder_t *dec)
+    { decoder_fixed_size(dec, VLC_CODEC_NV12, 800, 600); }
+
+static void decoder_i420_800_600_vctx(decoder_t *dec)
+{
+    /* We use VLC_VIDEO_CONTEXT_VAAPI here but it could be any other kind of
+     * video context type since we prevent the usual plugins from loading. */
+    struct vlc_video_context *vctx = vlc_video_context_Create(
+            NULL, VLC_VIDEO_CONTEXT_VAAPI, 0, NULL);
+    assert(vctx);
+    dec->p_sys = vctx;
+    decoder_i420_800_600(dec);
+}
+
+static int decoder_decode_dummy(decoder_t *dec, picture_t *pic)
+{
+    int ret = decoder_UpdateVideoOutput(dec, NULL);
+    assert(ret == VLC_SUCCESS);
+    decoder_QueueVideo(dec, pic);
+    return VLC_SUCCESS;
+}
+
+/* Picture context implementation */
+static void picture_context_destroy(struct picture_context_t *ctx)
+    { free(ctx); }
+
+static struct picture_context_t *
+picture_context_copy(struct picture_context_t *ctx)
+{
+    struct picture_context_t *copy = malloc(sizeof *copy);
+    copy = ctx;
+    copy->vctx = vlc_video_context_Hold(ctx->vctx);
+    return copy;
+}
+
+static int decoder_decode_vctx(decoder_t *dec, picture_t *pic)
+{
+    struct vlc_video_context *vctx = dec->p_sys;
+    assert(vctx);
+    scenario_data.decoder_vctx = vctx;
+
+    int ret = decoder_UpdateVideoOutput(dec, vctx);
+    assert(ret == VLC_SUCCESS);
+
+    picture_context_t *context = malloc(sizeof *context);
+    assert(context);
+    context->destroy = picture_context_destroy;
+    context->copy = picture_context_copy;
+    context->vctx = vlc_video_context_Hold(vctx);
+    pic->context = context;
+    pic->format.i_chroma = dec->fmt_out.video.i_chroma;
+    decoder_QueueVideo(dec, pic);
+    return VLC_SUCCESS;
+}
+
+static int decoder_decode_vctx_update(decoder_t *dec, picture_t *pic)
+{
+    bool should_switch = scenario_data.decoder_vctx != NULL;
+
+    if (should_switch)
+    {
+        switch (dec->fmt_out.i_codec)
+        {
+            case VLC_CODEC_I420:
+                msg_Dbg(dec, "Switching from I420 to NV12");
+                dec->fmt_out.video.i_chroma
+                    = dec->fmt_out.i_codec
+                    = VLC_CODEC_NV12;
+                break;
+            default:
+                break;
+        }
+    }
+    decoder_decode_vctx(dec, pic);
+    return VLC_SUCCESS;
+}
+
 static int decoder_decode_error(decoder_t *dec, picture_t *pic)
 {
     (void)dec;
@@ -73,9 +150,182 @@ static void wait_error_reported(sout_stream_t *stream)
     vlc_sem_post(&scenario_data.wait_stop);
 }
 
+static void encoder_fixed_size(encoder_t *enc, vlc_fourcc_t chroma,
+        unsigned width, unsigned height)
+{
+    assert(!scenario_data.encoder_opened);
+    msg_Info(enc, "Setting up the encoder %4.4s: %ux%u",
+             (const char *)&chroma, width, height);
+    enc->fmt_in.video.i_chroma
+        = enc->fmt_in.i_codec
+        = chroma;
+    enc->fmt_in.video.i_visible_width
+        = enc->fmt_in.video.i_width
+        = width;
+    enc->fmt_in.video.i_visible_height
+        = enc->fmt_in.video.i_height
+        = height;
+    scenario_data.encoder_opened = true;
+}
+
+static void encoder_i420_800_600(encoder_t *enc)
+    { encoder_fixed_size(enc, VLC_CODEC_I420, 800, 600); }
+
+static void encoder_nv12_800_600(encoder_t *enc)
+    { encoder_fixed_size(enc, VLC_CODEC_NV12, 800, 600); }
+
+static void encoder_i420_800_600_vctx(encoder_t *enc)
+{
+    encoder_fixed_size(enc, VLC_CODEC_I420, 800, 600);
+    assert(scenario_data.decoder_vctx != NULL);
+    assert(enc->vctx_in == scenario_data.decoder_vctx);
+}
+
+#if 0
+static void encoder_nv12_800_600_no_vctx(encoder_t *enc)
+{
+    encoder_fixed_size(enc, VLC_CODEC_NV12, 800, 600);
+    assert(enc->vctx_in == NULL);
+}
+
+static void encoder_i420_800_600_no_vctx(encoder_t *enc)
+{
+    encoder_fixed_size(enc, VLC_CODEC_I420, 800, 600);
+    assert(enc->vctx_in == NULL);
+}
+#endif
+
+static void encoder_encode_dummy(encoder_t *enc, picture_t *pic)
+{
+    (void)enc; (void)pic;
+    msg_Info(enc, "Encode");
+    vlc_sem_post(&scenario_data.wait_stop);
+}
+
+static void encoder_encode_wait_10_images(encoder_t *enc, picture_t *pic)
+{
+    (void)enc; (void)pic;
+    if (scenario_data.encoder_picture_count++ == 10)
+        vlc_sem_post(&scenario_data.wait_stop);
+}
+
+static void converter_fixed_size(filter_t *filter, vlc_fourcc_t chroma_in,
+        vlc_fourcc_t chroma_out, unsigned width, unsigned height)
+{
+    assert(filter->fmt_in.video.i_width == width);
+    assert(filter->fmt_in.video.i_visible_width == width);
+    assert(filter->fmt_in.video.i_height == height);
+    assert(filter->fmt_in.video.i_visible_height == height);
+
+    assert(filter->fmt_out.video.i_width == width);
+    assert(filter->fmt_out.video.i_visible_width == width);
+    assert(filter->fmt_out.video.i_height == height);
+    assert(filter->fmt_out.video.i_visible_height == height);
+
+    assert(filter->fmt_in.video.i_chroma == chroma_in);
+    assert(filter->fmt_out.video.i_chroma == chroma_out);
+
+    scenario_data.converter_opened = true;
+}
+
+static void converter_i420_to_nv12_800_600(filter_t *filter)
+    { converter_fixed_size(filter, VLC_CODEC_I420, VLC_CODEC_NV12, 800, 600); }
+
+static void converter_nv12_to_i420_800_600(filter_t *filter)
+    { converter_fixed_size(filter, VLC_CODEC_NV12, VLC_CODEC_I420, 800, 600); }
+
+static void converter_nv12_to_i420_800_600_vctx(filter_t *filter)
+{
+    converter_fixed_size(filter, VLC_CODEC_NV12, VLC_CODEC_I420, 800, 600);
+    assert(filter->vctx_in == scenario_data.decoder_vctx);
+}
+
+static void converter_i420_to_nv12_800_600_vctx(filter_t *filter)
+{
+    converter_fixed_size(filter, VLC_CODEC_I420, VLC_CODEC_NV12, 800, 600);
+    assert(filter->vctx_in == scenario_data.decoder_vctx);
+}
+
 const char source_800_600[] = "mock://video_track_count=1;length=100000000000;video_width=800;video_height=600";
 struct transcode_scenario transcode_scenarios[] =
 {{
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_i420_800_600,
+    .decoder_decode = decoder_decode_dummy,
+    .encoder_setup = encoder_i420_800_600,
+    .encoder_encode = encoder_encode_dummy,
+},{
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_nv12_800_600,
+    .decoder_decode = decoder_decode_dummy,
+    .encoder_setup = encoder_nv12_800_600,
+    .encoder_encode = encoder_encode_dummy,
+},{
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_i420_800_600,
+    .decoder_decode = decoder_decode_dummy,
+    .encoder_setup = encoder_nv12_800_600,
+    .encoder_encode = encoder_encode_dummy,
+    .converter_setup = converter_i420_to_nv12_800_600,
+},{
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_nv12_800_600,
+    .decoder_decode = decoder_decode_dummy,
+    .encoder_setup = encoder_i420_800_600,
+    .encoder_encode = encoder_encode_dummy,
+    .converter_setup = converter_nv12_to_i420_800_600,
+},{
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_i420_800_600_vctx,
+    .decoder_decode = decoder_decode_vctx,
+    .encoder_setup = encoder_i420_800_600_vctx,
+    .encoder_encode = encoder_encode_dummy,
+},{
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_i420_800_600_vctx,
+    .decoder_decode = decoder_decode_vctx,
+    .encoder_setup = encoder_nv12_800_600,
+    .encoder_encode = encoder_encode_dummy,
+    .converter_setup = converter_i420_to_nv12_800_600,
+},{
+    /* Make sure fps filter in transcode will forward the video context */
+    .source = source_800_600,
+    .sout = "sout=#transcode{fps=1}:dummy",
+    .decoder_setup = decoder_i420_800_600_vctx,
+    .decoder_decode = decoder_decode_vctx,
+    .encoder_setup = encoder_i420_800_600_vctx,
+    .encoder_encode = encoder_encode_dummy,
+},{
+    // - Decoder format with video context
+    // - Encoder format request a different chroma
+    // - Converter must convert from one to the other
+    //   but it doesn't forward any video context
+    /* Make sure converter will receive the video context */
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_i420_800_600_vctx,
+    .decoder_decode = decoder_decode_vctx,
+    .encoder_setup = encoder_nv12_800_600,
+    .encoder_encode = encoder_encode_dummy,
+    .converter_setup = converter_i420_to_nv12_800_600_vctx,
+},{
+    /* Make sure a change in format will lead to the addition of a converter.
+     * Here, decoder_decode_vctx_update will change format after the first
+     * frame. */
+    .source = source_800_600,
+    .sout = "sout=#transcode:dummy",
+    .decoder_setup = decoder_i420_800_600_vctx,
+    .decoder_decode = decoder_decode_vctx_update,
+    .encoder_setup = encoder_i420_800_600,
+    .encoder_encode = encoder_encode_wait_10_images,
+    .converter_setup = converter_nv12_to_i420_800_600_vctx,
+},{
     /* Ensure that error are correctly forwarded back to the stream output
      * pipeline. */
     .source = source_800_600,
@@ -84,7 +334,6 @@ struct transcode_scenario transcode_scenarios[] =
     .decoder_decode = decoder_decode_error,
     .report_error = wait_error_reported,
 }};
-
 size_t transcode_scenarios_count = ARRAY_SIZE(transcode_scenarios);
 
 void transcode_scenario_init(void)
