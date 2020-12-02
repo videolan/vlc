bd1ded483a180ca093a003c080b75d2fa72131aa
Ilya Yanok <ilya.yanok@gmail.com>
2020-12-02 22:42:27 +0100

    ftp: fix handling of NLST response
    Commit e1508444 broke the NLST response handling: getting
    1xx response is what we actually expect, not a reason to fail.
    
    This was reported as Android-specific bug:
    https://code.videolan.org/videolan/vlc-android/-/issues/644
    two years ago and is still not fixed.
    
    This is invisible if FTP server supports MSLT feature.
    
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- modules/access/ftp.c
+++ modules/access/ftp.c
@@ -1146,7 +1146,7 @@ static int ftp_StartStream( vlc_object_t *p_access, access_sys_t *p_sys,
         }
         else
         if( ftp_SendCommand( p_access, p_sys, "NLST" ) < 0 ||
-            ftp_RecvCommandInit( p_access, p_sys ) == 1 )
+            ftp_RecvCommandInit( p_access, p_sys ) != 1 )
         {
             msg_Err( p_access, "cannot list directory contents" );
             return VLC_EGENERIC;
