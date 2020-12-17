3103c63b8b434c0f47d88ba5102d526832fff22a
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2020-12-17 16:52:46 +0100

    preparser: art: Remove useless copy
    The content is always owned by the called, and since the
    ArtCacheCreateDir doesn't change the string content, we can simply use
    the string passed in parameter without any need for a copy

--- src/preparser/art.c
+++ src/preparser/art.c
@@ -37,20 +37,17 @@
 
 #include "art.h"
 
-static void ArtCacheCreateDir( const char *psz_dir )
+static void ArtCacheCreateDir( char *psz_dir )
 {
-    char newdir[strlen( psz_dir ) + 1];
-    strcpy( newdir, psz_dir );
-    char * psz_newdir = newdir;
-    char * psz = psz_newdir;
+    char * psz = psz_dir;
 
     while( *psz )
     {
         while( *psz && *psz != DIR_SEP_CHAR) psz++;
         if( !*psz ) break;
         *psz = 0;
-        if( !EMPTY_STR( psz_newdir ) )
-            vlc_mkdir( psz_newdir, 0700 );
+        if( !EMPTY_STR( psz_dir ) )
+            vlc_mkdir( psz_dir, 0700 );
         *psz = DIR_SEP_CHAR;
         psz++;
     }
