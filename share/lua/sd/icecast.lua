c8c78f75e7117498f1552f22c4ba53a229f50997
Hannes Domani <lamisedaxeh@yahoo.com>
2020-12-22 02:00:18 +0100

    lua: icecast: fix nil value error for some streams
    Due to a bug in the directory, some streams lack a listen URL.
    
    Fix #25309

--- share/lua/sd/icecast.lua
+++ share/lua/sd/icecast.lua
@@ -41,27 +41,30 @@ function main()
     local tree = simplexml.parse_url("http://dir.xiph.org/yp.xml")
     for _, station in ipairs( tree.children ) do
         simplexml.add_name_maps( station )
-        local station_name = station.children_map["server_name"][1].children[1]
-        if station_name == "Unspecified name" or station_name == "" or station_name == nil
+        if not( station.children_map["listen_url"][1].children[1] == nil )
         then
-                station_name = station.children_map["listen_url"][1].children[1]
-                if string.find( station_name, "radionomy.com" )
-                then
-                        station_name = string.match( station_name, "([^/]+)$")
-                        station_name = string.gsub( station_name, "-", " " )
-                end
+            local station_name = station.children_map["server_name"][1].children[1]
+            if station_name == "Unspecified name" or station_name == "" or station_name == nil
+            then
+                    station_name = station.children_map["listen_url"][1].children[1]
+                    if string.find( station_name, "radionomy.com" )
+                    then
+                            station_name = string.match( station_name, "([^/]+)$")
+                            station_name = string.gsub( station_name, "-", " " )
+                    end
+            end
+            vlc.sd.add_item( {path=station.children_map["listen_url"][1].children[1],
+                              title=station_name,
+                              genre=dropnil(station.children_map["genre"][1].children[1]),
+                              nowplaying=dropnil(station.children_map["current_song"][1].children[1]),
+                              uiddata=station.children_map["listen_url"][1].children[1]
+                                      .. dropnil(station.children_map["server_name"][1].children[1]),
+                              meta={
+                                      ["Listing Source"]="dir.xiph.org",
+                                      ["Listing Type"]="radio",
+                                      ["Icecast Bitrate"]=dropnil(station.children_map["bitrate"][1].children[1]),
+                                      ["Icecast Server Type"]=dropnil(station.children_map["server_type"][1].children[1])
+                              }} )
         end
-        vlc.sd.add_item( {path=station.children_map["listen_url"][1].children[1],
-                          title=station_name,
-                          genre=dropnil(station.children_map["genre"][1].children[1]),
-                          nowplaying=dropnil(station.children_map["current_song"][1].children[1]),
-                          uiddata=station.children_map["listen_url"][1].children[1]
-                                  .. dropnil(station.children_map["server_name"][1].children[1]),
-                          meta={
-                                  ["Listing Source"]="dir.xiph.org",
-                                  ["Listing Type"]="radio",
-                                  ["Icecast Bitrate"]=dropnil(station.children_map["bitrate"][1].children[1]),
-                                  ["Icecast Server Type"]=dropnil(station.children_map["server_type"][1].children[1])
-                          }} )
     end
 end
