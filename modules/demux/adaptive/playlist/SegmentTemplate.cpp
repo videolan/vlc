videolan/vlc

commit b15abfbf556c53b23a6bd86d3fd4ac4a4accec7d

demux: adaptive: rework profile specific types for global timeline

modules/demux/adaptive/playlist/SegmentTemplate.cpp
@@ -31,127 +31,58 @@
 
 using namespace adaptive::playlist;
 
-BaseSegmentTemplate::BaseSegmentTemplate( ICanonicalUrl *parent ) :
+SegmentTemplateSegment::SegmentTemplateSegment( SegmentTemplate *templ_,
+                                                ICanonicalUrl *parent ) :
     Segment( parent )
 {
-}
-
-BaseSegmentTemplate::~BaseSegmentTemplate()
-{
-
-}
-
-void BaseSegmentTemplate::setSourceUrl(const std::string &url)
-{
-    sourceUrl = Url(Url::Component(url, this));
-}
-
-MediaSegmentTemplate::MediaSegmentTemplate( SegmentInformation *parent ) :
-    BaseSegmentTemplate( parent ),
-    TimescaleAble( NULL ) /* we don't want auto inherit */
-{
-    debugName = "SegmentTemplate";
+    debugName = "SegmentTemplateSegment";
     classId = Segment::CLASSID_SEGMENT;
-    startNumber = std::numeric_limits<uint64_t>::max();
-    segmentTimeline = NULL;
-    initialisationSegment.Set( NULL );
     templated = true;
+    templ = templ_;
+}
+
+SegmentTemplateSegment::~SegmentTemplateSegment()
+{
+
+}
+
+void SegmentTemplateSegment::setSourceUrl(const std::string &url)
+{
+    sourceUrl = Url(Url::Component(url, templ));
+}
+
+SegmentTemplate::SegmentTemplate( SegmentInformation *parent ) :
+    AbstractMultipleSegmentBaseType( NULL ) /* we don't want auto inherit */
+{
+    initialisationSegment.Set( NULL );
     parentSegmentInformation = parent;
+    segments.push_back( new SegmentTemplateSegment( this, parent ) );
 }
 
-MediaSegmentTemplate::~MediaSegmentTemplate()
+SegmentTemplate::~SegmentTemplate()
 {
-    delete segmentTimeline;
+    delete *segments.begin();
 }
 
-void MediaSegmentTemplate::updateWith(MediaSegmentTemplate *updated)
+void SegmentTemplate::setSourceUrl( const std::string &url )
 {
-    SegmentTimeline *timeline = segmentTimeline;
-    if(timeline && updated->segmentTimeline)
-    {
-        timeline->updateWith(*updated->segmentTimeline);
-        /*if(prunebarrier)
-        {
-            const Timescale timescale = timeline->inheritTimescale();
-            const uint64_t number =
-                    timeline->getElementNumberByScaledPlaybackTime(timescale.ToScaled(prunebarrier));
-            timeline->pruneBySequenceNumber(number);
-        }*/
-    }
+    (*segments.begin())->setSourceUrl(url);
 }
 
-void MediaSegmentTemplate::pruneByPlaybackTime(vlc_tick_t time)
+void SegmentTemplate::pruneByPlaybackTime(vlc_tick_t time)
 {
     if(segmentTimeline)
         return segmentTimeline->pruneByPlaybackTime(time);
 }
 
-size_t MediaSegmentTemplate::pruneBySequenceNumber(uint64_t number)
+size_t SegmentTemplate::pruneBySequenceNumber(uint64_t number)
 {
     if(segmentTimeline)
         return segmentTimeline->pruneBySequenceNumber(number);
     return 0;
 }
 
-uint64_t MediaSegmentTemplate::inheritStartNumber() const
-{
-    if( startNumber != std::numeric_limits<uint64_t>::max() )
-        return startNumber;
-
-    const SegmentInformation *ulevel = parentSegmentInformation ? parentSegmentInformation
-                                                                : NULL;
-    for( ; ulevel ; ulevel = ulevel->parent )
-    {
-        if( ulevel->mediaSegmentTemplate &&
-            ulevel->mediaSegmentTemplate->startNumber !=
-                std::numeric_limits<uint64_t>::max() )
-            return ulevel->mediaSegmentTemplate->startNumber;
-    }
-    return 1;
-}
-
-Timescale MediaSegmentTemplate::inheritTimescale() const
-{
-    const SegmentInformation *ulevel = parentSegmentInformation ? parentSegmentInformation
-                                                                : NULL;
-    for( ; ulevel ; ulevel = ulevel->parent )
-    {
-        if( ulevel->mediaSegmentTemplate &&
-            ulevel->mediaSegmentTemplate->getTimescale().isValid() )
-            return ulevel->mediaSegmentTemplate->getTimescale();
-        if( ulevel->getTimescale().isValid() )
-            return ulevel->getTimescale();
-    }
-    return Timescale(1);
-}
-
-stime_t MediaSegmentTemplate::inheritDuration() const
-{
-    const SegmentInformation *ulevel = parentSegmentInformation ? parentSegmentInformation
-                                                                : NULL;
-    for( ; ulevel ; ulevel = ulevel->parent )
-    {
-        if( ulevel->mediaSegmentTemplate &&
-            ulevel->mediaSegmentTemplate->duration.Get() > 0 )
-            return ulevel->mediaSegmentTemplate->duration.Get();
-    }
-    return 0;
-}
-
-SegmentTimeline * MediaSegmentTemplate::inheritSegmentTimeline() const
-{
-    const SegmentInformation *ulevel = parentSegmentInformation ? parentSegmentInformation
-                                                          : NULL;
-    for( ; ulevel ; ulevel = ulevel->parent )
-    {
-        if( ulevel->mediaSegmentTemplate &&
-            ulevel->mediaSegmentTemplate->segmentTimeline )
-            return ulevel->mediaSegmentTemplate->segmentTimeline;
-    }
-    return NULL;
-}
-
-uint64_t MediaSegmentTemplate::getLiveTemplateNumber(vlc_tick_t playbacktime, bool abs) const
+uint64_t SegmentTemplate::getLiveTemplateNumber(vlc_tick_t playbacktime, bool abs) const
 {
     uint64_t number = inheritStartNumber();
     /* live streams / templated */
@@ -176,53 +107,181 @@ uint64_t MediaSegmentTemplate::getLiveTemplateNumber(vlc_tick_t playbacktime, bo
     return number;
 }
 
-stime_t MediaSegmentTemplate::getMinAheadScaledTime(uint64_t number) const
+void SegmentTemplate::debug(vlc_object_t *obj, int indent) const
 {
-    if( segmentTimeline )
-        return segmentTimeline->getMinAheadScaledTime(number);
-
-    uint64_t current = getLiveTemplateNumber(vlc_tick_from_sec(time(NULL)));
-    return (current - number) * inheritDuration();
-}
-
-uint64_t MediaSegmentTemplate::getSequenceNumber() const
-{
-    return inheritStartNumber();
-}
-
-void MediaSegmentTemplate::setStartNumber( uint64_t v )
-{
-    startNumber = v;
-}
-
-void MediaSegmentTemplate::setSegmentTimeline( SegmentTimeline *v )
-{
-    delete segmentTimeline;
-    segmentTimeline = v;
-}
-
-void MediaSegmentTemplate::debug(vlc_object_t *obj, int indent) const
-{
-    Segment::debug(obj, indent);
+    AbstractSegmentBaseType::debug(obj, indent);
+    (*segments.begin())->debug(obj, indent);
     if(segmentTimeline)
         segmentTimeline->debug(obj, indent + 1);
 }
 
-InitSegmentTemplate::InitSegmentTemplate( ICanonicalUrl *parent,
-                                          MediaSegmentTemplate *templ ) :
+vlc_tick_t SegmentTemplate::getMinAheadTime(uint64_t number) const
+{
+    if( segmentTimeline )
+    {
+        const Timescale timescale = segmentTimeline->inheritTimescale();
+        return timescale.ToTime(segmentTimeline->getMinAheadScaledTime(number));
+    }
+    else
+    {
+        const Timescale timescale = inheritTimescale();
+        uint64_t current = getLiveTemplateNumber(vlc_tick_from_sec(time(NULL)));
+        stime_t i_length = (current - number) * inheritDuration();
+        return timescale.ToTime(i_length);
+    }
+}
+
+Segment * SegmentTemplate::getMediaSegment(uint64_t number) const
+{
+    const SegmentTimeline *tl = inheritSegmentTimeline();
+    if(tl == NULL || tl->maxElementNumber() > number)
+        return *segments.begin();
+    return NULL;
+}
+
+InitSegment * SegmentTemplate::getInitSegment() const
+{
+    return initialisationSegment.Get();
+}
+
+Segment *  SegmentTemplate::getNextMediaSegment(uint64_t i_pos,uint64_t *pi_newpos,
+                                                     bool *pb_gap) const
+{
+    *pb_gap = false;
+    *pi_newpos = i_pos;
+    /* Check if we don't exceed timeline */
+    const SegmentTimeline *timeline = inheritSegmentTimeline();
+    if(timeline)
+    {
+        *pi_newpos = std::max(timeline->minElementNumber(), i_pos);
+        if (timeline->maxElementNumber() < i_pos)
+            return NULL;
+    }
+    else
+    {
+        /* check template upper bound */
+        const AbstractPlaylist *playlist = parentSegmentInformation->getPlaylist();
+        if(!playlist->isLive())
+        {
+            const Timescale timescale = inheritTimescale();
+            const stime_t segmentduration = inheritDuration();
+            vlc_tick_t totalduration = parentSegmentInformation->getPeriodDuration();
+            if(totalduration == 0)
+                totalduration = playlist->duration.Get();
+            if(totalduration && segmentduration)
+            {
+                uint64_t endnum = inheritStartNumber() +
+                        (timescale.ToScaled(totalduration) + segmentduration - 1) / segmentduration;
+                if(i_pos >= endnum)
+                {
+                    *pi_newpos = i_pos;
+                    return NULL;
+                }
+            }
+        }
+        *pi_newpos = i_pos;
+        /* start number */
+        *pi_newpos = std::max(inheritStartNumber(), i_pos);
+    }
+    return *segments.begin();
+}
+
+uint64_t SegmentTemplate::getStartSegmentNumber() const
+{
+    const SegmentTimeline *timeline = inheritSegmentTimeline();
+    return timeline ? timeline->minElementNumber() : inheritStartNumber();
+}
+
+bool SegmentTemplate::getSegmentNumberByTime(vlc_tick_t time, uint64_t *ret) const
+{
+    const SegmentTimeline *timeline = inheritSegmentTimeline();
+    if(timeline)
+    {
+        const Timescale timescale = timeline->getTimescale().isValid()
+                                  ? timeline->getTimescale()
+                                  : inheritTimescale();
+        stime_t st = timescale.ToScaled(time);
+        *ret = timeline->getElementNumberByScaledPlaybackTime(st);
+        return true;
+    }
+
+    const stime_t duration = inheritDuration();
+    if( duration && parent )
+    {
+        AbstractPlaylist *playlist = parent->getPlaylist();
+        if( playlist->isLive() )
+        {
+            vlc_tick_t now = vlc_tick_from_sec(::time(NULL));
+            if(playlist->availabilityStartTime.Get())
+            {
+                if(time >= playlist->availabilityStartTime.Get() && time < now)
+                    *ret = getLiveTemplateNumber(time, true);
+                else if(now - playlist->availabilityStartTime.Get() > time)
+                    *ret = getLiveTemplateNumber(time, false);
+            }
+            else return false;
+        }
+        else
+        {
+            const Timescale timescale = inheritTimescale();
+            *ret = inheritStartNumber();
+            *ret += timescale.ToScaled(time) / duration;
+        }
+        return true;
+    }
+
+    return false;
+}
+
+
+bool SegmentTemplate::getPlaybackTimeDurationBySegmentNumber(uint64_t number,
+                                                             vlc_tick_t *time,
+                                                             vlc_tick_t *duration) const
+{
+    if(number == std::numeric_limits<uint64_t>::max())
+        return false;
+
+    Timescale timescale;
+    stime_t stime, sduration;
+
+    const SegmentTimeline * timeline = inheritSegmentTimeline();
+    if(timeline)
+    {
+        timescale = timeline->inheritTimescale();
+        if(!timeline->getScaledPlaybackTimeDurationBySegmentNumber(number, &stime, &sduration))
+            return false;
+    }
+    else
+    {
+        timescale = inheritTimescale();
+        uint64_t startNumber = inheritStartNumber();
+        if(number < startNumber)
+            return false;
+        sduration = inheritDuration();
+        stime = (number - startNumber) * sduration;
+    }
+
+    *time = timescale.ToTime(stime);
+    *duration = timescale.ToTime(sduration);
+    return true;
+
+}
+
+SegmentTemplateInit::SegmentTemplateInit( SegmentTemplate *templ_,
+                                          ICanonicalUrl *parent ) :
     InitSegment(parent)
 {
     debugName = "InitSegmentTemplate";
     classId = InitSegment::CLASSID_INITSEGMENT;
-    maintempl = templ;
+    templ = templ_;
 }
 
-InitSegmentTemplate::~InitSegmentTemplate()
+SegmentTemplateInit::~SegmentTemplateInit()
 {
 
 }
 
-void InitSegmentTemplate::setSourceUrl(const std::string &url)
+void SegmentTemplateInit::setSourceUrl(const std::string &url)
 {
-    sourceUrl = Url(Url::Component(url, maintempl));
+    sourceUrl = Url(Url::Component(url, templ));
 }

modules/demux/adaptive/playlist/SegmentTemplate.h
@@ -25,61 +25,64 @@
 
 #include "Segment.h"
 #include "../tools/Properties.hpp"
-#include "SegmentInfoCommon.h"
+#include "SegmentBaseType.hpp"
 
 namespace adaptive
 {
     namespace playlist
     {
         class ICanonicalUrl;
-        class InitSegmentTemplate;
+        class SegmentTemplateInit;
         class SegmentInformation;
-        class SegmentTimeline;
+        class SegmentTemplate;
 
-        class BaseSegmentTemplate : public Segment
+        class SegmentTemplateSegment : public Segment
         {
             public:
-                BaseSegmentTemplate( ICanonicalUrl * = NULL );
-                virtual ~BaseSegmentTemplate();
+                SegmentTemplateSegment( SegmentTemplate *, ICanonicalUrl * = NULL );
+                virtual ~SegmentTemplateSegment();
                 virtual void setSourceUrl( const std::string &url ); /* reimpl */
+
+            protected:
+                const SegmentTemplate *templ;
         };
 
-        class MediaSegmentTemplate : public BaseSegmentTemplate,
-                                     public Initializable<InitSegmentTemplate>,
-                                     public TimescaleAble
+        class SegmentTemplate : public AbstractMultipleSegmentBaseType
         {
             public:
-                MediaSegmentTemplate( SegmentInformation * = NULL );
-                virtual ~MediaSegmentTemplate();
-                void setStartNumber( uint64_t );
-                void setSegmentTimeline( SegmentTimeline * );
-                void updateWith( MediaSegmentTemplate * );
-                virtual uint64_t getSequenceNumber() const; /* reimpl */
+                SegmentTemplate( SegmentInformation * = NULL );
+                virtual ~SegmentTemplate();
+                void setSourceUrl( const std::string &url );
                 uint64_t getLiveTemplateNumber(vlc_tick_t, bool = true) const;
-                stime_t getMinAheadScaledTime(uint64_t) const;
                 void pruneByPlaybackTime(vlc_tick_t);
                 size_t pruneBySequenceNumber(uint64_t);
-                virtual Timescale inheritTimescale() const; /* reimpl */
-                virtual uint64_t inheritStartNumber() const;
-                stime_t inheritDuration() const;
-                SegmentTimeline * inheritSegmentTimeline() const;
+
+                virtual vlc_tick_t getMinAheadTime(uint64_t curnum) const; /* impl */
+                virtual Segment * getMediaSegment(uint64_t number) const; /* impl */
+                virtual Segment * getNextMediaSegment(uint64_t, uint64_t *, bool *) const; /* impl */
+                virtual InitSegment *getInitSegment() const;/* reimpl */
+                virtual uint64_t getStartSegmentNumber() const; /* impl */
+
+                virtual bool getSegmentNumberByTime(vlc_tick_t time, uint64_t *ret) const; /* impl */
+                virtual bool getPlaybackTimeDurationBySegmentNumber(uint64_t number,
+                                            vlc_tick_t *time, vlc_tick_t *duration) const; /* impl */
+
                 virtual void debug(vlc_object_t *, int = 0) const; /* reimpl */
 
             protected:
-                uint64_t startNumber;
-                SegmentTimeline *segmentTimeline;
                 SegmentInformation *parentSegmentInformation;
+                std::vector<SegmentTemplateSegment *> segments; /* should have only 1 */
         };
 
-        class InitSegmentTemplate : public InitSegment
+        class SegmentTemplateInit : public InitSegment
         {
             public:
-                InitSegmentTemplate( ICanonicalUrl * = NULL, MediaSegmentTemplate * = NULL );
-                virtual ~InitSegmentTemplate();
+                SegmentTemplateInit( SegmentTemplate *, ICanonicalUrl * = NULL );
+                virtual ~SegmentTemplateInit();
                 virtual void setSourceUrl( const std::string &url ); /* reimpl */
 
             protected:
-                const MediaSegmentTemplate *maintempl;
+                const SegmentTemplate *templ;
         };
     }
 }

modules/demux/dash/mpd/IsoffMainParser.cpp
@@ -182,6 +182,49 @@ void IsoffMainParser::parsePeriods(MPD *mpd, Node *root)
     }
 }
 
+void IsoffMainParser::parseSegmentBaseType(MPD *, Node *node,
+                                           AbstractSegmentBaseType *base,
+                                           SegmentInformation *parent)
+{
+    parseInitSegment(DOMHelper::getFirstChildElementByName(node, "Initialization"), base, parent);
+
+    if(node->hasAttribute("indexRange"))
+    {
+        size_t start = 0, end = 0;
+        if (std::sscanf(node->getAttributeValue("indexRange").c_str(), "%zu-%zu", &start, &end) == 2)
+        {
+            IndexSegment *index = new (std::nothrow) DashIndexSegment(parent);
+            if(index)
+            {
+                index->setByteRange(start, end);
+                base->indexSegment.Set(index);
+                /* index must be before data, so data starts at index end */
+                if(dynamic_cast<SegmentBase *>(base))
+                    dynamic_cast<SegmentBase *>(base)->setByteRange(end + 1, 0);
+            }
+        }
+    }
+
+    if(node->hasAttribute("timescale"))
+        base->setTimescale(Integer<uint64_t>(node->getAttributeValue("timescale")));
+
+}
+
+void IsoffMainParser::parseMultipleSegmentBaseType(MPD *mpd, Node *node,
+                                                   AbstractMultipleSegmentBaseType *base,
+                                                   SegmentInformation *parent)
+{
+    parseSegmentBaseType(mpd, node, base, parent);
+
+    if(node->hasAttribute("duration"))
+        base->duration.Set(Integer<stime_t>(node->getAttributeValue("duration")));
+
+    if(node->hasAttribute("startNumber"))
+        base->setStartNumber(Integer<uint64_t>(node->getAttributeValue("startNumber")));
+
+    parseTimeline(DOMHelper::getFirstChildElementByName(node, "SegmentTimeline"), base);
+}
+
 size_t IsoffMainParser::parseSegmentTemplate(MPD *mpd, Node *templateNode, SegmentInformation *info)
 {
     size_t total = 0;
@@ -192,33 +235,26 @@ size_t IsoffMainParser::parseSegmentTemplate(MPD *mpd, Node *templateNode, Segme
     if(templateNode->hasAttribute("media"))
         mediaurl = templateNode->getAttributeValue("media");
 
-    MediaSegmentTemplate *mediaTemplate = NULL;
-    if( !(mediaTemplate = new (std::nothrow) MediaSegmentTemplate(info)) )
+    SegmentTemplate *mediaTemplate = new (std::nothrow) SegmentTemplate(info);
+    if(!mediaTemplate)
         return total;
     mediaTemplate->setSourceUrl(mediaurl);
 
-    if(templateNode->hasAttribute("startNumber"))
-        mediaTemplate->setStartNumber(Integer<uint64_t>(templateNode->getAttributeValue("startNumber")));
-
-    if(templateNode->hasAttribute("timescale"))
-        mediaTemplate->setTimescale(Integer<uint64_t>(templateNode->getAttributeValue("timescale")));
-
-    if(templateNode->hasAttribute("duration"))
-        mediaTemplate->duration.Set(Integer<stime_t>(templateNode->getAttributeValue("duration")));
+    parseMultipleSegmentBaseType(mpd, templateNode, mediaTemplate, info);
 
     parseAvailability<SegmentInformation>(mpd, templateNode, info);
 
-    InitSegmentTemplate *initTemplate = NULL;
-
-    if(templateNode->hasAttribute("initialization"))
+    if(templateNode->hasAttribute("initialization")) /* /!\ != Initialization */
     {
+        SegmentTemplateInit *initTemplate;
         std::string initurl = templateNode->getAttributeValue("initialization");
-        if(!initurl.empty() && (initTemplate = new (std::nothrow) InitSegmentTemplate(info, mediaTemplate)))
+        if(!initurl.empty() && (initTemplate = new (std::nothrow) SegmentTemplateInit(mediaTemplate, info)))
+        {
             initTemplate->setSourceUrl(initurl);
+            delete mediaTemplate->initialisationSegment.Get();
+            mediaTemplate->initialisationSegment.Set(initTemplate);
+        }
     }
-    mediaTemplate->initialisationSegment.Set(initTemplate);
-
-    parseTimeline(DOMHelper::getFirstChildElementByName(templateNode, "SegmentTimeline"), mediaTemplate);
 
     info->setSegmentTemplate(mediaTemplate);
 
@@ -371,23 +407,8 @@ size_t IsoffMainParser::parseSegmentBase(MPD *mpd, Node * segmentBaseNode, Segme
     if(!segmentBaseNode || !(base = new (std::nothrow) SegmentBase(info)))
         return 0;
 
-    if(segmentBaseNode->hasAttribute("indexRange"))
-    {
-        size_t start = 0, end = 0;
-        if (std::sscanf(segmentBaseNode->getAttributeValue("indexRange").c_str(), "%zu-%zu", &start, &end) == 2)
-        {
-            IndexSegment *index = new (std::nothrow) DashIndexSegment(info);
-            if(index)
-            {
-                index->setByteRange(start, end);
-                base->indexSegment.Set(index);
-                /* index must be before data, so data starts at index end */
-                base->setByteRange(end + 1, 0);
-            }
-        }
-    }
+    parseSegmentBaseType(mpd, segmentBaseNode, base, info);
 
-    parseInitSegment(DOMHelper::getFirstChildElementByName(segmentBaseNode, "Initialization"), base, info);
     parseAvailability<SegmentInformation>(mpd, segmentBaseNode, info);
 
     if(!base->initialisationSegment.Get() && base->indexSegment.Get() && base->indexSegment.Get()->getOffset())
@@ -412,13 +433,7 @@ size_t IsoffMainParser::parseSegmentList(MPD *mpd, Node * segListNode, SegmentIn
         SegmentList *list;
         if((list = new (std::nothrow) SegmentList(info)))
         {
-            parseInitSegment(DOMHelper::getFirstChildElementByName(segListNode, "Initialization"), list, info);
-
-            if(segListNode->hasAttribute("duration"))
-                list->duration.Set(Integer<stime_t>(segListNode->getAttributeValue("duration")));
-
-            if(segListNode->hasAttribute("timescale"))
-                list->setTimescale(Integer<uint64_t>(segListNode->getAttributeValue("timescale")));
+            parseMultipleSegmentBaseType(mpd, segListNode, list, info);
 
             parseAvailability<SegmentInformation>(mpd, segListNode, info);
 
@@ -480,7 +495,7 @@ void IsoffMainParser::parseInitSegment(Node *initNode, Initializable<InitSegment
     init->initialisationSegment.Set(seg);
 }
 
-void IsoffMainParser::parseTimeline(Node *node, MediaSegmentTemplate *templ)
+void IsoffMainParser::parseTimeline(Node *node, AbstractMultipleSegmentBaseType *base)
 {
     if(!node)
         return;
@@ -488,10 +503,10 @@ void IsoffMainParser::parseTimeline(Node *node, MediaSegmentTemplate *templ)
     uint64_t number = 0;
     if(node->hasAttribute("startNumber"))
         number = Integer<uint64_t>(node->getAttributeValue("startNumber"));
-    else if(templ->inheritStartNumber())
-        number = templ->inheritStartNumber();
+    else if(base->inheritStartNumber())
+        number = base->inheritStartNumber();
 
-    SegmentTimeline *timeline = new (std::nothrow) SegmentTimeline(templ);
+    SegmentTimeline *timeline = new (std::nothrow) SegmentTimeline(base);
     if(timeline)
     {
         std::vector<Node *> elements = DOMHelper::getElementByTagName(node, "S", false);
@@ -519,7 +534,7 @@ void IsoffMainParser::parseTimeline(Node *node, MediaSegmentTemplate *templ)
 
             number += (1 + r);
         }
-        templ->setSegmentTimeline(timeline);
+        base->setSegmentTimeline(timeline);
     }
 }
 

modules/demux/smooth/playlist/Parser.cpp
@@ -232,7 +232,7 @@ static void ParseStreamIndex(BasePeriod *period, Node *streamIndexNode, unsigned
         if(!url.empty())
         {
             /* SmoothSegment is a template holder */
-            SmoothSegment *templ = new SmoothSegment(adaptSet);
+            SmoothSegmentTemplate *templ = new SmoothSegmentTemplate(adaptSet);
             if(templ)
             {
                 templ->setSourceUrl(url);

commit 0e1610fa42fd9cb9843e1347c7e994bdcdaee144

demux: adaptive: fix smooth template overloading
regression by refactoring changes in b15abfbf556c53b23a6bd86d3fd4ac4a4accec7d

modules/demux/adaptive/playlist/SegmentTemplate.cpp
@@ -31,13 +31,12 @@
 
 using namespace adaptive::playlist;
 
-SegmentTemplateSegment::SegmentTemplateSegment( SegmentTemplate *templ_,
-                                                ICanonicalUrl *parent ) :
+SegmentTemplateSegment::SegmentTemplateSegment( ICanonicalUrl *parent ) :
     Segment( parent )
 {
     debugName = "SegmentTemplateSegment";
     templated = true;
-    templ = templ_;
+    templ = nullptr;
 }
 
 SegmentTemplateSegment::~SegmentTemplateSegment()
@@ -50,12 +49,19 @@ void SegmentTemplateSegment::setSourceUrl(const std::string &url)
     sourceUrl = Url(Url::Component(url, templ));
 }
 
-SegmentTemplate::SegmentTemplate( SegmentInformation *parent ) :
+void SegmentTemplateSegment::setParentTemplate( SegmentTemplate *templ_ )
+{
+    templ = templ_;
+}
+
+SegmentTemplate::SegmentTemplate( SegmentTemplateSegment *seg, SegmentInformation *parent ) :
     AbstractMultipleSegmentBaseType( parent, AbstractAttr::Type::SegmentTemplate )
 {
     initialisationSegment.Set( nullptr );
     parentSegmentInformation = parent;
-    virtualsegment = new SegmentTemplateSegment( this, parent );
+    virtualsegment = seg;
+    virtualsegment->setParent( parentSegmentInformation );
+    virtualsegment->setParentTemplate( this );
 }
 
 SegmentTemplate::~SegmentTemplate()

modules/demux/adaptive/playlist/SegmentTemplate.h
@@ -39,9 +39,10 @@ namespace adaptive
         class SegmentTemplateSegment : public Segment
         {
             public:
-                SegmentTemplateSegment( SegmentTemplate *, ICanonicalUrl * = nullptr );
+                SegmentTemplateSegment( ICanonicalUrl * = nullptr );
                 virtual ~SegmentTemplateSegment();
                 virtual void setSourceUrl( const std::string &url ); /* reimpl */
+                void setParentTemplate( SegmentTemplate * );
 
             protected:
                 const SegmentTemplate *templ;
@@ -50,7 +51,7 @@ namespace adaptive
         class SegmentTemplate : public AbstractMultipleSegmentBaseType
         {
             public:
-                SegmentTemplate( SegmentInformation * = nullptr );
+                SegmentTemplate( SegmentTemplateSegment *, SegmentInformation * = nullptr );
                 virtual ~SegmentTemplate();
                 void setSourceUrl( const std::string &url );
                 uint64_t getLiveTemplateNumber(vlc_tick_t, bool = true) const;

modules/demux/dash/mpd/IsoffMainParser.cpp
@@ -237,7 +237,7 @@ size_t IsoffMainParser::parseSegmentTemplate(MPD *mpd, Node *templateNode, Segme
     if(templateNode->hasAttribute("media"))
         mediaurl = templateNode->getAttributeValue("media");
 
-    SegmentTemplate *mediaTemplate = new (std::nothrow) SegmentTemplate(info);
+    SegmentTemplate *mediaTemplate = new (std::nothrow) SegmentTemplate(new SegmentTemplateSegment(), info);
     if(!mediaTemplate)
         return total;
     mediaTemplate->setSourceUrl(mediaurl);

modules/demux/smooth/playlist/Parser.cpp
@@ -237,7 +237,7 @@ static void ParseStreamIndex(BasePeriod *period, Node *streamIndexNode, unsigned
         if(!url.empty())
         {
             /* SmoothSegment is a template holder */
-            SmoothSegmentTemplate *templ = new SmoothSegmentTemplate(adaptSet);
+            SegmentTemplate *templ = new SegmentTemplate(new SmoothSegmentTemplateSegment(), adaptSet);
             if(templ)
             {
                 templ->setSourceUrl(url);

modules/demux/smooth/playlist/SmoothSegment.cpp
@@ -64,19 +64,20 @@ void SmoothSegmentChunk::onDownload(block_t **pp_block)
     }
 }
 
-SmoothSegmentTemplate::SmoothSegmentTemplate(SegmentInformation *parent) :
-    SegmentTemplate( parent )
+SmoothSegmentTemplateSegment::SmoothSegmentTemplateSegment(ICanonicalUrl *parent)
+    : SegmentTemplateSegment(parent)
 {
 
 }
 
-SmoothSegmentTemplate::~SmoothSegmentTemplate()
+SmoothSegmentTemplateSegment::~SmoothSegmentTemplateSegment()
 {
 
 }
 
-SegmentChunk* SmoothSegmentTemplate::createChunk(AbstractChunkSource *source, BaseRepresentation *rep)
+SegmentChunk* SmoothSegmentTemplateSegment::createChunk(AbstractChunkSource *source, BaseRepresentation *rep)
 {
      /* act as factory */
     return new (std::nothrow) SmoothSegmentChunk(source, rep);
 }
+

modules/demux/smooth/playlist/SmoothSegment.hpp
@@ -33,15 +33,15 @@ namespace smooth
         {
             public:
                 SmoothSegmentChunk(AbstractChunkSource *, BaseRepresentation *);
-                ~SmoothSegmentChunk();
+                virtual ~SmoothSegmentChunk();
                 virtual void onDownload(block_t **); /* reimpl */
         };
 
-        class SmoothSegmentTemplate : public SegmentTemplate
+        class SmoothSegmentTemplateSegment : public SegmentTemplateSegment
         {
             public:
-                SmoothSegmentTemplate(SegmentInformation * = nullptr);
-                ~SmoothSegmentTemplate();
+                SmoothSegmentTemplateSegment( ICanonicalUrl * = nullptr );
+                virtual ~SmoothSegmentTemplateSegment();
                 virtual SegmentChunk* createChunk(AbstractChunkSource *, BaseRepresentation *); /* reimpl */
         };
     }