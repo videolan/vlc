videolan/vlc

commit 0abed25515a492896c89cf36ab1e4befc7dd23b6

decoder: fix forgotten vout error state
If the pool allocation fails or if the vout fails to start (via
input_resource_RequestVout()), an error is returned from
decoder_UpdateVideoFormat(). The next call to
decoder_UpdateVideoFormat() will return a success because the vctx will
be the same since only this variable is checked to detect a vout change.

To fix this issue, clean the owner->fmt and release the vctx when
returning an error.

src/input/decoder.c
@@ -457,7 +457,7 @@ static int ModuleThread_UpdateVideoFormat( decoder_t *p_dec, vlc_video_context *
             msg_Err(p_dec, "Failed to create a pool of %d %4.4s pictures",
                            dpb_size + p_dec->i_extra_picture_buffers + 1,
                            (char*)&p_dec->fmt_out.video.i_chroma);
-            return -1;
+            goto error;
         }
 
         vlc_mutex_lock( &p_owner->lock );
@@ -481,6 +481,18 @@ static int ModuleThread_UpdateVideoFormat( decoder_t *p_dec, vlc_video_context *
         return 0;
     }
 
+error:
+    /* Clean fmt and vctx to trigger a new vout creation on the next update
+     * call */
+    vlc_mutex_lock( &p_owner->lock );
+    es_format_Clean( &p_owner->fmt );
+    vlc_mutex_unlock( &p_owner->lock );
+
+    if (p_owner->vctx != NULL)
+    {
+        vlc_video_context_Release(p_owner->vctx);
+        p_owner->vctx = NULL;
+    }
     return -1;
 }
 

commit 283145ed4d9f9033d9f9f563c6f77fef6271334f

decoder: fix abort on vout failure
Don't notify the format changed if the output creation failed.

Regression from 0abed25515a492896c89cf36ab1e4befc7dd23b6

Fixes #25387

src/input/decoder.c
@@ -2545,6 +2545,14 @@ bool vlc_input_decoder_HasFormatChanged( vlc_input_decoder_t *p_owner,
         return false;
 
     vlc_mutex_lock( &p_owner->lock );
+
+    if( p_owner->fmt.i_cat == UNKNOWN_ES )
+    {
+        /* The format changed but the output creation failed */
+        vlc_mutex_unlock( &p_owner->lock );
+        return false;
+    }
+
     if( p_fmt != NULL )
         es_format_Copy( p_fmt, &p_owner->fmt );
 