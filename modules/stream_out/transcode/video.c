videolan/vlc

commit 030183d79045a1747d891572b2fe77d8cd0fd946

filter: fps: forward video context and ensure input
The FPS filter is resampling the input source into the output source but
it only modifies metadata, so input chroma and input video context must
be used for output.

modules/stream_out/transcode/video.c
@@ -385,7 +385,7 @@ static int transcode_video_filters_init( sout_stream_t *p_stream,
 
     if( b_master_sync )
     {
-        filter_chain_AppendFilter( id->p_f_chain, "fps", NULL, p_dst );
+        filter_chain_AppendFilter( id->p_f_chain, "fps", NULL, p_src );
         p_src = filter_chain_GetFmtOut( id->p_f_chain );
         src_ctx = filter_chain_GetVideoCtxOut( id->p_f_chain );
     }

modules/video_filter/fps.c
@@ -138,6 +138,10 @@ static int Open( vlc_object_t *p_this)
     filter_t *p_filter = (filter_t*)p_this;
     filter_sys_t *p_sys;
 
+    /* This filter cannot change the format. */
+    if( p_filter->fmt_out.video.i_chroma != p_filter->fmt_in.video.i_chroma )
+        return VLC_EGENERIC;
+
     p_sys = p_filter->p_sys = malloc( sizeof( *p_sys ) );
 
     if( unlikely( !p_sys ) )
@@ -179,6 +183,11 @@ static int Open( vlc_object_t *p_this)
     p_sys->p_previous_pic = NULL;
 
     p_filter->pf_video_filter = Filter;
+
+    /* We don't change neither the format nor the picture */
+    if ( p_filter->vctx_in )
+        p_filter->vctx_out = vlc_video_context_Hold( p_filter->vctx_in );
+
     return VLC_SUCCESS;
 }
 
@@ -188,5 +197,7 @@ static void Close( vlc_object_t *p_this )
     filter_sys_t *p_sys = p_filter->p_sys;
     if( p_sys->p_previous_pic )
         picture_Release( p_sys->p_previous_pic );
+    if( p_filter->vctx_out )
+        vlc_video_context_Release( p_filter->vctx_out );
     free( p_sys );
 }

commit 57cd4abb46c64985188d7c3da62c014daa19111b

transcode: video: fix insertion of fps filter
The fps filter must be added when the target framerate is different
from the source framerate, not when we define the fps parameter.

We also check that fps.den > 0 to avoid divide-by-zero errors.

Probably regression from 030183d79045a1747d891572b2fe77d8cd0fd946 in
which the target framerate was defined in the destination format.

modules/stream_out/transcode/video.c
@@ -297,11 +297,19 @@ static int transcode_video_filters_init( sout_stream_t *p_stream,
         src_ctx = filter_chain_GetVideoCtxOut( id->p_f_chain );
     }
 
-    if( id->p_enccfg->video.fps.num > 0 )
+    if( id->p_enccfg->video.fps.num > 0 &&
+        id->p_enccfg->video.fps.den > 0 &&
+      ( id->p_enccfg->video.fps.num != p_src->video.i_frame_rate ||
+        id->p_enccfg->video.fps.den != p_src->video.i_frame_rate_base ) )
     {
-        filter_chain_AppendFilter( id->p_f_chain, "fps", NULL, p_src );
+        es_format_t dst;
+        es_format_Copy(&dst, p_src);
+        dst.video.i_frame_rate = id->p_enccfg->video.fps.num;
+        dst.video.i_frame_rate_base = id->p_enccfg->video.fps.den;
+        filter_chain_AppendFilter( id->p_f_chain, "fps", NULL, &dst );
         p_src = filter_chain_GetFmtOut( id->p_f_chain );
         src_ctx = filter_chain_GetVideoCtxOut( id->p_f_chain );
+        es_format_Clean(&dst);
     }
 
     /* User filters */