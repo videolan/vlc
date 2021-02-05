dca989173cf9fe355897662acd323735b37b1fcc
Alexandre Janniaux <ajanni@videolabs.io>
2021-02-05 10:36:40 +0100

    gl_common: disable GL_ASSERT_NOERROR in release
    ...and remove the obsolete comment, the cases have been fixed and should
    be fixed if it happens again.
    
    We don't need "proper event handling" as most errors are coming from
    programmation errors, except in a few tests (like 16bit texture test)
    which have been fixed already.

--- modules/video_output/opengl/gl_common.h
+++ modules/video_output/opengl/gl_common.h
@@ -160,12 +160,7 @@
 # define APIENTRY
 #endif
 
-/* FIXME: GL_ASSERT_NOERROR disabled for now because:
- * Proper GL error handling need to be implemented
- * glClear(GL_COLOR_BUFFER_BIT) throws a GL_INVALID_FRAMEBUFFER_OPERATION on macOS
- * assert fails on vout_display_opengl_Delete on iOS
- */
-#if 0
+#ifndef NDEBUG
 # define HAVE_GL_ASSERT_NOERROR
 #endif
 
