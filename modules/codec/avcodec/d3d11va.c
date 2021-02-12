26f60f9d783edf90998bd3e05ced2d73a047f6a8
Alexandre Janniaux <ajanni@videolabs.io>
2021-02-12 15:34:23 +0100

    d3d11va: return ENOMEM for malloc error

--- modules/codec/avcodec/d3d11va.c
+++ modules/codec/avcodec/d3d11va.c
@@ -205,7 +205,7 @@ static int Get(vlc_va_t *va, picture_t *pic, uint8_t **data)
     if (unlikely(pic->context == NULL))
     {
         va_surface_Release(va_surface);
-        return VLC_ENOITEM;
+        return VLC_ENOMEM;
     }
     data[3] = (uint8_t*)sys->hw_surface[va_surface_GetIndex(va_surface)];
     return VLC_SUCCESS;
