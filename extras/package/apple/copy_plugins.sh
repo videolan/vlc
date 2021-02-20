3f9b85e225676fc808e44ed65e9f7f93657a484b
Alexandre Janniaux <ajanni@videolabs.io>
2021-02-20 10:23:20 +0100

    extras: apple: add xcodegen spec project
    Add an XCodeGen project spec to generate an XCode project which will be
    using autoconf and automake to build, as well as exposing the generated
    artifacts to other XCode projects.
    
    This will be generating the build artifacts inside the product build
    directory from XCode, so `Clean build folder` will work as expected in
    the editor, and use dynamic plugins to copy them towards the final
    application for now.
    
    This project allows testing VLC directly through the iosvlc.m test
    driver, without building VLCKit nor VLC for iOS or another test
    application, by directly using XCode to run it on the targetted
    platform.
    
    Exposing such project also allows debug symbols and debugguer with
    breakpoint support to work automatically, even when using the artifacts
    in another project.
    
    The xcode.sh script is added as a wrapper script to handle the
    specificities of XCode when calling external buildsystem, in particular
    the multi-arch building for archiving application and packages.
    
    The copy_plugins.sh is added as a build phase script so that application
    can install the plugins matching the architecture being built. It
    currently probably fails with cases where some plugins are not built for
    every architecture of a platform since I haven't tested that yet. It is
    also quite "dumb" and re-do everything from scratch for now.

---
+++ extras/package/apple/copy_plugins.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+function plist_key()
+{
+    KEY="$1"
+    VALUE="$2"
+    echo '  <key>'"${KEY}"'</key>'
+    echo '  <string>'"${VALUE}"'</string>'
+}
+
+function generate_info_plist()
+{
+    EXECUTABLE_NAME="$1"
+    echo '<?xml version="1.0" encoding="UTF-8"?>'
+    echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
+    echo '<plist version="1.0">'
+    echo '<dict>'
+    plist_key CFBundleDevelopmentRegion "en"
+    plist_key CFBundleExecutable "${EXECUTABLE_NAME}"
+    plist_key CFBundleIdentifier "org.videolan.vlc.plugins.${EXECUTABLE_NAME}.framework"
+    plist_key CFBundleInfoDictionaryVersion "6.0"
+    plist_key CFBundlePackageType "FMWK"
+    plist_key CFBundleSignature "???"
+    echo '</dict>'
+    echo '</plist>'
+}
+
+PLUGINS=()
+for arch in ${ARCHS}; do
+  while read -r plugin; do
+    if [[ ! "${PLUGINS[@]}" =~ "${plugin}" ]]; then
+      PLUGINS+=( "${plugin}" )
+    fi
+  done < "${BUILT_PRODUCTS_DIR}/build-${PLATFORM_NAME}-${arch}/build/modules/vlc_modules_list"
+done
+
+for plugin in "${PLUGINS[@]}"; do
+    echo "Copying plugin $plugin for platform ${PLATFORM_NAME}"
+
+    INPUT_FILES=()
+    for arch in ${ARCHS}; do
+        input_file="${BUILT_PRODUCTS_DIR}/build-${PLATFORM_NAME}-${arch}/build/modules/.libs/lib${plugin}.dylib"
+        if [ ! -f "${input_file}" ]; then
+            continue
+        fi
+        echo " - Adding plugin file ${input_file} for lipo"
+        INPUT_FILES+=( "$input_file" )
+    done
+    FRAMEWORK_DIR="${BUILT_PRODUCTS_DIR}/${FRAMEWORKS_FOLDER_PATH}/${plugin}.framework"
+    FRAMEWORK_BIN_PATH="${FRAMEWORK_DIR}/${plugin}"
+    mkdir -p "${FRAMEWORK_DIR}"
+    lipo -create \
+        -output "${FRAMEWORK_BIN_PATH}" \
+        "${INPUT_FILES[@]}"
+    install_name_tool -change "@rpath/libvlccore.dylib" "@rpath/vlccore.framework/vlccore" \
+        "${FRAMEWORK_BIN_PATH}"
+    dsymutil -o "${FRAMEWORK_DIR}.dSYM" "${FRAMEWORK_BIN_PATH}"
+    generate_info_plist "${plugin}" > "${FRAMEWORK_DIR}/Info.plist"
+    codesign --force --sign "${EXPANDED_CODE_SIGN_IDENTITY}" "${FRAMEWORK_DIR}"
+done
