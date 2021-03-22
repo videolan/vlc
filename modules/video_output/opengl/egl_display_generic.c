90a6084eaa9611fdf44c91e6b48a24f2a4c46cd9
Romain Vimont <rom1v@videolabs.io>
2021-03-22 16:06:24 +0100

    egl_display: improve support detection
    A module egl_display must only provide EGL displays where eglTerminate()
    can be called, even if other modules are using EGL (typically because it
    is internally refcounted).
    
    This is always the case on Android, so egl_display was initially only
    enabled on Android:
    https://android.googlesource.com/platform/frameworks/native/+/master/opengl/libs/EGL/egl_display.cpp
    
    But this may also be the case on other platforms which support
    EGL_KHR_display_reference:
    https://www.khronos.org/registry/EGL/extensions/KHR/EGL_KHR_display_reference.txt
    
    Co-authored-by: Alexandre Janniaux <ajanni@videolabs.io>
    Signed-off-by: Alexandre Janniaux <ajanni@videolabs.io>

--- modules/video_output/opengl/egl_display_generic.c
+++ modules/video_output/opengl/egl_display_generic.c
@@ -31,11 +31,49 @@
 
 #include "egl_display.h"
 
+#ifndef __ANDROID__
+static EGLenum GetPlatform(const char *extensions)
+{
+#ifdef EGL_KHR_platform_x11
+    if (vlc_gl_StrHasToken(extensions, "EGL_EXT_platform_x11"))
+        return EGL_PLATFORM_X11_KHR;
+#endif
+
+#ifdef EGL_KHR_platform_wayland
+    if (vlc_gl_StrHasToken(extensions, "EGL_EXT_platform_wayland"))
+        return EGL_PLATFORM_WAYLAND_KHR;
+#endif
+
+    return 0;
+}
+#endif
+
 static vlc_egl_display_open_fn Open;
 static int
 Open(struct vlc_egl_display *display)
 {
+#ifdef __ANDROID__
+    /* The default display is refcounted on Android */
     display->display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+#elif defined(EGL_KHR_display_reference)
+    const char *extensions = eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
+
+    if (!vlc_gl_StrHasToken(extensions, "EGL_KHR_display_reference"))
+        return VLC_EGENERIC;
+
+    EGLenum platform = GetPlatform(extensions);
+    if (!platform)
+        return VLC_EGENERIC;
+
+    const EGLAttrib attribs[] = {
+        EGL_TRACK_REFERENCES_KHR, EGL_TRUE,
+        EGL_NONE,
+    };
+
+    display->display =
+        eglGetPlatformDisplay(platform, EGL_DEFAULT_DISPLAY, attribs);
+#endif
+
     if (display->display == EGL_NO_DISPLAY)
         return VLC_EGENERIC;
 
