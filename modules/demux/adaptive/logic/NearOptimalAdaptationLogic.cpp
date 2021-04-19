36eb43a78218a02f6f46c31fb73aa8f5dbcf2765
Francois Cartegnie <fcvlcdev@free.fr>
2021-04-19 16:53:14 +0200

    demux: adaptive: simplify logic case with single rep

--- modules/demux/adaptive/logic/NearOptimalAdaptationLogic.cpp
+++ modules/demux/adaptive/logic/NearOptimalAdaptationLogic.cpp
@@ -92,6 +92,9 @@ BaseRepresentation *NearOptimalAdaptationLogic::getNextRepresentation(BaseAdapta
     if(lowest == nullptr || highest == nullptr)
         return nullptr;
 
+    if(lowest == highest)
+        return lowest;
+
     const float umin = getUtility(lowest);
     const float umax = getUtility(highest);
 
