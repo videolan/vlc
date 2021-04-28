d47a0f2744201b2a5e70ed297c22635f62424a8a
Mehdi Sabwat <mehdi@videolabs.io>
2021-04-29 00:48:05 +0200

    package: add wasm-emscriten build script
    Co-Authored-By: Jean-Baptiste Kempf <jb@videolan.org>
    Co-Authored-By: Etienne Brateau <etienne.brateau@gmail.com>

---
+++ extras/package/wasm-emscripten/build.sh
@@ -0,0 +1,198 @@
+#! /bin/sh
+# Copyright (C) 2003-2021 the VideoLAN team
+#
+# This file is under the same license as the vlc package.
+
+set -e
+
+diagnostic()
+{
+    echo "### build logs ###: " "$@" 1>&2;
+}
+
+usage()
+{
+    echo "Usage: $0 [--mode=(default=1)]"
+    echo "  --with-prebuilt-contribs | -c"
+    echo "  --mode=1 build all "
+    echo "  --mode=0 incremental build (do not bootstrap and configure) "  
+}
+
+NM="$EMSDK/upstream/bin/llvm-nm"
+get_symbol()
+{
+    echo "$1" | grep vlc_entry_"$2" | cut -d " " -f 3
+}
+
+get_entryname()
+{
+    symbols=$($NM -g "$1")
+    entryname=$(get_symbol "$symbols" _)
+    echo "$entryname"
+}
+
+while test -n "$1"
+do
+    case "$1" in
+        --help|-h)
+            usage
+            exit 0
+            ;;
+        --mode=*)
+            BUILD_MODE="${1#--mode=}"
+            ;;
+	--with-prebuilt-contribs)
+	    VLC_USE_PREBUILT_CONTRIBS=1
+	    ;;
+	--gen-contrib-archive|-c)
+	    GENERATE_ARCHIVE=1
+	    ;;
+        *)
+            echo "Unrecognized options $1"
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+BUILD_MODE=${BUILD_MODE:=1}
+VLC_PREBUILT_CONTRIBS_URL=${VLC_PREBUILT_CONTRIBS_URL:-""}
+BUILDDIR_NAME="build-emscripten"
+GENERATE_ARCHIVE=${GENERATE_ARCHIVE:=0}
+VLC_USE_PREBUILT_CONTRIBS=${VLC_USE_PREBUILT_CONTRIBS:=0}
+
+diagnostic "setting MAKEFLAGS"
+if [ -z "$MAKEFLAGS" ]; then
+    UNAMES=$(uname -s)
+    MAKEFLAGS=
+    if which nproc >/dev/null; then
+        MAKEFLAGS=-j$(nproc)
+    elif [ "$UNAMES" = "Darwin" ] && which sysctl >/dev/null; then
+        MAKEFLAGS=-j$(sysctl -n machdep.cpu.thread_count)
+    fi
+    export MAKEFLAGS;
+fi
+
+diagnostic "setting up dir paths"
+
+OLD_PWD=$(pwd)
+VLC_SRCPATH="$(dirname "$0")/../../../"
+cd "$VLC_SRCPATH"
+VLC_SRCPATH=$(pwd)
+cd "$OLD_PWD"
+
+diagnostic "vlc sources path: "
+echo "$VLC_SRCPATH";
+
+diagnostic "vlc tools: bootstrap"
+cd "$VLC_SRCPATH"/extras/tools
+./bootstrap 
+
+diagnostic "vlc tools: make"
+make
+
+# update the PATH
+export PATH=$VLC_SRCPATH/extras/tools/bin:$PATH
+
+diagnostic "sdk tests: checking if autoconf supports emscripten"
+# https://code.videolan.org/-/snippets/1283
+for file in /usr/share/automake-*
+do
+    # This will pick the latest automake version
+    AUTOMAKE_VERSION="$(echo "$file" | cut -d- -f2)"
+done
+
+diagnostic "using automake version: /usr/share/automake-$AUTOMAKE_VERSION"
+if [ -f /usr/share/automake-"$AUTOMAKE_VERSION"/config.sub ]; then
+    /usr/share/automake-"$AUTOMAKE_VERSION"/config.sub wasm32-unknown-emscripten
+fi
+
+mkdir -p "$VLC_SRCPATH"/contrib/contrib-emscripten
+cd "$VLC_SRCPATH"/contrib/contrib-emscripten
+
+diagnostic "vlc contribs: bootstrap"
+../bootstrap --disable-disc --disable-sout --disable-net \
+	     --disable-postproc --disable-gmp --disable-gcrypt --disable-ass \
+	     --disable-gpgerror --disable-harfbuzz --disable-fontconfig \
+	     --disable-asdcplib --disable-caca --disable-gettext \
+             --disable-goom --disable-libplacebo \
+             --disable-lua --disable-luac --disable-sqlite \
+             --disable-medialibrary --disable-mpcdec --disable-schroedinger \
+             --disable-orc --disable-protobuf --disable-sidplay2 \
+             --disable-soxr --disable-spatialaudio --disable-speex \
+             --disable-speexdsp --disable-taglib --disable-zvbi \
+             --disable-rnnoise \
+             --host=wasm32-unknown-emscripten
+
+diagnostic "vlc contribs: make"
+if [ "$VLC_USE_PREBUILT_CONTRIBS" -eq "0" ]; then
+    emmake make
+    if [ "$GENERATE_ARCHIVE" -eq "1" ]; then
+	diagnostic "vlc contribs: generating package"
+	emmake make package
+    fi
+else
+    diagnostic "vlc contribs: using prebuilt contribs"
+    emmake make prebuilt PREBUILT_URL="$VLC_PREBUILT_CONTRIBS_URL"
+fi
+
+diagnostic "starting libvlc build"
+BUILD_PATH=$VLC_SRCPATH/$BUILDDIR_NAME
+mkdir -p "$BUILD_PATH"
+
+diagnostic "vlc build dir: "
+echo "$BUILD_PATH";
+
+cd "$BUILD_PATH"
+if [ $BUILD_MODE -eq 1 ]; then
+    diagnostic "libvlc build: bootstrap"
+    ../bootstrap
+
+    diagnostic "libvlc build: configure"
+    # if_nameindex is not supported in emscripten
+    # ie: not exposed from musl to src/library.js
+    # the test in configure.ac fails because htons is not
+    # in tools/deps_info.py
+
+    # shm.h is a blacklisted module
+    emconfigure "$VLC_SRCPATH"/configure --host=wasm32-unknown-emscripten --enable-debug \
+                        --disable-shared --disable-vlc \
+                        --disable-sout --disable-vlm --disable-xcb --disable-lua \
+                        --disable-addonmanagermodules --disable-ssp \
+                        ac_cv_func_if_nameindex=yes ac_cv_header_sys_shm_h=no \
+                        --with-contrib="$VLC_SRCPATH"/contrib/wasm32-unknown-emscripten
+fi
+
+diagnostic "libvlc build: make"
+emmake make
+
+diagnostic "libvlc build: generate static modules entry points"
+# start by deleting the previous version so that it's not overwritten
+rm -f "$BUILD_PATH"/vlc-modules.c "$BUILD_PATH"/vlc-modules.bc
+
+# create module list
+echo "creating module list"
+FUN_PROTOS=""
+FUN_LIST=""
+
+for file in "$BUILD_PATH"/modules/.libs/*plugin.a
+do
+    ENTRY=$(get_entryname "$file")
+    FUN_PROTOS="$FUN_PROTOS""int $ENTRY (int (*)(void *, void *, int, ...), void *);\n"
+    FUN_LIST="$FUN_LIST""$ENTRY,\n"
+done;
+
+printf "// This file is autogenerated
+#include <stddef.h>
+%b\n
+const void *vlc_static_modules[] = {
+%bNULL
+};\n" "$FUN_PROTOS" "$FUN_LIST" \
+       > "$BUILD_PATH"/vlc-modules.c
+
+diagnostic "vlc static modules: compiling static modules entry points"
+# compile vlc-modules.c
+emcc -pthread -c "$BUILD_PATH"/vlc-modules.c -o "$BUILD_PATH"/vlc-modules.bc
+
+echo "VLC for wasm32-unknown-emscripten built!"
