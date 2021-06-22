cb1e02fb4c3b361408c1b48a3e16a1c22cd0da5d
Steve Lhomme <robux4@ycbcr.xyz>
2021-06-22 09:08:59 +0200

    directx_va: only call profile readers if we don't have one
    That's how MediaCodec does it. These readers can be wrong so rely on lavc
    and container values instead.
    
    Fixes #25850
    
    (cherry picked from commit a69b492a6272c84b9c2efa912ad0b1dc612a2b83) (edited)
    edited:
    - the 3.0 branch sets the is_supported later
    
    Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

--- modules/codec/avcodec/directx_va.c
+++ modules/codec/avcodec/directx_va.c
@@ -365,13 +365,13 @@ static bool profile_supported(const directx_va_mode_t *mode, const es_format_t *
     if (!is_supported)
     {
         int profile = fmt->i_profile >= 0 ? fmt->i_profile : avctx->profile;
-        if (mode->codec == AV_CODEC_ID_H264)
+        if (mode->codec == AV_CODEC_ID_H264 && profile == -1)
         {
             uint8_t h264_profile;
             if ( h264_get_profile_level(fmt, &h264_profile, NULL, NULL) )
                 profile = h264_profile;
         }
-        if (mode->codec == AV_CODEC_ID_HEVC)
+        if (mode->codec == AV_CODEC_ID_HEVC && profile == -1)
         {
             uint8_t hevc_profile;
             if (hevc_get_profile_level(fmt, &hevc_profile, NULL, NULL) )
