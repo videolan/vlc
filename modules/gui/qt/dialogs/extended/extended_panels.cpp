videolan/vlc

commit 537d41a468f6f8d155b54214e0214e5576d52e68

qt: load Qt in a detached structure from the interface module.
  this will allow to unload the interface module without unloading Qt in a
  further patch.

  the new structure is still a vlc object as we use it notably for logging
  capacity and to interact with vlc variables

modules/gui/qt/dialogs/extended/extended_panels.cpp
@@ -369,7 +369,7 @@ void ExtVideo::browseLogo()
                         .arg( TITLE_EXTENSIONS_IMAGE )
                         .arg( TITLE_EXTENSIONS_ALL );
     QString file = QFileDialog::getOpenFileUrl( NULL, qtr( "Logo filenames" ),
-                   p_intf->p_sys->filepath, filter,
+                   p_intf->filepath, filter,
                    NULL, QFileDialog::Options(), schemes ).toLocalFile();
 
     UPDATE_AND_APPLY_TEXT( logoFileText, file );
@@ -382,7 +382,7 @@ void ExtVideo::browseEraseFile()
                         .arg( TITLE_EXTENSIONS_IMAGE )
                         .arg( TITLE_EXTENSIONS_ALL );
     QString file = QFileDialog::getOpenFileUrl( NULL, qtr( "Image mask" ),
-                   p_intf->p_sys->filepath, filter,
+                   p_intf->filepath, filter,
                    NULL, QFileDialog::Options(), schemes ).toLocalFile();
 
     UPDATE_AND_APPLY_TEXT( eraseMaskText, file );
@@ -623,8 +623,8 @@ void ExtV4l2::showEvent( QShowEvent *event )
 
 void ExtV4l2::Refresh( void )
 {
-    vlc_player_Lock(p_intf->p_sys->p_player);
-    vlc_object_t *p_obj = vlc_player_GetV4l2Object(p_intf->p_sys->p_player);
+    vlc_player_Lock(p_intf->p_player);
+    vlc_object_t *p_obj = vlc_player_GetV4l2Object(p_intf->p_player);
     help->hide();
     if( box )
     {
@@ -774,7 +774,7 @@ void ExtV4l2::Refresh( void )
         if ( isVisible() )
             QTimer::singleShot( 2000, this, SLOT(Refresh()) );
     }
-    vlc_player_Unlock(p_intf->p_sys->p_player);
+    vlc_player_Unlock(p_intf->p_player);
 }
 
 void ExtV4l2::ValueChange( bool value )
@@ -785,8 +785,8 @@ void ExtV4l2::ValueChange( bool value )
 void ExtV4l2::ValueChange( int value )
 {
     QObject *s = sender();
-    vlc_player_Lock(p_intf->p_sys->p_player);
-    vlc_object_t *p_obj = vlc_player_GetV4l2Object(p_intf->p_sys->p_player);
+    vlc_player_Lock(p_intf->p_player);
+    vlc_object_t *p_obj = vlc_player_GetV4l2Object(p_intf->p_player);
     if( p_obj )
     {
         QString var = s->objectName();
@@ -808,11 +808,11 @@ void ExtV4l2::ValueChange( int value )
                 var_TriggerCallback( p_obj, qtu( var ) );
                 break;
         }
-        vlc_player_Unlock(p_intf->p_sys->p_player);
+        vlc_player_Unlock(p_intf->p_player);
     }
     else
     {
-        vlc_player_Unlock(p_intf->p_sys->p_player);
+        vlc_player_Unlock(p_intf->p_player);
         msg_Warn( p_intf, "Oops, v4l2 object isn't available anymore" );
         Refresh();
     }
@@ -963,7 +963,7 @@ void AudioFilterControlWidget::enable( bool b_enable )
     QString result = ChangeFiltersString( p_intf, "audio-filter", qtu(name),
                                           b_enable );
     emit configChanged( qfu("audio-filter"), result );
-    vlc_player_aout_EnableFilter( p_intf->p_sys->p_player, qtu(name), b_enable );
+    vlc_player_aout_EnableFilter( p_intf->p_player, qtu(name), b_enable );
 }
 
 /**********************************************************************

modules/gui/qt/maininterface/main_interface.cpp
@@ -265,7 +265,7 @@ MainInterface::~MainInterface()
     var_DelCallback( libvlc, "intf-toggle-fscontrol", IntfShowCB, p_intf );
     var_DelCallback( libvlc, "intf-popupmenu", PopupMenuCB, p_intf );
 
-    p_intf->p_sys->p_mi = NULL;
+    p_intf->p_mi = NULL;
 }
 
 bool MainInterface::hasVLM() const {
@@ -691,8 +691,8 @@ void MainInterface::dragLeaveEvent(QDragLeaveEvent *event)
 
 void MainInterface::closeEvent( QCloseEvent *e )
 {
-    PlaylistControllerModel* playlistController = p_intf->p_sys->p_mainPlaylistController;
-    PlayerController* playerController = p_intf->p_sys->p_mainPlayerController;
+    PlaylistControllerModel* playlistController = p_intf->p_mainPlaylistController;
+    PlayerController* playerController = p_intf->p_mainPlayerController;
 
     if (m_videoSurfaceProvider)
         m_videoSurfaceProvider->onWindowClosed();
@@ -763,7 +763,7 @@ static int PopupMenuCB( vlc_object_t *, const char *,
 
     if( p_intf->pf_show_dialog )
     {
-        p_intf->pf_show_dialog( p_intf, INTF_DIALOG_POPUPMENU,
+        p_intf->pf_show_dialog( p_intf->intf, INTF_DIALOG_POPUPMENU,
                                 new_val.b_bool, NULL );
     }
 
@@ -777,7 +777,7 @@ static int IntfShowCB( vlc_object_t *, const char *,
                        vlc_value_t, vlc_value_t, void *param )
 {
     qt_intf_t *p_intf = (qt_intf_t *)param;
-    p_intf->p_sys->p_mi->emitShow();
+    p_intf->p_mi->emitShow();
 
     return VLC_SUCCESS;
 }
@@ -789,7 +789,7 @@ static int IntfRaiseMainCB( vlc_object_t *, const char *,
                             vlc_value_t, vlc_value_t, void *param )
 {
     qt_intf_t *p_intf = (qt_intf_t *)param;
-    p_intf->p_sys->p_mi->emitRaise();
+    p_intf->p_mi->emitRaise();
 
     return VLC_SUCCESS;
 }
@@ -801,7 +801,7 @@ static int IntfBossCB( vlc_object_t *, const char *,
                        vlc_value_t, vlc_value_t, void *param )
 {
     qt_intf_t *p_intf = (qt_intf_t *)param;
-    p_intf->p_sys->p_mi->emitBoss();
+    p_intf->p_mi->emitBoss();
 
     return VLC_SUCCESS;
 }

modules/gui/qt/menus/menus.cpp
@@ -293,7 +293,7 @@ QMenu *VLCMenuBar::ToolsMenu( qt_intf_t *p_intf, QMenu *menu )
         "", &DialogsProvider::pluginDialog );
     menu->addSeparator();
 
-    if( !p_intf->p_sys->b_isDialogProvider )
+    if( !p_intf->b_isDialogProvider )
         addDPStaticEntry( menu, qtr( "Customi&ze Interface..." ),
             ":/menu/preferences.svg", &DialogsProvider::showToolbarEditorDialog);
 
@@ -313,7 +313,7 @@ QMenu *VLCMenuBar::ViewMenu( qt_intf_t *p_intf, QMenu *current, MainInterface *_
     QAction *action;
     QMenu *menu;
 
-    MainInterface *mi = _mi ? _mi : p_intf->p_sys->p_mi;
+    MainInterface *mi = _mi ? _mi : p_intf->p_mi;
     assert( mi );
 
     if( !current )
@@ -528,7 +528,7 @@ QMenu *VLCMenuBar::NavigMenu( qt_intf_t *p_intf, QMenu *menu )
     menu->addMenu( submenu );
     menu->addMenu( new CheckableListMenu( qtr("&Program") , THEMIM->getPrograms(), CheckableListMenu::GROUPED , menu) );
 
-    if (p_intf->p_sys->p_mi && p_intf->p_sys->p_mi->hasMediaLibrary() )
+    if (p_intf->p_mi && p_intf->p_mi->hasMediaLibrary() )
     {
         submenu = new QMenu( qfut( I_MENU_BOOKMARK ), menu );
         submenu->setTearOffEnabled( true );
@@ -783,7 +783,7 @@ QMenu* VLCMenuBar::PopupMenu( qt_intf_t *p_intf, bool show )
     input_item_t* p_input = THEMIM->getInput();
     QAction *action;
     bool b_isFullscreen = false;
-    MainInterface *mi = p_intf->p_sys->p_mi;
+    MainInterface *mi = p_intf->p_mi;
 
     PopupMenuPlaylistEntries( menu, p_intf );
     menu->addSeparator();
@@ -851,7 +851,7 @@ QMenu* VLCMenuBar::PopupMenu( qt_intf_t *p_intf, bool show )
         }
 
         /* In skins interface, append some items */
-        if( p_intf->p_sys->b_isDialogProvider )
+        if( p_intf->b_isDialogProvider )
         {
             vlc_object_t* p_object = vlc_object_parent(p_intf);
             submenu->setTitle( qtr( "Interface" ) );
@@ -881,7 +881,7 @@ QMenu* VLCMenuBar::PopupMenu( qt_intf_t *p_intf, bool show )
     }
 
     /* Static entries for ending, like open */
-    if( p_intf->p_sys->b_isDialogProvider )
+    if( p_intf->b_isDialogProvider )
     {
         QMenu *openmenu = FileMenu( p_intf, menu );
         openmenu->setTitle( qtr( "Open Media" ) );

modules/gui/qt/dialogs/bookmarks/bookmarks.cpp
@@ -64,7 +64,7 @@ BookmarksDialog::BookmarksDialog( qt_intf_t *_p_intf ):QVLCFrame( _p_intf )
 
     bookmarksList = new QTreeView( this );
     m_model = new MLBookmarkModel( vlc_ml_instance_get(_p_intf ),
-                                   _p_intf->p_sys->p_player,
+                                   _p_intf->p_player,
                                    bookmarksList );
     bookmarksList->setModel( m_model );
     bookmarksList->setRootIsDecorated( false );

modules/gui/qt/dialogs/dialogs_provider.cpp
@@ -251,7 +251,7 @@ const QEvent::Type DialogEvent::DialogEvent_Type =
 
 void DialogsProvider::prefsDialog()
 {
-    PrefsDialog *p = new PrefsDialog( (QWidget *)p_intf->p_sys->p_mi, p_intf );
+    PrefsDialog *p = new PrefsDialog( (QWidget *)p_intf->p_mi, p_intf );
     p->toggleVisible();
 }
 
@@ -334,7 +334,7 @@ void DialogsProvider::mediaInfoDialog( const PlaylistItem& pItem )
     if( p_input )
     {
         MediaInfoDialog * const mid = new MediaInfoDialog( p_intf, p_input );
-        mid->setParent( p_intf->p_sys->p_mi, Qt::Dialog );
+        mid->setParent( p_intf->p_mi, Qt::Dialog );
         mid->showTab( MediaInfoDialog::META_PANEL );
     }
 }
@@ -424,7 +424,7 @@ void DialogsProvider::openFileGenericDialog( intf_dialog_args_t *p_arg )
     {
         QString file = getSaveFileName( NULL,
                                         qfu( p_arg->psz_title ),
-                                        p_intf->p_sys->filepath, extensions );
+                                        p_intf->filepath, extensions );
         if( !file.isEmpty() )
         {
             p_arg->i_results = 1;
@@ -437,14 +437,14 @@ void DialogsProvider::openFileGenericDialog( intf_dialog_args_t *p_arg )
     else /* non-save mode */
     {
         QList<QUrl> urls = QFileDialog::getOpenFileUrls( NULL, qfu( p_arg->psz_title ),
-                                       p_intf->p_sys->filepath, extensions );
+                                       p_intf->filepath, extensions );
         p_arg->i_results = urls.count();
         p_arg->psz_results = (char **)vlc_alloc( p_arg->i_results, sizeof( char * ) );
         i = 0;
         foreach( const QUrl &uri, urls )
             p_arg->psz_results[i++] = strdup( uri.toEncoded().constData() );
         if( !urls.isEmpty() )
-            p_intf->p_sys->filepath =  urls.last();
+            p_intf->filepath =  urls.last();
     }
 
     /* Callback */
@@ -469,7 +469,7 @@ void DialogsProvider::openFileGenericDialog( intf_dialog_args_t *p_arg )
 
 void DialogsProvider::openDialog( int i_tab )
 {
-    OpenDialog::getInstance( p_intf->p_sys->p_mi , p_intf )->showTab( i_tab );
+    OpenDialog::getInstance( p_intf->p_mi , p_intf )->showTab( i_tab );
 }
 void DialogsProvider::openDialog()
 {
@@ -495,7 +495,7 @@ void DialogsProvider::openCaptureDialog()
 /* Same as the open one, but force the enqueue */
 void DialogsProvider::PLAppendDialog( int tab )
 {
-    OpenDialog::getInstance( p_intf->p_sys->p_mi, p_intf, false,
+    OpenDialog::getInstance( p_intf->p_mi, p_intf, false,
                              OPEN_AND_ENQUEUE )->showTab( tab );
 }
 
@@ -528,11 +528,11 @@ QStringList DialogsProvider::showSimpleOpen( const QString& help,
 
     QList<QUrl> urls = QFileDialog::getOpenFileUrls( NULL,
         help.isEmpty() ? qfut(I_OP_SEL_FILES ) : help,
-        path.isEmpty() ? p_intf->p_sys->filepath : path,
+        path.isEmpty() ? p_intf->filepath : path,
         fileTypes );
 
     if( !urls.isEmpty() )
-        p_intf->p_sys->filepath = urls.last();
+        p_intf->filepath = urls.last();
 
     QStringList res;
     foreach( const QUrl &url, urls )
@@ -600,12 +600,12 @@ QString DialogsProvider::getDirectoryDialog( qt_intf_t *p_intf )
 {
     const QStringList schemes = QStringList(QStringLiteral("file"));
     QUrl dirurl = QFileDialog::getExistingDirectoryUrl( NULL,
-            qfut( I_OP_DIR_WINTITLE ), p_intf->p_sys->filepath,
+            qfut( I_OP_DIR_WINTITLE ), p_intf->filepath,
             QFileDialog::ShowDirsOnly, schemes );
 
     if( dirurl.isEmpty() ) return QString();
 
-    p_intf->p_sys->filepath = dirurl;
+    p_intf->filepath = dirurl;
 
     QString dir = dirurl.toLocalFile();
     const char *scheme = "directory";
@@ -669,7 +669,7 @@ void DialogsProvider::savePlayingToPlaylist()
     QString selected;
     QString file = getSaveFileName( NULL,
                                     qtr( "Save playlist as..." ),
-                                    p_intf->p_sys->filepath, filters.join( ";;" ),
+                                    p_intf->filepath, filters.join( ";;" ),
                                     &selected );
     const char *psz_selected_module = NULL;
     const char *psz_last_playlist_ext = NULL;
@@ -706,11 +706,11 @@ void DialogsProvider::savePlayingToPlaylist()
 
     if ( psz_selected_module )
     {
-        vlc_playlist_Lock( p_intf->p_sys->p_playlist  );
-        vlc_playlist_Export( p_intf->p_sys->p_playlist,
+        vlc_playlist_Lock( p_intf->p_playlist  );
+        vlc_playlist_Export( p_intf->p_playlist,
                              qtu( toNativeSeparators( file ) ),
                              psz_selected_module );
-        vlc_playlist_Unlock( p_intf->p_sys->p_playlist  );
+        vlc_playlist_Unlock( p_intf->p_playlist  );
         getSettings()->setValue( "last-playlist-ext", psz_last_playlist_ext );
     }
 }
@@ -788,13 +788,13 @@ void DialogsProvider::streamingDialog(const QList<QUrl> &urls, bool b_stream )
 
 void DialogsProvider::openAndStreamingDialogs()
 {
-    OpenDialog::getInstance( p_intf->p_sys->p_mi, p_intf, false, OPEN_AND_STREAM )
+    OpenDialog::getInstance( p_intf->p_mi, p_intf, false, OPEN_AND_STREAM )
                                 ->showTab( OPEN_FILE_TAB );
 }
 
 void DialogsProvider::openAndTranscodingDialogs()
 {
-    OpenDialog::getInstance( p_intf->p_sys->p_mi , p_intf, false, OPEN_AND_SAVE )
+    OpenDialog::getInstance( p_intf->p_mi , p_intf, false, OPEN_AND_SAVE )
                                 ->showTab( OPEN_FILE_TAB );
 }
 

modules/gui/qt/dialogs/epg/epg.cpp
@@ -176,8 +176,8 @@ void EpgDialog::updateInfos()
 {
     input_item_t *media = NULL;
     {
-        vlc_player_locker lock{ p_intf->p_sys->p_player };
-        media = vlc_player_HoldCurrentMedia( p_intf->p_sys->p_player ); /* w/hold */
+        vlc_player_locker lock{ p_intf->p_player };
+        media = vlc_player_HoldCurrentMedia( p_intf->p_player ); /* w/hold */
     }
 
     if( media )

modules/gui/qt/dialogs/errors/errors.cpp
@@ -34,7 +34,7 @@
 #include <QPushButton>
 
 ErrorsDialog::ErrorsDialog( qt_intf_t *_p_intf )
-             : QVLCDialog( (QWidget*)_p_intf->p_sys->p_mi, _p_intf )
+             : QVLCDialog( (QWidget*)_p_intf->p_mi, _p_intf )
 {
     setWindowTitle( qtr( "Errors" ) );
     setWindowRole( "vlc-errors" );

modules/gui/qt/dialogs/extended/extended.cpp
@@ -38,7 +38,7 @@
 #include <vlc_modules.h>
 
 ExtendedDialog::ExtendedDialog( qt_intf_t *_p_intf )
-               : QVLCDialog( (QWidget*)_p_intf->p_sys->p_mi, _p_intf )
+               : QVLCDialog( (QWidget*)_p_intf->p_mi, _p_intf )
 {
 #ifdef __APPLE__
     setWindowFlags( Qt::Drawer );
@@ -137,7 +137,7 @@ ExtendedDialog::ExtendedDialog( qt_intf_t *_p_intf )
     {
         resize( QSize( 400, 280 ) );
 
-        MainInterface *p_mi = p_intf->p_sys->p_mi;
+        MainInterface *p_mi = p_intf->p_mi;
         if( p_mi && p_mi->x() > 50 )
             move( ( p_mi->x() - frameGeometry().width() - 10 ), p_mi->y() );
         else

commit b9dfd6038a2ef33b1eca5ef8497dd9291463ac89

qt: fix regressions due to new intf wrapping
Since the actual interface object is now contained in the main Qt
Interface. This fixes regressions introduced by
537d41a468f6f8d155b54214e0214e5576d52e68 which were breaking the vlc var
fetching.

modules/gui/qt/dialogs/extended/extended_panels.cpp
@@ -1371,7 +1371,7 @@ void SyncWidget::setValue( double d )
 SyncControls::SyncControls( qt_intf_t *_p_intf, QWidget *_parent )
     : QWidget( _parent )
     , p_intf( _p_intf )
-    , m_SubsDelayCfgFactor(p_intf, SUBSDELAY_CFG_FACTOR)
+    , m_SubsDelayCfgFactor(VLC_OBJECT(p_intf->intf), SUBSDELAY_CFG_FACTOR)
 {
     QGroupBox *AVBox, *subsBox;
     QToolButton *updateButton;

modules/gui/qt/maininterface/main_interface.cpp
@@ -134,7 +134,7 @@ MainInterface::MainInterface(qt_intf_t *_p_intf , QWidget* parent, Qt::WindowFla
     updateIntfScaleFactor();
 
     /* Get the available interfaces */
-    m_extraInterfaces = new VLCVarChoiceModel(p_intf, "intf-add", this);
+    m_extraInterfaces = new VLCVarChoiceModel(VLC_OBJECT(p_intf->intf), "intf-add", this);
 
     vlc_medialibrary_t* ml = vlc_ml_instance_get( p_intf );
     b_hasMedialibrary = (ml != NULL);

modules/gui/qt/menus/menus.cpp
@@ -390,7 +390,7 @@ QMenu *VLCMenuBar::ViewMenu( qt_intf_t *p_intf, QMenu *current, MainInterface *_
 QMenu *VLCMenuBar::InterfacesMenu( qt_intf_t *p_intf, QMenu *current )
 {
     assert(current);
-    VLCVarChoiceModel* model = new VLCVarChoiceModel(VLC_OBJECT(p_intf), "intf-add", current);
+    VLCVarChoiceModel* model = new VLCVarChoiceModel(VLC_OBJECT(p_intf->intf), "intf-add", current);
     CheckableListMenu* submenu = new CheckableListMenu(qtr("Interfaces"), model, CheckableListMenu::UNGROUPED, current);
     current->addMenu(submenu);
     return current;
@@ -853,7 +853,7 @@ QMenu* VLCMenuBar::PopupMenu( qt_intf_t *p_intf, bool show )
         /* In skins interface, append some items */
         if( p_intf->b_isDialogProvider )
         {
-            vlc_object_t* p_object = vlc_object_parent(p_intf);
+            vlc_object_t* p_object = vlc_object_parent(p_intf->intf);
             submenu->setTitle( qtr( "Interface" ) );
 
             /* Open skin dialog box */