febdeca0b46551500563cf3f2b058f81c1814079
Alexandre Janniaux <ajanni@videolabs.io>
2021-06-30 11:08:05 +0200

    doc: QtGL: conditionnally set precision qualifier
    Precision is available but no-op on GLSL 130 (see [3], section 4.5.2)
    and mandatory for OpenGL ES. It was first reserved on GLSL 120 and
    didn't exist in GLSL <= 110 (= OpenGL 2.0). Since it's a no-op, the
    easiest solution is to never use it for OpenGL code.
    
    This description is also copied into the sample to explain to the users
    that they might have difference in behaviour depending on whether they
    are using OpenGL or OpenGL ES2, and depending on the version of OpenGL
    used.
    
    The precision qualifier was added because Qt was linked from the
    contribs, which was linked with -opengl angle and thus was actually
    using OpenGL ES2, which needs the precision qualifier.
    
    Resources used:
    [1]: https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.1.10.pdf
    [2]: https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.1.20.pdf
    [3]: https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.1.30.pdf

--- doc/libvlc/QtGL/qtvlcwidget.cpp
+++ doc/libvlc/QtGL/qtvlcwidget.cpp
@@ -284,7 +284,19 @@ static const char *vertexShaderSource =
     "}\n";
 
 static const char *fragmentShaderSource =
+    /* Precision is available but no-op on GLSL 130 (see [3], section 4.5.2)
+     * and mandatory for OpenGL ES. It was first reserved on GLSL 120 and
+     * didn't exist in GLSL <= 110 (= OpenGL 2.0). Since it's a no-op, the
+     * easiest solution is to never use it for OpenGL code.
+     *
+     * [1]: https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.1.10.pdf
+     * [2]: https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.1.20.pdf
+     * [3]: https://www.khronos.org/registry/OpenGL/specs/gl/GLSLangSpec.1.30.pdf
+     */
+    "#ifdef GL_ES\n"
     "precision highp float;\n"
+    "#endif\n"
+
     "\n"
     "uniform sampler2D texture;\n"
     "\n"
