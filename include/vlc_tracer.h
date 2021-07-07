ee52a2e1828fa3bd922a3792bbe14ecf358a5509
Nicolas LeQuec <nicolas.lequec@gmail.com>
2021-07-07 13:26:38 +0200

    core: add tracing API
    Tracing system is independant from the logging system and load modules with a "tracer" capability.
    The tracer module is loaded at the initialisation of a libvlc instance.

---
+++ include/vlc_tracer.h
@@ -0,0 +1,151 @@
+/*****************************************************************************
+ * vlc_tracer.h: tracing interface
+ * This library provides basic functions for threads to interact with user
+ * interface, such as trace output.
+ *****************************************************************************
+ * Copyright (C) 2021 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef VLC_TRACES_H
+#define VLC_TRACES_H
+
+#include <stdarg.h>
+
+/**
+ * \defgroup traces Tracing
+ * \ingroup os
+ * \brief Message traces
+ *
+ * Functions for modules to emit traces.
+ *
+ * @{
+ * \file
+ * Tracing functions
+ */
+
+/**
+ * Trace message values
+ */
+enum vlc_tracer_value
+{
+    VLC_TRACER_INT,
+    VLC_TRACER_STRING
+};
+
+typedef union
+{
+    int64_t integer;
+    const char *string;
+} vlc_tracer_value_t;
+
+/**
+ * Trace message
+ */
+struct vlc_tracer_entry
+{
+    const char *key;            /**< Key to identify the value */
+    vlc_tracer_value_t value;   /**< Trace value */
+    enum vlc_tracer_value type; /**< Type of the value */
+};
+
+struct vlc_tracer;
+
+/**
+ * Trace logging callback signature.
+ *
+ * va-args can only be \ref vlc_tracer_entry and the va-args list
+ * should be ended by a \ref vlc_tracer_entry with a NULL key.
+ * \param data data pointer as provided to vlc_tracer_Trace().
+ */
+typedef void (*vlc_trace_cb) (void *data, va_list entries);
+
+struct vlc_tracer_operations
+{
+    vlc_trace_cb trace;
+    void (*destroy)(void *data);
+};
+
+/**
+ * Emit traces
+ *
+ * va-args are a list of key / value parameters.
+ * Key must be a not NULL string.
+ * Value has to be defined with one of the type defined
+ * in the \ref vlc_tracer_entry union.
+ * \param tracer tracer emitting the traces
+ */
+VLC_API void vlc_tracer_Trace(struct vlc_tracer *tracer, ...);
+
+/**
+ * \defgroup tracer Tracer
+ * \brief Tracing back-end.
+ *
+ * @{
+ */
+
+static inline struct vlc_tracer_entry vlc_tracer_entry_FromTick(const char *key, vlc_tick_t value)
+{
+    vlc_tracer_value_t tracer_value;
+    tracer_value.integer = value;
+    struct vlc_tracer_entry trace = { key, tracer_value, VLC_TRACER_INT };
+    return trace;
+}
+
+static inline struct vlc_tracer_entry vlc_tracer_entry_FromString(const char *key, const char *value)
+{
+    vlc_tracer_value_t tracer_value;
+    tracer_value.string = value;
+    struct vlc_tracer_entry trace = { key, tracer_value, VLC_TRACER_STRING };
+    return trace;
+}
+
+#ifndef __cplusplus
+#define VLC_TRACE_END \
+        vlc_tracer_entry_FromString(NULL, NULL)
+
+#define VLC_TRACE(key, value) \
+        _Generic((value), \
+        vlc_tick_t: vlc_tracer_entry_FromTick, \
+        char *: vlc_tracer_entry_FromString, \
+        const char *: vlc_tracer_entry_FromString) (key, value)
+#else
+#define VLC_TRACE_END \
+        vlc_tracer_entry_FromString(nullptr, nullptr)
+
+static inline struct vlc_tracer_entry VLC_TRACE(const char *key, vlc_tick_t value)
+{
+    return vlc_tracer_entry_FromTick(key, value);
+}
+
+static inline struct vlc_tracer_entry VLC_TRACE(const char *key, char *value)
+{
+    return vlc_tracer_entry_FromString(key, value);
+}
+
+static inline struct vlc_tracer_entry VLC_TRACE(const char *key, const char *value)
+{
+    return vlc_tracer_entry_FromString(key, value);
+}
+#endif
+
+/**
+ * @}
+ */
+/**
+ * @}
+ */
+#endif
---
+++ src/misc/tracer.c
@@ -0,0 +1,113 @@
+/*****************************************************************************
+ * tracer.c: tracing interface
+ * This library provides an interface to the traces to be used by other
+ * modules. See vlc_config.h for output configuration.
+ *****************************************************************************
+ * Copyright (C) 2021 VLC authors and VideoLAN
+ * 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <assert.h>
+
+#include <vlc_common.h>
+#include <vlc_modules.h>
+#include <vlc_tracer.h>
+#include "../libvlc.h"
+
+struct vlc_tracer {
+    const struct vlc_tracer_operations *ops;
+};
+
+/**
+ * Module-based message trace.
+ */
+struct vlc_tracer_module {
+    struct vlc_object_t obj;
+    struct vlc_tracer tracer;
+    void *opaque;
+};
+
+void vlc_tracer_Trace(struct vlc_tracer *tracer, ...)
+{
+    assert(tracer->ops->trace != NULL);
+    struct vlc_tracer_module *module =
+            container_of(tracer, struct vlc_tracer_module, tracer);
+
+    /* Pass message to the callback */
+    va_list entries;
+    va_start(entries, tracer);
+    tracer->ops->trace(module->opaque, entries);
+    va_end(entries);
+}
+
+static int vlc_tracer_load(void *func, bool forced, va_list ap)
+{
+    const struct vlc_tracer_operations *(*activate)(vlc_object_t *,
+                                                    void **) = func;
+    struct vlc_tracer_module *module = va_arg(ap, struct vlc_tracer_module *);
+
+    (void) forced;
+    module->tracer.ops = activate(VLC_OBJECT(module), &module->opaque);
+    return (module->tracer.ops != NULL) ? VLC_SUCCESS : VLC_EGENERIC;
+}
+
+static struct vlc_tracer *vlc_TraceModuleCreate(vlc_object_t *parent)
+{
+    struct vlc_tracer_module *module;
+
+    module = vlc_custom_create(parent, sizeof (*module), "tracer");
+    if (unlikely(module == NULL))
+        return NULL;
+
+    if (vlc_module_load(VLC_OBJECT(module), "tracer", NULL, false,
+                        vlc_tracer_load, module) == NULL) {
+        vlc_object_delete(VLC_OBJECT(module));
+        return NULL;
+    }
+
+    return &module->tracer;
+}
+
+/**
+ * Initializes the messages tracing system */
+void vlc_tracer_Init(libvlc_int_t *vlc)
+{
+    struct vlc_tracer *tracer = vlc_TraceModuleCreate(VLC_OBJECT(vlc));
+    libvlc_priv_t *vlc_priv = libvlc_priv(vlc);
+    vlc_priv->tracer = tracer;
+}
+
+void vlc_tracer_Destroy(libvlc_int_t *vlc)
+{
+    libvlc_priv_t *vlc_priv = libvlc_priv(vlc);
+
+    if (vlc_priv->tracer != NULL)
+    {
+        struct vlc_tracer_module *module =
+            container_of(vlc_priv->tracer, struct vlc_tracer_module, tracer);
+
+        if (module->tracer.ops->destroy != NULL)
+            module->tracer.ops->destroy(module->opaque);
+
+        vlc_object_delete(VLC_OBJECT(module));
+    }
+}
