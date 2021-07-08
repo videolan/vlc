5fc6a7ca5b59b8d044a6305fe6810a4f7f426e42
Steve Lhomme <robux4@ycbcr.xyz>
2021-07-08 14:40:32 +0200

    chroma: copy: allow use from C++ code

--- modules/video_chroma/copy.h
+++ modules/video_chroma/copy.h
@@ -25,6 +25,10 @@
 
 #include <assert.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct {
 # ifdef CAN_COMPILE_SSE2
     uint8_t *buffer;
@@ -43,35 +47,35 @@ void CopyPacked(picture_t *dst, const uint8_t *src,
                 const copy_cache_t *cache);
 
 /* Copy planes from NV12/NV21 to NV12/NV21 */
-void Copy420_SP_to_SP(picture_t *dst, const uint8_t *src[static 2],
-                      const size_t src_pitch[static 2], unsigned height,
+void Copy420_SP_to_SP(picture_t *dst, const uint8_t *src[ARRAY_STATIC_SIZE 2],
+                      const size_t src_pitch[ARRAY_STATIC_SIZE 2], unsigned height,
                       const copy_cache_t *cache);
 
 /* Copy planes from I420/YV12 to I420/YV12 */
-void Copy420_P_to_P(picture_t *dst, const uint8_t *src[static 3],
-                    const size_t src_pitch[static 3], unsigned height,
+void Copy420_P_to_P(picture_t *dst, const uint8_t *src[ARRAY_STATIC_SIZE 3],
+                    const size_t src_pitch[ARRAY_STATIC_SIZE 3], unsigned height,
                     const copy_cache_t *cache);
 
 /* Copy planes from I420/YV12 to NV12/NV21 */
-void Copy420_P_to_SP(picture_t *dst, const uint8_t *src[static 3],
-                     const size_t src_pitch[static 3], unsigned height,
+void Copy420_P_to_SP(picture_t *dst, const uint8_t *src[ARRAY_STATIC_SIZE 3],
+                     const size_t src_pitch[ARRAY_STATIC_SIZE 3], unsigned height,
                      const copy_cache_t *cache);
 
 /* Copy planes from NV12/NV21 to I420/YV12 */
-void Copy420_SP_to_P(picture_t *dst, const uint8_t *src[static 2],
-                     const size_t src_pitch[static 2], unsigned height,
+void Copy420_SP_to_P(picture_t *dst, const uint8_t *src[ARRAY_STATIC_SIZE 2],
+                     const size_t src_pitch[ARRAY_STATIC_SIZE 2], unsigned height,
                      const copy_cache_t *cache);
 
 /* Copy planes from I420_10 to P010. A positive bitshift value will shift bits
  * to the right, a negative value will shift to the left. */
-void Copy420_16_P_to_SP(picture_t *dst, const uint8_t *src[static 3],
-                     const size_t src_pitch[static 3], unsigned height,
+void Copy420_16_P_to_SP(picture_t *dst, const uint8_t *src[ARRAY_STATIC_SIZE 3],
+                     const size_t src_pitch[ARRAY_STATIC_SIZE 3], unsigned height,
                      int bitshift, const copy_cache_t *cache);
 
 /* Copy planes from P010 to I420_10. A positive bitshift value will shift bits
  * to the right, a negative value will shift to the left. */
-void Copy420_16_SP_to_P(picture_t *dst, const uint8_t *src[static 2],
-                        const size_t src_pitch[static 2], unsigned height,
+void Copy420_16_SP_to_P(picture_t *dst, const uint8_t *src[ARRAY_STATIC_SIZE 2],
+                        const size_t src_pitch[ARRAY_STATIC_SIZE 2], unsigned height,
                         int bitshift, const copy_cache_t *cache);
 
 /**
@@ -84,5 +88,9 @@ void Copy420_16_SP_to_P(picture_t *dst, const uint8_t *src[static 2],
  * pitch is the internal line pitch for the buffer
  */
 int picture_UpdatePlanes(picture_t *picture, uint8_t *data, unsigned pitch);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
