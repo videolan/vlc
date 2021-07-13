51c1e3e7167c0a64e4d47f785d30c6ecab4bc6b6
Alexandre Janniaux <ajanni@videolabs.io>
2021-07-13 11:57:10 +0200

    input: decoder: add frame displayed reporting

--- src/input/decoder.h
+++ src/input/decoder.h
@@ -43,6 +43,9 @@ struct vlc_input_decoder_callbacks {
                                void *userdata);
     void (*on_new_audio_stats)(vlc_input_decoder_t *decoder, unsigned decoded,
                                unsigned lost, unsigned played, void *userdata);
+    void (*on_vout_frame_displayed)(vlc_input_decoder_t *decoder,
+                                    vout_thread_t *vout, vlc_tick_t pts,
+                                    void *userdata);
 
     /* requests */
     int (*get_attachments)(vlc_input_decoder_t *decoder,
