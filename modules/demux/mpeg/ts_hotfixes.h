071519bf5420e59ec043efe9c3a5012911eee06a
Shin, Jeong-Ho <deckard@sk.com>
2021-07-16 01:04:29 +0900

    Increase MIN_PAT_INTERVAL to 3 seconds.
    This is a workaround for
    https://skbdevice.atlassian.net/browse/BTVAV-68

--- modules/demux/mpeg/ts_hotfixes.h
+++ modules/demux/mpeg/ts_hotfixes.h
@@ -19,7 +19,7 @@
 #ifndef VLC_TS_HOTFIXES_H
 #define VLC_TS_HOTFIXES_H
 
-#define MIN_PAT_INTERVAL VLC_TICK_FROM_SEC(1) // DVB is 500ms
+#define MIN_PAT_INTERVAL VLC_TICK_FROM_SEC(3) // DVB is 500ms
 
 void ProbePES( demux_t *p_demux, ts_pid_t *pid, const uint8_t *p_pesstart, size_t i_data, bool b_adaptfield );
 void MissingPATPMTFixup( demux_t *p_demux );
