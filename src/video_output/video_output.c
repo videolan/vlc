videolan/vlc

commit a5d85a5f248b12a2ef78041cf2dfa14a6106d66d

video_output: count the time to do the prepare to determine if a frame is late
The system_pts is the date we should do the swap (vd->display) for the picture.
But between now and the time we do the swap, there is also the prepare
(+ static filtering) that will happen. We are late if now+that duration is
after the date we should do the swap.

src/video_output/video_output.c
@@ -1094,7 +1094,9 @@ static picture_t *ThreadDisplayPreparePicture(vout_thread_sys_t *vout, bool reus
                 }
                 else if (is_late_dropped && !decoded->b_force)
                 {
-                    vlc_tick_t late = system_now - system_pts;
+                    const vlc_tick_t prepare_decoded_duration = vout_chrono_GetHigh(&sys->render) +
+                                                                VOUT_MWAIT_TOLERANCE;
+                    vlc_tick_t late = system_now + prepare_decoded_duration - system_pts;
 
                     vlc_tick_t late_threshold;
                     if (decoded->format.i_frame_rate && decoded->format.i_frame_rate_base) {

commit 832b85190ea234c776a8b2f3023701d17b5f1a82

video_output: remove wait misfire hazard margin
The time taken by the prepare is compound of measurements taken in
actual preparation steps. It's made of the time needed to execute the
static filters, the interactive filters, and the time needed for the
display to prepare.

VOUT_MWAIT_TOLERANCE is the margin of error accepted when the scheduler
wakes up the vout_control_Pop deadline late compared to the deadline we
choosed, or wakes up the vlc_clock_Wait late. It's supposed to move the
deadline back wherever relevant to display a frame, to wait a little
less that in the ideal instantaneous or real time case.

It doesn't represent "time spent" so there's no need to account it in
the duration of the prepare as a discriminator for late frames. In
addition, it's hardcoded time not dependant on whether the media or the
system running the video output and accounts for 1/4 of the time budget
for rendering a frame with 60fps.

Regression from a5d85a5f248b12a2ef78041cf2dfa14a6106d66d.

src/video_output/video_output.c
@@ -955,7 +955,6 @@ static bool IsPictureLate(vout_thread_sys_t *vout, picture_t *decoded,
     vout_thread_sys_t *sys = vout;
 
     const vlc_tick_t prepare_decoded_duration = vout_chrono_GetHigh(&sys->render) +
-                                                VOUT_MWAIT_TOLERANCE +
                                                 vout_chrono_GetHigh(&sys->static_filter);
     vlc_tick_t late = system_now + prepare_decoded_duration - system_pts;
 