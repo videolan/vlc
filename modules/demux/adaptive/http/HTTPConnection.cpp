a7e5e0a900771ae460bbd1dfa9dd1ec5cd8bd408
Francois Cartegnie <fcvlcdev@free.fr>
2021-08-19 13:19:27 +0200

    demux: adaptive: reset keepalive for connection reuse

--- modules/demux/adaptive/http/HTTPConnection.cpp
+++ modules/demux/adaptive/http/HTTPConnection.cpp
@@ -133,6 +133,7 @@ void HTTPConnection::disconnect()
     queryOk = false;
     bytesRead = 0;
     contentLength = 0;
+    connectionClose = false;
     chunked = false;
     chunkLength = 0;
     bytesRange = BytesRange();
