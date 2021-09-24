videolan/vlc

commit ec78dfa64e0c3942fa8f82fa9ebfa26de8acdbf9

qt: menus: properly check player bound renderer
Use the player API instead of an opaque arbitrary vlc variable.

modules/gui/qt/menus/custom_menus.cpp
@@ -74,11 +74,8 @@ RendererMenu::RendererMenu( QMenu *parent, qt_intf_t *p_intf_ )
     addAction( action );
     group->addAction(action);
 
-    char *psz_renderer = var_InheritString( p_intf->p_player, "sout" );
-    if ( psz_renderer == NULL )
+    if ( vlc_player_GetRenderer( p_intf->p_player ) == nullptr )
         action->setChecked( true );
-    else
-        free( psz_renderer );
 
     addSeparator();
 

commit b6407a66893305f9442f5df29f0283448b38e0a8

qt: custom-menus: add lock guards to player accessor
Fix a regression introduced by ec78dfa64e0c3942fa8f82fa9ebfa26de8acdbf9
where the lock was forgotten.

modules/gui/qt/menus/custom_menus.cpp
@@ -74,8 +74,10 @@ RendererMenu::RendererMenu( QMenu *parent, qt_intf_t *p_intf_ )
     addAction( action );
     group->addAction(action);
 
+    vlc_player_Lock( p_intf_->p_player );
     if ( vlc_player_GetRenderer( p_intf->p_player ) == nullptr )
         action->setChecked( true );
+    vlc_player_Unlock( p_intf_->p_player );
 
     addSeparator();
 