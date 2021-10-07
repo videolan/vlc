44c2aa7bcb0804880ecd499393034b3185082b8a
Alexandre Janniaux <ajanni@videolabs.io>
2021-10-07 08:56:01 +0200

    picture_pool: fix uninitialized warnings
    In the case count=0, the loop is not processed and the picture array's
    single element is not initialized, leading to a warning. We don't use
    count=0 anyway so remove the case.
    
    Fix the warnings (<unknown> is the VLA):
    
    ../../src/misc/picture_pool.c: In function ‘picture_pool_NewFromFormat’:
    ../../src/misc/picture_pool.c:140:28: warning: ‘<unknown>’ may be used uninitialized [-Wmaybe-uninitialized]
      140 |     picture_pool_t *pool = picture_pool_New(count, picture);
          |                            ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ../../src/misc/picture_pool.c:102:17: note: by argument 2 of type ‘picture_t * const*’ to ‘picture_pool_New’ declared here
      102 | picture_pool_t *picture_pool_New(unsigned count, picture_t *const *tab)
          |                 ^~~~~~~~~~~~~~~~

--- src/misc/picture_pool.c
+++ src/misc/picture_pool.c
@@ -128,7 +128,10 @@ picture_pool_t *picture_pool_New(unsigned count, picture_t *const *tab)
 picture_pool_t *picture_pool_NewFromFormat(const video_format_t *fmt,
                                            unsigned count)
 {
-    picture_t *picture[count ? count : 1];
+    if (count == 0)
+        vlc_assert_unreachable();
+
+    picture_t *picture[count];
     unsigned i;
 
     for (i = 0; i < count; i++) {
@@ -151,7 +154,10 @@ error:
 
 picture_pool_t *picture_pool_Reserve(picture_pool_t *master, unsigned count)
 {
-    picture_t *picture[count ? count : 1];
+    if (count == 0)
+        vlc_assert_unreachable();
+
+    picture_t *picture[count];
     unsigned i;
 
     for (i = 0; i < count; i++) {
