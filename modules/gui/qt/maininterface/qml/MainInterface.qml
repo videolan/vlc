videolan/vlc

commit e2c81a8afa8f80fb9db8bdf2a2db775e6cf49fb9

qml/CSDMouseStealer: Avoid hover and click when maximized or fullscreen
fix #26161

modules/gui/qt/maininterface/qml/MainInterface.qml
@@ -19,6 +19,8 @@
 import QtQuick 2.11
 import QtQuick.Layouts 1.11
 import QtQuick.Controls 2.4
+import QtQuick.Window 2.15
+
 import org.videolan.vlc 0.1
 
 import "qrc:///widgets/" as Widgets
@@ -175,7 +177,11 @@ Rectangle {
     }
 
     Loader {
-        active: mainInterface.clientSideDecoration
+        active: (mainInterface.clientSideDecoration
+                 &&
+                 // NOTE: We don't want to steal the mouse when we are maximized or in fullscreen
+                 !((topWindow.visibility & Window.Maximized) || mainInterface.interfaceFullScreen))
+
         source: "qrc:///widgets/CSDMouseStealer.qml"
     }
 }

modules/gui/qt/widgets/qml/CSDMouseStealer.qml
@@ -57,8 +57,7 @@ Item {
             width: modelData.width
             height: modelData.height
 
-            // NOTE: We don't want to steal the mouse when we are maximized or in fullscreen.
-            hoverEnabled: (!(topWindow.visibility & Window.Maximized) && !(mainInterface.interfaceFullScreen))
+            hoverEnabled: true
         }
     }
 }

commit a8886ea4a5bb44876e9c69d44e6ff4c9ee4e632f

qml: fix import version
Version 2.15 of QtQuick.Window is not available in Qt 5.11

Fix regression introduced in e2c81a8afa8

modules/gui/qt/maininterface/qml/MainInterface.qml
@@ -19,7 +19,7 @@
 import QtQuick 2.11
 import QtQuick.Layouts 1.11
 import QtQuick.Controls 2.4
-import QtQuick.Window 2.15
+import QtQuick.Window 2.11
 
 import org.videolan.vlc 0.1
 