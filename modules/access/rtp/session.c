videolan/vlc

commit a8613e9b92db9a29b87ca81ef2ec12d8ec1f345a

rtp: optimise negation out
Perform the misordering comparison in unsigned space.

modules/access/rtp/session.c
@@ -331,8 +331,8 @@ rtp_queue (demux_t *demux, rtp_session_t *session, block_t *block)
         int16_t s;
     } delta_seq = { .u = seq - src->max_seq };
 
-    if ((delta_seq.s > 0) ? (delta_seq.s > p_sys->max_dropout)
-                          : (-delta_seq.s > p_sys->max_misorder))
+    if ((delta_seq.s > 0) ? (delta_seq.u > p_sys->max_dropout)
+                          : (delta_seq.u < p_sys->max_misorder))
     {
         msg_Dbg (demux, "sequence discontinuity"
                  " (got: %"PRIu16", expected: %"PRIu16")", seq, src->max_seq);

modules/access/rtp/rtp.c
@@ -301,7 +301,7 @@ static int OpenSDP(vlc_object_t *obj)
     sys->max_src = var_InheritInteger(obj, "rtp-max-src");
     sys->timeout = vlc_tick_from_sec(var_InheritInteger(obj, "rtp-timeout"));
     sys->max_dropout  = var_InheritInteger(obj, "rtp-max-dropout");
-    sys->max_misorder = var_InheritInteger(obj, "rtp-max-misorder");
+    sys->max_misorder = -var_InheritInteger(obj, "rtp-max-misorder");
     sys->autodetect = true;
 
     demux->pf_demux = NULL;
@@ -443,7 +443,7 @@ static int OpenURL(vlc_object_t *obj)
     p_sys->max_src      = var_CreateGetInteger (obj, "rtp-max-src");
     p_sys->timeout      = vlc_tick_from_sec( var_CreateGetInteger (obj, "rtp-timeout") );
     p_sys->max_dropout  = var_CreateGetInteger (obj, "rtp-max-dropout");
-    p_sys->max_misorder = var_CreateGetInteger (obj, "rtp-max-misorder");
+    p_sys->max_misorder = -var_CreateGetInteger (obj, "rtp-max-misorder");
     p_sys->autodetect   = true;
 
     demux->pf_demux   = NULL;

commit 0f8094155ba71df8a243494b8127483214c4f495

rtp: next sequence packet is in order
max_seq is the *next* expected sequence number, not the last seen
sequence number. Consequently, a perfectly ordered packet will yield a
delta_seq difference of 0. It must be compared against the maximum
dropout, not the minimum negated misorder.

This fixes a regression from a8613e9b92db9a29b87ca81ef2ec12d8ec1f345a.

modules/access/rtp/session.c
@@ -306,8 +306,8 @@ rtp_queue (demux_t *demux, rtp_session_t *session, block_t *block)
         int16_t s;
     } delta_seq = { .u = seq - src->max_seq };
 
-    if ((delta_seq.s > 0) ? (delta_seq.u > p_sys->max_dropout)
-                          : (delta_seq.u < p_sys->max_misorder))
+    if ((delta_seq.s >= 0) ? (delta_seq.u > p_sys->max_dropout)
+                           : (delta_seq.u < p_sys->max_misorder))
     {
         msg_Dbg (demux, "sequence discontinuity"
                  " (got: %"PRIu16", expected: %"PRIu16")", seq, src->max_seq);