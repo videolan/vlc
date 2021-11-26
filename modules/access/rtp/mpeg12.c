30d50a4eb03e8016b854a01c11de22f5a8410def
Rémi Denis-Courmont <remi@remlab.net>
2021-11-26 22:56:13 +0200

    rtp/mpeg12: add basic support for video/MPV

--- modules/access/rtp/mpeg12.c
+++ modules/access/rtp/mpeg12.c
@@ -190,6 +190,131 @@ static const struct vlc_rtp_pt_operations rtp_mpa_ops = {
     NULL, rtp_mpa_init, rtp_mpa_destroy, rtp_mpa_decode,
 };
 
+/* video/MPV: MPEG-1/2 Video ES */
+struct rtp_mpv {
+    block_t *frags;
+    block_t **frag_end;
+    struct vlc_rtp_es *es;
+};
+
+static void *rtp_mpv_init(struct vlc_rtp_pt *pt)
+{
+    struct rtp_mpv *sys = malloc(sizeof (*sys));
+    if (unlikely(sys == NULL))
+        return NULL;
+
+    sys->frags = NULL;
+    sys->frag_end = &sys->frags;
+
+    es_format_t fmt;
+
+    es_format_Init(&fmt, VIDEO_ES, VLC_CODEC_MPGV);
+    sys->es = vlc_rtp_pt_request_es(pt, &fmt);
+    return sys;
+}
+
+static void rtp_mpv_send(struct rtp_mpv *sys)
+{
+    block_t *frags = sys->frags;
+    block_t *frame = block_ChainGather(frags);
+
+    if (likely(frame != NULL))
+        vlc_rtp_es_send(sys->es, frame);
+    else
+        block_ChainRelease(frags);
+
+    sys->frags = NULL;
+    sys->frag_end = &sys->frags;
+}
+
+static void rtp_mpv_destroy(struct vlc_rtp_pt *pt, void *data)
+{
+    struct rtp_mpv *sys = data;
+
+    if (unlikely(sys == NULL))
+        return;
+
+    if (sys->frags != NULL)
+        rtp_mpv_send(sys);
+    vlc_rtp_es_destroy(sys->es);
+    free(sys);
+    (void) pt;
+}
+
+/* RFC2250 §3.4 */
+#define MPV_TWO_BIT  0x04000000u
+#define MPV_TR_MASK  0x03ff0000u
+#define MPV_AN_BIT   0x00008000u
+#define MPV_N_BIT    0x00004000u
+#define MPV_S_BIT    0x00002000u
+#define MPV_BS_BIT   0x00001000u
+#define MPV_ES_BIT   0x00000800u
+#define MPV_PT_MASK  0x00000700u
+#define MPV_FBV_BIT  0x00000080u
+#define MPV_FBC_MASK 0x00000070u
+#define MPV_FFV_BIT  0x00000008u
+#define MPV_FFC_MASK 0x00000007u
+
+static const unsigned short pic_types[8] = {
+    BLOCK_FLAG_CORRUPTED /* forbidden */, BLOCK_FLAG_TYPE_I,
+    BLOCK_FLAG_TYPE_P, BLOCK_FLAG_TYPE_B,
+    BLOCK_FLAG_TYPE_I /* actually DC */, 0, 0, 0
+};
+
+#define EXTRACT(x,mask) (((x) & (mask)) >> vlc_ctz(mask))
+
+static void rtp_mpv_decode(struct vlc_rtp_pt *pt, void *data, block_t *block,
+                           const struct vlc_rtp_pktinfo *restrict info)
+{
+    struct vlc_logger *log = pt->opaque;
+    struct rtp_mpv *sys = data;
+
+    if (unlikely(sys == NULL))
+        goto drop;
+    if (block->i_buffer < 4) {
+        vlc_warning(log, "malformatted packet (%zu bytes)", block->i_buffer);
+        goto drop;
+    }
+
+    uint_fast32_t header = GetDWBE(block->p_buffer);
+    size_t hlen = 4;
+
+    if (header & MPV_TWO_BIT) {
+        if (block->i_buffer < 8) {
+            vlc_warning(log, "malformatted packet (%zu bytes)",
+                        block->i_buffer);
+            goto drop;
+        }
+
+        /* TODO: extract frame structure from header extension */
+        hlen += 4;
+    }
+    block->p_buffer += hlen;
+    block->i_buffer -= hlen;
+
+#if 0 /* We could set this flag, but it would not be used for anything */
+    if (header & MPV_S_BIT)
+        block->i_flags |= BLOCK_FLAG_HEADER;
+#endif
+
+    block->i_flags |= pic_types[EXTRACT(header, MPV_PT_MASK)];
+    *sys->frag_end = block;
+    sys->frag_end = &block->p_next;
+
+    if (info->m)
+        rtp_mpv_send(sys);
+
+    return;
+
+drop:
+    block_Release(block);
+}
+
+static const struct vlc_rtp_pt_operations rtp_mpv_ops = {
+    NULL, rtp_mpv_init, rtp_mpv_destroy, rtp_mpv_decode,
+};
+
+
 static int rtp_mpeg12_open(vlc_object_t *obj, struct vlc_rtp_pt *pt,
                         const struct vlc_sdp_pt *desc)
 {
@@ -197,6 +322,8 @@ static int rtp_mpeg12_open(vlc_object_t *obj, struct vlc_rtp_pt *pt,
 
     if (vlc_ascii_strcasecmp(desc->name, "MPA") == 0) /* RFC2250 §3.2 */
         pt->ops = &rtp_mpa_ops;
+    else if (vlc_ascii_strcasecmp(desc->name, "MPV") == 0) /* RFC2250 §3.1 */
+        pt->ops = &rtp_mpv_ops;
     else
         return VLC_ENOTSUP;
 
@@ -209,5 +336,5 @@ vlc_module_begin()
     set_category(CAT_INPUT)
     set_subcategory(SUBCAT_INPUT_DEMUX)
     set_rtp_parser_callback(rtp_mpeg12_open)
-    add_shortcut("audio/MPA")
+    add_shortcut("audio/MPA", "video/MPV")
 vlc_module_end()
