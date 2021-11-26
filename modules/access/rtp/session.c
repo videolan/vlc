841c152223e759c9425bb20fde0aac9d2049f8f9
Rémi Denis-Courmont <remi@remlab.net>
2021-11-26 22:53:17 +0200

    rtp: do not overflow PTS (fixes #26258)
    VLC does not handle negative timestamps so far. So the initial NTP
    reference timestamp should be positive enough that the extrapolated
    PTS will not end up negative when the RTP packet timestamp is lower
    than the reference RTP timestamp. In that case the PTS is lower than
    the NTP reference.
    
    While 2^62 was a very safe value in that respect, it lead to integer
    overflows in the clock and in time base conversion code. To avoid this,
    we pick a default NTP reference value equal to the closest power of two
    of the NTP time of this changeset: 2^52 is in 2042 CE, 2^51 in 1971 CE.

--- modules/access/rtp/session.c
+++ modules/access/rtp/session.c
@@ -144,7 +144,7 @@ rtp_source_create (demux_t *demux, const rtp_session_t *session,
     source->ssrc = ssrc;
     source->jitter = 0;
     source->ref_rtp = 0;
-    source->ref_ntp = UINT64_C (1) << 62;
+    source->ref_ntp = UINT64_C (1) << 51;
     source->max_seq = source->bad_seq = init_seq;
     source->last_seq = init_seq - 1;
     source->blocks = NULL;
