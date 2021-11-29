e8700cbde5435adfacac978d6e8e4c2595b558a3
Rémi Denis-Courmont <remi@remlab.net>
2021-11-29 18:53:23 +0200

    messages: remove bogus assignment

--- src/misc/messages.c
+++ src/misc/messages.c
@@ -329,9 +329,7 @@ static void vlc_LogSwitchClose(void *d)
         container_of(logger, struct vlc_logger_switch, frontend);
     struct vlc_logger *backend = logswitch->backend;
 
-    logswitch->backend = &discard_log;
     backend->ops->destroy(backend);
-
     free(logswitch);
 }
 
