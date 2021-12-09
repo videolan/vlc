a3ed3d9cf7a4772e3be78169a8986fda5a56d7d8
Benjamin Arnaud <benjamin.arnaud@videolabs.io>
2021-12-09 17:42:12 +0100

    qt/mlvideomodel: Remove the 'id' sorting criteria

--- modules/gui/qt/medialibrary/mlvideomodel.cpp
+++ modules/gui/qt/medialibrary/mlvideomodel.cpp
@@ -32,7 +32,6 @@ QVariantList getVariantList(const QList<T> & desc)
 }
 
 QHash<QByteArray, vlc_ml_sorting_criteria_t> MLVideoModel::M_names_to_criteria = {
-    {"id", VLC_ML_SORTING_DEFAULT},
     {"title", VLC_ML_SORTING_ALPHA},
     {"duration", VLC_ML_SORTING_DURATION},
     {"playcount", VLC_ML_SORTING_PLAYCOUNT},
