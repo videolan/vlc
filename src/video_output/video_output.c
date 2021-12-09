a567c3f1a0da2c11ecc485ee4668e886dff050e4
Alexandre Janniaux <ajanni@videolabs.io>
2021-12-09 14:44:02 +0100

    Revert "video_output: ensure that we don't get killed twice"
    This reverts commit 91d57a7e53a3962af10f6f5d22c18832107cc72c.

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -2338,11 +2338,7 @@ void vout_StopDisplay(vout_thread_t *vout)
 {
     vout_thread_sys_t *sys = VOUT_THREAD_TO_SYS(vout);
 
-    /* Ensure that we don't get killed twice */
-    bool is_terminated = atomic_exchange(&sys->control_is_terminated, true);
-    if (is_terminated)
-        return;
-
+    atomic_store(&sys->control_is_terminated, true);
     // wake up so it goes back to the loop that will detect the terminated state
     vout_control_Wake(&sys->control);
     vlc_cond_signal(&sys->vsync.cond_update);
