videolan/vlc

commit 8cf02acd84a1e099e15037d7c1e4dce6e8888df9

avcodec: remove LIBAVCODEC_VERSION_MICRO >= 100 checks
This check was used to distinguish FFmpeg and libav, as libav support
is removed now, this is no longer necessary.

modules/codec/avcodec/video.c
@@ -220,10 +220,6 @@ static int lavc_GetVideoFormat(decoder_t *dec, video_format_t *restrict fmt,
     {
         fmt->i_frame_rate = ctx->framerate.num;
         fmt->i_frame_rate_base = ctx->framerate.den;
-# if LIBAVCODEC_VERSION_MICRO <  100
-        // for some reason libav don't thinkg framerate presents actually same thing as in ffmpeg
-        fmt->i_frame_rate_base *= __MAX(ctx->ticks_per_frame, 1);
-# endif
     }
     else if (ctx->time_base.num > 0 && ctx->time_base.den > 0)
     {
@@ -1081,11 +1077,7 @@ static int DecodeBlock( decoder_t *p_dec, block_t **pp_block )
 
         /* Compute the PTS */
 #if LIBAVCODEC_VERSION_CHECK( 57, 61, 100 )
-# if LIBAVCODEC_VERSION_MICRO >= 100
         int64_t av_pts = frame->best_effort_timestamp;
-# else
-        int64_t av_pts = frame->pts;
-# endif
 #else
         int64_t av_pts = frame->pkt_pts;
 #endif
@@ -1634,8 +1626,7 @@ no_reuse:
     if (!can_hwaccel)
         return swfmt;
 
-#if (LIBAVCODEC_VERSION_MICRO >= 100) \
-  && (LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 83, 101))
+#if LIBAVCODEC_VERSION_CHECK(57, 83, 101)
     if (p_context->active_thread_type)
     {
         msg_Warn(p_dec, "thread type %d: disabling hardware acceleration",

modules/codec/avcodec/avcommon_compat.h
@@ -34,7 +34,7 @@
  * c is the micro version
  */
 #define LIBAVCODEC_VERSION_CHECK( a, b, c ) \
-    (LIBAVCODEC_VERSION_MICRO >= 100 && LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( a, b, c ) )
+    (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( a, b, c ))
 
 #ifndef AV_CODEC_FLAG_OUTPUT_CORRUPT
 # define AV_CODEC_FLAG_OUTPUT_CORRUPT CODEC_FLAG_OUTPUT_CORRUPT

modules/codec/avcodec/directx_va.c
@@ -253,7 +253,7 @@ static const directx_va_mode_t DXVA_MODES[] = {
 
     /* VPx */
     { "VP8",                                                                          &DXVA_ModeVP8_VLD,                      8, {1, 1}, 0, NULL, 0 },
-#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 57, 17, 100 ) && LIBAVCODEC_VERSION_MICRO >= 100
+#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 57, 17, 100 )
     { "VP9 profile 0",                                                                &DXVA_ModeVP9_VLD_Profile0,             8, {1, 1}, AV_CODEC_ID_VP9, PROF_VP9_MAIN, 0 },
     { "VP9 profile 2",                                                                &DXVA_ModeVP9_VLD_10bit_Profile2,       10, {1, 1}, AV_CODEC_ID_VP9, PROF_VP9_10, 0 },
 #else
@@ -263,7 +263,7 @@ static const directx_va_mode_t DXVA_MODES[] = {
     { "VP9 profile Intel",                                                            &DXVA_ModeVP9_VLD_Intel,                8, {1, 1}, 0, NULL, 0 },
 
     /* AV1 */
-#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 58, 112, 103 ) && LIBAVCODEC_VERSION_MICRO >= 100
+#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 58, 112, 103 )
     { "AV1 Main profile 8",                                                           &DXVA_ModeAV1_VLD_Profile0,             8, {1, 1}, AV_CODEC_ID_AV1, PROF_AV1_MAIN, 0 },
     { "AV1 Main profile 10",                                                          &DXVA_ModeAV1_VLD_Profile0,            10, {1, 1}, AV_CODEC_ID_AV1, PROF_AV1_MAIN, 0 },
     { "AV1 High profile 8",                                                           &DXVA_ModeAV1_VLD_Profile1,             8, {1, 1}, AV_CODEC_ID_AV1, PROF_AV1_HIGH, 0 },

modules/codec/avcodec/fourcc.c
@@ -186,7 +186,7 @@ static const struct vlc_avcodec_fourcc video_codecs[] =
     /* AV_CODEC_ID_V210X */
     { VLC_CODEC_TMV, AV_CODEC_ID_TMV },
     { VLC_CODEC_V210, AV_CODEC_ID_V210 },
-#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 54, 50, 100 ) && LIBAVCODEC_VERSION_MICRO >= 100
+#if LIBAVCODEC_VERSION_CHECK( 54, 50, 100 )
     { VLC_CODEC_VUYA, AV_CODEC_ID_AYUV },
 #endif
     /* AV_CODEC_ID_DPX */
@@ -201,7 +201,7 @@ static const struct vlc_avcodec_fourcc video_codecs[] =
     /* AV_CODEC_ID_IFF_BYTERUN1 */
     { VLC_CODEC_KGV1, AV_CODEC_ID_KGV1 },
     { VLC_CODEC_YOP, AV_CODEC_ID_YOP },
-#if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 58, 53, 100 ) && LIBAVCODEC_VERSION_MICRO >= 100
+#if LIBAVCODEC_VERSION_CHECK( 58, 53, 100 )
     { VLC_CODEC_VP4, AV_CODEC_ID_VP4 },
 #endif
     { VLC_CODEC_VP8, AV_CODEC_ID_VP8 },
@@ -274,11 +274,11 @@ static const struct vlc_avcodec_fourcc video_codecs[] =
     /* ffmpeg only: AV_CODEC_ID_012V */
     /* ffmpeg only: AV_CODEC_ID_AVUI */
     /* ffmpeg only: AV_CODEC_ID_TARGA_Y216 */
-#if LIBAVCODEC_VERSION_MICRO >= 100
+
     { VLC_CODEC_V308, AV_CODEC_ID_V308 },
     { VLC_CODEC_V408, AV_CODEC_ID_V408 },
     { VLC_CODEC_YUV4, AV_CODEC_ID_YUV4 },
-#endif
+
     /* ffmpeg only: AV_CODEC_ID_SANM */
     /* ffmpeg only: AV_CODEC_ID_PAF_VIDEO */
     /* ffmpeg only: AV_CODEC_ID_AVRN */
@@ -294,7 +294,6 @@ static const struct vlc_avcodec_fourcc video_codecs[] =
     { VLC_CODEC_MAGICYUV, AV_CODEC_ID_MAGICYUV },
 #endif
 
-#if LIBAVCODEC_VERSION_MICRO >= 100 /* FFmpeg only */
 #if LIBAVCODEC_VERSION_INT >= AV_VERSION_INT( 57, 24, 102 )
     { VLC_CODEC_CINEFORM, AV_CODEC_ID_CFHD },
 #endif
@@ -327,7 +326,6 @@ static const struct vlc_avcodec_fourcc video_codecs[] =
     { VLC_CODEC_NOTCHLC, AV_CODEC_ID_NOTCHLC },
 #endif
 
-#endif /* FFmpeg only */
 };
 
 /*
@@ -443,7 +441,7 @@ static const struct vlc_avcodec_fourcc audio_codecs[] =
     /* AV_CODEC_ID_WESTWOOD_SND1 */
     { VLC_CODEC_GSM, AV_CODEC_ID_GSM },
     { VLC_CODEC_QDM2, AV_CODEC_ID_QDM2 },
-#if (LIBAVCODEC_VERSION_MICRO >= 100) && (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(57, 71, 100))
+#if (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(57, 71, 100))
     { VLC_CODEC_QDMC, AV_CODEC_ID_QDMC },
 #endif
     { VLC_CODEC_COOK, AV_CODEC_ID_COOK },
@@ -494,13 +492,11 @@ static const struct vlc_avcodec_fourcc audio_codecs[] =
     /* AV_CODEC_ID_PAF_AUDIO */
     { VLC_CODEC_ON2AVC, AV_CODEC_ID_ON2AVC },
 
-    /* DSD (FFmpeg only) */
-#if LIBAVCODEC_VERSION_MICRO >= 100
+    /* DSD */
     { VLC_CODEC_DSD_LSBF, AV_CODEC_ID_DSD_LSBF },
     { VLC_CODEC_DSD_MSBF, AV_CODEC_ID_DSD_MSBF },
     { VLC_CODEC_DSD_LSBF_PLANAR, AV_CODEC_ID_DSD_LSBF_PLANAR },
     { VLC_CODEC_DSD_MSBF_PLANAR, AV_CODEC_ID_DSD_MSBF_PLANAR },
-#endif
 
     /* ffmpeg only: AV_CODEC_ID_FFWAVESYNTH */
     /* ffmpeg only: AV_CODEC_ID_SONIC */
@@ -520,7 +516,7 @@ static const struct vlc_avcodec_fourcc spu_codecs[] =
     { VLC_CODEC_SSA, AV_CODEC_ID_SSA },
     { VLC_CODEC_TX3G, AV_CODEC_ID_MOV_TEXT },
     { VLC_CODEC_BD_PG, AV_CODEC_ID_HDMV_PGS_SUBTITLE },
-#if (LIBAVCODEC_VERSION_MICRO >= 100) && (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(57, 71, 100))
+#if (LIBAVCODEC_VERSION_INT >= AV_VERSION_INT(57, 71, 100))
     { VLC_CODEC_BD_TEXT, AV_CODEC_ID_HDMV_TEXT_SUBTITLE },
 #endif
     { VLC_CODEC_TELETEXT, AV_CODEC_ID_DVB_TELETEXT },

commit 4cbe586250e560d7e047535a14f49d8d074dee13

avcodec: fix invalid version check
Regression from 8cf02acd84a1e099e15037d7c1e4dce6e8888df9

This restore hw decoding with multiple threads (the default behavior).

modules/codec/avcodec/video.c
@@ -1622,7 +1622,7 @@ no_reuse:
     if (!can_hwaccel)
         return swfmt;
 
-#if LIBAVCODEC_VERSION_CHECK(57, 83, 101)
+#if !LIBAVCODEC_VERSION_CHECK(57, 83, 101)
     if (p_context->active_thread_type)
     {
         msg_Warn(p_dec, "thread type %d: disabling hardware acceleration",