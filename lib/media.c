videolan/vlc

commit c9ab8bd8ef2fb4ae92dd4b22946ba110d1cce07a

libvlc: add libvlc_media_get_stat
This new function can be used to retrieve the mtime, ctime, atime and
size properties. More properties can be added in the future without
modifying this function.

Co-authored-by: LE QUEC Nicolas <Nicolas.Le_Quec@bordeaux-inp.fr>

Fixes #25747

lib/media.c
@@ -838,6 +838,43 @@ libvlc_media_get_duration( libvlc_media_t * p_md )
     return from_mtime(input_item_GetDuration( p_md->p_input_item ));
 }
 
+int
+libvlc_media_get_stat( libvlc_media_t *p_md, unsigned type, uint64_t *out )
+{
+    assert( p_md );
+    assert( out );
+
+    if( !p_md->p_input_item )
+    {
+        libvlc_printerr( "No input item" );
+        return -1;
+    }
+
+    const char *name;
+    switch (type)
+    {
+        case libvlc_media_stat_mtime:   name = "mtime"; break;
+        case libvlc_media_stat_size:    name = "size"; break;
+        default:
+            libvlc_printerr( "unknown libvlc_media_stat" );
+            return -1;
+    };
+
+    char *str = input_item_GetInfo( p_md->p_input_item, ".stat", name );
+    if( str == NULL )
+        return 0;
+
+    char *end;
+    unsigned long long val = strtoull( str, &end, 10 );
+    free( str );
+
+    if( *end != '\0' )
+        return -1;
+
+    *out = val;
+    return 1;
+}
+
 static const input_preparser_callbacks_t input_preparser_callbacks = {
     .on_preparse_ended = input_item_preparse_ended,
     .on_subtree_added = input_item_subtree_added,

include/vlc/libvlc_media.h
@@ -212,6 +212,12 @@ typedef struct libvlc_media_slave_t
 } libvlc_media_slave_t;
 
 /**
+ * Type of stat that can be requested from libvlc_media_get_stat()
+ */
+#define libvlc_media_stat_mtime 0
+#define libvlc_media_stat_size 1
+
+/**
  * Callback prototype to open a custom bitstream input media.
  *
  * The same media item can be opened multiple times. Each time, this callback
@@ -559,6 +565,22 @@ LIBVLC_API libvlc_event_manager_t *
  */
 LIBVLC_API libvlc_time_t
    libvlc_media_get_duration( libvlc_media_t *p_md );
+
+/**
+ * Get a 'stat' value of media descriptor object item.
+ *
+ * \note 'stat' values are currently only parsed by directory accesses. This
+ * mean that only sub medias of a directory media, parsed with
+ * libvlc_media_parse_with_options() can have valid 'stat' properties.
+ * \version LibVLC 4.0.0 and later.
+ *
+ * \param p_md media descriptor object
+ * \param type a valid libvlc_media_stat_ define
+ * \param out field in which the value will be stored
+ * \return 1 on success, 0 if not found, -1 on error.
+ */
+LIBVLC_API int
+   libvlc_media_get_stat( libvlc_media_t *p_md, unsigned type, uint64_t *out );
 
 /**
  * Parse the media asynchronously with options.

lib/libvlc.sym
@@ -73,6 +73,7 @@ libvlc_media_duplicate
 libvlc_media_event_manager
 libvlc_media_get_codec_description
 libvlc_media_get_duration
+libvlc_media_get_stat
 libvlc_media_get_meta
 libvlc_media_get_mrl
 libvlc_media_get_state

commit e71404e5c6057ac60e78fa647b7b4ed4e2b9d71e

lib: media: fix use-after-free
Regression from c9ab8bd8ef2fb4ae92dd4b22946ba110d1cce07a

Fixes #26367

lib/media.c
@@ -866,10 +866,13 @@ libvlc_media_get_stat( libvlc_media_t *p_md, unsigned type, uint64_t *out )
 
     char *end;
     unsigned long long val = strtoull( str, &end, 10 );
-    free( str );
 
     if( *end != '\0' )
+    {
+        free( str );
         return -1;
+    }
+    free( str );
 
     *out = val;
     return 1;