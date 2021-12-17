da410fbd98daf325c4ffe168b7c649c1bd991af0
Alexandre Janniaux <ajanni@videolabs.io>
2021-12-17 15:41:26 +0100

    edummy: use vlc_encoder_operations

--- modules/codec/edummy.c
+++ modules/codec/edummy.c
@@ -31,18 +31,19 @@
 #include <vlc_plugin.h>
 #include <vlc_codec.h>
 
-static int OpenEncoder( vlc_object_t * );
+static int OpenVideoEncoder( vlc_object_t * );
+static int OpenAudioEncoder( vlc_object_t * );
 
 vlc_module_begin ()
     set_shortname( N_("Dummy") )
     set_description( N_("Dummy encoder") )
     set_capability( "video encoder", 0 )
-    set_callback( OpenEncoder )
+    set_callback( OpenVideoEncoder )
     add_shortcut( "dummy" )
 
     add_submodule()
     set_capability( "audio encoder", 0 )
-    set_callback( OpenEncoder )
+    set_callback( OpenAudioEncoder )
     add_shortcut( "dummy" )
 vlc_module_end ()
 
@@ -54,15 +55,31 @@ static block_t *EncodeVideo( encoder_t *p_enc, picture_t *p_pict );
 static block_t *EncodeAudio( encoder_t *p_enc, block_t *p_buf );
 
 /*****************************************************************************
- * OpenDecoder: open the dummy encoder.
+ * OpenVideoDecoder: open the dummy encoder.
  *****************************************************************************/
-static int OpenEncoder( vlc_object_t *p_this )
+static int OpenVideoEncoder( vlc_object_t *p_this )
 {
     encoder_t *p_enc = (encoder_t *)p_this;
 
-    p_enc->pf_encode_video = EncodeVideo;
-    p_enc->pf_encode_audio = EncodeAudio;
+    static const struct vlc_encoder_operations video_ops =
+        { .encode_video = EncodeVideo };
 
+    p_enc->ops = &video_ops;
+
+    return VLC_SUCCESS;
+}
+
+/*****************************************************************************
+ * OpenAudioDecoder: open the dummy encoder.
+ *****************************************************************************/
+static int OpenAudioEncoder( vlc_object_t *p_this )
+{
+    encoder_t *p_enc = (encoder_t *)p_this;
+
+    static const struct vlc_encoder_operations audio_ops =
+        { .encode_audio = EncodeAudio };
+
+    p_enc->ops = &audio_ops;
     return VLC_SUCCESS;
 }
 
