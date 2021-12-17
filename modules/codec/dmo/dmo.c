987b62eb6917e54eda6ee978f95c36b218ed1c46
Alexandre Janniaux <ajanni@videolabs.io>
2021-12-17 15:33:17 +0100

    dmo: use vlc_encoder_operations

--- modules/codec/dmo/dmo.c
+++ modules/codec/dmo/dmo.c
@@ -58,7 +58,7 @@ static void DecoderClose ( vlc_object_t * );
 static int DecodeBlock ( decoder_t *, block_t * );
 static void *DecoderThread( void * );
 static int  EncoderOpen  ( vlc_object_t * );
-static void EncoderClose ( vlc_object_t * );
+static void EncoderClose ( encoder_t * );
 static block_t *EncodeBlock( encoder_t *, void * );
 
 static int  EncOpen  ( vlc_object_t * );
@@ -83,13 +83,13 @@ vlc_module_begin ()
     set_description( N_("DirectMedia Object encoder") )
     add_shortcut( "dmo" )
     set_capability( "video encoder", 10 )
-    set_callbacks( EncoderOpen, EncoderClose )
+    set_callback( EncoderOpen )
 
     add_submodule ()
     set_description( N_("DirectMedia Object encoder") )
     add_shortcut( "dmo" )
     set_capability( "audio encoder", 10 )
-    set_callbacks( EncoderOpen, EncoderClose )
+    set_callback( EncoderOpen )
 
 
 vlc_module_end ()
@@ -1033,6 +1033,12 @@ typedef struct
 
 } encoder_sys_t;
 
+static block_t *EncodeVideo( encoder_t *p_enc, picture_t *pic )
+    { return EncodeBlock( p_enc, pic ); }
+
+static block_t *EncodeAudio( encoder_t *p_enc, block_t *samples )
+    { return EncodeBlock( p_enc, samples ); }
+
 /*****************************************************************************
  * EncoderOpen: open dmo codec
  *****************************************************************************/
@@ -1043,11 +1049,31 @@ static int EncoderOpen( vlc_object_t *p_this )
     int i_ret = EncOpen( p_this );
     if( i_ret != VLC_SUCCESS ) return i_ret;
 
-    /* Set callbacks */
-    p_enc->pf_encode_video = (block_t *(*)(encoder_t *, picture_t *))
-        EncodeBlock;
-    p_enc->pf_encode_audio = (block_t *(*)(encoder_t *, block_t *))
-        EncodeBlock;
+    static const struct vlc_encoder_operations video_ops =
+    {
+        .close = EncoderClose,
+        .encode_video = EncodeVideo,
+    };
+
+    static const struct vlc_encoder_operations audio_ops =
+    {
+        .close = EncoderClose,
+        .encode_audio = EncodeAudio,
+    };
+
+    switch( p_enc->fmt_in.i_cat )
+    {
+        case VIDEO_ES:
+            p_enc->ops = &video_ops;
+            break;
+
+        case AUDIO_ES:
+            p_enc->ops = &audio_ops;
+            break;
+
+        default:
+            vlc_assert_unreachable();
+    }
 
     return VLC_SUCCESS;
 }
@@ -1598,9 +1624,8 @@ static block_t *EncodeBlock( encoder_t *p_enc, void *p_data )
 /*****************************************************************************
  * EncoderClose: close codec
  *****************************************************************************/
-void EncoderClose( vlc_object_t *p_this )
+void EncoderClose( encoder_t *p_enc )
 {
-    encoder_t *p_enc = (encoder_t*)p_this;
     encoder_sys_t *p_sys = p_enc->p_sys;
 
     if( !p_sys ) return;
