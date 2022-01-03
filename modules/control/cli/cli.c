b7eeec2c06ee0d5962a95cbd0a1ef2e886adbb5e
Romain Vimont <rom1v@videolabs.io>
2022-01-03 17:12:48 +0100

    cli: fix possible uninitialized read
    On Android:
    
        ../../modules/control/cli/cli.c:335:9: warning: variable 'ret' is
                used uninitialized whenever 'if' condition is false
                [-Wsometimes-uninitialized]
            if (count > 0)
                ^~~~~~~~~
        ../../modules/control/cli/cli.c:359:12: note: uninitialized uses
                occurs here
            return ret;
                   ^~~
    
    Return an error if count == 0 in the `#ifdef HAVE_WORDEXP` branch, and
    replace the if-condition by an assertion.

--- modules/control/cli/cli.c
+++ modules/control/cli/cli.c
@@ -308,6 +308,11 @@ error:      wordfree(&we);
     }
 
     size_t count = we.we_wordc;
+    if (count == 0) {
+        ret = VLC_EGENERIC;
+        goto error;
+    }
+
     const char **args = vlc_alloc(count, sizeof (*args));
     if (unlikely(args == NULL))
     {
@@ -337,22 +342,19 @@ error:      wordfree(&we);
     }
 #endif
 
-    if (count > 0)
+    assert(count > 0);
+    const struct command **pp = tfind(&args[0], &sys->commands, cmdcmp);
+
+    if (pp != NULL)
     {
-        const struct command **pp = tfind(&args[0], &sys->commands, cmdcmp);
-
-        if (pp != NULL)
-        {
-            const struct command *c = *pp;
-
-            ret = c->handler.callback(cl, args, count, c->data);
-        }
-        else
-        {
-            cli_printf(cl, _("Unknown command `%s'. Type `help' for help."),
-                      args[0]);
-            ret = VLC_EGENERIC;
-        }
+        const struct command *c = *pp;;
+        ret = c->handler.callback(cl, args, count, c->data);
+    }
+    else
+    {
+        cli_printf(cl, _("Unknown command `%s'. Type `help' for help."),
+                  args[0]);
+        ret = VLC_EGENERIC;
     }
 
 #ifdef HAVE_WORDEXP
