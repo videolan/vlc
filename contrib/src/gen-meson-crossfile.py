09154ece03233a1f01e20aea5430b70616d58a93
Marvin Scholz <epirat07@gmail.com>
2022-01-22 15:13:04 +0100

    contrib: meson: fix writing pkg_config_libdir
    Write pkg_config_libdir to the correct crossfile section, should
    fix the build after 74060949afbbca285fefdc746f61ff43ed2cb213.

--- contrib/src/gen-meson-crossfile.py
+++ contrib/src/gen-meson-crossfile.py
@@ -34,12 +34,12 @@ if os.environ.get('HOST_SYSTEM') == 'darwin':
 _add_environ_val('ar', 'AR')
 _add_environ_val('strip', 'STRIP')
 _add_environ_val('pkgconfig', 'PKG_CONFIG')
-_add_environ_val('pkg_config_libdir', 'PKG_CONFIG_LIBDIR')
 _add_environ_val('windres', 'WINDRES')
 
 # Properties section
 args.file.write("\n[properties]\n")
 args.file.write("needs_exe_wrapper = true\n")
+_add_environ_val('pkg_config_libdir', 'PKG_CONFIG_LIBDIR')
 _add_environ_arr('c_args', 'CFLAGS')
 _add_environ_arr('c_link_args', 'LDFLAGS')
 _add_environ_arr('cpp_args', 'CXXFLAGS')
