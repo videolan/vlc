videolan/vlc

commit 2480a5159fd80916084777b78bbab59f6c6e789b

modules: handle error in module_need_var()
This ensures that all variables used have a sane non-empty default (only
"codec" was left). Then we can assume that NULL is an error rather than
an empty variable.

src/libvlc-module.c
@@ -1991,7 +1991,7 @@ vlc_module_begin ()
     //set_subcategory( SUBCAT_INPUT_ACODEC )
     set_subcategory( SUBCAT_INPUT_VCODEC )
 
-    add_string( "codec", NULL, CODEC_TEXT, CODEC_LONGTEXT )
+    add_string( "codec", "any", CODEC_TEXT, CODEC_LONGTEXT )
     add_bool( "hw-dec", true, HW_DEC_TEXT, HW_DEC_LONGTEXT )
     add_obsolete_string( "encoder" ) /* since 4.0.0 */
     add_module("dec-dev", "decoder device", "any", DEC_DEV_TEXT, DEC_DEV_LONGTEXT)

include/vlc_modules.h
@@ -114,6 +114,9 @@ static inline module_t *module_need_var(vlc_object_t *obj, const char *cap,
                                         const char *varname)
 {
     char *list = var_InheritString(obj, varname);
+    if (unlikely(list == NULL))
+        return NULL;
+
     module_t *m = module_need(obj, cap, list, false);
 
     free(list);

commit d89a4e99cd17989c690297beaa3933fd62b9148f

libvlc-module: set default packetizer to any
Set the default packetizer to any so that every packetizer are probed,
instead of NULL which is now considered an error.

Regression from 2480a5159fd80916084777b78bbab59f6c6e789b.

Fixes #26566, #26576

src/libvlc-module.c
@@ -2035,7 +2035,7 @@ vlc_module_begin ()
     add_integer( "dscp", 0, DSCP_TEXT, DSCP_LONGTEXT )
 
     set_subcategory( SUBCAT_SOUT_PACKETIZER )
-    add_module("packetizer", "packetizer", NULL,
+    add_module("packetizer", "packetizer", "any",
                PACKETIZER_TEXT, PACKETIZER_LONGTEXT)
 
 /* Advanced options */