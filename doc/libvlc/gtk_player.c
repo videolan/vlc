49dc4a6d11f8210b775b81d063b6d5a1127dcd7c
Romain Roffé <rofferom@gmail.com>
2022-02-04 15:31:45 +0100

    doc: gtk: Fix build with GTK3+

--- doc/libvlc/gtk_player.c
+++ doc/libvlc/gtk_player.c
@@ -1,4 +1,4 @@
-// gcc -o gtk_player gtk_player.c `pkg-config --libs gtk+-2.0 libvlc` `pkg-config --cflags gtk+-2.0 libvlc`
+// gcc -o gtk_player gtk_player.c `pkg-config --libs gtk+-3.0 libvlc` `pkg-config --cflags gtk+-3.0 libvlc`
 
 /* License WTFPL http://sam.zoy.org/wtfpl/ */
 /* Written by Vincent Schüßler */
@@ -105,7 +105,7 @@ int main( int argc, char *argv[] ) {
     filemenu_openitem = gtk_menu_item_new_with_label("Open");
     gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), filemenu_openitem);
     gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileitem), filemenu);
-    gtk_menu_bar_append(GTK_MENU_BAR(menubar), fileitem);
+    gtk_menu_shell_append(GTK_MENU_BAR(menubar), fileitem);
     gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
     g_signal_connect(filemenu_openitem, "activate", G_CALLBACK(on_open), window);
 
