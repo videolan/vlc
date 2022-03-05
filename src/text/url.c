videolan/vlc

commit 88e3a4f0017196af2139576daf76ad4564864eb6

url: Optimize vlc_url_decode

src/text/url.c
@@ -51,6 +51,20 @@ char *vlc_uri_decode_duplicate (const char *str)
     return buf;
 }
 
+static char hex_to_char( char c )
+{
+    unsigned char v = (unsigned char)c - '0';
+    if ( v < 10 )
+        return v;
+    v = (unsigned)c - 'a';
+    if ( v <= 5 )
+        return v + 10;
+    v = (unsigned)c - 'A';
+    if ( v <= 5 )
+        return v + 10;
+    return -1;
+}
+
 char *vlc_uri_decode (char *str)
 {
     char *in = str, *out = str;
@@ -62,15 +76,15 @@ char *vlc_uri_decode (char *str)
     {
         if (c == '%')
         {
-            char hex[3];
+            char a, b;
 
-            if (!(hex[0] = *(in++)) || !(hex[1] = *(in++)))
+            if ((a = hex_to_char(*(in++))) < 0 ||
+                (b = hex_to_char(*(in++))) < 0)
             {
                 errno = EINVAL;
                 return NULL;
             }
-            hex[2] = '\0';
-            *(out++) = strtoul (hex, NULL, 0x10);
+            *(out++) = a << 4 | b;
         }
         else
             *(out++) = c;

commit 7550e1ff5911059a5591b4e16438f41d4a059606

url: use signed type as needed
Errors are signaled by -1, so the type has to be signed.

There are no needs to force 8-bit sign extension here since the
non-error result is always positive. So better use a fast type.

Regression from 88e3a4f0017196af2139576daf76ad4564864eb6.
Fixes #26679.

src/text/url.c
@@ -51,7 +51,7 @@ char *vlc_uri_decode_duplicate (const char *str)
     return buf;
 }
 
-static char hex_to_char(unsigned char c)
+static int_fast8_t hex_to_char(unsigned char c)
 {
     uint_fast8_t v = c - '0';
     if ( v < 10 )
@@ -76,7 +76,7 @@ char *vlc_uri_decode (char *str)
     {
         if (c == '%')
         {
-            char a, b;
+            int_fast8_t a, b;
 
             if ((a = hex_to_char(*(in++))) < 0 ||
                 (b = hex_to_char(*(in++))) < 0)