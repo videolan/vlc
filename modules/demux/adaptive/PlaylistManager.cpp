310f94ef936e1e226c172a024657e90ecb5eadb8
Francois Cartegnie <fcvlcdev@free.fr>
2022-03-10 18:05:39 +0100

    demux: adaptive: rewrite media timeline fallback

--- modules/demux/adaptive/PlaylistManager.cpp
+++ modules/demux/adaptive/PlaylistManager.cpp
@@ -232,10 +232,6 @@ AbstractStream::BufferingStatus PlaylistManager::bufferize(Times deadline,
         PrioritizedAbstractStream &p = *it;
         p.st = stream;
         p.status = p.st->getBufferAndStatus(deadline, i_min_buffering, i_max_buffering, &p.demuxed_amount);
-        p.demuxed_amount = p.st->getDemuxedAmount(deadline).continuous;
-        vlc_tick_t mediaAmount;
-        if(p.st->getMediaAdvanceAmount(&mediaAmount) && mediaAmount > p.demuxed_amount)
-            p.demuxed_amount = mediaAmount;
         ++it;
     }
     std::sort(prioritized_streams.begin(), prioritized_streams.end(), streamCompare);
