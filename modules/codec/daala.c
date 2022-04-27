videolan/vlc

commit f710e4064641412215d6f80498ff8da84f0497ca

daala: use vlc_encoder_operations

modules/codec/daala.c
@@ -92,7 +92,7 @@ static void daala_CopyPicture( picture_t *, daala_image * );
 
 #ifdef ENABLE_SOUT
 static int  OpenEncoder( vlc_object_t *p_this );
-static void CloseEncoder( vlc_object_t *p_this );
+static void CloseEncoder( encoder_t *p_enc );
 static block_t *Encode( encoder_t *p_enc, picture_t *p_pict );
 
 static const char *const enc_chromafmt_list[] = {
@@ -130,7 +130,7 @@ vlc_module_begin ()
     add_submodule ()
     set_description( N_("Daala video encoder") )
     set_capability( "video encoder", 150 )
-    set_callbacks( OpenEncoder, CloseEncoder )
+    set_callback( OpenEncoder )
     add_shortcut( "daala" )
 
 #   define ENC_CFG_PREFIX "sout-daala-"
@@ -606,7 +606,6 @@ static int OpenEncoder( vlc_object_t *p_this )
         return VLC_ENOMEM;
     p_enc->p_sys = p_sys;
 
-    p_enc->pf_encode_video = Encode;
     p_enc->fmt_in.i_codec = VLC_CODEC_I420;
     p_enc->fmt_out.i_codec = VLC_CODEC_DAALA;
 
@@ -703,7 +702,7 @@ static int OpenEncoder( vlc_object_t *p_this )
     {
         if ( status < 0 )
         {
-            CloseEncoder( p_this );
+            CloseEncoder( p_enc );
             return VLC_EGENERIC;
         }
         if( xiph_AppendHeaders( &p_enc->fmt_out.i_extra,
@@ -714,6 +713,14 @@ static int OpenEncoder( vlc_object_t *p_this )
             p_enc->fmt_out.p_extra = NULL;
         }
     }
+
+    static const struct vlc_encoder_operations ops =
+    {
+        .close = CloseEncoder,
+        .encoder_video = Encode,
+    };
+    p_enc->ops = &ops;
+
     return VLC_SUCCESS;
 }
 
@@ -775,9 +782,8 @@ static block_t *Encode( encoder_t *p_enc, picture_t *p_pict )
     return p_block;
 }
 
-static void CloseEncoder( vlc_object_t *p_this )
+static void CloseEncoder( encoder_t *p_enc )
 {
-    encoder_t *p_enc = (encoder_t *)p_this;
     encoder_sys_t *p_sys = p_enc->p_sys;
 
     daala_info_clear(&p_sys->di);

commit ec4f3c10b70b208c1f6ce2ff0e11d84b46fb284f

daala: fix encoder_options initialization
Regression from f710e4064641412215d6f80498ff8da84f0497ca

modules/codec/daala.c
@@ -717,7 +717,7 @@ static int OpenEncoder( vlc_object_t *p_this )
     static const struct vlc_encoder_operations ops =
     {
         .close = CloseEncoder,
-        .encoder_video = Encode,
+        .encode_video = Encode,
     };
     p_enc->ops = &ops;
 