videolan/vlc

commit c344522ff264caa43c9dbc710484f01edbad04cb

opengl: init textures properties from modules
All interop modules reused the util function opengl_interop_init()
(written for the software interop) to initialize the textures properties
from the chroma.

However, the actual texture format may not always be deduced from the
chroma: only the interop implementations have the full knowledge of the
texture details.

Therefore, initialize texture properties directly from each interop
implementation.

Fixes #26735
Fixes #26769

modules/video_output/opengl/interop_vaapi.c
@@ -464,10 +464,48 @@ Open(vlc_object_t *obj)
         case VLC_CODEC_VAAPI_420:
             va_fourcc = VA_FOURCC_NV12;
             vlc_sw_chroma = VLC_CODEC_NV12;
+
+            interop->tex_count = 2;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_RED,
+                .format = GL_RED,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 2},
+                .h = {1, 2},
+                .internal = GL_RG,
+                .format = GL_RG,
+                .type = GL_UNSIGNED_BYTE,
+            };
+
             break;
         case VLC_CODEC_VAAPI_420_10BPP:
             va_fourcc = VA_FOURCC_P010;
             vlc_sw_chroma = VLC_CODEC_P010;
+
+            if (vlc_gl_interop_GetTexFormatSize(interop, GL_TEXTURE_2D, GL_RG,
+                                                GL_RG16, GL_UNSIGNED_SHORT) != 16)
+                goto error;
+
+            interop->tex_count = 2;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_R16,
+                .format = GL_RED,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 2},
+                .internal = GL_RG16,
+                .format = GL_RG,
+                .type = GL_UNSIGNED_BYTE,
+            };
+
             break;
         default:
             vlc_assert_unreachable();
@@ -525,10 +563,9 @@ Open(vlc_object_t *obj)
     /* The pictures are uploaded upside-down */
     video_format_TransformBy(&interop->fmt_out, TRANSFORM_VFLIP);
 
-    int ret = opengl_interop_init(interop, GL_TEXTURE_2D, vlc_sw_chroma,
-                                  interop->fmt_in.space);
-    if (ret != VLC_SUCCESS)
-        goto error;
+    interop->tex_target = GL_TEXTURE_2D;
+    interop->fmt_out.i_chroma = vlc_sw_chroma;
+    interop->fmt_out.space = interop->fmt_in.space;
 
     static const struct vlc_gl_interop_ops ops = {
         .update_textures = tc_vaegl_update,

modules/hw/nvdec/nvdec_gl.c
@@ -224,12 +224,73 @@ static int Open(vlc_object_t *obj)
         default:                         render_chroma = VLC_CODEC_NV12; break;
     }
 
-    int ret = opengl_interop_init(interop, GL_TEXTURE_2D, render_chroma, interop->fmt_in.space);
-    if (ret != VLC_SUCCESS)
+    switch (render_chroma)
     {
-        vlc_decoder_device_Release(device);
-        return ret;
+        case VLC_CODEC_P010:
+        case VLC_CODEC_P016:
+            interop->tex_count = 2;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_R16,
+                .format = GL_RED,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 2},
+                .internal = GL_RG16,
+                .format = GL_RG,
+                .type = GL_UNSIGNED_BYTE,
+            };
+
+            break;
+        case VLC_CODEC_I444:
+            interop->tex_count = 3;
+            interop->texs[0] = interop->texs[1] = interop->texs[2] =
+                (struct vlc_gl_tex_cfg) {
+                    .w = {1, 1},
+                    .h = {1, 1},
+                    .internal = GL_RED,
+                    .format = GL_RED,
+                    .type = GL_UNSIGNED_BYTE,
+                };
+
+            break;
+        case VLC_CODEC_I444_16L:
+            interop->tex_count = 3;
+            interop->texs[0] = interop->texs[1] = interop->texs[2] =
+                (struct vlc_gl_tex_cfg) {
+                    .w = {1, 1},
+                    .h = {1, 1},
+                    .internal = GL_R16,
+                    .format = GL_RED,
+                    .type = GL_UNSIGNED_BYTE,
+                };
+
+            break;
+        case VLC_CODEC_NV12:
+            interop->tex_count = 2;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_RED,
+                .format = GL_RED,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 2},
+                .h = {1, 2},
+                .internal = GL_RG,
+                .format = GL_RG,
+                .type = GL_UNSIGNED_BYTE,
+            };
+
+            break;
     }
+
+    interop->fmt_out.i_chroma = render_chroma;
+    interop->fmt_out.space = interop->fmt_in.space;
 
     static const struct vlc_gl_interop_ops ops = {
         .allocate_textures = tc_nvdec_gl_allocate_texture,

modules/video_output/opengl/interop_android.c
@@ -240,15 +240,18 @@ Open(vlc_object_t *obj)
     };
     interop->ops = &ops;
 
-    int ret = opengl_interop_init(interop, GL_TEXTURE_EXTERNAL_OES,
-                                  VLC_CODEC_RGB32,
-                                  COLOR_SPACE_UNDEF);
+    interop->tex_target = GL_TEXTURE_EXTERNAL_OES;
+    interop->fmt_out.i_chroma = VLC_CODEC_RGB32;
+    interop->fmt_out.space = COLOR_SPACE_UNDEF;
 
-    if (ret != VLC_SUCCESS)
-    {
-        free(priv);
-        return VLC_EGENERIC;
-    }
+    interop->tex_count = 1;
+    interop->texs[0] = (struct vlc_gl_tex_cfg) {
+        .w = {1, 1},
+        .h = {1, 1},
+        .internal = GL_RGBA,
+        .format = GL_RGBA,
+        .type = GL_UNSIGNED_BYTE,
+    };
 
     return VLC_SUCCESS;
 }

modules/video_output/opengl/interop_cvpx.m
@@ -24,6 +24,7 @@
 
 #include <vlc_common.h>
 #include <vlc_plugin.h>
+#include "gl_util.h"
 #include "interop.h"
 #include "../../codec/vt_utils.h"
 
@@ -234,7 +235,10 @@ Open(vlc_object_t *obj)
     }
 #endif
 
-    int ret;
+    bool has_texture_rg = vlc_gl_HasExtension(&priv->extension_vt, "GL_ARB_texture_rg");
+
+    interop->tex_target = tex_target;
+
     switch (interop->fmt_in.i_chroma)
     {
         case VLC_CODEC_CVPX_UYVY:
@@ -244,53 +248,108 @@ Open(vlc_object_t *obj)
              * and red color channels, respectively. cf. APPLE_rgb_422 khronos
              * extension. */
 
-            ret = opengl_interop_init(interop, tex_target, VLC_CODEC_VYUY,
-                                      interop->fmt_in.space);
-            if (ret != VLC_SUCCESS)
-                goto error;
+            interop->fmt_out.i_chroma = VLC_CODEC_VYUY;
+            interop->fmt_out.space = interop->fmt_in.space;
 
-            interop->texs[0].internal = GL_RGB;
-            interop->texs[0].format = GL_RGB_422_APPLE;
-            interop->texs[0].type = GL_UNSIGNED_SHORT_8_8_APPLE;
-            interop->texs[0].w = interop->texs[0].h = (vlc_rational_t) { 1, 1 };
+            interop->tex_count = 1;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_RGB,
+                .format = GL_RGB_422_APPLE,
+                .type = GL_UNSIGNED_SHORT_8_8_APPLE,
+            };
+
             break;
         case VLC_CODEC_CVPX_NV12:
         {
-            ret = opengl_interop_init(interop, tex_target, VLC_CODEC_NV12,
-                                      interop->fmt_in.space);
-            if (ret != VLC_SUCCESS)
-                goto error;
+            interop->fmt_out.i_chroma = VLC_CODEC_NV12;
+            interop->fmt_out.space = interop->fmt_in.space;
+
+            interop->tex_count = 2;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = has_texture_rg ? GL_RED : GL_LUMINANCE,
+                .format = has_texture_rg ? GL_RED : GL_LUMINANCE,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 2},
+                .h = {1, 2},
+                .internal = has_texture_rg ? GL_RG : GL_LUMINANCE_ALPHA,
+                .format = has_texture_rg ? GL_RG : GL_LUMINANCE_ALPHA,
+                .type = GL_UNSIGNED_BYTE,
+            };
+
             break;
         }
         case VLC_CODEC_CVPX_P010:
         {
-            ret = opengl_interop_init(interop, tex_target, VLC_CODEC_P010,
-                                      interop->fmt_in.space);
-            if (ret != VLC_SUCCESS)
+            if (!has_texture_rg
+             || vlc_gl_interop_GetTexFormatSize(interop, tex_target, GL_RG,
+                                                GL_RG16, GL_UNSIGNED_SHORT) != 16)
                 goto error;
+
+            interop->fmt_out.i_chroma = VLC_CODEC_P010;
+            interop->fmt_out.space = interop->fmt_in.space;
+
+            interop->tex_count = 2;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_R16,
+                .format = GL_RED,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 2},
+                .internal = GL_RG16,
+                .format = GL_RG,
+                .type = GL_UNSIGNED_BYTE,
+            };
 
             break;
         }
         case VLC_CODEC_CVPX_I420:
-            ret = opengl_interop_init(interop, tex_target, VLC_CODEC_I420,
-                                      interop->fmt_in.space);
-            if (ret != VLC_SUCCESS)
-                goto error;
+            interop->fmt_out.i_chroma = VLC_CODEC_I420;
+            interop->fmt_out.space = interop->fmt_in.space;
+
+            interop->tex_count = 3;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = has_rg_texture ? GL_RED : GL_LUMINANCE,
+                .format = has_rg_texture ? GL_RED : GL_LUMINANCE,
+                .type = GL_UNSIGNED_BYTE,
+            };
+            interop->texs[1] = interop->texs[2] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 2},
+                .h = {1, 2},
+                .internal = has_rg_texture ? GL_RED : GL_LUMINANCE,
+                .format = has_rg_texture ? GL_RED : GL_LUMINANCE,
+                .type = GL_UNSIGNED_BYTE,
+            };
 
             break;
         case VLC_CODEC_CVPX_BGRA:
-            ret = opengl_interop_init(interop, tex_target, VLC_CODEC_RGB32,
-                                      COLOR_SPACE_UNDEF);
-            if (ret != VLC_SUCCESS)
-                goto error;
+            interop->fmt_out.i_chroma = VLC_CODEC_RGB32;
+            interop->fmt_out.space = COLOR_SPACE_UNDEF;
 
-            interop->texs[0].internal = GL_RGBA;
-            interop->texs[0].format = GL_BGRA;
+            interop->tex_count = 1;
+            interop->texs[0] = (struct vlc_gl_tex_cfg) {
+                .w = {1, 1},
+                .h = {1, 1},
+                .internal = GL_RGBA,
+                .format = GL_BGRA,
 #if TARGET_OS_IPHONE
-            interop->texs[0].type = GL_UNSIGNED_BYTE;
+                .type = GL_UNSIGNED_BYTE,
 #else
-            interop->texs[0].type = GL_UNSIGNED_INT_8_8_8_8_REV;
+                .type = GL_UNSIGNED_INT_8_8_8_8_REV,
 #endif
+            };
+
             break;
         default:
             vlc_assert_unreachable();

modules/video_output/opengl/interop_dxva2.c
@@ -530,10 +530,18 @@ GLConvOpen(vlc_object_t *obj)
     /* The pictures are uploaded upside-down */
     video_format_TransformBy(&interop->fmt_out, TRANSFORM_VFLIP);
 
-    int ret = opengl_interop_init(interop, GL_TEXTURE_2D, VLC_CODEC_RGB32,
-                                  COLOR_SPACE_UNDEF);
-    if (ret != VLC_SUCCESS)
-        goto error;
+    interop->tex_target = GL_TEXTURE_2D;
+    interop->fmt_out.i_chroma = VLC_CODEC_RGB32;
+    interop->fmt_out.space = COLOR_SPACE_UNDEF;
+
+    interop->tex_count = 1;
+    interop->texs[0] = (struct vlc_gl_tex_cfg) {
+        .w = {1, 1},
+        .h = {1, 1},
+        .internal = GL_RGBA,
+        .format = GL_RGBA,
+        .type = GL_UNSIGNED_BYTE,
+    };
 
     return VLC_SUCCESS;
 

modules/video_output/opengl/interop_vdpau.c
@@ -191,13 +191,18 @@ Open(vlc_object_t *obj)
     /* The pictures are uploaded upside-down */
     video_format_TransformBy(&interop->fmt_out, TRANSFORM_VFLIP);
 
-    int ret = opengl_interop_init(interop, GL_TEXTURE_2D, VLC_CODEC_RGB32,
-                                  COLOR_SPACE_UNDEF);
-    if (ret != VLC_SUCCESS)
-    {
-        Close(interop);
-        return VLC_EGENERIC;
-    }
+    interop->tex_target = GL_TEXTURE_2D;
+    interop->fmt_out.i_chroma = VLC_CODEC_RGB32;
+    interop->fmt_out.space = COLOR_SPACE_UNDEF;
+
+    interop->tex_count = 1;
+    interop->texs[0] = (struct vlc_gl_tex_cfg) {
+        .w = {1, 1},
+        .h = {1, 1},
+        .internal = GL_RGBA,
+        .format = GL_RGBA,
+        .type = GL_UNSIGNED_BYTE,
+    };
 
     static const struct vlc_gl_interop_ops ops = {
         .update_textures = tc_vdpau_gl_update,

commit a478e7bca65627b08643273cb9edff744a677a97

interop_vaapi: fix 420_10BPP format description
VLC_CODEC_VAAPI_420_10BPP is equivalent to VLC_FOURCC_P010 and has a
plane description that is like NV12, so it should not be different from
NV12. Only the bits per component, and so texture storage type is
changing for this format.

It was leading to incorrect or green color rendering with vaapi.
Regression from c344522ff264caa43c9dbc710484f01edbad04cb.

Fixes #26883

modules/video_output/opengl/interop_vaapi.c
@@ -499,7 +499,7 @@ Open(vlc_object_t *obj)
                 .type = GL_UNSIGNED_BYTE,
             };
             interop->texs[1] = (struct vlc_gl_tex_cfg) {
-                .w = {1, 1},
+                .w = {1, 2},
                 .h = {1, 2},
                 .internal = GL_RG16,
                 .format = GL_RG,