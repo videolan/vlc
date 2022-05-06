videolan/vlc

commit 3f75f04af6be02fb9fd61715e0d429608e870668

vout: don't wait for display if not needed

src/video_output/video_output.c
@@ -1312,9 +1312,10 @@ static int ThreadDisplayRenderPicture(vout_thread_sys_t *vout, bool render_now)
              * rendered late. */
             system_pts = system_now;
         }
-        else
+        else if (vd->ops->display != NULL)
         {
-            /* Wait to reach system_pts */
+            /* Wait to reach system_pts if the plugin doesn't handle
+             * asynchronous display */
             vlc_clock_Wait(sys->clock, system_now, pts, sys->rate,
                            VOUT_REDISPLAY_DELAY);
 

commit 2edfdad37004aeb2035464b52f3f60cca465a5f9

test: vout: implement the display callback
Otherwise, the vout won't be paced at and will never wait for the clock.

Regression since 3f75f04af6b.

test/src/video_output/video_output.c
@@ -170,11 +170,18 @@ static void CloseDisplay(vout_display_t *vd)
 
 }
 
+static void Display(vout_display_t *vd, picture_t *picture)
+{
+    (void) vd;
+    (void) picture;
+}
+
 static int OpenDisplay(vout_display_t *vd, video_format_t *fmtp,
                        struct vlc_video_context *vctx)
 {
     static const struct vlc_display_operations ops =
     {
+        .display = Display,
         .close = CloseDisplay,
     };
     vd->ops = &ops;