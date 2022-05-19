videolan/vlc

commit c58018e41f11479c91b1cfe2a49c4f09426fc87e

display: update vout_display_PlacePicture() prototype
Take only a struct vout_display_size rather than a full
struct vout_display_cfg_t. No functional changes.

modules/video_output/caopengllayer.m
@@ -370,7 +370,7 @@ static int Open (vout_display_t *vd,
                 sys->videoLayer = nil;
             }
 
-            vout_display_PlacePicture(&sys->place, vd->source, &sys->cfg);
+            vout_display_PlacePicture(&sys->place, vd->source, &sys->cfg.display);
         });
 
         if (sys->videoView == nil) {
@@ -508,7 +508,7 @@ static int Control (vout_display_t *vd, int query)
                     cfg.display.align.vertical = VLC_VIDEO_ALIGN_TOP;
                 sys->cfg = cfg;
 
-                vout_display_PlacePicture(&sys->place, vd->source, &cfg);
+                vout_display_PlacePicture(&sys->place, vd->source, &cfg.display);
             }
 
             // Note!

include/vlc_vout_display.h
@@ -494,7 +494,10 @@ static inline bool vout_display_PlaceEquals(const vout_display_place_t *p1,
  * \param source Video source format
  * \param cfg Display configuration
  */
-VLC_API void vout_display_PlacePicture(vout_display_place_t *place, const video_format_t *source, const vout_display_cfg_t *cfg);
+VLC_API
+void vout_display_PlacePicture(vout_display_place_t *restrict place,
+                               const video_format_t *restrict source,
+                               const struct vout_display_placement *cfg);
 
 /**
  * Translates coordinates.

modules/hw/mmal/vout.c
@@ -515,17 +515,16 @@ place_dest(vout_display_t *vd, const video_format_t * fmt)
 {
     vout_display_sys_t * const sys = vd->sys;
     // Ignore what VLC thinks might be going on with display size
-    vout_display_cfg_t tcfg = *vd->cfg;
+    struct vout_display_placement dp = vd->cfg->display;
     vout_display_place_t place;
-    tcfg.display.width = sys->display_width;
-    tcfg.display.height = sys->display_height;
-    tcfg.display.autoscale = true;
-    vout_display_PlacePicture(&place, fmt, &tcfg);
+
+    dp.width = sys->display_width;
+    dp.height = sys->display_height;
+    dp.autoscale = true;
+    vout_display_PlacePicture(&place, fmt, &dp);
 
     sys->dest_rect = place_to_mmal_rect(place);
 }
-
-
 
 static int configure_display(vout_display_t *vd, const video_format_t *fmt)
 {

modules/hw/vdpau/display.c
@@ -230,7 +230,7 @@ static int ResetPictures(vout_display_t *vd, video_format_t *fmt)
     vout_display_place_t place;
 
     msg_Dbg(vd, "resetting pictures");
-    vout_display_PlacePicture(&place, src, vd->cfg);
+    vout_display_PlacePicture(&place, src, &vd->cfg->display);
 
     fmt->i_width = src->i_width * place.width / src->i_visible_width;
     fmt->i_height = src->i_height * place.height / src->i_visible_height;
@@ -259,7 +259,7 @@ static int Control(vout_display_t *vd, int query)
     {
         vout_display_place_t place;
 
-        vout_display_PlacePicture(&place, vd->source, vd->cfg);
+        vout_display_PlacePicture(&place, vd->source, &vd->cfg->display);
         if (place.width  != vd->fmt->i_visible_width
          || place.height != vd->fmt->i_visible_height)
             return VLC_EGENERIC;
@@ -395,7 +395,7 @@ static int Open(vout_display_t *vd,
         };
         vout_display_place_t place;
 
-        vout_display_PlacePicture(&place, vd->source, vd->cfg);
+        vout_display_PlacePicture(&place, vd->source, &vd->cfg->display);
         sys->window = xcb_generate_id(sys->conn);
 
         xcb_void_cookie_t c =

modules/video_output/drm/display.c
@@ -100,7 +100,7 @@ static void Display(vout_display_t *vd, picture_t *picture)
     picture_t *pic = sys->buffers[sys->front_buf];
     vout_display_place_t place;
 
-    vout_display_PlacePicture(&place, vd->fmt, vd->cfg);
+    vout_display_PlacePicture(&place, vd->fmt, &vd->cfg->display);
 
     struct drm_mode_set_plane sp = {
         .plane_id = sys->plane_id,

modules/video_output/kva.c
@@ -409,7 +409,7 @@ static int Control( vout_display_t *vd, int query )
     case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
     {
         vout_display_place_t place;
-        vout_display_PlacePicture(&place, vd->source, vd->cfg);
+        vout_display_PlacePicture(&place, vd->source, &vd->cfg->display);
 
         sys->kvas.ulAspectWidth  = place.width;
         sys->kvas.ulAspectHeight = place.height;
@@ -893,7 +893,7 @@ static MRESULT EXPENTRY WndProc( HWND hwnd, ULONG msg, MPARAM mp1, MPARAM mp2 )
             i_movie_height = movie_rect.yTop - movie_rect.yBottom;
 
             vout_display_place_t place;
-            vout_display_PlacePicture(&place, vd->source, vd->cfg);
+            vout_display_PlacePicture(&place, vd->source, &vd->cfg->display);
 
             int x = ( i_mouse_x - movie_rect.xLeft ) *
                     place.width / i_movie_width + place.x;

modules/video_output/libplacebo/display.c
@@ -282,22 +282,22 @@ static void PictureRender(vout_display_t *vd, picture_t *pic,
 
     // Set the target crop dynamically based on the swapchain flip state
     vout_display_place_t place;
-    vout_display_cfg_t cfg = *vd->cfg;
-    cfg.display.width = frame.fbo->params.w;
-    cfg.display.height = frame.fbo->params.h;
+    struct vout_display_placement dp = vd->cfg->display;
+    dp.width = frame.fbo->params.w;
+    dp.height = frame.fbo->params.h;
     if (need_vflip) {
-        switch (cfg.display.align.vertical) {
+        switch (dp.align.vertical) {
         case VLC_VIDEO_ALIGN_TOP:
-            cfg.display.align.vertical = VLC_VIDEO_ALIGN_BOTTOM;
+            dp.align.vertical = VLC_VIDEO_ALIGN_BOTTOM;
             break;
         case VLC_VIDEO_ALIGN_BOTTOM:
-            cfg.display.align.vertical = VLC_VIDEO_ALIGN_TOP;
+            dp.align.vertical = VLC_VIDEO_ALIGN_TOP;
             break;
         default:
             break;
         }
     }
-    vout_display_PlacePicture(&place, vd->fmt, &cfg);
+    vout_display_PlacePicture(&place, vd->fmt, &dp);
     if (need_vflip) {
         place.y = frame.fbo->params.h - place.y;
         place.height = -place.height;

modules/video_output/macosx.m
@@ -331,7 +331,7 @@ static void UpdatePlace (vout_display_t *vd, const vout_display_cfg_t *cfg)
     vout_display_sys_t *sys = vd->sys;
     vout_display_place_t place;
     /* We never receive resize from the core, so provide the size ourselves */
-    vout_display_PlacePicture(&place, vd->source, cfg);
+    vout_display_PlacePicture(&place, vd->source, &cfg->display);
     sys->place = place;
 }
 

commit c3df9c37123acd2575b309ddc4591fc75debe57d

vout: caopengllayer: fix resize
Fixes a regression from c58018e41f11479c91b1cfe2a49c4f09426fc87e

modules/video_output/caopengllayer.m
@@ -895,7 +895,7 @@ shouldInheritContentsScale:(CGFloat)newScale
                 sys->cfg.display.width = newSize.width;
                 sys->cfg.display.height = newSize.height;
 
-                vout_display_PlacePicture(&sys->place, _voutDisplay->source, &sys->cfg);
+                vout_display_PlacePicture(&sys->place, _voutDisplay->source, &sys->cfg.display);
             }
         }
 