aad924cd2b4a7bd7ed0e1bd6efd245a582b40a24
Thomas Guillem <thomas@gllm.fr>
2022-05-24 15:45:02 +0200

    clock: trace the calculated offset

--- src/clock/clock.c
+++ src/clock/clock.c
@@ -161,6 +161,12 @@ static vlc_tick_t vlc_clock_master_update(vlc_clock_t *clock,
         main_clock->offset =
             system_now - ((vlc_tick_t) (ts * main_clock->coeff / rate));
 
+        if (main_clock->tracer != NULL && clock->track_str_id)
+            vlc_tracer_Trace(main_clock->tracer, VLC_TRACE("type", "RENDER"),
+                             VLC_TRACE("id", clock->track_str_id),
+                             VLC_TRACE("offset", main_clock->offset),
+                             VLC_TRACE_END);
+
         main_clock->last = clock_point_Create(system_now, ts);
 
         main_clock->rate = rate;
