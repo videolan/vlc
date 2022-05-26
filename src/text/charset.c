9791d0cd3e50093b0a4bd246f5528aded6efe707
Rémi Denis-Courmont <remi@remlab.net>
2022-05-26 21:26:25 +0300

    charset: inline trivial us_atof()

--- src/text/charset.c
+++ src/text/charset.c
@@ -71,11 +71,6 @@ float us_strtof( const char *str, char **end )
     return res;
 }
 
-double us_atof( const char *str )
-{
-    return us_strtod( str, NULL );
-}
-
 int us_vasprintf( char **ret, const char *format, va_list ap )
 {
     locale_t loc = newlocale( LC_NUMERIC_MASK, "C", NULL );
