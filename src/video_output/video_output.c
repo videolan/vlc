639fde42fa5ec187168ec07d48292b01f8300684
Steve Lhomme <robux4@ycbcr.xyz>
2022-06-01 14:14:59 +0200

    video_output: remove global return redisplay test
    It's in each section before.

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -1452,12 +1452,6 @@ static vlc_tick_t DisplayPicture(vout_thread_sys_t *vout)
         return vlc_tick_now() + VOUT_REDISPLAY_DELAY;
     }
 
-    if (!first && !refresh && next == NULL) {
-        // nothing changed, wait until the next deadline or a control
-        vlc_tick_t max_deadline = vlc_tick_now() + VOUT_REDISPLAY_DELAY;
-        return __MIN(date_refresh, max_deadline);
-    }
-
     RenderPicture(vout, render_now);
     if (render_now)
         return vlc_tick_now() + VOUT_REDISPLAY_DELAY;
