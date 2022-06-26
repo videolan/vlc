videolan/vlc

commit 8a1f453f3f9560fd8c61c5b7fddeb8d9e5d5c95b

wasapi: write output values when have no errors

modules/audio_output/wasapi.c
@@ -526,9 +526,6 @@ static void LogWaveFormat(struct vlc_logger *l, const WAVEFORMATEX *restrict wf)
 static int vlc_FromWave(const WAVEFORMATEX *restrict wf,
                         audio_sample_format_t *restrict audio)
 {
-    audio->i_rate = wf->nSamplesPerSec;
-    audio->i_physical_channels = 0;
-
     if (wf->wFormatTag == WAVE_FORMAT_EXTENSIBLE &&
         wf->cbSize >= sizeof(WAVEFORMATEXTENSIBLE) - sizeof(WAVEFORMATEX))
     {
@@ -573,6 +570,9 @@ static int vlc_FromWave(const WAVEFORMATEX *restrict wf,
     }
     else
         return -1;
+
+    audio->i_rate = wf->nSamplesPerSec;
+    audio->i_physical_channels = 0;
 
     aout_FormatPrepare (audio);
 

commit 22590583f4ff444ffe83a74945b18344de30af9e

wasapi: fix no audio output due to invalid i_physical_channels
Regression from 8a1f453f3f9560fd8c61c5b7fddeb8d9e5d5c95b.

modules/audio_output/wasapi.c
@@ -526,6 +526,7 @@ static void LogWaveFormat(struct vlc_logger *l, const WAVEFORMATEX *restrict wf)
 static int vlc_FromWave(const WAVEFORMATEX *restrict wf,
                         audio_sample_format_t *restrict audio)
 {
+    uint32_t physical_channels = 0;
     if (wf->wFormatTag == WAVE_FORMAT_EXTENSIBLE &&
         wf->cbSize >= sizeof(WAVEFORMATEXTENSIBLE) - sizeof(WAVEFORMATEX))
     {
@@ -566,13 +567,13 @@ static int vlc_FromWave(const WAVEFORMATEX *restrict wf,
 
         for (unsigned i = 0; chans_in[i]; i++)
             if (wfe->dwChannelMask & chans_in[i])
-                audio->i_physical_channels |= pi_vlc_chan_order_wg4[i];
+                physical_channels |= pi_vlc_chan_order_wg4[i];
     }
     else
         return -1;
 
     audio->i_rate = wf->nSamplesPerSec;
-    audio->i_physical_channels = 0;
+    audio->i_physical_channels = physical_channels;
 
     aout_FormatPrepare (audio);
 