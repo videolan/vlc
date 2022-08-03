f74d86e194caec7a6203a35a2a19893cb87f56b5
Steve Lhomme <robux4@ycbcr.xyz>
2022-08-03 13:45:46 +0200

    d3d_shaders: handle all full YUV to full RGB conversions

--- modules/video_output/win32/d3d_shaders.c
+++ modules/video_output/win32/d3d_shaders.c
@@ -380,6 +380,18 @@ void D3D_SetupQuad(vlc_object_t *o, const video_format_t *fmt, d3d_quad_t *quad,
 #define COLOR_COEFF_FULL_8_UV     ((double)(COLOR_SHIFT_FULL_8_MAX_Y - COLOR_SHIFT_FULL_8_MIN_Y) / 2.0f)
 #define COLOR_SHIFT_FULL_8_UV     ((double)128)
 
+#define COLOR_SHIFT_FULL_10_MIN_Y   0
+#define COLOR_SHIFT_FULL_10_MAX_Y   COLOR_COEFF_FULL_10_RGB
+#define COLOR_COEFF_FULL_10_Y      ((double)(COLOR_SHIFT_FULL_10_MAX_Y - COLOR_SHIFT_FULL_10_MIN_Y))
+#define COLOR_COEFF_FULL_10_UV     ((double)(COLOR_SHIFT_FULL_10_MAX_Y - COLOR_SHIFT_FULL_10_MIN_Y) / 2.0f)
+#define COLOR_SHIFT_FULL_10_UV     ((double)512)
+
+#define COLOR_SHIFT_FULL_12_MIN_Y   0
+#define COLOR_SHIFT_FULL_12_MAX_Y   COLOR_COEFF_FULL_12_RGB
+#define COLOR_COEFF_FULL_12_Y      ((double)(COLOR_SHIFT_FULL_12_MAX_Y - COLOR_SHIFT_FULL_12_MIN_Y))
+#define COLOR_COEFF_FULL_12_UV     ((double)(COLOR_SHIFT_FULL_12_MAX_Y - COLOR_SHIFT_FULL_12_MIN_Y) / 2.0f)
+#define COLOR_SHIFT_FULL_12_UV     ((double)2048)
+
 #define COLOR_SHIFT_STUDIO_8_MIN_RGB   16
 #define COLOR_SHIFT_STUDIO_8_MAX_RGB   235
 #define COLOR_COEFF_STUDIO_8_RGB      ((double)(COLOR_SHIFT_STUDIO_8_MAX_RGB - COLOR_SHIFT_STUDIO_8_MIN_RGB))
@@ -444,13 +456,21 @@ void D3D_SetupQuad(vlc_object_t *o, const video_format_t *fmt, d3d_quad_t *quad,
     COLOR_MATRIX_YUV2RGB(601,8,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(709,8,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(2020,8,STUDIO,FULL);
-    COLOR_MATRIX_YUV2RGB(709,8,FULL,FULL);
     COLOR_MATRIX_YUV2RGB(601,10,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(709,10,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(2020,10,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(601,12,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(709,12,STUDIO,FULL);
     COLOR_MATRIX_YUV2RGB(2020,12,STUDIO,FULL);
+    COLOR_MATRIX_YUV2RGB(601,8,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(709,8,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(2020,8,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(601,10,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(709,10,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(2020,10,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(601,12,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(709,12,FULL,FULL);
+    COLOR_MATRIX_YUV2RGB(2020,12,FULL,FULL);
 
     static const FLOAT COLORSPACE_FULL_RGBA_TO_BT601_YUV[4*3] = {
         0.299000f,  0.587000f,  0.114000f, 0.f,
@@ -477,28 +497,64 @@ void D3D_SetupQuad(vlc_object_t *o, const video_format_t *fmt, d3d_quad_t *quad,
     {
         switch (fmt->space){
             case COLOR_SPACE_BT709:
-                if (quad->textureFormat->bitsPerChannel == 12)
-                    ppColorspace = COLORSPACE_BT709_STUDIO_12_TO_FULL_RGBA;
-                else if (quad->textureFormat->bitsPerChannel == 10)
-                    ppColorspace = COLORSPACE_BT709_STUDIO_10_TO_FULL_RGBA;
+                if (fmt->color_range == COLOR_RANGE_FULL)
+                {
+                    if (quad->textureFormat->bitsPerChannel == 12)
+                        ppColorspace = COLORSPACE_BT709_FULL_12_TO_FULL_RGBA;
+                    else if (quad->textureFormat->bitsPerChannel == 10)
+                        ppColorspace = COLORSPACE_BT709_FULL_10_TO_FULL_RGBA;
+                    else
+                        ppColorspace = COLORSPACE_BT709_FULL_8_TO_FULL_RGBA;
+                }
                 else
-                    ppColorspace = COLORSPACE_BT709_STUDIO_8_TO_FULL_RGBA;
+                {
+                    if (quad->textureFormat->bitsPerChannel == 12)
+                        ppColorspace = COLORSPACE_BT709_STUDIO_12_TO_FULL_RGBA;
+                    else if (quad->textureFormat->bitsPerChannel == 10)
+                        ppColorspace = COLORSPACE_BT709_STUDIO_10_TO_FULL_RGBA;
+                    else
+                        ppColorspace = COLORSPACE_BT709_STUDIO_8_TO_FULL_RGBA;
+                }
                 break;
             case COLOR_SPACE_BT2020:
-                if (quad->textureFormat->bitsPerChannel == 12)
-                    ppColorspace = COLORSPACE_BT2020_STUDIO_12_TO_FULL_RGBA;
-                else if (quad->textureFormat->bitsPerChannel == 10)
-                    ppColorspace = COLORSPACE_BT2020_STUDIO_10_TO_FULL_RGBA;
+                if (fmt->color_range == COLOR_RANGE_FULL)
+                {
+                    if (quad->textureFormat->bitsPerChannel == 12)
+                        ppColorspace = COLORSPACE_BT2020_FULL_12_TO_FULL_RGBA;
+                    else if (quad->textureFormat->bitsPerChannel == 10)
+                        ppColorspace = COLORSPACE_BT2020_FULL_10_TO_FULL_RGBA;
+                    else
+                        ppColorspace = COLORSPACE_BT2020_FULL_8_TO_FULL_RGBA;
+                }
                 else
-                    ppColorspace = COLORSPACE_BT2020_STUDIO_8_TO_FULL_RGBA;
+                {
+                    if (quad->textureFormat->bitsPerChannel == 12)
+                        ppColorspace = COLORSPACE_BT2020_STUDIO_12_TO_FULL_RGBA;
+                    else if (quad->textureFormat->bitsPerChannel == 10)
+                        ppColorspace = COLORSPACE_BT2020_STUDIO_10_TO_FULL_RGBA;
+                    else
+                        ppColorspace = COLORSPACE_BT2020_STUDIO_8_TO_FULL_RGBA;
+                }
                 break;
             case COLOR_SPACE_BT601:
-                if (quad->textureFormat->bitsPerChannel == 12)
-                    ppColorspace = COLORSPACE_BT601_STUDIO_12_TO_FULL_RGBA;
-                else if (quad->textureFormat->bitsPerChannel == 10)
-                    ppColorspace = COLORSPACE_BT601_STUDIO_10_TO_FULL_RGBA;
+                if (fmt->color_range == COLOR_RANGE_FULL)
+                {
+                    if (quad->textureFormat->bitsPerChannel == 12)
+                        ppColorspace = COLORSPACE_BT601_FULL_12_TO_FULL_RGBA;
+                    else if (quad->textureFormat->bitsPerChannel == 10)
+                        ppColorspace = COLORSPACE_BT601_FULL_10_TO_FULL_RGBA;
+                    else
+                        ppColorspace = COLORSPACE_BT601_FULL_8_TO_FULL_RGBA;
+                }
                 else
-                    ppColorspace = COLORSPACE_BT601_STUDIO_8_TO_FULL_RGBA;
+                {
+                    if (quad->textureFormat->bitsPerChannel == 12)
+                        ppColorspace = COLORSPACE_BT601_STUDIO_12_TO_FULL_RGBA;
+                    else if (quad->textureFormat->bitsPerChannel == 10)
+                        ppColorspace = COLORSPACE_BT601_STUDIO_10_TO_FULL_RGBA;
+                    else
+                        ppColorspace = COLORSPACE_BT601_STUDIO_8_TO_FULL_RGBA;
+                }
                 break;
             default:
             case COLOR_SPACE_UNDEF:
