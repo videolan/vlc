videolan/vlc

commit d0c4c5a9072c779bbec89a0600caf31e279478cd

win32: move thread priority init to vlc_thread_setup()
Fixes serialization.

src/win32/thread.c
@@ -987,12 +987,30 @@ static CRITICAL_SECTION setup_lock; /* FIXME: use INIT_ONCE */
 void vlc_threads_setup(libvlc_int_t *vlc)
 {
     EnterCriticalSection(&setup_lock);
-    if (mdate_selected == mdate_default)
+    if (mdate_selected != mdate_default)
     {
-        if (!SelectClockSource((vlc != NULL) ? VLC_OBJECT(vlc) : NULL))
-            abort();
-        assert(mdate_selected != mdate_default);
+        LeaveCriticalSection(&setup_lock);
+        return;
     }
+
+    if (!SelectClockSource((vlc != NULL) ? VLC_OBJECT(vlc) : NULL))
+        abort();
+    assert(mdate_selected != mdate_default);
+
+#if !VLC_WINSTORE_APP
+    /* Raise default priority of the current process */
+#ifndef ABOVE_NORMAL_PRIORITY_CLASS
+#   define ABOVE_NORMAL_PRIORITY_CLASS 0x00008000
+#endif
+    if (var_InheritBool(vlc, "high-priority"))
+    {
+        if (SetPriorityClass(GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS)
+         || SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS))
+            msg_Dbg(vlc, "raised process priority");
+        else
+            msg_Dbg(vlc, "could not raise process priority");
+    }
+#endif
     LeaveCriticalSection(&setup_lock);
 }
 

src/win32/specific.c
@@ -97,23 +97,6 @@ typedef struct
 void system_Configure( libvlc_int_t *p_this, int i_argc, const char *const ppsz_argv[] )
 {
 #if !VLC_WINSTORE_APP
-    /* Raise default priority of the current process */
-#ifndef ABOVE_NORMAL_PRIORITY_CLASS
-#   define ABOVE_NORMAL_PRIORITY_CLASS 0x00008000
-#endif
-    if( var_InheritBool( p_this, "high-priority" ) )
-    {
-        if( SetPriorityClass( GetCurrentProcess(), ABOVE_NORMAL_PRIORITY_CLASS )
-             || SetPriorityClass( GetCurrentProcess(), HIGH_PRIORITY_CLASS ) )
-        {
-            msg_Dbg( p_this, "raised process priority" );
-        }
-        else
-        {
-            msg_Dbg( p_this, "could not raise process priority" );
-        }
-    }
-
     if( var_InheritBool( p_this, "one-instance" )
      || ( var_InheritBool( p_this, "one-instance-when-started-from-file" )
        && var_InheritBool( p_this, "started-from-file" ) ) )

commit 6d233f3fc5168701f4f4801d0ec094ab14945983

win32: threads: do not use mdate_perf as the default clock source
It relies on clk.perf.freq.QuadPart which is uninitialized and unknown until
SelectClockSource() is called. So it calls vlc_threads_setup() each time until
vlc_threads_setup() is called with the libvlc instance.

Except vlc_threads_setup(NULL) will crash on the
var_InheritBool(vlc, "high-priority") call. It's broken since
d0c4c5a9072c779bbec89a0600caf31e279478cd

Usable candidates at startup are:
* mdate_interrupt which is a counter in 100 ns, not a wall clock
* mdate_tick which is a counter in 1 ms, not a wall clock
* mdate_perf_100ns which is a clock in 100 ns but we don't know if the system
uses that resolution
* mdate_wall which is a clock in 100 ns resolution (slowest)

mdate_wall gives the better results and should provide a safe transition with
mdate_perf/mdate_perf_100ns values.

src/win32/thread.c
@@ -583,8 +583,7 @@ static vlc_tick_t mdate_wall (void)
 
 static vlc_tick_t mdate_default(void)
 {
-    vlc_threads_setup(NULL);
-    return mdate_perf();
+    return mdate_wall();
 }
 
 static vlc_tick_t (*mdate_selected) (void) = mdate_default;