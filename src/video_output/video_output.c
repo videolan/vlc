videolan/vlc

commit 354f3aae4dacfbfff93bebcd05aa89294cd48896

vout: display: window: add ICC profile glue
The window backend triggers a callback of the vout, and the vout
forwards this to the display using another callback. There are several
unfortunate layers of glue and indirections in the callchain. In
particular, we need to persist these ICC profile objects in some layer,
because of init order between windowing system and the window / vout
display module itself. I've chosen to add them to `vout_display_cfg_t`
because it fits well with the other "dynamic window state" properties in
there.

This approach also allows capable vouts to read directly from the
allocated ICC profile memory, minimizing the number of memcpys.

src/video_output/video_output.c
@@ -652,6 +652,24 @@ void vout_ChangeViewpoint(vout_thread_t *vout,
     vlc_mutex_unlock(&sys->display_lock);
 }
 
+void vout_ChangeIccProfile(vout_thread_t *vout,
+                           vlc_icc_profile_t *profile)
+{
+    vout_thread_sys_t *sys = VOUT_THREAD_TO_SYS(vout);
+    assert(!sys->dummy);
+
+    vlc_mutex_lock(&sys->window_lock);
+    free(sys->display_cfg.icc_profile);
+    sys->display_cfg.icc_profile = profile;
+
+    vlc_mutex_lock(&sys->display_lock);
+    vlc_mutex_unlock(&sys->window_lock);
+
+    if (sys->display != NULL)
+        vout_SetDisplayIccProfile(sys->display, profile);
+    vlc_mutex_unlock(&sys->display_lock);
+}
+
 /* */
 static void VoutGetDisplayCfg(vout_thread_sys_t *p_vout, const video_format_t *fmt, vout_display_cfg_t *cfg)
 {
@@ -1883,6 +1901,7 @@ void vout_Release(vout_thread_t *vout)
     }
 
     free(sys->splitter_name);
+    free(sys->display_cfg.icc_profile);
 
     if (sys->dec_device)
         vlc_decoder_device_Release(sys->dec_device);
@@ -1989,6 +2008,7 @@ vout_thread_t *vout_Create(vlc_object_t *object)
 
     /* Display */
     sys->display = NULL;
+    sys->display_cfg.icc_profile = NULL;
     vlc_mutex_init(&sys->display_lock);
 
     /* Window */

include/vlc_vout_display.h
@@ -116,6 +116,7 @@ struct vout_display_placement {
 typedef struct vout_display_cfg {
     struct vlc_window *window; /**< Window */
     struct vout_display_placement display; /**< Display placement properties */
+    vlc_icc_profile_t *icc_profile; /**< Currently active ICC profile */
     vlc_viewpoint_t viewpoint;
 } vout_display_cfg_t;
 
@@ -310,6 +311,15 @@ struct vlc_display_operations
      * \param vp viewpoint to use on the next render
      */
     int        (*set_viewpoint)(vout_display_t *, const vlc_viewpoint_t *vp);
+
+    /**
+     * Notifies a change in output ICC profile.
+     *
+     * May be NULL. Memory owned by the caller.
+     *
+     * \param prof new ICC profile associated with display, or NULL for none
+     */
+    void       (*set_icc_profile)(vout_display_t *, const vlc_icc_profile_t *prof);
 };
 
 struct vout_display_t {

include/vlc_window.h
@@ -43,6 +43,7 @@
 struct vlc_window;
 struct wl_display;
 struct wl_surface;
+typedef struct vlc_icc_profile_t vlc_icc_profile_t;
 
 /**
  * Window handle type.
@@ -311,6 +312,18 @@ struct vlc_window_callbacks {
      */
     void (*output_event)(struct vlc_window *,
                          const char *id, const char *desc);
+
+    /**
+     * Callback for ICC profile update.
+     *
+     * This can happen either because of the window being moved to a different
+     * display, or because the ICC profile associated with a display is
+     * updated. Memory transfers to the callee.
+     *
+     * \param profile ICC profile associated with the window, or NULL to
+     *                indicate absence of an ICC profile
+     */
+    void (*icc_event)(struct vlc_window *, vlc_icc_profile_t *profile);
 };
 
 /**
@@ -733,6 +746,22 @@ static inline void vlc_window_ReportOutputDevice(vlc_window_t *window,
 {
     if (window->owner.cbs->output_event != NULL)
         window->owner.cbs->output_event(window, id, name);
+}
+
+/**
+ * Reports a change to the currently active ICC profile.
+ *
+ * \param window the window reporting the ICC profile
+ * \param prof the profile data, or NULL. Ownership transfers to callee
+ */
+static inline void vlc_window_ReportICCProfile(vlc_window_t *window,
+                                               vlc_icc_profile_t *prof)
+{
+    if (window->owner.cbs->icc_event != NULL) {
+        window->owner.cbs->icc_event(window, prof);
+    } else {
+        free(prof);
+    }
 }
 
 /** @} */

src/video_output/display.c
@@ -629,6 +629,15 @@ void vout_SetDisplayViewpoint(vout_display_t *vd,
     }
 }
 
+void vout_SetDisplayIccProfile(vout_display_t *vd,
+                               const vlc_icc_profile_t *profile)
+{
+    vout_display_priv_t *osys = container_of(vd, vout_display_priv_t, display);
+
+    if (vd->ops->set_icc_profile)
+        vd->ops->set_icc_profile(vd, profile);
+}
+
 vout_display_t *vout_display_New(vlc_object_t *parent,
                                  const video_format_t *source,
                                  vlc_video_context *vctx,

src/video_output/video_window.c
@@ -225,6 +225,20 @@ static void vout_display_window_OutputEvent(vlc_window_t *window,
         msg_Dbg(window, "fullscreen output %s removed", name);
 }
 
+static void vout_display_window_IccEvent(vlc_window_t *window,
+                                         vlc_icc_profile_t *profile)
+{
+    vout_display_window_t *state = window->owner.sys;
+    vout_thread_t *vout = state->vout;
+
+    if (profile != NULL)
+        msg_Dbg(window, "window got ICC profile (%zu bytes)", profile->size);
+    else
+        msg_Dbg(window, "window ICC profile cleared");
+
+    vout_ChangeIccProfile(vout, profile);
+}
+
 static const struct vlc_window_callbacks vout_display_window_cbs = {
     .resized = vout_display_window_ResizeNotify,
     .closed = vout_display_window_CloseNotify,
@@ -234,6 +248,7 @@ static const struct vlc_window_callbacks vout_display_window_cbs = {
     .mouse_event = vout_display_window_MouseEvent,
     .keyboard_event = vout_display_window_KeyboardEvent,
     .output_event = vout_display_window_OutputEvent,
+    .icc_event = vout_display_window_IccEvent,
 };
 
 void vout_display_window_SetMouseHandler(vlc_window_t *window,

src/video_output/vout_internal.h
@@ -164,6 +164,7 @@ void vout_ControlChangeSubSources(vout_thread_t *, const char *);
 void vout_ControlChangeSubFilters(vout_thread_t *, const char *);
 void vout_ChangeSpuChannelMargin(vout_thread_t *, enum vlc_vout_order order, int);
 void vout_ChangeViewpoint( vout_thread_t *, const vlc_viewpoint_t *);
+void vout_ChangeIccProfile(vout_thread_t *, vlc_icc_profile_t *);
 
 void vout_FilterMouse(vout_thread_t *vout, vlc_mouse_t *mouse);
 

src/video_output/vout_wrapper.h
@@ -39,6 +39,7 @@ void vout_SetDisplayZoom(vout_display_t *, unsigned num, unsigned den);
 void vout_SetDisplayAspect(vout_display_t *, unsigned num, unsigned den);
 void vout_SetDisplayCrop(vout_display_t *, const struct vout_crop *);
 void vout_SetDisplayViewpoint(vout_display_t *, const vlc_viewpoint_t *);
+void vout_SetDisplayIccProfile(vout_display_t *, const vlc_icc_profile_t *);
 
 #endif /* LIBVLC_VOUT_WRAPPER_H */
 

commit 575bef6915ed06d34ede68eae945e6d93aebf03c

vout: fix deadlock on ICC profile update
This was taking more locks than necessary, causing a possible deadlock,
because window events are not supposed to lock the window lock.

The display lock is more than sufficient to guard access to this field.

Fixes: 354f3aae4dacfbfff93bebcd05aa89294cd48896

src/video_output/video_output.c
@@ -681,13 +681,9 @@ void vout_ChangeIccProfile(vout_thread_t *vout,
     vout_thread_sys_t *sys = VOUT_THREAD_TO_SYS(vout);
     assert(!sys->dummy);
 
-    vlc_mutex_lock(&sys->window_lock);
+    vlc_queuedmutex_lock(&sys->display_lock);
     free(sys->display_cfg.icc_profile);
     sys->display_cfg.icc_profile = profile;
-
-    vlc_queuedmutex_lock(&sys->display_lock);
-    vlc_mutex_unlock(&sys->window_lock);
-
     if (sys->display != NULL)
         vout_SetDisplayIccProfile(sys->display, profile);
     vlc_queuedmutex_unlock(&sys->display_lock);