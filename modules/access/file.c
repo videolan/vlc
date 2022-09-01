videolan/vlc

commit a89c7662b3cc7956547ed00730a002fa8a962a26

access: file: simplify the check of remote path on Windows
PathIsNetworkPathW() is a basic function that only checks for \\ and
[a-zA-Z]:\\ pathes [1]. It's also not available on UWP apps.

We can do the same by checking the UTF-8 string directly without even a
conversion to wide char string.

[1] https://docs.microsoft.com/en-us/windows/win32/api/shlwapi/nf-shlwapi-pathisnetworkpathw

modules/access/file.c
@@ -47,9 +47,6 @@
 #if defined( _WIN32 )
 #   include <io.h>
 #   include <ctype.h>
-#if !defined(VLC_WINSTORE_APP)
-#   include <shlwapi.h> // for PathIsNetworkPathW
-#endif
 #else
 #   include <unistd.h>
 #endif
@@ -120,17 +117,25 @@ static bool IsRemote (int fd)
 }
 # define IsRemote(fd,path) IsRemote(fd)
 
-#else /* _WIN32 || __OS2__ */
+#elif defined(_WIN32)
+
+static bool IsRemote(int fd, const char *path)
+{
+    VLC_UNUSED(fd);
+
+    size_t len = strlen(path);
+    if (len < 2)
+        return false;
+    if (path[0] == '\\' && path[1] == '\\')
+        return true;
+    return false;
+}
+
+#else /* __OS2__ */
+
 static bool IsRemote (const char *path)
 {
-# if !defined(__OS2__) && !defined(VLC_WINSTORE_APP)
-    wchar_t *wpath = ToWide (path);
-    bool is_remote = (wpath != NULL && PathIsNetworkPathW (wpath));
-    free (wpath);
-    return is_remote;
-# else
     return (! strncmp(path, "\\\\", 2));
-# endif
 }
 # define IsRemote(fd,path) IsRemote(path)
 #endif

modules/access/Makefile.am
@@ -21,9 +21,6 @@ endif
 
 libfilesystem_plugin_la_SOURCES = access/fs.h access/file.c access/directory.c access/fs.c
 libfilesystem_plugin_la_CPPFLAGS = $(AM_CPPFLAGS)
-if HAVE_WIN32
-libfilesystem_plugin_la_LIBADD = -lshlwapi
-endif
 access_LTLIBRARIES += libfilesystem_plugin.la
 
 if HAVE_EMSCRIPTEN

commit 8aaf4675e6ec513df1c784bbf0eb1d45aa1e7d00

access: file: fix detection of mapped network drives
Regression from a89c7662b3cc7956547ed00730a002fa8a962a26 that is still usable
in UWP.

modules/access/file.c
@@ -128,6 +128,27 @@ static bool IsRemote(int fd, const char *path)
         return false;
     if (path[0] == '\\' && path[1] == '\\')
         return true;
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) ||  NTDDI_VERSION >= NTDDI_WIN10_RS3
+    if (path[1] == ':')
+    {
+        char drive[4];
+        drive[0] = path[0]; // can only be < 0x80 if second char is ':'
+        drive[1] = ':';
+        drive[2] = '\\';
+        drive[3] = '\0';
+        UINT driveType = GetDriveTypeA(drive);
+        switch (driveType)
+        {
+            case DRIVE_FIXED:
+            case DRIVE_REMOVABLE: // but a floppy drive is slower than network
+            case DRIVE_RAMDISK:
+            case DRIVE_CDROM:
+                return false;
+            default:
+                return true;
+        }
+    }
+#endif
     return false;
 }
 