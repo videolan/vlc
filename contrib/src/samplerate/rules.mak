videolan/vlc

commit 8a2109a3947ef513e64e9ccae4f85e9399a7d243

contrib: build all code before installing for autotools contribs
If the contrib install sub-directories it may install some and fail later. We
should only install when we know everything built correctly.

Do each build step in a separate make line.

contrib/src/samplerate/rules.mak
@@ -24,6 +24,8 @@ samplerate: libsamplerate-$(SAMPLERATE_VERSION).tar.gz .sum-samplerate
 	$(REQUIRE_GPL)
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE)
+	+$(MAKEBUILD) -C src
+	+$(MAKEBUILD)
 	+$(MAKEBUILD) -C src install
 	+$(MAKEBUILD) install-data
 	touch $@

contrib/src/a52/rules.mak
@@ -27,6 +27,8 @@ endif
 	$(RECONF)
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE)
+	+$(MAKEBUILD) -C liba52
+	+$(MAKEBUILD) -C include
 	+$(MAKEBUILD) -C liba52 install
 	+$(MAKEBUILD) -C include install
 	touch $@

contrib/src/aribb24/rules.mak
@@ -31,5 +31,6 @@ DEPS_aribb24 = png $(DEPS_png)
 	cd $< && $(SHELL) ./bootstrap
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE)
-	+$(MAKEBUILD) && $(MAKEBUILD) install
+	+$(MAKEBUILD)
+	+$(MAKEBUILD) install
 	touch $@

contrib/src/aribb25/rules.mak
@@ -33,5 +33,6 @@ ARIBB25_CONF := --disable-b25
 	$(RECONF)
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(ARIBB25_CONF)
-	+$(MAKEBUILD) && $(MAKEBUILD) install
+	+$(MAKEBUILD)
+	+$(MAKEBUILD) install
 	touch $@

contrib/src/asdcplib/rules.mak
@@ -42,5 +42,6 @@ ASDCPLIB_CONF += CXXFLAGS="$(ASDCPLIB_CXXFLAGS)"
 	$(RECONF)
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(ASDCPLIB_CONF)
+	+$(MAKEBUILD)
 	+$(MAKEBUILD) install
 	touch $@

contrib/src/bluray/rules.mak
@@ -57,5 +57,6 @@ bluray: libbluray-$(BLURAY_VERSION).tar.bz2 .sum-bluray
 	cd $< && ./bootstrap
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(BLURAY_CONF)
+	+$(MAKEBUILD)
 	+$(MAKEBUILD) install
 	touch $@

contrib/src/breakpad/rules.mak
@@ -37,6 +37,7 @@ else
 	$(RECONF)
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(BREAKPAD_CONF)
+	+Configuration=Release $(MAKEBUILD)
 	+Configuration=Release $(MAKEBUILD) install
 endif
 	touch $@

contrib/src/caca/rules.mak
@@ -67,5 +67,6 @@ CACA_CONF += \
 .caca: caca
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(CACA_CONF)
+	+$(MAKEBUILD) -C $<
 	+$(MAKEBUILD) -C $< install
 	touch $@

commit 0de7192b5c2908d4f202e1107365625ccb5db006

contrib: samplerate: fix build
Really only build the library. Fixes #27380.

Regression from 8a2109a3947ef513e64e9ccae4f85e9399a7d243.

contrib/src/samplerate/rules.mak
@@ -25,7 +25,6 @@ samplerate: libsamplerate-$(SAMPLERATE_VERSION).tar.gz .sum-samplerate
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE)
 	+$(MAKEBUILD) -C src
-	+$(MAKEBUILD)
 	+$(MAKEBUILD) -C src install
 	+$(MAKEBUILD) install-data
 	touch $@