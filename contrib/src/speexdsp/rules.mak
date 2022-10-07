videolan/vlc

commit ef98e81ff014175fd7494c8313de8883d15834b3

contrib: speexdsp: fix build with android-ndk10c
In android-ndk10c, there is a "arm_neon.h" for all archs. The arm_neon.h for
x86* contains correspondence between neon and sse. In speexdsp, There is a
conflict between resample_sse.h and resample_neon.h. So disable neon if arch is
known to don't have neon.

Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

contrib/src/speexdsp/rules.mak
@@ -27,6 +27,9 @@ ifeq ($(ARCH),aarch64)
 # old neon, not compatible with aarch64
 SPEEXDSP_CONF += --disable-neon
 endif
+ifndef HAVE_NEON
+SPEEXDSP_CONF += --disable-neon
+endif
 ifndef HAVE_FPU
 SPEEXDSP_CONF += --enable-fixed-point
 ifeq ($(ARCH),arm)

commit e26e077bd7241ef65017e7df545b71a75189ead9

contrib: speexdsp: fix NEON support on Linux
The HAVE_NEON flag is only ever defined on select platforms. That is to
say first Android which had one build with and one without it. Later on,
it was also added to iOS.

It cannot be assumed that NEON is not there just because the flag is
unspecified. This silently broke NEON support on Linux, and literally
any other platform at the time this regression was introduced.

Strictly speaking, we could keep checking the flag on Android or iOS on
ARM or AArch64. But as of now, all four of those platforms always set
HAVE_NEON, so this would not have any effect. We still forcefully
disable NEON on Android-x86, which was the original purpose of this
stanza.

Regression from ef98e81ff014175fd7494c8313de8883d15834b3.

contrib/src/speexdsp/rules.mak
@@ -24,10 +24,10 @@ ifeq ($(ARCH),aarch64)
 # old neon, not compatible with aarch64
 SPEEXDSP_CONF += --disable-neon
 endif
-ifeq ($(ARCH),arm)
-ifndef HAVE_NEON
+ifeq ($(filter arm aarch64, $(ARCH)),)
+# The configure script checks for NEON C intrinsics only.
+# This leads to false positives on Android-x86.
 SPEEXDSP_CONF += --disable-neon
-endif
 endif
 ifndef HAVE_FPU
 SPEEXDSP_CONF += --enable-fixed-point