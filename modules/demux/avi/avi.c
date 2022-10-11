c6cc1df8e233818ca0ae1d423012f5099223ed3c
Francois Cartegnie <fcvlcdev@free.fr>
2022-10-11 11:47:20 +0200

    demux: avi: move around code to ease understanding

--- modules/demux/avi/avi.c
+++ modules/demux/avi/avi.c
@@ -1080,7 +1080,6 @@ static int Demux_Seekable( demux_t *p_demux )
         block_t         *p_frame;
         int64_t i_pos;
         unsigned int i;
-        size_t i_size;
 
         /* search for first chunk to be read */
         for( i = 0, b_done = true, i_pos = -1; i < p_sys->i_track; i++ )
@@ -1205,6 +1204,10 @@ static int Demux_Seekable( demux_t *p_demux )
         /* Set the track to use */
         tk = p_sys->track[i_track];
 
+        size_t i_size;
+        unsigned i_ck_remaining_bytes = tk->idx.p_entry[tk->i_idxposc].i_length -
+                                        tk->i_idxposb;
+
         /* read those data */
         if( tk->i_samplesize )
         {
@@ -1222,13 +1225,12 @@ static int Demux_Seekable( demux_t *p_demux )
                     i_toread = __MAX( i_toread, 100 );
                 }
             }
-            i_size = __MIN( tk->idx.p_entry[tk->i_idxposc].i_length -
-                                tk->i_idxposb,
-                            (size_t) i_toread );
+            i_size = __MIN( i_ck_remaining_bytes, (size_t) i_toread );
         }
         else
         {
-            i_size = tk->idx.p_entry[tk->i_idxposc].i_length;
+            assert(tk->i_idxposb == 0);
+            i_size = i_ck_remaining_bytes;
         }
 
         /* need to read and skip tag/header */
