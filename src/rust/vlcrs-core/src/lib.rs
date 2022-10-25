255e3bc68fec8313f38f47a6724a821dfe5e6527
Loïc <loic@videolabs.io>
2022-10-25 16:33:58 +0200

    vlcrs-core: add the vlcrs-core crate
    vlcrs-core is the crate that will be responsible for having the safe
    abstractions around the unsafe C functions and definitions.

---
+++ src/rust/vlcrs-core/src/lib.rs
@@ -0,0 +1,9 @@
+#![deny(unsafe_op_in_unsafe_fn)]
+
+//! The `vlcrs-core` crate.
+//!
+//! This crate contains the vlc core APIs that have been ported or
+//! wrapped for usage by Rust code in the modules and is shared by all of them.
+//!
+//! If you need a vlc core C API that is not ported or wrapped yet here,
+//! then do so first instead of bypassing this crate.
