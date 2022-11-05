videolan/vlc

commit 91f426996c98642b5180bb7375b188c93b2701ea

es: replace i_original_channels with i_chan_mode
Rename i_original_channels to i_chan_mode. For now, chan modes can be
DOLBYSTEREO or DUALMONO.

This new value, i_chan_mode, should only be set by demuxes/codecs/packetizers
if there is a special stereo mode to handle.

modules/audio_output/coreaudio_common.c
@@ -324,7 +324,7 @@ MapOutputLayout(audio_output_t *p_aout, audio_sample_format_t *fmt,
 {
     /* Fill VLC physical_channels from output layout */
     fmt->i_physical_channels = 0;
-    uint32_t i_original = fmt->i_original_channels & AOUT_CHAN_PHYSMASK;
+    uint32_t i_original = fmt->i_physical_channels;
     AudioChannelLayout *reslayout = NULL;
 
     if (outlayout == NULL)
@@ -434,7 +434,6 @@ MapOutputLayout(audio_output_t *p_aout, audio_sample_format_t *fmt,
 
 end:
     free(reslayout);
-    fmt->i_original_channels = fmt->i_physical_channels;
     aout_FormatPrepare(fmt);
 
     msg_Dbg(p_aout, "selected %d physical channels for device output",

commit b73f7c40d8a8355f188d10ce919f90ecfaf745d7

coreaudio: fix original value never set
Regression from 91f426996c98

This fixes the shortcut to map channels to Center or Stereo.

modules/audio_output/coreaudio_common.c
@@ -606,8 +606,8 @@ MapOutputLayout(audio_output_t *p_aout, audio_sample_format_t *fmt,
                 const AudioChannelLayout *outlayout, bool *warn_configuration)
 {
     /* Fill VLC physical_channels from output layout */
-    fmt->i_physical_channels = 0;
     uint32_t i_original = fmt->i_physical_channels;
+    fmt->i_physical_channels = 0;
     AudioChannelLayout *reslayout = NULL;
 
     if (outlayout == NULL)