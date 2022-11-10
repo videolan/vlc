30a1608cb313213a2b60e98c2f137351d359b11b
Steve Lhomme <robux4@ycbcr.xyz>
2022-11-10 13:37:18 +0100

    demux: add an owner to the packetizer object

--- src/input/demux.c
+++ src/input/demux.c
@@ -374,19 +374,24 @@ int demux_vaControlHelper( stream_t *s,
     return VLC_SUCCESS;
 }
 
+struct packetizer_owner
+{
+    decoder_t   packetizer;
+};
+
 /****************************************************************************
  * Utility functions
  ****************************************************************************/
 decoder_t *demux_PacketizerNew( vlc_object_t *p_demux, es_format_t *p_fmt, const char *psz_msg )
 {
-    decoder_t *p_packetizer;
-    p_packetizer = vlc_custom_create( p_demux, sizeof( *p_packetizer ),
+    struct packetizer_owner *owner = vlc_custom_create( p_demux, sizeof( *owner ),
                                       "demux packetizer" );
-    if( !p_packetizer )
+    if( !owner )
     {
         es_format_Clean( p_fmt );
         return NULL;
     }
+    decoder_t *p_packetizer = &owner->packetizer;
     p_fmt->b_packetized = false;
 
     p_packetizer->pf_decode = NULL;
