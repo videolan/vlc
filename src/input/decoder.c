videolan/vlc

commit 08b24866dc443c30df8f9d5ee4f7e2045bf62956

codec/packetizer: make the writable input format private
The writable fmt_in is kept in the decoder/packetizer owner.

The decoder/packetizer only has access to a const version.

src/input/decoder.c
@@ -115,6 +115,7 @@ enum reload
 struct vlc_input_decoder_t
 {
     decoder_t        dec;
+    es_format_t      dec_fmt_in;
     input_resource_t*p_resource;
     vlc_clock_t     *p_clock;
     const char *psz_id;
@@ -132,6 +133,7 @@ struct vlc_input_decoder_t
 
     /* Some decoders require already packetized data (ie. not truncated) */
     decoder_t *p_packetizer;
+    es_format_t pktz_fmt_in;
     bool b_packetizer;
 
     /* Current format in use by the output */
@@ -263,10 +265,11 @@ static inline bool vlc_input_decoder_IsSynchronous( const vlc_input_decoder_t *d
 /**
  * Load a decoder module
  */
-static int LoadDecoder( decoder_t *p_dec, bool b_packetizer,
+static int LoadDecoder( decoder_t *p_dec, bool b_packetizer, es_format_t *fmt_in,
                         const es_format_t *restrict p_fmt )
 {
-    decoder_Init( p_dec, p_fmt );
+    vlc_input_decoder_t *owner = dec_get_owner( p_dec );
+    decoder_Init( p_dec, fmt_in, p_fmt );
 
     p_dec->b_frame_drop_allowed = true;
 
@@ -286,6 +289,7 @@ static int LoadDecoder( decoder_t *p_dec, bool b_packetizer,
 
     if( !p_dec->p_module )
     {
+        es_format_Clean(fmt_in);
         decoder_Clean( p_dec );
         return -1;
     }
@@ -306,6 +310,7 @@ static int DecoderThread_Reload( vlc_input_decoder_t *p_owner,
     }
 
     /* Restart the decoder module */
+    es_format_Clean( &p_owner->dec_fmt_in );
     decoder_Clean( p_dec );
     p_owner->error = false;
 
@@ -325,7 +330,7 @@ static int DecoderThread_Reload( vlc_input_decoder_t *p_owner,
         }
     }
 
-    if( LoadDecoder( p_dec, false, &fmt_in ) )
+    if( LoadDecoder( p_dec, false, &p_owner->dec_fmt_in, &fmt_in ) )
     {
         p_owner->error = true;
         es_format_Clean( &fmt_in );
@@ -1946,7 +1951,7 @@ CreateDecoder( vlc_object_t *p_parent, const struct vlc_input_decoder_cfg *cfg )
             vlc_custom_create( p_parent, sizeof( decoder_t ), "packetizer" );
         if( p_owner->p_packetizer )
         {
-            if( LoadDecoder( p_owner->p_packetizer, true, fmt ) )
+            if( LoadDecoder( p_owner->p_packetizer, true, &p_owner->pktz_fmt_in, fmt ) )
             {
                 vlc_object_delete(p_owner->p_packetizer);
                 p_owner->p_packetizer = NULL;
@@ -1979,7 +1984,7 @@ CreateDecoder( vlc_object_t *p_parent, const struct vlc_input_decoder_cfg *cfg )
     }
 
     /* Find a suitable decoder/packetizer module */
-    if( LoadDecoder( p_dec, cfg->sout != NULL, fmt ) )
+    if( LoadDecoder( p_dec, cfg->sout != NULL, &p_owner->dec_fmt_in, fmt ) )
         return p_owner;
 
     assert( p_dec->p_fmt_in->i_cat == p_dec->fmt_out.i_cat && fmt->i_cat == p_dec->p_fmt_in->i_cat);
@@ -2027,6 +2032,8 @@ static void DeleteDecoder( vlc_input_decoder_t *p_owner, enum es_format_category
     msg_Dbg( p_dec, "killing decoder fourcc `%4.4s'",
              (char*)&p_dec->p_fmt_in->i_codec );
 
+    es_format_Clean( &p_owner->dec_fmt_in );
+    es_format_Clean( &p_owner->pktz_fmt_in );
     decoder_Clean( p_dec );
     if ( p_owner->out_pool )
     {

test/src/input/decoder.c
@@ -42,6 +42,7 @@
 struct decoder_owner
 {
     decoder_t dec;
+    es_format_t fmt_in;
     decoder_t *packetizer;
 };
 
@@ -89,7 +90,9 @@ static void queue_sub(decoder_t *dec, subpicture_t *p_subpic)
 static int decoder_load(decoder_t *decoder, bool is_packetizer,
                          const es_format_t *restrict fmt)
 {
-    decoder_Init( decoder, fmt );
+    struct decoder_owner *owner = dec_get_owner(decoder);
+
+    decoder_Init( decoder, &owner->fmt_in, fmt );
 
     decoder->b_frame_drop_allowed = true;
 
@@ -108,6 +111,7 @@ static int decoder_load(decoder_t *decoder, bool is_packetizer,
 
     if (!decoder->p_module)
     {
+        es_format_Clean( &owner->fmt_in );
         decoder_Clean( decoder );
         return VLC_EGENERIC;
     }
@@ -118,6 +122,7 @@ void test_decoder_destroy(decoder_t *decoder)
 {
     struct decoder_owner *owner = dec_get_owner(decoder);
 
+    es_format_Clean(&owner->fmt_in);
     decoder_Destroy(owner->packetizer);
     decoder_Destroy(decoder);
 }
@@ -223,6 +228,7 @@ int test_decoder_process(decoder_t *decoder, block_t *p_block)
             decoder->pf_decode(decoder, NULL);
 
             /* Reload decoder */
+            es_format_Clean( &owner->fmt_in );
             decoder_Clean(decoder);
             if (decoder_load(decoder, false, &packetizer->fmt_out) != VLC_SUCCESS)
             {

include/vlc_codec.h
@@ -105,8 +105,6 @@ struct decoder_t
     module_t *          p_module;
     void               *p_sys;
 
-    /* Input format ie from demuxer (XXX: a lot of fields could be invalid) */
-    es_format_t         fmt_in;
     /* Input format ie from demuxer (XXX: a lot of fields could be invalid),
        cannot be NULL */
     const es_format_t   *p_fmt_in;
@@ -396,8 +394,12 @@ VLC_API picture_t *decoder_NewPicture( decoder_t *dec );
  *
  * To be used by decoder owners.
  * By default frame drop is not allowed.
+ *
+ * @param dec the decoder to be initialized
+ * @param fmt_in the es_format_t where the decoder owner stores the input ES format
+ * @param fmt the input es_format_t used to initialize the decoder
  */
-VLC_API void decoder_Init( decoder_t *dec, const es_format_t * );
+VLC_API void decoder_Init( decoder_t *dec, es_format_t *fmt_in, const es_format_t *fmt );
 
 /**
  * Destroy a decoder and reset the structure.

modules/stream_out/mosaic_bridge.c
@@ -64,6 +64,7 @@ typedef struct
 struct decoder_owner
 {
     decoder_t dec;
+    es_format_t fmt_in;
     vlc_decoder_device *dec_dev;
     sout_stream_t *p_stream;
 };
@@ -315,6 +316,7 @@ static void ReleaseDecoder( decoder_t *p_dec )
         vlc_decoder_device_Release( p_owner->dec_dev );
         p_owner->dec_dev = NULL;
     }
+    es_format_Clean( &p_owner->fmt_in );
     decoder_Destroy( p_dec );
 }
 
@@ -341,7 +343,7 @@ static void *Add( sout_stream_t *p_stream, const es_format_t *p_fmt )
     if( !p_owner )
         return NULL;
     p_sys->p_decoder = &p_owner->dec;
-    decoder_Init( p_sys->p_decoder, p_fmt );
+    decoder_Init( p_sys->p_decoder, &p_owner->fmt_in, p_fmt );
 
     p_sys->p_decoder->b_frame_drop_allowed = true;
     p_sys->p_decoder->fmt_out = *p_sys->p_decoder->p_fmt_in;

modules/stream_out/sdi/SDIStream.cpp
@@ -205,6 +205,7 @@ const StreamID & AbstractStream::getID() const
 struct decoder_owner
 {
     decoder_t dec;
+    es_format_t fmt_in;
     AbstractDecodedStream *id;
     bool b_error;
     es_format_t last_fmt_update;
@@ -268,7 +269,7 @@ bool AbstractDecodedStream::init(const es_format_t *p_fmt)
     p_owner->id = this;
 
     p_decoder = &p_owner->dec;
-    decoder_Init( p_decoder, p_fmt );
+    decoder_Init( p_decoder, &p_owner->fmt_in, p_fmt );
 
     setCallbacks();
 
@@ -284,6 +285,7 @@ bool AbstractDecodedStream::init(const es_format_t *p_fmt)
     {
         es_format_Clean(&p_owner->decoder_out);
         es_format_Clean(&p_owner->last_fmt_update);
+        es_format_Clean(&p_owner->fmt_in);
         decoder_Destroy( p_decoder );
         p_decoder = NULL;
         return false;
@@ -418,6 +420,7 @@ void AbstractDecodedStream::ReleaseDecoder()
     p_owner = container_of(p_decoder, struct decoder_owner, dec);
     es_format_Clean(&p_owner->decoder_out);
     es_format_Clean(&p_owner->last_fmt_update);
+    es_format_Clean(&p_owner->fmt_in);
     decoder_Destroy( p_decoder );
     p_decoder = NULL;
 }

modules/stream_out/transcode/transcode.c
@@ -592,7 +592,7 @@ static void *Add( sout_stream_t *p_stream, const es_format_t *p_fmt )
     p_owner->p_stream = p_stream;
 
     id->p_decoder = &p_owner->dec;
-    decoder_Init( id->p_decoder, p_fmt );
+    decoder_Init( id->p_decoder, &p_owner->fmt_in, p_fmt );
 
     es_format_SetMeta( &id->p_decoder->fmt_out, id->p_decoder->p_fmt_in );
 
@@ -673,6 +673,7 @@ static void *Add( sout_stream_t *p_stream, const es_format_t *p_fmt )
     return id;
 
 error:
+    es_format_Clean( &p_owner->fmt_in );
     decoder_Destroy( id->p_decoder );
     DeleteSoutStreamID( id );
     return NULL;
@@ -683,6 +684,7 @@ static void Del( sout_stream_t *p_stream, void *_id )
     sout_stream_sys_t *p_sys = p_stream->p_sys;
     sout_stream_id_sys_t *id = (sout_stream_id_sys_t *)_id;
 
+    struct decoder_owner *p_owner = id->p_decoder ? dec_get_owner(id->p_decoder) : NULL;
     if( id->b_transcode )
     {
         int i_cat = id->p_decoder ? id->p_decoder->p_fmt_in->i_cat : UNKNOWN_ES;
@@ -690,6 +692,7 @@ static void Del( sout_stream_t *p_stream, void *_id )
         {
         case AUDIO_ES:
             Send( p_stream, id, NULL );
+            es_format_Clean( &p_owner->fmt_in );
             decoder_Destroy( id->p_decoder );
             vlc_mutex_lock( &p_sys->lock );
             if( id == p_sys->id_master_sync )
@@ -702,6 +705,7 @@ static void Del( sout_stream_t *p_stream, void *_id )
              * decoder/encoder might not even exist. */
             if(!id->b_error)
                 Send( p_stream, id, NULL );
+            es_format_Clean( &p_owner->fmt_in );
             decoder_Destroy( id->p_decoder );
             vlc_mutex_lock( &p_sys->lock );
             if( id == p_sys->id_video )
@@ -710,6 +714,7 @@ static void Del( sout_stream_t *p_stream, void *_id )
             transcode_video_clean( id );
             break;
         case SPU_ES:
+            es_format_Clean( &p_owner->fmt_in );
             decoder_Destroy( id->p_decoder );
             transcode_spu_clean( p_stream, id );
             break;
@@ -718,7 +723,12 @@ static void Del( sout_stream_t *p_stream, void *_id )
         }
         transcode_track_pcr_helper_Delete( id->pcr_helper );
     }
-    else decoder_Destroy( id->p_decoder );
+    else
+    {
+        if (p_owner)
+            es_format_Clean( &p_owner->fmt_in );
+        decoder_Destroy( id->p_decoder );
+    }
 
     if( id->downstream_id ) sout_StreamIdDel( p_stream->p_next, id->downstream_id );
 

modules/stream_out/transcode/transcode.h
@@ -156,6 +156,7 @@ struct sout_stream_id_sys_t
 struct decoder_owner
 {
     decoder_t dec;
+    es_format_t fmt_in;
     sout_stream_t *p_stream;
     sout_stream_id_sys_t *id;
 };

src/input/decoder_helpers.c
@@ -34,7 +34,7 @@
 #include <vlc_picture.h>
 #include "libvlc.h"
 
-void decoder_Init( decoder_t *p_dec, const es_format_t *restrict p_fmt )
+void decoder_Init( decoder_t *p_dec, es_format_t *restrict fmt_in, const es_format_t *restrict p_fmt )
 {
     p_dec->i_extra_picture_buffers = 0;
     p_dec->b_frame_drop_allowed = false;
@@ -45,8 +45,9 @@ void decoder_Init( decoder_t *p_dec, const es_format_t *restrict p_fmt )
     p_dec->pf_flush = NULL;
     p_dec->p_module = NULL;
 
-    es_format_Copy( &p_dec->fmt_in, p_fmt );
-    p_dec->p_fmt_in = &p_dec->fmt_in;
+    assert(fmt_in != NULL);
+    es_format_Copy( fmt_in, p_fmt );
+    p_dec->p_fmt_in = fmt_in;
     es_format_Init( &p_dec->fmt_out, p_fmt->i_cat, 0 );
 }
 
@@ -58,7 +59,6 @@ void decoder_Clean( decoder_t *p_dec )
         p_dec->p_module = NULL;
     }
 
-    es_format_Clean( &p_dec->fmt_in );
     es_format_Clean( &p_dec->fmt_out );
 
     if ( p_dec->p_description )

commit 2e928f40f1c8fd6656a9527f623cd3bdb1647f33

input: decoder: fix format cleaning on reload
When reloading the decoder, the decoder module can be triggering a
decoder_UpdateVideoFormat call which will check the format we've been
clearing.

Regression from 08b24866dc443c30df8f9d5ee4f7e2045bf62956.

Refs #27532

src/input/decoder.c
@@ -309,8 +309,8 @@ static int DecoderThread_Reload( vlc_input_decoder_t *p_owner,
     }
 
     /* Restart the decoder module */
-    es_format_Clean( &p_owner->dec_fmt_in );
     decoder_Clean( p_dec );
+    es_format_Clean( &p_owner->dec_fmt_in );
     p_owner->error = false;
 
     if( reload == RELOAD_DECODER_AOUT )