videolan/vlc

commit e0b6a09a8d8d0a3c0011b9cc3097b4373f3cab30

picture: use the ancillary API
Fixes #18762

src/misc/picture.c
@@ -38,6 +38,8 @@
 #include <vlc_image.h>
 #include <vlc_block.h>
 
+#include "ancillary.h"
+
 static void PictureDestroyContext( picture_t *p_picture )
 {
     picture_context_t *ctx = p_picture->context;
@@ -98,6 +100,9 @@ void picture_Reset( picture_t *p_picture )
     p_picture->i_nb_fields = 2;
     p_picture->b_top_field_first = false;
     PictureDestroyContext( p_picture );
+
+    picture_priv_t *priv = container_of(p_picture, picture_priv_t, picture);
+    vlc_ancillary_array_Clear(&priv->ancillaries);
 }
 
 /*****************************************************************************
@@ -219,6 +224,8 @@ static bool picture_InitPrivate(const video_format_t *restrict p_fmt,
         priv->gc.destroy = p_resource->pf_destroy;
     else
         priv->gc.destroy = picture_DestroyDummy;
+
+    vlc_ancillary_array_Init(&priv->ancillaries);
 
     return true;
 }
@@ -344,6 +351,7 @@ void picture_Destroy(picture_t *picture)
     picture_priv_t *priv = container_of(picture, picture_priv_t, picture);
     assert(priv->gc.destroy != NULL);
     priv->gc.destroy(picture);
+    vlc_ancillary_array_Clear(&priv->ancillaries);
     free(priv);
 }
 
@@ -395,6 +403,10 @@ void picture_CopyProperties( picture_t *p_dst, const picture_t *p_src )
     p_dst->b_progressive = p_src->b_progressive;
     p_dst->i_nb_fields = p_src->i_nb_fields;
     p_dst->b_top_field_first = p_src->b_top_field_first;
+
+    const picture_priv_t *src_priv = container_of(p_src, picture_priv_t, picture);
+    picture_priv_t *dst_priv = container_of(p_dst, picture_priv_t, picture);
+    vlc_ancillary_array_Dup(&dst_priv->ancillaries, &src_priv->ancillaries);
 }
 
 void picture_CopyPixels( picture_t *p_dst, const picture_t *p_src )
@@ -453,7 +465,25 @@ picture_t *picture_InternalClone(picture_t *picture,
 picture_t *picture_Clone(picture_t *picture)
 {
     picture_t *clone = picture_InternalClone(picture, picture_DestroyClone, picture);
+
+    const picture_priv_t *priv = container_of(picture, picture_priv_t, picture);
+    picture_priv_t *clone_priv = container_of(clone, picture_priv_t, picture);
+    vlc_ancillary_array_Dup(&clone_priv->ancillaries, &priv->ancillaries);
     return clone;
+}
+
+int
+picture_AttachAncillary(picture_t *pic, struct vlc_ancillary *ancillary)
+{
+    picture_priv_t *priv = container_of(pic, picture_priv_t, picture);
+    return vlc_ancillary_array_Insert(&priv->ancillaries, ancillary);
+}
+
+struct vlc_ancillary *
+picture_GetAncillary(picture_t *pic, vlc_ancillary_id id)
+{
+    picture_priv_t *priv = container_of(pic, picture_priv_t, picture);
+    return vlc_ancillary_array_Get(&priv->ancillaries, id);
 }
 
 /*****************************************************************************

include/vlc_picture.h
@@ -28,6 +28,9 @@
 #include <assert.h>
 #include <vlc_atomic.h>
 
+struct vlc_ancillary;
+typedef uint32_t vlc_ancillary_id;
+
 /**
  * \defgroup picture Generic picture API
  * \ingroup output
@@ -410,6 +413,33 @@ VLC_API void picture_Copy( picture_t *p_dst, const picture_t *p_src );
  * \return A clone picture on success, NULL on error.
  */
 VLC_API picture_t *picture_Clone(picture_t *pic);
+
+/**
+ * Attach an ancillary to the picture
+ *
+ * @warning the ancillary will be released only if the picture is created from
+ * picture_New(), and picture_Clone().
+ *
+ * @note Several ancillaries can be attached to a picture, but if two
+ * ancillaries are identified by the same ID, only the last one take
+ * precedence.
+ *
+ * @param pic the picture to attach an ancillary
+ * @param ancillary ancillary that will be held by the frame, can't be NULL
+ * @return VLC_SUCCESS in case of success, VLC_ENOMEM in case of alloc error
+ */
+VLC_API int
+picture_AttachAncillary(picture_t *pic, struct vlc_ancillary *ancillary);
+
+/**
+ * Return the ancillary identified by an ID
+ *
+ * @param id id of ancillary to request
+ * @return the ancillary or NULL if the ancillary for that particular id is
+ * not present
+ */
+VLC_API struct vlc_ancillary *
+picture_GetAncillary(picture_t *pic, vlc_ancillary_id id);
 
 /**
  * This function will export a picture to an encoded bitstream.

src/libvlccore.sym
@@ -308,6 +308,7 @@ net_Read
 net_SetCSCov
 net_Write
 NTPtime64
+picture_AttachAncillary
 picture_BlendSubpicture
 picture_Clone
 picture_CopyPixels
@@ -321,6 +322,7 @@ picture_fifo_New
 picture_fifo_IsEmpty
 picture_fifo_Pop
 picture_fifo_Push
+picture_GetAncillary
 picture_New
 picture_NewFromFormat
 picture_NewFromResource

src/misc/picture.h
@@ -22,6 +22,7 @@
 #include <stddef.h>
 
 #include <vlc_picture.h>
+struct vlc_ancillary;
 
 typedef struct
 {
@@ -31,6 +32,10 @@ typedef struct
         void (*destroy)(picture_t *);
         void *opaque;
     } gc;
+
+    /** Private ancillary struct. Don't use it directly, but use it via
+     * picture_AttachAncillary() and picture_GetAncillary(). */
+    struct vlc_ancillary **ancillaries;
 } picture_priv_t;
 
 void *picture_Allocate(int *, size_t);

commit 183398650f0e30846a2fd481efdc79ecb538ed92

picture: fix potential use-after-free on error
picture_InternalClone can fail and return NULL, so the clone picture
must not be used in case of error.

Regression from e0b6a09a8d8d0a3c0011b9cc3097b4373f3cab30.

src/misc/picture.c
@@ -465,6 +465,8 @@ picture_t *picture_InternalClone(picture_t *picture,
 picture_t *picture_Clone(picture_t *picture)
 {
     picture_t *clone = picture_InternalClone(picture, picture_DestroyClone, picture);
+    if (clone == NULL)
+        return NULL;
 
     const picture_priv_t *priv = container_of(picture, picture_priv_t, picture);
     picture_priv_t *clone_priv = container_of(clone, picture_priv_t, picture);