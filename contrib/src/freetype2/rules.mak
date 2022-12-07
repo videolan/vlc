videolan/vlc

commit d9c9482b253762312bcb5c0ab5043e34bb23575f

contrib: freetype2: build with CMake
So we don't use a home made build system that doesn't allow out of tree builds.

contrib/src/freetype2/rules.mak
@@ -20,12 +20,13 @@ freetype: freetype-$(FREETYPE2_VERSION).tar.xz .sum-freetype2
 
 DEPS_freetype2 = zlib $(DEPS_zlib)
 
-.freetype2: freetype
+FREETYPE_CONF = -DFT_DISABLE_ZLIB=OFF -DFT_DISABLE_PNG=ON -DFT_DISABLE_BZIP2=NO -DDISABLE_FORCE_DEBUG_POSTFIX:BOOL=ON
+
+.freetype2: freetype toolchain.cmake
 ifndef AD_CLAUSES
 	$(REQUIRE_GPL)
 endif
-	cd $< && cp builds/unix/install-sh .
-	sed -i.orig s/-ansi// $</builds/unix/configure
-	cd $< && GNUMAKE=$(MAKE) $(HOSTVARS) ./configure --with-harfbuzz=no --with-zlib=yes --without-png --with-bzip2=no $(HOSTCONF)
-	cd $< && $(MAKE) && $(MAKE) install
+	$(CMAKECLEAN)
+	$(HOSTVARS) $(CMAKE) $(FREETYPE_CONF)
+	+$(CMAKEBUILD) --target install
 	touch $@

commit 0f6b7fab25d8c79f078b0c7b7a98ea4ebe3e2b5a

contrib: freetype: actually disable bzip2
Regression from d9c9482b253762312bcb5c0ab5043e34bb23575f.

This fixes linking on macOS with static Qt from contribs:

Undefined symbols for architecture x86_64:
  "_BZ2_bzDecompress", referenced from:
      _ft_bzip2_file_fill_output in libfreetype.a(ftbzip2.c.o)
  "_BZ2_bzDecompressEnd", referenced from:
      _ft_bzip2_stream_io in libfreetype.a(ftbzip2.c.o)
      _ft_bzip2_stream_close in libfreetype.a(ftbzip2.c.o)
  "_BZ2_bzDecompressInit", referenced from:
      _FT_Stream_OpenBzip2 in libfreetype.a(ftbzip2.c.o)
      _ft_bzip2_stream_io in libfreetype.a(ftbzip2.c.o)
ld: symbol(s) not found for architecture x86_64

contrib/src/freetype2/rules.mak
@@ -20,7 +20,7 @@ freetype: freetype-$(FREETYPE2_VERSION).tar.xz .sum-freetype2
 
 DEPS_freetype2 = zlib $(DEPS_zlib)
 
-FREETYPE_CONF = -DFT_DISABLE_ZLIB=OFF -DFT_DISABLE_PNG=ON -DFT_DISABLE_BZIP2=NO \
+FREETYPE_CONF = -DFT_DISABLE_ZLIB=OFF -DFT_DISABLE_PNG=ON -DFT_DISABLE_BZIP2=ON \
                 -DDISABLE_FORCE_DEBUG_POSTFIX:BOOL=ON -DFT_DISABLE_HARFBUZZ=ON \
                 -DFT_DISABLE_BROTLI=ON
 