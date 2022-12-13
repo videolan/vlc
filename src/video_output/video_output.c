12497780d6b1d109f5bdfaaa0128a0a2506d0711
Francois Cartegnie <fcvlcdev@free.fr>
2022-12-13 16:11:49 +0100

    vout: enforce valid viewpoint range

--- src/video_output/video_output.c
+++ src/video_output/video_output.c
@@ -222,6 +222,7 @@ static void VoutFixFormat(video_format_t *dst, const video_format_t *src)
     dst->i_chroma = vlc_fourcc_GetCodec(VIDEO_ES, src->i_chroma);
     VoutFixFormatAR( dst );
     video_format_FixRgb(dst);
+    vlc_viewpoint_clip( &dst->pose );
 }
 
 static void VoutRenderWakeUpUrgent(vout_thread_sys_t *sys)
