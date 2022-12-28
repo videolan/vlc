videolan/vlc

commit 0aecdcabaa27f32dc1e3de1832d22d9766cdfae6

demux: mp4: refactor getting sample timings
(cherry picked from commit fa6e7fed73becb9f73362e8013860a2708b6f9dd) (edited)
edited:
* 3.0 uses mtime_t
Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

modules/demux/mp4/mp4.c
@@ -294,80 +294,93 @@ static es_out_id_t * MP4_AddTrackES( es_out_t *out, mp4_track_t *p_track )
     return p_es;
 }
 
-/* Return time in microsecond of a track */
-static inline int64_t MP4_TrackGetDTS( demux_t *p_demux, mp4_track_t *p_track )
+static stime_t MP4_ChunkGetSampleDTS( const mp4_chunk_t *p_chunk,
+                                      uint32_t i_sample )
 {
-    demux_sys_t *p_sys = p_demux->p_sys;
-    const mp4_chunk_t *p_chunk = &p_track->chunk[p_track->i_chunk];
-
-    unsigned int i_index = 0;
-    unsigned int i_sample = p_track->i_sample - p_chunk->i_sample_first;
-    int64_t i_dts = p_chunk->i_first_dts;
-
+    uint32_t i_index = 0;
+    stime_t sdts = p_chunk->i_first_dts;
     while( i_sample > 0 && i_index < p_chunk->i_entries_dts )
     {
         if( i_sample > p_chunk->p_sample_count_dts[i_index] )
         {
-            i_dts += p_chunk->p_sample_count_dts[i_index] *
+            sdts += p_chunk->p_sample_count_dts[i_index] *
                 p_chunk->p_sample_delta_dts[i_index];
-            i_sample -= p_chunk->p_sample_count_dts[i_index];
-            i_index++;
+            i_sample -= p_chunk->p_sample_count_dts[i_index++];
         }
         else
         {
-            i_dts += i_sample * p_chunk->p_sample_delta_dts[i_index];
+            sdts += i_sample * p_chunk->p_sample_delta_dts[i_index];
             break;
         }
     }
-
-    i_dts = MP4_rescale( i_dts, p_track->i_timescale, CLOCK_FREQ );
-
-    /* now handle elst */
-    if( p_track->p_elst && p_track->BOXDATA(p_elst)->i_entry_count )
-    {
-        MP4_Box_data_elst_t *elst = p_track->BOXDATA(p_elst);
-
-        /* convert to offset */
-        if( ( elst->i_media_rate_integer[p_track->i_elst] > 0 ||
-              elst->i_media_rate_fraction[p_track->i_elst] > 0 ) &&
-            elst->i_media_time[p_track->i_elst] > 0 )
-        {
-            i_dts -= MP4_rescale( elst->i_media_time[p_track->i_elst], p_track->i_timescale, CLOCK_FREQ );
-        }
-
-        /* add i_elst_time */
-        i_dts += MP4_rescale( p_track->i_elst_time, p_sys->i_timescale, CLOCK_FREQ );
-
-        if( i_dts < 0 ) i_dts = 0;
-    }
-
-    return i_dts;
+    return sdts;
 }
 
-static inline bool MP4_TrackGetPTSDelta( demux_t *p_demux, mp4_track_t *p_track,
-                                         int64_t *pi_delta )
+static bool MP4_ChunkGetSampleCTSDelta( const mp4_chunk_t *p_chunk,
+                                        uint32_t i_sample, stime_t *pi_delta )
 {
-    VLC_UNUSED( p_demux );
-    mp4_chunk_t *ck = &p_track->chunk[p_track->i_chunk];
-
-    unsigned int i_index = 0;
-    unsigned int i_sample = p_track->i_sample - ck->i_sample_first;
-
-    if( ck->p_sample_count_pts == NULL || ck->p_sample_offset_pts == NULL )
-        return false;
-
-    for( i_index = 0; i_index < ck->i_entries_pts ; i_index++ )
+    if( p_chunk->p_sample_count_pts && p_chunk->p_sample_offset_pts )
     {
-        if( i_sample < ck->p_sample_count_pts[i_index] )
+        for( uint32_t i_index = 0; i_index < p_chunk->i_entries_pts ; i_index++ )
         {
-            *pi_delta = MP4_rescale( ck->p_sample_offset_pts[i_index],
-                                     p_track->i_timescale, CLOCK_FREQ );
-            return true;
+            if( i_sample < p_chunk->p_sample_count_pts[i_index] )
+            {
+                *pi_delta = p_chunk->p_sample_offset_pts[i_index];
+                return true;
+            }
+            i_sample -= p_chunk->p_sample_count_pts[i_index];
         }
-
-        i_sample -= ck->p_sample_count_pts[i_index];
     }
     return false;
+}
+
+static void MP4_TrackTimeApplyELST( const mp4_track_t *p_track,
+                                    stime_t *pi_dts )
+{
+    if( !p_track->p_elst || !p_track->BOXDATA(p_elst)->i_entry_count )
+        return;
+
+    const MP4_Box_data_elst_t *elst = p_track->BOXDATA(p_elst);
+
+    /* convert to offset */
+    if( elst->i_media_time[p_track->i_elst] > 0 &&
+        ( elst->i_media_rate_integer[p_track->i_elst] > 0 ||
+          elst->i_media_rate_fraction[p_track->i_elst] > 0 ) )
+    {
+        *pi_dts -= elst->i_media_time[p_track->i_elst];
+    }
+
+    /* add i_elst_time */
+    *pi_dts += p_track->i_elst_time;
+
+    if( *pi_dts < 0 ) *pi_dts = 0;
+}
+
+/* Return time in microsecond of a track */
+static inline mtime_t MP4_TrackGetDTS( demux_t *p_demux, mp4_track_t *p_track )
+{
+    VLC_UNUSED(p_demux);
+    const mp4_chunk_t *p_chunk = &p_track->chunk[p_track->i_chunk];
+
+    stime_t sdts = MP4_ChunkGetSampleDTS( p_chunk,
+                                          p_track->i_sample - p_chunk->i_sample_first );
+
+    /* now handle elst */
+    MP4_TrackTimeApplyELST( p_track, &sdts );
+
+    return MP4_rescale( sdts, p_track->i_timescale, CLOCK_FREQ );
+}
+
+static inline bool MP4_TrackGetPTSDelta( demux_t *p_demux, const mp4_track_t *p_track,
+                                         mtime_t *pi_delta )
+{
+    VLC_UNUSED( p_demux );
+    const mp4_chunk_t *ck = &p_track->chunk[p_track->i_chunk];
+    stime_t delta;
+    if( !MP4_ChunkGetSampleCTSDelta( ck, p_track->i_sample - ck->i_sample_first, &delta ) )
+        return false;
+    *pi_delta = MP4_rescale( delta, p_track->i_timescale, CLOCK_FREQ );
+    return true;
 }
 
 static inline int64_t MP4_GetMoviePTS(demux_sys_t *p_sys )

commit 379c00bd422ed73dae4e7efbcf0176e6f5d37b02

demux: mp4: fix bogus edit list timescale
regression after 0aecdcabaa27f32dc1e3de1832d22d9766cdfae6

refs #27669

modules/demux/mp4/mp4.c
@@ -382,7 +382,7 @@ static bool MP4_ChunkGetSampleCTSDelta( const mp4_chunk_t *p_chunk,
     return false;
 }
 
-static void MP4_TrackTimeApplyELST( const mp4_track_t *p_track,
+static void MP4_TrackTimeApplyELST( const mp4_track_t *p_track, uint64_t i_movie_timescale,
                                     stime_t *pi_dts )
 {
     if( !p_track->p_elst || !p_track->BOXDATA(p_elst)->i_entry_count )
@@ -399,7 +399,7 @@ static void MP4_TrackTimeApplyELST( const mp4_track_t *p_track,
     }
 
     /* add i_elst_time */
-    *pi_dts += p_track->i_elst_time;
+    *pi_dts += MP4_rescale( p_track->i_elst_time, i_movie_timescale, p_track->i_timescale );
 
     if( *pi_dts < 0 ) *pi_dts = 0;
 }
@@ -407,14 +407,14 @@ static void MP4_TrackTimeApplyELST( const mp4_track_t *p_track,
 /* Return time in microsecond of a track */
 static inline mtime_t MP4_TrackGetDTS( demux_t *p_demux, mp4_track_t *p_track )
 {
-    VLC_UNUSED(p_demux);
+    demux_sys_t *p_sys = p_demux->p_sys;
     const mp4_chunk_t *p_chunk = &p_track->chunk[p_track->i_chunk];
 
     stime_t sdts = MP4_ChunkGetSampleDTS( p_chunk,
                                           p_track->i_sample - p_chunk->i_sample_first );
 
     /* now handle elst */
-    MP4_TrackTimeApplyELST( p_track, &sdts );
+    MP4_TrackTimeApplyELST( p_track, p_sys->i_timescale, &sdts );
 
     return MP4_rescale( sdts, p_track->i_timescale, CLOCK_FREQ );
 }