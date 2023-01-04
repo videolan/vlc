9f36f8d72cfbc721e486b00d8b85b60702e00531
Steve Lhomme <robux4@ycbcr.xyz>
2023-01-04 07:34:13 +0100

    win32: provide a unistd.h when compiling with the Windows SDK

---
+++ compat/windows/unistd.h
@@ -0,0 +1,29 @@
+// Copyright © 2023 VideoLabs, VLC authors and VideoLAN
+// SPDX-License-Identifier: ISC
+//
+// Authors: Steve Lhomme <robux4@videolabs.io>
+
+#ifndef WINSDK_UNISTD_H__
+#define WINSDK_UNISTD_H__
+
+// Windows is not a real POSIX system and doesn't provide this header
+// provide a dummy one so the code can compile
+
+// many functions commonly found in unistd.h are found in io.h and process.h
+#define _CRT_DECLARE_NONSTDC_NAMES 1
+#include <io.h>
+#include <process.h>
+
+// defines corresponding to stdin/stdout/stderr without the __acrt_iob_func() call
+#define	STDIN_FILENO  0
+#define	STDOUT_FILENO 1
+#define	STDERR_FILENO 2
+
+// _access() doesn't function the same as access(), but this should work
+#define R_OK  04
+
+// _getpid() exists but it returns an int, not a pid_t
+typedef int pid_t;
+
+
+#endif // WINSDK_UNISTD_H__
