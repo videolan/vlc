86e598311e2b941ed87073526e8e8c9537c29470
Steve Lhomme <robux4@ycbcr.xyz>
2023-01-25 12:46:16 +0100

    vlc-thumb: fix error handling
    If the thumbnail fails, the returned picture is NULL. It is handled that way in
    the snapshot() function. But libvlc_picture_retain() doesn't handle NULL
    pictures.

--- doc/libvlc/vlc-thumb.c
+++ doc/libvlc/vlc-thumb.c
@@ -116,8 +116,9 @@ static void callback(const libvlc_event_t *ev, void *param)
     {
         libvlc_picture_t** pic = param;
         pthread_mutex_lock(&lock);
-        libvlc_picture_retain(ev->u.media_thumbnail_generated.p_thumbnail);
         *pic = ev->u.media_thumbnail_generated.p_thumbnail;
+        if (*pic != NULL)
+            libvlc_picture_retain(*pic);
         done = true;
         pthread_cond_signal(&wait);
         pthread_mutex_unlock(&lock);
