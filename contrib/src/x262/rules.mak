videolan/vlc

commit 637d00420fa1a3f4b71215ba869cb5d05bb7bead

contrib: call sed directly on the files to change

contrib/src/x262/rules.mak
@@ -27,8 +27,8 @@ x262: $(TARBALLS)/x262-git.tar.xz .sum-x262
 
 .x262: x262
 	$(REQUIRE_GPL)
-	cd $< && sed -i -e 's/x264/x262/g' configure
-	cd $< && sed -i -e 's/x264_config/x262_config/g' *.h Makefile *.c
+	sed -i -e 's/x264/x262/g' $</configure
+	sed -i -e 's/x264_config/x262_config/g' *.h Makefile $(UNPACK_DIR)/*.c
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(X264CONF)
 	sed -i -e 's/x264.pc/x262.pc/g' $(BUILD_DIR)/Makefile

contrib/src/flac/rules.mak
@@ -21,7 +21,7 @@ ifdef HAVE_WINSTORE
 	$(APPLY) $(SRC)/flac/no-createfilew.patch
 endif
 	# disable building a tool we don't use
-	cd $(UNPACK_DIR) && sed -e 's,add_subdirectory("microbench"),#add_subdirectory("microbench"),' -i.orig CMakeLists.txt
+	sed -e 's,add_subdirectory("microbench"),#add_subdirectory("microbench"),' -i.orig $(UNPACK_DIR)/CMakeLists.txt
 	$(call pkg_static,"src/libFLAC/flac.pc.in")
 	$(MOVE)
 

contrib/src/gmp/rules.mak
@@ -29,7 +29,7 @@ gmp: gmp-$(GMP_VERSION).tar.xz .sum-gmp
 	$(UNPACK)
 	$(APPLY) $(SRC)/gmp/gmp-fix-asm-detection.patch
 	# do not try the cross compiler to detect the build compiler
-	cd $(UNPACK_DIR) && sed -i.orig 's/"$$CC" "$$CC $$CFLAGS $$CPPFLAGS" cc gcc c89 c99/cc gcc c89 c99/' acinclude.m4
+	sed -i.orig 's/"$$CC" "$$CC $$CFLAGS $$CPPFLAGS" cc gcc c89 c99/cc gcc c89 c99/' $(UNPACK_DIR)/acinclude.m4
 	$(MOVE)
 
 # GMP requires either GPLv2 or LGPLv3

contrib/src/gnutls/rules.mak
@@ -32,7 +32,7 @@ gnutls: gnutls-$(GNUTLS_VERSION).tar.xz .sum-gnutls
 	$(APPLY) $(SRC)/gnutls/0001-explicit_bzero-Do-not-call-SecureZeroMemory-on-UWP-b.patch
 
 	# disable the dllimport in static linking (pkg-config --static doesn't handle Cflags.private)
-	cd $(UNPACK_DIR) && sed -i.orig -e s/"_SYM_EXPORT __declspec(dllimport)"/"_SYM_EXPORT"/g lib/includes/gnutls/gnutls.h.in
+	sed -i.orig -e s/"_SYM_EXPORT __declspec(dllimport)"/"_SYM_EXPORT"/g $(UNPACK_DIR)/lib/includes/gnutls/gnutls.h.in
 
 	# fix i686 UWP builds as they were using CertEnumCRLsInStore via invalid LoadLibrary
 	$(APPLY) $(SRC)/gnutls/0001-fix-mingw64-detection.patch

contrib/src/lame/rules.mak
@@ -18,7 +18,7 @@ endif
 	$(APPLY) $(SRC)/lame/lame-fix-i386-on-aarch64.patch
 	# Avoid relying on iconv.m4 from gettext, when reconfiguring.
 	# This is only used by the frontend which we disable.
-	cd $(UNPACK_DIR) && sed -i.orig 's/^AM_ICONV/#&/' configure.in
+	sed -i.orig 's/^AM_ICONV/#&/' $(UNPACK_DIR)/configure.in
 	$(UPDATE_AUTOCONFIG)
 	$(MOVE)
 

contrib/src/libtasn1/rules.mak
@@ -17,7 +17,7 @@ libtasn1: libtasn1-$(LIBTASN1_VERSION).tar.gz .sum-libtasn1
 	$(APPLY) $(SRC)/libtasn1/no-executables.patch
 	$(APPLY) $(SRC)/libtasn1/0001-fcntl-do-not-call-GetHandleInformation-in-Winstore-a.patch
 	# on iOS for some reason _GNU_SOURCE is found in config.h but strverscmp() is not found
-	cd $(UNPACK_DIR) && sed -i.orig -e 's, -DASN1_BUILDING, -DASN1_BUILDING -D_GNU_SOURCE,' lib/Makefile.am
+	sed -i.orig -e 's, -DASN1_BUILDING, -DASN1_BUILDING -D_GNU_SOURCE,' $(UNPACK_DIR)/lib/Makefile.am
 	$(MOVE)
 
 LIBTASN1_CONF := --disable-doc

contrib/src/mad/rules.mak
@@ -29,10 +29,9 @@ endif
 libmad: libmad-$(MAD_VERSION).tar.gz .sum-mad
 	$(UNPACK)
 ifdef HAVE_DARWIN_OS
-	cd $@-$(MAD_VERSION) && sed \
-		-e 's%-march=i486%$(EXTRA_CFLAGS) $(EXTRA_LDFLAGS)%' \
+	sed -e 's%-march=i486%$(EXTRA_CFLAGS) $(EXTRA_LDFLAGS)%' \
 		-e 's%-dynamiclib%-dynamiclib -arch $(ARCH)%' \
-		-i.orig configure
+		-i.orig $(UNPACK_DIR)/configure
 endif
 ifdef HAVE_IOS
 	$(APPLY) $(SRC)/mad/mad-ios-asm.patch

contrib/src/qt/rules.mak
@@ -112,7 +112,7 @@ QT_QINSTALL="$(shell cd $(SRC)/qt/; pwd -P)/install_wrapper.sh"
 
 .qt: qt
 	# Prevent all Qt contribs from generating and installing libtool .la files
-	cd $< && sed -i.orig "/CONFIG/ s/ create_libtool/ -create_libtool/g" mkspecs/features/qt_module.prf
+	sed -i.orig "/CONFIG/ s/ create_libtool/ -create_libtool/g" $(UNPACK_DIR)/mkspecs/features/qt_module.prf
 	+cd $< && $(QT_ENV_VARS) ./configure $(QT_PLATFORM) $(QT_CONFIG) -prefix $(PREFIX) -hostprefix $(PREFIX)/lib/qt5
 	# Make && Install libraries
 	cd $< && $(QT_ENV_VARS) $(MAKE)

commit 963799540f86029f5c27bc29de2764a5ced4ddf4

contrib: x262: fix sed call when globbing files
The sed call tried to glob the Makefile and *.h files in the contrib
directory, instead of the x262 source directory:

  sed -i -e 's/x264_config/x262_config/g' *.h Makefile x262/*.c
  sed: can't read *.h: No such file or directory
  make: *** [../src/x262/rules.mak:31: .x262] Error 2

Regression from 637d00420fa1a3f4b71215ba869cb5d05bb7bead.

contrib/src/x262/rules.mak
@@ -28,7 +28,7 @@ x262: $(TARBALLS)/x262-git.tar.xz .sum-x262
 .x262: x262
 	$(REQUIRE_GPL)
 	sed -i -e 's/x264/x262/g' $</configure
-	sed -i -e 's/x264_config/x262_config/g' *.h Makefile $(UNPACK_DIR)/*.c
+	sed -i -e 's/x264_config/x262_config/g' $</*.h $</Makefile $</*.c
 	$(MAKEBUILDDIR)
 	$(MAKECONFIGURE) $(X264CONF)
 	sed -i -e 's/x264.pc/x262.pc/g' $(BUILD_DIR)/Makefile