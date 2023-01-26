videolan/vlc

commit 697a2fcda3321c44f0ef2898b513851ba699ade7

contrib: build autotools/make targets in a build directory

contrib/src/x262/rules.mak
@@ -29,10 +29,11 @@ x262: $(TARBALLS)/x262-git.tar.xz .sum-x262
 	$(REQUIRE_GPL)
 	cd $< && sed -i -e 's/x264/x262/g' configure
 	cd $< && sed -i -e 's/x264_config/x262_config/g' *.h Makefile *.c
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF) $(X264CONF)
-	cd $< && sed -i -e 's/x264.pc/x262.pc/g' Makefile
-	cd $< && sed -i -e 's/x264.h/x262.h/g' Makefile
-	$(MAKE) -C $<
-	cd $< && cp x264.h x262.h
-	$(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF) $(X264CONF)
+	cd $</_build && sed -i -e 's/x264.pc/x262.pc/g' Makefile
+	cd $</_build && sed -i -e 's/x264.h/x262.h/g' Makefile
+	$(MAKE) -C $</_build
+	cd $</_build && cp x264.h x262.h
+	$(MAKE) -C $</_build install
 	touch $@

contrib/src/a52/rules.mak
@@ -25,7 +25,8 @@ endif
 .a52: a52dec
 	$(REQUIRE_GPL)
 	$(RECONF)
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF)
-	$(MAKE) -C $< -C liba52 install
-	$(MAKE) -C $< -C include install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF)
+	$(MAKE) -C $</_build -C liba52 install
+	$(MAKE) -C $</_build -C include install
 	touch $@

contrib/src/aribb24/rules.mak
@@ -29,6 +29,7 @@ DEPS_aribb24 = png $(DEPS_png)
 	$(REQUIRE_GPL)
 	$(REQUIRE_GNUV3)
 	cd $< && $(SHELL) ./bootstrap
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF)
-	$(MAKE) -C $< && $(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF)
+	$(MAKE) -C $</_build && $(MAKE) -C $</_build install
 	touch $@

contrib/src/aribb25/rules.mak
@@ -31,6 +31,7 @@ ARIBB25_CONF := --disable-b25
 
 .aribb25: aribb25
 	$(RECONF)
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF) $(ARIBB25_CONF)
-	$(MAKE) -C $< && $(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF) $(ARIBB25_CONF)
+	$(MAKE) -C $</_build && $(MAKE) -C $</_build install
 	touch $@

contrib/src/asdcplib/rules.mak
@@ -40,6 +40,7 @@ ASDCPLIB_CONF += CXXFLAGS="$(ASDCPLIB_CXXFLAGS)"
 
 .asdcplib: asdcplib
 	$(RECONF)
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF) $(ASDCPLIB_CONF)
-	$(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF) $(ASDCPLIB_CONF)
+	$(MAKE) -C $</_build install
 	touch $@

contrib/src/ass/rules.mak
@@ -62,8 +62,9 @@ ASS_CONF += --disable-asm
 endif
 
 .ass: libass
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF) $(ASS_CONF)
-	$(MAKE) -C $<
-	$(call pkg_static,"libass.pc")
-	$(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF) $(ASS_CONF)
+	$(MAKE) -C $</_build
+	$(call pkg_static,"_build/libass.pc")
+	$(MAKE) -C $</_build install
 	touch $@

contrib/src/bluray/rules.mak
@@ -55,6 +55,7 @@ bluray: libbluray-$(BLURAY_VERSION).tar.bz2 .sum-bluray
 .bluray: bluray
 	rm -rf $(PREFIX)/share/java/libbluray*.jar
 	cd $< && ./bootstrap
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF) $(BLURAY_CONF)
-	$(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF) $(BLURAY_CONF)
+	$(MAKE) -C $</_build install
 	touch $@

contrib/src/breakpad/rules.mak
@@ -35,8 +35,8 @@ ifdef HAVE_MACOSX
 		install build/Release/dump_syms "$(PREFIX)/bin"
 else
 	$(RECONF)
-	cd $< && $(HOSTVARS) ./configure $(HOSTCONF) $(BREAKPAD_CONF)
-	cd $< && Configuration=Release $(MAKE) install
-	Configuration=Release $(MAKE) -C $< install
+	mkdir -p $</_build
+	cd $</_build && $(HOSTVARS) ../configure $(HOSTCONF) $(BREAKPAD_CONF)
+	Configuration=Release $(MAKE) -C $</_build install
 endif
 	touch $@

commit ee858cc75cf42b9374f28d5cb8db3de1baf3b7a7

contrib: x262: fix cp call
The cp call is broken:

  cp x262/vlc_build/x264.h x262/vlc_build/x262.h
  cp: cannot stat 'x262/vlc_build/x264.h': No such file or directory
  make: *** [../src/x262/rules.mak:37: .x262] Error 1

x264.h isn't generated inside the build directory (x262/vlc_build), but
exists in the x262 source directory. The install rules also install it
from the source directory. Therefore, copy it from and to the source
dir, not the build dir.

Regression from 697a2fcda3321c44f0ef2898b513851ba699ade7.

contrib/src/x262/rules.mak
@@ -34,6 +34,6 @@ x262: $(TARBALLS)/x262-git.tar.xz .sum-x262
 	sed -i -e 's/x264.pc/x262.pc/g' $(BUILD_DIR)/Makefile
 	sed -i -e 's/x264.h/x262.h/g' $(BUILD_DIR)/Makefile
 	+$(MAKEBUILD)
-	cp $(BUILD_DIR)/x264.h $(BUILD_DIR)/x262.h
+	cp $</x264.h $</x262.h
 	+$(MAKEBUILD) install
 	touch $@