4dd7d6788fd2690153888e8b1c2b24d9dcf67198
Claudio Cambra <developer@claudiocambra.com>
2023-02-03 01:57:13 +0100

    macosx: Make VLCDetachedVideoWindow a VLCAspectRatioRetainingWindow
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/windows/video/VLCDetachedVideoWindow.h
+++ modules/gui/macosx/windows/video/VLCDetachedVideoWindow.h
@@ -21,8 +21,8 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#import "VLCVideoWindowCommon.h"
+#import "VLCAspectRatioRetainingVideoWindow.h"
 
-@interface VLCDetachedVideoWindow : VLCVideoWindowCommon
+@interface VLCDetachedVideoWindow : VLCAspectRatioRetainingVideoWindow
 
 @end
