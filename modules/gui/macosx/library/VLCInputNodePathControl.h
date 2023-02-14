45680115c24f0e0688a7435b0eaa40cb6ca7ad40
Claudio Cambra <developer@claudiocambra.com>
2023-02-14 02:16:05 +0100

    macosx: Add VLCInputNodePathControl
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/VLCInputNodePathControl.h
+++ modules/gui/macosx/library/VLCInputNodePathControl.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * VLCInputNodePathControlItem.m: MacOS X interface module
+ * VLCInputNodePathControl.h: MacOS X interface module
  *****************************************************************************
  * Copyright (C) 2023 VLC authors and VideoLAN
  *
@@ -20,31 +20,12 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#import "VLCInputNodePathControlItem.h"
+#import <Cocoa/Cocoa.h>
 
-#import "VLCInputItem.h"
-#import "VLCLibraryImageCache.h"
+NS_ASSUME_NONNULL_BEGIN
 
-@implementation VLCInputNodePathControlItem
-
-- (instancetype)initWithInputNode:(VLCInputNode *)inputNode
-{
-    self = [super init];
-    if (self && inputNode != nil && inputNode.inputItem != nil) {
-        _inputNode = inputNode;
-
-        VLCInputItem *inputItem = inputNode.inputItem;
-        self.image = [VLCLibraryImageCache thumbnailForInputItem:inputItem];
-        self.title = inputItem.name;
-
-        // HACK: We have no way when we get the clicked item from the path control
-        // of knowing specifically which input node this path item corresponds to,
-        // as the path control returns a copy for clickedPathItem that is not of
-        // this class. As a very awkward workaround, lets set the name of the image
-        // used here as the MRL of the node's input item
-        self.image.name = inputItem.MRL;
-    }
-    return self;
-}
+@interface VLCInputNodePathControl : NSPathControl
 
 @end
+
+NS_ASSUME_NONNULL_END
