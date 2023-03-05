videolan/vlc

commit 316fb5633f8ed100eabebedde958d71536a0ff09

vout: interrupt wait on UI resize request
On resize, the UI thread waits for the end of any current rendering at
the old size before returning.

To limit its waiting, interrupt the wait between prepare() and display()
on the vout thread to complete the rendering as soon as possible. As a
consequence, the display() call might be performed early (before its
expected PTS).

Refs !324

src/video_output/video_output.c
@@ -77,6 +77,7 @@ typedef struct vout_thread_sys_t
     const char      *str_id;
 
     vlc_mutex_t clock_lock;
+    bool clock_nowait; /* protected by vlc_clock_Lock()/vlc_clock_Unlock() */
 
     vlc_clock_t     *clock;
     float           rate;
@@ -220,6 +221,21 @@ static void VoutFixFormat(video_format_t *dst, const video_format_t *src)
     dst->i_chroma = vlc_fourcc_GetCodec(VIDEO_ES, src->i_chroma);
     VoutFixFormatAR( dst );
     video_format_FixRgb(dst);
+}
+
+static void VoutRenderWakeUpUrgent(vout_thread_sys_t *sys)
+{
+    /* The assignment to sys->clock is protected by sys->lock */
+    vlc_mutex_lock(&sys->clock_lock);
+    if (sys->clock)
+    {
+        /* Wake up the clock-wait between prepare() and display() */
+        vlc_clock_Lock(sys->clock);
+        sys->clock_nowait = true;
+        vlc_clock_Wake(sys->clock);
+        vlc_clock_Unlock(sys->clock);
+    }
+    vlc_mutex_unlock(&sys->clock_lock);
 }
 
 static bool VideoFormatIsCropArEqual(video_format_t *dst,
@@ -472,6 +488,8 @@ void vout_ChangeDisplaySize(vout_thread_t *vout,
     vout_thread_sys_t *sys = VOUT_THREAD_TO_SYS(vout);
 
     assert(!sys->dummy);
+
+    VoutRenderWakeUpUrgent(sys);
 
     /* DO NOT call this outside the vout window callbacks */
     vlc_queuedmutex_lock(&sys->display_lock);
@@ -1251,6 +1269,9 @@ static int RenderPicture(vout_thread_sys_t *sys, bool render_now)
     if (!filtered)
         return VLC_EGENERIC;
 
+    vlc_clock_Lock(sys->clock);
+    sys->clock_nowait = false;
+    vlc_clock_Unlock(sys->clock);
     vlc_queuedmutex_lock(&sys->display_lock);
 
     picture_t *todisplay;
@@ -1321,10 +1342,14 @@ static int RenderPicture(vout_thread_sys_t *sys, bool render_now)
                         deadline = max_deadline;
                 }
 
+                if (sys->clock_nowait)
+                    /* A caller (the UI thread) awaits for the rendering to
+                     * complete urgently, do not wait. */
+                    break;
+
                 system_pts = deadline;
                 timed_out = vlc_clock_Wait(sys->clock, deadline);
             }
-
             vlc_clock_Unlock(sys->clock);
         }
         sys->displayed.date = system_pts;
@@ -2013,6 +2038,7 @@ vout_thread_t *vout_Create(vlc_object_t *object)
     vlc_mutex_init(&sys->filter.lock);
 
     vlc_mutex_init(&sys->clock_lock);
+    sys->clock_nowait = false;
 
     /* Display */
     sys->display = NULL;

commit fc213d43090c24d3eb77031ec4acb66ef38978f6

video_output: reset clock on display error
The member sys->clock is assigned from the vout_Request to the decoder
clock, and was assigned back to NULL when the vout was stopped.

However, when the decoder_UpdateVideoOutput call would fail because of
the display, the clock would have been assigned to the video output and
vout_Stop would never be called since the vout was never started,
leading to the sys->clock member being unexpectedly non-null when
starting the video output.

 1/ Creation of the first decoder.
 2/ Decoder_UpdateVideoOutput() is called from the decoder.
 3/ Calling input_resource_RequestVout with first decoder clock.
 4/ Display fails to open, but sys->clock is now non-null.
 5/ Close of the first ES / decoder, sys->clock is destroyed.
 6/ Creation of a second decoder.
 7/ Decoder_UpdateVideoOutput() is called from the second decoder.
 8/ Calling input_resource_RequestVout with the second decoder clock.
 9/ If the window synchronously reports a size, then the previous
    released clock is used and it crashes.

The clock must either be NULL when failing to open the vout or assigned
before the window is started.

Having the clock being NULL when enabling the window is handy because it
won't trigger an interrupt of the clock for nothing, but also the
additional lock will still happen only on error, when no other client is
using the clock lock anyway.

Indirect regression from 316fb5633f8ed100eabebedde958d71536a0ff09.

Fixes #27756

src/video_output/video_output.c
@@ -2198,20 +2198,25 @@ int vout_Request(const vout_configuration_t *cfg, vlc_video_context *vctx, input
     if (vout_Start(vout, vctx, cfg))
     {
         msg_Err(cfg->vout, "video output display creation failed");
-        vout_DisableWindow(vout);
-        return -1;
+        goto error_display;
     }
     atomic_store(&sys->control_is_terminated, false);
-    if (vlc_clone(&sys->thread, Thread, vout)) {
-        vout_ReleaseDisplay(vout);
-        vout_DisableWindow(vout);
-        return -1;
-    }
+    if (vlc_clone(&sys->thread, Thread, vout))
+        goto error_thread;
 
     if (input != NULL && sys->spu)
         spu_Attach(sys->spu, input);
     vout_IntfReinit(cfg->vout);
     return 0;
+
+error_thread:
+    vout_ReleaseDisplay(vout);
+error_display:
+    vout_DisableWindow(vout);
+    vlc_mutex_lock(&sys->clock_lock);
+    sys->clock = NULL;
+    vlc_mutex_unlock(&sys->clock_lock);
+    return -1;
 }
 
 vlc_decoder_device *vout_GetDevice(vout_thread_t *vout)