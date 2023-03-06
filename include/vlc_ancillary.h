f459d4ed752c8314b80f9c68633b4ccd83897126
Steve Lhomme <robux4@ycbcr.xyz>
2023-03-06 16:05:29 +0100

    core: add ancillary data for VPX alpha channel

--- include/vlc_ancillary.h
+++ include/vlc_ancillary.h
@@ -217,5 +217,17 @@ typedef struct vlc_icc_profile_t
     uint8_t data[]; /* binary profile data, see ICC.1:2022 (or later) */
 } vlc_icc_profile_t;
 
+/**
+ * VPx alpha data
+ */
+
+#define VLC_ANCILLARY_ID_VPX_ALPHA VLC_FOURCC('v','p','x','A')
+
+typedef struct vlc_vpx_alpha_t
+{
+    size_t  size;
+    uint8_t *data;
+} vlc_vpx_alpha_t;
+
 /** @} */
 #endif /* VLC_ANCILLARY_H */
