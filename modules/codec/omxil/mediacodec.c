videolan/vlc

commit cdff503ed20f5e58aa35b330bfe9fd811c2b76a0

mediacodec: move input/video cleaning outside of CleanDecoder
Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

modules/codec/omxil/mediacodec.c
@@ -538,6 +538,21 @@ static void StopMediaCodec(decoder_t *p_dec)
     p_sys->api.stop(&p_sys->api);
 }
 
+static void CleanInputVideo(decoder_t *p_dec)
+{
+    decoder_sys_t *p_sys = p_dec->p_sys;
+
+    if (p_dec->fmt_in.i_cat == VIDEO_ES)
+    {
+        if (p_dec->fmt_in.i_codec == VLC_CODEC_H264
+         || p_dec->fmt_in.i_codec == VLC_CODEC_HEVC)
+            hxxx_helper_clean(&p_sys->video.hh);
+
+        if (p_sys->video.timestamp_fifo)
+            timestamp_FifoRelease(p_sys->video.timestamp_fifo);
+    }
+}
+
 /*****************************************************************************
  * OpenDecoder: Create the decoder instance
  *****************************************************************************/
@@ -800,6 +815,7 @@ static int OpenDecoder(vlc_object_t *p_this, pf_MediaCodecApi_init pf_init)
     return VLC_SUCCESS;
 
 bailout:
+    CleanInputVideo(p_dec);
     CleanDecoder(p_dec);
     return VLC_EGENERIC;
 }
@@ -838,15 +854,6 @@ static void CleanDecoder(decoder_t *p_dec)
     CSDFree(p_dec);
     p_sys->api.clean(&p_sys->api);
 
-    if (p_dec->fmt_in.i_cat == VIDEO_ES)
-    {
-        if (p_dec->fmt_in.i_codec == VLC_CODEC_H264
-         || p_dec->fmt_in.i_codec == VLC_CODEC_HEVC)
-            hxxx_helper_clean(&p_sys->video.hh);
-
-        if (p_sys->video.timestamp_fifo)
-            timestamp_FifoRelease(p_sys->video.timestamp_fifo);
-    }
     free(p_sys);
 }
 
@@ -867,6 +874,7 @@ static void CloseDecoder(vlc_object_t *p_this)
 
     vlc_join(p_sys->out_thread, NULL);
 
+    CleanInputVideo(p_dec);
     CleanDecoder(p_dec);
 }
 

commit 1f7f400ff74a80354efb467a5cb41b11a0cc98c8

mediacodec: release timestamp_fifo with video context
`CleanInputVideo()` is linked to the input side of the decoder
(.pf_decode) but `video.timestamp_fifo` can be used from the output side
(threads from mediacodec) and is actually also tied to the life cycle of
the video context, as visible in the destructor `CloseDecoder`. Since
the life cycle of the video context is at least the one from the input
side, destroy the timestamp_fifo there.

Fixes a use-after-free when the video context is not released from
CloseDestructor() while mediacodec is still running (not joined yet).

Regression from cdff503ed20f5e58aa35b330bfe9fd811c2b76a0 and
57323ddadbe6579697cc9ecdef0de2fbc6831dac

modules/codec/omxil/mediacodec.c
@@ -746,9 +746,6 @@ static void CleanInputVideo(decoder_t *p_dec)
         if (p_dec->fmt_in->i_codec == VLC_CODEC_H264
          || p_dec->fmt_in->i_codec == VLC_CODEC_HEVC)
             hxxx_helper_clean(&p_sys->video.hh);
-
-        if (p_sys->video.timestamp_fifo)
-            timestamp_FifoRelease(p_sys->video.timestamp_fifo);
     }
 }
 
@@ -1056,6 +1053,9 @@ static void CleanDecoder(decoder_sys_t *p_sys)
 
     if (p_sys->video.surfacetexture)
         vlc_asurfacetexture_Delete(p_sys->video.surfacetexture);
+
+    if (p_sys->video.timestamp_fifo)
+        timestamp_FifoRelease(p_sys->video.timestamp_fifo);
 
     free(p_sys);
 }