7421993f8d8809329f67d2812f0678e3901f6082
Alexandre Janniaux <ajanni@videolabs.io>
2023-03-07 17:02:03 +0100

    Revert "videotoolbox: add fallback option"
    This reverts commit 7859c00281aafc4b44b798a0ef56c2ba16f143aa.

--- modules/codec/videotoolbox/decoder.c
+++ modules/codec/videotoolbox/decoder.c
@@ -118,9 +118,6 @@ typedef struct decoder_sys_t
     CFDictionaryRef             (*pf_copy_extradata)(decoder_t *);
     bool                        (*pf_fill_reorder_info)(decoder_t *, const block_t *,
                                                         frame_info_t *);
-    /* Configuration variables */
-    bool                        b_fallback_on_error;
-
     /* !Codec specific callbacks */
 
     bool                        b_vt_feed;
@@ -1387,7 +1384,6 @@ static int OpenDecoder(vlc_object_t *p_this, bool h264_support)
     p_sys->i_pic_reorder_max = 4;
     p_sys->vtsession_status = VTSESSION_STATUS_OK;
     p_sys->b_cvpx_format_forced = false;
-    p_sys->b_fallback_on_error = var_InheritBool(p_dec, "videotoolbox-error-fallback");
 
     char *cvpx_chroma = var_InheritString(p_dec, "videotoolbox-cvpx-chroma");
     if (cvpx_chroma != NULL)
@@ -1782,7 +1778,8 @@ static int HandleVTStatus(decoder_t *p_dec, OSStatus status,
                 *p_vtsession_status = VTSESSION_STATUS_RESTART;
                 break;
             case kVTVideoDecoderBadDataErr:
-                if (p_sys->b_fallback_on_error)
+                p_sys->error_counter++;
+                if (p_sys->error_counter == 3)
                     *p_vtsession_status = VTSESSION_STATUS_ABORT;
                 else
                     *p_vtsession_status = VTSESSION_STATUS_RESTART;
@@ -1897,7 +1894,7 @@ static int DecodeBlock(decoder_t *p_dec, block_t *p_block)
         }
         else
         {
-            do_restart = p_sys->i_restart_count <= VT_RESTART_MAX || !p_sys->b_fallback_on_error;
+            do_restart = p_sys->i_restart_count <= VT_RESTART_MAX;
         }
 
         if (do_restart)
@@ -2295,11 +2292,6 @@ OpenDecDevice(vlc_decoder_device *device, vlc_window_t *window)
 
 #pragma mark - Module descriptor
 
-#define VT_FALLBACK_ERROR "Fallback on different decoders in case of decoder error"
-#define VT_FALLBACK_ERROR_LONG "Fallback on a different decoder in case of decoder error" \
-    " which could stall the decoder or crash the device. Disabling this option is not" \
-    " recommended unless you have specific data that you know to be playable with VT" \
-    " and want to play with HW decoding even if stream corruption happens."
 #define VT_REQUIRE_HW_DEC N_("Use Hardware decoders only")
 #define VT_FORCE_CVPX_CHROMA "Force the VideoToolbox output chroma"
 #define VT_FORCE_CVPX_CHROMA_LONG "Force the VideoToolbox decoder to output \
@@ -2332,7 +2324,6 @@ vlc_module_begin()
     set_capability("video decoder", 60)
     set_callbacks(OpenDecoderAll, CloseDecoder)
 
-    add_bool("videotoolbox-error-fallback", true, VT_FALLBACK_ERROR, VT_FALLBACK_ERROR_LONG)
     add_bool("videotoolbox-hw-decoder-only", true, VT_REQUIRE_HW_DEC, VT_REQUIRE_HW_DEC)
     add_string("videotoolbox-cvpx-chroma", "", VT_FORCE_CVPX_CHROMA, VT_FORCE_CVPX_CHROMA_LONG);
         change_string_list(chroma_list_values, chroma_list_names)
