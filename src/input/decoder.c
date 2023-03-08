23a0ea4b4ed74fff4ea6d2bcabca8c8b92187d4c
Alexandre Janniaux <ajanni@videolabs.io>
2023-03-08 10:01:07 +0100

    input: decoder: fix flushing when deleting decoder
    An input_decoder instance can be deleted while its DecoderThread, the
    thread calling decoder_t::pf_decode, is still running, meaning that
    the deletion request happens asynchronously to the decoding process.
    
    In particular, when the input_decoder instance is being deleted, an
    aborting state is signalled to the DecoderThread and the input decoder
    joins the thread afterwards.
    
    In order to return and be joined, the DecoderThread needs to finish what
    it was doing. In the case of a decoder, where input is paced by output
    availability, it's likely that the decoder will get paced and will be
    waiting on the decoder_t::pf_decode call. To finalize this call, the
    decoder must be provided pictures back from the output, so that the
    block from decoder_t::pf_decode can be queued.
    
    At the beginning of the deletion, by setting p_owner->flushing to true,
    we also prevent the decoder implementation to queue blocks into the
    output (from commit 34a548cc02ce67920efbd7fdaa5af752199236a8). When
    flushing the input decoder, the output was also correctly flushed
    (tested by 91aabbf0668dffd352f029a10783916d97b80a94) which matches with
    how the decoder implementation can be unblocked.
    
    But in the case of closing, output flushing was only happening when the
    output was paused, meaning that a deadlock could happen if every
    pictures were queued to the output when the decoder would be deleted
    while it was decoding another new block.
    
    This issue was reproduced with the Videotoolbox decoder.

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -2223,31 +2223,18 @@ void vlc_input_decoder_Delete( vlc_input_decoder_t *p_owner )
     /* Make sure we aren't waiting/decoding anymore */
     vlc_cond_signal( &p_owner->wait_request );
 
-    /* If the video output is paused or slow, or if the picture pool size was
-     * under-estimated (e.g. greedy video filter, buggy decoder...), the
-     * the picture pool may be empty, and the decoder thread or any decoder
-     * module worker threads may be stuck waiting for free picture buffers.
-     *
-     * This unblocks the thread, allowing the decoder module to join all its
-     * worker threads (if any) and the decoder thread to terminate. */
+    /* The DecoderThread could be stuck in pf_decode(), waiting for a frame
+     * to be released to queue the block and return from there. In that case,
+     * it's not possible to close the decoder before the return of the callback.
+     * To fix this issue, prevent the decoder to queue picture in the vout by
+     * setting p_owner->aborting, and flush the vout from here to return the
+     * pictures to the decoder. This unblocks the DecoderThread, allowing the
+     * decoder module to later join all its worker threads (if any) and the
+     * decoder thread to terminate. */
     if( p_dec->fmt_in->i_cat == VIDEO_ES && p_owner->p_vout != NULL
      && p_owner->vout_started )
     {
-        if( p_owner->paused )
-        {
-            /* The DecoderThread could be stuck in pf_decode(). This is likely the
-            * case with paused asynchronous decoder modules that have a limited
-            * input and output pool size. Indeed, with such decoders, you have to
-            * release an output buffer to get an input buffer. So, when paused and
-            * flushed, the DecoderThread could be waiting for an output buffer to
-            * be released (or rendered). In that case, the DecoderThread will
-            * never be flushed since it be never leave pf_decode(). To fix this
-            * issue, pre-flush the vout from here. The vout will have to be
-            * flushed again since the module could be outputting more buffers just
-            * after being unstuck. */
-
-            vout_FlushAll( p_owner->p_vout );
-        }
+        vout_FlushAll( p_owner->p_vout );
     }
     vlc_fifo_Unlock( p_owner->p_fifo );
 
