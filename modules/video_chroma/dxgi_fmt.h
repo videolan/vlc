d6c19a63ef434bc84a1776fbde8882a5d8c66155
Steve Lhomme <robux4@ycbcr.xyz>
2023-03-13 14:22:54 +0100

    dxgi_fmt: keep information about how many alpha bits has a DXGI format

--- modules/video_chroma/dxgi_fmt.h
+++ modules/video_chroma/dxgi_fmt.h
@@ -50,6 +50,7 @@ typedef struct
     uint8_t      bitsPerChannel;
     uint8_t      widthDenominator;
     uint8_t      heightDenominator;
+    uint8_t      bitsForAlpha;
     uint8_t      resourceFactor; // the plane depth doesn't match the resource depth
     DXGI_FORMAT  resourceFormat[DXGI_MAX_SHADER_VIEW];
 } d3d_format_t;
