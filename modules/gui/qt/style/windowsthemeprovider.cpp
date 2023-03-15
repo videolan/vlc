1a4eb729d9e1beac40d773a8ec3896b7403e69d0
Pierre Lamot <pierre@videolabs.io>
2023-03-15 17:16:13 +0100

    qt: use high-contrast colors on windows
    when the user enable the high-contrast mode on windows, we must comply to the
    system palette

--- modules/gui/qt/style/windowsthemeprovider.cpp
+++ modules/gui/qt/style/windowsthemeprovider.cpp
@@ -59,6 +59,11 @@ public:
             if (m_obj->paletteUpdated)
                 m_obj->paletteUpdated(m_obj, m_obj->paletteUpdatedData);
         }
+        else if (msg->message == WM_SYSCOLORCHANGE)
+        {
+            if (m_obj->paletteUpdated)
+                m_obj->paletteUpdated(m_obj, m_obj->paletteUpdatedData);
+        }
         return false;
     }
 
@@ -68,6 +73,47 @@ private:
     QSettings m_settings;
 };
 
+static void setQtColor(vlc_qt_theme_provider_t* obj,
+                        vlc_qt_theme_color_set set, vlc_qt_theme_color_section section,
+                        vlc_qt_theme_color_name name, vlc_qt_theme_color_state state,
+                        int color)
+{
+    const DWORD c = GetSysColor(color);
+    obj->setColorInt(obj, set, section, name, state, GetRValue(c), GetGValue(c), GetBValue(c), 255);
+}
+
+
+static void setQtColorFg(vlc_qt_theme_provider_t* obj,
+                        vlc_qt_theme_color_set set, vlc_qt_theme_color_name name, vlc_qt_theme_color_state state, int role)
+{
+    setQtColor(obj, set, VQTC_SECTION_FG, name, state, role);
+}
+
+static void setQtColorBg(vlc_qt_theme_provider_t* obj,
+                        vlc_qt_theme_color_set set, vlc_qt_theme_color_name name, vlc_qt_theme_color_state state, int role)
+{
+    setQtColor(obj, set, VQTC_SECTION_BG, name, state, role);
+}
+
+
+static void setQtColorButton(vlc_qt_theme_provider_t* obj,
+                        vlc_qt_theme_color_set CS)
+{
+    setQtColorBg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_NORMAL, COLOR_3DFACE);
+    setQtColorFg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_NORMAL, COLOR_BTNTEXT);
+    setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_BORDER, VQTC_STATE_NORMAL, COLOR_BTNTEXT);
+
+    setQtColorBg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_HOVERED, COLOR_HIGHLIGHT);
+    setQtColorFg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_HOVERED, COLOR_HIGHLIGHTTEXT);
+    setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_BORDER, VQTC_STATE_HOVERED, COLOR_HIGHLIGHTTEXT);
+
+    setQtColorBg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_FOCUSED, COLOR_HIGHLIGHT);
+    setQtColorFg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_FOCUSED, COLOR_HIGHLIGHTTEXT);
+    setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_BORDER, VQTC_STATE_FOCUSED, COLOR_HIGHLIGHTTEXT);
+
+
+}
+
 static bool isThemeDark(vlc_qt_theme_provider_t* obj)
 {
     auto sys = static_cast<WinColorSchemeObserver*>(obj->p_sys);
@@ -76,6 +122,67 @@ static bool isThemeDark(vlc_qt_theme_provider_t* obj)
 
 static int updatePalette(vlc_qt_theme_provider_t* obj)
 {
+    HIGHCONTRAST constrastInfo;
+    constrastInfo.cbSize = sizeof(HIGHCONTRAST);
+
+    bool ret = SystemParametersInfoA(SPI_GETHIGHCONTRAST, constrastInfo.cbSize, &constrastInfo, 0);
+
+    if (ret && ((constrastInfo.dwFlags & HCF_HIGHCONTRASTON) == HCF_HIGHCONTRASTON))
+    {
+        //we must comply with the high contrast palette, only few color can be used
+        //see https://learn.microsoft.com/en-us/windows/apps/design/accessibility/high-contrast-themes#contrast-colors
+
+        //most things will fallback to the default view colors, as we mainly have a foreground and a backgroud color
+
+        //View
+        {
+            vlc_qt_theme_color_set CS = VQTC_SET_VIEW;
+            setQtColorBg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_NORMAL, COLOR_WINDOW);
+            setQtColorFg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_NORMAL, COLOR_WINDOWTEXT);
+            setQtColorFg(obj, CS, VQTC_NAME_PRIMARY, VQTC_STATE_DISABLED, COLOR_GRAYTEXT);
+
+            setQtColorBg(obj, CS, VQTC_NAME_SECONDARY, VQTC_STATE_NORMAL, COLOR_WINDOW);
+            setQtColorFg(obj, CS, VQTC_NAME_SECONDARY, VQTC_STATE_NORMAL, COLOR_WINDOWTEXT);
+            setQtColorFg(obj, CS, VQTC_NAME_SECONDARY, VQTC_STATE_DISABLED, COLOR_GRAYTEXT);
+
+            setQtColorFg(obj, CS, VQTC_NAME_LINK, VQTC_STATE_NORMAL, COLOR_HOTLIGHT);
+
+            setQtColorBg(obj, CS, VQTC_NAME_HIGHLIGHT, VQTC_STATE_NORMAL, COLOR_HIGHLIGHT);
+            setQtColorFg(obj, CS, VQTC_NAME_HIGHLIGHT, VQTC_STATE_NORMAL, COLOR_HIGHLIGHTTEXT);
+
+            //we can't really have color here
+            setQtColorBg(obj, CS, VQTC_NAME_NEGATIVE, VQTC_STATE_NORMAL, COLOR_HIGHLIGHT);
+            setQtColorBg(obj, CS, VQTC_NAME_NEUTRAL, VQTC_STATE_NORMAL, COLOR_HIGHLIGHT);
+            setQtColorBg(obj, CS, VQTC_NAME_POSITIVE, VQTC_STATE_NORMAL, COLOR_HIGHLIGHT);
+
+            setQtColorFg(obj, CS, VQTC_NAME_NEGATIVE, VQTC_STATE_NORMAL, COLOR_HIGHLIGHTTEXT);
+            setQtColorFg(obj, CS, VQTC_NAME_NEUTRAL, VQTC_STATE_NORMAL, COLOR_HIGHLIGHTTEXT);
+            setQtColorFg(obj, CS, VQTC_NAME_POSITIVE, VQTC_STATE_NORMAL, COLOR_HIGHLIGHTTEXT);
+
+            //branding color,
+            //NOTE: can we keep our branding orange accent here?
+            //obj->setColorInt(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_ACCENT, VQTC_STATE_NORMAL, 0xFF, 0x80, 0x00, 0xFF);
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_ACCENT, VQTC_STATE_NORMAL, COLOR_HIGHLIGHT);
+
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_VISUAL_FOCUS, VQTC_STATE_NORMAL, COLOR_WINDOWTEXT);
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_SEPARATOR, VQTC_STATE_NORMAL, COLOR_BTNTEXT);
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_SHADOW, VQTC_STATE_NORMAL, COLOR_BTNTEXT);
+
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_BORDER, VQTC_STATE_NORMAL, COLOR_BTNTEXT);
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_BORDER, VQTC_STATE_FOCUSED, COLOR_HIGHLIGHT);
+            setQtColor(obj, CS, VQTC_SECTION_DECORATION, VQTC_NAME_BORDER, VQTC_STATE_HOVERED, COLOR_HIGHLIGHT);
+        }
+
+        //set colors on all buttons
+        setQtColorButton(obj, VQTC_SET_TOOL_BUTTON);
+        setQtColorButton(obj, VQTC_SET_TAB_BUTTON);
+        setQtColorButton(obj, VQTC_SET_BUTTON_ACCENT);
+        setQtColorButton(obj, VQTC_SET_BUTTON_STANDARD);
+        setQtColorButton(obj, VQTC_SET_SWITCH);
+
+        return VLC_SUCCESS;
+    }
+
     //use VLC palette, choose the one maching the dark/light settings
     return VLC_EGENERIC;
 }
