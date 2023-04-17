e9a72670fa37305718dea29c29b9a0438cbc7e86
Rémi Denis-Courmont <remi@remlab.net>
2023-04-17 21:41:50 +0300

    lua: use stdc_leading_ones()

--- modules/lua/libs/win.c
+++ modules/lua/libs/win.c
@@ -27,6 +27,7 @@
 # include "config.h"
 #endif
 
+#include <stdbit.h>
 #include <vlc_common.h>
 #include <vlc_charset.h>
 
@@ -93,7 +94,7 @@ static bool ReadWin32( HANDLE *hConsoleIn, unsigned char *p_buffer, int *pi_size
                     (*pi_size)--;
                     nbBytes++;
                 }
-                assert( clz( (unsigned char)~(p_buffer[*pi_size]) ) == nbBytes + 1 );
+                assert( stdc_leading_ones( p_buffer[*pi_size] ) == nbBytes + 1 );
                 // The first utf8 byte will be overridden by a \0
             }
             else
