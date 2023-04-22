videolan/vlc

commit 47486d9992c3c58a929ec535b2da59ecc325a367

dav1d: update the extra picture buffers needed on each picture
If the AV1 extra data in the container don't match the bitstream (invalid file)
we end up missing some pictures when using extra resolution.

This can be triggered by this bug in Handbrake with SVT-AV1:
https://github.com/HandBrake/HandBrake/issues/5081

Ref. #28002

modules/codec/dav1d.c
@@ -160,6 +160,7 @@ static void UpdateDecoderOutput(decoder_t *dec, const Dav1dSequenceHeader *seq_h
 static int NewPicture(Dav1dPicture *img, void *cookie)
 {
     decoder_t *dec = cookie;
+    decoder_sys_t *p_sys = dec->p_sys;
 
     video_format_t *v = &dec->fmt_out.video;
 
@@ -202,6 +203,11 @@ static int NewPicture(Dav1dPicture *img, void *cookie)
     v->i_width  = (img->seq_hdr->max_width + 0x7F) & ~0x7F;
     v->i_height = (img->seq_hdr->max_height + 0x7F) & ~0x7F;
     v->i_chroma = dec->fmt_out.i_codec;
+
+    dec->i_extra_picture_buffers = p_sys->s.max_frame_delay;
+    if (img->seq_hdr->super_res)
+        // dav1d seems to buffer more pictures when using super resolution
+        dec->i_extra_picture_buffers += p_sys->s.max_frame_delay > 1 ? 2 : 1;
 
     if (decoder_UpdateVideoFormat(dec) == 0)
     {

commit a3917a6ba3a268385141676e6b4bc1a5f6eed205

dav1d: fix compilation with older dav1d versions
This is a regression from 47486d9992c3c58a929ec535b2da59ecc325a367

modules/codec/dav1d.c
@@ -204,10 +204,14 @@ static int NewPicture(Dav1dPicture *img, void *cookie)
     v->i_height = (img->seq_hdr->max_height + 0x7F) & ~0x7F;
     v->i_chroma = dec->fmt_out.i_codec;
 
+#if DAV1D_API_VERSION_MAJOR >= 6
     dec->i_extra_picture_buffers = p_sys->s.max_frame_delay;
+#else
+    dec->i_extra_picture_buffers = (p_sys->s.n_frame_threads - 1);
+#endif
     if (img->seq_hdr->super_res)
         // dav1d seems to buffer more pictures when using super resolution
-        dec->i_extra_picture_buffers += p_sys->s.max_frame_delay > 1 ? 2 : 1;
+        dec->i_extra_picture_buffers += dec->i_extra_picture_buffers > 1 ? 2 : 1;
 
     if (decoder_UpdateVideoFormat(dec) == 0)
     {