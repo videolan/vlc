e2604b12b363ab0033c881116ad0509e3e3b72f4
Claudio Cambra <developer@claudiocambra.com>
2023-05-01 20:21:55 +0800

    macosx: Connect decorative view XIB components to outlets
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/windows/video/VLCMainVideoViewAudioMediaDecorativeView.h
+++ modules/gui/macosx/windows/video/VLCMainVideoViewAudioMediaDecorativeView.h
@@ -28,6 +28,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (instancetype)fromNibWithOwner:(id)owner;
 
+@property (readwrite, strong) IBOutlet NSImageView *foregroundCoverArtView;
+@property (readwrite, strong) IBOutlet NSImageView *backgroundCoverArtView;
+@property (readwrite, strong) IBOutlet NSVisualEffectView *backgroundVisualEffectView;
+
 @end
 
 NS_ASSUME_NONNULL_END
