d63a5eb061c11604bc04a56eda9f0730ec80b3bc
Claudio Cambra <developer@claudiocambra.com>
2023-05-13 17:38:43 +0800

    macosx: Set size of slider in prefs_widgets.m to NSControlSizeSmall, avoiding clipping
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/preferences/prefs_widgets.m
+++ modules/gui/macosx/preferences/prefs_widgets.m
@@ -369,6 +369,7 @@ o_textfield = [[NSSecureTextField alloc] initWithFrame: s_rc];              \
     [o_slider setToolTip: tooltip];                                         \
     [o_slider setMaxValue: higher];                                         \
     [o_slider setMinValue: lower];                                          \
+    [o_slider setControlSize: NSControlSizeSmall];                          \
 }
 
 #define ADD_CHECKBOX(o_checkbox, superFrame, x_offset, my_y_offset, label,  \
