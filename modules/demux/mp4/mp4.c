9a0f13b952db9896e895dcc1b39600cb8afe9242
Sebastian Dröge <sebastian@centricular.com>
2023-05-15 12:06:55 +0300

    demux: mp4: use correct CC field when extracting from cdt2
    This accidentally stored cdt2 as field 0/cc1 instead of field 1/cc2.
    
    (adapted from commit bb24a922a4657eb21a71c705ae0a7ad3c79aec11)

--- modules/demux/mp4/mp4.c
+++ modules/demux/mp4/mp4.c
@@ -578,7 +578,7 @@ static block_t * MP4_EIA608_Convert( block_t * p_block )
         i_remaining -= 8;
         do
         {
-            p_write[i_copied++] = CC_PKT_BYTE0(0); /* cc1 == field 0 */
+            p_write[i_copied++] = CC_PKT_BYTE0(1); /* cc2 == field 1 */
             p_write[i_copied++] = p_read[0];
             p_write[i_copied++] = p_read[1];
             p_read += 2;
