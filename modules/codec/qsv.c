videolan/vlc

commit a03fdfd9c1dc2cc99b30868de93c3eacd60cc478

codec: qsv: move enc->p_sys assignment down
Ensure that the p_enc->p_sys value is not set on error by moving it
down.

modules/codec/qsv.c
@@ -432,8 +432,6 @@ static int Open(vlc_object_t *this)
         return VLC_EGENERIC;
     }
 
-    enc->p_sys = sys;
-
     vlc_list_init(&sys->packets);
 
     config_ChainParse(enc, SOUT_CFG_PREFIX, sout_options, enc->p_cfg);
@@ -625,16 +623,15 @@ static int Open(vlc_object_t *this)
         .encode_video = Encode,
     };
     enc->ops = &ops;
+    enc->p_sys = sys;
 
     return VLC_SUCCESS;
 
  error:
     Close(enc);
-    enc->p_sys = NULL;
     return VLC_EGENERIC;
  nomem:
     Close(enc);
-    enc->p_sys = NULL;
     return VLC_ENOMEM;
 }
 

commit 78cd293ffc5b777f2ca05010b11c565eb30ad035

qsv: fix an error handling crash
By delaying `encoder->p_sys` initialization, a03fdfd9c1dc2cc99b30868de93c3eacd60cc478
introduced a crash due to `Close` requiring `encoder->p_sys` to be
valid.

This patches refactors the encoder closure to workaround that.

modules/codec/qsv.c
@@ -364,6 +364,19 @@ static uint64_t qsv_params_get_value(const char *const *text,
     return list[result];
 }
 
+static void delete_sys(encoder_sys_t *sys)
+{
+    MFXVideoENCODE_Close(sys->session);
+    MFXClose(sys->session);
+
+    assert(vlc_list_is_empty(&sys->packets));
+
+    if (sys->input_pool)
+        picture_pool_Release(sys->input_pool);
+
+    free(sys);
+}
+
 static int Open(vlc_object_t *this)
 {
     encoder_t *enc = (encoder_t *)this;
@@ -629,23 +642,16 @@ static int Open(vlc_object_t *this)
     return VLC_SUCCESS;
 
  error:
-    Close(enc);
+    delete_sys(sys);
     return VLC_EGENERIC;
  nomem:
-    Close(enc);
+    delete_sys(sys);
     return VLC_ENOMEM;
 }
 
 static void Close(encoder_t *enc)
 {
-    encoder_sys_t *sys = enc->p_sys;
-
-    MFXVideoENCODE_Close(sys->session);
-    MFXClose(sys->session);
-    assert(vlc_list_is_empty(&sys->packets));
-    if (sys->input_pool)
-        picture_pool_Release(sys->input_pool);
-    free(sys);
+    delete_sys(enc->p_sys);
 }
 
 /*