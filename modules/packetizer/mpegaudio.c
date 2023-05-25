ff6bd8f227e22939bf166339d58175ee1131882c
Francois Cartegnie <fcvlcdev@free.fr>
2023-05-25 15:32:43 +0700

    packetizer: mpegaudio: add layer 2 as input
    currently can be output by mp4.
    mp4->sout

--- modules/packetizer/mpegaudio.c
+++ modules/packetizer/mpegaudio.c
@@ -464,8 +464,9 @@ static int Open( vlc_object_t *p_this )
     decoder_t *p_dec = (decoder_t*)p_this;
     decoder_sys_t *p_sys;
 
-    if(( p_dec->fmt_in->i_codec != VLC_CODEC_MPGA ) &&
-       ( p_dec->fmt_in->i_codec != VLC_CODEC_MP3 ) )
+    if( p_dec->fmt_in->i_codec != VLC_CODEC_MPGA &&
+        p_dec->fmt_in->i_codec != VLC_CODEC_MP2 &&
+        p_dec->fmt_in->i_codec != VLC_CODEC_MP3 )
     {
         return VLC_EGENERIC;
     }
