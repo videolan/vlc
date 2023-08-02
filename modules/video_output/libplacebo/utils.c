videolan/vlc

commit d9eca774fc50df3af2404faf2ccbd2d15222b43f

libplacebo/utils: add new gamut mapping settings
The gamut mapping settings were substantially refactored upstream. Add
support for the new modes, while retaining backwards compatibility for
the older configuration API.

modules/video_output/libplacebo/utils.c
@@ -580,8 +580,31 @@ void vlc_placebo_ColorMapParams(vlc_object_t *obj, const char *prefix,
     char opt[64];
 
     *params = pl_color_map_default_params;
-    params->intent = var_InheritInteger(obj, PREFIX("rendering-intent"));
-    params->tone_mapping_param = var_InheritFloat(obj, PREFIX("tone-mapping-param"));
+
+    switch (var_InheritInteger(obj, PREFIX("gamut-mapping"))) {
+    case GAMUT_AUTO:        break;
+#if PL_API_VER >= 269
+    case GAMUT_CLIP:        params->gamut_mapping = &pl_gamut_map_clip; break;
+    case GAMUT_PERCEPTUAL:  params->gamut_mapping = &pl_gamut_map_perceptual; break;
+    case GAMUT_RELATIVE:    params->gamut_mapping = &pl_gamut_map_relative; break;
+    case GAMUT_SATURATION:  params->gamut_mapping = &pl_gamut_map_saturation; break;
+    case GAMUT_ABSOLUTE:    params->gamut_mapping = &pl_gamut_map_absolute; break;
+    case GAMUT_DESATURATE:  params->gamut_mapping = &pl_gamut_map_desaturate; break;
+    case GAMUT_DARKEN:      params->gamut_mapping = &pl_gamut_map_darken; break;
+    case GAMUT_WARN:        params->gamut_mapping = &pl_gamut_map_highlight; break;
+    case GAMUT_LINEAR:      params->gamut_mapping = &pl_gamut_map_linear; break;
+#else
+    case GAMUT_CLIP:        params->gamut_mode = PL_GAMUT_CLIP; break;
+    case GAMUT_PERCEPTUAL:  break; // unsupported
+    case GAMUT_RELATIVE:    params->intent = PL_INTENT_RELATIVE_COLORIMETRIC; break;
+    case GAMUT_SATURATION:  params->intent = PL_INTENT_SATURATION; break;
+    case GAMUT_ABSOLUTE:    params->intent = PL_INTENT_ABSOLUTE_COLORIMETRIC; break;
+    case GAMUT_DESATURATE:  params->gamut_mode = PL_GAMUT_DESATURATE;
+    case GAMUT_DARKEN:      params->gamut_mode = PL_GAMUT_DARKEN;
+    case GAMUT_WARN:        params->gamut_mode = PL_GAMUT_WARN;
+    case GAMUT_LINEAR:      break; // unsupported
+#endif
+    }
 
     switch (var_InheritInteger(obj, PREFIX("tone-mapping-function"))) {
     case TONEMAP_AUTO:      break;
@@ -596,12 +619,6 @@ void vlc_placebo_ColorMapParams(vlc_object_t *obj, const char *prefix,
     case TONEMAP_SPLINE:    params->tone_mapping_function = &pl_tone_map_spline; break;
     }
 
-    switch (var_InheritInteger(obj, PREFIX("gamut-mode"))) {
-    case GAMUT_MODE_CLIP:   params->gamut_mode = PL_GAMUT_CLIP; break;
-    case GAMUT_MODE_WARN:   params->gamut_mode = PL_GAMUT_WARN; break;
-    case GAMUT_MODE_DESAT:  params->gamut_mode = PL_GAMUT_DESATURATE; break;
-    case GAMUT_MODE_DARKEN: params->gamut_mode = PL_GAMUT_DARKEN; break;
-    }
-
+    params->tone_mapping_param = var_InheritFloat(obj, PREFIX("tone-mapping-param"));
     params->inverse_tone_mapping = var_InheritBool(obj, PREFIX("inverse-tone-mapping"));
 }

modules/video_output/libplacebo/utils.h
@@ -68,40 +68,22 @@ void vlc_placebo_ColorMapParams(vlc_object_t *obj, const char *prefix,
                                 struct pl_color_map_params *out_params);
 
 #define add_placebo_color_map_opts(prefix) \
-    add_integer(prefix"-rendering-intent", pl_color_map_default_params.intent, \
-            RENDER_INTENT_TEXT, RENDER_INTENT_LONGTEXT) \
-            change_integer_list(intent_values, intent_text) \
+    add_integer(prefix"-gamut-mapping", GAMUT_AUTO, \
+            GAMUT_MAPPING_TEXT, GAMUT_MAPPING_LONGTEXT) \
+            change_integer_list(gamut_values, gamut_text) \
+    add_obsolete_integer(prefix"-gamut-mode") /* since 4.0.0 */ \
+    add_obsolete_integer(prefix"-rendering-intent") /* since 4.0.0 */ \
     add_integer(prefix"-tone-mapping-function", TONEMAP_AUTO, \
             TONEMAP_FUNC_TEXT, TONEMAP_FUNC_LONGTEXT) \
             change_integer_list(tone_values, tone_text) \
     add_float(prefix"-tone-mapping-param", pl_color_map_default_params.tone_mapping_param, \
             TONEMAP_PARAM_TEXT, TONEMAP_PARAM_LONGTEXT) \
     add_obsolete_integer(prefix"-tone-mapping-mode") /* since 4.0.0 */ \
-    add_integer(prefix"-gamut-mode", GAMUT_MODE_CLIP, \
-            GAMUT_MODE_TEXT, GAMUT_MODE_LONGTEXT) \
-            change_integer_list(gamut_mode_values, gamut_mode_text) \
     add_bool(prefix"-inverse-tone-mapping", false, \
             INVERSE_TONEMAPPING_TEXT, INVERSE_TONEMAPPING_LONGTEXT) \
     add_obsolete_integer(prefix"-crosstalk") /* since 4.0.0 */
 
 // Shared options strings/structs for libplacebo options
-
-#define RENDER_INTENT_TEXT "Rendering intent for color conversion"
-#define RENDER_INTENT_LONGTEXT "The mapping type used to convert between color spaces."
-
-static const int intent_values[] = {
-    PL_INTENT_PERCEPTUAL,
-    PL_INTENT_RELATIVE_COLORIMETRIC,
-    PL_INTENT_SATURATION,
-    PL_INTENT_ABSOLUTE_COLORIMETRIC,
-};
-
-static const char * const intent_text[] = {
-    "Perceptual",
-    "Relative colorimetric",
-    "Absolute colorimetric",
-    "Saturation",
-};
 
 #define PRIM_TEXT "Override detected display primaries"
 #define PRIM_LONGTEXT "Override the auto-detected display primaries."
@@ -285,28 +267,46 @@ static const char * const tone_text[] = {
 #define TONEMAP_PARAM_LONGTEXT "This parameter can be used to tune the tone-mapping curve. Specifics depend on the curve used. If left as 0, the curve's preferred default is used."
 
 enum {
-    GAMUT_MODE_CLIP,
-    GAMUT_MODE_WARN,
-    GAMUT_MODE_DESAT,
-    GAMUT_MODE_DARKEN,
+    GAMUT_AUTO,
+    GAMUT_CLIP,
+    GAMUT_PERCEPTUAL,
+    GAMUT_RELATIVE,
+    GAMUT_SATURATION,
+    GAMUT_ABSOLUTE,
+    GAMUT_DESATURATE,
+    GAMUT_DARKEN,
+    GAMUT_WARN,
+    GAMUT_LINEAR,
 };
 
-static const int gamut_mode_values[] = {
-    GAMUT_MODE_CLIP,
-    GAMUT_MODE_WARN,
-    GAMUT_MODE_DESAT,
-    GAMUT_MODE_DARKEN,
+static const int gamut_values[] = {
+    GAMUT_AUTO,
+    GAMUT_CLIP,
+    GAMUT_PERCEPTUAL,
+    GAMUT_RELATIVE,
+    GAMUT_SATURATION,
+    GAMUT_ABSOLUTE,
+    GAMUT_DESATURATE,
+    GAMUT_DARKEN,
+    GAMUT_WARN,
+    GAMUT_LINEAR,
 };
 
-static const char * const gamut_mode_text[] = {
+static const char * const gamut_text[] = {
+    "Automatic selection",
     "Hard clip",
-    "Highlight invalid pixels",
+    "Perceptual soft-clip",
+    "Relative colorimetric",
+    "Saturation mapping",
+    "Absolute colorimetric",
     "Colorimetrically desaturate",
-    "Darken image",
+    "Darken and clip",
+    "Highlight invalid pixels",
+    "Linear desaturate",
 };
 
-#define GAMUT_MODE_TEXT "Out-of-gamut handling"
-#define GAMUT_MODE_LONGTEXT "How to handle out-of-gamut colors while tone mapping."
+#define GAMUT_MAPPING_TEXT "Out-of-gamut handling"
+#define GAMUT_MAPPING_LONGTEXT "How to handle out-of-gamut colors while tone mapping."
 
 #define INVERSE_TONEMAPPING_TEXT "Inverse tone-mapping"
 #define INVERSE_TONEMAPPING_LONGTEXT "Expand SDR signals to HDR (only works for certain curves)."

commit e1ebbce7d1a253e0b7bd5ee84b41aff9c9eddef9

libplacebo: utils: fix unwanted fall-through
Those cases are supposed to assign different values, but are overriding
each other because of the fall-through.

Regression from d9eca774fc50df3af2404faf2ccbd2d15222b43f when the
conditional PL_API < 269 is verified.

modules/video_output/libplacebo/utils.c
@@ -599,9 +599,9 @@ void vlc_placebo_ColorMapParams(vlc_object_t *obj, const char *prefix,
     case GAMUT_RELATIVE:    params->intent = PL_INTENT_RELATIVE_COLORIMETRIC; break;
     case GAMUT_SATURATION:  params->intent = PL_INTENT_SATURATION; break;
     case GAMUT_ABSOLUTE:    params->intent = PL_INTENT_ABSOLUTE_COLORIMETRIC; break;
-    case GAMUT_DESATURATE:  params->gamut_mode = PL_GAMUT_DESATURATE;
-    case GAMUT_DARKEN:      params->gamut_mode = PL_GAMUT_DARKEN;
-    case GAMUT_WARN:        params->gamut_mode = PL_GAMUT_WARN;
+    case GAMUT_DESATURATE:  params->gamut_mode = PL_GAMUT_DESATURATE; break;
+    case GAMUT_DARKEN:      params->gamut_mode = PL_GAMUT_DARKEN; break;
+    case GAMUT_WARN:        params->gamut_mode = PL_GAMUT_WARN; break;
     case GAMUT_LINEAR:      break; // unsupported
 #endif
     }