videolan/vlc

commit 8ff56952174af8e7a7d4ae61a413f88317c948a1

caopengllayer: ignore DISPLAY_SIZE controls
DISPLAY_SIZE controls are emitted by the windowing system to control the
display state, but the caopengllayer display module is handling its own
windowing state without window and doesn't need to react to those events.

Instead, store the new size into the internal vout_display_cfg_t and
avoid trying to report the size to itself.

Fixes #26845
Refs #25264

Cherry-picked from commit bd24889a0b7d0d223f751ea7c40598fd83f0b221.

Signed-off-by: Alexandre Janniaux <ajanni@videolabs.io>

modules/video_output/caopengllayer.m
@@ -496,6 +496,8 @@ static int Control(vout_display_t *vd, int query, va_list ap)
     switch (query)
     {
         case VOUT_DISPLAY_CHANGE_DISPLAY_SIZE:
+            return VLC_SUCCESS;
+
         case VOUT_DISPLAY_CHANGE_DISPLAY_FILLED:
         case VOUT_DISPLAY_CHANGE_ZOOM:
         case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
@@ -834,8 +836,11 @@ shouldInheritContentsScale:(CGFloat)newScale
         if (!_voutDisplay)
             return;
 
-        vout_display_SendEventDisplaySize(_voutDisplay,
-                                          newSize.width, newSize.height);
+        vout_display_sys_t *sys = _voutDisplay->sys;
+        @synchronized(sys->videoLayer) {
+            sys->cfg.display.width = newSize.width;
+            sys->cfg.display.height = newSize.height;
+        }
     }
 }
 

commit 5d4ade565056b7ab7323afdf8078aa54658b47d3

caopengllayer: restore vout_display_SendEventDisplaySize
VLC 4.0 doesn't need that but 3.0 still has no windowing module, leading
to display size not being reported to the subtitle text renderer, and
leading to blurry subtitles and OSD.

Because the size is coming from the display, it means that the first
time the subtitle is displayed, it will always be blurry depending on
the real display size and original media size.

Regression from 8ff56952174af8e7a7d4ae61a413f88317c948a1.

Fixes #27793

modules/video_output/caopengllayer.m
@@ -841,6 +841,10 @@ shouldInheritContentsScale:(CGFloat)newScale
             sys->cfg.display.width = newSize.width;
             sys->cfg.display.height = newSize.height;
         }
+
+        /* Workaround: there's no window module, so signal the correct size to the core. */
+        vout_display_SendEventDisplaySize (_voutDisplay,
+            newSize.width, newSize.height);
     }
 }
 