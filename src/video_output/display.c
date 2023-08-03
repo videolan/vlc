804b14d3cf87bccbab79ee82aff4e05eb997ae6a
Steve Lhomme <robux4@ycbcr.xyz>
2023-08-03 12:37:00 +0200

    vout/display: handle cropping bigger than visible dimensions
    Before it would just set the top/left values to 0, the visible width and
    the scaled height, always favoring the smaller value of the two.

--- src/video_output/display.c
+++ src/video_output/display.c
@@ -330,16 +330,21 @@ static void VoutDisplayCropRatio(int *left, int *top, int *right, int *bottom,
     unsigned scaled_width  = (uint64_t)source->i_visible_height * num * source->i_sar_den / den / source->i_sar_num;
     unsigned scaled_height = (uint64_t)source->i_visible_width  * den * source->i_sar_num / num / source->i_sar_den;
 
-    if (scaled_width < source->i_visible_width) {
+    if (source->i_visible_width > scaled_width) {
         *left   = (source->i_visible_width - scaled_width) / 2;
         *top    = 0;
         *right  = *left + scaled_width;
         *bottom = *top  + source->i_visible_height;
-    } else {
+    } else if (source->i_visible_height > scaled_height) {
         *left   = 0;
         *top    = (source->i_visible_height - scaled_height) / 2;
         *right  = *left + source->i_visible_width;
         *bottom = *top  + scaled_height;
+    } else {
+        *left   = 0;
+        *top    = 0;
+        *right  = source->i_visible_width;
+        *bottom = source->i_visible_height;
     }
 }
 
