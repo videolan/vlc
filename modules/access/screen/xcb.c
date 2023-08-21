fcca2403783494927c8ca5d15b659c6632f04a98
Steve Lhomme <robux4@ycbcr.xyz>
2023-08-21 10:34:37 +0200

    screen/xcb: replace VLC_CODEC_RGB32 with VLC_CODEC_XRGB
    This is the format once the mask will be "fixed".

--- modules/access/screen/xcb.c
+++ modules/access/screen/xcb.c
@@ -535,7 +535,7 @@ static es_out_id_t *InitES (demux_t *demux, uint_fast16_t width,
                 break;
             case 24:
                 if (fmt->bits_per_pixel == 32)
-                    chroma = VLC_CODEC_RGB32;
+                    chroma = VLC_CODEC_XRGB;
                 else if (fmt->bits_per_pixel == 24)
                     chroma = VLC_CODEC_RGB24;
                 break;
