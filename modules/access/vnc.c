8c12f1959263a7f9638660e738d44add62429211
Steve Lhomme <robux4@ycbcr.xyz>
2023-08-30 13:28:23 +0200

    access/vnc: use FixRGB to set the RGB16/24/32 mask
    The mask is exactly what we would set. It's better to signal we use the
    default values.

--- modules/access/vnc.c
+++ modules/access/vnc.c
@@ -219,20 +219,7 @@ static rfbBool mallocFrameBufferHandler( rfbClient* p_client )
 
     /* Fill input format */
     fmt.video.i_chroma = i_chroma;
-    switch( i_chroma )
-    {
-        case VLC_CODEC_RGB16:
-            fmt.video.i_rmask = 0xf800;
-            fmt.video.i_gmask = 0x07e0;
-            fmt.video.i_bmask = 0x001f;
-            break;
-        case VLC_CODEC_RGB24:
-        case VLC_CODEC_RGB32:
-            fmt.video.i_rmask = 0xff0000;
-            fmt.video.i_gmask = 0x00ff00;
-            fmt.video.i_bmask = 0x0000ff;
-            break;
-    }
+    video_format_FixRgb(&fmt.video);
 
     fmt.video.i_visible_width =
             fmt.video.i_width = i_width;
