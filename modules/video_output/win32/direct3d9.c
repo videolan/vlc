8d0f2b86ad03307ab8a4942f0ba0c05d3992eb03
Steve Lhomme <robux4@ycbcr.xyz>
2023-09-04 08:41:26 +0200

    direct3d9: request RGB24 without a mask
    The chroma corresponds to the mask.
    
    The chroma doesn't corresponds to the mask. It seems more logical
    given A8R8G8B8 and X8R8G8B8 map the BGR equivalents [1].
    
    [1] https://learn.microsoft.com/en-us/windows/win32/direct3d10/d3d10-graphics-programming-guide-resources-legacy-formats

--- modules/video_output/win32/direct3d9.c
+++ modules/video_output/win32/direct3d9.c
@@ -1343,7 +1343,7 @@ static const d3d9_format_t d3d_formats[] = {
     { "YUY2",       D3DFMT_YUY2,    VLC_CODEC_YUYV,  0,0,0 },
     { "X8R8G8B8",   D3DFMT_X8R8G8B8,VLC_CODEC_BGRX,  0,0,0 },
     { "A8R8G8B8",   D3DFMT_A8R8G8B8,VLC_CODEC_BGRA,  0,0,0 },
-    { "8G8B8",      D3DFMT_R8G8B8,  VLC_CODEC_RGB24M, 0xff0000, 0x00ff00, 0x0000ff },
+    { "R8G8B8",     D3DFMT_R8G8B8,  VLC_CODEC_BGR24, 0,0,0 },
     { "R5G6B5",     D3DFMT_R5G6B5,  VLC_CODEC_RGB16, 0x1f<<11, 0x3f<<5,  0x1f<<0 },
     { "X1R5G5B5",   D3DFMT_X1R5G5B5,VLC_CODEC_RGB15, 0x1f<<10, 0x1f<<5,  0x1f<<0 },
 
