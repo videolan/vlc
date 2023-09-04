b7e2b6a33d29694fbdeb911a1a147b3784a1dc05
Steve Lhomme <robux4@ycbcr.xyz>
2023-09-04 08:55:46 +0200

    demux/mp4: map 24-bit 'r','a','w',' ' to VLC_CODEC_RGB24
    It can't depend on endianness.
    
    It was probably the intention of fe489afd03fc820767b1a684d2bea3f4e201cbd4.
    In libavformat it's mapped to AV_PIX_FMT_RGB24.

--- modules/demux/mp4/essetup.c
+++ modules/demux/mp4/essetup.c
@@ -442,7 +442,7 @@ int SetupVideoES( demux_t *p_demux, const mp4_track_t *p_track, const MP4_Box_t 
                     p_fmt->i_codec = VLC_CODEC_RGB15;
                     break;
                 case 24:
-                    p_fmt->i_codec = VLC_CODEC_RGB24M;
+                    p_fmt->i_codec = VLC_CODEC_RGB24;
                     break;
                 case 32:
                     p_fmt->i_codec = VLC_CODEC_ARGB;
