a7463ceeeaa0f5761dff208fb899aca8070ad0ef
Steve Lhomme <robux4@ycbcr.xyz>
2023-09-06 14:37:10 +0200

    demux/mp4: map 16-bit raw to VLC_CODEC_RGB555BE
    In FFmpeg it's mapped to AV_PIX_FMT_RGB555BE [1] which corresponds
    to our VLC_CODEC_RGB555BE.
    
    [1] https://github.com/FFmpeg/FFmpeg/blob/27562bf0226b22b0dbc631f8177e17ef98028499/libavformat/movenc.c#L1743

--- modules/demux/mp4/essetup.c
+++ modules/demux/mp4/essetup.c
@@ -439,7 +439,7 @@ int SetupVideoES( demux_t *p_demux, const mp4_track_t *p_track, const MP4_Box_t 
         case VLC_FOURCC('r','a','w',' '):
             switch( p_vide->i_depth ) {
                 case 16:
-                    p_fmt->i_codec = VLC_CODEC_RGB15;
+                    p_fmt->i_codec = VLC_CODEC_RGB555BE;
                     break;
                 case 24:
                     p_fmt->i_codec = VLC_CODEC_RGB24;
