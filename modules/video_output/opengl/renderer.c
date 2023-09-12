videolan/vlc

commit e67f66ca50372e1c247d0d67bee3b0a92e7a9924

opengl: renderer: fix input variable qualifier

modules/video_output/opengl/renderer.c
@@ -195,7 +195,7 @@ opengl_link_program(struct vlc_gl_filter *filter)
         "}\n";
 
     static const char *const FRAGMENT_SHADER_BODY =
-        "varying vec2 PicCoords;\n"
+        "attribute vec2 PicCoords;\n"
         "void main() {\n"
         " gl_FragColor = vlc_texture(PicCoords);\n"
         "}\n";

commit 5ebfb251661d7587031261312154982d5acadafb

vout: opengl: fix var storage qualifier in renderer fragment shader
It fixes a GLSL storage qualifier issue introduced by a change in e67f66ca

Here `varying` storage qualifier has to be used in order to compile properly and get the value passed from the vertex shader

modules/video_output/opengl/renderer.c
@@ -203,7 +203,7 @@ opengl_link_program(struct vlc_gl_filter *filter)
     static const char *const FRAGMENT_SHADER_BODY =
         "#if __VERSION__ < 300\n"
           "#define FragColor gl_FragColor\n"
-          "attribute vec2 PicCoords;\n"
+          "varying vec2 PicCoords;\n"
         "#else\n"
           "in vec2 PicCoords;\n"
           "out vec4 FragColor;\n"