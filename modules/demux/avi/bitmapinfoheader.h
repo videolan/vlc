e9879dc95ba5d2d8f265390fc30bd2be5dd309b2
Steve Lhomme <robux4@ycbcr.xyz>
2023-09-12 09:44:16 +0200

    bitmapinfoheader: map 16-bit BI_RGB to VLC_CODEC_BGR565LE
    This corresponds to the actual format expected by the Windows player.

--- modules/demux/avi/bitmapinfoheader.h
+++ modules/demux/avi/bitmapinfoheader.h
@@ -267,6 +267,7 @@ static inline int ParseBitmapInfoHeader( const VLC_BITMAPINFOHEADER *p_bih, size
             {
                 case 32: bi_rgb_chroma = VLC_CODEC_XBGR; break;
                 case 24: bi_rgb_chroma = VLC_CODEC_BGR24; break;
+                case 16: bi_rgb_chroma = VLC_CODEC_BGR565LE; break;
                 default: bi_rgb_chroma = 0; break;
             }
             if (bi_rgb_chroma != 0)
@@ -354,6 +355,9 @@ static inline int CreateBitmapInfoHeader( const es_format_t *fmt,
             break;
         case VLC_CODEC_RGB24:
             return VLC_EINVAL;
+        case VLC_CODEC_BGR565LE:
+            biBitCount = 16;
+            break;
         case VLC_CODEC_RGB16:
         case VLC_CODEC_RGB15:
             biBitCount = 16;
