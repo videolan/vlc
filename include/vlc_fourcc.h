98d81db945f9f15aacc480e610b1b164c1ae35d9
Steve Lhomme <robux4@ycbcr.xyz>
2023-09-13 16:58:48 +0200

    fourcc: change the VLC_CODEC_RGB24 FourCC to RV24
    So that code passing a chroma as a string ends up on the on the same chroma
    layout.

--- include/vlc_fourcc.h
+++ include/vlc_fourcc.h
@@ -348,7 +348,7 @@
 /* 16-bit RGB (using explicit masks) */
 #define VLC_CODEC_RGB16           VLC_FOURCC('R','V','1','6')
 /* 24-bit RGB (using explicit masks) */
-#define VLC_CODEC_RGB24M          VLC_FOURCC('R','V','2','4')
+#define VLC_CODEC_RGB24M          VLC_FOURCC('R','_','2','4')
 /* 24-bit RGB padded to 32 bits (using explicit masks) */
 #define VLC_CODEC_RGB32           VLC_FOURCC('R','V','3','2')
 
@@ -362,7 +362,7 @@
 #define VLC_CODEC_XBGR            VLC_FOURCC('X','B','G','R')
 
 /* 24-bit RGB, in memory address order: "RGB" */
-#define VLC_CODEC_RGB24           VLC_FOURCC('R','G','B','3')
+#define VLC_CODEC_RGB24           VLC_FOURCC('R','V','2','4')
 /* 24-bit BGR, in memory address order: "BGR" */
 #define VLC_CODEC_BGR24           VLC_FOURCC('B','G','R','3')
 
