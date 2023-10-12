db95927947d791e84604a8d3a174036e46599af8
Steve Lhomme <robux4@ycbcr.xyz>
2023-10-12 10:54:57 +0200

    chroma: cvpx: use local variable consistently

--- modules/video_chroma/cvpx.c
+++ modules/video_chroma/cvpx.c
@@ -121,7 +121,7 @@ static void Copy(filter_t *p_filter, picture_t *dst, picture_t *src,
                 DO(Copy420_SP_to_SP);
             else
             {
-                assert(dst->format.i_chroma == VLC_CODEC_I420_10L);
+                assert(outfcc == VLC_CODEC_I420_10L);
                 DO_S(Copy420_16_SP_to_P, 6);
             }
             break;
