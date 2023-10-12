videolan/vlc

commit 1ea223dff935004e110542a6894c8b22d602876d

vlc_fourcc: add defines for host endianness 15-bit and 16-bit chromas
They are the chromas that should normally be used.

include/vlc_fourcc.h
@@ -371,6 +371,18 @@
 /* 16-bit BGR Little-Endian: "g3R5 B5G3" */
 #define VLC_CODEC_BGR565LE          VLC_FOURCC('B','L','1','6')
 
+#ifdef WORDS_BIGENDIAN
+/* 16-bit RGB with host endianness */
+#define VLC_CODEC_RGB565    VLC_CODEC_RGB565BE
+/* 16-bit BGR with host endianness */
+#define VLC_CODEC_BGR565    VLC_CODEC_BGR565BE
+#else
+/* 16-bit RGB with host endianness */
+#define VLC_CODEC_RGB565    VLC_CODEC_RGB565LE
+/* 16-bit BGR with host endianness */
+#define VLC_CODEC_BGR565    VLC_CODEC_BGR565LE
+#endif
+
 /* 15-bit RGB Big-Endian: "0R5G5B5" */
 #define VLC_CODEC_RGB555BE          VLC_FOURCC('R','B','1','5')
 /* 15-bit RGB Little-Endian: "g3B5 0R5G2" */
@@ -379,6 +391,19 @@
 #define VLC_CODEC_BGR555BE          VLC_FOURCC('B','B','1','5')
 /* 15-bit BGR Little-Endian: "g3R5 0B5G2" */
 #define VLC_CODEC_BGR555LE          VLC_FOURCC('B','L','1','5')
+
+#ifdef WORDS_BIGENDIAN
+/* 15-bit RGB with host endianness */
+#define VLC_CODEC_RGB555    VLC_CODEC_RGB555BE
+/* 15-bit BGR with host endianness */
+#define VLC_CODEC_BGR555    VLC_CODEC_BGR565BE
+#else
+/* 15-bit RGB with host endianness */
+#define VLC_CODEC_RGB555    VLC_CODEC_RGB555LE
+/* 15-bit BGR with host endianness */
+#define VLC_CODEC_BGR555    VLC_CODEC_BGR555LE
+#endif
+
 
 /* 8-bit RGB "R3G3B2" */
 #define VLC_CODEC_RGB332           VLC_FOURCC('R','3','3','2')

commit 235ba564cf74e4bcef7fad9c984274b326f046a7

vlc_fourcc: Fix regression introduced in 1ea223dff935004e110542a6894c8b22d602876d
VLC_CODEC_BGR555 should be defined on BE architectures as VLC_CODEC_BGR555BE and not VLC_CODEC_BGR565BE

Fixes build failure:

video_filter/blend.cpp: In constructor ‘{anonymous}::CPictureRGB16::CPictureRGB16(const {anonymous}::CPicture&)’:
video_filter/blend.cpp:357:9: error: duplicate case value
  357 |         case VLC_CODEC_BGR555:
      |         ^~~~
video_filter/blend.cpp:347:9: note: previously used here
  347 |         case VLC_CODEC_BGR565:
      |         ^~~~
video_filter/blend.cpp: In constructor ‘{anonymous}::convertRgbToRgbSmall::convertRgbToRgbSmall(const video_format_t*, const video_format_t*)’:
video_filter/blend.cpp:506:9: error: duplicate case value
  506 |         case VLC_CODEC_BGR555:
      |         ^~~~
video_filter/blend.cpp:496:9: note: previously used here
  496 |         case VLC_CODEC_BGR565:
      |         ^~~~

include/vlc_fourcc.h
@@ -384,7 +384,7 @@
 /* 15-bit RGB with host endianness */
 #define VLC_CODEC_RGB555    VLC_CODEC_RGB555BE
 /* 15-bit BGR with host endianness */
-#define VLC_CODEC_BGR555    VLC_CODEC_BGR565BE
+#define VLC_CODEC_BGR555    VLC_CODEC_BGR555BE
 #else
 /* 15-bit RGB with host endianness */
 #define VLC_CODEC_RGB555    VLC_CODEC_RGB555LE