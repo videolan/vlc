videolan/vlc

commit 8c047bb483c2d9ad08cfc04a085fbbe690e6fd1d

packetizer: hevc: provide picture left/top crop info

modules/packetizer/hevc_nal.c
@@ -1054,7 +1054,9 @@ bool hevc_get_sps_profile_tier_level( const hevc_sequence_parameter_set_t *p_sps
 }
 
 bool hevc_get_picture_size( const hevc_sequence_parameter_set_t *p_sps,
-                            unsigned *p_w, unsigned *p_h, unsigned *p_vw, unsigned *p_vh )
+                            unsigned *p_ox, unsigned *p_oy,
+                            unsigned *p_w, unsigned *p_h,
+                            unsigned *p_vw, unsigned *p_vh )
 {
     *p_w = *p_vw = p_sps->pic_width_in_luma_samples;
     *p_h = *p_vh = p_sps->pic_height_in_luma_samples;
@@ -1078,6 +1080,8 @@ bool hevc_get_picture_size( const hevc_sequence_parameter_set_t *p_sps,
             sub_height_c = 1;
         }
 
+        *p_oy = p_sps->conf_win.top_offset * sub_height_c;
+        *p_ox = p_sps->conf_win.left_offset * sub_width_c;
         *p_vh -= (p_sps->conf_win.bottom_offset + p_sps->conf_win.top_offset) * sub_height_c;
         *p_vw -= (p_sps->conf_win.left_offset +  p_sps->conf_win.right_offset) * sub_width_c;
     }

modules/codec/hxxx_helper.c
@@ -822,9 +822,8 @@ hxxx_helper_get_current_picture_size(const struct hxxx_helper *hh,
     }
     else if(hh->i_codec == VLC_CODEC_HEVC)
     {
-        *p_ox = *p_oy = 0;
         const struct hxxx_helper_nal *hsps = &hh->hevc.sps_list[hh->hevc.i_current_sps];
-        if(hsps && hsps->hevc_sps && hevc_get_picture_size(hsps->hevc_sps, p_w, p_h, p_vw, p_vh))
+        if(hsps && hsps->hevc_sps && hevc_get_picture_size(hsps->hevc_sps, p_ox, p_oy, p_w, p_h, p_vw, p_vh))
             return VLC_SUCCESS;
     }
     return VLC_EGENERIC;

modules/packetizer/hevc.c
@@ -616,16 +616,19 @@ static void ActivateSets(decoder_t *p_dec,
                                          &p_dec->fmt_out.video.color_range);
         }
 
-        unsigned sizes[4];
+        unsigned sizes[6];
         if( hevc_get_picture_size( p_sps, &sizes[0], &sizes[1],
-                                          &sizes[2], &sizes[3] ) )
+                                          &sizes[2], &sizes[3],
+                                          &sizes[4], &sizes[5] ) )
         {
-            p_dec->fmt_out.video.i_width = sizes[0];
-            p_dec->fmt_out.video.i_height = sizes[1];
+            p_dec->fmt_out.video.i_x_offset = sizes[0];
+            p_dec->fmt_out.video.i_y_offset = sizes[1];
+            p_dec->fmt_out.video.i_width = sizes[2];
+            p_dec->fmt_out.video.i_height = sizes[3];
             if(p_dec->fmt_in->video.i_visible_width == 0)
             {
-                p_dec->fmt_out.video.i_visible_width = sizes[2];
-                p_dec->fmt_out.video.i_visible_height = sizes[3];
+                p_dec->fmt_out.video.i_visible_width = sizes[4];
+                p_dec->fmt_out.video.i_visible_height = sizes[5];
             }
         }
 

modules/packetizer/hevc_nal.h
@@ -283,7 +283,9 @@ bool hevc_get_sps_profile_tier_level( const hevc_sequence_parameter_set_t *,
                                       uint8_t *pi_profile, uint8_t *pi_level );
 enum vlc_hevc_profile_e
      hevc_get_vlc_profile( const hevc_sequence_parameter_set_t * );
-bool hevc_get_picture_size( const hevc_sequence_parameter_set_t *, unsigned *p_w, unsigned *p_h,
+bool hevc_get_picture_size( const hevc_sequence_parameter_set_t *,
+                            unsigned *p_ox, unsigned *p_oy,
+                            unsigned *p_w, unsigned *p_h,
                             unsigned *p_vw, unsigned *p_vh );
 bool hevc_get_frame_rate( const hevc_sequence_parameter_set_t *,
                           const hevc_video_parameter_set_t * /* can be NULL */,

commit d6f3ef7aaa28419b61cffd8314be9d3826383282

packetizer: hevc: fix offset defaults on missing info
regression by 8c047bb483c2d9ad08cfc04a085fbbe690e6fd1d

modules/packetizer/hevc_nal.c
@@ -1085,6 +1085,10 @@ bool hevc_get_picture_size( const hevc_sequence_parameter_set_t *p_sps,
         *p_vh -= (p_sps->conf_win.bottom_offset + p_sps->conf_win.top_offset) * sub_height_c;
         *p_vw -= (p_sps->conf_win.left_offset +  p_sps->conf_win.right_offset) * sub_width_c;
     }
+    else
+    {
+        *p_oy = *p_ox = 0;
+    }
     return true;
 }
 