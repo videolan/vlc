04c2b96e960446e5d611b056916260e1311da92d
Claudio Cambra <developer@claudiocambra.com>
2023-10-29 16:54:17 +0800

    macosx: Lazily fetch parent item in VLCLibraryRepresentedItem
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/VLCLibraryRepresentedItem.m
+++ modules/gui/macosx/library/VLCLibraryRepresentedItem.m
@@ -22,11 +22,15 @@
 
 #import "VLCLibraryRepresentedItem.h"
 
+#import "extensions/NSString+Helpers.h"
+
 #import "library/VLCLibraryDataTypes.h"
+#import "library/audio-library/VLCLibraryAllAudioGroupsMediaLibraryItem.h"
 
 @interface VLCLibraryRepresentedItem ()
 {
     NSInteger _itemIndexInParent;
+    id<VLCMediaLibraryItemProtocol> _parentItem;
 }
 @end
 
@@ -76,6 +80,77 @@
 
     _itemIndexInParent = index;
     return _itemIndexInParent;
+}
+
+- (const id<VLCMediaLibraryItemProtocol>)parentItemForItem:(const id<VLCMediaLibraryItemProtocol>)item
+{
+    const enum vlc_ml_parent_type parentType = self.parentType;
+    if (parentType == VLC_ML_PARENT_UNKNOWN) {
+        return [[VLCLibraryAllAudioGroupsMediaLibraryItem alloc] initWithDisplayString:_NS("All items")];
+    }
+
+    // Decide which other items we are going to be adding to the playlist when playing the item.
+    // Key for playing in library mode, not in individual mode
+    int64_t parentItemId = -1;
+
+    if ([item isKindOfClass:VLCMediaLibraryMediaItem.class]) {
+        VLCMediaLibraryMediaItem * const mediaItem = (VLCMediaLibraryMediaItem *)item;
+
+        switch (parentType) {
+        case VLC_ML_PARENT_ALBUM:
+            parentItemId = mediaItem.albumID;
+            break;
+        case VLC_ML_PARENT_ARTIST:
+            parentItemId = mediaItem.artistID;
+            break;
+        case VLC_ML_PARENT_GENRE:
+            parentItemId = mediaItem.genreID;
+            break;
+        default:
+            break;
+        }
+    } else if ([item isKindOfClass:VLCMediaLibraryAlbum.class]) {
+        VLCMediaLibraryAlbum * const album = (VLCMediaLibraryAlbum *)item;
+
+        switch (parentType) {
+        case VLC_ML_PARENT_ARTIST:
+            parentItemId = album.artistID;
+            break;
+        case VLC_ML_PARENT_GENRE:
+        {
+            VLCMediaLibraryMediaItem * const firstChildItem = album.firstMediaItem;
+            parentItemId = firstChildItem.genreID;
+            break;
+        }
+        default:
+            break;
+        }
+    }
+
+    if (parentItemId < 0) {
+        return nil;
+    }
+
+    switch (parentType) {
+    case VLC_ML_PARENT_ALBUM:
+        return [VLCMediaLibraryAlbum albumWithID:parentItemId];
+    case VLC_ML_PARENT_ARTIST:
+        return [VLCMediaLibraryArtist artistWithID:parentItemId];
+    case VLC_ML_PARENT_GENRE:
+        return [VLCMediaLibraryGenre genreWithID:parentItemId];
+    default:
+        return nil;
+    }
+}
+
+- (id<VLCMediaLibraryItemProtocol>)parentItem
+{
+    if (_parentItem != nil) {
+        return _parentItem;
+    }
+
+    _parentItem = [self parentItemForItem:self.item];
+    return _parentItem;
 }
 
 @end
