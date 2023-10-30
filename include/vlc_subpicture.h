7df9e9cb4f281a4be8e6350bf41b69e7f3fda88f
Steve Lhomme <robux4@ycbcr.xyz>
2023-10-30 09:18:00 +0100

    vlc_subpictures: add helper functions for vlc_spu_regions

--- include/vlc_subpicture.h
+++ include/vlc_subpicture.h
@@ -90,6 +90,19 @@ struct subpicture_region_t
 
 typedef struct vlc_list vlc_spu_regions;
 
+#define vlc_spu_regions_init(p_rs) \
+    vlc_list_init((p_rs))
+#define vlc_spu_regions_push(p_rs,reg) \
+    vlc_list_append(&(reg)->node, (p_rs))
+#define vlc_spu_regions_foreach(reg,p_rs) \
+    vlc_list_foreach(reg, (p_rs), node)
+#define vlc_spu_regions_is_empty(p_rs) \
+    vlc_list_is_empty((p_rs))
+#define vlc_spu_regions_first_or_null(p_rs) \
+    vlc_list_first_entry_or_null((p_rs), subpicture_region_t, node)
+#define vlc_spu_regions_remove(p_rs, reg) \
+    vlc_list_remove(&(reg)->node)
+
 struct vlc_spu_highlight_t
 {
     int x_start;
