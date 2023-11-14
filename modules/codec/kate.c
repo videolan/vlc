b7ce4340bcbd5f0d60a87a8b029f09858d65454b
Steve Lhomme <robux4@ycbcr.xyz>
2023-11-14 11:11:36 +0100

    kate: merge TigerValidateSubpicture into TigerUpdateSubpicture

--- modules/codec/kate.c
+++ modules/codec/kate.c
@@ -795,46 +795,6 @@ static void PostprocessTigerImage( plane_t *p_plane, unsigned int i_width )
     PROFILE_STOP( tiger_renderer_postprocess );
 }
 
-static int TigerValidateSubpicture( subpicture_t *p_subpic,
-                                    bool b_fmt_src, const video_format_t *p_fmt_src,
-                                    bool b_fmt_dst, const video_format_t *p_fmt_dst,
-                                    vlc_tick_t ts )
-{
-    VLC_UNUSED(p_fmt_src); VLC_UNUSED(p_fmt_dst);
-
-    kate_spu_updater_sys_t *p_spusys = p_subpic->updater.p_sys;
-    decoder_sys_t *p_sys = p_spusys->p_dec_sys;
-
-    if( b_fmt_src || b_fmt_dst )
-        return VLC_EGENERIC;
-
-    PROFILE_START( TigerValidateSubpicture );
-
-    /* time in seconds from the start of the stream */
-    kate_float t = (p_spusys->i_start + ts - p_subpic->i_start ) / 1000000.0f;
-
-    /* it is likely that the current region (if any) can be kept as is; test for this */
-    vlc_mutex_lock( &p_sys->lock );
-    int i_ret;
-    if( p_sys->b_dirty || tiger_renderer_is_dirty( p_sys->p_tr ) )
-    {
-        i_ret = VLC_EGENERIC;
-        goto exit;
-    }
-    if( tiger_renderer_update( p_sys->p_tr, t, 1 ) >= 0 &&
-        tiger_renderer_is_dirty( p_sys->p_tr ) )
-    {
-        i_ret = VLC_EGENERIC;
-        goto exit;
-    }
-
-    i_ret = VLC_SUCCESS;
-exit:
-    vlc_mutex_unlock( &p_sys->lock );
-    PROFILE_STOP( TigerValidateSubpicture );
-    return i_ret;
-}
-
 /* Tiger renders can end up looking a bit crap since they get overlaid on top of
    a subsampled YUV image, so there can be a fair amount of chroma bleeding.
    Looks good with white though since it's all luma. Hopefully that will be the
@@ -847,17 +807,39 @@ static void TigerUpdateSubpicture( subpicture_t *p_subpic,
     kate_spu_updater_sys_t *p_spusys = p_subpic->updater.p_sys;
     decoder_sys_t *p_sys = p_spusys->p_dec_sys;
     plane_t *p_plane;
-    kate_float t;
-    int i_ret;
+    /* time in seconds from the start of the stream */
+    kate_float t = (p_spusys->i_start + ts - p_subpic->i_start ) / 1000000.0f;
 
-    if (TigerValidateSubpicture(p_subpic, b_fmt_src, p_fmt_src,
-                                          b_fmt_dst, p_fmt_dst, ts) == VLC_SUCCESS)
+    bool new_regions = false;
+    if( b_fmt_src || b_fmt_dst )
+        new_regions = true;
+
+    if (!new_regions)
+    {
+    PROFILE_START( TigerValidateSubpicture );
+
+    /* it is likely that the current region (if any) can be kept as is; test for this */
+    vlc_mutex_lock( &p_sys->lock );
+    if( p_sys->b_dirty || tiger_renderer_is_dirty( p_sys->p_tr ) )
+    {
+        new_regions = true;
+        goto exit;
+    }
+    if( tiger_renderer_update( p_sys->p_tr, t, 1 ) >= 0 &&
+        tiger_renderer_is_dirty( p_sys->p_tr ) )
+    {
+        new_regions = true;
+        goto exit;
+    }
+exit:
+    vlc_mutex_unlock( &p_sys->lock );
+    PROFILE_STOP( TigerValidateSubpicture );
+    }
+
+    if (!new_regions)
         return;
 
     vlc_spu_regions_Clear( &p_subpic->regions );
-
-    /* time in seconds from the start of the stream */
-    t = (p_spusys->i_start + ts - p_subpic->i_start ) / 1000000.0f;
 
     PROFILE_START( TigerUpdateSubpicture );
 
@@ -880,6 +862,7 @@ static void TigerUpdateSubpicture( subpicture_t *p_subpic,
 
     vlc_mutex_lock( &p_sys->lock );
 
+    int i_ret;
     p_plane = &p_r->p_picture->p[0];
     i_ret = tiger_renderer_set_buffer( p_sys->p_tr, p_plane->p_pixels, fmt.i_width, p_plane->i_lines, p_plane->i_pitch, 1 );
     if( i_ret < 0 )
