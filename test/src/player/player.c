c156bbfdfb1fd1a8f26455a10ff6205e794971a8
Thomas Guillem <thomas@gllm.fr>
2023-11-21 17:14:32 +0100

    test: player: add an aout module
    In order to not rely on adummy behavior.

--- test/src/player/player.c
+++ test/src/player/player.c
@@ -2182,7 +2182,7 @@ ctx_init(struct ctx *ctx, enum ctx_flags flags)
         "--codec=araw,rawvideo,subsdec,"TELETEXT_DECODER"none",
         "--dec-dev=none",
         (flags & DISABLE_VIDEO_OUTPUT) ? "--vout=none" : "--vout=dummy",
-        (flags & DISABLE_AUDIO_OUTPUT) ? "--aout=none" : "--aout=dummy",
+        (flags & DISABLE_AUDIO_OUTPUT) ? "--aout=none" : "--aout=test_src_player",
         (flags & DISABLE_VIDEO) ? "--no-video" : "--video",
         (flags & DISABLE_AUDIO) ? "--no-audio" : "--audio",
         "--text-renderer=tdummy",
@@ -3008,3 +3008,80 @@ main(void)
     libvlc_release(dummy);
     return 0;
 }
+
+#define MODULE_NAME test_src_player
+#undef VLC_DYNAMIC_PLUGIN
+#include <vlc_plugin.h>
+/* Define a builtin module for mocked parts */
+const char vlc_module_name[] = MODULE_STRING;
+
+struct aout_sys
+{
+    vlc_tick_t first_play_date;
+    vlc_tick_t pos;
+};
+
+static void aout_Play(audio_output_t *aout, block_t *block, vlc_tick_t date)
+{
+    struct aout_sys *sys = aout->sys;
+
+    if (sys->first_play_date == VLC_TICK_INVALID)
+        sys->first_play_date = date;
+
+    aout_TimingReport(aout, sys->first_play_date + sys->pos - VLC_TICK_0, sys->pos);
+    sys->pos += block->i_length;
+    block_Release(block);
+}
+
+static void aout_Flush(audio_output_t *aout)
+{
+    struct aout_sys *sys = aout->sys;
+    sys->pos = 0;
+    sys->first_play_date = VLC_TICK_INVALID;
+}
+
+static int aout_Start(audio_output_t *aout, audio_sample_format_t *restrict fmt)
+{
+    (void) aout;
+    return AOUT_FMT_LINEAR(fmt) ? VLC_SUCCESS : VLC_EGENERIC;
+}
+
+static void
+aout_Close(vlc_object_t *obj)
+{
+    audio_output_t *aout = (audio_output_t *)obj;
+    free(aout->sys);
+}
+
+static int aout_Open(vlc_object_t *obj)
+{
+    audio_output_t *aout = (audio_output_t *)obj;
+
+    aout->start = aout_Start;
+    aout->play = aout_Play;
+    aout->pause = NULL;
+    aout->flush = aout_Flush;
+    aout->stop = aout_Flush;
+    aout->volume_set = NULL;
+    aout->mute_set = NULL;
+
+    struct aout_sys *sys = aout->sys = malloc(sizeof(*sys));
+    assert(sys != NULL);
+    aout_Flush(aout);
+
+    return VLC_SUCCESS;
+}
+
+vlc_module_begin()
+    /* This aout module will report audio timings perfectly, but without any
+     * delay, in order to be usable for player tests. Indeed, this aout will
+     * report timings immediately from Play(), but points will be in the
+     * future (like when aout->time_get() is used). */
+    set_capability("audio output", 0)
+    set_callbacks(aout_Open, aout_Close)
+vlc_module_end()
+
+VLC_EXPORT const vlc_plugin_cb vlc_static_modules[] = {
+    VLC_SYMBOL(vlc_entry),
+    NULL
+};
