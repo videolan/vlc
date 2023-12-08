videolan/vlc

commit ad08ac23151d82c5eceec15db17b2e6d2b366758

mux/mp4: reduce use of FifoGet/FifoCount
Rework MuxStream to take in block_t instead of getting it itself from fifo

we can't handle whole fifo in Mux, as it checks smallest timestamp to mux for each packet

modules/mux/mp4/mp4.c
@@ -221,7 +221,7 @@ static void box_send(sout_mux_t *p_mux,  bo_t *box);
 
 static block_t *ConvertSUBT(block_t *);
 static bool CreateCurrentEdit(mp4_stream_t *, vlc_tick_t, bool);
-static int MuxStream(sout_mux_t *p_mux, sout_input_t *p_input, mp4_stream_t *p_stream);
+static int MuxStream(sout_mux_t *p_mux, sout_input_t *p_input, mp4_stream_t *p_stream, block_t *p_data);
 
 static int WriteSlowStartHeader(sout_mux_t *p_mux)
 {
@@ -540,8 +540,16 @@ static void DelStream(sout_mux_t *p_mux, sout_input_t *p_input)
 
     if(!mp4mux_Is(p_sys->muxh, FRAGMENTED))
     {
-        while(block_FifoCount(p_input->p_fifo) > 0 &&
-              MuxStream(p_mux, p_input, p_stream) == VLC_SUCCESS) {};
+        vlc_fifo_Lock( p_input->p_fifo );
+        block_t *p_data = vlc_fifo_DequeueAllUnlocked( p_input->p_fifo );
+        vlc_fifo_Unlock( p_input->p_fifo );
+        while( p_data != NULL )
+        {
+              block_t *p_next = p_data->p_next;
+              p_data->p_next = NULL;
+              MuxStream(p_mux, p_input, p_stream, p_data);
+              p_data = p_next;
+        }
 
         if(CreateCurrentEdit(p_stream, p_sys->i_start_dts, false))
             mp4mux_track_DebugEdits(VLC_OBJECT(p_mux), p_stream->tinfo);
@@ -637,11 +645,10 @@ static inline vlc_tick_t dts_fb_pts( const block_t *p_data )
     return p_data->i_dts != VLC_TICK_INVALID ? p_data->i_dts: p_data->i_pts;
 }
 
-static int MuxStream(sout_mux_t *p_mux, sout_input_t *p_input, mp4_stream_t *p_stream)
+static int MuxStream(sout_mux_t *p_mux, sout_input_t *p_input, mp4_stream_t *p_stream, block_t *p_data)
 {
     sout_mux_sys_t *p_sys = p_mux->p_sys;
 
-    block_t *p_data = BlockDequeue(p_input, p_stream);
     if(!p_data)
         return VLC_SUCCESS;
 
@@ -866,7 +873,13 @@ static int Mux(sout_mux_t *p_mux)
         sout_input_t *p_input  = p_mux->pp_inputs[i_stream];
         mp4_stream_t *p_stream = (mp4_stream_t*)p_input->p_sys;
 
-        i_ret = MuxStream(p_mux, p_input, p_stream);
+        vlc_fifo_Lock( p_input->p_fifo );
+        block_t *p_data = vlc_fifo_DequeueUnlocked( p_input->p_fifo );
+        vlc_fifo_Unlock( p_input->p_fifo );
+        if (unlikely(p_data == NULL))
+            continue;
+
+        i_ret = MuxStream(p_mux, p_input, p_stream, p_data);
     } while( i_ret == VLC_SUCCESS );
 
     return i_ret;

commit a69e4c3298dbfa6b61e17fdfc99604ec0db0df8e

mux: mp4: fix conversion regression
regression by ad08ac23151d82c5eceec15db17b2e6d2b366758

modules/mux/mp4/mp4.c
@@ -604,16 +604,10 @@ static bool CreateCurrentEdit(mp4_stream_t *p_stream, vlc_tick_t i_mux_start_dts
     return mp4mux_track_AddEdit(p_stream->tinfo, &newedit);
 }
 
-static int BlockDequeue(sout_input_t *p_input, mp4_stream_t *p_stream, block_t **out)
+static int BlockConvert(mp4_stream_t *p_stream, block_t **out)
 {
     assert(out);
-
-    block_t *p_block = block_FifoGet(p_input->p_fifo);
-    if(unlikely(!p_block))
-    {
-        *out = NULL;
-        return VLC_SUCCESS;
-    }
+    block_t *p_block = *out;
 
     /* Create on the fly extradata as packetizer is not in the loop */
     if(p_stream->extrabuilder && !mp4mux_track_HasSamplePriv(p_stream->tinfo))
@@ -889,6 +883,13 @@ static int Mux(sout_mux_t *p_mux)
         vlc_fifo_Lock( p_input->p_fifo );
         block_t *p_data = vlc_fifo_DequeueUnlocked( p_input->p_fifo );
         vlc_fifo_Unlock( p_input->p_fifo );
+        if (unlikely(p_data == NULL))
+            continue;
+
+        int ret = BlockConvert(p_stream, &p_data);
+        if (ret != VLC_SUCCESS)
+            return ret;
+
         if (unlikely(p_data == NULL))
             continue;
 
@@ -1469,8 +1470,11 @@ static int MuxFrag(sout_mux_t *p_mux)
     sout_input_t *p_input  = p_mux->pp_inputs[i_stream];
     mp4_stream_t *p_stream = (mp4_stream_t*) p_input->p_sys;
 
-    block_t *p_currentblock;
-    int ret = BlockDequeue(p_input, p_stream, &p_currentblock);
+    block_t *p_currentblock = block_FifoGet(p_input->p_fifo);
+    if(unlikely(!p_currentblock))
+        return VLC_EGENERIC;
+
+    int ret = BlockConvert(p_stream, &p_currentblock);
     if (ret != VLC_SUCCESS)
         return ret;
 