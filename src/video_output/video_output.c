videolan/vlc

commit 1caf1afa38debdb7c7decb590557c2091120275e

video_output: check the mask matches for blender output changes
Otherwise we don't really have the same chroma (for example RGBX vs XBGR).

src/video_output/video_output.c
@@ -1179,7 +1179,7 @@ static int PrerenderPicture(vout_thread_sys_t *sys, picture_t *filtered,
         }
 
         if (sys->spu_blend &&
-            sys->spu_blend->fmt_out.video.i_chroma != fmt_spu.i_chroma) {
+            video_format_IsSameChroma(&sys->spu_blend->fmt_out.video, &fmt_spu)) {
             filter_DeleteBlend(sys->spu_blend);
             sys->spu_blend = NULL;
         }

commit a5c39d7647b35f0b81e36bf45ae0f1c162215bad

video_output: fix blend filter being opened in loop
Logs:

    main generic debug: looking for video blending module matching "any": 1 candidates
    main generic debug: using video blending module "blend"
    main blend debug: removing "video blending" module "blend"
    main generic debug: looking for video blending module matching "any": 1 candidates
    main generic debug: using video blending module "blend"
    main blend debug: removing "video blending" module "blend"

Test scenario:

    ./vlc --vout=dummy sample_with_subpicture

Regression from 1caf1afa38debdb7c7decb590557c2091120275e.

src/video_output/video_output.c
@@ -1198,7 +1198,7 @@ static int PrerenderPicture(vout_thread_sys_t *sys, picture_t *filtered,
         }
 
         if (sys->spu_blend &&
-            video_format_IsSameChroma(&sys->spu_blend->fmt_out.video, &fmt_spu)) {
+            !video_format_IsSameChroma(&sys->spu_blend->fmt_out.video, &fmt_spu)) {
             filter_DeleteBlend(sys->spu_blend);
             sys->spu_blend = NULL;
         }