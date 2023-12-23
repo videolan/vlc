videolan/vlc

commit 811b77c304046b4b39ee614f57bd75029e2e4385

csa: do not handle impossible NULL argument
The CSA structure cannot be NULL in this context.

modules/mux/mpeg/ts.c
@@ -828,10 +828,10 @@ static int ActiveKeyCallback( vlc_object_t *p_this, char const *psz_cmd,
         return VLC_EBADVAR;
 
     vlc_mutex_lock( &p_sys->csa_lock );
-    i_res = csa_UseKey( p_this, p_sys->csa, use_odd );
+    csa_UseKey( p_this, p_sys->csa, use_odd );
     vlc_mutex_unlock( &p_sys->csa_lock );
 
-    return i_res;
+    return VLC_SUCCESS;
 }
 
 /*****************************************************************************

modules/mux/mpeg/csa.c
@@ -26,6 +26,7 @@
 # include "config.h"
 #endif
 
+#include <assert.h>
 #include <vlc_common.h>
 
 #include "csa.h"
@@ -124,15 +125,14 @@ int csa_SetCW( vlc_object_t *p_caller, csa_t *c, char *psz_ck, bool set_odd )
 /*****************************************************************************
  * csa_UseKey:
  *****************************************************************************/
-int csa_UseKey( vlc_object_t *p_caller, csa_t *c, bool use_odd )
+void csa_UseKey( vlc_object_t *p_caller, csa_t *c, bool use_odd )
 {
-    if ( !c ) return VLC_ENOOBJ;
+    assert(c != NULL);
     c->use_odd = use_odd;
 #ifndef TS_NO_CSA_CK_MSG
         msg_Dbg( p_caller, "using the %s key for scrambling",
                  use_odd ? "odd" : "even" );
 #endif
-    return VLC_SUCCESS;
 }
 
 /*****************************************************************************

modules/mux/mpeg/csa.h
@@ -35,7 +35,7 @@ csa_t *csa_New( void );
 void   csa_Delete( csa_t * );
 
 int    csa_SetCW( vlc_object_t *p_caller, csa_t *c, char *psz_ck, bool odd );
-int    csa_UseKey( vlc_object_t *p_caller, csa_t *, bool use_odd );
+void   csa_UseKey( vlc_object_t *p_caller, csa_t *, bool use_odd );
 
 void   csa_Decrypt( csa_t *, uint8_t *pkt, int i_pkt_size );
 void   csa_Encrypt( csa_t *, uint8_t *pkt, int i_pkt_size );

commit 50c3fc636aef9b95c78644e726ebc724c213305c

mux: ts: fix null deref
is null due to callback..
regression by 811b77c304046b4b39ee614f57bd75029e2e4385

modules/mux/mpeg/ts.c
@@ -458,25 +458,30 @@ static void GetPMT( sout_mux_t *p_mux, sout_buffer_chain_t *c );
 static block_t *TSNew( sout_mux_t *p_mux, sout_input_sys_t *p_stream, bool b_pcr );
 static void TSSetPCR( block_t *p_ts, vlc_tick_t i_dts );
 
-static csa_t *csaSetup( vlc_object_t *p_this )
+static void csaSetup( vlc_object_t *p_this )
 {
     sout_mux_t *p_mux = (sout_mux_t*)p_this;
     sout_mux_sys_t *p_sys = p_mux->p_sys;
     char *csack = var_CreateGetNonEmptyStringCommand( p_mux, SOUT_CFG_PREFIX "csa-ck" );
     if( !csack )
-        return NULL;
+        return;
 
     csa_t *csa = csa_New();
 
     if( unlikely(csa == NULL) )
-        return NULL;
+    {
+        free(csack);
+        return;
+    }
 
     if( csa_SetCW( p_this, csa, csack, true ) )
     {
         free(csack);
         csa_Delete( csa );
-        return NULL;
+        return;
     }
+
+    p_sys->csa = csa;
 
     vlc_mutex_init( &p_sys->csa_lock );
     p_sys->b_crypt_audio = var_GetBool( p_mux, SOUT_CFG_PREFIX "crypt-audio" );
@@ -509,8 +514,6 @@ static csa_t *csaSetup( vlc_object_t *p_this )
     msg_Dbg( p_mux, "encrypting %d bytes of packet", p_sys->i_csa_pkt_size );
 
     free(csack);
-
-    return csa;
 }
 
 /*****************************************************************************
@@ -701,7 +704,7 @@ static int Open( vlc_object_t *p_this )
 
     p_mux->p_sys        = p_sys;
 
-    p_sys->csa = csaSetup(p_this);
+    csaSetup( p_this );
 
     p_mux->pf_control   = Control;
     p_mux->pf_addstream = AddStream;