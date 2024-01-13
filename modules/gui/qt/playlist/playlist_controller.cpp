videolan/vlc

commit 1b95adaae51970753c10d929e9dba695ab6cfbbe

qt: add explore function to playlist controller

modules/gui/qt/playlist/playlist_controller.cpp
@@ -23,8 +23,10 @@
 #include "playlist_controller.hpp"
 #include "playlist_controller_p.hpp"
 #include "vlc_player.h"
+#include "vlc_url.h"
 #include <algorithm>
 #include <QVariant>
+#include <QDesktopServices>
 
 namespace vlc {
   namespace playlist {
@@ -459,6 +461,32 @@ void PlaylistControllerModel::sort(PlaylistControllerModel::SortKey key, Playlis
     };
     QVector<vlc_playlist_sort_criterion> criteria { crit };
     sort( criteria );
+}
+
+void PlaylistControllerModel::explore(const PlaylistItem& pItem)
+{
+    vlc_playlist_item_t * const playlistItem = pItem.raw();
+    if( playlistItem )
+    {
+        input_item_t * const p_input = vlc_playlist_item_GetMedia(playlistItem);
+        char * const uri = input_item_GetURI(p_input);
+
+        if( uri && uri[0] != '\0')
+        {
+            char * const path = vlc_uri2path( uri );
+
+            if( !path )
+                return;
+
+            QUrl file = QUrl::fromLocalFile( QFileInfo(path).absolutePath() );
+            free( path );
+
+            if( !file.isLocalFile() )
+                return;
+
+            QDesktopServices::openUrl( file );
+        }
+    }
 }
 
 void PlaylistControllerModel::play()

modules/gui/qt/playlist/playlist_controller.hpp
@@ -108,6 +108,7 @@ public:
     Q_INVOKABLE void shuffle();
     void sort(const QVector<vlc_playlist_sort_criterion> &);
     Q_INVOKABLE void sort(SortKey key, SortOrder order);
+    Q_INVOKABLE void explore(const PlaylistItem& pItem);
 
 public:
     PlaylistControllerModel(QObject *parent = nullptr);

commit 346113b36b5ff643a886c1b59c14a00ada39ce02

qt: playlist_controller: fix uri leak
As it stands, uri (result of input_item_GetURI()) is leaked when:
- path is NULL
- containingDir is not a dir
- file is not a local file
- none of the early returns are taken but uri is not NULL or empty

To tackle these all at once, use vlc::wrap_cptr.

Fixes: 1b95adaae51970753c10d929e9dba695ab6cfbbe

modules/gui/qt/playlist/playlist_controller.cpp
@@ -514,11 +514,11 @@ void PlaylistController::explore(const PlaylistItem& pItem)
     if( playlistItem )
     {
         input_item_t * const p_input = vlc_playlist_item_GetMedia(playlistItem);
-        char * const uri = input_item_GetURI(p_input);
+        auto uri = vlc::wrap_cptr( input_item_GetURI(p_input) );
 
-        if( uri && uri[0] != '\0')
+        if( uri && uri.get()[0] != '\0')
         {
-            auto path = vlc::wrap_cptr( vlc_uri2path( uri ) );
+            auto path = vlc::wrap_cptr( vlc_uri2path( uri.get() ) );
 
             if( !path )
                 return;