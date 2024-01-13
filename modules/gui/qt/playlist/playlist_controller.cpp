videolan/vlc

commit 73f7b186190ecf5811594b16c7eac4520159ef2d

qt: playlist_controller: Ensure the path points to a directory
Not doing so could end up opening a file with the default application
associated with it, instead of opening a folder, leading to a potential
remote code execution.

Reported-by: Fabian Bräunlein <fabian@positive.security>
Reported-by: Lukas Euler <lukas@positive.security>
Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

modules/gui/qt/playlist/playlist_controller.cpp
@@ -533,7 +533,11 @@ void PlaylistControllerModel::explore(const PlaylistItem& pItem)
             if( !path )
                 return;
 
-            QUrl file = QUrl::fromLocalFile( QFileInfo(path).absolutePath() );
+            QString containingDir = QFileInfo( path ).absolutePath();
+            if( !QFileInfo( containingDir ).isDir() )
+                return;
+
+            QUrl file = QUrl::fromLocalFile( containingDir );
             free( path );
 
             if( !file.isLocalFile() )

commit f48fd35844a5ae280e5078f85327de9b0e7eb976

qt: playlist_controller: fix path leak
Also free the pointer when returning in case the path is not a
directory (this error condition was added later). To make sure this leak
does not resurface, use vlc::wrap_cptr.

Fixes: 73f7b186190ecf5811594b16c7eac4520159ef2d

modules/gui/qt/playlist/playlist_controller.cpp
@@ -518,18 +518,16 @@ void PlaylistController::explore(const PlaylistItem& pItem)
 
         if( uri && uri[0] != '\0')
         {
-            char * const path = vlc_uri2path( uri );
+            auto path = vlc::wrap_cptr( vlc_uri2path( uri ) );
 
             if( !path )
                 return;
 
-            QString containingDir = QFileInfo( path ).absolutePath();
+            QString containingDir = QFileInfo( path.get() ).absolutePath();
             if( !QFileInfo( containingDir ).isDir() )
                 return;
 
             QUrl file = QUrl::fromLocalFile( containingDir );
-            free( path );
-
             if( !file.isLocalFile() )
                 return;
 