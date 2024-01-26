dee6d534aa1ec0807cb249d028328d2098dc1f0b
Steve Lhomme <robux4@ycbcr.xyz>
2024-01-26 14:56:25 +0100

    demux/mkv: add override on overriden virtual methods
    (cherry picked from commit 7e77a2335f5cd23c0c91c9b39ef451663e280971) (edited)
    edited:
    - 3.0 doesn't use regular integer types

--- modules/demux/mkv/stream_io_callback.hpp
+++ modules/demux/mkv/stream_io_callback.hpp
@@ -44,10 +44,10 @@ class vlc_stream_io_callback: public IOCallback
 
     bool IsEOF() const { return mb_eof; }
 
-    virtual uint32   read            ( void *p_buffer, size_t i_size);
-    virtual void     setFilePointer  ( int64_t i_offset, seek_mode mode = seek_beginning );
-    virtual size_t   write           ( const void *p_buffer, size_t i_size);
-    virtual uint64   getFilePointer  ( void );
-    virtual void     close           ( void ) { return; }
+    virtual uint32   read            ( void *p_buffer, size_t i_size) override;
+    virtual void     setFilePointer  ( int64_t i_offset, seek_mode mode = seek_beginning ) override;
+    virtual size_t   write           ( const void *p_buffer, size_t i_size) override;
+    virtual uint64   getFilePointer  ( void ) override;
+    virtual void     close           ( void ) override { return; }
 };
 
