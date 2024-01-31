7befacddeb733ae3b53a94587065f04e0f7295f4
Steve Lhomme <robux4@ycbcr.xyz>
2024-01-31 08:57:45 +0100

    globalhotkeys: start the win32 hotkeys as autorun
    Just like the XCB ones. The autorun is used when using the default interface.

--- modules/control/globalhotkeys/win32.c
+++ modules/control/globalhotkeys/win32.c
@@ -42,6 +42,11 @@ static void Close( vlc_object_t *p_this );
 static void *Thread( void *p_data );
 LRESULT CALLBACK WMHOTKEYPROC( HWND, UINT, WPARAM, LPARAM );
 
+static void AutoRun(libvlc_int_t *libvlc)
+{
+    intf_Create(libvlc, MODULE_STRING);
+}
+
 /*****************************************************************************
  * Module descriptor
  *****************************************************************************/
@@ -52,6 +57,10 @@ vlc_module_begin()
     set_capability( "interface", 0 )
     set_callbacks( Open, Close )
     add_shortcut( "globalhotkeys" )
+
+    add_submodule()
+    set_capability("autorun", 10)
+    set_callback(AutoRun)
 vlc_module_end()
 
 struct intf_sys_t
