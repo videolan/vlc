03c00e06dd70d25985a1f3accfcbe1521281afba
Steve Lhomme <robux4@ycbcr.xyz>
2024-02-01 16:14:36 +0100

    vout_subpictures: assert when the original dimensions are not set
    We can't position the region based on its original dimensions if they are not set.

--- src/video_output/vout_subpictures.c
+++ src/video_output/vout_subpictures.c
@@ -593,17 +593,25 @@ static void SpuRegionPlace(int *x, int *y,
     } else {
         if (i_align & SUBPICTURE_ALIGN_TOP)
             *y = region->i_y;
-        else if (i_align & SUBPICTURE_ALIGN_BOTTOM)
-            *y = subpic->i_original_picture_height - region->fmt.i_visible_height - region->i_y;
         else
-            *y = subpic->i_original_picture_height / 2 - region->fmt.i_visible_height / 2;
+        {
+            assert(subpic->i_original_picture_height != 0);
+            if (i_align & SUBPICTURE_ALIGN_BOTTOM)
+                *y = subpic->i_original_picture_height - region->fmt.i_visible_height - region->i_y;
+            else
+                *y = subpic->i_original_picture_height / 2 - region->fmt.i_visible_height / 2;
+        }
 
         if (i_align & SUBPICTURE_ALIGN_LEFT)
             *x = region->i_x;
-        else if (i_align & SUBPICTURE_ALIGN_RIGHT)
-            *x = subpic->i_original_picture_width - region->fmt.i_visible_width - region->i_x;
         else
-            *x = subpic->i_original_picture_width / 2 - region->fmt.i_visible_width / 2;
+        {
+            assert(subpic->i_original_picture_width != 0);
+            if (i_align & SUBPICTURE_ALIGN_RIGHT)
+                *x = subpic->i_original_picture_width - region->fmt.i_visible_width - region->i_x;
+            else
+                *x = subpic->i_original_picture_width / 2 - region->fmt.i_visible_width / 2;
+        }
     }
 }
 
