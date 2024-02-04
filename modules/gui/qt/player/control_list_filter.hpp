b1e82c1682483ad27e636ac4d9f4a2096274fe60
Alexandre Janniaux <ajanni@videolabs.io>
2024-02-04 12:37:59 +0100

    qt: control_list_filter: hide QSortFilterProxyModel::setSourceModel
    Fix the following warnings on Clang 16.0.6:
    
        player/../../../../../modules/gui/qt/player/control_list_filter.hpp:43:10: warning: 'ControlListFilter::setSourceModel' hides overloaded virtual function [-Woverloaded-virtual]
            void setSourceModel(ControlListModel* sourceModel);
                 ^
        /usr/include/qt/QtCore/qsortfilterproxymodel.h:82:10: note: hidden overloaded virtual function 'QSortFilterProxyModel::setSourceModel' declared here: type mismatch at 1st parameter ('QAbstractItemModel *' vs 'ControlListModel *')
            void setSourceModel(QAbstractItemModel *sourceModel) override;
             ^

--- modules/gui/qt/player/control_list_filter.hpp
+++ modules/gui/qt/player/control_list_filter.hpp
@@ -31,6 +31,8 @@ class ControlListModel;
 
 class ControlListFilter : public QSortFilterProxyModel
 {
+    using QSortFilterProxyModel::setSourceModel;
+
     Q_OBJECT
 
     Q_PROPERTY(ControlListModel* sourceModel READ sourceModel WRITE setSourceModel NOTIFY sourceModelChanged1 FINAL)
@@ -41,6 +43,7 @@ public:
     explicit ControlListFilter(QObject * parent = nullptr);
 
     void setSourceModel(ControlListModel* sourceModel);
+
     ControlListModel* sourceModel() const;
 
 protected: // QSortFilterProxyModel reimplementation
