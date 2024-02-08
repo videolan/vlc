videolan/vlc

commit 3f613a85f58edc1e37d56a1fe7769c9e14981258

vlc_threads: move poll() definitions in a separate header
It's using a compat version on Windows/Android/OS2 which is not available
to external modules.

This new header can't be used directly from external modules on Windows.
This makes vlc_threads.h usable from external modules on Windows.

include/vlc_poll.h
@@ -1,0 +1,99 @@
+/*****************************************************************************
+ * vlc_poll.h : poll implementation for the VideoLAN client
+ *****************************************************************************
+ * Copyright (C) 1999, 2002 VLC authors and VideoLAN
+ * Copyright © 2007-2016 Rémi Denis-Courmont
+ *
+ * Authors: Jean-Marc Dressler <polux@via.ecp.fr>
+ *          Samuel Hocevar <sam@via.ecp.fr>
+ *          Gildas Bazin <gbazin@netcourrier.com>
+ *          Christophe Massiot <massiot@via.ecp.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#ifndef VLC_POLL_H_
+#define VLC_POLL_H_
+
+#include <vlc_threads.h>
+
+/**
+ * \ingroup os
+ * \defgroup thread Poll implementations
+ * @{
+ * \file
+ * Poll implementations
+ */
+
+#if defined (_WIN32)
+static inline int vlc_poll(struct pollfd *fds, unsigned nfds, int timeout)
+{
+    int val;
+
+    vlc_testcancel();
+    val = poll(fds, nfds, timeout);
+    if (val < 0)
+        vlc_testcancel();
+    return val;
+}
+# define poll(u,n,t) vlc_poll(u, n, t)
+
+#elif defined (__OS2__)
+static inline int vlc_poll (struct pollfd *fds, unsigned nfds, int timeout)
+{
+    static int (*vlc_poll_os2)(struct pollfd *, unsigned, int) = NULL;
+
+    if (!vlc_poll_os2)
+    {
+        HMODULE hmod;
+        CHAR szFailed[CCHMAXPATH];
+
+        if (DosLoadModule(szFailed, sizeof(szFailed), "vlccore", &hmod))
+            return -1;
+
+        if (DosQueryProcAddr(hmod, 0, "_vlc_poll_os2", (PFN *)&vlc_poll_os2))
+            return -1;
+    }
+
+    return (*vlc_poll_os2)(fds, nfds, timeout);
+}
+# define poll(u,n,t) vlc_poll(u, n, t)
+
+#elif defined (__ANDROID__)      /* pthreads subset without pthread_cancel() */
+static inline int vlc_poll (struct pollfd *fds, unsigned nfds, int timeout)
+{
+    int val;
+
+    do
+    {
+        int ugly_timeout = ((unsigned)timeout >= 50) ? 50 : timeout;
+        if (timeout >= 0)
+            timeout -= ugly_timeout;
+
+        vlc_testcancel ();
+        val = poll (fds, nfds, ugly_timeout);
+    }
+    while (val == 0 && timeout != 0);
+
+    return val;
+}
+
+# define poll(u,n,t) vlc_poll(u, n, t)
+
+#else /* POSIX threads */
+
+#endif
+
+#endif /* !VLC_POLL_H_ */

include/vlc_threads.h
@@ -64,18 +64,6 @@ typedef struct vlc_thread *vlc_thread_t;
 typedef struct vlc_threadvar *vlc_threadvar_t;
 typedef struct vlc_timer *vlc_timer_t;
 
-static inline int vlc_poll(struct pollfd *fds, unsigned nfds, int timeout)
-{
-    int val;
-
-    vlc_testcancel();
-    val = poll(fds, nfds, timeout);
-    if (val < 0)
-        vlc_testcancel();
-    return val;
-}
-# define poll(u,n,t) vlc_poll(u, n, t)
-
 #elif defined (__OS2__)
 # include <errno.h>
 
@@ -87,26 +75,6 @@ typedef struct vlc_timer *vlc_timer_t;
 
 # define pthread_sigmask  sigprocmask
 
-static inline int vlc_poll (struct pollfd *fds, unsigned nfds, int timeout)
-{
-    static int (*vlc_poll_os2)(struct pollfd *, unsigned, int) = NULL;
-
-    if (!vlc_poll_os2)
-    {
-        HMODULE hmod;
-        CHAR szFailed[CCHMAXPATH];
-
-        if (DosLoadModule(szFailed, sizeof(szFailed), "vlccore", &hmod))
-            return -1;
-
-        if (DosQueryProcAddr(hmod, 0, "_vlc_poll_os2", (PFN *)&vlc_poll_os2))
-            return -1;
-    }
-
-    return (*vlc_poll_os2)(fds, nfds, timeout);
-}
-# define poll(u,n,t) vlc_poll(u, n, t)
-
 #elif defined (__ANDROID__)      /* pthreads subset without pthread_cancel() */
 # include <unistd.h>
 # include <pthread.h>
@@ -117,26 +85,6 @@ typedef struct vlc_thread *vlc_thread_t;
 #define VLC_THREAD_CANCELED ((void*) UINTPTR_MAX)
 typedef pthread_key_t   vlc_threadvar_t;
 typedef struct vlc_timer *vlc_timer_t;
-
-static inline int vlc_poll (struct pollfd *fds, unsigned nfds, int timeout)
-{
-    int val;
-
-    do
-    {
-        int ugly_timeout = ((unsigned)timeout >= 50) ? 50 : timeout;
-        if (timeout >= 0)
-            timeout -= ugly_timeout;
-
-        vlc_testcancel ();
-        val = poll (fds, nfds, ugly_timeout);
-    }
-    while (val == 0 && timeout != 0);
-
-    return val;
-}
-
-# define poll(u,n,t) vlc_poll(u, n, t)
 
 #else /* POSIX threads */
 # include <unistd.h> /* _POSIX_SPIN_LOCKS */

modules/access/dtv/en50221.c
@@ -30,6 +30,7 @@
 #include <vlc_arrays.h>
 #include <vlc_tick.h>
 #include <vlc_threads.h>
+#include <vlc_poll.h>
 #include <vlc_charset.h>
 #include <vlc_fs.h>
 

modules/access/dv.c
@@ -31,6 +31,7 @@
 
 #include <vlc_common.h>
 #include <vlc_threads.h>
+#include <vlc_poll.h>
 #include <vlc_plugin.h>
 #include <vlc_access.h>
 

modules/access/http/h2conn.c
@@ -34,6 +34,7 @@
 #endif
 #include <vlc_common.h>
 #include <vlc_threads.h>
+#include <vlc_poll.h>
 #include <vlc_block.h>
 #include <vlc_interrupt.h>
 #include <vlc_tls.h>

modules/access/http/h2output.c
@@ -33,6 +33,7 @@
 #endif
 #include <vlc_common.h>
 #include <vlc_threads.h>
+#include <vlc_poll.h>
 #include <vlc_tls.h>
 #include "h2frame.h"
 #include "h2output.h"

modules/access/linsys/linsys_hdsdi.c
@@ -42,6 +42,7 @@
 #include <vlc_input.h>
 #include <vlc_access.h>
 #include <vlc_demux.h>
+#include <vlc_poll.h>
 
 #include <vlc_fs.h>
 

modules/access/linsys/linsys_sdi.c
@@ -41,6 +41,7 @@
 #include <vlc_input.h>
 #include <vlc_access.h>
 #include <vlc_demux.h>
+#include <vlc_poll.h>
 
 #include <vlc_fs.h>
 

commit bd3fc34c8fbae9f47803b086ac350b78b5436b19

include: vlc_poll: fix documentation nesting
Because vlc_poll.h was parsed before vlc_thread and the group name was
also thread, every thread-related item was moved to the "Poll
implementations" list on the doxygen output.

Regression from 3f613a85f58edc1e37d56a1fe7769c9e14981258.

include/vlc_poll.h
@@ -31,7 +31,7 @@
 
 /**
  * \ingroup os
- * \defgroup thread Poll implementations
+ * \defgroup thread_poll Poll implementations
  * @{
  * \file
  * Poll implementations
@@ -95,5 +95,7 @@ static inline int vlc_poll (struct pollfd *fds, unsigned nfds, int timeout)
 #else /* POSIX threads */
 
 #endif
+
+/** @} */
 
 #endif /* !VLC_POLL_H_ */