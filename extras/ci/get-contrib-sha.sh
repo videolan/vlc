7cc326179032e0834bc1aff75660fdefb45697e8
Steve Lhomme <robux4@ycbcr.xyz>
2024-02-15 10:55:58 +0100

    CI: restrict the build scripts used to build contribs
    For example, updating a Windows build script shouldn't force the macOS jobs to rebuild contribs.
    
    By default the script behaves the same as before. It requires giving a job name or target OS name to
    select the changes only for that OS.
    
    (cherry picked from commit 4ac6168d4740b6416fa6abbff30b50b14d370ef9)
    Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

--- extras/ci/get-contrib-sha.sh
+++ extras/ci/get-contrib-sha.sh
@@ -7,7 +7,7 @@
 set -e
 
 # Array of paths that should trigger contrib rebuilds
-VLC_CONTRIB_REBUILD_PATHS=("extras/tools" "contrib" "extras/ci" "extras/package")
+VLC_CONTRIB_REBUILD_PATHS=("extras/tools" "contrib" "extras/ci")
 # Revision from which to start look for changes (backwards in time)
 VLC_START_REVISION="HEAD"
 
@@ -28,9 +28,37 @@ VLC_SRC_ROOT_DIR=$(git rev-parse --show-toplevel)
 [ -n "${VLC_SRC_ROOT_DIR}" ] || abort_err "This script must be run in the VLC Git repo and git must be available"
 [ -f "${VLC_SRC_ROOT_DIR}/src/libvlc.h" ] || abort_err "This script must be run in the VLC Git repository"
 
+VLC_BUILD_TARGET="extras/package"
+case $1 in
+    win32*|win64*|uwp*)
+        VLC_BUILD_TARGET="extras/package/win32"
+        ;;
+    debian*)
+        VLC_BUILD_TARGET="extras/package/linux"
+        ;;
+    ios*|tvos*)
+        VLC_BUILD_TARGET="extras/package/apple"
+        ;;
+    macos*)
+        VLC_BUILD_TARGET="extras/package/macosx"
+        ;;
+    raspbian*)
+        VLC_BUILD_TARGET="extras/package/raspberry"
+        ;;
+    snap*)
+        VLC_BUILD_TARGET="extras/package/snap"
+        ;;
+    wasm*)
+        VLC_BUILD_TARGET="extras/package/wasm-emscripten"
+        ;;
+    android*)
+        VLC_BUILD_TARGET=""
+        ;;
+esac
+
 VLC_LAST_CONTRIB_SHA=$(
     cd "$VLC_SRC_ROOT_DIR" &&
-    git rev-list -1 "${VLC_START_REVISION}" -- "${VLC_CONTRIB_REBUILD_PATHS[@]}"
+    git rev-list -1 "${VLC_START_REVISION}" -- "${VLC_CONTRIB_REBUILD_PATHS[@]}" "${VLC_BUILD_TARGET}"
 )
 
 [ -n "${VLC_LAST_CONTRIB_SHA}" ] || abort_err "Failed to determine last contrib SHA using Git!"
