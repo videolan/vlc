ef640c8b63b4ffb224faf2161dd23e40bdc0fd3b
Steve Lhomme <robux4@ycbcr.xyz>
2024-02-19 13:02:09 +0100

    d3d11_scaler: ensure the AMF input/output textures matches the scaler sizes

--- modules/video_output/win32/d3d11_scaler.cpp
+++ modules/video_output/win32/d3d11_scaler.cpp
@@ -603,12 +603,22 @@ int D3D11_UpscalerScale(vlc_object_t *vd, d3d11_scaler *scaleProc, picture_sys_d
 
         auto packedStaging = scaleProc->amfInput->GetPlane(amf::AMF_PLANE_PACKED);
         ID3D11Texture2D *amfStaging = reinterpret_cast<ID3D11Texture2D *>(packedStaging->GetNative());
+
+#ifndef NDEBUG
+        D3D11_TEXTURE2D_DESC stagingDesc, inputDesc;
+        amfStaging->GetDesc(&stagingDesc);
+        p_sys->texture[KNOWN_DXGI_INDEX]->GetDesc(&inputDesc);
+        assert(stagingDesc.Width == inputDesc.Width);
+        assert(stagingDesc.Height == inputDesc.Height);
+        assert(stagingDesc.Format == inputDesc.Format);
+#endif
+
         // copy source into staging as it may not be shared
         d3d11_device_lock( scaleProc->d3d_dev );
         scaleProc->d3d_dev->d3dcontext->CopySubresourceRegion(amfStaging,
                                                 0,
                                                 0, 0, 0,
-                                                p_sys->resource[KNOWN_DXGI_INDEX],
+                                                p_sys->texture[KNOWN_DXGI_INDEX],
                                                 p_sys->slice_index,
                                                 NULL);
         d3d11_device_unlock( scaleProc->d3d_dev );
@@ -639,6 +649,14 @@ int D3D11_UpscalerScale(vlc_object_t *vd, d3d11_scaler *scaleProc, picture_sys_d
         auto packed = amfOutputSurface->GetPlane(amf::AMF_PLANE_PACKED);
 
         ID3D11Texture2D *out = reinterpret_cast<ID3D11Texture2D *>(packed->GetNative());
+
+#ifndef NDEBUG
+        D3D11_TEXTURE2D_DESC outDesc;
+        out->GetDesc(&outDesc);
+        assert(outDesc.Width == scaleProc->Width);
+        assert(outDesc.Height == scaleProc->Height);
+        assert(outDesc.Format == inputDesc.Format);
+#endif
 
         ReleaseSRVs(scaleProc);
         ID3D11Texture2D *_upscaled[DXGI_MAX_SHADER_VIEW];
