videolan/vlc

commit e2e4df6826c712595b55c1d84f2a4c52766afad9

freetype: position the output region once it's settled
The region position is not used when rendering it.

modules/text_renderer/freetype/freetype.c
@@ -1115,15 +1115,8 @@ static int Render( filter_t *p_filter, subpicture_region_t *p_region_out,
         *        but update offsets to keep position and have same rendering */
 //        if( (bboxcolor & 0xFF) == 0 )
     {
-        p_region_out->i_x = (paddedbbox.xMin - regionbbox.xMin) + p_region_in->i_x;
-        p_region_out->i_y = (regionbbox.yMax - paddedbbox.yMax) + p_region_in->i_y;
         regionbbox = paddedbbox;
     }
-//        else /* case where the bounding box is larger and visible */
-//        {
-//            p_region_out->i_x = p_region_in->i_x;
-//            p_region_out->i_y = p_region_in->i_y;
-//        }
 
     rv = VLC_EGENERIC;
     for( const vlc_fourcc_t *p_chroma = p_chroma_list; *p_chroma != 0; p_chroma++ )
@@ -1172,6 +1165,20 @@ static int Render( filter_t *p_filter, subpicture_region_t *p_region_out,
         if( rv == VLC_SUCCESS )
         {
             subpicture_region_TextMarkRendered( p_region_out );
+
+            /* Avoid useless pixels:
+                *        reshrink/trim Region Box to padded text one,
+                *        but update offsets to keep position and have same rendering */
+//          if( (bboxcolor & 0xFF) == 0 )
+            {
+                p_region_out->i_x = (paddedbbox.xMin - regionbbox.xMin) + p_region_in->i_x;
+                p_region_out->i_y = (regionbbox.yMax - paddedbbox.yMax) + p_region_in->i_y;
+            }
+//          else /* case where the bounding box is larger and visible */
+//          {
+//              p_region_out->i_x = p_region_in->i_x;
+//              p_region_out->i_y = p_region_in->i_y;
+//          }
             break;
         }
     }

commit a6b5ec1b990fb64426359d2710a2caff4069711a

text_renderer: freetype: fix region inner rendered area global offset
refs #28525
regression by e2e4df6826c712595b55c1d84f2a4c52766afad9.

modules/text_renderer/freetype/freetype.c
@@ -1063,17 +1063,18 @@ static subpicture_region_t *Render( filter_t *p_filter,
     /* Avoid useless pixels:
         *        reshrink/trim Region Box to padded text one,
         *        but update offsets to keep position and have same rendering */
-//        if( (bboxcolor & 0xFF) == 0 )
-    {
-        regionbbox = paddedbbox;
-    }
+    FT_BBox renderbbox;
+    // if( (bboxcolor & 0xFF) == 0 )
+        renderbbox = paddedbbox;
+    // else
+    //     renderbbox = regionbbox;
 
     video_format_t fmt;
     video_format_Init( &fmt, 0 );
     fmt.i_width          =
-    fmt.i_visible_width  = regionbbox.xMax - regionbbox.xMin;
+    fmt.i_visible_width  = renderbbox.xMax - renderbbox.xMin;
     fmt.i_height         =
-    fmt.i_visible_height = regionbbox.yMax - regionbbox.yMin;
+    fmt.i_visible_height = renderbbox.yMax - renderbbox.yMin;
     fmt.i_sar_num = fmt.i_sar_den = 1;
 
     for( const vlc_fourcc_t *p_chroma = p_chroma_list; *p_chroma != 0; p_chroma++ )
@@ -1094,7 +1095,7 @@ static subpicture_region_t *Render( filter_t *p_filter,
 
         if( *p_chroma == VLC_CODEC_YUVP )
             RenderYUVP( p_region_in, region, text_block.p_laid,
-                                &regionbbox, &bbox );
+                                &renderbbox, &bbox );
         else
         {
             const ft_drawing_functions *func;
@@ -1132,14 +1133,11 @@ static subpicture_region_t *Render( filter_t *p_filter,
             }
 
             RenderAXYZ( p_filter, p_region_in, region, text_block.p_laid,
-                                 &regionbbox, &paddedbbox, &bbox,
+                                 &renderbbox, &paddedbbox, &bbox,
                                  *p_chroma,
                                  func );
         }
 
-        /* Avoid useless pixels:
-         *        reshrink/trim Region Box to padded text one,
-         *        but update offsets to keep position and have same rendering */
 //      if( (bboxcolor & 0xFF) == 0 )
         {
             region->i_x = (paddedbbox.xMin - regionbbox.xMin) + p_region_in->i_x;