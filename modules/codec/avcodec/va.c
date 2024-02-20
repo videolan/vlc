videolan/vlc

commit 8d94f8ecfc51cd655c9a0aa67678a2f4484381df

avcodec: va: add a function to quickly check if the format might be decoded
Similar to vlc_va_GetChroma() but without forcing an output format.

modules/codec/avcodec/va.c
@@ -95,6 +95,63 @@ vlc_fourcc_t vlc_va_GetChroma(enum PixelFormat hwfmt, enum PixelFormat swfmt)
     }
 }
 
+bool vlc_va_MightDecode(enum PixelFormat hwfmt, enum PixelFormat swfmt)
+{
+    switch (hwfmt)
+    {
+        case AV_PIX_FMT_VAAPI_VLD:
+            switch (swfmt)
+            {
+                case AV_PIX_FMT_YUVJ420P:
+                case AV_PIX_FMT_YUV420P:
+                case AV_PIX_FMT_YUV420P10LE:
+                    return true;
+                default:
+                    return false;
+            }
+        case AV_PIX_FMT_DXVA2_VLD:
+            switch (swfmt)
+            {
+                case AV_PIX_FMT_YUV420P10LE:
+                case AV_PIX_FMT_YUVJ420P:
+                case AV_PIX_FMT_YUV420P:
+                    return true;
+                default:
+                    return false;
+            }
+            break;
+
+        case AV_PIX_FMT_D3D11VA_VLD:
+            switch (swfmt)
+            {
+                case AV_PIX_FMT_YUV420P10LE:
+                case AV_PIX_FMT_YUVJ420P:
+                case AV_PIX_FMT_YUV420P:
+                    return true;
+                default:
+                    return false;
+            }
+        break;
+
+        case AV_PIX_FMT_VDPAU:
+            switch (swfmt)
+            {
+                case AV_PIX_FMT_YUVJ444P:
+                case AV_PIX_FMT_YUV444P:
+                case AV_PIX_FMT_YUVJ422P:
+                case AV_PIX_FMT_YUV422P:
+                case AV_PIX_FMT_YUVJ420P:
+                case AV_PIX_FMT_YUV420P:
+                    return true;
+                default:
+                    return false;
+            }
+            break;
+        default:
+            return false;
+    }
+}
+
 static int vlc_va_Start(void *func, bool forced, va_list ap)
 {
     vlc_va_t *va = va_arg(ap, vlc_va_t *);

modules/codec/avcodec/va.h
@@ -65,6 +65,15 @@ typedef int (*vlc_va_open)(vlc_va_t *, AVCodecContext *, const AVPixFmtDescripto
 vlc_fourcc_t vlc_va_GetChroma(enum PixelFormat hwfmt, enum PixelFormat swfmt);
 
 /**
+ * Determines whether the hardware acceleration PixelFormat can be used to
+ * decode pixels similar to the software PixelFormat.
+ * @param hwfmt the hardware acceleration pixel format
+ * @param swfmt the software pixel format
+ * @return true if the hardware acceleration should be supported
+ */
+bool vlc_va_MightDecode(enum PixelFormat hwfmt, enum PixelFormat swfmt);
+
+/**
  * Creates an accelerated video decoding back-end for libavcodec.
  * @param obj parent VLC object
  * @param fmt VLC format of the content to decode

commit eb282ddbb6dc4428e6d377dd686caf52e601bbce

avcodec: update VDPAU candidate chromas
This is a partial/short-term work-around for the functional regression
caused by 8d94f8ecfc5. That commit should be reverted to fix this properly
and more permanently.

modules/codec/avcodec/va.c
@@ -85,10 +85,14 @@ bool vlc_va_MightDecode(enum AVPixelFormat hwfmt, enum AVPixelFormat swfmt)
             {
                 case AV_PIX_FMT_YUVJ444P:
                 case AV_PIX_FMT_YUV444P:
+                case AV_PIX_FMT_YUV444P10:
+                case AV_PIX_FMT_YUV444P12:
                 case AV_PIX_FMT_YUVJ422P:
                 case AV_PIX_FMT_YUV422P:
                 case AV_PIX_FMT_YUVJ420P:
                 case AV_PIX_FMT_YUV420P:
+                case AV_PIX_FMT_YUV420P10:
+                case AV_PIX_FMT_YUV420P12:
                     return true;
                 default:
                     return false;