d69dcb393f6c31254dcd124ae8b9660020f7686e
Alexandre Janniaux <ajanni@videolabs.io>
2024-02-21 17:11:01 +0100

    mp4: essetup: fix sign vs unsigned comparison
    Since this index is only used as an array subscript, we can use a size
    type and compare against ARRAY_SIZE without warnings.

--- modules/demux/mp4/essetup.c
+++ modules/demux/mp4/essetup.c
@@ -134,7 +134,7 @@ static void SetupESDS( demux_t *p_demux, const mp4_track_t *p_track,
     if( p_fmt->i_codec == VLC_CODEC_SPU &&
             p_fmt->i_extra >= sizeof(p_fmt->subs.spu.palette) )
     {
-        for( int i = 0; i < ARRAY_SIZE(p_fmt->subs.spu.palette); i++ )
+        for (size_t i = 0; i < ARRAY_SIZE(p_fmt->subs.spu.palette); i++)
         {
             p_fmt->subs.spu.palette[i] = GetDWBE((char*)p_fmt->p_extra + i * 4);
         }
