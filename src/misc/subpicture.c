33dd66bd7fea507bdfcfb0c617b3e61b3c323b6d
Steve Lhomme <robux4@ycbcr.xyz>
2024-03-11 12:58:22 +0100

    subpicture: initialize the region picture with the region format
    The region format has its own copy of the palette, whereas the source may have a temporary pointer

--- src/misc/subpicture.c
+++ src/misc/subpicture.c
@@ -241,7 +241,7 @@ subpicture_region_t *subpicture_region_New( const video_format_t *p_fmt )
         return NULL;
 
     video_format_Copy( &p_region->fmt, p_fmt );
-    p_region->p_picture = picture_NewFromFormat( p_fmt );
+    p_region->p_picture = picture_NewFromFormat( &p_region->fmt );
     if( !p_region->p_picture )
     {
         video_format_Clean( &p_region->fmt );
