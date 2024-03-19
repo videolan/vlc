8b57bd3df6a58fa5b347b87225583202e2d2dfff
Alexandre Janniaux <ajanni@videolabs.io>
2024-03-19 19:18:38 +0100

    clock: check clock_main->last instead of offset
    Offset could very well be 0, which is currently equal to
    VLC_TICK_INVALID. Checking that clock_main->last has been set instead
    provides a safer alternative.
    
    Co-authored-by: Thomas Guillem <thomas@gllm.fr>

--- src/clock/clock.c
+++ src/clock/clock.c
@@ -182,7 +182,7 @@ static inline void TraceRender(struct vlc_tracer *tracer, const char *type,
 static vlc_tick_t main_stream_to_system(vlc_clock_main_t *main_clock,
                                         vlc_tick_t ts)
 {
-    if (main_clock->offset == VLC_TICK_INVALID)
+    if (main_clock->last.system == VLC_TICK_INVALID)
         return VLC_TICK_INVALID;
     return ((vlc_tick_t) (ts * main_clock->coeff / main_clock->rate))
             + main_clock->offset;
@@ -227,7 +227,7 @@ static void vlc_clock_master_update_coeff(
     vlc_clock_main_t *main_clock = clock->owner;
     vlc_mutex_assert(&main_clock->lock);
 
-    if (main_clock->offset != VLC_TICK_INVALID
+    if (main_clock->last.system != VLC_TICK_INVALID
      && ts != main_clock->last.stream)
     {
         if (rate == main_clock->rate)
@@ -634,7 +634,7 @@ void vlc_clock_main_ChangePause(vlc_clock_main_t *main_clock, vlc_tick_t now,
      * messing up the timings if the stream was paused then seeked
      */
     const vlc_tick_t delay = now - main_clock->pause_date;
-    if (main_clock->offset != VLC_TICK_INVALID)
+    if (main_clock->last.system != VLC_TICK_INVALID)
     {
         main_clock->last.system += delay;
         main_clock->offset += delay;
