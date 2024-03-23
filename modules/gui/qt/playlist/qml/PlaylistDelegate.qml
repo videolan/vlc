videolan/vlc

commit d34c08723d0edc3448704214280d7b8e97e7849b

qml: fix qt6 parameters injection warning from MouseArea

modules/gui/qt/playlist/qml/PlaylistDelegate.qml
@@ -245,7 +245,7 @@ T.ItemDelegate {
 
         acceptedButtons: Qt.LeftButton | Qt.RightButton
 
-        onClicked: {
+        onClicked: (mouse) => {
             /* to receive keys events */
             if (!(delegate.selected && mouse.button === Qt.RightButton)) {
                 view.selectionModel.updateSelection(mouse.modifiers, view.currentIndex, index)
@@ -256,12 +256,12 @@ T.ItemDelegate {
                 contextMenu.popup(index, this.mapToGlobal(mouse.x, mouse.y))
         }
 
-        onDoubleClicked: {
+        onDoubleClicked: (mouse) => {
             if (mouse.button !== Qt.RightButton)
                 MainPlaylistController.goTo(index, true)
         }
 
-        onPressed: {
+        onPressed: (mouse) => {
             delegate.forceActiveFocus(Qt.MouseFocusReason)
         }
 

modules/gui/qt/dialogs/toolbar/qml/EditorDNDDelegate.qml
@@ -78,7 +78,7 @@ Control {
             }
         }
 
-        onPressed: {
+        onPressed: (mouse) => {
             const pos = mapToItem(control.contentItem.target.parent, mouseX, mouseY)
             control.contentItem.target.y = pos.y + VLCStyle.dragDelta
             control.contentItem.target.x = pos.x + VLCStyle.dragDelta

modules/gui/qt/dialogs/toolbar/qml/ToolbarEditorButtonList.qml
@@ -159,7 +159,7 @@ GridView {
             }
         }
 
-        onPressed: {
+        onPressed: (mouse) => {
             const pos = mapToItem(buttonDragItem.parent, mouseX, mouseY)
             buttonDragItem.y = pos.y + VLCStyle.dragDelta
             buttonDragItem.x = pos.x + VLCStyle.dragDelta

modules/gui/qt/medialibrary/qml/MusicArtistDelegate.qml
@@ -118,19 +118,19 @@ T.ItemDelegate {
             }
         }
 
-        onClicked: function(mouse) {
+        onClicked: (mouse) => {
             if (!(root.selected && mouse.button === Qt.RightButton)) {
                 view.selectionModel.updateSelection(mouse.modifiers, view.currentIndex, index)
                 view.currentIndex = index
             }
         }
 
-        onDoubleClicked: function(mouse) {
+        onDoubleClicked: (mouse) => {
             if (mouse.button !== Qt.RightButton)
                 MediaLib.addAndPlay(model.id);
         }
 
-        onPressed: {
+        onPressed: (mouse) => {
             root.forceActiveFocus(Qt.MouseFocusReason)
         }
     }

modules/gui/qt/player/qml/PlaybackSpeed.qml
@@ -241,7 +241,7 @@ ColumnLayout {
 
             acceptedButtons: Qt.LeftButton
 
-            onPressed: {
+            onPressed: (mouse) => {
                 mouse.accepted = false
 
                 root._shiftPressed = (mouse.modifiers === Qt.ShiftModifier)

modules/gui/qt/player/qml/Player.qml
@@ -360,7 +360,7 @@ FocusScope {
 
         visible: !rootPlayer.hasEmbededVideo
 
-        onWheel: {
+        onWheel: (wheel) => {
             if (rootPlayer.hasEmbededVideo) {
                 wheel.accepted = false
 

modules/gui/qt/player/qml/SliderBar.qml
@@ -229,11 +229,11 @@ Slider {
 
             preventStealing: true
 
-            onPressed: fsm.pressControl(mouse.x / width, mouse.modifiers === Qt.ShiftModifier)
+            onPressed: (mouse) => { fsm.pressControl(mouse.x / width, mouse.modifiers === Qt.ShiftModifier) }
 
-            onReleased: fsm.releaseControl(mouse.x / width, mouse.modifiers === Qt.ShiftModifier)
+            onReleased: (mouse) => { fsm.releaseControl(mouse.x / width, mouse.modifiers === Qt.ShiftModifier) }
 
-            onPositionChanged: fsm.moveControl(mouse.x / width, mouse.modifiers === Qt.ShiftModifier)
+            onPositionChanged: (mouse) => { fsm.moveControl(mouse.x / width, mouse.modifiers === Qt.ShiftModifier) }
 
             onEntered: {
                 if(Player.hasChapters)

modules/gui/qt/player/qml/controlbarcontrols/PlayButton.qml
@@ -173,7 +173,7 @@ T.Control {
             }
         }
 
-        onPressed: {
+        onPressed: (mouse) => {
             if (!cursorInside) {
                 mouse.accepted = false
                 return
@@ -182,12 +182,12 @@ T.Control {
             root.forceActiveFocus(Qt.MouseFocusReason)
         }
 
-        onClicked: {
+        onClicked: (mouse) => {
             MainPlaylistController.togglePlayPause()
             mouse.accepted = true
         }
 
-        onPressAndHold: {
+        onPressAndHold: (mouse) => {
             _pressAndHoldAction()
             mouse.accepted = true
         }

commit 2b68d45d034a98ef84300d4d728578c8ca18497a

qml: fix context menu not opening in playlist delegate
Regression since d34c08723d0edc3448704214280d7b8e97e7849b,
and `this` here is not necessary. It is not clear why it has
been used before.

modules/gui/qt/playlist/qml/PlaylistDelegate.qml
@@ -254,7 +254,7 @@ T.ItemDelegate {
             }
 
             if (contextMenu && mouse.button === Qt.RightButton)
-                contextMenu.popup(index, this.mapToGlobal(mouse.x, mouse.y))
+                contextMenu.popup(index, mapToGlobal(mouse.x, mouse.y))
         }
 
         onDoubleClicked: (mouse) => {