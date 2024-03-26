f7498f6b19a43af10883fb5bc35d88a16f9b3bf2
Steve Lhomme <robux4@ycbcr.xyz>
2024-03-26 13:33:30 +0100

    vout_subpictures: handle margin from the center alignment in subpicture regions
    The margin is added, so to shift to the left/top the margin (i_x/i_y) should be negative.

--- src/video_output/vout_subpictures.c
+++ src/video_output/vout_subpictures.c
@@ -707,7 +707,7 @@ static void SpuRegionPlace(int *x, int *y,
             if (i_align & SUBPICTURE_ALIGN_BOTTOM)
                 *y = subpic->i_original_picture_height - region->fmt.i_visible_height - region->i_y;
             else
-                *y = subpic->i_original_picture_height / 2 - region->fmt.i_visible_height / 2;
+                *y = subpic->i_original_picture_height / 2 - region->fmt.i_visible_height / 2 + region->i_y;
         }
 
         if (i_align & SUBPICTURE_ALIGN_LEFT)
@@ -718,7 +718,7 @@ static void SpuRegionPlace(int *x, int *y,
             if (i_align & SUBPICTURE_ALIGN_RIGHT)
                 *x = subpic->i_original_picture_width - region->fmt.i_visible_width - region->i_x;
             else
-                *x = subpic->i_original_picture_width / 2 - region->fmt.i_visible_width / 2;
+                *x = subpic->i_original_picture_width / 2 - region->fmt.i_visible_width / 2 + region->i_x;
         }
     }
 }
