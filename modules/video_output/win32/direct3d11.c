8160b84a3892b7127b8fb9a638b459f222d39389
Steve Lhomme <robux4@ycbcr.xyz>
2024-04-05 12:50:42 +0200

    direct3d11: allow passing through the upscaler after the tonemapper

--- modules/video_output/win32/direct3d11.c
+++ modules/video_output/win32/direct3d11.c
@@ -1058,7 +1058,7 @@ static void Prepare(vout_display_t *vd, picture_t *picture, subpicture_t *subpic
                 return;
             p_sys = D3D11_TonemapperGetOutput(sys->tonemapProc);
         }
-        else if (sys->scaleProc && D3D11_UpscalerUsed(sys->scaleProc))
+        if (sys->scaleProc && D3D11_UpscalerUsed(sys->scaleProc))
         {
             if (D3D11_UpscalerScale(VLC_OBJECT(vd), sys->scaleProc, p_sys) != VLC_SUCCESS)
                 return;
