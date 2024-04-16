f833a6edd31c7b79c33d1d115c5ec0136a41a0bc
Steve Lhomme <robux4@ycbcr.xyz>
2024-04-16 15:36:48 +0200

    caca: handle CHANGE_SOURCE_PLACE like a CROP change
    The module only works in non windowed mode (dummy window). And it doesn't report any size change.
    So if the video placement changes that means:
    - the source/user cropping has changed
    - the source/user aspect ration has changed (unused here)
    - the user zoom has changed (unused here)
    - the filling mode has changed (unused here)

--- modules/video_output/caca.c
+++ modules/video_output/caca.c
@@ -196,11 +196,11 @@ static int Control(vout_display_t *vd, int query)
 
     switch (query) {
     case VOUT_DISPLAY_CHANGE_SOURCE_CROP:
+    case VOUT_DISPLAY_CHANGE_SOURCE_PLACE:
         sys->update_dither = true;
         return VLC_SUCCESS;
     case VOUT_DISPLAY_CHANGE_DISPLAY_SIZE:
     case VOUT_DISPLAY_CHANGE_SOURCE_ASPECT:
-    case VOUT_DISPLAY_CHANGE_SOURCE_PLACE:
         return VLC_SUCCESS;
 
     default:
