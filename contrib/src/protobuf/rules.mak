videolan/vlc

commit 38c15b26f8101b8b7e7bd4fff96e90f7fee9ac42

contrib: protobuf: don't prefix the protoc binary name
It only generates C++ files which are not platform specific.

We already assume that a "protoc" binary with the proper version is a valid protoc version.

contrib/src/protobuf/rules.mak
@@ -59,9 +59,7 @@ protoc: protoc-$(PROTOBUF_VERSION)-cpp.tar.gz .sum-protoc
 	sed -i.orig -e 's,install(DIRECTORY ,noinstall(DIRECTORY ,' $(UNPACK_DIR)/cmake/install.cmake
 	sed -i.orig -e 's,install(TARGETS ,noinstall(TARGETS ,' $(UNPACK_DIR)/cmake/install.cmake
 	sed -i.orig -e 's,noinstall(TARGETS protoc,install(TARGETS protoc,' $(UNPACK_DIR)/cmake/install.cmake
-	# set the binary prefix
-	echo "set_target_properties(protoc PROPERTIES PREFIX \"$(HOST)-\")" >> $(UNPACK_DIR)/cmake/protoc.cmake
-	# disable libprotobuf-ltie
+	# disable libprotobuf-lite
 	# sed -i.orig -e 's,libprotobuf-lite, ,' $(UNPACK_DIR)/cmake/install.cmake
 	# sed -i.orig -e 's,include(libprotobuf-lite,#include(libprotobuf-lite,' $(UNPACK_DIR)/cmake/CMakeLists.txt
 	$(MOVE)

commit 1f6ad2919344f0baa958356472ee6d4aa7de1a34

contrib: protobuf: fix path to protoc executable
protoc path was set to $(HOST)-protoc but it was changed to protoc and
cannot be found anymore by meson after that.

Regression from 38c15b26f8101b8b7e7bd4fff96e90f7fee9ac42.

contrib/src/protobuf/rules.mak
@@ -18,7 +18,7 @@ ifndef HAVE_CROSS_COMPILE
 PKGS_FOUND += protoc
 endif
 endif
-PKGS.tools.protoc.path = $(BUILDBINDIR)/$(HOST)-protoc
+PKGS.tools.protoc.path = $(BUILDBINDIR)/protoc
 
 ifeq ($(shell protoc --version 2>/dev/null | head -1 | sed s/'.* '// | cut -d '.' -f -2),$(PROTOBUF_MAJVERSION))
 PKGS_FOUND += protoc