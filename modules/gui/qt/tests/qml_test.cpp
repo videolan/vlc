e33592f80ca23fe4c70b9d1fde0397c65b02cc69
Fatih Uzunoglu <fuzun54@outlook.com>
2024-04-25 17:50:39 +0300

    qt: do not use `Q_INIT_RESOURCE` in qml_test.cpp
    We don't use the test with static Qt, and
    Q_INIT_RESOURCE should not be necessary
    with dynamically linked Qt.

--- modules/gui/qt/tests/qml_test.cpp
+++ modules/gui/qt/tests/qml_test.cpp
@@ -26,7 +26,5 @@ int main(int argc, char **argv)
     QTEST_SET_MAIN_SOURCE_PATH
     //run tests offscreen as the CI doesn't have a desktop environment
     qputenv("QT_QPA_PLATFORM", "offscreen");
-    Q_INIT_RESOURCE(qml);
-    Q_INIT_RESOURCE(assets);
     return quick_test_main(argc, argv, "qml_test", QUICK_TEST_SOURCE_DIR);
 }
