3f2959558d345b0659b45630e5b9fa812eaa5bd2
Thomas Guillem <thomas@gllm.fr>
2024-05-02 15:51:47 +0200

    jack: don't restart the audio decoder
    Only the aout stream need to be restarted on error.

--- modules/audio_output/jack.c
+++ modules/audio_output/jack.c
@@ -449,7 +449,7 @@ static int Buffer_ch( jack_nframes_t nframes, void *p_arg )
         jack_deactivate( p_sys->p_jack_client );
         p_sys->read_buffer = orig_buffer;
         status = ENOMEM;
-        aout_RestartRequest(p_aout, true);
+        aout_RestartRequest(p_aout, false);
     }
     return status;
 }
