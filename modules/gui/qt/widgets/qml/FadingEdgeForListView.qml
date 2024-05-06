videolan/vlc

commit 300b3b9c842434ed2029d270d7e3030b62bb63d4

qml: use `itemAtIndex()` in FadingEdgeForListView

modules/gui/qt/widgets/qml/FadingEdgeForListView.qml
@@ -44,11 +44,8 @@ FadingEdge {
                                                 listView.remove?.running ||
                                                 listView.removeDisplaced?.running)
 
-    // TODO: Use itemAtIndex(0) Qt >= 5.13
     // FIXME: Delegate with variable size
-    readonly property Item delegateItem: listView.contentItem.children.length > 0
-                                         ? listView.contentItem.children[listView.contentItem.children.length - 1]
-                                         : null
+    readonly property Item delegateItem: listView.itemAtIndex(0)
 
     readonly property Item firstVisibleItem: {
         if (transitionsRunning || !delegateItem)

commit 48b919841cd9e83febd58b8be6472c03051d5f5e

qml: establish binding in `delegateItem` property of FadingEdgeForListView
When `listView.itemAtIndex(0)` is used, the delegate item is
not resolved when new items added which makes it remain
null.

When `listView.count` is referred as done here, the QML
engine establishes a relational binding and this causes
the property to be updated each time the count is updated.

Regression since 300b3b9c.

modules/gui/qt/widgets/qml/FadingEdgeForListView.qml
@@ -45,7 +45,7 @@ FadingEdge {
                                                 listView.removeDisplaced?.running) ?? false
 
     // FIXME: Delegate with variable size
-    readonly property Item delegateItem: listView.itemAtIndex(0)
+    readonly property Item delegateItem: (listView.count > 0) ? listView.itemAtIndex(0) : null
 
     readonly property Item firstVisibleItem: {
         if (transitionsRunning || !delegateItem)