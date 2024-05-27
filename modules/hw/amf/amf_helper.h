0b65cc7f394b5985f214932ce4c64552bce83c63
Steve Lhomme <robux4@ycbcr.xyz>
2024-05-27 14:29:36 +0200

    amf_helper: share DXGIToAMF()
    (cherry picked from commit 957db11f12c04a2c7b2b459f71b56b4878009d5c)

--- modules/hw/amf/amf_helper.h
+++ modules/hw/amf/amf_helper.h
@@ -11,6 +11,10 @@
 #include <AMF/core/Context.h>
 #include <AMF/core/Factory.h>
 
+#ifdef _WIN32
+#include "../../video_chroma/d3d11_fmt.h"
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -30,6 +34,22 @@ struct vlc_amf_context
 
 int vlc_AMFCreateContext(struct vlc_amf_context *);
 void vlc_AMFReleaseContext(struct vlc_amf_context *);
+
+#ifdef _WIN32
+static inline AMT_TYPE(AMF_SURFACE_FORMAT) DXGIToAMF(DXGI_FORMAT fmt)
+{
+    switch (fmt)
+    {
+        case DXGI_FORMAT_NV12: return AMT_TYPE(AMF_SURFACE_NV12);
+        case DXGI_FORMAT_P010: return AMT_TYPE(AMF_SURFACE_P010);
+        case DXGI_FORMAT_P016: return AMT_TYPE(AMF_SURFACE_P016);
+        case DXGI_FORMAT_B8G8R8A8_UNORM: return AMT_TYPE(AMF_SURFACE_BGRA);
+        case DXGI_FORMAT_R8G8B8A8_UNORM: return AMT_TYPE(AMF_SURFACE_RGBA);
+        case DXGI_FORMAT_R10G10B10A2_UNORM: return AMT_TYPE(AMF_SURFACE_R10G10B10A2);
+        default: return AMT_TYPE(AMF_SURFACE_UNKNOWN);
+    }
+}
+#endif
 
 #ifdef __cplusplus
 } // extern "C"
