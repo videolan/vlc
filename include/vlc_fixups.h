73c858ddc134da61abd7aa0c76bfa6d6613ccc9c
Steve Lhomme <robux4@ycbcr.xyz>
2024-05-31 11:10:44 +0200

    vlc_fixups: remove old IN6_IS_ADDR_MULTICAST hack
    The proper fix has been in mingw-w64 since version 6, which is our minimum.
    
    https://github.com/mingw-w64/mingw-w64/commit/4c2df0d02ed71ae07e85f1b35ac857a148cd2b8f

--- include/vlc_fixups.h
+++ include/vlc_fixups.h
@@ -768,11 +768,6 @@ void sincosf(float, float *, float *);
 char *realpath(const char * restrict pathname, char * restrict resolved_path);
 #endif
 
-/* mingw-w64 has a broken IN6_IS_ADDR_MULTICAST macro */
-#if defined(_WIN32) && defined(__MINGW64_VERSION_MAJOR)
-# define IN6_IS_ADDR_MULTICAST IN6_IS_ADDR_MULTICAST
-#endif
-
 #ifdef __APPLE__
 # define fdatasync fsync
 
