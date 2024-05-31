106756a06c3f5b8ef2b58daeae9d8929bf352fef
Dave Nicolson <david.nicolson@gmail.com>
2024-05-31 19:52:31 +0000

    macosx: Fix brief menu offset shift

--- modules/gui/macosx/menus/VLCMainMenu.m
+++ modules/gui/macosx/menus/VLCMainMenu.m
@@ -1901,27 +1901,25 @@ typedef NS_ENUM(NSInteger, VLCObjectType) {
         const int menuItemOffset = 14;
         const int menuItemOffsetWithActiveState = 24;
 
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (menu == self->_controlsMenu) {
-                BOOL controlsMenuHasActiveState = NO;
-                for (NSMenuItem *viewMenuItem in menu.itemArray) {
-                    if (viewMenuItem.state == NSControlStateValueOn) {
-                        controlsMenuHasActiveState = YES;
-                    }
-                }
-
-                if (controlsMenuHasActiveState) {
-                    self->_rate_view_offset_constraint.constant = menuItemOffsetWithActiveState;
-                } else {
-                    self->_rate_view_offset_constraint.constant = menuItemOffset;
+        if (menu == _controlsMenu) {
+            BOOL controlsMenuHasActiveState = NO;
+            for (NSMenuItem * const viewMenuItem in menu.itemArray) {
+                if (viewMenuItem.state == NSControlStateValueOn) {
+                    controlsMenuHasActiveState = YES;
                 }
             }
 
-            if (menu == self->_subtitlesMenu) {
-                self->_subtitle_bgopacity_view_offset_constraint.constant = menuItemOffset;
-                self->_subtitleSizeViewOffsetConstraint.constant = menuItemOffset;
+            if (controlsMenuHasActiveState) {
+                _rate_view_offset_constraint.constant = menuItemOffsetWithActiveState;
+            } else {
+                _rate_view_offset_constraint.constant = menuItemOffset;
             }
-        });
+        }
+
+        if (menu == _subtitlesMenu) {
+            _subtitle_bgopacity_view_offset_constraint.constant = menuItemOffset;
+            _subtitleSizeViewOffsetConstraint.constant = menuItemOffset;
+        }
     }
 }
 
