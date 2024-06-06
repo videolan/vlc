e709012f02f752279784c58e3cdd9ff3883574d4
François Cartegnie <fcvlcdev@free.fr>
2024-06-06 18:10:03 +0700

    demux: adaptive: map VVC

--- modules/demux/adaptive/tools/FormatNamespace.cpp
+++ modules/demux/adaptive/tools/FormatNamespace.cpp
@@ -148,6 +148,12 @@ void FormatNamespace::Parse(vlc_fourcc_t fcc, const std::vector<std::string> &el
                 fmt.i_level = std::stoi(elements.at(1), nullptr, 16);
             }
             break;
+        case MP4RA("vvc1"):
+        case MP4RA("vvi1"):
+            es_format_Change(&fmt, VIDEO_ES, VLC_CODEC_VVC);
+            if(elements.size() > 0)
+                fmt.i_profile = std::stoi(elements.at(0), nullptr, 16);
+            break;
         case MSFCC("AVC1"):
         case MSFCC("AVCB"):
         case MSFCC("H264"):
