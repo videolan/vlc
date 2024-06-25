e68f089d5419c7832c625f535f9ec79d62a1c624
Steve Lhomme <robux4@ycbcr.xyz>
2024-06-25 09:58:40 +0200

    bdagraph: inlcude mpeg2structs.h to get official values
    No need to define the structures and classes. They are part of the Windows SDK and
    mingw-w64 v2 (03a5b84c5fdd3156ec477caf26b8775d6065c57d).
    
    mingw-w64 emits a warning about possible interface layout, but we don't use these interfaces.

--- modules/access/dtv/bdadefs.h
+++ modules/access/dtv/bdadefs.h
@@ -21,6 +21,7 @@
  *****************************************************************************/
 
 #include <tuner.h>
+#include <mpeg2structs.h>
 
 #ifndef __ISampleGrabberCB_FWD_DEFINED__
 #define __ISampleGrabberCB_FWD_DEFINED__
@@ -38,14 +39,11 @@ interface ISampleGrabber;
 #endif /* __cplusplus */
 #endif
 
-class IMpeg2Data;
 class IGuideData;
 class IGuideDataEvent;
-class ISectionList;
 class IEnumTuneRequests;
 class IEnumGuideDataProperties;
 class IGuideDataProperty;
-class IMpeg2Stream;
 
 
 /*****************************************************************************
@@ -290,183 +288,6 @@ static FORCEINLINE HRESULT ISampleGrabber_SetCallback(ISampleGrabber* This,ISamp
 #endif  /* __ISampleGrabber_INTERFACE_DEFINED__ */
 
 
-typedef struct _MPEG_HEADER_BITS_MIDL
-{
-    WORD Bits;
-} MPEG_HEADER_BITS_MIDL;
-
-typedef struct _MPEG_HEADER_VERSION_BITS_MIDL
-{
-    BYTE Bits;
-} MPEG_HEADER_VERSION_BITS_MIDL;
-
-typedef WORD PID;
-
-typedef BYTE TID;
-
-typedef struct _SECTION
-{
-    TID TableId;
-    union
-    {
-        MPEG_HEADER_BITS_MIDL S;
-        WORD W;
-    } Header;
-    BYTE SectionData[ 1 ];
-} SECTION, *PSECTION;
-
-typedef struct _LONG_SECTION
-{
-    TID TableId;
-    union
-    {
-        MPEG_HEADER_BITS_MIDL S;
-        WORD W;
-    } Header;
-    WORD TableIdExtension;
-    union
-    {
-        MPEG_HEADER_VERSION_BITS_MIDL S;
-        BYTE B;
-        } Version;
-    BYTE SectionNumber;
-    BYTE LastSectionNumber;
-    BYTE RemainingData[ 1 ];
-} LONG_SECTION;
-
-typedef struct _MPEG_BCS_DEMUX
-{
-    DWORD AVMGraphId;
-} MPEG_BCS_DEMUX;
-
-typedef struct _MPEG_WINSOC
-{
-    DWORD AVMGraphId;
-} MPEG_WINSOCK;
-
-typedef enum
-{
-    MPEG_CONTEXT_BCS_DEMUX = 0,
-    MPEG_CONTEXT_WINSOCK = MPEG_CONTEXT_BCS_DEMUX + 1
-} MPEG_CONTEXT_TYPE;
-
-typedef struct _MPEG_RQST_PACKET
-{
-    DWORD dwLength;
-    PSECTION pSection;
-} MPEG_RQST_PACKET, *PMPEG_RQST_PACKET;
-
-typedef struct _MPEG_PACKET_LIST
-{
-    WORD wPacketCount;
-    PMPEG_RQST_PACKET PacketList[ 1 ];
-} MPEG_PACKET_LIST, *PMPEG_PACKET_LIST;
-
-typedef struct _DSMCC_FILTER_OPTIONS
-{
-    BOOL fSpecifyProtocol;
-    BYTE Protocol;
-    BOOL fSpecifyType;
-    BYTE Type;
-    BOOL fSpecifyMessageId;
-    WORD MessageId;
-    BOOL fSpecifyTransactionId;
-    BOOL fUseTrxIdMessageIdMask;
-    DWORD TransactionId;
-    BOOL fSpecifyModuleVersion;
-    BYTE ModuleVersion;
-    BOOL fSpecifyBlockNumber;
-    WORD BlockNumber;
-    BOOL fGetModuleCall;
-    WORD NumberOfBlocksInModule;
-} DSMCC_FILTER_OPTIONS;
-
-typedef struct _ATSC_FILTER_OPTIONS
-{
-    BOOL fSpecifyEtmId;
-    DWORD EtmId;
-} ATSC_FILTER_OPTIONS;
-
-typedef struct _MPEG_STREAM_BUFFER
-{
-    HRESULT hr;
-    DWORD dwDataBufferSize;
-    DWORD dwSizeOfDataRead;
-    BYTE *pDataBuffer;
-} MPEG_STREAM_BUFFER, *PMPEG_STREAM_BUFFER;
-
-typedef struct _MPEG_CONTEXT
-{
-    MPEG_CONTEXT_TYPE Type;
-    union
-    {
-        MPEG_BCS_DEMUX Demux;
-        MPEG_WINSOCK Winsock;
-    } U;
-} MPEG_CONTEXT, *PMPEG_CONTEXT;
-
-typedef enum
-{
-   MPEG_RQST_UNKNOWN = 0,
-   MPEG_RQST_GET_SECTION = MPEG_RQST_UNKNOWN + 1,
-   MPEG_RQST_GET_SECTION_ASYNC = MPEG_RQST_GET_SECTION + 1,
-   MPEG_RQST_GET_TABLE = MPEG_RQST_GET_SECTION_ASYNC + 1,
-   MPEG_RQST_GET_TABLE_ASYNC = MPEG_RQST_GET_TABLE + 1,
-   MPEG_RQST_GET_SECTIONS_STREAM = MPEG_RQST_GET_TABLE_ASYNC + 1,
-   MPEG_RQST_GET_PES_STREAM = MPEG_RQST_GET_SECTIONS_STREAM + 1,
-   MPEG_RQST_GET_TS_STREAM = MPEG_RQST_GET_PES_STREAM + 1,
-   MPEG_RQST_START_MPE_STREAM = MPEG_RQST_GET_TS_STREAM + 1
-} MPEG_REQUEST_TYPE;
-
-typedef struct _MPEG2_FILTER
-{
-    BYTE bVersionNumber;
-    WORD wFilterSize;
-    BOOL fUseRawFilteringBits;
-    BYTE Filter[ 16 ];
-    BYTE Mask[ 16 ];
-    BOOL fSpecifyTableIdExtension;
-    WORD TableIdExtension;
-    BOOL fSpecifyVersion;
-    BYTE Version;
-    BOOL fSpecifySectionNumber;
-    BYTE SectionNumber;
-    BOOL fSpecifyCurrentNext;
-    BOOL fNext;
-    BOOL fSpecifyDsmccOptions;
-    DSMCC_FILTER_OPTIONS Dsmcc;
-    BOOL fSpecifyAtscOptions;
-    ATSC_FILTER_OPTIONS Atsc;
-} MPEG2_FILTER, *PMPEG2_FILTER;
-
-typedef struct _MPEG_HEADER_BITS
-{
-    WORD SectionLength          : 12;
-    WORD Reserved               :  2;
-    WORD PrivateIndicator       :  1;
-    WORD SectionSyntaxIndicator :  1;
-} MPEG_HEADER_BITS, *PMPEG_HEADER_BITS;
-
-typedef struct _MPEG_HEADER_VERSION_BITS
-{
-    BYTE CurrentNextIndicator : 1;
-    BYTE VersionNumber        : 5;
-    BYTE Reserved             : 2;
-} MPEG_HEADER_VERSION_BITS, *PMPEG_HEADER_VERSION_BITS;
-
-class IMpeg2Data : public IUnknown
-{
-public:
-    virtual HRESULT STDMETHODCALLTYPE GetSection( PID pid, TID tid,
-        PMPEG2_FILTER pFilter, DWORD dwTimeout,
-        ISectionList **ppSectionList )=0;
-    virtual HRESULT STDMETHODCALLTYPE GetTable( PID pid, TID tid, PMPEG2_FILTER pFilter,
-        DWORD dwTimeout, ISectionList **ppSectionList )=0;
-    virtual HRESULT STDMETHODCALLTYPE GetStreamOfSections( PID pid, TID tid,
-        PMPEG2_FILTER pFilter, HANDLE hDataReadyEvent,
-        IMpeg2Stream **ppMpegStream )=0;
-};
-
 class IGuideData : public IUnknown
 {
 public:
@@ -513,32 +334,6 @@ public:
     virtual  HRESULT STDMETHODCALLTYPE get_Value( VARIANT *pvar )=0;
 };
 
-class IMpeg2Stream : public IUnknown
-{
-public:
-    virtual HRESULT STDMETHODCALLTYPE Initialize( MPEG_REQUEST_TYPE requestType,
-        IMpeg2Data *pMpeg2Data, PMPEG_CONTEXT pContext, PID pid, TID tid,
-        PMPEG2_FILTER pFilter, HANDLE hDataReadyEvent )=0;
-    virtual HRESULT STDMETHODCALLTYPE SupplyDataBuffer(
-        PMPEG_STREAM_BUFFER pStreamBuffer )=0;
-};
-
-class ISectionList : public IUnknown
-{
-public:
-    virtual HRESULT STDMETHODCALLTYPE Initialize( MPEG_REQUEST_TYPE requestType,
-        IMpeg2Data *pMpeg2Data, PMPEG_CONTEXT pContext, PID pid, TID tid,
-        PMPEG2_FILTER pFilter, DWORD timeout, HANDLE hDoneEvent )=0;
-    virtual HRESULT STDMETHODCALLTYPE InitializeWithRawSections(
-        PMPEG_PACKET_LIST pmplSections )=0;
-    virtual HRESULT STDMETHODCALLTYPE CancelPendingRequest( void )=0;
-    virtual HRESULT STDMETHODCALLTYPE GetNumberOfSections( WORD *pCount )=0;
-    virtual HRESULT STDMETHODCALLTYPE GetSectionData( WORD sectionNumber,
-        DWORD *pdwRawPacketLength, PSECTION *ppSection )=0;
-    virtual HRESULT STDMETHODCALLTYPE GetProgramIdentifier( PID *pPid )=0;
-    virtual HRESULT STDMETHODCALLTYPE GetTableIdentifier( TID *pTableId )=0;
-};
-
 class IEnumGuideDataProperties : public IUnknown
 {
 public:
@@ -576,8 +371,5 @@ DEFINE_GUID(KSDATAFORMAT_SUBTYPE_BDA_MPEG2_TRANSPORT,
     0xF4AEB342,0x0329,0x4fdd,0xA8,0xFD,0x4A,0xFF,0x49,0x26,0xC9,0x78);
 
 extern const CLSID CLSID_SampleGrabber; // found in strmiids
-
-DEFINE_GUID(IID_IMpeg2Data,
-    0x9B396D40,0xF380,0x4e3c,0xA5,0x14,0x1A,0x82,0xBF,0x6E,0xBF,0xE6);
 
 };
