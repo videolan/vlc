videolan/vlc

commit 03d91093af3fb6f1bd913715bc4656b87ead8f09

input_clock: finer discontinuity handling
We need to compare both stream and system times for discontinuity.
Indeed, a big stream diff is OK if we have the same system diff.
This check is now done after buffering is complete (b_has_external_clock
== true).

src/clock/input_clock.c
@@ -232,18 +232,29 @@ vlc_tick_t input_clock_Update( input_clock_t *cl, vlc_object_t *p_log,
         /* */
         b_reset_reference= true;
     }
-    else if( cl->last.stream != VLC_TICK_INVALID &&
-             ( (cl->last.stream - i_ck_stream) > CR_MAX_GAP ||
-               (cl->last.stream - i_ck_stream) < -CR_MAX_GAP ) )
+    else if (cl->last.stream != VLC_TICK_INVALID && cl->b_has_external_clock)
     {
-        /* Stream discontinuity, for which we haven't received a
-         * warning from the stream control facilities (dd-edited
-         * stream ?). */
-        msg_Warn( p_log, "clock gap, unexpected stream discontinuity" );
+        assert(cl->last.system != VLC_TICK_INVALID);
 
-        /* */
-        msg_Warn( p_log, "feeding synchro with a new reference point trying to recover from clock gap" );
-        b_reset_reference= true;
+        /* We need compare both stream and system times for discontinuity.
+         * Indeed, a big stream diff is OK if we have the same system diff. */
+        vlc_tick_t stream_diff = i_ck_stream - cl->last.stream;
+        vlc_tick_t system_diff = i_ck_system - cl->last.system;
+        vlc_tick_t diff = stream_diff - system_diff;
+        if (diff > CR_MAX_GAP || diff < -CR_MAX_GAP)
+        {
+            /* Stream discontinuity, for which we haven't received a
+             * warning from the stream control facilities (dd-edited
+             * stream ?). */
+            msg_Warn(p_log, "clock gap, unexpected stream discontinuity: "
+                     "system_diff: %"PRId64" stream_diff: %"PRId64,
+                     system_diff, stream_diff);
+
+            /* */
+            msg_Warn(p_log, "feeding synchro with a new reference point trying"
+                     " to recover from clock gap");
+            b_reset_reference= true;
+        }
     }
 
     /* */

commit a7ddfd6102d349b1ca1e39a22eb57149052fe003

input_clock: fix discontinuity when origin has been changed
Regression from 03d91093af3fb6f1bd913715bc4656b87ead8f09

Thought that b_has_external_clock was always true after
input_clock_ChangeSystemOrigin() (called at the end of buffering) was
being called, but this variable was never changed.

Therefore, the clock offset after this call always led to a discontinuity.

src/clock/input_clock.c
@@ -130,6 +130,8 @@ struct input_clock_t
     clock_point_t ref;
     bool          b_has_reference;
 
+    bool          b_origin_changed;
+
     /* External clock drift */
     vlc_tick_t    i_external_clock;
     bool          b_has_external_clock;
@@ -173,6 +175,7 @@ input_clock_t *input_clock_New( float rate )
     cl->listener.opaque = NULL;
 
     cl->b_has_reference = false;
+    cl->b_origin_changed = false;
     cl->ref = clock_point_Create( VLC_TICK_INVALID, VLC_TICK_INVALID );
     cl->b_has_external_clock = false;
 
@@ -234,7 +237,8 @@ vlc_tick_t input_clock_Update( input_clock_t *cl, vlc_object_t *p_log,
         /* */
         b_reset_reference= true;
     }
-    else if (cl->last.stream != VLC_TICK_INVALID && cl->b_has_external_clock)
+    /* Don't check discontinuities if the origin has just been changed */
+    else if (cl->last.stream != VLC_TICK_INVALID && !cl->b_origin_changed)
     {
         assert(cl->last.system != VLC_TICK_INVALID);
 
@@ -258,6 +262,7 @@ vlc_tick_t input_clock_Update( input_clock_t *cl, vlc_object_t *p_log,
             b_reset_reference= true;
         }
     }
+    cl->b_origin_changed = false;
 
     /* */
     if( b_reset_reference )
@@ -325,6 +330,7 @@ vlc_tick_t input_clock_Update( input_clock_t *cl, vlc_object_t *p_log,
 void input_clock_Reset( input_clock_t *cl )
 {
     cl->b_has_reference = false;
+    cl->b_origin_changed = false;
     cl->ref = cl->last
         = clock_point_Create( VLC_TICK_INVALID, VLC_TICK_INVALID );
     cl->b_has_external_clock = false;
@@ -413,6 +419,8 @@ int input_clock_GetState( input_clock_t *cl,
 void input_clock_ChangeSystemOrigin( input_clock_t *cl, bool b_absolute, vlc_tick_t i_system )
 {
     assert( cl->b_has_reference );
+    cl->b_origin_changed = true;
+
     vlc_tick_t i_offset;
     if( b_absolute )
     {