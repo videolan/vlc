1cf816212aa806ae8888ae6143eadf1080cdced5
Claudio Cambra <developer@claudiocambra.com>
2024-07-13 00:39:33 +0800

    macosx: Make sure to update cover art on audio decorative view setup in main video view
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/windows/video/VLCMainVideoViewController.m
+++ modules/gui/macosx/windows/video/VLCMainVideoViewController.m
@@ -133,6 +133,7 @@
     VLCPlayerController * const controller =
         VLCMain.sharedInstance.playlistController.playerController;
     [self updateDecorativeViewVisibilityOnControllerChange:controller];
+    [self.audioDecorativeView updateCoverArt];
 }
 
 - (void)viewDidLoad
