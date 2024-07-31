3404decace19c8bb6aa5778f61b406f8a6eb2ff1
Claudio Cambra <developer@claudiocambra.com>
2024-07-31 17:35:15 +0800

    macosx: Add convenience method for getting index of requested group in data source
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/groups-library/VLCLibraryGroupsDataSource.m
+++ modules/gui/macosx/library/groups-library/VLCLibraryGroupsDataSource.m
@@ -75,6 +75,16 @@
     [self.collectionView reloadData];
 }
 
+- (NSUInteger)indexOfMediaItem:(const NSUInteger)libraryId inArray:(NSArray const *)array
+{
+    return [array indexOfObjectPassingTest:^BOOL(const id<VLCMediaLibraryItemProtocol> findItem,
+                                                 const NSUInteger idx,
+                                                 BOOL * const stop) {
+        NSAssert(findItem != nil, @"Collection should not contain nil items");
+        return findItem.libraryID == libraryId;
+    }];
+}
+
 #pragma mark - table view data source and delegation
 
 - (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
