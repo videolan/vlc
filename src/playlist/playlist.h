befc924c225a39bf49f6e4ac09d1660013ce0676
Alexandre Janniaux <ajanni@videolabs.io>
2024-08-04 16:44:15 +0200

    playlist: remove returned value in tests
    Commit 67d14d9218dad82a9644bd440f024dc47d9ece78 introduced the macro to
    mock vlc_player_SetNextMedia, but the function was returning void.
    Probably a copy-paste typo from the line right above.
    
        In file included from ../../src/playlist/control.c:29:
        ../../src/playlist/control.c: In function ‘vlc_playlist_UpdateNextMedia’:
        ../../src/playlist/playlist.h:41:53: warning: statement with no effect [-Wunused-value]
           41 | # define vlc_player_SetNextMedia(a,b) (VLC_UNUSED(b), VLC_SUCCESS)
              |                                       ~~~~~~~~~~~~~~^~~~~~~~~~~~~~
        ../../src/playlist/control.c:456:5: note: in expansion of macro ‘vlc_player_SetNextMedia’
          456 |     vlc_player_SetNextMedia(playlist->player, media);
              |     ^~~~~~~~~~~~~~~~~~~~~~~

--- src/playlist/playlist.h
+++ src/playlist/playlist.h
@@ -38,7 +38,7 @@ typedef struct input_item_t input_item_t;
 # define vlc_player_AddListener(a,b,c) (VLC_UNUSED(b), malloc(sizeof(vlc_player_listener_id)))
 # define vlc_player_RemoveListener(a,b) free(b)
 # define vlc_player_SetCurrentMedia(a,b) (VLC_UNUSED(b), VLC_SUCCESS)
-# define vlc_player_SetNextMedia(a,b) (VLC_UNUSED(b), VLC_SUCCESS)
+# define vlc_player_SetNextMedia(a,b) VLC_UNUSED(b)
 # define vlc_player_osd_Message(p, fmt...) VLC_UNUSED(p)
 #endif /* TEST_PLAYLIST */
 
