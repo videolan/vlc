videolan/vlc

commit 03d91093af3fb6f1bd913715bc4656b87ead8f09

input_clock: finer discontinuity handling
We need to compare both stream and system times for discontinuity.
Indeed, a big stream diff is OK if we have the same system diff.
This check is now done after buffering is complete (b_has_external_clock
== true).

src/clock/input_clock.c
@@ -232,18 +232,29 @@ vlc_tick_t input_clock_Update( input_clock_t *cl, vlc_object_t *p_log,
         /* */
         b_reset_reference= true;
     }
-    else if( cl->last.stream != VLC_TICK_INVALID &&
-             ( (cl->last.stream - i_ck_stream) > CR_MAX_GAP ||
-               (cl->last.stream - i_ck_stream) < -CR_MAX_GAP ) )
+    else if (cl->last.stream != VLC_TICK_INVALID && cl->b_has_external_clock)
     {
-        /* Stream discontinuity, for which we haven't received a
-         * warning from the stream control facilities (dd-edited
-         * stream ?). */
-        msg_Warn( p_log, "clock gap, unexpected stream discontinuity" );
+        assert(cl->last.system != VLC_TICK_INVALID);
 
-        /* */
-        msg_Warn( p_log, "feeding synchro with a new reference point trying to recover from clock gap" );
-        b_reset_reference= true;
+        /* We need compare both stream and system times for discontinuity.
+         * Indeed, a big stream diff is OK if we have the same system diff. */
+        vlc_tick_t stream_diff = i_ck_stream - cl->last.stream;
+        vlc_tick_t system_diff = i_ck_system - cl->last.system;
+        vlc_tick_t diff = stream_diff - system_diff;
+        if (diff > CR_MAX_GAP || diff < -CR_MAX_GAP)
+        {
+            /* Stream discontinuity, for which we haven't received a
+             * warning from the stream control facilities (dd-edited
+             * stream ?). */
+            msg_Warn(p_log, "clock gap, unexpected stream discontinuity: "
+                     "system_diff: %"PRId64" stream_diff: %"PRId64,
+                     system_diff, stream_diff);
+
+            /* */
+            msg_Warn(p_log, "feeding synchro with a new reference point trying"
+                     " to recover from clock gap");
+            b_reset_reference= true;
+        }
     }
 
     /* */

commit e6459d2b700e1509190b3fb3035a11f88a8d8abd

input_clock: finer backward discontinuity detection
This commit fixes a regression from
03d91093af3fb6f1bd913715bc4656b87ead8f09 that prevented a RESET_PCR when
the input was more than 300ms late.

Indeed, a PCR going backward must be a discontinuity, there is no need
to compare it with system dates. Comparing with system dates could lead
to a backward system discontinuity, that is generally handled by
increasing the pts-delay and calling RESET_PCR (when
input_clock_Update() return late > 0).

src/clock/input_clock.c
@@ -243,7 +243,10 @@ vlc_tick_t input_clock_Update( input_clock_t *cl, vlc_object_t *p_log,
         vlc_tick_t stream_diff = i_ck_stream - cl->last.stream;
         vlc_tick_t system_diff = (i_ck_system - cl->last.system) * cl->rate;
         vlc_tick_t diff = stream_diff - system_diff;
-        if (diff > CR_MAX_GAP || diff < -CR_MAX_GAP)
+
+        /* A discontinuity happen if stream timings increase much more than
+         * system timings or if the stream is going backward. */
+        if (diff > CR_MAX_GAP || stream_diff < 0)
         {
             /* Stream discontinuity, for which we haven't received a
              * warning from the stream control facilities (dd-edited