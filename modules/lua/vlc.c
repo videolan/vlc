fd442e5a8f3e16438f5acdbc4834a561b683efc2
Ayush Dey <deyayush6@gmail.com>
2024-08-08 13:09:17 +0530

    lua: modify TRY_META macro to call vlc_meta_SetWithPriority
    In TRY_META macro, replace input_item_Set##b() with vlc_meta_SetWithPriority().
    Rename the TrackNum field to TrackNumber and ArtURL to ArtworkURL for
    consistency with the vlc_meta_type_t enumeration.
    Refs #4143

--- modules/lua/vlc.c
+++ modules/lua/vlc.c
@@ -254,16 +254,20 @@ void vlclua_read_meta_data( vlc_object_t *p_this, lua_State *L,
         char *psz_value = strdup( lua_tostring( L, -1 ) );      \
         EnsureUTF8( psz_value );                                \
         msg_Dbg( p_this, #b ": %s", psz_value );                \
-        input_item_Set ## b ( p_input, psz_value );             \
+        vlc_meta_SetWithPriority( p_input->p_meta,              \
+                                  vlc_meta_ ## b,               \
+                                  psz_value,                    \
+                                  VLC_META_PRIORITY_BASIC );    \
         free( psz_value );                                      \
     }                                                           \
     lua_pop( L, 1 ); /* pop a */
+    vlc_mutex_lock( &p_input->lock );
     TRY_META( "title", Title );
     TRY_META( "artist", Artist );
     TRY_META( "genre", Genre );
     TRY_META( "copyright", Copyright );
     TRY_META( "album", Album );
-    TRY_META( "tracknum", TrackNum );
+    TRY_META( "tracknum", TrackNumber );
     TRY_META( "description", Description );
     TRY_META( "rating", Rating );
     TRY_META( "date", Date );
@@ -273,13 +277,14 @@ void vlclua_read_meta_data( vlc_object_t *p_this, lua_State *L,
     TRY_META( "nowplaying", NowPlaying );
     TRY_META( "publisher",  Publisher );
     TRY_META( "encodedby",  EncodedBy );
-    TRY_META( "arturl",     ArtURL );
+    TRY_META( "arturl",     ArtworkURL );
     TRY_META( "trackid",    TrackID );
     TRY_META( "director",   Director );
     TRY_META( "season",     Season );
     TRY_META( "episode",    Episode );
     TRY_META( "show_name",  ShowName );
     TRY_META( "actors",     Actors );
+    vlc_mutex_unlock( &p_input->lock );
 }
 
 #undef vlclua_read_custom_meta_data
