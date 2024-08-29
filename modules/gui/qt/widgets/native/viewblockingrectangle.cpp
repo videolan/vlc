a8cd2d21fc5d881fac9e7692ff19944df2e8e2c8
Fatih Uzunoglu <fuzun54@outlook.com>
2024-08-29 18:43:54 +0300

    qt: fix undefined behavior when openvg scene graph backend is used
    When OpenVG is used, the rectangle node provided does not offer
    a material. At the same time, it is also not a null pointer but
    it explicitly points to memory address "1" as a workaround done
    by Qt.
    
    Obviously, that memory address does not contain a real `QSGMaterial`
    instance, so dereferencing the material causes segmentation fault.

--- modules/gui/qt/widgets/native/viewblockingrectangle.cpp
+++ modules/gui/qt/widgets/native/viewblockingrectangle.cpp
@@ -95,7 +95,14 @@ ViewBlockingRectangle::ViewBlockingRectangle(QQuickItem *parent)
 {
     setFlag(QQuickItem::ItemHasContents);
     connect(this, &ViewBlockingRectangle::colorChanged, this, &QQuickItem::update);
-    connect(this, &ViewBlockingRectangle::windowChanged, this, [this] { m_windowChanged = true; });
+    connect(this, &ViewBlockingRectangle::windowChanged, this, [this] {
+        if (window())
+        {
+            // The new window might use an appropriate graphics API:
+            setFlag(QQuickItem::ItemHasContents);
+            m_windowChanged = true;
+        }
+    });
 }
 
 QSGNode *ViewBlockingRectangle::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
@@ -124,7 +131,18 @@ QSGNode *ViewBlockingRectangle::updatePaintNode(QSGNode *oldNode, UpdatePaintNod
         {
             rectangleNode = window()->createRectangleNode();
             assert(rectangleNode);
-            assert(rectangleNode->material());
+
+            const auto material = rectangleNode->material();
+            if (!material ||
+                material == reinterpret_cast<QSGMaterial*>(1) /* Qt may explicitly set the material pointer to 1 in OpenVG */)
+            {
+                // Scene graph adaptation does not support shading
+                qmlDebug(this) << "ViewBlockingRectangle is being used under an incompatible scene graph adaptation.";
+                delete rectangleNode;
+                setFlag(QQuickItem::ItemHasContents, false);
+                return nullptr;
+            }
+
             rectangleNode->material()->setFlag(QSGMaterial::Blending, false);
         }
     }
