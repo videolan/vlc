videolan/vlc

commit 1a54ee461afda0126951ed568426f5577739815a

video_output: read the user aspect-ratio values before creating the display

src/video_output/video_output.c
@@ -2198,6 +2198,14 @@ static int EnableWindowLocked(vout_thread_sys_t *vout, const video_format_t *ori
 
 static void vout_InitSource(vout_thread_sys_t *vout)
 {
+    char *psz_ar = var_InheritString(&vout->obj, "aspect-ratio");
+    if (psz_ar) {
+        unsigned num, den;
+        if (!GetAspectRatio(psz_ar, &num, &den))
+            vout_SetAspectRatio(vout, num, den);
+        free(psz_ar);
+    }
+
     char *psz_crop = var_InheritString(&vout->obj, "crop");
     if (psz_crop) {
         unsigned num, den;

src/video_output/vout_internal.h
@@ -108,6 +108,7 @@ bool GetCropMode(const char *crop_str, enum vout_crop_mode *mode,
                         unsigned *num, unsigned *den,
                         unsigned *x, unsigned *y,
                         unsigned *width, unsigned *height );
+bool GetAspectRatio(const char *ar_str, unsigned *num, unsigned *den);
 
 /* TODO to move them to vlc_vout.h */
 void vout_ChangeFullscreen(vout_thread_t *, const char *id);

src/video_output/vout_intf.c
@@ -523,7 +523,7 @@ static int CropBorderCallback(vlc_object_t *object, char const *cmd,
     return VLC_SUCCESS;
 }
 
-static bool GetAspectRatio(const char *ar_str, unsigned *num, unsigned *den)
+bool GetAspectRatio(const char *ar_str, unsigned *num, unsigned *den)
 {
     if (sscanf(ar_str, "%u:%u", num, den) == 2 &&
         (num != 0) == (den != 0))

commit e997e6ac87affbc708d9dbe9511d87272d7714b3

video_output: fix Display Aspect Ratio initialization
The read value is correct when GetAspectRatio() returns true.
Bug introduced in 1a54ee461afda0126951ed568426f5577739815a.

src/video_output/video_output.c
@@ -2253,7 +2253,7 @@ static void vout_InitSource(vout_thread_sys_t *vout)
     char *psz_ar = var_InheritString(&vout->obj, "aspect-ratio");
     if (psz_ar) {
         unsigned num, den;
-        if (!GetAspectRatio(psz_ar, &num, &den))
+        if (GetAspectRatio(psz_ar, &num, &den))
             vout_SetAspectRatio(vout, num, den);
         free(psz_ar);
     }