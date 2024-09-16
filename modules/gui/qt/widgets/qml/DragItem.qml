videolan/vlc

commit 72f086c9865bb6e67b51b5e23a364033fa70dfe2

qml: only use RoundImage in DragItem
network cover specialized item can easily be replaced by the default
implementation, as both its artwork and fallback image are URI.

DragItem shows the artworks in a rounded shape and low resolution, the default
fill policy of roundimage (PreserveAspectCrop) gives good results for this use
case

fixes: #28301

modules/gui/qt/widgets/qml/DragItem.qml
@@ -51,7 +51,7 @@ Item {
 
     property string defaultText: qsTr("Unknown")
 
-    // function(index, data) - returns cover for the index in the model in the form {artwork: <string> (file-name), cover: <component>}
+    // function(index, data) - returns cover for the index in the model in the form {artwork: <string> (file-name), fallback: <string> (file-name)}
     property var coverProvider: null
 
     // string => role
@@ -157,7 +157,10 @@ Item {
         }
 
         if (covers.length === 0)
-            covers.push({artwork: dragItem.defaultCover})
+            covers.push({
+                artwork: "",
+                fallback: dragItem.defaultCover
+            })
 
         _covers = covers
     }
@@ -177,7 +180,10 @@ Item {
         if (!!dragItem.coverProvider)
             return dragItem.coverProvider(index, data)
         else
-            return {artwork: data[dragItem.coverRole] || dragItem.defaultCover}
+            return {
+                artwork: data[dragItem.coverRole] || dragItem.defaultCover,
+                fallback: dragItem.defaultCover
+            }
     }
 
     function _startNativeDrag() {
@@ -415,44 +421,26 @@ Item {
             }
 
             Widgets.RoundImage {
+                id: artworkCover
+
+                anchors.centerIn: parent
+                width: coverSize
+                height: coverSize
+                radius: bg.radius
+                source: modelData.artwork ?? ""
+                sourceSize: dragItem.imageSourceSize ?? Qt.size(width, height)
+            }
+
+            Widgets.RoundImage {
                 id: fallbackCover
 
                 anchors.centerIn: parent
                 width: coverSize
                 height: coverSize
                 radius: bg.radius
-                source: dragItem.defaultCover
-                sourceSize.width: width
-                sourceSize.height: height
-                visible: !loader.visible
-            }
-
-            Loader {
-                id: loader
-
-                // parent may provide extra data with covers
-                property var model: modelData
-
-                anchors.centerIn: parent
-
-                visible: (status === Loader.Ready)
-                         && (!("status" in item) || (item.status === Image.Ready))
-
-                sourceComponent: (!modelData.artwork || modelData.artwork.toString() === "") ? modelData.cover : artworkLoader
-                layer.enabled: true
-                layer.effect: OpacityMask {
-                    maskSource: Rectangle {
-                        width: bg.width
-                        height: bg.height
-                        radius: bg.radius
-                        visible: false
-                    }
-                }
-
-                onItemChanged: {
-                    if (modelData.artwork && modelData.artwork.toString() !== "")
-                        item.source = modelData.artwork
-                }
+                source: modelData.fallback ?? defaultCover
+                sourceSize: dragItem.imageSourceSize ?? Qt.size(width, height)
+                visible: artworkCover.status !== Image.Ready
             }
 
             Rectangle {
@@ -510,14 +498,4 @@ Item {
         color: theme.fg.secondary
     }
 
-    Component {
-        id: artworkLoader
-
-        ScaledImage {
-            fillMode: Image.PreserveAspectCrop
-            width: coverSize
-            height: coverSize
-            cache: false
-        }
-    }
 }

modules/gui/qt/network/qml/BrowseTreeDisplay.qml
@@ -94,10 +94,33 @@ MainViewLoader {
         defaultText:  qsTr("Unknown Share")
 
         coverProvider: function(index, data) {
-            // this is used to provide context to NetworkCustomCover
-            // indexData is networkModel (model data) for this index
-            // cover is our custom cover that will be loaded insted of default DragItem cover
-            return {"indexData": data, "cover": custom_cover}
+            function _baseUri(type) {
+                switch (type) {
+                case NetworkMediaModel.TYPE_DISC:
+                    return "qrc:///sd/disc.svg"
+                case NetworkMediaModel.TYPE_CARD:
+                    return "qrc:///sd/capture-card.svg"
+                case NetworkMediaModel.TYPE_STREAM:
+                    return "qrc:///sd/stream.svg"
+                case NetworkMediaModel.TYPE_PLAYLIST:
+                    return "qrc:///sd/playlist.svg"
+                case NetworkMediaModel.TYPE_FILE:
+                    return "qrc:///sd/file.svg"
+                default:
+                    return "qrc:///sd/directory.svg"
+                }
+            }
+
+            const fallbackImage = SVGColorImage.colorize(_baseUri(data.type))
+                .background(networkDragItem.colorContext.bg.secondary)
+                .color1(networkDragItem.colorContext.fg.primary)
+                .accent(networkDragItem.colorContext.accent)
+                .uri()
+
+            return {
+                artwork: data.artwork,
+                fallback: fallbackImage
+            }
         }
 
         onRequestData: (indexes, resolve, reject) => {
@@ -110,25 +133,6 @@ MainViewLoader {
             resolve(
                 model.getItemsForIndexes(indexes)
             )
-        }
-
-        Component {
-            id: custom_cover
-
-            NetworkCustomCover {
-                networkModel: model.indexData
-
-                width: networkDragItem.coverSize
-                height: networkDragItem.coverSize
-
-                // we can not change the size of cover and shodows from here,
-                // so for best visual use scale image to fit
-                fillMode: Image.PreserveAspectCrop
-
-                bgColor: networkDragItem.colorContext.bg.secondary
-                color1: networkDragItem.colorContext.fg.primary
-                accent: networkDragItem.colorContext.accent
-            }
         }
     }
 

modules/gui/qt/network/qml/NetworkThumbnailItem.qml
@@ -90,6 +90,8 @@ Widgets.TableRowDelegate {
                 rectHeight: artwork.paintedHeight
             }
 
+            //FIXME: implement fillMode in RoundImage and use MediaCover here instead
+            //or directly TableCollumns.titleHeaderDelegate in place of NetworkThumbnailItem
             NetworkCustomCover {
                 id: artwork
 

commit 45b36521edbac9d375e915ceda8ff29dda5cc416

qml: fix drag item no longer showing any image regression
Since 72f086c9, `RoundImage` has been used in `DragItem`.
However, it only works asynchronously and by the time
the input items are resolved, `RoundImage` instance(s)
are still in the progress of loading the image.

I added an intermediate state to wait for images to be
loaded. If the loading takes too much time, then the
timer is going to start the drag, so this change does
not make drag initalization synchronous.

modules/gui/qt/widgets/qml/DragItem.qml
@@ -248,6 +248,8 @@ Item {
         signal startDrag()
         signal stopDrag()
 
+        signal allImagesAreLoaded()
+
         //internal signals
         signal resolveData(var requestId, var indexes)
         signal resolveInputItems(var requestId, var indexes)
@@ -258,7 +260,8 @@ Item {
             stopDrag: stopDrag,
             resolveData: resolveData,
             resolveInputItems: resolveInputItems,
-            resolveFailed: resolveFailed
+            resolveFailed: resolveFailed,
+            allImagesAreLoaded: allImagesAreLoaded
         })
 
         initialState: fsmDragInactive
@@ -354,10 +357,29 @@ Item {
                         action: (requestId, items) => {
                             dragItem._setInputItems(items)
                         },
-                        target: fsmLoadingDone,
+                        target: fsmWaitingForImages,
                     },
                     resolveFailed: fsmLoadingFailed
                 })
+            }
+
+            FSMState {
+                id: fsmWaitingForImages
+
+                transitions: ({
+                    allImagesAreLoaded: {
+                        target: fsmLoadingDone
+                    }
+                })
+
+                function enter() {
+                    if (coverRepeater.notReadyCount === 0) {
+                        // By the time the state changes
+                        // the images might have been
+                        // already loaded:
+                        fsm.allImagesAreLoaded()
+                    }
+                }
             }
 
             FSMState {
@@ -405,6 +427,19 @@ Item {
 
         model: dragItem._covers
 
+        property int notReadyCount: count
+
+        onModelChanged: {
+            notReadyCount = count
+        }
+
+        onNotReadyCountChanged: {
+            if (notReadyCount === 0) {
+                // All the images are loaded, don't wait anymore
+                fsm.allImagesAreLoaded()
+            }
+        }
+
         Item {
             required property var modelData
             required property int index
@@ -437,18 +472,18 @@ Item {
                 radius: bg.radius
                 source: modelData.artwork ?? ""
                 sourceSize: dragItem.imageSourceSize ?? Qt.size(width, height)
-            }
 
-            Widgets.RoundImage {
-                id: fallbackCover
-
-                anchors.centerIn: parent
-                width: coverSize
-                height: coverSize
-                radius: bg.radius
-                source: modelData.fallback ?? defaultCover
-                sourceSize: dragItem.imageSourceSize ?? Qt.size(width, height)
-                visible: artworkCover.status !== Image.Ready
+                onStatusChanged: {
+                    if (status === Widgets.RoundImage.Ready)
+                        coverRepeater.notReadyCount -= 1
+                    else if (status === Widgets.RoundImage.Error) {
+                        const fallbackSource = modelData.fallback ?? defaultCover
+                        if (source === fallbackSource)
+                            coverRepeater.notReadyCount -= 1
+                        else
+                            source = fallbackSource
+                    }
+                }
             }
 
             Rectangle {