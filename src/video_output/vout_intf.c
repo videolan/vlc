c10f5f62bb449cffc5f3018f9c7d495d2bf469c7
Steve Lhomme <robux4@ycbcr.xyz>
2024-09-17 09:42:43 +0200

    vout_intf: check the ratio is not 0:0 or x:y
    We should not check the pointer is not NULL, this is never the case.
    
    Regression from ef2c9f7818409ab17b13f276109f6dff4bf09e3f.

--- src/video_output/vout_intf.c
+++ src/video_output/vout_intf.c
@@ -569,7 +569,7 @@ static int CropBorderCallback(vlc_object_t *object, char const *cmd,
 bool GetAspectRatio(const char *ar_str, unsigned *num, unsigned *den)
 {
     if (sscanf(ar_str, "%u:%u", num, den) == 2 &&
-        (num != 0) == (den != 0))
+        (*num != 0) == (*den != 0))
         return true;
     else if (*ar_str == '\0') {
         *num = VLC_DAR_FROM_SOURCE.num;

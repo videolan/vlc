videolan/vlc

commit ef2c9f7818409ab17b13f276109f6dff4bf09e3f

video_output: move the aspect-ratio string parsing in a separate function
We need to evaluate it on startup.

src/video_output/vout_intf.c
@@ -523,6 +523,16 @@ static int CropBorderCallback(vlc_object_t *object, char const *cmd,
     return VLC_SUCCESS;
 }
 
+static bool GetAspectRatio(const char *ar_str, unsigned *num, unsigned *den)
+{
+    if (sscanf(ar_str, "%u:%u", num, den) == 2 &&
+        (num != 0) == (den != 0))
+        return true;
+    else if (*ar_str == '\0')
+        *num = *den = 0;
+    return false;
+}
+
 static int AspectCallback( vlc_object_t *object, char const *cmd,
                          vlc_value_t oldval, vlc_value_t newval, void *data )
 {
@@ -530,11 +540,8 @@ static int AspectCallback( vlc_object_t *object, char const *cmd,
     VLC_UNUSED(cmd); VLC_UNUSED(oldval); VLC_UNUSED(data);
     unsigned num, den;
 
-    if (sscanf(newval.psz_string, "%u:%u", &num, &den) == 2 &&
-        (num != 0) == (den != 0))
+    if (GetAspectRatio(newval.psz_string, &num, &den))
         vout_ChangeDisplayAspectRatio(vout, num, den);
-    else if (*newval.psz_string == '\0')
-        vout_ChangeDisplayAspectRatio(vout, 0, 0);
     return VLC_SUCCESS;
 }
 

commit c10f5f62bb449cffc5f3018f9c7d495d2bf469c7

vout_intf: check the ratio is not 0:0 or x:y
We should not check the pointer is not NULL, this is never the case.

Regression from ef2c9f7818409ab17b13f276109f6dff4bf09e3f.

src/video_output/vout_intf.c
@@ -569,7 +569,7 @@ static int CropBorderCallback(vlc_object_t *object, char const *cmd,
 bool GetAspectRatio(const char *ar_str, unsigned *num, unsigned *den)
 {
     if (sscanf(ar_str, "%u:%u", num, den) == 2 &&
-        (num != 0) == (den != 0))
+        (*num != 0) == (*den != 0))
         return true;
     else if (*ar_str == '\0') {
         *num = VLC_DAR_FROM_SOURCE.num;