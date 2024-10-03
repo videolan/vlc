videolan/vlc

commit 3a6bd45b9e8d1f4c5e24b6031a8df44523e6c4b1

darwinvlc: start interfaces in separate thread
Interfaces from libvlc were started from the main thread directly. But
UI inserting their own event loop inside the main runloop like Qt were
taking over the interface insertion. It was preventing other interfaces
to load correctly, required dedicated code to handle the startup in the
Qt interface and it was not possible to exit the interface correctly.

By moving the creation of the interfaces in a dedicated thread, we can
ensure all UI are created and destroyed correctly, but the macosx
interface must now dispatch its own creation into the main thread to
match Cocoa requirement on main thread usage for UI components.

Note that libvlc_new itself must be moved to the intf_queue given that
it starts additional extraintf.

The macosx interface must now start its own NSApp inside the main loop,
via a dispatch call, which simplify the main loop into running the
default main loop.

modules/gui/macosx/main/VLCMain.m
@@ -141,36 +141,43 @@ intf_thread_t *getIntf()
 
 int OpenIntf (vlc_object_t *p_this)
 {
-    @autoreleasepool {
-        intf_thread_t *p_intf = (intf_thread_t*) p_this;
-        p_interface_thread = p_intf;
-        msg_Dbg(p_intf, "Starting macosx interface");
+    __block int retcode = VLC_SUCCESS;
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        @autoreleasepool {
+            intf_thread_t *p_intf = (intf_thread_t*) p_this;
+            p_interface_thread = p_intf;
+            msg_Dbg(p_intf, "Starting macosx interface");
 
-        @try {
-            VLCApplication * const application = VLCApplication.sharedApplication;
-            NSCAssert(application != nil, @"VLCApplication must not be nil");
+            @try {
+                VLCApplication * const application = VLCApplication.sharedApplication;
+                NSCAssert(application != nil, @"VLCApplication must not be nil");
 
-            VLCMain * const main = VLCMain.sharedInstance;
-            NSCAssert(main != nil, @"VLCMain must not be nil");
+                VLCMain * const main = VLCMain.sharedInstance;
+                NSCAssert(main != nil, @"VLCMain must not be nil");
 
-            msg_Dbg(p_intf, "Finished loading macosx interface");
-            return VLC_SUCCESS;
-        } @catch (NSException *exception) {
-            msg_Err(p_intf, "Loading the macosx interface failed. Do you have a valid window server?");
-            return VLC_EGENERIC;
+                msg_Dbg(p_intf, "Finished loading macosx interface");
+            } @catch (NSException *exception) {
+                msg_Err(p_intf, "Loading the macosx interface failed. Do you have a valid window server?");
+                retcode = VLC_EGENERIC;
+                return;
+            }
         }
-    }
+    });
+
+    return retcode;
 }
 
 void CloseIntf (vlc_object_t *p_this)
 {
-    @autoreleasepool {
-        msg_Dbg(p_this, "Closing macosx interface");
-        [VLCMain.sharedInstance applicationWillTerminate:nil];
-        [VLCMain killInstance];
-    }
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        @autoreleasepool {
+            msg_Dbg(p_this, "Closing macosx interface");
+            [VLCMain.sharedInstance applicationWillTerminate:nil];
+            [VLCMain killInstance];
+        }
 
-    p_interface_thread = nil;
+        p_interface_thread = nil;
+    });
 }
 
 /*****************************************************************************

bin/darwinvlc.m
@@ -267,22 +267,38 @@ int main(int i_argc, const char *ppsz_argv[])
     argc += i_argc;
     argv[argc] = NULL;
 
-    /* Initialize libvlc */
-    libvlc_instance_t *vlc = libvlc_new(argc, argv);
-    if (vlc == NULL)
-        return 1;
-
+    dispatch_queue_t intf_queue = dispatch_queue_create("org.videolan.vlc", NULL);
+    __block bool intf_started = true;
+    __block libvlc_instance_t *vlc = NULL;
     int ret = 1;
-    libvlc_SetExitHandler(vlc->p_libvlc_int, vlc_terminate, NULL);
-    libvlc_set_app_id(vlc, "org.VideoLAN.VLC", PACKAGE_VERSION, PACKAGE_NAME);
-    libvlc_set_user_agent(vlc, "VLC media player", "VLC/"PACKAGE_VERSION);
+    dispatch_async(intf_queue, ^{
+        /* Initialize libvlc */
+        vlc = libvlc_new(argc, argv);
+        if (vlc == NULL)
+        {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                intf_started = false;
+                CFRunLoopStop(CFRunLoopGetMain());
+            });
+            return;
+        }
 
-    if (libvlc_InternalAddIntf(vlc->p_libvlc_int, NULL)) {
-        fprintf(stderr, "VLC cannot start any interface. Exiting.\n");
-        goto out;
-    }
+        libvlc_SetExitHandler(vlc->p_libvlc_int, vlc_terminate, NULL);
+        libvlc_set_app_id(vlc, "org.VideoLAN.VLC", PACKAGE_VERSION, PACKAGE_NAME);
+        libvlc_set_user_agent(vlc, "VLC media player", "VLC/"PACKAGE_VERSION);
 
-    libvlc_InternalPlay(vlc->p_libvlc_int);
+
+        if (libvlc_InternalAddIntf(vlc->p_libvlc_int, NULL)) {
+            fprintf(stderr, "VLC cannot start any interface. Exiting.\n");
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                intf_started = false;
+                CFRunLoopStop(CFRunLoopGetMain());
+            });
+            return;
+        }
+
+        libvlc_InternalPlay(vlc->p_libvlc_int);
+    });
 
     /*
      * Run the main loop. If the mac interface is not initialized, only the CoreFoundation
@@ -290,17 +306,16 @@ int main(int i_argc, const char *ppsz_argv[])
      * before actually starting the loop.
      */
     @autoreleasepool {
-        if(NSApp == nil) {
-            CFRunLoopRun();
-
-        } else {
-            [NSApp run];
-        }
+        CFRunLoopRun();
     }
+
+    if (!intf_started)
+        goto out;
 
     ret = 0;
     /* Cleanup */
 out:
+    dispatch_release(intf_queue);
     free(argv);
 
     dispatch_release(sigIntSource);

commit feefb1d76e489fd2ec0cd2b008ee63bdd49963d8

macosx: VLCMain: use CFRunLoop to avoid servicing dispatch
The main dispatch_queue is a serial queue and cannot be re-entered, so
we cannot start the NSApp runloop there since it will block into the
callback while servicing the main queue, and thus no more blocks will
be able to be executed on this main dispatch_queue. It is visible with
the callstack there:

      thread #1, queue = 'com.apple.main-thread'
        frame #0: 0x0000000183c3e1f4 libsystem_kernel.dylib`mach_msg2_trap + 8
        frame #1: 0x0000000183c50b24 libsystem_kernel.dylib`mach_msg2_internal + 80
        frame #2: 0x0000000183c46e34 libsystem_kernel.dylib`mach_msg_overwrite + 476
        frame #3: 0x0000000183c3e578 libsystem_kernel.dylib`mach_msg + 24
        frame #4: 0x0000000183d5e680 CoreFoundation`__CFRunLoopServiceMachPort + 160
        frame #5: 0x0000000183d5cf44 CoreFoundation`__CFRunLoopRun + 1208
        frame #6: 0x0000000183d5c434 CoreFoundation`CFRunLoopRunSpecific + 608
        frame #7: 0x000000018e50019c HIToolbox`RunCurrentEventLoopInMode + 292
        frame #8: 0x000000018e4fffd8 HIToolbox`ReceiveNextEventCommon + 648
        frame #9: 0x000000018e4ffd30 HIToolbox`_BlockUntilNextEventMatchingListInModeWithFilter + 76
        frame #10: 0x00000001875bbd68 AppKit`_DPSNextEvent + 660
        frame #11: 0x0000000187db1808 AppKit`-[NSApplication(NSEventRouting) _nextEventMatchingEventMask:untilDate:inMode:dequeue:] + 700
        frame #12: 0x00000001875af09c AppKit`-[NSApplication run] + 476
        frame #13: 0x00000001030b791c libmacosx_plugin.dylib`__OpenIntf_block_invoke_2(.block_descriptor=<unavailable>) at VLCMain.m:166:17 [opt]
        frame #14: 0x0000000183acc750 libdispatch.dylib`_dispatch_call_block_and_release + 32
        frame #15: 0x0000000183ace3e8 libdispatch.dylib`_dispatch_client_callout + 20
        frame #16: 0x0000000183adcbb8 libdispatch.dylib`_dispatch_main_queue_drain + 988
        frame #17: 0x0000000183adc7cc libdispatch.dylib`_dispatch_main_queue_callback_4CF + 44
        frame #18: 0x0000000183d9fad4 CoreFoundation`__CFRUNLOOP_IS_SERVICING_THE_MAIN_DISPATCH_QUEUE__ + 16
        frame #19: 0x0000000183d5d258 CoreFoundation`__CFRunLoopRun + 1996
        frame #20: 0x0000000183d5c434 CoreFoundation`CFRunLoopRunSpecific + 608
        frame #21: 0x0000000183dda45c CoreFoundation`CFRunLoopRun + 64
        frame #22: 0x0000000100003084 vlc-osx-static`main(i_argc=1, ppsz_argv=0x000000016fdff0c8) at darwinvlc.m:309:9 [opt]
        frame #23: 0x00000001838f60e0 dyld`start + 2360

By running the CFRunLoop instead, we don't have this serial execution
constraint and can re-enter the loop while servicing the main dispatch
queue from inside the [NSApp run] runloop. It now has the following
callstack when running:

     thread #1, queue = 'com.apple.main-thread', stop reason = signal SIGSTOP
        frame #0: 0x0000000183c3e1f4 libsystem_kernel.dylib`mach_msg2_trap + 8
        frame #1: 0x0000000183c50b24 libsystem_kernel.dylib`mach_msg2_internal + 80
        frame #2: 0x0000000183c46e34 libsystem_kernel.dylib`mach_msg_overwrite + 476
        frame #3: 0x0000000183c3e578 libsystem_kernel.dylib`mach_msg + 24
        frame #4: 0x0000000183d5e680 CoreFoundation`__CFRunLoopServiceMachPort + 160
        frame #5: 0x0000000183d5cf44 CoreFoundation`__CFRunLoopRun + 1208
        frame #6: 0x0000000183d5c434 CoreFoundation`CFRunLoopRunSpecific + 608
        frame #7: 0x000000018e50019c HIToolbox`RunCurrentEventLoopInMode + 292
        frame #8: 0x000000018e4fffd8 HIToolbox`ReceiveNextEventCommon + 648
        frame #9: 0x000000018e4ffd30 HIToolbox`_BlockUntilNextEventMatchingListInModeWithFilter + 76
        frame #10: 0x00000001875bbd68 AppKit`_DPSNextEvent + 660
        frame #11: 0x0000000187db1808 AppKit`-[NSApplication(NSEventRouting) _nextEventMatchingEventMask:untilDate:inMode:dequeue:] + 700
        frame #12: 0x00000001875af09c AppKit`-[NSApplication run] + 476
        frame #13: 0x00000001030b76c8 libmacosx_plugin.dylib`__OpenIntf_block_invoke(.block_descriptor=0x00006000038813c0) at VLCMain.m:166:13 [opt]
        frame #14: 0x0000000183d5e070 CoreFoundation`__CFRUNLOOP_IS_CALLING_OUT_TO_A_BLOCK__ + 28
        frame #15: 0x0000000183d5df84 CoreFoundation`__CFRunLoopDoBlocks + 356
        frame #16: 0x0000000183d5d414 CoreFoundation`__CFRunLoopRun + 2440
        frame #17: 0x0000000183d5c434 CoreFoundation`CFRunLoopRunSpecific + 608
        frame #18: 0x0000000183dda45c CoreFoundation`CFRunLoopRun + 64
        frame #19: 0x0000000100003084 vlc-osx-static`main(i_argc=1, ppsz_argv=0x000000016fdff0c8) at darwinvlc.m:309:9 [opt]
        frame #20: 0x00000001838f60e0 dyld`start + 2360

And now dispatch will correctly work inside this.

Regression from 3a6bd45b9e8d1f4c5e24b6031a8df44523e6c4b1 which

modules/gui/macosx/main/VLCMain.m
@@ -142,7 +142,8 @@ intf_thread_t *getIntf()
 int OpenIntf (vlc_object_t *p_this)
 {
     __block int retcode = VLC_SUCCESS;
-    dispatch_sync(dispatch_get_main_queue(), ^{
+    __block dispatch_semaphore_t sem = dispatch_semaphore_create(0);
+    CFRunLoopPerformBlock(CFRunLoopGetMain(), kCFRunLoopDefaultMode, ^{
         @autoreleasepool {
             intf_thread_t *p_intf = (intf_thread_t*) p_this;
             p_interface_thread = p_intf;
@@ -161,11 +162,13 @@ int OpenIntf (vlc_object_t *p_this)
                 retcode = VLC_EGENERIC;
                 return;
             }
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [NSApp run];
-            });
+            dispatch_semaphore_signal(sem);
+            [NSApp run];
         }
     });
+    CFRunLoopWakeUp(CFRunLoopGetMain());
+
+    dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER);
 
     return retcode;
 }