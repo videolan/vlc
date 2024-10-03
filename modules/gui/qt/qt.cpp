videolan/vlc

commit 9cae3668c7fc1fc00939f0683c0c2293556a8d39

qt: setup startup for Darwin
Now that the interfaces are not run from the main thread directly, it's
possible to dispatch the QApplication startup and UI creation to the
main thread directly on Darwin. It matches the behaviour of the
interface on other platform that Darwin, where a thread is created to
handle the interface, except that the thread is already existing as the
main thread for Darwin.

It means that there won't need any specific code (except for Thread()
startup) to handle the specificities of the Qt interface startup on
Darwin anymore.

Ticket #27103 showed issues when leaving the interface with the
medialibrary, because the path releasing the medialibrary was not taken
correctly, but other issues could arise, including deadlocks.

Fixes #27103

modules/gui/qt/qt.cpp
@@ -536,10 +536,13 @@ static void *Thread( void * );
 static void *ThreadCleanup( qt_intf_t *p_intf, CleanupReason cleanupReason );
 
 #ifdef Q_OS_MAC
+static void ThreadDarwin(void *);
 /* Used to abort the app.exec() on OSX after libvlc_Quit is called */
 #include "../../../lib/libvlc_internal.h" /* libvlc_SetExitHandler */
+#include <dispatch/dispatch.h>
 static void Abort( void *obj )
 {
+    (void)obj;
     triggerQuit();
 }
 #endif
@@ -579,7 +582,6 @@ static int OpenInternal( qt_intf_t *p_intf )
     /* Get the playlist before the lock to avoid a lock-order-inversion */
     vlc_playlist_t *playlist = vlc_intf_GetMainPlaylist(p_intf->intf);
 
-#ifndef Q_OS_MAC
     vlc::threads::mutex_locker locker (lock);
     if (busy || open_state == OPEN_STATE_ERROR)
     {
@@ -587,7 +589,6 @@ static int OpenInternal( qt_intf_t *p_intf )
             msg_Err (p_intf, "cannot start Qt multiple times");
         return VLC_EGENERIC;
     }
-#endif
 
     p_intf->p_mi = NULL;
 
@@ -599,7 +600,7 @@ static int OpenInternal( qt_intf_t *p_intf )
 #ifdef Q_OS_MAC
     /* Run mainloop on the main thread as Cocoa requires */
     libvlc_SetExitHandler( vlc_object_instance(p_intf), Abort, p_intf );
-    Thread( (void *)p_intf );
+    dispatch_async_f(dispatch_get_main_queue(), static_cast<void*>(p_intf), ThreadDarwin);
 #else
     if( vlc_clone( &p_intf->thread, Thread, p_intf ) )
     {
@@ -610,10 +611,8 @@ static int OpenInternal( qt_intf_t *p_intf )
     /* Wait for the interface to be ready. This prevents the main
      * LibVLC thread from starting video playback before we can create
      * an embedded video window. */
-#ifndef Q_OS_MAC
     while (open_state == OPEN_STATE_INIT)
         wait_ready.wait(lock);
-#endif
 
     if (open_state == OPEN_STATE_ERROR)
     {
@@ -1053,15 +1052,6 @@ static void *Thread( void *obj )
         open_state = OPEN_STATE_OPENED;
         wait_ready.signal();
     }
-#ifdef Q_OS_MAC
-    /* We took over main thread, register and start here */
-    if( !p_intf->b_isDialogProvider )
-    {
-        vlc_playlist_Lock( p_intf->p_playlist );
-        vlc_playlist_Start( p_intf->p_playlist );
-        vlc_playlist_Unlock( p_intf->p_playlist );
-    }
-#endif
 
     /* Last settings */
     app.setQuitOnLastWindowClosed( false );
@@ -1078,18 +1068,21 @@ static void *Thread( void *obj )
     return ThreadCleanup( p_intf, CLEANUP_APP_TERMINATED );
 }
 
+#ifdef Q_OS_MAC
+static void ThreadDarwin(void *opaque)
+{
+    Thread(opaque);
+}
+#endif
+
 static void *ThreadCleanup( qt_intf_t *p_intf, CleanupReason cleanupReason )
 {
     {
-#ifndef Q_OS_MAC
         vlc::threads::mutex_locker locker (lock);
-#endif
         if( cleanupReason == CLEANUP_ERROR )
         {
             open_state = OPEN_STATE_ERROR;
-#ifndef Q_OS_MAC
             wait_ready.signal();
-#endif
         }
         else
             open_state = OPEN_STATE_INIT;

modules/gui/macosx/main/VLCMain.m
@@ -161,6 +161,9 @@ int OpenIntf (vlc_object_t *p_this)
                 retcode = VLC_EGENERIC;
                 return;
             }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [NSApp run];
+            });
         }
     });
 
@@ -169,15 +172,18 @@ int OpenIntf (vlc_object_t *p_this)
 
 void CloseIntf (vlc_object_t *p_this)
 {
-    dispatch_sync(dispatch_get_main_queue(), ^{
+    void (^release_intf)() = ^{
         @autoreleasepool {
             msg_Dbg(p_this, "Closing macosx interface");
             [VLCMain.sharedInstance applicationWillTerminate:nil];
             [VLCMain killInstance];
         }
-
         p_interface_thread = nil;
-    });
+    };
+    if (CFRunLoopGetCurrent() == CFRunLoopGetMain())
+        release_intf();
+    else
+        dispatch_sync(dispatch_get_main_queue(), release_intf);
 }
 
 /*****************************************************************************

commit 0b2a6f48bd062d9b77164e14d731aa79abed3ae1

qt: use CFRunLoop to avoid servicing dispatch
The main dispatch_queue is a serial queue and cannot be re-entered, so
we cannot start the QApplication there since it will block into
a callback while servicing the main queue, and thus no more blocks will
be able to be executed on this main dispatch_queue.

By running the CFRunLoop instead, we don't have this serial execution
constraint and can re-enter the loop while servicing the main dispatch
queue from inside the QApplication runloop.

Regression from 9cae3668c7fc1fc00939f0683c0c2293556a8d39 which
introduced the dispatch call.

modules/gui/qt/qt.cpp
@@ -536,10 +536,9 @@ static void *Thread( void * );
 static void *ThreadCleanup( qt_intf_t *p_intf, CleanupReason cleanupReason );
 
 #ifdef Q_OS_MAC
-static void ThreadDarwin(void *);
 /* Used to abort the app.exec() on OSX after libvlc_Quit is called */
 #include "../../../lib/libvlc_internal.h" /* libvlc_SetExitHandler */
-#include <dispatch/dispatch.h>
+#include <CoreFoundation/CFRunLoop.h>
 static void Abort( void *obj )
 {
     (void)obj;
@@ -600,7 +599,10 @@ static int OpenInternal( qt_intf_t *p_intf )
 #ifdef Q_OS_MAC
     /* Run mainloop on the main thread as Cocoa requires */
     libvlc_SetExitHandler( vlc_object_instance(p_intf), Abort, p_intf );
-    dispatch_async_f(dispatch_get_main_queue(), static_cast<void*>(p_intf), ThreadDarwin);
+    CFRunLoopPerformBlock(CFRunLoopGetMain(), kCFRunLoopDefaultMode, ^{
+        Thread(static_cast<void*>(p_intf));
+    });
+    CFRunLoopWakeUp(CFRunLoopGetMain());
 #else
     if( vlc_clone( &p_intf->thread, Thread, p_intf ) )
     {
@@ -1067,13 +1069,6 @@ static void *Thread( void *obj )
     msg_Dbg( p_intf, "QApp exec() finished" );
     return ThreadCleanup( p_intf, CLEANUP_APP_TERMINATED );
 }
-
-#ifdef Q_OS_MAC
-static void ThreadDarwin(void *opaque)
-{
-    Thread(opaque);
-}
-#endif
 
 static void *ThreadCleanup( qt_intf_t *p_intf, CleanupReason cleanupReason )
 {