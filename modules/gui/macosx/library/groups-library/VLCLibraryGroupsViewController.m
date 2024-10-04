d5c1e55d62bb4f6f910563c2fbff639566763fd6
Claudio Cambra <developer@claudiocambra.com>
2024-10-04 18:08:24 +0800

    macosx: Present a placeholder view in groups view controller when there are no groups
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/groups-library/VLCLibraryGroupsViewController.m
+++ modules/gui/macosx/library/groups-library/VLCLibraryGroupsViewController.m
@@ -22,6 +22,8 @@
 
 #import "VLCLibraryGroupsViewController.h"
 
+#import "extensions/NSString+Helpers.h"
+
 #import "library/VLCLibraryCollectionViewDelegate.h"
 #import "library/VLCLibraryCollectionViewFlowLayout.h"
 #import "library/VLCLibraryCollectionViewItem.h"
@@ -194,8 +196,35 @@
     return self.dataSource;
 }
 
+- (void)presentPlaceholderGroupsView
+{
+    NSArray<NSLayoutConstraint *> * const oldViewPlaceholderConstraints =
+        self.libraryWindow.librarySegmentViewController.placeholderImageViewSizeConstraints;
+    for (NSLayoutConstraint * const constraint in oldViewPlaceholderConstraints) {
+        constraint.active = NO;
+    }
+    for (NSLayoutConstraint *constraint in self.placeholderImageViewSizeConstraints) {
+        constraint.active = YES;
+    }
+
+    self.emptyLibraryView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.libraryTargetView.subviews = @[self.emptyLibraryView];
+    NSView * const emptyLibraryView = self.emptyLibraryView;
+    NSDictionary * const dict = NSDictionaryOfVariableBindings(emptyLibraryView);
+    [self.libraryTargetView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[emptyLibraryView(>=572.)]|" options:0 metrics:0 views:dict]];
+    [self.libraryTargetView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[emptyLibraryView(>=444.)]|" options:0 metrics:0 views:dict]];
+
+    self.placeholderImageView.image = [NSImage imageNamed:@"placeholder-video"];
+    self.placeholderLabel.stringValue = _NS("Your favorite groups will appear here.");
+}
+
 - (void)presentGroupsView
 {
+    if (self.dataSource.libraryModel.numberOfGroups == 0) {
+        [self presentPlaceholderGroupsView];
+        return;
+    }
+
     const VLCLibraryViewModeSegment viewModeSegment =
         VLCLibraryWindowPersistentPreferences.sharedInstance.groupsLibraryViewMode;
     NSView *viewToPresent = nil;
