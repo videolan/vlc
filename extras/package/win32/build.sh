7f7405c94dce140a885d41fc4bde2f1a1dbe178f
Steve Lhomme <robux4@ycbcr.xyz>
2024-10-09 13:50:06 +0200

    package/win32: don't export HAVE_WINSTORE
    The contribs and VLC have their own way of detecting/unabling winstore builds.

--- extras/package/win32/build.sh
+++ extras/package/win32/build.sh
@@ -345,10 +345,6 @@ fi
 if [ ! -z "$DISABLEGUI" ]; then
     CONTRIBFLAGS="$CONTRIBFLAGS --disable-qt --disable-qtsvg --disable-qtdeclarative --disable-qt5compat --disable-qtshadertools --disable-qtwayland"
 fi
-if [ ! -z "$WINSTORE" ]; then
-    # we don't use a special toolchain to trigger the detection in contribs so force it manually
-    export HAVE_WINSTORE=1
-fi
 
 if [ "$COMPILING_WITH_CLANG" -gt 0 ]; then
     # avoid using gcc-ar with the clang toolchain, if both are installed
