6f2516f080fee4c104a04cfbdd3d0c22c5d14edb
Claudio Cambra <developer@claudiocambra.com>
2024-10-14 14:07:35 +0800

    macosx: Remove now unused itemCount from sidebar child view controller protocol
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/VLCLibraryWindowSidebarChildViewController.h
+++ modules/gui/macosx/library/VLCLibraryWindowSidebarChildViewController.h
@@ -27,7 +27,6 @@ NS_ASSUME_NONNULL_BEGIN
 @protocol VLCLibraryWindowSidebarChildViewController <NSObject>
 
 @property (readonly) BOOL supportsItemCount;
-@property (readonly) NSUInteger itemCount;
 
 @optional
 
