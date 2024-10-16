f50ccb66081b02ce4c635c71e7650e04f161009f
Claudio Cambra <developer@claudiocambra.com>
2024-10-16 16:00:19 +0800

    macosx: Use VLCMainWindowControlsBar for controlsBar outlet in VLCLibraryWindow
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/VLCLibraryWindow.h
+++ modules/gui/macosx/library/VLCLibraryWindow.h
@@ -26,7 +26,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@class VLCControlsBarCommon;
+@class VLCMainWindowControlsBar;
 @class VLCDragDropView;
 @class VLCRoundedCornerTextField;
 @class VLCInputNodePathControl;
@@ -59,7 +59,7 @@ extern const NSUserInterfaceItemIdentifier VLCLibraryWindowIdentifier;
 
 @property (readonly) NSView *libraryTargetView;
 
-@property (nonatomic, weak) IBOutlet VLCControlsBarCommon* controlsBar;
+@property (nonatomic, weak) IBOutlet VLCMainWindowControlsBar *controlsBar;
 @property (readwrite, weak) IBOutlet NSLayoutConstraint *controlsBarHeightConstraint;
 @property (readwrite, weak) IBOutlet NSLayoutConstraint *videoViewBottomConstraint;
 @property (readwrite, weak) IBOutlet NSSegmentedControl *gridVsListSegmentedControl;
