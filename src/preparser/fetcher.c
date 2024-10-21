videolan/vlc

commit ed25f895756e8d1bca1c2327db82e71d6b046f08

fetcher: configure executor domain
In order to avoid creating executors (thread) for nothing.

src/preparser/fetcher.c
@@ -425,8 +425,10 @@ static void RunSearchNetwork(void *userdata)
     TaskDelete(task);
 }
 
-input_fetcher_t* input_fetcher_New( vlc_object_t* owner )
+input_fetcher_t* input_fetcher_New( vlc_object_t* owner,
+                                    input_item_meta_request_option_t request_type )
 {
+    assert(request_type & META_REQUEST_OPTION_FETCH_ANY);
     input_fetcher_t* fetcher = malloc( sizeof( *fetcher ) );
 
     if( unlikely( !fetcher ) )
@@ -436,28 +438,44 @@ input_fetcher_t* input_fetcher_New( vlc_object_t* owner )
     if (max_threads < 1)
         max_threads = 1;
 
-    fetcher->executor_local = vlc_executor_New(max_threads);
-    if (!fetcher->executor_local)
+    if (request_type & META_REQUEST_OPTION_FETCH_LOCAL)
     {
-        free(fetcher);
-        return NULL;
+        fetcher->executor_local = vlc_executor_New(max_threads);
+        if (!fetcher->executor_local)
+        {
+            free(fetcher);
+            return NULL;
+        }
     }
+    else
+        fetcher->executor_local = NULL;
 
-    fetcher->executor_network = vlc_executor_New(max_threads);
-    if (!fetcher->executor_network)
+    if (request_type & META_REQUEST_OPTION_FETCH_NETWORK)
     {
-        vlc_executor_Delete(fetcher->executor_local);
-        free(fetcher);
-        return NULL;
+        fetcher->executor_network = vlc_executor_New(max_threads);
+        if (!fetcher->executor_network)
+        {
+            if (fetcher->executor_local)
+                vlc_executor_Delete(fetcher->executor_local);
+            free(fetcher);
+            return NULL;
+        }
+
+        fetcher->executor_downloader = vlc_executor_New(max_threads);
+        if (!fetcher->executor_downloader)
+        {
+            if (fetcher->executor_network)
+                vlc_executor_Delete(fetcher->executor_network);
+            if (fetcher->executor_local)
+                vlc_executor_Delete(fetcher->executor_local);
+            free(fetcher);
+            return NULL;
+        }
     }
-
-    fetcher->executor_downloader = vlc_executor_New(max_threads);
-    if (!fetcher->executor_downloader)
+    else
     {
-        vlc_executor_Delete(fetcher->executor_network);
-        vlc_executor_Delete(fetcher->executor_local);
-        free(fetcher);
-        return NULL;
+        fetcher->executor_network = NULL;
+        fetcher->executor_downloader = NULL;
     }
 
     fetcher->owner = owner;
@@ -475,6 +493,10 @@ int input_fetcher_Push(input_fetcher_t* fetcher, input_item_t* item,
     const input_fetcher_callbacks_t *cbs, void *cbs_userdata)
 {
     assert(options & META_REQUEST_OPTION_FETCH_ANY);
+    if (options & META_REQUEST_OPTION_FETCH_LOCAL)
+        assert(fetcher->executor_local != NULL);
+    if (options & META_REQUEST_OPTION_FETCH_NETWORK)
+        assert(fetcher->executor_network != NULL);
 
     vlc_executor_t *executor = options & META_REQUEST_OPTION_FETCH_LOCAL
                              ? fetcher->executor_local
@@ -508,9 +530,12 @@ void input_fetcher_Delete( input_fetcher_t* fetcher )
 {
     CancelAllTasks(fetcher);
 
-    vlc_executor_Delete(fetcher->executor_local);
-    vlc_executor_Delete(fetcher->executor_network);
-    vlc_executor_Delete(fetcher->executor_downloader);
+    if (fetcher->executor_local)
+        vlc_executor_Delete(fetcher->executor_local);
+    if (fetcher->executor_network)
+        vlc_executor_Delete(fetcher->executor_network);
+    if (fetcher->executor_downloader)
+        vlc_executor_Delete(fetcher->executor_downloader);
 
     vlc_dictionary_clear( &fetcher->album_cache, FreeCacheEntry, NULL );
     free( fetcher );

src/preparser/fetcher.h
@@ -41,8 +41,13 @@ typedef struct input_fetcher_callbacks_t {
 
 /**
  * This function creates the fetcher object and thread.
+ *
+ * @param request_type a combination of META_REQUEST_OPTION_FETCH_LOCAL and
+ * META_REQUEST_OPTION_FETCH_NETWORK, it is used to setup the executors for
+ * each domain.
  */
-input_fetcher_t *input_fetcher_New( vlc_object_t * );
+input_fetcher_t *input_fetcher_New( vlc_object_t *,
+                                    input_item_meta_request_option_t request_type );
 
 /**
  * This function enqueues the provided item to be art fetched.

src/preparser/preparser.c
@@ -327,7 +327,7 @@ vlc_preparser_t* vlc_preparser_New( vlc_object_t *parent, unsigned max_threads,
     preparser->default_timeout = default_timeout;
 
     preparser->owner = parent;
-    preparser->fetcher = input_fetcher_New( parent );
+    preparser->fetcher = input_fetcher_New( parent, META_REQUEST_OPTION_FETCH_ANY );
     atomic_init( &preparser->deactivated, false );
 
     vlc_mutex_init(&preparser->lock);

commit c59ee89f38602407a3ee74f0fb3b90289f709501

fetcher: always create a downloader thread
It can be finally used by local fetchers.

Regression from ed25f895756e8d1bca1c2327db82e71d6b046f08

 #0  0x00007f49abda59c9 in vlc_mutex_trylock (mtx=mtx@entry=0x0) at misc/threads.c:122
 #1  0x00007f49abda5a1c in vlc_mutex_lock (mtx=mtx@entry=0x0) at misc/threads.c:103
 #2  0x00007f49abd9c3b1 in vlc_executor_Submit (executor=0x0, runnable=runnable@entry=0x7f498800fd28) at misc/executor.c:212
 #3  0x00007f49abd27de7 in Submit (fetcher=0x55a9fc371820, executor=<optimized out>, item=0x55a9fc41c0a0, options=META_REQUEST_OPTION_FETCH_LOCAL, cbs=0x7f49abe0c2b8 <input_fetcher_callbacks>, userdata=0x55a9fc40ac60) at preparser/fetcher.c:142
 #4  0x00007f49abd28ab0 in SearchByScope (task=task@entry=0x7f498c004b30, scope=scope@entry=1) at preparser/fetcher.c:287
 #5  0x00007f49abd28c7f in RunSearchLocal (userdata=0x7f498c004b30) at preparser/fetcher.c:383
 #6  0x00007f49abd9c296 in ThreadRun (userdata=0x55a9fc3fd8e0) at misc/executor.c:134
 #7  0x00007f49abaa760c in start_thread (arg=<optimized out>) at pthread_create.c:444
 #8  0x00007f49abb2eaa8 in clone3 () at ../sysdeps/unix/sysv/linux/x86_64/clone3.S:78

Fixes #28832

src/preparser/fetcher.c
@@ -94,10 +94,7 @@ TaskNew(input_fetcher_t *fetcher, vlc_executor_t *executor, input_item_t *item,
     else if (executor == fetcher->executor_network)
         task->runnable.run = RunSearchNetwork;
     else
-    {
-        assert(executor == fetcher->executor_downloader);
         task->runnable.run = RunDownloader;
-    }
 
     task->runnable.userdata = task;
 
@@ -460,21 +457,19 @@ input_fetcher_t* input_fetcher_New( vlc_object_t* owner,
             return NULL;
         }
 
-        fetcher->executor_downloader = vlc_executor_New(max_threads);
-        if (!fetcher->executor_downloader)
-        {
-            if (fetcher->executor_network)
-                vlc_executor_Delete(fetcher->executor_network);
-            if (fetcher->executor_local)
-                vlc_executor_Delete(fetcher->executor_local);
-            free(fetcher);
-            return NULL;
-        }
     }
     else
-    {
         fetcher->executor_network = NULL;
-        fetcher->executor_downloader = NULL;
+
+    fetcher->executor_downloader = vlc_executor_New(max_threads);
+    if (!fetcher->executor_downloader)
+    {
+        if (fetcher->executor_network)
+            vlc_executor_Delete(fetcher->executor_network);
+        if (fetcher->executor_local)
+            vlc_executor_Delete(fetcher->executor_local);
+        free(fetcher);
+        return NULL;
     }
 
     fetcher->owner = owner;
@@ -533,8 +528,8 @@ void input_fetcher_Delete( input_fetcher_t* fetcher )
         vlc_executor_Delete(fetcher->executor_local);
     if (fetcher->executor_network)
         vlc_executor_Delete(fetcher->executor_network);
-    if (fetcher->executor_downloader)
-        vlc_executor_Delete(fetcher->executor_downloader);
+    assert(fetcher->executor_downloader);
+    vlc_executor_Delete(fetcher->executor_downloader);
 
     vlc_dictionary_clear( &fetcher->album_cache, FreeCacheEntry, NULL );
     free( fetcher );