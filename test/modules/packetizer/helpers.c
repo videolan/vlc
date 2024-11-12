90f06dc9f297deb39260e1659fafaadc268582d2
Steve Lhomme <robux4@ycbcr.xyz>
2024-11-12 09:37:54 +0100

    packetizer: fix bogus format for pointer difference
    Use the same cast as below.
    
    Co-authored-by: Alexandre Janniaux <ajanni@videolabs.io>

--- test/modules/packetizer/helpers.c
+++ test/modules/packetizer/helpers.c
@@ -49,7 +49,7 @@ static int check_set( const uint8_t *p_set, const uint8_t *p_end,
         p = pf_find( p, p_end );
         if( p == NULL )
             break;
-        printf("- entry %zu offset %ld\n", i_entry, p - p_set);
+        printf("- entry %zu offset %td\n", i_entry, p - p_set);
         if( i_entry == i_results )
             break;
         if( p_results[i_entry].offset + i_results_offset != (size_t) (p - p_set) )
