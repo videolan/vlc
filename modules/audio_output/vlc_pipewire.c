bef045810fe4f0333eaae399261db5e635ba8ebb
Rémi Denis-Courmont <remi@remlab.net>
2024-11-26 19:23:10 +0200

    pipewire: relicense

--- modules/audio_output/vlc_pipewire.c
+++ modules/audio_output/vlc_pipewire.c
@@ -4,8 +4,8 @@
  * Copyright (C) 2022 Rémi Denis-Courmont
  *
  * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,7 +13,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
