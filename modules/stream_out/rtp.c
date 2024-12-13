370bddff29eb1b83f7bd76026566c91a3e27ec2b
Tristan Matthews <tmatth@videolan.org>
2024-12-13 14:56:40 -0500

    sout: rtp: bail if xiph config packet failed
    This can happen on running out of memory, failed parsing or a NULL fmtp.

--- modules/stream_out/rtp.c
+++ modules/stream_out/rtp.c
@@ -1223,8 +1223,10 @@ static int Send( sout_stream_t *p_stream, void *_id, block_t *p_buffer )
             id->b_first_packet = false;
             if (!strcmp(id->rtp_fmt.ptname, "vorbis") ||
                 !strcmp(id->rtp_fmt.ptname, "theora"))
-                rtp_packetize_xiph_config(id, id->rtp_fmt.fmtp,
-                                          p_buffer->i_pts);
+            {
+                if (rtp_packetize_xiph_config(id, id->rtp_fmt.fmtp, p_buffer->i_pts))
+                    break;
+            }
         }
 
         if( id->rtp_fmt.pf_packetize( id, p_buffer ) )
