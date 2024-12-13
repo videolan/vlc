7309e0fd8551a8d3d824f77f526829b7a7af4a65
Tristan Matthews <tmatth@videolan.org>
2024-12-13 15:57:10 -0500

    sout: rtp: amr: bail if block_Alloc failed

--- modules/stream_out/rtpfmt.c
+++ modules/stream_out/rtpfmt.c
@@ -1552,6 +1552,11 @@ static int rtp_packetize_amr( sout_stream_id_sys_t *id, block_t *in )
     {
         int           i_payload = __MIN( i_max, i_data );
         block_t *out = block_Alloc( 14 + i_payload );
+        if (unlikely(out == NULL))
+        {
+            block_Release(in);
+            return VLC_ENOMEM;
+        }
 
         /* rtp common header */
         rtp_packetize_common( id, out, ((i == i_count - 1)?1:0),
