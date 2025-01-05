videolan/vlc

commit b1e1164154c80a1da9c30bf2a50ebc2b2c5e6ad1

qt: apply blur behind effect when the target window is activated
Currently, the effect is applied before the window is shown. Frankly,
there is no restriction from the compositors side (DWM, KWin) regarding
when the backdrop blur effect can be activated, and it was a courtesy
for the compositors to request the effect even before the window is shown
so that they could do the job potentially better.

For us, it does not matter much when the blur is applied. However,
with KWin Wayland it does not seem to be possible to use effects before
the window is shown. It is not clear if this is a KWin regression, but
since it does not matter, the blur can be requested when the window is
activated.

modules/gui/qt/maininterface/compositor_x11.cpp
@@ -209,7 +209,7 @@ bool CompositorX11::makeMainInterface(MainCtx* mainCtx)
 
     m_qmlView->show();
 
-    if (m_blurBehind)
+    if (m_mainCtx->hasAcrylicSurface())
         m_renderWindow->m_hasAcrylic = true;
 
     m_renderWindow->setInterfaceWindow(m_qmlView.get());

modules/gui/qt/maininterface/compositor.cpp
@@ -272,7 +272,16 @@ bool CompositorVideo::commonGUICreateImpl(QWindow* window, CompositorVideo::Flag
     }
     if (!backendIsOpenVg && (flags & CompositorVideo::HAS_ACRYLIC))
     {
-        setBlurBehind(window, true);
+        if (Q_LIKELY(!window->isActive()))
+        {
+            connect(window, &QWindow::activeChanged, this, [this, window = QPointer(window)]() {
+               setBlurBehind(window, true);
+            }, Qt::SingleShotConnection);
+        }
+        else
+        {
+            setBlurBehind(window, true);
+        }
     }
     m_videoWindowHandler = std::make_unique<VideoWindowHandler>(m_intf);
     m_videoWindowHandler->setWindow( window );
@@ -282,7 +291,6 @@ bool CompositorVideo::commonGUICreateImpl(QWindow* window, CompositorVideo::Flag
 #else
     m_interfaceWindowHandler = std::make_unique<InterfaceWindowHandler>(m_intf, m_mainCtx, window);
 #endif
-    m_mainCtx->setHasAcrylicSurface(m_blurBehind);
     m_mainCtx->setWindowSuportExtendedFrame(flags & CompositorVideo::HAS_EXTENDED_FRAME);
 
 #ifdef _WIN32
@@ -376,6 +384,6 @@ bool CompositorVideo::setBlurBehind(QWindow *window, const bool enable)
         return false;
 
     m_windowEffectsModule->setBlurBehind(window, enable);
-    m_blurBehind = enable;
+    m_mainCtx->setHasAcrylicSurface(enable);
     return true;
 }

modules/gui/qt/maininterface/compositor.hpp
@@ -187,7 +187,6 @@ protected:
     std::unique_ptr<WinTaskbarWidget> m_taskbarWidget;
 #endif
 
-    bool m_blurBehind = false;
     WindowEffectsModule* m_windowEffectsModule = nullptr;
     bool m_failedToLoadWindowEffectsModule = false;
 };

modules/gui/qt/maininterface/compositor_dcomp.cpp
@@ -213,7 +213,7 @@ void CompositorDirectComposition::setup()
 
     m_dcompDevice->Commit();
 
-    if (!m_blurBehind)
+    if (!m_mainCtx->hasAcrylicSurface())
     {
         if (var_InheritBool(m_intf, "qt-backdrop-blur"))
         {

commit 8ca8509b30c7bcd0ddaecc6f62e2b7770d88bead

qt: restore window backdrop blur effect in CompositorX11
CompositorX11, unlike others, requires explicit code to
adjust backdrop blur. When the effect gets activated at
a later time after initialization, it currently does not
have code to do the required adjustment.

This is a regression since b1e11641. I did not notice
it because I was using extended frame with CSD, where
this regression is masked.

modules/gui/qt/maininterface/compositor_x11.cpp
@@ -196,6 +196,8 @@ bool CompositorX11::makeMainInterface(MainCtx* mainCtx)
     setTransparentForMouseEvent(connection, m_videoWidget->winId());
     m_videoWidget->show();
 
+    connect(m_mainCtx, &MainCtx::hasAcrylicSurfaceChanged, m_renderWindow.get(), &CompositorX11RenderWindow::setAcrylic);
+
     m_qmlView = std::make_unique<CompositorX11UISurface>(m_renderWindow.get());
 
     m_qmlView->setFlag(Qt::WindowType::WindowTransparentForInput);

modules/gui/qt/maininterface/compositor_x11_renderwindow.cpp
@@ -420,6 +420,7 @@ bool CompositorX11RenderWindow::startRendering()
     connect(this, &CompositorX11RenderWindow::registerVideoWindow, m_renderTask, &RenderTask::onRegisterVideoWindow);
     connect(this, &CompositorX11RenderWindow::videoSurfaceChanged, m_renderTask, &RenderTask::onVideoSurfaceChanged, Qt::BlockingQueuedConnection);
     connect(this, &CompositorX11RenderWindow::hasExtendedFrameChanged, m_renderTask, &RenderTask::onExtendedFrameChanged, Qt::BlockingQueuedConnection);
+    connect(this, &CompositorX11RenderWindow::acrylicChanged, m_renderTask, &RenderTask::onAcrylicChanged);
     //pass initial values
     m_renderTask->onInterfaceSurfaceChanged(m_interfaceClient.get());
     m_renderTask->onVideoSurfaceChanged(m_videoClient.get());
@@ -525,6 +526,16 @@ void CompositorX11RenderWindow::setVideoSize(const QSize& size)
         m_videoPosition.setSize(size * devicePixelRatio());
         emit videoPositionChanged(m_videoPosition);
     }
+}
+
+void CompositorX11RenderWindow::setAcrylic(bool value)
+{
+    if (m_hasAcrylic == value)
+        return;
+
+    m_hasAcrylic = value;
+
+    emit acrylicChanged(value);
 }
 
 void CompositorX11RenderWindow::setVideoWindow( QWindow* window)

modules/gui/qt/maininterface/compositor_x11_renderwindow.hpp
@@ -167,6 +167,7 @@ public:
     void setVideoSize(const QSize& size);
 
     inline bool hasAcrylic() const { return m_hasAcrylic; }
+    void setAcrylic(bool value);
     inline bool supportExtendedFrame() const { return m_supportExtendedFrame; }
 
     void setVideoWindow(QWindow* window);
@@ -185,6 +186,7 @@ signals:
     void visiblityChanged(bool visible);
     void registerVideoWindow(unsigned int xid);
     bool hasExtendedFrameChanged(bool hasExtendedFrame);
+    void acrylicChanged(bool value);
 
 protected:
     //override from QWindow

modules/gui/qt/maininterface/mainctx.cpp
@@ -541,7 +541,7 @@ void MainCtx::setHasAcrylicSurface(const bool v)
         return;
 
     m_hasAcrylicSurface = v;
-    emit hasAcrylicSurfaceChanged();
+    emit hasAcrylicSurfaceChanged(v);
 }
 
 void MainCtx::incrementIntfUserScaleFactor(bool increment)

modules/gui/qt/maininterface/mainctx.hpp
@@ -482,7 +482,7 @@ signals:
     void pinVideoControlsChanged();
     void pinOpacityChanged();
 
-    void hasAcrylicSurfaceChanged();
+    void hasAcrylicSurfaceChanged(bool);
 
     void minimalViewChanged();
 