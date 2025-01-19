18fbe6216c4f88c557e674b4e931b43886d01ba8
Claudio Cambra <developer@claudiocambra.com>
2025-01-19 16:42:14 +0800

    macosx: Improve options getter in input item
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/VLCInputItem.m
+++ modules/gui/macosx/library/VLCInputItem.m
@@ -637,10 +637,10 @@ static const struct input_item_parser_cbs_t parserCallbacks =
 
 - (NSArray<NSString *> *)options
 {
-    NSMutableArray * const options = NSMutableArray.array;
     const int i_options = _vlcInputItem->i_options;
+    NSMutableArray * const options = [NSMutableArray arrayWithCapacity:i_options];
     for (NSUInteger i = 0; i < i_options; ++i) {
-        const char *psz_option = _vlcInputItem->ppsz_options[i];
+        const char * const psz_option = _vlcInputItem->ppsz_options[i];
         NSString * const option = NSTR(psz_option);
         [options addObject:option];
     }
