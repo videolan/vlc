7813d93f155fb8f7bde42d275201744da3e264d8
Steve Lhomme <robux4@ycbcr.xyz>
2025-01-21 12:46:27 +0100

    bluray: remove bogus comment
    We don't use vlc_spu_highlight_t but we need vlc_subpicture.h.

--- modules/access/bluray.c
+++ modules/access/bluray.c
@@ -51,7 +51,7 @@
 #include <vlc_url.h>                        /* vlc_path2uri */
 #include <vlc_iso_lang.h>
 #include <vlc_fs.h>
-#include <vlc_subpicture.h> // vlc_spu_highlight_t
+#include <vlc_subpicture.h>
 
 #include "../demux/mpeg/timestamps.h"
 #include "../demux/timestamps_filter.h"
