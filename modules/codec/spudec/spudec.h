36d98bfba1ff42d1d9cd76a251f6fd4f63da169c
Steve Lhomme <robux4@ycbcr.xyz>
2025-01-22 11:41:02 +0100

    spudec: allocate the SPU packet buffer dynamically
    (cherry picked from commit 3ad009527922f4ebd21c6ac38ee97cb1d5a191c1) (edited)
    edited:
    - VLC 3 doesn't have vlc_frame_ChainProperties()

--- modules/codec/spudec/spudec.h
+++ modules/codec/spudec/spudec.h
@@ -35,8 +35,8 @@ struct decoder_sys_t
 
     block_t *p_block;
 
-    /* We will never overflow */
-    uint8_t buffer[65536];
+    uint8_t *buffer;
+    size_t  buffer_size;
 };
 
 /*****************************************************************************
