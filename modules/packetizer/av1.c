videolan/vlc

commit a95213e20de69d584324f6d0c9f37c7a7cdfe3e3

packetizer/av1: fill the full extradata if it doesn't have the optional OBU
The optional part needed by the decoders.
This will restart the decoders if needed, but they should have received no frames yet
or the hardware decoder may be usable again or not (after a sequence header change).

modules/packetizer/av1.c
@@ -161,8 +161,9 @@ static void UpdateDecoderFormat(decoder_t *p_dec)
         p_dec->fmt_out.i_extra = 0;
     }
 
-    if(!p_dec->fmt_in->i_extra && !p_dec->fmt_out.i_extra)
+    if(p_dec->fmt_out.i_extra <= 4)
     {
+        free(p_dec->fmt_out.p_extra);
         p_dec->fmt_out.i_extra =
                 AV1_create_DecoderConfigurationRecord((uint8_t **)&p_dec->fmt_out.p_extra,
                                                       p_sys->p_sequence_header,

commit fc0325dbe13bb3d7a2fb0438beeb658897e01044

packetizer: av1: fix double free
If the p_extra is freed in the previous branch, it is also freed in this
branch without being reset to NULL before.

Regression from a95213e20de69d584324f6d0c9f37c7a7cdfe3e3.

modules/packetizer/av1.c
@@ -159,6 +159,7 @@ static void UpdateDecoderFormat(decoder_t *p_dec)
     {
         free(p_dec->fmt_out.p_extra);
         p_dec->fmt_out.i_extra = 0;
+        p_dec->fmt_out.p_extra = NULL;
     }
 
     if(p_dec->fmt_out.i_extra <= 4)