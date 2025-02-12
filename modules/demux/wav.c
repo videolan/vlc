96a6e495e82413737edd5ef3e5b5d4cfc8ef2325
Steve Lhomme <robux4@ycbcr.xyz>
2025-02-12 07:59:27 +0100

    demux: wav: reject ADPCM with negative number of samples
    Fixes #28978

--- modules/demux/wav.c
+++ modules/demux/wav.c
@@ -592,6 +592,9 @@ static int FrameInfo_MS_ADPCM( unsigned int *pi_size, int *pi_samples,
     if( p_fmt->audio.i_channels == 0 )
         return VLC_EGENERIC;
 
+    if( p_fmt->audio.i_blockalign < 7 * p_fmt->audio.i_channels )
+        return VLC_EGENERIC;
+
     *pi_samples = 2 + 2 * ( p_fmt->audio.i_blockalign -
         7 * p_fmt->audio.i_channels ) / p_fmt->audio.i_channels;
     *pi_size = p_fmt->audio.i_blockalign;
