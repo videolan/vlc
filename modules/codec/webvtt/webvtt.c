81b2dc3dc22c1eace7eddeb8e407904b50374439
Steve Lhomme <robux4@ycbcr.xyz>
2025-02-13 08:19:56 +0100

    codec: webvtt: parse timestamps as signed values
    sscanf("-1:00:27.500", "%u:%2u:%2u.%3u") returns a large for the negative value.
    This happens on Windows and in Linux too [^1].
    
    The returned timestamp is negative (matching what the text says).
    That bogus timestamps is then discarded as invalid.
    
    Fixes #28994
    
    [^1]: https://godbolt.org/z/YWEYzPeGq

--- modules/codec/webvtt/webvtt.c
+++ modules/codec/webvtt/webvtt.c
@@ -80,7 +80,7 @@ struct webvtt_text_parser_t
     webvtt_cue_t *p_cue;
 };
 
-static vlc_tick_t MakeTime( unsigned t[4] )
+static vlc_tick_t MakeTime( int t[4] )
 {
     return t[0] * 3600 * CLOCK_FREQ +
            t[1] * 60 * CLOCK_FREQ +
@@ -90,15 +90,15 @@ static vlc_tick_t MakeTime( unsigned t[4] )
 
 bool webvtt_scan_time( const char *psz, vlc_tick_t *p_time )
 {
-    unsigned t[4];
-    if( sscanf( psz, "%2u:%2u.%3u",
+    int t[4];
+    if( sscanf( psz, "%2d:%2d.%3d",
                       &t[1], &t[2], &t[3] ) == 3 )
     {
         t[0] = 0;
         *p_time = MakeTime( t );
         return true;
     }
-    else if( sscanf( psz, "%u:%2u:%2u.%3u",
+    else if( sscanf( psz, "%d:%2d:%2d.%3d",
                           &t[0], &t[1], &t[2], &t[3] ) == 4 )
     {
         *p_time = MakeTime( t );
