173ca7ee23db4bf570045fde1f17366fb2b70c0b
Alaric Senat <dev.asenat@posteo.net>
2025-03-11 00:10:08 +0100

    vlcrs-core: tracer: redefine license headers as comments
    These should not be considered as documentation, in some cases, it
    messes with the formatter and possibly rust-doc.

--- src/rust/vlcrs-core/src/tracer/mod.rs
+++ src/rust/vlcrs-core/src/tracer/mod.rs
@@ -1,19 +1,20 @@
-/// SPDX-License-Identifier: LGPL-2.1-or-later
-/// Copyright (C) 2024-2025 Alexandre Janniaux <ajanni@videolabs.io>
-///
-/// This program is free software; you can redistribute it and/or modify it
-/// under the terms of the GNU Lesser General Public License as published by
-/// the Free Software Foundation; either version 2.1 of the License, or
-/// (at your option) any later version.
-///
-/// This program is distributed in the hope that it will be useful,
-/// but WITHOUT ANY WARRANTY; without even the implied warranty of
-/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-/// GNU Lesser General Public License for more details.
-///
-/// You should have received a copy of the GNU Lesser General Public License
-/// along with this program; if not, write to the Free Software Foundation,
-/// Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright (C) 2024-2025 Alexandre Janniaux <ajanni@videolabs.io>
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation; either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+
 use std::{
     cell::UnsafeCell,
     ffi::{c_char, c_void, CStr},
