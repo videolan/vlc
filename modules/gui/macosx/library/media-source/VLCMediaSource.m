de4c97c7ea593c91c2775efaadab3b42f0298fe9
Claudio Cambra <developer@claudiocambra.com>
2025-03-12 00:56:42 +0800

    macosx: Do not clear child nodes before calling generateChildNodesForDirectoryNode
    generateChildNodesForDirectoryNode already does this
    
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/media-source/VLCMediaSource.m
+++ modules/gui/macosx/library/media-source/VLCMediaSource.m
@@ -311,7 +311,6 @@ static const char *const myFoldersDescription = "My Folders";
         input_item_node_t *vlcInputNode = inputNode.vlcInputItemNode;
         NSURL *dirUrl = [NSURL URLWithString:inputNode.inputItem.MRL];
 
-        [self clearChildNodesForNode:vlcInputNode]; // Clear existing nodes, refresh
         [self generateChildNodesForDirectoryNode:vlcInputNode withUrl:dirUrl];
         return;
     }
