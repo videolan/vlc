videolan/vlc

commit 44879313bab5b7208b12cb4968cf03a6f521e531

codec/demux: ttml: handle non-default namespaces

modules/codec/ttml/ttml.c
@@ -177,11 +177,22 @@ bool tt_node_Match( const tt_node_t *p_node, const char *psz_name, const char *p
     return !!psz_namespace == !!psz_nodens;
 }
 
-int tt_node_NameCompare( const char* psz_tagname, const char* psz_pattern )
+const char * tt_node_GetAttribute( tt_namespaces_t *p_nss, const tt_node_t *p_node,
+                                   const char *psz_name, const char *psz_namespace )
 {
-    if( !strncasecmp( "tt:", psz_tagname, 3 ) )
-        psz_tagname += 3;
-    return strcasecmp( psz_tagname, psz_pattern );
+    const void *value;
+    char *alloc = NULL;
+    if( psz_namespace )
+    {
+        const char *psz_prefix = tt_namespaces_GetPrefix( p_nss, psz_namespace );
+        if( psz_prefix == NULL ||
+            asprintf( &alloc, "%s:%s", psz_prefix, psz_name ) < 1 )
+            return NULL;
+        psz_name = alloc;
+    }
+    value = vlc_dictionary_value_for_key( &p_node->attr_dict, psz_name );
+    free( alloc );
+    return value != kVLCDictionaryNotFound ? (const char *)value : NULL;
 }
 
 bool tt_node_HasChild( const tt_node_t *p_node )
@@ -385,7 +396,8 @@ tt_node_t * tt_node_New( tt_node_t* p_parent,
     return p_node;
 }
 
-tt_node_t * tt_node_NewRead( xml_reader_t* reader, tt_node_t* p_parent,
+tt_node_t * tt_node_NewRead( xml_reader_t* reader,
+                             tt_namespaces_t *p_nss, tt_node_t* p_parent,
                              const char* psz_node_name, const char *psz_namespace )
 {
     tt_node_t *p_node = tt_node_New( p_parent, psz_node_name, psz_namespace );
@@ -397,7 +409,8 @@ tt_node_t * tt_node_NewRead( xml_reader_t* reader, tt_node_t* p_parent,
          psz_key != NULL;
          psz_key = xml_ReaderNextAttrNS( reader, &psz_value, &psz_ns ) )
     {
-        fprintf(stderr,"ATTR %s %s %s\n", psz_key, psz_value, psz_ns);
+        if( psz_ns && psz_key )
+        tt_namespaces_Register( p_nss, psz_key, psz_ns );
         char *psz_val = strdup( psz_value );
         if( psz_val )
         {
@@ -474,7 +487,7 @@ static int tt_node_Skip( xml_reader_t *p_reader, const char *psz_skipped )
     return VLC_EGENERIC;
 }
 #endif
-int tt_nodes_Read( xml_reader_t *p_reader, tt_node_t *p_root_node )
+int tt_nodes_Read( xml_reader_t *p_reader, tt_namespaces_t *p_nss, tt_node_t *p_root_node )
 {
     size_t i_depth = 0;
     tt_node_t *p_node = p_root_node;
@@ -496,7 +509,9 @@ int tt_nodes_Read( xml_reader_t *p_reader, tt_node_t *p_root_node )
 
             case XML_READER_STARTELEM:
             {
-                tt_node_t *p_newnode = tt_node_NewRead( p_reader, p_node, psz_node_name,
+                tt_namespaces_Register( p_nss, psz_node_name, psz_node_namespace );
+                tt_node_t *p_newnode = tt_node_NewRead( p_reader, p_nss, p_node,
+                                                        psz_node_name,
                                                         psz_node_namespace );
                 if( !p_newnode )
                     return VLC_EGENERIC;
@@ -517,7 +532,7 @@ int tt_nodes_Read( xml_reader_t *p_reader, tt_node_t *p_root_node )
 
             case XML_READER_ENDELEM:
             {
-                if( strcmp( psz_node_name, p_node->psz_node_name ) )
+                if( !tt_node_Match( p_node, psz_node_name, psz_node_namespace ) )
                     return VLC_EGENERIC;
 
                 if( i_depth == 0 )

modules/demux/ttml.c
@@ -51,6 +51,7 @@ typedef struct
     bool            b_first_time;
 
     tt_node_t         *p_rootnode;
+    tt_namespaces_t   namespaces;
 
     tt_timings_t    temporal_extent;
 
@@ -157,11 +158,11 @@ static int Control( demux_t* p_demux, int i_query, va_list args )
 static int ReadTTML( demux_t* p_demux )
 {
     demux_sys_t* p_sys = p_demux->p_sys;
-    const char* psz_node_name, *psz_namespace;
+    const char* psz_node_name, *psz_node_namespace;
 
     do
     {
-        int i_type = xml_ReaderNextNodeNS( p_sys->p_reader, &psz_node_name, &psz_namespace );
+        int i_type = xml_ReaderNextNodeNS( p_sys->p_reader, &psz_node_name, &psz_node_namespace );
         bool b_empty = xml_ReaderIsEmptyElement( p_sys->p_reader );
 
         if( i_type <= XML_READER_NONE )
@@ -173,22 +174,25 @@ static int ReadTTML( demux_t* p_demux )
                 break;
 
             case XML_READER_STARTELEM:
-                if( tt_node_NameCompare( psz_node_name, "tt" ) ||
+                if( strcmp( psz_node_namespace, TT_NS ) ||
+                    strcmp( tt_LocalName( psz_node_name ), "tt" ) ||
                     p_sys->p_rootnode != NULL )
                     return VLC_EGENERIC;
 
-                p_sys->p_rootnode = tt_node_NewRead( p_sys->p_reader, NULL, psz_node_name,
-                                                     psz_namespace );
+                p_sys->p_rootnode = tt_node_NewRead( p_sys->p_reader, &p_sys->namespaces, NULL,
+                                                     psz_node_name,
+                                                     psz_node_namespace );
                 if( b_empty )
                     break;
                 if( !p_sys->p_rootnode ||
-                    tt_nodes_Read( p_sys->p_reader, p_sys->p_rootnode ) != VLC_SUCCESS )
+                    tt_nodes_Read( p_sys->p_reader,
+                                  &p_sys->namespaces, p_sys->p_rootnode ) != VLC_SUCCESS )
                     return VLC_EGENERIC;
                 break;
 
             case XML_READER_ENDELEM:
                 if( !p_sys->p_rootnode ||
-                    tt_node_NameCompare( psz_node_name, p_sys->p_rootnode->psz_node_name ) )
+                    strcmp( psz_node_name, p_sys->p_rootnode->psz_node_name ) )
                     return VLC_EGENERIC;
                 break;
         }
@@ -342,6 +346,7 @@ int tt_OpenDemux( vlc_object_t* p_this )
     tt_time_Init( &p_sys->temporal_extent.end );
     tt_time_Init( &p_sys->temporal_extent.dur );
     p_sys->temporal_extent.begin.base = 0;
+    tt_namespaces_Init( &p_sys->namespaces );
 
     p_sys->p_xml = xml_Create( p_demux );
     if( !p_sys->p_xml )
@@ -418,6 +423,8 @@ void tt_CloseDemux( vlc_object_t* p_this )
 
     if( p_sys->p_xml )
         xml_Delete( p_sys->p_xml );
+
+    tt_namespaces_Clean( &p_sys->namespaces );
 
     free( p_sys->times.p_array );
 

modules/codec/ttml/ttml.h
@@ -129,16 +129,17 @@ static inline const char *tt_LocalName( const char *psz_qname )
 tt_textnode_t *tt_textnode_New( tt_node_t *p_parent, const char *psz_text );
 tt_textnode_t *tt_subtextnode_New( tt_node_t *p_parent, const char *psz_text, size_t );
 tt_node_t * tt_node_New( tt_node_t* p_parent, const char* psz_node_name, const char *psz_namespace );
-tt_node_t * tt_node_NewRead( xml_reader_t* reader, tt_node_t* p_parent, const char* psz_node_name,
-                             const char *psz_namespace );
+tt_node_t * tt_node_NewRead( xml_reader_t* reader, tt_namespaces_t *, tt_node_t* p_parent,
+                             const char* psz_node_name, const char *psz_namespace );
 void tt_node_RecursiveDelete( tt_node_t *p_node );
 bool tt_node_Match( const tt_node_t *p_node, const char* psz_name, const char* psz_namespace );
-int  tt_node_NameCompare( const char* psz_tagname, const char* psz_pattern );
+const char * tt_node_GetAttribute( tt_namespaces_t *, const tt_node_t *p_node,
+                                   const char *psz_name, const char *psz_namespace );
 bool tt_node_HasChild( const tt_node_t *p_node );
 int  tt_node_AddAttribute( tt_node_t *p_node, const char *key, const char *value );
 void tt_node_RemoveAttribute( tt_node_t *p_node, const char *key );
 
-int tt_nodes_Read( xml_reader_t *p_reader, tt_node_t *p_root_node );
+int tt_nodes_Read( xml_reader_t *p_reader, tt_namespaces_t *, tt_node_t *p_root_node );
 
 void tt_timings_Resolve( tt_basenode_t *p_child, const tt_timings_t *p_container_timings,
                          tt_time_t **pp_array, size_t *pi_count );

commit d16c61873f45898151d2ee6b9def1123038f652e

ttml: fix null deref on empty namespace uri
refs #29053
resf #29055
regression by 44879313bab5b7208b12cb4968cf03a6f521e531

ttml: fix null deref on empty uri

modules/codec/ttml/ttml.c
@@ -129,7 +129,7 @@ const char * tt_namespaces_GetPrefix( const tt_namespaces_t *nss,
 void tt_namespaces_Register( tt_namespaces_t *nss, const char *psz_prefix,
                              const char *psz_uri )
 {
-    if( tt_namespaces_GetPrefix( nss, psz_uri ) )
+    if( !psz_uri || tt_namespaces_GetPrefix( nss, psz_uri ) )
         return;
     struct tt_namespace_s *ns = malloc(sizeof(*ns));
     if( ns )

modules/demux/ttml.c
@@ -174,7 +174,7 @@ static int ReadTTML( demux_t* p_demux )
                 break;
 
             case XML_READER_STARTELEM:
-                if( strcmp( psz_node_namespace, TT_NS ) ||
+                if( (psz_node_namespace && strcmp( psz_node_namespace, TT_NS )) ||
                     strcmp( tt_LocalName( psz_node_name ), "tt" ) ||
                     p_sys->p_rootnode != NULL )
                     return VLC_EGENERIC;