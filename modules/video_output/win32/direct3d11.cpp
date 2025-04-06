videolan/vlc

commit e33e383fde533c17032614bd0cbf5be1d364917a

direct3d11: check the window accordingly
If the display is opened on a window that is not matching with a
DirectComposition backend or HWND backend, the module should not even
try to open.

modules/video_output/win32/direct3d11.cpp
@@ -532,6 +532,10 @@ static const auto ops = []{
 static int Open(vout_display_t *vd,
                 video_format_t *fmtp, vlc_video_context *context)
 {
+    if (vd->cfg->window->type != VLC_WINDOW_TYPE_HWND &&
+        vd->cfg->window->type != VLC_WINDOW_TYPE_DCOMP)
+        return VLC_ENOTSUP;
+
     vout_display_sys_t *sys = new (std::nothrow) vout_display_sys_t();
     if (!sys)
         return VLC_ENOMEM;

commit 7ff98c8e83f7ab391c883003ca532dc04d72fad0

direct3d11: separate display from drawable display
Separating the display used in VLC and the libvlc application-side
display ensures we're not trying to use the latter in the VLC scenarios
and still ensures we check the kind of windowing that is being provided
to the display.

Fix a regression with e33e383fde533c17032614bd0cbf5be1d364917a
preventing the callbacks from opening the display.

modules/video_output/win32/direct3d11.cpp
@@ -58,7 +58,9 @@
 
 using Microsoft::WRL::ComPtr;
 
-static int  Open(vout_display_t *,
+static int  OpenDisplay(vout_display_t *,
+                 video_format_t *, vlc_video_context *);
+static int  OpenDrawable(vout_display_t *,
                  video_format_t *, vlc_video_context *);
 static void Close(vout_display_t *);
 
@@ -98,7 +100,12 @@ vlc_module_begin ()
         change_string_list(ppsz_hdr_mode, ppsz_hdr_mode_text)
 
     add_shortcut("direct3d11")
-    set_callback_display(Open, 300)
+    set_callback_display(OpenDisplay, 300)
+
+    add_submodule()
+        set_callback_display(OpenDrawable, 0)
+        add_shortcut("d3d11drawable")
+
 vlc_module_end ()
 
 enum d3d11_upscale
@@ -529,13 +536,9 @@ static const auto ops = []{
     return ops;
 }();
 
-static int Open(vout_display_t *vd,
+static int OpenGeneric(vout_display_t *vd,
                 video_format_t *fmtp, vlc_video_context *context)
 {
-    if (vd->cfg->window->type != VLC_WINDOW_TYPE_HWND &&
-        vd->cfg->window->type != VLC_WINDOW_TYPE_DCOMP)
-        return VLC_ENOTSUP;
-
     vout_display_sys_t *sys = new (std::nothrow) vout_display_sys_t();
     if (!sys)
         return VLC_ENOMEM;
@@ -637,6 +640,27 @@ static int Open(vout_display_t *vd,
 error:
     Close(vd);
     return VLC_EGENERIC;
+}
+
+static int OpenDisplay(vout_display_t *vd,
+                       video_format_t *fmtp, vlc_video_context *context)
+{
+    if (vd->cfg->window->type != VLC_WINDOW_TYPE_HWND &&
+        vd->cfg->window->type != VLC_WINDOW_TYPE_DCOMP)
+        return VLC_ENOTSUP;
+
+    return OpenGeneric(vd, fmtp, context);
+}
+
+static int OpenDrawable(vout_display_t *vd,
+                        video_format_t *fmtp, vlc_video_context *context)
+{
+    if (vd->cfg->window->type != VLC_WINDOW_TYPE_DUMMY)
+        return VLC_ENOTSUP;
+
+    /* TODO: other checks for callback validity should be there. */
+
+    return OpenGeneric(vd, fmtp, context);
 }
 
 static void Close(vout_display_t *vd)