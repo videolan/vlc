33e05c8474f431fe735826961e89b38b9f4e3664
Kartik Dua <duakartik40@gmail.com>
2025-04-08 20:25:12 +0530

    Lua : replace recursive logic with iterative approach
    Converted recursive implementation to an iterative one to prevent
    stack overflows from deeply nested calls. This enhances stability
    and ensures safer execution for long sequences.

--- modules/lua/extension_thread.c
+++ modules/lua/extension_thread.c
@@ -146,28 +146,27 @@ error_listener:
     return VLC_ENOMEM;
 }
 
-/** Recursively drop and free commands starting from "command" */
 static void FreeCommands( struct command_t *command )
 {
-    if( !command ) return;
-    struct command_t *next = command->next;
-    switch( command->i_command )
-    {
-        case CMD_ACTIVATE:
-        case CMD_DEACTIVATE:
-        case CMD_CLICK: // Arg1 must not be freed
-            break;
-
-        case CMD_TRIGGERMENU:
-        case CMD_PLAYING_CHANGED:
-            free( command->data[0] ); // Arg1 is int*, to free
-            break;
-
-        default:
-            break;
+    while (command) {
+        struct command_t *next = command->next;
+        switch( command->i_command )
+        {
+            case CMD_ACTIVATE:
+            case CMD_DEACTIVATE:
+            case CMD_CLICK:
+                /* No extra memory to free */
+                break;
+            case CMD_TRIGGERMENU:
+            case CMD_PLAYING_CHANGED:
+                free( command->data[0] ); // free allocated data
+                break;
+            default:
+                break;
+        }
+        free(command);
+        command = next;
     }
-    free( command );
-    FreeCommands( next );
 }
 
 bool QueueDeactivateCommand( extension_t *p_ext )
