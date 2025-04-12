e8c5338db56a670ae894fe88d935f0d6e7f571ee
Claudio Cambra <developer@claudiocambra.com>
2025-04-13 01:10:32 +0800

    macosx: Add NSTextField extension with convenience class method
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ modules/gui/macosx/extensions/NSTextField+VLCAdditions.m
@@ -0,0 +1,40 @@
+/*****************************************************************************
+ * NSTextField+VLCAdditions.m: MacOS X interface module
+ *****************************************************************************
+ * Copyright (C) 2025 VLC authors and VideoLAN
+ *
+ * Authors: Claudio Cambra <developer@claudiocambra.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#import "NSTextField+VLCAdditions.h"
+
+@implementation NSTextField (VLCAdditions)
+
++ (instancetype)defaultLabelWithString:(NSString *)stringValue
+{
+    if (@available(macOS 10.12, *))
+        return [NSTextField wrappingLabelWithString:stringValue];
+    NSTextField * const textField = [[NSTextField alloc] initWithFrame:NSZeroRect];
+    textField.stringValue = stringValue;
+    textField.editable = NO;
+    textField.bordered = NO;
+    textField.drawsBackground = NO;
+    textField.lineBreakMode = NSLineBreakByWordWrapping;
+    return textField;
+}
+
+@end
