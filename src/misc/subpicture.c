35396660acf211bc481dffb3e2225072a738ea41
Steve Lhomme <robux4@ycbcr.xyz>
2025-05-16 14:44:34 +0200

    subpicture: set regions to full range SDR by default
    The caller of subpicture_region_New() can change it afterwards
    in the region format and the picture format.
    But the default values were undefined and might pick the
    colorimetry of the video.
    
    This is the same values already used in region_FixFmt().
    
    Ref. #27877 and #26180

--- src/misc/subpicture.c
+++ src/misc/subpicture.c
@@ -269,6 +269,14 @@ subpicture_region_t *subpicture_region_New( const video_format_t *p_fmt )
         return NULL;
 
     video_format_Copy( &p_region->fmt, p_fmt );
+    if (p_region->fmt.transfer == TRANSFER_FUNC_UNDEF)
+        p_region->fmt.transfer = TRANSFER_FUNC_SRGB;
+    if (p_region->fmt.primaries == COLOR_PRIMARIES_UNDEF)
+        p_region->fmt.primaries = COLOR_PRIMARIES_SRGB;
+    if (p_region->fmt.space == COLOR_SPACE_UNDEF)
+        p_region->fmt.space = COLOR_SPACE_SRGB;
+    if (p_region->fmt.color_range == COLOR_RANGE_UNDEF)
+        p_region->fmt.color_range = COLOR_RANGE_FULL;
     p_region->p_picture = picture_NewFromFormat( &p_region->fmt );
     if( !p_region->p_picture )
     {
