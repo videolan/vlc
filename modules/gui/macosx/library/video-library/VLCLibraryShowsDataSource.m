1610f7c54264cd50d8d05afc88fa1d3529b645ff
Claudio Cambra <developer@claudiocambra.com>
2025-08-25 17:06:13 +0800

    macosx: Granularly update show library views
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/library/video-library/VLCLibraryShowsDataSource.m
+++ modules/gui/macosx/library/video-library/VLCLibraryShowsDataSource.m
@@ -22,6 +22,7 @@
 
 #import "VLCLibraryShowsDataSource.h"
 
+#import "library/VLCLibraryDataTypes.h"
 #import "library/VLCLibraryModel.h"
 
 @implementation VLCLibraryShowsDataSource
@@ -42,6 +43,14 @@
     [notificationCenter addObserver:self
                            selector:@selector(libraryModelShowsListReset:)
                                name:VLCLibraryModelListOfShowsReset
+                             object:nil];
+    [notificationCenter addObserver:self
+                           selector:@selector(libraryModelShowDeleted:)
+                               name:VLCLibraryModelShowDeleted
+                             object:nil];
+    [notificationCenter addObserver:self
+                           selector:@selector(libraryModelShowUpdated:)
+                               name:VLCLibraryModelShowUpdated
                              object:nil];
 
     [self reloadData];
@@ -65,6 +74,52 @@
 - (VLCMediaLibraryParentGroupType)currentParentType
 {
     return VLCMediaLibraryParentGroupTypeShow;
+}
+
+- (void)libraryModelShowUpdated:(NSNotification *)notification
+{
+    VLCMediaLibraryShow * const show = notification.object;
+    NSIndexPath * const indexPath = [self indexPathForLibraryItem:show];
+
+    if (indexPath != nil) {
+        [self.collectionView reloadItemsAtIndexPaths:[NSSet setWithObject:indexPath]];
+    }
+
+    const NSInteger rowIndex = [self rowForLibraryItem:show];
+    if (rowIndex == NSNotFound) {
+        return;
+    }
+
+    const NSInteger selectedMasterRow = self.masterTableView.selectedRow;
+    [self.masterTableView reloadDataForRowIndexes:[NSIndexSet indexSetWithIndex:rowIndex]
+                                    columnIndexes:[NSIndexSet indexSetWithIndex:0]];
+
+    if (rowIndex == selectedMasterRow && self.masterTableView.selectedRow != selectedMasterRow) {
+        [self.masterTableView selectRowIndexes:[NSIndexSet indexSetWithIndex:selectedMasterRow]
+                          byExtendingSelection:NO];
+    } else {
+        [self.detailTableView reloadData];
+    }
+}
+
+- (void)libraryModelShowDeleted:(NSNotification *)notification
+{
+    NSNumber * const showIdNumber = notification.object;
+    const NSInteger showLibraryId = showIdNumber.integerValue;
+    const NSInteger rowIndex = [self.backingArray indexOfObjectPassingTest:^BOOL(VLCMediaLibraryShow * const show, const NSUInteger idx, BOOL * const stop) {
+        return show.libraryID == showLibraryId;
+    }];
+    if (rowIndex == NSNotFound) {
+        return;
+    }
+
+    NSIndexPath * const indexPath = [NSIndexPath indexPathForItem:0 inSection:rowIndex];
+    if (indexPath != nil) {
+        [self.collectionView deleteItemsAtIndexPaths:[NSSet setWithObject:indexPath]];
+    }
+
+    [self.masterTableView removeRowsAtIndexes:[NSIndexSet indexSetWithIndex:rowIndex]
+                                withAnimation:NSTableViewAnimationEffectFade];
 }
 
 @end
