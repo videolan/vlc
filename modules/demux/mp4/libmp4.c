7daea2eee6576d7fb616c66cd6f482ec0b9e9d87
Steve Lhomme <robux4@ycbcr.xyz>
2025-08-27 12:08:31 +0200

    demux: libmp4: simplify string length check

--- modules/demux/mp4/libmp4.c
+++ modules/demux/mp4/libmp4.c
@@ -3652,14 +3652,12 @@ static int MP4_ReadBox_rdrf( stream_t *p_stream, MP4_Box_t *p_box )
     MP4_GETVERSIONFLAGS( p_box->data.p_rdrf );
     MP4_GETFOURCC( p_box->data.p_rdrf->i_ref_type );
     MP4_GET4BYTES( i_len );
-    i_len++;
 
-    if( i_len > 0 )
+    if( i_len != 0 )
     {
-        p_box->data.p_rdrf->psz_ref = malloc( i_len );
+        p_box->data.p_rdrf->psz_ref = malloc( i_len + 1 );
         if( p_box->data.p_rdrf->psz_ref == NULL )
             MP4_READBOX_EXIT( 0 );
-        i_len--;
 
         for( unsigned i = 0; i < i_len; i++ )
         {
