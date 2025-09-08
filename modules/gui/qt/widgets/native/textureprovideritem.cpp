videolan/vlc

commit 0e5200bcbe10f04e13ab5da7d3e3f1eeef0d2e2a

qt: expose texture view state as properties in `TextureProviderItem`
This can be useful in certain situations, like tiling the source
texture without a layer (thanks to wrap mode).

It should be noted that these are the view states, we are not
modifying the source texture state here.

`ShaderEffectSource` already exposes these for the layer texture
it manages. Though, it seems more limited there, for example it
does not seem to allow disabling filtering completely, or allow
adjusting anisotropy level (which does not mean anything at the
moment but nevertheless).

modules/gui/qt/widgets/native/textureprovideritem.cpp
@@ -90,12 +90,34 @@ QSGTextureProvider *TextureProviderItem::textureProvider() const
                 provider->setRect(rect);
         };
 
+        const auto synchronizeState = [weakThis = QPointer(this), provider = m_textureProvider]() {
+            if (Q_UNLIKELY(!weakThis))
+                return;
+
+            provider->setFiltering(weakThis->m_filtering);
+            provider->setMipmapFiltering(weakThis->m_mipmapFiltering);
+            provider->setAnisotropyLevel(weakThis->m_anisotropyLevel);
+            provider->setHorizontalWrapMode(weakThis->m_horizontalWrapMode);
+            provider->setVerticalWrapMode(weakThis->m_verticalWrapMode);
+        };
+
+        // These are going to be queued when necessary:
         connect(this, &TextureProviderItem::sourceChanged, m_textureProvider, adjustSource);
         connect(this, &TextureProviderItem::rectChanged, m_textureProvider, adjustRect);
+
+        connect(this, &TextureProviderItem::filteringChanged, m_textureProvider, &QSGTextureViewProvider::setFiltering);
+        connect(this, &TextureProviderItem::mipmapFilteringChanged, m_textureProvider, &QSGTextureViewProvider::setMipmapFiltering);
+        connect(this, &TextureProviderItem::anisotropyLevelChanged, m_textureProvider, &QSGTextureViewProvider::setAnisotropyLevel);
+        connect(this, &TextureProviderItem::horizontalWrapModeChanged, m_textureProvider, &QSGTextureViewProvider::setHorizontalWrapMode);
+        connect(this, &TextureProviderItem::verticalWrapModeChanged, m_textureProvider, &QSGTextureViewProvider::setVerticalWrapMode);
+
+        // When the target texture changes, the texture view may reset its state, so we need to synchronize in that case:
+        connect(m_textureProvider, &QSGTextureProvider::textureChanged, m_textureProvider, synchronizeState); // Executed in texture provider's thread
 
         // Initial adjustments:
         adjustSource(m_source);
         adjustRect(m_rect);
+        synchronizeState();
     }
     return m_textureProvider;
 }

modules/gui/qt/widgets/native/textureprovideritem.hpp
@@ -58,10 +58,43 @@ class TextureProviderItem : public QQuickItem
     Q_PROPERTY(const QQuickItem* source MEMBER m_source NOTIFY sourceChanged FINAL)
     Q_PROPERTY(QRect textureSubRect MEMBER m_rect NOTIFY rectChanged RESET resetTextureSubRect FINAL)
 
+    Q_PROPERTY(QSGTexture::AnisotropyLevel anisotropyLevel MEMBER m_anisotropyLevel NOTIFY anisotropyLevelChanged FINAL)
+    Q_PROPERTY(QSGTexture::WrapMode horizontalWrapMode MEMBER m_horizontalWrapMode NOTIFY horizontalWrapModeChanged FINAL)
+    Q_PROPERTY(QSGTexture::WrapMode verticalWrapMode MEMBER m_verticalWrapMode NOTIFY verticalWrapModeChanged FINAL)
+    // Maybe we should use `Item::smooth` instead of these properties, or maybe not, as we should allow disabling filtering completely.
+    Q_PROPERTY(QSGTexture::Filtering filtering MEMBER m_filtering NOTIFY filteringChanged FINAL)
+    // WARNING: mipmap filtering is not respected if target texture has no mip maps:
+    Q_PROPERTY(QSGTexture::Filtering mipmapFiltering MEMBER m_mipmapFiltering NOTIFY mipmapFilteringChanged FINAL)
+
     QML_ELEMENT
 public:
     TextureProviderItem() = default;
     virtual ~TextureProviderItem();
+
+    // These enumerations must be in sync with `QSGTexture`:
+    // It appears that MOC is not clever enough to consider foreign enumerations with `Q_ENUM` (I tried)...
+    enum _WrapMode {
+        Repeat,
+        ClampToEdge,
+        MirroredRepeat
+    };
+    Q_ENUM(_WrapMode);
+
+    enum _Filtering {
+        None,
+        Nearest,
+        Linear
+    };
+    Q_ENUM(_Filtering);
+
+    enum _AnisotropyLevel {
+        AnisotropyNone,
+        Anisotropy2x,
+        Anisotropy4x,
+        Anisotropy8x,
+        Anisotropy16x
+    };
+    Q_ENUM(_AnisotropyLevel);
 
     bool isTextureProvider() const override;
 
@@ -77,6 +110,12 @@ signals:
     void rectChanged(const QRect& rect);
     void dprChanged();
 
+    void anisotropyLevelChanged(QSGTexture::AnisotropyLevel);
+    void filteringChanged(QSGTexture::Filtering);
+    void mipmapFilteringChanged(QSGTexture::Filtering);
+    void horizontalWrapModeChanged(QSGTexture::WrapMode);
+    void verticalWrapModeChanged(QSGTexture::WrapMode);
+
 protected:
     void releaseResources() override;
     void itemChange(QQuickItem::ItemChange change, const QQuickItem::ItemChangeData &value) override;
@@ -87,6 +126,13 @@ private:
 
     mutable QPointer<QSGTextureViewProvider> m_textureProvider;
     mutable QMutex m_textureProviderMutex; // I'm not sure if this mutex is necessary
+
+    std::atomic<QSGTexture::AnisotropyLevel> m_anisotropyLevel = QSGTexture::AnisotropyNone;
+    std::atomic<QSGTexture::Filtering> m_filtering = (smooth() ? QSGTexture::Linear : QSGTexture::Nearest);
+    std::atomic<QSGTexture::WrapMode> m_horizontalWrapMode = QSGTexture::ClampToEdge;
+    std::atomic<QSGTexture::WrapMode> m_verticalWrapMode = QSGTexture::ClampToEdge;
+    // When there are mip maps, no mip map filtering should be fine (unlike no mip maps with mip map filtering):
+    std::atomic<QSGTexture::Filtering> m_mipmapFiltering = QSGTexture::None;
 };
 
 #endif // TEXTUREPROVIDERITEM_HPP

commit bab4f9848ea8a5573a60984128b1f7f51068c26e

qt: use mipmap filtering by default if source is mipmapped in `TextureProviderItem`
This fixes the regression in 0e5200bc where it made mip map filtering off by default
for mipmapped source textures (unless `mipmap: TextureProviderItem.Linear` is used,
which we do not want in each case explicitly).

This is not really a bug, but I consider this a regression because it is hard to
justify not using mipmap filtering when the texture has mip maps.

modules/gui/qt/widgets/native/textureprovideritem.cpp
@@ -235,7 +235,6 @@ void QSGTextureViewProvider::setMipmapFiltering(QSGTexture::Filtering filter)
         if (targetTexture && !targetTexture->hasMipmaps())
         {
             // Having mip map filtering when there are no mip maps may be problematic with certain graphics backends (like OpenGL).
-            qWarning() << this << "Enabling mip map filtering is blocked if the target texture has no mip maps.";
             return;
         }
     }

modules/gui/qt/widgets/native/textureprovideritem.hpp
@@ -139,7 +139,8 @@ private:
     std::atomic<QSGTexture::WrapMode> m_horizontalWrapMode = QSGTexture::ClampToEdge;
     std::atomic<QSGTexture::WrapMode> m_verticalWrapMode = QSGTexture::ClampToEdge;
     // When there are mip maps, no mip map filtering should be fine (unlike no mip maps with mip map filtering):
-    std::atomic<QSGTexture::Filtering> m_mipmapFiltering = QSGTexture::None;
+    // But we want to have mip map filtering by default if the texture has mip maps (if not, it won't be respected):
+    std::atomic<QSGTexture::Filtering> m_mipmapFiltering = QSGTexture::Linear;
 };
 
 #endif // TEXTUREPROVIDERITEM_HPP