d6a50c1a61acea4d943e7272ccdf27bfa4c4306b
François Cartegnie <fcvlcdev@free.fr>
2025-09-25 19:32:02 +0700

    resampler: speex: check for overflow

--- modules/audio_filter/resampler/speex.c
+++ modules/audio_filter/resampler/speex.c
@@ -23,6 +23,8 @@
 #endif
 
 #include <inttypes.h>
+#include <stdckdint.h>
+#include <limits.h>
 
 #include <vlc_common.h>
 #include <vlc_aout.h>
@@ -129,11 +131,25 @@ static block_t *Resample (filter_t *filter, block_t *in)
     if( speex_resampler_set_rate (st, irate, orate) != RESAMPLER_ERR_SUCCESS )
         goto error;
 
-    spx_uint32_t ilen = in->i_nb_samples;
-    spx_uint32_t olen = ((ilen + 2) * orate * UINT64_C(11))
-                      / (irate * UINT64_C(10));
+    unsigned ilen = in->i_nb_samples;
+    unsigned olen;
 
-    block_t *out = block_Alloc (olen * framesize);
+    // spx_uint32_t olen = ((ilen + 2) * orate * UINT64_C(11))
+    //                   / (irate * UINT64_C(10));
+    uint64_t num, den;
+    if( ckd_add(&olen, ilen, 2U) ||
+        ckd_mul(&num, (uint64_t)orate, UINT64_C(11)) ||
+        ckd_mul(&den, (uint64_t)irate, UINT64_C(10)) ||
+        ckd_mul(&num, (uint64_t)olen, num) ||
+        num / UINT_MAX >= den )
+        goto error;
+
+    olen = num / den;
+
+    if(SIZE_MAX / framesize < olen)
+        goto error;
+
+    out = block_Alloc (olen * framesize);
     if (unlikely(out == NULL))
         goto error;
 
