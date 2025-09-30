548e797d83b716806fddd0c7fd17abab26632180
Steve Lhomme <robux4@ycbcr.xyz>
2025-09-30 15:00:42 +0200

    qt: remove unused DialogEventTypeOffset

--- modules/gui/qt/qt.hpp
+++ modules/gui/qt/qt.hpp
@@ -52,7 +52,6 @@
 #define HAS_QT510 ( QT_VERSION >= 0x051000 )
 
 enum {
-    DialogEventTypeOffset = 0,
     IMEventTypeOffset     = 100,
     PLEventTypeOffset     = 200,
     MsgEventTypeOffset    = 300,
