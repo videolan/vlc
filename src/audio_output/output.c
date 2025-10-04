99b1759de894e59b0482bf551c601b561269c81d
Rémi Denis-Courmont <remi@remlab.net>
2025-10-04 15:36:36 +0300

    aout: set volume to -1 if unknown
    This is consistent with the aout destruction path.

--- src/audio_output/output.c
+++ src/audio_output/output.c
@@ -285,6 +285,7 @@ audio_output_t *aout_New (vlc_object_t *parent)
 
     /* Audio output module callbacks */
     var_Create (aout, "volume", VLC_VAR_FLOAT);
+    var_SetFloat(aout, "volume", -1.f);
     var_AddCallback (aout, "volume", var_Copy, parent);
     var_Create (aout, "mute", VLC_VAR_BOOL);
     var_AddCallback (aout, "mute", var_Copy, parent);
