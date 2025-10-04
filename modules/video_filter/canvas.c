911133a0440e18300fe9876dc93c04c8fcbc671f
Johannes Kauffmann <johanneskauffmann@hotmail.com>
2025-10-04 21:42:56 +0200

    canvas: fix fmt leak
    (cherry picked from commit cc140332997025b835172c14adb020b8b7e42bf1)
    edited:
    - the filter_chain_Reset() signature is different on 3.0.x and caused
      a conflict

--- modules/video_filter/canvas.c
+++ modules/video_filter/canvas.c
@@ -329,6 +329,7 @@ static int Activate( vlc_object_t *p_this )
     fmt.video.i_height = p_filter->fmt_in.video.i_height * fmt.video.i_visible_height / p_filter->fmt_in.video.i_visible_height;
 
     filter_chain_Reset( p_sys->p_chain, &p_filter->fmt_in, &fmt );
+    es_format_Clean( &fmt );
     /* Append scaling module */
     if ( filter_chain_AppendConverter( p_sys->p_chain, NULL, NULL ) )
     {
