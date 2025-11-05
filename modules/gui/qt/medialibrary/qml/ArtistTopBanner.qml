videolan/vlc

commit fdcb6f07789b6873594c2fd736dcb06e0e184370

qml: do not display the effect during initialization in `ArtistTopBanner`

modules/gui/qt/medialibrary/qml/ArtistTopBanner.qml
@@ -77,7 +77,7 @@ FocusScope {
     Item {
         anchors.fill: background
 
-        visible: (GraphicsInfo.shaderType === GraphicsInfo.RhiShader)
+        visible: (GraphicsInfo.shaderType === GraphicsInfo.RhiShader) && (root.artist.id) // do not display the effect during initialization
 
         // This blur effect does not create an implicit layer that is updated
         // each time the size changes. The source texture is static, so the blur

commit 45692d49ff03efa78dfc57e319aeaa631b002b10

qml: fix "Unable to assign [undefined] to bool" in `ArtistTopBanner.qml`
Regression since fdcb6f07.

Co-authored-by: Pierre Lamot <pierre@videolabs.io>

modules/gui/qt/medialibrary/qml/ArtistTopBanner.qml
@@ -33,7 +33,7 @@ FocusScope {
 
     property int rightPadding: 0
 
-    property var artist: ({})
+    required property var artist
 
     implicitHeight: VLCStyle.artistBanner_height
 

modules/gui/qt/medialibrary/qml/MusicArtist.qml
@@ -68,7 +68,9 @@ FocusScope {
 
     property alias _currentView: loader.item
 
-    property var _artist: ({})
+    property var _artist: ({
+        id: 0
+    })
 
     function navigationShowHeader(y, height) {
         const newContentY = Helpers.flickablePositionContaining(_currentView, y, height, 0, 0)