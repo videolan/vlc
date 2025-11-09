2101c1258b171a75178e19a3f8bc1c6bab4000cf
Rémi Denis-Courmont <remi@remlab.net>
2025-11-09 12:56:31 +0200

    chroma_yuv: remove conversions to YUY2
    These were added for XVideo output on TI OMAP3xxx devices, notably the
    Nokia 900 and N9. With the removal of the XVideo video output,
    they have become useless.

--- modules/isa/arm/neon/chroma_neon.h
+++ modules/isa/arm/neon/chroma_neon.h
@@ -45,26 +45,6 @@ struct yuv_pack
     size_t pitch;
 };
 
-/* I420 to YUYV conversion. */
-void i420_yuyv_neon (struct yuv_pack *const out,
-                     const struct yuv_planes *const in,
-                     int width, int height);
-
-/* I420 to UYVY conversion. */
-void i420_uyvy_neon (struct yuv_pack *const out,
-                     const struct yuv_planes *const in,
-                     int width, int height);
-
-/* I422 to YUYV conversion. */
-void i422_yuyv_neon (struct yuv_pack *const out,
-                     const struct yuv_planes *const in,
-                     int width, int height);
-
-/* I422 to UYVY conversion. */
-void i422_uyvy_neon (struct yuv_pack *const out,
-                     const struct yuv_planes *const in,
-                     int width, int height);
-
 /* YUYV to I422 conversion. */
 void yuyv_i422_neon (struct yuv_planes *const out,
                      const struct yuv_pack *const in,
