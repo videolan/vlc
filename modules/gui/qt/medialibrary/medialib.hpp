3e607b82ff08ccd6dc9d48ca4f3499f22942fce3
Steve Lhomme <robux4@ycbcr.xyz>
2025-11-17 13:24:31 +0100

    qt: medialib: avoid including unused QVariant

--- modules/gui/qt/medialibrary/medialib.hpp
+++ modules/gui/qt/medialibrary/medialib.hpp
@@ -16,8 +16,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 #pragma once
-
-#include <QVariant>
 #include <QQuickItem>
 
 #include <memory>
