videolan/vlc

commit 5eb0c9c48312bfb57bdf174c071a7f984f4ef7fe

decoder: move draining check
No functional changes as IsDrained() is only used by IsEmpty()
internally.

src/input/decoder.c
@@ -2503,7 +2503,7 @@ static bool vlc_input_decoder_IsDrainedLocked(vlc_input_decoder_t *owner)
 bool vlc_input_decoder_IsDrained(vlc_input_decoder_t *owner)
 {
     vlc_fifo_Lock(owner->p_fifo);
-    bool drained = vlc_input_decoder_IsDrainedLocked(owner);
+    bool drained = !owner->b_draining && vlc_input_decoder_IsDrainedLocked(owner);
     vlc_fifo_Unlock(owner->p_fifo);
     return drained;
 }
@@ -2513,7 +2513,7 @@ bool vlc_input_decoder_IsEmpty( vlc_input_decoder_t * p_owner )
     assert( !p_owner->b_waiting );
 
     vlc_fifo_Lock( p_owner->p_fifo );
-    if( !vlc_fifo_IsEmpty( p_owner->p_fifo ) || p_owner->b_draining )
+    if( !vlc_fifo_IsEmpty( p_owner->p_fifo ) )
     {
         vlc_fifo_Unlock( p_owner->p_fifo );
         return false;

commit 0bec04f0e10562419cec05cddfc75ae3fb8f29d0

decoder: fix possible and recent drained miss
Regression from 5eb0c9c48312bfb57bdf174c071a7f984f4ef7fe

`draining` should be checked from `vlc_input_decoder_IsDrainedLocked()`.

src/input/decoder.c
@@ -2490,7 +2490,9 @@ static bool vlc_input_decoder_IsDrainedLocked(vlc_input_decoder_t *owner)
 {
     vlc_fifo_Assert(owner->p_fifo);
 
-    if (owner->p_sout_input != NULL)
+    if (owner->b_draining)
+        return false;
+    else if (owner->p_sout_input != NULL)
         return true;
     else if (owner->fmt.i_cat == VIDEO_ES && owner->video.vout != NULL)
         return vout_IsEmpty(owner->video.vout);
@@ -2503,7 +2505,7 @@ static bool vlc_input_decoder_IsDrainedLocked(vlc_input_decoder_t *owner)
 bool vlc_input_decoder_IsDrained(vlc_input_decoder_t *owner)
 {
     vlc_fifo_Lock(owner->p_fifo);
-    bool drained = !owner->b_draining && vlc_input_decoder_IsDrainedLocked(owner);
+    bool drained = vlc_input_decoder_IsDrainedLocked(owner);
     vlc_fifo_Unlock(owner->p_fifo);
     return drained;
 }