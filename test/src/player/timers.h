3eceb8cce01ef215021e692f5779b28d0cc87dfd
Thomas Guillem <thomas@gllm.fr>
2025-12-02 10:21:00 +0100

    test: player: timers: add wait helpers
    Current timer tests are fetching reports when the player is stopped and
    does not require any lock or wait mechanism. Add lock/cond and helpers
    in order to allow fetching timer reports when the player is running.

--- test/src/player/timers.h
+++ test/src/player/timers.h
@@ -35,6 +35,9 @@ struct timer_state
     vlc_player_timer_id *id;
     vlc_tick_t delay;
     vec_report_timer vec;
+    size_t last_report_idx;
+    vlc_mutex_t lock;
+    vlc_cond_t wait;
 };
 
 static inline void
@@ -46,7 +49,10 @@ timers_on_update(const struct vlc_player_timer_point *point, void *data)
         .type = REPORT_TIMER_POINT,
         .point = *point,
     };
+    vlc_mutex_lock(&timer->lock);
     bool success = vlc_vector_push(&timer->vec, report);
+    vlc_cond_signal(&timer->wait);
+    vlc_mutex_unlock(&timer->lock);
     assert(success);
 }
 
@@ -59,7 +65,10 @@ timers_on_paused(vlc_tick_t system_date, void *data)
         .type = REPORT_TIMER_PAUSED,
         .paused_date = system_date,
     };
+    vlc_mutex_lock(&timer->lock);
     bool success = vlc_vector_push(&timer->vec, report);
+    vlc_cond_signal(&timer->wait);
+    vlc_mutex_unlock(&timer->lock);
     assert(success);
 }
 
@@ -74,7 +83,10 @@ timers_on_seek(const struct vlc_player_timer_point *point, void *data)
     report.seek.finished = point == NULL;
     if (!report.seek.finished)
         report.seek.point = *point;
+    vlc_mutex_lock(&timer->lock);
     bool success = vlc_vector_push(&timer->vec, report);
+    vlc_cond_signal(&timer->wait);
+    vlc_mutex_unlock(&timer->lock);
     assert(success);
 }
 
@@ -88,7 +100,10 @@ timers_smpte_on_update(const struct vlc_player_timer_smpte_timecode *tc,
         .type = REPORT_TIMER_TC,
         .tc = *tc,
     };
+    vlc_mutex_lock(&timer->lock);
     bool success = vlc_vector_push(&timer->vec, report);
+    vlc_cond_signal(&timer->wait);
+    vlc_mutex_unlock(&timer->lock);
     assert(success);
 }
 
@@ -109,6 +124,9 @@ player_add_timer(vlc_player_t *player, struct timer_state *timer, bool smpte,
     };
 
     vlc_vector_init(&timer->vec);
+    vlc_mutex_init(&timer->lock);
+    vlc_cond_init(&timer->wait);
+    timer->last_report_idx = 0;
     timer->delay = delay;
     if (smpte)
         timer->id = vlc_player_AddSmpteTimer(player, &smpte_cbs, timer);
@@ -122,4 +140,28 @@ player_remove_timer(vlc_player_t *player, struct timer_state *timer)
 {
     vlc_vector_clear(&timer->vec);
     vlc_player_RemoveTimer(player, timer->id);
+}
+
+static inline void
+player_lock_timer(vlc_player_t *player, struct timer_state *timer)
+{
+    vlc_player_Unlock(player);
+    vlc_mutex_lock(&timer->lock);
+}
+
+static inline void
+player_unlock_timer(vlc_player_t *player, struct timer_state *timer)
+{
+    vlc_mutex_unlock(&timer->lock);
+    vlc_player_Lock(player);
+}
+
+static inline struct report_timer *
+timer_state_wait_next_report(struct timer_state *timer)
+{
+    while (timer->vec.size < timer->last_report_idx + 1)
+        vlc_cond_wait(&timer->wait, &timer->lock);
+    struct report_timer *r = &timer->vec.data[timer->last_report_idx];
+    timer->last_report_idx++;
+    return r;
 }
