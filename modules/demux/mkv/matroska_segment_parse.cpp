videolan/vlc

commit 7cac9cbe051287bab6763385fdf66a8039b813b8

demux: mkv: parse RealAudio extradata directly in Cook_PrivateTrackData
Using a byte reader to avoid relying on unpacked structures.

Fixes #29420

modules/demux/mkv/matroska_segment_parse.cpp
@@ -2140,42 +2140,27 @@ bool matroska_segment_c::TrackInit( mkv_track_t * p_tk )
         }
         static void A_REAL__helper (HandlerPayload& vars, vlc_fourcc_t i_codec) {
             mkv_track_t        * p_tk = vars.p_tk;
-            real_audio_private * priv = (real_audio_private*) p_tk->p_extra_data;
 
             p_tk->fmt.i_codec = i_codec;
 
-            /* FIXME RALF and SIPR */
-            uint16_t version = (uint16_t) hton16(priv->version);
-
-            p_tk->p_sys = new Cook_PrivateTrackData(
-                  hton16( priv->sub_packet_h ),
-                  hton16( priv->frame_size ),
-                  hton16( priv->sub_packet_size )
+            Cook_PrivateTrackData * p_realaudio = new Cook_PrivateTrackData(
+                p_tk->p_extra_data,
+                p_tk->i_extra_data
             );
+            p_tk->p_sys = p_realaudio;
 
-            if( unlikely( !p_tk->p_sys ) )
-                throw std::runtime_error ("p_tk->p_sys is NULL when handling A_REAL/28_8");
+            if( unlikely( !p_realaudio ) )
+                throw std::runtime_error ("Cook_PrivateTrackData is NULL when handling A_REAL/28_8");
 
-            if( unlikely( p_tk->p_sys->Init() ) )
-                throw std::runtime_error ("p_tk->p_sys->Init() failed when handling A_REAL/28_8");
+            if( unlikely( p_realaudio->Init() ) )
+                throw std::runtime_error ("Cook_PrivateTrackData::Init() failed when handling A_REAL/28_8");
 
             if (i_codec == VLC_CODEC_COOK || i_codec == VLC_CODEC_ATRAC3)
-                p_tk->fmt.audio.i_blockalign = hton16(priv->sub_packet_size);
+                p_tk->fmt.audio.i_blockalign = p_realaudio->i_subpacket_size;
 
-            if( version == 4 )
-            {
-                real_audio_private_v4 * v4 = (real_audio_private_v4*) priv;
-                p_tk->fmt.audio.i_channels = hton16(v4->channels);
-                p_tk->fmt.audio.i_bitspersample = hton16(v4->sample_size);
-                p_tk->fmt.audio.i_rate = hton16(v4->sample_rate);
-            }
-            else if( version == 5 )
-            {
-                real_audio_private_v5 * v5 = (real_audio_private_v5*) priv;
-                p_tk->fmt.audio.i_channels = hton16(v5->channels);
-                p_tk->fmt.audio.i_bitspersample = hton16(v5->sample_size);
-                p_tk->fmt.audio.i_rate = hton16(v5->sample_rate);
-            }
+            p_tk->fmt.audio.i_rate          = p_realaudio->i_rate;
+            p_tk->fmt.audio.i_bitspersample = p_realaudio->i_bitspersample;
+            p_tk->fmt.audio.i_channels      = p_realaudio->i_channels;
             msg_Dbg(vars.p_demuxer, "%d channels %d bits %d Hz",p_tk->fmt.audio.i_channels, p_tk->fmt.audio.i_bitspersample, p_tk->fmt.audio.i_rate);
 
             fill_extra_data( p_tk, p_tk->fmt.i_codec == VLC_CODEC_RA_288 ? 0 : 78);

modules/demux/mkv/util.cpp
@@ -392,6 +392,45 @@ void send_Block( demux_t * p_demux, mkv_track_t * p_tk, block_t * p_block, unsig
 
 int32_t Cook_PrivateTrackData::Init()
 {
+    // real_audio_private
+    bytes.skip(4);                      // fourcc
+
+    /* FIXME RALF and SIPR */
+    uint16_t version = bytes.GetBE16(); // version
+    bytes.skip(2);                      // unknown1
+    bytes.skip(12);                     // unknown2
+    bytes.skip(2);                      // unknown3
+    bytes.skip(2);                      // flavor
+    bytes.skip(4);                      // coded_frame_size
+    bytes.skip(3*4);                    // unknown4
+    i_sub_packet_h   = bytes.GetBE16(); // sub_packet_h
+    i_frame_size     = bytes.GetBE16(); // frame_size
+    i_subpacket_size = bytes.GetBE16(); // sub_packet_size
+    bytes.skip(2);                      // unknown5
+
+    if( version == 4 )
+    {
+        // real_audio_private_v4
+        i_rate          = bytes.GetBE16(); // sample_rate
+        bytes.skip(2);                     // unknown
+        i_bitspersample = bytes.GetBE16(); // sample_size
+        i_channels      = bytes.GetBE16(); // channels
+    }
+    else if( version == 5 )
+    {
+        // real_audio_private_v5
+        bytes.skip(4);                     // unknown1
+        bytes.skip(2);                     // unknown2
+        i_rate          = bytes.GetBE16(); // sample_rate
+        bytes.skip(2);                     // unknown2
+        i_bitspersample = bytes.GetBE16(); // sample_size
+        i_channels      = bytes.GetBE16(); // channels
+    }
+    else
+        return 0;
+    if (bytes.hasErrors())
+        return 0;
+
     i_subpackets = (size_t) i_sub_packet_h * (size_t) i_frame_size / (size_t) i_subpacket_size;
     p_subpackets = static_cast<block_t**> ( calloc(i_subpackets, sizeof(block_t*)) );
 

modules/demux/mkv/util.hpp
@@ -131,18 +131,24 @@ struct real_audio_private_v5
 class Cook_PrivateTrackData : public PrivateTrackData
 {
 public:
-    Cook_PrivateTrackData(uint16_t sph, uint16_t fs, uint16_t sps):
-        i_sub_packet_h(sph), i_frame_size(fs), i_subpacket_size(sps),
-        p_subpackets(NULL), i_subpackets(0), i_subpacket(0){}
+    Cook_PrivateTrackData(const uint8_t *reader, size_t reader_len):
+        bytes(reader, reader_len) {}
     ~Cook_PrivateTrackData();
     int32_t Init();
 
     uint16_t i_sub_packet_h;
     uint16_t i_frame_size;
     uint16_t i_subpacket_size;
-    block_t  **p_subpackets;
-    size_t   i_subpackets;
-    size_t   i_subpacket;
+
+    uint16_t i_rate;
+    uint16_t i_bitspersample;
+    uint16_t i_channels;
+
+    block_t  **p_subpackets = NULL;
+    size_t   i_subpackets = 0;
+    size_t   i_subpacket = 0;
+private:
+    ByteReader bytes;
 };
 
 block_t * packetize_wavpack( const mkv_track_t &, uint8_t *, size_t);

commit d51fc15b96817f34d68975e273b28561a84a9868

demux: mkv: fix PrivateTrackData not checking errors properly
Cook_PrivateTrackData::Init() was always returning the same value on error or success.

Bug introduced in 7cac9cbe051287bab6763385fdf66a8039b813b8.

Fixes #29523

modules/demux/mkv/matroska_segment_parse.cpp
@@ -2162,7 +2162,7 @@ bool matroska_segment_c::TrackInit( mkv_track_t * p_tk )
             if( unlikely( !p_realaudio ) )
                 throw std::runtime_error ("Cook_PrivateTrackData is NULL when handling A_REAL/28_8");
 
-            if( unlikely( p_realaudio->Init() ) )
+            if( unlikely( !p_realaudio->Init() ) )
                 throw std::runtime_error ("Cook_PrivateTrackData::Init() failed when handling A_REAL/28_8");
 
             if (i_codec == VLC_CODEC_COOK || i_codec == VLC_CODEC_ATRAC3)

modules/demux/mkv/mkv.hpp
@@ -185,7 +185,7 @@ class PrivateTrackData
 {
 public:
     virtual ~PrivateTrackData() {}
-    virtual int32_t Init() { return 0; }
+    virtual bool Init() { return true; }
 };
 
 class mkv_track_t

modules/demux/mkv/util.cpp
@@ -499,7 +499,7 @@ struct real_audio_private_v5
     uint16_t channels;
 };
 
-int32_t Cook_PrivateTrackData::Init()
+bool Cook_PrivateTrackData::Init()
 {
     // real_audio_private
     bytes.skip(4);                      // fourcc
@@ -518,7 +518,7 @@ int32_t Cook_PrivateTrackData::Init()
     bytes.skip(2);                      // unknown5
 
     if ( i_subpacket_size == 0 )
-        return 0;
+        return false;
 
     if( version == 4 )
     {
@@ -539,14 +539,14 @@ int32_t Cook_PrivateTrackData::Init()
         i_channels      = bytes.GetBE16(); // channels
     }
     else
-        return 0;
+        return false;
     if (bytes.hasErrors())
-        return 0;
+        return false;
 
     size_t i_subpackets = (size_t) i_sub_packet_h * (size_t) i_frame_size / (size_t) i_subpacket_size;
     p_subpackets.resize(i_subpackets);
 
-    return 0;
+    return true;
 }
 
 Cook_PrivateTrackData::~Cook_PrivateTrackData()

modules/demux/mkv/util.hpp
@@ -100,7 +100,7 @@ public:
     Cook_PrivateTrackData(const uint8_t *reader, size_t reader_len):
         bytes(reader, reader_len) {}
     ~Cook_PrivateTrackData();
-    int32_t Init();
+    bool Init() override;
 
     uint32_t coded_frame_size;
     uint16_t i_sub_packet_h;