746f0da32a0c0c358e8c56c71b58579369ae9a1e
Steve Lhomme <robux4@ycbcr.xyz>
2026-01-09 09:26:35 +0100

    decoder_helpers: also check adding the offset to visible dimensions is legit

--- src/input/decoder_helpers.c
+++ src/input/decoder_helpers.c
@@ -210,8 +210,8 @@ int decoder_UpdateVideoOutput( decoder_t *dec, vlc_video_context *vctx_out )
         }
     }
 
-    assert(dec->fmt_out.video.i_visible_width  + dec->fmt_out.video.i_x_offset <= dec->fmt_out.video.i_width);
-    assert(dec->fmt_out.video.i_visible_height + dec->fmt_out.video.i_y_offset <= dec->fmt_out.video.i_height);
+    assert(!ckd_add(&v, dec->fmt_out.video.i_visible_width,  dec->fmt_out.video.i_x_offset) && v <= dec->fmt_out.video.i_width);
+    assert(!ckd_add(&v, dec->fmt_out.video.i_visible_height, dec->fmt_out.video.i_y_offset) && v <= dec->fmt_out.video.i_height);
 
     video_format_AdjustColorSpace( &dec->fmt_out.video );
 
