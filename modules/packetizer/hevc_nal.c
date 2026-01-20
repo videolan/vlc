2eb3f45a4d332d29fef11273973cc59e67c280c0
François Cartegnie <fcvlcdev@free.fr>
2026-01-20 16:20:15 +0100

    packetizer: hevc: cap max_num_negative/positive_pics
    refs #29559
    
    (adapted from commit aa0bb8e56211d7fa7367291f54d9e4db61ae158f)

--- modules/packetizer/hevc_nal.c
+++ modules/packetizer/hevc_nal.c
@@ -759,7 +759,10 @@ static bool hevc_parse_st_ref_pic_set( bs_t *p_bs, unsigned stRpsIdx,
     {
         nal_ue_t num_negative_pics = bs_read_ue( p_bs );
         nal_ue_t num_positive_pics = bs_read_ue( p_bs );
-        if( bs_remain( p_bs ) < ((int64_t)num_negative_pics + num_positive_pics) * 2 )
+        // A.4.2 total to (MaxDpbSize -1) with MaxDpbSize capped either at 16 or 7
+        if( num_negative_pics >= 16 || num_positive_pics >= 16 ||
+            num_negative_pics + num_positive_pics >= 16 ||
+            bs_remain( p_bs ) < ((int64_t)num_negative_pics + num_positive_pics) * 2 )
             return false;
         for(unsigned int i=0; i<num_negative_pics; i++)
         {
