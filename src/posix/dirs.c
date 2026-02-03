videolan/vlc

commit e23555302f928e3ad750e2c8c8960ccf9ecffd58

posix: add $VLC_LIBEXEC_PATH
We already track $pkglibexecdir separately from $pkglibdir when VLC is
installed. This adds it in the build tree.

src/posix/dirs.c
@@ -50,7 +50,7 @@ char *config_GetSysPath(vlc_sysdir_t type, const char *filename)
     static const char env_vars[][16] = {
         [VLC_PKG_DATA_DIR] = "VLC_DATA_PATH",
         [VLC_PKG_LIB_DIR] = "VLC_LIB_PATH",
-        [VLC_PKG_LIBEXEC_DIR] = "VLC_LIB_PATH",
+        [VLC_PKG_LIBEXEC_DIR] = "VLC_LIBEXEC_PATH",
     };
 
     if (type < ARRAY_SIZE(env_vars)) {

bin/vlc.c
@@ -143,6 +143,7 @@ int main(int argc, const char *argv[])
     setenv ("VLC_PLUGIN_PATH", TOP_BUILDDIR"/modules", 1);
     setenv ("VLC_DATA_PATH", TOP_SRCDIR"/share", 1);
     setenv ("VLC_LIB_PATH", TOP_BUILDDIR"/modules", 1);
+    setenv ("VLC_LIBEXEC_PATH", TOP_BUILDDIR"/modules", 1);
 #endif
 
     /* Clear the X.Org startup notification ID. Otherwise the UI might try to

commit 5fe2c1c37ad989127f95d0404a62bdbe1babc92d

posix: dirs: fix libexec
VLC_PKG_LIBEXEC_DIR is truncated, leading to failure to find binaries
from LIBEXEC, and thus Qt interface not starting.

      [255/2305] Compiling C object src/libvlccore.so.9.0.0.p/posix_dirs.c.o
      ../src/posix/dirs.c: In function ‘config_GetSysPath’:
      ../src/posix/dirs.c:53:33: warning: initializer-string for array of ‘char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (17 chars into 16 available) [-Wunterminated-string-initialization]
         53 |         [VLC_PKG_LIBEXEC_DIR] = "VLC_LIBEXEC_PATH",
            |                                 ^~~~~~~~~~~~~~~~~~

Co-authored-by: Fatih Uzunoglu <fuzun54@outlook.com>

Fatih found the root cause and reported it after Gabriel signalled he
couldn't launch the UI anymore.

Regression from e23555302f928e3ad750e2c8c8960ccf9ecffd58.

src/posix/dirs.c
@@ -47,7 +47,7 @@ VLC_WEAK char *config_GetLibDir(void)
 
 char *config_GetSysPath(vlc_sysdir_t type, const char *filename)
 {
-    static const char env_vars[][16] = {
+    static const char env_vars[][17] = {
         [VLC_PKG_DATA_DIR] = "VLC_DATA_PATH",
         [VLC_PKG_LIB_DIR] = "VLC_LIB_PATH",
         [VLC_PKG_LIBEXEC_DIR] = "VLC_LIBEXEC_PATH",