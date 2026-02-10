videolan/vlc

commit 532e81b9395c6ca169b7b7fe7da7104825350c6a

demux: ogg: fix potential double free
refs #29220

modules/demux/ogg.c
@@ -1343,7 +1343,7 @@ static void Ogg_DecodePacket( demux_t *p_demux,
         }
 
         /* Backup the ogg packet (likely an header packet) */
-        if( !b_xiph )
+        if( !b_xiph && p_stream->i_headers )
         {
             uint8_t *p_realloc = realloc( p_stream->p_headers, p_stream->i_headers + p_oggpacket->bytes );
             if( p_realloc )

commit 54a2017590ad1c42cd6238de931d3d4ac2f6aef2

demux: ogg: fix regression with backup headers
regression by 532e81b9395c6ca169b7b7fe7da7104825350c6a

refs #29594

modules/demux/ogg.c
@@ -1394,7 +1394,7 @@ static void Ogg_DecodePacket( demux_t *p_demux,
         }
 
         /* Backup the ogg packet (likely an header packet) */
-        if( !b_xiph && p_stream->i_headers )
+        if( !b_xiph && (p_stream->i_headers + p_oggpacket->bytes) )
         {
             uint8_t *p_realloc = realloc( p_stream->p_headers, p_stream->i_headers + p_oggpacket->bytes );
             if( p_realloc )