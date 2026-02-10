8f3f83837a09241b0af91fcb8229bb1e43f48b52
Thomas Guillem <thomas@gllm.fr>
2026-02-10 06:02:35 +0100

    test: player: add pause_get_time_increase

--- test/src/player/pause.c
+++ test/src/player/pause.c
@@ -102,12 +102,66 @@ test_pause(struct ctx *ctx)
     player_remove_timer(player, &timer);
 }
 
+static void
+test_pause_get_time_increase(struct ctx *ctx)
+{
+    /* Ensure that vlc_player_GetTime() is always increasing, even when
+     * spamming pause/resume. Contrary to timers (async events), we can't
+     * control if vlc_player_GetTime() will be processed when paused or
+     * resumed (async calls), hence this "brute-force" test. */
+    test_log("pause_get_time_increase\n");
+    vlc_player_t *player = ctx->player;
+
+    struct media_params params = DEFAULT_MEDIA_PARAMS(VLC_TICK_FROM_SEC(10));
+    player_set_next_mock_media(ctx, "media1", &params);
+
+    player_start(ctx);
+
+    wait_state(ctx, VLC_PLAYER_STATE_PLAYING);
+
+    {
+        vec_on_position_changed *vec = &ctx->report.on_position_changed;
+        while (vec->size == 0)
+            vlc_player_CondWait(player, &ctx->wait);
+    }
+    vlc_tick_sleep(VLC_TICK_FROM_MS(100));
+
+    vlc_tick_t prev_time = vlc_player_GetTime(player);
+    assert(prev_time >= VLC_TICK_0);
+    for (unsigned i = 0; i < 100; i++)
+    {
+        vlc_tick_t time = vlc_player_GetTime(player);
+        assert(time >= prev_time);
+        prev_time = time;
+
+        vlc_tick_sleep(VLC_TICK_FROM_MS(10));
+
+        time = vlc_player_GetTime(player);
+        assert(time >= prev_time);
+        prev_time = time;
+
+        vlc_player_Pause(player);
+
+        time = vlc_player_GetTime(player);
+        assert(time >= prev_time);
+        prev_time = time;
+
+
+        vlc_player_Resume(player);
+    }
+
+    /* Ensure we stay paused */
+
+    test_end(ctx);
+}
+
 int
 main(void)
 {
     struct ctx ctx;
     ctx_init(&ctx, 0);
     test_pause(&ctx);
+    test_pause_get_time_increase(&ctx);
     ctx_destroy(&ctx);
     return 0;
 }
