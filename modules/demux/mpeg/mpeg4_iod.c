5a0e5d113df5782c7ca2cb580161d8dd7f0ba93e
François Cartegnie <fcvlcdev@free.fr>
2026-03-11 15:32:02 +0700

    demux: mpeg4: restrict sizes reads to 32 bits
    (cherry picked from commit dbcc17481522b3d21cf78f31aa7cd864c39934ed)

--- modules/demux/mpeg/mpeg4_iod.c
+++ modules/demux/mpeg/mpeg4_iod.c
@@ -50,25 +50,19 @@ static void od_debug( vlc_object_t *p_object, const char *format, ... )
  *****************************************************************************/
 static unsigned ODDescriptorLength( unsigned *pi_data, const uint8_t **pp_data )
 {
-    unsigned int i_b;
+    unsigned int i_b = 0x80;
     unsigned int i_len = 0;
 
-    if(*pi_data == 0)
-        return 0;
-
-    do
+    unsigned bytes = __MIN(*pi_data, 4);
+    for(unsigned i=0; i<bytes && (i_b&0x80); i++)
     {
         i_b = **pp_data;
         (*pp_data)++;
         (*pi_data)--;
         i_len = ( i_len << 7 ) + ( i_b&0x7f );
+    }
 
-    } while( i_b&0x80 && *pi_data > 0 );
-
-    if (i_len > *pi_data)
-        i_len = *pi_data;
-
-    return i_len;
+    return __MIN(i_len, *pi_data);
 }
 
 static unsigned ODGetBytes( unsigned *pi_data, const uint8_t **pp_data, size_t bytes )
