a1344b6288d4d39da150d098d67618d4a14b1edf
Thomas Guillem <thomas@gllm.fr>
2026-03-16 14:58:14 +0100

    real: really fix FPE
    corrected version of 3f8a631f4e47ec8343cef6e9c4d114c4b141fb23
    
    Because 1/2 = 0 ...

--- modules/demux/real.c
+++ modules/demux/real.c
@@ -697,7 +697,7 @@ static void DemuxAudioMethod1( demux_t *p_demux, real_track_t *tk, vlc_tick_t i_
     }
     else
     {
-        const int y = tk->i_subpacket_h > 0 ?
+        const int y = tk->i_subpacket_h > 1 ?
                       tk->i_subpacket / (tk->i_subpacket_h / 2) : 0;
         assert( tk->fmt.i_codec == VLC_CODEC_RA_288 );
 
