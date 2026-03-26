dc35967e7a6b752a591a491069cf3d314c549dd5
Fatih Uzunoglu <fuzun54@outlook.com>
2026-03-26 18:42:01 +0200

    qt: register qml singleton instance `UpdateModel`

--- modules/gui/qt/maininterface/mainui.cpp
+++ modules/gui/qt/maininterface/mainui.cpp
@@ -56,6 +56,9 @@
 #include "dialogs/help/aboutmodel.hpp"
 #include "dialogs/dialogs_provider.hpp"
 #include "dialogs/dialogs/dialogmodel.hpp"
+#ifdef UPDATE_CHECK
+#include "dialogs/help/help.hpp" // UpdateModel
+#endif
 #include "util/vlchotkeyconverter.hpp"
 
 #include "network/networkmediamodel.hpp"
@@ -237,6 +240,10 @@ void MainUI::registerQMLTypes()
         qmlRegisterUncreatableType<CSDButtonModel>(uri, versionMajor, versionMinor, "CSDButtonModel", "has CSD buttons and provides for communicating CSD events between UI and backend");
         qmlRegisterTypesAndRevisions<CSDMenu>( uri, versionMajor);
         qmlRegisterUncreatableType<NavigationAttached>( uri, versionMajor, versionMinor, "Navigation", "Navigation is only available via attached properties");
+#ifdef UPDATE_CHECK
+        qmlRegisterSingletonInstance<UpdateModel>( uri, versionMajor, versionMinor, "UpdateModel", m_mainCtx->getUpdateModel() );
+#endif
+
         qmlRegisterModule(uri, versionMajor, versionMinor);
         qmlProtectModule(uri, versionMajor);
     }
