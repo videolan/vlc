871b608f56a732b1156a21bd5d923f8c0c4a6004
Francesco Montefoschi <francesco.montefoschi@datalogic.com>
2026-03-26 09:24:25 +0100

    DirectShow: allow NV12 video format
    (cherry picked from commit 42e66c82bd7e2180af846773ff6fcefcc41e2fd9)

--- modules/access/dshow/filter.cpp
+++ modules/access/dshow/filter.cpp
@@ -137,6 +137,10 @@ int GetFourCCFromMediaType( const AM_MEDIA_TYPE &media_type )
             else if( media_type.subtype == MEDIASUBTYPE_YVU9 )
                i_fourcc = VLC_CODEC_I410;
 
+            /* Semi-planar YUV formats */
+            else if( media_type.subtype == MEDIASUBTYPE_NV12 )
+               i_fourcc = VLC_CODEC_NV12;
+
             /* Packed YUV formats */
             else if( media_type.subtype == MEDIASUBTYPE_YVYU )
                i_fourcc = VLC_CODEC_YVYU;
