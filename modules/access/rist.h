d85635e4e98b029aca39fa7c4a8ff71ba793478d
François Cartegnie <fcvlcdev@free.fr>
2026-03-31 19:27:48 +0700

    access: rist: use vlc setters/getters

--- modules/access/rist.h
+++ modules/access/rist.h
@@ -29,6 +29,8 @@
 #include <errno.h>
 #include <assert.h>
 
+#include <vlc_common.h>
+
 /*****************************************************************************
  * Public API
  *****************************************************************************/
@@ -112,26 +114,24 @@ struct rist_flow {
 
 static inline uint16_t rtcp_fb_nack_get_range_start(const uint8_t *p_rtcp_fb_nack)
 {
-    return (p_rtcp_fb_nack[0] << 8) | p_rtcp_fb_nack[1];
+    return GetWBE(p_rtcp_fb_nack);
 }
 
 static inline uint16_t rtcp_fb_nack_get_range_extra(const uint8_t *p_rtcp_fb_nack)
 {
-    return (p_rtcp_fb_nack[2] << 8) | p_rtcp_fb_nack[3];
+    return GetWBE(&p_rtcp_fb_nack[2]);
 }
 
 static inline void rtcp_fb_nack_set_range_start(uint8_t *p_rtcp_fb_nack,
                                               uint16_t start)
 {
-    p_rtcp_fb_nack[0] = (start >> 8) & 0xff;
-    p_rtcp_fb_nack[1] = start & 0xff;
+    SetWBE(p_rtcp_fb_nack, start);
 }
 
 static inline void rtcp_fb_nack_set_range_extra(uint8_t *p_rtcp_fb_nack,
                                                  uint16_t extra)
 {
-    p_rtcp_fb_nack[2] = (extra >> 8) & 0xff;
-    p_rtcp_fb_nack[3] = extra & 0xff;
+    SetWBE(&p_rtcp_fb_nack[2], extra);
 }
 
 static inline void populate_cname(int fd, char *identifier)
@@ -379,26 +379,22 @@ static bool is_multicast_address(char *psz_dst_server)
 
 static inline uint32_t rist_rtp_get_timestamp(const uint8_t *p_rtp)
 {
-    return (p_rtp[4] << 24) | (p_rtp[5] << 16) | (p_rtp[6] << 8) | p_rtp[7];
+    return GetDWBE(&p_rtp[4]);
 }
 
 static inline void rist_rtp_set_timestamp(uint8_t *p_rtp, uint32_t i_timestamp)
 {
-    p_rtp[4] = (i_timestamp >> 24) & 0xff;
-    p_rtp[5] = (i_timestamp >> 16) & 0xff;
-    p_rtp[6] = (i_timestamp >> 8) & 0xff;
-    p_rtp[7] = i_timestamp & 0xff;
+    SetDWBE(&p_rtp[4], i_timestamp);
 }
 
 static inline uint16_t rist_rtp_get_seqnum(const uint8_t *p_rtp)
 {
-    return (p_rtp[2] << 8) | p_rtp[3];
+    return GetWBE(&p_rtp[2]);
 }
 
 static inline void rist_rtp_set_seqnum(uint8_t *p_rtp, uint16_t i_seqnum)
 {
-    p_rtp[2] = i_seqnum >> 8;
-    p_rtp[3] = i_seqnum & 0xff;
+    SetWBE(&p_rtp[2], i_seqnum);
 }
 
 static inline int8_t rist_rtcp_sdes_get_name_length(const uint8_t *p_rtcp_sdes)
@@ -408,14 +404,13 @@ static inline int8_t rist_rtcp_sdes_get_name_length(const uint8_t *p_rtcp_sdes)
 
 static inline uint16_t rist_rtcp_get_length(const uint8_t *p_rtcp)
 {
-    return (p_rtcp[2] << 8) | p_rtcp[3];
+    return GetWBE(&p_rtcp[2]);
 }
 
 static inline void rist_rtcp_set_length(uint8_t *p_rtcp,
                                       uint16_t length)
 {
-    p_rtcp[2] = length >> 8;
-    p_rtcp[3] = length & 0xff;
+    SetWBE(&p_rtcp[2], length);
 }
 
 static inline uint8_t rist_rtcp_get_pt(const uint8_t *p_rtcp)
@@ -445,10 +440,7 @@ static inline void rist_rtcp_rr_set_pt(uint8_t *p_rtcp_rr)
 
 static inline void rist_rtcp_fb_set_int_ssrc_pkt_sender(uint8_t *p_rtcp_fb, uint32_t i_ssrc)
 {
-    p_rtcp_fb[4] = (i_ssrc >> 24) & 0xff;
-    p_rtcp_fb[5] = (i_ssrc >> 16) & 0xff;
-    p_rtcp_fb[6] = (i_ssrc >> 8) & 0xff;
-    p_rtcp_fb[7] = i_ssrc & 0xff;
+    SetDWBE(&p_rtcp_fb[4], i_ssrc);
 }
 
 static inline void rist_rtp_set_cc(uint8_t *p_rtp, uint8_t i_cc)
@@ -491,38 +483,30 @@ static inline uint16_t rist_rtcp_fb_nack_get_packet_id(const uint8_t *p_rtcp_fb_
 static inline void rist_rtcp_fb_nack_set_packet_id(uint8_t *p_rtcp_fb_nack,
                                               uint16_t packet_id)
 {
-    p_rtcp_fb_nack[0] = (packet_id >> 8) & 0xff;
-    p_rtcp_fb_nack[1] = packet_id & 0xff;
+    SetWBE(p_rtcp_fb_nack, packet_id);
 }
 
 static inline uint16_t rist_rtcp_fb_nack_get_bitmask_lost(const uint8_t *p_rtcp_fb_nack)
 {
-    return (p_rtcp_fb_nack[2] << 8) | p_rtcp_fb_nack[3];
+    return GetWBE(&p_rtcp_fb_nack[2]);
 }
 
 static inline void rist_rtcp_fb_nack_set_bitmask_lost(uint8_t *p_rtcp_fb_nack,
                                                  uint16_t bitmask)
 {
-    p_rtcp_fb_nack[2] = (bitmask >> 8) & 0xff;
-    p_rtcp_fb_nack[3] = bitmask & 0xff;
+    SetWBE(&p_rtcp_fb_nack[2], bitmask);
 }
 
 static inline void rist_rtcp_fb_get_ssrc_media_src(const uint8_t *p_rtcp_fb,
                                               uint8_t pi_ssrc[4])
 {
-    pi_ssrc[0] = p_rtcp_fb[8];
-    pi_ssrc[1] = p_rtcp_fb[9];
-    pi_ssrc[2] = p_rtcp_fb[10];
-    pi_ssrc[3] = p_rtcp_fb[11];
+    memcpy(&pi_ssrc[0], &p_rtcp_fb[8], 4);
 }
 
 static inline void rist_rtcp_fb_set_ssrc_media_src(uint8_t *p_rtcp_fb,
                                               const uint8_t pi_ssrc[4])
 {
-    p_rtcp_fb[8] = pi_ssrc[0];
-    p_rtcp_fb[9] = pi_ssrc[1];
-    p_rtcp_fb[10] = pi_ssrc[2];
-    p_rtcp_fb[11] = pi_ssrc[3];
+    memcpy(&p_rtcp_fb[8], &pi_ssrc[0], 4);
 }
 
 static inline void rist_rtcp_sr_set_pt(uint8_t *p_rtcp_sr)
@@ -539,46 +523,31 @@ static inline void rist_rtcp_sr_set_length(uint8_t *p_rtcp_sr,
 static inline void rist_rtcp_sr_set_ntp_time_msw(uint8_t *p_rtcp_sr,
                                             uint32_t ntp_time_msw)
 {
-    p_rtcp_sr[8] = (ntp_time_msw >> 24) & 0xff;
-    p_rtcp_sr[9] = (ntp_time_msw >> 16) & 0xff;
-    p_rtcp_sr[10] = (ntp_time_msw >> 8) & 0xff;
-    p_rtcp_sr[11] = ntp_time_msw & 0xff;
+    SetDWBE(&p_rtcp_sr[8], ntp_time_msw);
 }
 
 static inline void rist_rtcp_sr_set_ntp_time_lsw(uint8_t *p_rtcp_sr,
                                             uint32_t ntp_time_lsw)
 {
-    p_rtcp_sr[12] = (ntp_time_lsw >> 24) & 0xff;
-    p_rtcp_sr[13] = (ntp_time_lsw >> 16) & 0xff;
-    p_rtcp_sr[14] = (ntp_time_lsw >> 8) & 0xff;
-    p_rtcp_sr[15] = ntp_time_lsw & 0xff;
+    SetDWBE(&p_rtcp_sr[12], ntp_time_lsw);
 }
 
 static inline void rist_rtcp_sr_set_rtp_time(uint8_t *p_rtcp_sr,
                                             uint32_t rtp_time)
 {
-    p_rtcp_sr[16] = (rtp_time >> 24) & 0xff;
-    p_rtcp_sr[17] = (rtp_time >> 16) & 0xff;
-    p_rtcp_sr[18] = (rtp_time >> 8) & 0xff;
-    p_rtcp_sr[19] = rtp_time & 0xff;
+    SetDWBE(&p_rtcp_sr[16], rtp_time);
 }
 
 static inline void rist_rtcp_sr_set_packet_count(uint8_t *p_rtcp_sr,
                                             uint32_t packet_count)
 {
-    p_rtcp_sr[20] = (packet_count >> 24) & 0xff;
-    p_rtcp_sr[21] = (packet_count >> 16) & 0xff;
-    p_rtcp_sr[22] = (packet_count >> 8) & 0xff;
-    p_rtcp_sr[23] = packet_count & 0xff;
+    SetDWBE(&p_rtcp_sr[20], packet_count);
 }
 
 static inline void rist_rtcp_sr_set_octet_count(uint8_t *p_rtcp_sr,
                                             uint32_t octet_count)
 {
-    p_rtcp_sr[24] = (octet_count >> 24) & 0xff;
-    p_rtcp_sr[25] = (octet_count >> 16) & 0xff;
-    p_rtcp_sr[26] = (octet_count >> 8) & 0xff;
-    p_rtcp_sr[27] = octet_count & 0xff;
+    SetDWBE(&p_rtcp_sr[24], octet_count);
 }
 
 static inline void rist_rtp_set_type(uint8_t *p_rtp, uint8_t i_type)
@@ -588,8 +557,5 @@ static inline void rist_rtp_set_type(uint8_t *p_rtp, uint8_t i_type)
 
 static inline void rist_rtp_set_int_ssrc(uint8_t *p_rtp, uint32_t i_ssrc)
 {
-    p_rtp[8] = (i_ssrc >> 24) & 0xff;
-    p_rtp[9] = (i_ssrc >> 16) & 0xff;
-    p_rtp[10] = (i_ssrc >> 8) & 0xff;
-    p_rtp[11] = i_ssrc & 0xff;
+    SetDWBE(&p_rtp[8], i_ssrc);
 }
