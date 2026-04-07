4dddcefc7a6547f3de0aa73ff27fe35d6f32d2a5
François Cartegnie <fcvlcdev@free.fr>
2026-04-07 12:23:54 +0700

    video_filter: gaussianblur: replace xmalloc and check alloc
    (adapted from commit bbed282f603743ccf04160fce129d468bc9f2030)

--- modules/video_filter/gaussianblur.c
+++ modules/video_filter/gaussianblur.c
@@ -103,11 +103,13 @@ struct filter_sys_t
     type_t *pt_scale;
 };
 
-static void gaussianblur_InitDistribution( filter_sys_t *p_sys )
+static int gaussianblur_InitDistribution( filter_sys_t *p_sys )
 {
     double f_sigma = p_sys->f_sigma;
     int i_dim = (int)(3.*f_sigma);
-    type_t *pt_distribution = xmalloc( (2*i_dim+1) * sizeof( type_t ) );
+    type_t *pt_distribution = vlc_alloc( 2*i_dim+1, sizeof( type_t ) );
+    if( pt_distribution == NULL )
+        return VLC_ENOMEM;
 
     for( int x = -i_dim; x <= i_dim; x++ )
     {
@@ -123,6 +125,8 @@ static void gaussianblur_InitDistribution( filter_sys_t *p_sys )
     }
     p_sys->i_dim = i_dim;
     p_sys->pt_distribution = pt_distribution;
+
+    return VLC_SUCCESS;
 }
 
 static int Create( vlc_object_t *p_this )
@@ -165,7 +169,11 @@ static int Create( vlc_object_t *p_this )
         msg_Err( p_filter, "sigma must be greater than zero" );
         return VLC_EGENERIC;
     }
-    gaussianblur_InitDistribution( p_filter->p_sys );
+    if( gaussianblur_InitDistribution( p_filter->p_sys ) != VLC_SUCCESS )
+    {
+        free( p_filter->p_sys );
+        return VLC_ENOMEM;
+    }
     msg_Dbg( p_filter, "gaussian distribution is %d pixels wide",
              p_filter->p_sys->i_dim*2+1 );
 
@@ -208,6 +216,8 @@ static picture_t *Filter( filter_t *p_filter, picture_t *p_pic )
         p_sys->pt_buffer = realloc_or_free( p_sys->pt_buffer,
                                p_pic->p[Y_PLANE].i_visible_lines *
                                p_pic->p[Y_PLANE].i_pitch * sizeof( type_t ) );
+        if( !p_sys->pt_buffer )
+            return NULL;
     }
 
     pt_buffer = p_sys->pt_buffer;
@@ -217,7 +227,9 @@ static picture_t *Filter( filter_t *p_filter, picture_t *p_pic )
         const int i_visible_pitch = p_pic->p[Y_PLANE].i_visible_pitch;
         const int i_pitch = p_pic->p[Y_PLANE].i_pitch;
 
-        p_sys->pt_scale = xmalloc( i_visible_lines * i_pitch * sizeof( type_t ) );
+        p_sys->pt_scale = malloc( i_visible_lines * i_pitch * sizeof( type_t ) );
+        if( p_sys->pt_scale == NULL )
+            return NULL;
         pt_scale = p_sys->pt_scale;
 
         for( int i_line = 0; i_line < i_visible_lines; i_line++ )
