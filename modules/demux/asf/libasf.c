videolan/vlc

commit cddfefc80fd3fad92b1235b286763f6946c7640a

ASF: macro removal

modules/demux/asf/libasf.c
@@ -700,6 +700,14 @@ static void ASF_FreeObject_codec_list( asf_object_t *p_obj )
     FREENULL( p_cl->codec );
 }
 
+static inline char *get_wstring( const uint8_t *p_data, size_t i_size )
+{
+    char *psz_str = FromCharset( "UTF-16LE", p_data, i_size );
+    if( psz_str )
+        p_data += i_size;
+    return psz_str;
+}
+
 /* Microsoft should go to hell. This time the length give number of bytes
  * and for the some others object, length give char16 count ... */
 static int ASF_ReadObject_content_description(stream_t *s, asf_object_t *p_obj)
@@ -710,13 +718,6 @@ static int ASF_ReadObject_content_description(stream_t *s, asf_object_t *p_obj)
 
     if( ( i_peek = stream_Peek( s, &p_peek, p_cd->i_object_size ) ) < 34 )
        return VLC_EGENERIC;
-
-/* FIXME i_size*3 is the worst case. */
-#define GETSTRINGW( psz_str, i_size ) do { \
-    psz_str = FromCharset( "UTF-16LE", p_data, i_size ); \
-    if( psz_str ) { \
-        p_data += i_size; \
-    } } while(0)
 
     p_data = p_peek + 24;
 
@@ -729,13 +730,11 @@ static int ASF_ReadObject_content_description(stream_t *s, asf_object_t *p_obj)
     if( !ASF_HAVE( i_title+i_artist+i_copyright+i_description+i_rating ) )
         return VLC_EGENERIC;
 
-    GETSTRINGW( p_cd->psz_title, i_title );
-    GETSTRINGW( p_cd->psz_artist, i_artist );
-    GETSTRINGW( p_cd->psz_copyright, i_copyright );
-    GETSTRINGW( p_cd->psz_description, i_description );
-    GETSTRINGW( p_cd->psz_rating, i_rating );
-
-#undef  GETSTRINGW
+    p_cd->psz_title = get_wstring( p_data, i_title );
+    p_cd->psz_artist = get_wstring( p_data, i_artist );
+    p_cd->psz_copyright = get_wstring( p_data, i_copyright );
+    p_cd->psz_description = get_wstring( p_data, i_description );
+    p_cd->psz_rating = get_wstring( p_data, i_rating );
 
 #ifdef ASF_DEBUG
     msg_Dbg( s,

commit b9ef042036e636a82983022f1714bf95af92ef6b

demux: asf: fix content description fields reading same offset
Regression from cddfefc80f. Fixes #26217

modules/demux/asf/libasf.c
@@ -745,11 +745,10 @@ error:
     return VLC_EGENERIC;
 }
 
-static inline char *get_wstring( const uint8_t *p_data, size_t i_size )
+static inline char *get_wstring( const uint8_t **pp_data, size_t i_size )
 {
-    char *psz_str = FromCharset( "UTF-16LE", p_data, i_size );
-    if( psz_str )
-        p_data += i_size;
+    char *psz_str = FromCharset( "UTF-16LE", *pp_data, i_size );
+    *pp_data += i_size;
     return psz_str;
 }
 
@@ -779,11 +778,11 @@ static int ASF_ReadObject_content_description(stream_t *s, asf_object_t *p_obj)
     if( !ASF_HAVE( i_title+i_artist+i_copyright+i_description+i_rating ) )
         return VLC_EGENERIC;
 
-    p_cd->psz_title = get_wstring( p_data, i_title );
-    p_cd->psz_artist = get_wstring( p_data, i_artist );
-    p_cd->psz_copyright = get_wstring( p_data, i_copyright );
-    p_cd->psz_description = get_wstring( p_data, i_description );
-    p_cd->psz_rating = get_wstring( p_data, i_rating );
+    p_cd->psz_title = get_wstring( &p_data, i_title );
+    p_cd->psz_artist = get_wstring( &p_data, i_artist );
+    p_cd->psz_copyright = get_wstring( &p_data, i_copyright );
+    p_cd->psz_description = get_wstring( &p_data, i_description );
+    p_cd->psz_rating = get_wstring( &p_data, i_rating );
 
 #ifdef ASF_DEBUG
     msg_Dbg( s,