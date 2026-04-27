5fbc6904a2e4a96e9956a5d8503ac4db4ebaf702
François Cartegnie <fcvlcdev@free.fr>
2026-04-27 16:33:42 +0700

    codec: cvdsub: refactor RLE unpacking
    fix #29864
    
    (adapted picked from commit c2a9b0766817a392ac3231ef883728e2f2c9625c)

--- modules/codec/cvdsub.c
+++ modules/codec/cvdsub.c
@@ -594,8 +594,7 @@ static void RenderImage( decoder_t *p_dec, block_t *p_data,
     decoder_sys_t *p_sys = p_dec->p_sys;
     uint8_t *p_dest = p_region->p_picture->Y_PIXELS;
     int i_field;            /* The subtitles are interlaced */
-    int i_row, i_column;    /* scanline row/column number */
-    uint8_t i_color, i_count;
+    size_t i_row, i_column; /* scanline row/column number */
     bs_t bs;
 
     bs_init( &bs, p_data->p_buffer + p_sys->i_image_offset,
@@ -608,31 +607,27 @@ static void RenderImage( decoder_t *p_dec, block_t *p_data,
             for( i_column = 0; i_column < p_sys->i_width; i_column++ )
             {
                 uint8_t i_val = bs_read( &bs, 4 );
+                uint8_t *p = &p_dest[i_row * p_region->p_picture->Y_PITCH + i_column];
+                size_t i_count;
+                uint8_t i_color;
 
                 if( i_val == 0 )
                 {
                     /* Fill the rest of the line with next color */
                     i_color = bs_read( &bs, 4 );
-
-                    memset( &p_dest[i_row * p_region->p_picture->Y_PITCH +
-                                    i_column], i_color,
-                            p_sys->i_width - i_column );
+                    i_count = p_sys->i_width - i_column;
                     i_column = p_sys->i_width;
-                    continue;
                 }
                 else
                 {
                     /* Normal case: get color and repeat count */
                     i_count = (i_val >> 2);
                     i_color = i_val & 0x3;
-
                     i_count = __MIN( i_count, p_sys->i_width - i_column );
-
-                    memset( &p_dest[i_row * p_region->p_picture->Y_PITCH +
-                                    i_column], i_color, i_count );
                     i_column += i_count - 1;
-                    continue;
                 }
+
+                memset( p, i_color, i_count );
             }
 
             bs_align( &bs );
