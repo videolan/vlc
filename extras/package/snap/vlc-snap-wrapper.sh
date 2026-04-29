fd47a08fe3aa28e4856466009511258ecf95b69d
Pierre Lamot <pierre@videolabs.io>
2026-04-29 14:21:52 +0200

    snap: force x11 backend
    see #29476
    
    we where hopping for an update of gpu2024 in early 2026, but this didn't
    happend. Ubuntu 20.26 LTS force Wayland with pretty much every DE and KDE hasn't
    start porting QT/KDE to core26, so we need a workaround for the time being.

--- extras/package/snap/vlc-snap-wrapper.sh
+++ extras/package/snap/vlc-snap-wrapper.sh
@@ -32,6 +32,9 @@ export KDE_FORK_SLAVES=1
 ## Neon PATCH! make KIO look for slaves in a dynamic location depending on $SNAP
 #export KF5_LIBEXEC_DIR=$SNAP/usr/lib/$ARCH/libexec/kf5
 
+#libva+wayland is broken with gpu2404 base snap, force X11
+export QT_QPA_PLATFORM=xcb
+
 # Link the aacs directory from $HOME #28017
 if [ ! -L "$HOME/.config/aacs" ]; then
     ln -s $SNAP_REAL_HOME/.config/aacs $HOME/.config/aacs
