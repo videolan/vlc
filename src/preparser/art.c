17b47e55c005c087c4006a542ac5eff3a12e5734
Niklas Burtscher <niklas.burtscher@assessify.at>
2026-05-02 21:23:31 +0200

    art: harden album art save failures

--- src/preparser/art.c
+++ src/preparser/art.c
@@ -173,8 +173,8 @@ static char *ArtCacheNameFromDirPath( const char *psz_path, const char *psz_type
     char *psz_ext = strdup( psz_type ? psz_type : "" );
     char *psz_filename = NULL;
 
-    if( !psz_path )
-        goto end;
+    if( unlikely( !psz_path || !psz_ext ) )
+    goto end;
 
     ArtCacheCreateDir( psz_path );
     filename_sanitize( psz_ext );
@@ -326,46 +326,62 @@ int input_FindArtInCacheUsingItemUID( input_item_t *p_item )
 int input_SaveArt( vlc_object_t *obj, input_item_t *p_item,
                    const void *data, size_t length, const char *psz_type )
 {
+    int i_ret = VLC_EGENERIC;
     char *psz_filename = ArtCacheName( p_item, psz_type );
 
     if( !psz_filename )
+    {
         return VLC_EGENERIC;
+    }
 
     char *psz_uri = vlc_path2uri( psz_filename, "file" );
     if( !psz_uri )
-    {
-        free( psz_filename );
-        return VLC_EGENERIC;
-    }
+        {
+            free( psz_filename );
+            return VLC_EGENERIC;
+        }
 
     /* Check if we already dumped it */
     struct stat s;
     if( !vlc_stat( psz_filename, &s ) )
     {
         input_item_SetArtURL( p_item, psz_uri );
-        free( psz_filename );
-        free( psz_uri );
-        return VLC_SUCCESS;
+        i_ret = VLC_SUCCESS;
+        goto save_uid;
     }
 
     /* Dump it otherwise */
     FILE *f = vlc_fopen( psz_filename, "wb" );
-    if( f )
+    if( !f )
     {
-        if( fwrite( data, 1, length, f ) != length )
-        {
-            msg_Err( obj, "%s: %s", psz_filename, vlc_strerror_c(errno) );
-        }
-        else
-        {
-            msg_Dbg( obj, "album art saved to %s", psz_filename );
-            input_item_SetArtURL( p_item, psz_uri );
-        }
-        fclose( f );
+        msg_Err( obj, "%s: %s", psz_filename, vlc_strerror_c(errno) );
+        goto end;
     }
-    free( psz_uri );
 
+    if( fwrite( data, 1, length, f ) != length )
+    {
+        msg_Err( obj, "%s: %s", psz_filename, vlc_strerror_c(errno) );
+        fclose( f );
+        vlc_unlink( psz_filename );
+        goto end;
+    }
+
+    if( fclose( f ) )
+    {
+        msg_Err( obj, "%s: %s", psz_filename, vlc_strerror_c(errno) );
+        vlc_unlink( psz_filename );
+        goto end;
+    }
+
+    msg_Dbg( obj, "album art saved to %s", psz_filename );
+    input_item_SetArtURL( p_item, psz_uri );
+    i_ret = VLC_SUCCESS;
+
+save_uid:
     /* save uid info */
+    if( i_ret != VLC_SUCCESS )
+        goto end;
+
     char *uid = input_item_GetInfo( p_item, "uid", "md5" );
     if ( ! *uid )
     {
@@ -388,5 +404,5 @@ int input_SaveArt( vlc_object_t *obj, input_item_t *p_item,
 end:
     free( psz_uri );
     free( psz_filename );
-return VLC_SUCCESS;
+return i_ret;
 }
