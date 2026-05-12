cc19d191e18bed4bf8ce5ffb61d359372521a838
Steve Lhomme <robux4@ycbcr.xyz>
2026-05-12 09:05:04 +0200

    fourcc: don't compute the number of bytes
    We can use the number of bits and we don't need to check when the
    fourcc is not one of the 4 raw VLC formats.

--- src/misc/fourcc.c
+++ src/misc/fourcc.c
@@ -33,7 +33,6 @@
 #include <vlc_es.h>
 #include <vlc_chroma_probe.h>
 #include <assert.h>
-#include <limits.h>
 
 #include "fourcc_tables.h"
 
@@ -133,72 +132,51 @@ vlc_fourcc_t vlc_fourcc_GetCodecFromString( int i_cat, const char *psz_fourcc )
 
 vlc_fourcc_t vlc_fourcc_GetCodecAudio( vlc_fourcc_t i_fourcc, int i_bits )
 {
-    const int i_bytes = i_bits > (INT_MAX - 7) ? 0 : ( i_bits + 7 ) / 8;
-
     if( i_fourcc == VLC_FOURCC( 'a', 'f', 'l', 't' ) )
     {
-        switch( i_bytes )
-        {
-        case 4:
+        if ( i_bits >= 25 && i_bits <= 32 )
             return VLC_CODEC_FL32;
-        case 8:
+        if ( i_bits >= 57 && i_bits <= 64 )
             return VLC_CODEC_FL64;
-        default:
-            return 0;
-        }
+        return 0;
     }
-    else if( i_fourcc == VLC_FOURCC( 'a', 'r', 'a', 'w' ) )
+    if( i_fourcc == VLC_FOURCC( 'a', 'r', 'a', 'w' ) )
     {
-        switch( i_bytes )
-        {
-        case 1:
+        if ( i_bits >= 1 && i_bits <= 8 )
             return VLC_CODEC_U8;
-        case 2:
+        if ( i_bits >= 9 && i_bits <= 16 )
             return VLC_CODEC_S16L;
-        case 3:
+        if ( i_bits >= 17 && i_bits <= 24 )
             return VLC_CODEC_S24L;
-        case 4:
+        if ( i_bits >= 25 && i_bits <= 32 )
             return VLC_CODEC_S32L;
-        default:
-            return 0;
-        }
+        return 0;
     }
-    else if( i_fourcc == VLC_FOURCC( 't', 'w', 'o', 's' ) )
+    if( i_fourcc == VLC_FOURCC( 't', 'w', 'o', 's' ) )
     {
-        switch( i_bytes )
-        {
-        case 1:
+        if ( i_bits >= 1 && i_bits <= 8 )
             return VLC_CODEC_S8;
-        case 2:
+        if ( i_bits >= 9 && i_bits <= 16 )
             return VLC_CODEC_S16B;
-        case 3:
+        if ( i_bits >= 17 && i_bits <= 24 )
             return VLC_CODEC_S24B;
-        case 4:
+        if ( i_bits >= 25 && i_bits <= 32 )
             return VLC_CODEC_S32B;
-        default:
-            return 0;
-        }
+        return 0;
     }
-    else if( i_fourcc == VLC_FOURCC( 's', 'o', 'w', 't' ) )
+    if( i_fourcc == VLC_FOURCC( 's', 'o', 'w', 't' ) )
     {
-        switch( i_bytes )
-        {
-        case 1:
+        if ( i_bits >= 1 && i_bits <= 8 )
             return VLC_CODEC_S8;
-        case 2:
+        if ( i_bits >= 9 && i_bits <= 16 )
             return VLC_CODEC_S16L;
-        case 3:
+        if ( i_bits >= 17 && i_bits <= 24 )
             return VLC_CODEC_S24L;
-        case 4:
+        if ( i_bits >= 25 && i_bits <= 32 )
             return VLC_CODEC_S32L;
-        default:
-            return 0;
-        }
+        return 0;
     }
-    else
-    {
-        return vlc_fourcc_GetCodec( AUDIO_ES, i_fourcc );
-    }
+    return vlc_fourcc_GetCodec( AUDIO_ES, i_fourcc );
 }
 
 const char *vlc_fourcc_GetDescription(int cat, vlc_fourcc_t fourcc)
