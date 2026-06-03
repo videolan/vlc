370d216a2b3c0b5164ad5f21a8d4c2009ba160ed
Alexandre Janniaux <ajanni@videolabs.io>
2026-06-03 17:46:05 +0200

    opengl: move vlc_gl_picture usage to public header

--- modules/video_output/opengl/picture.h
+++ modules/video_output/opengl/picture.h
@@ -27,6 +27,7 @@
 
 #include <vlc_es.h>
 #include <vlc_picture.h>
+#include <vlc_opengl_picture.h>
 #include "gl_common.h"
 
 /**
@@ -44,103 +45,5 @@ struct vlc_gl_format {
     uint32_t formats[PICTURE_PLANE_MAX];
     bool half_float;
 };
-
-/**
- * OpenGL picture
- *
- * It can only be properly used if its format, described by a vlc_gl_format, is
- * known.
- */
-struct vlc_gl_picture {
-    GLuint textures[PICTURE_PLANE_MAX];
-
-    /**
-     * Matrix to convert from 2D pictures coordinates to texture coordinates
-     *
-     * tex_coords =     mtx    × pic_coords
-     *
-     *  / tex_x \    / a b c \   / pic_x \
-     *  \ tex_y / =  \ d e f / × | pic_y |
-     *                           \   1   /
-     *
-     * It is stored in column-major order: [a, d, b, e, c, f].
-     */
-    float mtx[2*3];
-
-    /**
-     * Indicate if the transform to convert picture coordinates to textures
-     * coordinates have changed due to the last picture.
-     *
-     * The filters should check this flag on every draw() call, and update
-     * their coordinates if necessary.
-     *
-     * It is guaranteed to be true for the first picture.
-     */
-    bool mtx_has_changed;
-};
-
-/**
- * Convert from picture coordinates to texture coordinates, which can be used to
- * sample at the correct location.
- *
- * This is a equivalent to retrieve the matrix and multiply manually.
- *
- * The picture and texture coords may point to the same memory, in that case
- * the transformation is applied in place (overwriting the picture coordinates
- * by the texture coordinates).
- *
- * \param picture the OpenGL picture
- * \param coords_count the number of coordinates (x,y) coordinates to convert
- * \param pic_coords picture coordinates as an array of 2*coords_count floats
- * \param tex_coords_out texture coordinates as an array of 2*coords_count
- *                       floats
- */
-void
-vlc_gl_picture_ToTexCoords(const struct vlc_gl_picture *pic,
-                           unsigned coords_count, const float *pic_coords,
-                           float *tex_coords_out);
-
-/**
- * Return a matrix to orient texture coordinates
- *
- * This matrix is 2x2 and is stored in column-major order.
- *
- * While pic_to_tex_matrix transforms any picture coordinates into texture
- * coordinates, it may be useful for example for vertex or fragment shaders to
- * sample one pixel to the left of the current one, or two pixels to the top.
- * Since the input texture may be rotated or flipped, the shaders need to
- * know in which direction is the top and in which direction is the right of
- * the picture.
- *
- * This 2x2 matrix allows to transform a 2D vector expressed in picture
- * coordinates into a 2D vector expressed in texture coordinates.
- *
- * Concretely, it contains the coordinates (U, V) of the transformed unit
- * vectors u = / 1 \ and v = / 0 \:
- *             \ 0 /         \ 1 /
- *
- *     / Ux Vx \
- *     \ Uy Vy /
- *
- * It is guaranteed that:
- *  - both U and V are unit vectors (this matrix does not change the scaling);
- *  - only one of their components have a non-zero value (they may not be
- *    oblique); in other words, here are the possible values for U and V:
- *
- *        /  0 \  or  / 0 \  or  / 1 \  or  / -1 \
- *        \ -1 /      \ 1 /      \ 0 /      \  0 /
- *
- *  - U and V are orthogonal.
- *
- * Therefore, there are 8 possible matrices (4 possible rotations, flipped or
- * not).
- *
- * It may theoretically change on every picture (the transform matrix provided
- * by Android may change). If it has changed since the last picture, then
- * pic->mtx_has_changed is true.
- */
-void
-vlc_gl_picture_ComputeDirectionMatrix(const struct vlc_gl_picture *pic,
-                                      float direction[2*2]);
 
 #endif
