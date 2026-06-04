videolan/vlc

commit faf9f526c9ba3fe3cda8dc12f48d0cc12cfb5ea1

avcodec/chroma: prioritize RGB mapping with the same mask
If the mask corresponds exactly, we have an exact match. If the RGB source
has a mask but none of the RGB mapping uses the same mask, we map to
a close RGB format with a different mask.

It's better than using the first VLC_CODEC_RGBxx in the list if there's
one corresponding to the exact match.

Another option would be to move the VLC_RGB_ES() calls at the end of the
list.

(cherry picked from commit a309fb4c10f238bb2319abe9ec489daa0c91521c)
Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

modules/codec/avcodec/chroma.c
@@ -198,14 +198,23 @@ int GetFfmpegChroma( int *restrict i_ffmpeg_chroma, const video_format_t *fmt )
 {
     for( int i = 0; chroma_table[i].i_chroma != 0; i++ )
     {
-        if( chroma_table[i].i_chroma == fmt->i_chroma )
+        if( chroma_table[i].i_chroma == fmt->i_chroma &&
+            chroma_table[i].i_rmask  == fmt->i_rmask &&
+            chroma_table[i].i_gmask  == fmt->i_gmask &&
+            chroma_table[i].i_bmask  == fmt->i_bmask )
         {
-            if( ( chroma_table[i].i_rmask == 0 &&
-                  chroma_table[i].i_gmask == 0 &&
-                  chroma_table[i].i_bmask == 0 ) ||
-                ( chroma_table[i].i_rmask == fmt->i_rmask &&
-                  chroma_table[i].i_gmask == fmt->i_gmask &&
-                  chroma_table[i].i_bmask == fmt->i_bmask ) )
+            return chroma_table[i].i_chroma_id;
+        }
+    }
+    // try again without the mask as they may not correspond exactly
+    if (fmt->i_rmask || fmt->i_gmask || fmt->i_bmask)
+    {
+        for( int i = 0; chroma_table[i].i_chroma != 0; i++ )
+        {
+            if( chroma_table[i].i_chroma == fmt->i_chroma &&
+                chroma_table[i].i_rmask == 0 &&
+                chroma_table[i].i_gmask == 0 &&
+                chroma_table[i].i_bmask == 0 )
             {
                 *i_ffmpeg_chroma = chroma_table[i].i_chroma_id;
                 return VLC_SUCCESS;

commit 8ffca71177fc753c8ed914e93cb875353c124fac

avcodec: chroma: fix chroma id return value
This fixes a regression from faf9f526c9ba3fe3cda8dc12f48d0cc12cfb5ea1,
where the backport assumed that the function returned a chroma ID. While
this is the case in 4.0, VLC 3.0 still returns an error code and fills
a param return value directly.

This was caught by the Medialibrary CI with the latest 3.0.x branch.

modules/codec/avcodec/chroma.c
@@ -204,7 +204,8 @@ int GetFfmpegChroma( int *restrict i_ffmpeg_chroma, const video_format_t *fmt )
             chroma_table[i].i_gmask  == fmt->i_gmask &&
             chroma_table[i].i_bmask  == fmt->i_bmask )
         {
-            return chroma_table[i].i_chroma_id;
+            *i_ffmpeg_chroma = chroma_table[i].i_chroma_id;
+            return VLC_SUCCESS;
         }
     }
     // try again without the mask as they may not correspond exactly