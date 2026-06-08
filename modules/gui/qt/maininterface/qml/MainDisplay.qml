videolan/vlc

commit f5be290dc6df16afb4de13e428865afb2d38e47b

qml: pixel align layer texture in `MainDisplay`
Since the previous patch removed using `sourceVisualRect`,
where `PartialEffect` does pixel alignment implicitly, we
now have to do it ourselves in `layer.sourceRect`.

modules/gui/qt/maininterface/qml/MainDisplay.qml
@@ -269,7 +269,20 @@ FocusScope {
                 // need to use background coloring. It is currently a todo to further reduce video memory
                 // consumption by covering the effect for only the area of interest, currently the blur
                 // effect does not support having an extension area for postprocessing.
-                layer.sourceRect: Qt.rect(0, 0, Math.min(stackView.width + edgeExtension, stackViewParent.width), height + edgeExtension)
+                layer.sourceRect: Qt.rect(0, 0,
+                                          Helpers.alignUp(Math.min(stackView.width + edgeExtension, stackViewParent.width), alignNumber),
+                                          Helpers.alignUp(height + edgeExtension, alignNumber))
+
+                property real eDPR: MainCtx.effectiveDevicePixelRatio(Window.window) || 1.0
+                readonly property int alignNumber: Helpers.denominatorForFloat(eDPR)
+
+                Connections {
+                    target: MainCtx
+
+                    function onIntfDevicePixelRatioChanged() {
+                        stackViewParent.eDPR = MainCtx.effectiveDevicePixelRatio(stackViewParent.Window.window) || 1.0
+                    }
+                }
 
                 Rectangle {
                     // Extension of parent rectangle for the bottom extension.
@@ -286,13 +299,13 @@ FocusScope {
 
                     // Setting `height` does not seem to work here. Anchoring the effect is not very nice, but it works:
                     anchors.fill: stackViewParent // WARNING: layered item is not necessarily the visual parent of its layer effect.
-                    anchors.bottomMargin: -stackViewParent.edgeExtension
+                    anchors.bottomMargin: (stackViewParent.height - stackViewParent.layer.sourceRect.height)
                     // Layer width is limited to `stackView` width to save memory, in `PartialEffect` the source visual uses the
                     // size of the `PartialEffect` unless `sourceVisualRect` is used, so we define the boundary in `PartialEffect`
                     // for the source visual here. The effect rect exceeds the boundaries of `PartialEffect` due to this (see
                     // `effectRect`), which is not particularly nice, but there is not much we can do about that here without
                     // using `sourceVisualRect`, and saving video memory is considered more important:
-                    anchors.rightMargin: (stackViewParent.width - stackView.width) - stackViewParent.edgeExtension
+                    anchors.rightMargin: (stackViewParent.width - stackViewParent.layer.sourceRect.width)
 
                     blending: stackViewParent.color.a < (1.0 - Number.EPSILON)
 

commit 52f1fe41f122bdc3846fc68bf1a7952667408ded

qml: make background cover fractional scale extension in `MainDisplay` layer
This fixes artifacting with fractional scale when mini player is shown and
playlist pane is closed.

Regression since f5be290d.

modules/gui/qt/maininterface/qml/MainDisplay.qml
@@ -324,9 +324,16 @@ FocusScope {
                 }
 
                 Rectangle {
-                    // Extension of parent rectangle for edge extension.
+                    // Extension of parent rectangle for edge and fractional scale alignment extension.
                     anchors.fill: parent
-                    anchors.margins: -stackViewParent.edgeExtension
+                    anchors.margins: -(stackViewParent.edgeExtension + fractionalScaleExtensionSize)
+
+                    // With fractional scale, if we align up the layer size to make sure the size is
+                    // an integer, we also need to extend here because the background must cover the
+                    // extension area. 8 should be enough for the fractions that we care (.25, .5, .75).
+                    // Note that the background extension here does not consume additional video memory.
+                    readonly property real fractionalScaleExtensionSize: (stackViewParent.alignNumber > 1 ? 8.0 : 0.0)
+
                     visible: stackViewParent.layer.enabled && (height > 0 && width > 0)
                     // We can't simply adjust the color because the color might not be opaque,
                     // so we use border instead. Note that border is always placed inside the