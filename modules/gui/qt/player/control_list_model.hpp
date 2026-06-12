ed412664d8e1a8608f72fe98ceed66f0a3a8719e
Pierre Lamot <pierre@videolabs.io>
2026-06-12 11:34:20 +0200

    qt: mark available control widgets id in the enum
    Available id are getting scarce before fixed IDs, make sure we won't have collision
    when attributing future widgets id.

--- modules/gui/qt/player/control_list_model.hpp
+++ modules/gui/qt/player/control_list_model.hpp
@@ -34,6 +34,8 @@ public:
         ID_ROLE = Qt::UserRole
     };
 
+    //changin the enum order or inserting new ID before the end will break user configuration
+    //RESERVED_0x__ are available for future use, they can be repurposed
     enum ControlType
     {
         PLAY_BUTTON,
@@ -67,6 +69,7 @@ public:
         PLAYBACK_SPEED_BUTTON,
         HIGH_RESOLUTION_TIME_WIDGET,
         FRAME_PREV_BUTTON,
+        RESERVED_0X1F,
 
         SPLITTER = 0x20,
         VOLUME,
@@ -79,13 +82,37 @@ public:
         NAVIGATION_BUTTONS,
         PROGRAM_BUTTON,
         NAVIGATION_BOX,
-        SPECIAL_MAX,
+        RESERVED_0X2B,
+        RESERVED_0X2C,
+        RESERVED_0X2D,
+        RESERVED_0X2E,
+        RESERVED_0X2F,
+        RESERVED_0X30,
+        RESERVED_0X31,
+        RESERVED_0X32,
+        RESERVED_0X33,
+        RESERVED_0X34,
+        RESERVED_0X35,
+        RESERVED_0X36,
+        RESERVED_0X37,
+        RESERVED_0X38,
+        RESERVED_0X39,
+        RESERVED_0X3A,
+        RESERVED_0X3B,
+        RESERVED_0X3C,
+        RESERVED_0X3D,
+        RESERVED_0X3E,
+        RESERVED_0X3F,
 
         WIDGET_SPACER = 0x40,
         WIDGET_SPACER_EXTEND,
         WIDGET_MAX,
     };
     Q_ENUM(ControlType)
+
+    static_assert(RESERVED_0X1F == 0x1F, "missaligned");
+    static_assert(RESERVED_0X2B == 0x2B, "missaligned");
+    static_assert(RESERVED_0X3F == 0x3F, "missaligned");
 
     // Basic functionality:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
