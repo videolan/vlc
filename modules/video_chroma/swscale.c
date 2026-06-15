6805e4584e6bafa77dd22f26049ea0cd49a7c17a
Tristan Matthews <tmatth@videolan.org>
2026-06-15 08:09:16 -0400

    swscale: allow for per-frame palette changes
    Fixes #24859 and #25017

--- modules/video_chroma/swscale.c
+++ modules/video_chroma/swscale.c
@@ -673,9 +673,10 @@ static void Convert( filter_t *p_filter, struct SwsContext *ctx,
                p_src, i_plane_count, b_swap_uvi );
     if( p_filter->fmt_in.video.i_chroma == VLC_CODEC_RGBP )
     {
-        if( p_filter->fmt_in.video.p_palette )
+        /* The palette may change every picture, that's why we check it here. */
+        const video_palette_t *p_palette = p_src->format.p_palette;
+        if( p_palette )
         {
-            const video_palette_t *p_palette = p_filter->fmt_in.video.p_palette;
             static_assert(sizeof(p_palette->palette) == AVPALETTE_SIZE,
                           "Palette size mismatch between vlc and libavutil");
             uint8_t *dstp = palette;
