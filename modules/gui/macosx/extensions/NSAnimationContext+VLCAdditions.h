89d125ba000e7dccfd7c73d3dbfc8fe98e7e931a
joseanemsilva <joseanemaria.dasilva@georgebrown.ca>
2026-06-17 23:17:32 -0400

    macosx: Add NSAnimationContext extension file
    This extension is implemented to add support to reduce motion user preferences.

---
+++ modules/gui/macosx/extensions/NSAnimationContext+VLCAdditions.h
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ * NSAnimationContext+VLCAdditions.h MacOS X interface module
+ *****************************************************************************
+ * Copyright (C) 2026 VLC authors and VideoLAN
+ *
+ * Authors: Joseane Silva
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSAnimationContext (VLCAdditions)
+
++ (void) runAnimationRespectingPreferencesWithDuration:(NSTimeInterval) duration
+                                               changes:(void (^)(NSAnimationContext *)) changes 
+                                     completionHandler:(nullable void (^)()) completionHandler;
+
++ (void) runAnimationRespectingPreferencesWithChanges: (void (^)(NSAnimationContext *)) changes 
+                                    completionHandler:(nullable void (^)()) completionHandler;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ modules/gui/macosx/extensions/NSAnimationContext+VLCAdditions.m
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ * NSAnimationContext+VLCAdditions.m MacOS X interface module
+ *****************************************************************************
+ * Copyright (C) 2026 VLC authors and VideoLAN
+ *
+ * Authors: Joseane Silva
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#import "NSAnimationContext+VLCAdditions.h"
+
+@implementation NSAnimationContext (VLCAdditions)
+
++ (void) runAnimationRespectingPreferences:(NSNumber *) duration
+                                   changes:(void (^)(NSAnimationContext *)) changes 
+                         completionHandler:(nullable void (^)()) completionHandler
+{
+    const BOOL reduceMotion = NSWorkspace.sharedWorkspace.accessibilityDisplayShouldReduceMotion;
+
+    [NSAnimationContext runAnimationGroup:^(NSAnimationContext *context){
+        if (duration != nil) {
+            context.duration = reduceMotion ? 0 : duration.doubleValue;
+        }
+        changes(context);
+    } completionHandler:completionHandler];
+}
+
+
++ (void) runAnimationRespectingPreferencesWithDuration:(NSTimeInterval) duration
+                                               changes:(void (^)(NSAnimationContext *)) changes 
+                                     completionHandler:(nullable void (^)()) completionHandler
+{
+    [self runAnimationRespectingPreferences:@(duration)
+                                    changes:changes
+                          completionHandler:completionHandler];
+}
+
++ (void) runAnimationRespectingPreferencesWithChanges:(void (^)(NSAnimationContext *)) changes 
+                                    completionHandler:(nullable void (^)()) completionHandler
+{
+    [self runAnimationRespectingPreferences:nil
+                                    changes:changes
+                          completionHandler:completionHandler];
+}
+
+@end
