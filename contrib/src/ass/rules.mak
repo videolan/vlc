videolan/vlc

commit 3a3cd6f0471282bcf21d137b3c9dae3d14045905

contrib: ass: build with meson

contrib/src/ass/rules.mak
@@ -40,30 +40,28 @@ $(TARBALLS)/libass-$(ASS_VERSION).tar.gz:
 
 libass: libass-$(ASS_VERSION).tar.gz .sum-ass
 	$(UNPACK)
-	$(call update_autoconfig,.)
-	$(call pkg_static,"libass.pc.in")
 	$(MOVE)
 
 DEPS_ass = freetype2 $(DEPS_freetype2) fribidi $(DEPS_fribidi) iconv $(DEPS_iconv) harfbuzz $(DEPS_harfbuzz)
 
-ASS_CONF = --disable-test
+ASS_CONF = -Dauto_features=disabled
 ifneq ($(WITH_FONTCONFIG), 0)
 DEPS_ass += fontconfig $(DEPS_fontconfig)
+ASS_CONF += -Dfontconfig=enabled
 else
-ASS_CONF += --disable-fontconfig --disable-require-system-font-provider
+ASS_CONF += -Drequire-system-font-provider=false
 endif
 
 ifeq ($(WITH_DWRITE), 1)
-ASS_CONF += --enable-directwrite
+ASS_CONF += -Ddirectwrite=enabled
 endif
 
-ifeq ($(WITH_ASS_ASM), 0)
-ASS_CONF += --disable-asm
+ifeq ($(WITH_ASS_ASM), 1)
+ASS_CONF += -Dasm=enabled
 endif
 
-.ass: libass
-	$(MAKEBUILDDIR)
-	$(MAKECONFIGURE) $(ASS_CONF)
-	+$(MAKEBUILD)
-	+$(MAKEBUILD) install
+.ass: libass crossfile.meson
+	$(MESONCLEAN)
+	$(MESON) $(ASS_CONF)
+	+$(MESONBUILD)
 	touch $@

commit f8dc762ea0bb640b4ef3d06f9279303c994d35cb

contrib/ass: enable coretext on anyAppleOS
This solves ASS rendering by fixing the font lookup.

Regression from 3a3cd6f047

contrib/src/ass/rules.mak
@@ -16,6 +16,7 @@ WITH_FONTCONFIG = 0
 else
 ifdef HAVE_DARWIN_OS
 WITH_FONTCONFIG = 0
+WITH_CORETEXT = 1
 else
 ifdef HAVE_WIN32
 WITH_FONTCONFIG = 0
@@ -51,6 +52,10 @@ endif
 
 ifeq ($(WITH_DWRITE), 1)
 ASS_CONF += -Ddirectwrite=enabled
+endif
+
+ifeq ($(WITH_CORETEXT), 1)
+ASS_CONF += -Dcoretext=enabled
 endif
 
 ifeq ($(WITH_ASS_ASM), 1)