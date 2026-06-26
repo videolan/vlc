bcaeca1281c4c6f5990a8d7120b9c79422f537d8
Felix Paul Kühne <fkuehne@videolan.org>
2026-06-26 20:30:48 +0200

    build/anyAppleOS: disable C23 to keep libtool ObjC tag inference working
    autoconf 2.73 adds -std=gnu23 to CC but not OBJC, so libtool can no
    longer infer the tag for .m compiles and aborts.
    
    This disables c23 to workaround until the full objc language support is
    added to libtool, which is well under way.
    
    (cherry picked from commit 5d30bfaf6a943ed3db7af944604b892bf69d5c1e) (rebased)
    rebased:
    - VLC 3 has more ac_cv_func_XXX overriding around
    Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

--- extras/package/macosx/env.build.sh
+++ extras/package/macosx/env.build.sh
@@ -116,6 +116,9 @@ vlcSetSymbolEnvironment() {
     # - Added symbols in macOS 10.13 / iOS 11 / watchOS 4 / tvOS 11
     # - Added symbol in macOS 10.14 / iOS 12 / tvOS 9
     # - Added symbols in macOS 10.15 / iOS 13 / tvOS 13
+    #
+    # ac_cv_prog_cc_c23: autoconf 2.73 adds -std=gnu23 to CC but not OBJC
+    #                    breaking libtool's ObjC tag inference.
     "${cmd}" "$@" \
     ac_cv_lib_network_connect=no \
     \
@@ -129,6 +132,8 @@ vlcSetSymbolEnvironment() {
     ac_cv_func_readlinkat=no \
     ac_cv_func_linkat=no \
     ac_cv_func_unlinkat=no \
+    \
+    ac_cv_prog_cc_c23=no \
     \
     ac_cv_func_basename_r=no \
     ac_cv_func_clock_getres=no \
