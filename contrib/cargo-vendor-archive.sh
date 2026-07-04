0db4148aa113f8f0dd3e46ce7e4721ce6e83c71e
Matthias Dressel <code@deadcode.eu>
2026-07-04 17:40:39 +0200

    contrib: rust: Sanitize cargo-vendor-archive.sh
    Fix all warnings from shellcheck.

--- contrib/cargo-vendor-archive.sh
+++ contrib/cargo-vendor-archive.sh
@@ -11,7 +11,7 @@ Fetch and archive all dependencies from a Rust archive using 'cargo vendor'.
 EOF
 }
 
-if [ "x$1" = "x" ]; then
+if [ "$#" -lt 1 ]; then
     usage
     exit 1
 fi
@@ -20,25 +20,25 @@ fi
 CARGO=cargo
 
 # Extract archive into a tmp dir
-TMP_DIR=.tmp-$(basename $1)
-rm -rf ${TMP_DIR}
-mkdir ${TMP_DIR}
+TMP_DIR=.tmp-$(basename "$1")
+rm -rf "${TMP_DIR}"
+mkdir "${TMP_DIR}"
 
-tar xf "$1" -C ${TMP_DIR}
-cd ${TMP_DIR}/*
+tar xf "$1" -C "${TMP_DIR}"
+cd "${TMP_DIR}"/*
 
 # Fetch all dependencies
 eval ${CARGO} vendor --locked
 
 # Archive all dependencies
-name=$(basename `pwd`)-vendor
+name=$(basename "$PWD")-vendor
 tar -jcf "../../${name}.tar.bz2" vendor --transform "s,vendor,${name},"
 cd ../..
-rm -rf ${TMP_DIR}
+rm -rf "${TMP_DIR}"
 
 echo ""
 echo "Please upload this package '${name}.tar.bz2' to our VideoLAN FTP,"
 echo ""
 echo "and write the following checksum into the contrib/src/<project>/cargo-vendor-SHA512SUMS:"
 echo ""
-sha512sum ${name}.tar.bz2
+sha512sum "${name}.tar.bz2"
