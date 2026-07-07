videolan/vlc

commit 52d4078f58afc72b80b93a64490e8ad77fde8e2c

package/apple: set the proper C flag to target xros
Found in the CMake code.

extras/package/apple/build.sh
@@ -217,7 +217,7 @@ set_deployment_target()
         VLC_DEPLOYMENT_TARGET_CFLAG="${VLC_DEPLOYMENT_TARGET_CFLAG}-version-min=${VLC_DEPLOYMENT_TARGET}"
         VLC_DEPLOYMENT_TARGET_LDFLAG="${VLC_DEPLOYMENT_TARGET_CFLAG}"
     else
-        VLC_DEPLOYMENT_TARGET_CFLAG=""
+        VLC_DEPLOYMENT_TARGET_CFLAG="--target=${VLC_HOST_ARCH}-apple-xros${VLC_DEPLOYMENT_TARGET}"
         VLC_DEPLOYMENT_TARGET_LDFLAG=""
     fi
 }

commit b8789e12d07ebe85ebc032ac022565c99767b0b3

package/apple: append -simulator to the xros target triple
This fixes a regression from  52d4078f which always set a device target
triple breaking simulator builds.

The proposed change matches the previous implicit behavior when we had
a empty CFLAG.

extras/package/apple/build.sh
@@ -218,6 +218,9 @@ set_deployment_target()
         VLC_DEPLOYMENT_TARGET_LDFLAG="${VLC_DEPLOYMENT_TARGET_CFLAG}"
     else
         VLC_DEPLOYMENT_TARGET_CFLAG="--target=${VLC_HOST_ARCH}-apple-xros${VLC_DEPLOYMENT_TARGET}"
+        if [ -n "$VLC_HOST_PLATFORM_SIMULATOR" ]; then
+            VLC_DEPLOYMENT_TARGET_CFLAG="${VLC_DEPLOYMENT_TARGET_CFLAG}-simulator"
+        fi
         VLC_DEPLOYMENT_TARGET_LDFLAG=""
     fi
 }