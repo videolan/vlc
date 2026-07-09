videolan/vlc

commit ee9ea36529e70199d5c81894e1e8dae6b7cd4446

extras/tools: build xz before tar
The reason we build tar is because it's missing xz support.

extras/tools/tools.mak
@@ -140,7 +140,7 @@ tar: tar-$(TAR_VERSION).tar.bz2
 	$(UNPACK)
 	$(MOVE)
 
-.buildtar: tar
+.buildtar: .xz tar
 	(cd $<; ./configure --prefix=$(PREFIX) && $(MAKE) && $(MAKE) install)
 	touch $@
 

commit 98efd7a1cebf707c481557c33d597d89de7b6c0a

extras/tools: Fix tar compilation
This has been broken since ee9ea36529e70199d5c81894e1e8dae6b7cd4446.

The first instruction in the recipe is `cd $<`.
"'$<' The name of the first prerequisite." [1]

Ref: videolan/vlc!5356
[1] https://www.gnu.org/software/make/manual/html_node/Automatic-Variables.html

extras/tools/tools.mak
@@ -159,7 +159,7 @@ tar: tar-$(TAR_VERSION).tar.bz2
 	$(UNPACK)
 	$(MOVE)
 
-.buildtar: .xz tar
+.buildtar: tar .xz
 	cd $<; ./configure --prefix=$(PREFIX)
 	+$(MAKE) -C $<
 	+$(MAKE) -C $< install