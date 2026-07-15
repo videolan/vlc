4dcaab5f513083df7d9c67a7d9053d47360f8d50
Steve Lhomme <robux4@ycbcr.xyz>
2026-07-15 07:39:04 +0200

    dxgi_fmt: add missing Moore Threads vendor name
    Missing from 1d65d4fa6e657d1d036fec4cb6ddfe46f2ce0483.

--- modules/video_chroma/dxgi_fmt.c
+++ modules/video_chroma/dxgi_fmt.c
@@ -155,6 +155,7 @@ const char *DxgiVendorStr(unsigned int gpu_vendor)
         { GPU_MANUFACTURER_S3,       "S3 Graphics" },
         { GPU_MANUFACTURER_QUALCOMM, "Qualcomm"    },
         { GPU_MANUFACTURER_MICROSOFT, "Microsoft"  },
+        { GPU_MANUFACTURER_MTGPU,    "Moore Threads" },
         { 0,                         "Unknown" }
     };
 
