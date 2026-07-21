videolan/vlc

commit 9c84ef60dda78d275c1f85b875a1ddae9c49bb65

qml: port Network & Service pages to PageExt

modules/gui/qt/network/qml/ServicesHomeDisplay.qml
@@ -36,7 +36,7 @@ Widgets.PageLoader {
         component: serviceSourceComponent
     }, {
         name: "services_manage",
-        url: "qrc:///qt/qml/VLC/Network/ServicesManage.qml"
+        component: serviceManageComponent
     }, {
         name: "source_root",
         component: sourceRootComponent
@@ -53,7 +53,7 @@ Widgets.PageLoader {
     property bool enableEndFade: true
 
     function _showServiceHome(reason) {
-        History.push([...root.pagePrefix, "services"], reason)
+        History.push([...root.pagePrefix, "all"], reason)
     }
 
     function _showServiceManage(reason) {
@@ -93,12 +93,35 @@ Widgets.PageLoader {
     Component {
         id: serviceSourceComponent
 
-        ServicesSources {
-            onBrowseServiceManage:  (reason) => root._showServiceManage(reason)
-            onBrowseSourceRoot: (name, reason) => root. _showServiceRoot(name, reason)
+        Widgets.PageExt {
+
+            title: qsTr("Services")
+
+            ServicesSources {
+                id: serviceSource
+
+                anchors.fill: parent
+
+                onBrowseServiceManage:  (reason) => root._showServiceManage(reason)
+                onBrowseSourceRoot: (name, reason) => root. _showServiceRoot(name, reason)
+            }
         }
     }
 
+    Component {
+        id: serviceManageComponent
+
+        Widgets.PageExt {
+
+            title: qsTr("Install services")
+
+            ServicesManage {
+                id: serviceSource
+
+                anchors.fill: parent
+            }
+        }
+    }
 
     Component {
         id: sourceRootComponent
@@ -106,15 +129,11 @@ Widgets.PageLoader {
         BrowseTreeDisplay {
             property alias source_name: deviceModel.source_name
 
-            property Component localMenuDelegate: NetworkAddressbar {
-                path: [{display: deviceModel.name, tree: {}}]
-
-                onHomeButtonClicked: _showServiceHome(reason)
-            }
-
             model: deviceModel
             contextMenu: contextMenu
+            headerPath: [{display: deviceModel.name, tree: {}}]
 
+            onHomeButtonClicked: reason => _showServiceHome(reason)
             onBrowse: (tree, reason) => {
                 root._showServiceNode(tree, deviceModel.source_name, reason)
             }
@@ -148,25 +167,13 @@ Widgets.PageLoader {
             property alias tree: mediaModel.tree
             property string source_name
 
-            property Component localMenuDelegate: NetworkAddressbar {
-                path: {
-                    const _path = mediaModel.path
-                    _path.unshift({display: root_name, tree: {"source_name": source_name, "isRoot": true}})
-                    return _path
-                }
-
-                onHomeButtonClicked: root._showServiceHome(reason)
-
-                onBrowse: (tree, reason) => {
-                    if (tree.isRoot)
-                        root._showServiceRoot(source_name, reason)
-                    else
-                        root._showServiceNode(tree, source_name, reason)
-                }
+            onBrowse: (tree, reason) => {
+                if (tree.isRoot)
+                    root._showServiceRoot(source_name, reason)
+                else
+                    root._showServiceNode(tree, source_name, reason)
             }
-
-            onBrowse: (tree, reason) => root._showServiceNode(tree, source_name, reason)
-
+            onHomeButtonClicked: (tree, reason) => root._showServiceHome(reason)
             onCurrentIndexChanged: History.viewProp.initialIndex = currentIndex
 
             model: NetworkMediaModel {
@@ -176,6 +183,13 @@ Widgets.PageLoader {
                 searchPattern: MainCtx.search.pattern
                 sortOrder: MainCtx.sort.order
                 sortCriteria: MainCtx.sort.criteria
+            }
+
+            headerPath:  {
+                let _path =  [{display: mediaModel.name, tree: {"source_name": source_name, "isRoot": true}}]
+                _path.concat(mediaModel.path)
+
+                return _path
             }
 
             contextMenu: NetworkMediaContextMenu {

modules/gui/qt/medialibrary/qml/MusicAlbumsDisplay.qml
@@ -32,6 +32,8 @@ Widgets.PageExt {
     MusicAlbums {
         anchors.fill: parent
 
+        focus: true
+
         searchPattern: MainCtx.search.pattern
         sortCriteria: MainCtx.sort.criteria
         sortOrder: MainCtx.sort.order

modules/gui/qt/medialibrary/qml/MusicArtistsDisplay.qml
@@ -58,6 +58,8 @@ Widgets.PageLoader {
 
                 anchors.fill: parent
 
+                focus: true
+
                 searchPattern: MainCtx.search.pattern
                 sortOrder: MainCtx.sort.order
                 sortCriteria: MainCtx.sort.criteria

modules/gui/qt/medialibrary/qml/MusicGenresDisplay.qml
@@ -55,6 +55,8 @@ Widgets.PageLoader {
 
                 anchors.fill: parent
 
+                focus: true
+
                 displayMarginBeginning: root.displayMarginBeginning
                 displayMarginEnd: root.displayMarginEnd
 
@@ -86,6 +88,8 @@ Widgets.PageLoader {
                 id: albumsView
 
                 anchors.fill: parent
+
+                focus: true
 
                 searchPattern: MainCtx.search.pattern
                 sortOrder: MainCtx.sort.order

modules/gui/qt/medialibrary/qml/MusicPlaylistsDisplay.qml
@@ -64,6 +64,8 @@ Widgets.PageLoader {
 
                 anchors.fill: parent
 
+                focus: true
+
                 isMusic: true
 
                 searchPattern: MainCtx.search.pattern

modules/gui/qt/medialibrary/qml/MusicTracksDisplay.qml
@@ -50,6 +50,9 @@ Widgets.PageExt {
     FocusScope {
 
         anchors.fill: parent
+
+        focus: true
+
         // Aliases
 
         property alias leftPadding: tracklistdisplay_id.leftPadding

modules/gui/qt/medialibrary/qml/VideoAllSubDisplay.qml
@@ -75,6 +75,8 @@ Widgets.PageExt {
 
         anchors.fill: parent
 
+        focus: true
+
         model: _meta?.model ?? null
 
         contextMenu: MLContextMenu { model: _meta ? _meta.model : null; showPlayAsAudioAction: true }

modules/gui/qt/medialibrary/qml/VideoGroupDisplay.qml
@@ -52,6 +52,8 @@ Widgets.PageExt {
 
         anchors.fill: parent
 
+        focus: true
+
         displayMarginBeginning: root.displayMarginBeginning
         displayMarginEnd: root.displayMarginEnd
         enableBeginningFade: root.enableBeginningFade

commit d84790b46c1205fe3183c854acfa7cdd3c2ddf21

qml: restore Radio Browser search and sort bindings
Resolves a regression introduced by commits 9c84ef60dd and 9ed87ebc62.
BrowseTreeDisplay's search input was not reaching the models because
search/sort states were decentralized from MainCtx to PageExt.

Fixes #29955

modules/gui/qt/network/qml/ServicesHomeDisplay.qml
@@ -113,6 +113,8 @@ Widgets.PageLoader {
         id: sourceRootComponent
 
         BrowseTreeDisplay {
+            id: sourceRootPage
+
             property alias source_name: deviceModel.source_name
 
             model: deviceModel
@@ -132,9 +134,9 @@ Widgets.PageLoader {
                 ctx: MainCtx
                 sd_source: NetworkDeviceModel.CAT_INTERNET
 
-                searchPattern: MainCtx.search.pattern
-                sortOrder: MainCtx.sort.order
-                sortCriteria: MainCtx.sort.criteria
+                searchPattern: sourceRootPage.search.pattern
+                sortOrder: sourceRootPage.sort.order
+                sortCriteria: sourceRootPage.sort.criteria
             }
 
             NetworkDeviceContextMenu {
@@ -150,6 +152,8 @@ Widgets.PageLoader {
         id: sourceBrowseComponent
 
         BrowseTreeDisplay {
+            id: sourceBrowsePage
+
             property alias tree: mediaModel.tree
             property string source_name
 
@@ -166,9 +170,9 @@ Widgets.PageLoader {
                 id: mediaModel
                 ctx: MainCtx
 
-                searchPattern: MainCtx.search.pattern
-                sortOrder: MainCtx.sort.order
-                sortCriteria: MainCtx.sort.criteria
+                searchPattern: sourceBrowsePage.search.pattern
+                sortOrder: sourceBrowsePage.sort.order
+                sortCriteria: sourceBrowsePage.sort.criteria
             }
 
             headerPath:  {