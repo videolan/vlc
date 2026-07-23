videolan/vlc

commit 5ea729abe44d9ae6c7a9492d9a4be9a414159438

player: assert that position is in [0;1] range
This is a kind of API missusage that we need to notify early (by aborting).

src/player/input.c
@@ -124,6 +124,10 @@ vlc_player_input_SeekByPos(struct vlc_player_input *input, double position,
     vlc_player_t *player = input->player;
     vlc_player_assert_seek_params(speed, whence);
 
+    /* Assert before we modify position internally, this only check that user
+     * positions are correct. */
+    assert(position >= 0 && position <=1);
+
     if (whence != VLC_PLAYER_WHENCE_ABSOLUTE)
         position += vlc_player_input_GetPos(input, true, vlc_tick_now());
 

commit 46c85aef2cf2f0b4ceff0ea340a937675c1e797f

player: fix assert when seeking to negative relative pos
Regression from 5ea729abe44d9ae6c7a9492d9a4be9a414159438

src/player/input.c
@@ -126,10 +126,15 @@ vlc_player_input_SeekByPos(struct vlc_player_input *input, double position,
 
     /* Assert before we modify position internally, this only check that user
      * positions are correct. */
-    assert(position >= 0 && position <=1);
-
     if (whence != VLC_PLAYER_WHENCE_ABSOLUTE)
+    {
+        assert(position >= -1 && position <=1);
         position += vlc_player_input_GetPos(input, true, vlc_tick_now());
+    }
+    else
+    {
+        assert(position >= 0 && position <=1);
+    }
 
     if (position < 0)
         position = 0;