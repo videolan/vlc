videolan/vlc

commit e018cc44508a62b381a5cbf256693a970cdc20b0

mp4: fix off-by-one reading with nul-terminated string

modules/demux/mp4/libmp4.c
@@ -105,16 +105,14 @@ static char *mp4_getstringz( uint8_t **restrict in, uint64_t *restrict size )
     assert( *size <= SSIZE_MAX );
 
     size_t len = strnlen( (const char *)*in, *size );
-    if( len == 0 )
+    if( len == 0 || len >= *size )
         return NULL;
 
-    char *ret = malloc( len + 1 );
-    if( likely(ret != NULL) )
-    {
-        memcpy( ret, *in, len );
-        ret[len] = '\0';
-    }
     len++;
+
+    char *ret = malloc( len );
+    if( likely(ret != NULL) )
+        memcpy( ret, *in, len );
     *in += len;
     *size -= len;
     return ret;

commit a4d89aaca7f5058673dd2ee6404ea12c10ce1fcc

demux: mp4: handle non terminated strings
regression by e018cc44508a62b381a5cbf256693a970cdc20b0

modules/demux/mp4/libmp4.c
@@ -151,22 +151,23 @@ static char *mp4_getstringz( uint8_t **restrict in, uint64_t *restrict size )
     if( *size == 0 )
         return NULL;
 
-    if( *in == 0 ) /* Null string stored */
+    size_t len = strnlen( (const char *)*in, *size );
+    if( len == 0 ) /* Null string stored */
     {
         *in += 1;
         *size -= 1;
         return NULL;
     }
 
-    size_t len = strnlen( (const char *)*in, *size );
-    if( len == 0 || len >= *size )
-        return NULL;
-
-    len++;
-
-    char *ret = malloc( len );
+    const bool b_terminated = len < *size;
+    char *ret = malloc( len + 1 );
     if( likely(ret != NULL) )
+    {
         memcpy( ret, *in, len );
+        ret[len] = 0; // ensure termination
+    }
+    if( b_terminated ) // terminated
+        len++;
     *in += len;
     *size -= len;
     return ret;