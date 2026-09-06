859369a7d8800aa96c0b77bc1d394b315afdfa9c
Claudio Cambra <developer@claudiocambra.com>
2026-09-07 00:40:40 +0800

    macosx: Remove trailing semicolon from adError in VLCErrorWindowController
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- modules/gui/macosx/windows/VLCErrorWindowController.m
+++ modules/gui/macosx/windows/VLCErrorWindowController.m
@@ -57,7 +57,7 @@
     }
 }
 
-- (void)addError:(NSString *)title withMsg:(NSString *)message;
+- (void)addError:(NSString *)title withMsg:(NSString *)message
 {
     /* format our string as desired */
     NSMutableAttributedString * ourError;
