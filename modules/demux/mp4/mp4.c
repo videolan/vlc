videolan/vlc

commit 07a2ad663da8631e904f22b0deee29de96a38bca

demux: mp4: avoid using sbgp if *pi_sync_sample was not modified
*pi_sync_sample can never be modified in that code block if *pi_sync_sample == i_sample.

No need to set the return value as we can only get in this block if the previous one was successful.

modules/demux/mp4/mp4.c
@@ -3413,17 +3413,19 @@ static int TrackGetNearestSeekPoint( demux_t *p_demux, mp4_track_t *p_track,
         }
     }
 
-    /* try or refine using RAP with recovery roll info */
-    uint32_t i_alternative_sync_sample = i_sample;
-    if( MP4_SampleToGroupInfo( p_track->p_stbl, i_sample, SAMPLEGROUP_rap,
-                               0, &i_alternative_sync_sample, true, NULL ) )
+    if (*pi_sync_sample != i_sample)
     {
-        i_ret = VLC_SUCCESS;
-        msg_Dbg( p_demux, "tk %u sbgp gives %d --> %" PRIu32 " (sample number)",
-                 p_track->i_track_ID, i_sample, i_alternative_sync_sample );
-        if( i_alternative_sync_sample > *pi_sync_sample &&
-            i_alternative_sync_sample < i_sample )
-            *pi_sync_sample = i_alternative_sync_sample;
+        /* try or refine using RAP with recovery roll info */
+        uint32_t i_alternative_sync_sample = i_sample;
+        if( MP4_SampleToGroupInfo( p_track->p_stbl, i_sample, SAMPLEGROUP_rap,
+                                0, &i_alternative_sync_sample, true, NULL ) )
+        {
+            msg_Dbg( p_demux, "tk %u sbgp gives %d --> %" PRIu32 " (sample number)",
+                    p_track->i_track_ID, i_sample, i_alternative_sync_sample );
+            if( i_alternative_sync_sample > *pi_sync_sample &&
+                i_alternative_sync_sample < i_sample )
+                *pi_sync_sample = i_alternative_sync_sample;
+        }
     }
 
     return i_ret;

commit 691c9c51b1ef18f71e377e194d09151d0c98af65

demux: mp4: fix RAP fallback
no stss -> fail
empty stss -> look further

Also fixes sample 0 returns.
And regression from 07a2ad663da8631e904f22b0deee29de96a38bca

modules/demux/mp4/mp4.c
@@ -3456,19 +3456,25 @@ static int TrackGetNearestSeekPoint( demux_t *p_demux, mp4_track_t *p_track,
             }
         }
     }
-
-    if (*pi_sync_sample != i_sample)
+    else // no stss, any sample is sync point
     {
-        /* try or refine using RAP with recovery roll info */
-        uint32_t i_alternative_sync_sample = i_sample;
-        if( MP4_SampleToGroupInfo( p_track->p_stbl, i_sample, SAMPLEGROUP_rap,
-                                0, &i_alternative_sync_sample, true, NULL ) )
+        return VLC_EGENERIC;
+    }
+
+    if( i_ret == VLC_SUCCESS && *pi_sync_sample == i_sample )
+        return VLC_SUCCESS; // nothing to do
+
+    /* Empty but present stss, try or refine using RAP with recovery roll info */
+    uint32_t i_alternative_sync_sample = i_sample;
+    if( MP4_SampleToGroupInfo( p_track->p_stbl, i_sample, SAMPLEGROUP_rap,
+                               0, &i_alternative_sync_sample, true, NULL ) )
+    {
+        msg_Dbg( p_demux, "tk %u sbgp gives %d --> %" PRIu32 " (sample number)",
+                 p_track->i_track_ID, i_sample, i_alternative_sync_sample );
+        if( i_alternative_sync_sample > *pi_sync_sample || i_ret != VLC_SUCCESS )
         {
-            msg_Dbg( p_demux, "tk %u sbgp gives %d --> %" PRIu32 " (sample number)",
-                    p_track->i_track_ID, i_sample, i_alternative_sync_sample );
-            if( i_alternative_sync_sample > *pi_sync_sample &&
-                i_alternative_sync_sample < i_sample )
-                *pi_sync_sample = i_alternative_sync_sample;
+            *pi_sync_sample = i_alternative_sync_sample;
+            return VLC_SUCCESS;
         }
     }
 