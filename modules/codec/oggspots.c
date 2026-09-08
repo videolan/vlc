fe13e7d90a8d30c33e5f7305bcf4ee7d4eb8154a
Tristan Matthews <tmatth@videolan.org>
2026-09-08 08:16:10 -0400

    oggspots: check decoder_UpdateVideoFormat return
    Fixes CID 1374330

--- modules/codec/oggspots.c
+++ modules/codec/oggspots.c
@@ -388,7 +388,11 @@ static picture_t* DecodePacket(decoder_t* p_dec, block_t* p_block)
 
     p_pic->b_force = true;
     p_dec->fmt_out.i_codec = p_dec->fmt_out.video.i_chroma;
-    decoder_UpdateVideoFormat(p_dec);
+    if (decoder_UpdateVideoFormat(p_dec) != 0) {
+        msg_Dbg(p_dec, "Failed to update decoder format");
+        picture_Release(p_pic);
+        return NULL;
+    }
 
     return p_pic;
 
