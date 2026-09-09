videolan/vlc

commit cc79f1f98f89465385c595f572eee9be1ce80c03

modules: use vlc_tick_from_samples()

modules/codec/speex.c
@@ -1095,8 +1095,8 @@ static block_t *Encode( encoder_t *p_enc, block_t *p_aout_buf )
     int i_samples_delay = p_sys->i_samples_delay;
 
     vlc_tick_t i_pts = p_aout_buf->i_pts -
-                CLOCK_FREQ * (vlc_tick_t)p_sys->i_samples_delay /
-                (vlc_tick_t)p_enc->fmt_in.audio.i_rate;
+                vlc_tick_from_samples( p_sys->i_samples_delay,
+                            p_enc->fmt_in.audio.i_rate );
 
     p_sys->i_samples_delay += i_samples;
 
@@ -1154,9 +1154,9 @@ static block_t *Encode( encoder_t *p_enc, block_t *p_aout_buf )
         p_block = block_Alloc( i_out );
         memcpy( p_block->p_buffer, p_sys->p_buffer_out, i_out );
 
-        p_block->i_length = CLOCK_FREQ *
-            (vlc_tick_t)p_sys->i_frame_length * p_sys->header.frames_per_packet /
-            (vlc_tick_t)p_enc->fmt_in.audio.i_rate;
+        p_block->i_length = vlc_tick_from_samples(
+            p_sys->i_frame_length * p_sys->header.frames_per_packet,
+            p_enc->fmt_in.audio.i_rate );
 
         p_block->i_dts = p_block->i_pts = i_pts;
 

modules/audio_filter/resampler/speex.c
@@ -156,7 +156,7 @@ static block_t *Resample (filter_t *filter, block_t *in)
     out->i_buffer = olen * framesize;
     out->i_nb_samples = olen;
     out->i_pts = in->i_pts;
-    out->i_length = olen * CLOCK_FREQ / filter->fmt_out.audio.i_rate;
+    out->i_length = vlc_tick_from_samples(olen, filter->fmt_out.audio.i_rate);
 error:
     block_Release (in);
     return out;

commit 16e73392b77d308a537ac2ae54ecfa96723e39d2

speex: encoder: fix integer overflow
Fixes CID 1439603. Regression introduced in cc79f1f98f89465385c595f572eee9be1ce80c03

modules/codec/speex.c
@@ -1269,7 +1269,7 @@ static block_t *Encode( encoder_t *p_enc, block_t *p_aout_buf )
         speex_bits_reset( &p_sys->bits );
 
         p_block->i_length = vlc_tick_from_samples(
-            p_sys->i_frame_length * p_sys->header.frames_per_packet,
+            p_sys->i_frame_length * (vlc_tick_t)p_sys->header.frames_per_packet,
             p_enc->fmt_in.audio.i_rate );
 
         p_block->i_dts = p_block->i_pts = i_pts;