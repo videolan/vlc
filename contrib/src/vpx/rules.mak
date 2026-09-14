videolan/vlc

commit 3f287a4cf65f86379ec96023fe0c583c8774beb4

contrib: Fix Apple Clang specific version checks
The versions of Clang and Apple Clang can't be treated
the same, so add a version check specific to Apple Clang
and make the normal one always return false when checking
with Apple Clang.

This also makes the check for Xcode 11 for the -fno-stack-check
workaround more accurate, as it does not rely on the assumption
that Apple Clang is used when the host is Darwin, anymore.

(cherry picked from commit fb16e8db90aafb8bc67d0ae07511b9c7ddb31f50) (edited)
edited:
- VLC 3 used HAVE_MACOSX instead of HAVE_DARWIN_OS for the vpx check
Signed-off-by: Steve Lhomme <robux4@ycbcr.xyz>

contrib/src/vpx/rules.mak
@@ -155,12 +155,10 @@ VPX_CONF += --disable-runtime-cpu-detect
 endif
 VPX_LDFLAGS := -L$(IOS_SDK)/usr/lib -isysroot $(IOS_SDK) $(LDFLAGS)
 endif
-ifdef HAVE_MACOSX
 ifeq ($(ARCH),$(filter $(ARCH), arm aarch64))
-ifneq ($(call clang_at_least, 13), true)
-# arm_neon.h broken on clang 12
+ifneq ($(call apple_clang_at_least, 13), true)
+# arm_neon.h broken on Apple Clang 12
 VPX_CONF += --disable-neon-dotprod
-endif
 endif
 VPX_LDFLAGS := -L$(MACOSX_SDK)/usr/lib -isysroot $(MACOSX_SDK) -mmacosx-version-min=10.7
 endif

contrib/src/main.mak
@@ -168,12 +168,23 @@ EXTRA_CFLAGS += -DWINSTORECOMPAT
 EXTRA_LDFLAGS += -lwinstorecompat
 endif
 
+apple_clang_at_least = $(shell echo false)
+apple_clang_at_most  = $(shell echo false)
+apple_clang_major_is = $(shell echo false)
 clang_at_least = $(shell echo false)
 clang_at_most  = $(shell echo false)
 clang_major_is = $(shell echo false)
 gcc_at_least = $(shell echo false)
 gcc_at_most  = $(shell echo false)
 gcc_major_is = $(shell echo false)
+ifeq ($(shell $(CC) --version 2>/dev/null | grep -qi "Apple clang" || echo FAIL),)
+HAVE_APPLE_CLANG := 1
+HAVE_CLANG := 1
+CLANG_VERSION := $(shell $(CC) --version | head -1 | grep -o '[0-9]\+\.' | head -1 | cut -d '.' -f 1)
+apple_clang_at_least = $(shell [ $(CLANG_VERSION) -ge $(1) ] && echo true)
+apple_clang_at_most  = $(shell [ $(CLANG_VERSION) -le $(1) ] && echo true)
+apple_clang_major_is = $(shell [ $(CLANG_VERSION) -eq $(1) ] && echo true)
+else
 ifneq ($(findstring clang, $(shell $(CC) --version 2>/dev/null | grep -qi clang && echo "clang")),)
 HAVE_CLANG := 1
 CLANG_VERSION := $(shell $(CC) --version | head -1 | grep -o '[0-9]\+\.' | head -1 | cut -d '.' -f 1)
@@ -189,14 +200,13 @@ gcc_at_most  = $(shell [ $(GCC_VERSION) -le $(1) ] && echo true)
 gcc_major_is = $(shell [ $(GCC_VERSION) -eq $(1) ] && echo true)
 endif
 endif
+endif
 
 # -fno-stack-check is a workaround for a possible
 # bug in Xcode 11 or macOS 10.15+
-ifdef HAVE_DARWIN_OS
-ifeq ($(call clang_major_is, 11), true)
+ifeq ($(call apple_clang_major_is,11), true)
 EXTRA_CFLAGS += -fno-stack-check
 XCODE_FLAGS += OTHER_CFLAGS=-fno-stack-check
-endif
 endif
 
 cppcheck = $(shell printf '$(2)' | $(CC) $(CFLAGS) -E -dM - 2>/dev/null | grep -E $(1))

commit a4783e59c79d0d710e2320b2bea4028e2f017292

contrib: vpx: only use the macOS linker flags on macOS
This is a regression from 3f287a4cf6

contrib/src/vpx/rules.mak
@@ -160,6 +160,8 @@ ifneq ($(call apple_clang_at_least, 13), true)
 # arm_neon.h broken on Apple Clang 12
 VPX_CONF += --disable-neon-dotprod
 endif
+endif
+ifdef HAVE_MACOSX
 VPX_LDFLAGS := -L$(MACOSX_SDK)/usr/lib -isysroot $(MACOSX_SDK) -mmacosx-version-min=10.7
 endif
 VPX_LDFLAGS += -arch $(PLATFORM_SHORT_ARCH)