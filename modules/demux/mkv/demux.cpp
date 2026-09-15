0d4eda28f33f15c06a5d5f0102529c4864cc95f0
Steve Lhomme <robux4@ycbcr.xyz>
2026-09-15 11:18:56 +0200

    demux: mkv: fix pci_t button read order
    It does not match dvdread order after 0916735803bfc09634effd87579f9f61fe744056.
    
    Fixes #30127
    
    (cherry picked from commit 13343ab0e98e64518c8cf7321af29923ecae02b1)

--- modules/demux/mkv/demux.cpp
+++ modules/demux/mkv/demux.cpp
@@ -134,13 +134,13 @@ void event_thread_t::SetPci(const uint8_t *data, size_t size)
         bs_skip(&bs, 2);
         btn.x_end    = bs_read(&bs, 10);
 
-        bs_skip(&bs, 2);
-        btn.up = bs_read(&bs, 6);
-
         btn.auto_action_mode = bs_read(&bs, 2);
         btn.y_start = bs_read(&bs, 10);
         bs_skip(&bs, 2);
         btn.y_end   = bs_read(&bs, 10);
+
+        bs_skip(&bs, 2);
+        btn.up = bs_read(&bs, 6);
 
         bs_skip(&bs, 2);
         btn.down    = bs_read(&bs, 6);
