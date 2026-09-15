42796f919b76fa263eb8e7b7231766320d85ab95
Thomas Guillem <thomas@gllm.fr>
2026-09-15 17:52:26 +0200

    update: 2026 release key
    pub   rsa4096/1D77C0AE835B911E 2026-09-14 [expires: 2036-09-11]
          Key fingerprint = A341 FD76 84DB C1FF D69B  C25A 1D77 C0AE 835B 911E
    uid                  VideoLAN Release Signing Key 2026 <videolan@videolan.org>
    
    (cherry picked from commit 68bd59146e5c2d5bfc1897a2e2bd61ca5c814403)

--- include/vlc_pgpkey.h
+++ include/vlc_pgpkey.h
@@ -5,38 +5,48 @@
 /* We trust this public key, and by extension, also keys signed by it. */
 
 /* NOTE:
- * We need a 1024 bits DSA key.
+ * The embedded key is an OpenPGP v4 RSA-4096 primary key.
  * Don't forget to upload the key to http://download.videolan.org/pub/keys/
  */
 
 /*
- * VideoLAN Release Signing Key (2013)
- * expires on 2014-02-03
+ * VideoLAN Release Signing Key (2026)
+ * created on 2026-09-14
  */
 
 static const uint8_t videolan_public_key_longid[8] = {
-  0x71, 0x80, 0x71, 0x3B, 0xE5, 0x8D, 0x1A, 0xDC
+    0x1D, 0x77, 0xC0, 0xAE, 0x83, 0x5B, 0x91, 0x1E
 };
 
 /* gpg --export --armor "<id>"|sed -e s/^/\"/ -e s/\$/\\\\n\"/ */
 static const uint8_t videolan_public_key[] = {
-  "-----BEGIN PGP PUBLIC KEY BLOCK-----\n"
-  "Version: GnuPG v2.0.19 (GNU/Linux)\n"
-  "\n"
-  "mQGiBFD9w2QRBACoEzH9KKirWE4wgiuPPynNnxks+p+t5i1z3CG+1XhagmTHoOf3\n"
-  "v8i19kKHV6WnVMn2CKJFgwTTLYXOJTrBM/4ABVtu11cHeeueeo+pCSkdoLzYJ5QF\n"
-  "HbByB6j33QUbwKF0frEs+ge4LxzvYyCDAmNAW560QtOAR9Lk1Fo5B1GXzwCg1kDk\n"
-  "RkSe7EOZNm1U2rYAQ2VPrfsEAIHr4ooOyUByPR7XpoDOKoaXEG0hjpgh46lbgse+\n"
-  "dQx8YrxS9vXQLwYokfWLrs55avx9Ys0iVv2TMv7X4Tn5sTVaK5K+NbKhxhLORxGI\n"
-  "sgKqRn7W5SG5xoO0w/dmQj756ppjITGbxjFuhYE0X5S6NeMhUuFci7sJ42R7F1Ko\n"
-  "6sYuA/wOMUxCk4XOXeQF16ApyyenjE/UWbBNEhBmjEsZkYAFNc89pAEnEFSnIxK8\n"
-  "fcuCQioM6ojjaW+aEs/q3/klI0nat9LMLhNSCebjriMHwJDU70NeCn4nPWsfItT1\n"
-  "eKvbHNcX+3bq3D/i2Wa3PZ5YFFF01C61dHmVC9YGh4sAOXO09LQjVmlkZW9MQU4g\n"
-  "UmVsZWFzZSBTaWduaW5nIEtleSAoMjAxMymIaAQTEQIAKAUCUP3DZAIbAwUJAfJX\n"
-  "AAYLCQgHAwIGFQgCCQoLBBYCAwECHgECF4AACgkQcYBxO+WNGtwQQwCg1JX6dDn5\n"
-  "gMpV4oczkpwlj5noOQwAn0HdTOxfmefXNQF1x+Gt9BXBYXrNiEYEEBECAAYFAlD9\n"
-  "xH4ACgkQp0FUn1ntKYdB+gCfS641cDBN2rOKf/+Fra/p3bXgAeAAn2sJtSdN07Dv\n"
-  "rZeDWEbkhT620YOS\n"
-  "=Npzf\n"
-  "-----END PGP PUBLIC KEY BLOCK-----\n"
+    "-----BEGIN PGP PUBLIC KEY BLOCK-----\n"
+    "\n"
+    "mQINBGqngYUBEADETQiomfQGsYJXlYynUZ0hrzgH5YlYq1qhXAIRrhcupN2eNUPl\n"
+    "+Yx0usbWhe4+a0jizrK1Gukc8rtVq7MpgapQSfj/39Q/rjzoR/UDcE//WNAs1DhO\n"
+    "A7TNkWeDCwINjKOKhypHMuqMxAo6Ax35VDc2rHW3Q0XPJJF9Pxl+SyRz4gCR43a4\n"
+    "ZZZHRveJElC8aq+hyu0rirdA5VpTc4Ha2avbgBKI3cu7xUvuRH4i4yF3dJH+C4Qn\n"
+    "Gf/4XqsI26FfT9OgcEh4lfhUQLAucJwXwPtfTsv6+QmwU+JyzMhfy8mYc46r3OSX\n"
+    "DHr+jU39cBWMChDgNSPiTUqFU+d7fUVtUgold6orV8iB9NhqpP4yQ+b0UszDqyak\n"
+    "rZsOgxedWbL6B67D804FgUUWyHjG8oNYahgLhLGDymTtmw4J7J3GE5eQV1u0K3sL\n"
+    "6NaXZAnT44Knhq/iaA2rmgGwH1a58p3v7Q33n8tCcu0DI//8ytiWmJdTPooXVOCD\n"
+    "txJsYht4HARQR+GiKPBl25p4Nolh4xCX3PtVJKeHWYOglEs8BCCLNcrUfky/zExa\n"
+    "rT6eE/oXQ++kUC30XiW8oRquCkCfVsRr+gWuRQLbXFhDGbLdeDWuL2Z0dam/L+lr\n"
+    "kJbel8m4BwMLZhrKskLiyGmLfbX72LfvrM917OOd4SKIZP4whlxfb/KJTwARAQAB\n"
+    "tDlWaWRlb0xBTiBSZWxlYXNlIFNpZ25pbmcgS2V5IDIwMjYgPHZpZGVvbGFuQHZp\n"
+    "ZGVvbGFuLm9yZz6JAlcEEwEKAEECGwMFCRLMAwAFCwkIBwIGFQoJCAsCBBYCAwEC\n"
+    "HgECF4AWIQSjQf12hNvB/9abwlodd8Cug1uRHgUCaqkwaQIZAQAKCRAdd8Cug1uR\n"
+    "HvOKD/wIHt7eerEmEda5tevwgrVRc3SpfIcNQXPVw3F66sX0YseToFz/ov8FsrK2\n"
+    "WKiM4EGKsvrzySspUyvkukCUJ+J6A5ZjaFPGdFu6DXkx7opxIaSSgMESkxUgT6/F\n"
+    "c0cVprnpnXU0hugjVQFpkvFE2+wivAiyNliOOWaGiCAl0adrM7k0Q+BJZ38LIgYR\n"
+    "ZYRD7g7zjWRHBvosboPGLsmjPvz3SmG/h+dgPgK1ABvHDRUgTfsoRCDfrmhCYpsv\n"
+    "lxAo+070OIiI6yyi1+lVKdtdqidzsjsel+L9InvsEvmvPj7b0eeMwx4niXOWqLHM\n"
+    "9KFG8NLIZ9kCr3XtQ8CA2WYqNzjlvJhetcOzOE+ZdNsbvWMKrRVhYCxWV9+7Ez8u\n"
+    "IS35xcDhWyOdxmYLzfzxHDA0QOOQC+BJGVdUlnOyzA2XcZ0/0Ajqxl7ToPcvR3ga\n"
+    "+tGqywHjetATbQBfZg8Sro2AxJtq/UKfjTAi9MeAER52Pz7SuTVNvMkjSjlTpdgd\n"
+    "1pCFQh8afEJ0ZZAKo63GxE4PtmUCuGH04zr1xD91sQHTLgjJOqL+2gVdzs+Li2xZ\n"
+    "JXim5XH2g3tcQb2bCdNSp78b216nqa9S9DfQsIHez2dp+cJ+vot6bKglD/sqsPNm\n"
+    "gtlZ8hRR13f++XgRyJW8CWEqOuyU5thd1QKpaQkjhJQwogBMTw==\n"
+    "=e7qD\n"
+    "-----END PGP PUBLIC KEY BLOCK-----\n"
 };
