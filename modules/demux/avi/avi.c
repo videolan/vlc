videolan/vlc

commit 08e0999642c6435ce39a94d56969e3b4814801ee

demux: avi: fix potential leak reparsing for index
refs #29225

modules/demux/avi/avi.c
@@ -3034,6 +3034,7 @@ static void AVI_ExtractSubtitle( demux_t *p_demux,
 
     p_indx = AVI_ChunkFind( p_strl, AVIFOURCC_indx, 0, false );
     avi_chunk_t ck;
+    AVI_ChunkInit( &ck );
     int64_t  i_position;
     unsigned i_size;
     if( p_indx )
@@ -3045,6 +3046,7 @@ static void AVI_ExtractSubtitle( demux_t *p_demux,
                 AVI_ChunkRead( p_demux->s, &ck, NULL  ) ||
                 ck.common.i_chunk_fourcc != AVIFOURCC_indx )
                 goto exit;
+
             p_indx = &ck.indx;
         }
 
@@ -3147,8 +3149,7 @@ exit:
     else
         msg_Warn( p_demux, "Failed to load an embedded subtitle" );
 
-    if( p_indx == &ck.indx )
-        AVI_ChunkClean( p_demux->s, &ck );
+    AVI_ChunkClean( p_demux->s, &ck );
 }
 
 static avi_track_t * AVI_GetVideoTrackForXsub( demux_sys_t *p_sys )

commit 5239242cef9641b468944f6dc423aa7fa24112ed

demux: avi: fix cleanup crash due to jump over init
The regression by 08e0999642c6435ce39a94d56969e3b4814801ee
removed a check that was in fact concealing the existing issue.

fixes #30125

(cherry picked from commit d8857b991db5ade9ed6b3a1436f6f0d121f406bd)

modules/demux/avi/avi.c
@@ -3105,14 +3105,15 @@ static void AVI_ExtractSubtitle( demux_t *p_demux,
     char *psz_description = NULL;
     avi_chunk_indx_t *p_indx = NULL;
 
-    if( !p_sys->b_seekable )
-        goto exit;
-
-    p_indx = AVI_ChunkFind( p_strl, AVIFOURCC_indx, 0, false );
     avi_chunk_t ck;
     AVI_ChunkInit( &ck );
     int64_t  i_position;
     unsigned i_size;
+
+    if( !p_sys->b_seekable )
+        goto exit;
+
+    p_indx = AVI_ChunkFind( p_strl, AVIFOURCC_indx, 0, false );
     if( p_indx )
     {
         if( p_indx->i_indextype == AVI_INDEX_OF_INDEXES &&