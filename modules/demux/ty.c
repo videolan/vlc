videolan/vlc

commit 04b62205bcadd7d1ebb126d278d9e6cafebe917f

demux: add check to verify sequence table size to prevent oom
(cherry picked from commit 7acd6b4d267e8cec330eca594d034552ff80d728)
Signed-off-by: Thomas Guillem <thomas@gllm.fr>

modules/demux/ty.c
@@ -1663,6 +1663,9 @@ static int parse_master(demux_t *p_demux)
     uint32_t i_map_size = U32_AT(&mst_buf[20]);  /* size of bitmask, in bytes */
     uint32_t i = U32_AT(&mst_buf[28]);   /* size of SEQ table, in bytes */
 
+    if( i_save_pos + 32 + i > p_sys->i_stream_size )
+        return VLC_EGENERIC;
+
     if(i_map_size > UINT32_MAX / 8)
         return VLC_EGENERIC;
 

commit ce1df66d25b2ac195ce4d07ffcc618cf0a9eeda5

demux: ty: fix regression with streams
regression by 04b62205bca

(cherry picked from commit 4b1b33d020d41958ae838bcc9e82e83191ac8287)

modules/demux/ty.c
@@ -1671,7 +1671,8 @@ static int parse_master(demux_t *p_demux)
     uint32_t i_map_size = U32_AT(&mst_buf[20]);  /* size of bitmask, in bytes */
     uint32_t i = U32_AT(&mst_buf[28]);   /* size of SEQ table, in bytes */
 
-    if( i_save_pos + 32 + i > p_sys->i_stream_size )
+    /* Check seek index size, if any */
+    if( p_sys->i_stream_size && (i_save_pos + 32 + i > p_sys->i_stream_size) )
         return VLC_EGENERIC;
 
     if(i_map_size > UINT32_MAX / 8)