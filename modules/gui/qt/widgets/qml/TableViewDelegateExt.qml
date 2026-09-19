videolan/vlc

commit 9dcc03a34a3b3d43f525da6525db083d08128560

qml: use incubator with mode `AsynchronousIfNested` instead of `createObject()` in delegates
As noted in the docs, `AsynchronousIfNested` is explained to be [1]:

> The creation will occur asynchronously if part of a nested
> asynchronous creation, or synchronously if not.

For that reason, we should not be using `createObject()` in view
delegates since if the delegate is being instantiated asynchronously,
such custom object should also be instantiated asynchronously.

[1] https://doc.qt.io/qt-6/qqmlincubator.html#details

modules/gui/qt/widgets/qml/TableViewDelegateExt.qml
@@ -251,15 +251,26 @@ T.Control {
 
                 Component.onCompleted: {
                     const del = modelData.model.colDelegate || delegate.defaultDelegate
-                    item = del.createObject(loader, {
+                    // https://doc.qt.io/qt-6/qqmlincubator.html#details
+                    const incubator = del.incubateObject(loader, {
                             cellModel: cellModel,
                             width: Qt.binding(() => loader.width),
                             height: Qt.binding(() => loader.height),
-                        }
+                        }, 1 /* QQmlIncubator::AsynchronousIfNested */
                     )
-                    if (item.artworkTextureProvider) {
-                        delegate.artworkTextureProvider = Qt.binding(() => item.artworkTextureProvider)
+
+                    // https://doc.qt.io/qt-6/qml-qtqml-component.html#incubateObject-method
+                    if (incubator.status !== Component.Ready) {
+                        incubator.onStatusChanged = function(status) {
+                            if (status === Component.Ready) {
+                                loader.item = incubator.object
+                            }
+                        }
+                    } else {
+                        loader.item = incubator.object
                     }
+
+                    delegate.artworkTextureProvider = Qt.binding(() => loader?.item?.artworkTextureProvider ?? null)
                 }
                 Component.onDestruction: {
                     item?.destroy()

modules/gui/qt/widgets/qml/MediaCover.qml
@@ -202,10 +202,30 @@ Item {
 
     property Widgets.PlayCover _playCoverItem
 
+    property var _playCoverItemIncubator
+
     onPlayCoverShowPlayChanged: {
         // NOTE: We are lazy loading the component when this gets visible and it stays loaded.
         //       We could consider unloading it when visible goes to false.
-        if (playCoverShowPlay && !_playCoverItem)
-            _playCoverItem = playCoverComponent.createObject(root)
+        if (playCoverShowPlay && !_playCoverItem && !_playCoverItemIncubator) {
+            // https://doc.qt.io/qt-6/qqmlincubator.html#details
+            const incubator = playCoverComponent.incubateObject(root, {}, 1 /* QQmlIncubator::AsynchronousIfNested */)
+            _playCoverItemIncubator = incubator
+
+            // https://doc.qt.io/qt-6/qml-qtqml-component.html#incubateObject-method
+            if (incubator.status !== Component.Ready) {
+                incubator.onStatusChanged = function(status) {
+                    if (status === Component.Ready) {
+                        root._playCoverItem = incubator.object
+                        // Incubator should be garbage collected, we don't need it anymore:
+                        root._playCoverItemIncubator = null
+                    }
+                }
+            } else {
+                root._playCoverItem = incubator.object
+                // Incubator should be garbage collected, we don't need it anymore:
+                root._playCoverItemIncubator = null
+            }
+        }
     }
 }

commit d9c22e06487e13c070912b3dcbfe4fe5ef8ffb6f

qml: fix `artworkTextureProvider` may no longer be provided in `TableViewDelegateExt`
This fixes texture reusing no longer being applicable, such as when dragging the
delegate.

Regression since 9dcc03a3.

modules/gui/qt/widgets/qml/TableViewDelegateExt.qml
@@ -229,6 +229,16 @@ T.Control {
                 id: loader
                 required property var modelData
                 property TableRowDelegate item: null
+
+                onItemChanged: {
+                    if (item) {
+                        // `TableViewDelegateExt` uses the last sub-delegate that declares property `artworkTextureProvider`:
+                        if (item.artworkTextureProvider !== undefined) {
+                            delegate.artworkTextureProvider = Qt.binding(() => loader.item?.artworkTextureProvider ?? null)
+                        }
+                    }
+                }
+
                 width: {
                     if (!!modelData.size)
                         return modelData.size * delegate.fixedColumnWidth
@@ -271,8 +281,6 @@ T.Control {
                     } else {
                         loader.item = incubator.object
                     }
-
-                    delegate.artworkTextureProvider = Qt.binding(() => loader?.item?.artworkTextureProvider ?? null)
                 }
                 Component.onDestruction: {
                     item?.destroy()