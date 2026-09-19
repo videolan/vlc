videolan/vlc

commit 84486e2fb2eb0b2e866e879c3501505430df8045

qt: dcomp: disable redirection surface in `PlaylistDetachedWindow`

modules/gui/qt/playlist/qml/PlaylistDetachedWindow.qml
@@ -51,6 +51,14 @@ Window {
         MainCtx.playqueuePanel.visible = false
     }
 
+    Component.onCompleted: {
+        if (MainCtx.createWindowWithoutRedirectionSurface) { // Win32
+            const ret = MainCtx.createWindowWithoutRedirectionSurface(this)
+            if (!ret)
+                console.debug("MainCtx::createWindowWithoutRedirectionSurface(): returned false for window", this)
+        }
+    }
+
     PlaylistPane {
         id: playlistView
 

commit 5a7fd5b4fd8c0343963f09c517ba7c2549c0c802

qml: fix assertion failure when opening detached playlist window on Windows
The bindings are evaluated before `Component.onCompleted` is executed, since
we have `visible: MainCtx.playqueuePanel.visible`, the window would create
the platform window before `MainCtx.createWindowWithoutRedirectionSurface()`
is called, which is forbidden because it needs to be called before the platform
window is created (before `CreateWindowExW()` is called by the platform window).

I have checked if `transientParent` would cause the same, but it is not the
case. Nevertheless, I confirmed that the change here is enough on Windows 8.1.

Delaying the visible binding up to component completion is intentionally
platform agnostic, since it provides benefit of resolving properties that
are meant to be set before showing the window (such as `width`), at the
expense of negligible delay in being visible.

Regression since 84486e2f.

modules/gui/qt/playlist/qml/PlaylistDetachedWindow.qml
@@ -25,7 +25,7 @@ import VLC.Style
 
 
 Window {
-    visible: MainCtx.playqueuePanel.visible
+    visible: _componentCompleted && MainCtx.playqueuePanel.visible
 
     transientParent: MainCtx.intfMainWindow
 
@@ -36,6 +36,8 @@ Window {
 
     title: qsTr("Play Queue")
     color: theme.bg.primary
+
+    property bool _componentCompleted: false
 
     onVisibleChanged: {
         if (visible) {
@@ -57,6 +59,8 @@ Window {
             if (!ret)
                 console.debug("MainCtx::createWindowWithoutRedirectionSurface(): returned false for window", this)
         }
+
+        _componentCompleted = true
     }
 
     PlaylistPane {